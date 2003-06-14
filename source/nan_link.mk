5e0ec8c0579cc947e77bc312cb74f39a980f8939
Chris Want <cwant@ualberta.ca>
2003-06-14 22:47:54 +0000

    This makes the new renderwin.c behavior work for windows-gcc.

--- source/nan_link.mk
+++ source/nan_link.mk
@@ -129,7 +129,7 @@ ifeq ($(OS),windows)
 		MINGWLIB = /usr/lib/w32api
 		LDFLAGS += -mwindows -mno-cygwin -mconsole
 		DADD += -L/usr/lib/w32api -lnetapi32 -lopengl32 -lglu32
-		DADD += -L/usr/lib/w32api 
+		DADD += -L/usr/lib/w32api -lwinmm
     else
 	    DADD = kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib
 		DADD += advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib
