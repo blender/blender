ae68b653b56d3eab44d20afeb31cf43db3bf3ec0
Stephen Swaney <sswaney@centurytel.net>
2003-10-29 23:03:30 +0000

    shell script to generate blender python api docs using epydoc

---
+++ source/blender/python/api2_2x/doc/epy_docgen.sh
@@ -0,0 +1,9 @@
+# epy_docgen.sh
+# generates blender python doc using epydoc
+# requires epydoc in your PATH.
+# run from the doc directory containing the .py files
+# usage:  sh epy_docgen.sh
+
+epydoc -o BPY_API_230 --url "http://www.blender.org" -t Blender.py \
+ -n "Blender" --no-private --no-frames \
+$( ls [A-Z]*.py )
