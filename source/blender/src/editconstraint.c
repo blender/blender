b43e74a1db8d2f448395d5b4656d1c53cd86f9ca
Martin Poirier <theeth@yahoo.com>
2003-11-17 02:25:33 +0000

    Fixed the fix for constraint loop :P
    Everything works properly now

--- source/blender/src/editconstraint.c
+++ source/blender/src/editconstraint.c
@@ -501,9 +501,10 @@ static short detect_constraint_loop (Object *owner, const char* substring, int d
 								//	return 1;
 							}
 						}
-						if (typefrom == CONSTRAINT_TYPE_TRACKTO && typefrom == CONSTRAINT_TYPE_LOCKTRACK){
+						else {
 							curcon->flag |= CONSTRAINT_NOREFRESH;
 						}
+
 						if (detect_constraint_loop (data->tar, data->subtarget, disable, CONSTRAINT_TYPE_TRACKTO)){
 							curcon->flag |= CONSTRAINT_DISABLE;
 							result = 1;
@@ -541,9 +542,10 @@ static short detect_constraint_loop (Object *owner, const char* substring, int d
 								//		return 1;
 							}
 						}
-						if (typefrom == CONSTRAINT_TYPE_TRACKTO && typefrom == CONSTRAINT_TYPE_LOCKTRACK){
+						else {
 							curcon->flag |= CONSTRAINT_NOREFRESH;
 						}
+
 						if (detect_constraint_loop (data->tar, data->subtarget, disable, CONSTRAINT_TYPE_LOCKTRACK)){
 							curcon->flag |= CONSTRAINT_DISABLE;
 							result = 1;
