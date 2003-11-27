26ffe6ef93a857e888045e43abde89af8a228cb6
Ton Roosendaal <ton@blender.org>
2003-11-27 23:34:52 +0000

    - while i am committing: small button aligning arror for 'separate textures'   in F5 menu.

--- source/blender/src/buttons_shading.c
+++ source/blender/src/buttons_shading.c
@@ -2160,7 +2160,7 @@ static void material_panel_texture(Material *ma)
 		if(mtex && mtex->tex) {
 			if(ma->septex & (1<<a)) 
 				uiDefButC(block, TOG|BIT|a, B_MATPRV_DRAW, " ",	-20, 180-20*a, 28, 20, &ma->septex, 0.0, 0.0, 0, 0, "Disable or enable this channel");
-			else uiDefIconButC(block, TOG|BIT|a, B_MATPRV_DRAW, ICON_CHECKBOX_HLT,	-20, 180-22*a, 28, 20, &ma->septex, 0.0, 0.0, 0, 0, "Disable or enable this channel");
+			else uiDefIconButC(block, TOG|BIT|a, B_MATPRV_DRAW, ICON_CHECKBOX_HLT,	-20, 180-20*a, 28, 20, &ma->septex, 0.0, 0.0, 0, 0, "Disable or enable this channel");
 		}
 	}
 	
