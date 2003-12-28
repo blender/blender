50448cdb1a37481b4093866bfb85b19e8bccf5b9
Martin Poirier <theeth@yahoo.com>
2003-12-28 20:24:32 +0000

    Function without a return (warning)
    This function just calls another function which returns a success value, so I'm just passing that value directly as a return value.
    
    Slap me with a trout if I wasn't suppose to fix this.

--- intern/ghost/intern/GHOST_WindowWin32.cpp
+++ intern/ghost/intern/GHOST_WindowWin32.cpp
@@ -581,9 +581,8 @@ static GHOST_TUns16 uns16ReverseBits(GHOST_TUns16 shrt)
 GHOST_TSuccess GHOST_WindowWin32::setWindowCustomCursorShape(GHOST_TUns8 bitmap[16][2], 
 					GHOST_TUns8 mask[16][2], int hotX, int hotY)
 {
-	setWindowCustomCursorShape((GHOST_TUns8*)bitmap, (GHOST_TUns8*)mask, 
+	return setWindowCustomCursorShape((GHOST_TUns8*)bitmap, (GHOST_TUns8*)mask, 
 									16, 16, hotX, hotY, 0, 1);
-
 }
 
 GHOST_TSuccess GHOST_WindowWin32::setWindowCustomCursorShape(GHOST_TUns8 *bitmap, 
