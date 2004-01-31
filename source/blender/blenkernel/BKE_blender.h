f5e85cc53881ce078385832fd214b44e7c7b9f8d
Ton Roosendaal <ton@blender.org>
2004-01-31 13:00:31 +0000

    - version upped to 2.32   (warn: still waiting for splash!)

--- source/blender/blenkernel/BKE_blender.h
+++ source/blender/blenkernel/BKE_blender.h
@@ -42,7 +42,7 @@ extern "C" {
 
 struct ListBase;
 
-#define BLENDER_VERSION		231
+#define BLENDER_VERSION		232
 
 int	BKE_read_file(char *dir, void *type_r);
 int BKE_read_file_from_memory(char* filebuf, int filelength, void *type_r);
