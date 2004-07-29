3b29759bbfceb40a18e7a606dd2731f117cf1ffc
Matt Ebb <matt@mke3.net>
2004-07-29 12:13:35 +0000

    And the necessary scripts.

---
+++ release/scripts/help_web_usercomm.py
@@ -0,0 +1,33 @@
+#!BPY
+
+"""
+Name: 'User Community'
+Blender: 234
+Group: 'HelpWebsites'
+Tooltip: 'Get involved with other Blender users'
+"""
+
+# --------------------------------------------------------------------------
+# Blender Website Help Menu -> Websites Item
+# --------------------------------------------------------------------------
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+# --------------------------------------------------------------------------
+
+import webbrowser
+webbrowser.open('http://www.blender3d.org/Community')
