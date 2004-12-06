74256b8b7d7a087e45e8ad031d43904fb23ecef5
Ton Roosendaal <ton@blender.org>
2004-12-06 09:57:01 +0000

    Bug fix #1972
    AKEY didn't toggle open/close outliner. It'll be back temporal, until
    definitely defined what to do with selections in it.

--- source/blender/src/space.c
+++ source/blender/src/space.c
@@ -3823,7 +3823,7 @@ static void winqreadoopsspace(ScrArea *sa, void *spacedata, BWinEvent *evt)
 			break;
 			
 		case AKEY:
-			// outliner_toggle_selected(sa);
+			outliner_toggle_visible(sa);
 			break;
 		case WKEY:
 			// outliner_operation_menu(sa);
