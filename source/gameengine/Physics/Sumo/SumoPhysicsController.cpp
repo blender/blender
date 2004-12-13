551806bce89c142ac731651637dc9f240d9c2302
Kester Maddock <Christopher.Maddock.1@uni.massey.ac.nz>
2004-12-13 09:53:30 +0000

    Set the physics scale on added objects. (Fix bug #1837)

--- source/gameengine/Physics/Sumo/SumoPhysicsController.cpp
+++ source/gameengine/Physics/Sumo/SumoPhysicsController.cpp
@@ -174,7 +174,8 @@ void		SumoPhysicsController::setPosition(float posX,float posY,float posZ)
 
 void		SumoPhysicsController::setScaling(float scaleX,float scaleY,float scaleZ)
 {
-	m_sumoObj->setScaling(MT_Vector3(scaleX,scaleY,scaleZ));
+	if (!m_bDyna)
+		m_sumoObj->setScaling(MT_Vector3(scaleX,scaleY,scaleZ));
 }
 	
 	// physics methods
