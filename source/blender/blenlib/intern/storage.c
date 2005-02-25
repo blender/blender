e13933bc7ca3879946df2a079a5fe47b0b217ae2
Kent Mein <mein@cs.umn.edu>
2005-02-25 14:50:57 +0000

    Added include sys/vfs.h for hpux  was in the patches tracker.... and cleaned up the include for dirent.h
    Kent

--- source/blender/blenlib/intern/storage.c
+++ source/blender/blenlib/intern/storage.c
@@ -36,12 +36,7 @@
 #include <stdio.h>
 #include <stdlib.h>	
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #ifdef WIN32
-
 #include <windows.h>
 #include "BLI_winstuff.h"
 #include <sys/types.h>
@@ -65,7 +60,7 @@
 #include <sys/mount.h>
 #endif
 
-#if defined(linux) || defined(__CYGWIN32__)
+#if defined(linux) || defined(__CYGWIN32__) || defined(__hpux)
 #include <sys/vfs.h>
 #endif
 
