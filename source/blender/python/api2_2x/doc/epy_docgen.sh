7e816642871ad5259330af59f816696500ff66f1
Stephen Swaney <sswaney@centurytel.net>
2005-06-12 16:20:29 +0000

    remove version number for doc directory name as discussed at today's meeting. Was BPY_API_237, now BPY_API

--- source/blender/python/api2_2x/doc/epy_docgen.sh
+++ source/blender/python/api2_2x/doc/epy_docgen.sh
@@ -4,6 +4,6 @@
 # run from the doc directory containing the .py files
 # usage:  sh epy_docgen.sh
 
-epydoc -o BPY_API_237 --url "http://www.blender.org" -t API_intro.py \
+epydoc -o BPY_API --url "http://www.blender.org" -t API_intro.py \
  -n "Blender" --no-private --no-frames \
 $( ls [A-Z]*.py )
