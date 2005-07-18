28057fa2f634856cd97d3440e82067f4c4585bc5
Daniel Dunbar <daniel@zuster.org>
2005-07-18 20:03:52 +0000

    - yet another warning fix

--- source/blender/renderconverter/intern/convertBlenderScene.c
+++ source/blender/renderconverter/intern/convertBlenderScene.c
@@ -1305,7 +1305,7 @@ static float *get_mesh_orco(Object *ob)
 }
 static void free_mesh_orco_hash(void) 
 {
-	BLI_ghash_free(g_orco_hash, NULL, MEM_freeN);
+	BLI_ghash_free(g_orco_hash, NULL, (GHashValFreeFP)MEM_freeN);
 	g_orco_hash = NULL;
 }
 
