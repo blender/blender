3181991e86393070d13b4dc4826e64108fc824c2
Daniel Dunbar <daniel@zuster.org>
2005-07-19 01:12:14 +0000

    - silly bug, didn't test for hash before freeing

--- source/blender/renderconverter/intern/convertBlenderScene.c
+++ source/blender/renderconverter/intern/convertBlenderScene.c
@@ -1298,8 +1298,10 @@ static float *get_mesh_orco(Object *ob)
 }
 static void free_mesh_orco_hash(void) 
 {
-	BLI_ghash_free(g_orco_hash, NULL, (GHashValFreeFP)MEM_freeN);
-	g_orco_hash = NULL;
+	if (g_orco_hash) {
+		BLI_ghash_free(g_orco_hash, NULL, (GHashValFreeFP)MEM_freeN);
+		g_orco_hash = NULL;
+	}
 }
 
 static void init_render_mesh(Object *ob)
