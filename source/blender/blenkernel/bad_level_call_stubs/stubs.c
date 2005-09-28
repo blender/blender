b1755b86bd2d6e9e4426899119cf3b534e83acf1
Nils Thuerey <nils@thuerey.de>
2005-09-28 18:09:46 +0000

    - added stub for elbeemDebugOut in   source/blender/blenkernel/bad_level_call_stubs/stubs.c 	for read/write bobj debug output

--- source/blender/blenkernel/bad_level_call_stubs/stubs.c
+++ source/blender/blenkernel/bad_level_call_stubs/stubs.c
@@ -227,3 +227,8 @@ void bglVertex3f(float x, float y, float z) {}
 void bglEnd(void) {}
 
 struct DispListMesh *NewBooleanMeshDLM(struct Object *ob, struct Object *ob_select, int int_op_type) { return 0; }
+
+// bobj read/write debug messages
+void elbeemDebugOut(char *msg) {}
+
+
