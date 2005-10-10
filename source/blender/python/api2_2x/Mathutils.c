28e585b05813288f742c342a00312b97caae3ff6
Joseph Gilbert <ascotan@gmail.com>
2005-10-10 19:50:53 +0000

    -Another fix for mathutils.anglebetweenvecs() * test dot before clamping

--- source/blender/python/api2_2x/Mathutils.c
+++ source/blender/python/api2_2x/Mathutils.c
@@ -485,10 +485,12 @@ PyObject *M_Mathutils_AngleBetweenVecs(PyObject * self, PyObject * args)
 		dot += vec1->vec[x] * vec2->vec[x];
 	}
 	dot /= (sqrt(test_v1) * sqrt(test_v2));
-	CLAMP(dot,-1.0f,1.0f);
+
+	if (dot < -1.0f || dot > 1.0f) 
+		CLAMP(dot,-1.0f,1.0f);
 	angleRads = (double)acos(dot);
 
-	return PyFloat_FromDouble(angleRads * (180 / Py_PI));
+	return PyFloat_FromDouble(angleRads * (180/ Py_PI));
 
 AttributeError1:
 	return EXPP_ReturnPyObjError(PyExc_AttributeError, 
