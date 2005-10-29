a0c17879574796921fa4b42164cee5246b722b35
Martin Poirier <theeth@yahoo.com>
2005-10-29 15:24:24 +0000

    Mirror cliping polishing: 	- Better use of tolerance to check for elements already on seam 	- remove debug printfs

--- source/blender/src/transform_generics.c
+++ source/blender/src/transform_generics.c
@@ -157,18 +157,15 @@ static void clipMirrorModifier(TransInfo *t, Object *ob)
 			if (td->flag & TD_NOACTION)
 				break;
 			
-			printf("iloc (%f, %f, %f)\n", td->iloc[0], td->iloc[1], td->iloc[2]);
-			printvecf("loc", td->loc);
-			printf("(%i, %i)\n", td->iloc[0] == 0.0f?1:0, td->iloc[0] == -0.0f?1:0);
 			if(axis & 1) {
-				if(td->loc[0]*td->iloc[0]<tolerance[0]) td->loc[0]= 0.0f;
+				if(abs(td->iloc[0])<=tolerance[0] || td->loc[0]*td->iloc[0]<0.0f) td->loc[0]= 0.0f;
 			}
 			
 			if(axis & 2) {
-				if(td->loc[1]*td->iloc[1]<tolerance[1]) td->loc[1]= 0.0f;
+				if(abs(td->iloc[1])<=tolerance[1] || td->loc[1]*td->iloc[1]<0.0f) td->loc[1]= 0.0f;
 			}
 			if(axis & 4) {
-				if(td->loc[2]*td->iloc[2]<=tolerance[2]) td->loc[2]= 0.0f;
+				if(abs(td->iloc[2])<=tolerance[2] || td->loc[2]*td->iloc[2]<0.0f) td->loc[2]= 0.0f;
 			}
 		}
 	}
