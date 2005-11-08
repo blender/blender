ece7963545db10e8ea627f2e6fd75801d49067f1
Alexander Ewering <blender@instinctive.de>
2005-11-08 12:56:32 +0000

    Just have been told fmodf is C99, so I replaced it with fmod and casts...

--- source/blender/src/sequence.c
+++ source/blender/src/sequence.c
@@ -1722,7 +1722,7 @@ StripElem *give_stripelem(Sequence *seq, int cfra)
 	}
 	if (seq->strobe < 1.0) seq->strobe = 1.0;
 	if (seq->strobe > 1.0) {
-		nr -= (int)fmodf(nr, seq->strobe);
+		nr -= (int)fmod((double)nr, (double)seq->strobe);
 	}
 
 	se+= nr; /* don't get confused by the increment, this is the same as strip->stripdata[nr], which works on some compilers...*/
