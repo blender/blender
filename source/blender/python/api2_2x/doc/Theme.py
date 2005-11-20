44c581644a952ae1a785e749e74c78f9b4ed1ee8
Ken Hughes <khughes@pacific.edu>
2005-11-20 18:41:41 +0000

    -- fix indentation to make epydoc happy

--- source/blender/python/api2_2x/doc/Theme.py
+++ source/blender/python/api2_2x/doc/Theme.py
@@ -105,93 +105,93 @@ class Theme:
       - (type): the chosen sub-theme.
     """
 
-  class ThemeUI:
-    """
-    The User Interface sub-theme
-    ============================
-      This can be accessed with theme.get(t), where t can be 'ui' or -1.
-      The available variables follow the internal (C coded) ThemeUI struct in
-      Blender.  Most of them represent rgba (red, green, blue, alpha) colors,
-      with each component in the range [0, 255].  There is more than one way to
-      access them.
+class ThemeUI:
+  """
+  The User Interface sub-theme
+  ============================
+    This can be accessed with theme.get(t), where t can be 'ui' or -1.
+    The available variables follow the internal (C coded) ThemeUI struct in
+    Blender.  Most of them represent rgba (red, green, blue, alpha) colors,
+    with each component in the range [0, 255].  There is more than one way to
+    access them.
 
-      Examples::
-        print outline.R
-        outline.r = 180 # it's case insensitive
-        outline[0] = 94 # 0 for red, 1 for green, ...
-        outline = [200, 200, 200, 255] # setting all components at once
-    @type theme: string
-    @ivar theme: the parent Theme for this object.
-    @ivar outline: theme rgba var.
-    @ivar neutral: theme rgba var.
-    @ivar action: theme rgba var.
-    @ivar setting: theme rgba var.
-    @ivar setting1: theme rgba var.
-    @ivar setting2: theme rgba var.
-    @ivar num: theme rgba var.
-    @ivar textfield: theme rgba var.
-	@ivar textfield_hi: theme rgba var.
-    @ivar popup: theme rgba var.
-    @ivar text: theme rgba var.
-    @ivar text_hi: theme rgba var.
-    @ivar menu_back: theme rgba var.
-    @ivar menu_item: theme rgba var.
-    @ivar menu_hilite: theme rgba var.
-    @ivar menu_text: theme rgba var.
-    @ivar menu_text_hi: theme rgba var.
-    @type drawType: int
-    @ivar drawType: the draw type (minimal, rounded, etc) in the range [1, 4].
-    """
+    Examples::
+      print outline.R
+      outline.r = 180 # it's case insensitive
+      outline[0] = 94 # 0 for red, 1 for green, ...
+      outline = [200, 200, 200, 255] # setting all components at once
+  @type theme: string
+  @ivar theme: the parent Theme for this object.
+  @ivar outline: theme rgba var.
+  @ivar neutral: theme rgba var.
+  @ivar action: theme rgba var.
+  @ivar setting: theme rgba var.
+  @ivar setting1: theme rgba var.
+  @ivar setting2: theme rgba var.
+  @ivar num: theme rgba var.
+  @ivar textfield: theme rgba var.
+  @ivar textfield_hi: theme rgba var.
+  @ivar popup: theme rgba var.
+  @ivar text: theme rgba var.
+  @ivar text_hi: theme rgba var.
+  @ivar menu_back: theme rgba var.
+  @ivar menu_item: theme rgba var.
+  @ivar menu_hilite: theme rgba var.
+  @ivar menu_text: theme rgba var.
+  @ivar menu_text_hi: theme rgba var.
+  @type drawType: int
+  @ivar drawType: the draw type (minimal, rounded, etc) in the range [1, 4].
+  """
 
-  class ThemeSpace:
-    """
-    The Space sub-themes
-    ====================
-      There is a sub-theme for each space in Blender (except for the Scripts
-      window, but it will be added soon).  Please read the information about
-      L{Theme.ThemeUI}, since it is also relevant here.  In Blender,
-      all theme spaces share the same C structure.  For this reason, all of
-      them here share the same variables, event though some spaces only use
-      a few of them.  This lower-level access is acceptable because generally
-      users will prefer to use the interface to change single theme options
-      and only use scripting to save or restore themes.  But anyway, checking
-      the Themes tab in the User Preferences space in Blender and using the
-      bundled "Save current theme" script (or its simplified version written
-      on the top of this page) can help you finding out any specific info you
-      may need.
-    @type theme: string
-    @ivar theme: the parent Theme for this object.
-    @ivar back: theme rgba var.
-    @ivar text: theme rgba var.
-    @ivar text_hi: theme rgba var.
-    @ivar header: theme rgba var.
-    @ivar panel: theme rgba var.
-    @ivar shade1: theme rgba var.
-    @ivar shade2: theme rgba var.
-    @ivar hilite: theme rgba var.
-    @ivar grid: theme rgba var.
-    @ivar wire: theme rgba var.
-    @ivar select: theme rgba var.
-    @ivar active: theme rgba var.
-    @ivar transform: theme rgba var.
-    @ivar vertex: theme rgba var.
-    @ivar vertex_select: theme rgba var.
-    @ivar edge: theme rgba var.
-    @ivar edge_select: theme rgba var.
-    @ivar edge_seam: theme rgba var.
-    @ivar edge_facesel: theme rgba var.
-    @ivar face: theme rgba var.
-    @ivar face_select: theme rgba var.
-    @ivar face_dot: theme rgba var.
-    @ivar normal: theme rgba var.
-    @ivar syntaxl: theme rgba var.
-    @ivar syntaxn: theme rgba var.
-    @ivar syntaxb: theme rgba var.
-    @ivar syntaxv: theme rgba var.
-    @ivar syntaxc: theme rgba var.
-    @type vertex_size: int
-    @ivar vertex_size: size of the vertices dots on screen in the range [1, 10].
-    @type facedot_size: int
-    @ivar facedot_size: size of the face dots on screen in the range [1, 10].
-    """
+class ThemeSpace:
+  """
+  The Space sub-themes
+  ====================
+    There is a sub-theme for each space in Blender (except for the Scripts
+    window, but it will be added soon).  Please read the information about
+    L{Theme.ThemeUI}, since it is also relevant here.  In Blender,
+    all theme spaces share the same C structure.  For this reason, all of
+    them here share the same variables, event though some spaces only use
+    a few of them.  This lower-level access is acceptable because generally
+    users will prefer to use the interface to change single theme options
+    and only use scripting to save or restore themes.  But anyway, checking
+    the Themes tab in the User Preferences space in Blender and using the
+    bundled "Save current theme" script (or its simplified version written
+    on the top of this page) can help you finding out any specific info you
+    may need.
+  @type theme: string
+  @ivar theme: the parent Theme for this object.
+  @ivar back: theme rgba var.
+  @ivar text: theme rgba var.
+  @ivar text_hi: theme rgba var.
+  @ivar header: theme rgba var.
+  @ivar panel: theme rgba var.
+  @ivar shade1: theme rgba var.
+  @ivar shade2: theme rgba var.
+  @ivar hilite: theme rgba var.
+  @ivar grid: theme rgba var.
+  @ivar wire: theme rgba var.
+  @ivar select: theme rgba var.
+  @ivar active: theme rgba var.
+  @ivar transform: theme rgba var.
+  @ivar vertex: theme rgba var.
+  @ivar vertex_select: theme rgba var.
+  @ivar edge: theme rgba var.
+  @ivar edge_select: theme rgba var.
+  @ivar edge_seam: theme rgba var.
+  @ivar edge_facesel: theme rgba var.
+  @ivar face: theme rgba var.
+  @ivar face_select: theme rgba var.
+  @ivar face_dot: theme rgba var.
+  @ivar normal: theme rgba var.
+  @ivar syntaxl: theme rgba var.
+  @ivar syntaxn: theme rgba var.
+  @ivar syntaxb: theme rgba var.
+  @ivar syntaxv: theme rgba var.
+  @ivar syntaxc: theme rgba var.
+  @type vertex_size: int
+  @ivar vertex_size: size of the vertices dots on screen in the range [1, 10].
+  @type facedot_size: int
+  @ivar facedot_size: size of the face dots on screen in the range [1, 10].
+  """
 
