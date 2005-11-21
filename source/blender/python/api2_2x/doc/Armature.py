0e62c58b0895c14f4391af26dd55d9596917a2c8
Joseph Gilbert <ascotan@gmail.com>
2005-11-21 15:36:36 +0000

    Some updates to armature documentation.

--- source/blender/python/api2_2x/doc/Armature.py
+++ source/blender/python/api2_2x/doc/Armature.py
@@ -17,37 +17,40 @@ Example::
   armatures = Armature.Get()
   for a in armatures:
     print "Armature ", a
-    print "- The root bones of %s: %s" % (a.name, a.getBones())
-"""
+    for bone_name, bone in a.bones.items():
+       print bone_name, bone.weight
 
-def New (name = 'ArmatureData'):
-  """
-  Create a new Armature object.
-  @type name: string
-  @param name: The Armature name.
-  @rtype: Blender Armature
-  @return: The created Armature object.
-  """
+"""
 
 def Get (name = None):
   """
   Get the Armature object(s) from Blender.
-  @type name: string
-  @param name: The name of the Armature.
+  @type name: string, nothing, or list of strings
+  @param name: The string name of an armature.
   @rtype: Blender Armature or a list of Blender Armatures
   @return: It depends on the I{name} parameter:
       - (name): The Armature object with the given I{name};
+      - (name, name, ...): A list of Armature objects
       - ():     A list with all Armature objects in the current scene.
   """
 
-class Armature:
+class ArmatureType:
   """
-  The Armature object
+  The ArmatureType object
   ===================
     This object gives access to Armature-specific data in Blender.
   @ivar name: The Armature name.
-  @ivar bones: A List of Bones that make up this armature.
+  @ivar bones: A Dictionary of Bones that make up this armature.
   """
+
+  def __init__(name = myArmature):
+    """
+    Initializer for the ArmatureType TypeObject.
+    @param name: The name for the new armature
+    @type name: string
+    Example::
+        myNewArmature = Blender.Armature.ArmatureType('AR_1')
+    """
 
   def getName():
     """
@@ -65,30 +68,70 @@ class Armature:
   def getBones():
     """
     Get all the Armature bones.
-    @rtype: PyList
+    @rtype: PyBonesDict
     @return: a list of PyBone objects that make up the armature.
     """
+   
+  def makeEditable():
+    """
+    Put the armature into EditMode for editing purposes.
+    @warning: The armature should not be in manual editmode
+    prior to calling this method.
+    """
 
-  def addBone(bone):
+  def saveChanges():
     """
-    Add a bone to the armature.
-    @type bone: PyBone
-    @param bone: The Python Bone to add to the armature.
-    @warn: If a bone is added to the armature with no parent
-    if will not be parented. You should set the parent of the bone
-    before adding to the armature.
+    Save all changes and update the armature.
+    @preconditions: Must have called makeEditable() first.
     """
-    
-  def drawAxes(bool):
+
+class BonesDict:
+  """
+  The BonesDict object
+  ===============
+    This object gives gives dictionary like access to the bones in an armature.
+  """
+
+  def items():
     """
-    Set whether or not to draw the armature's axes per bone.
-    @type bool: boolean (true or false)
-    @param bool: specifies whether or not to draw axes
+    Retun the key, value pairs in this dictionary
+    @rtype: string, BPy_bone
+    @return: All strings, and py_bones in the armature (in that order)
     """
-    
-  def drawNames(bool):
+
+  def keys():
     """
-    Set whether or not to draw the armature's names per bone.
-    @type bool: boolean (true or false)
-    @param bool: specifies whether or not to draw names
+    Retun the keys in this dictionary
+    @rtype: string
+    @return: All strings representing the bone names
     """
+
+  def values():
+    """
+    Retun the values in this dictionary
+    @rtype: BPy_bone
+    @return: All BPy_bones in this dictionary
+    """
+
+class BoneType:
+  """
+  The BoneType object
+  ===============
+    This object gives access to Bone-specific data in Blender.
+  @ivar name: The name of this Bone.
+  @ivar roll: This Bone's roll value.
+  @ivar head: This Bone's "head" ending position when in rest state.
+  @ivar tail: This Bone's "tail" ending position when in rest state.
+  @ivar matrix: This Bone's matrix.
+  @ivar parent: The parent Bone.
+  @ivar children: The children bones.
+  @ivar weight: The bone's weight.
+  @ivar options: Various bone options which can be:
+      -CONNECTED: IK to parent
+      -HINGE: No parent rotation or scaling
+      -NO_DEFORM: The bone does not deform geometetry
+      -MULTIPLY: Multiply vgroups by envelope
+      -HIDDEN_EDIT: Hide bones in editmode
+  @ivar subdivision: The number of bone subdivisions.
+  @ivar deform_dist: The deform distance of the bone
+  """
