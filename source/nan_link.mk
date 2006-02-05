64751fba9fadfcabbbae75655459e970be4036b7
Peter Schlaile <peter@schlaile.de>
2006-02-05 23:32:17 +0000

    -lopenal slipped between ffmpeg-link on Linux

--- source/nan_link.mk
+++ source/nan_link.mk
@@ -100,7 +100,7 @@ ifeq ($(OS),linux)
   ifeq ($(CPU),$(findstring $(CPU), "i386 x86_64 ia64"))
     COMMENT = "MESA 3.1"
     LLIBS = -L$(NAN_MESA)/lib -L/usr/X11R6/lib -lXmu -lXext -lX11 -lXi
-    LLIBS += -lutil -lc -lm -ldl -lpthread -lopenal -lavformat -lavcodec -lavutil -ldts -lz
+    LLIBS += -lutil -lc -lm -ldl -lpthread -lavformat -lavcodec -lavutil -ldts -lz
 #    LLIBS += -L$(NAN_ODE)/lib -lode
     LOPTS = -export-dynamic
     DADD = -lGL -lGLU
