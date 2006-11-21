ed3559aa8ee7b039f8018555fe8962b4cbbc4824
Ken Hughes <khughes@pacific.edu>
2006-11-21 19:00:12 +0000

    Python API ========== Bugfix #4905:  correcting errors and out-of-date NLA API documentation.

--- source/blender/python/api2_2x/doc/NLA.py
+++ source/blender/python/api2_2x/doc/NLA.py
@@ -6,55 +6,9 @@ The Blender.Armature.NLA submodule.
 NLA
 ===
 
-This module provides access to B{Action} objects in Blender.  Actions are a series of keyframes/Ipo curves
-that define the movement of a bone. Actions are linked to objects of type armature.
-
-Example::
-  import Blender
-  from Blender import *
-  from Blender.Armature import *
-  from Blender.Armature.Bone import *
-
-  armObj = Object.New('Armature', "Armature_obj")
-  armData = Armature.New()
-
-  bn1=Blender.Armature.Bone.New("bone1")
-  bn1.setHead(0.0,0.0,0.0)
-  bn1.setTail(2.0,1.0,0.0)
-
-  bn2=Blender.Armature.Bone.New("bone2")
-  bn2.setHead(3.0,2.0,1.0)
-  bn2.setTail(4.0,4.0,1.0)
-  bn2.setRoll(.5)
-  bn2.setParent(bn1)
-
-  armData.addBone(bn1)
-  armData.addBone(bn2)
-
-  armObj.link(armData)
-  scn = Blender.Scene.getCurrent()
-  scn.link(armObj)
-
-  armObj.makeDisplayList()
-  Blender.Window.RedrawAll()
-
-  action = Blender.Armature.NLA.NewAction()
-  action.setActive(armObj)
-  
-  bn2.setPose([ROT,LOC,SIZE])  
-  
-  context = scn.getRenderingContext()
-  context.currentFrame(2)
-
-  quat = Blender.Mathutils.Quaternion([1,2,3,4])
-  bn2.setQuat(quat)
-  bn2.setLoc([3,4,5])
-  
-  bn2.setPose([ROT,LOC,SIZE])
-
-  print action.name
-  action2 = Blender.Armature.NLA.CopyAction(action)
-  action2.name = "Copy"
+This module provides access to B{Action} objects in Blender.  Actions are a
+series of keyframes/Ipo curves that define the movement of a bone. 
+Actions are linked to objects of type armature.
 
 @type Flags: readonly dictionary
 @var Flags: Constant dict used by the L{ActionStrip.flag} attribute.
