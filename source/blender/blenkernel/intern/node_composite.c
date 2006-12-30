7d7714a6757b863195553bff3f1fe46c3ab45dc9
Matt Ebb <matt@mke3.net>
2006-12-30 14:47:03 +0000

    == Compositor == * Displace Node
    Displaces an input image's pixels based on an input vector mask. This can be
    useful for a lot of things, like hot air distortion, quick-and-dirty compo
    refraction, compositing live footage behind refracting objects, and more!
    
    The amount of displacement in the X and Y directions is determined by:
    * The value of the mask's channels
      - (red) channel 1's value determines displacement along the positive or
      negative X axis
      - (green) channel 2's value determines displacement along the positive or
      negative Y axis
    
    If both the channel's values are equal (i.e. a greyscale image) the input
    image will be displaced equally in both X and Y directions, also according to:
    * The X scale and Y scale buttons
      - These act as multipliers to increase or decrease the strength of the
      displacement along their respective axes. They need to be set to non-zero
      values for the node to have any effect.
    
    Because of this, you can use the displace node in two ways, with a greyscale
    mask(easy to paint, or take from a procedural texture), or with a vector
    channel or RGB image, such as a normal pass, which will displace the pixels
    based on the normal direction.
    
    A quick practical example:
    http://mke3.net/blender/etc/displace-desert-h264.mov
    http://mke3.net/blender/etc/displace-desert.blend.zip
    
    And some techie examples:
    
    Using a greyscale mask
    http://mke3.net/blender/etc/displace-bw-h264.mov
    http://mke3.net/blender/etc/displace-bw.png
    
    Using a vector mask
    http://mke3.net/blender/etc/displace-vec-h264.mov
    http://mke3.net/blender/etc/displace-vec.png

--- source/blender/blenkernel/intern/node_composite.c
+++ source/blender/blenkernel/intern/node_composite.c
@@ -5227,11 +5227,13 @@ static void do_mapuv(CompBuf *stackbuf, CompBuf *cbuf, CompBuf *uvbuf, float thr
 			if(x>0 && x<sx-1 && y>0 && y<sy-1) {
 				if(uv[2]!=0.0f) {
 					
+					/* adaptive sampling, red (U) channel */
 					dx= 0.5f*(fabs(uv[0]-uv[-3]) + fabs(uv[0]-uv[3]));
 					
 					dx+= 0.25f*(fabs(uv[0]-uvprev[-3]) + fabs(uv[0]-uvnext[-3]));
 					dx+= 0.25f*(fabs(uv[0]-uvprev[+3]) + fabs(uv[0]-uvnext[+3]));
 					
+					/* adaptive sampling, green (V) channel */
 					dy= 0.5f*(fabs(uv[1]-uv[-row+1]) + fabs(uv[1]-uv[row+1]));
 							 
 					dy+= 0.25f*(fabs(uv[1]-uvprev[+1-3]) + fabs(uv[1]-uvnext[+1-3]));
@@ -5365,6 +5367,132 @@ static bNodeType cmp_node_idmask= {
 };
 
 
+/* **************** Displace  ******************** */
+
+static bNodeSocketType cmp_node_displace_in[]= {
+	{	SOCK_RGBA, 1, "Image",			0.8f, 0.8f, 0.8f, 1.0f, 0.0f, 1.0f},
+	{	SOCK_VECTOR, 1, "Vector",			1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f},
+	{	SOCK_VALUE, 1, "X Scale",				0.0f, 0.0f, 0.0f, 0.0f, -1000.0f, 1000.0f},
+	{	SOCK_VALUE, 1, "Y Scale",				0.0f, 0.0f, 0.0f, 0.0f, -1000.0f, 1000.0f},
+	{	-1, 0, ""	}
+};
+static bNodeSocketType cmp_node_displace_out[]= {
+	{	SOCK_RGBA, 0, "Image",			0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f},
+	{	-1, 0, ""	}
+};
+
+static void do_displace(CompBuf *stackbuf, CompBuf *cbuf, CompBuf *vecbuf, float *veccol, float *xscale, float *yscale)
+{
+	ImBuf *ibuf;
+	int x, y, sx, sy;
+	float dx=0.0, dy=0.0;
+	float dspx, dspy;
+	float uv[2];
+
+	float *out= stackbuf->rect, *vec=vecbuf->rect, *in= cbuf->rect;
+	float *vp, *vpnext, *vpprev;
+	
+	int row = 3*vecbuf->x;
+	
+	/* ibuf needed for sampling */
+	ibuf= IMB_allocImBuf(cbuf->x, cbuf->y, 32, 0, 0);
+	ibuf->rect_float= cbuf->rect;
+	
+	vec = vecbuf->rect;
+	
+	sx= stackbuf->x;
+	sy= stackbuf->y;
+	
+	for(y=0; y<sy; y++) {
+		for(x= 0; x< sx; x++, out+=4, in+=4, vec+=3) {
+			
+			/* the x-xrad stuff is a bit weird, but i seem to need it otherwise 
+			 * my returned pixels are offset weirdly */
+			vp = compbuf_get_pixel(vecbuf, veccol, x-vecbuf->xrad, y-vecbuf->yrad, vecbuf->xrad, vecbuf->yrad);
+			
+			/* find the new displaced co-ords, also correcting for translate offset */
+			dspx = x - (*xscale * vp[0]);
+			dspy = y - (*yscale * vp[1]);
+
+			/* convert image space to 0.0-1.0 UV space for sampling, correcting for translate offset */
+			uv[0] = dspx / (float)sx;
+			uv[1] = dspy / (float)sy;
+			
+			if(x>0 && x< vecbuf->x-1 && y>0 && y< vecbuf->y-1)  {
+				vpnext = vp+row;
+				vpprev = vp-row;
+			
+				/* adaptive sampling, X channel */
+				dx= 0.5f*(fabs(vp[0]-vp[-3]) + fabs(vp[0]-vp[3]));
+				
+				dx+= 0.25f*(fabs(vp[0]-vpprev[-3]) + fabs(vp[0]-vpnext[-3]));
+				dx+= 0.25f*(fabs(vp[0]-vpprev[+3]) + fabs(vp[0]-vpnext[+3]));
+				
+				/* adaptive sampling, Y channel */
+				dy= 0.5f*(fabs(vp[1]-vp[-row+1]) + fabs(vp[1]-vp[row+1]));
+						 
+				dy+= 0.25f*(fabs(vp[1]-vpprev[+1-3]) + fabs(vp[1]-vpnext[+1-3]));
+				dy+= 0.25f*(fabs(vp[1]-vpprev[+1+3]) + fabs(vp[1]-vpnext[+1+3]));
+				
+				/* scaled down to prevent blurriness */
+				/* 8: magic number, provides a good level of sharpness without getting too aliased */
+				dx /= 8;
+				dy /= 8;
+			}
+
+			/* should use mipmap */
+			if(dx > 0.006f) dx= 0.006f;
+			if(dy > 0.006f) dy= 0.006f;
+			if ((vp[0]> 0.0) && (dx < 0.004)) dx = 0.004;
+			if ((vp[1]> 0.0) && (dy < 0.004)) dy = 0.004;
+			
+
+			ibuf_sample(ibuf, uv[0], uv[1], dx, dy, out);
+		}
+	}
+
+	IMB_freeImBuf(ibuf);	
+}
+
+
+static void node_composit_exec_displace(void *data, bNode *node, bNodeStack **in, bNodeStack **out)
+{
+	if(out[0]->hasoutput==0)
+		return;
+	
+	if(in[0]->data && in[1]->data) {
+		CompBuf *cbuf= in[0]->data;
+		CompBuf *vecbuf= in[1]->data;
+		CompBuf *stackbuf;
+		
+		cbuf= typecheck_compbuf(cbuf, CB_RGBA);
+		vecbuf= typecheck_compbuf(vecbuf, CB_VEC3);
+		stackbuf= alloc_compbuf(cbuf->x, cbuf->y, CB_RGBA, 1); // allocs
+
+		do_displace(stackbuf, cbuf, vecbuf, in[1]->vec, in[2]->vec, in[3]->vec);
+		
+		out[0]->data= stackbuf;
+		
+		
+		if(cbuf!=in[0]->data)
+			free_compbuf(cbuf);
+		if(vecbuf!=in[1]->data)
+			free_compbuf(vecbuf);
+	}
+}
+
+static bNodeType cmp_node_displace= {
+	/* type code   */	CMP_NODE_DISPLACE,
+	/* name        */	"Displace",
+	/* width+range */	140, 100, 320,
+	/* class+opts  */	NODE_CLASS_OP_FILTER, NODE_OPTIONS,
+	/* input sock  */	cmp_node_displace_in,
+	/* output sock */	cmp_node_displace_out,
+	/* storage     */	"",
+	/* execfunc    */	node_composit_exec_displace
+};
+
+
 /* ****************** types array for all shaders ****************** */
 
 bNodeType *node_all_composit[]= {
@@ -5412,6 +5540,7 @@ bNodeType *node_all_composit[]= {
 	&cmp_node_mapuv,
 	&cmp_node_idmask,
  	&cmp_node_defocus,
+	&cmp_node_displace,
 	NULL
 };
 
