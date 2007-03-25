0eff40f01f9ebf3995d43ebe84e071848ff2fb9c
Ton Roosendaal <ton@blender.org>
2007-03-25 16:05:46 +0000

    Reverting my fix for nodes. This is a bigger task... now scons should work again.
    Robert promised to work on fixes for makefile today!

--- source/blender/nodes/intern/SHD_util.h
+++ source/blender/nodes/intern/SHD_util.h
@@ -61,6 +61,17 @@
 
 #include "../SHD_node.h"
 
+#include "BIF_gl.h"
+#include "BIF_glutil.h"
+#include "BIF_interface.h"
+#include "BIF_interface_icons.h"
+#include "BIF_language.h"
+#include "BIF_mywindow.h"
+#include "BIF_previewrender.h"
+#include "BIF_resources.h"
+#include "BIF_screen.h"
+#include "BIF_space.h"
+
 #include "BLI_arithb.h"
 #include "BLI_blenlib.h"
 #include "BLI_rand.h"
@@ -69,8 +80,16 @@
 #include "IMB_imbuf_types.h"
 #include "IMB_imbuf.h"
 
+#include "BSE_drawipo.h"
+#include "BSE_node.h"
+#include "BSE_view.h"
+
 #include "RE_pipeline.h"
 #include "RE_shader_ext.h"
+
+#include "butspace.h"
+#include "blendef.h"
+#include "mydevice.h"
 
 
 /* ********* exec data struct, remains internal *********** */
