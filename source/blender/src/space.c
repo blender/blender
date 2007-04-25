3f153c25be407aaa087796f309540dacbedf631d
Campbell Barton <ideasman42@gmail.com>
2007-04-25 14:47:29 +0000

    hiding objects in texture paint mode was buggy - (you couldnt exit texxture paint) - so only hide/show objects in object mode. removed radiosity support.
    According to the author -
    
    "The Radiosity file format is my own personal format. I created it to
    learn how meshes and vertex colors were stored. See IO-Examples.zip, the
    example *.radio files on my web page."

--- source/blender/src/space.c
+++ source/blender/src/space.c
@@ -1924,9 +1924,9 @@ static void winqreadview3dspace(ScrArea *sa, void *spacedata, BWinEvent *evt)
 						hide_unselected_pose_bones();
 					else if (G.qual==LR_ALTKEY)
 						show_all_pose_bones();
-				} 
-				
-				else {
+				}
+				/* Object mode only, other modes are accounted for above */
+				else if ((G.f & G_VERTEXPAINT|G_TEXTUREPAINT|G_WEIGHTPAINT)==0) {
 					if(G.qual==LR_CTRLKEY)hookmenu();
 					else if(G.qual==LR_ALTKEY) show_objects();
 					else if(G.qual==LR_SHIFTKEY) hide_objects(0);
