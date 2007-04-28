6e27e1b6ebb76f937182468d4f8fa64f028c90e7
Ton Roosendaal <ton@blender.org>
2007-04-28 16:15:12 +0000

    Part 2 of 64 bits fixing; the files.
    The good news; previously written 64 bits are still valid! All fixes
    appeared to be possible in code, no versioning patches needed. :)
    That also removes the I AM STUPID 64 bits ban from the code.
    
    The bad news:
    I couldn't get a 64 bits Blender running here (ghost-mac issues... it
    has to be recoded using Quartz to be able to run 64 bits). So what I
    have tested was:
    
    32 bits binary:
      - Appending/linking data from 64 bits file.
      - Reading 64 bits chained library-linked files (file -> file -> etc)
      - Linking 32 bits files with 64 bits files
    
    This has to be tested for 64 bits too. Will drop in IRC now to help.

--- source/creator/creator.c
+++ source/creator/creator.c
@@ -234,23 +234,6 @@ int main(int argc, char **argv)
 	int audio = 0;
 #endif
 
-#ifndef YESIAMSTUPID
-	/* temporary: prevent people to make/use 64 bits versions without them knowing it might be
-		risky. I don't know for sure yet if we get problems, but I rather not get the burden of
-		having to fix all faulty saved 64 bits files (ton) */
-	if(sizeof(void *)==8) {
-		printf("64 bits compiles will give incorrectly saved .blend files. Do not use it. For testing purposes please remove this line from creator.c\n"); 
-		exit(0);
-	}
-#else
-	if(sizeof(void *)==8) {
-		printf("64 bits compiles will give incorrectly saved .blend files. Do not use it.\n\n"
-       "*** If you continue to run this executable, you really are quite stupid ***\n\n"); 
-
-	}
-
-#endif
-
 	setCallbacks();
 #ifdef __APPLE__
 		/* patch to ignore argument finder gives us (pid?) */
