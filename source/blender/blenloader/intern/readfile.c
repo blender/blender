e64e6ae3a32f7afed3efb917b3bb5edd4e170e26
Andrea Weikert <elubie@gmx.net>
2007-07-15 14:55:09 +0000

    == MSVC compilation == two small fixes in this commit: - added new file for nodes - fixed compile error in readfile.c, as reported by Jean-Michel Soler here:   http://lists.blender.org/pipermail/bf-committers/2007-July/018849.html

--- source/blender/blenloader/intern/readfile.c
+++ source/blender/blenloader/intern/readfile.c
@@ -6531,9 +6531,9 @@ static void do_versions(FileData *fd, Library *lib, Main *main)
 		if (main->subversionfile < 3) {
 			for(ob = main->object.first; ob; ob= ob->id.next) {
 				ListBase *list;
-				list = &ob->constraints;
 				float temp_size[3];
-
+				list = &ob->constraints;
+				
 				/* fix up constraints due to constraint recode changes */
 				if (list) {
 					bConstraint *curcon;
