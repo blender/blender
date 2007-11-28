7154e84ee1d5a6086eb47b499712d4bbc8a05a8f
Ton Roosendaal <ton@blender.org>
2007-11-28 12:32:25 +0000

    Images now have option to be tagged "convert to premul". Option available in the Image (properties) panel.

--- source/blender/src/buttons_shading.c
+++ source/blender/src/buttons_shading.c
@@ -1174,9 +1174,12 @@ void uiblock_image_panel(uiBlock *block, Image **ima_pp, ImageUser *iuser,
 		 but= uiDefButBitS(block, TOG, IMA_FIELDS, imagechanged, "Fields",	10, 90, 100, 20, &ima->flag, 0, 0, 0, 0, "Click to enable use of fields in Image");
 		 uiButSetFunc(but, image_field_test, ima, iuser);
 		 uiDefButBitS(block, TOG, IMA_STD_FIELD, B_NOP, "Odd",			10, 70, 100, 20, &ima->flag, 0, 0, 0, 0, "Standard Field Toggle");
-		 uiBlockEndAlign(block);
 		 
+		 uiBlockBeginAlign(block);
+		 uiBlockSetFunc(block, image_reload_cb, ima, iuser);
 		 uiDefButBitS(block, TOG, IMA_ANTIALI, B_NOP, "Anti",			10, 35, 100, 20, &ima->flag, 0, 0, 0, 0, "Toggles Image anti-aliasing, only works with solid colors");
+		 uiDefButBitS(block, TOG, IMA_DO_PREMUL, imagechanged, "Premul",		110, 35, 100, 20, &ima->flag, 0, 0, 0, 0, "Toggles premultiplying alpha");
+		 uiBlockEndAlign(block);
 		 
 		 if( ELEM(ima->source, IMA_SRC_MOVIE, IMA_SRC_SEQUENCE)) {
 			 sprintf(str, "(%d) Frames:", iuser->framenr);
