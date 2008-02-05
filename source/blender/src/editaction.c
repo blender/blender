53d86458371d9d595d693c10437cfd08817acaf9
Brecht Van Lommel <brechtvanlommel@pandora.be>
2008-02-05 22:46:26 +0000

    Bugfix for infinite loop in Sample Keys function in the action editor, with two keyframes on the same location.

--- source/blender/src/editaction.c
+++ source/blender/src/editaction.c
@@ -1634,11 +1634,11 @@ void sample_action_keys (void)
 						
 						/* free temp cache */
 						MEM_freeN(value_cache);
+
+						/* as we added keyframes, we need to compensate so that bezt is at the right place */
+						bezt = icu->bezt + i + range - 1;
+						i += (range - 1);
 					}
-					
-					/* as we added keyframes, we need to compensate so that bezt is at the right place */
-					bezt = icu->bezt + i + range - 1;
-					i += (range - 1);
 					
 					/* bezt was selected, so it now marks the start of a whole new chain to search */
 					start= bezt;
