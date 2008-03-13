ecc4ccf68f057788cbd1e117672b8531ca7f286f
Martin Poirier <theeth@yahoo.com>
2008-03-13 18:47:55 +0000

    Fix compiling error.
    Typo in text.c

--- source/blender/blenkernel/intern/text.c
+++ source/blender/blenkernel/intern/text.c
@@ -1033,7 +1033,7 @@ int txt_find_string(Text *text, char *findstr)
 
 void txt_cut_sel (Text *text)
 {
-	txt_copy_clipboad(text);
+	txt_copy_clipboard(text);
 	
 	txt_delete_sel(text);
 	txt_make_dirty(text);
