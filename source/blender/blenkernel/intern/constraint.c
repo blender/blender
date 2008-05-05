4c0bcbf4da991e3a8fe5b01bebbb3dc54fe183f8
Kent Mein <mein@cs.umn.edu>
2008-05-05 17:26:33 +0000

    fix for bad indexing found by Cyril Brulebois
    Its making the identity matrix so I just removed the initalizing values
    that were out of bounds.
    
    Kent

--- source/blender/blenkernel/intern/constraint.c
+++ source/blender/blenkernel/intern/constraint.c
@@ -1104,9 +1104,9 @@ static void vectomat (float *vec, float *target_up, short axis, short upflag, sh
 	/* identity matrix - don't do anything if the two axes are the same */
 	else {
 		m[0][0]= m[1][1]= m[2][2]= 1.0;
-		m[0][1]= m[0][2]= m[0][3]= 0.0;
-		m[1][0]= m[1][2]= m[1][3]= 0.0;
-		m[2][0]= m[2][1]= m[2][3]= 0.0;
+		m[0][1]= m[0][2]= 0.0;
+		m[1][0]= m[1][2]= 0.0;
+		m[2][0]= m[2][1]= 0.0;
 	}
 }
 
