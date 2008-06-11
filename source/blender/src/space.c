bdb3ef08cf694b167689a54e25ab026118b43e6d
Campbell Barton <ideasman42@gmail.com>
2008-06-11 05:25:52 +0000

    Increased new nla, sound, action, ipo, timeline spaces max zoomlevel to use MAXFRAMEF, so you can view an entire animation ans so all time spaces have the same maximum which is important when the views lock to each others zoomlevel. Not ideal since it only affects newly created spaces, but probably not worth going through doversions since it wont effect many blender users.

--- source/blender/src/space.c
+++ source/blender/src/space.c
@@ -3216,7 +3216,7 @@ void initipo(ScrArea *sa)
 	sipo->v2d.min[0]= 0.01f;
 	sipo->v2d.min[1]= 0.01f;
 
-	sipo->v2d.max[0]= 15000.0f;
+	sipo->v2d.max[0]= MAXFRAMEF;
 	sipo->v2d.max[1]= 10000.0f;
 	
 	sipo->v2d.scroll= L_SCROLL+B_SCROLL;
@@ -5175,7 +5175,7 @@ static void init_actionspace(ScrArea *sa)
 	saction->v2d.min[0]= 0.0;
 	saction->v2d.min[1]= 0.0;
 
-	saction->v2d.max[0]= 32000.0;
+	saction->v2d.max[0]= MAXFRAMEF;
 	saction->v2d.max[1]= 1000.0;
 	
 	saction->v2d.minzoom= 0.01;
@@ -5247,7 +5247,7 @@ static void init_soundspace(ScrArea *sa)
 	ssound->v2d.min[0]= 1.0;
 	ssound->v2d.min[1]= 259.0;
 
-	ssound->v2d.max[0]= 32000.0;
+	ssound->v2d.max[0]= MAXFRAMEF;
 	ssound->v2d.max[1]= 259;
 	
 	ssound->v2d.minzoom= 0.1f;
@@ -5977,7 +5977,7 @@ static void init_nlaspace(ScrArea *sa)
 	snla->v2d.min[0]= 0.0;
 	snla->v2d.min[1]= 0.0;
 	
-	snla->v2d.max[0]= 1000.0;
+	snla->v2d.max[0]= MAXFRAMEF;
 	snla->v2d.max[1]= 1000.0;
 	
 	snla->v2d.minzoom= 0.1F;
@@ -6065,7 +6065,7 @@ static void init_timespace(ScrArea *sa)
 	stime->v2d.min[0]= 1.0;
 	stime->v2d.min[1]= (float)sa->winy;
 	
-	stime->v2d.max[0]= 32000.0;
+	stime->v2d.max[0]= MAXFRAMEF;
 	stime->v2d.max[1]= (float)sa->winy;
 	
 	stime->v2d.minzoom= 0.1f;
