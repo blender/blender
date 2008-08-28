f88c5d972130e7bcde3fdd289e91546c5946c888
Campbell Barton <ideasman42@gmail.com>
2008-08-28 07:03:23 +0000

    BGE Bugfix, lights were not being removed when their gameobject was removed causing odd shadows to hang about the scene.

--- source/gameengine/Ketsji/KX_Scene.cpp
+++ source/gameengine/Ketsji/KX_Scene.cpp
@@ -944,6 +944,8 @@ int KX_Scene::NewRemoveObject(class CValue* gameobj)
 	
 	newobj->RemoveMeshes();
 	ret = 1;
+	if (newobj->IsLight() && m_lightlist->RemoveValue(newobj))
+		ret = newobj->Release();
 	if (m_objectlist->RemoveValue(newobj))
 		ret = newobj->Release();
 	if (m_tempObjectList->RemoveValue(newobj))
