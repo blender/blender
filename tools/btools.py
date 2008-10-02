7ba46c70ad377bad7c623acadbf006a9ddbe682b
Nathan Letwory <nathan@letworyinteractive.com>
2008-10-02 08:46:04 +0000

    * return the right error code * print error message when something goes wrong with nsis installer creation

--- tools/btools.py
+++ tools/btools.py
@@ -531,5 +531,8 @@ def NSIS_Installer(target=None, source=None, env=None):
     data, err = proc.communicate()
     rv = proc.wait()
 
-    return err
+    if rv != 0:
+        print
+        print data.strip().split("\n")[-1]
+    return rv
 
