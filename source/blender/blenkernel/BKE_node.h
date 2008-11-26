402fbd95cc6c95a0218b58e6fcc8421fb6a8ae7f
Robin Allen <roblovski@gmail.com>
2008-11-26 13:07:24 +0000

    2 things:
    * Patch #17998
    * tex_input_vec now takes 3-vector as first argument (was 4-vector).

--- source/blender/blenkernel/BKE_node.h
+++ source/blender/blenkernel/BKE_node.h
@@ -401,6 +401,8 @@ struct TexResult;
 #define TEX_NODE_ROTATE     114
 #define TEX_NODE_VIEWER     115
 #define TEX_NODE_TRANSLATE  116
+#define TEX_NODE_COORD      117
+#define TEX_NODE_DISTANCE   118
 
 /* 201-299 reserved. Use like this: TEX_NODE_PROC + TEX_CLOUDS, etc */
 #define TEX_NODE_PROC      200
