5fc657cdc614334c9c4c29910f06ebe1a286947b
Joshua Leung <aligorith@gmail.com>
2009-02-04 22:33:24 +0000

    Bugfix for 2.5 - Bugfix #18251
    The patch was pasted in the wrong place, thus destroying all custom OpenGL solid light setups too.

--- source/blender/windowmanager/intern/wm_files.c
+++ source/blender/windowmanager/intern/wm_files.c
@@ -571,13 +571,13 @@ int WM_read_homefile(bContext *C, int from_memory)
 		success = BKE_read_file(C, tstr, NULL, NULL);
 	} else {
 		success = BKE_read_file_from_memory(C, datatoc_B_blend, datatoc_B_blend_size, NULL, NULL);
+		
+		/* When loading factory settings, the reset solid OpenGL lights need to be applied. */
+		U.light[0].flag=0;
+		U.light[1].flag=0;
+		U.light[2].flag=0;
+		GPU_default_lights();
 	}
-	
-	/* When loading factory settings, the reset solid OpenGL lights need to be applied. */
-	U.light[0].flag=0;
-	U.light[1].flag=0;
-	U.light[2].flag=0;
-	GPU_default_lights();
 	
 	/* match the read WM with current WM */
 	wm_window_match_do(C, &wmbase); 
