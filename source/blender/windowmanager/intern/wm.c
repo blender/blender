35de68c67259ad6ad9cfb14faf77bea398d39345
Ton Roosendaal <ton@blender.org>
2009-02-20 10:39:31 +0000

    2.5
    Bugfix: on closing wm, it was using a free'd pointer to screen.

--- source/blender/windowmanager/intern/wm.c
+++ source/blender/windowmanager/intern/wm.c
@@ -150,6 +150,7 @@ void wm_close_and_free(bContext *C, wmWindowManager *wm)
 	
 	while((win= wm->windows.first)) {
 		BLI_remlink(&wm->windows, win);
+		win->screen= NULL; /* prevent draw clear to use screen */
 		wm_draw_window_clear(win);
 		wm_window_free(C, win);
 	}
