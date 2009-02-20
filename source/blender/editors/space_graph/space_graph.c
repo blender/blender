d56fd6d4cef0230665e0acf299dce4d400a7461d
Brecht Van Lommel <brechtvanlommel@pandora.be>
2009-02-20 20:43:22 +0000

    2.5: Fix build temporarily, seems there was a file missing in the graph properties panel commit. Ton, please check.

--- source/blender/editors/space_graph/space_graph.c
+++ source/blender/editors/space_graph/space_graph.c
@@ -354,7 +354,7 @@ static void graph_buttons_area_draw(const bContext *C, ARegion *ar)
 	/* set view2d view matrix for scrolling (without scrollers) */
 	UI_view2d_view_ortho(C, &ar->v2d);
 	
-	graph_region_buttons(C, ar);
+	// XXX file is not in svn: graph_region_buttons(C, ar);
 	
 	/* restore view matrix? */
 	UI_view2d_view_restore(C);
