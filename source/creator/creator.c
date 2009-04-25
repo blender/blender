fb7803c300a89892f1d5a76398b3c2506465cf60
Nathan Letwory <nathan@letworyinteractive.com>
2009-04-25 20:26:27 +0000

    Amendment for 19924 - no setenv() on Windows, make it work with _putenv_s().

--- source/creator/creator.c
+++ source/creator/creator.c
@@ -538,7 +538,11 @@ int main(int argc, char **argv)
 		BLI_where_is_temp( btempdir, 1 ); /* call after loading the .B.blend so we can read U.tempdir */
 
 #ifndef DISABLE_SDL
+#ifndef WIN32
 		setenv("SDL_VIDEODRIVER", "dummy", 1); /* initializing the video driver can cause crashes on some systems - Campbell */
+#else
+		_putenv_s("SDL_VIDEODRIVER", "dummy");
+#endif
 #ifdef __linux__
 		/* On linux the default SDL driver dma often would not play
 		 * use alsa if none is set */
