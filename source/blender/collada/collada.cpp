7482c14190f25a93b7a74e5b7536e6f6d8efb48e
Arystanbek Dyussenov <arystan.d@gmail.com>
2009-05-25 11:42:14 +0000

    Small fix that should make C functions properly export from bf_collada.

--- source/blender/collada/collada.cpp
+++ source/blender/collada/collada.cpp
@@ -1,11 +1,14 @@
 #include "BKE_scene.h"
 
-int collada_import(Scene *sce, const char *filepath)
+extern "C"
 {
-	return 1;
-}
+	int collada_import(Scene *sce, const char *filepath)
+	{
+		return 1;
+	}
 
-int collada_export(Scene *sce, const char *filepath)
-{
-	return 1;
+	int collada_export(Scene *sce, const char *filepath)
+	{
+		return 1;
+	}
 }
