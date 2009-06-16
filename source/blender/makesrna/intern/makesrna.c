bbec5c03c989fca91ba8be9f3c002c6cb2980415
Arystanbek Dyussenov <arystan.d@gmail.com>
2009-06-16 16:15:19 +0000

    Updated makesrna to generate correct CollectionPropertyRNAs.

--- source/blender/makesrna/intern/makesrna.c
+++ source/blender/makesrna/intern/makesrna.c
@@ -1628,7 +1628,7 @@ static void rna_generate_property(FILE *f, StructRNA *srna, const char *nest, Pr
 												 }
 			case PROP_COLLECTION: {
 				CollectionPropertyRNA *cprop= (CollectionPropertyRNA*)prop;
-				fprintf(f, "\t%s, %s, %s, %s, %s, %s, %s, ", rna_function_string(cprop->begin), rna_function_string(cprop->next), rna_function_string(cprop->end), rna_function_string(cprop->get), rna_function_string(cprop->length), rna_function_string(cprop->lookupint), rna_function_string(cprop->lookupstring));
+				fprintf(f, "\t%s, %s, %s, %s, %s, %s, %s, %s, ", rna_function_string(cprop->begin), rna_function_string(cprop->next), rna_function_string(cprop->end), rna_function_string(cprop->get), rna_function_string(cprop->length), rna_function_string(cprop->lookupint), rna_function_string(cprop->lookupstring), rna_function_string(cprop->add));
 				if(cprop->type) fprintf(f, "&RNA_%s\n", (char*)cprop->type);
 				else fprintf(f, "NULL\n");
 				break;
