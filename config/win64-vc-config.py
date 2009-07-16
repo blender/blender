d1169a4e12060f228ae1d3473a609d7226fca9f5
Nathan Letwory <nathan@letworyinteractive.com>
2009-07-16 20:00:15 +0000

    * revert reversion by Genscher. This '/MT' business is now handled in Blender.py

--- config/win64-vc-config.py
+++ config/win64-vc-config.py
@@ -153,7 +153,7 @@ CC = 'cl.exe'
 CXX = 'cl.exe'
 
 CFLAGS = []
-CCFLAGS = ['/MT', '/nologo', '/Ob1', '/J', '/W3', '/Gd', '/wd4244', '/wd4305', '/wd4800', '/wd4065', '/wd4267']
+CCFLAGS = ['/nologo', '/Ob1', '/J', '/W3', '/Gd', '/wd4244', '/wd4305', '/wd4800', '/wd4065', '/wd4267']
 CXXFLAGS = ['/EHsc']
 
 BF_DEBUG_CCFLAGS = ['/Zi', '/FR${TARGET}.sbr']
