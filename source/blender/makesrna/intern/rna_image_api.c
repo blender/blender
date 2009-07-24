d1e42cc5f85a6665e85a388763696fd291b86db8
Arystanbek Dyussenov <arystan.d@gmail.com>
2009-07-24 15:27:59 +0000

    Forgot to add rna_image_api.c.

---
+++ source/blender/makesrna/intern/rna_image_api.c
@@ -0,0 +1,73 @@
+/**
+ *
+ * 
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * The Original Code is Copyright (C) 2009 Blender Foundation.
+ * All rights reserved.
+ *
+ * 
+ * Contributor(s): Blender Foundation
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include "RNA_define.h"
+#include "RNA_types.h"
+
+#include "DNA_object_types.h"
+
+#ifdef RNA_RUNTIME
+
+#include "BKE_utildefines.h"
+#include "BKE_image.h"
+
+static char *rna_Image_export(Image *image, char *dest_dir)
+{
+	int length = FILE_MAX;
+	char *path= MEM_callocN(length, "image file path");
+
+	if (!BKE_export_image(image, dest_dir, path, length)) {
+		MEM_freeN(path);
+		return NULL;
+	}
+
+	return path;
+}
+
+#else
+
+void RNA_api_image(StructRNA *srna)
+{
+	FunctionRNA *func;
+	PropertyRNA *parm;
+
+	func= RNA_def_function(srna, "export", "rna_Image_export");
+	RNA_def_function_ui_description(func, "Copy image file to a directory rebuilding subdirectory structure.");
+	parm= RNA_def_string(func, "dest_dir", "", 0, "", "Destination directory.");
+	RNA_def_property_flag(parm, PROP_REQUIRED);
+	parm= RNA_def_string(func, "path", "", 0, "", "Absolute file path of copied image.");
+	RNA_def_function_return(func, parm);
+}
+
+#endif
+
