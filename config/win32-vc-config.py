1e4fb484001994f4e164aab7b44c9578df59f67d
Nathan Letwory <nathan@letworyinteractive.com>
2009-08-06 08:23:07 +0000

    * update windows default config for the openal updates, merging r22241 from soundsystem branch

--- config/win32-vc-config.py
+++ config/win32-vc-config.py
@@ -19,7 +19,7 @@ WITH_BF_OPENAL = True
 WITH_BF_STATICOPENAL = False
 BF_OPENAL = LIBDIR + '/openal'
 BF_OPENAL_INC = '${BF_OPENAL}/include ${BF_OPENAL}/include/AL '
-BF_OPENAL_LIB = 'dxguid openal_static'
+BF_OPENAL_LIB = 'OpenAL32 wrap_oal' #'dxguid openal_static'
 BF_OPENAL_LIBPATH = '${BF_OPENAL}/lib'
 # Warning, this static lib configuration is untested! users of this OS please confirm.
 BF_OPENAL_LIB_STATIC = '${BF_OPENAL}/lib/libopenal.a'
