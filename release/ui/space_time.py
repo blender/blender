ec00cec3e852991b429c70bd459502a0dd089881
Thomas Dinges <blender@dingto.org>
2009-08-13 22:10:50 +0000

    2.5 Timeline:
    * Added some menus and buttons. Still uncommented.

--- release/ui/space_time.py
+++ release/ui/space_time.py
@@ -15,8 +15,8 @@ class TIME_HT_header(bpy.types.Header):
 		if context.area.show_menus:
 			row = layout.row()
 			#row.itemM("TIME_MT_view")
-			#row.itemM("TIME_MT_frame")
-			#row.itemM("TIME_MT_playback")
+			row.itemM("TIME_MT_frame")
+			row.itemM("TIME_MT_playback")
 
 		layout.itemR(scene, "use_preview_range", text="PR", toggle=True)
 		
@@ -44,36 +44,66 @@ class TIME_HT_header(bpy.types.Header):
 		row.item_booleanO("screen.keyframe_jump", "next", True, text="", icon='ICON_NEXT_KEYFRAME')
 		row.item_booleanO("screen.frame_jump", "end", True, text="", icon='ICON_FF')
 
+		layout.itemR(scene, "active_keyingset")
+		
+		row = layout.row(align=True)
+		row.itemO("anim.insert_keyframe", text="", icon="ICON_KEY_HLT")
+		row.itemO("anim.delete_keyframe", text="", icon="ICON_KEY_DEHLT")
+
 """
 class TIME_MT_view(bpy.types.Menu):
-	__space_type__ = "TEXT_EDITOR"
+	__space_type__ = "TIMELINE"
 	__label__ = "View"
 
 	def draw(self, context):
 		layout = self.layout
 		
 		st = context.space_data
-		
+
+"""
+
 class TIME_MT_frame(bpy.types.Menu):
-	__space_type__ = "TEXT_EDITOR"
+	__space_type__ = "TIMELINE"
 	__label__ = "Frame"
 
 	def draw(self, context):
 		layout = self.layout
 		
-		st = context.space_data
+		layout.itemO("marker.add", text="Add Marker")
+		layout.itemO("marker.duplicate", text="Duplicate Marker")
+		layout.itemO("marker.move", text="Grab/Move Marker")
+		layout.itemO("marker.delete", text="Delete Marker")
+		layout.itemL(text="ToDo: Name Marker")
+		
+		layout.itemS()
+		
+		layout.itemO("time.start_frame_set")
+		layout.itemO("time.end_frame_set")
+
 
 class TIME_MT_playback(bpy.types.Menu):
-	__space_type__ = "TEXT_EDITOR"
+	__space_type__ = "TIMELINE"
 	__label__ = "Playback"
 
 	def draw(self, context):
 		layout = self.layout
 		
 		st = context.space_data
-"""
+		time = st.XXX #ToDo
+		
+		layout.itemR(time, "play_top_left")
+		layout.itemR(time, "play_all_3d")
+		layout.itemR(time, "play_anim")
+		layout.itemR(time, "play_buttons")
+		layout.itemR(time, "play_image")
+		layout.itemR(time, "play_sequencer")
+		
+		layout.itemS()
+		
+		layout.itemR(time, "continue_physics")
+
 
 bpy.types.register(TIME_HT_header)
 #bpy.types.register(TIME_MT_view)
-#bpy.types.register(TIME_MT_frame)
-#bpy.types.register(TIME_MT_playback)
+bpy.types.register(TIME_MT_frame)
+bpy.types.register(TIME_MT_playback)
