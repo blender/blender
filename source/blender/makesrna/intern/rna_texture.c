315229ea5ffb6d0519b0a5d144996760871dd3bb
Campbell Barton <ideasman42@gmail.com>
2009-08-19 14:04:21 +0000

    texture enum was crashing when generating docs, removed radiosity from makefiles.

--- source/blender/makesrna/intern/rna_texture.c
+++ source/blender/makesrna/intern/rna_texture.c
@@ -142,7 +142,7 @@ static EnumPropertyItem *rna_TextureSlot_output_node_itemf(bContext *C, PointerR
 	EnumPropertyItem *item= NULL;
 	int totitem= 0;
 	
-	if(tex) {
+	if(tex && C) { /* Note, tex/mtex/ntree can be invalid of C is NULL, TODO - can this give valid results when C is NULL? */
 		bNodeTree *ntree= tex->nodetree;
 		if(ntree) {
 			EnumPropertyItem tmp= {0, "", 0, "", ""};
