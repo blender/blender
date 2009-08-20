e56c32073d8b4855e9a6b83d281b80d4b0a2aa4d
Daniel Genrich <daniel.genrich@gmx.net>
2009-08-20 01:02:34 +0000

    LZMA: Deleting unused bad files (bad for gcc)

--- extern/lzma/7zVersion.h
+++
@@ -1,7 +0,0 @@
-#define MY_VER_MAJOR 4
-#define MY_VER_MINOR 65
-#define MY_VER_BUILD 0
-#define MY_VERSION "4.65"
-#define MY_DATE "2009-02-03"
-#define MY_COPYRIGHT ": Igor Pavlov : Public domain"
-#define MY_VERSION_COPYRIGHT_DATE MY_VERSION " " MY_COPYRIGHT " : " MY_DATE
