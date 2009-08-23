f82ce68a9eda382fead176398b624f28b5c9945c
Matt Ebb <matt@mke3.net>
2009-08-23 02:59:18 +0000

    * enable ztransp transparency type for new materials by default.

--- source/blender/blenkernel/intern/material.c
+++ source/blender/blenkernel/intern/material.c
@@ -170,7 +170,7 @@ void init_material(Material *ma)
 	ma->sss_front= 1.0f;
 	ma->sss_back= 1.0f;
 
-	ma->mode= MA_TRACEBLE|MA_SHADBUF|MA_SHADOW|MA_RAYBIAS|MA_TANGENT_STR;
+	ma->mode= MA_TRACEBLE|MA_SHADBUF|MA_SHADOW|MA_RAYBIAS|MA_TANGENT_STR|MA_ZTRANSP;
 
 	ma->preview = NULL;
 }
