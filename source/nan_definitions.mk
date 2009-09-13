a4532b202b960fcff3049fa047c8e828802e91ce
Guillermo S. Romero <gsr.b3d@infernal-iceberg.com>
2009-09-13 22:08:27 +0000

    Prepare for removal of FFMPEG from extern. make clean and full rebuild recommended.

--- source/nan_definitions.mk
+++ source/nan_definitions.mk
@@ -138,9 +138,9 @@ ifndef CONFIG_GUESS
   endif
 
   ifeq ($(NAN_USE_FFMPEG_CONFIG), true)
-    export NAN_FFMPEG ?= $(shell ffmpeg-config --prefix)
-    export NAN_FFMPEGLIBS ?= $(shell ffmpeg-config --libs avformat avcodec)
-    export NAN_FFMPEGCFLAGS ?= $(shell ffmpeg-config --cflags)
+    export NAN_FFMPEG = $(shell pkg-config --variable=prefix libavcodec) # Assume they are all in the same prefix
+    export NAN_FFMPEGLIBS = $(shell pkg-config --libs libavcodec libavdevice libavformat libswscale libavutil)
+    export NAN_FFMPEGCFLAGS = $(shell pkg-config --cflags libavcodec libavdevice libavformat libswscale libavutil)
   endif
 
   # Platform Dependent settings go below:
@@ -335,12 +335,6 @@ ifndef CONFIG_GUESS
     export NAN_SDLLIBS ?= $(shell sdl-config --libs)
     export NAN_SDLCFLAGS ?= $(shell sdl-config --cflags)
     export NAN_SAMPLERATE ?= /usr
-
-ifneq ($(NAN_USE_FFMPEG_CONFIG), true)
-    export NAN_FFMPEG ?= /usr
-    export NAN_FFMPEGLIBS ?= -L$(NAN_FFMPEG)/lib -lavformat -lavcodec -lavutil -lswscale -lavdevice -ldts -lz
-    export NAN_FFMPEGCFLAGS ?= -I$(NAN_FFMPEG)/include
-endif
 
     ifeq ($(WITH_OPENEXR), true)
       export NAN_OPENEXR ?= $(shell pkg-config --variable=prefix OpenEXR )
