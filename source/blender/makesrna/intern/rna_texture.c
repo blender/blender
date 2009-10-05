4df1e400066b30a0bf378b17b6c2168456920be1
Thomas Dinges <blender@dingto.org>
2009-10-05 15:59:12 +0000

    Fixed [#19539] Texture > RGB Factor property doesn't appear in the UI

--- source/blender/makesrna/intern/rna_texture.c
+++ source/blender/makesrna/intern/rna_texture.c
@@ -1653,12 +1653,24 @@ static void rna_def_texture(BlenderRNA *brna)
 	RNA_def_property_range(prop, 0.01, 5);
 	RNA_def_property_ui_text(prop, "Contrast", "");
 	RNA_def_property_update(prop, 0, "rna_Texture_update");
-
-	prop= RNA_def_property(srna, "rgb_factor", PROP_FLOAT, PROP_RGB);
+	
+	/* RGB Factor */
+	prop= RNA_def_property(srna, "factor_red", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "rfac");
-	RNA_def_property_array(prop, 3);
 	RNA_def_property_range(prop, 0, 2);
-	RNA_def_property_ui_text(prop, "RGB Factor", "");
+	RNA_def_property_ui_text(prop, "Factor Red", "");
+	RNA_def_property_update(prop, 0, "rna_Texture_update");
+	
+	prop= RNA_def_property(srna, "factor_green", PROP_FLOAT, PROP_NONE);
+	RNA_def_property_float_sdna(prop, NULL, "gfac");
+	RNA_def_property_range(prop, 0, 2);
+	RNA_def_property_ui_text(prop, "Factor Green", "");
+	RNA_def_property_update(prop, 0, "rna_Texture_update");
+	
+	prop= RNA_def_property(srna, "factor_blue", PROP_FLOAT, PROP_NONE);
+	RNA_def_property_float_sdna(prop, NULL, "bfac");
+	RNA_def_property_range(prop, 0, 2);
+	RNA_def_property_ui_text(prop, "Factor Blue", "");
 	RNA_def_property_update(prop, 0, "rna_Texture_update");
 	
 	/* nodetree */
