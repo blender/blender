33cd5fb85a7c50f8bf58c5e1422c90a4d0181aea
William Reynish <william@reynish.com>
2009-10-15 13:52:27 +0000

    Tiny tweak to make Anim Player button wide enough for icon+text.

--- source/blender/editors/interface/interface_templates.c
+++ source/blender/editors/interface/interface_templates.c
@@ -2270,7 +2270,7 @@ void uiTemplateRunningJobs(uiLayout *layout, bContext *C)
 	if(WM_jobs_test(wm, screen))
 		uiDefIconTextBut(block, BUT, B_STOPCAST, ICON_CANCEL, "Capture", 0,0,85,UI_UNIT_Y, NULL, 0.0f, 0.0f, 0, 0, "Stop screencast");
 	if(screen->animtimer)
-		uiDefIconTextBut(block, BUT, B_STOPANIM, ICON_CANCEL, "Anim Player", 0,0,85,UI_UNIT_Y, NULL, 0.0f, 0.0f, 0, 0, "Stop animation playback");
+		uiDefIconTextBut(block, BUT, B_STOPANIM, ICON_CANCEL, "Anim Player", 0,0,100,UI_UNIT_Y, NULL, 0.0f, 0.0f, 0, 0, "Stop animation playback");
 }
 
 
