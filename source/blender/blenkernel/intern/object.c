596d25773a41e801637c2427878ece81aa7bfb2b
Brecht Van Lommel <brechtvanlommel@pandora.be>
2009-10-19 13:21:37 +0000

    Another fix for duplicating objects with particles.

--- source/blender/blenkernel/intern/object.c
+++ source/blender/blenkernel/intern/object.c
@@ -1133,6 +1133,7 @@ ParticleSystem *copy_particlesystem(ParticleSystem *psys)
 	psysn->pathcache= NULL;
 	psysn->childcache= NULL;
 	psysn->edit= NULL;
+	psysn->frand= NULL;
 	psysn->pdd= NULL;
 	
 	psysn->pathcachebufs.first = psysn->pathcachebufs.last = NULL;
