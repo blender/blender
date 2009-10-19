05d33dc6af190a774b61eeff66cc1cb66b5a5319
Brecht Van Lommel <brechtvanlommel@pandora.be>
2009-10-19 11:00:43 +0000

    Fix #19633: material & texture didn't use autocomplete for selecting uv layers yet.

--- release/scripts/ui/buttons_texture.py
+++ release/scripts/ui/buttons_texture.py
@@ -171,7 +171,9 @@ class TEXTURE_PT_mapping(TextureSlotPanel):
 			elif tex.texture_coordinates == 'UV':
 				split = layout.split(percentage=0.3)
 				split.itemL(text="Layer:")
-				split.itemR(tex, "uv_layer", text="")
+				ob = context.object
+				if ob and ob.type == 'MESH': split.item_pointerR(tex, "uv_layer", ob.data, "uv_textures", text="")
+				else: split.itemR(tex, "uv_layer", text="")
 			elif tex.texture_coordinates == 'OBJECT':
 				split = layout.split(percentage=0.3)
 				split.itemL(text="Object:")
