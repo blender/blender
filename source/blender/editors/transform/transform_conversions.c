3266e1045650b8f3855a4978c0fd438da056ce31
Martin Poirier <theeth@yahoo.com>
2009-10-27 18:50:26 +0000

    Proper cast to silence warning and comment to explain WTH is happening here.

--- source/blender/editors/transform/transform_conversions.c
+++ source/blender/editors/transform/transform_conversions.c
@@ -4092,7 +4092,8 @@ static void freeSeqData(TransInfo *t)
 					for(a=0; a<t->total; a++, td++) {
 						seq= ((TransDataSeq *)td->extra)->seq;
 						if ((seq != seq_prev)) {
-							seq->tmp= 1; // XXX what's the intention here? this currently gives a compiler warning
+							/* Tag seq with a non zero value, used by shuffle_seq_time to identify the ones to shuffle */
+							seq->tmp= (void*)1;
 						}
 					}
 
