41c0236aaa1f4b07650f55905ad1cfa886336268
Campbell Barton <ideasman42@gmail.com>
2009-10-31 20:16:59 +0000

    GPL2 header from firebird (without disclaimer), notice theres no copyright attributed and only the GPLv2 (without the v2 or later clause). Contributors list isnt used much in our C code so probably its easier if people just use svn blame for this.
    Can change if this isnt acceptable but I guessed people didnt care so much since most scripts had no header.

--- release/scripts/modules/bpy_sys.py
+++ release/scripts/modules/bpy_sys.py
@@ -1,3 +1,8 @@
+# This software is distributable under the terms of the GNU
+# General Public License (GPL) v2, the text of which can be found at
+# http://www.gnu.org/copyleft/gpl.html. Installing, importing or otherwise
+# using this module constitutes acceptance of the terms of this License.
+
 import bpy
 import os
 
