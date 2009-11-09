0bb70e4fae11603e5a267825d66312a2ef34da39
Campbell Barton <ideasman42@gmail.com>
2009-11-09 11:15:49 +0000

    workaround for crash when dragging nodes

--- source/blender/blenkernel/intern/node.c
+++ source/blender/blenkernel/intern/node.c
@@ -1662,7 +1662,8 @@ void ntreeSocketUseFlags(bNodeTree *ntree)
 	/* tag all thats in use */
 	for(link= ntree->links.first; link; link= link->next) {
 		link->fromsock->flag |= SOCK_IN_USE;
-		link->tosock->flag |= SOCK_IN_USE;
+		if(link->tosock) // FIXME This can be NULL, when dragging a new link in the UI, should probably copy the node tree for preview render - campbell
+			link->tosock->flag |= SOCK_IN_USE;
 	}
 }
 
