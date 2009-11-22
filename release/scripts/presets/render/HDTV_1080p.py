f4a0c9239ff21a75a336ed3f496dd88d3997d28e
Thomas Dinges <blender@dingto.org>
2009-11-22 14:10:58 +0000

    * Added fps and fps_base to some Render presets. They were missing there.

--- release/scripts/presets/render/HDTV_1080p.py
+++ release/scripts/presets/render/HDTV_1080p.py
@@ -3,3 +3,5 @@ bpy.context.scene.render_data.resolution_y = 1080
 bpy.context.scene.render_data.resolution_percentage = 100
 bpy.context.scene.render_data.pixel_aspect_x = 1
 bpy.context.scene.render_data.pixel_aspect_y = 1
+bpy.context.scene.render_data.fps = 24
+bpy.context.scene.render_data.fps_base = 1
