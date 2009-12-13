58808a7befc403dfef341317f864bca36364cbc7
Campbell Barton <ideasman42@gmail.com>
2009-12-13 14:04:36 +0000

    remove inline cleanName function, use bpy.utils.clean_name

--- release/scripts/io/engine_render_pov.py
+++ release/scripts/io/engine_render_pov.py
@@ -41,20 +41,6 @@ def write_pov(filename, scene=None, info_callback = None):
     render = scene.render_data
     world = scene.world
 
-    # --- taken from fbx exporter
-    ## This was used to make V, but faster not to do all that
-    ##valid = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_,.()[]{}'
-    ##v = range(255)
-    ##for c in valid: v.remove(ord(c))
-    v = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,42,43,46,47,58,59,60,61,62,63,64,92,94,96,124,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254]
-    invalid = ''.join([chr(i) for i in v])
-    def cleanName(name):
-        for ch in invalid:	name = name.replace(ch, '_')
-        return name
-    del v
-
-    # --- done with clean name.
-
     def uniqueName(name, nameSeq):
 
         if name not in nameSeq:
@@ -95,7 +81,7 @@ def write_pov(filename, scene=None, info_callback = None):
         else:
             name_orig = DEF_MAT_NAME
 
-        name = materialNames[name_orig] = uniqueName(cleanName(name_orig), materialNames)
+        name = materialNames[name_orig] = uniqueName(bpy.utils.clean_name(name_orig), materialNames)
 
         file.write('#declare %s = finish {\n' % name)
 
