8f5b2e946bd24cc4ee2b7496b1e022cb24f56f4f
Benoit Bolsee <benoit.bolsee@online.be>
2009-12-22 09:46:03 +0000

    BGE bug #20446: revert to orignal code, the problems comes from misunderstanding of matrix theory. More details in bug report.

--- source/blender/python/generic/matrix.c
+++ source/blender/python/generic/matrix.c
@@ -944,7 +944,7 @@ static PyObject *Matrix_mul(PyObject * m1, PyObject * m2)
 				for(z = 0; z < mat1->rowSize; z++) {
 					dot += (mat1->matrix[z][y] * mat2->matrix[x][z]);
 				}
-				mat[((y * mat1->rowSize) + x)] = (float)dot;
+				mat[((x * mat1->colSize) + y)] = (float)dot;
 				dot = 0.0f;
 			}
 		}
