26fcb7f9d2a7c3446490642b970c570e1aa425ee
Matt Ebb <matt@mke3.net>
2010-01-07 10:49:16 +0000

    Fix [#19664] updating key in graph editor doesn't update value for Shapekeyweight in realtime

--- source/blender/editors/space_buttons/space_buttons.c
+++ source/blender/editors/space_buttons/space_buttons.c
@@ -351,6 +351,12 @@ static void buttons_area_listener(ScrArea *sa, wmNotifier *wmn)
 			if(wmn->action == NA_RENAME)
 				ED_area_tag_redraw(sa);
 			break;
+		case NC_ANIMATION:
+			switch(wmn->data) {
+				case ND_KEYFRAME_EDIT:
+					ED_area_tag_redraw(sa);
+					break;
+			}
 	}
 
 	if(wmn->data == ND_KEYS)
