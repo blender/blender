6920a0cc1dac513f78da5cb721aacc6f0427a0d4
Thomas Dinges <blender@dingto.org>
2010-02-07 10:04:18 +0000

    Outliner: Modifier Icons for Smoke and Solidify were missing.

--- source/blender/editors/space_outliner/outliner.c
+++ source/blender/editors/space_outliner/outliner.c
@@ -4206,6 +4206,10 @@ static void tselem_draw_icon(uiBlock *block, int xmax, float x, float y, TreeSto
 						UI_icon_draw(x, y, ICON_MOD_FLUIDSIM); break;
 					case eModifierType_Multires:
 						UI_icon_draw(x, y, ICON_MOD_MULTIRES); break;
+					case eModifierType_Smoke:
+						UI_icon_draw(x, y, ICON_MOD_SMOKE); break;
+					case eModifierType_Solidify:
+						UI_icon_draw(x, y, ICON_MOD_SOLIDIFY); break;
 					default:
 						UI_icon_draw(x, y, ICON_DOT); break;
 				}
