c7662c1cf750ada5e3c64aed6b4730756e96b3fe
Martin Poirier <theeth@yahoo.com>
2010-02-12 21:40:46 +0000

    Calling operators from Python with non-default context was broken (need to assign to the right variable).

--- source/blender/python/intern/bpy_operator.c
+++ source/blender/python/intern/bpy_operator.c
@@ -72,7 +72,7 @@ static PyObject *pyop_call( PyObject * self, PyObject * args)
 	}
 	
 	if(context_str) {
-		if(RNA_enum_value_from_id(operator_context_items, context_str, &operator_ret)==0) {
+		if(RNA_enum_value_from_id(operator_context_items, context_str, &context)==0) {
 			char *enum_str= BPy_enum_as_string(operator_context_items);
 			PyErr_Format(PyExc_TypeError, "Calling operator \"bpy.ops.%s\" error, expected a string enum in (%.200s)", opname, enum_str);
 			MEM_freeN(enum_str);
