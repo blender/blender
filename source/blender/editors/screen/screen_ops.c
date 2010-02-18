b5adeb922f39dc0424bfbf9fa0ce44c9c917a5a9
Campbell Barton <ideasman42@gmail.com>
2010-02-18 17:00:24 +0000

    revert 27010, sorry but this makes playback of animation unusable & jerky, we can provide some sample files for testing.

--- source/blender/editors/screen/screen_ops.c
+++ source/blender/editors/screen/screen_ops.c
@@ -2429,17 +2429,13 @@ static int screen_animation_step(bContext *C, wmOperator *op, wmEvent *event)
 		else sync= (scene->audio.flag & AUDIO_SYNC);
 		
 		if(sync) {
+			/* skip frames */
+			int step = floor(wt->duration * FPS);
 			if(sad->flag & ANIMPLAY_FLAG_REVERSE) // XXX does this option work with audio?
-			{                                     // XXX - no
-				/* skip frames */
-				int step = floor(wt->duration * FPS);
 				scene->r.cfra -= step;
-				wt->duration -= ((float)step)/FPS;
-			}
 			else
-			{
-				scene->r.cfra = floor(sound_sync_scene(scene) * FPS);
-			}
+				scene->r.cfra += step;
+			wt->duration -= ((float)step)/FPS;
 		}
 		else {
 			/* one frame +/- */
