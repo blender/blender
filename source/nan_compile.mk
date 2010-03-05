d08c480cfe4fc2f7a7b57e5ab0005ed587b6ee66
Brecht Van Lommel <brechtvanlommel@pandora.be>
2010-03-05 14:29:48 +0000

    Fix #21374: OS X Makefiles missed optimization flags in 2.5 since at least two years, got (presumably) commented out for some test and never added back.

--- source/nan_compile.mk
+++ source/nan_compile.mk
@@ -94,13 +94,11 @@ ifeq ($(OS),darwin)
     CCFLAGS += -arch $(MACOSX_ARCHITECTURE) #-isysroot $(MACOSX_SDK) -mmacosx-version-min=$(MACOSX_MIN_VERS)
 
     ifeq ($(MACOSX_ARCHITECTURE), $(findstring $(MACOSX_ARCHITECTURE), "i386 x86_64"))
-        REL_CFLAGS += -ftree-vectorize -msse -msse2 -msse3
-        REL_CCFLAGS += -ftree-vectorize -msse -msse2 -msse3
-    endif
-
-    ifeq ($(MACOSX_ARCHITECTURE), x86_64)
-        REL_CFLAGS += -mssse3
-        REL_CCFLAGS += -mssse3
+        REL_CFLAGS += -O2 -ftree-vectorize -msse -msse2 -msse3
+        REL_CCFLAGS += -O2 -ftree-vectorize -msse -msse2 -msse3
+    else
+        REL_CFLAGS += -O2
+        REL_CCFLAGS += -O2
     endif
     
     CPPFLAGS += -D_THREAD_SAFE -fpascal-strings
