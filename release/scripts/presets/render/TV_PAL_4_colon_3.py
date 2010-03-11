5ec57e80b059be1989bcf4df3ab9f235669f34cc
Tom Musgrove <LetterRip@gmail.com>
2010-03-11 07:24:26 +0000

    A number of common rendering presets, looked up and added by Simon Rainerson

---
+++ release/scripts/presets/render/TV_PAL_4_colon_3.py
@@ -0,0 +1,7 @@
+bpy.context.scene.render.resolution_x = 720
+bpy.context.scene.render.resolution_y = 576
+bpy.context.scene.render.resolution_percentage = 100
+bpy.context.scene.render.pixel_aspect_x = 16
+bpy.context.scene.render.pixel_aspect_y = 15
+bpy.context.scene.render.fps = 25
+bpy.context.scene.render.fps_base = 1
