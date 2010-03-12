478e90ffdcd3f00c9346f430583c5d99ffb61337
Tom Musgrove <LetterRip@gmail.com>
2010-03-12 00:22:33 +0000

    Changed NTSC and PAL to be consistent with the ITU BT 601 spec, other variations can be done as custom if the user wants it.

--- release/scripts/presets/render/D16.py
+++
@@ -1,7 +0,0 @@
-bpy.context.scene.render.resolution_x = 2880
-bpy.context.scene.render.resolution_y = 2048
-bpy.context.scene.render.resolution_percentage = 100
-bpy.context.scene.render.pixel_aspect_x = 128
-bpy.context.scene.render.pixel_aspect_y = 135
-bpy.context.scene.render.fps = 24
-bpy.context.scene.render.fps_base = 1
