426aaaa0a29ea2ddaf008142752fc1efb91033f0
Sergey Sharybin <sergey.vfx@gmail.com>
2010-04-04 11:07:42 +0000

    Use DerivedMesh->getNumFaces function in drawSolidSelect() if curve object has got derived mesh.

--- source/blender/editors/space_view3d/drawobject.c
+++ source/blender/editors/space_view3d/drawobject.c
@@ -5329,8 +5329,15 @@ static void drawSolidSelect(Scene *scene, View3D *v3d, ARegion *ar, Base *base)
 	if(ELEM3(ob->type, OB_FONT,OB_CURVE, OB_SURF)) {
 		Curve *cu = ob->data;
 		DerivedMesh *dm = ob->derivedFinal;
+		int hasfaces= 0;
 
-		if (displist_has_faces(&cu->disp) && boundbox_clip(rv3d, ob->obmat, ob->bb ? ob->bb : cu->bb)) {
+		if (dm) {
+			hasfaces= dm->getNumFaces(dm);
+		} else {
+			hasfaces= displist_has_faces(&cu->disp);
+		}
+
+		if (hasfaces && boundbox_clip(rv3d, ob->obmat, ob->bb ? ob->bb : cu->bb)) {
 			draw_index_wire= 0;
 			if (dm) {
 				draw_mesh_object_outline(v3d, ob, dm);
