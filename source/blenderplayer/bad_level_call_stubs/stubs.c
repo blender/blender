f4e312dd1b53d3fa4c65f47bfb9972bbb9f5abab
Thomas Dinges <blender@dingto.org>
2010-04-07 05:48:52 +0000

    Revert a crappy line from revision 28060.

--- source/blenderplayer/bad_level_call_stubs/stubs.c
+++ source/blenderplayer/bad_level_call_stubs/stubs.c
@@ -2,7 +2,7 @@
  * $Id$
  *
  * ***** BEGIN GPL LICENSE BLOCK *****
- *file://localhost/Users/tj/Desktop/rock2.blend
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
