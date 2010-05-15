3db490d20fd37be39723dcfabf4d103ed6837e2f
Andrea Weikert <elubie@gmx.net>
2010-05-15 10:37:21 +0000

    Make creating and saving previews for Lamp, World, Texture, Material and Image datablocks consistent. - For now the larger previews are created at the same time the small preview icons are created - This brings back the previews when appending/linking

--- source/blender/blenloader/intern/readblenentry.c
+++ source/blender/blenloader/intern/readblenentry.c
@@ -239,10 +239,19 @@ LinkNode *BLO_blendhandle_get_previews(BlendHandle *bh, int ofblocktype)
 	for (bhead= blo_firstbhead(fd); bhead; bhead= blo_nextbhead(fd, bhead)) {
 		if (bhead->code==ofblocktype) {
 			ID *id= (ID*) (bhead+1);
-			if ( (GS(id->name) == ID_MA) || (GS(id->name) == ID_TE)) {
-				new_prv = MEM_callocN(sizeof(PreviewImage), "newpreview");
-				BLI_linklist_prepend(&previews, new_prv);
-				looking = 1;
+			switch(GS(id->name))
+			{
+				case ID_MA: /* fall through */
+				case ID_TE: /* fall through */
+				case ID_IM: /* fall through */
+				case ID_WO: /* fall through */
+				case ID_LA: /* fall through */
+					new_prv = MEM_callocN(sizeof(PreviewImage), "newpreview");
+					BLI_linklist_prepend(&previews, new_prv);
+					looking = 1;
+					break;
+				default:
+					break;
 			}
 		} else if (bhead->code==DATA) {
 			if (looking) {
