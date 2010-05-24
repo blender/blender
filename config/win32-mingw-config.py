2d450c9e58051a7ef69f38e144155bab79c7a13d
Sergey Sharybin <sergey.vfx@gmail.com>
2010-05-24 16:07:17 +0000

    Changed BF_TIFF_LIB from libtiff to tiff for MinGW

--- config/win32-mingw-config.py
+++ config/win32-mingw-config.py
@@ -76,7 +76,7 @@ BF_PNG_LIBPATH = '${BF_PNG}/lib'
 WITH_BF_TIFF = True
 BF_TIFF = LIBDIR + '/gcc/tiff'
 BF_TIFF_INC = '${BF_TIFF}/include'
-BF_TIFF_LIB = 'libtiff'
+BF_TIFF_LIB = 'tiff'
 BF_TIFF_LIBPATH = '${BF_TIFF}/lib'
 
 WITH_BF_ZLIB = True
