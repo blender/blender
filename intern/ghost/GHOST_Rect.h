d5cdfb18f68bd86cdb060c54b7af57f46e67c4aa
Mike Erwin <significant.bit@gmail.com>
2010-06-26 01:34:49 +0000

    removed unused #include

--- intern/ghost/GHOST_Rect.h
+++ intern/ghost/GHOST_Rect.h
@@ -228,7 +228,7 @@ inline void GHOST_Rect::unionPoint(GHOST_TInt32 x, GHOST_TInt32 y)
 	if (y < m_t) m_t = y;
 	if (y > m_b) m_b = y;
 }
-#include <stdio.h>
+
 inline void GHOST_Rect::wrapPoint(GHOST_TInt32 &x, GHOST_TInt32 &y, GHOST_TInt32 ofs)
 {
 	GHOST_TInt32 w= getWidth();
