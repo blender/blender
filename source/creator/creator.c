ea4e5a08cdb782d314cee48dde5dff9331054fe3
Campbell Barton <ideasman42@gmail.com>
2010-06-27 00:33:12 +0000

    script to create a man page from blenders --help text, replaced the hand edited man page with this.

--- source/creator/creator.c
+++ source/creator/creator.c
@@ -234,11 +234,11 @@ static int print_help(int argc, char **argv, void *data)
 	BLI_argsPrintArgDoc(ba, "--window-geometry");
 
 	printf("\n");
-	printf ("Game Engine specific options:\n");
+	printf ("Game Engine Specific Options:\n");
 	BLI_argsPrintArgDoc(ba, "-g");
 
 	printf("\n");
-	printf ("Misc options:\n");
+	printf ("Misc Options:\n");
 	BLI_argsPrintArgDoc(ba, "--debug");
 	BLI_argsPrintArgDoc(ba, "--debug-fpe");
 
@@ -273,10 +273,25 @@ static int print_help(int argc, char **argv, void *data)
 	printf ("Other Options:\n");
 	BLI_argsPrintOtherDoc(ba);
 
+	printf ("Argument Parsing:\n");
+	printf ("\targuments must be separated by white space. eg\n");
+	printf ("\t\t\"blender -ba test.blend\"\n");
+	printf ("\t...will ignore the 'a'\n");
+	printf ("\t\t\"blender -b test.blend -f8\"\n");
+	printf ("\t...will ignore 8 because there is no space between the -f and the frame value\n\n");
+
+	printf ("Argument Order:\n");
+	printf ("Arguments are executed in the order they are given. eg\n");
+	printf ("\t\t\"blender --background test.blend --render-frame 1 --render-output /tmp\"\n");
+	printf ("\t...will not render to /tmp because '--render-frame 1' renders before the output path is set\n");
+	printf ("\t\t\"blender --background --render-output /tmp test.blend --render-frame 1\"\n");
+	printf ("\t...will not render to /tmp because loading the blend file overwrites the render output that was set\n");
+	printf ("\t\t\"blender --background test.blend --render-output /tmp --render-frame 1\" works as expected.\n\n");
+
 	printf ("\nEnvironment Variables:\n");
 	printf ("  $HOME\t\t\tStore files such as .blender/ .B.blend .Bfs .Blog here.\n");
 	printf ("  $BLENDERPATH  System directory to use for data files and scripts.\n");
-	printf ("                For this build of blender the default BLENDERPATH is...\n");
+	printf ("                For this build of blender the default $BLENDERPATH is...\n");
 	printf ("                \"%s\"\n", blender_path);
 	printf ("                setting the $BLENDERPATH will override this\n");
 #ifdef WIN32
@@ -288,19 +303,6 @@ static int print_help(int argc, char **argv, void *data)
 	printf ("  $SDL_AUDIODRIVER  LibSDL audio driver - alsa, esd, alsa, dma.\n");
 #endif
 	printf ("  $PYTHONHOME   Path to the python directory, eg. /usr/lib/python.\n\n");
-
-	printf ("Note: Arguments must be separated by white space. eg:\n");
-	printf ("    \"blender -ba test.blend\"\n");
-	printf ("  ...will ignore the 'a'\n");
-	printf ("    \"blender -b test.blend -f8\"\n");
-	printf ("  ...will ignore 8 because there is no space between the -f and the frame value\n\n");
-
-	printf ("Note: Arguments are executed in the order they are given. eg:\n");
-	printf ("    \"blender --background test.blend --render-frame 1 --render-output /tmp\"\n");
-	printf ("  ...will not render to /tmp because '--render-frame 1' renders before the output path is set\n");
-	printf ("    \"blender --background --render-output /tmp test.blend --render-frame 1\"\n");
-	printf ("  ...will not render to /tmp because loading the blend file overwrites the render output that was set\n");
-	printf ("    \"blender --background test.blend --render-output /tmp --render-frame 1\" works as expected.\n\n");
 
 	exit(0);
 
