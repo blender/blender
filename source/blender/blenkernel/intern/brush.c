3a042d205321574838b08fcfe99aa2d97fc4bc97
Jason Wilkins <Jason.A.Wilkins@gmail.com>
2010-07-22 21:10:29 +0000

    Fix [#22965] Icons for new brushes won't get sticky

--- source/blender/blenkernel/intern/brush.c
+++ source/blender/blenkernel/intern/brush.c
@@ -132,6 +132,8 @@ Brush *copy_brush(Brush *brush)
 	if (brush->mtex.tex)
 		id_us_plus((ID*)brush->mtex.tex);
 
+	//brushn->preview= NULL;
+
 	if (brush->icon_imbuf)
 		brushn->icon_imbuf= IMB_dupImBuf(brush->icon_imbuf);
 
