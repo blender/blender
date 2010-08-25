fc0af23d2de880c5555fceadeb68f270ccc3fe7a
Dalai Felinto <dfelinto@gmail.com>
2010-08-25 00:15:10 +0000

    Bugfix: [#23472] mesh.uv_textures.new() creates a MeshColorLayer

--- source/blender/makesrna/intern/rna_mesh.c
+++ source/blender/makesrna/intern/rna_mesh.c
@@ -1715,7 +1715,7 @@ static void rna_def_uv_textures(BlenderRNA *brna, PropertyRNA *cprop)
 	RNA_def_function_flag(func, FUNC_USE_CONTEXT);
 	RNA_def_function_ui_description(func, "Add a UV texture layer to Mesh.");
 	parm= RNA_def_string(func, "name", "UVTex", 0, "", "UV Texture name.");
-	parm= RNA_def_pointer(func, "layer", "MeshColorLayer", "", "The newly created layer.");
+	parm= RNA_def_pointer(func, "layer", "MeshTextureFaceLayer", "", "The newly created layer.");
 	RNA_def_function_return(func, parm);
 
 /*
