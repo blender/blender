44cbc14771f2117537e2624675a00d583a4a4ec5
Mitchell Stokes <mogurijin@gmail.com>
2010-08-27 10:05:33 +0000

    The duplicate Text option in the TexFace panel came from a bad rename of tex -> use_bitmap_text instead of use_image, so I'm adding the use_image (Tex) back to the TexFace panel.

--- release/scripts/ui/properties_data_mesh.py
+++ release/scripts/ui/properties_data_mesh.py
@@ -300,6 +300,7 @@ class DATA_PT_texface(MeshButtonsPanel, bpy.types.Panel):
             split = layout.split()
             col = split.column()
 
+            col.prop(tf, "use_image")
             col.prop(tf, "use_light")
             col.prop(tf, "hide")
             col.prop(tf, "use_collision")
