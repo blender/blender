a98b93be575c5618c0d2c3ae5338b4b3138cd5e3
Thomas Dinges <blender@dingto.org>
2010-08-31 12:54:17 +0000

    2.5 Fluid UI: * Required memory is now shown on the Bake Operator, rather as an extra label.

--- release/scripts/ui/properties_physics_fluid.py
+++ release/scripts/ui/properties_physics_fluid.py
@@ -58,10 +58,8 @@ class PHYSICS_PT_fluid(PhysicButtonsPanel, bpy.types.Panel):
             split.operator("object.modifier_add", text="Add").type = 'FLUID_SIMULATION'
             split.label()
 
-            fluid = None
 
-
-        if fluid:
+        if md:
             row = layout.row()
             row.prop(fluid, "type")
             if fluid.type not in ('NONE', 'DOMAIN', 'PARTICLE'):
@@ -72,7 +70,7 @@ class PHYSICS_PT_fluid(PhysicButtonsPanel, bpy.types.Panel):
                 layout.active = fluid.use
 
             if fluid.type == 'DOMAIN':
-                layout.operator("fluid.bake", text="Bake Fluid Simulation", icon='MOD_FLUIDSIM')
+                layout.operator("fluid.bake", text="Bake (Req. Memory: %s)" % fluid.memory_estimate, icon='MOD_FLUIDSIM')
                 split = layout.split()
 
                 col = split.column()
@@ -82,7 +80,7 @@ class PHYSICS_PT_fluid(PhysicButtonsPanel, bpy.types.Panel):
                 col.prop(fluid, "render_display_mode", text="")
 
                 col = split.column()
-                col.label(text="Required Memory: " + fluid.memory_estimate)
+                col.label()
                 col.prop(fluid, "preview_resolution", text="Preview")
                 col.label(text="Viewport Display:")
                 col.prop(fluid, "viewport_display_mode", text="")
