e5b9ad3817a348cd8e34e073dd0f0497f8d58b1d
Benoit Bolsee <benoit.bolsee@online.be>
2010-09-01 08:09:23 +0000

    Added missing ID_NEW() in set_sca_new_poins_ob() as pointed out by Dalai.

--- source/blender/blenkernel/intern/sca.c
+++ source/blender/blenkernel/intern/sca.c
@@ -572,9 +572,22 @@ void set_sca_new_poins_ob(Object *ob)
 				bObjectActuator *oa= act->data;
 				ID_NEW(oa->reference);
 			}
-			else if(act->type==ACT_SCENE) {
-				bSceneActuator *sca= act->data;
-				ID_NEW(sca->camera);
+			else if(act->type==ACT_MESSAGE) {
+				bMessageActuator *ma= act->data;
+				ID_NEW(ma->toObject);
+			}
+			else if(act->type==ACT_PARENT) {
+				bParentActuator *para = act->data;
+				ID_NEW(para->ob);
+			}
+			else if(act->type==ACT_ARMATURE) {
+				bArmatureActuator *aa = act->data;
+				ID_NEW(aa->target);
+				ID_NEW(aa->subtarget);
+			}
+			else if(act->type==ACT_PROPERTY) {
+				bPropertyActuator *pa= act->data;
+				ID_NEW(pa->ob);
 			}
 		}
 		act= act->next;
