3a2fc4d6dc1c6936b7f1e380602cb39231e7c165
Dalai Felinto <dfelinto@gmail.com>
2010-09-11 19:38:11 +0000

    BGE module reshuffling - VideoTexture -> bge.texture this was the one module left to be renamed.
    We didn't get to any conclusion in the mailing list, but Campbell suggested texture and I think its a good name. We can change any time before next Beta, but for now I think it's handy to have something.
    
    The original proposal is from May:
    http://lists.blender.org/pipermail/bf-committers/2010-May/027587.html

--- source/gameengine/Ketsji/KX_PythonInit.cpp
+++ source/gameengine/Ketsji/KX_PythonInit.cpp
@@ -1997,7 +1997,7 @@ void setupGamePython(KX_KetsjiEngine* ketsjiengine, KX_Scene* startscene, Main *
 	initVideoTexture();
 
 	/* could be done a lot more nicely, but for now a quick way to get bge.* working */
-	PyRun_SimpleString("sys = __import__('sys');mod = sys.modules['bge'] = type(sys)('bge');mod.__dict__.update({'logic':__import__('GameLogic'), 'render':__import__('Rasterizer'), 'events':__import__('GameKeys'), 'constraints':__import__('PhysicsConstraints'), 'types':__import__('GameTypes')});");
+	PyRun_SimpleString("sys = __import__('sys');mod = sys.modules['bge'] = type(sys)('bge');mod.__dict__.update({'logic':__import__('GameLogic'), 'render':__import__('Rasterizer'), 'events':__import__('GameKeys'), 'constraints':__import__('PhysicsConstraints'), 'types':__import__('GameTypes'), 'texture':__import__('VideoTexture')});");
 }
 
 static struct PyModuleDef Rasterizer_module_def = {
