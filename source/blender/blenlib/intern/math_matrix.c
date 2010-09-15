d2a12182102aecb9a3d4987dd126776f4d560a55
Joshua Leung <aligorith@gmail.com>
2010-09-15 04:15:42 +0000

    Two small bugfixes: - A property used in an 'active' poll for UI code in the NLA Editor was missed during the RNA renaming madness, leading to error prints in the console
    - For matrix blending code, 'fsize' arrays were being declared wrong size (4 elements long instead of 3).

--- source/blender/blenlib/intern/math_matrix.c
+++ source/blender/blenlib/intern/math_matrix.c
@@ -998,7 +998,7 @@ void rotate_m4(float mat[][4], const char axis, const float angle)
 void blend_m3_m3m3(float out[][3], float dst[][3], float src[][3], float srcweight)
 {
 	float squat[4], dquat[4], fquat[4];
-	float ssize[3], dsize[3], fsize[4];
+	float ssize[3], dsize[3], fsize[3];
 	float rmat[3][3], smat[3][3];
 	
 	mat3_to_quat(dquat,dst);
@@ -1020,7 +1020,7 @@ void blend_m3_m3m3(float out[][3], float dst[][3], float src[][3], float srcweig
 void blend_m4_m4m4(float out[][4], float dst[][4], float src[][4], float srcweight)
 {
 	float squat[4], dquat[4], fquat[4];
-	float ssize[3], dsize[3], fsize[4];
+	float ssize[3], dsize[3], fsize[3];
 	float sloc[3], dloc[3], floc[3];
 	
 	mat4_to_quat(dquat,dst);
