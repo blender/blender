764a760c66608a396daa0dfe398d665bee8ce7f3
Daniel Salazar <zanqdo@gmail.com>
2010-09-22 08:51:18 +0000

    Increasing compositor blur max size

--- source/blender/makesrna/intern/rna_nodetree.c
+++ source/blender/makesrna/intern/rna_nodetree.c
@@ -871,13 +871,13 @@ static void def_cmp_blur(StructRNA *srna)
 	
 	prop = RNA_def_property(srna, "size_x", PROP_INT, PROP_NONE);
 	RNA_def_property_int_sdna(prop, NULL, "sizex");
-	RNA_def_property_range(prop, 0, 256);
+	RNA_def_property_range(prop, 0, 2048);
 	RNA_def_property_ui_text(prop, "Size X", "");
 	RNA_def_property_update(prop, NC_NODE|NA_EDITED, "rna_Node_update");
 	
 	prop = RNA_def_property(srna, "size_y", PROP_INT, PROP_NONE);
 	RNA_def_property_int_sdna(prop, NULL, "sizey");
-	RNA_def_property_range(prop, 0, 256);
+	RNA_def_property_range(prop, 0, 2048);
 	RNA_def_property_ui_text(prop, "Size Y", "");
 	RNA_def_property_update(prop, NC_NODE|NA_EDITED, "rna_Node_update");
 
