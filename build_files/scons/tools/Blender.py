c5086987ece1d0c0bb090ac50bff1ecb46b363fe
Campbell Barton <ideasman42@gmail.com>
2010-10-03 11:33:44 +0000

    fix for scons with recent buildinfo change

--- build_files/scons/tools/Blender.py
+++ build_files/scons/tools/Blender.py
@@ -295,9 +295,12 @@ def buildinfo(lenv, build_type):
                                     'BUILD_TYPE="%s"'%(build_type),
                                     'BUILD_REV="%s"'%(build_rev),
                                     'NAN_BUILDINFO',
-				    'BUILD_PLATFORM="%s:%s"'%(platform.system(), platform.architecture()[0])])
-        obj = [lenv.Object (root_build_dir+'source/creator/%s_buildinfo'%build_type,
-                        [root_build_dir+'source/creator/buildinfo.c'])]
+                                    'BUILD_PLATFORM="%s:%s"'%(platform.system(), platform.architecture()[0])])
+
+        lenv.Append (CPPPATH = [root_build_dir+'source/blender/blenkernel'])
+
+        obj = [lenv.Object (root_build_dir+'source/creator/%s_buildinfo'%build_type, [root_build_dir+'source/creator/buildinfo.c'])]
+
     return obj
 
 ##### END LIB STUFF ############
