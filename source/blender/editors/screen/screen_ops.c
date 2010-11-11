662b35a2c0815258adb8abc8265dca01ba703b3b
Joshua Leung <aligorith@gmail.com>
2010-11-11 05:11:37 +0000

    Preview Range operators were broken in Graph Editor

--- source/blender/editors/screen/screen_ops.c
+++ source/blender/editors/screen/screen_ops.c
@@ -144,7 +144,7 @@ int ED_operator_animview_active(bContext *C)
 {
 	if(ED_operator_areaactive(C)) {
 		SpaceLink *sl= (SpaceLink *)CTX_wm_space_data(C);
-		if (sl && (ELEM5(sl->spacetype, SPACE_SEQ, SPACE_SOUND, SPACE_ACTION, SPACE_NLA, SPACE_TIME)))
+		if (sl && (ELEM6(sl->spacetype, SPACE_SEQ, SPACE_SOUND, SPACE_ACTION, SPACE_NLA, SPACE_IPO, SPACE_TIME)))
 			return TRUE;
 	}
 
