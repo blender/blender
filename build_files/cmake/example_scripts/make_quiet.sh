9d289c602d289e0dbe2af951dbefeb67c89434af
Campbell Barton <ideasman42@gmail.com>
2010-12-21 04:55:46 +0000

    better make_quiet.sh filter with threaded builds.

--- build_files/cmake/example_scripts/make_quiet.sh
+++ build_files/cmake/example_scripts/make_quiet.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # filters CMake output to be more like nan-makefiles
 
-FILTER="^Scanning \|Linking \(C\|CXX\) static library \|Built target "
-make $@ | grep --line-buffered -v "$FILTER" | sed  -e 's/^.*\//  /'
+FILTER="^\[ *[0-9]*%] \|^Built target "
+make $@ | \
+		sed -u -e 's/^Linking .*\//Linking /' | \
+		sed -u -e 's/^.*\//  /' | \
+		grep --line-buffered -v "$FILTER"
+
 echo "Build Done"
