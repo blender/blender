a0b134cd888ba481cae83328c8c7081dd1a5d18c
Campbell Barton <ideasman42@gmail.com>
2011-01-06 05:04:07 +0000

    bugfix [#25488] Game engine crash when try to run after blender app start - also found other places where utf8 isnt ensured. - remove duplicate NULL checks in object_edit.c

--- source/blender/editors/object/object_edit.c
+++ source/blender/editors/object/object_edit.c
@@ -523,11 +523,11 @@ static int editmode_toggle_poll(bContext *C)
 	/* covers proxies too */
 	if(ELEM(NULL, ob, ob->data) || ((ID *)ob->data)->lib)
 		return 0;
-	
-	if (ob && (ob->restrictflag & OB_RESTRICT_VIEW))
+
+	if (ob->restrictflag & OB_RESTRICT_VIEW)
 		return 0;
 
-	return ob && (ob->type == OB_MESH || ob->type == OB_ARMATURE ||
+	return (ob->type == OB_MESH || ob->type == OB_ARMATURE ||
 			  ob->type == OB_FONT || ob->type == OB_MBALL ||
 			  ob->type == OB_LATTICE || ob->type == OB_SURF ||
 			  ob->type == OB_CURVE);
