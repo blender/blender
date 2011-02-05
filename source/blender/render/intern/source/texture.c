311f5a8d78d22b2d1034f1e54c783c1b71100e9a
M.G. Kishalmi <lmg@kishalmi.net>
2011-02-05 11:46:44 +0000

    accidentally left scaling code commented out when testing.

--- source/blender/render/intern/source/texture.c
+++ source/blender/render/intern/source/texture.c
@@ -1607,7 +1607,6 @@ static void texco_mapping(ShadeInput* shi, Tex* tex, MTex* mtex, float* co, floa
 		do_2d_mapping(mtex, texvec, shi->vlr, shi->facenor, dxt, dyt);
 
 		// translate and scale
-		/*
 		texvec[0] = mtex->size[0]*(texvec[0] - 0.5f) + mtex->ofs[0] + 0.5f;
 		texvec[1] = mtex->size[1]*(texvec[1] - 0.5f) + mtex->ofs[1] + 0.5f;
 		if (shi->osatex) {
@@ -1616,7 +1615,6 @@ static void texco_mapping(ShadeInput* shi, Tex* tex, MTex* mtex, float* co, floa
 			dyt[0] = mtex->size[0]*dyt[0];
 			dyt[1] = mtex->size[1]*dyt[1];
 		}
-		*/
 		
 		/* problem: repeat-mirror is not a 'repeat' but 'extend' in imagetexture.c */
 		// TXF: bug was here, only modify texvec when repeat mode set, old code affected other modes too.
