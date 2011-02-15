52c08fa4d351e37506f164dde77486af69b752ce
Campbell Barton <ideasman42@gmail.com>
2011-02-15 18:33:05 +0000

    CPack packaging support from Dan Eicher, tested to build redhat RPM's, a Mac bundle also defined.

---
+++ build_files/cmake/RpmBuild.cmake
@@ -0,0 +1,34 @@
+#
+# VLMC RPM Finder
+# Authors: Rohit Yadav <rohityadav89@gmail.com>
+#
+
+find_program(RPMBUILD
+    NAMES rpmbuild
+    PATHS "/usr/bin")
+
+if ( RPMBUILD )
+    get_filename_component(RPMBUILD_PATH ${RPMBUILD} ABSOLUTE)
+    message(STATUS "Found rpmbuild : ${RPMBUILD_PATH}")
+    set(RPMBUILD_FOUND "YES")
+else ( RPMBUILD ) 
+    message(STATUS "rpmbuild NOT found. RPM generation will not be available")
+    set(RPMBUILD_FOUND "NO")
+endif ( RPMBUILD )
+#
+# VLMC RPM Finder
+# Authors: Rohit Yadav <rohityadav89@gmail.com>
+#
+
+find_program(RPMBUILD
+    NAMES rpmbuild
+    PATHS "/usr/bin")
+
+if ( RPMBUILD )
+    get_filename_component(RPMBUILD_PATH ${RPMBUILD} ABSOLUTE)
+    message(STATUS "Found rpmbuild : ${RPMBUILD_PATH}")
+    set(RPMBUILD_FOUND "YES")
+else ( RPMBUILD ) 
+    message(STATUS "rpmbuild NOT found. RPM generation will not be available")
+    set(RPMBUILD_FOUND "NO")
+endif ( RPMBUILD )
