8ce6da76b6e5c720cf9bbeaf87feca8d02dcd07a
Campbell Barton <ideasman42@gmail.com>
2011-02-24 14:03:29 +0000

    patch [#26178] build_debian.sh: Use BLENDER_VERSION_CHAR from Shinsuke Irie (irie)     also apply to pacman.

--- build_files/package_spec/build_debian.sh
+++ build_files/package_spec/build_debian.sh
@@ -14,9 +14,16 @@ cp -a build_files/package_spec/debian .
 # value may be formatted: 35042:35051M
 BLENDER_REVISION=$(svnversion | cut -d: -f2 | tr -dc 0-9)
 
-blender_srcdir=$PWD
-blender_version=$(grep BLENDER_VERSION $blender_srcdir/source/blender/blenkernel/BKE_blender.h | tr -dc 0-9)
+blender_version=$(grep BLENDER_VERSION source/blender/blenkernel/BKE_blender.h | tr -dc 0-9)
+blender_version_char=$(sed -ne 's/.*BLENDER_VERSION_CHAR.*\([a-z]\)$/\1/p' source/blender/blenkernel/BKE_blender.h)
 BLENDER_VERSION=$(expr $blender_version / 100).$(expr $blender_version % 100)
+
+# map the version a -> 1, to conform to debian naming convention
+# not to be confused with blender's internal subversions
+if [ "$blender_version_char" ]; then
+    BLENDER_VERSION=${BLENDER_VERSION}.$(expr index abcdefghijklmnopqrstuvwxyz $blender_version_char)
+fi
+
 DEB_VERSION=${BLENDER_VERSION}+svn${BLENDER_REVISION}-bf
 
 # update debian/changelog
