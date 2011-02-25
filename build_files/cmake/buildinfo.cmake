aa9c91f01a67d12e37809ad29f46bc46c19fd3d9
Guillermo S. Romero <gsr.b3d@infernal-iceberg.com>
2011-02-25 04:02:58 +0000

    Make buildinfo not fail with git-svn as suggested by Nicholas Bishop. Improve formating and comments a bit.

--- build_files/cmake/buildinfo.cmake
+++ build_files/cmake/buildinfo.cmake
@@ -1,17 +1,21 @@
-# this is called by cmake as an extermal process from
+# This is called by cmake as an extermal process from
 # ./source/creator/CMakeLists.txt to write ./source/creator/buildinfo.h
 
-# the FindSubversion.cmake module is part of the standard distribution
+# The FindSubversion.cmake module is part of the standard distribution
 include(FindSubversion)
-# extract working copy information for SOURCE_DIR into MY_XXX variables
-if(Subversion_FOUND)
-	Subversion_WC_INFO(${SOURCE_DIR} MY)
-else()
-	set(MY_WC_REVISION "unknown")
+
+# Extract working copy information for SOURCE_DIR into MY_XXX variables
+# with a default in case anything fails, for examble when using git-svn
+set(MY_WC_REVISION "unknown")
+# Guess if this is a SVN working copy and then look up the revision
+if(EXISTS ${CMAKE_SOURCE_DIR}/.svn/)
+	if(Subversion_FOUND)
+		bversion_WC_INFO(${SOURCE_DIR} MY)
+	endif()
 endif()
 
 # BUILD_PLATFORM and BUILD_PLATFORM are taken from CMake
-# But BUILD_DATE is plataform dependant
+# but BUILD_DATE and BUILD_TIME are plataform dependant
 if(UNIX)
 	execute_process(COMMAND date "+%Y-%m-%d" OUTPUT_VARIABLE BUILD_DATE OUTPUT_STRIP_TRAILING_WHITESPACE)
 	execute_process(COMMAND date "+%H:%M:%S" OUTPUT_VARIABLE BUILD_TIME OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -21,14 +25,14 @@ if(WIN32)
 	execute_process(COMMAND cmd /c time /t OUTPUT_VARIABLE BUILD_TIME OUTPUT_STRIP_TRAILING_WHITESPACE)
 endif()
 
-# write a file with the SVNVERSION define
+# Write a file with the SVNVERSION define
 file(WRITE buildinfo.h.txt
 	"#define BUILD_REV ${MY_WC_REVISION}\n"
 	"#define BUILD_DATE ${BUILD_DATE}\n"
 	"#define BUILD_TIME ${BUILD_TIME}\n"
 )
 
-# copy the file to the final header only if the version changes
-# reduces needless rebuilds
+# Copy the file to the final header only if the version changes
+# and avoid needless rebuilds
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
                         buildinfo.h.txt buildinfo.h)
