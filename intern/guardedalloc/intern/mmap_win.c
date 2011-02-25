56a4b21695739b81d1c2ca06bde1483e18107da6
Nathan Letwory <nathan@letworyinteractive.com>
2011-02-25 11:41:12 +0000

    doxygen: intern/guardedalloc tagged

--- intern/guardedalloc/intern/mmap_win.c
+++ intern/guardedalloc/intern/mmap_win.c
@@ -1,4 +1,4 @@
-/**
+/*
  * $Id$
  *
  * ***** BEGIN GPL LICENSE BLOCK *****
@@ -26,6 +26,11 @@
  *
  * ***** END GPL LICENSE BLOCK *****
  */
+
+/** \file guardedalloc/intern/mmap_win.c
+ *  \ingroup MEM
+ */
+
  
 #if defined(WIN32)
 
