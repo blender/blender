34dd52046633a8a7f9e11a937fd5bde478778abd
Nathan Letwory <nathan@letworyinteractive.com>
2011-02-25 10:47:28 +0000

    doxygen: intern/decimation tagged

--- intern/decimation/intern/LOD_FaceNormalEditor.h
+++ intern/decimation/intern/LOD_FaceNormalEditor.h
@@ -1,4 +1,4 @@
-/**
+/*
  * $Id$
  * ***** BEGIN GPL LICENSE BLOCK *****
  *
@@ -25,6 +25,11 @@
  *
  * ***** END GPL LICENSE BLOCK *****
  */
+
+/** \file decimation/intern/LOD_FaceNormalEditor.h
+ *  \ingroup decimation
+ */
+
 
 #ifndef NAN_INCLUDED_FaceNormalEditor_h
 #define NAN_INCLUDED_FaceNormalEditor_h
--- intern/decimation/intern/LOD_MeshPrimitives.cpp
+++ intern/decimation/intern/LOD_MeshPrimitives.cpp
@@ -1,4 +1,4 @@
-/**
+/*
  * $Id$
  * ***** BEGIN GPL LICENSE BLOCK *****
  *
@@ -25,6 +25,11 @@
  *
  * ***** END GPL LICENSE BLOCK *****
  */
+
+/** \file decimation/intern/LOD_MeshPrimitives.cpp
+ *  \ingroup decimation
+ */
+
 
 #include "LOD_MeshPrimitives.h"
 
