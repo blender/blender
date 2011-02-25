b07988274ea04cef641ef5203b24fc281a6332c7
Nathan Letwory <nathan@letworyinteractive.com>
2011-02-25 11:49:19 +0000

    doxygen: intern/moto tagged.

--- intern/moto/include/MT_Tuple4.h
+++ intern/moto/include/MT_Tuple4.h
@@ -1,4 +1,4 @@
-/**
+/*
  * $Id$
  * ***** BEGIN GPL LICENSE BLOCK *****
  *
@@ -25,6 +25,11 @@
  *
  * ***** END GPL LICENSE BLOCK *****
  */
+
+/** \file moto/include/MT_Tuple4.h
+ *  \ingroup moto
+ */
+
 
 /*
 
--- intern/moto/include/NM_Scalar.h
+++ intern/moto/include/NM_Scalar.h
@@ -1,4 +1,4 @@
-/**
+/*
  * $Id$
  * ***** BEGIN GPL LICENSE BLOCK *****
  *
@@ -25,6 +25,11 @@
  *
  * ***** END GPL LICENSE BLOCK *****
  */
+
+/** \file moto/include/NM_Scalar.h
+ *  \ingroup moto
+ */
+
 
 #include <math.h>
 #include <iostream>
