87acf919a5023e690e67bfbf3846081a70838bea
Campbell Barton <ideasman42@gmail.com>
2011-02-27 14:08:33 +0000

    lazy load pydoc module. move help() replacement into console code rather then on startup since importing pydoc pulls in lots of other modules too.

--- release/scripts/modules/bpy/__init__.py
+++ release/scripts/modules/bpy/__init__.py
@@ -37,18 +37,6 @@ import sys as _sys
 
 def _main():
 
-    ## security issue, dont allow the $CWD in the path.
-    ## note: this removes "" but not "." which are the same, security
-    ## people need to explain how this is even a fix.
-    # _sys.path[:] = filter(None, _sys.path)
-
-    # because of how the console works. we need our own help() pager func.
-    # replace the bold function because it adds crazy chars
-    import pydoc
-    pydoc.getpager = lambda: pydoc.plainpager
-    pydoc.Helper.getline = lambda self, prompt: None
-    pydoc.TextDoc.use_bold = lambda self, text: text
-
     # Possibly temp. addons path
     from os.path import join, dirname, normpath
     _sys.path.append(normpath(join(dirname(__file__), "..", "..", "addons", "modules")))
