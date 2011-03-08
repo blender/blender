e713d76f0e16d379d5993ba6439fef8ba7949783
Campbell Barton <ideasman42@gmail.com>
2011-03-08 01:23:42 +0000

    patch [#26404] UnicodeDecodeError from user: perfection cat (sindra1961)

--- source/blender/python/intern/bpy.c
+++ source/blender/python/intern/bpy.c
@@ -163,7 +163,7 @@ static PyObject *bpy_user_resource(PyObject *UNUSED(self), PyObject *args, PyObj
 	if (!path)
 		path = BLI_get_user_folder_notest(folder_id, subdir);
 
-	return PyUnicode_FromString(path ? path : "");
+	return PyUnicode_DecodeFSDefault(path ? path : "");
 }
 
 static PyMethodDef meth_bpy_script_paths = {"script_paths", (PyCFunction)bpy_script_paths, METH_NOARGS, bpy_script_paths_doc};
