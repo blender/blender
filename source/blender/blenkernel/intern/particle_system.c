e134e0139ff2ec3888c7599b64b1a0e57bce0f76
Janne Karhu <jhkarh@gmail.com>
2011-03-19 13:36:43 +0000

    ugh, left in a debug print in the previous commit

--- source/blender/blenkernel/intern/particle_system.c
+++ source/blender/blenkernel/intern/particle_system.c
@@ -3230,7 +3230,7 @@ static void collision_fail(ParticleData *pa, ParticleCollision *col)
 	mul_v3_fl(pa->state.vel, col->inv_timestep);
 
 
-	printf("max iterations\n");
+	/* printf("max iterations\n"); */
 }
 
 /* Particle - Mesh collision detection and response
