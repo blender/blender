c7f37b84d8dff7091ae7952f6fc4336027c5befa
Erwin Coumans <blender@erwincoumans.com>
2011-03-22 16:30:46 +0000

    remove constructors, they cause compile errors under Fedora

--- extern/bullet2/src/BulletSoftBody/btSoftBody.h
+++ extern/bullet2/src/BulletSoftBody/btSoftBody.h
@@ -205,14 +205,12 @@ public:
 		btScalar				m_kAST;			// Area/Angular stiffness coefficient [0,1]
 		btScalar				m_kVST;			// Volume stiffness coefficient [0,1]
 		int						m_flags;		// Flags
-		Material() : Element() {}
 	};
 
 	/* Feature		*/ 
 	struct	Feature : Element
 	{
 		Material*				m_material;		// Material
-		Feature() : Element() {}
 	};
 	/* Node			*/ 
 	struct	Node : Feature
@@ -226,7 +224,6 @@ public:
 		btScalar				m_area;			// Area
 		btDbvtNode*				m_leaf;			// Leaf data
 		int						m_battach:1;	// Attached
-		Node() : Feature() {}
 	};
 	/* Link			*/ 
 	struct	Link : Feature
@@ -238,7 +235,6 @@ public:
 		btScalar				m_c1;			// rl^2
 		btScalar				m_c2;			// |gradient|^2/c0
 		btVector3				m_c3;			// gradient
-		Link() : Feature() {}
 	};
 	/* Face			*/ 
 	struct	Face : Feature
@@ -247,7 +243,6 @@ public:
 		btVector3				m_normal;		// Normal
 		btScalar				m_ra;			// Rest area
 		btDbvtNode*				m_leaf;			// Leaf data
-		Face() : Feature() {}
 	};
 	/* Tetra		*/ 
 	struct	Tetra : Feature
@@ -258,7 +253,6 @@ public:
 		btVector3				m_c0[4];		// gradients
 		btScalar				m_c1;			// (4*kVST)/(im0+im1+im2+im3)
 		btScalar				m_c2;			// m_c1/sum(|g0..3|^2)
-		Tetra() : Feature() {}
 	};
 	/* RContact		*/ 
 	struct	RContact
@@ -300,7 +294,6 @@ public:
 		int						m_rank;			// Rank
 		Node*					m_nodes[4];		// Nodes
 		btScalar				m_coords[4];	// Coordinates
-		Note() : Element() {}
 	};	
 	/* Pose			*/ 
 	struct	Pose
