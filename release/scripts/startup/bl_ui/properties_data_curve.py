bc88e61efa3fdf9b0f2dc2d3790567d0c2c26a82
Sergey Sharybin <sergey.vfx@gmail.com>
2011-03-25 22:02:50 +0000

    "Fill Deformed" option for curves isn't dependent from bevel object, so it shouldn't be grayed out even if bevel object is set.

--- release/scripts/startup/bl_ui/properties_data_curve.py
+++ release/scripts/startup/bl_ui/properties_data_curve.py
@@ -106,12 +106,12 @@ class DATA_PT_shape_curve(CurveButtonsPanel, bpy.types.Panel):
             sub.prop(curve, "render_resolution_v", text="Render V")
 
         if (is_curve or is_text):
+            col.label(text="Fill:")
             sub = col.column()
             sub.active = (curve.bevel_object is None)
-            sub.label(text="Fill:")
             sub.prop(curve, "use_fill_front")
             sub.prop(curve, "use_fill_back")
-            sub.prop(curve, "use_fill_deform", text="Fill Deformed")
+            col.prop(curve, "use_fill_deform", text="Fill Deformed")
 
         col.label(text="Textures:")
         col.prop(curve, "use_uv_as_generated")
