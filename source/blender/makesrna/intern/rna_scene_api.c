3c67374f50ef5da6745ca78eaf3b6b20e658383a
Nathan Letwory <nathan@letworyinteractive.com>
2011-03-27 22:02:54 +0000

    Revert r34026, which in it turn reverted r33039. Removal breaks renderers that use COLLADA as their format through render() callback.

--- source/blender/makesrna/intern/rna_scene_api.c
+++ source/blender/makesrna/intern/rna_scene_api.c
@@ -81,6 +81,17 @@ static void rna_SceneRender_get_frame_path(RenderData *rd, int frame, char *name
 		BKE_makepicstring(name, rd->pic, (frame==INT_MIN) ? rd->cfra : frame, rd->imtype, rd->scemode & R_EXTENSION, TRUE);
 }
 
+#ifdef WITH_COLLADA
+/* don't remove this, as COLLADA exporting cannot be done through operators in render() callback. */
+#include "../../collada/collada.h"
+
+static void rna_Scene_collada_export(Scene *scene, const char *filepath)
+{
+	collada_export(scene, filepath);
+}
+
+#endif
+
 #else
 
 void RNA_api_scene(StructRNA *srna)
@@ -96,7 +107,17 @@ void RNA_api_scene(StructRNA *srna)
 
 	func= RNA_def_function(srna, "update", "rna_Scene_update_tagged");
 	RNA_def_function_ui_description(func, "Update data tagged to be updated from previous access to data or operators.");
+
+#ifdef WITH_COLLADA
+	/* don't remove this, as COLLADA exporting cannot be done through operators in render() callback. */
+	func= RNA_def_function(srna, "collada_export", "rna_Scene_collada_export");
+	parm= RNA_def_string(func, "filepath", "", FILE_MAX, "File Path", "File path to write Collada file.");
+	RNA_def_property_flag(parm, PROP_REQUIRED);
+	RNA_def_property_subtype(parm, PROP_FILEPATH); /* allow non utf8 */
+	RNA_def_function_ui_description(func, "Export to collada file.");
+#endif
 }
+
 
 void RNA_api_scene_render(StructRNA *srna)
 {
