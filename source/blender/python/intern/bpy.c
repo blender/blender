7e3d044bdd398c712bd65094b0f1d444580e8011
Campbell Barton <ideasman42@gmail.com>
2011-04-20 12:55:42 +0000

    fix [#27072] Calling bpy.utils.blend_paths(True) results in segmantaion fault since 2.56

--- source/blender/python/intern/bpy.c
+++ source/blender/python/intern/bpy.c
@@ -46,7 +46,7 @@
 #include "BLI_bpath.h"
 #include "BLI_utildefines.h"
 
-
+#include "BKE_main.h"
 #include "BKE_global.h" /* XXX, G.main only */
 #include "BKE_blender.h"
 
@@ -111,7 +111,7 @@ static PyObject *bpy_blend_paths(PyObject *UNUSED(self), PyObject *args, PyObjec
 
 	list= PyList_New(0);
 
-	for(BLI_bpathIterator_init(&bpi, G.main, NULL, 0); !BLI_bpathIterator_isDone(bpi); BLI_bpathIterator_step(bpi)) {
+	for(BLI_bpathIterator_init(&bpi, G.main, G.main->name, 0); !BLI_bpathIterator_isDone(bpi); BLI_bpathIterator_step(bpi)) {
 		/* build the list */
 		if (absolute) {
 			BLI_bpathIterator_getPathExpanded(bpi, filepath_expanded);
