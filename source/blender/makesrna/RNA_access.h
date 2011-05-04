5ed5c7441cdb23c4f5d0efe550fc3562bdbb9cf8
Campbell Barton <ideasman42@gmail.com>
2011-05-04 17:36:13 +0000

    new rna api call: RNA_struct_idprops_unset(op->ptr, "someprop"), added to allow un-setting operator properties.

--- source/blender/makesrna/RNA_access.h
+++ source/blender/makesrna/RNA_access.h
@@ -620,7 +620,7 @@ void RNA_struct_blender_type_set(StructRNA *srna, void *blender_type);
 struct IDProperty *RNA_struct_idprops(PointerRNA *ptr, int create);
 int RNA_struct_idprops_check(StructRNA *srna);
 int RNA_struct_idprops_register_check(StructRNA *type);
-
+int RNA_struct_idprops_unset(PointerRNA *ptr, const char *identifier);
 
 PropertyRNA *RNA_struct_find_property(PointerRNA *ptr, const char *identifier);
 int RNA_struct_contains_property(PointerRNA *ptr, PropertyRNA *prop_test);
