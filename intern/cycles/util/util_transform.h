88b25b871d965718e693a2766a19d75efa0f0dfc
Brecht Van Lommel <brechtvanlommel@pandora.be>
2011-05-12 11:34:16 +0000

    Cycles: fix mapping node rotation not working correct for Y/Z axes, patch by Sanne.

--- intern/cycles/util/util_transform.h
+++ intern/cycles/util/util_transform.h
@@ -173,8 +173,8 @@ __device_inline Transform transform_euler(float3 euler)
 {
 	return
 		transform_rotate(euler.x, make_float3(1.0f, 0.0f, 0.0f)) *
-		transform_rotate(euler.y, make_float3(1.0f, 0.0f, 0.0f)) *
-		transform_rotate(euler.z, make_float3(1.0f, 0.0f, 0.0f));
+		transform_rotate(euler.y, make_float3(0.0f, 1.0f, 0.0f)) *
+		transform_rotate(euler.z, make_float3(0.0f, 0.0f, 1.0f));
 }
 
 __device_inline Transform transform_orthographic(float znear, float zfar)
