450f176a8f7d545dc866aecea768b16d200aa910
Mike Erwin <significant.bit@gmail.com>
2011-06-11 02:26:28 +0000

    split Win32 NDOF manager into 2 files to fix Mac scons build

--- intern/ghost/intern/GHOST_NDOFManagerWin32.cpp
+++ intern/ghost/intern/GHOST_NDOFManagerWin32.cpp
@@ -21,18 +21,17 @@
  * ***** END GPL LICENSE BLOCK *****
  */
  
-#ifndef _GHOST_NDOFMANAGERWIN32_H_
-#define _GHOST_NDOFMANAGERWIN32_H_
-
-#include "GHOST_NDOFManager.h"
+#include "GHOST_NDOFManagerWin32.h"
 
 
-class GHOST_NDOFManagerWin32 : public GHOST_NDOFManager
-{
-public:
-	GHOST_NDOFManagerWin32(GHOST_System&);
-	bool available();
-};
+GHOST_NDOFManagerWin32::GHOST_NDOFManagerWin32(GHOST_System& sys)
+	: GHOST_NDOFManager(sys)
+	{}
 
-
-#endif
+// whether multi-axis functionality is available (via the OS or driver)
+// does not imply that a device is plugged in or being used
+bool GHOST_NDOFManagerWin32::available()
+	{
+	// always available since RawInput is built into Windows
+	return true;
+	}
