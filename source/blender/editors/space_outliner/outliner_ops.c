099760cf1f7969d1cd16c6b5bd1a77d86bc62547
Sergey Sharybin <sergey.vfx@gmail.com>
2011-08-29 17:46:07 +0000

    Fix #28295 Outliner, mouse button on menu's pass through.
    It was introduced in rev33603 with not good patch -- release event
    was catching by outliner after clicking on menu item.
    
    Use KM_CLICK instead of KM_RELEASE to deal with icon drag/drop.
    This not changes drag/drop behavior, but prevents unneeded event be handled.
    Also make consistent behavior of activating and extending selection.

--- source/blender/editors/space_outliner/outliner_ops.c
+++ source/blender/editors/space_outliner/outliner_ops.c
@@ -90,8 +90,8 @@ void outliner_keymap(wmKeyConfig *keyconf)
 	
 	WM_keymap_add_item(keymap, "OUTLINER_OT_item_rename", LEFTMOUSE, KM_DBL_CLICK, 0, 0);
 	
-	RNA_boolean_set(WM_keymap_add_item(keymap, "OUTLINER_OT_item_activate", LEFTMOUSE, KM_RELEASE, 0, 0)->ptr, "extend", 0);
-	RNA_boolean_set(WM_keymap_add_item(keymap, "OUTLINER_OT_item_activate", LEFTMOUSE, KM_PRESS, KM_SHIFT, 0)->ptr, "extend", 1);
+	RNA_boolean_set(WM_keymap_add_item(keymap, "OUTLINER_OT_item_activate", LEFTMOUSE, KM_CLICK, 0, 0)->ptr, "extend", 0);
+	RNA_boolean_set(WM_keymap_add_item(keymap, "OUTLINER_OT_item_activate", LEFTMOUSE, KM_CLICK, KM_SHIFT, 0)->ptr, "extend", 1);
 	
 	RNA_boolean_set(WM_keymap_add_item(keymap, "OUTLINER_OT_item_openclose", RETKEY, KM_PRESS, 0, 0)->ptr, "all", 0);
 	RNA_boolean_set(WM_keymap_add_item(keymap, "OUTLINER_OT_item_openclose", RETKEY, KM_PRESS, KM_SHIFT, 0)->ptr, "all", 1);
