6c9ee34dd8acba35808847d3669798c6d3459b9c
Thomas Dinges <blender@dingto.org>
2011-08-30 10:44:02 +0000

    2.5 UI Files: * Code cleanup after Pepper merge.

--- release/scripts/startup/bl_ui/properties_data_armature.py
+++ release/scripts/startup/bl_ui/properties_data_armature.py
@@ -72,8 +72,7 @@ class DATA_PT_skeleton(ArmatureButtonsPanel, Panel):
         flow.prop(arm, "use_deform_preserve_volume", text="Quaternion")
 
         if context.scene.render.engine == "BLENDER_GAME":
-            col = layout.column()
-            col.prop(arm, "vert_deformer")
+            layout.row().prop(arm, "vert_deformer", expand=True, text="")
 
 class DATA_PT_display(ArmatureButtonsPanel, Panel):
     bl_label = "Display"
