fbf3a76f1d4b50b81334f55efbff7a1e6ba1f413
Sergey Sharybin <sergey.vfx@gmail.com>
2011-09-10 16:46:30 +0000

    Fix #28609: UV Sphere Operator Panel allow Segments and Ring to be manually set to 0
    Set hard min for segments and rings to 3.
    Lower values doesn't make real sense and can only confuse.

--- source/blender/editors/mesh/editmesh_add.c
+++ source/blender/editors/mesh/editmesh_add.c
@@ -1682,8 +1682,8 @@ void MESH_OT_primitive_uv_sphere_add(wmOperatorType *ot)
 	ot->flag= OPTYPE_REGISTER|OPTYPE_UNDO;
 	
 	/* props */
-	RNA_def_int(ot->srna, "segments", 32, INT_MIN, INT_MAX, "Segments", "", 3, 500);
-	RNA_def_int(ot->srna, "ring_count", 16, INT_MIN, INT_MAX, "Rings", "", 3, 500);
+	RNA_def_int(ot->srna, "segments", 32, 3, INT_MAX, "Segments", "", 3, 500);
+	RNA_def_int(ot->srna, "ring_count", 16, 3, INT_MAX, "Rings", "", 3, 500);
 	RNA_def_float(ot->srna, "size", 1.0f, 0.0, FLT_MAX, "Size", "", 0.001, 100.00);
 
 	ED_object_add_generic_props(ot, TRUE);
