8167d8c2f1df361aff874a5d08f8a78508ba6422
Campbell Barton <ideasman42@gmail.com>
2011-09-11 15:36:11 +0000

    pep8 edits

--- release/scripts/startup/bl_ui/properties_data_modifier.py
+++ release/scripts/startup/bl_ui/properties_data_modifier.py
@@ -759,11 +759,11 @@ class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
             split.template_ID(md, "mask_texture", new="texture.new")
             if md.mask_texture:
                 split = layout.split()
-                
+
                 col = split.column()
                 col.label(text="Texture Coordinates:")
                 col.prop(md, "mask_tex_mapping", text="")
-                
+
                 col = split.column()
                 col.label(text="Use Channel:")
                 col.prop(md, "mask_tex_use_channel", text="")
@@ -806,7 +806,7 @@ class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
 
     def VERTEX_WEIGHT_MIX(self, layout, ob, md):
         split = layout.split()
-        
+
         col = split.column()
         col.label(text="Vertex Group A:")
         col.prop_search(md, "vertex_group_a", ob, "vertex_groups", text="")
@@ -831,7 +831,7 @@ class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
 
     def VERTEX_WEIGHT_PROXIMITY(self, layout, ob, md):
         split = layout.split()
-        
+
         col = split.column()
         col.label(text="Vertex Group:")
         col.prop_search(md, "vertex_group", ob, "vertex_groups", text="")
