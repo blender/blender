f571d1d238372b668eab88bb491eb48bd1327554
Campbell Barton <ideasman42@gmail.com>
2011-10-03 04:48:14 +0000

    fix [#27578] Fileselector UI area does not scale with UI (partially hides buttons)

--- source/blender/editors/space_file/space_file.c
+++ source/blender/editors/space_file/space_file.c
@@ -520,8 +520,13 @@ static void file_ui_area_draw(const bContext *C, ARegion *ar)
 	glClearColor(col[0], col[1], col[2], 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	/* scrolling here is just annoying, disable it */
+	ar->v2d.cur.ymax= ar->v2d.cur.ymax - ar->v2d.cur.ymin;
+	ar->v2d.cur.ymin= 0;
+
 	/* set view2d view matrix for scrolling (without scrollers) */
 	UI_view2d_view_ortho(&ar->v2d);
+
 
 	file_draw_buttons(C, ar);
 
