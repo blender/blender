ff683ca5e511638da82357816a8d9e053536e2b5
Campbell Barton <ideasman42@gmail.com>
2011-10-25 04:01:09 +0000

    misc small edits, remove remaining Id's

--- source/gameengine/GamePlayer/ghost/GPG_ghost.cpp
+++ source/gameengine/GamePlayer/ghost/GPG_ghost.cpp
@@ -365,7 +365,6 @@ int main(int argc, char** argv)
 	GHOST_TEmbedderWindowID parentWindow = 0;
 	bool isBlenderPlayer = false;
 	int validArguments=0;
-	bool samplesParFound = false;
 	GHOST_TUns16 aasamples = 0;
 	
 #ifdef __linux__
@@ -407,8 +406,6 @@ int main(int argc, char** argv)
 	
 	initglobals();
 
-	// Blender's VBOs cause odd problems with modifiers (we have our own vbo code)
-	U.gameflags |= USER_DISABLE_VBO;
 	// We load our own G.main, so free the one that initglobals() gives us
 	free_main(G.main);
 	G.main = NULL;
@@ -584,14 +581,8 @@ int main(int argc, char** argv)
 				break;
 			case 'm':
 				i++;
-				samplesParFound = true;
 				if ((i+1) <= validArguments )
-					aasamples = atoi(argv[i++]);
-				else
-				{
-					error = true;
-					printf("error: No argument supplied for -m");
-				}
+				aasamples = atoi(argv[i++]);
 				break;
 			case 'c':
 				i++;
@@ -827,7 +818,7 @@ int main(int argc, char** argv)
 						if ((!fullScreenParFound) && (!windowParFound))
 						{
 							// Only use file settings when command line did not override
-							if ((scene->gm.playerflag & GAME_PLAYER_FULLSCREEN)) {
+							if (scene->gm.fullscreen) {
 								//printf("fullscreen option found in Blender file\n");
 								fullScreen = true;
 								fullScreenWidth= scene->gm.xplay;
@@ -855,9 +846,6 @@ int main(int argc, char** argv)
 						}
 						else
 							scene->gm.stereoflag = STEREO_ENABLED;
-
-						if (!samplesParFound)
-							aasamples = scene->gm.aasamples;
 
 						if (stereoFlag == STEREO_DOME){
 							stereomode = RAS_IRasterizer::RAS_STEREO_DOME;
@@ -904,7 +892,7 @@ int main(int argc, char** argv)
 #endif
 								{
 									app.startFullScreen(fullScreenWidth, fullScreenHeight, fullScreenBpp, fullScreenFrequency,
-										stereoWindow, stereomode, aasamples, (scene->gm.playerflag & GAME_PLAYER_DESKTOP_RESOLUTION));
+										stereoWindow, stereomode, aasamples);
 								}
 							}
 							else
