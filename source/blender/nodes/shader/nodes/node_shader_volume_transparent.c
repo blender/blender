e094ebfcdff19b80d07bdec22c3fea5a841620be
Campbell Barton <ideasman42@gmail.com>
2011-11-13 16:08:01 +0000

    remove Id:'s from shader node headers

--- source/blender/nodes/shader/nodes/node_shader_volume_transparent.c
+++ source/blender/nodes/shader/nodes/node_shader_volume_transparent.c
@@ -1,12 +1,10 @@
-/**
- * $Id: node_shader_output.c 32517 2010-10-16 14:32:17Z campbellbarton $
- *
+/*
  * ***** BEGIN GPL LICENSE BLOCK *****
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version. 
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
