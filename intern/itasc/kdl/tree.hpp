bff59a5b2eb9f8fd50df6250f020b46ff71bcdba
Jens Verwiebe <info@jensverwiebe.de>
2011-11-15 21:55:07 +0000

    OSX: buildfix for itask on 10.5.sdk

--- intern/itasc/kdl/tree.hpp
+++ intern/itasc/kdl/tree.hpp
@@ -27,15 +27,20 @@
 
 #include <string>
 #include <map>
+#if !defined(__APPLE__) && !defined(MAC_OS_X_VERSION_10_5)
 #include <Eigen/Core>
+#endif
 
 namespace KDL
 {
     //Forward declaration
     class TreeElement;
+#if !defined(__APPLE__) && !defined(MAC_OS_X_VERSION_10_5)
     // Eigen allocator is needed for alignment of Eigen data types
     typedef std::map<std::string,TreeElement, std::less<std::string>, Eigen::aligned_allocator<std::pair<std::string, TreeElement> > > SegmentMap;
-
+#else
+    typedef std::map<std::string,TreeElement> SegmentMap;
+#endif
     class TreeElement
     {
     private:
