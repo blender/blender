4ab1dadf72a821b344a714fff59aed11d15ecb14
Sergey Sharybin <sergey.vfx@gmail.com>
2011-11-20 17:09:54 +0000

    Remove sensor height from Sony A55 preset. Sensor height isn't supported by motion tracking yet.

--- release/scripts/presets/tracking_camera/Sony_A55.py
+++ release/scripts/presets/tracking_camera/Sony_A55.py
@@ -2,7 +2,6 @@ import bpy
 camera = bpy.context.edit_movieclip.tracking.camera
 
 camera.sensor_width = 23.4
-camera.sensor_height = 15.6
 camera.units = 'MILLIMETERS'
 camera.focal_length = 24.0
 camera.pixel_aspect = 1
