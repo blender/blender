be701c7336a3f967a3a2807f8a7a0c511cb76815
Thomas Dinges <blender@dingto.org>
2011-11-20 14:36:23 +0000

    * UI fix for recent Ocean Foam change, makes it a bit more compact.

--- release/scripts/startup/bl_ui/properties_data_modifier.py
+++ release/scripts/startup/bl_ui/properties_data_modifier.py
@@ -454,14 +454,18 @@ class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
         layout.separator()
 
         layout.prop(md, "use_normals")
-
-        layout.prop(md, "use_foam")
-        row = layout.row()
-        row.active = md.use_foam
-        col = row.column()
-        col.prop(md, "foam_coverage", text="Coverage")
-        col = row.column()
-        col.label("Foam Data Layer Name")
+        
+        split = layout.split()
+        
+        col = split.column()
+        col.prop(md, "use_foam")
+        sub = col.row()
+        sub.active = md.use_foam
+        sub.prop(md, "foam_coverage", text="Coverage")
+        
+        col = split.column()
+        col.active = md.use_foam
+        col.label("Foam Data Layer Name:")
         col.prop(md, "foam_layer_name", text="")
 
         layout.separator()
