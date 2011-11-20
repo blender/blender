dde84e32915d2f3c2cea85a2d9116c237e3c4b0d
Sergey Sharybin <sergey.vfx@gmail.com>
2011-11-20 11:34:25 +0000

    Fix #29322: 'Active Clip' not saving after appending.
    Address for active clip used to be updated in direct scene linking,
    should be in library linking.

--- source/blender/blenloader/intern/readfile.c
+++ source/blender/blenloader/intern/readfile.c
@@ -4726,6 +4726,9 @@ static void lib_link_scene(FileData *fd, Main *main)
 			/*Game Settings: Dome Warp Text*/
 			sce->gm.dome.warptext= newlibadr(fd, sce->id.lib, sce->gm.dome.warptext);
 
+			/* Motion Tracking */
+			sce->clip= newlibadr_us(fd, sce->id.lib, sce->clip);
+
 			sce->id.flag -= LIB_NEEDLINK;
 		}
 
@@ -4929,8 +4932,6 @@ static void direct_link_scene(FileData *fd, Scene *sce)
 	sce->nodetree= newdataadr(fd, sce->nodetree);
 	if(sce->nodetree)
 		direct_link_nodetree(fd, sce->nodetree);
-	
-	sce->clip= newlibadr_us(fd, sce->id.lib, sce->clip);
 }
 
 /* ************ READ WM ***************** */
