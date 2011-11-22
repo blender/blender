04fe6fc76472c894e5e78d079b84cac112e782b1
Thomas Dinges <blender@dingto.org>
2011-11-22 17:14:47 +0000

    * UI Improvements for the uiTemplateImageSettings.

--- source/blender/editors/space_image/image_buttons.c
+++ source/blender/editors/space_image/image_buttons.c
@@ -830,7 +830,7 @@ void uiTemplateImageSettings(uiLayout *layout, PointerRNA *imfptr)
 	col= uiLayoutColumn(layout, 0);
 
 	uiItemR(col, imfptr, "file_format", 0, "", ICON_NONE);
-
+	
 	row= uiLayoutRow(col, 0);
 	uiItemR(row, imfptr, "color_mode", UI_ITEM_R_EXPAND, "Color", ICON_NONE);
 
@@ -858,24 +858,27 @@ void uiTemplateImageSettings(uiLayout *layout, PointerRNA *imfptr)
 	if (ELEM(imf->imtype, R_IMF_IMTYPE_OPENEXR, R_IMF_IMTYPE_MULTILAYER)) {
 		uiItemR(col, imfptr, "exr_codec", 0, NULL, ICON_NONE);
 	}
-
+	
+	row= uiLayoutRow(col, 0);
 	if (BKE_imtype_supports_zbuf(imf->imtype)) {
-		uiItemR(col, imfptr, "use_zbuffer", 0, NULL, ICON_NONE);
+		uiItemR(row, imfptr, "use_zbuffer", 0, NULL, ICON_NONE);
 	}
 
 	if (is_render_out && (imf->imtype == R_IMF_IMTYPE_OPENEXR)) {
-		uiItemR(col, imfptr, "use_preview", 0, NULL, ICON_NONE);
+		uiItemR(row, imfptr, "use_preview", 0, NULL, ICON_NONE);
 	}
 
 	if (imf->imtype == R_IMF_IMTYPE_JP2) {
+		row= uiLayoutRow(col, 0);
+		uiItemR(row, imfptr, "use_jpeg2k_cinema_preset", 0, NULL, ICON_NONE);
+		uiItemR(row, imfptr, "use_jpeg2k_cinema_48", 0, NULL, ICON_NONE);
+		
 		uiItemR(col, imfptr, "use_jpeg2k_ycc", 0, NULL, ICON_NONE);
-		uiItemR(col, imfptr, "use_jpeg2k_cinema_preset", 0, NULL, ICON_NONE);
-		uiItemR(col, imfptr, "use_jpeg2k_cinema_48", 0, NULL, ICON_NONE);
 	}
 
 	if (imf->imtype == R_IMF_IMTYPE_CINEON) {
 #if 1
-		uiItemL(col, "FIXME: hard coded Non-Linear, Gamma:1.0", ICON_NONE);
+		uiItemL(col, "Hard coded Non-Linear, Gamma:1.0", ICON_NONE);
 #else
 		uiItemR(col, imfptr, "use_cineon_log", 0, NULL, ICON_NONE);
 		uiItemR(col, imfptr, "cineon_black", 0, NULL, ICON_NONE);
