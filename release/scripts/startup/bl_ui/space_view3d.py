1906410cc2ba065e0586238f4913bdd58b04c676
Andrew Wiggin <ender79bl@gmail.com>
2011-12-02 16:56:05 +0000

    Remove face menu (ctrl+f) links to f-gon commands

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -1665,11 +1665,6 @@ class VIEW3D_MT_edit_mesh_faces(Menu):
 
         layout.separator()
 
-        layout.operator("mesh.fgon_make")
-        layout.operator("mesh.fgon_clear")
-
-        layout.separator()
-
         layout.operator("mesh.quads_convert_to_tris")
         layout.operator("mesh.tris_convert_to_quads")
         layout.operator("mesh.edge_flip")
