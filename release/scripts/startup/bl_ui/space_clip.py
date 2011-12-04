e98e8acf1a00ec4fb1adf0c4e30bc687cfc8efad
Keir Mierle <mierle@gmail.com>
2011-12-04 14:08:58 +0000

    Add minimum correlation widget to tracker default settings panel.

--- release/scripts/startup/bl_ui/space_clip.py
+++ release/scripts/startup/bl_ui/space_clip.py
@@ -129,8 +129,7 @@ class CLIP_PT_tools_marker(Panel):
 
             if settings.default_tracker == 'KLT':
                 col.prop(settings, "default_pyramid_levels")
-            elif settings.default_tracker == 'SAD':
-                col.prop(settings, "default_correlation_min")
+            col.prop(settings, "default_correlation_min")
 
             col.separator()
 
