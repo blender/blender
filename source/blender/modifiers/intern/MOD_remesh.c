defa99b65ce56b9c536ce7ade76a0ccfa3b4ceff
Bastien Montagne <montagne29@wanadoo.fr>
2012-01-01 16:09:32 +0000

    Fixed names of VertexWeight modifiers RNA structs, in RNA_access.h (probably forgotten when they were renamed, before the merge in trunk, and was never noticed as they are not used...).
    Also removed the $ID$ from remesh modifier header, and quited a gcc warning.

--- source/blender/modifiers/intern/MOD_remesh.c
+++ source/blender/modifiers/intern/MOD_remesh.c
@@ -1,27 +1,24 @@
 /*
-* $Id$
-*
-* ***** BEGIN GPL LICENSE BLOCK *****
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software  Foundation,
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*
-* The Original Code is Copyright (C) 2011 by Nicholas Bishop.
-*
-* ***** END GPL LICENSE BLOCK *****
-*
-*/
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The Original Code is Copyright (C) 2011 by Nicholas Bishop.
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
 
 /** \file blender/modifiers/intern/MOD_remesh.c
  *  \ingroup modifiers
@@ -152,7 +149,7 @@ static DerivedMesh *applyModifier(ModifierData *md,
 	DualConInput input;
 	DerivedMesh *result;
 	DualConFlags flags = 0;
-	DualConMode mode;
+	DualConMode mode = 0;
 
 	rmd = (RemeshModifierData*)md;
 
