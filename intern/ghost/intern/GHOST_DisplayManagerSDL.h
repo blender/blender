15dc3d4609883fc8c9b0b8ea3e3294a1b0940d13
Alex Fraser <alex@phatcore.com>
2012-01-02 12:35:06 +0000

    Fixes two crashers for games, with GHOST under SDL: - Fixes SDL fullscreen mode for game engine (blenderplayer). Mode switching (resolution changes) not supported yet though. - Fixes embedded game engine exit. See patch tracker [#29748].

--- intern/ghost/intern/GHOST_DisplayManagerSDL.h
+++ intern/ghost/intern/GHOST_DisplayManagerSDL.h
@@ -46,20 +46,20 @@ public:
 	GHOST_DisplayManagerSDL(GHOST_SystemSDL *system);
 
 	GHOST_TSuccess
-	getNumDisplays(GHOST_TUns8& numDisplays);
+	getNumDisplays(GHOST_TUns8& numDisplays) const;
 
 	GHOST_TSuccess
 	getNumDisplaySettings(GHOST_TUns8 display,
-	                      GHOST_TInt32& numSettings);
+	                      GHOST_TInt32& numSettings) const;
 
 	GHOST_TSuccess
 	getDisplaySetting(GHOST_TUns8 display,
 	                  GHOST_TInt32 index,
-	                  GHOST_DisplaySetting& setting);
+	                  GHOST_DisplaySetting& setting) const;
 
 	GHOST_TSuccess
 	getCurrentDisplaySetting(GHOST_TUns8 display,
-	                         GHOST_DisplaySetting& setting);
+	                         GHOST_DisplaySetting& setting) const;
 
 	GHOST_TSuccess
 	setCurrentDisplaySetting(GHOST_TUns8 display,
