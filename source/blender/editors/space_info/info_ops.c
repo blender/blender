fcc54520d1f029b86cb8c6f43c239ef81090a99a
Sergey Sharybin <sergey.vfx@gmail.com>
2012-01-17 16:22:22 +0000

    Disable "Ask for each file" menu option for files unpack. It was never implemented in 2.5x

--- source/blender/editors/space_info/info_ops.c
+++ source/blender/editors/space_info/info_ops.c
@@ -124,7 +124,7 @@ static const EnumPropertyItem unpack_all_method_items[] = {
 	{PF_USE_ORIGINAL, "USE_ORIGINAL", 0, "Use files in original location (create when necessary)", ""},
 	{PF_WRITE_ORIGINAL, "WRITE_ORIGINAL", 0, "Write files to original location (overwrite existing files)", ""},
 	{PF_KEEP, "KEEP", 0, "Disable AutoPack, keep all packed files", ""},
-	{PF_ASK, "ASK", 0, "Ask for each file", ""},
+	/* {PF_ASK, "ASK", 0, "Ask for each file", ""}, */
 	{0, NULL, 0, NULL, NULL}};
 
 static int unpack_all_exec(bContext *C, wmOperator *op)
