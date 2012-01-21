3667cddf1ada28b6964bc9beeb7cce31f04e18a8
Tamito Kajiyama <rd6t-kjym@asahi-net.or.jp>
2012-01-21 21:58:47 +0000

    Fix for color management not working properly with Freestyle.

--- source/blender/freestyle/intern/blender_interface/BlenderStrokeRenderer.cpp
+++ source/blender/freestyle/intern/blender_interface/BlenderStrokeRenderer.cpp
@@ -60,7 +60,7 @@ BlenderStrokeRenderer::BlenderStrokeRenderer(Render* re, int render_count)
 	freestyle_scene->r.size= 100; // old_scene->r.size
 	freestyle_scene->r.maximsize= old_scene->r.maximsize;
 	freestyle_scene->r.ocres = old_scene->r.ocres;
-	freestyle_scene->r.color_mgt_flag = old_scene->r.color_mgt_flag;
+	freestyle_scene->r.color_mgt_flag = 0; // old_scene->r.color_mgt_flag;
 	freestyle_scene->r.scemode= old_scene->r.scemode & ~( R_SINGLE_LAYER );
 	freestyle_scene->r.flag= old_scene->r.flag;
 	freestyle_scene->r.threads= old_scene->r.threads;
