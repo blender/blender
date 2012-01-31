67dca2275e0dbd399b72f13c23932d6c663aed75
Nathan Vegdahl <cessen@cessen.com>
2012-01-31 04:23:49 +0000

    Action constraint transform channel was listing X Scale as Z Scale, resulting in two Z Scale listings in the menu.  Fixed.

--- source/blender/makesrna/intern/rna_constraint.c
+++ source/blender/makesrna/intern/rna_constraint.c
@@ -1032,7 +1032,7 @@ static void rna_def_constraint_action(BlenderRNA *brna)
 		{00, "ROTATION_X", 0, "X Rotation", ""},
 		{01, "ROTATION_Y", 0, "Y Rotation", ""},
 		{02, "ROTATION_Z", 0, "Z Rotation", ""},
-		{10, "SCALE_X", 0, "Z Scale", ""},
+		{10, "SCALE_X", 0, "X Scale", ""},
 		{11, "SCALE_Y", 0, "Y Scale", ""},
 		{12, "SCALE_Z", 0, "Z Scale", ""},
 		{0, NULL, 0, NULL, NULL}};
