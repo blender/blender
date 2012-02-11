91390b712aa42fff2f5937816b010e157a4333b3
Campbell Barton <ideasman42@gmail.com>
2012-02-11 04:16:17 +0000

    GPL Header Cleanup

--- source/blender/bmesh/intern/bmesh_eulers.c
+++ source/blender/bmesh/intern/bmesh_eulers.c
@@ -8,7 +8,6 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * about this.	
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * The Original Code is Copyright (C) 2004 Blender Foundation.
  * All rights reserved.
