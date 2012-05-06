fbf06d0f5d5f2990256c89598c825b4dfa7da063
Campbell Barton <ideasman42@gmail.com>
2012-05-06 13:41:27 +0000

    update for recent RNA change

--- release/scripts/startup/bl_ui/space_view3d_toolbar.py
+++ release/scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -145,10 +145,10 @@ class VIEW3D_PT_tools_meshedit(View3DPanel, Panel):
         row = col.row(align=True)
         props = row.operator("mesh.knife_tool", text="Knife")
         props.use_occlude_geometry = True
-        props.only_select = False
+        props.only_selected = False
         props = row.operator("mesh.knife_tool", text="Select")
         props.use_occlude_geometry = False
-        props.only_select = True
+        props.only_selected = True
 
         col = layout.column(align=True)
         col.label(text="Remove:")
