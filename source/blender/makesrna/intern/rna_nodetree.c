00460458d0cb06d3358c08621b45cb7c8b4548d3
Sergey Sharybin <sergey.vfx@gmail.com>
2012-05-09 10:50:16 +0000

    blender 2.63a: merge last bunch of fixes for 'a' release
    This merges commits form trunk: 46423, 46424, 46450, 46439, 46445, 46449, 46453, 46454
    
    Changes checked up to rev46454

--- source/blender/makesrna/intern/rna_nodetree.c
+++ source/blender/makesrna/intern/rna_nodetree.c
@@ -742,7 +742,8 @@ static bNodeLink *rna_NodeTree_link_new(bNodeTree *ntree, ReportList *reports,
 	ret = nodeAddLink(ntree, fromnode, fromsock, tonode, tosock);
 	
 	if (ret) {
-		nodeUpdate(ntree, tonode);
+		if (tonode)
+			nodeUpdate(ntree, tonode);
 
 		ntreeUpdateTree(ntree);
 
