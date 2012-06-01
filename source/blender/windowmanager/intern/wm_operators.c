4fc4aa383b71a7d0a86eef29fd7ffa0383893f52
Sergey Sharybin <sergey.vfx@gmail.com>
2012-06-01 13:55:00 +0000

    There's no MASK_OT_select_border in trunk yet

--- source/blender/windowmanager/intern/wm_operators.c
+++ source/blender/windowmanager/intern/wm_operators.c
@@ -3936,7 +3936,6 @@ static void gesture_border_modal_keymap(wmKeyConfig *keyconf)
 	WM_modalkeymap_assign(keymap, "UV_OT_select_border");
 	WM_modalkeymap_assign(keymap, "CLIP_OT_select_border");
 	WM_modalkeymap_assign(keymap, "CLIP_OT_graph_select_border");
-	WM_modalkeymap_assign(keymap, "MASK_OT_select_border");
 	WM_modalkeymap_assign(keymap, "VIEW2D_OT_zoom_border");
 	WM_modalkeymap_assign(keymap, "VIEW3D_OT_clip_border");
 	WM_modalkeymap_assign(keymap, "VIEW3D_OT_render_border");
