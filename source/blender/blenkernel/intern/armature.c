463a4ebf1c158c3094f9f73bcf8ce69ac5f8fbd7
Campbell Barton <ideasman42@gmail.com>
2012-06-20 21:10:34 +0000

    fix for segfault loading a file with a proxy that is a missing link.

--- source/blender/blenkernel/intern/armature.c
+++ source/blender/blenkernel/intern/armature.c
@@ -1588,7 +1588,11 @@ static void pose_proxy_synchronize(Object *ob, Object *from, int layer_protected
 	for (pchan = pose->chanbase.first; pchan; pchan = pchan->next) {
 		pchanp = BKE_pose_channel_find_name(frompose, pchan->name);
 
-		if (pchan->bone->layer & layer_protected) {
+		if (UNLIKELY(pchanp == NULL)) {
+			/* happens for proxies that become invalid because of a missing link
+			 * for regulat cases it shouldn't happen at all */
+		}
+		else if (pchan->bone->layer & layer_protected) {
 			ListBase proxylocal_constraints = {NULL, NULL};
 
 			/* copy posechannel to temp, but restore important pointers */
