91de70a7883af41d6f25e0c7bdc70087ca16a761
Thomas Dinges <blender@dingto.org>
2012-06-30 18:20:14 +0000

    Cycles / Boost: * Windows (msvc 2008) is now using Boost 1.49

--- build_files/scons/config/win64-vc-config.py
+++ build_files/scons/config/win64-vc-config.py
@@ -156,7 +156,7 @@ BF_OIIO_LIBPATH = '${BF_OIIO}/lib'
 WITH_BF_BOOST = True
 BF_BOOST = '${LIBDIR}/boost'
 BF_BOOST_INC = '${BF_BOOST}/include'
-BF_BOOST_LIB = 'libboost_date_time-vc90-mt-s-1_47 libboost_filesystem-vc90-mt-s-1_47 libboost_regex-vc90-mt-s-1_47 libboost_system-vc90-mt-s-1_47 libboost_thread-vc90-mt-s-1_47'
+BF_BOOST_LIB = 'libboost_date_time-vc90-mt-s-1_49 libboost_filesystem-vc90-mt-s-1_49 libboost_regex-vc90-mt-s-1_49 libboost_system-vc90-mt-s-1_49 libboost_thread-vc90-mt-s-1_49'
 BF_BOOST_LIBPATH = '${BF_BOOST}/lib'
 
 #CUDA
