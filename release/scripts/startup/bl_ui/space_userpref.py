3270438678c8022c00de9655300b9589803a50b1
Campbell Barton <ideasman42@gmail.com>
2012-07-29 01:02:25 +0000

    fix for own regression with handling of script paths, however this didnt work quite right before either.
    Handle these 2 kinds of script paths
    * user script path: ~/.blender/scripts OR $BLENDER_USER_SCRIPTS
    * pref script path: always bpy.context.user_preferences.filepaths.script_directory
    
    now both are returned by bpy.utils.script_paths()

--- release/scripts/startup/bl_ui/space_userpref.py
+++ release/scripts/startup/bl_ui/space_userpref.py
@@ -1004,10 +1004,10 @@ class USERPREF_PT_addons(Panel):
     @staticmethod
     def is_user_addon(mod, user_addon_paths):
         if not user_addon_paths:
-            user_script_path = bpy.utils.user_script_path()
-            if user_script_path is not None:
-                user_addon_paths.append(os.path.join(user_script_path, "addons"))
-            user_addon_paths.append(os.path.join(bpy.utils.resource_path('USER'), "scripts", "addons"))
+            for path in (bpy.utils.script_path_user(),
+                         bpy.utils.script_path_pref()):
+                if path is not None:
+                    user_addon_paths.append(os.path.join(path, "addons"))
 
         for path in user_addon_paths:
             if bpy.path.is_subdir(mod.__file__, path):
