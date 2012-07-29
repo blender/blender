9d900fdd11a8452701cdebc2be8799559136a645
Mitchell Stokes <mogurijin@gmail.com>
2012-07-29 06:28:50 +0000

    Fix for [#32213] "Action actuator doesn't finish playing if frame rate drops" reported by Alex Fraser (z0r). The action actuator was calling StopAction when it's time ran out. Now I'm just letting BL_Action handle stopping. Hopefully this doesn't break something else now....

--- source/gameengine/Converter/BL_ActionActuator.cpp
+++ source/gameengine/Converter/BL_ActionActuator.cpp
@@ -145,7 +145,6 @@ void BL_ActionActuator::SetLocalTime(float curtime)
 		case ACT_ACTION_PLAY:
 			// Clamp
 			m_localtime = m_endframe;
-			((KX_GameObject*)GetParent())->StopAction(m_layer);
 			break;
 		case ACT_ACTION_LOOP_END:
 			// Put the time back to the beginning
