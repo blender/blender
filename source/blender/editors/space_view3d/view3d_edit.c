bec90b40c3fa84c4c84c3b9e7190cb8ed2e00faf
Campbell Barton <ideasman42@gmail.com>
2012-08-10 11:43:53 +0000

    fix [#32289] Border Zoom, Zoom Border or Zoom to Border?
    pedantic report but may as well name them the same.

--- source/blender/editors/space_view3d/view3d_edit.c
+++ source/blender/editors/space_view3d/view3d_edit.c
@@ -2685,7 +2685,7 @@ static int view3d_zoom_border_invoke(bContext *C, wmOperator *op, wmEvent *event
 void VIEW3D_OT_zoom_border(wmOperatorType *ot)
 {
 	/* identifiers */
-	ot->name = "Border Zoom";
+	ot->name = "Zoom to Border";
 	ot->description = "Zoom in the view to the nearest object contained in the border";
 	ot->idname = "VIEW3D_OT_zoom_border";
 
