316b72647a2e0a7e5d2c5a3f799f023cd127b42e
Bastien Montagne <montagne29@wanadoo.fr>
2012-08-27 20:58:43 +0000

    Picky spell-checking...

--- source/blender/makesrna/intern/rna_nodetree.c
+++ source/blender/makesrna/intern/rna_nodetree.c
@@ -2161,7 +2161,7 @@ static void def_cmp_despeckle(StructRNA *srna)
 	prop = RNA_def_property(srna, "threshold_neighbour", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "custom4");
 	RNA_def_property_range(prop, 0.0, 1.0f);
-	RNA_def_property_ui_text(prop, "Neighbour", "Threshold for the number of neighbour pixels that must match");
+	RNA_def_property_ui_text(prop, "Neighbor", "Threshold for the number of neighbor pixels that must match");
 	RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_Node_update");
 }
 
