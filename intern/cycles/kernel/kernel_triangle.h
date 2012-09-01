779ecb06dd3f3b5c47391675622fb96d041b23ae
Thomas Dinges <blender@dingto.org>
2012-09-01 15:30:35 +0000

    Cycles / OSL: * Compile fix for the "direction_to_panorama" issue. Added kernel_projection.h to kernel_triangle.h.

--- intern/cycles/kernel/kernel_triangle.h
+++ intern/cycles/kernel/kernel_triangle.h
@@ -15,6 +15,8 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+ 
+#include "kernel_projection.h"
 
 CCL_NAMESPACE_BEGIN
 
