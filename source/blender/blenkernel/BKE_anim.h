7283709a40ddcb4f39c6955b44920c79d21d200d
Campbell Barton <ideasman42@gmail.com>
2012-09-03 22:53:34 +0000

    code cleanup: curve path calculation

--- source/blender/blenkernel/BKE_anim.h
+++ source/blender/blenkernel/BKE_anim.h
@@ -60,7 +60,6 @@ void animviz_calc_motionpaths(struct Scene *scene, ListBase *targets);
 
 void free_path(struct Path *path);
 void calc_curvepath(struct Object *ob);
-int interval_test(int min, int max, int p1, int cycl);
 int where_on_path(struct Object *ob, float ctime, float vec[4], float dir[3], float quat[4], float *radius, float *weight);
 
 /* ---------------------------------------------------- */
