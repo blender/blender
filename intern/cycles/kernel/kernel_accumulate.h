d4902771947372f10fb095629021a3e247887d92
Lukas Toenne <lukas.toenne@googlemail.com>
2012-09-05 18:12:17 +0000

    Quick fix for compiler error. Somehow the isfinite symbol got lost for SVM too now, no idea how this happened or where it actually came from. This will likely also cause trouble with CUDA/OpenCL compilers, will have to be fixed properly later.

--- intern/cycles/kernel/kernel_accumulate.h
+++ intern/cycles/kernel/kernel_accumulate.h
@@ -301,9 +301,7 @@ __device_inline float3 path_radiance_sum(KernelGlobals *kg, PathRadiance *L)
 
 __device_inline void path_radiance_clamp(PathRadiance *L, float3 *L_sum, float clamp)
 {
-	#ifdef WITH_OSL
 	using std::isfinite;
-	#endif
 
 	float sum = fabsf((*L_sum).x) + fabsf((*L_sum).y) + fabsf((*L_sum).z);
 
