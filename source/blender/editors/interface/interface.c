a803f5d6d05159be5f94673c688dad32231c3d96
Sergey Sharybin <sergey.vfx@gmail.com>
2012-09-14 16:43:07 +0000

    Merging r50600 through r50601 from trunk into soc-2011-tomato

--- source/blender/editors/interface/interface.c
+++ source/blender/editors/interface/interface.c
@@ -2623,7 +2623,7 @@ static uiBut *ui_def_but(uiBlock *block, int type, int retval, const char *str,
 
 	/* we could do some more error checks here */
 	if ((type & BUTTYPE) == LABEL) {
-		BLI_assert((poin != NULL || a1 != 0.0f || a2 != 0.0f || min != 0.0f || max != 0.0f) == FALSE);
+		BLI_assert((poin != NULL || min != 0.0f || max != 0.0f || (a1 == 0.0f && a2 != 0.0f) || (a1 != 0.0f && a1 != 1.0f)) == FALSE);
 	}
 
 	if (type & UI_BUT_POIN_TYPES) {  /* a pointer is required */
