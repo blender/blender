687cacfdd0aec599e56f7f5e2ed69f0bf6c61b4e
Campbell Barton <ideasman42@gmail.com>
2012-09-24 22:41:10 +0000

    rename RNA duration --> frame_duration to match sequence strip.

--- source/blender/makesrna/intern/rna_movieclip.c
+++ source/blender/makesrna/intern/rna_movieclip.c
@@ -300,7 +300,7 @@ static void rna_def_movieclip(BlenderRNA *brna)
 	RNA_def_property_update(prop, NC_MOVIECLIP | ND_DISPLAY, "rna_MovieClip_reload_update");
 
 	/* length */
-	prop = RNA_def_property(srna, "duration", PROP_INT, PROP_NONE);
+	prop = RNA_def_property(srna, "frame_duration", PROP_INT, PROP_NONE);
 	RNA_def_property_clear_flag(prop, PROP_EDITABLE);
 	RNA_def_property_int_sdna(prop, NULL, "len");
 	RNA_def_property_ui_text(prop, "Duration", "Detected duration of movie clip in frames");
