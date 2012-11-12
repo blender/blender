d934ae70a04ce1a095b21e6124bb2198f2907532
Brecht Van Lommel <brechtvanlommel@pandora.be>
2012-11-12 14:25:29 +0000

    Fix conflict of cycles persistent data flag with freestyle, add reserved value for freestyle now.

--- source/blender/makesdna/DNA_scene_types.h
+++ source/blender/makesdna/DNA_scene_types.h
@@ -1192,7 +1192,8 @@ typedef struct Scene {
 #define R_NO_OVERWRITE		0x400000  /* skip existing files */
 #define R_TOUCH				0x800000  /* touch files before rendering */
 #define R_SIMPLIFY			0x1000000
-#define R_PERSISTENT_DATA	0x2000000 /* keep data around for re-render */
+#define R_EDGE_FRS			0x2000000 /* R_EDGE reserved for Freestyle */
+#define R_PERSISTENT_DATA	0x4000000 /* keep data around for re-render */
 
 /* seq_flag */
 #define R_SEQ_GL_PREV 1
