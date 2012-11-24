89f45669304f83f0cc75babdec94e60f2a2c0dfd
Mitchell Stokes <mogurijin@gmail.com>
2012-11-24 00:58:57 +0000

    BGE: Committing a patch from HG1 to fix [#30262] "bgl.glClipPlane don't work on Nvidia cards," which was also reported by HG1.

--- source/blender/gpu/shaders/gpu_shader_vertex.glsl
+++ source/blender/gpu/shaders/gpu_shader_vertex.glsl
@@ -10,3 +10,11 @@ void main()
 	varnormal = normalize(gl_NormalMatrix * gl_Normal);
 	gl_Position = gl_ProjectionMatrix * co;
 
+	// Setting gl_ClipVertex is necessary to get glClipPlane working on NVIDIA graphic cards.
+	// gl_ClipVertex works only on NVIDIA graphic cards so we have to check with 
+	// __GLSL_CG_DATA_TYPES if a NVIDIA graphic card is used (Cg support).
+	// gl_ClipVerte is supported up to GLSL 1.20.
+	#ifdef __GLSL_CG_DATA_TYPES 
+		gl_ClipVertex = gl_ModelViewMatrix * gl_Vertex; 
+	#endif 
+
