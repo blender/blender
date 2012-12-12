743ecc7bd0bb499956ac3a64af9b82214d8164cb
Sergey Sharybin <sergey.vfx@gmail.com>
2012-12-12 12:23:30 +0000

    Revert recent workaround for sequencer, it'll only work in cases scene strip is added at frame 1, which is very limited usecase.
    Proper solution is not possible yet..

--- source/blender/blenkernel/intern/sequencer.c
+++ source/blender/blenkernel/intern/sequencer.c
@@ -2408,8 +2408,7 @@ static ImBuf *seq_render_scene_strip(SeqRenderData context, Sequence *seq, float
 			context.scene->r.seq_prev_type = 3 /* == OB_SOLID */;
 
 		/* opengl offscreen render */
-		if (oldcfra != scene->r.cfra)
-			BKE_scene_update_for_newframe(context.bmain, scene, scene->lay);
+		BKE_scene_update_for_newframe(context.bmain, scene, scene->lay);
 		ibuf = sequencer_view3d_cb(scene, camera, context.rectx, context.recty,
 		                           IB_rect, context.scene->r.seq_prev_type, TRUE, FALSE, err_out);
 		if (ibuf == NULL) {
@@ -2432,8 +2431,7 @@ static ImBuf *seq_render_scene_strip(SeqRenderData context, Sequence *seq, float
 			if (re == NULL)
 				re = RE_NewRender(scene->id.name);
 			
-			if (oldcfra != scene->r.cfra)
-				BKE_scene_update_for_newframe(context.bmain, scene, scene->lay);
+			BKE_scene_update_for_newframe(context.bmain, scene, scene->lay);
 			RE_BlenderFrame(re, context.bmain, scene, NULL, camera, scene->lay, frame, FALSE);
 
 			/* restore previous state after it was toggled on & off by RE_BlenderFrame */
