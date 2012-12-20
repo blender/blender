2421ffc85eba43efc10d95edea98b325ab8bccb9
Ton Roosendaal <ton@blender.org>
2012-12-20 17:40:30 +0000

    Small fix in drawing 'selection circle' in outliner. It wasn't circular.

--- source/blender/editors/space_outliner/outliner_draw.c
+++ source/blender/editors/space_outliner/outliner_draw.c
@@ -1227,12 +1227,12 @@ static void outliner_draw_iconrow(bContext *C, uiBlock *block, Scene *scene, Spa
 				float ufac = UI_UNIT_X / 20.0f;
 
 				uiSetRoundBox(UI_CNR_ALL);
-				glColor4ub(255, 255, 255, 128);
+				glColor4ub(255, 255, 255, 100);
 				uiRoundBox((float) *offsx - 1.0f * ufac,
 				           (float)ys + 1.0f * ufac,
-				           (float)*offsx + UI_UNIT_X - 1.0f * ufac,
-				           (float)ys + UI_UNIT_Y - 1.0f * ufac,
-				           (float)UI_UNIT_Y / 2.0f - 2.0f * ufac);
+				           (float)*offsx + UI_UNIT_X - 2.0f * ufac,
+				           (float)ys + UI_UNIT_Y - ufac,
+				           (float)UI_UNIT_Y / 2.0f - ufac);
 				glEnable(GL_BLEND); /* roundbox disables */
 			}
 			
@@ -1365,11 +1365,11 @@ static void outliner_draw_tree_element(bContext *C, uiBlock *block, Scene *scene
 		/* active circle */
 		if (active) {
 			uiSetRoundBox(UI_CNR_ALL);
-			uiRoundBox((float)startx + UI_UNIT_X - 1.0f * ufac,
+			uiRoundBox((float)startx + UI_UNIT_X,
 			           (float)*starty + 1.0f * ufac,
-			           (float)startx + 2.0f * UI_UNIT_X - 1.0f * ufac,
+			           (float)startx + 2.0f * UI_UNIT_X - 2.0f * ufac,
 			           (float)*starty + UI_UNIT_Y - 1.0f * ufac,
-			           UI_UNIT_Y / 2.0f - 2.0f * ufac);
+			           UI_UNIT_Y / 2.0f - 1.0f * ufac);
 			glEnable(GL_BLEND); /* roundbox disables it */
 			
 			te->flag |= TE_ACTIVE; // for lookup in display hierarchies
