e7485d46cd17f0a7aa376b40a2a6091315d7d1e6
Brecht Van Lommel <brechtvanlommel@pandora.be>
2012-12-20 15:49:59 +0000

    Fix #33630: DDS compressed textures were not actually using mipmaps for minification.

--- source/blender/gpu/intern/gpu_draw.c
+++ source/blender/gpu/intern/gpu_draw.c
@@ -768,7 +768,7 @@ int GPU_upload_dxt_texture(ImBuf *ibuf)
 		return FALSE;
 	}
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, gpu_get_mipmap_filter(0));
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, gpu_get_mipmap_filter(1));
 
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
