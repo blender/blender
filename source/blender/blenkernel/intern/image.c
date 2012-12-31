334ca1d344123f4d8f54c16cf93dab9f2fdae88f
Sergey Sharybin <sergey.vfx@gmail.com>
2012-12-31 14:02:33 +0000

    Correction to recent PNG commit
    Need to set default depth to 8bit.

--- source/blender/blenkernel/intern/image.c
+++ source/blender/blenkernel/intern/image.c
@@ -1309,6 +1309,7 @@ void BKE_imformat_defaults(ImageFormatData *im_format)
 	memset(im_format, 0, sizeof(*im_format));
 	im_format->planes = R_IMF_PLANES_RGB;
 	im_format->imtype = R_IMF_IMTYPE_PNG;
+	im_format->depth = R_IMF_CHAN_DEPTH_8;
 	im_format->quality = 90;
 	im_format->compress = 90;
 
