9592cade86b8ca15b72584f038508278434f8d89
Ton Roosendaal <ton@blender.org>
2013-01-24 11:21:40 +0000

    Armature rigging: Added more clear warning print for cases when a Proxy cannot be resolved.

--- source/blender/blenkernel/intern/object.c
+++ source/blender/blenkernel/intern/object.c
@@ -2711,8 +2711,8 @@ void BKE_object_handle_update(Scene *scene, Object *ob)
 
 				case OB_ARMATURE:
 					if (ob->id.lib && ob->proxy_from) {
-						// printf("pose proxy copy, lib ob %s proxy %s\n", ob->id.name, ob->proxy_from->id.name);
-						BKE_pose_copy_result(ob->pose, ob->proxy_from->pose);
+						if (0 == BKE_pose_copy_result(ob->pose, ob->proxy_from->pose))
+							printf("Proxy copy error, lib Object: %s proxy Object: %s\n", ob->id.name+2, ob->proxy_from->id.name+2);
 					}
 					else {
 						BKE_pose_where_is(scene, ob);
