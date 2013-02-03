4e94fca8965a0012631b4fd92c96fce41fd1d3d9
Campbell Barton <ideasman42@gmail.com>
2013-02-03 08:13:15 +0000

    fix for error in own recent change, sculpt triangulate was done on a bmesh with uninitialized faces normals, add warning that BM_mesh_bm_from_me() dosn't calculate face normals.

--- source/blender/editors/sculpt_paint/sculpt.c
+++ source/blender/editors/sculpt_paint/sculpt.c
@@ -4571,6 +4571,7 @@ void sculpt_dynamic_topology_enable(bContext *C)
 	ss->bm = BM_mesh_create(&bm_mesh_allocsize_default);
 
 	BM_mesh_bm_from_me(ss->bm, me, TRUE, ob->shapenr);
+	BM_mesh_normals_update(ss->bm, false);
 	sculpt_dynamic_topology_triangulate(ss->bm);
 	BM_data_layer_add(ss->bm, &ss->bm->vdata, CD_PAINT_MASK);
 	BM_mesh_normals_update(ss->bm, TRUE);
