88aa33d3f426837db88c3d4bf880f9433058287e
Campbell Barton <ideasman42@gmail.com>
2013-02-04 12:32:24 +0000

    fix [#34104] vertex color: color select bug
    changing the hue in a color picker on a panel when black/white was selected would fail because the hue from the previous state wasnt stored.

--- source/blender/editors/interface/interface.c
+++ source/blender/editors/interface/interface.c
@@ -927,6 +927,8 @@ void uiEndBlock(const bContext *C, uiBlock *block)
 		block->auto_open = block->oldblock->auto_open;
 		block->auto_open_last = block->oldblock->auto_open_last;
 		block->tooltipdisabled = block->oldblock->tooltipdisabled;
+		copy_v3_v3(ui_block_hsv_get(block),
+		           ui_block_hsv_get(block->oldblock));
 
 		block->oldblock = NULL;
 	}
