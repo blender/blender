8c9906eb9190b40ffec981944174e96e6a471af2
Mitchell Stokes <mogurijin@gmail.com>
2013-02-16 04:38:53 +0000

    BGE: Fix for issue #34242 "It does not render in "P" mode for Game engine if you UV mapa a face of a cube" reported by joaclint.
    Material caching can now be disabled for Multitexture and GLSL Materials.

--- source/gameengine/Converter/BL_BlenderDataConversion.cpp
+++ source/gameengine/Converter/BL_BlenderDataConversion.cpp
@@ -921,9 +921,6 @@ static RAS_MaterialBucket *material_from_mesh(Material *ma, MFace *mface, MTFace
 		/* do Texture Face materials */
 		Image* bima = (tface)? (Image*)tface->tpage: NULL;
 		STR_String imastr =  (tface)? (bima? (bima)->id.name : "" ) : "";
-
-		if (!converter->GetCacheMaterials())
-			polymat = NULL;
 		
 		char alpha_blend=0;
 		short tile=0;
@@ -1045,9 +1042,8 @@ static RAS_MaterialBucket *material_from_mesh(Material *ma, MFace *mface, MTFace
 				polymat->m_specular.setValue(0.0f,0.0f,0.0f);
 				polymat->m_shininess = 35.0;
 			}
-			
-			if (converter->GetCacheMaterials())
-				converter->CachePolyMaterial(ma, polymat);
+
+			converter->CachePolyMaterial(ma, polymat);
 		}
 	}
 	
