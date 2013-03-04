7a1086d9a19152ec986e350a0db9a02398bd4761
Sergey Sharybin <sergey.vfx@gmail.com>
2013-03-04 13:14:21 +0000

    Fix #34475: Weird noise bug with Texture nodes
    Made Texture compositor input node single-threaded since
    texture trees are not thread-safe.
    
    Also fixed texture being flipped horizontally and vertically.
    Why nobody noticed this for 3 releases already??

--- source/blender/compositor/operations/COM_TextureOperation.h
+++ source/blender/compositor/operations/COM_TextureOperation.h
@@ -24,7 +24,7 @@
 #ifndef _COM_TextureOperation_h
 #define _COM_TextureOperation_h
 
-#include "COM_NodeOperation.h"
+#include "COM_SingleThreadedNodeOperation.h"
 #include "DNA_texture_types.h"
 #include "BLI_listbase.h"
 extern "C" {
@@ -39,7 +39,7 @@ extern "C" {
  *
  * @todo: rename to operation.
  */
-class TextureBaseOperation : public NodeOperation {
+class TextureBaseOperation : public SingleThreadedNodeOperation {
 private:
 	Tex *m_texture;
 	const RenderData *m_rd;
@@ -59,6 +59,7 @@ protected:
 	 */
 	TextureBaseOperation();
 
+	MemoryBuffer *createMemoryBuffer(rcti *rect2);
 public:
 	void executePixel(float output[4], float x, float y, PixelSampler sampler);
 
