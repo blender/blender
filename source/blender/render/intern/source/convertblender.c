b4bd232da0516f0ce6ba96759f25461ad7cbc035
Tamito Kajiyama <rd6t-kjym@asahi-net.or.jp>
2013-04-05 19:34:26 +0000

    Removed CD_MASK_FREESTYLE_EDGE and CD_MASK_FREESTYLE_FACE from CD_MASK_BAREMESH and moved them to init_render_mesh() in convertblender.c where these CustomData layers are specifically required.

--- source/blender/render/intern/source/convertblender.c
+++ source/blender/render/intern/source/convertblender.c
@@ -3363,6 +3363,10 @@ static void init_render_mesh(Render *re, ObjectRen *obr, int timeoffset)
 		if (need_orco)
 			mask |= CD_MASK_ORCO;
 
+#ifdef WITH_FREESTYLE
+	mask |= CD_MASK_FREESTYLE_EDGE | CD_MASK_FREESTYLE_FACE;
+#endif
+
 	dm= mesh_create_derived_render(re->scene, ob, mask);
 	if (dm==NULL) return;	/* in case duplicated object fails? */
 
