ce12370b79d670278bcfa0e135e0e4a8c343cd2b
Gaia Clary <gaia.clary@machinimatrix.org>
2013-04-06 22:29:47 +0000

    fix: display weight brush strength widget only in weight paint mode

--- source/blender/editors/sculpt_paint/paint_ops.c
+++ source/blender/editors/sculpt_paint/paint_ops.c
@@ -767,9 +767,10 @@ static void ed_keymap_paint_brush_size(wmKeyMap *keymap, const char *UNUSED(path
 }
 
 typedef enum {
-	RC_COLOR = 1,
+	RC_COLOR    = 1,
 	RC_ROTATION = 2,
-	RC_ZOOM = 4
+	RC_ZOOM     = 4,
+	RC_WEIGHT   = 8
 } RCFlags;
 
 static void set_brush_rc_path(PointerRNA *ptr, const char *brush_path,
@@ -829,8 +830,10 @@ static void ed_keymap_paint_brush_radial_control(wmKeyMap *keymap, const char *p
 	kmi = WM_keymap_add_item(keymap, "WM_OT_radial_control", FKEY, KM_PRESS, KM_SHIFT, 0);
 	set_brush_rc_props(kmi->ptr, paint, "strength", "use_unified_strength", flags_nozoom);
 
-	kmi = WM_keymap_add_item(keymap, "WM_OT_radial_control", WKEY, KM_PRESS, 0, 0);
-	set_brush_rc_props(kmi->ptr, paint, "weight", "use_unified_weight", flags_nozoom);
+	if (flags & RC_WEIGHT) {
+		kmi = WM_keymap_add_item(keymap, "WM_OT_radial_control", WKEY, KM_PRESS, 0, 0);
+		set_brush_rc_props(kmi->ptr, paint, "weight", "use_unified_weight", flags_nozoom);
+	}
 
 	if (flags & RC_ROTATION) {
 		kmi = WM_keymap_add_item(keymap, "WM_OT_radial_control", FKEY, KM_PRESS, KM_CTRL, 0);
@@ -979,7 +982,7 @@ void ED_keymap_paint(wmKeyConfig *keyconf)
 
 	ed_keymap_paint_brush_switch(keymap, "weight_paint");
 	ed_keymap_paint_brush_size(keymap, "tool_settings.weight_paint.brush.size");
-	ed_keymap_paint_brush_radial_control(keymap, "weight_paint", 0);
+	ed_keymap_paint_brush_radial_control(keymap, "weight_paint", RC_WEIGHT);
 
 	ed_keymap_stencil(keymap);
 
