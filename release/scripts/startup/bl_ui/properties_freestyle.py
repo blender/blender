384e14bc84564e2da8ecd358fc89108cae50a17e
Tamito Kajiyama <rd6t-kjym@asahi-net.or.jp>
2013-04-10 20:20:20 +0000

    Added to the Freestyle panel in the Render buttons a button to switch to the Render Layers context.

--- release/scripts/startup/bl_ui/properties_freestyle.py
+++ release/scripts/startup/bl_ui/properties_freestyle.py
@@ -60,7 +60,9 @@ class RENDER_PT_freestyle(RenderFreestyleButtonsPanel, Panel):
         row.active = (rd.line_thickness_mode == 'ABSOLUTE')
         row.prop(rd, "line_thickness")
 
-        layout.label(text="Line style settings are found in the Render Layers context")
+        row = layout.row()
+        row.label(text="Line style settings are found in the Render Layers context")
+        row.operator("wm.properties_context_change", text="", icon='BUTS').context = 'RENDER_LAYER'
 
 
 # Render layer properties
