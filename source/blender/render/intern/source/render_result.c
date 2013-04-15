d3a878688e81613fb722f40ba51314fa6964d470
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-04-15 23:12:54 +0000

    Fix crash rendering environment maps. I think this freestyle code was not intended to be here and probably was left over from some earlier code, as resetting the configuration on render would lead to data loss.

--- source/blender/render/intern/source/render_result.c
+++ source/blender/render/intern/source/render_result.c
@@ -538,8 +538,6 @@ RenderResult *render_result_new(Render *re, rcti *partrct, int crop, int savebuf
 			render_layer_add_pass(rr, rl, 3, SCE_PASS_TRANSM_INDIRECT);
 		if (srl->passflag  & SCE_PASS_TRANSM_COLOR)
 			render_layer_add_pass(rr, rl, 3, SCE_PASS_TRANSM_COLOR);
-		
-		BKE_freestyle_config_init(&srl->freestyleConfig);
 	}
 	/* sss, previewrender and envmap don't do layers, so we make a default one */
 	if (rr->layers.first == NULL && !(layername && layername[0])) {
