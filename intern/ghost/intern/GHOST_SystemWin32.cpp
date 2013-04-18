177080a15dbce20e0156e3bf50bfa6531e685a7e
Alexander Kuznetsov <kuzsasha@gmail.com>
2013-04-18 13:30:41 +0000

    Disabling live resize for Windows for now. Bug #34990
    However, cleanscreen on redraw is still disabled, so when resizing blender will have frozen image.

--- intern/ghost/intern/GHOST_SystemWin32.cpp
+++ intern/ghost/intern/GHOST_SystemWin32.cpp
@@ -983,12 +983,13 @@ LRESULT WINAPI GHOST_SystemWin32::s_wndProc(HWND hwnd, UINT msg, WPARAM wParam, 
 					{
 						eventHandled = true;
 					} else
-					if((wParam&0xfff0)==SC_SIZE)
+						/* XXX Disable for now due to area resizing issue. bug# 34990 */
+					/*if((wParam&0xfff0)==SC_SIZE)
 					{
 						window->registerMouseClickEvent(0);
 						window->m_wsh.startSizing(wParam);
 						eventHandled = true;
-					}
+					}*/
 					break;
 				////////////////////////////////////////////////////////////////////////
 				// Tablet events, processed
