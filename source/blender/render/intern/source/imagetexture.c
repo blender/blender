3b230352828d6ad1eb29610e3e6bdf40f2be67ba
Sergey Sharybin <sergey.vfx@gmail.com>
2013-04-26 14:32:06 +0000

    Fix 35091: Blender internal does not render face texture alpha
    Issue was introduced in svn rev54374.
    
    Now made it so image's flag from imagewrap*() argument
    is checked instead of using tex->ima, which seems how
    this functions were supposed to work.
    
    Doublechecking on this change is really welcome!

--- source/blender/render/intern/source/imagetexture.c
+++ source/blender/render/intern/source/imagetexture.c
@@ -224,7 +224,7 @@ int imagewrap(Tex *tex, Image *ima, ImBuf *ibuf, const float texvec[3], TexResul
 	}
 
 	/* keep this before interpolation [#29761] */
-	if ((tex->imaflag & TEX_USEALPHA) && tex->ima && (tex->ima->flag & IMA_IGNORE_ALPHA) == 0) {
+	if ((tex->imaflag & TEX_USEALPHA) && (tex->ima->flag & IMA_IGNORE_ALPHA) == 0) {
 		if ((tex->imaflag & TEX_CALCALPHA) == 0) {
 			texres->talpha = TRUE;
 		}
@@ -1109,7 +1109,7 @@ static int imagewraposa_aniso(Tex *tex, Image *ima, ImBuf *ibuf, const float tex
 	/* mipmap test */
 	image_mipmap_test(tex, ibuf);
 	
-	if ((tex->imaflag & TEX_USEALPHA) && tex->ima && (tex->ima->flag & IMA_IGNORE_ALPHA) == 0) {
+	if ((tex->imaflag & TEX_USEALPHA) && (ima->flag & IMA_IGNORE_ALPHA) == 0) {
 		if ((tex->imaflag & TEX_CALCALPHA) == 0)
 			texres->talpha = 1;
 	}
@@ -1526,7 +1526,7 @@ int imagewraposa(Tex *tex, Image *ima, ImBuf *ibuf, const float texvec[3], const
 	/* mipmap test */
 	image_mipmap_test(tex, ibuf);
 
-	if ((tex->imaflag & TEX_USEALPHA) && tex->ima && (tex->ima->flag & IMA_IGNORE_ALPHA) == 0) {
+	if ((tex->imaflag & TEX_USEALPHA) && (tex->ima->flag & IMA_IGNORE_ALPHA) == 0) {
 		if ((tex->imaflag & TEX_CALCALPHA) == 0) {
 			texres->talpha = TRUE;
 		}
