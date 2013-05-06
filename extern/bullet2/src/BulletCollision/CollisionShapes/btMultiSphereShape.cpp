424e8b69f1a008aa5e06ec05b67536bc76715695
Sergej Reich <sergej.reich@googlemail.com>
2013-05-06 22:53:11 +0000

    bullet: Don't use sse in api for windows.
    Remove stray BT_USE_SSE_IN_API definitions.
    Was causing problems especially for 32 bit windows.
    
    It's not quite clear why they were added in the first place since
    this should be defined in btScalar.h, needs further investigation.
    
    Thanks to Francisco De La Cruz (xercesblue) for looking into this.
    
    Should fix [#35071] Bullet Convex Hull Crashes on Win32 with SSE

--- extern/bullet2/src/BulletCollision/CollisionShapes/btMultiSphereShape.cpp
+++ extern/bullet2/src/BulletCollision/CollisionShapes/btMultiSphereShape.cpp
@@ -13,9 +13,9 @@ subject to the following restrictions:
 3. This notice may not be removed or altered from any source distribution.
 */
 
-#if defined (_WIN32) || defined (__i386__)
-#define BT_USE_SSE_IN_API
-#endif
+//#if defined (_WIN32) || defined (__i386__)
+//#define BT_USE_SSE_IN_API
+//#endif
 
 #include "btMultiSphereShape.h"
 #include "BulletCollision/CollisionShapes/btCollisionMargin.h"
