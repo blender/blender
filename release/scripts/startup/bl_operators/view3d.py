8e13bb3f602eac73296e98fe149d8bd18165a244
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-05-10 20:49:16 +0000

    Fix #35273: click in empty space to deselect as used by the Maya keymap gave a python error when used in pose mode.

--- release/scripts/startup/bl_operators/view3d.py
+++ release/scripts/startup/bl_operators/view3d.py
@@ -154,6 +154,10 @@ class VIEW3D_OT_select_or_deselect_all(Operator):
                         bpy.ops.mball.select_all(action='DESELECT')
                     elif active_object.type == 'ARMATURE':
                         bpy.ops.armature.select_all(action='DESELECT')
+                elif active_object.mode == 'POSE':
+                    bpy.ops.pose.select_all(action='DESELECT')
+                elif active_object.mode == 'PARTICLE_EDIT':
+                    bpy.ops.particle.select_all(action='DESELECT')
                 else:
                     bpy.ops.object.select_all(action='DESELECT')
             else:
