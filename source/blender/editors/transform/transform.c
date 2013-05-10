50b270ca362e0bac8c20dd9e8226e7b23a550d20
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-05-10 20:17:18 +0000

    Fix #35292: edge slide slow with big image in image editor. Avoid redrawing the image editor during slide like other operators, instant feedback can be nice but is not so important here and we don't have notifiers fine grained enough to avoid full redraws.

--- source/blender/editors/transform/transform.c
+++ source/blender/editors/transform/transform.c
@@ -491,9 +491,6 @@ static void viewRedrawForce(const bContext *C, TransInfo *t)
 		else
 			WM_event_add_notifier(C, NC_OBJECT | ND_TRANSFORM, NULL);
 
-		if (t->mode == TFM_EDGE_SLIDE && (t->settings->uvcalc_flag & UVCALC_TRANSFORM_CORRECT))
-			WM_event_add_notifier(C, NC_GEOM | ND_DATA, NULL);
-
 		/* for realtime animation record - send notifiers recognised by animation editors */
 		// XXX: is this notifier a lame duck?
 		if ((t->animtimer) && IS_AUTOKEY_ON(t->scene))
