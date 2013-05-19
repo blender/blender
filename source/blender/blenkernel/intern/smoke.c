3b2256f9af04ea59a748b437f748a25c86995fdc
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-05-19 04:12:00 +0000

    Fix for last fix, compile error in smoke code.

--- source/blender/blenkernel/intern/smoke.c
+++ source/blender/blenkernel/intern/smoke.c
@@ -1227,7 +1227,6 @@ static void emit_from_particles(Object *flow_ob, SmokeDomainSettings *sds, Smoke
 		sim.scene = scene;
 		sim.ob = flow_ob;
 		sim.psys = psys;
-		sim.rng = BLI_rng_new(psys->seed);
 
 		/* initialize particle cache */
 		if (psys->part->type == PART_HAIR) {
@@ -1403,8 +1402,6 @@ static void emit_from_particles(Object *flow_ob, SmokeDomainSettings *sds, Smoke
 			MEM_freeN(particle_pos);
 		if (particle_vel)
 			MEM_freeN(particle_vel);
-
-		BLI_rng_free(sim.rng);
 	}
 }
 
