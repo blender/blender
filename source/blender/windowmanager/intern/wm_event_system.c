5cdddb12d96d8f6ab3b7f5da470dd6c5a284c71d
Campbell Barton <ideasman42@gmail.com>
2013-05-20 05:06:52 +0000

    fix [#35434] Segmentation fault switching screen layout from python

--- source/blender/windowmanager/intern/wm_event_system.c
+++ source/blender/windowmanager/intern/wm_event_system.c
@@ -247,6 +247,9 @@ void wm_event_do_notifiers(bContext *C)
 			if (note->window == win) {
 				if (note->category == NC_SCREEN) {
 					if (note->data == ND_SCREENBROWSE) {
+						/* free popup handlers */
+						WM_event_remove_handlers(C, &win->modalhandlers);
+
 						ED_screen_set(C, note->reference);  // XXX hrms, think this over!
 						if (G.debug & G_DEBUG_EVENTS)
 							printf("%s: screen set %p\n", __func__, note->reference);
