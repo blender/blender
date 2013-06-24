86bebb338013fd9ac4e39c3f3a8665b1d3b0ed53
Jürgen Herrmann <shadowrom@me.com>
2013-06-24 05:45:03 +0000

    Fix for #35851:
    - Make scons work for non western CL.exe.

--- build_files/scons/config/win64-vc-config.py
+++ build_files/scons/config/win64-vc-config.py
@@ -3,13 +3,13 @@ import subprocess
 CL_OUT = subprocess.Popen(["cl.exe"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 CL_STDOUT, CL_STDERR = CL_OUT.communicate()
 
-if CL_STDERR.find("Version 15.00.") == -1:
-    VC_VERSION = '11.0'
-    LCGDIR = '#../lib/win64_vc11'
-    
-else:
+if CL_STDERR.find("17.00.") == -1:
     VC_VERSION = '9.0'
     LCGDIR = '#../lib/win64'
+    
+else:
+    VC_VERSION = '11.0'
+    LCGDIR = '#../lib/win64_vc11'
 
 LIBDIR = '${LCGDIR}'
 
