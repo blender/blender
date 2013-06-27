b466a5c9a9f1147da9f3c0e5970c740b62e142f5
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-06-27 13:24:55 +0000

    Fix #35890: memory leak in OS X ghost locale detection.

--- intern/locale/boost_locale_wrapper.cpp
+++ intern/locale/boost_locale_wrapper.cpp
@@ -65,8 +65,8 @@ void bl_locale_set(const char *locale)
 		}
 		else {
 #ifdef __APPLE__
-			extern const char *user_locale; // pulled from Ghost_SystemCocoa
-			std::string locale_osx = user_locale + std::string(".UTF-8");
+			extern char GHOST_user_locale[128]; // pulled from Ghost_SystemCocoa
+			std::string locale_osx = GHOST_user_locale + std::string(".UTF-8");
 			_locale = gen(locale_osx.c_str());
 #else
 			_locale = gen("");
