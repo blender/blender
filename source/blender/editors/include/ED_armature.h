ea5f0ec96245b2bc26d7f21090752b57a7de0aca
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-07-04 14:59:26 +0000

    Fix #35997: add armature > single bone, then change location or rotation would move only the origin and not the bone. It doesn't need to use any object matrices to add the bone, the only reason this worked before is because they were still unit matrices due to depsgraph not running immediately on add.

--- source/blender/editors/include/ED_armature.h
+++ source/blender/editors/include/ED_armature.h
@@ -132,7 +132,7 @@ EditBone *ED_armature_bone_get_mirrored(struct ListBase *edbo, EditBone *ebo); /
 void ED_armature_sync_selection(struct ListBase *edbo);
 void ED_armature_validate_active(struct bArmature *arm);
 
-void add_primitive_bone(struct Scene *scene, struct View3D *v3d, struct RegionView3D *rv3d);
+void add_primitive_bone(struct Object *obedit_arm, bool view_aligned);
 struct EditBone *ED_armature_edit_bone_add(struct bArmature *arm, const char *name);
 void ED_armature_edit_bone_remove(struct bArmature *arm, EditBone *exBone);
 bool ED_armature_ebone_is_child_recursive(EditBone *ebone_parent, EditBone *ebone_child);
