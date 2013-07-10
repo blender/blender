ffaffd32cb1f4c5e54a496665250fcf55a0f0108
Sergey Sharybin <sergey.vfx@gmail.com>
2013-07-10 11:36:47 +0000

    Get rid of confusing usage of cu->bb in BKE_object_minmax
    Curve-type objects now always does have object'level
    bounding box, meaning cu->bb wouldn't evenr used in
    BKE_object_minmax.
    
    So zapping this confusing check for whether ob->bb
    exists, which is always truth. Makes it easier to
    follow what cu->bb is for and what ob->bb is for.

--- source/blender/blenkernel/intern/object.c
+++ source/blender/blenkernel/intern/object.c
@@ -2366,13 +2366,7 @@ void BKE_object_minmax(Object *ob, float min_r[3], float max_r[3], const bool us
 
 			/* Use the object bounding box so that modifier output
 			 * gets taken into account */
-			if (ob->bb)
-				bb = *(ob->bb);
-			else {
-				if (cu->bb == NULL)
-					BKE_curve_texspace_calc(cu);
-				bb = *(cu->bb);
-			}
+			bb = *(ob->bb);
 
 			for (a = 0; a < 8; a++) {
 				mul_m4_v3(ob->obmat, bb.vec[a]);
