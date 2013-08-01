9caaa78c32a4f990e9dae5cc6113c177089424a0
Thomas Dinges <blender@dingto.org>
2013-08-01 14:00:24 +0000

    Fix for r58798, forgot to update the presets.

--- release/scripts/presets/cycles/sampling/preview.py
+++ release/scripts/presets/cycles/sampling/preview.py
@@ -3,10 +3,13 @@ cycles = bpy.context.scene.cycles
 
 cycles.squared_samples = True
 
-cycles.samples = 12
-cycles.preview_samples = 12
+# Progressive
+cycles.samples = 100
+cycles.preview_samples = 10
+
+# Non-Progressive (squared)
 cycles.aa_samples = 4
-cycles.preview_aa_samples = 4
+cycles.preview_aa_samples = 2
 
 cycles.diffuse_samples = 3
 cycles.glossy_samples = 2
