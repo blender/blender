226d6fbfc5c91b2ab3f801e26471584cfd795fa5
Tamito Kajiyama <rd6t-kjym@asahi-net.or.jp>
2013-09-26 16:41:47 +0000

    Fix for the name of line style ID blocks, from "FreestyleLineStyle" to "Line Style".

--- source/blender/makesrna/intern/rna_ID.c
+++ source/blender/makesrna/intern/rna_ID.c
@@ -58,7 +58,7 @@ EnumPropertyItem id_type_items[] = {
 	{ID_KE, "KEY", ICON_SHAPEKEY_DATA, "Key", ""},
 	{ID_LA, "LAMP", ICON_LAMP_DATA, "Lamp", ""},
 	{ID_LI, "LIBRARY", ICON_LIBRARY_DATA_DIRECT, "Library", ""},
-	{ID_LS, "LINESTYLE", ICON_BRUSH_DATA, "FreestyleLineStyle", ""}, /* FIXME proper icon */
+	{ID_LS, "LINESTYLE", ICON_BRUSH_DATA, "Line Style", ""}, /* FIXME proper icon */
 	{ID_LT, "LATTICE", ICON_LATTICE_DATA, "Lattice", ""},
 	{ID_MA, "MATERIAL", ICON_MATERIAL_DATA, "Material", ""},
 	{ID_MB, "META", ICON_META_DATA, "MetaBall", ""},
