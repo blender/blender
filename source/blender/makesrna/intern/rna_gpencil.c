fd9cb5a59e96d9f6e3e5afaa9043417f44293057
Bastien Montagne <montagne29@wanadoo.fr>
2013-09-29 18:12:07 +0000

    Fix [#36859] Grease Pencil - Frames.new() doesn't create frame if frame_number <= 0
    Related to [#36831] (r60412), RNA was still forbidding negative frame numbers...

--- source/blender/makesrna/intern/rna_gpencil.c
+++ source/blender/makesrna/intern/rna_gpencil.c
@@ -390,7 +390,8 @@ static void rna_def_gpencil_frame(BlenderRNA *brna)
 	/* Frame Number */
 	prop = RNA_def_property(srna, "frame_number", PROP_INT, PROP_NONE);
 	RNA_def_property_int_sdna(prop, NULL, "framenum");
-	RNA_def_property_range(prop, MINFRAME, MAXFRAME); /* XXX note: this cannot occur on the same frame as another sketch */
+	/* XXX note: this cannot occur on the same frame as another sketch */
+	RNA_def_property_range(prop, MINAFRAME, MAXFRAME);
 	RNA_def_property_ui_text(prop, "Frame Number", "The frame on which this sketch appears");
 	
 	/* Flags */
@@ -421,7 +422,8 @@ static void rna_def_gpencil_frames_api(BlenderRNA *brna, PropertyRNA *cprop)
 	func = RNA_def_function(srna, "new", "rna_GPencil_frame_new");
 	RNA_def_function_ui_description(func, "Add a new grease pencil frame");
 	RNA_def_function_flag(func, FUNC_USE_REPORTS);
-	parm = RNA_def_int(func, "frame_number", 1, MINFRAME, MAXFRAME, "Frame Number", "The frame on which this sketch appears", MINFRAME, MAXFRAME);
+	parm = RNA_def_int(func, "frame_number", 1, MINAFRAME, MAXFRAME, "Frame Number",
+	                   "The frame on which this sketch appears", MINAFRAME, MAXFRAME);
 	RNA_def_property_flag(parm, PROP_REQUIRED);
 	parm = RNA_def_pointer(func, "frame", "GPencilFrame", "", "The newly created frame");
 	RNA_def_function_return(func, parm);
