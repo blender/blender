1fa59cdf4dcae382904ea06bfdeba19d89a9f0ad
Campbell Barton <ideasman42@gmail.com>
2013-10-01 15:52:29 +0000

    allow knife-project to use sub-pixel length edges.

--- source/blender/editors/mesh/editmesh_knife.c
+++ source/blender/editors/mesh/editmesh_knife.c
@@ -1546,8 +1546,16 @@ static void knife_find_line_hits(KnifeTool_OpData *kcd)
 	knife_project_v2(kcd, v1, s1);
 	knife_project_v2(kcd, v2, s2);
 
-	if (len_squared_v2v2(s1, s2) < 1)
-		return;
+	if (kcd->is_interactive) {
+		if (len_squared_v2v2(s1, s2) < 1.0f) {
+			return;
+		}
+	}
+	else {
+		if (len_squared_v2v2(s1, s2) < KNIFE_FLT_EPS_SQUARED) {
+			return;
+		}
+	}
 
 	/* unproject screen line */
 	ED_view3d_win_to_segment(kcd->ar, kcd->vc.v3d, s1, v1, v3, true);
