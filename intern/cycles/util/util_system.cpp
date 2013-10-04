6737a040614180f8ac27221e29bb35a09485fedf
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-10-04 14:47:37 +0000

    Attempt to fix #36935: disable SSE optimizations on 32 bit windows too. Something strange is going on here, but I don't think it can be fixed before the release, if it is worth at all spending time on this.

--- intern/cycles/util/util_system.cpp
+++ intern/cycles/util/util_system.cpp
@@ -116,8 +116,8 @@ int system_cpu_bits()
 	return (sizeof(void*)*8);
 }
 
-/* SSE optimization disabled for now on 32 bit GCC, see bug #36316 */
-#if defined(__x86_64__) || defined(_M_X64) || ((defined(i386) || defined(_M_IX86)) && !defined(__GNUC__))
+/* SSE optimization disabled for now on 32 bit, see bug #36316 and #36935 */
+#if defined(__x86_64__) || defined(_M_X64) // ((defined(i386) || defined(_M_IX86))
 
 struct CPUCapabilities {
 	bool x64;
