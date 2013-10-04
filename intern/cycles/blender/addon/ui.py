4c5d6dcbafeafa724530950c8b9f19306c802239
Brecht Van Lommel <brechtvanlommel@pandora.be>
2013-10-04 18:04:29 +0000

    Fix cycles hair render UI panel showing for particles that are not hair, patch by Greg Zaal.

--- intern/cycles/blender/addon/ui.py
+++ intern/cycles/blender/addon/ui.py
@@ -1180,7 +1180,7 @@ class CyclesRender_PT_CurveRendering(CyclesButtonsPanel, Panel):
         scene = context.scene
         cscene = scene.cycles
         psys = context.particle_system
-        return CyclesButtonsPanel.poll(context) and psys
+        return CyclesButtonsPanel.poll(context) and psys and psys.settings.type == 'HAIR'
 
     def draw_header(self, context):
         ccscene = context.scene.cycles_curves
@@ -1220,8 +1220,9 @@ class CyclesParticle_PT_CurveSettings(CyclesButtonsPanel, Panel):
         scene = context.scene
         cscene = scene.cycles
         ccscene = scene.cycles_curves
-        use_curves = ccscene.use_curves and context.particle_system
-        return CyclesButtonsPanel.poll(context) and use_curves
+        psys = context.particle_system
+        use_curves = ccscene.use_curves and psys
+        return CyclesButtonsPanel.poll(context) and use_curves and psys.settings.type == 'HAIR'
 
     def draw(self, context):
         layout = self.layout
