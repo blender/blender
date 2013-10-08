bfd6f6041dadc78fc6c41f5fbb9725a0171d64b2
Campbell Barton <ideasman42@gmail.com>
2013-10-08 19:47:53 +0000

    remove redundant NULL checks

--- source/blender/editors/space_view3d/view3d_select.c
+++ source/blender/editors/space_view3d/view3d_select.c
@@ -725,7 +725,7 @@ static void do_lasso_select_paintvert(ViewContext *vc, const int mcords[][2], sh
 {
 	const int use_zbuf = (vc->v3d->flag & V3D_ZBUF_SELECT);
 	Object *ob = vc->obact;
-	Mesh *me = ob ? ob->data : NULL;
+	Mesh *me = ob->data;
 	rcti rect;
 
 	if (me == NULL || me->totvert == 0)
@@ -764,7 +764,7 @@ static void do_lasso_select_paintvert(ViewContext *vc, const int mcords[][2], sh
 static void do_lasso_select_paintface(ViewContext *vc, const int mcords[][2], short moves, bool extend, bool select)
 {
 	Object *ob = vc->obact;
-	Mesh *me = ob ? ob->data : NULL;
+	Mesh *me = ob->data;
 	rcti rect;
 
 	if (me == NULL || me->totpoly == 0)
@@ -2405,7 +2405,7 @@ static void mesh_circle_select(ViewContext *vc, const bool select, const int mva
 static void paint_facesel_circle_select(ViewContext *vc, const bool select, const int mval[2], float rad)
 {
 	Object *ob = vc->obact;
-	Mesh *me = ob ? ob->data : NULL;
+	Mesh *me = ob->data;
 	bool bbsel;
 
 	bm_vertoffs = me->totpoly + 1; /* max index array */
