916f793a7046c1447d217b4360647a7812dde48e
Campbell Barton <ideasman42@gmail.com>
2013-10-08 11:27:39 +0000

    code cleanup: strange formatting.

--- intern/ghost/intern/GHOST_NDOFManager3Dconnexion.c
+++ intern/ghost/intern/GHOST_NDOFManager3Dconnexion.c
@@ -30,7 +30,7 @@
 
 /* It is to be noted that these implementations are linked in as
  * 'extern "C"' calls from GHOST_NDOFManagerCocoa.
- 
+
  * This is done in order to
  * preserve weak linking capability (which as of clang-3.3 and xcode5
  * breaks weak linking when there is name mangling of c++ libraries.)
@@ -42,42 +42,53 @@
 
 OSErr GHOST_NDOFManager3Dconnexion_available(void)
 {
-    // extern unsigned int InstallConnexionHandlers() __attribute__((weak_import));
-    // Make the linker happy for the framework check (see link below for more info)
-    // http://developer.apple.com/documentation/MacOSX/Conceptual/BPFrameworks/Concepts/WeakLinking.html
-    return InstallConnexionHandlers != 0;
-    // this means that the driver is installed and dynamically linked to blender
+	// extern unsigned int InstallConnexionHandlers() __attribute__((weak_import));
+	// Make the linker happy for the framework check (see link below for more info)
+	// http://developer.apple.com/documentation/MacOSX/Conceptual/BPFrameworks/Concepts/WeakLinking.html
+	return InstallConnexionHandlers != 0;
+	// this means that the driver is installed and dynamically linked to blender
 }
 
 OSErr GHOST_NDOFManager3Dconnexion_oldDRV()
 {
-    //extern unsigned int SetConnexionClientButtonMask() __attribute__((weak_import));
-    // Make the linker happy for the framework check (see link below for more info)
-    // http://developer.apple.com/documentation/MacOSX/Conceptual/BPFrameworks/Concepts/WeakLinking.html
-    return SetConnexionClientButtonMask != 0;
-    // this means that the driver has this symbol
+	//extern unsigned int SetConnexionClientButtonMask() __attribute__((weak_import));
+	// Make the linker happy for the framework check (see link below for more info)
+	// http://developer.apple.com/documentation/MacOSX/Conceptual/BPFrameworks/Concepts/WeakLinking.html
+	return SetConnexionClientButtonMask != 0;
+	// this means that the driver has this symbol
 }
 
-UInt16			GHOST_NDOFManager3Dconnexion_RegisterConnexionClient				(UInt32 signature, UInt8 *name, UInt16 mode, UInt32 mask) {
-    return RegisterConnexionClient(signature,  name,  mode,  mask);
-};
-void			GHOST_NDOFManager3Dconnexion_SetConnexionClientButtonMask		(UInt16 clientID, UInt32 buttonMask){
-    return SetConnexionClientButtonMask( clientID,  buttonMask);
-};
-void			GHOST_NDOFManager3Dconnexion_UnregisterConnexionClient			(UInt16 clientID){
-    return UnregisterConnexionClient( clientID);
-};
-OSErr			GHOST_NDOFManager3Dconnexion_InstallConnexionHandlers			(ConnexionMessageHandlerProc messageHandler, ConnexionAddedHandlerProc addedHandler, ConnexionRemovedHandlerProc removedHandler){
-    return InstallConnexionHandlers( messageHandler,  addedHandler,  removedHandler);
-    
-    
-};
-void			GHOST_NDOFManager3Dconnexion_CleanupConnexionHandlers			(void){
-    return CleanupConnexionHandlers();
-};
-OSErr	GHOST_NDOFManager3Dconnexion_ConnexionControl(UInt32 message, SInt32 param, SInt32 *result){
-return ConnexionControl( message,  param, result);
+UInt16 GHOST_NDOFManager3Dconnexion_RegisterConnexionClient(UInt32 signature, UInt8 *name, UInt16 mode, UInt32 mask)
+{
+	return RegisterConnexionClient(signature,  name,  mode,  mask);
 }
 
+void GHOST_NDOFManager3Dconnexion_SetConnexionClientButtonMask(UInt16 clientID, UInt32 buttonMask)
+{
+	return SetConnexionClientButtonMask( clientID,  buttonMask);
+}
+
+void GHOST_NDOFManager3Dconnexion_UnregisterConnexionClient(UInt16 clientID)
+{
+	return UnregisterConnexionClient( clientID);
+}
+
+OSErr GHOST_NDOFManager3Dconnexion_InstallConnexionHandlers(
+        ConnexionMessageHandlerProc messageHandler,
+        ConnexionAddedHandlerProc addedHandler,
+        ConnexionRemovedHandlerProc removedHandler)
+{
+	return InstallConnexionHandlers( messageHandler,  addedHandler,  removedHandler);
+}
+
+void GHOST_NDOFManager3Dconnexion_CleanupConnexionHandlers(void)
+{
+	return CleanupConnexionHandlers();
+}
+
+OSErr GHOST_NDOFManager3Dconnexion_ConnexionControl(UInt32 message, SInt32 param, SInt32 *result)
+{
+	return ConnexionControl( message,  param, result);
+}
 
 #endif // WITH_INPUT_NDOF
