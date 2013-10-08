dfe16105041292a1fc7ee29d825c25135a4f6a3c
Thomas Dinges <blender@dingto.org>
2013-10-08 15:23:19 +0000

    Release Maintenance: * Blender 2.70 release cycle begins, BCon1.

--- source/blender/blenkernel/BKE_blender.h
+++ source/blender/blenkernel/BKE_blender.h
@@ -41,15 +41,15 @@ extern "C" {
 /* these lines are grep'd, watch out for our not-so-awesome regex
  * and keep comment above the defines.
  * Use STRINGIFY() rather than defining with quotes */
-#define BLENDER_VERSION         268
-#define BLENDER_SUBVERSION      5
+#define BLENDER_VERSION         269
+#define BLENDER_SUBVERSION      0
 /* 262 was the last editmesh release but it has compatibility code for bmesh data */
 #define BLENDER_MINVERSION      262
 #define BLENDER_MINSUBVERSION   0
 
 /* used by packaging tools */
 /* can be left blank, otherwise a,b,c... etc with no quotes */
-#define BLENDER_VERSION_CHAR   a
+#define BLENDER_VERSION_CHAR   
 /* alpha/beta/rc/release, docs use this */
 #define BLENDER_VERSION_CYCLE   alpha
 
