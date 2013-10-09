0a862cb61a5193b1a0effd3926ba53286d04e55e
Sergey Sharybin <sergey.vfx@gmail.com>
2013-10-09 20:45:29 +0000

    Fix compilation error of OCIO with MSVC

--- intern/opencolorio/ocio_impl_glsl.cc
+++ intern/opencolorio/ocio_impl_glsl.cc
@@ -48,7 +48,9 @@ using namespace OCIO_NAMESPACE;
 
 static const int LUT3D_EDGE_SIZE = 64;
 
-extern char datatoc_gpu_shader_display_transform_glsl[];
+extern "C" {
+	extern char datatoc_gpu_shader_display_transform_glsl[];
+}
 
 /* **** OpenGL drawing routines using GLSL for color space transform ***** */
 
