223c637a9383d01db809a888b6f82c70231910af
Thomas Dinges <blender@dingto.org>
2013-10-10 16:11:57 +0000

    * Fix Windows compiler errors after recent Lock-free memory allocator commit. Patch by Sergey, thanks. :)

--- intern/guardedalloc/mmap_win.h
+++ intern/guardedalloc/mmap_win.h
@@ -51,6 +51,8 @@
 /* needed for uintptr_t, exception, dont use BLI anywhere else in MEM_* */
 #include "../../source/blender/blenlib/BLI_sys_types.h"
 
+#include <sys/types.h>
+
 void *mmap(void *start, size_t len, int prot, int flags, int fd, off_t offset);
 intptr_t munmap(void *ptr, size_t size);
 
