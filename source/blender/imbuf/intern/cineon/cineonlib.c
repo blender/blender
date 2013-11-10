73c2253ac629665715ec700b049e97fe1b17bc56
Campbell Barton <ideasman42@gmail.com>
2013-11-10 12:29:15 +0000

    mistake in last commit

--- source/blender/imbuf/intern/cineon/cineonlib.c
+++ source/blender/imbuf/intern/cineon/cineonlib.c
@@ -277,10 +277,10 @@ LogImageFile *cineonOpen(const unsigned char *byteStuff, int fromMemory, size_t 
 				return NULL;
 		}
 
-		if (cineon->element[i].refLowData == CINEON_UNDEFINED_U32 || cineon->element[i].refLowData)
+		if (cineon->element[i].refLowData == CINEON_UNDEFINED_U32)
 			cineon->element[i].refLowData = 0;
 
-		if (cineon->element[i].refHighData == CINEON_UNDEFINED_U32 || cineon->element[i].refHighData)
+		if (cineon->element[i].refHighData == CINEON_UNDEFINED_U32)
 			cineon->element[i].refHighData = (unsigned int)cineon->element[i].maxValue;
 
 		if (cineon->element[i].refLowQuantity == CINEON_UNDEFINED_R32 || isnan(cineon->element[i].refLowQuantity))
