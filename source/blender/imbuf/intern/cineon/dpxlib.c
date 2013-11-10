07534f05d5cc5ad7f31d6544da6e2aafb0051508
Campbell Barton <ideasman42@gmail.com>
2013-11-10 12:26:03 +0000

    no need to call isnan() on unsigned ints (causes error on freebsd/clang)

--- source/blender/imbuf/intern/cineon/dpxlib.c
+++ source/blender/imbuf/intern/cineon/dpxlib.c
@@ -300,10 +300,10 @@ LogImageFile *dpxOpen(const unsigned char *byteStuff, int fromMemory, size_t buf
 			case descriptor_RGB:
 			case descriptor_RGBA:
 			case descriptor_ABGR:
-				if (dpx->element[i].refLowData == DPX_UNDEFINED_U32 || isnan(dpx->element[i].refLowData))
+				if (dpx->element[i].refLowData == DPX_UNDEFINED_U32 || dpx->element[i].refLowData)
 					dpx->element[i].refLowData = 0;
 
-				if (dpx->element[i].refHighData == DPX_UNDEFINED_U32 || isnan(dpx->element[i].refHighData))
+				if (dpx->element[i].refHighData == DPX_UNDEFINED_U32 || dpx->element[i].refHighData)
 					dpx->element[i].refHighData = (unsigned int)dpx->element[i].maxValue;
 
 				if (dpx->element[i].refLowQuantity == DPX_UNDEFINED_R32 || isnan(dpx->element[i].refLowQuantity))
@@ -324,10 +324,10 @@ LogImageFile *dpxOpen(const unsigned char *byteStuff, int fromMemory, size_t buf
 			case descriptor_CbYCr:
 			case descriptor_CbYACrYA:
 			case descriptor_CbYCrA:
-				if (dpx->element[i].refLowData == DPX_UNDEFINED_U32 || isnan(dpx->element[i].refLowData))
+				if (dpx->element[i].refLowData == DPX_UNDEFINED_U32 || dpx->element[i].refLowData)
 					dpx->element[i].refLowData = 16.0f / 255.0f * dpx->element[i].maxValue;
 
-				if (dpx->element[i].refHighData == DPX_UNDEFINED_U32 || isnan(dpx->element[i].refHighData))
+				if (dpx->element[i].refHighData == DPX_UNDEFINED_U32 || dpx->element[i].refHighData)
 					dpx->element[i].refHighData = 235.0f / 255.0f * dpx->element[i].maxValue;
 
 				if (dpx->element[i].refLowQuantity == DPX_UNDEFINED_R32 || isnan(dpx->element[i].refLowQuantity))
