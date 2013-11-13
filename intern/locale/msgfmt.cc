efd518b3795e4548fcd6ca849c81ccd17eb6dbe8
Sergey Sharybin <sergey.vfx@gmail.com>
2013-11-13 17:02:28 +0600

    Fix for msgfmt leaving context from previous message string

--- intern/locale/msgfmt.cc
+++ intern/locale/msgfmt.cc
@@ -221,6 +221,7 @@ void make(const char *input_file_name,
     if (l[0] == '#' && section == SECTION_STR) {
       add(msgctxt, msgid, msgstr, fuzzy);
       section = SECTION_NONE;
+      msgctxt = "";
       fuzzy = false;
     }
     // Record a fuzzy mark.
