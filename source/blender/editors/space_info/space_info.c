d846c9a3b75c3d6f20bc7ab7d2da6cdd18bbbef2
Jens Verwiebe <info@jensverwiebe.de>
2013-11-23 16:39:07 +0100

    OSX: fix T37065 cmd+c not working in Info - console

--- source/blender/editors/space_info/space_info.c
+++ source/blender/editors/space_info/space_info.c
@@ -220,6 +220,9 @@ static void info_keymap(struct wmKeyConfig *keyconf)
 	WM_keymap_add_item(keymap, "INFO_OT_report_delete", XKEY, KM_PRESS, 0, 0);
 	WM_keymap_add_item(keymap, "INFO_OT_report_delete", DELKEY, KM_PRESS, 0, 0);
 	WM_keymap_add_item(keymap, "INFO_OT_report_copy", CKEY, KM_PRESS, KM_CTRL, 0);
+#ifdef __APPLE__
+    WM_keymap_add_item(keymap, "INFO_OT_report_copy", CKEY, KM_PRESS, KM_OSKEY, 0);
+#endif
 }
 
 /* add handlers, stuff you only do once or on area/region changes */
