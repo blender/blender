20d96761cbadbe738f94844f5757f6ca67bae5dd
Thomas Beck <software@plasmasolutions.de>
2013-11-25 21:44:19 +0100

    Code cleanup: As Sergey stated on bf-committers one should use id_us_min() if the user counter has to be decremented and not decrement the counter by hand.

--- source/blender/editors/space_image/image_ops.c
+++ source/blender/editors/space_image/image_ops.c
@@ -1793,7 +1793,7 @@ static int image_new_exec(bContext *C, wmOperator *op)
 		Tex *tex = CTX_data_pointer_get_type(C, "texture", &RNA_Texture).data;
 		if (tex && tex->type == TEX_IMAGE) {
 			if (tex->ima)
-				tex->ima->id.us--;
+				id_us_min(&tex->ima->id);
 			tex->ima = ima;
 			ED_area_tag_redraw(CTX_wm_area(C));
 		}
