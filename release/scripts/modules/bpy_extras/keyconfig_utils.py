e63d5f23565a4fe426f45fec4152d55a7eafb2d8
Campbell Barton <ideasman42@gmail.com>
2013-12-04 00:40:45 +1100

    Keymap: add walk mode

--- release/scripts/modules/bpy_extras/keyconfig_utils.py
+++ release/scripts/modules/bpy_extras/keyconfig_utils.py
@@ -60,6 +60,7 @@ KM_HIERARCHY = [
 
         ('Object Non-modal', 'EMPTY', 'WINDOW', []),  # mode change
 
+        ('View3D Walk Modal', 'EMPTY', 'WINDOW', []),
         ('View3D Fly Modal', 'EMPTY', 'WINDOW', []),
         ('View3D Rotate Modal', 'EMPTY', 'WINDOW', []),
         ('View3D Move Modal', 'EMPTY', 'WINDOW', []),
