c05209ec017c2aad97d83f36ca2b10eb4b5aee18
Brecht Van Lommel <brechtvanlommel@gmail.com>
2013-12-16 11:07:44 +0100

    Fix T37827: cycles XML mesh export utility not longer worked.

--- intern/cycles/app/io_export_cycles_xml.py
+++ intern/cycles/app/io_export_cycles_xml.py
@@ -116,7 +116,7 @@ class ExportCyclesXML(bpy.types.Operator, ExportHelper):
         for v in mesh.vertices:
             P += "%f %f %f  " % (v.co[0], v.co[1], v.co[2])
 
-        for i, f in enumerate(mesh.faces):
+        for i, f in enumerate(mesh.tessfaces):
             nverts += str(len(f.vertices)) + " "
 
             for v in f.vertices:
