7a3594dbd7ffbca8a9cbe0db4507907907104334
Campbell Barton <ideasman42@gmail.com>
2013-12-26 08:28:00 +1100

    BGE: Add missing NULL check from recent LOD updates

--- source/gameengine/Converter/BL_BlenderDataConversion.cpp
+++ source/gameengine/Converter/BL_BlenderDataConversion.cpp
@@ -942,10 +942,11 @@ RAS_MeshObject* BL_ConvertMesh(Mesh* mesh, Object* blenderobj, KX_Scene* scene, 
 	// Without checking names, we get some reuse we don't want that can cause
 	// problems with material LoDs.
 	if ((meshobj = converter->FindGameMesh(mesh/*, ob->lay*/)) != NULL) {
-		STR_String bge_name = meshobj->GetName();
-		STR_String blender_name = ((Mesh*)blenderobj->data)->id.name+2;
-		if (bge_name == blender_name)
+		const char *bge_name = meshobj->GetName().ReadPtr();
+		const char *blender_name = ((ID *)blenderobj->data)->name + 2;
+		if (STREQ(bge_name, blender_name)) {
 			return meshobj;
+		}
 	}
 
 	// Get DerivedMesh data
