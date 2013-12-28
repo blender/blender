133f770ab3e7a6db49562cb73cb7aa5f75cf13f8
Brecht Van Lommel <brechtvanlommel@gmail.com>
2013-12-28 02:56:53 +0100

    Code cleanup: move shadow_blocked function into separate file.

--- intern/cycles/kernel/kernel_path.h
+++ intern/cycles/kernel/kernel_path.h
@@ -40,85 +40,9 @@
 #include "kernel_subsurface.h"
 #endif
 
+#include "kernel_shadow.h"
+
 CCL_NAMESPACE_BEGIN
-
-ccl_device_inline bool shadow_blocked(KernelGlobals *kg, PathState *state, Ray *ray, float3 *shadow)
-{
-	*shadow = make_float3(1.0f, 1.0f, 1.0f);
-
-	if(ray->t == 0.0f)
-		return false;
-	
-	Intersection isect;
-#ifdef __HAIR__
-	bool result = scene_intersect(kg, ray, PATH_RAY_SHADOW_OPAQUE, &isect, NULL, 0.0f, 0.0f);
-#else
-	bool result = scene_intersect(kg, ray, PATH_RAY_SHADOW_OPAQUE, &isect);
-#endif
-
-#ifdef __TRANSPARENT_SHADOWS__
-	if(result && kernel_data.integrator.transparent_shadows) {
-		/* transparent shadows work in such a way to try to minimize overhead
-		 * in cases where we don't need them. after a regular shadow ray is
-		 * cast we check if the hit primitive was potentially transparent, and
-		 * only in that case start marching. this gives on extra ray cast for
-		 * the cases were we do want transparency.
-		 *
-		 * also note that for this to work correct, multi close sampling must
-		 * be used, since we don't pass a random number to shader_eval_surface */
-		if(shader_transparent_shadow(kg, &isect)) {
-			float3 throughput = make_float3(1.0f, 1.0f, 1.0f);
-			float3 Pend = ray->P + ray->D*ray->t;
-			int bounce = state->transparent_bounce;
-
-			for(;;) {
-				if(bounce >= kernel_data.integrator.transparent_max_bounce) {
-					return true;
-				}
-				else if(bounce >= kernel_data.integrator.transparent_min_bounce) {
-					/* todo: get random number somewhere for probabilistic terminate */
-#if 0
-					float probability = average(throughput);
-					float terminate = 0.0f;
-
-					if(terminate >= probability)
-						return true;
-
-					throughput /= probability;
-#endif
-				}
-
-#ifdef __HAIR__
-				if(!scene_intersect(kg, ray, PATH_RAY_SHADOW_TRANSPARENT, &isect, NULL, 0.0f, 0.0f)) {
-#else
-				if(!scene_intersect(kg, ray, PATH_RAY_SHADOW_TRANSPARENT, &isect)) {
-#endif
-					*shadow *= throughput;
-					return false;
-				}
-
-				if(!shader_transparent_shadow(kg, &isect))
-					return true;
-
-				ShaderData sd;
-				shader_setup_from_ray(kg, &sd, &isect, ray, state->bounce+1);
-				shader_eval_surface(kg, &sd, 0.0f, PATH_RAY_SHADOW, SHADER_CONTEXT_SHADOW);
-
-				throughput *= shader_bsdf_transparency(kg, &sd);
-
-				ray->P = ray_offset(sd.P, -sd.Ng);
-				if(ray->t != FLT_MAX)
-					ray->D = normalize_len(Pend - ray->P, &ray->t);
-
-				bounce++;
-			}
-		}
-	}
-#endif
-
-	return result;
-}
-
 
 #if defined(__BRANCHED_PATH__) || defined(__SUBSURFACE__)
 
