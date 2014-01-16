4f87ca47a6daf7e748ed0f1f078c884a052ba7da
Sergey Sharybin <sergey.vfx@gmail.com>
2014-01-16 15:05:04 +0600

    Compilation error fix: mismatch declaration

--- source/blender/editors/space_outliner/outliner_edit.c
+++ source/blender/editors/space_outliner/outliner_edit.c
@@ -320,7 +320,7 @@ static int outliner_count_levels(SpaceOops *soops, ListBase *lb, const int curle
 	return level;
 }
 
-int outliner_has_one_flag(SpaceOops *soops, ListBase *lb, short flag, const int curlevel)
+bool outliner_has_one_flag(SpaceOops *soops, ListBase *lb, short flag, const int curlevel)
 {
 	TreeElement *te;
 	TreeStoreElem *tselem;
