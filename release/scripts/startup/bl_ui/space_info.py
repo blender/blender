3110526bcacbb11717d99b87d3a00cc99e8b3ef1
gaiaclary <gaia.clary@machinimatrix.org>
2014-01-16 21:42:16 +0100

    Improved the Quit Confirm behaviour: Now confirm only shows up when current session is dirty

--- release/scripts/startup/bl_ui/space_info.py
+++ release/scripts/startup/bl_ui/space_info.py
@@ -83,7 +83,6 @@ class INFO_MT_file(Menu):
     bl_label = "File"
 
     def draw(self, context):
-        import sys
         layout = self.layout
 
         layout.operator_context = 'INVOKE_AREA'
@@ -132,7 +131,7 @@ class INFO_MT_file(Menu):
         layout.separator()
 
         layout.operator_context = 'EXEC_AREA'
-        if sys.platform == "darwin":
+        if bpy.data.is_dirty:
             layout.operator_context = 'INVOKE_SCREEN' # quit dialog
         layout.operator("wm.quit_blender", text="Quit", icon='QUIT')
 
