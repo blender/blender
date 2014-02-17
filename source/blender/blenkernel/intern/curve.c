1a4b718582efdfc39e81e86577d5d25a6bed9e98
Brecht Van Lommel <brechtvanlommel@gmail.com>
2014-02-17 22:01:38 +0100

    Fix crash on file load with linked groups and curve bevel.
    This isn't a proper fix, curve_cache should not be NULL, but it makes it
    possible to open the file at least.

--- source/blender/blenkernel/intern/curve.c
+++ source/blender/blenkernel/intern/curve.c
@@ -1647,9 +1647,12 @@ void BKE_curve_bevel_make(Scene *scene, Object *ob, ListBase *disp, int forRende
 				BKE_displist_make_curveTypes_forRender(scene, cu->bevobj, &bevdisp, NULL, 0, renderResolution);
 				dl = bevdisp.first;
 			}
+			else if (cu->bevobj->curve_cache) {
+				dl = cu->bevobj->curve_cache->disp.first;
+			}
 			else {
 				BLI_assert(cu->bevobj->curve_cache != NULL);
-				dl = cu->bevobj->curve_cache->disp.first;
+				dl = NULL;
 			}
 
 			while (dl) {
