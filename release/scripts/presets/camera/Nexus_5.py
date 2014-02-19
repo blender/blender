df34df50f7b95980a587ab8ec2812de84d4c73d0
Sergey Sharybin <sergey.vfx@gmail.com>
2014-02-19 18:09:50 +0600

    Camera presets changes
    - Fixed typo in Canon C300 camera name
    - Added preset for Nexus5

---
+++ release/scripts/presets/camera/Nexus_5.py
@@ -0,0 +1,5 @@
+import bpy
+bpy.context.object.data.sensor_width = 4.5
+bpy.context.object.data.sensor_height = 3.37
+bpy.context.object.data.lens = 3.91
+bpy.context.object.data.sensor_fit = 'HORIZONTAL'
