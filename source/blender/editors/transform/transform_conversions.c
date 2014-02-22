3fe487217db6af766ca428975a0e70c35f31fa03
Bastien Montagne <montagne29@wanadoo.fr>
2014-02-22 11:12:44 +0100

    Fix T38407: Bone roll calculation flips local axes at wrong rotation angle.
    Basic idea is now to have the transformes bones keep "facing" the armature's Z axis, see comments in code for details.
    
    That might not be ideal, but at least we now have humanly predictable and consistent results.

--- source/blender/editors/transform/transform_conversions.c
+++ source/blender/editors/transform/transform_conversions.c
@@ -1182,6 +1182,7 @@ static void createTransArmatureVerts(TransInfo *t)
 				}
 			}
 			else {
+				const float Z[3] = {0.0f, 0.0f, 1.0f};
 				if (ebo->flag & BONE_TIPSEL) {
 					copy_v3_v3(td->iloc, ebo->tail);
 					copy_v3_v3(td->center, (t->around == V3D_LOCAL) ? ebo->head : td->iloc);
@@ -1196,8 +1197,9 @@ static void createTransArmatureVerts(TransInfo *t)
 					ED_armature_ebone_to_mat3(ebo, td->axismtx);
 
 					if ((ebo->flag & BONE_ROOTSEL) == 0) {
+						/* To fix roll, see comments in transform_generic.c::recalcData_objects() */
 						td->extra = ebo;
-						td->ival = ebo->roll;
+						td->ival = ebo->roll - ED_rollBoneToVector(ebo, Z, false);
 					}
 
 					td->ext = NULL;
@@ -1219,8 +1221,9 @@ static void createTransArmatureVerts(TransInfo *t)
 
 					ED_armature_ebone_to_mat3(ebo, td->axismtx);
 
-					td->extra = ebo; /* to fix roll */
-					td->ival = ebo->roll;
+					/* To fix roll, see comments in transform_generic.c::recalcData_objects() */
+					td->extra = ebo;
+					td->ival = ebo->roll - ED_rollBoneToVector(ebo, Z, false);
 
 					td->ext = NULL;
 					td->val = NULL;
