056d86ec83c5e66c636893b3ab19032dd4876d49
Jens Verwiebe <info@jensverwiebe.de>
2014-02-23 23:17:19 +0100

    OSX/scons: Fix language menu not showing up in prefs: The languages file was not installed to locale due it has no extension, using ditto now

--- build_files/scons/tools/Blender.py
+++ build_files/scons/tools/Blender.py
@@ -676,7 +676,7 @@ def AppIt(target=None, source=None, env=None):
         commands.getoutput(cmd)
         cmd = 'cp -R %s/release/datafiles/fonts %s/%s.app/Contents/MacOS/%s/datafiles/'%(bldroot,installdir,binary,VERSION)
         commands.getoutput(cmd)
-        cmd = 'cp -R %s/release/datafiles/locale/languages %s/%s.app/Contents/MacOS/%s/datafiles/locale/'%(bldroot, installdir, binary, VERSION)
+        cmd = 'ditto %s/release/datafiles/locale/languages %s/%s.app/Contents/MacOS/%s/datafiles/locale/'%(bldroot, installdir, binary, VERSION)
         commands.getoutput(cmd)
         mo_dir = os.path.join(builddir[:-4], "locale")
         for f in os.listdir(mo_dir):
