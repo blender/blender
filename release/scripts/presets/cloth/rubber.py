32c18276ef4e3b4035d03f459399f87517798c4e
Bastien Montagne <montagne29@wanadoo.fr>
2014-02-28 17:28:01 +0100

    Fix T38881: cloth preset rubber not working with "translated data names" i18n option
    Actually, was broken for any custom modifier name, since it was explicitly using 'Cloth' one. Changed to mimic other cloth pressets (wonder why this one was different!).

--- release/scripts/presets/cloth/rubber.py
+++ release/scripts/presets/cloth/rubber.py
@@ -1,7 +1,7 @@
 import bpy
-bpy.context.active_object.modifiers['Cloth'].settings.quality = 7
-bpy.context.active_object.modifiers['Cloth'].settings.mass = 3
-bpy.context.active_object.modifiers['Cloth'].settings.structural_stiffness = 15
-bpy.context.active_object.modifiers['Cloth'].settings.bending_stiffness = 25
-bpy.context.active_object.modifiers['Cloth'].settings.spring_damping = 25
-bpy.context.active_object.modifiers['Cloth'].settings.air_damping = 1
+bpy.context.cloth.settings.quality = 7
+bpy.context.cloth.settings.mass = 3
+bpy.context.cloth.settings.structural_stiffness = 15
+bpy.context.cloth.settings.bending_stiffness = 25
+bpy.context.cloth.settings.spring_damping = 25
+bpy.context.cloth.settings.air_damping = 1
