8e7ec3652e94f75d6ffe134dcac7fc4cc5c66f89
Lukas Tönne <lukas.toenne@gmail.com>
2014-03-05 17:42:48 +0100

    Fix T38969: RenderResult in RenderPart can be NULL if multithreaded renders are cancelled early.

--- source/blender/editors/render/render_internal.c
+++ source/blender/editors/render/render_internal.c
@@ -532,7 +532,10 @@ static void image_rect_update(void *rjv, RenderResult *rr, volatile rcti *renrec
 		*(rj->do_update) = TRUE;
 		return;
 	}
-
+	
+	if (rr == NULL)
+		return;
+	
 	/* update part of render */
 	render_image_update_pass_and_layer(rj, rr, &rj->iuser);
 	ibuf = BKE_image_acquire_ibuf(ima, &rj->iuser, &lock);
@@ -550,7 +553,7 @@ static void image_rect_update(void *rjv, RenderResult *rr, volatile rcti *renrec
 		{
 			image_buffer_rect_update(rj, rr, ibuf, &rj->iuser, renrect);
 		}
-
+		
 		/* make jobs timer to send notifier */
 		*(rj->do_update) = TRUE;
 	}
