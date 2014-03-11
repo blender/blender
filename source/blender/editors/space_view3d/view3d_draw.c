997b49a5f30ee0c1862d165530321811e5977d9d
Bastien Montagne <montagne29@wanadoo.fr>
2014-03-11 16:58:09 +0100

    Fix for own rB6919ffbf4b2f (we only consider unit scale when we do use a unit system!).
    Noted by Campbell, thanks!

--- source/blender/editors/space_view3d/view3d_draw.c
+++ source/blender/editors/space_view3d/view3d_draw.c
@@ -3263,7 +3263,10 @@ static void view3d_main_area_draw_objects(const bContext *C, ARegion *ar, const 
 	}
 
 	/* needs to be done always, gridview is adjusted in drawgrid() now, but only for ortho views. */
-	rv3d->gridview = v3d->grid / scene->unit.scale_length;
+	rv3d->gridview = v3d->grid;
+	if (scene->unit.system) {
+		rv3d->gridview /= scene->unit.scale_length;
+	}
 
 	if ((rv3d->view == RV3D_VIEW_USER) || (rv3d->persp != RV3D_ORTHO)) {
 		if ((v3d->flag2 & V3D_RENDER_OVERRIDE) == 0) {
