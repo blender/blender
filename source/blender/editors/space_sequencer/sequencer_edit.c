467596dd36f48c5779b094c2bfe74c322f19de3d
Sergey Sharybin <sergey.vfx@gmail.com>
2014-04-01 16:40:31 +0600

    Fix T38860: Video preview in VSE shows a distorted and jagged appearance at scale 1:1
    Issue was cauzed by the set zoom ratio optator which didn't really set pixel zoom to 1:1.

--- source/blender/editors/space_sequencer/sequencer_edit.c
+++ source/blender/editors/space_sequencer/sequencer_edit.c
@@ -2296,7 +2296,7 @@ static int sequencer_view_zoom_ratio_exec(bContext *C, wmOperator *op)
 	float facx = BLI_rcti_size_x(&v2d->mask) / winx;
 	float facy = BLI_rcti_size_y(&v2d->mask) / winy;
 
-	BLI_rctf_resize(&v2d->cur, (int)(winx * facx * ratio) + 1, (int)(winy * facy * ratio) + 1);
+	BLI_rctf_resize(&v2d->cur, floorf(winx * facx * ratio + 0.5f), floorf(winy * facy * ratio + 0.5f));
 
 	ED_region_tag_redraw(CTX_wm_region(C));
 
