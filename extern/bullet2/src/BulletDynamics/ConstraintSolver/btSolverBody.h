8a4210074c0d20af7aa1fe8b03839086f3f39078
Jens Verwiebe <info@jensverwiebe.de>
2014-04-17 00:25:15 +0200

    Bullet: better workaround for failing friction with clang 3.4, remove the obsolete compileflag magic

--- extern/bullet2/src/BulletDynamics/ConstraintSolver/btSolverBody.h
+++ extern/bullet2/src/BulletDynamics/ConstraintSolver/btSolverBody.h
@@ -37,8 +37,13 @@ struct	btSimdScalar
 	{
 
 	}
-
+/* workaround for an clang 3.4 issue, friction would fail with forced inlining */
+#if (defined(__APPLE__) && defined(__clang__) && (__clang_major__ == 5) && (__clang_minor__ == 1)) \
+	|| (defined(__clang__) && (__clang_major__ == 3) && (__clang_minor__ == 4))
+	inline __attribute__ ((noinline)) btSimdScalar(float	fl)
+#else
 	SIMD_FORCE_INLINE	btSimdScalar(float	fl)
+#endif
 	:m_vec128 (_mm_set1_ps(fl))
 	{
 	}
