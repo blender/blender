d0e80b50e92d6c4099f7c4839cf587199d01498c
Bastien Montagne <montagne29@wanadoo.fr>
2014-04-17 14:39:13 +0200

    Fix another Freestyle building glitch.

--- source/blender/freestyle/intern/view_map/SilhouetteGeomEngine.cpp
+++ source/blender/freestyle/intern/view_map/SilhouetteGeomEngine.cpp
@@ -298,11 +298,11 @@ iter:
 		}
 #if 0
 		if (G.debug & G_DEBUG_FREESTYLE) {
-			printf("SilhouetteGeomEngine::ImageToWorldParameter(): #iters = %d, dist = %e\n", i, dist);
+			cout << "SilhouetteGeomEngine::ImageToWorldParameter(): #iters = " << i << ", dist = " << dist << "\n";
 		}
 #endif
 		if (i == max_iters && G.debug & G_DEBUG_FREESTYLE) {
-			printf("SilhouetteGeomEngine::ImageToWorldParameter(): reached to max_iters (dist = %e)\n", dist);
+			cout << "SilhouetteGeomEngine::ImageToWorldParameter(): reached to max_iters (dist = " << dist << ")\n";
 		}
 	}
 
