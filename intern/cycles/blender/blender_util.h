0e2f6c7fc4db06b8fa9be758e294a3376c32bc5d
Bastien Montagne <montagne29@wanadoo.fr>
2014-04-21 23:41:39 +0200

    Fix T39833: Nurbs Crash
    Converting empty object to mesh can generate a 'Null' BL:Mesh, we have to check against it.

--- intern/cycles/blender/blender_util.h
+++ intern/cycles/blender/blender_util.h
@@ -43,10 +43,12 @@ void python_thread_state_restore(void **python_thread_state);
 static inline BL::Mesh object_to_mesh(BL::BlendData data, BL::Object object, BL::Scene scene, bool apply_modifiers, bool render, bool calc_undeformed)
 {
 	BL::Mesh me = data.meshes.new_from_object(scene, object, apply_modifiers, (render)? 2: 1, false, calc_undeformed);
-	if (me.use_auto_smooth()) {
-		me.calc_normals_split(me.auto_smooth_angle());
+	if ((bool)me) {
+		if (me.use_auto_smooth()) {
+			me.calc_normals_split(me.auto_smooth_angle());
+		}
+		me.calc_tessface();
 	}
-	me.calc_tessface();
 	return me;
 }
 
