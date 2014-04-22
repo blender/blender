830a8847bd567de44e31a188219c03b86ee0d2fb
Sergey Sharybin <sergey.vfx@gmail.com>
2014-04-22 13:00:35 +0600

    Switch buildbot to Python-3.4.0

--- build_files/buildbot/config/user-config-player-glibc211-i686.py
+++ build_files/buildbot/config/user-config-player-glibc211-i686.py
@@ -3,9 +3,9 @@ BF_INSTALLDIR = '../blender-install/linux-glibc211-i686'
 BF_NUMJOBS = 4
 
 # Python configuration
-BF_PYTHON_VERSION = '3.3'
+BF_PYTHON_VERSION = '3.4'
 BF_PYTHON_ABI_FLAGS = 'm'
-BF_PYTHON = '/opt/lib/python-3.3'
+BF_PYTHON = '/opt/lib/python-3.4'
 WITH_BF_PYTHON_INSTALL_NUMPY = True
 WITH_BF_PYTHON_INSTALL_REQUESTS = True
 
