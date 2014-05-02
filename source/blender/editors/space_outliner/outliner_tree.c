38b997991980558cda1a58e5da68c3c5fe58fddb
Tamito Kajiyama <rd6t-kjym@asahi-net.or.jp>
2014-05-02 12:33:24 +0900

    Minor code style fix.

--- source/blender/editors/space_outliner/outliner_tree.c
+++ source/blender/editors/space_outliner/outliner_tree.c
@@ -800,7 +800,8 @@ static void outliner_add_id_contents(SpaceOops *soops, TreeElement *te, TreeStor
 				outliner_add_element(soops, &te->subtree, linestyle, te, TSE_ANIM_DATA, 0);
 
 			for (a = 0; a < MAX_MTEX; a++) {
-				if (linestyle->mtex[a]) outliner_add_element(soops, &te->subtree, linestyle->mtex[a]->tex, te, 0, a);
+				if (linestyle->mtex[a])
+					outliner_add_element(soops, &te->subtree, linestyle->mtex[a]->tex, te, 0, a);
 			}
 			break;
 		}
