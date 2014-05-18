76f7a5bd6b75a1267ada200f108a9164ce069d10
Sebastian Ramacher <sramacher@debian.org>
2014-05-18 17:06:20 +0200

    Fix compilation error on kFreeBSD

--- intern/guardedalloc/intern/mallocn_intern.h
+++ intern/guardedalloc/intern/mallocn_intern.h
@@ -51,7 +51,7 @@
 
 #undef HAVE_MALLOC_STATS
 
-#if defined(__linux__)
+#if defined(__linux__) || (defined(__FreeBSD_kernel__) && !defined(__FreeBSD__))
 #  include <malloc.h>
 #  define HAVE_MALLOC_STATS
 #elif defined(__FreeBSD__)
