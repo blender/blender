85975be58e626356a1be108ff4a0f5f3b3b8c9d2
Sergey Sharybin <sergey.vfx@gmail.com>
2014-05-19 16:25:06 +0200

    Show warning when hiding a proxy bone
    It can't be undoe with Ctrl-Z, but Alt-H works just fine.
    So instead of breaking someone's workflow let's just add
    an info about how to bring bones back.

--- source/blender/editors/armature/pose_edit.c
+++ source/blender/editors/armature/pose_edit.c
@@ -1034,6 +1034,10 @@ static int pose_hide_exec(bContext *C, wmOperator *op)
 	Object *ob = BKE_object_pose_armature_get(CTX_data_active_object(C));
 	bArmature *arm = ob->data;
 
+	if (ob->proxy != NULL) {
+		BKE_report(op->reports, RPT_INFO, "Undo of hiding can only be done with Reveal Selected");
+	}
+
 	if (RNA_boolean_get(op->ptr, "unselected"))
 		bone_looper(ob, arm->bonebase.first, NULL, hide_unselected_pose_bone_cb);
 	else
