95d6c014780a980ed9f1552edd4196ceefbce9b6
Lukas Tönne <lukas.toenne@gmail.com>
2014-06-23 08:20:35 +0200

    Fix T40756 Select Linked by Particle System matches ignores selected psys.
    This is inconsistent with other comparable cases like materials, where
    the active slot is used to define matching objects.

--- source/blender/editors/object/object_select.c
+++ source/blender/editors/object/object_select.c
@@ -315,22 +315,20 @@ static bool object_select_all_by_dup_group(bContext *C, Object *ob)
 
 static bool object_select_all_by_particle(bContext *C, Object *ob)
 {
+	ParticleSystem *psys_act = psys_get_current(ob);
 	bool changed = false;
 
 	CTX_DATA_BEGIN (C, Base *, base, visible_bases)
 	{
 		if ((base->flag & SELECT) == 0) {
-			/* loop through other, then actives particles*/
+			/* loop through other particles*/
 			ParticleSystem *psys;
-			ParticleSystem *psys_act;
-
+			
 			for (psys = base->object->particlesystem.first; psys; psys = psys->next) {
-				for (psys_act = ob->particlesystem.first; psys_act; psys_act = psys_act->next) {
-					if (psys->part == psys_act->part) {
-						base->flag |= SELECT;
-						changed = true;
-						break;
-					}
+				if (psys->part == psys_act->part) {
+					base->flag |= SELECT;
+					changed = true;
+					break;
 				}
 
 				if (base->flag & SELECT) {
