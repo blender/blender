cba34986298b89df2c56c90a04fdeb0155258187
Sergey Sharybin <sergey.vfx@gmail.com>
2014-06-27 15:38:58 +0600

    Update Carve to latest upstream version
    This brings new copyright header which supports GPL2 and 3.
    
    It wasn't really an issue before because we had agreement with
    Tobias, but now it's all documented in sources.

--- extern/carve/include/carve/poly.hpp
+++ extern/carve/include/carve/poly.hpp
@@ -1,13 +1,13 @@
 // Begin License:
-// Copyright (C) 2006-2011 Tobias Sargeant (tobias.sargeant@gmail.com).
+// Copyright (C) 2006-2014 Tobias Sargeant (tobias.sargeant@gmail.com).
 // All rights reserved.
 //
 // This file is part of the Carve CSG Library (http://carve-csg.com/)
 //
-// This file may be used under the terms of the GNU General Public
-// License version 2.0 as published by the Free Software Foundation
-// and appearing in the file LICENSE.GPL2 included in the packaging of
-// this file.
+// This file may be used under the terms of either the GNU General
+// Public License version 2 or 3 (at your option) as published by the
+// Free Software Foundation and appearing in the files LICENSE.GPL2
+// and LICENSE.GPL3 included in the packaging of this file.
 //
 // This file is provided "AS IS" with NO WARRANTY OF ANY KIND,
 // INCLUDING THE WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR
