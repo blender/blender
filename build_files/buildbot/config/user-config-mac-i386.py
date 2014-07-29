c4dd294f407fcfb12e7c1451a3aaae41b0a0114a
Jens Verwiebe <info@jensverwiebe.de>
2014-07-29 20:08:41 +0200

    OSX/buildbot: after recent scons fix,  UTF lib linkage is handled dependent on config, so remove now obsolete lines

--- build_files/buildbot/config/user-config-mac-i386.py
+++ build_files/buildbot/config/user-config-mac-i386.py
@@ -4,7 +4,5 @@ MACOSX_ARCHITECTURE = 'i386' # valid archs: ppc, i386, ppc64, x86_64
 WITH_BF_CYCLES_CUDA_BINARIES = True
 
 WITH_BF_CYCLES_OSL = False # OSL never worked on OSX 32bit !
-WITH_BF_LLVM = False # no OSL -> no LLVM -> OpenCollada must have libUTF.a then !
 
-BF_OPENCOLLADA_LIB = 'OpenCOLLADASaxFrameworkLoader OpenCOLLADAFramework OpenCOLLADABaseUtils OpenCOLLADAStreamWriter MathMLSolver GeneratedSaxParser xml2 buffer ftoa UTF'
 
