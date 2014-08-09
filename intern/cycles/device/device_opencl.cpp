c020bd2e73de1d20c0d5996d35a5a21821ecc9be
Martijn Berger <martijn.berger@gmail.com>
2014-08-09 14:27:40 +0200

    Cycles OpenCL error to string removed in favour of the same function in clew.

--- intern/cycles/device/device_opencl.cpp
+++ intern/cycles/device/device_opencl.cpp
@@ -335,63 +335,10 @@ public:
 	bool device_initialized;
 	string platform_name;
 
-	const char *opencl_error_string(cl_int err)
-	{
-		switch (err) {
-			case CL_SUCCESS: return "Success!";
-			case CL_DEVICE_NOT_FOUND: return "Device not found.";
-			case CL_DEVICE_NOT_AVAILABLE: return "Device not available";
-			case CL_COMPILER_NOT_AVAILABLE: return "Compiler not available";
-			case CL_MEM_OBJECT_ALLOCATION_FAILURE: return "Memory object allocation failure";
-			case CL_OUT_OF_RESOURCES: return "Out of resources";
-			case CL_OUT_OF_HOST_MEMORY: return "Out of host memory";
-			case CL_PROFILING_INFO_NOT_AVAILABLE: return "Profiling information not available";
-			case CL_MEM_COPY_OVERLAP: return "Memory copy overlap";
-			case CL_IMAGE_FORMAT_MISMATCH: return "Image format mismatch";
-			case CL_IMAGE_FORMAT_NOT_SUPPORTED: return "Image format not supported";
-			case CL_BUILD_PROGRAM_FAILURE: return "Program build failure";
-			case CL_MAP_FAILURE: return "Map failure";
-			case CL_INVALID_VALUE: return "Invalid value";
-			case CL_INVALID_DEVICE_TYPE: return "Invalid device type";
-			case CL_INVALID_PLATFORM: return "Invalid platform";
-			case CL_INVALID_DEVICE: return "Invalid device";
-			case CL_INVALID_CONTEXT: return "Invalid context";
-			case CL_INVALID_QUEUE_PROPERTIES: return "Invalid queue properties";
-			case CL_INVALID_COMMAND_QUEUE: return "Invalid command queue";
-			case CL_INVALID_HOST_PTR: return "Invalid host pointer";
-			case CL_INVALID_MEM_OBJECT: return "Invalid memory object";
-			case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR: return "Invalid image format descriptor";
-			case CL_INVALID_IMAGE_SIZE: return "Invalid image size";
-			case CL_INVALID_SAMPLER: return "Invalid sampler";
-			case CL_INVALID_BINARY: return "Invalid binary";
-			case CL_INVALID_BUILD_OPTIONS: return "Invalid build options";
-			case CL_INVALID_PROGRAM: return "Invalid program";
-			case CL_INVALID_PROGRAM_EXECUTABLE: return "Invalid program executable";
-			case CL_INVALID_KERNEL_NAME: return "Invalid kernel name";
-			case CL_INVALID_KERNEL_DEFINITION: return "Invalid kernel definition";
-			case CL_INVALID_KERNEL: return "Invalid kernel";
-			case CL_INVALID_ARG_INDEX: return "Invalid argument index";
-			case CL_INVALID_ARG_VALUE: return "Invalid argument value";
-			case CL_INVALID_ARG_SIZE: return "Invalid argument size";
-			case CL_INVALID_KERNEL_ARGS: return "Invalid kernel arguments";
-			case CL_INVALID_WORK_DIMENSION: return "Invalid work dimension";
-			case CL_INVALID_WORK_GROUP_SIZE: return "Invalid work group size";
-			case CL_INVALID_WORK_ITEM_SIZE: return "Invalid work item size";
-			case CL_INVALID_GLOBAL_OFFSET: return "Invalid global offset";
-			case CL_INVALID_EVENT_WAIT_LIST: return "Invalid event wait list";
-			case CL_INVALID_EVENT: return "Invalid event";
-			case CL_INVALID_OPERATION: return "Invalid operation";
-			case CL_INVALID_GL_OBJECT: return "Invalid OpenGL object";
-			case CL_INVALID_BUFFER_SIZE: return "Invalid buffer size";
-			case CL_INVALID_MIP_LEVEL: return "Invalid mip-map level";
-			default: return "Unknown";
-		}
-	}
-
 	bool opencl_error(cl_int err)
 	{
 		if(err != CL_SUCCESS) {
-			string message = string_printf("OpenCL error (%d): %s", err, opencl_error_string(err));
+			string message = string_printf("OpenCL error (%d): %s", err, clewErrorString(err));
 			if(error_msg == "")
 				error_msg = message;
 			fprintf(stderr, "%s\n", message.c_str());
@@ -413,7 +360,7 @@ public:
 		cl_int err = stmt; \
 		\
 		if(err != CL_SUCCESS) { \
-			string message = string_printf("OpenCL error: %s in %s", opencl_error_string(err), #stmt); \
+			string message = string_printf("OpenCL error: %s in %s", clewErrorString(err), #stmt); \
 			if(error_msg == "") \
 				error_msg = message; \
 			fprintf(stderr, "%s\n", message.c_str()); \
@@ -423,7 +370,7 @@ public:
 	void opencl_assert_err(cl_int err, const char* where)
 	{
 		if(err != CL_SUCCESS) {
-			string message = string_printf("OpenCL error (%d): %s in %s", err, opencl_error_string(err), where);
+			string message = string_printf("OpenCL error (%d): %s in %s", err, clewErrorString(err), where);
 			if(error_msg == "")
 				error_msg = message;
 			fprintf(stderr, "%s\n", message.c_str());
