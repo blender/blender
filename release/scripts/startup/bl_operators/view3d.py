f11057f54a8d7d8810f4be5c1688a871a950768a
Sergey Sharybin <sergey.vfx@gmail.com>
2014-08-20 14:00:57 +0600

    Fix T41499: Can't use Deselect/Select All tool while in Texture Paint mode

--- release/scripts/startup/bl_operators/view3d.py
+++ release/scripts/startup/bl_operators/view3d.py
@@ -169,6 +169,13 @@ class VIEW3D_OT_select_or_deselect_all(Operator):
             default=False,
             )
 
+    @classmethod
+    def poll(cls, context):
+        active_object = context.active_object
+        if active_object:
+            return active_object.mode in {'EDIT', 'OBJECT', 'POSE'}
+        return True
+
     def invoke(self, context, event):
         x = event.mouse_region_x
         y = event.mouse_region_y
