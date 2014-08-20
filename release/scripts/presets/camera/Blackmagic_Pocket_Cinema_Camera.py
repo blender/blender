dcf9bc82d8617897e8b964b94cc4d6bd7044d8d5
Daniel Salazar <zanqdo@gmail.com>
2014-08-20 00:55:26 -0600

    Adding presets for Blackmagic Pocket Cinema Camera and Blackmagic Production Camera 4K

---
+++ release/scripts/presets/camera/Blackmagic_Pocket_Cinema_Camera.py
@@ -0,0 +1,4 @@
+import bpy
+bpy.context.camera.sensor_width = 12.48
+bpy.context.camera.sensor_height = 7.02
+bpy.context.camera.sensor_fit = 'HORIZONTAL'
---
+++ release/scripts/presets/camera/Blackmagic_Production_Camera_4K.py
@@ -0,0 +1,4 @@
+import bpy
+bpy.context.camera.sensor_width = 21.12
+bpy.context.camera.sensor_height = 11.88
+bpy.context.camera.sensor_fit = 'HORIZONTAL'
