a4c5570aba1374ad7c75b65b553810fabe57d931
Ines Almeida <britalmeida@gmail.com>
2014-08-23 20:24:19 +0200

    gameengine: fix T41272 - KX_Light.color returns wrong values

--- source/gameengine/Ketsji/KX_Light.cpp
+++ source/gameengine/Ketsji/KX_Light.cpp
@@ -242,7 +242,7 @@ int KX_LightObject::pyattr_set_distance(void *self_v, const KX_PYATTRIBUTE_DEF *
 PyObject *KX_LightObject::pyattr_get_color(void *self_v, const KX_PYATTRIBUTE_DEF *attrdef)
 {
 	KX_LightObject* self = static_cast<KX_LightObject*>(self_v);
-	return Py_BuildValue("[fff]", self->m_lightobj->m_color[0], self->m_lightobj->m_color[1], self->m_lightobj->m_color[1]);
+	return Py_BuildValue("[fff]", self->m_lightobj->m_color[0], self->m_lightobj->m_color[1], self->m_lightobj->m_color[2]);
 }
 
 int KX_LightObject::pyattr_set_color(void *self_v, const KX_PYATTRIBUTE_DEF *attrdef, PyObject *value)
