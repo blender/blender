885f862ce81139e7d31c7b0e20affc12d845c85d
Dalai Felinto <dfelinto@gmail.com>
2014-08-31 01:17:26 +0200

    Fix preview icons update script to work in OSX

--- release/datafiles/prvicons_update.py
+++ release/datafiles/prvicons_update.py
@@ -2,9 +2,16 @@
 
 # This script updates icons from the SVG file
 import os
+import sys
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__)) + os.sep
 
-cmd = 'inkscape "%sprvicons.svg" --without-gui --export-png="%sprvicons.png"' % (BASEDIR, BASEDIR)
-os.system(cmd)
+inkscape_path = 'inkscape'
 
+if sys.platform == 'darwin':
+    inkscape_app_path = '/Applications/Inkscape.app/Contents/Resources/script'
+    if os.path.exists(inkscape_app_path):
+        inkscape_path = inkscape_app_path
+
+cmd = inkscape_path + ' "%sprvicons.svg" --without-gui --export-png="%sprvicons.png"' % (BASEDIR, BASEDIR)
+os.system(cmd)
