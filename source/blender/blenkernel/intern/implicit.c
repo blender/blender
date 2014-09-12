3007fc6eb7612238d39962229bfac657e56cab02
Lukas Tönne <lukas.toenne@gmail.com>
2014-09-12 15:48:59 +0200

    Revert "Ignore velocity changes when the cloth solver does not converge."
    This reverts commit c52b8ae818844965d56714a71255408873275dc1.
    
    Sadly, at this point solver convergence is an exception rather than the
    rule... Individual hairs can "explode" easily and thus disable the whole
    simulation, which isn't helpful either.

--- source/blender/blenkernel/intern/implicit.c
+++ source/blender/blenkernel/intern/implicit.c
@@ -2430,10 +2430,8 @@ static bool simulate_implicit_euler(Implicit_Data *id, float dt)
 	// itend();
 	// printf("cg_filtered calc time: %f\n", (float)itval());
 
-	if (ok) {
-		// advance velocities
-		add_lfvector_lfvector(id->Vnew, id->V, id->dV, numverts);
-	}
+	// advance velocities
+	add_lfvector_lfvector(id->Vnew, id->V, id->dV, numverts);
 
 	del_lfvector(dFdXmV);
 	
