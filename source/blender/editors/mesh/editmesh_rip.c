7803cb09f3852ff14a5169f1a31bf1f4389bd296
Campbell Barton <ideasman42@gmail.com>
2014-09-28 13:41:26 +1000

    BMesh: replace BLI_array_grow_one -> BLI_array_append_ret

--- source/blender/editors/mesh/editmesh_rip.c
+++ source/blender/editors/mesh/editmesh_rip.c
@@ -309,8 +309,7 @@ static EdgeLoopPair *edbm_ripsel_looptag_helper(BMesh *bm)
 		uid_start = uid;
 		uid = uid_end + bm->totedge;
 
-		BLI_array_grow_one(eloop_pairs);
-		lp = &eloop_pairs[BLI_array_count(eloop_pairs) - 1];
+		lp = BLI_array_append_ret(eloop_pairs);
 		BM_edge_loop_pair(e_last, &lp->l_a, &lp->l_b); /* no need to check, we know this will be true */
 
 
@@ -323,8 +322,7 @@ static EdgeLoopPair *edbm_ripsel_looptag_helper(BMesh *bm)
 	}
 
 	/* null terminate */
-	BLI_array_grow_one(eloop_pairs);
-	lp = &eloop_pairs[BLI_array_count(eloop_pairs) - 1];
+	lp = BLI_array_append_ret(eloop_pairs);
 	lp->l_a = lp->l_b = NULL;
 
 	return eloop_pairs;
