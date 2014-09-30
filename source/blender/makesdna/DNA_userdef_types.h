c68c32a409526a66c99e6762a492c2d7a3caf9b4
Lukas Tönne <lukas.toenne@gmail.com>
2014-09-30 18:37:42 +0200

    Fix for duplicate member in UserDef from own commit (merge mistake).

--- source/blender/makesdna/DNA_userdef_types.h
+++ source/blender/makesdna/DNA_userdef_types.h
@@ -535,14 +535,12 @@ typedef struct UserDef {
 	float fcu_inactive_alpha;	/* opacity of inactive F-Curves in F-Curve Editor */
 	float pixelsize;			/* private, set by GHOST, to multiply DPI with */
 	int virtual_pixel;			/* virtual pixelsize mode */
-	int pad2;
 
 	short pie_interaction_type;     /* if keeping a pie menu spawn button pressed after this time, it turns into
 	                             * a drag/release pie menu */
 	short pie_initial_timeout;  /* direction in the pie menu will always be calculated from the initial position
 	                             * within this time limit */
 	int pie_animation_timeout;
-	int pad2;
 	short pie_menu_radius;        /* pie menu radius */
 	short pie_menu_threshold;     /* pie menu distance from center before a direction is set */
 
