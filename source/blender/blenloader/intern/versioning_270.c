6a8ff9ee4b90370eecfcfe60cb9080b74ab00402
Campbell Barton <ideasman42@gmail.com>
2014-10-02 17:10:24 +0200

    Fix error in last commit

--- source/blender/blenloader/intern/versioning_270.c
+++ source/blender/blenloader/intern/versioning_270.c
@@ -394,7 +394,7 @@ void blo_do_versions_270(FileData *fd, Library *UNUSED(lib), Main *main)
 	if (!DNA_struct_elem_find(fd->filesdna, "Image", "float", "gen_color")) {
 		Image *image;
 		for (image = main->image.first; image != NULL; image = image->id.next) {
-			image->gen_color[3] - 1.0f;
+			image->gen_color[3] = 1.0f;
 		}
 	}
 }
