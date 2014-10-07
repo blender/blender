e36cc29a69c57c9818fcb5018dead575ba7e4263
Bastien Montagne <montagne29@wanadoo.fr>
2014-10-07 23:39:56 +0200

    Fix T42137: Keymap editing is broken
    Own mistake in previous fix here, was too greedy in finding an existing kmi of UserPref add diff kmi, we only want
    to ignore exact same kmi's here.

--- source/blender/windowmanager/intern/wm_keymap.c
+++ source/blender/windowmanager/intern/wm_keymap.c
@@ -590,9 +590,8 @@ static void wm_keymap_patch(wmKeyMap *km, wmKeyMap *diff_km)
 		/* add item */
 		if (kmdi->add_item) {
 			/* Do not re-add an already existing keymap item! See T42088. */
+			/* We seek only for exact copy here! See T42137. */
 			kmi_add = wm_keymap_find_item_equals(km, kmdi->add_item);
-			if (!kmi_add)
-				kmi_add = wm_keymap_find_item_equals_result(km, kmdi->add_item);
 			/* only if nothing to remove or item to remove found */
 			if (!kmi_add && (!kmdi->remove_item || kmi_remove)) {
 				kmi_add = wm_keymap_item_copy(kmdi->add_item);
