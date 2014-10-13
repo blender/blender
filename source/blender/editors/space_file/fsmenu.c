92311505770edcac59440851a2b7a5d823b68690
Campbell Barton <ideasman42@gmail.com>
2014-10-13 15:51:31 +0200

    Cleanup: redundant cast

--- source/blender/editors/space_file/fsmenu.c
+++ source/blender/editors/space_file/fsmenu.c
@@ -446,7 +446,7 @@ void fsmenu_read_system(struct FSMenu *fsmenu, int read_bookmarks)
 
 				/* Exclude "all my files" as it makes no sense in blender fileselector */
 				/* Exclude "airdrop" if wlan not active as it would show "" ) */
-				if (!strstr((char *)line, "myDocuments.cannedSearch") && (strcmp((char *)line, ""))) {
+				if (!strstr(line, "myDocuments.cannedSearch") && (*line != '\0')) {
 					fsmenu_insert_entry(fsmenu, FS_CATEGORY_SYSTEM_BOOKMARKS, line, NULL);
 				}
 				
