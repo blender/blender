10b16ae2bc4f24c8f44334ede387fe7ec017b515
Antony Riakiotakis <kalast@gmail.com>
2014-10-17 14:11:13 +0200

    Fix issue reported by Sebastian Koenig on irc, Clone brush would paint trash with float images.
    Issue is uninitialized alpha in the clone brush + a clamping issue for
    float images. There's still some 'swimming' and brightness issue here
    but I have to do work for the weekly, will investigate further later.

--- source/blender/editors/sculpt_paint/paint_image_proj.c
+++ source/blender/editors/sculpt_paint/paint_image_proj.c
@@ -1507,6 +1507,7 @@ static ProjPixel *project_paint_uvpixel_init(
 						project_face_pixel(tf_other, ibuf_other, w, side, NULL, rgba);
 						premul_to_straight_v4(rgba);
 						linearrgb_to_srgb_uchar3(((ProjPixelClone *)projPixel)->clonepx.ch, rgba);
+						((ProjPixelClone *)projPixel)->clonepx.ch[3] =  rgba[3] * 255;
 					}
 					else { /* char to char */
 						project_face_pixel(tf_other, ibuf_other, w, side, ((ProjPixelClone *)projPixel)->clonepx.ch, NULL);
