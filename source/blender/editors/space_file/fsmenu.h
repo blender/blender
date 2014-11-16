ed52706d352334f15c1183131461a8112427ea22
Jens Verwiebe <info@jensverwiebe.de>
2014-11-16 17:12:49 +0100

    Just 2 comments cleaned

--- source/blender/editors/space_file/fsmenu.h
+++ source/blender/editors/space_file/fsmenu.h
@@ -48,7 +48,7 @@ typedef enum FSMenuInsert {
 	FS_INSERT_SORTED = (1 << 0),
 	FS_INSERT_SAVE   = (1 << 1),
 	FS_INSERT_FIRST  = (1 << 2),   /* moves the item to the front of the list when its not already there */
-	FS_APPEND_LAST   = (1 << 3)   /*just append to preseve delivered order */
+	FS_APPEND_LAST   = (1 << 3)   /* just append to preseve delivered order */
 } FSMenuInsert;
 
 struct FSMenu;
