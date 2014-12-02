b5399deac32733e4baef176bce51e2f12c9a1615
Sergey Sharybin <sergey.vfx@gmail.com>
2014-12-02 17:30:59 +0500

    Enable sm52 for linux buildbot
    Status of other builders:
    - Windows are not known for me, would ask Martijn to look into
    - OSX buildbot still needs a CUDA toolkit update

--- build_files/buildbot/config/user-config-cuda-glibc211-x86_64.py
+++ build_files/buildbot/config/user-config-cuda-glibc211-x86_64.py
@@ -2,4 +2,4 @@ BF_BUILDDIR = '../blender-build/linux-glibc211-x86_64'
 BF_INSTALLDIR = '../blender-install/linux-glibc211-x86_64'
 BF_NUMJOBS = 1
 
-BF_CYCLES_CUDA_BINARIES_ARCH = ['sm_20', 'sm_21', 'sm_30', 'sm_35', 'sm_50']
+BF_CYCLES_CUDA_BINARIES_ARCH = ['sm_20', 'sm_21', 'sm_30', 'sm_35', 'sm_50', 'sm_52']
