81ae71fcd3ea1406f0bca05471c17d984738ff4c
Jens Verwiebe <info@jensverwiebe.de>
2014-12-06 12:30:59 +0100

    Fix crash in RE_AcquiredResultGet32 on debug builds introduced yesterday

--- source/blender/editors/render/render_preview.c
+++ source/blender/editors/render/render_preview.c
@@ -561,8 +561,9 @@ static bool ed_preview_draw_rect(ScrArea *sa, int split, int first, rcti *rect, 
 				unsigned char *rect_byte = MEM_mallocN(rres.rectx * rres.recty * sizeof(int), "ed_preview_draw_rect");
 				float fx = rect->xmin + offx;
 				float fy = rect->ymin;
+				if (re)
+					RE_AcquiredResultGet32(re, &rres, (unsigned int *)rect_byte);
 				
-				RE_AcquiredResultGet32(re, &rres, (unsigned int *)rect_byte);
 				glaDrawPixelsSafe(fx, fy, rres.rectx, rres.recty, rres.rectx, GL_RGBA, GL_UNSIGNED_BYTE, rect_byte);
 				
 				MEM_freeN(rect_byte);
