c1d3532d2cbf85e1339026b7f77a4e8730124a50
Campbell Barton <ideasman42@gmail.com>
2014-12-14 18:24:13 +0100

    Fix for idproperty comparison
    comparing arrays had flipped logic

--- source/blender/blenkernel/intern/idprop.c
+++ source/blender/blenkernel/intern/idprop.c
@@ -810,7 +810,7 @@ bool IDP_EqualsProperties_ex(IDProperty *prop1, IDProperty *prop2, const bool is
 			if (prop1->len == prop2->len && prop1->subtype == prop2->subtype) {
 				return (memcmp(IDP_Array(prop1),
 				               IDP_Array(prop2),
-				               idp_size_table[(int)prop1->subtype] * (size_t)prop1->len));
+				               idp_size_table[(int)prop1->subtype] * (size_t)prop1->len) == 0);
 			}
 			return false;
 		case IDP_GROUP:
