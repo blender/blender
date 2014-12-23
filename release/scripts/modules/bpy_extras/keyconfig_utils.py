20aed660f5d852ea81ff42d1e998ebc74374900b
Campbell Barton <ideasman42@gmail.com>
2014-12-23 20:01:21 +1100

    Add missing keymap for paint curve

--- release/scripts/modules/bpy_extras/keyconfig_utils.py
+++ release/scripts/modules/bpy_extras/keyconfig_utils.py
@@ -59,6 +59,7 @@ KM_HIERARCHY = [
 
         ('Knife Tool Modal Map', 'EMPTY', 'WINDOW', []),
         ('Paint Stroke Modal', 'EMPTY', 'WINDOW', []),
+        ('Paint Curve', 'EMPTY', 'WINDOW', []),
 
         ('Object Non-modal', 'EMPTY', 'WINDOW', []),  # mode change
 
