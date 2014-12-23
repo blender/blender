273fa0a4efa72457c22fb8e355895c98354b572b
Sergey Sharybin <sergey.vfx@gmail.com>
2014-12-23 20:03:27 +0500

    Blender 2.73 release candidate commit
    This commit contains:
    
    - Subversion bump to 2.73
    - Release cycle is set to "rc"
    - Submodules are pointed to appropriate tag now
      (addons contrib just points to the latest contrib repo)
    - New shiny splash screen!

--- source/blender/blenkernel/BKE_blender.h
+++ source/blender/blenkernel/BKE_blender.h
@@ -41,8 +41,8 @@ extern "C" {
 /* these lines are grep'd, watch out for our not-so-awesome regex
  * and keep comment above the defines.
  * Use STRINGIFY() rather than defining with quotes */
-#define BLENDER_VERSION         272
-#define BLENDER_SUBVERSION      3
+#define BLENDER_VERSION         273
+#define BLENDER_SUBVERSION      0
 /* 262 was the last editmesh release but it has compatibility code for bmesh data */
 #define BLENDER_MINVERSION      270
 #define BLENDER_MINSUBVERSION   5
@@ -51,7 +51,7 @@ extern "C" {
 /* can be left blank, otherwise a,b,c... etc with no quotes */
 #define BLENDER_VERSION_CHAR   
 /* alpha/beta/rc/release, docs use this */
-#define BLENDER_VERSION_CYCLE   alpha
+#define BLENDER_VERSION_CYCLE   rc
 
 extern char versionstr[]; /* from blender.c */
 
