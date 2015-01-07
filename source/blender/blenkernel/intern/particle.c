19169de9f7bcc6438a91be2133fe21bea513953a
Lukas Tönne <lukas.toenne@gmail.com>
2015-01-07 16:15:03 +0100

    Recalculate particle pathcache stuff for all particles instead of trying to be smart.
    This breaks child interpolation otherwise because sometimes parent
    paths are not calculated and give bad clumping results.

--- source/blender/blenkernel/intern/particle.c
+++ source/blender/blenkernel/intern/particle.c
@@ -3000,7 +3000,7 @@ void psys_cache_paths(ParticleSimulationData *sim, float cfra)
 	}
 
 	/*---first main loop: create all actual particles' paths---*/
-	LOOP_SHOWN_PARTICLES {
+	LOOP_PARTICLES {
 		if (!psys->totchild) {
 			psys_get_texture(sim, pa, &ptex, PAMAP_LENGTH, 0.f);
 			pa_length = ptex.length * (1.0f - part->randlength * psys_frand(psys, psys->seed + p));
