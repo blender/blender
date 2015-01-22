9617446be24af967d64f9bbb611b1f84eba29b3a
Sergey Sharybin <sergey.vfx@gmail.com>
2015-01-22 17:04:01 +0500

    Cycles: Fix compilation error with some compilers
    Not sure why this was not visible previously, but the change is
    logical anyway.

--- intern/cycles/util/util_math.h
+++ intern/cycles/util/util_math.h
@@ -1457,7 +1457,7 @@ ccl_device void map_to_tube(float *r_u, float *r_v,
 	*r_v = (z + 1.0f) * 0.5f;
 	len = sqrtf(x * x + y * y);
 	if (len > 0.0f) {
-		*r_u = (1.0f - (atan2f(x / len, y / len) / (float)M_PI)) * 0.5f;
+		*r_u = (1.0f - (atan2f(x / len, y / len) / M_PI_F)) * 0.5f;
 	}
 	else {
 		*r_v = *r_u = 0.0f; /* To avoid un-initialized variables. */
