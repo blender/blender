4013ae63821a455ac25bd1d588321caac8bf8849
Lukas Tönne <lukas.toenne@gmail.com>
2015-01-26 14:57:01 +0100

    Fix T43409: Particle instancing on hair sometimes doesn't have a path cache available.

--- source/blender/blenkernel/intern/object_dupli.c
+++ source/blender/blenkernel/intern/object_dupli.c
@@ -969,7 +969,7 @@ static void make_duplis_particle_system(const DupliContext *ctx, ParticleSystem 
 			}
 
 			/* some hair paths might be non-existent so they can't be used for duplication */
-			if (hair &&
+			if (hair && psys->pathcache &&
 			    ((a < totpart && psys->pathcache[a]->segments < 0) ||
 			     (a >= totpart && psys->childcache[a - totpart]->segments < 0)))
 			{
