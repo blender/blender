8668006519e39c0fcaf2e3f2b5e508eb8478e2e4
Bastien Montagne <montagne29@wanadoo.fr>
2015-02-11 11:16:32 +0100

    Fix crasher in own UIList bookmarks commit.
    Immediate crash in merged asset-experiments branch, no idea why it did not show in master too?

--- source/blender/editors/space_file/space_file.c
+++ source/blender/editors/space_file/space_file.c
@@ -260,7 +260,8 @@ static void file_refresh(const bContext *C, ScrArea *sa)
 		sfile->layout->dirty = true;
 	}
 
-	if (BKE_area_find_region_type(sa, RGN_TYPE_TOOLS) == NULL) {
+	/* Might be called with NULL sa, see file_main_area_draw() below. */
+	if (sa && BKE_area_find_region_type(sa, RGN_TYPE_TOOLS) == NULL) {
 		/* Create TOOLS/TOOL_PROPS regions. */
 		file_tools_region(sa);
 
