eda2d563bc2a1333ba98e26e192167cadcc26026
Antony Riakiotakis <kalast@gmail.com>
2015-02-12 22:05:47 +0100

    Cleanup horrible artifact of Greek to English translation.

--- source/blender/makesrna/intern/rna_screen.c
+++ source/blender/makesrna/intern/rna_screen.c
@@ -415,7 +415,7 @@ static void rna_def_screen(BlenderRNA *brna)
 
 	prop = RNA_def_property(srna, "use_follow", PROP_BOOLEAN, PROP_NONE);
 	RNA_def_property_boolean_sdna(prop, NULL, "redraws_flag", TIME_FOLLOW);
-	RNA_def_property_ui_text(prop, "Follow", "Follow current frame in editors that update");
+	RNA_def_property_ui_text(prop, "Follow", "Follow current frame in editors");
 	RNA_def_property_update(prop, NC_SPACE | ND_SPACE_TIME, "rna_Screen_redraw_update");
 
 	prop = RNA_def_property(srna, "use_play_animation_editors", PROP_BOOLEAN, PROP_NONE);
