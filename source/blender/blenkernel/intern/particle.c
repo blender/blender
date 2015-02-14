db297e7ea314ee1e0f6db796c3dfc6683f64cd22
Lukas Tönne <lukas.toenne@gmail.com>
2015-02-14 16:40:57 +0100

    Revert "Fix T43471, based on patch submitted by @sean_loh."
    This reverts commit b2b54b0902045ec4f0c1a4cae3209be863878f5b.
    
    The patch breaks particle distribution even in simple cases, not worth
    doing this for keeping a hackish loophole open.

--- source/blender/blenkernel/intern/particle.c
+++ source/blender/blenkernel/intern/particle.c
@@ -3429,7 +3429,7 @@ static void get_cpa_texture(DerivedMesh *dm, ParticleSystem *psys, ParticleSetti
 	CLAMP_PARTICLE_TEXTURE_POS(PAMAP_ROUGH, ptex->rough1);
 	CLAMP_PARTICLE_TEXTURE_POS(PAMAP_DENS, ptex->exist);
 }
-bool psys_get_texture(ParticleSimulationData *sim, ParticleData *pa, ParticleTexture *ptex, int event, float cfra)
+void psys_get_texture(ParticleSimulationData *sim, ParticleData *pa, ParticleTexture *ptex, int event, float cfra)
 {
 	Object *ob = sim->ob;
 	Mesh *me = (Mesh *)ob->data;
@@ -3439,7 +3439,6 @@ bool psys_get_texture(ParticleSimulationData *sim, ParticleData *pa, ParticleTex
 	int m;
 	float value, rgba[4], co[3], texvec[3];
 	int setvars = 0;
-	bool has_texture = false;
 
 	/* initialize ptex */
 	ptex->ivel = ptex->life = ptex->exist = ptex->size = ptex->damp =
@@ -3515,8 +3514,6 @@ bool psys_get_texture(ParticleSimulationData *sim, ParticleData *pa, ParticleTex
 			SET_PARTICLE_TEXTURE(PAMAP_GRAVITY, ptex->gravity, mtex->gravityfac);
 			SET_PARTICLE_TEXTURE(PAMAP_DAMP, ptex->damp, mtex->dampfac);
 			SET_PARTICLE_TEXTURE(PAMAP_LENGTH, ptex->length, mtex->lengthfac);
-			
-			has_texture = true;
 		}
 	}
 
@@ -3529,8 +3526,6 @@ bool psys_get_texture(ParticleSimulationData *sim, ParticleData *pa, ParticleTex
 	CLAMP_PARTICLE_TEXTURE_POSNEG(PAMAP_GRAVITY, ptex->gravity);
 	CLAMP_PARTICLE_TEXTURE_POS(PAMAP_DAMP, ptex->damp);
 	CLAMP_PARTICLE_TEXTURE_POS(PAMAP_LENGTH, ptex->length);
-	
-	return has_texture;
 }
 /************************************************/
 /*			Particle State						*/
