38c30c41d7a2d4befa8a533911781364a3936ab4
Thomas Szepe <HG1_public@gmx.net>
2015-02-27 23:05:06 +0100

    BGE: Fix ShowDeprecationWarning macro.
    Reviewers: lordloki, brita_, sybren, moguri
    
    Reviewed By: moguri
    
    Differential Revision: https://developer.blender.org/D1144

--- source/gameengine/Expressions/PyObjectPlus.h
+++ source/gameengine/Expressions/PyObjectPlus.h
@@ -65,21 +65,21 @@ typedef struct {
 #define ShowDeprecationWarning(old_way, new_way)                              \
 {                                                                             \
 	static WarnLink wlink = {false, NULL};                                    \
-	if ((m_ignore_deprecation_warnings || wlink.warn_done)==0)                \
+	if ((PyObjectPlus::m_ignore_deprecation_warnings || wlink.warn_done)==0)  \
 	{                                                                         \
-		ShowDeprecationWarning_func(old_way, new_way);                        \
+		PyObjectPlus::ShowDeprecationWarning_func(old_way, new_way);          \
 		                                                                      \
-		WarnLink *wlink_last= GetDeprecationWarningLinkLast();                \
+		WarnLink *wlink_last= PyObjectPlus::GetDeprecationWarningLinkLast();  \
 		wlink.warn_done = true;                                               \
 		wlink.link = NULL;                                                    \
 		                                                                      \
 		if(wlink_last) {                                                      \
 			wlink_last->link= (void *)&(wlink);                               \
-			SetDeprecationWarningLinkLast(&(wlink));                          \
+			PyObjectPlus::SetDeprecationWarningLinkLast(&(wlink));            \
 		}                                                                     \
 		else {                                                                \
-			SetDeprecationWarningFirst(&(wlink));                             \
-			SetDeprecationWarningLinkLast(&(wlink));                          \
+			PyObjectPlus::SetDeprecationWarningFirst(&(wlink));               \
+			PyObjectPlus::SetDeprecationWarningLinkLast(&(wlink));            \
 		}                                                                     \
 	}                                                                         \
 }                                                                             \
