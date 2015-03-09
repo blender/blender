9ffc66b1c0e33b107193af38c71efb86c714dc5b
Campbell Barton <ideasman42@gmail.com>
2015-03-09 20:20:39 +1100

    Math Lib: add mid_v3_v3v3v3v3

--- source/blender/blenlib/intern/math_vector.c
+++ source/blender/blenlib/intern/math_vector.c
@@ -252,6 +252,13 @@ void mid_v3_v3v3v3(float v[3], const float v1[3], const float v2[3], const float
 	v[2] = (v1[2] + v2[2] + v3[2]) / 3.0f;
 }
 
+void mid_v3_v3v3v3v3(float v[3], const float v1[3], const float v2[3], const float v3[3], const float v4[3])
+{
+	v[0] = (v1[0] + v2[0] + v3[0] + v4[0]) / 4.0f;
+	v[1] = (v1[1] + v2[1] + v3[1] + v4[1]) / 4.0f;
+	v[2] = (v1[2] + v2[2] + v3[2] + v4[2]) / 4.0f;
+}
+
 /**
  * Specialized function for calculating normals.
  * fastpath for:
