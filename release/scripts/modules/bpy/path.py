5e92d1254e31dd3d5d43a24254bc6f32d5bd40a5
Campbell Barton <ideasman42@gmail.com>
2015-03-26 16:29:14 +1100

    Fix T44137: bpy.path.is_subdir fails
    `bpy.path.is_subdir("/abc/def/ghi","/abc/de")` incorrectly returned True

--- release/scripts/modules/bpy/path.py
+++ release/scripts/modules/bpy/path.py
@@ -116,7 +116,11 @@ def is_subdir(path, directory):
     from os.path import normpath, normcase
     path = normpath(normcase(path))
     directory = normpath(normcase(directory))
-    return path.startswith(directory)
+    if len(path) > len(directory):
+        if path.startswith(directory):
+            sep = ord(_os.sep) if isinstance(directory, bytes) else _os.sep
+            return (path[len(directory)] == sep)
+    return False
 
 
 def clean_name(name, replace="_"):
