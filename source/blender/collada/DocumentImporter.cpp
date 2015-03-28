44a4b90b4f303912390f5f846c1882ef37d975d1
Jens Verwiebe <info@jensverwiebe.de>
2015-03-28 18:17:54 +0100

    Fix collada compile after BKE cleanup

--- source/blender/collada/DocumentImporter.cpp
+++ source/blender/collada/DocumentImporter.cpp
@@ -771,9 +771,9 @@ MTex *DocumentImporter::create_texture(COLLADAFW::EffectCommon *ef, COLLADAFW::T
 		return NULL;
 	}
 	
-	ma->mtex[i] = add_mtex();
+	ma->mtex[i] = BKE_texture_mtex_add();
 	ma->mtex[i]->texco = TEXCO_UV;
-	ma->mtex[i]->tex = add_texture(G.main, "Texture");
+	ma->mtex[i]->tex = BKE_texture_add(G.main, "Texture");
 	ma->mtex[i]->tex->type = TEX_IMAGE;
 	ma->mtex[i]->tex->ima = uid_image_map[ima_uid];
 	
