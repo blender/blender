b6d878fa960c25e2c07f5950c02371e5d0e50425
Joshua Leung <aligorith@gmail.com>
2015-03-29 20:58:10 +1300

    Fix for incorrect description

--- source/blender/editors/interface/interface_eyedropper.c
+++ source/blender/editors/interface/interface_eyedropper.c
@@ -632,7 +632,7 @@ void UI_OT_eyedropper_id(wmOperatorType *ot)
 	/* identifiers */
 	ot->name = "Eyedropper Datablock";
 	ot->idname = "UI_OT_eyedropper_id";
-	ot->description = "Sample a color from the Blender Window to store in a property";
+	ot->description = "Sample a datablock from the Blender Window to store in a property";
 
 	/* api callbacks */
 	ot->invoke = datadropper_invoke;
