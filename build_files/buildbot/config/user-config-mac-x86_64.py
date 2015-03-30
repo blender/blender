23c0e46a003c491bbcafc8c42eace83fc95f3974
Sergey Sharybin <sergey.vfx@gmail.com>
2015-03-30 13:05:14 +0500

    Buildbot: Use Clang with OpenMP support for 64bit OSX builds
    This should make such areas as simulations and sculpting nicely threaded.
    
    32bit will be tried to be supported later.

--- build_files/buildbot/config/user-config-mac-x86_64.py
+++ build_files/buildbot/config/user-config-mac-x86_64.py
@@ -1,5 +1,6 @@
+CC = "../lib/darwin-9.x.universal/clang-omp-3.5/bin/clang"
+CXX = "../lib/darwin-9.x.universal/clang-omp-3.5/bin/clang++"
 
 MACOSX_ARCHITECTURE = 'x86_64' # valid archs: ppc, i386, ppc64, x86_64
 
 WITH_BF_CYCLES_CUDA_BINARIES = True
-
