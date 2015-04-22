147d0cdf985147d82c6b2f93c77e2052ab343e25
Sergey Sharybin <sergey.vfx@gmail.com>
2015-04-22 23:20:00 +0500

    Tracking: Show plane track outline if image failed to load

--- source/blender/editors/space_clip/clip_draw.c
+++ source/blender/editors/space_clip/clip_draw.c
@@ -1123,7 +1123,9 @@ static void draw_plane_marker_ex(SpaceClip *sc, Scene *scene, MovieTrackingPlane
 {
 	bool tiny = (sc->flag & SC_SHOW_TINY_MARKER) != 0;
 	bool is_selected_track = (plane_track->flag & SELECT) != 0;
-	bool draw_plane_quad = plane_track->image == NULL || plane_track->image_opacity == 0.0f;
+	const bool has_image = plane_track->image != NULL &&
+	                       BKE_image_has_ibuf(plane_track->image, NULL);
+	const bool draw_plane_quad = !has_image || plane_track->image_opacity == 0.0f;
 	float px[2];
 
 	if (draw_outline) {
