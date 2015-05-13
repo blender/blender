730e61b446879f71eae7267de42026069a53d66f
Sergey Sharybin <sergey.vfx@gmail.com>
2015-05-13 16:11:26 +0500

    Clip editor: Selecting curves in graph view always extends tracks selection
    Was actually an old TODO, hopefully solved now in a way so everyone is happy.

--- source/blender/editors/space_clip/clip_graph_ops.c
+++ source/blender/editors/space_clip/clip_graph_ops.c
@@ -246,10 +246,12 @@ static bool mouse_select_curve(bContext *C, float co[2], bool extend)
 		else if (act_track != userdata.track) {
 			SelectUserData selectdata = {SEL_DESELECT};
 			MovieTrackingObject *object = BKE_tracking_object_get_active(tracking);
-			ListBase *tracksbase = BKE_tracking_object_get_tracks(tracking, object);
 
 			tracking->act_track = userdata.track;
-			BKE_tracking_track_select(tracksbase, userdata.track, TRACK_AREA_ALL, true);
+			if ((sc->flag & SC_SHOW_GRAPH_SEL_ONLY) == 0) {
+				ListBase *tracksbase = BKE_tracking_object_get_tracks(tracking, object);
+				BKE_tracking_track_select(tracksbase, userdata.track, TRACK_AREA_ALL, false);
+			}
 
 			/* deselect all knots on newly selected curve */
 			clip_graph_tracking_iterate(sc,
