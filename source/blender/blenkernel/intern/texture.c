5fa0b8778bba85261526f818f392a95bc3035a1a
Porteries Tristan <republicthunderbolt9@gmail.com>
2015-05-18 18:10:03 +0200

    BGE: Set default envMap resolution to 512 pixels
    I propose to change  the environment map render default resolution (600) to 512 (Thanks brecht for the correction... I don't know why I said dpi) (to make it compatible with this script: http://en.wikibooks.org/wiki/GLSL_Pr...cting_Surfaces ) to make cubeMap reflections easier and avoid this error:
    
    invalid envmap size please render with CubeRes @ power of two
    unable to initialize image(0) in MAMaterial, image will not be available
    
    http://blenderartists.org/forum/showthread.php?371232-BGE-proposal-Set-default-envMap-resolution-to-512-dpi
    
    http://blenderartists.org/forum/showthread.php?370026-Problem-with-cube-map-textures (post 11)
    
    Author : youle (Ulysse MARTIN)
    
    Reviewers: hg1, agoose77, lordloki, moguri, panzergame, dfelinto
    
    Reviewed By: panzergame, dfelinto
    
    Subscribers: brecht
    
    Projects: #game_engine
    
    Differential Revision: https://developer.blender.org/D1306

--- source/blender/blenkernel/intern/texture.c
+++ source/blender/blenkernel/intern/texture.c
@@ -629,7 +629,7 @@ void BKE_texture_default(Tex *tex)
 		tex->env->stype = ENV_ANIM;
 		tex->env->clipsta = 0.1;
 		tex->env->clipend = 100;
-		tex->env->cuberes = 600;
+		tex->env->cuberes = 512;
 		tex->env->depth = 0;
 	}
 
@@ -1423,7 +1423,7 @@ EnvMap *BKE_texture_envmap_add(void)
 	env->stype = ENV_ANIM;
 	env->clipsta = 0.1;
 	env->clipend = 100.0;
-	env->cuberes = 600;
+	env->cuberes = 512;
 	env->viewscale = 0.5;
 	
 	return env;
