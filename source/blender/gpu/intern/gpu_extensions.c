150a4b23ba848041fb2ba606435207cee8b2bbc2
Antony Riakiotakis <kalast@gmail.com>
2015-05-19 16:25:42 +0200

    Fix part of T44768 Yet another Intel driver failing on viewport Ambient occlusion
    (Maybe we should add drivers that DO work instead, but in the future
    those will be much more since Intel claims to have fixed the issue)

--- source/blender/gpu/intern/gpu_extensions.c
+++ source/blender/gpu/intern/gpu_extensions.c
@@ -264,7 +264,8 @@ void gpu_extensions_init(void)
 		GG.dfdyfactors[1] = -1.0;
 	}
 	else if (GG.device == GPU_DEVICE_INTEL && GG.os == GPU_OS_WIN  &&
-	        (strstr(version, "4.0.0 - Build 9.18.10.3165") ||
+	        (strstr(version, "4.0.0 - Build 9.18.10.3186") ||
+	         strstr(version, "4.0.0 - Build 9.18.10.3165") ||
 	         strstr(version, "3.1.0 - Build 9.17.10.3347") ||
 	         strstr(version, "3.1.0 - Build 9.17.10.4101")))
 	{
