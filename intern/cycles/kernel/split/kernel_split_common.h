62f2d9b56655ca840b4f883adcc1d331d911444e
Sergey Sharybin <sergey.vfx@gmail.com>
2015-05-25 14:06:46 +0500

    Cycles: Fix compilation error of split kernel
    The code was failing to compile on runtime because of some path differences,
    and it seems we don't need to specify full path to the file which originally
    seemed to be needed to make include directives expansion working correct.

--- intern/cycles/kernel/split/kernel_split_common.h
+++ intern/cycles/kernel/split/kernel_split_common.h
@@ -17,46 +17,46 @@
 #ifndef  _KERNEL_SPLIT_H_
 #define  _KERNEL_SPLIT_H_
 
-#include "../kernel_compat_opencl.h"
-#include "../kernel_math.h"
-#include "../kernel_types.h"
-#include "../kernel_globals.h"
+#include "kernel_compat_opencl.h"
+#include "kernel_math.h"
+#include "kernel_types.h"
+#include "kernel_globals.h"
 
-#include "../util_atomic.h"
+#include "util_atomic.h"
 
-#include "../kernel_random.h"
-#include "../kernel_projection.h"
-#include "../kernel_montecarlo.h"
-#include "../kernel_differential.h"
-#include "../kernel_camera.h"
+#include "kernel_random.h"
+#include "kernel_projection.h"
+#include "kernel_montecarlo.h"
+#include "kernel_differential.h"
+#include "kernel_camera.h"
 
-#include "../geom/geom.h"
+#include "geom/geom.h"
 
-#include "../kernel_accumulate.h"
-#include "../kernel_shader.h"
-#include "../kernel_light.h"
-#include "../kernel_passes.h"
+#include "kernel_accumulate.h"
+#include "kernel_shader.h"
+#include "kernel_light.h"
+#include "kernel_passes.h"
 
 #ifdef __SUBSURFACE__
-#include "../kernel_subsurface.h"
+#include "kernel_subsurface.h"
 #endif
 
 #ifdef __VOLUME__
-#include "../kernel_volume.h"
+#include "kernel_volume.h"
 #endif
 
-#include "../kernel_path_state.h"
-#include "../kernel_shadow.h"
-#include "../kernel_emission.h"
-#include "../kernel_path_common.h"
-#include "../kernel_path_surface.h"
-#include "../kernel_path_volume.h"
+#include "kernel_path_state.h"
+#include "kernel_shadow.h"
+#include "kernel_emission.h"
+#include "kernel_path_common.h"
+#include "kernel_path_surface.h"
+#include "kernel_path_volume.h"
 
 #ifdef __KERNEL_DEBUG__
-#include "../kernel_debug.h"
+#include "kernel_debug.h"
 #endif
 
-#include "../kernel_queues.h"
-#include "../kernel_work_stealing.h"
+#include "kernel_queues.h"
+#include "kernel_work_stealing.h"
 
 #endif
