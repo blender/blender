bcfe38aa037e2fcf1c41a3a85895c1482e21e0aa
Campbell Barton <ideasman42@gmail.com>
2015-06-08 22:29:52 +1000

    Fix T44964: Bisect tool /w nonuniform scale

--- source/blender/editors/mesh/editmesh_bisect.c
+++ source/blender/editors/mesh/editmesh_bisect.c
@@ -264,7 +264,7 @@ static int mesh_bisect_exec(bContext *C, wmOperator *op)
 
 	invert_m4_m4(imat, obedit->obmat);
 	mul_m4_v3(imat, plane_co);
-	mul_mat3_m4_v3(imat, plane_no);
+	mul_transposed_mat3_m4_v3(obedit->obmat, plane_no);
 
 	EDBM_op_init(em, &bmop, op,
 	             "bisect_plane geom=%hvef plane_co=%v plane_no=%v dist=%f clear_inner=%b clear_outer=%b",
