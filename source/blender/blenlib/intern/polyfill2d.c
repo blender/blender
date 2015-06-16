b986f955e96578e691e3561142b571609957ade0
Campbell Barton <ideasman42@gmail.com>
2015-06-16 21:13:22 +1000

    Disable assert for polyfill
    Would fail on some zero-area ngons.

--- source/blender/blenlib/intern/polyfill2d.c
+++ source/blender/blenlib/intern/polyfill2d.c
@@ -61,6 +61,9 @@
 #  define USE_KDTREE
 #endif
 
+/* disable in production, it can fail on near zero area ngons */
+// #define USE_STRICT_ASSERT
+
 // #define DEBUG_TIME
 #ifdef DEBUG_TIME
 #  include "PIL_time_utildefines.h"
@@ -797,6 +800,7 @@ static void polyfill_prepare(
 	}
 	else {
 		/* chech we're passing in correcty args */
+#ifdef USE_STRICT_ASSERT
 #ifndef NDEBUG
 		if (coords_sign == 1) {
 			BLI_assert(cross_poly_v2(coords, coords_tot) >= 0.0f);
@@ -804,6 +808,7 @@ static void polyfill_prepare(
 		else {
 			BLI_assert(cross_poly_v2(coords, coords_tot) <= 0.0f);
 		}
+#endif
 #endif
 	}
 
