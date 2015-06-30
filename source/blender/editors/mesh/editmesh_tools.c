6de17c60bf65d8c0479dd5e1ee75071ab169624c
Campbell Barton <ideasman42@gmail.com>
2015-06-30 22:50:32 +1000

    Select flush deleting edgeloop in edge mode too

--- source/blender/editors/mesh/editmesh_tools.c
+++ source/blender/editors/mesh/editmesh_tools.c
@@ -4271,9 +4271,7 @@ static int edbm_delete_edgeloop_exec(bContext *C, wmOperator *op)
 
 	BM_mesh_elem_hflag_enable_test(em->bm, BM_FACE, BM_ELEM_SELECT, true, false, BM_ELEM_TAG);
 
-	if (em->selectmode & SCE_SELECT_VERTEX) {
-		EDBM_selectmode_flush_ex(em, SCE_SELECT_VERTEX);
-	}
+	EDBM_selectmode_flush_ex(em, SCE_SELECT_VERTEX);
 
 	EDBM_update_generic(em, true, true);
 
