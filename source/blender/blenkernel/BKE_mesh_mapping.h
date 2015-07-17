19614f4395b3a5404c96fdec02e0d5e89bfe9e32
Campbell Barton <ideasman42@gmail.com>
2015-07-17 20:17:33 +1000

    Add macro BKE_MESH_TESSTRI_VINDEX_ORDER
    gives the index of a vertex in a looptri

--- source/blender/blenkernel/BKE_mesh_mapping.h
+++ source/blender/blenkernel/BKE_mesh_mapping.h
@@ -199,4 +199,13 @@ int *BKE_mesh_calc_smoothgroups(
      (_mf->v4 && _mf->v4 == _v) ? 3 : -1)                                   \
     )
 
+/* use on looptri vertex values */
+#define BKE_MESH_TESSTRI_VINDEX_ORDER(_tri, _v)  (                          \
+    (CHECK_TYPE_ANY(_tri, unsigned int *, int *, const unsigned int *, const int *), \
+     CHECK_TYPE_ANY(_v, unsigned int, int)),                                \
+    (((_tri)[0] == _v) ? 0 :                                                \
+     ((_tri)[1] == _v) ? 1 :                                                \
+     ((_tri)[2] == _v) ? 2 : -1)                                            \
+    )
+
 #endif  /* __BKE_MESH_MAPPING_H__ */
