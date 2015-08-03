1d549530d882bd9b4d7466e59c4cddd8b4cb4586
Campbell Barton <ideasman42@gmail.com>
2015-08-03 23:06:03 +1000

    Fix own mistake in curve refactor

--- source/blender/editors/curve/editcurve_select.c
+++ source/blender/editors/curve/editcurve_select.c
@@ -148,7 +148,7 @@ int ED_curve_nurb_select_count(Curve *cu, Nurb *nu)
 
 		for (i = nu->pntsu, bezt = nu->bezt; i--; bezt++) {
 			if (BEZT_ISSEL_ANY_HIDDENHANDLES(cu, bezt)) {
-				return sel++;
+				sel++;
 			}
 		}
 	}
@@ -158,7 +158,7 @@ int ED_curve_nurb_select_count(Curve *cu, Nurb *nu)
 
 		for (i = nu->pntsu * nu->pntsv, bp = nu->bp; i--; bp++) {
 			if (bp->f1 & SELECT) {
-				return sel++;
+				sel++;
 			}
 		}
 	}
