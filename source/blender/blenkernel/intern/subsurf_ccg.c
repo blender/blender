41ae6d7829100bd86acdc8293ca61414f97de04b
Sergey Sharybin <sergey.vfx@gmail.com>
2015-08-25 15:22:26 +0200

    OpenSubdiv: Support multiple materials for GLSL view

--- source/blender/blenkernel/intern/subsurf_ccg.c
+++ source/blender/blenkernel/intern/subsurf_ccg.c
@@ -2659,8 +2659,7 @@ static void ccgDM_drawFacesSolid(DerivedMesh *dm, float (*partial_redraw_planes)
 			}
 			if (new_draw_smooth != draw_smooth || new_matnr != mat_nr) {
 				if (num_draw_patches != 0) {
-					bool do_draw = setMaterial == NULL ||
-					               setMaterial(mat_nr, NULL);
+					bool do_draw = setMaterial(mat_nr, NULL);
 					if (do_draw) {
 						glShadeModel(draw_smooth ? GL_SMOOTH : GL_FLAT);
 						ccgSubSurf_drawGLMesh(ss,
@@ -2727,24 +2726,66 @@ static void ccgDM_drawMappedFacesGLSL(DerivedMesh *dm,
 
 #ifdef WITH_OPENSUBDIV
 	if (ccgdm->useGpuBackend) {
-		int new_matnr;
-		bool draw_smooth;
+		CCGSubSurf *ss = ccgdm->ss;
+		const DMFlagMat *faceFlags = ccgdm->faceFlags;
+		const int level = ccgSubSurf_getSubdivisionLevels(ss);
+		const int face_side = 1 << level;
+		const int grid_side = 1 << (level - 1);
+		const int face_patches = face_side * face_side;
+		const int grid_patches = grid_side * grid_side;
+		const int num_base_faces = ccgSubSurf_getNumGLMeshBaseFaces(ss);
+		int i, current_patch = 0;
+		int mat_nr = -1;
+		bool draw_smooth = false;
+		int start_draw_patch = -1, num_draw_patches = 0;
 		GPU_draw_update_fvar_offset(dm);
 		if (UNLIKELY(ccgSubSurf_prepareGLMesh(ss, false) == false)) {
 			return;
 		}
-		/* TODO(sergey): Single matierial currently. */
-		if (faceFlags) {
-			draw_smooth = (faceFlags[0].flag & ME_SMOOTH);
-			new_matnr = (faceFlags[0].mat_nr + 1);
+		for (i = 0; i < num_base_faces; ++i) {
+			const int num_face_verts = ccgSubSurf_getNumGLMeshBaseFaceVerts(ss, i);
+			const int num_patches = (num_face_verts == 4) ? face_patches
+			                                              : num_face_verts * grid_patches;
+			int new_matnr;
+			bool new_draw_smooth;
+			if (faceFlags) {
+				new_draw_smooth = (faceFlags[i].flag & ME_SMOOTH);
+				new_matnr = (faceFlags[i].mat_nr + 1);
+			}
+			else {
+				new_draw_smooth = true;
+				new_matnr = 1;
+			}
+			if (new_draw_smooth != draw_smooth || new_matnr != mat_nr) {
+				if (num_draw_patches != 0) {
+					bool do_draw = setMaterial(mat_nr, &gattribs);
+					if (do_draw) {
+						glShadeModel(draw_smooth ? GL_SMOOTH : GL_FLAT);
+						ccgSubSurf_drawGLMesh(ss,
+						                      true,
+						                      start_draw_patch,
+						                      num_draw_patches);
+					}
+				}
+				start_draw_patch = current_patch;
+				num_draw_patches = num_patches;
+				mat_nr = new_matnr;
+				draw_smooth = new_draw_smooth;
+			}
+			else {
+				num_draw_patches += num_patches;
+			}
+			current_patch += num_patches;
 		}
-		else {
-			draw_smooth = true;
-			new_matnr = 1;
-		}
-		glShadeModel(draw_smooth ? GL_SMOOTH : GL_FLAT);
-		if (setMaterial(new_matnr, &gattribs)) {
-			ccgSubSurf_drawGLMesh(ss, true, -1, -1);
+		if (num_draw_patches != 0) {
+			bool do_draw = setMaterial(mat_nr, &gattribs);
+			if (do_draw) {
+				glShadeModel(draw_smooth ? GL_SMOOTH : GL_FLAT);
+				ccgSubSurf_drawGLMesh(ss,
+				                      true,
+				                      start_draw_patch,
+				                      num_draw_patches);
+			}
 		}
 		return;
 	}
