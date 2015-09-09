6ddf77f1c083f0456027a009d6d94cacd2e08901
Campbell Barton <ideasman42@gmail.com>
2015-09-09 19:42:49 +1000

    Fix crash in preview render w/ too small border

--- source/blender/render/intern/source/pipeline.c
+++ source/blender/render/intern/source/pipeline.c
@@ -1270,8 +1270,11 @@ static void main_render_result_new(Render *re)
 
 	BLI_rw_mutex_unlock(&re->resultmutex);
 
-	if (re->result->do_exr_tile)
-		render_result_exr_file_begin(re);
+	if (re->result) {
+		if (re->result->do_exr_tile) {
+			render_result_exr_file_begin(re);
+		}
+	}
 }
 
 static void threaded_tile_processor(Render *re)
