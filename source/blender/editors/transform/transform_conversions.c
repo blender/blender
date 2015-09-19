cfc109eb928951b402eb759350a7588290e614b9
Bastien Montagne <montagne29@wanadoo.fr>
2015-09-19 11:08:21 +0200

    Fix T46161: Rotate around selection changes bezier curve handle type.
    Issue is, when 'Rotate Aroud Selection' is set, in Edit mode we do a fake transform operation
    to get center point around which to rotate. For curves, most transform operations involve
    a check of handle types. For now, added 'TFM_DUMMY' as an exception here.
    
    Think it would be best to actually undo those changes in case of cancelled operation,
    but this is much more involved, while this fix is safe enough to be included in final 2.76.

--- source/blender/editors/transform/transform_conversions.c
+++ source/blender/editors/transform/transform_conversions.c
@@ -1661,7 +1661,7 @@ static void createTransCurveVerts(TransInfo *t)
 
 			/* TODO - in the case of tilt and radius we can also avoid allocating the initTransDataCurveHandles
 			 * but for now just don't change handle types */
-			if (ELEM(t->mode, TFM_CURVE_SHRINKFATTEN, TFM_TILT) == 0) {
+			if (ELEM(t->mode, TFM_CURVE_SHRINKFATTEN, TFM_TILT, TFM_DUMMY) == 0) {
 				/* sets the handles based on their selection, do this after the data is copied to the TransData */
 				BKE_nurb_handles_test(nu, !hide_handles);
 			}
