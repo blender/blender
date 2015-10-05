776a98f3e8813b0ad94570423410dac40c2d35e1
Sergey Sharybin <sergey.vfx@gmail.com>
2015-10-05 16:16:58 +0500

    Fix T46354: Curve Modifier does not update (new Dependency graph)
    Result of curve modifier depends on transform of the object which should
    be reflected by the depsgraph relations.

--- source/blender/modifiers/intern/MOD_curve.c
+++ source/blender/modifiers/intern/MOD_curve.c
@@ -112,7 +112,7 @@ static void updateDepgraph(ModifierData *md, DagForest *forest,
 static void updateDepsgraph(ModifierData *md,
                             struct Main *UNUSED(bmain),
                             struct Scene *scene,
-                            Object *UNUSED(ob),
+                            Object *object,
                             struct DepsNodeHandle *node)
 {
 	CurveModifierData *cmd = (CurveModifierData *)md;
@@ -126,6 +126,8 @@ static void updateDepsgraph(ModifierData *md,
 		DEG_add_object_relation(node, cmd->object, DEG_OB_COMP_GEOMETRY, "Curve Modifier");
 		DEG_add_special_eval_flag(scene->depsgraph, &cmd->object->id, DAG_EVAL_NEED_CURVE_PATH);
 	}
+
+	DEG_add_object_relation(node, object, DEG_OB_COMP_TRANSFORM, "Curve Modifier");
 }
 
 static void deformVerts(ModifierData *md, Object *ob,
