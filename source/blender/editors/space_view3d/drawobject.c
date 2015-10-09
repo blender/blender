48c50bea72de544938bdb92dd5ce5b0b2b6bc795
Bastien Montagne <montagne29@wanadoo.fr>
2015-10-09 10:51:38 +0200

    Fix T46420: Segfault when instancing smoke domain.
    Looks like instancing of smoke sim is not supported at all
    (was fake-working in 3DView in 2.74, but not rendered).
    
    But it should not crash - code was adding temp 'fromdupli' base to the delayed
    drawing list...
    
    Nice to backport this to 2.76 I think.

--- source/blender/editors/space_view3d/drawobject.c
+++ source/blender/editors/space_view3d/drawobject.c
@@ -7653,7 +7653,9 @@ void draw_object(Scene *scene, ARegion *ar, View3D *v3d, Base *base, const short
 		}
 	}
 
-	if ((md = modifiers_findByType(ob, eModifierType_Smoke)) && (modifier_isEnabled(scene, md, eModifierMode_Realtime))) {
+	if (((base->flag & OB_FROMDUPLI) == 0) &&
+	    (md = modifiers_findByType(ob, eModifierType_Smoke)) &&
+	    (modifier_isEnabled(scene, md, eModifierMode_Realtime))) {
 		smd = (SmokeModifierData *)md;
 
 		if (smd->domain) {
