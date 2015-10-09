a3cf9c54d8a000c757a0ee33a399f7a8e2a31120
Campbell Barton <ideasman42@gmail.com>
2015-10-09 23:01:22 +1100

    Use PyThreadState_GetDict, avoid Python internals
    Works around problems caused by exposing Py internals (Py_BUILD_CORE).
    
        Build error with GCC, Py3.6 & OpenMP
        Linking error on MSVC

--- source/blender/python/generic/py_capi_utils.c
+++ source/blender/python/generic/py_capi_utils.c
@@ -666,7 +666,8 @@ void PyC_SetHomePath(const char *py_path_bundle)
 
 bool PyC_IsInterpreterActive(void)
 {
-	return (((PyThreadState *)_Py_atomic_load_relaxed(&_PyThreadState_Current)) != NULL);
+	/* instead of PyThreadState_Get, which calls Py_FatalError */
+	return (PyThreadState_GetDict() != NULL);
 }
 
 /* Would be nice if python had this built in
