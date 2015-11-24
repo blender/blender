14221521fb03224cf11fedf49555e98c9ef67426
Bastien Montagne <montagne29@wanadoo.fr>
2015-11-24 15:36:49 +0100

    Fix previous own fix - second message was actually OK, first one had bad comma placement...
    Thanks to psy-fi for the head-up.

--- intern/ghost/intern/GHOST_ContextWGL.cpp
+++ intern/ghost/intern/GHOST_ContextWGL.cpp
@@ -926,14 +926,16 @@ GHOST_TSuccess GHOST_ContextWGL::initializeDrawingContext()
 		                   "* A missing or faulty graphics driver installation.\n"
 		                   "  Blender needs a graphics card driver to work correctly.\n"
 		                   "* Accessing Blender through a remote connection.\n"
-		                   "* Using Blender through a virtual machine.\n\n",
-		                   "The program will now close Blender - Can't detect 3D hardware accelerated Driver!",
+		                   "* Using Blender through a virtual machine.\n\n"
+		                   "The program will now close.",
+		           "Blender - Can't detect 3D hardware accelerated Driver!",
 		           MB_OK | MB_ICONERROR);
 		exit(0);
 	}
 	else if (version[0] < '2' || (version[0] == '2' && version[2] < '1')) {
-		MessageBox(m_hWnd, "Blender requires a graphics driver with OpenGL 2.1 support\n",
-		                   "The program will now close Blender - Unsupported Graphics Driver!",
+		MessageBox(m_hWnd, "Blender requires a graphics driver with OpenGL 2.1 support.\n\n"
+		                   "The program will now close.",
+		           "Blender - Unsupported Graphics Driver!",
 		           MB_OK | MB_ICONERROR);
 		exit(0);
 	}
