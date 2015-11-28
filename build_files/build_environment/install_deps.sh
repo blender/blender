37801588d4b9d919bab40f1fa22d631d31eb2105
Bastien Montagne <montagne29@wanadoo.fr>
2015-11-28 19:49:03 +0100

    install_deps.sh: always give py version arg to cmake/scons (same as we already do with llvm).
    This should avoid 'casual builders' to have issues when we switch py versions.

--- build_files/build_environment/install_deps.sh
+++ build_files/build_environment/install_deps.sh
@@ -3664,6 +3664,9 @@ print_info() {
 
   _buildargs=""
 
+  _1="-D PYTHON_VERSION=$PYTHON_VERSION_MIN"
+  PRINT "  $_1"
+  _buildargs="$_buildargs $_1"
   if [ -d $INST/python-$PYTHON_VERSION_MIN ]; then
     _1="-D PYTHON_ROOT_DIR=$INST/python-$PYTHON_VERSION_MIN"
     PRINT "  $_1"
@@ -3758,6 +3761,7 @@ print_info() {
   PRINT ""
   PRINT "If you're using SCons add this to your user-config:"
 
+  PRINT "BF_PYTHON_VERSION = $PYTHON_VERSION_MIN"
   if [ -d $INST/python-$PYTHON_VERSION_MIN ]; then
     PRINT "BF_PYTHON = '$INST/python-$PYTHON_VERSION_MIN'"
     PRINT "BF_PYTHON_ABI_FLAGS = 'm'"
