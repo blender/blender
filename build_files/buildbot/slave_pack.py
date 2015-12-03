95e2ee6f501eb9a0454057413046995e4a9d06e1
Sergey Sharybin <sergey.vfx@gmail.com>
2015-12-03 15:15:38 +0500

    Buildbot: Always expect buildbot_upload.zip to be in install/ folder
    This we we don't have difference between builders on different platforms.

--- build_files/buildbot/slave_pack.py
+++ build_files/buildbot/slave_pack.py
@@ -160,7 +160,7 @@ else:
         os.rename(result_file, "{}.zip".format(builderified_name))
         # create zip file
         try:
-            upload_zip = "../buildbot_upload.zip"
+            upload_zip = "../install/buildbot_upload.zip"
             if os.path.exists(upload_zip):
                 os.remove(upload_zip)
             z = zipfile.ZipFile(upload_zip, "w", compression=zipfile.ZIP_STORED)
