9e0757a3671d15ec8b50aad404d3b5c38e549577
Campbell Barton <ideasman42@gmail.com>
2015-12-07 19:13:30 +1100

    BLI_linklist: add alloca append macro

--- source/blender/blenlib/BLI_linklist.h
+++ source/blender/blenlib/BLI_linklist.h
@@ -91,5 +91,7 @@ LinkNode *BLI_linklist_sort_r(LinkNode *list, int (*cmp)(void *, const void *, c
 
 #define BLI_linklist_prepend_alloca(listp, ptr) \
 	BLI_linklist_prepend_nlink(listp, ptr, alloca(sizeof(LinkNode)))
+#define BLI_linklist_append_alloca(list_pair, ptr) \
+	BLI_linklist_append_nlink(list_pair, ptr, alloca(sizeof(LinkNode)))
 
 #endif  /* __BLI_LINKLIST_H__ */
