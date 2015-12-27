12874c703cb4fe28600e2533309b3f855b226635
Bastien Montagne <montagne29@wanadoo.fr>
2015-12-28 00:33:07 +0100

    Cleanup typo.
    Harmless but irritating... :P

--- source/blender/blenkernel/BKE_material.h
+++ source/blender/blenkernel/BKE_material.h
@@ -45,7 +45,7 @@ struct Scene;
 /* materials */
 
 void init_def_material(void);
-void BKE_material_free(struct Material *sc); 
+void BKE_material_free(struct Material *ma);
 void BKE_material_free_ex(struct Material *ma, bool do_id_user);
 void test_object_materials(struct Main *bmain, struct ID *id);
 void BKE_material_resize_object(struct Object *ob, const short totcol, bool do_id_user);
