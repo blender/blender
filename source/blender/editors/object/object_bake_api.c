353d0237bcee46ba8e9636a962229c8bc9d2d612
Dalai Felinto <dfelinto@gmail.com>
2016-01-04 12:13:52 -0200

    Cycles-Bake: Unset properties fallback only working for invoke

--- source/blender/editors/object/object_bake_api.c
+++ source/blender/editors/object/object_bake_api.c
@@ -76,6 +76,8 @@
 
 #include "object_intern.h"
 
+/* prototypes */
+static void bake_set_props(wmOperator *op, Scene *scene);
 
 typedef struct BakeAPIRender {
 	Object *ob;
@@ -1078,6 +1080,9 @@ static int bake_exec(bContext *C, wmOperator *op)
 	Render *re;
 	int result = OPERATOR_CANCELLED;
 	BakeAPIRender bkr = {NULL};
+	Scene *scene = CTX_data_scene(C);
+
+	bake_set_props(op, scene);
 
 	bake_init_api_data(op, C, &bkr);
 	re = bkr.render;
