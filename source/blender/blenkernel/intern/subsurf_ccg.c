b4e10aa70b917eaeaeaa59a6ed16a4f824e4e2f3
Brecht Van Lommel <brechtvanlommel@gmail.com>
2016-02-14 20:24:36 +0100

    Fix T47405: subsurf triangulation was producing incorrect normals after last fix.

--- source/blender/blenkernel/intern/subsurf_ccg.c
+++ source/blender/blenkernel/intern/subsurf_ccg.c
@@ -2082,13 +2082,13 @@ static void ccgDM_buffer_copy_triangles(
 					for (x = 0; x < gridFaces; x++) {
 						start = gpumat->start + fc[mati].i_tri_visible;
 
-						varray[start++] = totloops;
-						varray[start++] = totloops + 2;
 						varray[start++] = totloops + 3;
-
-						varray[start++] = totloops;
-						varray[start++] = totloops + 1;
 						varray[start++] = totloops + 2;
+						varray[start++] = totloops;
+
+						varray[start++] = totloops + 2;
+						varray[start++] = totloops + 1;
+						varray[start++] = totloops;
 
 						fc[mati].i_tri_visible += 6;
 
