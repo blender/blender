c0d43871e01c4dee8ef712bbb1b9a75af9947ae6
Sergey Sharybin <sergey.vfx@gmail.com>
2016-02-20 16:43:46 +0100

    Make Blender ready for C++11
    Did a full compile of debug build with C++11 enabled, it all passed compilation
    apart from some deprecated type used in GE's Video Texture. Solved it inside of
    ifdef block now.
    
    In the future we should uncomment the MSVC part of it, it should all be safe and
    correct (MSVC2013 does not define new C++ version but supports C++11). The reason
    it is commented is to have absolutely no effect on the upcoming release.

--- source/gameengine/VideoTexture/PyTypeList.h
+++ source/gameengine/VideoTexture/PyTypeList.h
@@ -66,7 +66,11 @@ public:
 
 protected:
 	/// pointer to list of types
+#if (__cplusplus > 199711L) /* || (defined(_MSC_VER) && _MSC_VER >= 1800) */
+	std::unique_ptr<PyTypeListType> m_list;
+#else
 	std::auto_ptr<PyTypeListType> m_list;
+#endif
 };
 
 
