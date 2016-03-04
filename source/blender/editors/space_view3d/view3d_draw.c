d8b8a6e275cf200dc06779b6c30d02c2f08fd4eb
Bastien Montagne <montagne29@wanadoo.fr>
2016-03-04 12:32:35 +0100

    Fix T47683: broken BBox logic when drawing duplis.
    Once a dupli had a valid bbox, that bbox would be used for all following objects
    without bbox, instead of skipping clipping check.
    
    Issue unveiled by rB3fa0a1a5bc0ff2, but not related at all (in fact, bug was present before that commit).

--- source/blender/editors/space_view3d/view3d_draw.c
+++ source/blender/editors/space_view3d/view3d_draw.c
@@ -2063,7 +2063,6 @@ static void draw_dupli_objects_color(
 	short transflag;
 	bool use_displist = false;  /* -1 is initialize */
 	char dt;
-	bool testbb = false;
 	short dtx;
 	DupliApplyData *apply_data;
 
@@ -2087,10 +2086,11 @@ static void draw_dupli_objects_color(
 	if (dob) dob_next = dupli_step(dob->next);
 
 	for (; dob; dob_prev = dob, dob = dob_next, dob_next = dob_next ? dupli_step(dob_next->next) : NULL) {
+		bool testbb = false;
+
 		tbase.object = dob->ob;
 
 		/* Make sure lod is updated from dupli's position */
-
 		savedlod = dob->ob->currentlod;
 
 #ifdef WITH_GAMEENGINE
