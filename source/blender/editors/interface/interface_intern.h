blender/blender

commit 4d6b8925e621f534eea4ebb440149053f06da496

Fix T47158: Labels w/ icons use button text color
Icon buttons with text labels now use the same color as labels.

This was causing problems with theming since there was no way to set color
for all labels without also changing button text color.

source/blender/editors/interface/interface_widgets.c
@@ -3492,6 +3492,8 @@ static uiWidgetType *widget_type(uiWidgetTypeEnum type)
 			
 		/* specials */
 		case UI_WTYPE_ICON:
+			/* behave like regular labels (this is simply a label with an icon) */
+			wt.state = widget_state_label;
 			wt.custom = widget_icon_has_anim;
 			break;
 			

commit 0f1d711681dca29cf03fa3198aaa3cdceac0795f

Fix T47830: Multi-edit w/ ui-list wont highlight
Regression from 4d6b892.

source/blender/editors/interface/interface_intern.h
@@ -93,6 +93,7 @@ typedef enum {
 
 	/* specials */
 	UI_WTYPE_ICON,
+	UI_WTYPE_ICON_LABEL,
 	UI_WTYPE_SWATCH,
 	UI_WTYPE_RGB_PICKER,
 	UI_WTYPE_UNITVEC,

source/blender/editors/interface/interface_widgets.c
@@ -3499,6 +3499,10 @@ static uiWidgetType *widget_type(uiWidgetTypeEnum type)
 			
 		/* specials */
 		case UI_WTYPE_ICON:
+			wt.custom = widget_icon_has_anim;
+			break;
+
+		case UI_WTYPE_ICON_LABEL:
 			/* behave like regular labels (this is simply a label with an icon) */
 			wt.state = widget_state_label;
 			wt.custom = widget_icon_has_anim;
@@ -3628,7 +3632,14 @@ void ui_draw_but(const bContext *C, ARegion *ar, uiStyle *style, uiBut *but, rct
 	}
 	else if (but->dt == UI_EMBOSS_NONE) {
 		/* "nothing" */
-		wt = widget_type(UI_WTYPE_ICON);
+		switch (but->type) {
+			case UI_BTYPE_LABEL:
+				wt = widget_type(UI_WTYPE_ICON_LABEL);
+				break;
+			default:
+				wt = widget_type(UI_WTYPE_ICON);
+				break;
+		}
 	}
 	else if (but->dt == UI_EMBOSS_RADIAL) {
 		wt = widget_type(UI_WTYPE_MENU_ITEM_RADIAL);