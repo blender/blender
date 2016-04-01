8e1e3e0228a6917fb368bbc7675a484941cff0ef
Bastien Montagne <montagne29@wanadoo.fr>
2016-04-01 16:54:01 +0200

    Fix T48001: Highlighting selected item in filtered lists.
    active index was not reset to 'nothing' in case actual active item was filtered out.

--- source/blender/editors/interface/interface_templates.c
+++ source/blender/editors/interface/interface_templates.c
@@ -3017,7 +3017,7 @@ void uiTemplateList(
 						/* So that we do not map again activei! */
 						activei_mapping_pending = false;
 					}
-# if 0 /* For now, do not alter active element, even if it will be hidden... */
+#if 0 /* For now, do not alter active element, even if it will be hidden... */
 					else if (activei < i) {
 						/* We do not want an active but invisible item!
 						 * Only exception is when all items are filtered out...
@@ -3038,6 +3038,11 @@ void uiTemplateList(
 				i++;
 			}
 			RNA_PROP_END;
+
+			if (activei_mapping_pending) {
+				/* No active item found, set to 'invalid' -1 value... */
+				activei = -1;
+			}
 		}
 		if (dyn_data->items_shown >= 0) {
 			len = dyn_data->items_shown;
