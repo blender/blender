a51e9ece538fb00c939ccaca7d6fbb1fe491bf10
Sergey Sharybin <sergey.vfx@gmail.com>
2016-04-06 16:30:52 +0200

    Cycles: Fix misleading remaining time report when using infinite number of samples in viewport

--- intern/cycles/render/session.h
+++ intern/cycles/render/session.h
@@ -72,7 +72,7 @@ public:
 
 		progressive = false;
 		experimental = false;
-		samples = USHRT_MAX;
+		samples = INT_MAX;
 		tile_size = make_int2(64, 64);
 		start_resolution = INT_MAX;
 		threads = 0;
