5ae448735123f149c391d8245d33190669a21a94
Campbell Barton <ideasman42@gmail.com>
2016-05-09 23:51:42 +1000

    Missed last commit

--- source/blender/editors/gpencil/drawgpencil.c
+++ source/blender/editors/gpencil/drawgpencil.c
@@ -399,20 +399,13 @@ static void gp_triangulate_stroke_fill(bGPDstroke *gps)
 			gps->triangles = MEM_recallocN(gps->triangles, sizeof(*gps->triangles) * gps->tot_triangles);
 		}
 		
-		int triangle_index = 0;
 		int i;
 		
-		for (i = 0; (i < gps->totpoints) && (triangle_index < gps->tot_triangles); i++) {
-			if ((tmp_triangles[i][0] >= 0) && (tmp_triangles[i][0] < gps->totpoints) &&
-			    (tmp_triangles[i][1] >= 0) && (tmp_triangles[i][1] < gps->totpoints) &&
-			    (tmp_triangles[i][2] >= 0) && (tmp_triangles[i][2] < gps->totpoints))
-			{
-				bGPDtriangle *stroke_triangle = &gps->triangles[triangle_index++];
-				
-				stroke_triangle->v1 = tmp_triangles[i][0];
-				stroke_triangle->v2 = tmp_triangles[i][1];
-				stroke_triangle->v3 = tmp_triangles[i][2];
-			}
+		for (i = 0; i < gps->tot_triangles; i++) {
+			bGPDtriangle *stroke_triangle = &gps->triangles[i];
+			stroke_triangle->v1 = tmp_triangles[i][0];
+			stroke_triangle->v2 = tmp_triangles[i][1];
+			stroke_triangle->v3 = tmp_triangles[i][2];
 		}
 	}
 	else {
