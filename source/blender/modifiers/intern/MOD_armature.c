b4b269f38e6dbf45f97d2b80e6a4d7f787d85192
Sergey Sharybin <sergey.vfx@gmail.com>
2016-05-11 09:23:11 +0200

    Depsgraph: Add some missing relations
    Those cases requires not only geometry component, but also a transform one
    to be ready before evaluation can start.

--- source/blender/modifiers/intern/MOD_armature.c
+++ source/blender/modifiers/intern/MOD_armature.c
@@ -124,6 +124,7 @@ static void updateDepsgraph(ModifierData *md,
 	ArmatureModifierData *amd = (ArmatureModifierData *)md;
 	if (amd->object != NULL) {
 		DEG_add_object_relation(node, amd->object, DEG_OB_COMP_EVAL_POSE, "Armature Modifier");
+		DEG_add_object_relation(node, amd->object, DEG_OB_COMP_TRANSFORM, "Armature Modifier");
 	}
 }
 
