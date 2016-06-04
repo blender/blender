ebbfd92859e70e0ecb2886329cb352a5875f1588
Martijn Berger <martijn.berger@gmail.com>
2016-06-04 12:36:21 +0200

    buildbot work around cuda / msvc compatibility issues

--- build_files/buildbot/slave_compile.py
+++ build_files/buildbot/slave_compile.py
@@ -77,10 +77,10 @@ if 'cmake' in builder:
     elif builder.startswith('win'):
       if builder.endswith('_vc2015'):
         if builder.startswith('win64'):
-            cmake_options.extend(['-G', 'Visual Studio 14 2015 Win64'])
+            cmake_options.extend(['-G', 'Visual Studio 14 2015 Win64', b'-DCUDA_NVCC_FLAGS="-ccbin \\"C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\""'])
         elif builder.startswith('win32'):
             bits = 32
-            cmake_options.extend(['-G', 'Visual Studio 14 2015'])
+            cmake_options.extend(['-G', 'Visual Studio 14 2015', b'-DCUDA_NVCC_FLAGS="-ccbin \\"C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\""'])
       else:
         if builder.startswith('win64'):
             cmake_options.extend(['-G', 'Visual Studio 12 2013 Win64'])
