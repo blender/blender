91c146c42e51676c71eea39342be932c150d5d45
Martijn Berger <martijn.berger@gmail.com>
2016-06-07 20:58:53 +0200

    Cuda 7.5 cannot be made to work with a supported cl.exe version in the same way as cuda 6.0 does allow, disabling cuda kernels on buildbot for now

--- build_files/buildbot/slave_compile.py
+++ build_files/buildbot/slave_compile.py
@@ -77,7 +77,7 @@ if 'cmake' in builder:
     elif builder.startswith('win'):
       if builder.endswith('_vc2015'):
         if builder.startswith('win64'):
-            cmake_options.extend(['-G', 'Visual Studio 14 2015 Win64', '-DWITH_CYCLES_CUDA_BINARIES=0''])
+            cmake_options.extend(['-G', 'Visual Studio 14 2015 Win64', '-DWITH_CYCLES_CUDA_BINARIES=0'])
         elif builder.startswith('win32'):
             bits = 32
             cmake_options.extend(['-G', 'Visual Studio 14 2015', '-DWITH_CYCLES_CUDA_BINARIES=0'])
