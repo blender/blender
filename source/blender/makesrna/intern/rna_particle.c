097611a92a48e5dc45cd9b7d2f5d2c4edb364207
Bastien Montagne <montagne29@wanadoo.fr>
2016-06-23 13:09:32 +0200

    Fix T48710: 'velocity' particle settings were incorrectly using 'speed' in their tooltips.
    Not the same thing, velocity is a vector conveying both speed *and* direction...

--- source/blender/makesrna/intern/rna_particle.c
+++ source/blender/makesrna/intern/rna_particle.c
@@ -2635,35 +2635,35 @@ static void rna_def_particle_settings(BlenderRNA *brna)
 	RNA_def_property_float_sdna(prop, NULL, "normfac"); /*optional if prop names are the same */
 	RNA_def_property_range(prop, -1000.0f, 1000.0f);
 	RNA_def_property_ui_range(prop, 0, 100, 1, 3);
-	RNA_def_property_ui_text(prop, "Normal", "Let the surface normal give the particle a starting speed");
+	RNA_def_property_ui_text(prop, "Normal", "Let the surface normal give the particle a starting velocity");
 	RNA_def_property_update(prop, 0, "rna_Particle_reset");
 
 	prop = RNA_def_property(srna, "object_factor", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "obfac");
 	RNA_def_property_range(prop, -200.0f, 200.0f);
 	RNA_def_property_ui_range(prop, -1.0f, 1.0f, 0.1, 3);
-	RNA_def_property_ui_text(prop, "Object", "Let the object give the particle a starting speed");
+	RNA_def_property_ui_text(prop, "Object", "Let the object give the particle a starting velocity");
 	RNA_def_property_update(prop, 0, "rna_Particle_reset");
 
 	prop = RNA_def_property(srna, "factor_random", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "randfac"); /*optional if prop names are the same */
 	RNA_def_property_range(prop, 0.0f, 200.0f);
 	RNA_def_property_ui_range(prop, 0, 100, 1, 3);
-	RNA_def_property_ui_text(prop, "Random", "Give the starting speed a random variation");
+	RNA_def_property_ui_text(prop, "Random", "Give the starting velocity a random variation");
 	RNA_def_property_update(prop, 0, "rna_Particle_reset");
 
 	prop = RNA_def_property(srna, "particle_factor", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "partfac");
 	RNA_def_property_range(prop, -200.0f, 200.0f);
 	RNA_def_property_ui_range(prop, -1.0f, 1.0f, 0.1, 3);
-	RNA_def_property_ui_text(prop, "Particle", "Let the target particle give the particle a starting speed");
+	RNA_def_property_ui_text(prop, "Particle", "Let the target particle give the particle a starting velocity");
 	RNA_def_property_update(prop, 0, "rna_Particle_reset");
 
 	prop = RNA_def_property(srna, "tangent_factor", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "tanfac");
 	RNA_def_property_range(prop, -1000.0f, 1000.0f);
 	RNA_def_property_ui_range(prop, -100, 100, 1, 2);
-	RNA_def_property_ui_text(prop, "Tangent", "Let the surface tangent give the particle a starting speed");
+	RNA_def_property_ui_text(prop, "Tangent", "Let the surface tangent give the particle a starting velocity");
 	RNA_def_property_update(prop, 0, "rna_Particle_reset");
 
 	prop = RNA_def_property(srna, "tangent_phase", PROP_FLOAT, PROP_NONE);
@@ -2677,7 +2677,7 @@ static void rna_def_particle_settings(BlenderRNA *brna)
 	RNA_def_property_range(prop, -10.0f, 10.0f);
 	RNA_def_property_ui_text(prop, "Reactor",
 	                         "Let the vector away from the target particle's location give the particle "
-	                         "a starting speed");
+	                         "a starting velocity");
 	RNA_def_property_update(prop, 0, "rna_Particle_reset");
 
 	prop = RNA_def_property(srna, "object_align_factor", PROP_FLOAT, PROP_VELOCITY);
@@ -2686,7 +2686,7 @@ static void rna_def_particle_settings(BlenderRNA *brna)
 	RNA_def_property_range(prop, -200.0f, 200.0f);
 	RNA_def_property_ui_range(prop, -100, 100, 1, 3);
 	RNA_def_property_ui_text(prop, "Object Aligned",
-	                         "Let the emitter object orientation give the particle a starting speed");
+	                         "Let the emitter object orientation give the particle a starting velocity");
 	RNA_def_property_update(prop, 0, "rna_Particle_reset");
 
 	prop = RNA_def_property(srna, "angular_velocity_factor", PROP_FLOAT, PROP_NONE);
