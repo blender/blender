4b0aeaf327d383ec760171261a1087abbb714d04
Alexander Romanov <a.romanov@blend4web.com>
2016-06-30 12:40:10 +0300

    Fix T48757: Broken in D1120 normal baking

--- source/blender/render/intern/source/convertblender.c
+++ source/blender/render/intern/source/convertblender.c
@@ -3172,9 +3172,8 @@ static void init_render_mesh(Render *re, ObjectRen *obr, int timeoffset)
 		/* exception for tangent space baking */
 		if (me->mtpoly==NULL) {
 			need_orco= 1;
-			need_tangent= 1;
 		}
-		need_nmap_tangent_concrete = true;
+		need_tangent= 1;
 	}
 
 	/* check autosmooth and displacement, we then have to skip only-verts optimize
