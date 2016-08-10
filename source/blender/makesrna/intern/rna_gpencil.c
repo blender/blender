ebdb5490b3a63e37ac5638f8a06737cf34ba36d3
Antonioya <blendergit@gmail.com>
2016-08-10 15:51:40 +0200

    GPencil: Avoid segment fault if new stroke function is called without colorname

--- source/blender/makesrna/intern/rna_gpencil.c
+++ source/blender/makesrna/intern/rna_gpencil.c
@@ -482,7 +482,9 @@ static void rna_GPencil_stroke_point_pop(bGPDstroke *stroke, ReportList *reports
 static bGPDstroke *rna_GPencil_stroke_new(bGPDframe *frame, const char *colorname)
 {
 	bGPDstroke *stroke = MEM_callocN(sizeof(bGPDstroke), "gp_stroke");
-	strcpy(stroke->colorname, colorname);
+	if (colorname) {
+		strcpy(stroke->colorname, colorname);
+	}
 	stroke->palcolor = NULL;
 	stroke->flag |= GP_STROKE_RECALC_COLOR;
 	BLI_addtail(&frame->strokes, stroke);
