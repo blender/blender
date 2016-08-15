33c83a289d269986678caaf7e90ec6d5b729099a
Brecht Van Lommel <brechtvanlommel@gmail.com>
2016-08-15 16:17:47 +0200

    Fix Cycles OpenCL textures after recent CUDA fix.
    kernel_textures.h is included in device_opencl.cpp, so we can't check
    __KERNEL_OPENCL__ there.

--- intern/cycles/kernel/kernel_compat_cuda.h
+++ intern/cycles/kernel/kernel_compat_cuda.h
@@ -67,6 +67,7 @@ typedef texture<float, 1> texture_float;
 typedef texture<uint, 1> texture_uint;
 typedef texture<int, 1> texture_int;
 typedef texture<uint4, 1> texture_uint4;
+typedef texture<uchar, 1> texture_uchar;
 typedef texture<uchar4, 1> texture_uchar4;
 typedef texture<float4, 2> texture_image_float4;
 typedef texture<float4, 3> texture_image3d_float4;
