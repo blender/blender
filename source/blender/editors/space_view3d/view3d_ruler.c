34928ca98aba6706a1126c60b4b02c5d39f7e53c
Antonioya <blendergit@gmail.com>
2016-08-17 09:04:50 +0200

    GPencil: Change ruler line thickness to make it thicker

--- source/blender/editors/space_view3d/view3d_ruler.c
+++ source/blender/editors/space_view3d/view3d_ruler.c
@@ -355,6 +355,7 @@ static bool view3d_ruler_to_gpencil(bContext *C, RulerInfo *ruler_info)
 			}
 		}
 		gps->flag = GP_STROKE_3DSPACE;
+		gps->thickness = 3;
 		/* assign color to stroke */
 		strcpy(gps->colorname, palcolor->info);
 		gps->palcolor = palcolor;
