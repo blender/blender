cc2faa409fc8508fb0131bdc344651f2863957f6
Jörg Müller <nexyon@gmail.com>
2016-08-29 10:54:32 +0200

    Fix T49172: mixdown sound op not exporting full length
    Thanks Flavio Perez for the fix.

--- source/blender/editors/sound/sound_ops.c
+++ source/blender/editors/sound/sound_ops.c
@@ -348,10 +348,10 @@ static int sound_mixdown_exec(bContext *C, wmOperator *op)
 	BLI_path_abs(filename, bmain->name);
 
 	if (split)
-		result = AUD_mixdown_per_channel(scene->sound_scene, SFRA * specs.rate / FPS, (EFRA - SFRA) * specs.rate / FPS,
+		result = AUD_mixdown_per_channel(scene->sound_scene, SFRA * specs.rate / FPS, (EFRA - SFRA + 1) * specs.rate / FPS,
 		                                 accuracy, filename, specs, container, codec, bitrate);
 	else
-		result = AUD_mixdown(scene->sound_scene, SFRA * specs.rate / FPS, (EFRA - SFRA) * specs.rate / FPS,
+		result = AUD_mixdown(scene->sound_scene, SFRA * specs.rate / FPS, (EFRA - SFRA + 1) * specs.rate / FPS,
 		                     accuracy, filename, specs, container, codec, bitrate);
 
 	if (result) {
