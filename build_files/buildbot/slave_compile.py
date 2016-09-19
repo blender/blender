12680d36f539a6006308a2b27282f0cb3acc5836
Sergey Sharybin <sergey.vfx@gmail.com>
2016-09-19 16:24:55 +0200

    Buildbot: Temporary disable CUBIN compilation

--- build_files/buildbot/slave_compile.py
+++ build_files/buildbot/slave_compile.py
@@ -75,7 +75,7 @@ if 'cmake' in builder:
         cmake_extra_options.append('-DCUDA_NVCC_EXECUTABLE=/usr/local/cuda-hack/bin/nvcc')
         cmake_extra_options.append('-DCUDA_NVCC8_EXECUTABLE=/usr/local/cuda8-hack/bin/nvcc')
         cmake_extra_options.append('-DWITH_CODEC_QUICKTIME=OFF')
-        cmake_extra_options.append('-DCUDA_HOST_COMPILER=/usr/local/cuda-hack/bin/clang')
+        build_cubins = False
 
 
     elif builder.startswith('win'):
