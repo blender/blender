5fc9bed8b319271876bd0c868309946e201b4b6e
Mike Erwin <significant.bit@gmail.com>
2016-10-10 01:08:49 -0400

    BGE: remove calls to GLSL support query (it's always supported)
    Reworked logic in the few places that still called this. Deleted the "GLSL not supported" fallbacks.
    
    Also removed some nearby checks for ARB_multitexture and OpenGL 1.1. Blender 2.77 removed checks like this, but game engine still has some.

--- source/gameengine/BlenderRoutines/BL_KetsjiEmbedStart.cpp
+++ source/gameengine/BlenderRoutines/BL_KetsjiEmbedStart.cpp
@@ -465,19 +465,9 @@ extern "C" void StartKetsjiShell(struct bContext *C, struct ARegion *ar, rcti *c
 			if (always_use_expand_framing)
 				sceneconverter->SetAlwaysUseExpandFraming(true);
 
-			bool usemat = false, useglslmat = false;
+			sceneconverter->SetMaterials(true);
 
-			if (GLEW_ARB_multitexture && GLEW_VERSION_1_1)
-				usemat = true;
-
-			if (GPU_glsl_support())
-				useglslmat = true;
-			else if (gs.matmode == GAME_MAT_GLSL)
-				usemat = false;
-
-			if (usemat)
-				sceneconverter->SetMaterials(true);
-			if (useglslmat && (gs.matmode == GAME_MAT_GLSL))
+			if (gs.matmode == GAME_MAT_GLSL)
 				sceneconverter->SetGLSLMaterials(true);
 			if (scene->gm.flag & GAME_NO_MATERIAL_CACHING)
 				sceneconverter->SetCacheMaterials(false);
