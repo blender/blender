c25c3bb6cbb2c9562bb60af7fd494b4376e39048
Dalai Felinto <dfelinto@gmail.com>
2016-11-02 11:09:27 +0100

    Fix UI for smoke cache
    (this was probably broken since we got rid of point cache in 2.8)

--- release/scripts/startup/bl_ui/properties_physics_smoke.py
+++ release/scripts/startup/bl_ui/properties_physics_smoke.py
@@ -298,21 +298,17 @@ class PHYSICS_PT_smoke_cache(PhysicButtonsPanel, Panel):
     def draw(self, context):
         layout = self.layout
 
+        if not bpy.app.build_options.openvdb:
+            layout.label("Built without OpenVDB support")
+            return
+
         domain = context.smoke.domain_settings
-        cache_file_format = domain.cache_file_format
 
-        layout.prop(domain, "cache_file_format")
-
-        if cache_file_format == 'OPENVDB':
-            if not bpy.app.build_options.openvdb:
-                layout.label("Built without OpenVDB support")
-                return
-
-            layout.label(text="Compression:")
-            layout.prop(domain, "openvdb_cache_compress_type", expand=True)
-            row = layout.row()
-            row.label("Data Depth:")
-            row.prop(domain, "data_depth", expand=True, text="Data Depth")
+        layout.label(text="Compression:")
+        layout.prop(domain, "openvdb_cache_compress_type", expand=True)
+        row = layout.row()
+        row.label("Data Depth:")
+        row.prop(domain, "data_depth", expand=True, text="Data Depth")
 
 
 class PHYSICS_PT_smoke_field_weights(PhysicButtonsPanel, Panel):
