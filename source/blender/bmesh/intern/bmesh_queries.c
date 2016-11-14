c418ef48cb1ae1c9fc3748e32b43cfa5a7cadd00
Campbell Barton <ideasman42@gmail.com>
2016-11-15 07:55:55 +1100

    BMesh: match BM_vert_find_first_loop with iterator logic
    Use changes from previous commit for BM_vert_find_first_loop.

--- source/blender/bmesh/intern/bmesh_queries.c
+++ source/blender/bmesh/intern/bmesh_queries.c
@@ -381,17 +381,7 @@ BMFace *BM_vert_pair_share_face_by_angle(
  */
 BMLoop *BM_vert_find_first_loop(BMVert *v)
 {
-	BMEdge *e;
-
-	if (!v->e)
-		return NULL;
-
-	e = bmesh_disk_faceedge_find_first(v->e, v);
-
-	if (!e)
-		return NULL;
-
-	return bmesh_radial_faceloop_find_first(e->l, v);
+	return v->e ? bmesh_disk_faceloop_find_first(v->e, v) : NULL;
 }
 
 /**
