0a446d7276e74e3c8472453948204ca6463d158e
Luca Rood <dev@lucarood.com>
2016-12-22 02:13:03 -0200

    Add 3d to 2d plane mapping functions to math lib
    This adds two functions to project 3d coordinates onto a 3d plane,
    to get 2d coordinates, essentially eliminating the plane's normal axis
    from the coordinates.
    
    Reviewed By: mont29
    
    Differential Revision: https://developer.blender.org/D2460

--- source/blender/blenlib/BLI_math_geom.h
+++ source/blender/blenlib/BLI_math_geom.h
@@ -389,6 +389,8 @@ void box_minmax_bounds_m4(float min[3], float max[3],
 
 void map_to_tube(float *r_u, float *r_v, const float x, const float y, const float z);
 void map_to_sphere(float *r_u, float *r_v, const float x, const float y, const float z);
+void map_to_plane_v2_v3v3(float r_co[2], const float co[3], const float no[3]);
+void map_to_plane_axis_angle_v2_v3v3fl(float r_co[2], const float co[3], const float axis[3], const float angle);
 
 /********************************** Normals **********************************/
 
