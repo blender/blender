c4f60319d1709723d8076429c13965b0c94dc88b
Julian Eisel <eiseljulian@gmail.com>
2017-01-11 22:11:13 +0100

    UI: Try limiting 'x' icon to search buttons
    Reusing PROP_TEXTEDIT_UPDATE instead of adding a new property flag just for search strings. Currently it's only used for search strings anyway so seems fine for now.
    
    Fixes T50336.

--- source/blender/makesrna/RNA_types.h
+++ source/blender/makesrna/RNA_types.h
@@ -174,8 +174,10 @@ typedef enum PropertyFlag {
 	 * and collections */
 	PROP_ANIMATABLE              = (1 << 1),
 
-	/* This flag means when the property's widget is in 'textedit' mode, it will be updated after every typed char,
-	 * instead of waiting final validation. Used e.g. for text searchbox. */
+	/* This flag means when the property's widget is in 'textedit' mode, it will be updated
+	 * after every typed char, instead of waiting final validation. Used e.g. for text searchbox.
+	 * It will also cause UI_BUT_VALUE_CLEAR to be set for text buttons. We could add an own flag
+	 * for search/filter properties, but this works just fine for now. */
 	PROP_TEXTEDIT_UPDATE         = (1 << 31),
 
 	/* icon */
