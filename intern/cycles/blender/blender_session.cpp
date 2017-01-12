e9e1c95e6461774d65b91b1ec3d8f4d0840a37d4
Sergey Sharybin <sergey.vfx@gmail.com>
2017-01-12 12:45:37 +0100

    Cycles: Cleanup, use switch() instead of if-else chain
    About to add extra debug passes, which will be more clear to use switch().

--- intern/cycles/blender/blender_session.cpp
+++ intern/cycles/blender/blender_session.cpp
@@ -305,12 +305,14 @@ static PassType get_pass_type(BL::RenderPass& b_pass)
 #ifdef WITH_CYCLES_DEBUG
 		case BL::RenderPass::type_DEBUG:
 		{
-			if(b_pass.debug_type() == BL::RenderPass::debug_type_BVH_TRAVERSAL_STEPS)
-				return PASS_BVH_TRAVERSAL_STEPS;
-			if(b_pass.debug_type() == BL::RenderPass::debug_type_BVH_TRAVERSED_INSTANCES)
-				return PASS_BVH_TRAVERSED_INSTANCES;
-			if(b_pass.debug_type() == BL::RenderPass::debug_type_RAY_BOUNCES)
-				return PASS_RAY_BOUNCES;
+			switch(b_pass.debug_type()) {
+				case BL::RenderPass::debug_type_BVH_TRAVERSAL_STEPS:
+					return PASS_BVH_TRAVERSAL_STEPS;
+				case BL::RenderPass::debug_type_BVH_TRAVERSED_INSTANCES:
+					return PASS_BVH_TRAVERSED_INSTANCES;
+				case BL::RenderPass::debug_type_RAY_BOUNCES:
+					return PASS_RAY_BOUNCES;
+			}
 			break;
 		}
 #endif
