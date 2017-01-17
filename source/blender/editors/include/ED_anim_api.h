61b52f6d900d123835dc8577374a1ddbd4f9277e
Antonio Vazquez <blendergit@gmail.com>
2017-01-17 15:53:31 +0100

    Fix previous commit because there are some code using this define, but really it is not used in editors. This is a temporary fix to broken editors, but this entry must be removed or moved to the right place when particles are in place again.

--- source/blender/editors/include/ED_anim_api.h
+++ source/blender/editors/include/ED_anim_api.h
@@ -183,7 +183,13 @@ typedef enum eAnim_ChannelType {
 	ANIMTYPE_NLAACTION,
 	
 	/* always as last item, the total number of channel types... */
-	ANIMTYPE_NUM_TYPES
+	ANIMTYPE_NUM_TYPES,
+
+	/* TODO: WARNING remove this entry or move to the right place when particle system is enabled again. 
+	   This definition was set here only to compile, but it is not used and cannot be in the middle of the list 
+	   or break the editors */
+	ANIMTYPE_DSPART
+
 } eAnim_ChannelType;
 
 /* types of keyframe data in bAnimListElem */
