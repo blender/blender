8ea09252c8261c7057a130763c7f44bd45c9f9d2
Sergey Sharybin <sergey.vfx@gmail.com>
2017-01-25 11:18:12 +0100

    Fix T50517: Rendering expecting time is negative

--- intern/cycles/render/tile.cpp
+++ intern/cycles/render/tile.cpp
@@ -157,7 +157,7 @@ void TileManager::set_samples(int num_samples_)
 			divider >>= 1;
 		}
 
-		state.total_pixel_samples = pixel_samples + get_num_effective_samples() * params.width*params.height;
+		state.total_pixel_samples = pixel_samples + (uint64_t)get_num_effective_samples() * params.width*params.height;
 	}
 }
 
