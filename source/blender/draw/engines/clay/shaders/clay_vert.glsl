0c1c646118df316468b5a49386da10f6d1ca127b
Mike Erwin <significant.bit@gmail.com>
2017-03-02 18:11:21 -0500

    clean up clay vertex shader
    Clay engine only works on modern GL, so this shader doesn't need compatibility with legacy Mac GL.

--- source/blender/draw/engines/clay/shaders/clay_vert.glsl
+++ source/blender/draw/engines/clay/shaders/clay_vert.glsl
@@ -1,20 +1,12 @@
 uniform mat4 ModelViewProjectionMatrix;
 uniform mat3 NormalMatrix;
 
-#if __VERSION__ == 120
-attribute vec3 pos;
-attribute vec3 nor;
-varying vec3 normal;
-#else
 in vec3 pos;
 in vec3 nor;
 out vec3 normal;
-#endif
-
 
 void main()
 {
 	normal = normalize(NormalMatrix * nor);
 	gl_Position = ModelViewProjectionMatrix * vec4(pos, 1.0);
 }
-
