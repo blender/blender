blender/blender

commit eba09b1520c06df304bc353e93d7220b4e83b755

Blender 2.8: Hook of layer collections evaluation in DEG
This moves selectability/visibility flag flush from some hardcoded
places in the code to depsgraph. This way it is possible to simply
tag depsgraph to update those flags and rest it'll do on its own.

Using depsgraph for such flush is an overkill: those flags are fully
static and can not be animated, so it doesn't really make sense to
hook only those to depsgraph.

However, in the future we will have overrides on collections, which
ideally would need to be animatable and drivable and easiest way
to support this is to do this on depsgraph level, so it ensures
proper order of evaluation for animation and drivers. And it seems
logical to do both overrides and flags flush from depsgraph from
this point of view.

This commit also includes the evaluation of IDProperty for collections,
which basically are just another form of override. So once we implement
the other kind of overrides the flushing and collection evaluation won't
change.

Patch by Sergey Sharybin and Dalai Felinto

source/blender/depsgraph/CMakeLists.txt
@@ -43,11 +43,13 @@ set(SRC
 	intern/builder/deg_builder.cc
 	intern/builder/deg_builder_cycle.cc
 	intern/builder/deg_builder_nodes.cc
+	intern/builder/deg_builder_nodes_layer.cc
 	intern/builder/deg_builder_nodes_rig.cc
 	intern/builder/deg_builder_nodes_scene.cc
 	intern/builder/deg_builder_pchanmap.cc
 	intern/builder/deg_builder_relations.cc
 	intern/builder/deg_builder_relations_keys.cc
+	intern/builder/deg_builder_relations_layer.cc
 	intern/builder/deg_builder_relations_rig.cc
 	intern/builder/deg_builder_relations_scene.cc
 	intern/builder/deg_builder_transitive.cc

source/blender/blenkernel/BKE_layer.h
@@ -44,6 +44,7 @@ extern "C" {
 
 #define ROOT_PROP "root"
 
+struct EvaluationContext;
 struct LayerCollection;
 struct ID;
 struct IDProperty;
@@ -72,12 +73,6 @@ struct SceneLayer *BKE_scene_layer_find_from_collection(const struct Scene *scen
 struct Base *BKE_scene_layer_base_find(struct SceneLayer *sl, struct Object *ob);
 void BKE_scene_layer_base_deselect_all(struct SceneLayer *sl);
 void BKE_scene_layer_base_select(struct SceneLayer *sl, struct Base *selbase);
-void BKE_scene_layer_base_flag_recalculate(struct SceneLayer *sl);
-
-void BKE_scene_layer_engine_settings_recalculate(struct SceneLayer *sl);
-void BKE_scene_layer_engine_settings_object_recalculate(struct SceneLayer *sl, struct Object *ob);
-void BKE_scene_layer_engine_settings_collection_recalculate(struct SceneLayer *sl, struct LayerCollection *lc);
-void BKE_scene_layer_engine_settings_update(struct Scene *scene, struct SceneLayer *sl);
 
 void BKE_layer_collection_free(struct SceneLayer *sl, struct LayerCollection *lc);
 
@@ -128,6 +123,18 @@ bool BKE_collection_engine_property_value_get_bool(struct IDProperty *props, con
 void BKE_collection_engine_property_value_set_int(struct IDProperty *props, const char *name, int value);
 void BKE_collection_engine_property_value_set_float(struct IDProperty *props, const char *name, float value);
 void BKE_collection_engine_property_value_set_bool(struct IDProperty *props, const char *name, bool value);
+
+/* evaluation */
+
+void BKE_layer_eval_layer_collection_pre(struct EvaluationContext *eval_ctx,
+                                         struct Scene *scene,
+                                         struct SceneLayer *scene_layer);
+void BKE_layer_eval_layer_collection(struct EvaluationContext *eval_ctx,
+                                     struct Scene *scene,
+                                     struct LayerCollection *layer_collection,
+                                     struct LayerCollection *parent_layer_collection);
+void BKE_layer_eval_layer_collection_post(struct EvaluationContext *eval_ctx,
+                                          struct SceneLayer *scene_layer);
 
 /* iterators */
 
@@ -188,18 +195,18 @@ void BKE_visible_bases_Iterator_end(Iterator *iter);
 	IteratorBeginCb func_begin;                                               \
 	IteratorCb func_next, func_end;                                           \
 	void *data_in;                                                            \
-                                                                                  \
+	                                                                          \
 	if (flag == SELECT) {                                                     \
-		func_begin = &BKE_selected_objects_Iterator_begin;                \
-		func_next = &BKE_selected_objects_Iterator_next;                  \
-		func_end = &BKE_selected_objects_Iterator_end;                    \
-		data_in = (sl);                                                   \
+	    func_begin = &BKE_selected_objects_Iterator_begin;                    \
+	    func_next = &BKE_selected_objects_Iterator_next;                      \
+	    func_end = &BKE_selected_objects_Iterator_end;                        \
+	    data_in = (sl);                                                       \
 	}                                                                         \
 	else {                                                                    \
-		func_begin = BKE_scene_objects_Iterator_begin;                    \
-		func_next = BKE_scene_objects_Iterator_next;                      \
-		func_end = BKE_scene_objects_Iterator_end;                        \
-		data_in = (scene);                                                \
+	    func_begin = BKE_scene_objects_Iterator_begin;                        \
+	    func_next = BKE_scene_objects_Iterator_next;                          \
+	    func_end = BKE_scene_objects_Iterator_end;                            \
+	    data_in = (scene);                                                    \
 	}                                                                         \
 	ITER_BEGIN(func_begin, func_next, func_end, data_in, Object *, _instance)
 
@@ -208,24 +215,20 @@ void BKE_visible_bases_Iterator_end(Iterator *iter);
 	ITER_END                                                                  \
 }
 
-/* temporary hacky solution waiting for final depsgraph evaluation */
-#define DEG_OBJECT_ITER(scene_, sl_, instance_)                               \
+/* temporary hacky solution waiting for CoW depsgraph implementation */
+#define DEG_OBJECT_ITER(sl_, instance_)                                       \
 {                                                                             \
+	/* flush all the depsgraph data to objects */                             \
 	Object *instance_;                                                        \
-	/* temporary solution, waiting for depsgraph update */                    \
-	BKE_scene_layer_engine_settings_update(scene_, sl_);                      \
-                                                                                  \
-	/* flush all the data to objects*/                                        \
 	Base *base_;                                                              \
 	for (base_ = (sl_)->object_bases.first; base_; base_ = base_->next) {     \
-	if ((base_->flag & BASE_VISIBLED) == 0) {                                 \
-		continue;                                                         \
-	}                                                                         \
-                                                                                  \
-	instance_ = base_->object;                                                \
-	instance_->base_flag = base_->flag;
+	    if ((base_->flag & BASE_VISIBLED) != 0) {                             \
+	        instance_ = base_->object;                                        \
+	        instance_->base_flag = base_->flag;                               \
+	        instance_->base_collection_properties = base_->collection_properties;
 
 #define DEG_OBJECT_ITER_END                                                   \
+        }                                                                     \
     }                                                                         \
 }
 

source/blender/blenkernel/intern/collection.c
@@ -119,7 +119,6 @@ static void layer_collection_remove(SceneLayer *sl, ListBase *lb, const SceneCol
 	LayerCollection *lc = lb->first;
 	while (lc) {
 		if (lc->scene_collection == sc) {
-			BKE_scene_layer_engine_settings_collection_recalculate(sl, lc);
 			BKE_layer_collection_free(sl, lc);
 			BLI_remlink(lb, lc);
 
@@ -165,7 +164,6 @@ bool BKE_collection_remove(Scene *scene, SceneCollection *sc)
 	/* check all layers that use this collection and clear them */
 	for (SceneLayer *sl = scene->render_layers.first; sl; sl = sl->next) {
 		layer_collection_remove(sl, &sl->layer_collections, sc);
-		BKE_scene_layer_base_flag_recalculate(sl);
 		sl->active_collection = 0;
 	}
 

commit 5a5f1dc8e495d8c3e8c4acce0f45830e968979c2

Unittests for depsgraph layer collection render settings  evaluation
Some of the tests are failing at the moment.
Those problems were introduced in eba09b1520

tests/python/render_layer/CMakeLists.txt
@@ -62,6 +62,13 @@ endmacro()
 
 RENDER_LAYER_TEST(active_collection)
 RENDER_LAYER_TEST(collection_rename)
+RENDER_LAYER_TEST(evaluation_render_settings_a)
+RENDER_LAYER_TEST(evaluation_render_settings_b)
+RENDER_LAYER_TEST(evaluation_render_settings_c)
+RENDER_LAYER_TEST(evaluation_render_settings_d)
+RENDER_LAYER_TEST(evaluation_render_settings_e)
+RENDER_LAYER_TEST(evaluation_render_settings_f)
+RENDER_LAYER_TEST(evaluation_render_settings_g)
 RENDER_LAYER_TEST(evaluation_visibility_a)
 RENDER_LAYER_TEST(evaluation_visibility_b)
 RENDER_LAYER_TEST(evaluation_visibility_c)

tests/python/render_layer/render_layer_common.py
@@ -693,3 +693,88 @@ class MoveLayerCollectionTesting(MoveSceneCollectionSyncTesting):
         layer_collection_src = self.parse_move(src)
         layer_collection_dst = self.parse_move(dst)
         return layer_collection_src.move_below(layer_collection_dst)
+
+
+class Clay:
+    def __init__(self, extra_kid_layer=False):
+        import bpy
+
+        self._scene = bpy.context.scene
+        self._layer = self._fresh_layer()
+        self._object = bpy.data.objects.new('guinea pig', bpy.data.meshes.new('mesh'))
+
+        # update depsgraph
+        self._scene.update()
+
+        scene_collection_grandma = self._scene.master_collection.collections.new("Grandma")
+        scene_collection_mom = scene_collection_grandma.collections.new("Mom")
+        scene_collection_kid = scene_collection_mom.collections.new("Kid")
+        scene_collection_kid.objects.link(self._object)
+
+        layer_collection_grandma = self._layer.collections.link(scene_collection_grandma)
+        layer_collection_mom = layer_collection_grandma.collections[0]
+        layer_collection_kid = layer_collection_mom.collections[0]
+
+        # store the variables
+        self._scene_collections = {
+                'grandma': scene_collection_grandma,
+                'mom': scene_collection_mom,
+                'kid': scene_collection_kid,
+                }
+        self._layer_collections = {
+                'grandma': layer_collection_grandma,
+                'mom': layer_collection_mom,
+                'kid': layer_collection_kid,
+                }
+
+        if extra_kid_layer:
+            layer_collection_extra = self._layer.collections.link(scene_collection_kid)
+            self._layer_collections['extra'] = layer_collection_extra
+
+        self._update()
+
+    def _fresh_layer(self):
+        import bpy
+
+        # remove all other objects
+        while bpy.data.objects:
+            bpy.data.objects.remove(bpy.data.objects[0])
+
+        layer = self._scene.render_layers.new('Evaluation Test')
+        layer.collections.unlink(layer.collections[0])
+        self._scene.render_layers.active = layer
+
+        # remove all other layers
+        for layer_iter in self._scene.render_layers:
+            if layer_iter != layer:
+                self._scene.render_layers.remove(layer_iter)
+
+        return layer
+
+    def _update(self):
+        """
+        Force depsgrpah evaluation
+        and update pointers to IDProperty collections
+        """
+        ENGINE = 'BLENDER_CLAY'
+
+        self._scene.update()  # update depsgraph
+        self._layer.update()  # flush depsgraph evaluation
+
+        # change scene settings
+        self._properties = {
+                'scene': self._scene.collection_properties[ENGINE],
+                'object': self._object.collection_properties[ENGINE],
+                }
+
+        for key, value in self._layer_collections.items():
+            self._properties[key] = self._layer_collections[key].engine_overrides[ENGINE]
+
+    def get(self, name, data_path):
+        self._update()
+        return getattr(self._properties[name], data_path)
+
+    def set(self, name, data_path, value):
+        self._update()
+        self._properties[name].use(data_path)
+        setattr(self._properties[name], data_path, value)

tests/python/render_layer/test_evaluation_render_settings_a.py
@@ -1,0 +1,38 @@
+# ############################################################
+# Importing - Same For All Render Layer Tests
+# ############################################################
+
+import unittest
+import os
+import sys
+
+from render_layer_common import *
+
+
+# ############################################################
+# Testing
+# ############################################################
+
+class UnitTesting(RenderLayerTesting):
+    def test_render_settings(self):
+        """
+        See if the depsgraph evaluation is correct
+        """
+        clay = Clay()
+        self.assertEqual(clay.get('object', 'matcap_icon'), '01')
+        clay.set('scene', 'matcap_icon', '05')
+        self.assertEqual(clay.get('object', 'matcap_icon'), '05')
+
+
+# ############################################################
+# Main - Same For All Render Layer Tests
+# ############################################################
+
+if __name__ == '__main__':
+    import sys
+
+    extra_arguments = sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else []
+    sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 2:] if "--" in sys.argv else [])
+
+    UnitTesting._extra_arguments = extra_arguments
+    unittest.main()

tests/python/render_layer/test_evaluation_render_settings_b.py
@@ -1,0 +1,39 @@
+# ############################################################
+# Importing - Same For All Render Layer Tests
+# ############################################################
+
+import unittest
+import os
+import sys
+
+from render_layer_common import *
+
+
+# ############################################################
+# Testing
+# ############################################################
+
+class UnitTesting(RenderLayerTesting):
+    def test_render_settings(self):
+        """
+        See if the depsgraph evaluation is correct
+        """
+        clay = Clay()
+        self.assertEqual(clay.get('object', 'matcap_icon'), '01')
+        clay.set('scene', 'matcap_icon', '05')
+        clay.set('grandma', 'matcap_icon', '03')
+        self.assertEqual(clay.get('object', 'matcap_icon'), '03')
+
+
+# ############################################################
+# Main - Same For All Render Layer Tests
+# ############################################################
+
+if __name__ == '__main__':
+    import sys
+
+    extra_arguments = sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else []
+    sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 2:] if "--" in sys.argv else [])
+
+    UnitTesting._extra_arguments = extra_arguments
+    unittest.main()

tests/python/render_layer/test_evaluation_render_settings_c.py
@@ -1,0 +1,38 @@
+# ############################################################
+# Importing - Same For All Render Layer Tests
+# ############################################################
+
+import unittest
+import os
+import sys
+
+from render_layer_common import *
+
+
+# ############################################################
+# Testing
+# ############################################################
+
+class UnitTesting(RenderLayerTesting):
+    def test_render_settings(self):
+        """
+        See if the depsgraph evaluation is correct
+        """
+        clay = Clay()
+        self.assertEqual(clay.get('object', 'matcap_icon'), '01')
+        clay.set('mom', 'matcap_icon', '02')
+        self.assertEqual(clay.get('object', 'matcap_icon'), '02')
+
+
+# ############################################################
+# Main - Same For All Render Layer Tests
+# ############################################################
+
+if __name__ == '__main__':
+    import sys
+
+    extra_arguments = sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else []
+    sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 2:] if "--" in sys.argv else [])
+
+    UnitTesting._extra_arguments = extra_arguments
+    unittest.main()

tests/python/render_layer/test_evaluation_render_settings_d.py
@@ -1,0 +1,38 @@
+# ############################################################
+# Importing - Same For All Render Layer Tests
+# ############################################################
+
+import unittest
+import os
+import sys
+
+from render_layer_common import *
+
+
+# ############################################################
+# Testing
+# ############################################################
+
+class UnitTesting(RenderLayerTesting):
+    def test_render_settings(self):
+        """
+        See if the depsgraph evaluation is correct
+        """
+        clay = Clay()
+        self.assertEqual(clay.get('object', 'matcap_icon'), '01')
+        clay.set('kid', 'matcap_icon', '05')
+        self.assertEqual(clay.get('object', 'matcap_icon'), '05')
+
+
+# ############################################################
+# Main - Same For All Render Layer Tests
+# ############################################################
+
+if __name__ == '__main__':
+    import sys
+
+    extra_arguments = sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else []
+    sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 2:] if "--" in sys.argv else [])
+
+    UnitTesting._extra_arguments = extra_arguments
+    unittest.main()

tests/python/render_layer/test_evaluation_render_settings_e.py
@@ -1,0 +1,39 @@
+# ############################################################
+# Importing - Same For All Render Layer Tests
+# ############################################################
+
+import unittest
+import os
+import sys
+
+from render_layer_common import *
+
+
+# ############################################################
+# Testing
+# ############################################################
+
+class UnitTesting(RenderLayerTesting):
+    def test_render_settings(self):
+        """
+        See if the depsgraph evaluation is correct
+        """
+        clay = Clay()
+        self.assertEqual(clay.get('object', 'matcap_icon'), '01')
+        clay.set('grandma', 'matcap_icon', '04')
+        clay.set('kid', 'matcap_icon', '05')
+        self.assertEqual(clay.get('object', 'matcap_icon'), '05')
+
+
+# ############################################################
+# Main - Same For All Render Layer Tests
+# ############################################################
+
+if __name__ == '__main__':
+    import sys
+
+    extra_arguments = sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else []
+    sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 2:] if "--" in sys.argv else [])
+
+    UnitTesting._extra_arguments = extra_arguments
+    unittest.main()

tests/python/render_layer/test_evaluation_render_settings_f.py
@@ -1,0 +1,40 @@
+# ############################################################
+# Importing - Same For All Render Layer Tests
+# ############################################################
+
+import unittest
+import os
+import sys
+
+from render_layer_common import *
+
+
+# ############################################################
+# Testing
+# ############################################################
+
+class UnitTesting(RenderLayerTesting):
+    def test_render_settings(self):
+        """
+        See if the depsgraph evaluation is correct
+        """
+        clay = Clay(extra_kid_layer=True)
+        self.assertEqual(clay.get('object', 'matcap_icon'), '01')
+
+        clay.set('scene', 'matcap_icon', '05')
+        clay.set('extra', 'matcap_icon', '07')
+        self.assertEqual(clay.get('object', 'matcap_icon'), '07')
+
+
+# ############################################################
+# Main - Same For All Render Layer Tests
+# ############################################################
+
+if __name__ == '__main__':
+    import sys
+
+    extra_arguments = sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else []
+    sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 2:] if "--" in sys.argv else [])
+
+    UnitTesting._extra_arguments = extra_arguments
+    unittest.main()

tests/python/render_layer/test_evaluation_render_settings_g.py
@@ -1,0 +1,40 @@
+# ############################################################
+# Importing - Same For All Render Layer Tests
+# ############################################################
+
+import unittest
+import os
+import sys
+
+from render_layer_common import *
+
+
+# ############################################################
+# Testing
+# ############################################################
+
+class UnitTesting(RenderLayerTesting):
+    def test_render_settings(self):
+        """
+        See if the depsgraph evaluation is correct
+        """
+        clay = Clay(extra_kid_layer=True)
+        self.assertEqual(clay.get('object', 'matcap_icon'), '01')
+
+        clay.set('mom', 'matcap_icon', '02')
+        clay.set('extra', 'matcap_icon', '06')
+        self.assertEqual(clay.get('object', 'matcap_icon'), '06')
+
+
+# ############################################################
+# Main - Same For All Render Layer Tests
+# ############################################################
+
+if __name__ == '__main__':
+    import sys
+
+    extra_arguments = sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else []
+    sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 2:] if "--" in sys.argv else [])
+
+    UnitTesting._extra_arguments = extra_arguments
+    unittest.main()