53c56dcd080dccc041096d72a7fca837994ae386
Mike Erwin <significant.bit@gmail.com>
2017-04-18 12:08:59 -0400

    Gawain: enable 10.10.10 vertex format for core profile (any platform)
    This is an OpenGL 3.3 feature, currently being used for surface normals. It's 1/3 the size of floating point triples and perfect for normals or colors.
    
    See T49012 for why Windows can use this feature regardless of core vs compatibility.

--- intern/gawain/gawain/vertex_format.h
+++ intern/gawain/gawain/vertex_format.h
@@ -17,7 +17,7 @@
 #define AVG_VERTEX_ATTRIB_NAME_LEN 5
 #define VERTEX_ATTRIB_NAMES_BUFFER_LEN ((AVG_VERTEX_ATTRIB_NAME_LEN + 1) * MAX_VERTEX_ATTRIBS)
 
-#if defined(_WIN32)
+#if defined(WITH_GL_PROFILE_CORE) || defined(_WIN32)
   // (GLEW_VERSION_3_3 || GLEW_ARB_vertex_type_2_10_10_10_rev)
   //   ^-- this is only guaranteed on Windows right now, will be true on all platforms soon
   #define USE_10_10_10 1
