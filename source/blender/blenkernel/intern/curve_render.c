09bf8f4a69603ad4faefc2c5777a3b485680ad3a
Campbell Barton <ideasman42@gmail.com>
2017-04-20 18:56:53 +1000

    Use reflect for calculating second normal

--- source/blender/blenkernel/intern/curve_render.c
+++ source/blender/blenkernel/intern/curve_render.c
@@ -560,17 +560,14 @@ static VertexBuffer *curve_batch_cache_get_normal_verts(CurveRenderData *rdata, 
 				vec_a[1] = 0.0f;
 				vec_a[2] = 0.0f;
 
-				vec_b[0] = -fac;
-				vec_b[1] = 0.0f;
-				vec_b[2] = 0.0f;
-
 				mul_qt_v3(bevp->quat, vec_a);
-				mul_qt_v3(bevp->quat, vec_b);
+				madd_v3_v3fl(vec_a, bevp->dir, -fac);
+
+				reflect_v3_v3v3(vec_b, vec_a, bevp->dir);
+				negate_v3(vec_b);
+
 				add_v3_v3(vec_a, bevp->vec);
 				add_v3_v3(vec_b, bevp->vec);
-
-				madd_v3_v3fl(vec_a, bevp->dir, -fac);
-				madd_v3_v3fl(vec_b, bevp->dir, -fac);
 
 				VertexBuffer_set_attrib(vbo, pos_id, vbo_len_used++, vec_a);
 				VertexBuffer_set_attrib(vbo, pos_id, vbo_len_used++, bevp->vec);
