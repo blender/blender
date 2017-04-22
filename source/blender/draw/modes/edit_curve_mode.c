23c4f01fb48f087b64c6ac3593d8b188df4b77b0
Campbell Barton <ideasman42@gmail.com>
2017-04-23 03:28:53 +1000

    Draw Manager: hide 2d curves normals

--- source/blender/draw/modes/edit_curve_mode.c
+++ source/blender/draw/modes/edit_curve_mode.c
@@ -227,7 +227,7 @@ static void EDIT_CURVE_cache_populate(void *vedata, Object *ob)
 			geom = DRW_cache_curve_edge_wire_get(ob);
 			DRW_shgroup_call_add(stl->g_data->wire_shgrp, geom, ob->obmat);
 
-			if ((cu->drawflag & CU_HIDE_NORMALS) == 0) {
+			if ((cu->flag & CU_3D) && (cu->drawflag & CU_HIDE_NORMALS) == 0) {
 				geom = DRW_cache_curve_edge_normal_get(ob, scene->toolsettings->normalsize);
 				DRW_shgroup_call_add(stl->g_data->wire_shgrp, geom, ob->obmat);
 			}
