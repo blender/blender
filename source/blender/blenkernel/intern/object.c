b180900e52a5e662faf354d6343cb2ad620d9866
Bastien Montagne <montagne29@wanadoo.fr>
2017-05-04 15:08:05 +0200

    Fix missing usercount update of poselib when copying Object.
    Still finding such missing cases... :(

--- source/blender/blenkernel/intern/object.c
+++ source/blender/blenkernel/intern/object.c
@@ -1145,6 +1145,7 @@ Object *BKE_object_copy_ex(Main *bmain, Object *ob, bool copy_caches)
 
 	/* increase user numbers */
 	id_us_plus((ID *)obn->data);
+	id_us_plus((ID *)obn->poselib);
 	id_us_plus((ID *)obn->gpd);
 	id_us_plus((ID *)obn->dup_group);
 
