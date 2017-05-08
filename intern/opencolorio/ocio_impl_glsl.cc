883c62d5d3c896b44d8e9503629c1079654fb284
Sergey Sharybin <sergey.vfx@gmail.com>
2017-05-08 16:40:48 +0200

    Add comment about vertex format created in OCIO

--- intern/opencolorio/ocio_impl_glsl.cc
+++ intern/opencolorio/ocio_impl_glsl.cc
@@ -416,7 +416,14 @@ bool OCIOImpl::setupGLSLDraw(OCIO_GLSLDrawState **state_r, OCIO_ConstProcessorRc
 
 		glActiveTexture(GL_TEXTURE0);
 
-		/* IMM needs vertex format even if we don't draw with it */
+		/* IMM needs vertex format even if we don't draw with it.
+		 *
+		 * NOTE: The only reason why it's here is because of Cycles viewport.
+		 * All other areas are managing their own vertex formats.
+		 * Doing it here is probably harmless, but kind of stupid.
+		 *
+		 * TODO(sergey): Look into some nicer solution.
+		 */
 		VertexFormat *format = immVertexFormat();
 		VertexFormat_add_attrib(format, "pos", COMP_F32, 2, KEEP_FLOAT);
 		VertexFormat_add_attrib(format, "texCoord", COMP_F32, 2, KEEP_FLOAT);
