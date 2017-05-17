c8e96a8b6bf511757887875390c592891ee8b496
Dalai Felinto <dfelinto@gmail.com>
2017-05-17 10:35:36 +0200

    Fixup on weightpaint shader after rBfabd55a5e9

--- source/blender/draw/modes/shaders/paint_wire_frag.glsl
+++ source/blender/draw/modes/shaders/paint_wire_frag.glsl
@@ -1,9 +1,16 @@
 
-flat in int finalSelect;
+flat in int finalFlag;
 out vec4 fragColor;
+
+#define VERTEX_SELECTED (1 << 0)
+#define VERTEX_HIDE     (1 << 4)
 
 void main()
 {
+	if (bool(finalFlag & VERTEX_HIDE)) {
+		discard;
+	}
+
 #ifdef VERTEX_MODE
 	vec4 colSel = colorEdgeSelect;
 	colSel.rgb = clamp(colSel.rgb - 0.2, 0.0, 1.0);
@@ -13,5 +20,5 @@ void main()
 
 	const vec4 colUnsel = vec4(0.5, 0.5, 0.5, 1.0);
 
-	fragColor = bool(finalSelect) ? colSel : colUnsel;
+	fragColor = bool(finalFlag & VERTEX_SELECTED) ? colSel : colUnsel;
 }
