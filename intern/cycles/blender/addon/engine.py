00212683116dce1f335cafb83a42980eeb0cf406
Lukas Stockner <lukas.stockner@freenet.de>
2017-05-26 02:15:09 +0200

    Cycles: Cleanup: Remove semicolons from line endings in Python code

--- intern/cycles/blender/addon/engine.py
+++ intern/cycles/blender/addon/engine.py
@@ -240,13 +240,13 @@ def register_passes(engine, scene, srl):
     if crl.pass_debug_ray_bounces:             engine.register_pass(scene, srl, "Debug Ray Bounces",             1, "X", 'VALUE')
 
     if crl.use_denoising and crl.denoising_store_passes:
-        engine.register_pass(scene, srl, "Denoising Normal",          3, "XYZ", 'VECTOR');
-        engine.register_pass(scene, srl, "Denoising Normal Variance", 3, "XYZ", 'VECTOR');
-        engine.register_pass(scene, srl, "Denoising Albedo",          3, "RGB", 'COLOR');
-        engine.register_pass(scene, srl, "Denoising Albedo Variance", 3, "RGB", 'COLOR');
-        engine.register_pass(scene, srl, "Denoising Depth",           1, "Z",   'VALUE');
-        engine.register_pass(scene, srl, "Denoising Depth Variance",  1, "Z",   'VALUE');
-        engine.register_pass(scene, srl, "Denoising Shadow A",        3, "XYV", 'VECTOR');
-        engine.register_pass(scene, srl, "Denoising Shadow B",        3, "XYV", 'VECTOR');
-        engine.register_pass(scene, srl, "Denoising Image",           3, "RGB", 'COLOR');
-        engine.register_pass(scene, srl, "Denoising Image Variance",  3, "RGB", 'COLOR');
+        engine.register_pass(scene, srl, "Denoising Normal",          3, "XYZ", 'VECTOR')
+        engine.register_pass(scene, srl, "Denoising Normal Variance", 3, "XYZ", 'VECTOR')
+        engine.register_pass(scene, srl, "Denoising Albedo",          3, "RGB", 'COLOR')
+        engine.register_pass(scene, srl, "Denoising Albedo Variance", 3, "RGB", 'COLOR')
+        engine.register_pass(scene, srl, "Denoising Depth",           1, "Z",   'VALUE')
+        engine.register_pass(scene, srl, "Denoising Depth Variance",  1, "Z",   'VALUE')
+        engine.register_pass(scene, srl, "Denoising Shadow A",        3, "XYV", 'VECTOR')
+        engine.register_pass(scene, srl, "Denoising Shadow B",        3, "XYV", 'VECTOR')
+        engine.register_pass(scene, srl, "Denoising Image",           3, "RGB", 'COLOR')
+        engine.register_pass(scene, srl, "Denoising Image Variance",  3, "RGB", 'COLOR')
