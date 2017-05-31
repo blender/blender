8b0f968a312e4293349c91d1d50621d120b5c899
Bastien Montagne <montagne29@wanadoo.fr>
2017-05-31 21:24:41 +0200

    Fix bad handling of 'extra' user for groups at their creation.
    Was just keeping the default '1' user from `BKE_libblock_alloc()`,
    instead of using correct way to handle extra virtual user needed when we
    want to keep unused datablocks around...

--- source/blender/blenkernel/intern/group.c
+++ source/blender/blenkernel/intern/group.c
@@ -80,6 +80,8 @@ Group *BKE_group_add(Main *bmain, const char *name)
 	Group *group;
 	
 	group = BKE_libblock_alloc(bmain, ID_GR, name);
+	id_us_min(&group->id);
+	id_us_ensure_real(&group->id);
 	group->layer = (1 << 20) - 1;
 
 	group->preview = NULL;
