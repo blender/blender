618aef1e5820faca452dc86169430120db236ef1
Clément Foucault <foucault.clem@gmail.com>
2017-06-07 11:13:56 +0200

    Eevee: Fix normal orientation on default shader.

--- source/blender/draw/engines/eevee/shaders/default_frag.glsl
+++ source/blender/draw/engines/eevee/shaders/default_frag.glsl
@@ -11,5 +11,5 @@ void main()
 	vec3 dielectric = vec3(0.034) * specular * 2.0;
 	vec3 diffuse = mix(basecol, vec3(0.0), metallic);
 	vec3 f0 = mix(dielectric, basecol, metallic);
-	FragColor = vec4(eevee_surface_lit(worldNormal, diffuse, f0, roughness, 1.0), 1.0);
+	FragColor = vec4(eevee_surface_lit((gl_FrontFacing) ? worldNormal : -worldNormal, diffuse, f0, roughness, 1.0), 1.0);
 }
