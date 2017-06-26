37593b6a4242c8a31ffd6ce82b861b2097769ccf
Dalai Felinto <dfelinto@gmail.com>
2017-06-26 12:47:17 +0200

    New scenes should use Eevee, not Blender Render

--- source/blender/blenkernel/intern/scene.c
+++ source/blender/blenkernel/intern/scene.c
@@ -818,7 +818,7 @@ void BKE_scene_init(Scene *sce)
 	sce->r.ffcodecdata.audio_bitrate = 192;
 	sce->r.ffcodecdata.audio_channels = 2;
 
-	BLI_strncpy(sce->r.engine, RE_engine_id_BLENDER_RENDER, sizeof(sce->r.engine));
+	BLI_strncpy(sce->r.engine, RE_engine_id_BLENDER_EEVEE, sizeof(sce->r.engine));
 
 	sce->audio.distance_model = 2.0f;
 	sce->audio.doppler_factor = 1.0f;
