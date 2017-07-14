78e2238fe4c03e8beeb06ed801aa2e30c62172dd
Dalai Felinto <dfelinto@gmail.com>
2017-07-14 11:44:26 +0200

    Revert "Fix T51794: Crash on hair file when opening with no UI"
    This reverts commit 0095f4f83465bc9fdc13b79d2126f5623e291d7c.

--- source/blender/blenkernel/intern/blendfile.c
+++ source/blender/blenkernel/intern/blendfile.c
@@ -332,18 +332,7 @@ static void setup_app_data(
 			}
 		}
 	}
-
-	if (mode == LOAD_UI_OFF && BLI_listbase_is_empty(&G.main->wm)) {
-		/* XXX prevent crash in pdInitEffectors called through DEG_scene_relations_rebuild (see T51794).
-		 * Can be removed once BKE_scene_layer_context_active_ex_PLACEHOLDER gets
-		 * replaced by BKE_scene_layer_from_workspace_get. */
-		BLI_addhead(&G.main->wm, CTX_wm_manager(C));
-		BKE_scene_set_background(G.main, curscene);
-		BLI_listbase_clear(&G.main->wm);
-	}
-	else {
-		BKE_scene_set_background(G.main, curscene);
-	}
+	BKE_scene_set_background(G.main, curscene);
 
 	if (mode != LOAD_UNDO) {
 		RE_FreeAllPersistentData();
