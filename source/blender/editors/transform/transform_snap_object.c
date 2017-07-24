7fbd529fb5d081443709b50caebeba875aba19b8
mano-wii <germano.costa@ig.com.br>
2017-07-24 12:08:23 -0300

    Revert "ransform: fix unreported bug: geometry hidden by clipping border (alt + b) is not bypassed in snap to faces"
    This reverts commit 7f09b55d01c248a741e967af597b7519f095983b.

--- source/blender/editors/transform/transform_snap_object.c
+++ source/blender/editors/transform/transform_snap_object.c
@@ -2408,12 +2408,10 @@ bool ED_transform_snap_object_project_view3d_ex(
 		return false;
 	}
 
-	float ray_end_dist = depth_get(ray_end, ray_start, ray_normal);
+	float ray_depth_fallback;
 	if (ray_depth == NULL) {
-		ray_depth = &ray_end_dist;
-	}
-	else if (*ray_depth > ray_end_dist) {
-		*ray_depth = ray_end_dist;
+		ray_depth_fallback = BVH_RAYCAST_DIST_MAX;
+		ray_depth = &ray_depth_fallback;
 	}
 
 	if (snap_to == SCE_SNAP_MODE_FACE) {
