3b57504c0907bdecf748ce390077a1a64896f991
Campbell Barton <ideasman42@gmail.com>
2017-08-03 20:05:43 +1000

    error in last commit

--- source/blender/editors/manipulator_library/manipulator_types/cage2d_manipulator.c
+++ source/blender/editors/manipulator_library/manipulator_types/cage2d_manipulator.c
@@ -537,7 +537,7 @@ static void manipulator_rect_transform_modal(
 	}
 	else if (mpr->highlight_part == ED_MANIPULATOR_RECT_TRANSFORM_INTERSECT_SCALEY_UP) {
 		int a = (transform_flag & ED_MANIPULATOR_RECT_TRANSFORM_FLAG_SCALE_UNIFORM) ? 0 : 1;
-		valuey = max_ff(valuey, dims[0] * data->orig_scale[a] / (pivot_center ? -2 : -1));
+		valuey = max_ff(valuey, dims[1] * data->orig_scale[a] / (pivot_center ? -2 : -1));
 		if (pivot_center == false) {
 			mpr->matrix_offset[3][1] = data->orig_offset[1] + valuey / 2.0f;
 		}
