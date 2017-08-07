518d6906287947fd92388bd6438f6552177e051c
Brecht Van Lommel <brechtvanlommel@gmail.com>
2017-08-07 22:42:47 +0200

    DPI: add back option to control line width, tweak default width.
    Adds thin/default/thick modes to add -1/0/1 to the auto detected line width,
    while leaving the overall UI scale unchanged.
    
    Also tweaks the default line width threshold, so thicker lines start from
    slightly high UI scales.
    
    Differential Revision: https://developer.blender.org/D2778

--- source/blender/makesdna/DNA_userdef_types.h
+++ source/blender/makesdna/DNA_userdef_types.h
@@ -473,7 +473,7 @@ typedef struct UserDef {
 	int scrollback;     /* console scrollback limit */
 	int dpi;            /* range 48-128? */
 	float ui_scale;     /* interface scale */
-	int pad1;
+	int ui_line_width;  /* interface line width */
 	char node_margin;   /* node insert offset (aka auto-offset) margin, but might be useful for later stuff as well */
 	char pad2;
 	short transopts;    /* eUserpref_Translation_Flags */
