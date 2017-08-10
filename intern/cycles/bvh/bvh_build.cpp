422fddab87aa2c3b7c27f7e6dd1887087e778277
Sergey Sharybin <sergey.vfx@gmail.com>
2017-08-10 09:22:33 +0200

    Cycles: Fix instanced shadow catcher objects influencing each other

--- intern/cycles/bvh/bvh_build.cpp
+++ intern/cycles/bvh/bvh_build.cpp
@@ -865,7 +865,7 @@ BVHNode *BVHBuild::create_object_leaf_nodes(const BVHReference *ref, int start, 
 			prim_time[start] = make_float2(ref->time_from(), ref->time_to());
 		}
 
-		uint visibility = objects[ref->prim_object()]->visibility;
+		const uint visibility = objects[ref->prim_object()]->visibility_for_tracing();
 		BVHNode *leaf_node =  new LeafNode(ref->bounds(), visibility, start, start+1);
 		leaf_node->time_from = ref->time_from();
 		leaf_node->time_to = ref->time_to();
@@ -939,7 +939,7 @@ BVHNode* BVHBuild::create_leaf_node(const BVHRange& range,
 			                                         ref.time_to()));
 
 			bounds[type_index].grow(ref.bounds());
-			visibility[type_index] |= objects[ref.prim_object()]->visibility;
+			visibility[type_index] |= objects[ref.prim_object()]->visibility_for_tracing();
 			if(ref.prim_type() & PRIMITIVE_ALL_CURVE) {
 				visibility[type_index] |= PATH_RAY_CURVE;
 			}
