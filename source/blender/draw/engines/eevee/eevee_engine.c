00d10977c88125016285a985fa3362d6bea23e4b
Clément Foucault <foucault.clem@gmail.com>
2017-08-11 12:59:32 +0200

    Eevee: SSR: Change clamp default.

--- source/blender/draw/engines/eevee/eevee_engine.c
+++ source/blender/draw/engines/eevee/eevee_engine.c
@@ -257,7 +257,7 @@ static void EEVEE_scene_layer_settings_create(RenderEngine *UNUSED(engine), IDPr
 	BKE_collection_engine_property_add_float(props, "ssr_max_roughness", 0.5f);
 	BKE_collection_engine_property_add_float(props, "ssr_thickness", 0.2f);
 	BKE_collection_engine_property_add_float(props, "ssr_border_fade", 0.075f);
-	BKE_collection_engine_property_add_float(props, "ssr_firefly_fac", 0.0f);
+	BKE_collection_engine_property_add_float(props, "ssr_firefly_fac", 10.0f);
 
 	BKE_collection_engine_property_add_bool(props, "volumetric_enable", false);
 	BKE_collection_engine_property_add_float(props, "volumetric_start", 0.1f);
