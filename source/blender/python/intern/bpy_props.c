f09dee5aed35ede00bdbaedba171ce24edf5e0b0
Campbell Barton <ideasman42@gmail.com>
2017-08-23 02:14:33 +1000

    Fix error in PointerProperty argument list
    Regression in a7b3047

--- source/blender/python/intern/bpy_props.c
+++ source/blender/python/intern/bpy_props.c
@@ -2880,7 +2880,7 @@ PyObject *BPy_PointerProperty(PyObject *self, PyObject *args, PyObject *kw)
 		PyObject *update_cb = NULL, *poll_cb = NULL;
 
 		if (!PyArg_ParseTupleAndKeywords(args, kw,
-		                                 "s#O|ssO!OOO:PointerProperty",
+		                                 "s#O|ssO!OO:PointerProperty",
 		                                 (char **)kwlist, &id, &id_len,
 		                                 &type, &name, &description,
 		                                 &PySet_Type, &pyopts,
