b07e983b613b9dd61262d4ff45908c33b0b9e8a1
Campbell Barton <ideasman42@gmail.com>
2017-09-05 00:22:31 +1000

    PyAPI: Add bpy.utils.register_classes_factory
    Avoids having boiler plate register/unregister functions.

--- release/scripts/modules/bpy/utils/__init__.py
+++ release/scripts/modules/bpy/utils/__init__.py
@@ -37,6 +37,7 @@ __all__ = (
     "register_module",
     "register_manual_map",
     "unregister_manual_map",
+    "register_classes_factory",
     "register_submodule_factory",
     "make_rna_paths",
     "manual_map",
@@ -700,6 +701,24 @@ def unregister_module(module, verbose=False):
             traceback.print_exc()
     if verbose:
         print("done.\n")
+
+
+def register_classes_factory(classes):
+    """
+    Utility function to create register and unregister functions
+    which simply registers and unregisters a sequence of classes.
+    """
+    def register():
+        from bpy.utils import register_class
+        for cls in classes:
+            register_class(cls)
+
+    def unregister():
+        from bpy.utils import unregister_class
+        for cls in reversed(classes):
+            unregister_class(cls)
+
+    return register, unregister
 
 
 def register_submodule_factory(module_name, submodule_names):
