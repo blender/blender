ee30a4381f8989ed9f39b4baae9f74e45c9dcdc9
Stefan Werner <stewreo@gmail.com>
2017-09-20 21:47:37 +0200

    Added extra "const" to satisfy the strict clang version in Xcode 9

--- intern/itasc/kdl/tree.hpp
+++ intern/itasc/kdl/tree.hpp
@@ -34,7 +34,7 @@ namespace KDL
     //Forward declaration
     class TreeElement;
     // Eigen allocator is needed for alignment of Eigen data types
-    typedef std::map<std::string,TreeElement, std::less<std::string>, Eigen::aligned_allocator<std::pair<std::string, TreeElement> > > SegmentMap;
+    typedef std::map<std::string,TreeElement, std::less<std::string>, Eigen::aligned_allocator<std::pair<const std::string, TreeElement> > > SegmentMap;
 
     class TreeElement
     {
