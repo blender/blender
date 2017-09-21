02cd48a087d3aa21b210c70d34a610685c62dba0
Clément Foucault <foucault.clem@gmail.com>
2017-09-22 00:40:31 +0200

    Edit Mode : Bias Face dot position

--- source/blender/draw/modes/shaders/edit_mesh_overlay_facedot_vert.glsl
+++ source/blender/draw/modes/shaders/edit_mesh_overlay_facedot_vert.glsl
@@ -9,6 +9,8 @@ flat out int isSelected;
 void main()
 {
 	gl_Position = ModelViewProjectionMatrix * vec4(pos, 1.0);
+	/* Bias Facedot Z position in clipspace. */
+	gl_Position.z -= 0.0002;
 	gl_PointSize = sizeFaceDot;
 	isSelected = int(norAndFlag.w);
 }
