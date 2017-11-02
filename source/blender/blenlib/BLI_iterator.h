e0a6582b4008e5cb5757e2af7b3f8557ff53adff
Sergey Sharybin <sergey.vfx@gmail.com>
2017-11-02 17:54:06 +0100

    Fix iterator usage from C++ code
    Old code was not supported by GCC, see some details and progress
    
      https://gcc.gnu.org/bugzilla/show_bug.cgi?id=55606

--- source/blender/blenlib/BLI_iterator.h
+++ source/blender/blenlib/BLI_iterator.h
@@ -41,9 +41,8 @@ typedef void (*IteratorBeginCb)(BLI_Iterator *iter, void *data_in);
 {                                                                                    \
 	_type _instance;                                                                 \
 	IteratorCb callback_end_func = callback_end;                                     \
-	BLI_Iterator iter_macro = {                                                      \
-		.skip = false,                                                               \
-	};                                                                               \
+	BLI_Iterator iter_macro;                                                         \
+	iter_macro.skip = false;                                                         \
 	for (callback_begin(&iter_macro, (_data_in));                                    \
 	     iter_macro.valid;                                                           \
 	     callback_next(&iter_macro))                                                 \
