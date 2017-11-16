blender/blender

commit 087331c495b04ebd37903c0dc0e46262354cf026

Cycles: Replace __MAX_CLOSURE__ build option with runtime integrator variable
Goal is to reduce OpenCL kernel recompilations.

Currently viewport renders are still set to use 64 closures as this seems to
be faster and we don't want to cause a performance regression there. Needs
to be investigated.

Reviewed By: brecht

Differential Revision: https://developer.blender.org/D2775

intern/cycles/kernel/split/kernel_branched.h
@@ -30,9 +30,13 @@ ccl_device_inline void kernel_split_branched_path_indirect_loop_init(KernelGloba
 	BRANCHED_STORE(path_state);
 	BRANCHED_STORE(throughput);
 	BRANCHED_STORE(ray);
-	BRANCHED_STORE(sd);
 	BRANCHED_STORE(isect);
 	BRANCHED_STORE(ray_state);
+
+	branched_state->sd = *kernel_split_sd(sd, ray_index);
+	for(int i = 0; i < branched_state->sd.num_closure; i++) {
+		branched_state->sd.closure[i] = kernel_split_sd(sd, ray_index)->closure[i];
+	}
 
 #undef BRANCHED_STORE
 
@@ -53,9 +57,13 @@ ccl_device_inline void kernel_split_branched_path_indirect_loop_end(KernelGlobal
 	BRANCHED_RESTORE(path_state);
 	BRANCHED_RESTORE(throughput);
 	BRANCHED_RESTORE(ray);
-	BRANCHED_RESTORE(sd);
 	BRANCHED_RESTORE(isect);
 	BRANCHED_RESTORE(ray_state);
+
+	*kernel_split_sd(sd, ray_index) = branched_state->sd;
+	for(int i = 0; i < branched_state->sd.num_closure; i++) {
+		kernel_split_sd(sd, ray_index)->closure[i] = branched_state->sd.closure[i];
+	}
 
 #undef BRANCHED_RESTORE
 

intern/cycles/kernel/split/kernel_next_iteration_setup.h
@@ -58,7 +58,7 @@ ccl_device void kernel_split_branched_indirect_light_end(KernelGlobals *kg, int 
 	kernel_split_branched_path_indirect_loop_end(kg, ray_index);
 
 	ccl_global float3 *throughput = &kernel_split_state.throughput[ray_index];
-	ShaderData *sd = &kernel_split_state.sd[ray_index];
+	ShaderData *sd = kernel_split_sd(sd, ray_index);
 	ccl_global PathState *state = &kernel_split_state.path_state[ray_index];
 	ccl_global Ray *ray = &kernel_split_state.ray[ray_index];
 
@@ -126,7 +126,7 @@ ccl_device void kernel_next_iteration_setup(KernelGlobals *kg,
 	if(active) {
 		ccl_global float3 *throughput = &kernel_split_state.throughput[ray_index];
 		ccl_global Ray *ray = &kernel_split_state.ray[ray_index];
-		ShaderData *sd = &kernel_split_state.sd[ray_index];
+		ShaderData *sd = kernel_split_sd(sd, ray_index);
 		ccl_global PathState *state = &kernel_split_state.path_state[ray_index];
 		PathRadiance *L = &kernel_split_state.path_radiance[ray_index];
 

intern/cycles/kernel/split/kernel_split_data.h
@@ -31,6 +31,14 @@ ccl_device_inline uint64_t split_data_buffer_size(KernelGlobals *kg, size_t num_
 	size = size SPLIT_DATA_ENTRIES;
 #undef SPLIT_DATA_ENTRY
 
+	uint64_t closure_size = sizeof(ShaderClosure) * (kernel_data.integrator.max_closures-1);
+
+#ifdef __BRANCHED_PATH__
+	size += align_up(closure_size * num_elements, 16);
+#endif
+
+	size += align_up(num_elements * (sizeof(ShaderData) + closure_size), 16);
+
 	return size;
 }
 
@@ -48,6 +56,15 @@ ccl_device_inline void split_data_init(KernelGlobals *kg,
 	split_data->name = (type*)p; p += align_up(num_elements * num * sizeof(type), 16);
 	SPLIT_DATA_ENTRIES;
 #undef SPLIT_DATA_ENTRY
+
+	uint64_t closure_size = sizeof(ShaderClosure) * (kernel_data.integrator.max_closures-1);
+
+#ifdef __BRANCHED_PATH__
+	p += align_up(closure_size * num_elements, 16);
+#endif
+
+	split_data->_sd = (ShaderData*)p;
+	p += align_up(num_elements * (sizeof(ShaderData) + closure_size), 16);
 
 	split_data->ray_state = ray_state;
 }

intern/cycles/kernel/split/kernel_split_data_types.h
@@ -51,7 +51,6 @@ typedef ccl_global struct SplitBranchedState {
 	float3 throughput;
 	Ray ray;
 
-	struct ShaderData sd;
 	Intersection isect;
 
 	char ray_state;
@@ -77,6 +76,9 @@ typedef ccl_global struct SplitBranchedState {
 	int shared_sample_count; /* number of branched samples shared with other threads */
 	int original_ray; /* index of original ray when sharing branched samples */
 	bool waiting_on_shared_samples;
+
+	/* Must be last in to allow for dynamic size of closures */
+	struct ShaderData sd;
 } SplitBranchedState;
 
 #define SPLIT_DATA_BRANCHED_ENTRIES \
@@ -110,11 +112,11 @@ typedef ccl_global struct SplitBranchedState {
 	SPLIT_DATA_ENTRY(ccl_global Ray, light_ray, 1) \
 	SPLIT_DATA_ENTRY(ccl_global int, queue_data, (NUM_QUEUES*2)) /* TODO(mai): this is too large? */ \
 	SPLIT_DATA_ENTRY(ccl_global uint, buffer_offset, 1) \
-	SPLIT_DATA_ENTRY(ShaderData, sd, 1) \
 	SPLIT_DATA_ENTRY(ShaderDataTinyStorage, sd_DL_shadow, 1) \
 	SPLIT_DATA_SUBSURFACE_ENTRIES \
 	SPLIT_DATA_VOLUME_ENTRIES \
 	SPLIT_DATA_BRANCHED_ENTRIES \
+	SPLIT_DATA_ENTRY(ShaderData, _sd, 0)
 
 /* entries to be copied to inactive rays when sharing branched samples (TODO: which are actually needed?) */
 #define SPLIT_DATA_ENTRIES_BRANCHED_SHARED \
@@ -126,11 +128,11 @@ typedef ccl_global struct SplitBranchedState {
 	SPLIT_DATA_ENTRY(ccl_global BsdfEval, bsdf_eval, 1) \
 	SPLIT_DATA_ENTRY(ccl_global int, is_lamp, 1) \
 	SPLIT_DATA_ENTRY(ccl_global Ray, light_ray, 1) \
-	SPLIT_DATA_ENTRY(ShaderData, sd, 1) \
 	SPLIT_DATA_ENTRY(ShaderDataTinyStorage, sd_DL_shadow, 1) \
 	SPLIT_DATA_SUBSURFACE_ENTRIES \
 	SPLIT_DATA_VOLUME_ENTRIES \
 	SPLIT_DATA_BRANCHED_ENTRIES \
+	SPLIT_DATA_ENTRY(ShaderData, _sd, 0)
 
 /* struct that holds pointers to data in the shared state buffer */
 typedef struct SplitData {
@@ -153,6 +155,12 @@ __device__ SplitData __split_data;
 __device__ SplitParams __split_param_data;
 #  define kernel_split_params (__split_param_data)
 #endif  /* __KERNEL_CUDA__ */
+
+#define kernel_split_sd(sd, ray_index) ((ShaderData*) \
+	( \
+		((ccl_global char*)kernel_split_state._##sd) + \
+		(sizeof(ShaderData) + sizeof(ShaderClosure)*(kernel_data.integrator.max_closures-1)) * (ray_index) \
+	))
 
 /* Local storage for queue_enqueue kernel. */
 typedef struct QueueEnqueueLocals {

intern/cycles/kernel/split/kernel_subsurface_scatter.h
@@ -98,7 +98,7 @@ ccl_device_noinline bool kernel_split_branched_path_subsurface_indirect_light_it
 
 			/* compute lighting with the BSDF closure */
 			for(int hit = branched_state->next_hit; hit < branched_state->num_hits; hit++) {
-				ShaderData *bssrdf_sd = &kernel_split_state.sd[ray_index];
+				ShaderData *bssrdf_sd = kernel_split_sd(sd, ray_index);
 				*bssrdf_sd = *sd; /* note: copy happens each iteration of inner loop, this is
 				                   * important as the indirect path will write into bssrdf_sd */
 
@@ -228,7 +228,7 @@ ccl_device void kernel_subsurface_scatter(KernelGlobals *kg)
 		ccl_global Ray *ray = &kernel_split_state.ray[ray_index];
 		ccl_global float3 *throughput = &kernel_split_state.throughput[ray_index];
 		ccl_global SubsurfaceIndirectRays *ss_indirect = &kernel_split_state.ss_rays[ray_index];
-		ShaderData *sd = &kernel_split_state.sd[ray_index];
+		ShaderData *sd = kernel_split_sd(sd, ray_index);
 		ShaderData *emission_sd = AS_SHADER_DATA(&kernel_split_state.sd_DL_shadow[ray_index]);
 
 		if(sd->flag & SD_BSSRDF) {

intern/cycles/device/device.cpp
@@ -47,7 +47,6 @@ std::ostream& operator <<(std::ostream &os,
 {
 	os << "Experimental features: "
 	   << (requested_features.experimental ? "On" : "Off") << std::endl;
-	os << "Max closure count: " << requested_features.max_closure << std::endl;
 	os << "Max nodes group: " << requested_features.max_nodes_group << std::endl;
 	/* TODO(sergey): Decode bitflag into list of names. */
 	os << "Nodes features: " << requested_features.nodes_features << std::endl;

intern/cycles/device/device.h
@@ -91,9 +91,6 @@ public:
 	/* Use experimental feature set. */
 	bool experimental;
 
-	/* Maximum number of closures in shader trees. */
-	int max_closure;
-
 	/* Selective nodes compilation. */
 
 	/* Identifier of a node group up to which all the nodes needs to be
@@ -146,7 +143,6 @@ public:
 	{
 		/* TODO(sergey): Find more meaningful defaults. */
 		experimental = false;
-		max_closure = 0;
 		max_nodes_group = 0;
 		nodes_features = 0;
 		use_hair = false;
@@ -167,7 +163,6 @@ public:
 	bool modified(const DeviceRequestedFeatures& requested_features)
 	{
 		return !(experimental == requested_features.experimental &&
-		         max_closure == requested_features.max_closure &&
 		         max_nodes_group == requested_features.max_nodes_group &&
 		         nodes_features == requested_features.nodes_features &&
 		         use_hair == requested_features.use_hair &&
@@ -198,7 +193,6 @@ public:
 			string_printf("%d", max_nodes_group);
 		build_options += " -D__NODES_FEATURES__=" +
 			string_printf("%d", nodes_features);
-		build_options += string_printf(" -D__MAX_CLOSURE__=%d", max_closure);
 		if(!use_hair) {
 			build_options += " -D__NO_HAIR__";
 		}

intern/cycles/device/device_cpu.cpp
@@ -760,7 +760,6 @@ public:
 		CPUSplitKernel *split_kernel = NULL;
 		if(use_split_kernel) {
 			split_kernel = new CPUSplitKernel(this);
-			requested_features.max_closure = MAX_CLOSURE;
 			if(!split_kernel->load_kernels(requested_features)) {
 				thread_kernel_globals_free((KernelGlobals*)kgbuffer.device_pointer);
 				kgbuffer.free();

commit 470b4cb62f543263f0087a1e9d39b4b125752d23

Cycles: Fix crash with split branched path tracing
ShaderData memory was getting clobbered in the branched path code paths.

Was caused by 087331c495b04ebd37903c0dc0e46262354cf026

intern/cycles/kernel/split/kernel_branched.h
@@ -33,9 +33,9 @@ ccl_device_inline void kernel_split_branched_path_indirect_loop_init(KernelGloba
 	BRANCHED_STORE(isect);
 	BRANCHED_STORE(ray_state);
 
-	branched_state->sd = *kernel_split_sd(sd, ray_index);
-	for(int i = 0; i < branched_state->sd.num_closure; i++) {
-		branched_state->sd.closure[i] = kernel_split_sd(sd, ray_index)->closure[i];
+	*kernel_split_sd(branched_state_sd, ray_index) = *kernel_split_sd(sd, ray_index);
+	for(int i = 0; i < kernel_split_sd(branched_state_sd, ray_index)->num_closure; i++) {
+		kernel_split_sd(branched_state_sd, ray_index)->closure[i] = kernel_split_sd(sd, ray_index)->closure[i];
 	}
 
 #undef BRANCHED_STORE
@@ -60,9 +60,9 @@ ccl_device_inline void kernel_split_branched_path_indirect_loop_end(KernelGlobal
 	BRANCHED_RESTORE(isect);
 	BRANCHED_RESTORE(ray_state);
 
-	*kernel_split_sd(sd, ray_index) = branched_state->sd;
-	for(int i = 0; i < branched_state->sd.num_closure; i++) {
-		kernel_split_sd(sd, ray_index)->closure[i] = branched_state->sd.closure[i];
+	*kernel_split_sd(sd, ray_index) = *kernel_split_sd(branched_state_sd, ray_index);
+	for(int i = 0; i < kernel_split_sd(branched_state_sd, ray_index)->num_closure; i++) {
+		kernel_split_sd(sd, ray_index)->closure[i] = kernel_split_sd(branched_state_sd, ray_index)->closure[i];
 	}
 
 #undef BRANCHED_RESTORE
@@ -83,9 +83,16 @@ ccl_device_inline bool kernel_split_branched_indirect_start_shared(KernelGlobals
 	}
 
 #define SPLIT_DATA_ENTRY(type, name, num) \
-		kernel_split_state.name[inactive_ray] = kernel_split_state.name[ray_index];
+		if(num) { \
+			kernel_split_state.name[inactive_ray] = kernel_split_state.name[ray_index]; \
+		}
 	SPLIT_DATA_ENTRIES_BRANCHED_SHARED
 #undef SPLIT_DATA_ENTRY
+
+	*kernel_split_sd(sd, inactive_ray) = *kernel_split_sd(sd, ray_index);
+	for(int i = 0; i < kernel_split_sd(sd, ray_index)->num_closure; i++) {
+		kernel_split_sd(sd, inactive_ray)->closure[i] = kernel_split_sd(sd, ray_index)->closure[i];
+	}
 
 	kernel_split_state.branched_state[inactive_ray].shared_sample_count = 0;
 	kernel_split_state.branched_state[inactive_ray].original_ray = ray_index;

intern/cycles/kernel/split/kernel_next_iteration_setup.h
@@ -145,7 +145,7 @@ ccl_device void kernel_next_iteration_setup(KernelGlobals *kg,
 			if(kernel_split_branched_path_surface_indirect_light_iter(kg,
 			                                                          ray_index,
 			                                                          1.0f,
-			                                                          &kernel_split_state.branched_state[ray_index].sd,
+			                                                          kernel_split_sd(branched_state_sd, ray_index),
 			                                                          true,
 			                                                          true))
 			{
@@ -190,7 +190,7 @@ ccl_device void kernel_next_iteration_setup(KernelGlobals *kg,
 		if(kernel_split_branched_path_surface_indirect_light_iter(kg,
 		                                                          ray_index,
 		                                                          1.0f,
-		                                                          &kernel_split_state.branched_state[ray_index].sd,
+		                                                          kernel_split_sd(branched_state_sd, ray_index),
 		                                                          true,
 		                                                          true))
 		{

intern/cycles/kernel/split/kernel_split_data.h
@@ -34,7 +34,7 @@ ccl_device_inline uint64_t split_data_buffer_size(KernelGlobals *kg, size_t num_
 	uint64_t closure_size = sizeof(ShaderClosure) * (kernel_data.integrator.max_closures-1);
 
 #ifdef __BRANCHED_PATH__
-	size += align_up(closure_size * num_elements, 16);
+	size += align_up(num_elements * (sizeof(ShaderData) + closure_size), 16);
 #endif
 
 	size += align_up(num_elements * (sizeof(ShaderData) + closure_size), 16);
@@ -60,7 +60,8 @@ ccl_device_inline void split_data_init(KernelGlobals *kg,
 	uint64_t closure_size = sizeof(ShaderClosure) * (kernel_data.integrator.max_closures-1);
 
 #ifdef __BRANCHED_PATH__
-	p += align_up(closure_size * num_elements, 16);
+	split_data->_branched_state_sd = (ShaderData*)p;
+	p += align_up(num_elements * (sizeof(ShaderData) + closure_size), 16);
 #endif
 
 	split_data->_sd = (ShaderData*)p;

intern/cycles/kernel/split/kernel_split_data_types.h
@@ -76,13 +76,11 @@ typedef ccl_global struct SplitBranchedState {
 	int shared_sample_count; /* number of branched samples shared with other threads */
 	int original_ray; /* index of original ray when sharing branched samples */
 	bool waiting_on_shared_samples;
-
-	/* Must be last in to allow for dynamic size of closures */
-	struct ShaderData sd;
 } SplitBranchedState;
 
 #define SPLIT_DATA_BRANCHED_ENTRIES \
-	SPLIT_DATA_ENTRY( SplitBranchedState, branched_state, 1)
+	SPLIT_DATA_ENTRY( SplitBranchedState, branched_state, 1) \
+	SPLIT_DATA_ENTRY(ShaderData, _branched_state_sd, 0)
 #else
 #define SPLIT_DATA_BRANCHED_ENTRIES
 #endif  /* __BRANCHED_PATH__ */

intern/cycles/kernel/split/kernel_subsurface_scatter.h
@@ -37,7 +37,7 @@ ccl_device_noinline bool kernel_split_branched_path_subsurface_indirect_light_it
 {
 	SplitBranchedState *branched_state = &kernel_split_state.branched_state[ray_index];
 
-	ShaderData *sd = &branched_state->sd;
+	ShaderData *sd = kernel_split_sd(branched_state_sd, ray_index);
 	PathRadiance *L = &kernel_split_state.path_radiance[ray_index];
 	ShaderData *emission_sd = AS_SHADER_DATA(&kernel_split_state.sd_DL_shadow[ray_index]);
 