7a58ff928c21110d9798e0d94ed2be1de7400f90
Campbell Barton <ideasman42@gmail.com>
2017-12-19 13:59:18 +1100

    Math Lib: signed versions of quaternion angle
    There was no simple way to get the shortest quaternion angle.

--- source/blender/blenlib/BLI_math_rotation.h
+++ source/blender/blenlib/BLI_math_rotation.h
@@ -97,6 +97,11 @@ float angle_normalized_qtqt(const float q1[4], const float q2[4]);
 float angle_qt(const float q[4]);
 float angle_qtqt(const float q1[4], const float q2[4]);
 
+float angle_signed_normalized_qt(const float q[4]);
+float angle_signed_normalized_qtqt(const float q1[4], const float q2[4]);
+float angle_signed_qt(const float q[4]);
+float angle_signed_qtqt(const float q1[4], const float q2[4]);
+
 /* TODO: don't what this is, but it's not the same as mat3_to_quat */
 void mat3_to_quat_is_ok(float q[4], float mat[3][3]);
 
