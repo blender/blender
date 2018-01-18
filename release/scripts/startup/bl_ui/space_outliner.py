97e2b0c594a0ecca0ecd0391ed0b65399c177372
Dalai Felinto <dfelinto@gmail.com>
2018-01-18 18:22:13 -0200

    Outliner: Follow up on "Alphabetical sorting"
    Technicall this works for collections modes too (View Layer and Collections) even
    if only for objects. So it should be in the menu as well.

--- release/scripts/startup/bl_ui/space_outliner.py
+++ release/scripts/startup/bl_ui/space_outliner.py
@@ -93,9 +93,7 @@ class OUTLINER_MT_view(Menu):
         space = context.space_data
 
         if space.display_mode not in {'DATABLOCKS', 'USER_PREFERENCES', 'KEYMAPS'}:
-            if space.display_mode not in {'VIEW_LAYER', 'COLLECTIONS'}:
-                layout.prop(space, "use_sort_alpha")
-
+            layout.prop(space, "use_sort_alpha")
             layout.prop(space, "show_restrict_columns")
             layout.separator()
             layout.operator("outliner.show_active")
