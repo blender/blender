fa91b43e8cbe55179bfb783be4371e8bc50e9d74
Sergey Sharybin <sergey.vfx@gmail.com>
2018-01-19 15:47:53 +0100

    Cycles: Make it more proper check on vectorization flags from DebugFlags
    Mimics to checks in system_cpu_support() checks.

--- intern/cycles/util/util_debug.h
+++ intern/cycles/util/util_debug.h
@@ -45,6 +45,15 @@ public:
 		bool sse3;
 		bool sse2;
 
+		/* Check functions to see whether instructions up to the given one
+		 * are allowed for use.
+		 */
+		bool has_avx2()  { return has_avx()   && avx2; }
+		bool has_avx()   { return has_sse41() && avx; }
+		bool has_sse41() { return has_sse3()  && sse41; }
+		bool has_sse3()  { return has_sse2()  && sse3; }
+		bool has_sse2()  { return sse2; }
+
 		/* Whether QBVH usage is allowed or not. */
 		bool qbvh;
 
