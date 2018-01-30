4801e3fd9280a6ed0305a841d9a913704e094d1e
Clément Foucault <foucault.clem@gmail.com>
2018-01-30 23:22:17 +0100

    Eevee: Display compositing tab.

--- release/scripts/startup/bl_ui/properties_render.py
+++ release/scripts/startup/bl_ui/properties_render.py
@@ -322,7 +322,7 @@ class RENDER_PT_performance(RenderButtonsPanel, Panel):
 class RENDER_PT_post_processing(RenderButtonsPanel, Panel):
     bl_label = "Post Processing"
     bl_options = {'DEFAULT_CLOSED'}
-    COMPAT_ENGINES = {'BLENDER_RENDER'}
+    COMPAT_ENGINES = {'BLENDER_RENDER', 'BLENDER_EEVEE'}
 
     def draw(self, context):
         layout = self.layout
@@ -336,6 +336,9 @@ class RENDER_PT_post_processing(RenderButtonsPanel, Panel):
         col.prop(rd, "use_sequencer")
 
         split.prop(rd, "dither_intensity", text="Dither", slider=True)
+
+        if context.scene.view_render.engine == 'BLENDER_EEVEE':
+            return
 
         layout.separator()
 
