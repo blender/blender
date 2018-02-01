78a77fe622b86fb9b477d10533729a7889b3e115
Gaia Clary <gaia.clary@machinimatrix.org>
2018-02-02 00:49:32 +0100

    fix: unintentionally commented out collada animation export

--- source/blender/collada/DocumentExporter.cpp
+++ source/blender/collada/DocumentExporter.cpp
@@ -301,15 +301,15 @@ int DocumentExporter::exportCurrentScene(Scene *sce)
 	// <library_visual_scenes>
 
 	SceneExporter se(writer, &arm_exporter, this->export_settings);
-#if 0
-	/* The following code seems to be an obsolete workaround
-	   Comment out until it proofs correct that we no longer need it.
-	*/
 
 	// <library_animations>
 	AnimationExporter ae(writer, this->export_settings);
 	bool has_animations = ae.exportAnimations(sce);
 
+#if 0
+	/* The following code seems to be an obsolete workaround
+	Comment out until it proofs correct that we no longer need it.
+	*/
 	if (has_animations && this->export_settings->export_transformation_type == BC_TRANSFORMATION_TYPE_MATRIX) {
 		// channels adressing <matrix> objects is not (yet) supported
 		// So we force usage of <location>, <translation> and <scale>
