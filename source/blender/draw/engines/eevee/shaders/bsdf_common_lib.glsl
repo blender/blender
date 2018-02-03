e1d5d55b5683d9461c92f97ac5cc92b013b2bea5
Clément Foucault <foucault.clem@gmail.com>
2018-02-03 03:00:09 +0100

    Eevee: Codestyle: Indent #ifdefs

--- source/blender/draw/engines/eevee/shaders/bsdf_common_lib.glsl
+++ source/blender/draw/engines/eevee/shaders/bsdf_common_lib.glsl
@@ -654,12 +654,12 @@ Closure closure_add(Closure cl1, Closure cl2)
 struct Closure {
 	vec3 radiance;
 	float opacity;
-#ifdef USE_SSS
+#  ifdef USE_SSS
 	vec4 sss_data;
-#ifdef USE_SSS_ALBEDO
+#    ifdef USE_SSS_ALBEDO
 	vec3 sss_albedo;
-#endif
-#endif
+#    endif
+#  endif
 	vec4 ssr_data;
 	vec2 ssr_normal;
 	int ssr_id;
@@ -669,15 +669,15 @@ struct Closure {
 #define TRANSPARENT_CLOSURE_FLAG -2
 #define REFRACT_CLOSURE_FLAG -3
 
-#ifdef USE_SSS
-#ifdef USE_SSS_ALBEDO
+#  ifdef USE_SSS
+#    ifdef USE_SSS_ALBEDO
 #define CLOSURE_DEFAULT Closure(vec3(0.0), 1.0, vec4(0.0), vec3(0.0), vec4(0.0), vec2(0.0), -1)
-#else
+#    else
 #define CLOSURE_DEFAULT Closure(vec3(0.0), 1.0, vec4(0.0), vec4(0.0), vec2(0.0), -1)
-#endif
-#else
+#    endif
+#  else
 #define CLOSURE_DEFAULT Closure(vec3(0.0), 1.0, vec4(0.0), vec2(0.0), -1)
-#endif
+#  endif
 
 uniform int outputSsrId;
 
@@ -697,33 +697,33 @@ Closure closure_mix(Closure cl1, Closure cl2, float fac)
 	}
 	if (cl1.ssr_id == TRANSPARENT_CLOSURE_FLAG) {
 		cl1.radiance = cl2.radiance;
-#ifdef USE_SSS
+#  ifdef USE_SSS
 		cl1.sss_data = cl2.sss_data;
-#ifdef USE_SSS_ALBEDO
+#    ifdef USE_SSS_ALBEDO
 		cl1.sss_albedo = cl2.sss_albedo;
-#endif
-#endif
+#    endif
+#  endif
 	}
 	if (cl2.ssr_id == TRANSPARENT_CLOSURE_FLAG) {
 		cl2.radiance = cl1.radiance;
-#ifdef USE_SSS
+#  ifdef USE_SSS
 		cl2.sss_data = cl1.sss_data;
-#ifdef USE_SSS_ALBEDO
+#    ifdef USE_SSS_ALBEDO
 		cl2.sss_albedo = cl1.sss_albedo;
-#endif
-#endif
+#    endif
+#  endif
 	}
 	cl.radiance = mix(cl1.radiance, cl2.radiance, fac);
 	cl.opacity = mix(cl1.opacity, cl2.opacity, fac);
 
-#ifdef USE_SSS
+#  ifdef USE_SSS
 	cl.sss_data.rgb = mix(cl1.sss_data.rgb, cl2.sss_data.rgb, fac);
 	cl.sss_data.a = (cl1.sss_data.a > 0.0) ? cl1.sss_data.a : cl2.sss_data.a;
-#ifdef USE_SSS_ALBEDO
+#    ifdef USE_SSS_ALBEDO
 	/* TODO Find a solution to this. Dither? */
 	cl.sss_albedo = (cl1.sss_data.a > 0.0) ? cl1.sss_albedo : cl2.sss_albedo;
-#endif
-#endif
+#    endif
+#  endif
 
 	return cl;
 }
@@ -731,80 +731,80 @@ Closure closure_mix(Closure cl1, Closure cl2, float fac)
 Closure closure_add(Closure cl1, Closure cl2)
 {
 	Closure cl = (cl1.ssr_id == outputSsrId) ? cl1 : cl2;
-#ifdef USE_SSS
+#  ifdef USE_SSS
 	cl.sss_data = (cl1.sss_data.a > 0.0) ? cl1.sss_data : cl2.sss_data;
-#ifdef USE_SSS_ALBEDO
+#    ifdef USE_SSS_ALBEDO
 	/* TODO Find a solution to this. Dither? */
 	cl.sss_albedo = (cl1.sss_data.a > 0.0) ? cl1.sss_albedo : cl2.sss_albedo;
-#endif
-#endif
+#    endif
+#  endif
 	cl.radiance = cl1.radiance + cl2.radiance;
 	cl.opacity = saturate(cl1.opacity + cl2.opacity);
 	return cl;
 }
 
-#if defined(MESH_SHADER) && !defined(USE_ALPHA_HASH) && !defined(USE_ALPHA_CLIP) && !defined(SHADOW_SHADER) && !defined(USE_MULTIPLY)
+#  if defined(MESH_SHADER) && !defined(USE_ALPHA_HASH) && !defined(USE_ALPHA_CLIP) && !defined(SHADOW_SHADER) && !defined(USE_MULTIPLY)
 layout(location = 0) out vec4 fragColor;
-#ifdef USE_SSS
-#ifdef USE_SSS_ALBEDO
+#    ifdef USE_SSS
+#      ifdef USE_SSS_ALBEDO
 layout(location = 1) out vec4 sssData;
 layout(location = 2) out vec4 sssAlbedo;
 layout(location = 3) out vec4 ssrNormals;
 layout(location = 4) out vec4 ssrData;
-#else
+#      else
 layout(location = 1) out vec4 sssData;
 layout(location = 2) out vec4 ssrNormals;
 layout(location = 3) out vec4 ssrData;
-#endif /* USE_SSS_ALBEDO */
-#else
+#      endif /* USE_SSS_ALBEDO */
+#    else
 layout(location = 1) out vec4 ssrNormals;
 layout(location = 2) out vec4 ssrData;
-#endif /* USE_SSS */
+#    endif /* USE_SSS */
 
 Closure nodetree_exec(void); /* Prototype */
 
-#if defined(USE_ALPHA_BLEND_VOLUMETRICS)
+#    if defined(USE_ALPHA_BLEND_VOLUMETRICS)
 /* Prototype because this file is included before volumetric_lib.glsl */
 vec4 volumetric_resolve(vec4 scene_color, vec2 frag_uvs, float frag_depth);
-#endif
+#    endif
 
 #define NODETREE_EXEC
 void main()
 {
 	Closure cl = nodetree_exec();
-#ifndef USE_ALPHA_BLEND
+#    ifndef USE_ALPHA_BLEND
 	/* Prevent alpha hash material writing into alpha channel. */
 	cl.opacity = 1.0;
-#endif
+#    endif
 
-#if defined(USE_ALPHA_BLEND_VOLUMETRICS)
+#    if defined(USE_ALPHA_BLEND_VOLUMETRICS)
 	/* XXX fragile, better use real viewport resolution */
 	vec2 uvs = gl_FragCoord.xy / vec2(2 * textureSize(maxzBuffer, 0).xy);
 	fragColor = volumetric_resolve(vec4(cl.radiance, cl.opacity), uvs, gl_FragCoord.z);
-#else
+#    else
 	fragColor = vec4(cl.radiance, cl.opacity);
-#endif
+#    endif
 
 	ssrNormals = cl.ssr_normal.xyyy;
 	ssrData = cl.ssr_data;
-#ifdef USE_SSS
+#    ifdef USE_SSS
 	sssData = cl.sss_data;
-#ifdef USE_SSS_ALBEDO
+#      ifdef USE_SSS_ALBEDO
 	sssAlbedo = cl.sss_albedo.rgbb;
-#endif
-#endif
+#      endif
+#    endif
 
 	/* For Probe capture */
-#ifdef USE_SSS
-#ifdef USE_SSS_ALBEDO
+#    ifdef USE_SSS
+#      ifdef USE_SSS_ALBEDO
 	fragColor.rgb += cl.sss_data.rgb * cl.sss_albedo.rgb * float(!sssToggle);
-#else
+#      else
 	fragColor.rgb += cl.sss_data.rgb * float(!sssToggle);
-#endif
-#endif
+#      endif
+#    endif
 }
 
-#endif /* MESH_SHADER && !SHADOW_SHADER */
+#  endif /* MESH_SHADER && !SHADOW_SHADER */
 
 #endif /* VOLUMETRICS */
 
