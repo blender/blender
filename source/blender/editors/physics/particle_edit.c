eb0d430b4dde44958a6e3dd583cd2f5718af5516
Mai Lavelle <mai.lavelle@gmail.com>
2018-02-08 00:15:09 -0500

    Fix crash with particle cut brush and other brushes
    Why is bedit->data.context NULL?

--- source/blender/editors/physics/particle_edit.c
+++ source/blender/editors/physics/particle_edit.c
@@ -3869,6 +3869,7 @@ static void brush_edit_apply(bContext *C, wmOperator *op, PointerRNA *itemptr)
 	     (sqrtf(dx * dx + dy * dy) > pset->brush[PE_BRUSH_ADD].step) : (dx != 0 || dy != 0)) || bedit->first)
 	{
 		PEData data= bedit->data;
+		data.context = C; // TODO(mai): why isnt this set in bedit->data?
 
 		view3d_operator_needs_opengl(C);
 		selected= (short)count_selected_keys(scene, edit);
