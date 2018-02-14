blender/blender

commit ce0f70fbd62fc812a2508c27a2f392cde0acbdfa

Eevee: Fix SSR & AO on Vega + Mesa.
Really strange bug. Maybe gl_FragDepth is broken on this implementation.

source/blender/draw/engines/eevee/shaders/effect_minmaxz_frag.glsl
@@ -23,7 +23,9 @@ uniform sampler2D depthBuffer;
 #define minmax(a, b) max(a, b)
 #endif
 
-#ifdef GPU_INTEL
+/* On some AMD card / driver conbination, it is needed otherwise,
+ * the shader does not write anything. */
+#if defined(GPU_INTEL) || defined(GPU_ATI)
 out vec4 fragColor;
 #endif
 
@@ -65,7 +67,7 @@ void main()
 	}
 #endif
 
-#ifdef GPU_INTEL
+#if defined(GPU_INTEL) || defined(GPU_ATI)
 	/* Use color format instead of 24bit depth texture */
 	fragColor = vec4(val);
 #else

commit 2464dcef37f1a52db3c8a05922c0475fd3bff238

Eevee: Fix broken AO and Contact shadows on certain platform.
This was caused by ce0f70fbd62fc812a2508c27a2f392cde0acbdfa

source/blender/draw/engines/eevee/shaders/effect_minmaxz_frag.glsl
@@ -70,7 +70,6 @@ void main()
 #if defined(GPU_INTEL) || defined(GPU_ATI)
 	/* Use color format instead of 24bit depth texture */
 	fragColor = vec4(val);
-#else
-	gl_FragDepth = val;
 #endif
+	gl_FragDepth = val;
 }