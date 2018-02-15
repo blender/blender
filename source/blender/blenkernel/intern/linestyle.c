ad37ccbc32446fef542faf696a54a8135b2e3d26
Brecht Van Lommel <brechtvanlommel@gmail.com>
2018-02-15 16:25:37 +0100

    Fix crash reading deg_point_cache_transform.blend test, saved with 2.8.
    In general 2.8 files may not be readable in master, but might as well fix
    this case so all the lib/tests can be opened still.

--- source/blender/blenkernel/intern/linestyle.c
+++ source/blender/blenkernel/intern/linestyle.c
@@ -216,6 +216,10 @@ void BKE_linestyle_make_local(struct Main *bmain, FreestyleLineStyle *linestyle,
 FreestyleLineStyle *BKE_linestyle_active_from_scene(Scene *scene)
 {
 	SceneRenderLayer *actsrl = BLI_findlink(&scene->r.layers, scene->r.actlay);
+	if (!actsrl) {
+		return NULL;
+	}
+
 	FreestyleConfig *config = &actsrl->freestyleConfig;
 	FreestyleLineSet *lineset = BKE_freestyle_lineset_get_active(config);
 
