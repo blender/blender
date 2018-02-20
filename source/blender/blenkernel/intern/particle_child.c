87e37224c74a4336278e27b0de9e2223701439ae
Sergey Sharybin <sergey.vfx@gmail.com>
2018-02-20 10:47:01 +0100

    Fix T54069: Vertex groups for hair length is wrong in some cases
    This is a regression in rB4f1c0a1 which only allowed cutting haior at the
    second segment only, while there is nothing wrong with cutting hair at the
    first segmewnt.

--- source/blender/blenkernel/intern/particle_child.c
+++ source/blender/blenkernel/intern/particle_child.c
@@ -394,7 +394,7 @@ void psys_apply_child_modifiers(ParticleThreadContext *ctx, struct ListBase *mod
 					get_strand_normal(ma, ornor, cur_length, (key-1)->vel);
 			}
 
-			if (use_length_check && k > 1) {
+			if (use_length_check && k > 0) {
 				float dvec[3];
 				/* check if path needs to be cut before actual end of data points */
 				if (!check_path_length(k, keys, key, max_length, step_length, &cur_length, dvec)) {
