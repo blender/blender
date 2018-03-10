f3161bd2abe4bcc41f0e9169275be315ecc6b054
Clément Foucault <foucault.clem@gmail.com>
2018-03-10 02:18:12 +0100

    Eevee: Planar Reflections: Fix corrupted results in downsampling step.
    It only seems to happen in some drivers/gpu, the vertices gets culled if 4th component is 0.0. So lesson learned: always use 1.0 if constant.

--- source/blender/draw/engines/eevee/shaders/lightprobe_planar_downsample_geom.glsl
+++ source/blender/draw/engines/eevee/shaders/lightprobe_planar_downsample_geom.glsl
@@ -11,13 +11,13 @@ void main() {
 	gl_Layer = instance[0];
 	layer = float(instance[0]);
 
-	gl_Position = vec4(vPos[0], 0.0, 0.0);
+	gl_Position = vec4(vPos[0], 0.0, 1.0);
 	EmitVertex();
 
-	gl_Position = vec4(vPos[1], 0.0, 0.0);
+	gl_Position = vec4(vPos[1], 0.0, 1.0);
 	EmitVertex();
 
-	gl_Position = vec4(vPos[2], 0.0, 0.0);
+	gl_Position = vec4(vPos[2], 0.0, 1.0);
 	EmitVertex();
 
 	EndPrimitive();
