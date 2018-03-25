6d6c292cf8b81c2579c1ac37f316b57579128d51
Clément Foucault <foucault.clem@gmail.com>
2018-03-25 03:17:05 +0200

    Clay: Fix alpha problem cause by FXAA pass.

--- source/blender/draw/engines/clay/shaders/clay_fxaa.glsl
+++ source/blender/draw/engines/clay/shaders/clay_fxaa.glsl
@@ -7,12 +7,12 @@ uniform sampler2D colortex;
 
 void main()
 {
-	fragColor = FxaaPixelShader(
+	fragColor = vec4(FxaaPixelShader(
 		uvcoordsvar.st,
 		colortex,
 		invscreenres,
 		1.0,
 		0.166,
 		0.0833
-	);
+	).rgb, 1.0);
 }
