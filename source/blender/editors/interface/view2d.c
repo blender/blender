ccbc14d995a4de2609125435ad108b6e17739e46
Clément Foucault <foucault.clem@gmail.com>
2018-04-08 16:57:39 +0200

    UI: Perf: Batch horizontal scrollers numbers together.

--- source/blender/editors/interface/view2d.c
+++ source/blender/editors/interface/view2d.c
@@ -1892,7 +1892,9 @@ void UI_view2d_scrollers_draw(const bContext *C, View2D *v2d, View2DScrollers *v
 			/* draw numbers in the appropriate range */
 			if (dfac > 0.0f) {
 				float h = 0.1f * UI_UNIT_Y + (float)(hor.ymin);
-				
+
+				BLF_batch_draw_begin();
+
 				for (; fac < hor.xmax - 0.5f * U.widget_unit; fac += dfac, val += grid->dx) {
 					
 					/* make prints look nicer for scrollers */
@@ -1919,6 +1921,8 @@ void UI_view2d_scrollers_draw(const bContext *C, View2D *v2d, View2DScrollers *v
 							break;
 					}
 				}
+
+				BLF_batch_draw_end();
 			}
 		}
 	}
