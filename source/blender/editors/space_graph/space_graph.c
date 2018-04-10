16b795cb95c8006993ebfd5398eca02bae6b75ea
Mai Lavelle <mai.lavelle@gmail.com>
2018-04-09 21:39:10 -0400

    Fix crash in graph editor id remapping
    Dopsheet pointer isn't set...?

--- source/blender/editors/space_graph/space_graph.c
+++ source/blender/editors/space_graph/space_graph.c
@@ -686,7 +686,7 @@ static void graph_id_remap(ScrArea *UNUSED(sa), SpaceLink *slink, ID *old_id, ID
 	if (sgraph->ads && (ID *)sgraph->ads->filter_grp == old_id) {
 		sgraph->ads->filter_grp = (Group *)new_id;
 	}
-	if ((ID *)sgraph->ads->source == old_id) {
+	if (sgraph->ads && (ID *)sgraph->ads->source == old_id) {
 		sgraph->ads->source = new_id;
 	}
 }
