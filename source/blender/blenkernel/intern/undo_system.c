c1680902ac9a0a6431a2ae5258b0806b60a95a75
Bastien Montagne <montagne29@wanadoo.fr>
2018-04-11 15:14:20 +0200

    Fix again 'auto override' system.
    Got lost in big undo refactor.
    
    Note that this is probably (maybe) not how we want to have it in the
    end, things like EditMode undo should probably not trigger this check?

--- source/blender/blenkernel/intern/undo_system.c
+++ source/blender/blenkernel/intern/undo_system.c
@@ -39,6 +39,7 @@
 
 #include "BKE_context.h"
 #include "BKE_global.h"
+#include "BKE_library_override.h"
 #include "BKE_main.h"
 #include "BKE_undo_system.h"
 
@@ -391,6 +392,10 @@ bool BKE_undosys_step_push_with_type(UndoStack *ustack, bContext *C, const char 
 	UNDO_NESTED_ASSERT(false);
 	undosys_stack_validate(ustack, false);
 	bool is_not_empty = ustack->step_active != NULL;
+
+	/* Might not be final place for this to be called - probably only want to call it from some
+	 * undo handlers, not all of them? */
+	BKE_main_override_static_operations_create(CTX_data_main(C));
 
 	/* Remove all undos after (also when 'ustack->step_active == NULL'). */
 	while (ustack->steps.last != ustack->step_active) {
