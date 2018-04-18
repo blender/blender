7c415e6fd7b42cbe9ae06e8354e0d8bf510d80da
Thomas Beck <software@plasmasolutions.de>
2018-04-19 00:03:51 +0200

    Edit Mesh: multi-object support for 'Vertices Only' extrusion (Alt+E)

--- source/blender/editors/mesh/editmesh_extrude.c
+++ source/blender/editors/mesh/editmesh_extrude.c
@@ -446,12 +446,24 @@ void MESH_OT_extrude_region(wmOperatorType *ot)
 
 static int edbm_extrude_verts_exec(bContext *C, wmOperator *op)
 {
-	Object *obedit = CTX_data_edit_object(C);
-	BMEditMesh *em = BKE_editmesh_from_object(obedit);
+	ViewLayer *view_layer = CTX_data_view_layer(C);
+	uint objects_len = 0;
+	Object **objects = BKE_view_layer_array_from_objects_in_edit_mode_unique_data(view_layer, &objects_len);
 
-	edbm_extrude_verts_indiv(em, op, BM_ELEM_SELECT);
 
-	EDBM_update_generic(em, true, true);
+	for (uint ob_index = 0; ob_index < objects_len; ob_index++)
+	{
+		Object *obedit = objects[ob_index];
+		BMEditMesh *em = BKE_editmesh_from_object(obedit);
+		if (em->bm->totvertsel == 0) {
+			continue;
+		}
+
+		edbm_extrude_verts_indiv(em, op, BM_ELEM_SELECT);
+
+		EDBM_update_generic(em, true, true);
+	}
+	MEM_freeN(objects);
 
 	return OPERATOR_FINISHED;
 }
