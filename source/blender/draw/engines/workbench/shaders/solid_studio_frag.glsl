a305652d87b7d14562e8e22aefe9073f34d2f06e
Jeroen Bakker <j.bakker@atmind.nl>
2018-04-19 14:48:53 +0200

    Workbench: Diffuse studio lighting

--- source/blender/draw/engines/workbench/shaders/solid_studio_frag.glsl
+++ source/blender/draw/engines/workbench/shaders/solid_studio_frag.glsl
@@ -1,11 +1,29 @@
 uniform vec3 color;
+uniform vec3 light_direction = vec3(0.0, 0.0, 1.0);
 
-in vec3 normal;
+uniform vec3 world_diffuse_light_xp = vec3(0.5, 0.5, 0.6);
+uniform vec3 world_diffuse_light_xn = vec3(0.5, 0.5, 0.6);
+uniform vec3 world_diffuse_light_yp = vec3(0.5, 0.5, 0.6);
+uniform vec3 world_diffuse_light_yn = vec3(0.5, 0.5, 0.6);
+uniform vec3 world_diffuse_light_zp = vec3(0.8, 0.8, 0.8);
+uniform vec3 world_diffuse_light_zn = vec3(0.0, 0.0, 0.0);
+
+in vec3 normal_viewport;
 out vec4 fragColor;
+#define USE_WORLD_DIFFUSE
+#define AMBIENT_COLOR vec3(0.2, 0.2, 0.2)
 
 void main()
 {
-	float intensity = lambert_diffuse(vec3(0.0, 0.0, 1.0), normal);
+
+#ifdef USE_WORLD_DIFFUSE
+	vec3 diffuse_light_color = get_world_diffuse_light(normal_viewport, world_diffuse_light_xp, world_diffuse_light_xn, world_diffuse_light_yp, world_diffuse_light_yn, world_diffuse_light_zp, world_diffuse_light_zn);
+	vec3 shaded_color = (AMBIENT_COLOR + diffuse_light_color)  * color;
+
+#else
+	float intensity = lambert_diffuse(light_direction, normal_viewport);
 	vec3 shaded_color = color * intensity;
+
+#endif
 	fragColor = vec4(shaded_color, 1.0);
 }
--- source/blender/draw/engines/workbench/shaders/workbench_studio_vert.glsl
+++ source/blender/draw/engines/workbench/shaders/workbench_studio_vert.glsl
@@ -4,10 +4,10 @@ uniform mat3 NormalMatrix;
 in vec3 pos;
 in vec3 nor;
 
-out vec3 normal;
+out vec3 normal_viewport;
 
 void main()
 {
-	normal = normalize(NormalMatrix * nor);
+	normal_viewport = normalize(NormalMatrix * nor);
 	gl_Position = ModelViewProjectionMatrix * vec4(pos, 1.0);
 }
