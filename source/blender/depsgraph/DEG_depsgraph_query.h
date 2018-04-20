3b100c5a2a78b196eb7fe6fb559c1e5ea0c47e66
Sergey Sharybin <sergey.vfx@gmail.com>
2018-04-20 11:01:43 +0200

    Depsgraph: Add utility functions to go from evaluated to original datablock

--- source/blender/depsgraph/DEG_depsgraph_query.h
+++ source/blender/depsgraph/DEG_depsgraph_query.h
@@ -85,6 +85,13 @@ struct Object *DEG_get_evaluated_object(const struct Depsgraph *depsgraph,
 struct ID *DEG_get_evaluated_id(const struct Depsgraph *depsgraph,
                                 struct ID *id);
 
+
+/* Get original version of object for given evaluated one. */
+struct Object *DEG_get_original_object(struct Object *object);
+
+/* Get original version of given evaluated ID datablock. */
+struct ID *DEG_get_original_id(struct ID *id);
+
 /* ************************ DEG iterators ********************* */
 
 enum {
