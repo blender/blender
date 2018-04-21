46a9038f60da4449d58075c4bbeb26b4b3982c55
Campbell Barton <ideasman42@gmail.com>
2018-04-21 20:22:12 +0200

    Cleanup: variable naming

--- source/blender/blenlib/BLI_bitmap_draw_2d.h
+++ source/blender/blenlib/BLI_bitmap_draw_2d.h
@@ -27,7 +27,7 @@
 
 void BLI_bitmap_draw_2d_line_v2v2i(
         const int p1[2], const int p2[2],
-        bool (*callback)(int, int, void *), void *userData);
+        bool (*callback)(int, int, void *), void *user_data);
 
 void BLI_bitmap_draw_2d_tri_v2i(
         const int p1[2], const int p2[2], const int p3[2],
@@ -35,7 +35,7 @@ void BLI_bitmap_draw_2d_tri_v2i(
 
 void BLI_bitmap_draw_2d_poly_v2i_n(
         const int xmin, const int ymin, const int xmax, const int ymax,
-        const int polyXY[][2], const int polyCorners,
-        void (*callback)(int x, int x_end, int y, void *), void *userData);
+        const int verts[][2], const int verts_len,
+        void (*callback)(int x, int x_end, int y, void *), void *user_data);
 
 #endif  /* __BLI_BITMAP_DRAW_2D_H__ */
