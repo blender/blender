e0b0f7afa0dd64c2295a5c4a934c5fcb60c1190e
Sergey Sharybin <sergey.vfx@gmail.com>
2018-05-14 15:34:41 +0200

    Fix hair edit mode after recent fix for DEG sued from draw code

--- source/blender/editors/physics/particle_edit.c
+++ source/blender/editors/physics/particle_edit.c
@@ -4003,6 +4003,7 @@ static void brush_edit_apply(bContext *C, wmOperator *op, PointerRNA *itemptr)
 		if (edit->psys) {
 			WM_event_add_notifier(C, NC_OBJECT|ND_PARTICLE|NA_EDITED, ob);
 			BKE_particle_batch_cache_dirty(edit->psys, BKE_MESH_BATCH_DIRTY_ALL);
+			DEG_id_tag_update(&ob->id, DEG_TAG_COPY_ON_WRITE);
 		}
 		else {
 			DEG_id_tag_update(&ob->id, OB_RECALC_DATA);
