4f5bcc30d3447a7a498b124bf6e35fe177a8a320
Sergey Sharybin <sergey.vfx@gmail.com>
2018-05-15 12:52:54 +0200

    Particle edit: Add missing looking tag in cut brush

--- source/blender/editors/physics/particle_edit.c
+++ source/blender/editors/physics/particle_edit.c
@@ -4268,6 +4268,7 @@ static int shape_cut_exec(bContext *C, wmOperator *UNUSED(op))
 		if (edit->psys) {
 			WM_event_add_notifier(C, NC_OBJECT|ND_PARTICLE|NA_EDITED, ob);
 			BKE_particle_batch_cache_dirty(edit->psys, BKE_PARTICLE_BATCH_DIRTY_ALL);
+			DEG_id_tag_update(&ob->id, DEG_TAG_COPY_ON_WRITE);
 		}
 		else {
 			DEG_id_tag_update(&ob->id, OB_RECALC_DATA);
