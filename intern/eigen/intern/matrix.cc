44a8070db3c03a4e12d1ec1ca15432ec2b36e2fc
Brecht Van Lommel <brechtvanlommel@gmail.com>
2018-06-01 15:42:11 +0200

    Cleanup: fix incorrect contributor information.

--- intern/eigen/intern/matrix.cc
+++ intern/eigen/intern/matrix.cc
@@ -15,11 +15,10 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * The Original Code is Copyright (C) 2015 Blender Foundation.
+ * The Original Code is Copyright (C) 2018 Blender Foundation.
  * All rights reserved.
  *
- * Contributor(s): Blender Foundation,
- *                 Bastien Montagne
+ * Contributor(s): Blender Foundation
  *
  * ***** END GPL LICENSE BLOCK *****
  */
