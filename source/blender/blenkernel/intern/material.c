b85d5b6d524956e212ac329fb99f5ff8d629035c
Campbell Barton <ideasman42@gmail.com>
2018-06-01 13:56:39 +0200

    Cleanup: quiet warning, don't set unused alpha

--- source/blender/blenkernel/intern/material.c
+++ source/blender/blenkernel/intern/material.c
@@ -113,7 +113,7 @@ void BKE_material_init(Material *ma)
 
 	ma->r = ma->g = ma->b = 0.8;
 	ma->specr = ma->specg = ma->specb = 1.0;
-	ma->alpha = 1.0;
+	// ma->alpha = 1.0;  /* DEPRECATED */
 	ma->spec = 0.5;
 
 	ma->roughness = 0.25f;
