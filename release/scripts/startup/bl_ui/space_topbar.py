18a45538384564a5dffbdd88b778bb10da92a7b2
Pablo Vazquez <venomgfx@gmail.com>
2018-06-02 23:13:03 +0200

    Edit menu: add Repeat Last and Repeat History

--- release/scripts/startup/bl_ui/space_topbar.py
+++ release/scripts/startup/bl_ui/space_topbar.py
@@ -610,6 +610,11 @@ class INFO_MT_edit(Menu):
 
         layout.separator()
 
+        layout.operator("screen.repeat_last")
+        layout.operator("screen.repeat_history")
+
+        layout.separator()
+
         layout.operator("screen.userpref_show", text="User Preferences...", icon='PREFERENCES')
 
 
