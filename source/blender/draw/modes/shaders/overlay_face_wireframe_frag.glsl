570804882d70195b2edb88ab08d0c64444027831
Clément Foucault <foucault.clem@gmail.com>
2018-06-04 15:58:23 +0200

    Wireframe: Lower line thickness and front color blending.

--- source/blender/draw/modes/shaders/overlay_face_wireframe_frag.glsl
+++ source/blender/draw/modes/shaders/overlay_face_wireframe_frag.glsl
@@ -12,10 +12,10 @@ float min_v3(vec3 v) { return min(v.x, min(v.y, v.z)); }
 
 /* In pixels */
 const float wire_size = 0.0; /* Expands the core of the wire (part that is 100% wire color) */
-const float wire_smooth = 1.4; /* Smoothing distance after the 100% core. */
+const float wire_smooth = 1.2; /* Smoothing distance after the 100% core. */
 
 /* Alpha constants could be exposed in the future. */
-const float front_alpha = 0.55;
+const float front_alpha = 0.35;
 const float rim_alpha = 0.75;
 
 void main()
@@ -30,7 +30,7 @@ void main()
 	float fac = smoothstep(wire_size, wire_size + wire_smooth, min_v3(abs(dist_to_edge)));
 	float facing_clamped = clamp((gl_FrontFacing) ? facing : -facing, 0.0, 1.0);
 
-	vec3 final_front_col = rimColor * 0.5 + wireColor * 0.5;
+	vec3 final_front_col = mix(rimColor, wireColor, 0.05);
 	fragColor = mix(vec4(rimColor, rim_alpha), vec4(final_front_col, front_alpha), facing_clamped);
 	fragColor.a *= (1.0 - fac);
 }
