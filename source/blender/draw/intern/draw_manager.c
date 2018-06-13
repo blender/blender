2f3e890b94eb905b13243af955da2baeae9a1f5c
Clément Foucault <foucault.clem@gmail.com>
2018-06-13 22:20:48 +0200

    DRW: Fix crash caused by hair transform feedback
    Seem to be cause by the lack of proper FBO bound.

--- source/blender/draw/intern/draw_manager.c
+++ source/blender/draw/intern/draw_manager.c
@@ -1311,12 +1311,13 @@ void DRW_draw_render_loop_ex(
 	}
 
 	DRW_stats_begin();
-	DRW_hair_update();
 
 	GPU_framebuffer_bind(DST.default_framebuffer);
 
 	/* Start Drawing */
 	DRW_state_reset();
+
+	DRW_hair_update();
 
 	drw_engines_draw_background();
 
@@ -1737,8 +1738,6 @@ void DRW_draw_select_loop(
 		DRW_render_instance_buffer_finish();
 	}
 
-	DRW_hair_update();
-
 	/* Setup framebuffer */
 	draw_select_framebuffer_setup(rect);
 	GPU_framebuffer_bind(g_select_buffer.framebuffer);
@@ -1747,6 +1746,8 @@ void DRW_draw_select_loop(
 	/* Start Drawing */
 	DRW_state_reset();
 	DRW_draw_callbacks_pre_scene();
+
+	DRW_hair_update();
 
 	DRW_state_lock(
 	        DRW_STATE_WRITE_DEPTH |
@@ -1897,10 +1898,11 @@ void DRW_draw_depth_loop(
 		DRW_render_instance_buffer_finish();
 	}
 
-	DRW_hair_update();
-
 	/* Start Drawing */
 	DRW_state_reset();
+
+	DRW_hair_update();
+
 	DRW_draw_callbacks_pre_scene();
 	drw_engines_draw_scene();
 	DRW_draw_callbacks_post_scene();
