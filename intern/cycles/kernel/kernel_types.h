8148d549bbe8afdbf3116c2083d30ac288ac7a1b
Brecht Van Lommel <brechtvanlommel@gmail.com>
2018-06-15 10:06:32 +0200

    Fix Cycles CUDA error after recent changes.

--- intern/cycles/kernel/kernel_types.h
+++ intern/cycles/kernel/kernel_types.h
@@ -1272,12 +1272,12 @@ typedef struct KernelFilm {
 	int pass_denoising_clean;
 	int denoising_flags;
 
+	int pad1, pad2, pad3;
+
 	float3 xyz_to_r;
 	float3 xyz_to_g;
 	float3 xyz_to_b;
 	float3 rgb_to_y;
-
-	int pad1, pad2, pad3;
 
 #ifdef __KERNEL_DEBUG__
 	int pass_bvh_traversed_nodes;
