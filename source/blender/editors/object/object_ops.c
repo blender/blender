43d22d80e72d1a1bf49d33fc4f8bd1a031e3358b
Campbell Barton <ideasman42@gmail.com>
2018-06-15 17:09:17 +0200

    Keymap: use click event for tab-key
    Checking release events would toggle edit-mode during alt-tab
    cycling on X11.
    
    Now both press and release events are needed.

--- source/blender/editors/object/object_ops.c
+++ source/blender/editors/object/object_ops.c
@@ -289,7 +289,7 @@ void ED_keymap_object(wmKeyConfig *keyconf)
 	keymap = WM_keymap_find(keyconf, "Object Non-modal", 0, 0);
 
 	/* modes */
-	kmi = WM_keymap_add_item(keymap, "OBJECT_OT_mode_set", TABKEY, KM_RELEASE, 0, 0);
+	kmi = WM_keymap_add_item(keymap, "OBJECT_OT_mode_set", TABKEY, KM_CLICK, 0, 0);
 	RNA_enum_set(kmi->ptr, "mode", OB_MODE_EDIT);
 	RNA_boolean_set(kmi->ptr, "toggle", true);
 
