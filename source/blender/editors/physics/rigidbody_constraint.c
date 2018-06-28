af9fcb6a333b575e7f1c2ab1e3d8fbcf1a69b7e1
Sybren A. Stüvel <sybren@stuvel.eu>
2018-06-28 12:31:44 +0200

    Give RigidBodyWorld constraints collection a fake user
    This prevents it from disappearing when the blend file is saved.

--- source/blender/editors/physics/rigidbody_constraint.c
+++ source/blender/editors/physics/rigidbody_constraint.c
@@ -84,6 +84,7 @@ bool ED_rigidbody_constraint_add(Main *bmain, Scene *scene, Object *ob, int type
 	/* create constraint group if it doesn't already exits */
 	if (rbw->constraints == NULL) {
 		rbw->constraints = BKE_collection_add(bmain, NULL, "RigidBodyConstraints");
+		id_fake_user_set(&rbw->constraints->id);
 	}
 	/* make rigidbody constraint settings */
 	ob->rigidbody_constraint = BKE_rigidbody_create_constraint(scene, ob, type);
