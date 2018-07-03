969111f9b5b075470a6de995f048d19e25c1b9ea
Campbell Barton <ideasman42@gmail.com>
2018-07-03 22:35:37 +0200

    Cleanup: pep8

--- doc/python_api/sphinx_doc_update.py
+++ doc/python_api/sphinx_doc_update.py
@@ -29,7 +29,7 @@ You'll need to specify your user login and password, obviously.
 
 Example usage:
 
-   ./sphinx_doc_update.py --mirror ../../../docs/remote_api_backup/ --source ../.. --blender ../../../build_cmake/bin/blender --user foobar --password barfoo 
+   ./sphinx_doc_update.py --mirror ../../../docs/remote_api_backup/ --source ../.. --blender ../../../build_cmake/bin/blender --user foobar --password barfoo
 
 """
 
@@ -114,7 +114,7 @@ def main():
 
         # III) Get Blender version info.
         getver_file = os.path.join(tmp_dir, "blendver.txt")
-        getver_script = (""
+        getver_script = (
             "import sys, bpy\n"
             "with open(sys.argv[-1], 'w') as f:\n"
             "    is_release = bpy.app.version_cycle in {'rc', 'release'}\n"
@@ -124,7 +124,8 @@ def main():
             "    f.write('%d.%d%s\\n' % (bpy.app.version[0], bpy.app.version[1], bpy.app.version_char)\n"
             "            if is_release else '%s\\n' % branch)\n"
             "    f.write('%d_%d%s_release' % (bpy.app.version[0], bpy.app.version[1], bpy.app.version_char)\n"
-            "            if is_release else '%d_%d_%d' % bpy.app.version)\n")
+            "            if is_release else '%d_%d_%d' % bpy.app.version)\n"
+        )
         get_ver_cmd = (args.blender, "--background", "-noaudio", "--factory-startup", "--python-exit-code", "1",
                        "--python-expr", getver_script, "--", getver_file)
         subprocess.run(get_ver_cmd)
