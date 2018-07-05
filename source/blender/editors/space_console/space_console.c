edd59aa2be375170515fb8bd54ecafecbdbc068c
Campbell Barton <ideasman42@gmail.com>
2018-07-05 08:27:42 +0200

    Revert "Keymap: use Shift-Space for auto-complete"
    This reverts commit 30680b8d51caada48a4f773caee8b934f06e3f30.
    
    Conflicts with typing in capitals.

--- source/blender/editors/space_console/space_console.c
+++ source/blender/editors/space_console/space_console.c
@@ -333,7 +333,7 @@ static void console_keymap(struct wmKeyConfig *keyconf)
 	kmi = WM_keymap_add_item(keymap, "CONSOLE_OT_execute", PADENTER, KM_PRESS, 0, 0);
 	RNA_boolean_set(kmi->ptr, "interactive", true);
 
-	WM_keymap_add_item(keymap, "CONSOLE_OT_autocomplete", SPACEKEY, KM_PRESS, KM_SHIFT, 0); /* python operator - space_text.py */
+	WM_keymap_add_item(keymap, "CONSOLE_OT_autocomplete", SPACEKEY, KM_PRESS, KM_CTRL, 0); /* python operator - space_text.py */
 #endif
 
 	WM_keymap_add_item(keymap, "CONSOLE_OT_copy_as_script", CKEY, KM_PRESS, KM_CTRL | KM_SHIFT, 0);
