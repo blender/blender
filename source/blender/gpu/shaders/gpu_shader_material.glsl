dd7a727910dd02d830d11d726acf52fd88ea6e8e
Brecht Van Lommel <brechtvanlommel@gmail.com>
2018-07-10 15:35:59 +0200

    Fix accidentally removed particle function in GLSL, still not supported though.

--- source/blender/gpu/shaders/gpu_shader_material.glsl
+++ source/blender/gpu/shaders/gpu_shader_material.glsl
@@ -170,6 +170,23 @@ void color_to_blender_normal_new_shading(vec3 color, out vec3 normal)
 
 /*********** SHADER NODES ***************/
 
+void particle_info(
+        vec4 sprops, vec4 loc, vec3 vel, vec3 avel,
+        out float index, out float random, out float age,
+        out float life_time, out vec3 location,
+        out float size, out vec3 velocity, out vec3 angular_velocity)
+{
+	index = sprops.x;
+	random = loc.w;
+	age = sprops.y;
+	life_time = sprops.z;
+	size = sprops.w;
+
+	location = loc.xyz;
+	velocity = vel;
+	angular_velocity = avel;
+}
+
 void vect_normalize(vec3 vin, out vec3 vout)
 {
 	vout = normalize(vin);
