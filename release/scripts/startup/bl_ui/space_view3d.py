dc3b3d945386b3651bb0fb4413753297f044e535
Pablo Vazquez <venomgfx@gmail.com>
2018-07-17 00:45:56 +0200

    UI: Lamp -> Light for the object type visibility operators
    Spotted in a live stream by Emilton Mendoza, thanks!

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -3714,7 +3714,7 @@ class VIEW3D_PT_object_type_visibility(Panel):
             "lattice",
             "empty",
             "camera",
-            "lamp",
+            "light",
             "light_probe",
             "speaker",
         )
