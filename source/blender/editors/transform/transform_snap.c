5f1ead63953527abd9ddca16986990d59f123aff
Bastien Montagne <montagne29@wanadoo.fr>
2018-07-25 14:52:29 +0200

    Fix T55895: VSE crash while moving a strip
    Glitch from multi-edit project. ;)

--- source/blender/editors/transform/transform_snap.c
+++ source/blender/editors/transform/transform_snap.c
@@ -1462,7 +1462,7 @@ void snapSequenceBounds(TransInfo *t, const int mval[2])
 	float xmouse, ymouse;
 	int frame;
 	int mframe;
-	TransSeq *ts = t->custom.type.data;
+	TransSeq *ts = TRANS_DATA_CONTAINER_FIRST_SINGLE(t)->custom.type.data;
 	/* reuse increment, strictly speaking could be another snap mode, but leave as is */
 	if (!(t->modifiers & MOD_SNAP_INVERT))
 		return;
