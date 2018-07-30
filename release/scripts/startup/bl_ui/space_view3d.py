4473427d8175d68a97e8b803266b3751175f183f
Pablo Vazquez <venomgfx@gmail.com>
2018-07-30 14:50:44 +0200

    UI: Hide labels from MatCaps
    They were added as placeholder to show something until they lazy-load.
    But since the load is fast and it's hard to fit the text, and their name
    is displayed already in the tooltip, we can do without for now.
    
    In the future when we have insta-tooltips we should make them use this.

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -3784,7 +3784,7 @@ class VIEW3D_PT_shading_lighting(Panel):
             sub.scale_y = 0.6 # smaller matcap/hdri preview
 
             if shading.light == 'STUDIO':
-                sub.template_icon_view(shading, "studio_light", show_labels=True, scale=3)
+                sub.template_icon_view(shading, "studio_light", scale=3)
 
                 if shading.selected_studio_light.orientation == 'WORLD':
                     col.prop(shading, "studiolight_rotate_z", text="Rotation")
@@ -3793,7 +3793,7 @@ class VIEW3D_PT_shading_lighting(Panel):
                 col.operator('wm.studiolight_userpref_show', emboss=False, text="", icon='PREFERENCES')
 
             elif shading.light == 'MATCAP':
-                sub.template_icon_view(shading, "studio_light", show_labels=True, scale=3)
+                sub.template_icon_view(shading, "studio_light", scale=3)
 
                 col = split.column()
                 col.operator('wm.studiolight_userpref_show', emboss=False, text="", icon='PREFERENCES')
@@ -3810,7 +3810,7 @@ class VIEW3D_PT_shading_lighting(Panel):
                 col = split.column()
                 sub = col.row()
                 sub.scale_y = 0.6
-                sub.template_icon_view(shading, "studio_light", show_labels=True, scale=3)
+                sub.template_icon_view(shading, "studio_light", scale=3)
 
                 col = split.column()
                 col.operator('wm.studiolight_userpref_show', emboss=False, text="", icon='PREFERENCES')
