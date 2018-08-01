5f55bbf3d07db4f1b89921cbe742873fd5645e11
Campbell Barton <ideasman42@gmail.com>
2018-08-01 16:02:20 +1000

    RNA: add space_type to tool

--- source/blender/makesrna/intern/rna_workspace.c
+++ source/blender/makesrna/intern/rna_workspace.c
@@ -224,6 +224,12 @@ static void rna_def_workspace_tool(BlenderRNA *brna)
 	RNA_def_property_ui_text(prop, "Index", "");
 	RNA_def_property_int_funcs(prop, "rna_WorkspaceTool_index_get", NULL, NULL);
 
+	prop = RNA_def_property(srna, "space_type", PROP_ENUM, PROP_NONE);
+	RNA_def_property_enum_sdna(prop, NULL, "space_type");
+	RNA_def_property_clear_flag(prop, PROP_EDITABLE);
+	RNA_def_property_enum_items(prop, rna_enum_space_type_items);
+	RNA_def_property_ui_text(prop, "Space Type", "");
+
 	RNA_api_workspace_tool(srna);
 }
 
