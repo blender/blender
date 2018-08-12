444a0202d40705b6a6f21e59e883518be577cf4e
Campbell Barton <ideasman42@gmail.com>
2018-08-12 13:17:10 +1000

    Gizmo: add blank gizmo definition
    Missed from 98c304e865f by accident.

--- source/blender/editors/gizmo_library/gizmo_types/blank3d_gizmo.c
+++ source/blender/editors/gizmo_library/gizmo_types/blank3d_gizmo.c
@@ -23,6 +23,14 @@
  * ***** END GPL LICENSE BLOCK *****
  */
 
+/** \file blank3d_gizmo.c
+ *  \ingroup wm
+ *
+ * \name Blank Gizmo
+ *
+ * \brief Gizmo to use as a fallback (catch events).
+ */
+
 #include "BLI_math.h"
 
 #include "BKE_context.h"
@@ -37,6 +45,45 @@
 #include "../gizmo_geometry.h"
 #include "../gizmo_library_intern.h"
 
+
+static void gizmo_blank_draw(const bContext *UNUSED(C), wmGizmo *UNUSED(gz))
+{
+	/* pass */
+}
+
+static int gizmo_blank_invoke(
+        bContext *UNUSED(C), wmGizmo *UNUSED(gz), const wmEvent *UNUSED(event))
+{
+	return OPERATOR_RUNNING_MODAL;
+}
+
+static int gizmo_blank_test_select(
+        bContext *UNUSED(C), wmGizmo *UNUSED(gz), const int UNUSED(mval[2]))
+{
+	return 0;
+}
+
+/* -------------------------------------------------------------------- */
+/** \name Blank Gizmo API
+ *
+ * \{ */
+
+static void GIZMO_GT_blank_3d(wmGizmoType *gzt)
+{
+	/* identifiers */
+	gzt->idname = "GIZMO_GT_blank_3d";
+
+	/* api callbacks */
+	gzt->draw = gizmo_blank_draw;
+	gzt->invoke = gizmo_blank_invoke;
+	gzt->test_select = gizmo_blank_test_select;
+
+	gzt->struct_size = sizeof(wmGizmo);
+}
+
 void ED_gizmotypes_blank_3d(void)
 {
+	WM_gizmotype_append(GIZMO_GT_blank_3d);
 }
+
+/** \} */
