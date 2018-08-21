5dd64b3c88beb2ba3de6d38255cf20570cf49870
Kenneth Perry <thothonegan>
2018-08-21 12:37:20 +0200

    Python API: expose missing LAYER_MEMBER property type registration.
    Differential Revision: https://developer.blender.org/D3628

--- source/blender/python/intern/bpy_props.c
+++ source/blender/python/intern/bpy_props.c
@@ -133,6 +133,7 @@ static const EnumPropertyItem property_subtype_array_items[] = {
 	{PROP_XYZ, "XYZ", 0, "XYZ", ""},
 	{PROP_COLOR_GAMMA, "COLOR_GAMMA", 0, "Color Gamma", ""},
 	{PROP_LAYER, "LAYER", 0, "Layer", ""},
+	{PROP_LAYER_MEMBER, "LAYER_MEMBER", 0, "Layer Member", ""},
 
 	{PROP_NONE, "NONE", 0, "None", ""},
 	{0, NULL, 0, NULL, NULL}};
@@ -140,7 +141,7 @@ static const EnumPropertyItem property_subtype_array_items[] = {
 #define BPY_PROPDEF_SUBTYPE_ARRAY_DOC \
 "   :arg subtype: Enumerator in ['COLOR', 'TRANSLATION', 'DIRECTION', " \
                                 "'VELOCITY', 'ACCELERATION', 'MATRIX', 'EULER', 'QUATERNION', 'AXISANGLE', " \
-                                "'XYZ', 'COLOR_GAMMA', 'LAYER', 'NONE'].\n" \
+                                "'XYZ', 'COLOR_GAMMA', 'LAYER', 'LAYER_MEMBER', 'NONE'].\n" \
 "   :type subtype: string\n"
 
 /* PyObject's */
