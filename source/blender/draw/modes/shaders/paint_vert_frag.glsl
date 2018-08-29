46fc3f39c5992d4daaabfd6ac9df29abcba2a831
Campbell Barton <ideasman42@gmail.com>
2018-08-29 12:14:54 +1000

    Weight Paint: use black for unselected vertices
    Contrast for selected vertices mode wasn't very strong.

--- source/blender/draw/modes/shaders/paint_vert_frag.glsl
+++ source/blender/draw/modes/shaders/paint_vert_frag.glsl
@@ -15,7 +15,7 @@ void main()
 	float dist_squared = dot(centered, centered);
 	const float rad_squared = 0.25;
 	const vec4 colSel = vec4(1.0, 1.0, 1.0, 1.0);
-	const vec4 colUnsel = vec4(0.5, 0.5, 0.5, 1.0);
+	const vec4 colUnsel = vec4(0.0, 0.0, 0.0, 1.0);
 
 	// round point with jaggy edges
 	if (dist_squared > rad_squared) {
