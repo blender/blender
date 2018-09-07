ee6b95f7e0c19c47e8e4a550e0b0bb51de79508e
Campbell Barton <ideasman42@gmail.com>
2018-09-07 12:21:24 +1000

    UI: move toolbar arrow closer to button edge
    Could overlap icons.

--- source/blender/gpu/shaders/gpu_shader_2D_widget_base_vert.glsl
+++ source/blender/gpu/shaders/gpu_shader_2D_widget_base_vert.glsl
@@ -74,10 +74,12 @@ const vec2 triavec[43] = vec2[43](
 	vec2(-0.130000, -0.170000),  vec2(0.720000, 0.430000),   vec2(0.530000, 0.590000),
 
 	/* ROUNDBOX_TRIA_HOLD_ACTION_ARROW - hold action arrows */
-#define OY (-0.2 / 2)
+#define OX (-0.32)
+#define OY (0.1)
 #define SC (0.35 * 2)
 //	vec2(-0.5 + SC, 1.0 + OY),  vec2( 0.5, 1.0 + OY),  vec2( 0.5, 0.0 + OY + SC),
-	vec2( 0.5 - SC, 1.0 + OY),  vec2(-0.5, 1.0 + OY),  vec2(-0.5, 0.0 + OY + SC)
+	vec2((0.5 - SC) + OX, 1.0 + OY), vec2(-0.5 + OX, 1.0 + OY), vec2(-0.5 + OX, SC + OY)
+#undef OX
 #undef OY
 #undef SC
 );
