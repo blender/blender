f440fdec84453a3dec6f6d6cdff594cf825b3d7b
Sergey Sharybin <sergey.vfx@gmail.com>
2018-09-07 16:40:54 +0200

    Fix operators from W menu for the API changes

--- release/scripts/startup/bl_operators/wm.py
+++ release/scripts/startup/bl_operators/wm.py
@@ -831,7 +831,7 @@ class WM_OT_context_modal_mouse(Operator):
                     header_text = header_text % eval("item.%s" % self.data_path_item)
                 else:
                     header_text = (self.header_text % delta) + " (delta)"
-                context.area.header_text_set(header_text)
+                context.area.header_text_set(text=header_text)
 
         elif 'LEFTMOUSE' == event_type:
             item = next(iter(self._values.keys()))
