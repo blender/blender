dc29ae579f2dcdab0326ab3fcc60dc1f488f029d
Campbell Barton <ideasman42@gmail.com>
2018-09-08 10:50:53 +1000

    UI: make extrude gizmo similar size to transform

--- source/blender/editors/mesh/editmesh_extrude.c
+++ source/blender/editors/mesh/editmesh_extrude.c
@@ -368,8 +368,8 @@ void MESH_OT_extrude_repeat(wmOperatorType *ot)
 static const float extrude_button_scale = 0.15f;
 static const float extrude_button_offset_scale = 1.5f;
 static const float extrude_arrow_scale = 1.0f;
-static const float extrude_arrow_xyz_axis_scale = 1.0f;
-static const float extrude_arrow_normal_axis_scale = 1.75f;
+static const float extrude_arrow_xyz_axis_scale = 0.4f;
+static const float extrude_arrow_normal_axis_scale = 1.0f;
 
 static const uchar shape_plus[] = {
 	0x5f, 0xfb, 0x40, 0xee, 0x25, 0xda, 0x11, 0xbf, 0x4, 0xa0, 0x0, 0x80, 0x4, 0x5f, 0x11,
