ef32be25c1796da9957f3a146f89ff8035db0c07
Campbell Barton <ideasman42@gmail.com>
2018-09-12 07:27:17 +1000

    Fix scale-cage editmode pivot point

--- source/blender/editors/transform/transform_gizmo_3d.c
+++ source/blender/editors/transform/transform_gizmo_3d.c
@@ -1805,6 +1805,9 @@ static void WIDGETGROUP_xform_cage_refresh(const bContext *C, wmGizmoGroup *gzgr
 		mid_v3_v3v3(gz->matrix_offset[3], rv3d->tw_axis_max, rv3d->tw_axis_min);
 		mul_m3_v3(rv3d->tw_axis_matrix, gz->matrix_offset[3]);
 
+		float matrix_offset_global[4][4];
+		mul_m4_m4m4(matrix_offset_global, gz->matrix_space, gz->matrix_offset);
+
 		PropertyRNA *prop_center_override = NULL;
 		float center[3];
 		float center_global[3];
@@ -1819,7 +1822,7 @@ static void WIDGETGROUP_xform_cage_refresh(const bContext *C, wmGizmoGroup *gzgr
 					if (prop_center_override == NULL) {
 						prop_center_override = RNA_struct_find_property(&mpop->ptr, "center_override");
 					}
-					mul_v3_m4v3(center_global, gz->matrix_offset, center);
+					mul_v3_m4v3(center_global, matrix_offset_global, center);
 					RNA_property_float_set_array(&mpop->ptr, prop_center_override, center_global);
 					i++;
 				}
