f088bbae6af2224b49103d8cba5129f92796ffa6
Sergey Sharybin <sergey.vfx@gmail.com>
2018-09-12 11:00:38 +0200

    Cleanup: More correct terminology work
    Matches threading API.

--- source/blender/render/intern/source/external_engine.c
+++ source/blender/render/intern/source/external_engine.c
@@ -453,7 +453,7 @@ rcti* RE_engine_get_current_tiles(Render *re, int *r_total_tiles, bool *r_needs_
 		if (pa->status == PART_STATUS_IN_PROGRESS) {
 			if (total_tiles >= allocation_size) {
 				/* Just in case we're using crazy network rendering with more
-				 * slaves as BLENDER_MAX_THREADS.
+				 * workers than BLENDER_MAX_THREADS.
 				 */
 				allocation_size += allocation_step;
 				if (tiles == tiles_static) {
