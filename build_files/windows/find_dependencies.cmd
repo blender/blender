424839e3c2d950fce315f9eefcd1334356f43f8d
Ray Molenkamp <github@lazydodo.com>
2018-09-19 10:16:46 -0600

    make.bat: fix build error when there are spaces in the path to svn/git/cmake

--- build_files/windows/find_dependencies.cmd
+++ build_files/windows/find_dependencies.cmd
@@ -3,9 +3,9 @@ for %%X in (svn.exe) do (set SVN=%%~$PATH:X)
 for %%X in (cmake.exe) do (set CMAKE=%%~$PATH:X)
 for %%X in (git.exe) do (set GIT=%%~$PATH:X)
 if NOT "%verbose%" == "" (
-	echo svn   : %SVN%
-	echo cmake : %CMAKE%
-	echo git   : %GIT%
+	echo svn   : "%SVN%"
+	echo cmake : "%CMAKE%"
+	echo git   : "%GIT%"
 )
 if "%CMAKE%" == "" (
 	echo Cmake not found in path, required for building, exiting...
