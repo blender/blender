3816592fc688c0fd76e82e18f63cb7b18c547a0d
Roel Koster <kostex>
2018-09-25 14:54:53 +0200

    Fix broken shift + A menu in curve edit mode.
    Differential Revision: https://developer.blender.org/D3699

--- source/blender/editors/curve/curve_ops.c
+++ source/blender/editors/curve/curve_ops.c
@@ -234,7 +234,7 @@ void ED_keymap_curve(wmKeyConfig *keyconf)
 	keymap = WM_keymap_ensure(keyconf, "Curve", 0, 0);
 	keymap->poll = ED_operator_editsurfcurve;
 
-	WM_keymap_add_menu(keymap, "VIEW3D_MT_edit_curve_add", AKEY, KM_PRESS, KM_SHIFT, 0);
+	WM_keymap_add_menu(keymap, "VIEW3D_MT_curve_add", AKEY, KM_PRESS, KM_SHIFT, 0);
 
 	WM_keymap_add_item(keymap, "CURVE_OT_handle_type_set", VKEY, KM_PRESS, 0, 0);
 
