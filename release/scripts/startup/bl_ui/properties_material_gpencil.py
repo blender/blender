074cbc403cb5498fddb1f963317fcd67b227c7c6
Brecht Van Lommel <brechtvanlommel@gmail.com>
2018-10-10 14:00:14 +0200

    Fix material properties Python error when there is no active object.

--- release/scripts/startup/bl_ui/properties_material_gpencil.py
+++ release/scripts/startup/bl_ui/properties_material_gpencil.py
@@ -157,11 +157,14 @@ class MATERIAL_PT_gpencil_surface(GPMaterialButtonsPanel, Panel):
     @classmethod
     def poll(cls, context):
         ob = context.object
+        if ob is None:
+            return False
+
         ma = context.object.active_material
         if ma is None or ma.grease_pencil is None:
             return False
 
-        return ob and ob.type == 'GPENCIL'
+        return ob.type == 'GPENCIL'
 
     def draw_header_preset(self, context):
         MATERIAL_PT_gpencil_material_presets.draw_panel_header(self.layout)
