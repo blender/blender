1fad887d9e22da97009f28288cc6f21cc32a6c37
Philipp Oeser <info@graphics-engineer.com>
2018-10-10 14:30:44 +0200

    Fix T57130: Crash when running 'set active object as camera' on an empty
    Reviewed By: brecht
    
    https://developer.blender.org/D3782

--- source/blender/draw/modes/object_mode.c
+++ source/blender/draw/modes/object_mode.c
@@ -634,7 +634,7 @@ static void OBJECT_engine_init(void *vedata)
 		}
 
 		float dist;
-		if (rv3d->persp == RV3D_CAMOB && v3d->camera) {
+		if (rv3d->persp == RV3D_CAMOB && v3d->camera && v3d->camera->type == OB_CAMERA) {
 			Object *camera_object = DEG_get_evaluated_object(draw_ctx->depsgraph, v3d->camera);
 			dist = ((Camera *)(camera_object->data))->clipend;
 		}
