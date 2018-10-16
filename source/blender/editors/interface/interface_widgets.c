90d1d39a95bd6219ce50a1dcdd597f27a59a23f3
Harley Acheson <harley>
2018-10-16 20:31:07 +0200

    UI: fix misaligned underline for menu item shortcut keys.

--- source/blender/editors/interface/interface_widgets.c
+++ source/blender/editors/interface/interface_widgets.c
@@ -1988,7 +1988,7 @@ static void widget_draw_text(uiFontStyle *fstyle, uiWidgetColors *wcol, uiBut *b
 					}
 
 					fixedbuf[ul_index] = '\0';
-					ul_advance = BLF_width(fstyle->uifont_id, fixedbuf, ul_index);
+					ul_advance = BLF_width(fstyle->uifont_id, fixedbuf, ul_index) + (1.0f * UI_DPI_FAC);
 
 					BLF_position(fstyle->uifont_id, rect->xmin + font_xofs + ul_advance, rect->ymin + font_yofs, 0.0f);
 					BLF_color4ubv(fstyle->uifont_id, (unsigned char *)wcol->text);
