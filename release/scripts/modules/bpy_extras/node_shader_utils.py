ce4cfbe108d0641832d86a731ea32f596220b7c5
Dalai Felinto <dfelinto@gmail.com>
2018-10-17 17:04:04 +0000

    Fix error on FBX export after material changes
    Update the addons repository as well.

--- release/scripts/modules/bpy_extras/node_shader_utils.py
+++ release/scripts/modules/bpy_extras/node_shader_utils.py
@@ -466,7 +466,7 @@ class PrincipledBSDFWrapper(ShaderWrapper):
 
 
     def normalmap_texture_get(self):
-        if not self.use_nodes or self.node_normalmap is None:
+        if not self.use_nodes or self.node_normalmap is None or self.node_normalmap == ...:
             return None
         return ShaderImageTextureWrapper(
             self, self.node_normalmap,
