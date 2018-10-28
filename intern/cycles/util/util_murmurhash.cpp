47953dee79202e1074ddfd0f85a2f5fa6a73d3dd
Brecht Van Lommel <brechtvanlommel@gmail.com>
2018-10-28 14:53:08 +0100

    Fix Linux build after Cryptomatte commit.

--- intern/cycles/util/util_murmurhash.cpp
+++ intern/cycles/util/util_murmurhash.cpp
@@ -21,20 +21,22 @@
  *
  */
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "util/util_algorithm.h"
 #include "util/util_murmurhash.h"
 
 #if defined(_MSC_VER)
-#  include <stdlib.h>
-#  define ROTL32(x,y)	_rotl(x,y)
-#  define ROTL64(x,y)	_rotl64(x,y)
+#  define ROTL32(x,y) _rotl(x,y)
+#  define ROTL64(x,y) _rotl64(x,y)
 #  define BIG_CONSTANT(x) (x)
 #else
 ccl_device_inline uint32_t rotl32(uint32_t x, int8_t r)
 {
 	return (x << r) | (x >> (32 - r));
 }
-#  define	ROTL32(x,y)	rotl32(x,y)
+#  define ROTL32(x,y) rotl32(x,y)
 #  define BIG_CONSTANT(x) (x##LLU)
 #endif
 
