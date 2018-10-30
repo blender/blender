09416fe6e1359ba18b84972e11a4c2c11b671de6
Roel Koster <kostex>
2018-10-30 18:58:13 +0100

    Fix outliner scrollbar overlapping icons.
    Differential Revision: https://developer.blender.org/D3650

--- source/blender/editors/space_outliner/outliner_intern.h
+++ source/blender/editors/space_outliner/outliner_intern.h
@@ -129,10 +129,10 @@ typedef enum {
 /* size constants */
 #define OL_Y_OFFSET 2
 
-#define OL_TOG_HIDEX            (UI_UNIT_X * 4.0f)
-#define OL_TOG_RESTRICT_SELECTX (UI_UNIT_X * 3.0f)
-#define OL_TOG_RESTRICT_VIEWX   (UI_UNIT_X * 2.0f)
-#define OL_TOG_RESTRICT_RENDERX UI_UNIT_X
+#define OL_TOG_HIDEX            (UI_UNIT_X * 4.0f + V2D_SCROLL_WIDTH)
+#define OL_TOG_RESTRICT_SELECTX (UI_UNIT_X * 3.0f + V2D_SCROLL_WIDTH)
+#define OL_TOG_RESTRICT_VIEWX   (UI_UNIT_X * 2.0f + V2D_SCROLL_WIDTH)
+#define OL_TOG_RESTRICT_RENDERX (UI_UNIT_X + V2D_SCROLL_WIDTH)
 
 #define OL_TOGW OL_TOG_HIDEX
 
