59f0db430a9b30de3e96646a2b9c81a6df5bd609
Campbell Barton <ideasman42@gmail.com>
2018-11-16 11:04:19 +1100

    Cleanup: remove 27X define

--- source/blender/windowmanager/WM_keymap.h
+++ source/blender/windowmanager/WM_keymap.h
@@ -150,12 +150,6 @@ char *WM_key_event_operator_string(
 
 const char *WM_bool_as_string(bool test);
 
-/* Minimal keymap, see: T55666, will eventually remove.
- * Keep this until final design is settled on. */
-#if 0
-#define USE_WM_KEYMAP_27X
-#endif
-
 #ifdef __cplusplus
 }
 #endif
