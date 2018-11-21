b93c81e0023624f62695b082e7e35a2b8c7b3b5a
Jacques Lucke <mail@jlucke.com>
2018-11-21 13:52:12 +0100

    Image Empties: always show in orthographic view by default

--- release/scripts/startup/bl_operators/object.py
+++ release/scripts/startup/bl_operators/object.py
@@ -927,9 +927,7 @@ class LoadBackgroundImage(LoadImageAsEmpty, Operator):
         obj.show_empty_image_backside = False
 
         if context.space_data.type == "VIEW_3D":
-            if context.space_data.region_3d.is_perspective:
-                obj.show_empty_image_orthographic = False
-            else:
+            if not context.space_data.region_3d.is_perspective:
                 obj.show_empty_image_perspective = False
 
 
