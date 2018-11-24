23d66c5d609ad9d113214ab0ec99668dded35ac3
Julian Eisel <eiseljulian@gmail.com>
2018-11-24 19:20:31 +0100

    Fix T58022: Changing workspace may change layout of User Pref window
    Logic to update child windows on workspace changes should simply ignore
    temporary child windows. Users opened those for a specific purpose (i.e. edit
    user preferences or show render result). Blender should not come in and
    repurpose it.

--- source/blender/windowmanager/intern/wm_window.c
+++ source/blender/windowmanager/intern/wm_window.c
@@ -2230,6 +2230,11 @@ void WM_window_set_active_workspace(bContext *C, wmWindow *win, WorkSpace *works
 
 	for (wmWindow *win_child = wm->windows.first; win_child; win_child = win_child->next) {
 		if (win_child->parent == win_parent) {
+			bScreen *screen = WM_window_get_active_screen(win_child);
+			/* Don't change temporary screens, they only serve a single purpose. */
+			if (screen->temp) {
+				continue;
+			}
 			ED_workspace_change(workspace, C, wm, win_child);
 		}
 	}
