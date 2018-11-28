78a6c2e12c8d144d10f759d05cbb3503f555883b
Campbell Barton <ideasman42@gmail.com>
2018-11-28 19:48:19 +1100

    Transform: use scene-orientation for MMB axis constraint
    Follows the change made for axis keys.

--- source/blender/editors/transform/transform.c
+++ source/blender/editors/transform/transform.c
@@ -1371,16 +1371,7 @@ int transformEvent(TransInfo *t, const wmEvent *event)
 							stopConstraint(t);
 						}
 						else {
-							if (event->shift) {
-								initSelectConstraint(t, t->spacemtx);
-							}
-							else {
-								/* bit hackish... but it prevents mmb select to print the orientation from menu */
-								float mati[3][3];
-								strcpy(t->spacename, "global");
-								unit_m3(mati);
-								initSelectConstraint(t, mati);
-							}
+							initSelectConstraint(t, t->spacemtx);
 							postSelectConstraint(t);
 						}
 					}
