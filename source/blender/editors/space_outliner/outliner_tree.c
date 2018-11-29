dc7f3123266ba98a905b0b4936fffbc13bddff84
Dalai Felinto <dfelinto@gmail.com>
2018-11-29 18:56:10 -0200

    Outliner: Gray out collections only when they are invisible
    Re-factor of 4521d3e7074d.

--- source/blender/editors/space_outliner/outliner_tree.c
+++ source/blender/editors/space_outliner/outliner_tree.c
@@ -1313,8 +1313,7 @@ static void outliner_add_layer_collections_recursive(
 
 		const bool exclude = (lc->flag & LAYER_COLLECTION_EXCLUDE) != 0;
 		if (exclude ||
-		    ((layer->runtime_flag & VIEW_LAYER_HAS_HIDE) &&
-		     !(lc->runtime_flag & LAYER_COLLECTION_HAS_VISIBLE_OBJECTS)))
+		    ((lc->runtime_flag & LAYER_COLLECTION_VISIBLE) == 0))
 		{
 			ten->flag |= TE_DISABLED;
 		}
