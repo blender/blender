18c9101acc58428fd02d6877c8b1c1711780826e
Campbell Barton <ideasman42@gmail.com>
2018-11-30 07:10:52 +1100

    Templates: tweak sculpt to work w/o opensubdiv

--- release/scripts/startup/bl_app_templates_system/Sculpting/__init__.py
+++ release/scripts/startup/bl_app_templates_system/Sculpting/__init__.py
@@ -9,9 +9,14 @@ def load_handler(dummy):
     if bpy.data.filepath == "":
         # Apply subdivision modifier on startup
         bpy.ops.object.mode_set(mode='OBJECT')
-        bpy.ops.object.modifier_apply(modifier="Subdivision")
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.transform.tosphere(value=1.0)
+        if bpy.app.opensubdiv.supported:
+            bpy.ops.object.modifier_apply(modifier="Subdivision")
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.transform.tosphere(value=1.0)
+        else:
+            bpy.ops.object.modifier_remove(modifier="Subdivision")
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.subdivide(number_cuts=6, smoothness=1.0)
         bpy.ops.object.mode_set(mode='SCULPT')
 
 def register():
