1119f1eb3b5f5e509238eb376102079a9fd150c4
Campbell Barton <ideasman42@gmail.com>
2018-11-30 13:33:17 +1100

    Fix sculpt app-template editing the users startup

--- release/scripts/startup/bl_app_templates_system/Sculpting/__init__.py
+++ release/scripts/startup/bl_app_templates_system/Sculpting/__init__.py
@@ -6,23 +6,20 @@ from bpy.app.handlers import persistent
 @persistent
 def load_handler(dummy):
     import bpy
-    if bpy.data.filepath == "":
-        # Apply subdivision modifier on startup
-        bpy.ops.object.mode_set(mode='OBJECT')
-        if bpy.app.opensubdiv.supported:
-            bpy.ops.object.modifier_apply(modifier="Subdivision")
-            bpy.ops.object.mode_set(mode='EDIT')
-            bpy.ops.transform.tosphere(value=1.0)
-        else:
-            bpy.ops.object.modifier_remove(modifier="Subdivision")
-            bpy.ops.object.mode_set(mode='EDIT')
-            bpy.ops.mesh.subdivide(number_cuts=6, smoothness=1.0)
-        bpy.ops.object.mode_set(mode='SCULPT')
+    # Apply subdivision modifier on startup
+    bpy.ops.object.mode_set(mode='OBJECT')
+    if bpy.app.opensubdiv.supported:
+        bpy.ops.object.modifier_apply(modifier="Subdivision")
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.transform.tosphere(value=1.0)
+    else:
+        bpy.ops.object.modifier_remove(modifier="Subdivision")
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.subdivide(number_cuts=6, smoothness=1.0)
+    bpy.ops.object.mode_set(mode='SCULPT')
 
 def register():
-    import bpy
-    bpy.app.handlers.load_post.append(load_handler)
+    bpy.app.handlers.load_factory_startup_post.append(load_handler)
 
 def unregister():
-    import bpy
-    bpy.app.handlers.load_post.remove(load_handler)
+    bpy.app.handlers.load_factory_startup_post.remove(load_handler)
