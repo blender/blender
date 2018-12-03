dc26c5e1ad98b47675ee9345af301a937905808d
Bastien Montagne <montagne29@wanadoo.fr>
2018-12-03 20:06:35 +0100

    Fix T58635: 2.8 - "3D Markers to Mesh" error.

--- release/scripts/startup/bl_operators/clip.py
+++ release/scripts/startup/bl_operators/clip.py
@@ -299,7 +299,7 @@ class CLIP_OT_bundles_to_mesh(Operator):
         if camera:
             reconstruction = tracking_object.reconstruction
             framenr = scene.frame_current - clip.frame_start + 1
-            reconstructed_matrix = reconstruction.cameras.matrix_from_frame(framenr)
+            reconstructed_matrix = reconstruction.cameras.matrix_from_frame(frame=framenr)
             matrix = camera.matrix_world @ reconstructed_matrix.inverted()
 
         for track in tracking_object.tracks:
