c86e828eee8b7e7e0cde4fa211991b6ebac1ff0f
Bastien Montagne <montagne29@wanadoo.fr>
2018-12-04 12:04:27 +0100

    Fix (IRC reported) missing update for RNA Object's matrix_local property.
    Reported by @zeffii, thanks!

--- source/blender/makesrna/intern/rna_object.c
+++ source/blender/makesrna/intern/rna_object.c
@@ -2404,7 +2404,7 @@ static void rna_def_object(BlenderRNA *brna)
 	                         "WARNING: Only takes into account 'Object' parenting, so e.g. in case of bone parenting "
 	                         "you get a matrix relative to the Armature object, not to the actual parent bone");
 	RNA_def_property_float_funcs(prop, "rna_Object_matrix_local_get", "rna_Object_matrix_local_set", NULL);
-	RNA_def_property_update(prop, NC_OBJECT | ND_TRANSFORM, NULL);
+	RNA_def_property_update(prop, NC_OBJECT | ND_TRANSFORM, "rna_Object_internal_update");
 
 	prop = RNA_def_property(srna, "matrix_basis", PROP_FLOAT, PROP_MATRIX);
 	RNA_def_property_multi_array(prop, 2, rna_matrix_dimsize_4x4);
