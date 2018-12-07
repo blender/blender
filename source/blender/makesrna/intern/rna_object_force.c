497a9ef7a39608d62df83c10e209f22809e94e6e
Sebastian Parborg <zeddb>
2018-12-07 01:13:59 +0100

    Fix T58750: missing fallof power tooltip.
    Differential Revision: https://developer.blender.org/D4042

--- source/blender/makesrna/intern/rna_object_force.c
+++ source/blender/makesrna/intern/rna_object_force.c
@@ -1266,7 +1266,7 @@ static void rna_def_field(BlenderRNA *brna)
 	prop = RNA_def_property(srna, "falloff_power", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "f_power");
 	RNA_def_property_range(prop, 0.0f, 10.0f);
-	RNA_def_property_ui_text(prop, "Falloff Power", "");
+	RNA_def_property_ui_text(prop, "Falloff Power", "How quickly strength falls off with distance from the force field");
 	RNA_def_property_update(prop, 0, "rna_FieldSettings_update");
 
 	prop = RNA_def_property(srna, "distance_min", PROP_FLOAT, PROP_NONE);
