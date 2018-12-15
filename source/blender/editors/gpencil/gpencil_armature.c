b73529fafd63d4d8326001482f44e7eb578a5f77
Antonioya <blendergit@gmail.com>
2018-12-15 09:36:08 +0100

    GP: Add NULL checking for object

--- source/blender/editors/gpencil/gpencil_armature.c
+++ source/blender/editors/gpencil/gpencil_armature.c
@@ -491,6 +491,10 @@ bool ED_gpencil_add_armature_weights(
 	Main *bmain = CTX_data_main(C);
 	Scene *scene = CTX_data_scene(C);
 
+	if (ob == NULL) {
+		return false;
+	}
+
 	/* if no armature modifier, add a new one */
 	GpencilModifierData *md = BKE_gpencil_modifiers_findByType(ob, eGpencilModifierType_Armature);
 	if (md == NULL) {
