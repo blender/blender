8c0a77b8949f05f97f350ff496a0113b8d0e2af7
Sebastian Parborg <zeddb>
2019-01-07 10:20:20 +1100

    Fix T59944: Template operator_mesh_uv.py fails

--- release/scripts/templates_py/operator_mesh_uv.py
+++ release/scripts/templates_py/operator_mesh_uv.py
@@ -8,7 +8,6 @@ def main(context):
     bm = bmesh.from_edit_mesh(me)
 
     uv_layer = bm.loops.layers.uv.verify()
-    bm.faces.layers.tex.verify()  # currently blender needs both layers.
 
     # adjust UVs
     for f in bm.faces:
