24a84c4742576ab1ade54933ae95c1cb983a5f92
Campbell Barton <ideasman42@gmail.com>
2019-01-08 23:46:55 +1100

    Fix T60314: Missing strength w/ weight gradient

--- release/scripts/startup/bl_ui/space_toolsystem_toolbar.py
+++ release/scripts/startup/bl_ui/space_toolsystem_toolbar.py
@@ -934,7 +934,10 @@ class _defs_weight_paint:
             brush = context.tool_settings.weight_paint.brush
             if brush is not None:
                 from .properties_paint_common import UnifiedPaintPanel
-                UnifiedPaintPanel.prop_unified_weight(layout, context, brush, "weight", slider=True, text="Weight")
+                UnifiedPaintPanel.prop_unified_weight(
+                    layout, context, brush, "weight", slider=True, text="Weight")
+                UnifiedPaintPanel.prop_unified_strength(
+                    layout, context, brush, "strength", slider=True, text="Strength")
             props = tool.operator_properties("paint.weight_gradient")
             layout.prop(props, "type")
 
