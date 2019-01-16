fcbbfb7789890624cdfb78e2b6036414d925616b
Sergey Sharybin <sergey.vfx@gmail.com>
2019-01-16 11:39:30 +0100

    Fix crash creating new file after modifications
    Seems to be caused by cae3750 which changed free() function used
    by bmain free to the one which does dependency graph tag. We do
    no want to do any tags here.

--- source/blender/blenkernel/intern/main.c
+++ source/blender/blenkernel/intern/main.c
@@ -65,7 +65,10 @@ void BKE_main_free(Main *mainvar)
 	int a;
 
 	/* Since we are removing whole main, no need to bother 'properly' (and slowly) removing each ID from it. */
-	const int free_flag = LIB_ID_FREE_NO_MAIN | LIB_ID_FREE_NO_UI_USER | LIB_ID_FREE_NO_USER_REFCOUNT;
+	const int free_flag = (LIB_ID_FREE_NO_MAIN |
+	                       LIB_ID_FREE_NO_UI_USER |
+	                       LIB_ID_FREE_NO_USER_REFCOUNT |
+	                       LIB_ID_FREE_NO_DEG_TAG);
 
 	MEM_SAFE_FREE(mainvar->blen_thumb);
 
