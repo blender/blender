2b0fb0d7bfb84cc5d77cdaa2e89c8fd5d8bd5e59
William Reynish <billrey@me.com>
2019-01-21 17:20:43 +0100

    Fix keymap preferences UI layout issues for modifier keys.

--- release/scripts/modules/rna_keymap_ui.py
+++ release/scripts/modules/rna_keymap_ui.py
@@ -190,11 +190,11 @@ def draw_kmi(display_keymaps, kc, km, kmi, layout, level):
 
             subrow = sub.row()
             subrow.scale_x = 0.75
-            subrow.prop(kmi, "any")
-            subrow.prop(kmi, "shift")
-            subrow.prop(kmi, "ctrl")
-            subrow.prop(kmi, "alt")
-            subrow.prop(kmi, "oskey", text="Cmd")
+            subrow.prop(kmi, "any", toggle=True)
+            subrow.prop(kmi, "shift", toggle=True)
+            subrow.prop(kmi, "ctrl", toggle=True)
+            subrow.prop(kmi, "alt", toggle=True)
+            subrow.prop(kmi, "oskey", text="Cmd", toggle=True)
             subrow.prop(kmi, "key_modifier", text="", event=True)
 
         # Operator properties
