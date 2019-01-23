dc84a03a954ded7ebd6a3f6156c60f5cd1cf4c4f
Campbell Barton <ideasman42@gmail.com>
2019-01-23 11:28:59 +1100

    License: add missing GPL headers
    Information taken from related files committed at the time.

--- source/blender/modifiers/intern/MOD_surfacedeform.c
+++ source/blender/modifiers/intern/MOD_surfacedeform.c
@@ -1,3 +1,27 @@
+/*
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software  Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Copyright 2017, Blender Foundation.
+ * Contributor(s): Luca Rood
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ *
+ */
+
 #include "DNA_mesh_types.h"
 #include "DNA_meshdata_types.h"
 #include "DNA_object_types.h"
