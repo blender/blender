87aa456ea516f49c6029cf30f8f0dff16cbb58bc
Campbell Barton <ideasman42@gmail.com>
2019-02-04 19:50:46 +1100

    Fix T61172: Crash in texture paint undo/redo
    Mixing texture paint undo w/ memfile undo was crashing.
    
    The gputextures and render result are written to the FileData.imamap
    but weren't read back.

--- source/blender/blenloader/intern/readfile.c
+++ source/blender/blenloader/intern/readfile.c
@@ -3938,6 +3938,12 @@ static void direct_link_image(FileData *fd, Image *ima)
 		}
 		ima->rr = NULL;
 	}
+	else {
+		for (int i = 0; i < TEXTARGET_COUNT; i++) {
+			ima->gputexture[i] = newimaadr(fd, ima->gputexture[i]);
+		}
+		ima->rr = newimaadr(fd, ima->rr);
+	}
 
 	/* undo system, try to restore render buffers */
 	link_list(fd, &(ima->renderslots));
