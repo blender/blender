351b24ac142d8f3b8f067b004e055018399eb469
Campbell Barton <ideasman42@gmail.com>
2019-02-08 19:18:35 +1100

    Fix T58625: Knife project fails w/ curves

--- source/blender/editors/mesh/editmesh_knife_project.c
+++ source/blender/editors/mesh/editmesh_knife_project.c
@@ -69,7 +69,8 @@ static LinkNode *knifeproject_poly_from_object(const bContext *C, Scene *scene, 
 		me_eval_needs_free = false;
 	}
 	else if (ELEM(ob->type, OB_FONT, OB_CURVE, OB_SURF)) {
-		me_eval = BKE_mesh_new_nomain_from_curve(ob);
+		Object *ob_eval = DEG_get_evaluated_object(depsgraph, ob);
+		me_eval = BKE_mesh_new_nomain_from_curve(ob_eval);
 		me_eval_needs_free = true;
 	}
 	else {
