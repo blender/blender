d07cde8de8ecf388c36115d137b8809ceb07f836
Clément Foucault <foucault.clem@gmail.com>
2019-02-11 14:37:19 +0100

    Fix T61374 : vertex not visible/selectable if it is at objects origin
    It was caused by Null normal if the vertex is at origin. Just add a small
    bias to avoid this case.

--- source/blender/draw/modes/shaders/edit_mesh_overlay_vert.glsl
+++ source/blender/draw/modes/shaders/edit_mesh_overlay_vert.glsl
@@ -77,7 +77,7 @@ void main()
 #if !defined(FACE) && !defined(EDGE_DECORATION)
 	/* Facing based color blend */
 	vec4 vpos = ModelViewMatrix * vec4(pos, 1.0);
-	vec3 view_normal = normalize(NormalMatrix * vnor);
+	vec3 view_normal = normalize(NormalMatrix * vnor + 1e-4);
 	vec3 view_vec = (ProjectionMatrix[3][3] == 0.0)
 		? normalize(vpos.xyz)
 		: vec3(0.0, 0.0, 1.0);
