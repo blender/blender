349357a7bdffcf82705598d3cfc203812dd6d9a3
Francesco Siddi <francesco.siddi@gmail.com>
2019-02-14 18:16:18 +0100

    UI: reword error message when Python script fails.
    Differential Revision: https://developer.blender.org/D4353

--- source/blender/editors/space_text/text_ops.c
+++ source/blender/editors/space_text/text_ops.c
@@ -613,7 +613,7 @@ static int text_run_script(bContext *C, ReportList *reports)
 			}
 		}
 
-		BKE_report(reports, RPT_ERROR, "Python script fail, look in the console for now...");
+		BKE_report(reports, RPT_ERROR, "Python script failed, check the message in the system console");
 
 		return OPERATOR_FINISHED;
 	}
