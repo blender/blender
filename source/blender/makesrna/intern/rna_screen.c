03ede79c4fbee3b8c97259b6a6cc1d6de7c4dc34
Campbell Barton <ideasman42@gmail.com>
2019-02-16 21:25:29 +1100

    Fix T61593: Python can't access HUD region-type

--- source/blender/makesrna/intern/rna_screen.c
+++ source/blender/makesrna/intern/rna_screen.c
@@ -39,6 +39,7 @@ const EnumPropertyItem rna_enum_region_type_items[] = {
 	{RGN_TYPE_TOOLS, "TOOLS", 0, "Tools", ""},
 	{RGN_TYPE_TOOL_PROPS, "TOOL_PROPS", 0, "Tool Properties", ""},
 	{RGN_TYPE_PREVIEW, "PREVIEW", 0, "Preview", ""},
+	{RGN_TYPE_HUD, "HUD", 0, "Floating Region", ""},
 	{RGN_TYPE_NAV_BAR, "NAVIGATION_BAR", 0, "Navigation Bar", ""},
 	{RGN_TYPE_EXECUTE, "EXECUTE", 0, "Execute Buttons", ""},
 	{0, NULL, 0, NULL, NULL},
