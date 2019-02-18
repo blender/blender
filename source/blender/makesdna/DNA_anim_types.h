c3ceefb6715bc17fd3a6a0f409eb27d50695681b
Sergey Sharybin <sergey.vfx@gmail.com>
2019-02-18 16:01:31 +0100

    Cleanup: Remove unused field

--- source/blender/makesdna/DNA_anim_types.h
+++ source/blender/makesdna/DNA_anim_types.h
@@ -567,8 +567,7 @@ typedef struct FCurve {
 		/* value cache + settings */
 	/** Value stored from last time curve was evaluated (not threadsafe, debug display only!). */
 	float curval;
-	/* Value which comes from original DNA ddatablock at a time f-curve was evaluated. */
-	float orig_dna_val;
+	char pad2[4];
 	/** User-editable settings for this curve. */
 	short flag;
 	/** Value-extending mode for this curve (does not cover). */
