1d2fc413c89d434f5d5e140cd01522e562c10ecd
Ray Molenkamp <github@lazydodo.com>
2019-02-22 20:49:22 -0700

    make.bat: Preliminary Visual Studio 2019 support.
    VS2019 is binary compatible with the existing vc14 libraries and no
    new libraries libs are required in svn.
    
    VS2019 support requires cmake 3.14.
    
    VS2019 is still in pre-release state, you are required to explicitly
    select the pre-release version by using:
    
    make full 2019pre

--- build_files/windows/autodetect_msvc.cmd
+++ build_files/windows/autodetect_msvc.cmd
@@ -3,6 +3,9 @@ echo No explicit msvc version requested, autodetecting version.
 call "%~dp0\detect_msvc2017.cmd"
 if %ERRORLEVEL% EQU 0 goto DetectionComplete
 
+call "%~dp0\detect_msvc2019.cmd"
+if %ERRORLEVEL% EQU 0 goto DetectionComplete
+
 call "%~dp0\detect_msvc2015.cmd"
 if %ERRORLEVEL% EQU 0 goto DetectionComplete
 
