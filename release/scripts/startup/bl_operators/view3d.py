b64856f308ad128f23b6ac5172c7f89f7c6cfdc3
Campbell Barton <ideasman42@gmail.com>
2019-02-27 02:05:18 +1100

    Fix T61637: Edge extrude always uses normal orientation

--- release/scripts/startup/bl_operators/view3d.py
+++ release/scripts/startup/bl_operators/view3d.py
@@ -101,8 +101,10 @@ class VIEW3D_OT_edit_mesh_extrude_move(Operator):
             bpy.ops.mesh.extrude_region_move(
                 'INVOKE_REGION_WIN',
                 TRANSFORM_OT_translate={
-                    "constraint_orientation": 'NORMAL',
-                    # not a popular choice, too restrictive for retopo.
+                    # Don't set the constraint axis since users will expect MMB
+                    # to use the user setting, see: T61637
+                    # "constraint_orientation": 'NORMAL',
+                    # Not a popular choice, too restrictive for retopo.
                     # "constraint_axis": (True, True, False)})
                     "constraint_axis": (False, False, False),
                 })
