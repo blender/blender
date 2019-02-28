175cb0a0af9b9d165827f4f9d57f573e00f5cc55
Antonioya <blendergit@gmail.com>
2019-02-28 12:23:58 +0100

    GPencil: Remove comment line added by error to header text
    This line was added accidentally in the header text.

--- source/blender/draw/engines/gpencil/gpencil_engine.c
+++ source/blender/draw/engines/gpencil/gpencil_engine.c
@@ -4,7 +4,6 @@
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
