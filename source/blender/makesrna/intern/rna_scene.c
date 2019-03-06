bc8d1feb4d585ab617079c9b994a8cf8e0772e2b
William Reynish <billrey@me.com>
2019-03-06 23:19:48 +0100

    Units: Fix unreported. Audio volume is not a percentage

--- source/blender/makesrna/intern/rna_scene.c
+++ source/blender/makesrna/intern/rna_scene.c
@@ -6705,7 +6705,7 @@ void RNA_def_scene(BlenderRNA *brna)
 	RNA_def_property_ui_text(prop, "Distance Model", "Distance model for distance attenuation calculation");
 	RNA_def_property_update(prop, NC_SCENE, "rna_Scene_listener_update");
 
-	prop = RNA_def_property(srna, "audio_volume", PROP_FLOAT, PROP_PERCENTAGE);
+	prop = RNA_def_property(srna, "audio_volume", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "audio.volume");
 	RNA_def_property_range(prop, 0.0f, 100.0f);
 	RNA_def_property_ui_text(prop, "Volume", "Audio volume");
