4572354fe59f7ac56cb7ec72f530aa8f717c63be
Ray Molenkamp <github@lazydodo.com>
2019-03-12 11:36:44 -0600

    Windows: Add convenience logging batch files.
    To make triaging a little easier these batchfiles generate the
    debug log and sysinfo files and instruct the user how to attach
    them to their bug report
    
    Differential Revision: https://developer.blender.org/D4505
    
    Reviewers: brecht, zeddb

---
+++ release/windows/batch/blender_debug_gpu.cmd
@@ -0,0 +1,15 @@
+@echo off
+echo Starting blender with GPU debugging options, log files will be created
+echo in your temp folder, windows explorer will open after you close blender
+echo to help you find them.
+echo.
+echo If you report a bug on https://developer.blender.org you can attach these files
+echo by dragging them into the text area of your bug report, please include both
+echo blender_debug_output.txt and blender_system_info.txt in your report. 
+echo.
+pause
+mkdir "%temp%\blender\debug_logs" > NUL 2>&1
+echo.
+echo Starting blender and waiting for it to exit....
+blender --debug --debug-gpu --python-expr "import bpy; bpy.ops.wm.sysinfo(filepath=r'%temp%\blender\debug_logs\blender_system_info.txt')" > "%temp%\blender\debug_logs\blender_debug_output.txt" 2>&1 < %0
+explorer "%temp%\blender\debug_logs"
---
+++ release/windows/batch/blender_debug_gpu_glitchworkaround.cmd
@@ -0,0 +1,15 @@
+@echo off
+echo Starting blender with GPU debugging and glitch workaround options, log files 
+echo will be created in your temp folder, windows explorer will open after you 
+echo close blender to help you find them.
+echo.
+echo If you report a bug on https://developer.blender.org you can attach these files
+echo by dragging them into the text area of your bug report, please include both
+echo blender_debug_output.txt and blender_system_info.txt in your report. 
+echo.
+pause
+mkdir "%temp%\blender\debug_logs" > NUL 2>&1
+echo.
+echo Starting blender and waiting for it to exit....
+blender --debug --debug-gpu --debug-gpu-force-workarounds --python-expr "import bpy; bpy.ops.wm.sysinfo(filepath=r'%temp%\blender\debug_logs\blender_system_info.txt')" > "%temp%\blender\debug_logs\blender_debug_output.txt" 2>&1 < %0
+explorer "%temp%\blender\debug_logs"
