c0f96855e42e45895605fc1023242e114ea6e54b
Ray Molenkamp <github@lazydodo.com>
2019-03-21 21:25:20 -0600

    Fix T57580: Update windows installer package to have the GPL3 as EULA.

--- build_files/cmake/packaging.cmake
+++ build_files/cmake/packaging.cmake
@@ -86,7 +86,7 @@ if(WIN32)
 	set(CPACK_NSIS_MUI_ICON ${CMAKE_SOURCE_DIR}/release/windows/icons/winblender.ico)
 	set(CPACK_NSIS_COMPRESSOR "/SOLID lzma")
 
-	set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/release/text/GPL-license.txt)
+	set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/release/text/GPL3-license.txt)
 	set(CPACK_WIX_PRODUCT_ICON ${CMAKE_SOURCE_DIR}/release/windows/icons/winblender.ico)
 	set(CPACK_WIX_UPGRADE_GUID "B767E4FD-7DE7-4094-B051-3AE62E13A17A")
 
