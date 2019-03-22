530e7cc799e3f70e346bbcaf428575c35232775d
Adrian Newton <TFS>
2019-03-22 16:45:30 +0100

    UI: Rename editor "Compositing" to "Compositor"
    Reviewers: brecht, billreynish
    
    Differential Revision: https://developer.blender.org/D4480

--- source/blender/nodes/composite/node_composite_tree.c
+++ source/blender/nodes/composite/node_composite_tree.c
@@ -195,7 +195,7 @@ void register_node_tree_type_cmp(void)
 
 	tt->type = NTREE_COMPOSIT;
 	strcpy(tt->idname, "CompositorNodeTree");
-	strcpy(tt->ui_name, N_("Compositing"));
+	strcpy(tt->ui_name, N_("Compositor"));
 	tt->ui_icon = 0;    /* defined in drawnode.c */
 	strcpy(tt->ui_description, N_("Compositing nodes"));
 
