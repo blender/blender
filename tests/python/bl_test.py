3c6deb69f79afd3c3483b7a2eafb28a24a050712
Howard Trickey <howard.trickey@gmail.com>
2019-04-04 08:50:17 -0400

    Fix ply import tests.
    The function for clearing out objects in the startup file
    needed updating for 2.8 collections api.

--- tests/python/bl_test.py
+++ tests/python/bl_test.py
@@ -44,9 +44,9 @@ def replace_bpy_app_version():
 def clear_startup_blend():
     import bpy
 
-    for scene in bpy.data.scenes:
-        for obj in scene.objects:
-            scene.objects.unlink(obj)
+    for col in bpy.data.collections:
+        for obj in col.objects:
+            col.objects.unlink(obj)
 
 
 def blend_to_md5():
