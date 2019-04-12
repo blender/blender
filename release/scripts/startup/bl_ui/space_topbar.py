f3c313e77983edb84e1bcd93cc7db707259bff6e
Campbell Barton <ideasman42@gmail.com>
2019-04-12 12:58:31 +0200

    Revert "Rename Label instead of Name in Node editor using F2 key"
    This reverts commit 98b06c20a0d4a66e2d9f4496c788f3d0902ea7c6.
    
    RNA properties should give predictable results,
    the desired behavior can be added only in operator logic.

--- release/scripts/startup/bl_ui/space_topbar.py
+++ release/scripts/startup/bl_ui/space_topbar.py
@@ -1084,11 +1084,11 @@ class TOPBAR_PT_name(Panel):
                 row.prop(item, "name", text="")
                 found = True
         elif space_type == 'NODE_EDITOR':
-            layout.label(text="Node Label")
+            layout.label(text="Node Name")
             item = context.active_node
             if item:
                 row = row_with_icon(layout, 'NODE')
-                row.prop(item, "label", text="")
+                row.prop(item, "name", text="")
                 found = True
         else:
             if mode == 'POSE' or (mode == 'WEIGHT_PAINT' and context.pose_object):
