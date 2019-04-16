65aebd0c11f9fd820b4dda34c7053ed7a138bcb1
Campbell Barton <ideasman42@gmail.com>
2019-04-16 16:47:05 +0200

    GNUmakefile: move clang format utility
    Makes more sense to place in 'utils_maintenance'.

--- build_files/windows/format.cmd
+++ build_files/windows/format.cmd
@@ -1,6 +1,6 @@
 REM TODO(sergey): Switch to Python from libraries when available.
 set PYTHON="python.exe"
-set FORMAT_PATHS=%BLENDER_DIR%\source\tools\utils\clang_format_paths.py
+set FORMAT_PATHS=%BLENDER_DIR%\source\tools\utils_maintenance\clang_format_paths.py
 
 REM The formatting script expects clang-format to be in the current PATH.
 set PATH=%BUILD_VS_LIBDIR%\llvm\bin;%PATH%
