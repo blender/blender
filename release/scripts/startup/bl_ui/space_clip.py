01555d29fad42e5c404794990dd10fedd2dafb64
William Reynish <billrey@me.com>
2019-05-08 09:10:29 +0200

    UI: Remove Filters toggle in the Clip Editor Graph view
    This was trying to emulate the 2.79 Graph Editor.
    
    The toggle took up more room that the filter toggle it revealed, and it made the header buttons jump around
    
    Better to just have the filter toggles showing immediately.

--- release/scripts/startup/bl_ui/space_clip.py
+++ release/scripts/startup/bl_ui/space_clip.py
@@ -202,19 +202,12 @@ class CLIP_HT_header(Header):
 
                 row = layout.row(align=True)
 
-                if sc.show_filters:
-                    row.prop(sc, "show_filters", icon='DISCLOSURE_TRI_DOWN',
-                             text="Filters")
+                sub = row.row(align=True)
+                sub.active = clip.tracking.reconstruction.is_valid
+                sub.prop(sc, "show_graph_frames", icon='SEQUENCE', text="")
 
-                    sub = row.row(align=True)
-                    sub.active = clip.tracking.reconstruction.is_valid
-                    sub.prop(sc, "show_graph_frames", icon='SEQUENCE', text="")
-
-                    row.prop(sc, "show_graph_tracks_motion", icon='GRAPH', text="")
-                    row.prop(sc, "show_graph_tracks_error", icon='ANIM', text="")
-                else:
-                    row.prop(sc, "show_filters", icon='DISCLOSURE_TRI_RIGHT',
-                             text="Filters")
+                row.prop(sc, "show_graph_tracks_motion", icon='GRAPH', text="")
+                row.prop(sc, "show_graph_tracks_error", icon='ANIM', text="")
 
             elif sc.view == 'DOPESHEET':
                 dopesheet = tracking.dopesheet
