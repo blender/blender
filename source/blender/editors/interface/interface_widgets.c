eff273c0bc31fa321c35e132949eb8a6062952dc
Harley Acheson <harley.acheson@gmail.com>
2019-05-12 15:21:43 -0700

    Fix T64463: Visual Artifacts with ColorRamp
    This patch fixes an issue with the ColorRamp overflowing its drawing bounds
    
    Differential Revision: https://developer.blender.org/D4843
    
    Reviewed by Brecht Van Lommel

--- source/blender/editors/interface/interface_widgets.c
+++ source/blender/editors/interface/interface_widgets.c
@@ -4645,6 +4645,9 @@ void ui_draw_but(const bContext *C, ARegion *ar, uiStyle *style, uiBut *but, rct
         break;
 
       case UI_BTYPE_COLORBAND:
+        /* do not draw right to edge of rect */
+        rect->xmin += (0.25f * UI_UNIT_X);
+        rect->xmax -= (0.3f * UI_UNIT_X);
         ui_draw_but_COLORBAND(but, &tui->wcol_regular, rect);
         break;
 
