cf878a0f7c80d0a2d2b62f75920c8f34fe87c571
Campbell Barton <ideasman42@gmail.com>
2019-05-19 19:55:17 +1000

    Keymap: Space-L sets "Lasso" with Space tool activation

--- release/scripts/presets/keyconfig/keymap_data/blender_default.py
+++ release/scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -5057,7 +5057,7 @@ def km_popup_toolbar(_params):
         {"space_type": 'EMPTY', "region_type": 'TEMPORARY'},
         {"items": [
             op_tool("builtin.select", {"type": 'W', "value": 'PRESS'}),
-            op_tool("builtin.transform", {"type": 'T', "value": 'PRESS'}),
+            op_tool("builtin.select_lasso", {"type": 'L', "value": 'PRESS'}),
             op_tool("builtin.measure", {"type": 'M', "value": 'PRESS'}),
         ]},
     )
