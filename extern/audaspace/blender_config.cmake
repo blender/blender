c56133c84686048d278009d22e0a53ffd6b35be7
Jörg Müller <nexyon@gmail.com>
2019-05-22 12:14:51 +0200

    Fix T64480: Tweak needed to Audaspace CMake options
    The issue was that Audaspace options ended up in the cmake cache though
    they should not be there.
    
    Also reverting indentation change by @ideasman42.
    Thanks to @mont29 for reporting and helping with the fix.

--- extern/audaspace/blender_config.cmake
+++ extern/audaspace/blender_config.cmake
@@ -1,20 +1,24 @@
+# Do NOT use CACHE behavior here, as this has the same effect as defining an option,
+# it exposes the setting in main CMake config (which we do not want here).
+
 set(AUDASPACE_STANDALONE FALSE)
-set(BUILD_DEMOS FALSE CACHE BOOL "Build and install demos")
-set(SHARED_LIBRARY FALSE CACHE BOOL "Build Shared Library")
-set(WITH_C TRUE CACHE BOOL "Build C Module")
-set(WITH_DOCS FALSE CACHE BOOL "Build C++ HTML Documentation with Doxygen")
-set(WITH_FFMPEG ${WITH_CODEC_FFMPEG} CACHE BOOL "Build With FFMPEG")
-set(WITH_FFTW FALSE CACHE BOOL "Build With FFTW")
-set(WITH_LIBSNDFILE ${WITH_CODEC_SNDFILE} CACHE BOOL "Build With LibSndFile")
-set(SEPARATE_C FALSE CACHE BOOL "Build C Binding as separate library")
-set(PLUGIN_FFMPEG FALSE CACHE BOOL "Build FFMPEG Plugin")
-set(PLUGIN_JACK FALSE CACHE BOOL "Build JACK Plugin")
-set(PLUGIN_LIBSNDFILE FALSE CACHE BOOL "Build LibSndFile Plugin")
-set(PLUGIN_OPENAL FALSE CACHE BOOL "Build OpenAL Plugin")
-set(PLUGIN_SDL FALSE CACHE BOOL "Build SDL Plugin")
-set(WITH_PYTHON_MODULE FALSE CACHE BOOL "Build Python Module")
-set(DYNLOAD_JACK ${WITH_JACK_DYNLOAD} CACHE BOOL "Dynamically load JACK")
-set(WITH_BINDING_DOCS FALSE CACHE BOOL "Build C/Python HTML Documentation with Sphinx")
+set(BUILD_DEMOS FALSE)  # "Build and install demos"
+set(SHARED_LIBRARY FALSE)  # "Build Shared Library"
+set(WITH_C TRUE)  # "Build C Module"
+set(WITH_DOCS FALSE)  # "Build C++ HTML Documentation with Doxygen"
+set(WITH_FFMPEG ${WITH_CODEC_FFMPEG})  # "Build With FFMPEG"
+set(WITH_FFTW FALSE)  # "Build With FFTW"
+set(WITH_LIBSNDFILE ${WITH_CODEC_SNDFILE})  # "Build With LibSndFile"
+set(SEPARATE_C FALSE)  # "Build C Binding as separate library"
+set(PLUGIN_FFMPEG FALSE)  # "Build FFMPEG Plugin"
+set(PLUGIN_JACK FALSE)  # "Build JACK Plugin"
+set(PLUGIN_LIBSNDFILE FALSE)  # "Build LibSndFile Plugin"
+set(PLUGIN_OPENAL FALSE)  # "Build OpenAL Plugin"
+set(PLUGIN_SDL FALSE)  # "Build SDL Plugin"
+set(WITH_PYTHON_MODULE FALSE)  # "Build Python Module"
+set(DYNLOAD_JACK ${WITH_JACK_DYNLOAD})  # "Dynamically load JACK"
+set(WITH_BINDING_DOCS FALSE)  # "Build C/Python HTML Documentation with Sphinx"
+set(DEFAULT_PLUGIN_PATH "plugins")  # "Default plugin installation and loading path."
 set(FFMPEG_FOUND ${WITH_CODEC_FFMPEG})
 set(JACK_FOUND ${WITH_JACK})
 set(LIBSNDFILE_FOUND ${WITH_CODEC_SNDFILE})
@@ -23,9 +27,3 @@ set(PYTHONLIBS_FOUND TRUE)
 set(NUMPY_FOUND TRUE)
 set(NUMPY_INCLUDE_DIRS ${PYTHON_NUMPY_INCLUDE_DIRS})
 set(SDL_FOUND ${WITH_SDL})
-
-if(WIN32)
-  set(DEFAULT_PLUGIN_PATH "plugins" CACHE STRING "Default plugin installation and loading path.")
-endif()
-
-mark_as_advanced(BUILD_DEMOS SHARED_LIBRARY WITH_C WITH_DOCS WITH_FFMPEG WITH_FFTW WITH_LIBSNDFILE SEPARATE_C PLUGIN_FFMPEG PLUGIN_JACK PLUGIN_LIBSNDFILE PLUGIN_OPENAL PLUGIN_SDL WITH_PYTHON_MODULE DYNLOAD_JACK WITH_BINDING_DOCS DEFAULT_PLUGIN_PATH)
