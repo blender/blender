8389cc7e674bd54102a0244fa625ff4d49b678bf
Sergey Sharybin <sergey.vfx@gmail.com>
2019-05-23 10:02:37 +0200

    Fix T65023: Tracking marker color is not updated when using Copy Color
    Missing dependency graph update.
    
    Ideally need to introduce more clear ID_RECALC flag, and maybe go over all
    of the operators (some of them might not use dependency graph still).

--- source/blender/editors/space_clip/tracking_ops.c
+++ source/blender/editors/space_clip/tracking_ops.c
@@ -1625,6 +1625,7 @@ static int track_copy_color_exec(bContext *C, wmOperator *UNUSED(op))
     }
   }
 
+  DEG_id_tag_update(&clip->id, 0);
   WM_event_add_notifier(C, NC_MOVIECLIP | ND_DISPLAY, clip);
 
   return OPERATOR_FINISHED;
@@ -1806,6 +1807,7 @@ static int clean_tracks_exec(bContext *C, wmOperator *op)
     }
   }
 
+  DEG_id_tag_update(&clip->id, 0);
   BKE_tracking_dopesheet_tag_update(tracking);
 
   WM_event_add_notifier(C, NC_MOVIECLIP | ND_SELECT, clip);
