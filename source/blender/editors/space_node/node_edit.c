eed19d9f456c5ae6416c4d62d75a009f3a34bcc7
Philipp Oeser <info@graphics-engineer.com>
2019-05-27 18:31:02 +0200

    Fix T64253: missing update when moving nodegroup input/output sockets up or down
    Reviewers: brecht
    
    Maniphest Tasks: T64253
    
    Differential Revision: https://developer.blender.org/D4959

--- source/blender/editors/space_node/node_edit.c
+++ source/blender/editors/space_node/node_edit.c
@@ -2418,6 +2418,7 @@ static int ntree_socket_move_exec(bContext *C, wmOperator *op)
     }
   }
 
+  ntree->update |= NTREE_UPDATE_GROUP;
   ntreeUpdateTree(CTX_data_main(C), ntree);
 
   snode_notify(C, snode);
