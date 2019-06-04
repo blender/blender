ff7401b02c4b9e111c0632f33bf5b51fc63026f6
Clément Foucault <foucault.clem@gmail.com>
2019-06-04 12:52:00 +0200

    Fix T64214 Eevee: Issue with Emission node and Alpha blending
    Emission node was outputing alpha information but is was not
    supposed to. Match cycles reference.

--- source/blender/gpu/shaders/gpu_shader_material.glsl
+++ source/blender/gpu/shaders/gpu_shader_material.glsl
@@ -1797,14 +1797,12 @@ void node_wireframe_screenspace(float size, vec2 barycentric, out float fac)
 
 void node_emission(vec4 color, float strength, vec3 vN, out Closure result)
 {
-#ifndef VOLUMETRICS
-  color *= strength;
   result = CLOSURE_DEFAULT;
-  result.radiance = color.rgb;
-  result.opacity = color.a;
+#ifndef VOLUMETRICS
+  result.radiance = color.rgb * strength;
   result.ssr_normal = normal_encode(vN, viewCameraVec);
 #else
-  result = Closure(vec3(0.0), vec3(0.0), color.rgb * strength, 0.0);
+  result.emission = color.rgb * strength;
 #endif
 }
 
