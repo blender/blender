c257e1a2988df786950b75eedb9e3233f2e97631
Campbell Barton <ideasman42@gmail.com>
2019-06-06 14:42:51 +1000

    Fix use of removed Image.use_alpha property
    Change missing from 7aaa7aa9dd79b8c6

--- release/scripts/startup/bl_ui/properties_texture.py
+++ release/scripts/startup/bl_ui/properties_texture.py
@@ -438,7 +438,7 @@ class TEXTURE_PT_image_alpha(TextureTypePanel, Panel):
         tex = context.texture
 
         col = layout.column()
-        col.active = bool(tex.image and tex.image.use_alpha)
+        col.active = bool(tex.image and tex.image.alpha_mode != 'NONE')
         col.prop(tex, "use_calculate_alpha", text="Calculate")
         col.prop(tex, "invert_alpha", text="Invert")
 
