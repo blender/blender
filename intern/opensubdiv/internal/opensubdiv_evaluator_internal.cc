9f48a04799fb494520c1121d591e03dc1b01641f
Sergey Sharybin <sergey.vfx@gmail.com>
2019-06-21 14:58:47 +0200

    Fix T63766: Multiresolution behavior when using crease edge
    Switch to Gregory basis patches which are tangent continuous across their
    boundaries.
    
    Originally we've used BSpline basis patches to be more compatible with the
    old subdivision code, but a lot of things changed anyway.

--- intern/opensubdiv/internal/opensubdiv_evaluator_internal.cc
+++ intern/opensubdiv/internal/opensubdiv_evaluator_internal.cc
@@ -812,11 +812,8 @@ OpenSubdiv_EvaluatorInternal *openSubdiv_createEvaluatorInternal(
         StencilTableFactory::Create(*refiner, face_varying_stencil_options));
   }
   // Generate bi-cubic patch table for the limit surface.
-  // TODO(sergey): Ideally we would want to expose end-cap settings via
-  // C-API to make it more generic. Currently it matches old Blender's
-  // subsurf code.
   PatchTableFactory::Options patch_options(level);
-  patch_options.SetEndCapType(PatchTableFactory::Options::ENDCAP_BSPLINE_BASIS);
+  patch_options.SetEndCapType(PatchTableFactory::Options::ENDCAP_GREGORY_BASIS);
   patch_options.useInfSharpPatch = use_inf_sharp_patch;
   patch_options.generateFVarTables = has_face_varying_data;
   patch_options.generateFVarLegacyLinearPatches = false;
