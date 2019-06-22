dd7e1c23abd2c9af11ddfd6ffa5744c7d2e8e6c4
Campbell Barton <ideasman42@gmail.com>
2019-06-22 18:52:34 +1000

    Cleanup: use doxy groups

--- source/blender/windowmanager/intern/wm_window.c
+++ source/blender/windowmanager/intern/wm_window.c
@@ -125,7 +125,9 @@ static struct WMInitStruct {
     .native_pixels = true,
 };
 
-/* ******** win open & close ************ */
+/* -------------------------------------------------------------------- */
+/** \name Window Open & Close
+ * \{ */
 
 static void wm_window_set_drawable(wmWindowManager *wm, wmWindow *win, bool activate);
 static int wm_window_timer(const bContext *C);
@@ -352,6 +354,8 @@ wmWindow *wm_window_copy_test(bContext *C,
     return NULL;
   }
 }
+
+/** \} */
 
 /* -------------------------------------------------------------------- */
 /** \name Quit Confirmation Dialog
@@ -1602,10 +1606,14 @@ void wm_window_process_events(const bContext *C)
   }
 }
 
-/* **************** init ********************** */
+/* -------------------------------------------------------------------- */
+/** \name Ghost Init/Exit
+ * \{ */
 
-/* bContext can be null in background mode because we don't
- * need to event handling. */
+/**
+ * \note #bContext can be null in background mode because we don't
+ * need to event handling.
+ */
 void wm_ghost_init(bContext *C)
 {
   if (!g_system) {
@@ -1639,7 +1647,11 @@ void wm_ghost_exit(void)
   g_system = NULL;
 }
 
-/* **************** timer ********************** */
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Event Timer
+ * \{ */
 
 /* to (de)activate running timers temporary */
 void WM_event_timer_sleep(wmWindowManager *wm,
@@ -1739,7 +1751,11 @@ void WM_event_remove_timer_notifier(wmWindowManager *wm, wmWindow *win, wmTimer 
   WM_event_remove_timer(wm, win, timer);
 }
 
-/* ******************* clipboard **************** */
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Clipboard
+ * \{ */
 
 static char *wm_clipboard_text_get_ex(bool selection, int *r_len, bool firstline)
 {
@@ -1844,7 +1860,11 @@ void WM_clipboard_text_set(const char *buf, bool selection)
   }
 }
 
-/* ******************* progress bar **************** */
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Progress Bar
+ * \{ */
 
 void WM_progress_set(wmWindow *win, float progress)
 {
@@ -1856,7 +1876,11 @@ void WM_progress_clear(wmWindow *win)
   GHOST_EndProgressBar(win->ghostwin);
 }
 
-/* ************************************ */
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Window Position/Size (internal)
+ * \{ */
 
 void wm_window_get_position(wmWindow *win, int *r_pos_x, int *r_pos_y)
 {
@@ -1868,6 +1892,12 @@ void wm_window_set_size(wmWindow *win, int width, int height)
 {
   GHOST_SetClientSize(win->ghostwin, width, height);
 }
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Window Depth (Raise/Lower)
+ * \{ */
 
 void wm_window_lower(wmWindow *win)
 {
@@ -1882,6 +1912,12 @@ void wm_window_raise(wmWindow *win)
   }
   GHOST_SetWindowOrder(win->ghostwin, GHOST_kWindowOrderTop);
 }
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Window Buffers
+ * \{ */
 
 void wm_window_swap_buffers(wmWindow *win)
 {
@@ -1898,7 +1934,11 @@ bool wm_window_get_swap_interval(wmWindow *win, int *intervalOut)
   return GHOST_GetSwapInterval(win->ghostwin, intervalOut);
 }
 
-/* ******************* exported api ***************** */
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Initial Window State API
+ * \{ */
 
 /* called whem no ghost system was initialized */
 void WM_init_state_size_set(int stax, int stay, int sizx, int sizy)
@@ -1938,6 +1978,12 @@ void WM_init_native_pixels(bool do_it)
 {
   wm_init_state.native_pixels = do_it;
 }
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Cursor API
+ * \{ */
 
 void WM_init_tablet_api(void)
 {
@@ -2001,8 +2047,17 @@ float WM_cursor_pressure(const struct wmWindow *win)
   }
 }
 
-/* support for native pixel size */
-/* mac retina opens window in size X, but it has up to 2 x more pixels */
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Window Size (public)
+ * \{ */
+
+/**
+ * Support for native pixel size
+ *
+ * \note macOS retina opens window in size X, but it has up to 2 x more pixels.
+ */
 int WM_window_pixels_x(const wmWindow *win)
 {
   float f = GHOST_GetNativePixelSize(win->ghostwin);
@@ -2077,6 +2132,12 @@ bool WM_window_is_fullscreen(wmWindow *win)
 {
   return win->windowstate == GHOST_kWindowStateFullScreen;
 }
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Window Screen/Scene/WorkSpaceViewLayer API
+ * \{ */
 
 /**
  * Some editor data may need to be synced with scene data (3D View camera and layers).
@@ -2253,8 +2314,16 @@ bool WM_window_is_temp_screen(const wmWindow *win)
   return (screen && screen->temp != 0);
 }
 
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Window IME API
+ * \{ */
+
 #ifdef WITH_INPUT_IME
-/* note: keep in mind wm_window_IME_begin is also used to reposition the IME window */
+/**
+ * \note Keep in mind #wm_window_IME_begin is also used to reposition the IME window.
+ */
 void wm_window_IME_begin(wmWindow *win, int x, int y, int w, int h, bool complete)
 {
   BLI_assert(win);
@@ -2271,7 +2340,11 @@ void wm_window_IME_end(wmWindow *win)
 }
 #endif /* WITH_INPUT_IME */
 
-/* ****** direct opengl context management ****** */
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Direct OpenGL Context Management
+ * \{ */
 
 void *WM_opengl_context_create(void)
 {
@@ -2301,3 +2374,5 @@ void WM_opengl_context_release(void *context)
   BLI_assert(GPU_framebuffer_active_get() == NULL);
   GHOST_ReleaseOpenGLContext((GHOST_ContextHandle)context);
 }
+
+/** \} */
