30bf48c9caee40ce457d58b5bfe7fc398b7e9891
Sebastian Parborg <darkdefende@gmail.com>
2019-06-25 12:24:25 +0200

    Fix T63921: Unable to use confirm on release for keyboard shortcuts
    The issue was that we were still working around a Xorg bug that has been solved since a very long time:
    https://bugs.freedesktop.org/show_bug.cgi?id=22515
    The second issue was that the global "confirm on release for mouse clicks" was used for keyboard shortcuts as well.
    
    Reviewed By: Campbell
    
    Differential Revision: http://developer.blender.org/D5128

--- source/blender/editors/transform/transform_generics.c
+++ source/blender/editors/transform/transform_generics.c
@@ -1692,7 +1692,8 @@ void initTransInfo(bContext *C, TransInfo *t, wmOperator *op, const wmEvent *eve
     }
   }
   else {
-    if (U.flag & USER_RELEASECONFIRM) {
+    if (ISMOUSE(t->launch_event) && (U.flag & USER_RELEASECONFIRM)) {
+      /* Global "release confirm" on mouse bindings */
       t->flag |= T_RELEASE_CONFIRM;
     }
   }
