14de0d6a5d299f6e6f4d55f05c264df023259c82
Sergey Sharybin <sergey.vfx@gmail.com>
2019-06-26 19:35:10 +0200

    Buildbot: Fix pack stage for CentOS based builder

--- build_files/buildbot/slave_pack.py
+++ build_files/buildbot/slave_pack.py
@@ -135,24 +135,28 @@ if builder.find('cmake') != -1:
         blender_version = "%d.%d" % (blender_version // 100, blender_version % 100)
         blender_hash = parse_header_file(buildinfo_h, 'BUILD_HASH')[1:-1]
         blender_glibc = builder.split('_')[1]
+        command_prefix = []
 
-        if builder.endswith('x86_64_cmake'):
-            chroot_name = 'buildbot_jessie_x86_64'
-            bits = 64
-            blender_arch = 'x86_64'
-        elif builder.endswith('i686_cmake'):
-            chroot_name = 'buildbot_jessie_i686'
-            bits = 32
-            blender_arch = 'i686'
+        if blender_glibc == 'glibc224':
+            if builder.endswith('x86_64_cmake'):
+                chroot_name = 'buildbot_stretch_x86_64'
+                bits = 64
+                blender_arch = 'x86_64'
+            elif builder.endswith('i686_cmake'):
+                chroot_name = 'buildbot_stretch_i686'
+                bits = 32
+                blender_arch = 'i686'
+            command_prefix = ['schroot', '-c', chroot_name, '--']
+        elif blender_glibc == 'glibc217':
+            command_prefix = ['scl', 'enable', 'devtoolset-6', '--']
 
         # Strip all unused symbols from the binaries
         print("Stripping binaries...")
-        chroot_prefix = ['schroot', '-c', chroot_name, '--']
-        subprocess.call(chroot_prefix + ['strip', '--strip-all', blender])
+        subprocess.call(command_prefix + ['strip', '--strip-all', blender])
 
         print("Stripping python...")
         py_target = os.path.join(install_dir, blender_version)
-        subprocess.call(chroot_prefix + ['find', py_target, '-iname', '*.so', '-exec', 'strip', '-s', '{}', ';'])
+        subprocess.call(command_prefix + ['find', py_target, '-iname', '*.so', '-exec', 'strip', '-s', '{}', ';'])
 
         # Copy all specific files which are too specific to be copied by
         # the CMake rules themselves
