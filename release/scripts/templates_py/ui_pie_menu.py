0519bb584bbc8bc933472ffd70cf8326b3599297
Dalai Felinto <dfelinto@gmail.com>
2019-06-28 09:47:32 -0300

    Python Templates: Silence _MT_ warning in pie menu template

--- release/scripts/templates_py/ui_pie_menu.py
+++ release/scripts/templates_py/ui_pie_menu.py
@@ -4,7 +4,7 @@ from bpy.types import Menu
 # spawn an edit mode selection pie (run while object is in edit mode to get a valid output)
 
 
-class VIEW3D_PIE_template(Menu):
+class VIEW3D_MT_PIE_template(Menu):
     # label is displayed at the center of the pie menu.
     bl_label = "Select Mode"
 
@@ -18,14 +18,14 @@ class VIEW3D_PIE_template(Menu):
 
 
 def register():
-    bpy.utils.register_class(VIEW3D_PIE_template)
+    bpy.utils.register_class(VIEW3D_MT_PIE_template)
 
 
 def unregister():
-    bpy.utils.unregister_class(VIEW3D_PIE_template)
+    bpy.utils.unregister_class(VIEW3D_MT_PIE_template)
 
 
 if __name__ == "__main__":
     register()
 
-    bpy.ops.wm.call_menu_pie(name="VIEW3D_PIE_template")
+    bpy.ops.wm.call_menu_pie(name="VIEW3D_MT_PIE_template")
