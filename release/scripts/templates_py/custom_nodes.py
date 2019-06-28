30d65c326aaef310f8a949dc0e99f0e3a656d307
Dalai Felinto <dfelinto@gmail.com>
2019-06-28 09:53:39 -0300

    Python Templates: Update Custom Nodes tooltip
    Since we exposed the editors sub-types to the type editor selector, this template
    no longer shows in the head, but in the type editor selector.

--- release/scripts/templates_py/custom_nodes.py
+++ release/scripts/templates_py/custom_nodes.py
@@ -7,7 +7,7 @@ from bpy.types import NodeTree, Node, NodeSocket
 # Derived from the NodeTree base type, similar to Menu, Operator, Panel, etc.
 class MyCustomTree(NodeTree):
     # Description string
-    '''A custom node tree type that will show up in the node editor header'''
+    '''A custom node tree type that will show up in the editor type list'''
     # Optional identifier string. If not explicitly defined, the python class name is used.
     bl_idname = 'CustomTreeType'
     # Label for nice name display
