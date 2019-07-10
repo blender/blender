167f5a61e553ab48048870943224228d8fb9bc3a
Campbell Barton <ideasman42@gmail.com>
2019-07-10 19:36:54 +1000

    C Logging: add macro to check if logging is enabled

--- intern/clog/CLG_log.h
+++ intern/clog/CLG_log.h
@@ -158,6 +158,10 @@ void CLG_logref_init(CLG_LogRef *clg_ref);
 #define CLOG_ENSURE(clg_ref) \
   ((clg_ref)->type ? (clg_ref)->type : (CLG_logref_init(clg_ref), (clg_ref)->type))
 
+#define CLOG_CHECK(clg_ref, verbose_level, ...) \
+  ((void)CLOG_ENSURE(clg_ref), \
+   ((clg_ref)->type->flag & CLG_FLAG_USE) && ((clg_ref)->type->level >= verbose_level))
+
 #define CLOG_AT_SEVERITY(clg_ref, severity, verbose_level, ...) \
   { \
     CLG_LogType *_lg_ty = CLOG_ENSURE(clg_ref); \
