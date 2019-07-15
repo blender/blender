ff1b5af30710a802c5137dab3bce3a18203b3645
Sergey Sharybin <sergey.vfx@gmail.com>
2019-07-15 13:59:07 +0200

    Fix compositor ignoring mask parenting
    There are two aspects to the problem:
    
    - Dependency graph update for compositor preview was missing
      updates flush.
    
      Apparently, update for new frame style of update will take
      care of flushing, but not the update tagged style of update.
    
      This goes to a legacy dependency graph and is to be changed
      at some point, but not so close to the release.
    
    - Movie clips were missing from the compositor dependency graph.
    
    This fixes part of T66519.
    
    Reviewers: brecht
    
    Reviewed By: brecht
    
    Differential Revision: https://developer.blender.org/D5256

--- source/blender/depsgraph/intern/builder/deg_builder_relations.cc
+++ source/blender/depsgraph/intern/builder/deg_builder_relations.cc
@@ -2329,6 +2329,24 @@ void DepsgraphRelationBuilder::build_mask(Mask *mask)
   /* Final mask evaluation. */
   OperationKey mask_eval_key(mask_id, NodeType::PARAMETERS, OperationCode::MASK_EVAL);
   add_relation(mask_animation_key, mask_eval_key, "Mask Animation -> Mask Eval");
+  /* Build parents. */
+  LISTBASE_FOREACH (MaskLayer *, mask_layer, &mask->masklayers) {
+    LISTBASE_FOREACH (MaskSpline *, spline, &mask_layer->splines) {
+      for (int i = 0; i < spline->tot_point; i++) {
+        MaskSplinePoint *point = &spline->points[i];
+        MaskParent *parent = &point->parent;
+        if (parent == NULL || parent->id == NULL) {
+          continue;
+        }
+        build_id(parent->id);
+        if (parent->id_type == ID_MC) {
+          OperationKey movieclip_eval_key(
+              parent->id, NodeType::PARAMETERS, OperationCode::MOVIECLIP_EVAL);
+          add_relation(movieclip_eval_key, mask_eval_key, "Movie Clip -> Mask Eval");
+        }
+      }
+    }
+  }
 }
 
 void DepsgraphRelationBuilder::build_movieclip(MovieClip *clip)
