4404a0dcd4c593d199786d01a092105267243235
Clément Foucault <foucault.clem@gmail.com>
2019-07-15 19:24:32 +0200

    Fix T66707 Modifying bone's 'Custom Object' reference shape causes blender to crash
    This was caused by an instancing batch not being initialized correctly.

--- source/blender/draw/intern/draw_instance_data.c
+++ source/blender/draw/intern/draw_instance_data.c
@@ -141,7 +141,8 @@ GPUBatch *DRW_temp_batch_instance_request(DRWInstanceDataList *idatalist,
 
   GPUBatch *batch = BLI_memblock_alloc(idatalist->pool_instancing);
   bool is_compatible = (batch->gl_prim_type == geom->gl_prim_type) && (batch->inst == buf) &&
-                       (buf->vbo_id != 0) && (batch->phase == GPU_BATCH_READY_TO_DRAW);
+                       (buf->vbo_id != 0) && (batch->phase == GPU_BATCH_READY_TO_DRAW) &&
+                       (batch->elem == geom->elem);
   for (int i = 0; i < GPU_BATCH_VBO_MAX_LEN && is_compatible; i++) {
     if (batch->verts[i] != geom->verts[i]) {
       is_compatible = false;
