blender/blender

commit 28dfc47cf0b068d0103986424f79eeea59f2039b

Object Mode: Add to EvaluationContext & DRWContextState

source/blender/draw/intern/draw_armature.c
@@ -1308,7 +1308,9 @@ static void draw_armature_pose(Object *ob, const float const_color[4])
 
 	// if (!(base->flag & OB_FROMDUPLI)) // TODO
 	{
-		if (ob->mode & OB_MODE_POSE) {
+		const DRWContextState *draw_ctx = DRW_context_state_get();
+
+		if (draw_ctx->object_mode & OB_MODE_POSE) {
 			arm->flag |= ARM_POSEMODE;
 		}
 

source/blender/blenkernel/intern/context.c
@@ -1273,8 +1273,10 @@ void CTX_data_eval_ctx(const bContext *C, EvaluationContext *eval_ctx)
 
 	Scene *scene = CTX_data_scene(C);
 	ViewLayer *view_layer = CTX_data_view_layer(C);
+	Object *obact = OBACT(view_layer);
 	RenderEngineType *engine_type = CTX_data_engine_type(C);
-	DEG_evaluation_context_init_from_scene(eval_ctx,
-	                                       scene, view_layer, engine_type,
-	                                       DAG_EVAL_VIEWPORT);
+	DEG_evaluation_context_init_from_scene(
+	        eval_ctx,
+	        scene, view_layer, engine_type,
+	        obact ? obact->mode : OB_MODE_OBJECT, DAG_EVAL_VIEWPORT);
 }

source/blender/depsgraph/DEG_depsgraph.h
@@ -83,6 +83,7 @@ typedef enum eEvaluationMode {
 typedef struct EvaluationContext {
 	eEvaluationMode mode;
 	float ctime;
+	short object_mode;
 
 	struct Depsgraph *depsgraph;
 	struct ViewLayer *view_layer;
@@ -213,11 +214,13 @@ struct EvaluationContext *DEG_evaluation_context_new(eEvaluationMode mode);
  */
 void DEG_evaluation_context_init(struct EvaluationContext *eval_ctx,
                                  eEvaluationMode mode);
-void DEG_evaluation_context_init_from_scene(struct EvaluationContext *eval_ctx,
-                                            struct Scene *scene,
-                                            struct ViewLayer *view_layer,
-                                            struct RenderEngineType *engine_type,
-                                            eEvaluationMode mode);
+void DEG_evaluation_context_init_from_scene(
+        struct EvaluationContext *eval_ctx,
+        struct Scene *scene,
+        struct ViewLayer *view_layer,
+        struct RenderEngineType *engine_type,
+        const short object_mode,
+        eEvaluationMode mode);
 
 /* Free evaluation context. */
 void DEG_evaluation_context_free(struct EvaluationContext *eval_ctx);

source/blender/depsgraph/intern/depsgraph_eval.cc
@@ -76,17 +76,20 @@ void DEG_evaluation_context_init(EvaluationContext *eval_ctx,
 	eval_ctx->mode = mode;
 }
 
-void DEG_evaluation_context_init_from_scene(EvaluationContext *eval_ctx,
-                                            Scene *scene,
-                                            ViewLayer *view_layer,
-                                            RenderEngineType *engine_type,
-                                            eEvaluationMode mode)
+void DEG_evaluation_context_init_from_scene(
+        EvaluationContext *eval_ctx,
+        Scene *scene,
+        ViewLayer *view_layer,
+        RenderEngineType *engine_type,
+        short object_mode,
+        eEvaluationMode mode)
 {
 	DEG_evaluation_context_init(eval_ctx, mode);
 	eval_ctx->depsgraph = BKE_scene_get_depsgraph(scene, view_layer, true);
 	eval_ctx->view_layer = view_layer;
 	eval_ctx->engine_type = engine_type;
 	eval_ctx->ctime = BKE_scene_frame_get(scene);
+	eval_ctx->object_mode = object_mode;
 }
 
 /* Free evaluation context. */

source/blender/draw/DRW_engine.h
@@ -91,25 +91,25 @@ void DRW_draw_view(const struct bContext *C);
 void DRW_draw_render_loop_ex(
         struct Depsgraph *depsgraph,
         struct RenderEngineType *engine_type,
-        struct ARegion *ar, struct View3D *v3d,
+        struct ARegion *ar, struct View3D *v3d, const short object_mode,
         const struct bContext *evil_C);
 void DRW_draw_render_loop(
         struct Depsgraph *depsgraph,
-        struct ARegion *ar, struct View3D *v3d);
+        struct ARegion *ar, struct View3D *v3d, const short object_mode);
 void DRW_draw_render_loop_offscreen(
         struct Depsgraph *depsgraph,
         struct RenderEngineType *engine_type,
-        struct ARegion *ar, struct View3D *v3d,
+        struct ARegion *ar, struct View3D *v3d, const short object_mode,
         const bool draw_background,
         struct GPUOffScreen *ofs,
         struct GPUViewport *viewport);
 void DRW_draw_select_loop(
         struct Depsgraph *depsgraph,
-        struct ARegion *ar, struct View3D *v3d,
+        struct ARegion *ar, struct View3D *v3d, const short object_mode,
         bool use_obedit_skip, bool use_nearest, const struct rcti *rect);
 void DRW_draw_depth_loop(
         struct Depsgraph *depsgraph,
-        struct ARegion *ar, struct View3D *v3d);
+        struct ARegion *ar, struct View3D *v3d, const short object_mode);
 
 /* This is here because GPUViewport needs it */
 void DRW_pass_free(struct DRWPass *pass);

source/blender/draw/engines/clay/clay_engine.c
@@ -820,7 +820,7 @@ static void clay_cache_populate(void *vedata, Object *ob)
 	if (geom) {
 		IDProperty *ces_mode_ob = BKE_layer_collection_engine_evaluated_get(ob, COLLECTION_MODE_OBJECT, "");
 		const bool do_cull = BKE_collection_engine_property_value_get_bool(ces_mode_ob, "show_backface_culling");
-		const bool is_sculpt_mode = is_active && (ob->mode & OB_MODE_SCULPT) != 0;
+		const bool is_sculpt_mode = is_active && (draw_ctx->object_mode & OB_MODE_SCULPT) != 0;
 		const bool is_default_mode_shader = is_sculpt_mode;
 
 		/* Depth Prepass */

source/blender/draw/engines/eevee/eevee_materials.c
@@ -1241,7 +1241,7 @@ void EEVEE_materials_cache_populate(EEVEE_Data *vedata, EEVEE_ViewLayerData *sld
 	IDProperty *ces_mode_ob = BKE_layer_collection_engine_evaluated_get(ob, COLLECTION_MODE_OBJECT, "");
 	const bool do_cull = BKE_collection_engine_property_value_get_bool(ces_mode_ob, "show_backface_culling");
 	const bool is_active = (ob == draw_ctx->obact);
-	const bool is_sculpt_mode = is_active && (ob->mode & OB_MODE_SCULPT) != 0;
+	const bool is_sculpt_mode = is_active && (draw_ctx->object_mode & OB_MODE_SCULPT) != 0;
 #if 0
 	const bool is_sculpt_mode_draw = is_sculpt_mode && (draw_ctx->v3d->flag2 & V3D_SHOW_MODE_SHADE_OVERRIDE) == 0;
 #else

source/blender/draw/engines/eevee/eevee_motion_blur.c
@@ -79,6 +79,7 @@ static void eevee_motion_blur_camera_get_matrix_at_time(
 	        scene,
 	        draw_ctx->view_layer,
 	        draw_ctx->engine_type,
+	        draw_ctx->object_mode,
 	        DAG_EVAL_VIEWPORT);
 	eval_ctx.ctime = time;
 

commit 0b4c0472a3aac40a77ea97485755bc79880d80b9

Fix T61117: Selecting an armature doesn't select the bone
Regression from 2.7x caused by 28dfc47cf0b06

source/blender/draw/intern/draw_armature.c
@@ -1978,6 +1978,7 @@ static void draw_armature_edit(Object *ob)
 static void draw_armature_pose(Object *ob, const float const_color[4])
 {
   const DRWContextState *draw_ctx = DRW_context_state_get();
+  const Scene *scene = draw_ctx->scene;
   bArmature *arm = ob->data;
   bPoseChannel *pchan;
   int index = -1;
@@ -1990,19 +1991,35 @@ static void draw_armature_pose(Object *ob, const float const_color[4])
     return;
   }
 
-  // if (!(base->flag & OB_FROMDUPLI)) // TODO
-  {
+  bool is_pose_select = false;
+  /* Object can be edited in the scene. */
+  if ((ob->base_flag & (BASE_FROM_SET | BASE_FROM_DUPLI)) == 0) {
     if ((draw_ctx->object_mode & OB_MODE_POSE) || (ob == draw_ctx->object_pose)) {
       arm->flag |= ARM_POSEMODE;
     }
+    is_pose_select =
+        /* If we're in pose-mode or object-mode with the ability to enter pose mode. */
+        (
+            /* Draw as if in pose mode (when selection is possible). */
+            (arm->flag & ARM_POSEMODE) ||
+            /* When we're in object mode, which may select bones. */
+            ((ob->mode & OB_MODE_POSE) &&
+             (
+                 /* Switch from object mode when object lock is disabled. */
+                 ((draw_ctx->object_mode == OB_MODE_OBJECT) &&
+                  (scene->toolsettings->object_flag & SCE_OBJECT_MODE_LOCK) == 0) ||
+                 /* Allow selection when in weight-paint mode
+                  * (selection code ensures this wont become active). */
+                 ((draw_ctx->object_mode == OB_MODE_WEIGHT_PAINT) &&
+                  (draw_ctx->object_pose != NULL))))) &&
+        DRW_state_is_select();
 
-    if (arm->flag & ARM_POSEMODE) {
+    if (is_pose_select) {
       const Object *ob_orig = DEG_get_original_object(ob);
       index = ob_orig->runtime.select_id;
     }
   }
 
-  const bool is_pose_select = (arm->flag & ARM_POSEMODE) && DRW_state_is_select();
   const bool show_text = DRW_state_show_text();
   const bool show_relations = ((draw_ctx->v3d->flag & V3D_HIDE_HELPLINES) == 0);
 