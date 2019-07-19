7f77f5f4a4090e0f49c2d33f2601261265b53dc7
Bastien Montagne <montagne29@wanadoo.fr>
2019-07-19 16:10:20 +0200

    Fix assert in some cases when using DataTransfer modifier for custom normals.
    Reported by @sybren on IRC, thanks.
    
    Should be safe for 2.80.

--- source/blender/blenkernel/intern/data_transfer.c
+++ source/blender/blenkernel/intern/data_transfer.c
@@ -1,3 +1,4 @@
+
 /*
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -85,6 +86,7 @@ void BKE_object_data_transfer_dttypes_to_cdmask(const int dtdata_types,
     }
     else if (cddata_type == CD_FAKE_LNOR) {
       r_data_masks->vmask |= CD_MASK_NORMAL;
+      r_data_masks->pmask |= CD_MASK_NORMAL;
       r_data_masks->lmask |= CD_MASK_NORMAL | CD_MASK_CUSTOMLOOPNORMAL;
     }
   }
