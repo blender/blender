b63f0266a056c9ebc7ddc298353d612572216e02
Sergey Sharybin <sergey.vfx@gmail.com>
2019-07-23 15:52:39 +0200

    Fix T67459: Dope Editor, muting channels with shortcut doesn't work

--- source/blender/editors/animation/anim_channels_edit.c
+++ source/blender/editors/animation/anim_channels_edit.c
@@ -1705,7 +1705,7 @@ static void ANIM_OT_channels_ungroup(wmOperatorType *ot)
 
 /* ******************** Delete Channel Operator *********************** */
 
-static void update_dependencies_on_delete(bAnimListElem *ale)
+static void tag_update_animation_element(bAnimListElem *ale)
 {
   ID *id = ale->id;
   AnimData *adt = BKE_animdata_from_id(id);
@@ -1801,7 +1801,7 @@ static int animchannels_delete_exec(bContext *C, wmOperator *UNUSED(op))
 
         /* try to free F-Curve */
         ANIM_fcurve_delete_from_animdata(&ac, adt, fcu);
-        update_dependencies_on_delete(ale);
+        tag_update_animation_element(ale);
         break;
       }
       case ANIMTYPE_NLACURVE: {
@@ -1823,7 +1823,7 @@ static int animchannels_delete_exec(bContext *C, wmOperator *UNUSED(op))
         /* unlink and free the F-Curve */
         BLI_remlink(&strip->fcurves, fcu);
         free_fcurve(fcu);
-        update_dependencies_on_delete(ale);
+        tag_update_animation_element(ale);
         break;
       }
       case ANIMTYPE_GPLAYER: {
@@ -1967,6 +1967,7 @@ static void setflag_anim_channels(bAnimContext *ac,
 
     /* set the setting in the appropriate way */
     ANIM_channel_setting_set(ac, ale, setting, mode);
+    tag_update_animation_element(ale);
 
     /* if flush status... */
     if (flush) {
