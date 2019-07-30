a85963bed86bb25b29a70f893cc9e9279a0b9d15
Philipp Oeser <info@graphics-engineer.com>
2019-07-30 11:22:57 +0200

    Fix T67906: empty menus for hooks if non-hook modifiers are present

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2746,7 +2746,7 @@ class VIEW3D_MT_hook(Menu):
         layout.operator("object.hook_add_selob").use_bone = False
         layout.operator("object.hook_add_selob", text="Hook to Selected Object Bone").use_bone = True
 
-        if [mod.type == 'HOOK' for mod in context.active_object.modifiers]:
+        if any([mod.type == 'HOOK' for mod in context.active_object.modifiers]):
             layout.separator()
 
             layout.operator_menu_enum("object.hook_assign", "modifier")
