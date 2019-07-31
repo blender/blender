4c12764a7f3c9a41e0d72ba5e950fff1946f5b50
Sergey Sharybin <sergey.vfx@gmail.com>
2019-07-31 18:20:18 +0200

    Fix negative boolean
    Was a nice looking invention, but is obviously wrong.
    
    Is likely no function changes, since -false is like -0
    which is 0 and which false. And -true is like -<non-zero> which
    is non-zero as well.
    
    Spotted by Sybren, thanks!

--- source/blender/blenkernel/intern/anim_sys.c
+++ source/blender/blenkernel/intern/anim_sys.c
@@ -3920,7 +3920,7 @@ void BKE_animsys_evaluate_all_animation(Main *main,
     printf("Evaluate all animation - %f\n", ctime);
   }
 
-  const bool flush_to_original = -DEG_is_active(depsgraph);
+  const bool flush_to_original = DEG_is_active(depsgraph);
 
   /* macros for less typing
    * - only evaluate animation data for id if it has users (and not just fake ones)
