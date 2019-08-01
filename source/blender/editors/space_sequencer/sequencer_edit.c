f66b5edf98c98fcf04f99d962647db54f17b79e4
Richard Antalik <richardantalik@gmail.com>
2019-08-01 12:43:15 -0700

    VSE: Use sequencer boundbox for view_all operator
    Fix T67279
    Fix T59954
    
    Reviewed By: brecht
    
    Differential Revision: https://developer.blender.org/D5368

--- source/blender/editors/space_sequencer/sequencer_edit.c
+++ source/blender/editors/space_sequencer/sequencer_edit.c
@@ -2813,10 +2813,12 @@ void SEQUENCER_OT_meta_separate(wmOperatorType *ot)
 static int sequencer_view_all_exec(bContext *C, wmOperator *op)
 {
   ARegion *ar = CTX_wm_region(C);
-  View2D *v2d = UI_view2d_fromcontext(C);
+  rctf box;
+
   const int smooth_viewtx = WM_operator_smooth_viewtx_get(op);
 
-  UI_view2d_smooth_view(C, ar, &v2d->tot, smooth_viewtx);
+  boundbox_seq(CTX_data_scene(C), &box);
+  UI_view2d_smooth_view(C, ar, &box, smooth_viewtx);
   return OPERATOR_FINISHED;
 }
 
