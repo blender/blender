a14805684163c9e93cae56d46aebb9d70b16158b
Campbell Barton <ideasman42@gmail.com>
2019-08-10 00:06:54 +1000

    UI: Show suffix '*' instead of graying out 'Save Preferences'
    Save preferences still works when preferences have not been edited.

--- release/scripts/startup/bl_ui/space_userpref.py
+++ release/scripts/startup/bl_ui/space_userpref.py
@@ -44,9 +44,11 @@ class USERPREF_HT_header(Header):
         if prefs.use_preferences_save and (not bpy.app.use_userpref_skip_save_on_exit):
             pass
         else:
-            sub = row.row(align=True)
-            sub.active = prefs.is_dirty
-            sub.operator("wm.save_userpref")
+            # Show '*' to let users know the preferences have been modified.
+            row.operator(
+                "wm.save_userpref",
+                text="Save Preferences{:s}".format(" *" if prefs.is_dirty else ""),
+            )
 
     def draw(self, context):
         layout = self.layout
