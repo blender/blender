cbd1739004f854f4970f95ea3ed512f2aecf2fe7
Dalai Felinto <dfelinto@gmail.com>
2019-08-15 16:38:31 -0300

    Fix T68705: Changing any editor to the properties crashes Blender
    Issue introduced (more like exposed) in b7f86ff72273.

--- source/blender/editors/space_buttons/buttons_context.c
+++ source/blender/editors/space_buttons/buttons_context.c
@@ -785,11 +785,11 @@ int buttons_context(const bContext *C, const char *member, bContextDataResult *r
   SpaceProperties *sbuts = CTX_wm_space_properties(C);
   ButsContextPath *path = sbuts ? sbuts->path : NULL;
 
-  if (sbuts->mainb == BCONTEXT_TOOL) {
+  if (!path) {
     return 0;
   }
 
-  if (!path) {
+  if (sbuts->mainb == BCONTEXT_TOOL) {
     return 0;
   }
 
