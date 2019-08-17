463eef36d3b9e3b4673462cf6df8945fe5e81f9e
Antonio Vazquez <blendergit@gmail.com>
2019-08-17 11:04:03 +0200

    Fix T68753: GPencil tooltip typo

--- source/blender/makesrna/intern/rna_gpencil_modifier.c
+++ source/blender/makesrna/intern/rna_gpencil_modifier.c
@@ -697,7 +697,7 @@ static void rna_def_modifier_gpencilsimplify(BlenderRNA *brna)
   prop = RNA_def_property(srna, "distance", PROP_FLOAT, PROP_NONE);
   RNA_def_property_float_sdna(prop, NULL, "distance");
   RNA_def_property_range(prop, 0, 100.0f);
-  RNA_def_property_ui_text(prop, "Distance", "Distance between vertex");
+  RNA_def_property_ui_text(prop, "Distance", "Distance between points");
   RNA_def_property_update(prop, 0, "rna_GpencilModifier_update");
 }
 
