899d2461bf84c9be25ae6c24675ba4a263542855
mano-wii <germano.costa@ig.com.br>
2019-08-20 21:56:39 -0300

    Fix T68911: "illegal recursive expansion of macro id"
    I imagined that could be a problem for some drivers.
    I should have avoided.

--- source/blender/draw/engines/select/shaders/selection_id_frag.glsl
+++ source/blender/draw/engines/select/shaders/selection_id_frag.glsl
@@ -1,14 +1,15 @@
 
 #ifdef UNIFORM_ID
 uniform int id;
-#  define id floatBitsToUint(intBitsToFloat(id))
+#  define _id floatBitsToUint(intBitsToFloat(id))
 #else
 flat in uint id;
+#  define _id id
 #endif
 
 out uint fragColor;
 
 void main()
 {
-  fragColor = id;
+  fragColor = _id;
 }
