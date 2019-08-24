b7f786c0b150378a84a0377e79e9cf14023055d5
Antonio Vazquez <blendergit@gmail.com>
2019-08-24 09:02:42 +0200

    GPencil: Fix typo error

--- source/blender/blenkernel/intern/gpencil.c
+++ source/blender/blenkernel/intern/gpencil.c
@@ -148,19 +148,19 @@ bool BKE_gpencil_free_strokes(bGPDframe *gpf)
 }
 
 /* Free strokes and colors belonging to a gp-frame */
-bool BKE_gpencil_free_frame_runtime_data(bGPDframe *gpu_eval)
+bool BKE_gpencil_free_frame_runtime_data(bGPDframe *gpf_eval)
 {
   bGPDstroke *gps_next;
-  if (!gpu_eval) {
+  if (!gpf_eval) {
     return false;
   }
 
   /* free strokes */
-  for (bGPDstroke *gps = gpu_eval->strokes.first; gps; gps = gps_next) {
+  for (bGPDstroke *gps = gpf_eval->strokes.first; gps; gps = gps_next) {
     gps_next = gps->next;
     BKE_gpencil_free_stroke(gps);
   }
-  BLI_listbase_clear(&gpu_eval->strokes);
+  BLI_listbase_clear(&gpf_eval->strokes);
 
   return true;
 }
