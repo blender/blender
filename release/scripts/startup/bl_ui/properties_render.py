52b32fde187b9817b2f8f9c7741a9a2147a3db3a
Antonio Vazquez <blendergit@gmail.com>
2019-09-12 16:19:43 +0200

    GPencil: Add Simplify panel to Workbench engine

--- release/scripts/startup/bl_ui/properties_render.py
+++ release/scripts/startup/bl_ui/properties_render.py
@@ -658,7 +658,7 @@ class RENDER_PT_simplify_render(RenderButtonsPanel, Panel):
 class RENDER_PT_simplify_greasepencil(RenderButtonsPanel, Panel, GreasePencilSimplifyPanel):
     bl_label = "Grease Pencil"
     bl_parent_id = "RENDER_PT_simplify"
-    COMPAT_ENGINES = {'BLENDER_RENDER', 'BLENDER_GAME', 'BLENDER_CLAY', 'BLENDER_EEVEE'}
+    COMPAT_ENGINES = {'BLENDER_RENDER', 'BLENDER_GAME', 'BLENDER_CLAY', 'BLENDER_EEVEE', 'BLENDER_WORKBENCH'}
     bl_options = {'DEFAULT_CLOSED'}
 
 
