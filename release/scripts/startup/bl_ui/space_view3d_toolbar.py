0927e7b83558af51673ab1ed97279ce8c9b13632
Pablo Dobarro <pablodp606@gmail.com>
2019-09-16 17:09:01 +0200

    Sculpt: Rename pinch to magnify in some brushes
    Reviewed By: jbakker
    
    Differential Revision: https://developer.blender.org/D5815

--- release/scripts/startup/bl_ui/space_view3d_toolbar.py
+++ release/scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -456,7 +456,10 @@ class VIEW3D_PT_tools_brush(Panel, View3DPaintPanel):
             # crease_pinch_factor
             if capabilities.has_pinch_factor:
                 row = col.row(align=True)
-                row.prop(brush, "crease_pinch_factor", slider=True, text="Pinch")
+                if (brush.sculpt_tool in ('BLOB', 'SNAKE_HOOK')):
+                    row.prop(brush, "crease_pinch_factor", slider=True, text="Magnify")
+                else:
+                    row.prop(brush, "crease_pinch_factor", slider=True, text="Pinch")
 
             # rake_factor
             if capabilities.has_rake_factor:
