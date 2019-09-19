2f6a0647659de2cfa695b3f53bb401e80503ed40
Brecht Van Lommel <brechtvanlommel@gmail.com>
2019-09-19 14:40:33 +0200

    Fix T70068: sculpt mode hide masked not working after show all

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2831,6 +2831,7 @@ class VIEW3D_MT_sculpt(Menu):
         props.area = 'INSIDE'
 
         props = layout.operator("paint.hide_show", text="Hide Masked")
+        props.action = 'HIDE'
         props.area = 'MASKED'
 
         layout.separator()
