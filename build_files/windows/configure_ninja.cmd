34bd23dbb71b2f8bfc696acf5107650bd750364d
Ray Molenkamp <github@lazydodo.com>
2019-10-01 12:36:11 -0600

    MSVC: Support Building clang+ninja+VS2019
    1) Clang was given the wrong VS version to emulate when used in
    combination with VS2019 causing build issues.
    
    2) The erroneous supplied parameter `-std::c++11`caused CMake to
    fail running its compiler detection scripts.

--- build_files/windows/configure_ninja.cmd
+++ build_files/windows/configure_ninja.cmd
@@ -30,9 +30,15 @@ set LLVM_DIR=
 :DetectionComplete	
 	set CC=%LLVM_DIR%\bin\clang-cl
 	set CXX=%LLVM_DIR%\bin\clang-cl
-	rem build and tested against 2017 15.7
-	set CFLAGS=-m64 -fmsc-version=1914
-	set CXXFLAGS=-m64 -fmsc-version=1914
+	if "%BUILD_VS_YEAR%" == "2019" (
+		rem build and tested against 2019 16.2
+		set CFLAGS=-m64 -fmsc-version=1922
+		set CXXFLAGS=-m64 -fmsc-version=1922
+	) else (
+		rem build and tested against 2017 15.7
+		set CFLAGS=-m64 -fmsc-version=1914
+		set CXXFLAGS=-m64 -fmsc-version=1914
+	)
 	if "%WITH_ASAN%"=="1" (
 		set BUILD_CMAKE_ARGS=%BUILD_CMAKE_ARGS% -DWITH_COMPILER_ASAN=On
 	)	
