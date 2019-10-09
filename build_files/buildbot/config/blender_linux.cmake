62a7e8d4299ccc9254f0dc63dc20a4a78c2dfcd4
Sergey Sharybin <sergey.vfx@gmail.com>
2019-10-09 09:50:58 +0200

    Buildbot: Point to pre-compiled CentOS 7 libraries

--- build_files/buildbot/config/blender_linux.cmake
+++ build_files/buildbot/config/blender_linux.cmake
@@ -2,33 +2,20 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/../../cmake/config/blender_release.cmake")
 
-# For libc-2.24 we are using chroot which runs on a 64bit system.
-# There we can not use CPU bitness check since it is always 64bit. So instead
-# we check for a specific libraries.
-#
-# Other builders we are running in a bare virtual machine, and the libraries
-# are installed to /opt/.
-# We assume that only 64bit builders exists in such configuration.
-if(EXISTS "/lib/x86_64-linux-gnu/libc-2.24.so")
-  message(STATUS "Building in GLibc-2.24 environment")
-  set(LIBDIR_NAME "linux_x86_64")
-elseif(EXISTS "/lib/i386-linux-gnu//libc-2.24.so")
-  message(STATUS "Building in GLibc-2.24 environment")
-  set(LIBDIR_NAME "linux_i686")
-else()
-  message(STATUS "Building in generic 64bit environment")
-  set(LIBDIR_NAME "linux_x86_64")
-endif()
+message(STATUS "Building in CentOS 7 64bit environment")
+set(LIBDIR_NAME "linux_centos7_x86_64")
 
 # Default to only build Blender
 set(WITH_BLENDER             ON  CACHE BOOL "" FORCE)
 
 # ######## Linux-specific build options ########
 # Options which are specific to Linux-only platforms
+
 set(WITH_DOC_MANPAGE         OFF CACHE BOOL "" FORCE)
 
 # ######## Official release-specific build options ########
 # Options which are specific to Linux release builds only
+
 set(WITH_JACK_DYNLOAD        ON  CACHE BOOL "" FORCE)
 set(WITH_SDL_DYNLOAD         ON  CACHE BOOL "" FORCE)
 set(WITH_SYSTEM_GLEW         OFF CACHE BOOL "" FORCE)
@@ -40,7 +27,7 @@ set(WITH_PYTHON_INSTALL_REQUESTS ON CACHE BOOL "" FORCE)
 
 # ######## Release environment specific settings ########
 
-set(LIBDIR "/opt/blender-deps/${LIBDIR_NAME}" CACHE BOOL "" FORCE)
+set(LIBDIR "${CMAKE_SOURCE_DIR}/../lib/${LIBDIR_NAME}" CACHE STRING "" FORCE)
 
 # Platform specific configuration, to ensure static linking against everything.
 
