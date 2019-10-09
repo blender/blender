95199dca1f12bd8bf2433047361b5ffde9cd879c
Campbell Barton <ideasman42@gmail.com>
2019-10-09 15:12:15 +1100

    Fix T70662: Batch rename adds "\" before "."

--- release/scripts/startup/bl_operators/wm.py
+++ release/scripts/startup/bl_operators/wm.py
@@ -2042,7 +2042,7 @@ class WM_OT_batch_rename(Operator):
                     if action.use_replace_regex_dst:
                         replace_dst = action.replace_dst
                     else:
-                        replace_dst = re.escape(action.replace_dst)
+                        replace_dst = action.replace_dst.replace("\\", "\\\\")
                 else:
                     replace_src = re.escape(action.replace_src)
                     replace_dst = re.escape(action.replace_dst)
