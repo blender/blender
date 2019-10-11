67b1a15b3f4dfccf1e2118f21158e14774cd071a
Brecht Van Lommel <brechtvanlommel@gmail.com>
2019-10-11 12:15:19 +0200

    Version bump to 2.82 alpha, master is now open for new features and changes

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -26,8 +26,8 @@
  *
  * \note Use #STRINGIFY() rather than defining with quotes.
  */
-#define BLENDER_VERSION 281
-#define BLENDER_SUBVERSION 15
+#define BLENDER_VERSION 282
+#define BLENDER_SUBVERSION 0
 /** Several breakages with 280, e.g. collections vs layers. */
 #define BLENDER_MINVERSION 280
 #define BLENDER_MINSUBVERSION 0
