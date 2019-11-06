863ca3dd76c0ea77fb35c76df9d6936355ca07b5
Campbell Barton <ideasman42@gmail.com>
2019-11-06 13:05:34 +1100

    Cleanup: remove unused game engine variable

--- source/blender/makesdna/DNA_armature_types.h
+++ source/blender/makesdna/DNA_armature_types.h
@@ -128,9 +128,7 @@ typedef struct bArmature {
 
   int flag;
   int drawtype;
-  /** How vertex deformation is handled in the ge. */
-  int gevertdeformer;
-  char _pad[4];
+
   short deformflag;
   short pathflag;
 
@@ -174,12 +172,6 @@ typedef enum eArmature_Drawtype {
   ARM_ENVELOPE = 3,
   ARM_WIRE = 4,
 } eArmature_Drawtype;
-
-/* armature->gevertdeformer */
-typedef enum eArmature_VertDeformer {
-  ARM_VDEF_BLENDER = 0,
-  ARM_VDEF_BGE_CPU = 1,
-} eArmature_VertDeformer;
 
 /* armature->deformflag */
 typedef enum eArmature_DeformFlag {
