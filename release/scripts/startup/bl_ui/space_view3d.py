1f6c3699a836d485ed37f443cd0fcd19e978dbb6
Kevin Buhr <buhr>
2019-12-02 15:12:10 +1100

    Fix T72099: Exception drawing guide UI in quad view

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -5567,7 +5567,7 @@ class VIEW3D_PT_overlay_guides(Panel):
         row = sub.row()
         row_el = row.column()
         row_el.prop(overlay, "show_ortho_grid", text="Grid")
-        grid_active = (
+        grid_active = bool(
             view.region_quadviews or
             (view.region_3d.is_orthographic_side_view and not view.region_3d.is_perspective)
         )
