blender/blender

commit 6183688c3560100dc1dcce3354cb622a135bc275

Fix T70919: Proxies crash after building motion path
Was cause by recent fix for T65134 which assigned original object's
proxy_from to an evaluated pointer.

This is because motion path depsgraph does not include proxies, so
the pointer in an evaluated object was kept pointing to an original
object.

source/blender/depsgraph/intern/eval/deg_eval_copy_on_write.cc
@@ -755,6 +755,25 @@ void update_animation_data_after_copy(const ID *id_orig, ID *id_cow)
   update_nla_tracks_orig_pointers(&anim_data_orig->nla_tracks, &anim_data_cow->nla_tracks);
 }
 
+/* Some builders (like motion path one) will ignore proxies from being built. This code makes it so
+ * proxy and proxy_group pointers never point to an original objects, preventing evaluation code
+ * from assign evaluated pointer to an original proxy->proxy_from. */
+void update_proxy_pointers_after_copy(const Depsgraph * /*depsgraph*/,
+                                      const Object * /*object_orig*/,
+                                      Object *object_cow)
+{
+  if (object_cow->proxy != NULL) {
+    if ((object_cow->proxy->id.tag & LIB_TAG_COPIED_ON_WRITE) == 0) {
+      object_cow->proxy = NULL;
+    }
+  }
+  if (object_cow->proxy_group != NULL) {
+    if ((object_cow->proxy_group->id.tag & LIB_TAG_COPIED_ON_WRITE) == 0) {
+      object_cow->proxy_group = NULL;
+    }
+  }
+}
+
 /* Do some special treatment of data transfer from original ID to it's
  * CoW complementary part.
  *
@@ -788,6 +807,7 @@ void update_id_after_copy(const Depsgraph *depsgraph,
       }
       update_particles_after_copy(depsgraph, object_orig, object_cow);
       update_modifiers_orig_pointers(object_orig, object_cow);
+      update_proxy_pointers_after_copy(depsgraph, object_orig, object_cow);
       break;
     }
     case ID_SCE: {

commit 2e06a6bec3f5ed078a544dea9c4f842e403abbc0

Fix T72820: Linked objects jumping around during render
Was caused by 6183688c3560 (thanks ronsn for nailing it down!).

The issue is that order of copy-on-write operations is not defined, so
can not use flags set by that operation to make decision.

source/blender/depsgraph/intern/builder/deg_builder.cc
@@ -55,6 +55,12 @@ extern "C" {
 
 namespace DEG {
 
+bool deg_check_id_in_depsgraph(const Depsgraph *graph, ID *id_orig)
+{
+  IDNode *id_node = graph->find_id_node(id_orig);
+  return id_node != NULL;
+}
+
 bool deg_check_base_in_depsgraph(const Depsgraph *graph, Base *base)
 {
   Object *object_orig = base->base_orig->object;

source/blender/depsgraph/intern/builder/deg_builder.h
@@ -24,6 +24,7 @@
 #pragma once
 
 struct Base;
+struct ID;
 struct Main;
 struct Object;
 struct bPoseChannel;
@@ -53,6 +54,7 @@ class DepsgraphBuilder {
   DepsgraphBuilderCache *cache_;
 };
 
+bool deg_check_id_in_depsgraph(const Depsgraph *graph, ID *id_orig);
 bool deg_check_base_in_depsgraph(const Depsgraph *graph, Base *base);
 void deg_graph_build_finalize(Main *bmain, Depsgraph *graph);
 

source/blender/depsgraph/intern/eval/deg_eval_copy_on_write.cc
@@ -759,17 +759,17 @@ void update_animation_data_after_copy(const ID *id_orig, ID *id_cow)
 /* Some builders (like motion path one) will ignore proxies from being built. This code makes it so
  * proxy and proxy_group pointers never point to an original objects, preventing evaluation code
  * from assign evaluated pointer to an original proxy->proxy_from. */
-void update_proxy_pointers_after_copy(const Depsgraph * /*depsgraph*/,
-                                      const Object * /*object_orig*/,
+void update_proxy_pointers_after_copy(const Depsgraph *depsgraph,
+                                      const Object *object_orig,
                                       Object *object_cow)
 {
   if (object_cow->proxy != NULL) {
-    if ((object_cow->proxy->id.tag & LIB_TAG_COPIED_ON_WRITE) == 0) {
+    if (!deg_check_id_in_depsgraph(depsgraph, &object_orig->proxy->id)) {
       object_cow->proxy = NULL;
     }
   }
   if (object_cow->proxy_group != NULL) {
-    if ((object_cow->proxy_group->id.tag & LIB_TAG_COPIED_ON_WRITE) == 0) {
+    if (!deg_check_id_in_depsgraph(depsgraph, &object_orig->proxy_group->id)) {
       object_cow->proxy_group = NULL;
     }
   }