a5f19e3f7270b335d324c4fa5a9ca33e59376ef0
Antonio Vazquez <blendergit@gmail.com>
2020-01-20 16:18:04 +0100

    Fix T73265: GPencil console error when delete frist frame

--- source/blender/blenkernel/intern/gpencil.c
+++ source/blender/blenkernel/intern/gpencil.c
@@ -939,9 +939,15 @@ bGPDframe *BKE_gpencil_layer_getframe(bGPDlayer *gpl, int cframe, eGP_GetFrame_M
       gpl->actframe = gpf;
     }
     else {
-      /* unresolved errogenous situation! */
-      CLOG_STR_ERROR(&LOG, "cannot find appropriate gp-frame");
-      /* gpl->actframe should still be NULL */
+      /* If delete first frame, need to find one. */
+      if (gpl->frames.first != NULL) {
+        gpl->actframe = gpl->frames.first;
+      }
+      else {
+        /* unresolved errogenous situation! */
+        CLOG_STR_ERROR(&LOG, "cannot find appropriate gp-frame");
+        /* gpl->actframe should still be NULL */
+      }
     }
   }
   else {
