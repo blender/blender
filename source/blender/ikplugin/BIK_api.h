f37b9ffdcb9f6fc40b98168bae4c4838a3296935
Campbell Barton <ideasman42@gmail.com>
2020-02-15 12:02:29 +1100

    Cleanup: unused BIK_api struct & defines
    This was part of an unfinished API,
    however this wasn't touched since 2009.

--- source/blender/ikplugin/BIK_api.h
+++ source/blender/ikplugin/BIK_api.h
@@ -36,23 +36,6 @@ struct bConstraint;
 struct bPose;
 struct bPoseChannel;
 
-enum BIK_ParamType {
-  BIK_PARAM_TYPE_FLOAT = 0,
-  BIK_PARAM_TYPE_INT,
-  BIK_PARAM_TYPE_STRING,
-};
-
-struct BIK_ParamValue {
-  short type;   /* BIK_PARAM_TYPE_.. */
-  short length; /* for string, does not include terminating 0 */
-  union {
-    float f[8];
-    int i[8];
-    char s[32];
-  } value;
-};
-typedef struct BIK_ParamValue BIK_ParamValue;
-
 void BIK_initialize_tree(struct Depsgraph *depsgraph,
                          struct Scene *scene,
                          struct Object *ob,
@@ -67,33 +50,6 @@ void BIK_clear_data(struct bPose *pose);
 void BIK_clear_cache(struct bPose *pose);
 void BIK_update_param(struct bPose *pose);
 void BIK_test_constraint(struct Object *ob, struct bConstraint *cons);
-// not yet implemented
-// int BIK_get_constraint_param(struct bPose *pose,
-//                              struct bConstraint *cons,
-//                              int id,
-//                              BIK_ParamValue *value);
-// int BIK_get_channel_param(struct bPose *pose,
-//                           struct bPoseChannel *pchan,
-//                           int id,
-//                           BIK_ParamValue *value);
-// int BIK_get_solver_param(struct bPose *pose,
-//                          struct bPoseChannel *pchan,
-//                          int id,
-//                          BIK_ParamValue *value);
-
-// number of solver available
-// 0 = iksolver
-// 1 = iTaSC
-
-/* for use in BIK_get_constraint_param */
-#define BIK_PARAM_CONSTRAINT_ERROR 0
-
-/* for use in BIK_get_channel_param */
-#define BIK_PARAM_CHANNEL_JOINT 0
-
-/* for use in BIK_get_solver_param */
-#define BIK_PARAM_SOLVER_RANK 0
-#define BIK_PARAM_SOLVER_ITERATION 1
 
 #ifdef __cplusplus
 }
