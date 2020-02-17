4353bc919030a6eea5e0dc41247f452bea226d16
Richard Antalik <richardantalik@gmail.com>
2020-02-17 16:38:53 +0100

    VSE: Fix missed renaming of cut operator
    Fix errors introduced by commit rB819af2094b21.
    
    Reviewed By: ISS
    
    Differential Revision: https://developer.blender.org/D6870y

--- release/scripts/startup/bl_ui/space_sequencer.py
+++ release/scripts/startup/bl_ui/space_sequencer.py
@@ -783,7 +783,7 @@ class SEQUENCER_MT_context_menu(Menu):
 
         layout.operator_context = 'INVOKE_REGION_WIN'
 
-        layout.operator("sequencer.cut", text="Cut").type = 'SOFT'
+        layout.operator("sequencer.split", text="Split").type = 'SOFT'
 
         layout.separator()
 
@@ -1115,11 +1115,11 @@ class SEQUENCER_PT_effect(SequencerButtonsPanel, Panel):
                     if i == strip.multicam_source:
                         sub = row.row(align=True)
                         sub.enabled = False
-                        sub.operator("sequencer.cut_multicam", text="%d" % i).camera = i
+                        sub.operator("sequencer.split_multicam", text="%d" % i).camera = i
                     else:
                         sub_1 = row.row(align=True)
                         sub_1.enabled = True
-                        sub_1.operator("sequencer.cut_multicam", text="%d" % i).camera = i
+                        sub_1.operator("sequencer.split_multicam", text="%d" % i).camera = i
 
                 if strip.channel > BT_ROW and (strip_channel - 1) % BT_ROW:
                     for i in range(strip.channel, strip_channel + ((BT_ROW + 1 - strip_channel) % BT_ROW)):
