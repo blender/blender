71d6fbcd516db196d68088319ede5d2ffd254d71
Campbell Barton <ideasman42@gmail.com>
2020-02-20 11:18:40 +1100

    Fix missing error message on convex hull failure
    Also remove unused errors.

--- source/blender/bmesh/intern/bmesh_operators.c
+++ source/blender/bmesh/intern/bmesh_operators.c
@@ -44,18 +44,14 @@ static int bmo_name_to_slotcode_check(BMOpSlot slot_args[BMO_OP_MAX_SLOTS],
 
 static const char *bmo_error_messages[] = {
     NULL,
-    N_("Self intersection error"),
-    N_("Could not dissolve vert"),
     N_("Could not connect vertices"),
-    N_("Could not traverse mesh"),
     N_("Could not dissolve faces"),
-    N_("Tessellation error"),
-    N_("Cannot deal with non-manifold geometry"),
     N_("Invalid selection"),
     N_("Internal mesh error"),
+    N_("Convex hull failed"),
 };
 
-BLI_STATIC_ASSERT(ARRAY_SIZE(bmo_error_messages) + 1 == BMERR_TOTAL, "message mismatch");
+BLI_STATIC_ASSERT(ARRAY_SIZE(bmo_error_messages) == BMERR_TOTAL, "message mismatch");
 
 /* operator slot type information - size of one element of the type given. */
 const int BMO_OPSLOT_TYPEINFO[BMO_OP_SLOT_TOTAL_TYPES] = {
