e0f41d32c972db36e86289136b517f54db92de06
Jeroen Bakker <jeroen@blender.org>
2020-02-28 14:31:03 +0100

    Code Cleanup: UNDEF not existing define

--- source/blender/draw/engines/eevee/eevee_render.c
+++ source/blender/draw/engines/eevee/eevee_render.c
@@ -664,5 +664,6 @@ void EEVEE_render_update_passes(RenderEngine *engine, Scene *scene, ViewLayer *v
   CHECK_PASS_EEVEE(VOLUME_TRANSMITTANCE, SOCK_RGBA, 3, "RGB");
   CHECK_PASS_EEVEE(BLOOM, SOCK_RGBA, 3, "RGB");
 
-#undef CHECK_PASS
+#undef CHECK_PASS_LEGACY
+#undef CHECK_PASS_EEVEE
 }
