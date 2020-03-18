c9c08dc7c8736e864352a41437592670a5d341e9
Sergey Sharybin <sergey.vfx@gmail.com>
2020-03-18 18:16:18 +0100

    Fix compilation error after recent change

--- source/blender/makesrna/intern/rna_userdef.c
+++ source/blender/makesrna/intern/rna_userdef.c
@@ -2040,7 +2040,7 @@ static void rna_def_userdef_theme_spaces_curves(
     prop = RNA_def_property(srna, "handle_sel_auto_clamped", PROP_FLOAT, PROP_COLOR_GAMMA);
     RNA_def_property_float_sdna(prop, NULL, "handle_sel_auto_clamped");
     RNA_def_property_array(prop, 3);
-    RNA_def_property_ui_text(prop, "Auto-Clamped Handle Selected, "");
+    RNA_def_property_ui_text(prop, "Auto-Clamped Handle Selected", "");
     RNA_def_property_update(prop, 0, "rna_userdef_theme_update");
   }
 
