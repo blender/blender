a2243f1b5178fde2a49e5536b674cae57c096659
Brecht Van Lommel <brecht@blender.org>
2020-04-07 23:41:30 +0200

    Debugging: change Undo/Redo redraw timer to include dependency graph update
    This is often the slowest part and was not counted before.

--- source/blender/windowmanager/intern/wm_operators.c
+++ source/blender/windowmanager/intern/wm_operators.c
@@ -103,6 +103,7 @@
 
 #include "wm.h"
 #include "wm_draw.h"
+#include "wm_event_system.h"
 #include "wm_event_types.h"
 #include "wm_files.h"
 #include "wm_window.h"
@@ -3244,8 +3245,12 @@ static void redraw_timer_step(bContext *C,
     }
   }
   else { /* eRTUndo */
+    /* Undo and redo, including depsgraph update since that can be a
+     * significant part of the cost. */
     ED_undo_pop(C);
+    wm_event_do_refresh_wm_and_depsgraph(C);
     ED_undo_redo(C);
+    wm_event_do_refresh_wm_and_depsgraph(C);
   }
 }
 
