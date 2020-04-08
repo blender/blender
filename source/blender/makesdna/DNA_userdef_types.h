056ebb56b16b92de8a3889c530da1cf14d445170
Campbell Barton <ideasman42@gmail.com>
2020-04-08 10:34:39 +1000

    Cleanup: clang-format

--- source/blender/makesdna/DNA_userdef_types.h
+++ source/blender/makesdna/DNA_userdef_types.h
@@ -1121,12 +1121,12 @@ typedef enum eAutokey_Flag {
 typedef enum eUserpref_Translation_Flags {
   USER_TR_TOOLTIPS = (1 << 0),
   USER_TR_IFACE = (1 << 1),
-  USER_TR_UNUSED_2 = (1 << 2), /* cleared */
-  USER_TR_UNUSED_3 = (1 << 3), /* cleared */
-  USER_TR_UNUSED_4 = (1 << 4), /* cleared */
+  USER_TR_UNUSED_2 = (1 << 2),            /* cleared */
+  USER_TR_UNUSED_3 = (1 << 3),            /* cleared */
+  USER_TR_UNUSED_4 = (1 << 4),            /* cleared */
   USER_DOTRANSLATE_DEPRECATED = (1 << 5), /* Deprecated in 2.83. */
-  USER_TR_UNUSED_6 = (1 << 6), /* cleared */
-  USER_TR_UNUSED_7 = (1 << 7), /* cleared */
+  USER_TR_UNUSED_6 = (1 << 6),            /* cleared */
+  USER_TR_UNUSED_7 = (1 << 7),            /* cleared */
   USER_TR_NEWDATANAME = (1 << 8),
 } eUserpref_Translation_Flags;
 
