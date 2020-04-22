c7991bcefcaa2924597bd8fe55ce63905fe0f23e
Jacques Lucke <jacques@blender.org>
2020-04-22 12:53:47 +0200

    BLI: add ScopedTimer
    This adds a simple timer that can be used for performance measurements in C++.
    More sophisticated timers are possible (e.g. one that takes averages, logs the results, ...).
    However, I found that this simple timer is good enough for 99% of my use cases.
    
    To use it just write `SCOPED_TIMER("my timer name");` or more commonly `SCOPED_TIMER(__func__);`
    into some scope.
    
    Reviewers: sergey
    
    Differential Revision: https://developer.blender.org/D7491

---
+++ source/blender/blenlib/BLI_timeit.hh
@@ -0,0 +1,62 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __BLI_TIMEIT_HH__
+#define __BLI_TIMEIT_HH__
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+#include "BLI_sys_types.h"
+
+namespace BLI {
+namespace Timeit {
+
+using Clock = std::chrono::steady_clock;
+using TimePoint = Clock::time_point;
+using Nanoseconds = std::chrono::nanoseconds;
+
+void print_duration(Nanoseconds duration);
+
+class ScopedTimer {
+ private:
+  std::string m_name;
+  TimePoint m_start;
+
+ public:
+  ScopedTimer(std::string name) : m_name(std::move(name))
+  {
+    m_start = Clock::now();
+  }
+
+  ~ScopedTimer()
+  {
+    TimePoint end = Clock::now();
+    Nanoseconds duration = end - m_start;
+
+    std::cout << "Timer '" << m_name << "' took ";
+    print_duration(duration);
+    std::cout << '\n';
+  }
+};
+
+}  // namespace Timeit
+}  // namespace BLI
+
+#define SCOPED_TIMER(name) BLI::Timeit::ScopedTimer scoped_timer(name)
+
+#endif /* __BLI_TIMEIT_HH__ */
---
+++ source/blender/blenlib/intern/timeit.cc
@@ -0,0 +1,36 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "BLI_timeit.hh"
+
+namespace BLI {
+namespace Timeit {
+
+void print_duration(Nanoseconds duration)
+{
+  if (duration < std::chrono::microseconds(100)) {
+    std::cout << duration.count() << " ns";
+  }
+  else if (duration < std::chrono::seconds(5)) {
+    std::cout << duration.count() / 1.0e6 << " ms";
+  }
+  else {
+    std::cout << duration.count() / 1.0e9 << " s";
+  }
+}
+
+}  // namespace Timeit
+}  // namespace BLI
