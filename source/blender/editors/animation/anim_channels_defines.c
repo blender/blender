af876b12f1f7925228a568d7ae1f412babf08114
yves <valfeur>
2020-04-27 18:22:54 +0200

    Fix T75519: Graph editor tooltips give false impression of the toggle state
    It is just a quick fix for the tooltips in the graph editor, it replaces:
    -  "F-curve modifiers are disabled" with "Enable F-Curve modifiers"
    -  "F-curve is visible in graph editor for editing" with "F-Curve visibility in Graph Editor".
    
    Reviewed By: billreynish
    
    Maniphest Tasks: T75519
    
    Differential Revision: https://developer.blender.org/D7387

--- source/blender/editors/animation/anim_channels_defines.c
+++ source/blender/editors/animation/anim_channels_defines.c
@@ -4819,7 +4819,7 @@ static void draw_setting_widget(bAnimContext *ac,
       icon = ICON_HIDE_ON;
 
       if (ELEM(ale->type, ANIMTYPE_FCURVE, ANIMTYPE_NLACURVE)) {
-        tooltip = TIP_("F-Curve is visible in Graph Editor for editing");
+        tooltip = TIP_("F-Curve visibility in Graph Editor");
       }
       else if (ale->type == ANIMTYPE_GPLAYER) {
         tooltip = TIP_("Grease Pencil layer is visible in the viewport");
@@ -4836,7 +4836,7 @@ static void draw_setting_widget(bAnimContext *ac,
 
     case ACHANNEL_SETTING_MOD_OFF: /* modifiers disabled */
       icon = ICON_MODIFIER_OFF;
-      tooltip = TIP_("F-Curve modifiers are disabled");
+      tooltip = TIP_("Enable F-Curve modifiers");
       break;
 
     case ACHANNEL_SETTING_EXPAND: /* expanded triangle */
