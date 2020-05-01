5ee1c7f695390c4cd3800319e6f032b0293e63ad
Campbell Barton <ideasman42@gmail.com>
2020-05-01 12:36:11 +1000

    Cleanup: spelling, comments

--- source/blender/blenkernel/intern/multires_unsubdivide.h
+++ source/blender/blenkernel/intern/multires_unsubdivide.h
@@ -37,34 +37,35 @@ typedef struct MultiresUnsubdivideGrid {
   int grid_index;
   int grid_size;
 
-  /* Grid coordinates in object space. */
+  /** Grid coordinates in object space. */
   float (*grid_co)[3];
 
 } MultiresUnsubdivideGrid;
 
 typedef struct MultiresUnsubdivideContext {
-  /* Input Mesh to unsubdivide. */
+  /* Input Mesh to un-subdivide. */
   struct Mesh *original_mesh;
   struct MDisps *original_mdisp;
 
-  /* Number of subdivision in the grids of the input mesh. */
+  /** Number of subdivision in the grids of the input mesh. */
   int num_original_levels;
 
-  /* Level 0 base mesh after applying the maximum amount of unsubdivisions. */
+  /** Level 0 base mesh after applying the maximum amount of unsubdivisions. */
   struct Mesh *base_mesh;
 
-  /* Limit on how many levels down the unsubdivide operation should create, if possible. */
+  /** Limit on how many levels down the unsubdivide operation should create, if possible. */
   int max_new_levels;
 
-  /* New levels that were created after unsubdividing. */
+  /** New levels that were created after unsubdividing. */
   int num_new_levels;
 
-  /* Number of subdivisions that should be applied to the base mesh. (num_new_levels +
-   * num_original_levels)
+  /**
+   * Number of subdivisions that should be applied to the base mesh.
+   * (num_new_levels + num_original_levels).
    */
   int num_total_levels;
 
-  /* Data for the new grids, indexed by base mesh loop index. */
+  /** Data for the new grids, indexed by base mesh loop index. */
   int num_grids;
   struct MultiresUnsubdivideGrid *base_mesh_grids;
 
@@ -74,7 +75,7 @@ typedef struct MultiresUnsubdivideContext {
   int *base_to_orig_vmap;
 } MultiresUnsubdivideContext;
 
-/* ================================================================================================
+/* --------------------------------------------------------------------
  * Construct/destruct reshape context.
  */
 
@@ -83,7 +84,7 @@ void multires_unsubdivide_context_init(MultiresUnsubdivideContext *context,
                                        struct MultiresModifierData *mmd);
 void multires_unsubdivide_context_free(MultiresUnsubdivideContext *context);
 
-/* ================================================================================================
+/* --------------------------------------------------------------------
  * Rebuild Lower Subdivisions.
  */
 
