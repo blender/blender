d15efbdc56a3b052ab1b700611d647ec12066d1a
Campbell Barton <ideasman42@gmail.com>
2020-05-21 18:18:00 +1000

    Fix T76922: Meta-balls created twice the specified size

--- source/blender/editors/include/ED_object.h
+++ source/blender/editors/include/ED_object.h
@@ -260,6 +260,7 @@ float ED_object_new_primitive_matrix(struct bContext *C,
 #define OBJECT_ADD_SIZE_MAXF 1.0e12f
 
 void ED_object_add_unit_props_size(struct wmOperatorType *ot);
+void ED_object_add_unit_props_radius_ex(struct wmOperatorType *ot, float default_value);
 void ED_object_add_unit_props_radius(struct wmOperatorType *ot);
 void ED_object_add_generic_props(struct wmOperatorType *ot, bool do_editmode);
 void ED_object_add_mesh_props(struct wmOperatorType *ot);
