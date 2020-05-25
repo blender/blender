9f1f7ba2bbee2bf82b09e63fde4d8a187265f49a
Brecht Van Lommel <brecht@blender.org>
2020-05-25 16:48:11 +0200

    Fix T76925: more Cycles OpenCL compile errors with some drivers on Linux

--- intern/cycles/kernel/kernel_adaptive_sampling.h
+++ intern/cycles/kernel/kernel_adaptive_sampling.h
@@ -185,19 +185,19 @@ ccl_device bool kernel_do_adaptive_filter_x(KernelGlobals *kg, int y, ccl_global
     ccl_global float *buffer = tile->buffer + index * kernel_data.film.pass_stride;
     ccl_global float4 *aux = (ccl_global float4 *)(buffer +
                                                    kernel_data.film.pass_adaptive_aux_buffer);
-    if (aux->w == 0.0f) {
+    if ((*aux).w == 0.0f) {
       any = true;
       if (x > tile->x && !prev) {
         index = index - 1;
         buffer = tile->buffer + index * kernel_data.film.pass_stride;
         aux = (ccl_global float4 *)(buffer + kernel_data.film.pass_adaptive_aux_buffer);
-        aux->w = 0.0f;
+        (*aux).w = 0.0f;
       }
       prev = true;
     }
     else {
       if (prev) {
-        aux->w = 0.0f;
+        (*aux).w = 0.0f;
       }
       prev = false;
     }
@@ -214,19 +214,19 @@ ccl_device bool kernel_do_adaptive_filter_y(KernelGlobals *kg, int x, ccl_global
     ccl_global float *buffer = tile->buffer + index * kernel_data.film.pass_stride;
     ccl_global float4 *aux = (ccl_global float4 *)(buffer +
                                                    kernel_data.film.pass_adaptive_aux_buffer);
-    if (aux->w == 0.0f) {
+    if ((*aux).w == 0.0f) {
       any = true;
       if (y > tile->y && !prev) {
         index = index - tile->stride;
         buffer = tile->buffer + index * kernel_data.film.pass_stride;
         aux = (ccl_global float4 *)(buffer + kernel_data.film.pass_adaptive_aux_buffer);
-        aux->w = 0.0f;
+        (*aux).w = 0.0f;
       }
       prev = true;
     }
     else {
       if (prev) {
-        aux->w = 0.0f;
+        (*aux).w = 0.0f;
       }
       prev = false;
     }
--- intern/cycles/kernel/kernel_path.h
+++ intern/cycles/kernel/kernel_path.h
@@ -662,7 +662,7 @@ ccl_device void kernel_path_trace(
   if (kernel_data.film.pass_adaptive_aux_buffer) {
     ccl_global float4 *aux = (ccl_global float4 *)(buffer +
                                                    kernel_data.film.pass_adaptive_aux_buffer);
-    if (aux->w > 0.0f) {
+    if ((*aux).w > 0.0f) {
       return;
     }
   }
