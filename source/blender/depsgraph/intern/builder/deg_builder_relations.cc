0749cff957c347becd5ba8a8a8abe40d80e6f21f
Jeroen Bakker <jbakker>
2020-06-02 16:02:34 +0200

    Depsgraph: Remove unneeded `RNA_path_resolve`
    Found during research of {T77124}. In `build_driver_data` an identical
     RNA_path is resolved twice. In stead of resolving it twice this patch
    will construct the `property_exit_key` based on the resolution of
    `property_entry_key`.
    
    This change isn't noticeable for users. Just a cleanup as it isn't
    needed to do the same logic twice.
    
    Reviewed By: Sergey Sharybin
    
    Differential Revision: https://developer.blender.org/D7872

--- source/blender/depsgraph/intern/builder/deg_builder_relations.cc
+++ source/blender/depsgraph/intern/builder/deg_builder_relations.cc
@@ -1500,7 +1500,10 @@ void DepsgraphRelationBuilder::build_driver_data(ID *id, FCurve *fcu)
       }
     }
     if (property_entry_key.prop != nullptr && RNA_property_is_idprop(property_entry_key.prop)) {
-      RNAPathKey property_exit_key(id, rna_path, RNAPointerSource::EXIT);
+      RNAPathKey property_exit_key(property_entry_key.id,
+                                   property_entry_key.ptr,
+                                   property_entry_key.prop,
+                                   RNAPointerSource::EXIT);
       OperationKey parameters_key(id, NodeType::PARAMETERS, OperationCode::PARAMETERS_EVAL);
       add_relation(property_exit_key, parameters_key, "Driven Property -> Properties");
     }
