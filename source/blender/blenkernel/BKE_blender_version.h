d114288f90a8fbb2675d802f7e13f25fbee75801
Jeroen Bakker <j.bakker@atmind.nl>
2020-06-22 11:26:26 +0200

    Bumped blender version to 2.83.1

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@
 /* Blender major and minor version. */
 #define BLENDER_VERSION 283
 /* Blender patch version for bugfix releases. */
-#define BLENDER_VERSION_PATCH 0
+#define BLENDER_VERSION_PATCH 1
 /** Blender release cycle stage: alpha/beta/rc/release. */
 #define BLENDER_VERSION_CYCLE release
 
