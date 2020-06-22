5fbbe1fca9b5a4d30a846a068dcfff03991d1622
Aaron Carlisle <carlisle.b3d@gmail.com>
2020-06-22 19:51:29 -0400

    UI: Remove unnecessary ellipsis

--- release/scripts/startup/bl_ui/space_sequencer.py
+++ release/scripts/startup/bl_ui/space_sequencer.py
@@ -731,7 +731,7 @@ class SEQUENCER_MT_strip(Menu):
         layout.operator("sequencer.copy", text="Copy")
         layout.operator("sequencer.paste", text="Paste")
         layout.operator("sequencer.duplicate_move")
-        layout.operator("sequencer.delete", text="Delete...")
+        layout.operator("sequencer.delete", text="Delete")
 
         strip = act_strip(context)
 
