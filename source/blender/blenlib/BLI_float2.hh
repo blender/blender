3623db7784b377cfe9c0428f7a9dd4c06415241a
Jacques Lucke <jacques@blender.org>
2020-07-09 19:03:50 +0200

    BLI: add more operator overloads for float2

--- source/blender/blenlib/BLI_float2.hh
+++ source/blender/blenlib/BLI_float2.hh
@@ -48,6 +48,34 @@ struct float2 {
     return &x;
   }
 
+  float2 &operator+=(const float2 &other)
+  {
+    x += other.x;
+    y += other.y;
+    return *this;
+  }
+
+  float2 &operator-=(const float2 &other)
+  {
+    x -= other.x;
+    y -= other.y;
+    return *this;
+  }
+
+  float2 &operator*=(float factor)
+  {
+    x *= factor;
+    y *= factor;
+    return *this;
+  }
+
+  float2 &operator/=(float divisor)
+  {
+    x /= divisor;
+    y /= divisor;
+    return *this;
+  }
+
   friend float2 operator+(const float2 &a, const float2 &b)
   {
     return {a.x + b.x, a.y + b.y};
