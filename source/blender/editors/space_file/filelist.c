78148e20fa1b983aba71b89cadbf63327a9b09a2
Campbell Barton <ideasman42@gmail.com>
2020-07-14 22:47:18 +1000

    Cleanup: remove tab indention

--- source/blender/editors/space_file/filelist.c
+++ source/blender/editors/space_file/filelist.c
@@ -2660,9 +2660,9 @@ static void filelist_readjob_main_recursive(Main *bmain, FileList *filelist)
   if (filelist->dir[0] == 0) {
     /* make directories */
 #  ifdef WITH_FREESTYLE
-		filelist->filelist.nbr_entries = 27;
+    filelist->filelist.nbr_entries = 27;
 #  else
-		filelist->filelist.nbr_entries = 26;
+    filelist->filelist.nbr_entries = 26;
 #  endif
     filelist_resize(filelist, filelist->filelist.nbr_entries);
 
@@ -2693,11 +2693,11 @@ static void filelist_readjob_main_recursive(Main *bmain, FileList *filelist)
     filelist->filelist.entries[20].entry->relpath = BLI_strdup("Action");
     filelist->filelist.entries[21].entry->relpath = BLI_strdup("NodeTree");
     filelist->filelist.entries[22].entry->relpath = BLI_strdup("Speaker");
-		filelist->filelist.entries[23].entry->relpath = BLI_strdup("Hair");
-		filelist->filelist.entries[24].entry->relpath = BLI_strdup("Point Cloud");
-		filelist->filelist.entries[25].entry->relpath = BLI_strdup("Volume");
+    filelist->filelist.entries[23].entry->relpath = BLI_strdup("Hair");
+    filelist->filelist.entries[24].entry->relpath = BLI_strdup("Point Cloud");
+    filelist->filelist.entries[25].entry->relpath = BLI_strdup("Volume");
 #  ifdef WITH_FREESTYLE
-		filelist->filelist.entries[26].entry->relpath = BLI_strdup("FreestyleLineStyle");
+    filelist->filelist.entries[26].entry->relpath = BLI_strdup("FreestyleLineStyle");
 #  endif
   }
   else {
