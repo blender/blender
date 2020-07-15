2c1edcf3efd8d260a02d74c10a734992eab74072
Clément Foucault <foucault.clem@gmail.com>
2020-07-15 15:01:59 +0200

    EEVEE: Fix undefined behavior in world output

--- source/blender/gpu/shaders/material/gpu_shader_material_output_world.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_output_world.glsl
@@ -4,6 +4,7 @@ void node_output_world(Closure surface, Closure volume, out Closure result)
 {
 #ifndef VOLUMETRICS
   float alpha = renderPassEnvironment ? 1.0 : backgroundAlpha;
+  result = CLOSURE_DEFAULT;
   result.radiance = surface.radiance * alpha;
   result.transmittance = vec3(1.0 - alpha);
 #else
