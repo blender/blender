5a1a4f4dd24de20c369780a0fa0d2f5e73bbeae4
Nathan Letwory <nathan@blender.org>
2020-07-22 16:48:29 +0300

    master is now 2.91.0 alpha.

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -32,15 +32,15 @@ extern "C" {
  */
 
 /* Blender major and minor version. */
-#define BLENDER_VERSION 290
+#define BLENDER_VERSION 291
 /* Blender patch version for bugfix releases. */
 #define BLENDER_VERSION_PATCH 0
 /** Blender release cycle stage: alpha/beta/rc/release. */
-#define BLENDER_VERSION_CYCLE beta
+#define BLENDER_VERSION_CYCLE alpha
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 7
+#define BLENDER_FILE_SUBVERSION 0
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and show a warning if the file
