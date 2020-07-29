84f7de432c72e941477b74ab7cb3b749b85566ac
Germano Cavalcante <germano.costa@ig.com.br>
2020-07-29 09:09:34 -0300

    Fix T78826: Move constraint is not fully disabled
    Caused by rB45f17e10ec50

--- source/blender/editors/transform/transform_constraints.c
+++ source/blender/editors/transform/transform_constraints.c
@@ -510,10 +510,12 @@ static void applyObjectConstraintVec(
   else {
     /* Specific TransData's space. */
     copy_v3_v3(out, in);
-    mul_m3_v3(t->spacemtx_inv, out);
-    mul_m3_v3(td->axismtx, out);
-    if (t->flag & T_EDIT) {
-      mul_m3_v3(tc->mat3_unit, out);
+    if (t->con.mode & CON_APPLY) {
+      mul_m3_v3(t->spacemtx_inv, out);
+      mul_m3_v3(td->axismtx, out);
+      if (t->flag & T_EDIT) {
+        mul_m3_v3(tc->mat3_unit, out);
+      }
     }
   }
 }
