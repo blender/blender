189eb2550d40ba1c6a8c51fd8ccdb1fb762c7403
Richard Antalik <richardantalik@gmail.com>
2020-08-02 20:53:55 +0200

    Fix T66805: No cache invalidation with remove gaps operator
    Reviewed By: brecht
    
    Differential Revision: https://developer.blender.org/D8379

--- source/blender/editors/space_sequencer/sequencer_edit.c
+++ source/blender/editors/space_sequencer/sequencer_edit.c
@@ -1068,6 +1068,7 @@ static bool sequence_offset_after_frame(Scene *scene, const int delta, const int
     if (seq->startdisp >= cfra) {
       BKE_sequence_translate(scene, seq, delta);
       BKE_sequence_calc(scene, seq);
+      BKE_sequence_invalidate_cache_preprocessed(scene, seq);
       done = true;
     }
   }
