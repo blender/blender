6a6cb83eb306d2b774ad2f029dc7beadc43bf28b
Clément Foucault <foucault.clem@gmail.com>
2020-08-18 12:26:10 +0200

    Fix T79568 EEVEE: Film transparent not working
    Since world shader use the same standard output and are considered opaque,
    we need to set alpha as holdout.

--- source/blender/gpu/shaders/material/gpu_shader_material_output_world.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_output_world.glsl
@@ -6,7 +6,8 @@ void node_output_world(Closure surface, Closure volume, out Closure result)
   float alpha = renderPassEnvironment ? 1.0 : backgroundAlpha;
   result = CLOSURE_DEFAULT;
   result.radiance = surface.radiance * alpha;
-  result.transmittance = vec3(1.0 - alpha);
+  result.transmittance = vec3(0.0);
+  result.holdout = (1.0 - alpha);
 #else
   result = volume;
 #endif /* VOLUMETRICS */
