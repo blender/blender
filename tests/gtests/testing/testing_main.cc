48bd6e71026a1ad1e910a1f544c11550115a4b46
Jacques Lucke <jacques@blender.org>
2020-08-19 18:18:20 +0200

    Tests: use guarded allocator in tests
    This helps finding memory leaks in tests.
    
    Reviewers: brecht
    
    Differential Revision: https://developer.blender.org/D8631

--- tests/gtests/testing/testing_main.cc
+++ tests/gtests/testing/testing_main.cc
@@ -48,6 +48,7 @@ const std::string &flags_test_release_dir()
 
 int main(int argc, char **argv)
 {
+  MEM_use_guarded_allocator();
   MEM_init_memleak_detection();
   testing::InitGoogleTest(&argc, argv);
   BLENDER_GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
