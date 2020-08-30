1a623c183f32fb913f4cadd63d2a667087df7637
Campbell Barton <ideasman42@gmail.com>
2020-08-30 13:34:27 +1000

    UI: add "Object -> Clean Up" menu
    Useful to perform cleanup operations on many objects at once,
    also these operations weren't accessible from the search menu.
    
    This follows the convention for other clean up menus
    when editing mesh, curve & grease-pencil.
    
    Resolves issues raised in T80011

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2287,6 +2287,7 @@ class VIEW3D_MT_object(Menu):
         layout.separator()
 
         layout.menu("VIEW3D_MT_object_showhide")
+        layout.menu("VIEW3D_MT_object_cleanup")
 
         layout.separator()
 
@@ -2724,6 +2725,16 @@ class VIEW3D_MT_object_showhide(Menu):
 
         layout.operator("object.hide_view_set", text="Hide Selected").unselected = False
         layout.operator("object.hide_view_set", text="Hide Unselected").unselected = True
+
+
+class VIEW3D_MT_object_cleanup(Menu):
+    bl_label = "Clean Up"
+
+    def draw(self, _context):
+        layout = self.layout
+
+        layout.operator("object.vertex_group_clean", text="Clean Vertex Group Weights").group_select_mode = 'ALL'
+        layout.operator("object.material_slot_remove_unused", text="Remove Unused Material Slots")
 
 
 class VIEW3D_MT_make_single_user(Menu):
@@ -7447,6 +7458,7 @@ classes = (
     VIEW3D_MT_object_constraints,
     VIEW3D_MT_object_quick_effects,
     VIEW3D_MT_object_showhide,
+    VIEW3D_MT_object_cleanup,
     VIEW3D_MT_make_single_user,
     VIEW3D_MT_make_links,
     VIEW3D_MT_brush_paint_modes,
