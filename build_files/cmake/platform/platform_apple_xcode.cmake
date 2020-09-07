5eb5978043c8e7036e15572ea43b083965140e77
Ankit <ankitm>
2020-09-08 00:24:59 +0530

    Generate Xcode scheme files during configuration.
    Every time CMake is re-run, Xcode shows a popup asking if
    user wants to manage schemes automatically or manually.
    Building Blender wiki page recommends managing schemes automatically.
    
    This change sets the default behavior to "automatically" and generates
    the .xcscheme files while CMake is running, instead of hogging Xcode
    later on. With tests enabled, the number of schemes is 203.
    
    Reviewed By: brecht
    
    Differential Revision: https://developer.blender.org/D8820

--- build_files/cmake/platform/platform_apple_xcode.cmake
+++ build_files/cmake/platform/platform_apple_xcode.cmake
@@ -154,3 +154,11 @@ if(NOT ${CMAKE_GENERATOR} MATCHES "Xcode")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
   add_definitions("-DMACOSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
 endif()
+
+if(${CMAKE_GENERATOR} MATCHES "Xcode")
+  # Generate schemes in Blender.xcodeproj/xcshareddata/xcschemes/ early, at
+  # configuration time, not when Xcode is opened.
+  # This gets rid of "Manage schemes automatically" confirmation dialog that
+  # appears whenever CMake is run.
+  set(CMAKE_XCODE_GENERATE_SCHEME ON)
+endif()
