5a28090977d66d8e52d269fb5bb938d953191dde
Nathan Craddock <nzcraddock@gmail.com>
2020-09-22 21:04:47 -0600

    Fix outliner collection icon in light theme
    Ensure the theme colors are used for the collection icon when there are
    no color tags present, otherwise it draws completely black.

--- source/blender/editors/space_outliner/outliner_draw.c
+++ source/blender/editors/space_outliner/outliner_draw.c
@@ -2769,42 +2769,37 @@ static void tselem_draw_icon(uiBlock *block,
                              const bool is_clickable)
 {
   TreeElementIcon data = tree_element_get_icon(tselem, te);
-
   if (data.icon == 0) {
     return;
   }
 
-  if (outliner_is_collection_tree_element(te)) {
-    Collection *collection = outliner_collection_from_tree_element(te);
+  const bool is_collection = outliner_is_collection_tree_element(te);
 
+  /* Collection colors and icons covered by restrict buttons. */
+  if (!is_clickable || x >= xmax || is_collection) {
     /* placement of icons, copied from interface_widgets.c */
     float aspect = (0.8f * UI_UNIT_Y) / ICON_DEFAULT_HEIGHT;
     x += 2.0f * aspect;
     y += 2.0f * aspect;
-    if (collection->color_tag != COLLECTION_COLOR_NONE) {
-      bTheme *btheme = UI_GetTheme();
-      UI_icon_draw_ex(x,
-                      y,
-                      data.icon,
-                      U.inv_dpi_fac,
-                      alpha,
-                      0.0f,
-                      btheme->collection_color[collection->color_tag].color,
-                      true);
+
+    if (is_collection) {
+      Collection *collection = outliner_collection_from_tree_element(te);
+      if (collection->color_tag != COLLECTION_COLOR_NONE) {
+        bTheme *btheme = UI_GetTheme();
+        UI_icon_draw_ex(x,
+                        y,
+                        data.icon,
+                        U.inv_dpi_fac,
+                        alpha,
+                        0.0f,
+                        btheme->collection_color[collection->color_tag].color,
+                        true);
+        return;
+      }
     }
-    else {
-      UI_icon_draw_ex(x, y, data.icon, U.inv_dpi_fac, alpha, 0.0f, NULL, true);
-    }
-  }
-  /* Icon is covered by restrict buttons */
-  else if (!is_clickable || x >= xmax) {
+
     /* Reduce alpha to match icon buttons */
     alpha *= 0.8f;
-
-    /* placement of icons, copied from interface_widgets.c */
-    float aspect = (0.8f * UI_UNIT_Y) / ICON_DEFAULT_HEIGHT;
-    x += 2.0f * aspect;
-    y += 2.0f * aspect;
 
     /* restrict column clip... it has been coded by simply overdrawing,
      * doesn't work for buttons */
