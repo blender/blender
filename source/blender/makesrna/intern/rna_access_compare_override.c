ee51283da1973d1f147f7c51bd7506e4dd804289
Bastien Montagne <bastien@blender.org>
2020-09-25 15:30:25 +0200

    LibOverride: Tweak messages when some sub-item is not found when applying overrides.

--- source/blender/makesrna/intern/rna_access_compare_override.c
+++ source/blender/makesrna/intern/rna_access_compare_override.c
@@ -973,14 +973,16 @@ static void rna_property_override_apply_ex(Main *bmain,
       ptr_item_storage = &private_ptr_item_storage;
 
       if (ptr_item_dst->type == NULL) {
-        printf("Failed to find destination sub-item '%s' of '%s' in new override data '%s'\n",
+        printf("Failed to find destination sub-item '%s' (%d) of '%s' in new override data '%s'\n",
                opop->subitem_reference_name,
+               opop->subitem_reference_index,
                op->rna_path,
                ptr_dst->owner_id->name);
       }
       if (ptr_item_src->type == NULL) {
-        printf("Failed to find source sub-item '%s' of '%s' in old override data '%s'\n",
+        printf("Failed to find source sub-item '%s' (%d) of '%s' in old override data '%s'\n",
                opop->subitem_local_name,
+               opop->subitem_local_index,
                op->rna_path,
                ptr_src->owner_id->name);
       }
