92a0ec87a241fb279f8362e5f2d7cc2c4e57c6d1
Dalai Felinto <dalai@blender.org>
2020-10-02 12:24:59 +0200

    Experimental Features: Rename Image editor option
    This prevents having multiple UI strings starting exactly the same:
    * Legacy A...
    * Legacy B ...

--- source/blender/makesrna/intern/rna_userdef.c
+++ source/blender/makesrna/intern/rna_userdef.c
@@ -6168,7 +6168,7 @@ static void rna_def_userdef_experimental(BlenderRNA *brna)
   prop = RNA_def_property(srna, "use_image_editor_legacy_drawing", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, NULL, "use_image_editor_legacy_drawing", 1);
   RNA_def_property_ui_text(
-      prop, "Legacy Image Editor Drawing", "Use legacy UV/Image editor drawing");
+      prop, "Image Editor Legacy Drawing", "Use legacy UV/Image editor drawing");
 
   prop = RNA_def_property(srna, "use_tools_missing_icons", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, NULL, "use_tools_missing_icons", 1);
