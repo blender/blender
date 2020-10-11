c9a9b032043aca909e61a9e7e184ea0a47535b4a
Aaron Carlisle <carlisle.b3d@gmail.com>
2020-10-11 15:50:24 -0400

    Pydoc: Fix duplicate totree for freestyle

--- release/scripts/freestyle/modules/freestyle/__init__.py
+++ release/scripts/freestyle/modules/freestyle/__init__.py
@@ -21,18 +21,6 @@ This module provides data types of view map components (0D and 1D
 elements), base classes for defining line stylization rules
 (predicates, functions, chaining iterators, and stroke shaders),
 as well as helper functions for style module writing.
-
-Submodules:
-
-.. toctree::
-   :maxdepth: 1
-
-   freestyle.types.rst
-   freestyle.predicates.rst
-   freestyle.functions.rst
-   freestyle.chainingiterators.rst
-   freestyle.shaders.rst
-   freestyle.utils.rst
 """
 
 
