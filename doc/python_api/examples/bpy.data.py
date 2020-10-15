fffa19c017f2024f1dd7f5623ec338f76dedc03f
Jacob Merrill <BluePrintRandom1@gmail.com>
2020-10-15 12:09:21 -0700

    Update Bpy.data.py to include an poseBone example
    this adds an example of getting a pose bone from a armature, and also getting it's F-curve path - to be able to create a new location f-curve if one does not exist or access the existing f-curve

--- doc/python_api/examples/bpy.data.py
+++ doc/python_api/examples/bpy.data.py
@@ -16,6 +16,18 @@ if "Cube" in bpy.data.meshes:
     print("removing mesh", mesh)
     bpy.data.meshes.remove(mesh)
 
+# get a pose bone from an armature and get it's data path to create F-curve if it 
+# does not exist or access it if it does exist
+if 'Armature' in bpy.data.armatures:
+    if 'Bone' in bpy.data.armatures['Armature'].pose.bones:
+        poseBone = bpy.data.armatures['ArmatureName'].pose.bones['BoneName']
+        path = poseBone.path_from_id("location") 
+
+        #if fcurve for location X does not exist create one                 
+        fcu_x = b_obj.animation_data.action.fcurves.find(dpath, index=0)
+        if not fcu_x:
+            fcu_x = b_obj.animation_data.action.fcurves.new(dpath, index=0)
+
 
 # write images into a file next to the blend
 import os
