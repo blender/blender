15cebd8565d093e141a1ca0332eee372fd4ba89e
Pablo Dobarro <pablodp606@gmail.com>
2020-10-20 22:59:12 +0200

    Fix T81901: Use the 2.90 scrape brush preset as default
    The new preset I made for 2.91 is way more controllable with lower
    strength values and does not have the accumulate bug, but until the
    brush management is in place to ship multiple versions of the brush,
    probably most people expect something closer to the old version to be
    the default.
    
    Reviewed By: sergey
    
    Maniphest Tasks: T81901
    
    Differential Revision: https://developer.blender.org/D9289

--- source/blender/blenkernel/intern/brush.c
+++ source/blender/blenkernel/intern/brush.c
@@ -1707,11 +1707,10 @@ void BKE_brush_sculpt_reset(Brush *br)
     case SCULPT_TOOL_SCRAPE:
     case SCULPT_TOOL_FILL:
       br->alpha = 0.7f;
-      br->area_radius_factor = 1.0f;
+      br->area_radius_factor = 0.5f;
       br->spacing = 7;
       br->flag |= BRUSH_ACCUMULATE;
       br->flag |= BRUSH_INVERT_TO_SCRAPE_FILL;
-      br->flag2 |= BRUSH_AREA_RADIUS_PRESSURE;
       break;
     case SCULPT_TOOL_ROTATE:
       br->alpha = 1.0;
