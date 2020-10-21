23ec1f90e71b8c2a63f42811ab4a7729f005f265
Philipp Oeser <info@graphics-engineer.com>
2020-10-21 11:04:39 +0200

    Fix T81896: Outliner missing redraw after "Set Parent (Without Inverse)"
    Outliners listener (outliner_main_region_listener) needs ND_PARENT
    notifier to redraw, the parenting operator only spawned ND_TRANSFORM
    (which doesnt do a redraw).
    
    Maniphest Tasks: T81896
    
    Differential Revision: https://developer.blender.org/D9295

--- source/blender/editors/object/object_relations.c
+++ source/blender/editors/object/object_relations.c
@@ -1244,6 +1244,7 @@ static int parent_noinv_set_exec(bContext *C, wmOperator *op)
 
   DEG_relations_tag_update(bmain);
   WM_event_add_notifier(C, NC_OBJECT | ND_TRANSFORM, NULL);
+  WM_event_add_notifier(C, NC_OBJECT | ND_PARENT, NULL);
 
   return OPERATOR_FINISHED;
 }
