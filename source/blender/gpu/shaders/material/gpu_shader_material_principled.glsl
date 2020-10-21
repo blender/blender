b0f34eee30c42a485e83ca6f5e9495fc0c9283fa
Clément Foucault <foucault.clem@gmail.com>
2020-10-22 00:55:38 +0200

    EEVEE: Principle BSDF: Use multi-scatter switch for the glass variant
    This avoid strange discrepency between the general purpose variant and
    the specialized glass variant which did not have a way to turn
    multi-scatter off.

--- source/blender/gpu/shaders/material/gpu_shader_material_principled.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_principled.glsl
@@ -450,7 +450,7 @@ void node_bsdf_principled_glass(vec4 base_color,
 
   eevee_closure_glass(N,
                       vec3(1.0),
-                      vec3(1.0),
+                      vec3((use_multiscatter != 0.0) ? 1.0 : -1.0),
                       int(ssr_id),
                       roughness,
                       1.0,
