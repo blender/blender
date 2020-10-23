23eaf3117c68f0c2bc636171dddb77630f8c8659
Brecht Van Lommel <brecht@blender.org>
2020-10-23 18:54:45 +0200

    Fix Cycles unnecessary overhead cancelling finished task pool

--- intern/cycles/util/util_task.h
+++ intern/cycles/util/util_task.h
@@ -71,8 +71,8 @@ class TaskPool {
   /* Time time stamp of first task pushed. */
   double start_time;
 
-  /* Number of all tasks handled by this pool. */
-  int num_tasks_handled;
+  /* Number of all tasks pushed to the pool. Cleared after wait_work() and cancel(). */
+  int num_tasks_pushed;
 };
 
 /* Task Scheduler
