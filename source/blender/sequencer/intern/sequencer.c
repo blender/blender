8eb7344731f2e8cc910b03beb13722a31fc46f61
Richard Antalik <richardantalik@gmail.com>
2020-10-26 00:31:49 +0100

    Fix Recursion when rendering scene strip
    Recursion happens in case when scene strip point to it's own scene
    indirectly by using SEQ_SCENE_STRIPS option.
    
    Reviewed By: sergey
    
    Differential Revision: https://developer.blender.org/D9264

--- source/blender/sequencer/intern/sequencer.c
+++ source/blender/sequencer/intern/sequencer.c
@@ -6045,6 +6045,12 @@ static Sequence *sequencer_check_scene_recursion(Scene *scene, ListBase *seqbase
       return seq;
     }
 
+    if (seq->type == SEQ_TYPE_SCENE && (seq->flag & SEQ_SCENE_STRIPS)) {
+      if (sequencer_check_scene_recursion(scene, &seq->scene->ed->seqbase)) {
+        return seq;
+      }
+    }
+
     if (seq->type == SEQ_TYPE_META && sequencer_check_scene_recursion(scene, &seq->seqbase)) {
       return seq;
     }
