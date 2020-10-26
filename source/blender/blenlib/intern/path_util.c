8926b09fa9a065912aa0237015c6085200834cd3
Bastien Montagne <bastien@blender.org>
2020-10-26 16:31:11 +0100

    Fix T81421: "Saving As..." a blend file with a Script node file path filled with 1023 symbols crashes Blender.
    Usual lack of protection against buffer overflows when manipulating
    strings.
    
    Also add some basic tests for `BLI_path_rel`.

--- source/blender/blenlib/intern/path_util.c
+++ source/blender/blenlib/intern/path_util.c
@@ -641,7 +641,7 @@ void BLI_path_rel(char *file, const char *relfile)
     }
 
     /* don't copy the slash at the beginning */
-    r += BLI_strcpy_rlen(r, q + 1);
+    r += BLI_strncpy_rlen(r, q + 1, FILE_MAX - (r - res));
 
 #ifdef WIN32
     BLI_str_replace_char(res + 2, '/', '\\');
