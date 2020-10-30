8b806c86e12e92ffc8b486a69ce4de5938f09b4a
Brecht Van Lommel <brecht@blender.org>
2020-10-30 17:28:08 +0100

    Fix T82027, T81718: Cycles crash with volume animation playback

--- intern/cycles/render/volume.cpp
+++ intern/cycles/render/volume.cpp
@@ -583,6 +583,7 @@ void GeometryManager::create_volume_mesh(Volume *volume, Progress &progress)
   volume->clear();
   volume->reserve_mesh(vertices.size(), indices.size() / 3);
   volume->used_shaders.push_back(volume_shader);
+  volume->need_update = true;
   volume->need_update_rebuild = true;
 
   for (size_t i = 0; i < vertices.size(); ++i) {
