1d4f3e2a2663206e707b00145cf4f13710e2ccac
Nathan Craddock <nzcraddock@gmail.com>
2020-11-16 13:54:05 -0700

    Outliner: Collection icon color tweaks
    The icons originally chosen for the collection colors were selected
    during development and had a few issues with contrast in the light
    theme, and the gray color was not a good choice against the default gray
    backgrounds.
    
    The new colors are more readable in both default Blender themes. Gray
    was replaced with pink.
    
    Differential Revision: https://developer.blender.org/D9504

--- release/datafiles/userdef/userdef_default_theme.c
+++ release/datafiles/userdef/userdef_default_theme.c
@@ -1108,28 +1108,28 @@ const bTheme U_theme_default = {
   },
   .collection_color = {
     {
-      .color = RGBA(0xe4312bff),
+      .color = RGBA(0xe2605bff),
     },
     {
-      .color = RGBA(0xef7e42ff),
+      .color = RGBA(0xf1a355ff),
     },
     {
-      .color = RGBA(0xe4dd52ff),
+      .color = RGBA(0xf1dc55ff),
     },
     {
-      .color = RGBA(0x9ac546ff),
+      .color = RGBA(0x7bcc7bff),
     },
     {
-      .color = RGBA(0x46bcc2ff),
+      .color = RGBA(0x5db6eaff),
     },
     {
-      .color = RGBA(0x8b65dcff),
+      .color = RGBA(0x8d59daff),
     },
     {
-      .color = RGBA(0x999999ff),
+      .color = RGBA(0xc673b8ff),
     },
     {
-      .color = RGBA(0x06d4432ff),
+      .color = RGBA(0x7a5441ff),
     },
   },
 };
