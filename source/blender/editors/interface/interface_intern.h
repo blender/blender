0688309988e546382748b9e755d84ae8a5059192
Campbell Barton <ideasman42@gmail.com>
2020-11-18 15:09:34 +1100

    Fix undo UI text containing shortcuts & newlines
    - Shortcuts were being shown in the undo history.
    - Multi-line tool-tips now only use the first line.

--- source/blender/editors/interface/interface_intern.h
+++ source/blender/editors/interface/interface_intern.h
@@ -1088,6 +1088,9 @@ uiBut *ui_list_find_mouse_over_ex(struct ARegion *region, int x, int y) ATTR_WAR
 
 bool ui_but_contains_password(const uiBut *but) ATTR_WARN_UNUSED_RESULT;
 
+size_t ui_but_drawstr_len_without_sep_char(const uiBut *but);
+size_t ui_but_tip_len_only_first_line(const uiBut *but);
+
 uiBut *ui_but_prev(uiBut *but) ATTR_WARN_UNUSED_RESULT;
 uiBut *ui_but_next(uiBut *but) ATTR_WARN_UNUSED_RESULT;
 uiBut *ui_but_first(uiBlock *block) ATTR_WARN_UNUSED_RESULT;
