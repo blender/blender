9234a6a619fdb7530ad7e2e005a6c389e3e6f886
Pablo Dobarro <pablodp606@gmail.com>
2020-11-20 18:26:18 +0100

    Fix T82872: Add design task link for tilt support
    Reviewed By: sergey, Blendify
    
    Maniphest Tasks: T82872
    
    Differential Revision: https://developer.blender.org/D9615

--- release/scripts/startup/bl_ui/space_userpref.py
+++ release/scripts/startup/bl_ui/space_userpref.py
@@ -2187,7 +2187,7 @@ class USERPREF_PT_experimental_new_features(ExperimentalPanel, Panel):
             context, (
                 ({"property": "use_sculpt_vertex_colors"}, "T71947"),
                 ({"property": "use_switch_object_operator"}, "T80402"),
-                ({"property": "use_sculpt_tools_tilt"}, "T00000"),
+                ({"property": "use_sculpt_tools_tilt"}, "T82877"),
                 ({"property": "use_object_add_tool"}, "T57210"),
             ),
         )
