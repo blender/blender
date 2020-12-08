d7cf464b493581a381540673aa7ed9e4ff47b425
Patrick Mours <pmours@nvidia.com>
2020-12-08 16:13:04 +0100

    Cycles: Remove "OptiX support is experimental" notice
    OptiX support is not in fact experimental anymore, so it is time for that notice to go.
    All Cycles features that are currently supported on the GPU do work now when OptiX is selected.

--- intern/cycles/blender/addon/properties.py
+++ intern/cycles/blender/addon/properties.py
@@ -1620,11 +1620,6 @@ class CyclesPreferences(bpy.types.AddonPreferences):
         for device in devices:
             box.prop(device, "use", text=device.name)
 
-        if device_type == 'OPTIX':
-            col = box.column(align=True)
-            col.label(text="OptiX support is experimental", icon='INFO')
-            col.label(text="Not all Cycles features are supported yet", icon='BLANK1')
-
     def draw_impl(self, layout, context):
         row = layout.row()
         row.prop(self, "compute_device_type", expand=True)
