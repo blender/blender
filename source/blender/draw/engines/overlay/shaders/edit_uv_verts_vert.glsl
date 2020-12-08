2b9eb5eee58acb81d8ae7e64a5a95526623d64d7
Philipp Oeser <info@graphics-engineer.com>
2020-12-08 16:10:08 +0100

    Fix T83361: UV Editor does not draw pinned selected UVs as pinned
    Caused by rB4212b6528af.
    
    outlineColor is computed by the vertex shader, so not a uniform.
    So outlineColor was undefined.
    
    note: it was still possible to run into the situation that a selected UV
    is drawn ontop of a selected pinned UV [you had to disable sticky
    selection for this], now also make sure selected-pinned are drawn
    topmost, then selected, then unselected UVs.
    
    Maniphest Tasks: T83361
    
    Differential Revision: https://developer.blender.org/D9786

--- source/blender/draw/engines/overlay/shaders/edit_uv_verts_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/edit_uv_verts_vert.glsl
@@ -26,7 +26,7 @@ void main()
   /* Move selected vertices to the top
    * Vertices are between 0.0 and 0.2, Edges between 0.2 and 0.4
    * actual pixels are at 0.75, 1.0 is used for the background. */
-  float depth = is_selected ? 0.05 : 0.15;
+  float depth = is_selected ? (is_pinned ? 0.05 : 0.10) : 0.15;
   gl_Position = vec4(point_world_to_ndc(world_pos).xy, depth, 1.0);
   gl_PointSize = pointSize;
 
