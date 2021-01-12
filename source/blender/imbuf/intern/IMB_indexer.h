28b17ef0e6a82361f7061b9e074609dde9a76254
Campbell Barton <ideasman42@gmail.com>
2021-01-12 14:31:30 +1100

    Cleanup: replace 'long long' with int64_t in imbuf indexer

--- source/blender/imbuf/intern/IMB_indexer.h
+++ source/blender/imbuf/intern/IMB_indexer.h
@@ -48,9 +48,9 @@
 
 typedef struct anim_index_entry {
   int frameno;
-  unsigned long long seek_pos;
-  unsigned long long seek_pos_dts;
-  unsigned long long pts;
+  uint64_t seek_pos;
+  uint64_t seek_pos_dts;
+  uint64_t pts;
 } anim_index_entry;
 
 struct anim_index {
@@ -77,28 +77,25 @@ typedef struct anim_index_builder {
 } anim_index_builder;
 
 anim_index_builder *IMB_index_builder_create(const char *name);
-void IMB_index_builder_add_entry(anim_index_builder *fp,
-                                 int frameno,
-                                 unsigned long long seek_pos,
-                                 unsigned long long seek_pos_dts,
-                                 unsigned long long pts);
+void IMB_index_builder_add_entry(
+    anim_index_builder *fp, int frameno, uint64_t seek_pos, uint64_t seek_pos_dts, uint64_t pts);
 
 void IMB_index_builder_proc_frame(anim_index_builder *fp,
                                   unsigned char *buffer,
                                   int data_size,
                                   int frameno,
-                                  unsigned long long seek_pos,
-                                  unsigned long long seek_pos_dts,
-                                  unsigned long long pts);
+                                  uint64_t seek_pos,
+                                  uint64_t seek_pos_dts,
+                                  uint64_t pts);
 
 void IMB_index_builder_finish(anim_index_builder *fp, int rollback);
 
 struct anim_index *IMB_indexer_open(const char *name);
-unsigned long long IMB_indexer_get_seek_pos(struct anim_index *idx, int frame_index);
-unsigned long long IMB_indexer_get_seek_pos_dts(struct anim_index *idx, int frame_index);
+uint64_t IMB_indexer_get_seek_pos(struct anim_index *idx, int frame_index);
+uint64_t IMB_indexer_get_seek_pos_dts(struct anim_index *idx, int frame_index);
 
 int IMB_indexer_get_frame_index(struct anim_index *idx, int frameno);
-unsigned long long IMB_indexer_get_pts(struct anim_index *idx, int frame_index);
+uint64_t IMB_indexer_get_pts(struct anim_index *idx, int frame_index);
 int IMB_indexer_get_duration(struct anim_index *idx);
 
 int IMB_indexer_can_scan(struct anim_index *idx, int old_frame_index, int new_frame_index);
