zyxcambridge/blender_llm

GPU: Add HQ normals workaround.
This change makes it possible for platforms to only support high quality
normal rendering. This is part of {T82856} where current AMD drivers
running on the polaris architecture does not support the low quality
setting due to a driver bug.

In a next commit the work around will be enabled.

source/blender/draw/intern/draw_cache_impl_curve.c
@@ -36,6 +36,7 @@
 #include "BKE_font.h"
 
 #include "GPU_batch.h"
+#include "GPU_capabilities.h"
 #include "GPU_material.h"
 #include "GPU_texture.h"
 
@@ -634,11 +635,11 @@ static void curve_create_edit_curves_nor(CurveRenderData *rdata,
                                          GPUVertBuf *vbo_curves_nor,
                                          const Scene *scene)
 {
-  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0;
+  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0 ||
+                             GPU_use_hq_normals_workaround();
 
   static GPUVertFormat format = {0};
   static GPUVertFormat format_hq = {0};
-  /* TODO(jeroen): add support for high quality normals */
   static struct {
     uint pos, nor, tan, rad;
     uint pos_hq, nor_hq, tan_hq, rad_hq;

source/blender/draw/intern/draw_cache_impl_volume.c
@@ -40,6 +40,7 @@
 #include "BKE_volume_render.h"
 
 #include "GPU_batch.h"
+#include "GPU_capabilities.h"
 #include "GPU_texture.h"
 
 #include "DEG_depsgraph_query.h"
@@ -157,7 +158,8 @@ static void drw_volume_wireframe_cb(
   Scene *scene = data->scene;
   Volume *volume = data->volume;
   VolumeBatchCache *cache = volume->batch_cache;
-  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0;
+  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0 ||
+                             GPU_use_hq_normals_workaround();
 
   /* Create vertex buffer. */
   static GPUVertFormat format = {0};

blender/blender

source/blender/draw/intern/draw_cache_impl_curve.c
@@ -36,6 +36,7 @@
 #include "BKE_font.h"
 
 #include "GPU_batch.h"
+#include "GPU_extensions.h"
 #include "GPU_material.h"
 #include "GPU_texture.h"
 
@@ -633,11 +634,11 @@ static void curve_create_edit_curves_nor(CurveRenderData *rdata,
                                          GPUVertBuf *vbo_curves_nor,
                                          const Scene *scene)
 {
-  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0;
+  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0 ||
+                             GPU_use_hq_normals_workaround();
 
   static GPUVertFormat format = {0};
   static GPUVertFormat format_hq = {0};
-  /* TODO(jeroen): add support for high quality normals */
   static struct {
     uint pos, nor, tan, rad;
     uint pos_hq, nor_hq, tan_hq, rad_hq;

source/blender/draw/intern/draw_cache_impl_volume.c
@@ -40,6 +40,7 @@
 #include "BKE_volume_render.h"
 
 #include "GPU_batch.h"
+#include "GPU_extensions.h"
 #include "GPU_texture.h"
 
 #include "DRW_render.h"
@@ -151,7 +152,8 @@ static void drw_volume_wireframe_cb(
   Scene *scene = data->scene;
   Volume *volume = data->volume;
   VolumeBatchCache *cache = volume->batch_cache;
-  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0;
+  const bool do_hq_normals = (scene->r.perf_flag & SCE_PERF_HQ_NORMALS) != 0 ||
+                             GPU_use_hq_normals_workaround();
 
   /* Create vertex buffer. */
   static GPUVertFormat format = {0};