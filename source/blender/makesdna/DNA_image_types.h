fba35a02c6a8b722984ea9a9572cfbd1cb65d6ad
Sergey Sharybin <sergey@blender.org>
2021-01-25 11:35:29 +0100

    Fix padding issue in DNA
    Was introduced in the previous change in the image.

--- source/blender/makesdna/DNA_image_types.h
+++ source/blender/makesdna/DNA_image_types.h
@@ -158,7 +158,7 @@ typedef struct Image {
   short gpu_pass;
   short gpu_layer;
   short gpu_view;
-  char _pad2[6];
+  char _pad2[4];
 
   /** Deprecated. */
   struct PackedFile *packedfile DNA_DEPRECATED;
