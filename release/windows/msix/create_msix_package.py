zyxcambridge/blender_llm

Fix T79356: Improved icons for MSIX builds
Fixed an issue that was causing the app icon to render with a
'plated' background color in the taskbar and other areas of Windows.

Updated all app icons in Microsoft Store package to match Microsoft's
design recommendations.

Added multiple scales for app icons for high resolution displays.

Added high contrast app icons.

Reviewed By: pablovazquez, jmonteath

Maniphest Tasks: T79356

Differential Revision: https://developer.blender.org/D9681

release/windows/msix/create_msix_package.py
@@ -95,6 +95,9 @@ content_blender_folder = pathlib.Path(content_folder, "Blender").absolute()
 content_assets_folder = pathlib.Path(content_folder, "Assets")
 assets_original_folder = pathlib.Path(".", "Assets")
 
+pri_config_file = pathlib.Path(".", "priconfig.xml")
+pri_resources_file = pathlib.Path(content_folder, "resources.pri")
+
 local_blender_zip = pathlib.Path(".", "blender.zip")
 
 if args.pfx:
@@ -106,6 +109,13 @@ if args.pfx:
 else:
     pfx_path = None
     print("Creating MSIX package without signing.")
+
+pri_new_command = ["makepri",
+                   "new",
+                   "/pr", f"{content_folder.absolute()}",
+                   "/cf", f"{pri_config_file.absolute()}",
+                   "/of", f"{pri_resources_file.absolute()}"
+                   ]
 
 msix_command = ["makeappx",
                 "pack",
@@ -164,6 +174,9 @@ with zipfile.ZipFile(local_blender_zip, "r") as blender_zip:
 
 print("... extraction complete.")
 
+
+print(f"Generating Package Resource Index (PRI) file using command: {' '.join(pri_new_command)}")
+execute_command(pri_new_command, "MakePri", 4)
 
 print(f"Creating MSIX package using command: {' '.join(msix_command)}")
 

blender/blender

release/windows/msix/create_msix_package.py
@@ -46,6 +46,9 @@ content_blender_folder = pathlib.Path(content_folder, "Blender").absolute()
 content_assets_folder = pathlib.Path(content_folder, "Assets")
 assets_original_folder = pathlib.Path(".", "Assets")
 
+pri_config_file = pathlib.Path(".", "priconfig.xml")
+pri_resources_file = pathlib.Path(content_folder, "resources.pri")
+
 local_blender_zip = pathlib.Path(".", "blender.zip")
 
 if args.pfx:
@@ -57,6 +60,13 @@ if args.pfx:
 else:
     pfx_path = None
     print("Creating MSIX package without signing.")
+
+pri_command = ["makepri",
+                   "new",
+                   "/pr", f"{content_folder.absolute()}",
+                   "/cf", f"{pri_config_file.absolute()}",
+                   "/of", f"{pri_resources_file.absolute()}"
+                   ]
 
 msix_command = ["makeappx",
                 "pack",
@@ -115,6 +125,8 @@ with zipfile.ZipFile(local_blender_zip, "r") as blender_zip:
 print("... extraction complete.")
 
 
+print(f"Generating Package Resource Index (PRI) file using command: {' '.join(pri_command)}")
+execute_command(pri_command, "MakePri", 4)
 
 print(f"Creating MSIX package using command: {' '.join(msix_command)}")
 