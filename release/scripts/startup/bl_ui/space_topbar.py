fb10eaf6e8a4f959cd5d46fd1bb7e8fb26b67205
Campbell Barton <ideasman42@gmail.com>
2021-01-27 15:16:30 +1100

    UI: add ellipsis to batch rename menu item

--- release/scripts/startup/bl_ui/space_topbar.py
+++ release/scripts/startup/bl_ui/space_topbar.py
@@ -567,7 +567,7 @@ class TOPBAR_MT_edit(Menu):
         props.name = "TOPBAR_PT_name"
         props.keep_open = False
 
-        layout.operator("wm.batch_rename")
+        layout.operator("wm.batch_rename", text="Batch Rename...")
 
         layout.separator()
 
