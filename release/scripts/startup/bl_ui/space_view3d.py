88f845c881d8e9285c86810755d673642bcbe1e0
Antonio Vazquez <blendergit@gmail.com>
2021-03-13 19:52:10 +0100

    GPencil: Remove word "Strokes" in menu
    This remove redundant word.

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -5004,7 +5004,7 @@ class VIEW3D_MT_edit_gpencil(Menu):
 
         layout.menu("VIEW3D_MT_edit_gpencil_showhide")
 
-        layout.operator_menu_enum("gpencil.stroke_separate", "mode")
+        layout.operator_menu_enum("gpencil.stroke_separate", "mode", text="Separate")
         layout.menu("GPENCIL_MT_cleanup")
 
         layout.separator()
