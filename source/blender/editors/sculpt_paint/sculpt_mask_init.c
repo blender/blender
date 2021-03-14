4ad1a7ab313d9a0382cf4c522412b3b882184b86
Pablo Dobarro <pablodp606@gmail.com>
2021-03-14 21:14:39 +0100

    Cleanup: Clang format

--- source/blender/editors/sculpt_paint/sculpt_mask_init.c
+++ source/blender/editors/sculpt_paint/sculpt_mask_init.c
@@ -24,8 +24,8 @@
 #include "MEM_guardedalloc.h"
 
 #include "BLI_blenlib.h"
-#include "BLI_math.h"
 #include "BLI_hash.h"
+#include "BLI_math.h"
 #include "BLI_task.h"
 
 #include "BLT_translation.h"
