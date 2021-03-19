a7455ae5f8e86c28b15a31429a7c7cfe767211e8
Campbell Barton <ideasman42@gmail.com>
2021-03-19 16:23:52 +1100

    Cleanup: remove unused function

--- source/blender/blenkernel/BKE_armature.h
+++ source/blender/blenkernel/BKE_armature.h
@@ -201,11 +201,6 @@ void BKE_pose_apply_action(struct Object *ob,
                            struct bAction *action,
                            struct AnimationEvalContext *anim_eval_context);
 
-/* get_objectspace_bone_matrix has to be removed still */
-void get_objectspace_bone_matrix(struct Bone *bone,
-                                 float M_accumulatedMatrix[4][4],
-                                 int root,
-                                 int posed);
 void vec_roll_to_mat3(const float vec[3], const float roll, float r_mat[3][3]);
 void vec_roll_to_mat3_normalized(const float nor[3], const float roll, float r_mat[3][3]);
 void mat3_to_vec_roll(const float mat[3][3], float r_vec[3], float *r_roll);
