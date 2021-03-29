bc872e0c8e3e951aff6363acb4372b498a256d18
Brecht Van Lommel <brecht@blender.org>
2021-03-29 16:27:18 +0200

    Fix: Line Art panel in properties showing in the wrong order with add-ons
    It should set bl_order to show below panels registered by render engine add-ons
    like Cycles.

--- release/scripts/startup/bl_ui/properties_material.py
+++ release/scripts/startup/bl_ui/properties_material.py
@@ -277,6 +277,7 @@ class MATERIAL_PT_viewport(MaterialButtonsPanel, Panel):
 class MATERIAL_PT_lineart(MaterialButtonsPanel, Panel):
     bl_label = "Line Art"
     bl_options = {'DEFAULT_CLOSED'}
+    bl_order = 10
 
     @classmethod
     def poll(cls, context):
