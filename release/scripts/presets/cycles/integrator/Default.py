3fa580866ef5514f6fa7f9c5a369249f69135c78
Brecht Van Lommel <brecht@blender.org>
2021-04-05 13:53:26 +0200

    Cycles: update Light Paths preset
    * Add Fast GI / AO bounces to presets
    * Add Default preset matching defaults
    * Add Fast Approximate GI preset
    * Lower Full GI depths to 32

--- release/scripts/presets/cycles/integrator/Default.py
+++ release/scripts/presets/cycles/integrator/Default.py
@@ -1,12 +1,12 @@
 import bpy
 cycles = bpy.context.scene.cycles
 
-cycles.max_bounces = 8
-cycles.caustics_reflective = False
-cycles.caustics_refractive = False
-cycles.diffuse_bounces = 0
-cycles.glossy_bounces = 1
-cycles.transmission_bounces = 2
+cycles.max_bounces = 12
+cycles.caustics_reflective = True
+cycles.caustics_refractive = True
+cycles.diffuse_bounces = 4
+cycles.glossy_bounces = 4
+cycles.transmission_bounces = 12
 cycles.volume_bounces = 0
 cycles.transparent_max_bounces = 8
 cycles.use_fast_gi = False
--- release/scripts/presets/cycles/integrator/Full_Global_Illumination.py
+++ release/scripts/presets/cycles/integrator/Full_Global_Illumination.py
@@ -1,11 +1,14 @@
 import bpy
 cycles = bpy.context.scene.cycles
 
-cycles.max_bounces = 128
+cycles.max_bounces = 32
 cycles.caustics_reflective = True
 cycles.caustics_refractive = True
-cycles.diffuse_bounces = 128
-cycles.glossy_bounces = 128
-cycles.transmission_bounces = 128
-cycles.volume_bounces = 128
-cycles.transparent_max_bounces = 128
+cycles.diffuse_bounces = 32
+cycles.glossy_bounces = 32
+cycles.transmission_bounces = 32
+cycles.volume_bounces = 32
+cycles.transparent_max_bounces = 32
+cycles.use_fast_gi = False
+cycles.ao_bounces = 1
+cycles.ao_bounces_render = 1
