18a86af4162dded0fd961a9f965bf5c5a9e6d5bf
Pablo Dobarro <pablodp606@gmail.com>
2021-04-18 19:00:02 +0200

    Fix neighbor connectivy with hidden edges

--- source/blender/editors/sculpt_paint/sculpt.c
+++ source/blender/editors/sculpt_paint/sculpt.c
@@ -816,6 +816,9 @@ static void sculpt_vertex_neighbors_get_faces(SculptSession *ss,
   iter->neighbors = iter->neighbors_fixed;
 
   for (int i = 0; i < ss->pmap[index].count; i++) {
+    if (ss->face_sets[vert_map->indices[i]] <= 0) {
+      continue;
+    }
     const MPoly *p = &ss->mpoly[vert_map->indices[i]];
     uint f_adj_v[2];
     if (poly_get_adj_loops_from_vert(p, ss->mloop, index, f_adj_v) != -1) {
