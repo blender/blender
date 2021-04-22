47892d6695bc9a8e3fab012c3ba4005a2cb4a767
Julian Eisel <julian@blender.org>
2021-04-22 19:40:04 +0200

    Fix too big grid plane for Add Object tool on Retina displays
    The grid plane was drawn too big on retina displays compared to other screens,
    because the factor was multiplied by the native pixel-size, which is 2 for
    Retina displays.

--- source/blender/editors/space_view3d/view3d_placement.c
+++ source/blender/editors/space_view3d/view3d_placement.c
@@ -2061,7 +2061,7 @@ static void cursor_plane_draw(bContext *C, int x, int y, void *customdata)
     GPU_matrix_projection_set(rv3d->winmat);
     GPU_matrix_set(rv3d->viewmat);
 
-    const float scale_mod = U.gizmo_size * 2 * U.dpi_fac;
+    const float scale_mod = U.gizmo_size * 2 * U.dpi_fac / U.pixelsize;
 
     float final_scale = (scale_mod * pixel_size);
 
