ec0a3ff4568a918b3cc86ff845347d552087e726
Wannes Malfait <Wannes>
2021-04-29 14:48:21 +0530

    Fix build error: use of unintialized variable
    Differential Revision: https://developer.blender.org/D11115

--- source/blender/bmesh/tools/bmesh_bisect_plane.c
+++ source/blender/bmesh/tools/bmesh_bisect_plane.c
@@ -212,7 +212,7 @@ static void bm_face_bisect_verts(
     }
     else {
 
-      uint i;
+      uint i = 0;
 
       /* ---- */
       /* Check contiguous spans of centered vertices (skipping when necessary). */
