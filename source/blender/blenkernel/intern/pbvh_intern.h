45a14a20de9cfd11695195ea7b2aaceb2734e747
Hans Goudey <h.goudey@me.com>
2021-04-29 21:25:34 -0500

    Cleanup: Mismatched array bounds in function declaration

--- source/blender/blenkernel/intern/pbvh_intern.h
+++ source/blender/blenkernel/intern/pbvh_intern.h
@@ -194,9 +194,9 @@ bool ray_face_intersection_quad(const float ray_start[3],
                                 float *depth);
 bool ray_face_intersection_tri(const float ray_start[3],
                                struct IsectRayPrecalc *isect_precalc,
-                               const float *t0,
-                               const float *t1,
-                               const float *t2,
+                               const float t0[3],
+                               const float t1[3],
+                               const float t2[3],
                                float *depth);
 
 bool ray_face_nearest_quad(const float ray_start[3],
