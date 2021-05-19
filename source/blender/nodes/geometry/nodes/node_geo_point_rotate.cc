3d604ba867ed34b0e0ad7f58bcd2ffc482f8ada3
Hans Goudey <h.goudey@me.com>
2021-05-19 15:58:34 -0400

    Geometry Nodes: Execute point rotate node on curve component
    When adding attributes on the curve component, I missed calling this
    node's execute function on the curve component, like the other nodes.

--- source/blender/nodes/geometry/nodes/node_geo_point_rotate.cc
+++ source/blender/nodes/geometry/nodes/node_geo_point_rotate.cc
@@ -176,6 +176,9 @@ static void geo_node_point_rotate_exec(GeoNodeExecParams params)
   if (geometry_set.has<PointCloudComponent>()) {
     point_rotate_on_component(geometry_set.get_component_for_write<PointCloudComponent>(), params);
   }
+  if (geometry_set.has<CurveComponent>()) {
+    point_rotate_on_component(geometry_set.get_component_for_write<CurveComponent>(), params);
+  }
 
   params.set_output("Geometry", geometry_set);
 }
