7438f0c6c0513dec73c0b91e62c5fc52bbcde3dd
Campbell Barton <ideasman42@gmail.com>
2021-05-27 02:23:19 +1000

    Cleanup: array-parameter warning with GCC 11
    Pass the string size as this is less error prone in general.

--- source/blender/editors/transform/transform_mode_skin_resize.c
+++ source/blender/editors/transform/transform_mode_skin_resize.c
@@ -64,7 +64,7 @@ static void applySkinResize(TransInfo *t, const int UNUSED(mval[2]))
 
   size_to_mat3(mat, t->values_final);
 
-  headerResize(t, t->values_final, str);
+  headerResize(t, t->values_final, str, sizeof(str));
 
   FOREACH_TRANS_DATA_CONTAINER (t, tc) {
     TransData *td = tc->data;
