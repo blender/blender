1f19a861502f4a670871460a25eafa43e6a090ce
Joseph Eagar <joeedh@gmail.com>
2021-05-27 12:15:49 -0700

    DynTopo:
    * Fixed failed patch reversion from a while ago
    * Also tweaked a few comments.

--- source/blender/draw/intern/draw_cache.c
+++ source/blender/draw/intern/draw_cache.c
@@ -35,9 +35,9 @@
 #include "BLI_math.h"
 #include "BLI_utildefines.h"
 
+#include "BKE_customdata.h"
 #include "BKE_object.h"
 #include "BKE_paint.h"
-#include "BKE_customdata.h"
 
 #include "GPU_batch.h"
 #include "GPU_batch_utils.h"
@@ -243,7 +243,7 @@ static void UNUSED_FUNCTION(add_fancy_edge)(GPUVertBuf *vbo,
   GPU_vertbuf_attr_set(vbo, pos_id, (*v_idx)++, co2);
 }
 
-#if 0  /* UNUSED */
+#if 0 /* UNUSED */
 static void add_lat_lon_vert(GPUVertBuf *vbo,
                              uint pos_id,
                              uint nor_id,
@@ -485,16 +485,19 @@ static void sphere_lat_lon_vert(GPUVertBuf *vbo, int *v_ofs, float lat, float lo
   (*v_ofs)++;
 }
 
-void DRW_make_cdlayer_attr_aliases(GPUVertFormat *format, char *base_name, CustomData *data, CustomDataLayer *cl)
+void DRW_make_cdlayer_attr_aliases(GPUVertFormat *format,
+                                   char *base_name,
+                                   CustomData *data,
+                                   CustomDataLayer *cl)
 {
   char attr_name[32], attr_safe_name[GPU_MAX_SAFE_ATTR_NAME];
   const char *layer_name = cl->name;
 
-  int i = (int) (cl - data->typemap[cl->type]);
+  int i = (int)(cl - data->typemap[cl->type]);
 
   GPU_vertformat_safe_attr_name(layer_name, attr_safe_name, GPU_MAX_SAFE_ATTR_NAME);
 
-  /* UV layer name. */
+  /* Attribute layer name. */
   BLI_snprintf(attr_name, sizeof(attr_name), "%s%s", base_name, attr_safe_name);
   GPU_vertformat_alias_add(format, attr_name);
 
@@ -512,7 +515,7 @@ void DRW_make_cdlayer_attr_aliases(GPUVertFormat *format, char *base_name, Custo
     GPU_vertformat_alias_add(format, attr_name);
   }
 
-  /* Stencil mask uv layer name. */
+  /* Stencil mask layer name. */
   if (i == CustomData_get_stencil_layer(data, cl->type)) {
     BLI_snprintf(attr_name, sizeof(attr_name), "m%s", base_name);
     GPU_vertformat_alias_add(format, attr_name);
