1f557867916bd6ab43ced4b4fd651a7d1d11f8ac
Richard Antalik <richardantalik@gmail.com>
2021-06-02 21:29:38 +0200

    Fix T57397: Movies are blurred after sws_scale
    Images with 4:2:2 and 4:4:4 chroma subsampling were blurred when
    `SWS_FAST_BILINEAR` interpolation is set for `anim->img_convert_ctx`.
    
    Use `SWS_BILINEAR` interpolation for all movies, as performance is
    not impacted by this change.
    
    Reviewed By: sergey
    
    Differential Revision: https://developer.blender.org/D11457

--- source/blender/imbuf/intern/anim_movie.c
+++ source/blender/imbuf/intern/anim_movie.c
@@ -719,7 +719,7 @@ static int startffmpeg(struct anim *anim)
                                          anim->x,
                                          anim->y,
                                          AV_PIX_FMT_RGBA,
-                                         SWS_FAST_BILINEAR | SWS_PRINT_INFO | SWS_FULL_CHR_H_INT,
+                                         SWS_BILINEAR | SWS_PRINT_INFO | SWS_FULL_CHR_H_INT,
                                          NULL,
                                          NULL,
                                          NULL);
