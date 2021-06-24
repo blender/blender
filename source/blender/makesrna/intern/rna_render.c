15868b1ff4ad07ca5f7274b6c54f2f43a4ded955
Thomas Lachmann <TL>
2021-06-24 19:01:44 +0200

    Fix wrong default value for RenderEngine.bl_use_image_save
    Differential Revision: https://developer.blender.org/D11680

--- source/blender/makesrna/intern/rna_render.c
+++ source/blender/makesrna/intern/rna_render.c
@@ -858,6 +858,7 @@ static void rna_def_render_engine(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "bl_use_image_save", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_negative_sdna(prop, NULL, "type->flag", RE_USE_NO_IMAGE_SAVE);
+  RNA_def_property_boolean_default(prop, true);
   RNA_def_property_flag(prop, PROP_REGISTER_OPTIONAL);
   RNA_def_property_ui_text(
       prop,
