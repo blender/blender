d3788207aae6e7a3546850a5d6e71ed552030cc0
Johnny Matthews <guitargeek>
2021-06-29 22:39:08 -0500

    Geometry Nodes: Curve Primitive Quadratic Bezier Segment
    This patch is for a node that creates a poly spline from a
    3 point quadratic Bezier. Resolution is also specified.
    
    Curve primitives design task: T89220
    
    Differential Revision: https://developer.blender.org/D11649

--- release/scripts/startup/nodeitems_builtins.py
+++ release/scripts/startup/nodeitems_builtins.py
@@ -512,6 +512,7 @@ geometry_node_categories = [
     GeometryNodeCategory("GEO_PRIMITIVES_CURVE", "Curve Primitives", items=[
         NodeItem("GeometryNodeCurveStar"),
         NodeItem("GeometryNodeCurveSpiral"),
+        NodeItem("GeometryNodeCurveQuadraticBezier"),
     ]),
     GeometryNodeCategory("GEO_GEOMETRY", "Geometry", items=[
         NodeItem("GeometryNodeBoundBox"),
