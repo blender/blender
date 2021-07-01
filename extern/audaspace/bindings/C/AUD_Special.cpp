c634d859b216602d3d479966a5e48a433777ddce
Sebastian Parborg <darkdefende@gmail.com>
2021-07-01 18:51:26 +0200

    VSE: Use lines to draw waveform
    Refactor and improve waveform drawing.
    
    Drawing now can use line strips to draw waveforms instead of only
    triangle strips. This makes us able to properly visualize thin waveforms
    as they would not be visible before. We now also draw the RMS value of
    the waveform.
    
    The waveform drawing is now also properly aligned to the screen pixels
    to avoid flickering when transforming the strip.
    
    Reviewed By: Richard Antalik
    
    Differential Revision: https://developer.blender.org/D11184

--- extern/audaspace/bindings/C/AUD_Special.cpp
+++ extern/audaspace/bindings/C/AUD_Special.cpp
@@ -247,7 +247,7 @@ AUD_API int AUD_readSound(AUD_Sound* sound, float* buffer, int length, int sampl
 
 		buffer[i * 3] = min;
 		buffer[i * 3 + 1] = max;
-		buffer[i * 3 + 2] = sqrt(power) / len;
+		buffer[i * 3 + 2] = sqrt(power / len); // RMS
 
 		if(overallmax < max)
 			overallmax = max;
