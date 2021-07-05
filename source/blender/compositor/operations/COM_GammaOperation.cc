c94877ae3d38f2bd0a772caa09ad84647641b7f3
Manuel Castilla <manzanillawork@gmail.com>
2021-07-05 19:53:39 +0200

    Compositor: Full frame Gamma node
    Adds full frame implementation to this node operation.
    No functional changes.
    1.5x faster than tiled fallback.
    
    Reviewed By: jbakker
    
    Differential Revision: https://developer.blender.org/D11767

--- source/blender/compositor/operations/COM_GammaOperation.cc
+++ source/blender/compositor/operations/COM_GammaOperation.cc
@@ -51,6 +51,20 @@ void GammaOperation::executePixelSampled(float output[4], float x, float y, Pixe
   output[3] = inputValue[3];
 }
 
+void GammaOperation::update_memory_buffer_row(PixelCursor &p)
+{
+  for (; p.out < p.row_end; p.next()) {
+    const float *in_value = p.ins[0];
+    const float *in_gamma = p.ins[1];
+    const float gamma = in_gamma[0];
+    /* Check for negative to avoid nan's. */
+    p.out[0] = in_value[0] > 0.0f ? powf(in_value[0], gamma) : in_value[0];
+    p.out[1] = in_value[1] > 0.0f ? powf(in_value[1], gamma) : in_value[1];
+    p.out[2] = in_value[2] > 0.0f ? powf(in_value[2], gamma) : in_value[2];
+    p.out[3] = in_value[3];
+  }
+}
+
 void GammaOperation::deinitExecution()
 {
   this->m_inputProgram = nullptr;
