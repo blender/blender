ae8fa7062cb4ac9e48acb4c7633142e17c4c9c29
Sergey Sharybin <sergey@blender.org>
2021-07-06 12:36:42 +0200

    Fix incompatible type passed to XR haptic
    Likely caused by recent fixed-size types changes.
    Seems to be no-functional-changes since the function is unused.

--- source/blender/windowmanager/WM_api.h
+++ source/blender/windowmanager/WM_api.h
@@ -31,6 +31,7 @@
 
 /* dna-savable wmStructs here */
 #include "BLI_compiler_attrs.h"
+#include "BLI_sys_types.h"
 #include "DNA_windowmanager_types.h"
 #include "WM_keymap.h"
 
@@ -1015,7 +1016,7 @@ bool WM_xr_action_state_get(const wmXrData *xr,
 bool WM_xr_haptic_action_apply(wmXrData *xr,
                                const char *action_set_name,
                                const char *action_name,
-                               const long long *duration,
+                               const int64_t *duration,
                                const float *frequency,
                                const float *amplitude);
 void WM_xr_haptic_action_stop(wmXrData *xr, const char *action_set_name, const char *action_name);
