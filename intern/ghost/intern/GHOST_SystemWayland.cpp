ecbf838feefc9b064b71297b74613e7b094ee8d7
Christian Rauch <Rauch.Christian@gmx.de>
2021-07-07 13:21:57 +0100

    Cleanup: comment on source of button event codes

--- intern/ghost/intern/GHOST_SystemWayland.cpp
+++ intern/ghost/intern/GHOST_SystemWayland.cpp
@@ -52,6 +52,13 @@
 
 #include <cstring>
 
+#include <linux/input-event-codes.h>
+
+/* selected input event code defines from 'linux/input-event-codes.h'
+ * We include some of the button input event codes here, since the header is
+ * only available in more recent kernel versions. The event codes are used to
+ * to differentiate from which mouse button an event comes from.
+ */
 #define BTN_LEFT    0x110
 #define BTN_RIGHT   0x111
 #define BTN_MIDDLE  0x112
