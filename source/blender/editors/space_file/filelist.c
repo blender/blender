139c3f791ed4a3d2003bd27d833945583b47706a
Julian Eisel <julian@blender.org>
2021-07-07 19:49:28 +0200

    Cleanup: Remove wrong File Browser comment
    Accidentally included this in rB01e1944cd455, it came from a merge
    conflict.

--- source/blender/editors/space_file/filelist.c
+++ source/blender/editors/space_file/filelist.c
@@ -2090,9 +2090,6 @@ void filelist_uid_unset(FileUID *r_uid)
   *r_uid = FILE_UID_UNSET;
 }
 
-/**
- * \warning The UID will only be valid for the current session. Use as runtime data only!
- */
 void filelist_file_cache_slidingwindow_set(FileList *filelist, size_t window_size)
 {
   /* Always keep it power of 2, in [256, 8192] range for now,
