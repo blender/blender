f76dfe8fb45c9c270c295ff76f6fd058e2b64d0d
Hans Goudey <h.goudey@me.com>
2021-07-22 17:20:04 -0400

    Cleanup: Add function to check a curve's spline types
    The need for this has come up a few times.

--- source/blender/blenkernel/BKE_spline.hh
+++ source/blender/blenkernel/BKE_spline.hh
@@ -543,6 +543,7 @@ struct CurveEval {
 
   blender::Span<SplinePtr> splines() const;
   blender::MutableSpan<SplinePtr> splines();
+  bool has_spline_with_type(const Spline::Type type) const;
 
   void resize(const int size);
   void add_spline(SplinePtr spline);
