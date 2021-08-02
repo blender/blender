3fd5c93f9ce805b1a59bb6a03a9d39767697e336
Jacques Lucke <jacques@blender.org>
2021-08-02 10:38:11 +0200

    Geometry Nodes: increase threshold to make regression tests pass
    The real fix here is to use some kind of relative error in `customdata_compare`
    instead of the absolute error used now. If vertex coordinates get larger in magnitude,
    the allowed error should increase as well.

--- tests/python/geo_node_test.py
+++ tests/python/geo_node_test.py
@@ -25,7 +25,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from modules.mesh_test import BlendFileTest
 
-geo_node_test = BlendFileTest("test_object", "expected_object")
+geo_node_test = BlendFileTest("test_object", "expected_object", threshold=1e-4)
 result = geo_node_test.run_test()
 
 # Telling `ctest` about the failed test by raising Exception.
