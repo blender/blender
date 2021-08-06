ec00d218c9fbb018cfd02e97be150e4e4c75fa28
Germano Cavalcante <germano.costa@ig.com.br>
2021-08-06 11:44:15 -0300

    Fix T90477: Cursor vertex snapping not working in UV editor
    `t->obedit_type` is read to indicate if we are in uv edit mode.
    
    Also fixes a crash introduced in {rBdd14ea18190ff27082009f73a556569a43377a71}.

--- source/blender/editors/transform/transform.c
+++ source/blender/editors/transform/transform.c
@@ -78,7 +78,10 @@ static void initSnapSpatial(TransInfo *t, float r_snap[3]);
 
 bool transdata_check_local_islands(TransInfo *t, short around)
 {
-  return ((around == V3D_AROUND_LOCAL_ORIGINS) && ((ELEM(t->obedit_type, OB_MESH, OB_GPENCIL))));
+  if (t->options & (CTX_CURSOR | CTX_TEXTURE_SPACE)) {
+    return false;
+  }
+  return ((around == V3D_AROUND_LOCAL_ORIGINS) && (ELEM(t->obedit_type, OB_MESH, OB_GPENCIL)));
 }
 
 /* ************************** SPACE DEPENDENT CODE **************************** */
