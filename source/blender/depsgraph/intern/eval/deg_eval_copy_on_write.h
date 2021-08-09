3886ab05b449979959dfbb4950fea9ec473ecb83
Sergey Sharybin <sergey@blender.org>
2021-08-09 14:31:56 +0200

    Cleanup: Spelling and typos in comment

--- source/blender/depsgraph/intern/eval/deg_eval_copy_on_write.h
+++ source/blender/depsgraph/intern/eval/deg_eval_copy_on_write.h
@@ -68,11 +68,11 @@ ID *deg_expand_copy_on_write_datablock(const struct Depsgraph *depsgraph,
 ID *deg_update_copy_on_write_datablock(const struct Depsgraph *depsgraph, const IDNode *id_node);
 ID *deg_update_copy_on_write_datablock(const struct Depsgraph *depsgraph, struct ID *id_orig);
 
-/* Helper function which frees memory used by copy-on-written databnlock. */
+/* Helper function which frees memory used by copy-on-written data-block. */
 void deg_free_copy_on_write_datablock(struct ID *id_cow);
 
 /* Callback function for depsgraph operation node which ensures copy-on-write
- * datablock is ready for use by further evaluation routines.
+ * data-block is ready for use by further evaluation routines.
  */
 void deg_evaluate_copy_on_write(struct ::Depsgraph *depsgraph, const struct IDNode *id_node);
 
@@ -80,16 +80,16 @@ void deg_evaluate_copy_on_write(struct ::Depsgraph *depsgraph, const struct IDNo
  * copies inside. */
 bool deg_validate_copy_on_write_datablock(ID *id_cow);
 
-/* Tag given ID block as being copy-on-wtritten. */
+/* Tag given ID block as being copy-on-written. */
 void deg_tag_copy_on_write_id(struct ID *id_cow, const struct ID *id_orig);
 
-/* Check whether ID datablock is expanded.
+/* Check whether ID data-block is expanded.
  *
  * TODO(sergey): Make it an inline function or a macro.
  */
 bool deg_copy_on_write_is_expanded(const struct ID *id_cow);
 
-/* Check whether copy-on-write datablock is needed for given ID.
+/* Check whether copy-on-write data-block is needed for given ID.
  *
  * There are some exceptions on data-blocks which are covered by dependency graph
  * but which we don't want to start duplicating.
