42032db1c220e2a10d2d80879ec0037b4e12257e
Campbell Barton <ideasman42@gmail.com>
2021-08-26 12:27:16 +1000

    Cleanup: remove deprecated flag use in collada

--- source/blender/io/collada/BCAnimationCurve.cpp
+++ source/blender/io/collada/BCAnimationCurve.cpp
@@ -95,7 +95,7 @@ void BCAnimationCurve::delete_fcurve(FCurve *fcu)
 FCurve *BCAnimationCurve::create_fcurve(int array_index, const char *rna_path)
 {
   FCurve *fcu = BKE_fcurve_create();
-  fcu->flag = (FCURVE_VISIBLE | FCURVE_AUTO_HANDLES | FCURVE_SELECTED);
+  fcu->flag = (FCURVE_VISIBLE | FCURVE_SELECTED);
   fcu->rna_path = BLI_strdupn(rna_path, strlen(rna_path));
   fcu->array_index = array_index;
   return fcu;
