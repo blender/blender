fc460351170478e712740ae1917a2e24803eba3b
Sybren A. Stüvel <sybren@blender.org>
2021-09-09 14:27:00 +0200

    Depsgraph: release GIL when evaluating the depsgraph
    Evaluating the dependency graph potentially executes Python code when
    evaluating drivers. In specific situations (see T91046) this could deadlock
    Blender entirely. Temporarily releasing the GIL when evaluating the depsgraph
    resolves this.
    
    Calling the `BPy_BEGIN_ALLOW_THREADS` macro is relatively safe, as it's
    a no-op when the current thread does not have the GIL.
    
    Developed in collaboration with @sergey
    
    Manifest task: T91046

--- source/blender/depsgraph/intern/eval/deg_eval.cc
+++ source/blender/depsgraph/intern/eval/deg_eval.cc
@@ -41,6 +41,10 @@
 #include "DEG_depsgraph.h"
 #include "DEG_depsgraph_query.h"
 
+#ifdef WITH_PYTHON
+#  include "BPY_extern.h"
+#endif
+
 #include "atomic_ops.h"
 
 #include "intern/depsgraph.h"
@@ -375,6 +379,11 @@ void deg_evaluate_on_refresh(Depsgraph *graph)
 
   graph->debug.begin_graph_evaluation();
 
+#ifdef WITH_PYTHON
+  /* Release the GIL so that Python drivers can be evaluated. See T91046. */
+  BPy_BEGIN_ALLOW_THREADS;
+#endif
+
   graph->is_evaluating = true;
   depsgraph_ensure_view_layer(graph);
   /* Set up evaluation state. */
@@ -414,6 +423,10 @@ void deg_evaluate_on_refresh(Depsgraph *graph)
   /* Clear any uncleared tags - just in case. */
   deg_graph_clear_tags(graph);
   graph->is_evaluating = false;
+
+#ifdef WITH_PYTHON
+  BPy_END_ALLOW_THREADS;
+#endif
 
   graph->debug.end_graph_evaluation();
 }
