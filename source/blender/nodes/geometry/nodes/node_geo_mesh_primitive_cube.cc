cb833138633e1c402c87115e504c492a12451810
Hans Goudey <h.goudey@me.com>
2021-09-10 22:48:49 -0500

    Nodes: Add vector min/max support to new socket builder API
    Also use it to fix an incorrect min and max in the cube mesh
    primitive node.

--- source/blender/nodes/geometry/nodes/node_geo_mesh_primitive_cube.cc
+++ source/blender/nodes/geometry/nodes/node_geo_mesh_primitive_cube.cc
@@ -26,7 +26,7 @@ namespace blender::nodes {
 
 static void geo_node_mesh_primitive_cube_declare(NodeDeclarationBuilder &b)
 {
-  b.add_input<decl::Vector>("Size").default_value({1.0f, 1.0f, 1.0f}).subtype(PROP_TRANSLATION);
+  b.add_input<decl::Vector>("Size").default_value(float3(1)).min(0.0f).subtype(PROP_TRANSLATION);
   b.add_input<decl::Int>("Vertices X").default_value(2).min(2).max(1000);
   b.add_input<decl::Int>("Vertices Y").default_value(2).min(2).max(1000);
   b.add_input<decl::Int>("Vertices Z").default_value(2).min(2).max(1000);
