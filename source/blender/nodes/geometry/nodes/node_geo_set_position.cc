525def99bc907d4768b00632a3e9f8e1eb47669c
Hans Goudey <h.goudey@me.com>
2021-09-15 14:16:16 -0500

    Geometry Nodes: Hide values for selection inputs
    Toggling the selection off in the node is the same as muting it,
    so exposing it there doesn't help, and makes it less clear that it's
    meant to be used as a field.

--- source/blender/nodes/geometry/nodes/node_geo_set_position.cc
+++ source/blender/nodes/geometry/nodes/node_geo_set_position.cc
@@ -24,7 +24,7 @@ static void geo_node_set_position_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Geometry>("Geometry");
   b.add_input<decl::Vector>("Position");
-  b.add_input<decl::Bool>("Selection").default_value(true);
+  b.add_input<decl::Bool>("Selection").default_value(true).hide_value();
   b.add_output<decl::Geometry>("Geometry");
 }
 
