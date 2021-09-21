3642e17428448e4e9760ca5a8900d02e0abe2df7
Hans Goudey <h.goudey@me.com>
2021-09-21 11:56:54 -0500

    Cleanup: Move curve to mesh node implementation to blenkernel
    I plan to use this for curve object data conversion to mesh in D12533,
    and possibly for the implicit curve to mesh conversion in the curve
    and text object modifier stack in the future.
    
    Differential Revision: https://developer.blender.org/D12585

---
+++ source/blender/blenkernel/BKE_curve_to_mesh.hh
@@ -0,0 +1,31 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#pragma once
+
+struct Mesh;
+struct CurveEval;
+
+/** \file
+ * \ingroup geo
+ */
+
+namespace blender::bke {
+
+Mesh *curve_to_mesh_sweep(const CurveEval &curve, const CurveEval &profile);
+Mesh *curve_to_wire_mesh(const CurveEval &curve);
+
+}  // namespace blender::bke
