aea2287af33c17c5f310ddb20f57d23d2cbe09ac
Brecht Van Lommel <brecht@blender.org>
2021-10-21 18:31:17 +0200

    Tests: updated Python bundled modules test
    Add the new zstandard module, as well as previously missing ones.
    
    Ref D12777, T88438

--- tests/python/bl_bundled_modules.py
+++ tests/python/bl_bundled_modules.py
@@ -21,9 +21,14 @@
 # Test that modules we ship with our Python installation are available
 
 import bz2
+import certifi
 import ctypes
+import cython
 import lzma
 import numpy
+import requests
 import sqlite3
 import ssl
+import urllib3
 import zlib
+import zstandard
