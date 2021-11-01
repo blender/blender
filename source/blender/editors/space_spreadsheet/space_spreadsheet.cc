adc540cf7ccfa665fda4c387c26760cf5d60ebba
Harley Acheson <harley.acheson@gmail.com>
2021-11-01 08:42:07 -0700

    Fix T92655: spreadsheet_duplicate Split Exception
    Check SpaceSpreadsheet's runtime is not null when trying to duplicate
    the data when doing an area split.
    
    See D13047 for further details.
    
    Differential Revision: https://developer.blender.org/D13047
    
    Reviewed by Jacques Lucke

--- source/blender/editors/space_spreadsheet/space_spreadsheet.cc
+++ source/blender/editors/space_spreadsheet/space_spreadsheet.cc
@@ -137,7 +137,12 @@ static SpaceLink *spreadsheet_duplicate(SpaceLink *sl)
 {
   const SpaceSpreadsheet *sspreadsheet_old = (SpaceSpreadsheet *)sl;
   SpaceSpreadsheet *sspreadsheet_new = (SpaceSpreadsheet *)MEM_dupallocN(sspreadsheet_old);
-  sspreadsheet_new->runtime = new SpaceSpreadsheet_Runtime(*sspreadsheet_old->runtime);
+  if (sspreadsheet_old->runtime) {
+    sspreadsheet_new->runtime = new SpaceSpreadsheet_Runtime(*sspreadsheet_old->runtime);
+  }
+  else {
+    sspreadsheet_new->runtime = new SpaceSpreadsheet_Runtime();
+  }
 
   BLI_listbase_clear(&sspreadsheet_new->row_filters);
   LISTBASE_FOREACH (const SpreadsheetRowFilter *, src_filter, &sspreadsheet_old->row_filters) {
