68e86c4816a0fb18a8bd393d938dc8f95c1d0807
Campbell Barton <ideasman42@gmail.com>
2021-11-05 15:58:01 +1100

    Cleanup: use (s) postfix for messages that may be plural
    Ref 01c824ac88a0ff95a26c26be09f7a8853e47e446

--- source/blender/editors/lattice/editlattice_select.c
+++ source/blender/editors/lattice/editlattice_select.c
@@ -540,10 +540,7 @@ static int lattice_select_ungrouped_exec(bContext *C, wmOperator *op)
   MEM_freeN(objects);
 
   if (!changed) {
-    BKE_report(op->reports,
-               RPT_ERROR,
-               objects_len > 1 ? "No weights/vertex groups on objects" :
-                                 "No weights/vertex groups on object");
+    BKE_report(op->reports, RPT_ERROR, "No weights/vertex groups on object(s)");
     return OPERATOR_CANCELLED;
   }
   return OPERATOR_FINISHED;
