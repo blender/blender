da14a482f281e8d330ef5210ef22335a43eb726c
Diptangshu Dey <Tha_Hobbist>
2021-11-16 10:45:23 +0100

    Fix T90866: Python operator templates are not accessible from menus
    Python Operator templates made accessible from respective menus
    (required to also use F3 search for quick access)
    Also fixed Modal Draw Operator id_name (had duplicate name from other template)
    
    Maniphest Tasks: T90866
    
    Differential Revision: https://developer.blender.org/D13182

--- doc/python_api/examples/bpy.types.Operator.6.py
+++ doc/python_api/examples/bpy.types.Operator.6.py
@@ -31,8 +31,13 @@ class SearchEnumOperator(bpy.types.Operator):
         context.window_manager.invoke_search_popup(self)
         return {'RUNNING_MODAL'}
 
+# Only needed if you want to add into a dynamic menu
+def menu_func(self, context):
+    self.layout.operator(SearchEnumOperator.bl_idname, text="Search Enum Operator")
 
+# Register and add to the object menu (required to also use F3 search "Search Enum Operator" for quick access)
 bpy.utils.register_class(SearchEnumOperator)
+bpy.types.VIEW3D_MT_object.append(menu_func)
 
 # test call
 bpy.ops.object.search_enum_operator('INVOKE_DEFAULT')
