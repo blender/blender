9bdf3fa5f01b01bf9461a3ba6707d231bbbc5e9e
Richard Antalik <richardantalik@gmail.com>
2021-11-17 02:44:47 +0100

    Fix T91724: Strip height is too limited
    This change was introduced in 997b5fe45dab, to not display pixelated
    thumbnails. However when VSE timeline height is made smaller, this
    limits strip height.
    
    Change limit, so one strip can occupy full height of VSE timeline

--- source/blender/blenloader/intern/versioning_300.c
+++ source/blender/blenloader/intern/versioning_300.c
@@ -2177,5 +2177,21 @@ void blo_do_versions_300(FileData *fd, Library *UNUSED(lib), Main *bmain)
       }
     }
     FOREACH_NODETREE_END;
+
+    LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
+      LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+        LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
+          if (sl->spacetype == SPACE_SEQ) {
+            ListBase *regionbase = (sl == area->spacedata.first) ? &area->regionbase :
+                                                                   &sl->regionbase;
+            LISTBASE_FOREACH (ARegion *, region, regionbase) {
+              if (region->regiontype == RGN_TYPE_WINDOW) {
+                region->v2d.min[1] = 1.0f;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
