51791004ea8b2589f7cf7bd68187b60951ad27ca
Dalai Felinto <dalai@blender.org>
2021-12-01 11:24:55 +0100

    Fix errors in user preferences after 2.8 hack removal
    How to reproduce it:
    * Open User Preferences.
    * Got to Add-ons tab.
    
    Issue introduced on d723e331f155
    
    Reported via chat by Pedro Alcaide (povmaniac).

--- release/scripts/startup/bl_ui/space_userpref.py
+++ release/scripts/startup/bl_ui/space_userpref.py
@@ -1966,13 +1966,7 @@ class USERPREF_PT_addons(AddOnPanel, Panel):
                 sub.active = is_enabled
                 sub.label(text="%s: %s" % (info["category"], info["name"]))
 
-                # WARNING: 2.8x exception, may be removed
-                # use disabled state for old add-ons, chances are they are broken.
-                if is_addon_27x:
-                    sub.label(text="Upgrade to 2.8x required")
-                    sub.label(icon='ERROR')
-                # Remove code above after 2.8x migration is complete.
-                elif info["warning"]:
+                if info["warning"]:
                     sub.label(icon='ERROR')
 
                 # icon showing support level.
