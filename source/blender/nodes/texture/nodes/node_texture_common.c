c5ee3ac7e0e35e98db4e111d5b192581cebb09bd
Aaron Carlisle <carlisle.b3d@gmail.com>
2022-01-09 01:03:39 -0500

    Cleanup: Remove no op socket templates

--- source/blender/nodes/texture/nodes/node_texture_common.c
+++ source/blender/nodes/texture/nodes/node_texture_common.c
@@ -170,7 +170,6 @@ void register_node_type_tex_group(void)
   BLI_assert(ntype.rna_ext.srna != NULL);
   RNA_struct_blender_type_set(ntype.rna_ext.srna, &ntype);
 
-  node_type_socket_templates(&ntype, NULL, NULL);
   node_type_size(&ntype, 140, 60, 400);
   ntype.labelfunc = node_group_label;
   node_type_group_update(&ntype, node_group_update);
