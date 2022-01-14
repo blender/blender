49f25ae23ce8570b0aa7df07a5ed53ecf835f933
Antonio Vazquez <blendergit@gmail.com>
2022-01-14 13:51:30 +0100

    Fix T94089: GPencil Drawing don't Update after paste in Dopesheet
    When paste new frames, the datablock need to be tagged to update the drawings.

--- source/blender/editors/space_action/action_edit.c
+++ source/blender/editors/space_action/action_edit.c
@@ -642,6 +642,10 @@ static int actkeys_paste_exec(bContext *C, wmOperator *op)
     }
   }
 
+  /* Grease Pencil needs extra update to refresh the added keyframes. */
+  if (ac.datatype == ANIMCONT_GPENCIL) {
+    WM_event_add_notifier(C, NC_GPENCIL | ND_DATA, NULL);
+  }
   /* set notifier that keyframes have changed */
   WM_event_add_notifier(C, NC_ANIMATION | ND_KEYFRAME | NA_EDITED, NULL);
 
