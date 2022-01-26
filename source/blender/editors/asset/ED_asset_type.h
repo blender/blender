37848d1c8edf18ac52095cec7c50dfd1dde65e7c
Jacques Lucke <jacques@blender.org>
2022-01-26 15:22:15 +0100

    Assets: enable node group assets
    This enables support for node group assets. Previously, node group
    assets only worked when the "extended asset browser" experimental
    features is enabled.
    
    Differential Revision: https://developer.blender.org/D13748

--- source/blender/editors/asset/ED_asset_type.h
+++ source/blender/editors/asset/ED_asset_type.h
@@ -30,7 +30,7 @@ struct ID;
 
 bool ED_asset_type_id_is_non_experimental(const struct ID *id);
 #define ED_ASSET_TYPE_IDS_NON_EXPERIMENTAL_FLAGS \
-  (FILTER_ID_MA | FILTER_ID_OB | FILTER_ID_AC | FILTER_ID_WO)
+  (FILTER_ID_MA | FILTER_ID_OB | FILTER_ID_AC | FILTER_ID_WO | FILTER_ID_NT)
 
 /**
  * Check if the asset type for \a id (which doesn't need to be an asset right now) can be an asset,
@@ -52,7 +52,8 @@ int64_t ED_asset_types_supported_as_filter_flags(void);
  * strings with this (not all UI code supports dynamic strings nicely).
  * Should start with a consonant, so usages can prefix it with "a" (not "an").
  */
-#define ED_ASSET_TYPE_IDS_NON_EXPERIMENTAL_UI_STRING "Material, Object, Pose Action, or World"
+#define ED_ASSET_TYPE_IDS_NON_EXPERIMENTAL_UI_STRING \
+  "Material, Object, Pose Action, Node Group or World"
 
 #ifdef __cplusplus
 }
