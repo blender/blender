3bcbbf8992b0f41f19bef466129ce5b88984ac2b
Howard Trickey <howard.trickey@gmail.com>
2022-02-03 09:30:55 -0500

    Split Python OBJ importer and exporter, enabling only the importer.
    This is from patch D13988. It removes the "- New" from the menu of the
    new obj exporter, changes the default addon to just io_import_obj,
    and does the right versioning thing.
    Also disables the python tests for the old python exporter.

--- release/scripts/startup/bl_ui/space_topbar.py
+++ release/scripts/startup/bl_ui/space_topbar.py
@@ -481,7 +481,7 @@ class TOPBAR_MT_file_export(Menu):
     bl_owner_use_filter = False
 
     def draw(self, _context):
-        self.layout.operator("wm.obj_export", text="Wavefront OBJ (.obj) - New")
+        self.layout.operator("wm.obj_export", text="Wavefront OBJ (.obj)")
         if bpy.app.build_options.collada:
             self.layout.operator("wm.collada_export",
                                  text="Collada (Default) (.dae)")
