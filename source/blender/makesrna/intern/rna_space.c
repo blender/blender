3cd686cae8e9a767fb6d6a4e4206e4ad24725bb8
Bastien Montagne <bastien@blender.org>
2022-02-07 10:59:24 +0100

    Fix UI messages (typos etc.).

--- source/blender/makesrna/intern/rna_space.c
+++ source/blender/makesrna/intern/rna_space.c
@@ -4308,7 +4308,7 @@ static void rna_def_space_view3d_overlay(BlenderRNA *brna)
   prop = RNA_def_property(srna, "bone_wire_alpha", PROP_FLOAT, PROP_FACTOR);
   RNA_def_property_float_sdna(prop, NULL, "overlay.bone_wire_alpha");
   RNA_def_property_ui_text(
-      prop, "Bone Wireframe Opacity", "Maximim opacity of bones in wireframe display mode");
+      prop, "Bone Wireframe Opacity", "Maximum opacity of bones in wireframe display mode");
   RNA_def_property_range(prop, 0.0f, FLT_MAX);
   RNA_def_property_ui_range(prop, 0.0f, 1.0f, 1, 2);
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
