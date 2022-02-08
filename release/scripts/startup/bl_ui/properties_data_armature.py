67c12ed765942ec27072fb0a2dfad2364d21d8c1
Bastien Montagne <bastien@blender.org>
2022-02-08 09:16:49 +0100

    Cleanup: remove useless statement.
    Layouts are active by default when created, no need to set this
    explicitely. Leftover from proxy removal.

--- release/scripts/startup/bl_ui/properties_data_armature.py
+++ release/scripts/startup/bl_ui/properties_data_armature.py
@@ -149,7 +149,6 @@ class DATA_PT_bone_groups(ArmatureButtonsPanel, Panel):
             col.operator("pose.group_move", icon='TRIA_DOWN', text="").direction = 'DOWN'
 
             split = layout.split()
-            split.active = True
 
             col = split.column()
             col.prop(group, "color_set")
