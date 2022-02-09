2c7f2a0ba42898e248920ecd3a03e49c0d9f99a8
Campbell Barton <campbell@blender.org>
2022-02-09 16:21:21 +1100

    Cleanup: simplify copyright headers for sequence & imbuf

--- source/blender/editors/space_sequencer/sequencer_scopes.c
+++ source/blender/editors/space_sequencer/sequencer_scopes.c
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Author: Peter Schlaile < peter [at] schlaile [dot] de >
+ * Copyright 2006-2008 Peter Schlaile < peter [at] schlaile [dot] de >
  */
 
 /** \file
--- source/blender/sequencer/intern/strip_relations.c
+++ source/blender/sequencer/intern/strip_relations.c
@@ -13,11 +13,9 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * The Original Code is Copyright (C) 2001-2002 by NaN Holding BV.
- * All rights reserved.
- *
- * - Blender Foundation, 2003-2009
- * - Peter Schlaile <peter [at] schlaile [dot] de> 2005/2006
+ * Copyright 2001-2002 NaN Holding BV. All rights reserved.
+ *           2003-2009 Blender Foundation.
+ *           2005-2006 Peter Schlaile <peter [at] schlaile [dot] de>
  */
 
 /** \file
