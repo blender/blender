fe55789b6f5921b80242e51f79397d17ee2f3470
Campbell Barton <campbell@blender.org>
2022-02-11 14:57:02 +1100

    File headers: manually convert files to use SPDX headers
    Also add BSD-2-Clause to SPDX license list.

--- intern/eigen/intern/linear_solver.h
+++ intern/eigen/intern/linear_solver.h
@@ -1,29 +1,10 @@
-/*
- *  Sparse linear solver.
- *  Copyright (C) 2004 Bruno Levy
- *  Copyright (C) 2005-2015 Blender Foundation
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *  If you modify this software, you should include a notice giving the
- *  name of the person performing the modification, the date of modification,
- *  and the reason for such modification.
- */
+/* SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright 2004 Bruno Levy.
+ *           2005-2015 Blender Foundation. */
 
 /** \file
  * \ingroup intern_eigen
+ * Sparse linear solver.
  */
 
 #pragma once
