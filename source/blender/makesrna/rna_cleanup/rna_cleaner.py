f7bbec290dc750cb146408c6e9098673f34c29c3
Campbell Barton <campbell@blender.org>
2022-02-11 14:30:11 +1100

    File headers: add license to Python scripts

--- source/blender/makesrna/rna_cleanup/rna_cleaner.py
+++ source/blender/makesrna/rna_cleanup/rna_cleaner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 """
 This script is used to help cleaning RNA api.
--- tests/python/view_layer/view_layer_common.py
+++ tests/python/view_layer/view_layer_common.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 import unittest
 
 __all__ = (
