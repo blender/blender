9802d50c6bbffd0559791e2930890bc1915a9818
Campbell Barton <campbell@blender.org>
2022-02-11 14:23:55 +1100

    File headers: use SPDX for Zlib license

--- source/blender/blenlib/intern/math_rotation.c
+++ source/blender/blenlib/intern/math_rotation.c
@@ -1840,30 +1840,13 @@ void sub_eul_euleul(float r_eul[3], float a[3], float b[3], const short order)
 
 /******************************* Dual Quaternions ****************************/
 
-/**
- * Conversion routines between (regular quaternion, translation) and
- * dual quaternion.
+/* Conversion routines between (regular quaternion, translation) and dual quaternion.
  *
  * Version 1.0.0, February 7th, 2007
  *
- * Copyright (C) 2006-2007 University of Dublin, Trinity College, All Rights
- * Reserved
+ * SPDX-License-Identifier: Zlib
+ * Copyright 2006-2007 University of Dublin, Trinity College, All Rights Reserved.
  *
- * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the author(s) be held liable for any damages
- * arising from the use of this software.
- *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
  * Changes for Blender:
  * - renaming, style changes and optimization's
  * - added support for scaling
