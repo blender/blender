302c74e596882cea13ac878cfe6b12d37d0cd9c6
Clément Foucault <foucault.clem@gmail.com>
2022-02-15 00:10:50 +0100

    GLCompute: Apply state before dispatch

--- source/blender/gpu/opengl/gl_compute.cc
+++ source/blender/gpu/opengl/gl_compute.cc
@@ -15,6 +15,9 @@ namespace blender::gpu {
 void GLCompute::dispatch(int group_x_len, int group_y_len, int group_z_len)
 {
   GL_CHECK_RESOURCES("Compute");
+
+  GLContext::get()->state_manager->apply_state();
+
   glDispatchCompute(group_x_len, group_y_len, group_z_len);
 }
 
