cbdd57864049f6f300494d56b623934a4134fa2b
Hans Goudey <h.goudey@me.com>
2022-02-15 13:57:17 -0600

    Fix: Build error in debug build
    Error in a9f023e226389461b1140

--- source/blender/blenlib/BLI_math_vec_types.hh
+++ source/blender/blenlib/BLI_math_vec_types.hh
@@ -349,7 +349,7 @@ template<typename T, int Size> struct vec_base : public vec_struct_base<T, Size>
 
   friend vec_base operator/(const vec_base &a, const vec_base &b)
   {
-    BLI_assert(!math::is_any_zero());
+    BLI_assert(!math::is_any_zero(b));
     BLI_VEC_OP_IMPL(ret, i, ret[i] = a[i] / b[i]);
   }
 
@@ -361,7 +361,7 @@ template<typename T, int Size> struct vec_base : public vec_struct_base<T, Size>
 
   friend vec_base operator/(T a, const vec_base &b)
   {
-    BLI_assert(!math::is_any_zero());
+    BLI_assert(!math::is_any_zero(b));
     BLI_VEC_OP_IMPL(ret, i, ret[i] = a / b[i]);
   }
 
@@ -373,7 +373,7 @@ template<typename T, int Size> struct vec_base : public vec_struct_base<T, Size>
 
   vec_base &operator/=(const vec_base &b)
   {
-    BLI_assert(!b != T(0));
+    BLI_assert(b != T(0));
     BLI_VEC_OP_IMPL_SELF(i, (*this)[i] /= b[i]);
   }
 
@@ -505,7 +505,7 @@ template<typename T, int Size> struct vec_base : public vec_struct_base<T, Size>
 
   BLI_INT_OP(T) friend vec_base operator%(const vec_base &a, const vec_base &b)
   {
-    BLI_assert(!math::is_any_zero());
+    BLI_assert(!math::is_any_zero(b));
     BLI_VEC_OP_IMPL(ret, i, ret[i] = a[i] % b[i]);
   }
 
