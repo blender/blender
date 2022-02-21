7f2beb79c656bcacc7d0492da13df8379cf44311
Jacques Lucke <jacques@blender.org>
2022-02-21 11:57:43 +0100

    Cleanup: move curves sculpt mode toggle operator to sculpt/paint module
    This is necessary, because the operator will have to use functions that
    are currently private within this module. E.g. `paint_cursor_start`.

--- source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
+++ source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
@@ -6,8 +6,10 @@
 #include "BKE_paint.h"
 
 #include "WM_api.h"
+#include "WM_toolsystem.h"
 
 #include "ED_curves_sculpt.h"
+#include "ED_object.h"
 
 #include "curves_sculpt_intern.h"
 #include "paint_intern.h"
@@ -84,7 +86,57 @@ static void SCULPT_CURVES_OT_brush_stroke(struct wmOperatorType *ot)
   paint_stroke_operator_properties(ot);
 }
 
+static bool curves_sculptmode_toggle_poll(bContext *C)
+{
+  Object *ob = CTX_data_active_object(C);
+  if (ob == nullptr) {
+    return false;
+  }
+  if (ob->type != OB_CURVES) {
+    return false;
+  }
+  return true;
+}
+
+static int curves_sculptmode_toggle_exec(bContext *C, wmOperator *op)
+{
+  Scene *scene = CTX_data_scene(C);
+  Object *ob = CTX_data_active_object(C);
+  const bool is_mode_set = ob->mode == OB_MODE_SCULPT_CURVES;
+
+  if (is_mode_set) {
+    if (!ED_object_mode_compat_set(C, ob, OB_MODE_SCULPT_CURVES, op->reports)) {
+      return OPERATOR_CANCELLED;
+    }
+  }
+
+  if (is_mode_set) {
+    ob->mode = OB_MODE_OBJECT;
+  }
+  else {
+    BKE_paint_ensure(scene->toolsettings, (Paint **)&scene->toolsettings->curves_sculpt);
+    ob->mode = OB_MODE_SCULPT_CURVES;
+  }
+
+  WM_toolsystem_update_from_context_view3d(C);
+  WM_event_add_notifier(C, NC_SCENE | ND_MODE, nullptr);
+  return OPERATOR_CANCELLED;
+}
+
+static void CURVES_OT_sculptmode_toggle(wmOperatorType *ot)
+{
+  ot->name = "Curve Sculpt Mode Toggle";
+  ot->idname = "CURVES_OT_sculptmode_toggle";
+  ot->description = "Enter/Exit sculpt mode for curves";
+
+  ot->exec = curves_sculptmode_toggle_exec;
+  ot->poll = curves_sculptmode_toggle_poll;
+
+  ot->flag = OPTYPE_UNDO | OPTYPE_REGISTER;
+}
+
 void ED_operatortypes_sculpt_curves()
 {
   WM_operatortype_append(SCULPT_CURVES_OT_brush_stroke);
+  WM_operatortype_append(CURVES_OT_sculptmode_toggle);
 }
