4c407f20a62c7d437a9b3f2498b658f968102573
Hans Goudey <h.goudey@me.com>
2022-02-28 16:20:55 -0500

    Cleanup: Move object_add.c to C++
    This patch was tested on the buildbot first, it builds on all platforms.

--- source/blender/editors/object/object_intern.h
+++ source/blender/editors/object/object_intern.h
@@ -96,7 +96,7 @@ void OBJECT_OT_select_more(struct wmOperatorType *ot);
 void OBJECT_OT_select_less(struct wmOperatorType *ot);
 void OBJECT_OT_select_same_collection(struct wmOperatorType *ot);
 
-/* object_add.c */
+/* object_add.cc */
 
 void OBJECT_OT_add(struct wmOperatorType *ot);
 void OBJECT_OT_add_named(struct wmOperatorType *ot);
