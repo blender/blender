4a93c4bf1d3931a41000afcca1d963fccac18d26
Hans Goudey <h.goudey@me.com>
2022-03-29 20:46:41 -0500

    Geometry Nodes: Remove soft min from extrude node scale
    Extusion with a negative offset is possible and expected.

--- source/blender/nodes/geometry/nodes/node_geo_extrude_mesh.cc
+++ source/blender/nodes/geometry/nodes/node_geo_extrude_mesh.cc
@@ -25,7 +25,7 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Geometry>("Mesh").supported_type(GEO_COMPONENT_TYPE_MESH);
   b.add_input<decl::Bool>(N_("Selection")).default_value(true).supports_field().hide_value();
   b.add_input<decl::Vector>(N_("Offset")).subtype(PROP_TRANSLATION).implicit_field().hide_value();
-  b.add_input<decl::Float>(N_("Offset Scale")).default_value(1.0f).min(0.0f).supports_field();
+  b.add_input<decl::Float>(N_("Offset Scale")).default_value(1.0f).supports_field();
   b.add_input<decl::Bool>(N_("Individual")).default_value(true);
   b.add_output<decl::Geometry>("Mesh");
   b.add_output<decl::Bool>(N_("Top")).field_source();
