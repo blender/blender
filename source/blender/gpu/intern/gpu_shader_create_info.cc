5aa81594e6d931de01cb67399373eddae2fc7622
Clément Foucault <foucault.clem@gmail.com>
2022-03-30 14:30:22 +0200

    GPU: ShaderBuilder: Remove do_static_compilation_ inheritance
    This is because the inheritance is not done before checking if the shader
    should be statically compiled. Also some inheritance scheme
    might have intermediate permutation that are not compilable.

--- source/blender/gpu/intern/gpu_shader_create_info.cc
+++ source/blender/gpu/intern/gpu_shader_create_info.cc
@@ -102,8 +102,6 @@ void ShaderCreateInfo::finalize()
       assert_no_overlap(compute_source_.is_empty(), "Compute source already existing");
       compute_source_ = info.compute_source_;
     }
-
-    do_static_compilation_ = do_static_compilation_ || info.do_static_compilation_;
   }
 
   if (auto_resource_location_) {
