e994b728124ffd0f96d2535e65e8756b381e1c4c
Campbell Barton <campbell@blender.org>
2022-04-04 17:25:19 +1000

    UI: move "use_select_nearest_on_first_click" into it's own panel
    Add a new panel called "Tweaks" so we can get feedback from users
    about minor tweaks to behavior as exposing these minor changes.
    
    Currently this only has a single item in it, however we may want to
    get feedback from users about small changes in the future so I'd
    prefer to have a place to list these kinds of options.
    
    Ref D14542

--- release/scripts/startup/bl_ui/space_userpref.py
+++ release/scripts/startup/bl_ui/space_userpref.py
@@ -2262,7 +2262,6 @@ class USERPREF_PT_experimental_new_features(ExperimentalPanel, Panel):
                 ({"property": "use_sculpt_vertex_colors"}, "T71947"),
                 ({"property": "use_sculpt_tools_tilt"}, "T82877"),
                 ({"property": "use_sculpt_texture_paint"}, "T96225"),
-                ({"property": "use_select_nearest_on_first_click"}, "T96752"),
                 ({"property": "use_extended_asset_browser"}, ("project/view/130/", "Project Page")),
                 ({"property": "use_override_templates"}, ("T73318", "Milestone 4")),
                 ({"property": "use_named_attribute_nodes"}, ("T91742")),
@@ -2280,6 +2279,17 @@ class USERPREF_PT_experimental_prototypes(ExperimentalPanel, Panel):
                 ({"property": "use_new_point_cloud_type"}, "T75717"),
                 ({"property": "use_full_frame_compositor"}, "T88150"),
                 ({"property": "enable_eevee_next"}, "T93220"),
+            ),
+        )
+
+
+class USERPREF_PT_experimental_tweaks(ExperimentalPanel, Panel):
+    bl_label = "Tweaks"
+
+    def draw(self, context):
+        self._draw_items(
+            context, (
+                ({"property": "use_select_nearest_on_first_click"}, "T96752"),
             ),
         )
 
@@ -2404,6 +2414,7 @@ classes = (
 
     USERPREF_PT_experimental_new_features,
     USERPREF_PT_experimental_prototypes,
+    USERPREF_PT_experimental_tweaks,
     USERPREF_PT_experimental_debugging,
 
     # Add dynamically generated editor theme panels last,
