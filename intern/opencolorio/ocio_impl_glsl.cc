a410cea291a118187efdca8b440c27df69dd7bd5
Sergey Sharybin <sergey@blender.org>
2022-04-08 11:27:51 +0200

    Fix prefix used for display OCIO shader
    Didn't notice this to have real harmful behavior, but is still best
    to do things properly.

--- intern/opencolorio/ocio_impl_glsl.cc
+++ intern/opencolorio/ocio_impl_glsl.cc
@@ -619,7 +619,7 @@ static OCIO_GPUDisplayShader &getGPUDisplayShader(
     GpuShaderDescRcPtr shaderdesc_to_display = GpuShaderDesc::CreateShaderDesc();
     shaderdesc_to_display->setLanguage(GPU_LANGUAGE_GLSL_1_3);
     shaderdesc_to_display->setFunctionName("OCIO_to_display");
-    shaderdesc_to_scene_linear->setResourcePrefix("to_display");
+    shaderdesc_to_display->setResourcePrefix("to_display");
     (*(ConstProcessorRcPtr *)processor_to_display)
         ->getDefaultGPUProcessor()
         ->extractGpuShaderInfo(shaderdesc_to_display);
