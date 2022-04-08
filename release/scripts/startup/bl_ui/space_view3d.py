3039b215ba2af5ed63446aba72244b6bc5a4fd18
Antonio Vazquez <blendergit@gmail.com>
2022-04-08 18:10:38 +0200

    GPencil: Fix unreported missing Sculpt popover menu
    The menu with the options was not visible because the tool checked must be the sculpt, not draw.
    
    This was broken in old version, but I cannot determine when or if never worked at expected.

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -108,7 +108,7 @@ class VIEW3D_HT_tool_header(Header):
         elif tool_mode == 'SCULPT_GPENCIL':
             if is_valid_context:
                 brush = context.tool_settings.gpencil_sculpt_paint.brush
-                tool = brush.gpencil_tool
+                tool = brush.gpencil_sculpt_tool
                 if tool in {'SMOOTH', 'RANDOMIZE'}:
                     layout.popover("VIEW3D_PT_tools_grease_pencil_sculpt_options")
                 layout.popover("VIEW3D_PT_tools_grease_pencil_sculpt_appearance")
