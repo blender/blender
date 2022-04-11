b2143da253edb7609bd7bb202e5b1d8fc5d99973
Kévin Dietrich <kevin.dietrich@mailoo.org>
2022-04-11 11:54:11 +0200

    BMesh Python API: add access to vertex crease layer
    This simply adds access to the vertex crease layer from Python
    code. Documentation was modified to remove "Edge" as it is
    shared between edges and vertices, similarly to bevel weights.
    
    Differential Revision: https://developer.blender.org/D14605

--- source/blender/python/bmesh/bmesh_py_types_customdata.c
+++ source/blender/python/bmesh/bmesh_py_types_customdata.c
@@ -90,7 +90,7 @@ PyDoc_STRVAR(
 PyDoc_STRVAR(bpy_bmlayeraccess_collection__bevel_weight_doc,
              "Bevel weight float in [0 - 1].\n\n:type: :class:`BMLayerCollection`");
 PyDoc_STRVAR(bpy_bmlayeraccess_collection__crease_doc,
-             "Edge crease for subdivision surface - float in [0 - 1].\n\n:type: "
+             "Crease for subdivision surface - float in [0 - 1].\n\n:type: "
              ":class:`BMLayerCollection`");
 PyDoc_STRVAR(
     bpy_bmlayeraccess_collection__uv_doc,
@@ -210,6 +210,11 @@ static PyGetSetDef bpy_bmlayeraccess_vert_getseters[] = {
      (setter)NULL,
      bpy_bmlayeraccess_collection__bevel_weight_doc,
      (void *)CD_BWEIGHT},
+    {"crease",
+     (getter)bpy_bmlayeraccess_collection_get,
+     (setter)NULL,
+     bpy_bmlayeraccess_collection__crease_doc,
+     (void *)CD_CREASE},
     {"skin",
      (getter)bpy_bmlayeraccess_collection_get,
      (setter)NULL,
