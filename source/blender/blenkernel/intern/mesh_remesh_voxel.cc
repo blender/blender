402845744f9de6f2ae08aaa80347323fe6ca5684
Sergey Sharybin <sergey@blender.org>
2022-04-13 11:51:38 +0200

    Cleanup: Strict compiler warning in remesher

--- source/blender/blenkernel/intern/mesh_remesh_voxel.cc
+++ source/blender/blenkernel/intern/mesh_remesh_voxel.cc
@@ -375,8 +375,8 @@ void BKE_remesh_reproject_vertex_paint(Mesh *target, const Mesh *source)
   MeshElemMap *target_lmap = nullptr;
   int *target_lmap_mem = nullptr;
 
-  while (layer = BKE_id_attribute_from_index(
-             const_cast<ID *>(&source->id), i++, ATTR_DOMAIN_MASK_COLOR, CD_MASK_COLOR_ALL)) {
+  while ((layer = BKE_id_attribute_from_index(
+              const_cast<ID *>(&source->id), i++, ATTR_DOMAIN_MASK_COLOR, CD_MASK_COLOR_ALL))) {
     AttributeDomain domain = BKE_id_attribute_domain(&source->id, layer);
 
     CustomData *target_cdata = domain == ATTR_DOMAIN_POINT ? &target->vdata : &target->ldata;
