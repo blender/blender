d2a01bb7cb5c61cc778057c5b976d19c8d5cd506
Brecht Van Lommel <brecht>
2022-04-25 15:44:59 +0200

    Build: add autopep8 to bundled Python packages
    For use in "make format" to automatically format Python code, see D14686.
    
    Differential Revision: https://developer.blender.org/D14716

--- build_files/build_environment/cmake/versions.cmake
+++ build_files/build_environment/cmake/versions.cmake
@@ -225,6 +225,7 @@ set(CYTHON_VERSION 0.29.26)
 # The version of the zstd library used to build the Python package should match ZSTD_VERSION defined below.
 # At this time of writing, 0.17.0 was already released, but built against zstd 1.5.1, while we use 1.5.0.
 set(ZSTANDARD_VERSION 0.16.0)
+set(AUTOPEP8_VERSION 1.6.0)
 
 set(NUMPY_VERSION 1.22.0)
 set(NUMPY_SHORT_VERSION 1.22)
