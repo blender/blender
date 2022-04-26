8016d8a4bd21b2c113f0e2c9252d658cd48a5bd8
Ray Molenkamp <github@lazydodo.com>
2022-04-26 11:02:16 -0600

    deps_builder/robinmap: remove file copy on windows
    - It's not needed, since it's a build time dependency only.
    - It was setup to copy to the wrong folder.

--- build_files/build_environment/cmake/robinmap.cmake
+++ build_files/build_environment/cmake/robinmap.cmake
@@ -11,12 +11,3 @@ ExternalProject_Add(external_robinmap
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${LIBDIR}/robinmap ${DEFAULT_CMAKE_FLAGS} ${ROBINMAP_EXTRA_ARGS}
   INSTALL_DIR ${LIBDIR}/robinmap
 )
-
-if(WIN32)
-  if(BUILD_MODE STREQUAL Release)
-    ExternalProject_Add_Step(external_robinmap after_install
-      COMMAND ${CMAKE_COMMAND} -E copy_directory ${LIBDIR}/zstd/include/ ${HARVEST_TARGET}/zstd/include/
-      DEPENDEES install
-    )
-  endif()
-endif()
