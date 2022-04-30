2fc6563a597ad8877fea5e580c87eb4e13e58961
Ramil Roosileht <Limarest>
2022-04-29 20:12:45 -0700

    D14808: Use saturation as multiplier for saturation filter
    A change proposed in T97697, using existing saturation as a multiplier for the filter operation
    {F13038602}
    
    Review By: Joseph Eagar, Julian Kaspar
    Differential Revision: https://developer.blender.org/D14808
    Ref D14808

--- source/blender/editors/sculpt_paint/sculpt_filter_color.c
+++ source/blender/editors/sculpt_paint/sculpt_filter_color.c
@@ -132,7 +132,7 @@ static void color_filter_task_cb(void *__restrict userdata,
         rgb_to_hsv_v(orig_color, hsv_color);
 
         if (hsv_color[1] > 0.001f) {
-          hsv_color[1] = clamp_f(hsv_color[1] + fade, 0.0f, 1.0f);
+          hsv_color[1] = clamp_f(hsv_color[1] + fade * hsv_color[1], 0.0f, 1.0f);
           hsv_to_rgb_v(hsv_color, final_color);
         }
         else {
