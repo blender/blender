073139e32943b863828474ca70bbdfc258520769
Jason Fielder <jason_apple>
2022-05-12 20:49:09 +0200

    Metal: MTLState module implementation.
    MTLState module implementation and supporting functionality in MTLContext for state tracking, texture binding and sampler state caching.
    
    Ref T96261
    
    Reviewed By: fclem
    
    Maniphest Tasks: T96261
    
    Differential Revision: https://developer.blender.org/D14827

---
+++ source/blender/gpu/GPU_common_types.h
@@ -0,0 +1,18 @@
+/** \file
+ * \ingroup gpu
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum eGPUFrontFace {
+  GPU_CLOCKWISE,
+  GPU_COUNTERCLOCKWISE,
+} eGPUFrontFace;
+
+#ifdef __cplusplus
+}
+#endif
