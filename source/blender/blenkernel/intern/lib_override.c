74420d95b32a2d91af52fcf6e2fa0c4d0a53469e
Bastien Montagne <bastien@blender.org>
2022-05-19 11:51:01 +0200

    Revert "Fix liboverride extreme resync times in case of libraries dependency loops."
    This reverts commit e42e4e8568edeb4e0b962e2059586c2a3d3b457d.
    
    Wrong commit, sorry for the noise.

--- source/blender/blenkernel/intern/lib_override.c
+++ source/blender/blenkernel/intern/lib_override.c
@@ -2335,7 +2335,6 @@ static int lib_override_sort_libraries_func(LibraryIDLinkCallbackData *cb_data)
   if (id != NULL && ID_IS_LINKED(id) && id->lib != id_owner->lib) {
     const int owner_library_indirect_level = ID_IS_LINKED(id_owner) ? id_owner->lib->temp_index :
                                                                       0;
-<<<<<<< Updated upstream
     if (owner_library_indirect_level > 10000) {
       CLOG_ERROR(
           &LOG,
@@ -2344,17 +2343,6 @@ static int lib_override_sort_libraries_func(LibraryIDLinkCallbackData *cb_data)
           id_owner->lib->filepath,
           id->lib->filepath);
       BLI_assert(0);
-=======
-    if (owner_library_indirect_level > 200) {
-      CLOG_ERROR(&LOG,
-                 "Levels of indirect usages of libraries is way too high, there are most likely "
-                 "dependency loops, skipping further building loops (involves at least '%s' from "
-                 "'%s' and '%s' from '%s')",
-                 id_owner->name,
-                 id_owner->lib->filepath,
-                 id->name,
-                 id->lib->filepath);
->>>>>>> Stashed changes
       return IDWALK_RET_NOP;
     }
 
