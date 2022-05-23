2655f47ca35472931fbbeb2769d06c47f236c6c6
Ray Molenkamp <github@lazydodo.com>
2022-05-23 13:09:51 -0600

    Revert "LineArt: Use CAS for add_triangles."
    This reverts commit 14a5a91e0e033d712134c112a4778b495bd73ba1.
    
    This caused build errors on x64+arm mac builds and some versions
    of MSVC, reverting for now till a better solution is found.

--- source/blender/gpencil_modifiers/intern/lineart/MOD_lineart.h
+++ source/blender/gpencil_modifiers/intern/lineart/MOD_lineart.h
@@ -236,9 +236,6 @@ typedef struct LineartRenderBuffer {
   ListBase line_buffer_pointers;
   ListBase triangle_buffer_pointers;
 
-  LineartElementLinkNode *isect_scheduled_up_to;
-  int isect_scheduled_up_to_index;
-
   /** This one's memory is not from main pool and is free()ed after culling stage. */
   ListBase triangle_adjacent_pointers;
 
@@ -432,19 +429,15 @@ typedef struct LineartBoundingArea {
   /** 1,2,3,4 quadrant */
   struct LineartBoundingArea *child;
 
-  SpinLock lock;
-
   ListBase lp;
   ListBase rp;
   ListBase up;
   ListBase bp;
 
-  /* Need uint32 for the atomic cas instruction. */
-  uint32_t triangle_count;
-  uint32_t max_triangle_count;
-  uint32_t line_count;
-  uint32_t max_line_count;
-  uint32_t user_count;
+  uint16_t triangle_count;
+  uint16_t max_triangle_count;
+  uint16_t line_count;
+  uint16_t max_line_count;
 
   /* Use array for speeding up multiple accesses. */
   struct LineartTriangle **linked_triangles;
