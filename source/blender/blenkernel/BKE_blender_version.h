a493956eaae00e7cf6b895d8c26e7ed3db8d6cc8
Thomas Dinges <blender@dingto.org>
2022-06-01 15:39:52 +0200

    Release schedule: Entering Bcon4 for Blender 3.2 (RC)

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -21,7 +21,7 @@ extern "C" {
 /* Blender patch version for bugfix releases. */
 #define BLENDER_VERSION_PATCH 0
 /** Blender release cycle stage: alpha/beta/rc/release. */
-#define BLENDER_VERSION_CYCLE beta
+#define BLENDER_VERSION_CYCLE rc
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
