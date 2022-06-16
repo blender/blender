78f59d7f02e2e8a458134fe65f4f453e656d7bce
Muhammad Zeeshanh <PlainCircle@outlook.com>
2022-06-16 06:04:21 +0500

    Update find_dependencies.cmd

--- build_files/windows/find_dependencies.cmd
+++ build_files/windows/find_dependencies.cmd
@@ -3,6 +3,12 @@ for %%X in (svn.exe) do (set SVN=%%~$PATH:X)
 for %%X in (cmake.exe) do (set CMAKE=%%~$PATH:X)
 for %%X in (ctest.exe) do (set CTEST=%%~$PATH:X)
 for %%X in (git.exe) do (set GIT=%%~$PATH:X)
+for %%X in (python.exe) do (set PYTHON=%%~$PATH:X)
+if EXIST %PYTHON% (
+	goto detect_python_done
+)
+REM Latest version is PYTHON setup for Windows update envernment variable PATH.
+REM So in most cases above lines detect python, if not older code still work.
 REM For python, default on 310 but if that does not exist also check
 REM the 311, 312 and finally 39 folders to see if those are there, it checks
 REM this far ahead to ensure good lib folder compatibility in the future
