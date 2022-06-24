585d81ba2b77371bf7ada99e7d4bab8bfd7be52f
Jeroen Bakker <jeroen@blender.org>
2022-06-24 14:09:15 +0200

    Workbench: Increase render tests fail threshold for hair.
    When running the render test cases on MacOS/Intel the hair render
    test fail. Most likely due to the dense geometry and the low
    resolution of the test image.
    
    This patch increases the fail threshold so these tests will pass.
    Note that I haven't been able to test whether this is also the case
    for Linux/Windows. If that is the case we should remove the platform
    specific test.

--- tests/python/workbench_render_tests.py
+++ tests/python/workbench_render_tests.py
@@ -3,10 +3,12 @@
 
 import argparse
 import os
+import platform
 import shlex
 import shutil
 import subprocess
 import sys
+from pathlib import Path
 
 
 def setup():
@@ -73,6 +75,11 @@ def main():
     report.set_pixelated(True)
     report.set_reference_dir("workbench_renders")
     report.set_compare_engine('eevee')
+
+    test_dir_name = Path(test_dir).name
+    if test_dir_name.startswith('hair') and platform.system() == "Darwin":
+        report.set_fail_threshold(0.050)
+
     ok = report.run(test_dir, blender, get_arguments, batch=True)
 
     sys.exit(not ok)
