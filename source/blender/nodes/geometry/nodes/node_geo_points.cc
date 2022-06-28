2d18dd9309556e896037f30e8f72a7831c6e44e8
Hans Goudey <h.goudey@me.com>
2022-06-28 16:03:04 -0500

    Fix: Use distance unit for points node radius input

--- source/blender/nodes/geometry/nodes/node_geo_points.cc
+++ source/blender/nodes/geometry/nodes/node_geo_points.cc
@@ -20,7 +20,7 @@ static void node_declare(NodeDeclarationBuilder &b)
       .description(N_("The positions of the new points"));
   b.add_input<decl::Float>(N_("Radius"))
       .supports_field()
-      .supports_field()
+      .subtype(PROP_DISTANCE)
       .default_value(float(0.1f))
       .description(N_("The radii of the new points"));
   b.add_output<decl::Geometry>(N_("Geometry"));
