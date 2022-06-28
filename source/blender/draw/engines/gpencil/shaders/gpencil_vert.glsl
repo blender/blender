270ed1c7164c2da47b27e4403bf44554a80ed71c
Germano Cavalcante <germano.costa@ig.com.br>
2022-06-28 13:13:48 -0300

    Fix T98882: Regression: Gradient colors in a Grease Pencil material change depending on the visibility of other objects
    The material ID was being wrongly passed in the shader.

--- source/blender/draw/engines/gpencil/shaders/gpencil_vert.glsl
+++ source/blender/draw/engines/gpencil/shaders/gpencil_vert.glsl
@@ -125,7 +125,7 @@ void main()
     gpencil_color_output(fill_col, fcol_decode, 1.0, gp_mat._fill_texture_mix);
 
     gp_interp.mat_flag = gp_flag & GP_FILL_FLAGS;
-    gp_interp.mat_flag |= uint(ma1.x) << GPENCIl_MATID_SHIFT;
+    gp_interp.mat_flag |= uint(ma1.x + gpMaterialOffset) << GPENCIl_MATID_SHIFT;
 
     gp_interp.uv = mat2(gp_mat.fill_uv_rot_scale.xy, gp_mat.fill_uv_rot_scale.zw) * uv1.xy +
                    gp_mat._fill_uv_offset;
