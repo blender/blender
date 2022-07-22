003dfae270781bfa642a2c5804fa190edd4dfca4
Xavier Hallade <xavier.hallade@intel.com>
2022-07-22 13:00:46 +0200

    Cycles: enable oneAPI in Linux release builds
    0f50ae131f54d51f778424d4c9655128cafbbefc didn't do it reliably
    since it was deactivated explicitly a bit above.

--- build_files/cmake/config/blender_release.cmake
+++ build_files/cmake/config/blender_release.cmake
@@ -78,11 +78,6 @@ if(UNIX AND NOT APPLE)
   set(WITH_PULSEAUDIO          ON  CACHE BOOL "" FORCE)
   set(WITH_X11_XINPUT          ON  CACHE BOOL "" FORCE)
   set(WITH_X11_XF86VMODE       ON  CACHE BOOL "" FORCE)
-
-  # Disable oneAPI on Linux for the time being.
-  # The AoT compilation takes too long to be used officially in the buildbot CI/CD and the JIT
-  # compilation has ABI compatibility issues when running builds made on centOS on Ubuntu.
-  set(WITH_CYCLES_DEVICE_ONEAPI OFF  CACHE BOOL "" FORCE)
 endif()
 if(NOT APPLE)
   set(WITH_XR_OPENXR              ON  CACHE BOOL "" FORCE)
@@ -93,6 +88,6 @@ if(NOT APPLE)
   set(WITH_CYCLES_HIP_BINARIES    ON  CACHE BOOL "" FORCE)
   set(WITH_CYCLES_DEVICE_ONEAPI   ON  CACHE BOOL "" FORCE)
 
-  # Disable AoT kernels compilations until buildbot can deliver them in a reasonabel time.
+  # Disable AoT kernels compilations until buildbot can deliver them in a reasonable time.
   set(WITH_CYCLES_ONEAPI_BINARIES OFF CACHE BOOL "" FORCE)
 endif()
