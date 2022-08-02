78ad9ebed3a55138c0a576c4200ab0fd6d8e3712
Jeroen Bakker <jeroen@blender.org>
2022-08-02 13:38:34 +0200

    Fix T99715: Only force optimal display during on cage editing.
    This loosens the current implementation a bit to only force optimal
    display when editing on cage. It used to be any editing mode.
    
    Brings GPU based subdivision closer to the CPU version.

--- source/blender/draw/intern/draw_cache_impl_subdivision.cc
+++ source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -2019,6 +2019,7 @@ static bool draw_subdiv_create_requested_buffers(Object *ob,
                                                  const float obmat[4][4],
                                                  const bool do_final,
                                                  const bool do_uvedit,
+                                                 const bool do_cage,
                                                  const ToolSettings *ts,
                                                  const bool use_hide,
                                                  OpenSubdiv_EvaluatorCache *evaluator_cache)
@@ -2062,9 +2063,8 @@ static bool draw_subdiv_create_requested_buffers(Object *ob,
     return false;
   }
 
-  /* Edges which do not come from coarse edges should not be drawn in edit mode, only in object
-   * mode when optimal display in turned off. */
-  const bool optimal_display = runtime_data->use_optimal_display || is_editmode;
+  /* Edges which do not come from coarse edges should not be drawn in edit cage mode. */
+  const bool optimal_display = runtime_data->use_optimal_display || (is_editmode && !do_cage);
 
   draw_cache->bm = bm;
   draw_cache->mesh = mesh_eval;
@@ -2216,6 +2216,7 @@ void DRW_create_subdivision(Object *ob,
                             const float obmat[4][4],
                             const bool do_final,
                             const bool do_uvedit,
+                            const bool do_cage,
                             const ToolSettings *ts,
                             const bool use_hide)
 {
@@ -2239,6 +2240,7 @@ void DRW_create_subdivision(Object *ob,
                                             obmat,
                                             do_final,
                                             do_uvedit,
+                                            do_cage,
                                             ts,
                                             use_hide,
                                             g_evaluator_cache)) {
