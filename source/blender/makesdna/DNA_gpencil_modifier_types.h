8dadc091d02a7a046a73f77757766ef6fdeffda9
YimingWu <xp8110@outlook.com>
2022-08-06 11:06:34 +0800

    LineArt: Usability improvement for "Enclosed Shapes".
    This patch removes the [rather confusing] separate checkbox for enclosed
    shapes in favour of integrating that option into illumination filtering,
    with the benefit of not limiting the selection to cached result.
    
    Reviewed By: Sebastian Parborg (zeddb)
    
    Differential Revision: https://developer.blender.org/D15327

--- source/blender/makesdna/DNA_gpencil_modifier_types.h
+++ source/blender/makesdna/DNA_gpencil_modifier_types.h
@@ -999,9 +999,14 @@ typedef enum eLineartGpencilModifierSource {
 } eLineartGpencilModifierSource;
 
 typedef enum eLineartGpencilModifierShadowFilter {
+  /* These options need to be ordered in this way because those latter options requires line art to
+     run a few extra stages. Having those values set up this way will allow
+     #BKE_gpencil_get_lineart_modifier_limits() to find out maximum stages needed in multiple
+     cached line art modifiers. */
   LRT_SHADOW_FILTER_NONE = 0,
-  LRT_SHADOW_FILTER_LIT = 1,
+  LRT_SHADOW_FILTER_ILLUMINATED = 1,
   LRT_SHADOW_FILTER_SHADED = 2,
+  LRT_SHADOW_FILTER_ILLUMINATED_ENCLOSED_SHAPES = 3,
 } eLineartGpencilModifierShadowFilter;
 
 typedef enum eLineartGpencilModifierSilhouetteFilter {
