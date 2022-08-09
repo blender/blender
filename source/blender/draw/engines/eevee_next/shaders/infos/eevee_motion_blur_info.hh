26b9c54b01a0aa8a1550b94fe790cc8e0e05577d
Campbell Barton <campbell@blender.org>
2022-08-09 13:25:11 +1000

    License headers: add missing license headers

--- source/blender/draw/engines/eevee_next/shaders/infos/eevee_motion_blur_info.hh
+++ source/blender/draw/engines/eevee_next/shaders/infos/eevee_motion_blur_info.hh
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "eevee_defines.hh"
 #include "gpu_shader_create_info.hh"
--- tests/python/gpu_info.py
+++ tests/python/gpu_info.py
@@ -1,7 +1,9 @@
-"""
-    Prints GPU backend information to the console and exits.
+# SPDX-License-Identifier: GPL-2.0-or-later
 
-    Use this script as `blender --background --python gpu_info.py`.
+"""
+Prints GPU back-end information to the console and exits.
+
+Use this script as `blender --background --python gpu_info.py`.
 """
 import bpy
 import gpu
