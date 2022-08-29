0db582c41c3b8aba65eabaa97093b386fa4d0ddf
Damien Picard <pioverfour>
2022-08-29 15:08:13 +0200

    Add missing RNA text for the MeshUVLoop struct
    The structures in rna_mesh.c all had a UI text which showed up in the
    animation editors, except for MeshUVLoop.
    
    Reviewed By: mont29
    
    Differential Revision: https://developer.blender.org/D15803

--- source/blender/makesrna/intern/rna_mesh.c
+++ source/blender/makesrna/intern/rna_mesh.c
@@ -2301,6 +2301,7 @@ static void rna_def_mloopuv(BlenderRNA *brna)
 
   srna = RNA_def_struct(brna, "MeshUVLoop", NULL);
   RNA_def_struct_sdna(srna, "MLoopUV");
+  RNA_def_struct_ui_text(srna, "Mesh UV Layer", "Layer of UV coordinates in a Mesh data-block");
   RNA_def_struct_path_func(srna, "rna_MeshUVLoop_path");
 
   prop = RNA_def_property(srna, "uv", PROP_FLOAT, PROP_XYZ);
