191872a836715e8f7ac4d7ecb10a2bd0555213f1
Antonio Vazquez <blendergit@gmail.com>
2022-09-05 17:08:23 +0200

    GPencil: Improve previous commit error checking
    Better check if the strokes has more points. 
    
    A 0 or 1 point stroke never need to set start point.

--- source/blender/blenkernel/intern/gpencil_geom.cc
+++ source/blender/blenkernel/intern/gpencil_geom.cc
@@ -3545,7 +3545,7 @@ void BKE_gpencil_stroke_join(bGPDstroke *gps_a,
 
 void BKE_gpencil_stroke_start_set(bGPdata *gpd, bGPDstroke *gps, int start_idx)
 {
-  if ((start_idx < 1) || (start_idx >= gps->totpoints) || (gps->totpoints == 0)) {
+  if ((start_idx < 1) || (start_idx >= gps->totpoints) || (gps->totpoints < 2)) {
     return;
   }
 
