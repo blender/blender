c36c403cdb7e8a9d9c0b7aa1765e42fc0e9818d0
Colin Basnett <cmbasnett@gmail.com>
2022-09-07 18:12:32 -0700

    Cleanup: Removed handling of unused flag in TimeMarkers
    This removes the defunct handling of the ACTIVE flag in
    TimeMarker::flags. It's not possible for that flag to be set though
    normal operation.
    
    Differential Revision: https://developer.blender.org/D15828

--- source/blender/editors/animation/anim_markers.c
+++ source/blender/editors/animation/anim_markers.c
@@ -459,9 +459,7 @@ static void draw_marker_line(const uchar *color, int xpos, int ymin, int ymax)
 static int marker_get_icon_id(TimeMarker *marker, int flag)
 {
   if (flag & DRAW_MARKERS_LOCAL) {
-    return (marker->flag & ACTIVE) ? ICON_PMARKER_ACT :
-           (marker->flag & SELECT) ? ICON_PMARKER_SEL :
-                                     ICON_PMARKER;
+    return (marker->flag & SELECT) ? ICON_PMARKER_SEL : ICON_PMARKER;
   }
 #ifdef DURIAN_CAMERA_SWITCH
   if (marker->camera) {
