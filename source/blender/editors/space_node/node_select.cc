400151833db01928a62a13b12f047cd0f90c6ed2
Hans Goudey <h.goudey@me.com>
2022-09-12 18:21:04 -0500

    Nodes: Avoid unnecessary sorting when selecting or moving nodes
    Previously the nodes were sorted wven when there was no change.
    This is a fixed version of e3ef6a6660032ca18, which was reverted by
    dc937c5aee4532.

--- source/blender/editors/space_node/node_select.cc
+++ source/blender/editors/space_node/node_select.cc
@@ -644,28 +644,29 @@ static bool node_mouse_select(bContext *C,
     }
   }
 
-  /* update node order */
-  if (changed || found) {
-    bool active_texture_changed = false;
-    bool viewer_node_changed = false;
-    if ((node != nullptr) && (node_was_selected == false || params->select_passthrough == false)) {
-      viewer_node_changed = (node->flag & NODE_DO_OUTPUT) == 0 && node->type == GEO_NODE_VIEWER;
-      ED_node_set_active(&bmain, &snode, snode.edittree, node, &active_texture_changed);
-    }
-    else if (node != nullptr && node->type == GEO_NODE_VIEWER) {
-      ED_spreadsheet_context_paths_set_geometry_node(&bmain, &snode, node);
-    }
-    ED_node_set_active_viewer_key(&snode);
-    node_sort(*snode.edittree);
-    if ((active_texture_changed && has_workbench_in_texture_color(wm, scene, ob)) ||
-        viewer_node_changed) {
-      DEG_id_tag_update(&snode.edittree->id, ID_RECALC_COPY_ON_WRITE);
-    }
-
-    WM_event_add_notifier(C, NC_NODE | NA_SELECTED, nullptr);
+  if (!(changed || found)) {
+    return false;
   }
 
-  return changed || found;
+  bool active_texture_changed = false;
+  bool viewer_node_changed = false;
+  if ((node != nullptr) && (node_was_selected == false || params->select_passthrough == false)) {
+    viewer_node_changed = (node->flag & NODE_DO_OUTPUT) == 0 && node->type == GEO_NODE_VIEWER;
+    ED_node_set_active(&bmain, &snode, snode.edittree, node, &active_texture_changed);
+  }
+  else if (node != nullptr && node->type == GEO_NODE_VIEWER) {
+    ED_spreadsheet_context_paths_set_geometry_node(&bmain, &snode, node);
+  }
+  ED_node_set_active_viewer_key(&snode);
+  node_sort(*snode.edittree);
+  if ((active_texture_changed && has_workbench_in_texture_color(wm, scene, ob)) ||
+      viewer_node_changed) {
+    DEG_id_tag_update(&snode.edittree->id, ID_RECALC_COPY_ON_WRITE);
+  }
+
+  WM_event_add_notifier(C, NC_NODE | NA_SELECTED, nullptr);
+
+  return true;
 }
 
 static int node_select_exec(bContext *C, wmOperator *op)
