blender/blender

commit 1514e1a5b7e15ec0c11cd40c2b9389982bd5d00e

Metal: First set of Geometry Shader alternative implementations using SSBO vertex shader fetch.
These implementations remove dependency on the Geometry pass by instead invoking one vertex shader instance for each expected output vertex, matching what a geometry shader would emit. Each vertex shader instance is then responsible for calculating the same output position based on its vertex_id as the logic would in the geometry shader version.

SSBO Vertex fetch enables full random-access into a vertex buffer by binding it as a read-only SSBO. This enables each instance to read neighbouring vertex data to perform contextual calculations as a geometry shader would, for cases where attribute Multiload is not supported.

Authored by Apple: Michael Parkin-White

Ref T96261

Reviewed By: fclem

Differential Revision: https://developer.blender.org/D15901

source/blender/draw/engines/overlay/shaders/infos/overlay_armature_info.hh
@@ -69,6 +69,11 @@ GPU_SHADER_INTERFACE_INFO(overlay_armature_shape_outline_iface, "geom_in")
     .smooth(Type::VEC4, "vColSize")
     .flat(Type::INT, "inverted");
 
+GPU_SHADER_INTERFACE_INFO(overlay_armature_shape_outline_no_geom_iface, "")
+    .flat(Type::VEC4, "finalColor")
+    .flat(Type::VEC2, "edgeStart")
+    .no_perspective(Type::VEC2, "edgePos");
+
 GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline)
     .do_static_compilation(true)
     .vertex_in(0, Type::VEC3, "pos")
@@ -84,9 +89,25 @@ GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline)
     .fragment_source("overlay_armature_wire_frag.glsl")
     .additional_info("overlay_frag_output", "overlay_armature_common", "draw_globals");
 
+GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline_no_geom)
+    .do_static_compilation(true)
+    .vertex_in(0, Type::VEC3, "pos")
+    .vertex_in(1, Type::VEC3, "snor")
+    /* Per instance. */
+    .vertex_in(2, Type::VEC4, "color")
+    .vertex_in(3, Type::MAT4, "inst_obmat")
+    .vertex_out(overlay_armature_shape_outline_no_geom_iface)
+    .vertex_source("overlay_armature_shape_outline_vert_no_geom.glsl")
+    .fragment_source("overlay_armature_wire_frag.glsl")
+    .additional_info("overlay_frag_output", "overlay_armature_common", "draw_globals");
+
 GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline_clipped)
     .do_static_compilation(true)
     .additional_info("overlay_armature_shape_outline", "drw_clipped");
+
+GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline_clipped_no_geom)
+    .do_static_compilation(true)
+    .additional_info("overlay_armature_shape_outline_no_geom", "drw_clipped");
 
 GPU_SHADER_INTERFACE_INFO(overlay_armature_shape_solid_iface, "")
     .smooth(Type::VEC4, "finalColor")

source/blender/draw/engines/overlay/shaders/infos/overlay_edit_mode_info.hh
@@ -22,6 +22,17 @@ GPU_SHADER_CREATE_INFO(overlay_edit_mesh_common)
     .vertex_source("overlay_edit_mesh_vert.glsl")
     .additional_info("draw_modelmat", "draw_globals");
 
+GPU_SHADER_CREATE_INFO(overlay_edit_mesh_common_no_geom)
+    .define("blender_srgb_to_framebuffer_space(a)", "a")
+    .sampler(0, ImageType::DEPTH_2D, "depthTex")
+    .fragment_out(0, Type::VEC4, "fragColor")
+    .push_constant(Type::BOOL, "selectFaces")
+    .push_constant(Type::BOOL, "selectEdges")
+    .push_constant(Type::FLOAT, "alpha")
+    .push_constant(Type::IVEC4, "dataMask")
+    .vertex_source("overlay_edit_mesh_vert_no_geom.glsl")
+    .additional_info("draw_modelmat", "draw_globals");
+
 GPU_SHADER_INTERFACE_INFO(overlay_edit_mesh_vert_iface, "")
     .smooth(Type::VEC4, "finalColor")
     .smooth(Type::FLOAT, "vertexCrease");
@@ -61,10 +72,27 @@ GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge)
     .fragment_source("overlay_edit_mesh_frag.glsl")
     .additional_info("overlay_edit_mesh_common");
 
+/* The Non-Geometry shader variant passes directly to fragment. */
+GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_no_geom)
+    .do_static_compilation(true)
+    .define("EDGE")
+    .vertex_in(0, Type::VEC3, "pos")
+    .vertex_in(1, Type::UCHAR4, "data")
+    .vertex_in(2, Type::VEC3_101010I2, "vnor")
+    .push_constant(Type::BOOL, "do_smooth_wire")
+    .vertex_out(overlay_edit_mesh_edge_geom_iface)
+    .fragment_source("overlay_edit_mesh_frag.glsl")
+    .additional_info("overlay_edit_mesh_common_no_geom");
+
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat)
     .do_static_compilation(true)
     .define("FLAT")
     .additional_info("overlay_edit_mesh_edge");
+
+GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat_no_geom)
+    .do_static_compilation(true)
+    .define("FLAT")
+    .additional_info("overlay_edit_mesh_edge_no_geom");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_face)
     .do_static_compilation(true)
@@ -136,9 +164,17 @@ GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_clipped)
     .do_static_compilation(true)
     .additional_info("overlay_edit_mesh_edge", "drw_clipped");
 
+GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_clipped_no_geom)
+    .do_static_compilation(true)
+    .additional_info("overlay_edit_mesh_edge_no_geom", "drw_clipped");
+
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat_clipped)
     .do_static_compilation(true)
     .additional_info("overlay_edit_mesh_edge_flat", "drw_clipped");
+
+GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat_clipped_no_geom)
+    .do_static_compilation(true)
+    .additional_info("overlay_edit_mesh_edge_flat_no_geom", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_face_clipped)
     .do_static_compilation(true)
@@ -326,9 +362,28 @@ GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle)
     .fragment_source("overlay_varying_color.glsl")
     .additional_info("draw_mesh", "draw_globals");
 
+GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle_no_geom)
+    .do_static_compilation(true)
+    .typedef_source("overlay_shader_shared.h")
+    /* NOTE: Color already in Linear space. Which is what we want. */
+    .define("srgbTarget", "false")
+    .vertex_in(0, Type::VEC3, "pos")
+    .vertex_in(1, Type::INT, "data")
+    .vertex_out(overlay_edit_curve_handle_iface)
+    .push_constant(Type::BOOL, "showCurveHandles")
+    .push_constant(Type::INT, "curveHandleDisplay")
+    .fragment_out(0, Type::VEC4, "fragColor")
+    .vertex_source("overlay_edit_curve_handle_vert_no_geom.glsl")
+    .fragment_source("overlay_varying_color.glsl")
+    .additional_info("draw_mesh", "draw_globals");
+
 GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle_clipped)
     .do_static_compilation(true)
     .additional_info("overlay_edit_curve_handle", "drw_clipped");
+
+GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle_clipped_no_geom)
+    .do_static_compilation(true)
+    .additional_info("overlay_edit_curve_handle_no_geom", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_curve_point)
     .do_static_compilation(true)

source/blender/draw/engines/overlay/shaders/infos/overlay_extra_info.hh
@@ -183,6 +183,9 @@ GPU_SHADER_INTERFACE_INFO(overlay_motion_path_line_iface, "interp")
     .flat(Type::VEC2, "ss_pos")
     .smooth(Type::VEC4, "color");
 
+GPU_SHADER_INTERFACE_INFO(overlay_motion_path_line_no_geom_iface, "interp")
+    .smooth(Type::VEC4, "color");
+
 GPU_SHADER_CREATE_INFO(overlay_motion_path_line)
     .do_static_compilation(true)
     .vertex_in(0, Type::VEC3, "pos")
@@ -199,9 +202,26 @@ GPU_SHADER_CREATE_INFO(overlay_motion_path_line)
     .fragment_source("overlay_motion_path_line_frag.glsl")
     .additional_info("draw_view", "draw_globals");
 
+GPU_SHADER_CREATE_INFO(overlay_motion_path_line_no_geom)
+    .do_static_compilation(true)
+    .vertex_in(0, Type::VEC3, "pos")
+    .push_constant(Type::IVEC4, "mpathLineSettings")
+    .push_constant(Type::BOOL, "selected")
+    .push_constant(Type::VEC3, "customColor")
+    .push_constant(Type::INT, "lineThickness") /* In pixels. */
+    .vertex_out(overlay_motion_path_line_no_geom_iface)
+    .fragment_out(0, Type::VEC4, "fragColor")
+    .vertex_source("overlay_motion_path_line_vert_no_geom.glsl")
+    .fragment_source("overlay_motion_path_line_frag.glsl")
+    .additional_info("draw_view", "draw_globals");
+
 GPU_SHADER_CREATE_INFO(overlay_motion_path_line_clipped)
     .do_static_compilation(true)
     .additional_info("overlay_motion_path_line", "drw_clipped");
+
+GPU_SHADER_CREATE_INFO(overlay_motion_path_line_clipped_no_geom)
+    .do_static_compilation(true)
+    .additional_info("overlay_motion_path_line_no_geom", "drw_clipped");
 
 GPU_SHADER_INTERFACE_INFO(overlay_motion_path_point_iface, "").flat(Type::VEC4, "finalColor");
 

source/blender/gpu/shaders/infos/gpu_shader_3D_polyline_info.hh
@@ -29,11 +29,30 @@ GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline)
     .fragment_source("gpu_shader_3D_polyline_frag.glsl")
     .additional_info("gpu_srgb_to_framebuffer_space");
 
+GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_no_geom)
+    .define("SMOOTH_WIDTH", "1.0")
+    .push_constant(Type::MAT4, "ModelViewProjectionMatrix")
+    .push_constant(Type::VEC2, "viewportSize")
+    .push_constant(Type::FLOAT, "lineWidth")
+    .push_constant(Type::BOOL, "lineSmooth")
+    .vertex_in(0, Type::VEC3, "pos")
+    .vertex_out(gpu_shader_3D_polyline_iface)
+    .fragment_out(0, Type::VEC4, "fragColor")
+    .vertex_source("gpu_shader_3D_polyline_vert_no_geom.glsl")
+    .fragment_source("gpu_shader_3D_polyline_frag.glsl")
+    .additional_info("gpu_srgb_to_framebuffer_space");
+
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color)
     .do_static_compilation(true)
     .define("UNIFORM")
     .push_constant(Type::VEC4, "color")
     .additional_info("gpu_shader_3D_polyline");
+
+GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color_no_geom)
+    .do_static_compilation(true)
+    .define("UNIFORM")
+    .push_constant(Type::VEC4, "color")
+    .additional_info("gpu_shader_3D_polyline_no_geom");
 
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color_clipped)
     .do_static_compilation(true)
@@ -43,14 +62,34 @@ GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color_clipped)
     .define("CLIP")
     .additional_info("gpu_shader_3D_polyline_uniform_color");
 
+GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color_clipped_no_geom)
+    .do_static_compilation(true)
+    /* TODO(fclem): Put in an UBO to fit the 128byte requirement. */
+    .push_constant(Type::MAT4, "ModelMatrix")
+    .push_constant(Type::VEC4, "ClipPlane")
+    .define("CLIP")
+    .additional_info("gpu_shader_3D_polyline_uniform_color_no_geom");
+
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_flat_color)
     .do_static_compilation(true)
     .define("FLAT")
     .vertex_in(1, Type::VEC4, "color")
     .additional_info("gpu_shader_3D_polyline");
 
+GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_flat_color_no_geom)
+    .do_static_compilation(true)
+    .define("FLAT")
+    .vertex_in(1, Type::VEC4, "color")
+    .additional_info("gpu_shader_3D_polyline_no_geom");
+
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_smooth_color)
     .do_static_compilation(true)
     .define("SMOOTH")
     .vertex_in(1, Type::VEC4, "color")
     .additional_info("gpu_shader_3D_polyline");
+
+GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_smooth_color_no_geom)
+    .do_static_compilation(true)
+    .define("SMOOTH")
+    .vertex_in(1, Type::VEC4, "color")
+    .additional_info("gpu_shader_3D_polyline_no_geom");

source/blender/draw/CMakeLists.txt
@@ -555,6 +555,7 @@ set(GLSL_SRC
   engines/overlay/shaders/overlay_armature_envelope_solid_vert.glsl
   engines/overlay/shaders/overlay_armature_shape_outline_geom.glsl
   engines/overlay/shaders/overlay_armature_shape_outline_vert.glsl
+  engines/overlay/shaders/overlay_armature_shape_outline_vert_no_geom.glsl
   engines/overlay/shaders/overlay_armature_shape_solid_frag.glsl
   engines/overlay/shaders/overlay_armature_shape_solid_vert.glsl
   engines/overlay/shaders/overlay_armature_shape_wire_vert.glsl
@@ -572,6 +573,7 @@ set(GLSL_SRC
   engines/overlay/shaders/overlay_depth_only_vert.glsl
   engines/overlay/shaders/overlay_edit_curve_handle_geom.glsl
   engines/overlay/shaders/overlay_edit_curve_handle_vert.glsl
+  engines/overlay/shaders/overlay_edit_curve_handle_vert_no_geom.glsl
   engines/overlay/shaders/overlay_edit_curve_point_vert.glsl
   engines/overlay/shaders/overlay_edit_curve_wire_vert.glsl
   engines/overlay/shaders/overlay_edit_gpencil_canvas_vert.glsl
@@ -587,6 +589,7 @@ set(GLSL_SRC
   engines/overlay/shaders/overlay_edit_mesh_normal_vert.glsl
   engines/overlay/shaders/overlay_edit_mesh_skin_root_vert.glsl
   engines/overlay/shaders/overlay_edit_mesh_vert.glsl
+  engines/overlay/shaders/overlay_edit_mesh_vert_no_geom.glsl
   engines/overlay/shaders/overlay_edit_particle_point_vert.glsl
   engines/overlay/shaders/overlay_edit_particle_strand_vert.glsl
   engines/overlay/shaders/overlay_edit_uv_edges_frag.glsl
@@ -619,6 +622,7 @@ set(GLSL_SRC
   engines/overlay/shaders/overlay_motion_path_line_frag.glsl
   engines/overlay/shaders/overlay_motion_path_line_geom.glsl
   engines/overlay/shaders/overlay_motion_path_line_vert.glsl
+  engines/overlay/shaders/overlay_motion_path_line_vert_no_geom.glsl
   engines/overlay/shaders/overlay_motion_path_point_vert.glsl
   engines/overlay/shaders/overlay_outline_detect_frag.glsl
   engines/overlay/shaders/overlay_outline_prepass_curves_vert.glsl

source/blender/draw/engines/overlay/shaders/overlay_armature_shape_outline_vert_no_geom.glsl
@@ -1,0 +1,182 @@
+
+#pragma USE_SSBO_VERTEX_FETCH(LineList, 2)
+#pragma BLENDER_REQUIRE(common_view_clipping_lib.glsl)
+#pragma BLENDER_REQUIRE(common_view_lib.glsl)
+
+#define DISCARD_VERTEX \
+  gl_Position = finalColor = vec4(0.0); \
+  edgeStart = edgePos = vec2(0.0); \
+  return;
+
+/* Project to screen space. */
+vec2 proj(vec4 pos)
+{
+  return (0.5 * (pos.xy / pos.w) + 0.5) * sizeViewport.xy;
+}
+
+void do_vertex_shader(mat4 in_inst_obmat,
+                      vec3 in_pos,
+                      vec3 in_snor,
+                      out vec4 out_pPos,
+                      out vec3 out_vPos,
+                      out vec2 out_ssPos,
+                      out vec2 out_ssNor,
+                      out vec4 out_vColSize,
+                      out int out_inverted,
+                      out vec4 out_wpos)
+{
+  vec4 bone_color, state_color;
+  mat4 model_mat = extract_matrix_packed_data(in_inst_obmat, state_color, bone_color);
+
+  vec4 worldPosition = model_mat * vec4(in_pos, 1.0);
+  vec4 viewpos = ViewMatrix * worldPosition;
+  out_wpos = worldPosition;
+  out_vPos = viewpos.xyz;
+  out_pPos = ProjectionMatrix * viewpos;
+
+  out_inverted = int(dot(cross(model_mat[0].xyz, model_mat[1].xyz), model_mat[2].xyz) < 0.0);
+
+  /* This is slow and run per vertex, but it's still faster than
+   * doing it per instance on CPU and sending it on via instance attribute. */
+  mat3 normal_mat = transpose(inverse(mat3(model_mat)));
+  /* TODO FIX: there is still a problem with this vector
+   * when the bone is scaled or in persp mode. But it's
+   * barely visible at the outline corners. */
+  out_ssNor = normalize(normal_world_to_view(normal_mat * in_snor).xy);
+
+  out_ssPos = proj(out_pPos);
+
+  out_vColSize = bone_color;
+}
+
+void main()
+{
+  /* Outputs a singular vertex as part of a LineList primitive, however, requires access to
+   * neighbouring 4 vertices. */
+  /* Fetch verts from input type lines adjacency. */
+  int line_prim_id = (gl_VertexID / 2);
+  int line_vertex_id = gl_VertexID % 2;
+  int base_vertex_id = line_prim_id * 2;
+
+  /* IF Input Primitive Type == Lines_Adjacency, then indices are accessed as per GL specification:
+   * i.e. 4 indices per unique prim (Provoking vert 4i-2)
+   *
+   * IF Input Primitive Type == LineStrip_Adjacency, then indices are accessed using:
+   *  - 2 indices per unique prim, plus 1 index at each end, such that the strided
+   *  - 4-index block can be walked. */
+  vec3 in_pos[4];
+  in_pos[0] = vertex_fetch_attribute_raw(vertex_id_from_index_id(4 * line_prim_id), pos, vec3);
+  in_pos[1] = vertex_fetch_attribute_raw(vertex_id_from_index_id(4 * line_prim_id + 1), pos, vec3);
+  in_pos[2] = vertex_fetch_attribute_raw(vertex_id_from_index_id(4 * line_prim_id + 2), pos, vec3);
+  in_pos[3] = vertex_fetch_attribute_raw(vertex_id_from_index_id(4 * line_prim_id + 3), pos, vec3);
+
+  vec3 in_snor[4];
+  in_snor[0] = vertex_fetch_attribute_raw(vertex_id_from_index_id(4 * line_prim_id), snor, vec3);
+  in_snor[1] = vertex_fetch_attribute_raw(
+      vertex_id_from_index_id(4 * line_prim_id + 1), snor, vec3);
+  in_snor[2] = vertex_fetch_attribute_raw(
+      vertex_id_from_index_id(4 * line_prim_id + 2), snor, vec3);
+  in_snor[3] = vertex_fetch_attribute_raw(
+      vertex_id_from_index_id(4 * line_prim_id + 3), snor, vec3);
+
+  mat4 in_inst_obmat = vertex_fetch_attribute(gl_VertexID, inst_obmat, mat4);
+
+  /* Run original GL vertex shader implementation per vertex in adjacency list. */
+  vec4 pPos[4];
+  vec3 vPos[4];
+  vec2 ssPos[4];
+  vec2 ssNor[4];
+  vec4 vColSize[4];
+  int inverted[4];
+  vec4 wPos[4];
+
+  for (int v = 0; v < 4; v++) {
+    do_vertex_shader(in_inst_obmat,
+                     in_pos[v],
+                     in_snor[v],
+                     pPos[v],
+                     vPos[v],
+                     ssPos[v],
+                     ssNor[v],
+                     vColSize[v],
+                     inverted[v],
+                     wPos[v]);
+  }
+
+  /* Geometry Shader equivalent to calculate vertex output position. */
+  finalColor = vec4(vColSize[0].rgb, 1.0);
+
+  bool is_persp = (ProjectionMatrix[3][3] == 0.0);
+
+  vec3 view_vec = (is_persp) ? normalize(vPos[1]) : vec3(0.0, 0.0, -1.0);
+  vec3 v10 = vPos[0] - vPos[1];
+  vec3 v12 = vPos[2] - vPos[1];
+  vec3 v13 = vPos[3] - vPos[1];
+
+  vec3 n0 = cross(v12, v10);
+  vec3 n3 = cross(v13, v12);
+
+  float fac0 = dot(view_vec, n0);
+  float fac3 = dot(view_vec, n3);
+
+  /* If one of the face is perpendicular to the view,
+   * consider it and outline edge. */
+  if (abs(fac0) > 1e-5 && abs(fac3) > 1e-5) {
+    /* If both adjacent verts are facing the camera the same way,
+     * then it isn't an outline edge. */
+    if (sign(fac0) == sign(fac3)) {
+      DISCARD_VERTEX
+    }
+  }
+
+  n0 = (inverted[0] == 1) ? -n0 : n0;
+  /* Don't outline if concave edge. */
+  if (dot(n0, v13) > 0.0001) {
+    DISCARD_VERTEX
+  }
+
+  vec2 perp = normalize(ssPos[2] - ssPos[1]);
+  vec2 edge_dir = vec2(-perp.y, perp.x);
+
+  vec2 hidden_point;
+  /* Take the farthest point to compute edge direction
+   * (avoid problems with point behind near plane).
+   * If the chosen point is parallel to the edge in screen space,
+   * choose the other point anyway.
+   * This fixes some issue with cubes in orthographic views.*/
+  if (vPos[0].z < vPos[3].z) {
+    hidden_point = (abs(fac0) > 1e-5) ? ssPos[0] : ssPos[3];
+  }
+  else {
+    hidden_point = (abs(fac3) > 1e-5) ? ssPos[3] : ssPos[0];
+  }
+  vec2 hidden_dir = normalize(hidden_point - ssPos[1]);
+
+  float fac = dot(-hidden_dir, edge_dir);
+  edge_dir *= (fac < 0.0) ? -1.0 : 1.0;
+
+  /* Output corresponding value based on which vertex this corresponds to in the
+   * original input primitive. */
+  if (line_vertex_id == 0) {
+    gl_Position = pPos[1];
+    /* Offset away from the center to avoid overlap with solid shape. */
+    gl_Position.xy += (edge_dir - perp) * drw_view.viewport_size_inverse * gl_Position.w;
+    /* Improve AA bleeding inside bone silhouette. */
+    gl_Position.z -= (is_persp) ? 1e-4 : 1e-6;
+    edgeStart = edgePos = ((gl_Position.xy / gl_Position.w) * 0.5 + 0.5) * sizeViewport.xy;
+#ifdef USE_WORLD_CLIP_PLANES
+    world_clip_planes_calc_clip_distance(wPos[1].xyz);
+#endif
+  }
+  else {
+    gl_Position = pPos[2];
+    /* Offset away from the center to avoid overlap with solid shape. */
+    gl_Position.xy += (edge_dir + perp) * drw_view.viewport_size_inverse * gl_Position.w;
+    /* Improve AA bleeding inside bone silhouette. */
+    gl_Position.z -= (is_persp) ? 1e-4 : 1e-6;
+    edgeStart = edgePos = ((gl_Position.xy / gl_Position.w) * 0.5 + 0.5) * sizeViewport.xy;
+#ifdef USE_WORLD_CLIP_PLANES
+    world_clip_planes_calc_clip_distance(wPos[2].xyz);
+#endif
+  }
+}

source/blender/draw/engines/overlay/shaders/overlay_edit_curve_handle_vert_no_geom.glsl
@@ -1,0 +1,16 @@
+/* TODO(Metal): Implement correct SSBO implementation for geom shader workaround.
+ * Currently included as placeholder to unblock failing compilation in Metal. */
+
+#pragma BLENDER_REQUIRE(common_view_clipping_lib.glsl)
+#pragma BLENDER_REQUIRE(common_view_lib.glsl)
+
+void main()
+{
+  GPU_INTEL_VERTEX_SHADER_WORKAROUND
+
+  vec3 world_pos = point_object_to_world(pos);
+  gl_Position = point_world_to_ndc(world_pos);
+  vert.flag = data;
+
+  view_clipping_distances(world_pos);
+}

commit 1b5c94630eb78c3a8f8249245dc7e70e23d644b0

GPU: Disable static compilation for geometry shaders workaround
These shaders are only supported by the Metal backed.
Regression introduced by 1514e1a5b7e15ec0c11cd40c2b9389982bd5d00e

source/blender/draw/engines/overlay/shaders/infos/overlay_armature_info.hh
@@ -90,7 +90,7 @@ GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline)
     .additional_info("overlay_frag_output", "overlay_armature_common", "draw_globals");
 
 GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .vertex_in(0, Type::VEC3, "pos")
     .vertex_in(1, Type::VEC3, "snor")
     /* Per instance. */
@@ -106,7 +106,7 @@ GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline_clipped)
     .additional_info("overlay_armature_shape_outline", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_armature_shape_outline_clipped_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .additional_info("overlay_armature_shape_outline_no_geom", "drw_clipped");
 
 GPU_SHADER_INTERFACE_INFO(overlay_armature_shape_solid_iface, "")

source/blender/draw/engines/overlay/shaders/infos/overlay_edit_mode_info.hh
@@ -74,7 +74,7 @@ GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge)
 
 /* The Non-Geometry shader variant passes directly to fragment. */
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .define("EDGE")
     .vertex_in(0, Type::VEC3, "pos")
     .vertex_in(1, Type::UCHAR4, "data")
@@ -90,7 +90,7 @@ GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat)
     .additional_info("overlay_edit_mesh_edge");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .define("FLAT")
     .additional_info("overlay_edit_mesh_edge_no_geom");
 
@@ -165,7 +165,7 @@ GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_clipped)
     .additional_info("overlay_edit_mesh_edge", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_clipped_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .additional_info("overlay_edit_mesh_edge_no_geom", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat_clipped)
@@ -173,7 +173,7 @@ GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat_clipped)
     .additional_info("overlay_edit_mesh_edge_flat", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_edge_flat_clipped_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .additional_info("overlay_edit_mesh_edge_flat_no_geom", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_mesh_face_clipped)
@@ -363,7 +363,7 @@ GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle)
     .additional_info("draw_mesh", "draw_globals");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .typedef_source("overlay_shader_shared.h")
     /* NOTE: Color already in Linear space. Which is what we want. */
     .define("srgbTarget", "false")
@@ -382,7 +382,7 @@ GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle_clipped)
     .additional_info("overlay_edit_curve_handle", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_curve_handle_clipped_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .additional_info("overlay_edit_curve_handle_no_geom", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_edit_curve_point)

source/blender/draw/engines/overlay/shaders/infos/overlay_extra_info.hh
@@ -203,7 +203,7 @@ GPU_SHADER_CREATE_INFO(overlay_motion_path_line)
     .additional_info("draw_view", "draw_globals");
 
 GPU_SHADER_CREATE_INFO(overlay_motion_path_line_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .vertex_in(0, Type::VEC3, "pos")
     .push_constant(Type::IVEC4, "mpathLineSettings")
     .push_constant(Type::BOOL, "selected")
@@ -220,7 +220,7 @@ GPU_SHADER_CREATE_INFO(overlay_motion_path_line_clipped)
     .additional_info("overlay_motion_path_line", "drw_clipped");
 
 GPU_SHADER_CREATE_INFO(overlay_motion_path_line_clipped_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .additional_info("overlay_motion_path_line_no_geom", "drw_clipped");
 
 GPU_SHADER_INTERFACE_INFO(overlay_motion_path_point_iface, "").flat(Type::VEC4, "finalColor");

source/blender/gpu/shaders/infos/gpu_shader_3D_polyline_info.hh
@@ -49,7 +49,7 @@ GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color)
     .additional_info("gpu_shader_3D_polyline");
 
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .define("UNIFORM")
     .push_constant(Type::VEC4, "color")
     .additional_info("gpu_shader_3D_polyline_no_geom");
@@ -63,7 +63,7 @@ GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color_clipped)
     .additional_info("gpu_shader_3D_polyline_uniform_color");
 
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_uniform_color_clipped_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     /* TODO(fclem): Put in an UBO to fit the 128byte requirement. */
     .push_constant(Type::MAT4, "ModelMatrix")
     .push_constant(Type::VEC4, "ClipPlane")
@@ -77,7 +77,7 @@ GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_flat_color)
     .additional_info("gpu_shader_3D_polyline");
 
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_flat_color_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .define("FLAT")
     .vertex_in(1, Type::VEC4, "color")
     .additional_info("gpu_shader_3D_polyline_no_geom");
@@ -89,7 +89,7 @@ GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_smooth_color)
     .additional_info("gpu_shader_3D_polyline");
 
 GPU_SHADER_CREATE_INFO(gpu_shader_3D_polyline_smooth_color_no_geom)
-    .do_static_compilation(true)
+    // .do_static_compilation(true) /* TODO fix on GL */
     .define("SMOOTH")
     .vertex_in(1, Type::VEC4, "color")
     .additional_info("gpu_shader_3D_polyline_no_geom");