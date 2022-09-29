e2df1958449ae94c9178686795e889cead7b960a
Brecht Van Lommel <brecht@blender.org>
2022-09-29 03:56:12 +0200

    Fix wrong version bump to 3.4.1, we are still on 3.4.0
    The patch levels are reserved for bugfix releases.

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -19,7 +19,7 @@ extern "C" {
 /* Blender major and minor version. */
 #define BLENDER_VERSION 304
 /* Blender patch version for bugfix releases. */
-#define BLENDER_VERSION_PATCH 1
+#define BLENDER_VERSION_PATCH 0
 /** Blender release cycle stage: alpha/beta/rc/release. */
 #define BLENDER_VERSION_CYCLE alpha
 
