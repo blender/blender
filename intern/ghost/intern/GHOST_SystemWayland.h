8bb211a771642144ac75ba6be0c1897c836a6276
Campbell Barton <campbell@blender.org>
2022-10-22 16:49:09 +1100

    GHOST/Wayland: multi-touch gesture support
    Add support for zoom & rotate gestures, hold and swipe may
    be used in the future although swipe maps to 2D smooth-scroll for
    Gnome & KDE.
    
    Tested to work with Apple track-pad & Wacom tablet on Gnome & KDE.

--- intern/ghost/intern/GHOST_SystemWayland.h
+++ intern/ghost/intern/GHOST_SystemWayland.h
@@ -159,6 +159,7 @@ class GHOST_SystemWayland : public GHOST_System {
   struct wl_display *wl_display();
   struct wl_compositor *wl_compositor();
   struct zwp_primary_selection_device_manager_v1 *wp_primary_selection_manager();
+  struct zwp_pointer_gestures_v1 *wp_pointer_gestures();
 
 #ifdef WITH_GHOST_WAYLAND_LIBDECOR
   libdecor *libdecor_context();
