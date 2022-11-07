aefeb1bf83211a3817d13662fc05331855d9275f
Nathan Rozendaal <nathan.rozendaal@gmail.com>
2022-11-07 14:26:52 +0100

    Added boilerplate code for the PLY importer/exporter

---
+++ source/blender/io/ply/exporter/ply_export.cc
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup stl
+ */
+
+#include <cstdio>
+
+#include "BKE_customdata.h"
+#include "BKE_layer.h"
+#include "BKE_mesh.h"
+#include "BKE_object.h"
+
+#include "DNA_collection_types.h"
+#include "DNA_scene_types.h"
+
+#include "BLI_fileops.hh"
+#include "BLI_math_vector.h"
+#include "BLI_memory_utils.hh"
+
+#include "DNA_object_types.h"
+
+#include "DEG_depsgraph.h"
+#include "DEG_depsgraph_build.h"
+
+#include "ply_export.hh"
+
+namespace blender::io::ply {
+
+
+void exporter_main(bContext *C, const PLYExportParams &export_params)
+{
+  Main *bmain = CTX_data_main(C);
+  Scene *scene = CTX_data_scene(C);
+  ViewLayer *view_layer = CTX_data_view_layer(C);
+  exporter_main(bmain, scene, view_layer, export_params);
+}
+
+void exporter_main(Main *bmain,
+                   Scene *scene,
+                   ViewLayer *view_layer,
+                   const PLYExportParams &export_params)
+{
+    perror("Error");
+}
+}  // namespace blender::io::ply
---
+++ source/blender/io/ply/importer/ply_import.hh
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup ply
+ */
+
+#pragma once
+
+#include "IO_ply.h"
+
+namespace blender::io::ply {
+
+void ply_import_report_error(FILE *file);
+
+/* Main import function used from within Blender. */
+void importer_main(bContext *C, const PLYImportParams &import_params);
+
+/* Used from tests, where full bContext does not exist. */
+void importer_main(Main *bmain,
+                   Scene *scene,
+                   ViewLayer *view_layer,
+                   const PLYImportParams &import_params);
+
+}  // namespace blender::io::ply
