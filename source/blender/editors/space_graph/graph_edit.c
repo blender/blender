59e69fc2bd3b6f06f179d77acca7634968d15969
Sergey Sharybin <sergey@blender.org>
2022-11-09 09:47:24 +0100

    Fix strict compiler warnings
    Functions which are local to a translation unit should either be
    marked as static, or be in an anonymous namespace.

--- source/blender/editors/space_graph/graph_edit.c
+++ source/blender/editors/space_graph/graph_edit.c
@@ -2333,7 +2333,7 @@ static int graphkeys_snap_exec(bContext *C, wmOperator *op)
   return OPERATOR_FINISHED;
 }
 
-bool graph_has_selected_control_points(struct bContext *C)
+static bool graph_has_selected_control_points(struct bContext *C)
 {
   bAnimContext ac;
   ListBase anim_data = {NULL, NULL};
@@ -2363,9 +2363,9 @@ bool graph_has_selected_control_points(struct bContext *C)
   return has_selected_control_points;
 }
 
-int graphkeys_selected_control_points_invoke(struct bContext *C,
-                                             struct wmOperator *op,
-                                             const struct wmEvent *event)
+static int graphkeys_selected_control_points_invoke(struct bContext *C,
+                                                    struct wmOperator *op,
+                                                    const struct wmEvent *event)
 {
   if (!graph_has_selected_control_points(C)) {
     BKE_report(op->reports, RPT_ERROR, "No control points are selected");
