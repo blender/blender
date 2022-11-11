c5b36aa9404be6859fee7c3d212e484ea9b1d1dc
Ray Molenkamp <github@lazydodo.com>
2022-11-11 13:49:12 -0700

    tests: Disable lattice_deform_performance test
    This test is disabled for the following reasons:
    
    This test is one of the longer ones in this suite (2979 out of 3559ms total)
    and nothing is currently monitoring the performance, if this test were to be
    20% slower one day, no-one would actually notice.
    
    there are no asserts, the test actually cannot fail.
    
    it's good to have some benchmark code, so like some of the other mesh
    benchmark code, exclude it using an `#ifdef` guard so i can be easily
    re-enabled when needed.
    
    reviewed by: jbakker
    Differential Revision: https://developer.blender.org/D16314

--- source/blender/blenkernel/intern/lattice_deform_test.cc
+++ source/blender/blenkernel/intern/lattice_deform_test.cc
@@ -13,6 +13,9 @@
 
 #include "BLI_rand.hh"
 
+#define DO_PERF_TESTS 0
+
+#if DO_PERF_TESTS
 namespace blender::bke::tests {
 
 struct LatticeDeformTestContext {
@@ -122,3 +125,4 @@ TEST(lattice_deform_performance, performance_no_dvert_10000000)
 }
 
 }  // namespace blender::bke::tests
+#endif
