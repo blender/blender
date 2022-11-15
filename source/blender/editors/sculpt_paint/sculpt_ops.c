6a97668c9bfac211a0b05ed2e7a099ed9446fd49
Joseph Eagar <joeedh@gmail.com>
2022-11-15 08:41:42 -0800

    Sculpt: Port cavity_from_mask propery naming changes to 3.4 branch

--- source/blender/editors/sculpt_paint/sculpt_ops.c
+++ source/blender/editors/sculpt_paint/sculpt_ops.c
@@ -1242,7 +1242,7 @@ static void SCULPT_OT_mask_from_cavity(wmOperatorType *ot)
                 0.5f,
                 0.0f,
                 5.0f,
-                "Cavity Factor",
+                "Factor",
                 "The contrast of the cavity mask",
                 0.0f,
                 1.0f);
@@ -1251,11 +1251,11 @@ static void SCULPT_OT_mask_from_cavity(wmOperatorType *ot)
               2,
               0,
               25,
-              "Cavity Blur",
+              "Blur",
               "The number of times the cavity mask is blurred",
               0,
               25);
-  RNA_def_boolean(ot->srna, "use_curve", false, "Use Curve", "");
+  RNA_def_boolean(ot->srna, "use_curve", false, "Custom Curve", "");
 
   RNA_def_boolean(ot->srna, "invert", false, "Cavity (Inverted)", "");
 }
