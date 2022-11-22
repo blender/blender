b79e5ae4f26d79bb1c76ffbdda6a7dfbb3f4e577
Jeroen Bakker <jeroen@blender.org>
2022-11-22 12:45:49 +0100

    GHOST: Add missing C_API function to header file.
    - GHOST_GetDrawingContext was missing.

--- intern/ghost/GHOST_C-api.h
+++ intern/ghost/GHOST_C-api.h
@@ -561,6 +561,13 @@ extern GHOST_TSuccess GHOST_SetDrawingContextType(GHOST_WindowHandle windowhandl
                                                   GHOST_TDrawingContextType type);
 
 /**
+ * Returns the drawing context used in the this window.
+ * \param windowhandle: The handle to the window.
+ * \return The window drawing context.
+ */
+extern GHOST_ContextHandle GHOST_GetDrawingContext(GHOST_WindowHandle windowhandle);
+
+/**
  * Sets the title displayed in the title bar.
  * \param windowhandle: The handle to the window.
  * \param title: The title to display in the title bar.
