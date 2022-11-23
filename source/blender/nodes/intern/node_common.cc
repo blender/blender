a87d78860b3c5a5ba89b0c05c59201997dac6350
Jacques Lucke <jacques@blender.org>
2022-11-23 17:01:49 +0100

    Fix T102693: adding shader node group to geometry nodes crashes
    This is not supported currently, so just cancelling the operation
    solves it. This might be supported in the future.

--- source/blender/nodes/intern/node_common.cc
+++ source/blender/nodes/intern/node_common.cc
@@ -101,6 +101,10 @@ bool nodeGroupPoll(const bNodeTree *nodetree,
     }
     return false;
   }
+  if (nodetree->type != grouptree->type) {
+    *r_disabled_hint = TIP_("Node group has different type");
+    return false;
+  }
 
   LISTBASE_FOREACH (const bNode *, node, &grouptree->nodes) {
     if (node->typeinfo->poll_instance &&
