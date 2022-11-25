826535979a8b35bcc060159c96d30d3094b9153a
Jacques Lucke <jacques@blender.org>
2022-11-25 12:10:56 +0100

    Nodes: add non-const utility to find socket by identifier
    This does the same as the corresponding const method.

--- source/blender/makesdna/DNA_node_types.h
+++ source/blender/makesdna/DNA_node_types.h
@@ -170,6 +170,7 @@ typedef struct bNodeSocket {
   bNodeSocketRuntimeHandle *runtime;
 
 #ifdef __cplusplus
+  bool is_hidden() const;
   bool is_available() const;
   bool is_multi_input() const;
   bool is_input() const;
@@ -356,6 +357,8 @@ typedef struct bNode {
   /** Lookup socket of this node by its identifier. */
   const bNodeSocket &input_by_identifier(blender::StringRef identifier) const;
   const bNodeSocket &output_by_identifier(blender::StringRef identifier) const;
+  bNodeSocket &input_by_identifier(blender::StringRef identifier);
+  bNodeSocket &output_by_identifier(blender::StringRef identifier);
   /** If node is frame, will return all children nodes. */
   blender::Span<bNode *> direct_children_in_frame() const;
   /** Node tree this node belongs to. */
