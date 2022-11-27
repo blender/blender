95003c99d90bb8d6a682cb11036fb943ba0f4e4b
Clément Foucault <foucault.clem@gmail.com>
2022-11-27 23:22:32 +0100

    GPU: Change inheritance of depth write and default values
    This new inheritance behavior is more beneficial for the metal Backend.
    Also change the default depth write behavior of shaders to be unchanged.
    This makes fragment shader depth amendment more explicit.
    
    This also add the missing depth_write for metal kernels.

--- source/blender/gpu/intern/gpu_shader_create_info.cc
+++ source/blender/gpu/intern/gpu_shader_create_info.cc
@@ -68,7 +68,9 @@ void ShaderCreateInfo::finalize()
     if (info.early_fragment_test_) {
       early_fragment_test_ = true;
     }
-    if (info.depth_write_ != DepthWrite::ANY) {
+    /* Override depth-write with additional info if this specifies a writing mode
+     * other than the default. */
+    if (info.depth_write_ != DepthWrite::UNCHANGED) {
       depth_write_ = info.depth_write_;
     }
 
