2addc089cfd1cfb72e296154edb7c3e995ce81d7
Lilith <48443491+Stroopwafe1@users.noreply.github.com>
2022-12-01 14:38:02 +0100

    Feat: Moved function to generic location

---
+++ source/blender/io/ply/importer/ply_import_mesh.hh
@@ -0,0 +1,17 @@
+#ifndef BLENDER_PLY_IMPORT_MESH_HH
+#define BLENDER_PLY_IMPORT_MESH_HH
+
+#include "DNA_mesh_types.h"
+#include "ply_data.hh"
+
+namespace blender::io::ply {
+
+/**
+ * Converts the PlyData datastructure to a mesh
+ * @param data The PLY data
+ * @return The mesh that can be used inside blender
+ */
+Mesh *convert_ply_to_mesh(PlyData &data, Mesh *mesh);
+
+} // namespace blender::io::ply
+#endif  // BLENDER_PLY_IMPORT_MESH_HH
