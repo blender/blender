d57f68616a668ef974953be42311a78a539a3fe0
Jacques Lucke <jacques@blender.org>
2022-12-02 13:18:54 +0100

    Fix: bump minimum version
    rB9fa4ceb340951 caused a forward compatibility issue.
    Going forward, when changing socket names, only the name should be
    changed and not the identifier if possible.

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@ extern "C" {
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and show a warning if the file
  * was written with too new a version. */
-#define BLENDER_FILE_MIN_VERSION 304
+#define BLENDER_FILE_MIN_VERSION 305
 #define BLENDER_FILE_MIN_SUBVERSION 3
 
 /** User readable version string. */
