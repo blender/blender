43f482c023448282ce1a4319ca57d64ac436c6af
Lilith <48443491+Stroopwafe1@users.noreply.github.com>
2022-12-06 15:08:39 +0100

    Fix: Differing amounts of vertex indices in faces

--- source/blender/io/ply/importer/ply_import_mesh.cc
+++ source/blender/io/ply/importer/ply_import_mesh.cc
@@ -19,21 +19,25 @@ Mesh *convert_ply_to_mesh(PlyData &data, Mesh *mesh)
   // Add faces and edges to the mesh
   if (data.faces.size() > 0) {
     mesh->totpoly = data.faces.size();
-    mesh->totloop = data.faces.size() *
-                    data.faces[0].size();  // TODO: Make this more dynamic using data.edges()
+    mesh->totloop = 0; // TODO: Make this more dynamic using data.edges()
+    for (int i = 0; i < data.faces.size(); i++) {
+    	mesh->totloop += data.faces[i].size();
+	}
     CustomData_add_layer(&mesh->pdata, CD_MPOLY, CD_SET_DEFAULT, nullptr, mesh->totpoly);
     CustomData_add_layer(&mesh->ldata, CD_MLOOP, CD_SET_DEFAULT, nullptr, mesh->totloop);
     MutableSpan<MPoly> polys = mesh->polys_for_write();
     MutableSpan<MLoop> loops = mesh->loops_for_write();
 
+    int offset = 0;
     for (int i = 0; i < mesh->totpoly; i++) {
       int size = data.faces[i].size();
-      polys[i].loopstart = size * i;
+      polys[i].loopstart = offset;
       polys[i].totloop = size;
 
       for (int j = 0; j < size; j++) {
-        loops[size * i + j].v = data.faces[i][j];
+        loops[offset + j].v = data.faces[i][j];
       }
+      offset += size;
     }
   }
 
