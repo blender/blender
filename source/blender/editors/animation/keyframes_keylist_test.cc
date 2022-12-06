67dd652557da07c207f8bb79d87abc9f94ebe868
Sybren A. Stüvel <sybren@blender.org>
2022-12-06 12:23:21 +0100

    Tests: anim keylist test, avoid interaction between checks
    Simplify checks so that one check doesn't influence the following one.
    
    Checks no longer pass the last-visited frame number into the "start frame"
    parameter of the next check. This way all test values are hard-coded and
    easy to read, without having to understand how all the checks fit together.
    
    No functional changes.

--- source/blender/editors/animation/keyframes_keylist_test.cc
+++ source/blender/editors/animation/keyframes_keylist_test.cc
@@ -21,6 +21,7 @@ namespace blender::editor::animation::tests {
 const float KEYLIST_NEAR_ERROR = 0.1;
 const float FRAME_STEP = 0.005;
 
+/* Build FCurve with keys on frames 10, 20, and 30. */
 static void build_fcurve(FCurve &fcurve)
 {
   fcurve.totvert = 3;
@@ -51,92 +52,88 @@ static void assert_act_key_column(const ActKeyColumn *column,
                                   const std::optional<float> expected_frame)
 {
   if (expected_frame.has_value()) {
-    ASSERT_NE(column, nullptr);
+    ASSERT_NE(column, nullptr) << "Expected a frame to be found at " << *expected_frame;
     EXPECT_NEAR(column->cfra, *expected_frame, KEYLIST_NEAR_ERROR);
   }
   else {
-    EXPECT_EQ(column, nullptr);
+    EXPECT_EQ(column, nullptr) << "Expected no frame to be found, but found " << column->cfra;
   }
 }
 
 using KeylistFindFunction = std::function<const ActKeyColumn *(const AnimKeylist *, float)>;
 
-static float check_keylist_find_range(const AnimKeylist *keylist,
-                                      KeylistFindFunction keylist_find_func,
-                                      const float frame_from,
-                                      const float frame_to,
-                                      const std::optional<float> expected_frame)
+static void check_keylist_find_range(const AnimKeylist *keylist,
+                                     KeylistFindFunction keylist_find_func,
+                                     const float frame_from,
+                                     const float frame_to,
+                                     const std::optional<float> expected_frame)
 {
   float cfra = frame_from;
   for (; cfra < frame_to; cfra += FRAME_STEP) {
     const ActKeyColumn *found = keylist_find_func(keylist, cfra);
     assert_act_key_column(found, expected_frame);
   }
-  return cfra;
 }
 
-static float check_keylist_find_next_range(const AnimKeylist *keylist,
-                                           const float frame_from,
-                                           const float frame_to,
-                                           const std::optional<float> expected_frame)
+static void check_keylist_find_next_range(const AnimKeylist *keylist,
+                                          const float frame_from,
+                                          const float frame_to,
+                                          const std::optional<float> expected_frame)
 {
-  return check_keylist_find_range(
-      keylist, ED_keylist_find_next, frame_from, frame_to, expected_frame);
+  check_keylist_find_range(keylist, ED_keylist_find_next, frame_from, frame_to, expected_frame);
 }
 
 TEST(keylist, find_next)
 {
   AnimKeylist *keylist = create_test_keylist();
 
-  float cfra = check_keylist_find_next_range(keylist, 0.0f, 9.99f, 10.0f);
-  cfra = check_keylist_find_next_range(keylist, cfra, 19.99f, 20.0f);
-  cfra = check_keylist_find_next_range(keylist, cfra, 29.99f, 30.0f);
-  cfra = check_keylist_find_next_range(keylist, cfra, 39.99f, std::nullopt);
+  check_keylist_find_next_range(keylist, 0.0f, 9.99f, 10.0f);
+  check_keylist_find_next_range(keylist, 10.0f, 19.99f, 20.0f);
+  check_keylist_find_next_range(keylist, 20.0f, 29.99f, 30.0f);
+  check_keylist_find_next_range(keylist, 30.0f, 39.99f, std::nullopt);
 
   ED_keylist_free(keylist);
 }
 
-static float check_keylist_find_prev_range(const AnimKeylist *keylist,
-                                           const float frame_from,
-                                           const float frame_to,
-                                           const std::optional<float> expected_frame)
+static void check_keylist_find_prev_range(const AnimKeylist *keylist,
+                                          const float frame_from,
+                                          const float frame_to,
+                                          const std::optional<float> expected_frame)
 {
-  return check_keylist_find_range(
-      keylist, ED_keylist_find_prev, frame_from, frame_to, expected_frame);
+  check_keylist_find_range(keylist, ED_keylist_find_prev, frame_from, frame_to, expected_frame);
 }
 
 TEST(keylist, find_prev)
 {
   AnimKeylist *keylist = create_test_keylist();
 
-  float cfra = check_keylist_find_prev_range(keylist, 0.0f, 10.01f, std::nullopt);
-  cfra = check_keylist_find_prev_range(keylist, cfra, 20.01f, 10.0f);
-  cfra = check_keylist_find_prev_range(keylist, cfra, 30.01f, 20.0f);
-  cfra = check_keylist_find_prev_range(keylist, cfra, 49.99f, 30.0f);
+  check_keylist_find_prev_range(keylist, 0.0f, 10.00f, std::nullopt);
+  check_keylist_find_prev_range(keylist, 10.01f, 20.00f, 10.0f);
+  check_keylist_find_prev_range(keylist, 20.01f, 30.00f, 20.0f);
+  check_keylist_find_prev_range(keylist, 30.01f, 49.99f, 30.0f);
 
   ED_keylist_free(keylist);
 }
 
-static float check_keylist_find_exact_range(const AnimKeylist *keylist,
-                                            const float frame_from,
-                                            const float frame_to,
-                                            const std::optional<float> expected_frame)
+static void check_keylist_find_exact_range(const AnimKeylist *keylist,
+                                           const float frame_from,
+                                           const float frame_to,
+                                           const std::optional<float> expected_frame)
 {
-  return check_keylist_find_range(
-      keylist, ED_keylist_find_exact, frame_from, frame_to, expected_frame);
+  check_keylist_find_range(keylist, ED_keylist_find_exact, frame_from, frame_to, expected_frame);
 }
 
 TEST(keylist, find_exact)
 {
   AnimKeylist *keylist = create_test_keylist();
 
-  float cfra = check_keylist_find_exact_range(keylist, 0.0f, 9.99f, std::nullopt);
-  cfra = check_keylist_find_exact_range(keylist, cfra, 10.01f, 10.0f);
-  cfra = check_keylist_find_exact_range(keylist, cfra, 19.99f, std::nullopt);
-  cfra = check_keylist_find_exact_range(keylist, cfra, 20.01f, 20.0f);
-  cfra = check_keylist_find_exact_range(keylist, cfra, 29.99f, std::nullopt);
-  cfra = check_keylist_find_exact_range(keylist, cfra, 30.01f, 30.0f);
-  cfra = check_keylist_find_exact_range(keylist, cfra, 49.99f, std::nullopt);
+  check_keylist_find_exact_range(keylist, 0.0f, 9.99f, std::nullopt);
+  check_keylist_find_exact_range(keylist, 9.9901f, 10.01f, 10.0f);
+  check_keylist_find_exact_range(keylist, 10.01f, 19.99f, std::nullopt);
+  check_keylist_find_exact_range(keylist, 19.9901f, 20.01f, 20.0f);
+  check_keylist_find_exact_range(keylist, 20.01f, 29.99f, std::nullopt);
+  check_keylist_find_exact_range(keylist, 29.9901f, 30.01f, 30.0f);
+  check_keylist_find_exact_range(keylist, 30.01f, 49.99f, std::nullopt);
 
   ED_keylist_free(keylist);
 }
