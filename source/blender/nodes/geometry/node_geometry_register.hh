63e0bd1da62a9e3428b44a5bd5385badd7300040
Howard Trickey <howard.trickey@gmail.com>
2022-12-10 14:06:02 -0500

    Fix after merge for bevel_mesh node init.

--- source/blender/nodes/geometry/node_geometry_register.hh
+++ source/blender/nodes/geometry/node_geometry_register.hh
@@ -11,6 +11,7 @@ void register_node_type_geo_attribute_capture();
 void register_node_type_geo_attribute_domain_size();
 void register_node_type_geo_attribute_separate_xyz();
 void register_node_type_geo_attribute_statistic();
+void register_node_type_geo_bevel_mesh();
 void register_node_type_geo_blur_attribute();
 void register_node_type_geo_boolean();
 void register_node_type_geo_bounding_box();
