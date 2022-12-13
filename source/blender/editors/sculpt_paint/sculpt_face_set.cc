d666c64f5d6939fec81bc51b8aed140e1c17ddf5
Hans Goudey <h.goudey@me.com>
2022-12-13 14:26:46 -0600

    Fix T103195: Initialize face sets from bevel weights broken
    The conversion from char to float (divide by 255) wasn't removed in
    291c313f80b4cccc8fcc. Also fix a crash when the edge crease layer
    didn't exist.

--- source/blender/editors/sculpt_paint/sculpt_face_set.cc
+++ source/blender/editors/sculpt_paint/sculpt_face_set.cc
@@ -692,7 +692,7 @@ static int sculpt_face_set_init_exec(bContext *C, wmOperator *op)
           CustomData_get_layer(&mesh->edata, CD_CREASE));
       sculpt_face_sets_init_flood_fill(
           ob, [&](const int /*from_face*/, const int edge, const int /*to_face*/) -> bool {
-            return creases[edge] < threshold;
+            return creases ? creases[edge] < threshold : true;
           });
       break;
     }
@@ -709,7 +709,7 @@ static int sculpt_face_set_init_exec(bContext *C, wmOperator *op)
           CustomData_get_layer(&mesh->edata, CD_BWEIGHT));
       sculpt_face_sets_init_flood_fill(
           ob, [&](const int /*from_face*/, const int edge, const int /*to_face*/) -> bool {
-            return bevel_weights ? bevel_weights[edge] / 255.0f < threshold : true;
+            return bevel_weights ? bevel_weights[edge] < threshold : true;
           });
       break;
     }
