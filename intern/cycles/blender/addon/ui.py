ba347d7c9e4ae53d98cce1231fc919591b3c5d81
Thomas Dinges <blender@dingto.org>
2022-12-15 10:44:56 +0100

    Fix Cycles UI for Scrambling Distance, only works with Tabulated Sobol.
    Thanks to Alaska for finding this.

--- intern/cycles/blender/addon/ui.py
+++ intern/cycles/blender/addon/ui.py
@@ -370,7 +370,7 @@ class CYCLES_RENDER_PT_sampling_advanced(CyclesButtonsPanel, Panel):
         layout.separator()
 
         heading = layout.column(align=True, heading="Scrambling Distance")
-        heading.active = cscene.sampling_pattern != 'TABULATED_SOBOL'
+        heading.active = cscene.sampling_pattern == 'TABULATED_SOBOL'
         heading.prop(cscene, "auto_scrambling_distance", text="Automatic")
         heading.prop(cscene, "preview_scrambling_distance", text="Viewport")
         heading.prop(cscene, "scrambling_distance", text="Multiplier")
