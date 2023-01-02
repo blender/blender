a4e843c25024eb1e3a988e74d2f0efd887eb2588
Sybren A. Stüvel <sybren@blender.org>
2023-01-02 11:14:14 +0100

    Cleanup: resolve `variable ‘update’ set but not used` warning
    Resolve `variable ‘update’ set but not used` compiler warning, by removing
    the variable altogether.
    
    No functional changes.

--- source/blender/editors/sculpt_paint/sculpt_expand.c
+++ source/blender/editors/sculpt_paint/sculpt_expand.c
@@ -2144,11 +2144,9 @@ static int sculpt_expand_invoke(bContext *C, wmOperator *op, const wmEvent *even
         BKE_pbvh_search_gather(ss->pbvh, NULL, NULL, &nodes, &nodes_num);
         for (int i = 0; i < nodes_num; i++) {
           PBVHVertexIter vd;
-          bool update = false;
 
           BKE_pbvh_vertex_iter_begin (ss->pbvh, nodes[i], vd, PBVH_ITER_UNIQUE) {
             *vd.mask = 1.0f;
-            update = true;
           }
           BKE_pbvh_vertex_iter_end;
 
