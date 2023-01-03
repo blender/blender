e438e8e04ee83c194c4be9dea8560c53b3aee429
Pratik Borhade <pratikborhade302@gmail.com>
2023-01-03 14:56:11 +0530

    Fix T102993: Incorrect icon displaying of Weighted Normal modifier in the outliner
    Mistake in {rBd15e8bdaa3343cf97a74f918b2570e66fb7abfa0}
    
    Reviewed by: JacquesLucke
    
    Differential Revision: https://developer.blender.org/D16890

--- source/blender/modifiers/intern/MOD_weighted_normal.cc
+++ source/blender/modifiers/intern/MOD_weighted_normal.cc
@@ -738,7 +738,7 @@ ModifierTypeInfo modifierType_WeightedNormal = {
     /* type */ eModifierTypeType_Constructive,
     /* flags */ eModifierTypeFlag_AcceptsMesh | eModifierTypeFlag_SupportsMapping |
         eModifierTypeFlag_SupportsEditmode | eModifierTypeFlag_EnableInEditmode,
-    /* icon */ ICON_MOD_VERTEX_WEIGHT,
+    /* icon */ ICON_MOD_NORMALEDIT,
 
     /* copyData */ BKE_modifier_copydata_generic,
 
