589cbbf0e382a77b19f90886370682a0feab6fa6
Brecht Van Lommel <brecht@blender.org>
2023-01-09 13:15:20 +0100

    Tests: test availability of new bundled Python libraries
    Differential Revision: https://developer.blender.org/D16733

--- tests/python/bl_bundled_modules.py
+++ tests/python/bl_bundled_modules.py
@@ -1,7 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-# Test that modules we ship with our Python installation are available
+# Test that modules we ship with our Python installation are available,
+# both for Blender itself and the bundled Python executable.
 
+import os
+import subprocess
+import sys
+
+app = "Blender" if sys.argv[-1] == "--inside-blender" else "Python"
+sys.stderr.write(f"Testing bundled modules in {app} executable.\n")
+
+# General purpose modules.
 import bz2
 import certifi
 import ctypes
@@ -14,3 +23,16 @@ import ssl
 import urllib3
 import zlib
 import zstandard
+
+# VFX platform modules.
+from pxr import Usd
+import MaterialX
+import OpenImageIO
+import PyOpenColorIO
+import pyopenvdb
+
+# Test modules in bundled Python standalone executable.
+if app == "Blender":
+    script_filepath = os.path.abspath(__file__)
+    proc = subprocess.Popen([sys.executable, script_filepath])
+    sys.exit(proc.wait())
