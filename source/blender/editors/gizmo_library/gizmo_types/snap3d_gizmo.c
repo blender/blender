blender/blender

commit 2b32a2c3b2193ccfe9062efd842f35798fca0cd5

Snap Gizmo: hide snap cursor if another gizmo is highlighted

source/blender/editors/gizmo_library/gizmo_types/snap3d_gizmo.c
@@ -242,6 +242,10 @@ static void snap_cursor_free(SnapGizmo3D *snap_gizmo)
 static bool snap_cursor_poll(ARegion *region, void *data)
 {
   SnapGizmo3D *snap_gizmo = (SnapGizmo3D *)data;
+  if (!(snap_gizmo->gizmo.state & WM_GIZMO_STATE_HIGHLIGHT)) {
+    return false;
+  }
+
   if (snap_gizmo->gizmo.flag & WM_GIZMO_HIDDEN) {
     snap_cursor_free(snap_gizmo);
     return false;

commit 0349a6e6e05ee5f3c941890a8ce4062615ecf5cc

Fix disappearing snap indication in Measure tool
Snap cursor disappeared while dragging.

Regression in 2b32a2c3b

source/blender/editors/gizmo_library/gizmo_types/snap3d_gizmo.c
@@ -242,7 +242,8 @@ static void snap_cursor_free(SnapGizmo3D *snap_gizmo)
 static bool snap_cursor_poll(ARegion *region, void *data)
 {
   SnapGizmo3D *snap_gizmo = (SnapGizmo3D *)data;
-  if (!(snap_gizmo->gizmo.state & WM_GIZMO_STATE_HIGHLIGHT)) {
+  if (!(snap_gizmo->gizmo.state & WM_GIZMO_STATE_HIGHLIGHT) &&
+      !(snap_gizmo->gizmo.flag & WM_GIZMO_DRAW_VALUE)) {
     return false;
   }
 

source/blender/editors/space_view3d/view3d_gizmo_ruler.c
@@ -301,10 +301,13 @@ static void ruler_state_set(RulerInfo *ruler_info, int state)
   }
 
   if (state == RULER_STATE_NORMAL) {
-    /* pass */
+    WM_gizmo_set_flag(ruler_info->snap_data.gizmo, WM_GIZMO_DRAW_VALUE, false);
   }
   else if (state == RULER_STATE_DRAG) {
     memset(&ruler_info->drag_state_prev, 0x0, sizeof(ruler_info->drag_state_prev));
+
+    /* Force the snap cursor to appear even though it is not highlighted. */
+    WM_gizmo_set_flag(ruler_info->snap_data.gizmo, WM_GIZMO_DRAW_VALUE, true);
   }
   else {
     BLI_assert(0);