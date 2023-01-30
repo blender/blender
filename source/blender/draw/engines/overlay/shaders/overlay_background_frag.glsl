596ee79a9f590c1d66aa1cb5c19f994ddb0f0272
Jason Fielder <jason_apple>
2023-01-30 13:44:46 +0100

    Metal: Optimize shader local memory usage.
    Due to shader global scope emulation via class interface, global constant arrays in shaders are allocated in per-thread shader local memory. To reduce memory pressure, placing these constant arrays inside function scope will ensure they only reside within device constant memory. This results in a tangible 1.5-2x performance uplift for the specific shaders affected.
    
    Authored by Apple: Michael Parkin-White
    
    Ref T96261
    
    Reviewed By: fclem
    
    Maniphest Tasks: T96261
    
    Differential Revision: https://developer.blender.org/D17089

--- source/blender/draw/engines/overlay/shaders/overlay_background_frag.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_background_frag.glsl
@@ -4,13 +4,16 @@
 
 /* 4x4 bayer matrix prepared for 8bit UNORM precision error. */
 #define P(x) (((x + 0.5) * (1.0 / 16.0) - 0.5) * (1.0 / 255.0))
-const vec4 dither_mat4x4[4] = vec4[4](vec4(P(0.0), P(8.0), P(2.0), P(10.0)),
-                                      vec4(P(12.0), P(4.0), P(14.0), P(6.0)),
-                                      vec4(P(3.0), P(11.0), P(1.0), P(9.0)),
-                                      vec4(P(15.0), P(7.0), P(13.0), P(5.0)));
 
 float dither(void)
 {
+  /* NOTE(Metal): Declaring constant array in function scope to avoid increasing local shader
+   * memory pressure. */
+  const vec4 dither_mat4x4[4] = vec4[4](vec4(P(0.0), P(8.0), P(2.0), P(10.0)),
+                                        vec4(P(12.0), P(4.0), P(14.0), P(6.0)),
+                                        vec4(P(3.0), P(11.0), P(1.0), P(9.0)),
+                                        vec4(P(15.0), P(7.0), P(13.0), P(5.0)));
+
   ivec2 co = ivec2(gl_FragCoord.xy) % 4;
   return dither_mat4x4[co.x][co.y];
 }
