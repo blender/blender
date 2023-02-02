0de368dce3d5140b7d1116de1f79584c16f6b148
Kwonunn <kwonunnx@gmail.com>
2023-02-02 13:44:29 +0100

    Retrieve vtx colors in a nicer way
    ply_export_load_plydata.cc:183 - Access to active color attributes was changed Blender-PLY-Project/blender#158
    
    ply_export_load_plydata.cc:184 - Type Cast style Blender-PLY-Project/blender#159

--- source/blender/io/ply/exporter/ply_export_load_plydata.cc
+++ source/blender/io/ply/exporter/ply_export_load_plydata.cc
@@ -6,6 +6,7 @@
 
 #include "BLI_math.h"
 
+#include "BKE_attribute.hh"
 #include "BKE_lib_id.h"
 #include "BKE_mesh.h"
 #include "BKE_mesh_mapping.h"
@@ -181,10 +182,18 @@ void load_plydata(PlyData &plyData, Depsgraph *depsgraph, const PLYExportParams 
     }
 
     /* Colors */
-    if (export_params.export_colors && CustomData_has_layer(&mesh->vdata, CD_PROP_COLOR)) {
-      const float4 *colors = (float4 *)CustomData_get_layer(&mesh->vdata, CD_PROP_COLOR);
-      for (int i = 0; i < vertex_map.size(); i++) {
-        plyData.vertex_colors.append(colors[mesh_vertex_index_LUT[i]]);
+    if (export_params.export_colors) {
+      const StringRef name = mesh->active_color_attribute;
+      if (!name.is_empty()) {
+        const bke::AttributeAccessor attributes = mesh->attributes();
+        const VArray<ColorGeometry4f> colorAttribute = attributes.lookup_or_default<ColorGeometry4f>(
+            name, ATTR_DOMAIN_POINT, {0.0f, 0.0f, 0.0f, 0.0f});
+
+        for (int i = 0; i < vertex_map.size(); i++) {
+          ColorGeometry4f colorGeometry = colorAttribute[mesh_vertex_index_LUT[i]];
+          float4 vertColor(colorGeometry.r, colorGeometry.g, colorGeometry.b, colorGeometry.a);
+          plyData.vertex_colors.append(vertColor);
+        }
       }
     }
 
