3002670332afffb3b662ae839fc568eb5903af51
Bastien Montagne <bastien@blender.org>
2023-02-07 09:50:50 +0100

    Fix T104368: Incorrect tooltip text in Blender 3.4.1's Preferences > File Paths > Scripts field.
    Use backticks to cleary identify 'path' parts of this tooltip.

--- source/blender/makesrna/intern/rna_userdef.c
+++ source/blender/makesrna/intern/rna_userdef.c
@@ -6227,7 +6227,7 @@ static void rna_def_userdef_filepaths(BlenderRNA *brna)
       prop,
       "Python Scripts Directory",
       "Alternate script path, matching the default layout with subdirectories: "
-      "startup, add-ons, modules, and presets (requires restart)");
+      "`startup`, `addons`, `modules`, and `presets` (requires restart)");
   /* TODO: editing should reset sys.path! */
 
   prop = RNA_def_property(srna, "i18n_branches_directory", PROP_STRING, PROP_DIRPATH);
