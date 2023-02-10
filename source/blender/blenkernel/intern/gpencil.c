608916b0e1d6c62e73f66d1b0d19b5ad14106de3
Amelie Fondevilla <amelief@noreply.localhost>
2023-02-10 12:55:06 +0100

    Fix #104514: GPencil merge down layer misses some frames
    When merging two gpencil layers, if the destination layer had a keyframe
    where the source layer did not, strokes of the previous keyframe
    in source layer were lost in that frame.
    
    This happened because the merge operator was looping through
    frames of the source layer and appending strokes in the
    corresponding destination layer, but never completing
    other frames than the ones existing in the source layer.
    
    This patch fixes it by first adding in source layer
    all frames that are in destination layer.
    
    Co-authored-by: Amelie Fondevilla <amelie.fondevilla@les-fees-speciales.coop>
    Pull Request #104558

--- source/blender/blenkernel/intern/gpencil.c
+++ source/blender/blenkernel/intern/gpencil.c
@@ -1261,7 +1261,9 @@ bGPDframe *BKE_gpencil_layer_frame_get(bGPDlayer *gpl, int cframe, eGP_GetFrame_
           found = true;
           break;
         }
-        if ((gpf->next) && (gpf->next->framenum > cframe)) {
+        /* If this is the last frame or the next frame is at a later time, we found the right
+         * frame. */
+        if (!(gpf->next) || (gpf->next->framenum > cframe)) {
           found = true;
           break;
         }
