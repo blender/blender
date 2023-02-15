5a927d134e5922d6c436db34968a653a1c719d47
Lilith <48443491+Stroopwafe1@users.noreply.github.com>
2023-02-15 12:20:41 +0100

    Chore: Implemented feedback on ply_import_mesh.cc

--- source/blender/io/ply/importer/ply_import_mesh.cc
+++ source/blender/io/ply/importer/ply_import_mesh.cc
@@ -24,8 +24,7 @@ Mesh *convert_ply_to_mesh(PlyData &data, Mesh *mesh, const PLYImportParams &para
   mesh->totvert = int(data.vertices.size());
   CustomData_add_layer_named(
       &mesh->vdata, CD_PROP_FLOAT3, CD_CONSTRUCT, nullptr, mesh->totvert, "position");
-  MutableSpan<float3> verts = mesh->vert_positions_for_write();
-  verts.copy_from(data.vertices);
+  mesh->vert_positions_for_write().copy_from(data.vertices);
 
   bke::MutableAttributeAccessor attributes = mesh->attributes_for_write();
 
@@ -83,16 +82,16 @@ Mesh *convert_ply_to_mesh(PlyData &data, Mesh *mesh, const PLYImportParams &para
 
   /* Uvmap */
   if (!data.UV_coordinates.is_empty()) {
-    bke::SpanAttributeWriter<float2> Uv = attributes.lookup_or_add_for_write_only_span<float2>(
+    bke::SpanAttributeWriter<float2> uv_map = attributes.lookup_or_add_for_write_only_span<float2>(
         "UVMap", ATTR_DOMAIN_CORNER);
     int counter = 0;
     for (int i = 0; i < data.faces.size(); i++) {
       for (int j = 0; j < data.faces[i].size(); j++) {
-        copy_v2_v2(Uv.span[counter], data.UV_coordinates[data.faces[i][j]]);
+        copy_v2_v2(uv_map.span[counter], data.UV_coordinates[data.faces[i][j]]);
         counter++;
       }
     }
-    Uv.finish();
+    uv_map.finish();
   }
 
   /* Calculate edges from the rest of the mesh. */
@@ -100,25 +99,8 @@ Mesh *convert_ply_to_mesh(PlyData &data, Mesh *mesh, const PLYImportParams &para
 
   /* Note: This is important to do after initializing the loops. */
   if (!data.vertex_normals.is_empty()) {
-    float(*vertex_normals)[3] = static_cast<float(*)[3]>(
-        MEM_malloc_arrayN(data.vertex_normals.size(), sizeof(float[3]), __func__));
-
-    /* Below code is necessary to access vertex normals within Blender.
-     * Until Blender supports custom vertex normals, this is a workaround. */
-    bke::SpanAttributeWriter<float3> normals;
-    if (params.import_normals_as_attribute) {
-      normals = attributes.lookup_or_add_for_write_span<float3>("Normal", ATTR_DOMAIN_POINT);
-    }
-
-    for (int i = 0; i < data.vertex_normals.size(); i++) {
-      copy_v3_v3(vertex_normals[i], data.vertex_normals[i]);
-      if (normals) {
-        copy_v3_v3(normals.span[i], data.vertex_normals[i]);
-      }
-    }
-    normals.finish();
-    BKE_mesh_set_custom_normals_from_verts(mesh, vertex_normals);
-    MEM_freeN(vertex_normals);
+    BKE_mesh_set_custom_normals_from_verts(
+        mesh, reinterpret_cast<float(*)[3]>(data.vertex_normals.data()));
   }
 
   /* Merge all vertices on the same location. */
