efc2e5134f5d179e3b02b73e125e751d0f331969
Hans Goudey <h.goudey@me.com>
2023-02-16 16:59:58 -0500

    Fix #104841: Split function for Cycles for sharp edges ignores attribute
    Cycles uses the "split faces" mesh function to support sharp edges
    and auto-smooth. However, 75ad8da1ea6799e3983e updated that
    function to ignore the edges that are explicitly tagged as sharp and
    only use the edge angle. Fix by taking the attribute into account too.

--- source/blender/editors/mesh/mesh_data.cc
+++ source/blender/editors/mesh/mesh_data.cc
@@ -1534,8 +1534,13 @@ void ED_mesh_split_faces(Mesh *mesh)
   const Span<MPoly> polys = mesh->polys();
   const Span<MLoop> loops = mesh->loops();
   const float split_angle = (mesh->flag & ME_AUTOSMOOTH) != 0 ? mesh->smoothresh : float(M_PI);
+  const bke::AttributeAccessor attributes = mesh->attributes();
+  const VArray<bool> mesh_sharp_edges = attributes.lookup_or_default<bool>(
+      "sharp_edge", ATTR_DOMAIN_EDGE, false);
 
-  Array<bool> sharp_edges(mesh->totedge, false);
+  Array<bool> sharp_edges(mesh->totedge);
+  mesh_sharp_edges.materialize(sharp_edges);
+
   BKE_edges_sharp_from_angle_set(mesh->totedge,
                                  loops.data(),
                                  loops.size(),
