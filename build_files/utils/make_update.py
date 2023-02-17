867bee6125b8eb59e612f00f03e338fd2fe73ae6
Sergey Sharybin <sergey@noreply.localhost>
2023-02-17 13:42:15 +0100

    Make update: Add --architecture command line attribute
    Possible values are x86_64 and arm64.
    
    Allows to use make_update.py in a cross-compile environment, like
    building x86_64 macOS Blender from Apple Silicon machine.
    
    Pull Request #104863

--- build_files/utils/make_update.py
+++ build_files/utils/make_update.py
@@ -44,6 +44,8 @@ def parse_arguments():
     parser.add_argument("--use-centos-libraries", action="store_true")
     parser.add_argument("--use-linux-libraries", action="store_true")
 
+    parser.add_argument("--architecture", type=str, choices=("x86_64", "arm64",))
+
     return parser.parse_args()
 
 
@@ -51,6 +53,16 @@ def get_blender_git_root():
     return check_output([args.git_command, "rev-parse", "--show-toplevel"])
 
 # Setup for precompiled libraries and tests from svn.
+
+def get_effective_architecture(args):
+    if args.architecture:
+        return args.architecture
+
+    # Check platform.version to detect arm64 with x86_64 python binary.
+    if "ARM64" in platform.version():
+        return "arm64"
+
+    return platform.machine().lower()
 
 
 def svn_update(args, release_version):
@@ -60,11 +72,11 @@ def svn_update(args, release_version):
     svn_url = make_utils.svn_libraries_base_url(release_version, args.svn_branch)
 
     # Checkout precompiled libraries
+    architecture = get_effective_architecture(args)
     if sys.platform == 'darwin':
-        # Check platform.version to detect arm64 with x86_64 python binary.
-        if platform.machine() == 'arm64' or ('ARM64' in platform.version()):
+        if architecture == 'arm64':
             lib_platform = "darwin_arm64"
-        elif platform.machine() == 'x86_64':
+        elif architecture == 'x86_64':
             lib_platform = "darwin"
         else:
             lib_platform = None
