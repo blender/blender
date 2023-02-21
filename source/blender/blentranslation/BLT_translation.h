ece43dd716e78f3dbc1644bad9fd69de4e692edd
Chris Blackbourn <chrisbblend@gmail.com>
2023-02-21 17:07:02 +1300

    Cleanup: format

--- source/blender/blentranslation/BLT_translation.h
+++ source/blender/blentranslation/BLT_translation.h
@@ -126,7 +126,7 @@ const char *BLT_translate_do_new_dataname(const char *msgctxt, const char *msgid
 #define BLT_I18NCONTEXT_EDITOR_VIEW3D "View3D"
 #define BLT_I18NCONTEXT_EDITOR_FILEBROWSER "File browser"
 
-  /* Generic contexts. */
+/* Generic contexts. */
 #define BLT_I18NCONTEXT_VIRTUAL_REALITY "Virtual reality"
 #define BLT_I18NCONTEXT_CONSTRAINT "Constraint"
 
@@ -194,7 +194,7 @@ typedef struct {
         BLT_I18NCONTEXTS_ITEM(BLT_I18NCONTEXT_ID_WINDOWMANAGER, "id_windowmanager"), \
         BLT_I18NCONTEXTS_ITEM(BLT_I18NCONTEXT_EDITOR_VIEW3D, "editor_view3d"), \
         BLT_I18NCONTEXTS_ITEM(BLT_I18NCONTEXT_EDITOR_FILEBROWSER, "editor_filebrowser"), \
-        BLT_I18NCONTEXTS_ITEM(BLT_I18NCONTEXT_VIRTUAL_REALITY, "virtual_reality"),  \
+        BLT_I18NCONTEXTS_ITEM(BLT_I18NCONTEXT_VIRTUAL_REALITY, "virtual_reality"), \
         BLT_I18NCONTEXTS_ITEM(BLT_I18NCONTEXT_CONSTRAINT, "constraint"), \
     { \
       NULL, NULL, NULL \
