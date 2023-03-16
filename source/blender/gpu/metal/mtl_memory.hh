7bdd82eca0ba1d8135b4c932ed1eac993788640b
Jason Fielder <jason-fielder@noreply.localhost>
2023-03-16 08:25:15 +0100

    Metal: Resolve Race Condition in Memory Manager
    Fix race condition if several competing threads are inserting Metal
    buffers into the MTLSafeFreeList simultaneously while a new list
    chunk is being created.
    
    Also raise the limit for an MTLSafeFreeListChunk size to optimize
    for interactivity when releasing lots of memory simultaneously.
    
    Authored by Apple: Michael Parkin-White
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/105254

--- source/blender/gpu/metal/mtl_memory.hh
+++ source/blender/gpu/metal/mtl_memory.hh
@@ -287,17 +287,17 @@ class MTLSafeFreeList {
   std::atomic<bool> in_free_queue_;
   std::atomic<bool> referenced_by_workload_;
   std::recursive_mutex lock_;
-
   /* Linked list of next MTLSafeFreeList chunk if current chunk is full. */
-  std::atomic<int> has_next_pool_;
   std::atomic<MTLSafeFreeList *> next_;
 
   /* Lockless list. MAX_NUM_BUFFERS_ within a chunk based on considerations
-   * for performance and memory.
+   * for performance and memory. Higher chunk counts are preferable for efficiently
+   * performing block operations such as copying several objects simultaneously.
+   *
    * MIN_BUFFER_FLUSH_COUNT refers to the minimum count of buffers in the MTLSafeFreeList
    * before buffers are returned to global memory pool. This is set at a point to reduce
    * overhead of small pool flushes, while ensuring floating memory overhead is not excessive. */
-  static const int MAX_NUM_BUFFERS_ = 1024;
+  static const int MAX_NUM_BUFFERS_ = 8192;
   static const int MIN_BUFFER_FLUSH_COUNT = 120;
   std::atomic<int> current_list_index_;
   gpu::MTLBuffer *safe_free_pool_[MAX_NUM_BUFFERS_];
@@ -305,8 +305,8 @@ class MTLSafeFreeList {
  public:
   MTLSafeFreeList();
 
-  /* Add buffer to Safe Free List, can be called from secondary threads.
-   * Performs a lockless list insert. */
+  /* Can be used from multiple threads. Performs insertion into Safe Free List with the least
+   * amount of threading synchronization. */
   void insert_buffer(gpu::MTLBuffer *buffer);
 
   /* Whether we need to start a new safe free list, or can carry on using the existing one. */
@@ -321,10 +321,11 @@ class MTLSafeFreeList {
   void flag_in_queue()
   {
     in_free_queue_ = true;
-    if (has_next_pool_) {
+    if (current_list_index_ >= MTLSafeFreeList::MAX_NUM_BUFFERS_) {
       MTLSafeFreeList *next_pool = next_.load();
-      BLI_assert(next_pool != nullptr);
-      next_pool->flag_in_queue();
+      if (next_pool) {
+        next_pool->flag_in_queue();
+      }
     }
   }
 };
