10b03e66b98feb8f077ee2948e3eaf262c062eb2
Campbell Barton <campbell@blender.org>
2023-03-28 13:09:54 +1100

    Cleanup: remove unreachable code

--- source/blender/blenkernel/intern/tracking.cc
+++ source/blender/blenkernel/intern/tracking.cc
@@ -1834,8 +1834,6 @@ MovieTrackingPlaneMarker *BKE_tracking_plane_marker_get(MovieTrackingPlaneTrack 
 
   /* If there's no marker for exact position, use nearest marker from left side. */
   return &plane_track->markers[a];
-
-  return nullptr;
 }
 
 MovieTrackingPlaneMarker *BKE_tracking_plane_marker_get_exact(MovieTrackingPlaneTrack *plane_track,
