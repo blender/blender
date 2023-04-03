1205111fe9a8db4e66d57b7db52cf0fd88ed2f09
Weizhen Huang <weizhen@blender.org>
2023-04-03 10:54:17 +0200

    Refactor: move #object_usable_as_light() from #LightManager class to #Object
    so that the function can also be used by other classes.
    Also change `!bounds.valid()` to `!is_traceable()` so zero-sized objects
    are skipped.

--- intern/cycles/scene/object.cpp
+++ intern/cycles/scene/object.cpp
@@ -370,6 +370,34 @@ int Object::get_device_index() const
   return index;
 }
 
+bool Object::usable_as_light() const
+{
+  Geometry *geom = get_geometry();
+  if (!geom->is_mesh() && !geom->is_volume()) {
+    return false;
+  }
+  /* Skip non-traceable objects. */
+  if (!is_traceable()) {
+    return false;
+  }
+  /* Skip if we are not visible for BSDFs. */
+  if (!(get_visibility() & (PATH_RAY_DIFFUSE | PATH_RAY_GLOSSY | PATH_RAY_TRANSMIT))) {
+    return false;
+  }
+  /* Skip if we have no emission shaders. */
+  /* TODO(sergey): Ideally we want to avoid such duplicated loop, since it'll
+   * iterate all geometry shaders twice (when counting and when calculating
+   * triangle area.
+   */
+  foreach (Node *node, geom->get_used_shaders()) {
+    Shader *shader = static_cast<Shader *>(node);
+    if (shader->emission_sampling != EMISSION_SAMPLING_NONE) {
+      return true;
+    }
+  }
+  return false;
+}
+
 /* Object Manager */
 
 ObjectManager::ObjectManager()
