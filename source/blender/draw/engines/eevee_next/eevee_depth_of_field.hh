1ae54bb4fdc781edfbc2f33af40f30e6a951629b
Omar Emara <mail@OmarEmara.dev>
2023-04-04 18:20:44 +0200

    Fix MSVC C1001 error after texture sampler refactor
    This patch fixes the MSVC C1001 error that was introduced after
    ff3b2226fb202db4c2f77f7d52f578529b38f35b.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/106554

--- source/blender/draw/engines/eevee_next/eevee_depth_of_field.hh
+++ source/blender/draw/engines/eevee_next/eevee_depth_of_field.hh
@@ -44,11 +44,6 @@ class DepthOfField {
  private:
   class Instance &inst_;
 
-  /** Samplers */
-  static constexpr GPUSamplerState gather_bilinear = {GPU_SAMPLER_FILTERING_MIPMAP |
-                                                      GPU_SAMPLER_FILTERING_LINEAR};
-  static constexpr GPUSamplerState gather_nearest = {GPU_SAMPLER_FILTERING_MIPMAP};
-
   /** Input/Output texture references. */
   GPUTexture *input_color_tx_ = nullptr;
   GPUTexture *output_color_tx_ = nullptr;
