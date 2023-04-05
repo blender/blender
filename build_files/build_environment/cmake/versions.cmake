8216623e250beb7252d4afe6c6064f367803cc5a
Campbell Barton <campbell@blender.org>
2023-04-05 13:57:20 +1000

    Build: bump wayland to v1.22
    Use the latest Wayland version, resolves inverted track-pad scroll
    direction #104272 by adding support for physical scroll direction.
    Updates to GHOST/Wayland have already been made.

--- build_files/build_environment/cmake/versions.cmake
+++ build_files/build_environment/cmake/versions.cmake
@@ -534,10 +534,10 @@ set(WL_PROTOCOLS_URI https://gitlab.freedesktop.org/wayland/wayland-protocols/-/
 set(WL_PROTOCOLS_HASH a28ff59a56e2ebb746048b6ef8d931d6)
 set(WL_PROTOCOLS_HASH_TYPE MD5)
 
-set(WAYLAND_VERSION 1.21.0)
+set(WAYLAND_VERSION 1.22.0)
 set(WAYLAND_FILE wayland-${WAYLAND_VERSION}.tar.xz)
-set(WAYLAND_URI https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.21.0/downloads/wayland-${WAYLAND_VERSION}.tar.xz)
-set(WAYLAND_HASH f2653a2293bcd882d756c6a83d278903)
+set(WAYLAND_URI https://gitlab.freedesktop.org/wayland/wayland/-/releases/${WAYLAND_VERSION}/downloads/wayland-${WAYLAND_VERSION}.tar.xz)
+set(WAYLAND_HASH 7410ab549e3928fce9381455b17b0803)
 set(WAYLAND_HASH_TYPE MD5)
 
 set(WAYLAND_LIBDECOR_VERSION 0.1.0)
