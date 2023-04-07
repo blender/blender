984da82283455ab4a4b4283bba4b892cdedad7df
Chris Blackbourn <chrisbblend@gmail.com>
2023-04-07 12:58:46 +1200

    Cleanup: format

--- source/blender/bmesh/intern/bmesh_query_uv.cc
+++ source/blender/bmesh/intern/bmesh_query_uv.cc
@@ -46,8 +46,7 @@ BMUVOffsets BM_uv_map_get_offsets_n(const BMesh *bm, const int layer)
 
 BMUVOffsets BM_uv_map_get_offsets(const BMesh *bm)
 {
-  return BM_uv_map_get_offsets_n(bm,
-                                 CustomData_get_active_layer(&bm->ldata, CD_PROP_FLOAT2));
+  return BM_uv_map_get_offsets_n(bm, CustomData_get_active_layer(&bm->ldata, CD_PROP_FLOAT2));
 }
 
 static void uv_aspect(const BMLoop *l,
