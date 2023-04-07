253b4172257a2e78db67157d9421b44c3b78e8e1
Julian Eisel <julian@blender.org>
2023-04-07 11:38:37 +0200

    Add theme options for the asset shelf

--- source/blender/blenloader/intern/versioning_userdef.c
+++ source/blender/blenloader/intern/versioning_userdef.c
@@ -93,6 +93,11 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(space_view3d.face_retopology);
   }
 
+  if (!USER_VERSION_ATLEAST(306, 5)) {
+    FROM_DEFAULT_V4_UCHAR(space_view3d.asset_shelf.back);
+    FROM_DEFAULT_V4_UCHAR(space_view3d.asset_shelf.header_back);
+  }
+
   /**
    * Versioning code until next subversion bump goes here.
    *
