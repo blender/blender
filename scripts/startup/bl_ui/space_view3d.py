f8108d6dfd6d0b8c4e3fccfd8abce421d8836697
zanqdo <zanqdo@gmail.com>
2023-04-11 21:32:29 +0200

    UI: Rename Bright/Contrast to Brightness/Contrast
    Rename *Bright/Contrast* to *Brightness/Contrast* in order to avoid the use of shortened names and improve consistency within Blender and with industry conventions.
    
    Reasoning:
    
    The modified color characteristic is called *brightness*, not *bright*. You don't modify the *bright* of an image.
    
    This also interferes with search in case someone searches for brightness, producing no results.
    
    *Note: This patch should only touch UI names which do not need versioning. It leaves the actual property name in nodes for a future breaking release.*
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/104998

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -1962,7 +1962,7 @@ class VIEW3D_MT_paint_gpencil(Menu):
         layout.operator("gpencil.vertex_color_invert", text="Invert")
         layout.operator("gpencil.vertex_color_levels", text="Levels")
         layout.operator("gpencil.vertex_color_hsv", text="Hue Saturation Value")
-        layout.operator("gpencil.vertex_color_brightness_contrast", text="Bright/Contrast")
+        layout.operator("gpencil.vertex_color_brightness_contrast", text="Brightness/Contrast")
 
 
 class VIEW3D_MT_select_gpencil(Menu):
@@ -3060,7 +3060,7 @@ class VIEW3D_MT_paint_vertex(Menu):
         layout.operator("paint.vertex_color_invert", text="Invert")
         layout.operator("paint.vertex_color_levels", text="Levels")
         layout.operator("paint.vertex_color_hsv", text="Hue Saturation Value")
-        layout.operator("paint.vertex_color_brightness_contrast", text="Bright/Contrast")
+        layout.operator("paint.vertex_color_brightness_contrast", text="Brightness/Contrast")
 
 
 class VIEW3D_MT_hook(Menu):
