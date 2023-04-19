9344deed8981cbb1d8ac9488b552708083beae12
zanqdo <zanqdo@gmail.com>
2023-04-19 21:52:20 +0200

    UI: Change the name of Invert nodes to Invert Color
    The nodes for inverting a color are named simply Invert, which begs the question: invert what?
    
    This patch changes the naming for the node in Shading, Texture and Compositing nodes to *Invert Color*
    
    This matches the naming of other color dedicated nodes like Separate Color or Combine Color
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/106750

--- source/blender/nodes/texture/nodes/node_texture_invert.cc
+++ source/blender/nodes/texture/nodes/node_texture_invert.cc
@@ -47,7 +47,7 @@ void register_node_type_tex_invert()
 {
   static bNodeType ntype;
 
-  tex_node_type_base(&ntype, TEX_NODE_INVERT, "Invert", NODE_CLASS_OP_COLOR);
+  tex_node_type_base(&ntype, TEX_NODE_INVERT, "Invert Color", NODE_CLASS_OP_COLOR);
   node_type_socket_templates(&ntype, inputs, outputs);
   ntype.exec_fn = exec;
 
