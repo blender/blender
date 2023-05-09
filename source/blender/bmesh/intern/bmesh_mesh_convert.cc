3f44b24cf2efb8efa4d5029818b21aac122230d1
Hans Goudey <h.goudey@me.com>
2023-05-09 12:54:55 -0400

    BLI: Use inline instead of static for bounds functions
    Avoid instantiating the templates separately in every translation unit.
    This saves 20 KB in my Blender binary. Also remove a timer mistakenly
    committed.

--- source/blender/bmesh/intern/bmesh_mesh_convert.cc
+++ source/blender/bmesh/intern/bmesh_mesh_convert.cc
@@ -1613,7 +1613,6 @@ void BM_mesh_bm_to_me(Main *bmain, BMesh *bm, Mesh *me, const struct BMeshToMesh
 
 void BM_mesh_bm_to_me_for_eval(BMesh *bm, Mesh *me, const CustomData_MeshMasks *cd_mask_extra)
 {
-  SCOPED_TIMER_AVERAGED(__func__);
   /* NOTE: The function is called from multiple threads with the same input BMesh and different
    * mesh objects. */
 
