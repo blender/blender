4436909c1d9ba790189f91a44c601ef326c4757e
Clément Foucault <foucault.clem@gmail.com>
2023-05-09 14:37:13 +0200

    Workbench-Next: Fix shader compilation
    Was a missing case from recent fix:
    aa31d9be80c832bdaaec44c38cb5ac80d6faaeca

--- source/blender/draw/engines/workbench/shaders/workbench_shadow_caps_geom.glsl
+++ source/blender/draw/engines/workbench/shaders/workbench_shadow_caps_geom.glsl
@@ -34,7 +34,11 @@ void main()
   vec3 v12 = vData[2].pos - vData[1].pos;
 
   vec3 n = cross(v12, v10);
+#ifdef WORKBENCH_NEXT
+  float facing = dot(n, vData[0].light_direction_os);
+#else
   float facing = dot(n, lightDirection);
+#endif
 
   bool backface = facing > 0.0;
 
