24d3d815e3664abd73d66bd1f61d964506e8723e
Julian Eisel <julian@blender.org>
2023-05-10 19:01:58 +0200

    Update version bump

--- source/blender/blenloader/intern/versioning_userdef.c
+++ source/blender/blenloader/intern/versioning_userdef.c
@@ -102,11 +102,11 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(space_action.simulated_frames);
   }
 
-  if (!USER_VERSION_ATLEAST(306, 6)) {
+  if (!USER_VERSION_ATLEAST(306, 9)) {
     FROM_DEFAULT_V4_UCHAR(space_view3d.asset_shelf.back);
     FROM_DEFAULT_V4_UCHAR(space_view3d.asset_shelf.header_back);
   }
-  
+
   /**
    * Versioning code until next subversion bump goes here.
    *
