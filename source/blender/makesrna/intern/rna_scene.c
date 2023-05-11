061712fcc61143a42249a7dd5863f7690e3b5544
Kevin C. Burke <kevincburke@noreply.localhost>
2023-05-11 14:50:15 +0200

    Add ORIENTATION_PARENT icon
    Add ORIENTATION_PARENT icon for Parent Orientation enum
    
    The Parent orientation was introduced in 5c4b0c98d37d7fa1d7248217af0d0cb32f63c9cf.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/107763

--- source/blender/makesrna/intern/rna_scene.c
+++ source/blender/makesrna/intern/rna_scene.c
@@ -630,7 +630,7 @@ const EnumPropertyItem rna_enum_transform_orientation_items[] = {
      "Align the transformation axes to the 3D cursor"},
     {V3D_ORIENT_PARENT,
      "PARENT",
-     ICON_BLANK1,
+     ICON_ORIENTATION_PARENT,
      "Parent",
      "Align the transformation axes to the object's parent space"},
     // {V3D_ORIENT_CUSTOM, "CUSTOM", 0, "Custom", "Use a custom transform orientation"},
