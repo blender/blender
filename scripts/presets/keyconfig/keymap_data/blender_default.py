0f67ac45471d51521a21b66a3f87f81aef35d96d
Nate Rupsis <nrupsis@gmail.com>
2023-05-18 22:06:19 +0200

    Animation: Update NLA "TAB" tweak mode to be full stack evaluation
    This PR updates the key mapping (TAB) for Tweak mode (Lower stack) to Full stack for the NLA editor
    
    [Issue](https://projects.blender.org/blender/blender/issues/108026)
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/108050

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -2497,7 +2497,7 @@ def km_nla_generic(_params):
             sidebar_key={"type": 'N', "value": 'PRESS'},
         ),
         ("nla.tweakmode_enter", {"type": 'TAB', "value": 'PRESS'},
-         {"properties": [("use_upper_stack_evaluation", False)]}),
+         {"properties": [("use_upper_stack_evaluation", True)]}),
         ("nla.tweakmode_exit", {"type": 'TAB', "value": 'PRESS'}, None),
         ("nla.tweakmode_enter", {"type": 'TAB', "value": 'PRESS', "shift": True},
          {"properties": [("isolate_action", True)]}),
