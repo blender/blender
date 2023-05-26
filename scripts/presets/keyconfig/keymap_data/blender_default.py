adef3a4556ac3448392f68bb6f9ae9dd3d4406df
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-05-26 09:04:00 +0200

    Animation: Remove collection hotkeys from pose mode
    We have been chatting in the Animation&Rigging module meeting that the collection hotkeys (1, 2, 3 etc.) in Pose Mode are unwanted and could be replaced with something more useful.
    This patch only removes the hotkeys, we can later decide what should be in their place.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/105120

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -4581,7 +4581,6 @@ def km_pose(params):
         ("pose.breakdown", {"type": 'E', "value": 'PRESS', "shift": True}, None),
         ("pose.blend_to_neighbor", {"type": 'E', "value": 'PRESS', "shift": True, "alt": True}, None),
         op_menu("VIEW3D_MT_pose_propagate", {"type": 'P', "value": 'PRESS', "alt": True}),
-        *_template_object_hide_collection_from_number_keys(),
         *_template_items_context_menu("VIEW3D_MT_pose_context_menu", params.context_menu_event),
     ])
 
