179a2421ca1b62564a8dd72edec09840189efb1c
Julian Eisel <julian@blender.org>
2023-06-08 17:34:34 +0200

    Refactor: Better context override for text button activate API function
    No user visible change expected.
    
    It wasn't clear that the caller of `UI_textbutton_activate_rna()` would
    have to override the context region for it to work correctly. This is
    only the case because UI internals happen to require it.
    
    Instead of implicitly requiring the caller to do the context override so
    UI internals work, do the override inside of the API function in
    question.

--- source/blender/editors/space_file/file_ops.c
+++ source/blender/editors/space_file/file_ops.c
@@ -3210,18 +3210,13 @@ static int file_start_filter_exec(bContext *C, wmOperator *UNUSED(op))
   const SpaceFile *sfile = CTX_wm_space_file(C);
   const FileSelectParams *params = ED_fileselect_get_active_params(sfile);
 
-  ARegion *region_ctx = CTX_wm_region(C);
-
   if (area) {
     LISTBASE_FOREACH (ARegion *, region, &area->regionbase) {
-      CTX_wm_region_set(C, region);
       if (UI_textbutton_activate_rna(C, region, params, "filter_search")) {
         break;
       }
     }
   }
-
-  CTX_wm_region_set(C, region_ctx);
 
   return OPERATOR_FINISHED;
 }
@@ -3251,18 +3246,13 @@ static int file_edit_directory_path_exec(bContext *C, wmOperator *UNUSED(op))
   const SpaceFile *sfile = CTX_wm_space_file(C);
   const FileSelectParams *params = ED_fileselect_get_active_params(sfile);
 
-  ARegion *region_ctx = CTX_wm_region(C);
-
   if (area) {
     LISTBASE_FOREACH (ARegion *, region, &area->regionbase) {
-      CTX_wm_region_set(C, region);
       if (UI_textbutton_activate_rna(C, region, params, "directory")) {
         break;
       }
     }
   }
-
-  CTX_wm_region_set(C, region_ctx);
 
   return OPERATOR_FINISHED;
 }
