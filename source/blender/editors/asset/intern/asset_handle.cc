133c3953360b7ee2529f4a4f4521bad79f5ee11f
Julian Eisel <julian@blender.org>
2023-06-08 17:18:13 +0200

    Fix string buffer size for asset library path query
    `FILE_MAX` is enough here, since the returned string will only be the
    path to the .blend file, not the full path into the .blend library.

--- source/blender/editors/asset/intern/asset_handle.cc
+++ source/blender/editors/asset/intern/asset_handle.cc
@@ -58,7 +58,7 @@ std::optional<eAssetImportMethod> ED_asset_handle_get_import_method(
 }
 
 void ED_asset_handle_get_full_library_path(const AssetHandle *asset_handle,
-                                           char r_full_lib_path[FILE_MAX_LIBEXTRA])
+                                           char r_full_lib_path[FILE_MAX])
 {
   *r_full_lib_path = '\0';
 
@@ -68,7 +68,7 @@ void ED_asset_handle_get_full_library_path(const AssetHandle *asset_handle,
     return;
   }
 
-  BLI_strncpy(r_full_lib_path, library_path.c_str(), FILE_MAX_LIBEXTRA);
+  BLI_strncpy(r_full_lib_path, library_path.c_str(), FILE_MAX);
 }
 
 bool ED_asset_handle_get_use_relative_path(const AssetHandle *asset)
