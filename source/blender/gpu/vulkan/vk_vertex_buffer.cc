db4cd61001ca255faa0fae921d3e3a0f55cc76ff
Jeroen Bakker <jeroen@blender.org>
2023-06-08 09:46:00 +0200

    Vulkan: Unbind Uniform Buffers When Destroyed
    When Uniform buffers are destroyed they need to be removed from the
    state manager. Otherwise Vulkan points to unallocated memory and leads
    to artifacts.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/108742

--- source/blender/gpu/vulkan/vk_vertex_buffer.cc
+++ source/blender/gpu/vulkan/vk_vertex_buffer.cc
@@ -119,9 +119,9 @@ void VKVertexBuffer::resize_data()
 
 void VKVertexBuffer::release_data()
 {
-  if (should_unbind_) {
-    VKContext &context = *VKContext::get();
-    context.state_manager_get().texel_buffer_unbind(this);
+  VKContext *context = VKContext::get();
+  if (should_unbind_ && context) {
+    context->state_manager_get().texel_buffer_unbind(this);
   }
 
   if (vk_buffer_view_ != VK_NULL_HANDLE) {
