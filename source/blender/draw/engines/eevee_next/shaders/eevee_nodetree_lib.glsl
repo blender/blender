dd044afd1e83656ea2f60b93754d5488ebc9d03f
Jeroen Bakker <jeroen@blender.org>
2023-06-12 13:25:57 +0200

    Vulkan: Fix Shader Compilation Errors
    It is not allowed to add an extra semicolon after a function body.
    
    ```
    eevee_nodetree_lib.glsl:259:0: Error: 'extraneous semicolon' : not
    supported for this version or the enabled extensions
    ```
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/108886

--- source/blender/draw/engines/eevee_next/shaders/eevee_nodetree_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_nodetree_lib.glsl
@@ -246,7 +246,7 @@ void output_renderpass_color(int id, vec4 color)
     imageStore(rp_color_img, ivec3(texel, id), color);
   }
 #endif
-};
+}
 
 void output_renderpass_value(int id, float value)
 {
@@ -256,7 +256,7 @@ void output_renderpass_value(int id, float value)
     imageStore(rp_value_img, ivec3(texel, id), vec4(value));
   }
 #endif
-};
+}
 
 void clear_aovs()
 {
