a05dfdbadfacd4a26d87c2ae1d10d6bf6f6d75f4
Jeroen Bakker <jeroen@blender.org>
2023-06-12 08:16:30 +0200

    Cleanup: Make format

--- source/blender/editors/sculpt_paint/grease_pencil_paint.cc
+++ source/blender/editors/sculpt_paint/grease_pencil_paint.cc
@@ -139,7 +139,7 @@ void PaintOperation::on_stroke_done(const bContext &C)
   int material_index = 0;
   SpanAttributeWriter<int> materials = attributes.lookup_or_add_for_write_span<int>(
       "material_index", ATTR_DOMAIN_CURVE);
-  
+
   materials.span.slice(new_curves_range).fill(material_index);
 
   /* Set curve_type attribute. */
