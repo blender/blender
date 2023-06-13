blender/blender

commit a958ae36e8cbe687c77f77b375a2dd21c01d271d

Fix #104305: Crash in node editor with large asset libraries
Various UI code would store the `AssetHandle` in a way that turns out to
be unsafe. The file-data is part of the file browser caching system that
releases file-data when a certain maximum of items is in the cache. So
even while just iterating over the assets, earlier iterated asset
handles may become invalid. Now asset handles are really treated as
volatile, short lived objects.

For the asset-view, the fix was more involved. There we need an RNA
collection of asset-handles, because the UI list code requires that. So
we create a dummy collection and get the asset handles as needed by
index. This again meant that I had to keep the index of the collection
and the asset-list in sync, so all filtering had to be moved to the UI
list.
I tried duplicating the file-data out of the cache instead, but that
caused problems with managing the memory/ownership of the preview
images.

`AssetHandle` should be removed and replaced by `AssetRepresentation`,
but this would be an even more disruptive change (breaking API
compatibility too).

Fixes #104305, #105535.

Pull Request: #105773

source/blender/editors/interface/interface_template_list.cc
@@ -8,9 +8,11 @@
 #include <cstring>
 
 #include "BLI_fnmatch.h"
+#include "BLI_function_ref.hh"
 #include "BLI_listbase.h"
 #include "BLI_math_base.h"
 #include "BLI_string.h"
+#include "BLI_string_ref.hh"
 #include "BLI_utildefines.h"
 
 #include "BKE_screen.h"
@@ -26,11 +28,14 @@
 #include "RNA_prototypes.h"
 
 #include "UI_interface.h"
+#include "UI_interface.hh"
 #include "UI_view2d.h"
 
 #include "WM_api.h"
 
 #include "interface_intern.hh"
+
+using namespace blender;
 
 /**
  * The validated data that was passed to #uiTemplateList (typically through Python).
@@ -148,6 +153,45 @@ static void uilist_draw_filter_default(struct uiList *ui_list,
   }
 }
 
+uiListNameFilter::uiListNameFilter(uiList &list)
+{
+  const char *filter_raw = list.filter_byname;
+
+  if (filter_raw[0]) {
+    const size_t slen = strlen(filter_raw);
+
+    /* Implicitly add heading/trailing wildcards if needed. */
+    if (slen + 3 <= sizeof(storage_.filter_buff)) {
+      filter_ = storage_.filter_buff;
+    }
+    else {
+      filter_ = storage_.filter_dyn = static_cast<char *>(
+          MEM_mallocN((slen + 3) * sizeof(char), "filter_dyn"));
+    }
+    BLI_strncpy_ensure_pad(filter_, filter_raw, '*', slen + 3);
+  }
+}
+
+uiListNameFilter::~uiListNameFilter()
+{
+  MEM_SAFE_FREE(storage_.filter_dyn);
+}
+
+eUIListFilterResult uiListNameFilter::operator()(const PointerRNA & /* itemptr */,
+                                                 StringRefNull name,
+                                                 int /* index */)
+{
+  if (!filter_) {
+    return UI_LIST_ITEM_FILTER_MATCHES;
+  }
+
+  /* Case-insensitive! */
+  if (fnmatch(filter_, name.c_str(), FNM_CASEFOLD) == 0) {
+    return UI_LIST_ITEM_FILTER_MATCHES;
+  }
+  return UI_LIST_ITEM_FILTER_MISMATCHES;
+}
+
 struct StringCmp {
   char name[MAX_IDPROP_NAME];
   int org_idx;
@@ -159,16 +203,16 @@ static int cmpstringp(const void *p1, const void *p2)
   return BLI_strcasecmp(((StringCmp *)p1)->name, ((StringCmp *)p2)->name);
 }
 
-static void uilist_filter_items_default(struct uiList *ui_list,
-                                        const struct bContext * /*C*/,
-                                        struct PointerRNA *dataptr,
-                                        const char *propname)
+void UI_list_filter_and_sort_items(uiList *ui_list,
+                                   const bContext * /*C*/,
+                                   uiListItemFilterFn item_filter_fn,
+                                   PointerRNA *dataptr,
+                                   const char *propname,
+                                   uiListItemGetNameFn get_name_fn)
 {
   uiListDyn *dyn_data = ui_list->dyn_data;
   PropertyRNA *prop = RNA_struct_find_property(dataptr, propname);
 
-  const char *filter_raw = ui_list->filter_byname;
-  char *filter = (char *)filter_raw, filter_buff[32], *filter_dyn = nullptr;
   const bool filter_exclude = (ui_list->filter_flag & UILST_FLT_EXCLUDE) != 0;
   const bool order_by_name = (ui_list->filter_sort_flag & UILST_FLT_SORT_MASK) ==
                              UILST_FLT_SORT_ALPHA;
@@ -176,41 +220,26 @@ static void uilist_filter_items_default(struct uiList *ui_list,
 
   dyn_data->items_shown = dyn_data->items_len = len;
 
-  if (len && (order_by_name || filter_raw[0])) {
+  if (len && (order_by_name || item_filter_fn)) {
     StringCmp *names = nullptr;
     int order_idx = 0, i = 0;
 
     if (order_by_name) {
       names = static_cast<StringCmp *>(MEM_callocN(sizeof(StringCmp) * len, "StringCmp"));
     }
-    if (filter_raw[0]) {
-      const size_t slen = strlen(filter_raw);
 
+    if (item_filter_fn) {
       dyn_data->items_filter_flags = static_cast<int *>(
           MEM_callocN(sizeof(int) * len, "items_filter_flags"));
       dyn_data->items_shown = 0;
-
-      /* Implicitly add heading/trailing wildcards if needed. */
-      if (slen + 3 <= sizeof(filter_buff)) {
-        filter = filter_buff;
-      }
-      else {
-        filter = filter_dyn = static_cast<char *>(
-            MEM_mallocN((slen + 3) * sizeof(char), "filter_dyn"));
-      }
-      BLI_strncpy_ensure_pad(filter, filter_raw, '*', slen + 3);
     }
 
     RNA_PROP_BEGIN (dataptr, itemptr, prop) {
       bool do_order = false;
 
       char *namebuf;
-      if (RNA_struct_is_a(itemptr.type, &RNA_AssetHandle)) {
-        /* XXX The AssetHandle design is hacky and meant to be temporary. It can't have a proper
-         * name property, so for now this hardcoded exception is needed. */
-        AssetHandle *asset_handle = (AssetHandle *)itemptr.data;
-        const char *asset_name = ED_asset_handle_get_name(asset_handle);
-        namebuf = BLI_strdup(asset_name);
+      if (get_name_fn) {
+        namebuf = BLI_strdup(get_name_fn(itemptr, i).c_str());
       }
       else {
         namebuf = RNA_struct_name_get_alloc(&itemptr, nullptr, 0, nullptr);
@@ -218,9 +247,13 @@ static void uilist_filter_items_default(struct uiList *ui_list,
 
       const char *name = namebuf ? namebuf : "";
 
-      if (filter[0]) {
-        /* Case-insensitive! */
-        if (fnmatch(filter, name, FNM_CASEFOLD) == 0) {
+      if (item_filter_fn) {
+        const eUIListFilterResult filter_result = item_filter_fn(itemptr, name, i);
+
+        if (filter_result == UI_LIST_ITEM_NEVER_SHOW) {
+          /* Pass. */
+        }
+        else if (filter_result == UI_LIST_ITEM_FILTER_MATCHES) {
           dyn_data->items_filter_flags[i] = UILST_FLT_ITEM;
           if (!filter_exclude) {
             dyn_data->items_shown++;
@@ -266,12 +299,27 @@ static void uilist_filter_items_default(struct uiList *ui_list,
       }
     }
 
-    if (filter_dyn) {
-      MEM_freeN(filter_dyn);
-    }
     if (names) {
       MEM_freeN(names);
     }
+  }
+}
+
+/**
+ * Default UI List filtering: Filter by name.
+ */
+static void uilist_filter_items_default(struct uiList *ui_list,
+                                        const struct bContext *C,
+                                        struct PointerRNA *dataptr,
+                                        const char *propname)
+{
+  if (ui_list->filter_byname[0]) {
+    uiListNameFilter name_filter(*ui_list);
+    UI_list_filter_and_sort_items(ui_list, C, name_filter, dataptr, propname);
+  }
+  /* Optimization: Skip filtering entirely when there is no filter string set. */
+  else {
+    UI_list_filter_and_sort_items(ui_list, C, nullptr, dataptr, propname);
   }
 }
 

source/blender/asset_system/AS_asset_representation.h
@@ -21,6 +21,8 @@ const char *AS_asset_representation_name_get(const AssetRepresentation *asset)
     ATTR_WARN_UNUSED_RESULT;
 AssetMetaData *AS_asset_representation_metadata_get(const AssetRepresentation *asset)
     ATTR_WARN_UNUSED_RESULT;
+struct ID *AS_asset_representation_local_id_get(const AssetRepresentation *asset)
+    ATTR_WARN_UNUSED_RESULT;
 bool AS_asset_representation_is_local_id(const AssetRepresentation *asset) ATTR_WARN_UNUSED_RESULT;
 bool AS_asset_representation_is_never_link(const AssetRepresentation *asset)
     ATTR_WARN_UNUSED_RESULT;

source/blender/asset_system/AS_asset_representation.hh
@@ -82,6 +82,9 @@ class AssetRepresentation {
    * #get_import_method(). Also returns true if there is no predefined import method
    * (when #get_import_method() returns no value). */
   bool may_override_import_method() const;
+  /** If this asset is stored inside this current file (#is_local_id() is true), this returns the
+   * ID's pointer, otherwise null. */
+  ID *local_id() const;
   /** Returns if this asset is stored inside this current file, and as such fully editable. */
   bool is_local_id() const;
   const AssetLibrary &owner_asset_library() const;

source/blender/asset_system/intern/asset_representation.cc
@@ -97,6 +97,11 @@ bool AssetRepresentation::may_override_import_method() const
   return owner_asset_library_->may_override_import_method_;
 }
 
+ID *AssetRepresentation::local_id() const
+{
+  return is_local_id_ ? local_asset_id_ : nullptr;
+}
+
 bool AssetRepresentation::is_local_id() const
 {
   return is_local_id_;
@@ -157,6 +162,13 @@ AssetMetaData *AS_asset_representation_metadata_get(const AssetRepresentation *a
   const asset_system::AssetRepresentation *asset =
       reinterpret_cast<const asset_system::AssetRepresentation *>(asset_handle);
   return &asset->get_metadata();
+}
+
+ID *AS_asset_representation_local_id_get(const AssetRepresentation *asset_handle)
+{
+  const asset_system::AssetRepresentation *asset =
+      reinterpret_cast<const asset_system::AssetRepresentation *>(asset_handle);
+  return asset->local_id();
 }
 
 bool AS_asset_representation_is_local_id(const AssetRepresentation *asset_handle)

source/blender/blenkernel/BKE_context.h
@@ -381,6 +381,8 @@ bool CTX_data_editable_gpencil_strokes(const bContext *C, ListBase *list);
 const struct AssetLibraryReference *CTX_wm_asset_library_ref(const bContext *C);
 struct AssetHandle CTX_wm_asset_handle(const bContext *C, bool *r_is_valid);
 
+struct AssetRepresentation *CTX_wm_asset(const bContext *C);
+
 bool CTX_wm_interface_locked(const bContext *C);
 
 /**

source/blender/blenkernel/intern/context.cc
@@ -1493,6 +1493,11 @@ AssetHandle CTX_wm_asset_handle(const bContext *C, bool *r_is_valid)
   return AssetHandle{nullptr};
 }
 
+AssetRepresentation *CTX_wm_asset(const bContext *C)
+{
+  return static_cast<AssetRepresentation *>(ctx_data_pointer_get(C, "asset"));
+}
+
 Depsgraph *CTX_data_depsgraph_pointer(const bContext *C)
 {
   Main *bmain = CTX_data_main(C);

source/blender/editors/asset/CMakeLists.txt
@@ -24,6 +24,7 @@ set(SRC
   intern/asset_catalog.cc
   intern/asset_filter.cc
   intern/asset_handle.cc
+  intern/asset_import.cc
   intern/asset_indexer.cc
   intern/asset_library_reference.cc
   intern/asset_library_reference_enum.cc
@@ -37,6 +38,7 @@ set(SRC
   ED_asset_catalog.hh
   ED_asset_filter.h
   ED_asset_handle.h
+  ED_asset_import.h
   ED_asset_indexer.h
   ED_asset_library.h
   ED_asset_list.h

source/blender/editors/asset/ED_asset_handle.h
@@ -21,6 +21,7 @@ extern "C" {
 
 struct AssetHandle;
 
+struct AssetRepresentation *ED_asset_handle_get_representation(const struct AssetHandle *asset);
 const char *ED_asset_handle_get_name(const struct AssetHandle *asset);
 struct AssetMetaData *ED_asset_handle_get_metadata(const struct AssetHandle *asset);
 struct ID *ED_asset_handle_get_local_id(const struct AssetHandle *asset);
@@ -44,12 +45,5 @@ void ED_asset_handle_get_full_library_path(
  * returns it. Otherwise a reasonable method should be used, usually "Append (Reuse Data)". */
 std::optional<eAssetImportMethod> ED_asset_handle_get_import_method(
     const struct AssetHandle *asset);
-
-namespace blender::ed::asset {
-
-/** If the ID already exists in the database, return it, otherwise add it. */
-ID *get_local_id_from_asset_or_append_and_reuse(Main &bmain, AssetHandle asset);
-
-}  // namespace blender::ed::asset
 
 #endif

commit 160678d224456f6884a8159b440896c0fc8ed346

Fix #108078: Crash when inverting results in pose library sidebar
a958ae36e8 introduced support for UI lists to reference items that would
never be shown, regardless of filter settings. This was to skip assets
in the asset view template that were not of the requested type. UI list
sorting code wasn't updated to account for such items that should be
entirely ignored.

The following commit will remove some code duplication, just keeping
that cleanup separate from the fix.

source/blender/editors/interface/interface_handlers.cc
@@ -9678,7 +9678,8 @@ static int ui_handle_list_event(bContext *C, const wmEvent *event, ARegion *regi
 
         for (int i = 0; i < len; i++) {
           if (!dyn_data->items_filter_flags ||
-              ((dyn_data->items_filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude))
+              (((dyn_data->items_filter_flags[i] & UILST_FLT_ITEM_NEVER_SHOW) == 0) &&
+               (dyn_data->items_filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude))
           {
             org_order[new_order ? new_order[++org_idx] : ++org_idx] = i;
             if (i == value) {

source/blender/editors/interface/interface_template_list.cc
@@ -251,7 +251,7 @@ void UI_list_filter_and_sort_items(uiList *ui_list,
         const eUIListFilterResult filter_result = item_filter_fn(itemptr, name, i);
 
         if (filter_result == UI_LIST_ITEM_NEVER_SHOW) {
-          /* Pass. */
+          dyn_data->items_filter_flags[i] = UILST_FLT_ITEM_NEVER_SHOW;
         }
         else if (filter_result == UI_LIST_ITEM_FILTER_MATCHES) {
           dyn_data->items_filter_flags[i] = UILST_FLT_ITEM;
@@ -432,7 +432,8 @@ static void ui_template_list_collect_items(PointerRNA *list_ptr,
 
   RNA_PROP_BEGIN (list_ptr, itemptr, list_prop) {
     if (!dyn_data->items_filter_flags ||
-        ((dyn_data->items_filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude))
+        (((dyn_data->items_filter_flags[i] & UILST_FLT_ITEM_NEVER_SHOW) == 0) &&
+         ((dyn_data->items_filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude)))
     {
       int new_order_idx;
       if (dyn_data->items_filter_neworder) {

source/blender/makesdna/DNA_screen_types.h
@@ -614,7 +614,8 @@ enum {
 /* WARNING! Those values are used by integer RNA too, which does not handle well values > INT_MAX.
  *          So please do not use 32nd bit here. */
 enum {
-  UILST_FLT_ITEM = 1 << 30, /* This item has passed the filter process successfully. */
+  UILST_FLT_ITEM_NEVER_SHOW = (1 << 0), /* Filtering returned #UI_LIST_ITEM_NEVER_SHOW. */
+  UILST_FLT_ITEM = 1 << 30,        /* This item has passed the filter process successfully. */
 };
 
 /** #uiList.filter_flag */

source/blender/makesrna/intern/rna_ui.c
@@ -615,7 +615,9 @@ static void uilist_filter_items(uiList *ui_list,
         int t_idx, t_ni, prev_ni;
         flt_data->items_shown = 0;
         for (i = 0, shown_idx = 0; i < len; i++) {
-          if ((filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude) {
+          if (((filter_flags[i] & UILST_FLT_ITEM_NEVER_SHOW) == 0) &&
+              (filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude)
+          {
             filter_neworder[shown_idx++] = filter_neworder[i];
           }
         }
@@ -642,7 +644,9 @@ static void uilist_filter_items(uiList *ui_list,
         /* we still have to set flt_data->items_shown... */
         flt_data->items_shown = 0;
         for (i = 0; i < len; i++) {
-          if ((filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude) {
+          if (((filter_flags[i] & UILST_FLT_ITEM_NEVER_SHOW) == 0) &&
+              (filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude)
+          {
             flt_data->items_shown++;
           }
         }