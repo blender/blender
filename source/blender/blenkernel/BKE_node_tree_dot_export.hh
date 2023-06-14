37a0b97b518850330dd0f2d95ee11c72896fe32d
Jacques Lucke <jacques@blender.org>
2023-06-14 12:19:13 +0200

    Nodes: add debug utility to export bNodeTree as dot graph
    It's possible to customize how the generated graph looks like by
    passing in a custom `bNodeTreeToDotOptions`. This allows making
    specialized visualizations for specific algorithms.

---
+++ source/blender/blenkernel/BKE_node_tree_dot_export.hh
@@ -0,0 +1,34 @@
+/* SPDX-FileCopyrightText: 2023 Blender Foundation
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#include <string>
+
+#include "DNA_node_types.h"
+
+namespace blender::dot {
+class DirectedEdge;
+}
+
+namespace blender::bke {
+
+/**
+ * Allows customizing how the generated dot graph looks like.
+ */
+class bNodeTreeToDotOptions {
+ public:
+  virtual std::string socket_name(const bNodeSocket &socket) const;
+  virtual std::optional<std::string> socket_font_color(const bNodeSocket &socket) const;
+  virtual void add_edge_attributes(const bNodeLink &link, dot::DirectedEdge &dot_edge) const;
+};
+
+/**
+ * Convert a node tree into the dot format. This can be visualized with tools like graphviz and is
+ * very useful for debugging purposes.
+ */
+std::string node_tree_to_dot(const bNodeTree &tree,
+                             const bNodeTreeToDotOptions &options = bNodeTreeToDotOptions());
+
+}  // namespace blender::bke
