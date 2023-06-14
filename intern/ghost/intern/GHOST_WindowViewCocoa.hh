a2c6371e3edaa47cb6b588f6611f0a7bd6fe16a2
Campbell Barton <campbell@blender.org>
2023-06-14 17:39:46 +1000

    License headers: use SPDX-FileCopyrightText in intern/ghost

--- intern/ghost/intern/GHOST_WindowViewCocoa.hh
+++ intern/ghost/intern/GHOST_WindowViewCocoa.hh
@@ -1,5 +1,6 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later
- * Copyright 2001-2002 NaN Holding BV. All rights reserved. */
+/* SPDX-FileCopyrightText: 2001-2002 NaN Holding BV. All rights reserved.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
 
 /* The Carbon API is still needed to check if the Input Source (Input Method or IME) is valid. */
 #ifdef WITH_INPUT_IME
