543a70170ec452a260b18c3eb7430a6598c2f544
Brecht Van Lommel <brecht@blender.org>
2023-06-21 14:49:27 +0200

    Linux: add launcher script to fix issue launching on Steam
    The steam environment sets LD_LIBRARY_PATH with a libtbb.so.2 that is
    incompatible with our own. This wrapper scripts gives our own library
    priority.
    
    There is a more modern "Steam Linux Runtime" that can be used instead of
    the "LD_LIBRARY_PATH Steam Runtime" and which launches Blender, but it
    fails to detect GPU compute libraries. So that was not an option.
    
    Ref #107385
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/109169

--- tools/check_blender_release/check_static_binaries.py
+++ tools/check_blender_release/check_static_binaries.py
@@ -51,7 +51,7 @@ ALLOWED_LIBS = (
     "libfreetype.so",
 )
 
-IGNORE_FILES = ("blender-softwaregl", )
+IGNORE_FILES = ("blender-launcher", "blender-softwaregl", )
 IGNORE_EXTENSION = (".sh", ".py", )
 
 
