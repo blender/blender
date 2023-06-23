9383d06688de711120d18398a6931e897970ba09
Germano Cavalcante <germano.costa@ig.com.br>
2023-06-22 21:06:08 -0300

    Fix measure tool snapping to perpendicular while not dragging

--- source/blender/editors/space_view3d/view3d_gizmo_ruler.cc
+++ source/blender/editors/space_view3d/view3d_gizmo_ruler.cc
@@ -313,6 +313,8 @@ static void ruler_state_set(RulerInfo *ruler_info, int state)
 
   if (state == RULER_STATE_NORMAL) {
     WM_gizmo_set_flag(ruler_info->snap_data.gizmo, WM_GIZMO_DRAW_VALUE, false);
+    RNA_property_float_set_array(
+        ruler_info->snap_data.gizmo->ptr, ruler_info->snap_data.prop_snap_source, nullptr);
   }
   else if (state == RULER_STATE_DRAG) {
     memset(&ruler_info->drag_state_prev, 0x0, sizeof(ruler_info->drag_state_prev));
