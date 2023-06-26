be06c4b38399869c0e11125f54547133442a66d4
Weizhen Huang <weizhen@blender.org>
2023-06-26 11:47:50 +0200

    Cleanup: minor computational simplification in Cycles area light

--- intern/cycles/kernel/light/area.h
+++ intern/cycles/kernel/light/area.h
@@ -118,9 +118,9 @@ ccl_device bool area_light_spread_clamp_light(const float3 P,
                                               const float tan_half_spread,
                                               ccl_private bool *sample_rectangle)
 {
-  /* Closest point in area light plane and distance to that plane. */
-  const float3 closest_P = P - dot(lightNg, P - *lightP) * lightNg;
-  const float t = len(closest_P - P);
+  /* Distance from shading point to area light plane and the closest point on that plane. */
+  const float t = dot(lightNg, P - *lightP);
+  const float3 closest_P = P - t * lightNg;
 
   /* Radius of circle on area light that actually affects the shading point. */
   const float r_spread = t * tan_half_spread;
