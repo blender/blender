b14b67db40a4a15839af467898a4e01bba7fec3d
Miguel Pozo <pragma37@gmail.com>
2023-06-26 11:56:10 +0200

    Workbench Next: Sculpt Support
    Add Sculpt support for the new Draw Manager
    and implement Sculpt rendering in Workbench Next.
    Instead of using a callback approach, it's now possible to
    retrieve a vector of GPUBatches and their material index,
    so engines can use them as they see fit.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/109294

---
+++ source/blender/draw/intern/draw_sculpt.hh
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup draw
+ */
+
+#pragma once
+
+#include "draw_manager.hh"
+
+namespace blender::draw {
+
+#define SCULPT_DEBUG_DRAW (G.debug_value == 889)
+
+struct SculptBatch {
+  GPUBatch *batch;
+  int material_slot;
+  int debug_index;
+  float3 debug_color();
+};
+
+enum SculptBatchFeature {
+  SCULPT_BATCH_DEFAULT = 0,
+  SCULPT_BATCH_WIREFRAME = 1 << 0,
+  SCULPT_BATCH_MASK = 1 << 1,
+  SCULPT_BATCH_FACE_SET = 1 << 2,
+  SCULPT_BATCH_VERTEX_COLOR = 1 << 3,
+  SCULPT_BATCH_UV = 1 << 4
+};
+ENUM_OPERATORS(SculptBatchFeature, SCULPT_BATCH_UV);
+
+Vector<SculptBatch> sculpt_batches_get(Object *ob, SculptBatchFeature features);
+
+Vector<SculptBatch> sculpt_batches_per_material_get(Object *ob,
+                                                    MutableSpan<GPUMaterial *> materials);
+
+}  // namespace blender::draw
