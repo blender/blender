d9c88f32a50db909d3ae0935020c4858b5303638
Joseph Eagar <joeedh@gmail.com>
2023-07-09 01:36:57 -0700

    temp-sculpt-dyntopo: remove unused code

--- source/blender/editors/space_spreadsheet/spreadsheet_column_values.hh
+++ source/blender/editors/space_spreadsheet/spreadsheet_column_values.hh
@@ -40,61 +40,6 @@ class ColumnValues final {
 
   ColumnValues(const ColumnValues &b) = delete;
 
-  ColumnValues(std::string name, BLI_mempool *pool, int type, int offset, int htype)
-      : name_(std::move(name))
-  {
-    using namespace blender;
-
-    auto create_array = [&](auto typevar) {
-      using T = decltype(typevar);
-
-      int count = BLI_mempool_len(pool);
-
-      T *array = MEM_cnew_array<T>(count, "ColumnValues::bmesh_data");
-      bmesh_data_ = static_cast<void *>(array);
-
-      BLI_mempool_iter iter;
-      BLI_mempool_iternew(pool, &iter);
-      void *velem;
-      int i = 0;
-
-      for (velem = BLI_mempool_iterstep(&iter); velem; velem = BLI_mempool_iterstep(&iter), i++) {
-        BMElem *elem = static_cast<BMElem *>(velem);
-        T *value = static_cast<T *>(POINTER_OFFSET(elem->head.data, offset));
-
-        array[i] = *value;
-      }
-
-      data_ = VArray<T>::ForSpan({array, count});
-    };
-
-    switch (type) {
-      case CD_PROP_FLOAT:
-        create_array(float(0));
-        break;
-      case CD_PROP_FLOAT2:
-        create_array(float2());
-        break;
-      case CD_PROP_FLOAT3:
-        create_array(float3());
-        break;
-      case CD_PROP_COLOR:
-        create_array(float4());
-        break;
-      case CD_PROP_BYTE_COLOR:
-        create_array(uchar4());
-        break;
-      case CD_PROP_INT32:
-        create_array(int(0));
-        break;
-      case CD_PROP_INT8:
-        create_array(int8_t(0));
-      case CD_PROP_BOOL:
-        create_array(bool(0));
-        break;
-    }
-  }
-
   ~ColumnValues() {
     if (bmesh_data_) {
       MEM_freeN(bmesh_data_);
