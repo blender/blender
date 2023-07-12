98336cb8cad38269b8daace82baec57a4ecae628
Ray Molenkamp <github@lazydodo.com>
2023-07-12 22:55:57 +0200

    USD: Use pre-compiled headers
    bf_usd is a bit slow to build, and most time is spend on the front end
    of the compiler parsing USD's headers.
    
    this change enables pre-compiled headers for the most expensive
    headers to parse.
    
    When building `bf_usd` single threaded with -j1 (for accurate measurements)
    
    ```
    before Wall time:      0h 4min 30sec 931 ms 504299 ns
    after  Wall time:      0h 0min 50sec 808 ms 24399 ns
    ```
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/110027

---
+++ source/blender/io/usd/intern/usd_precomp.h
@@ -0,0 +1,6 @@
+#include <pxr/pxr.h>
+#include <pxr/usd/sdf/layer.h>
+#include <pxr/usd/usd/object.h>
+#include <pxr/usd/usd/prim.h>
+#include <pxr/usd/usd/primData.h>
+#include <pxr/usd/usdGeom/xformable.h>
