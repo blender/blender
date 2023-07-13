7551767f9f1e46f037bb228d1e492e15632b3f89
Brecht Van Lommel <brecht@blender.org>
2023-07-13 19:14:04 +0200

    USD: more complete import and export of lights
    * Exposure import
    * Diffuse factor support
    * Correct disk radius
    * Better non-normalized intensity import
    * Sun radius export
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/109929

--- source/blender/io/usd/intern/usd_reader_light.cc
+++ source/blender/io/usd/intern/usd_reader_light.cc
@@ -51,34 +51,106 @@ void USDLightReader::read_object_data(Main *bmain, const double motionSampleTime
     return;
   }
 
-  pxr::UsdLuxShapingAPI shaping_api;
-
-  /* Set light type. */
+  float light_surface_area = 1.0f;
 
   if (prim_.IsA<pxr::UsdLuxDiskLight>()) {
+    /* Disk area light. */
     blight->type = LA_AREA;
     blight->area_shape = LA_AREA_DISK;
-    /* Ellipse lights are not currently supported */
+
+    pxr::UsdLuxDiskLight disk_light(prim_);
+    if (disk_light) {
+      if (pxr::UsdAttribute radius_attr = disk_light.GetRadiusAttr()) {
+        float radius = 0.0f;
+        if (radius_attr.Get(&radius, motionSampleTime)) {
+          blight->area_size = radius * 2.0f;
+        }
+      }
+    }
+
+    const float radius = 0.5f * blight->area_size;
+    light_surface_area = radius * radius * M_PI;
   }
   else if (prim_.IsA<pxr::UsdLuxRectLight>()) {
+    /* Rectangular area light. */
     blight->type = LA_AREA;
     blight->area_shape = LA_AREA_RECT;
+
+    pxr::UsdLuxRectLight rect_light(prim_);
+    if (rect_light) {
+      if (pxr::UsdAttribute width_attr = rect_light.GetWidthAttr()) {
+        float width = 0.0f;
+        if (width_attr.Get(&width, motionSampleTime)) {
+          blight->area_size = width;
+        }
+      }
+
+      if (pxr::UsdAttribute height_attr = rect_light.GetHeightAttr()) {
+        float height = 0.0f;
+        if (height_attr.Get(&height, motionSampleTime)) {
+          blight->area_sizey = height;
+        }
+      }
+    }
+
+    light_surface_area = blight->area_size * blight->area_sizey;
   }
   else if (prim_.IsA<pxr::UsdLuxSphereLight>()) {
+    /* Point and spot light. */
     blight->type = LA_LOCAL;
 
-    shaping_api = pxr::UsdLuxShapingAPI(prim_);
+    pxr::UsdLuxSphereLight sphere_light(prim_);
+    if (sphere_light) {
+      pxr::UsdAttribute treatAsPoint_attr = sphere_light.GetTreatAsPointAttr();
+      bool treatAsPoint;
+      if (treatAsPoint_attr && treatAsPoint_attr.Get(&treatAsPoint, motionSampleTime) &&
+          treatAsPoint) {
+        blight->radius = 0.0f;
+      }
+      else if (pxr::UsdAttribute radius_attr = sphere_light.GetRadiusAttr()) {
+        float radius = 0.0f;
+        if (radius_attr.Get(&radius, motionSampleTime)) {
+          blight->radius = radius;
+        }
+      }
+    }
 
+    light_surface_area = 4.0f * M_PI * blight->radius * blight->radius;
+
+    pxr::UsdLuxShapingAPI shaping_api = pxr::UsdLuxShapingAPI(prim_);
     if (shaping_api && shaping_api.GetShapingConeAngleAttr().IsAuthored()) {
       blight->type = LA_SPOT;
+
+      if (pxr::UsdAttribute cone_angle_attr = shaping_api.GetShapingConeAngleAttr()) {
+        float cone_angle = 0.0f;
+        if (cone_angle_attr.Get(&cone_angle, motionSampleTime)) {
+          blight->spotsize = DEG2RADF(cone_angle) * 2.0f;
+        }
+      }
+
+      if (pxr::UsdAttribute cone_softness_attr = shaping_api.GetShapingConeSoftnessAttr()) {
+        float cone_softness = 0.0f;
+        if (cone_softness_attr.Get(&cone_softness, motionSampleTime)) {
+          blight->spotblend = cone_softness;
+        }
+      }
+    }
+    else if (prim_.IsA<pxr::UsdLuxDistantLight>()) {
+      blight->type = LA_SUN;
+
+      pxr::UsdLuxDistantLight distant_light(prim_);
+      if (distant_light) {
+        if (pxr::UsdAttribute angle_attr = distant_light.GetAngleAttr()) {
+          float angle = 0.0f;
+          if (angle_attr.Get(&angle, motionSampleTime)) {
+            blight->sun_angle = DEG2RADF(angle) * 2.0f;
+          }
+        }
+      }
     }
   }
-  else if (prim_.IsA<pxr::UsdLuxDistantLight>()) {
-    blight->type = LA_SUN;
-  }
 
-  /* Set light values. */
-
+  /* Intensity */
   if (pxr::UsdAttribute intensity_attr = light_api.GetIntensityAttr()) {
     float intensity = 0.0f;
     if (intensity_attr.Get(&intensity, motionSampleTime)) {
@@ -94,25 +166,15 @@ void USDLightReader::read_object_data(Main *bmain, const double motionSampleTime
     }
   }
 
-  /* TODO(makowalsk): Not currently supported. */
-#if 0
-  pxr::VtValue exposure;
-  light_prim.GetExposureAttr().Get(&exposure, motionSampleTime);
-#endif
-
-  /* TODO(makowalsk): Not currently supported */
-#if 0
-  pxr::VtValue diffuse;
-  light_prim.GetDiffuseAttr().Get(&diffuse, motionSampleTime);
-#endif
-
-  if (pxr::UsdAttribute spec_attr = light_api.GetSpecularAttr()) {
-    float spec = 0.0f;
-    if (spec_attr.Get(&spec, motionSampleTime)) {
-      blight->spec_fac = spec;
+  /* Exposure. */
+  if (pxr::UsdAttribute exposure_attr = light_api.GetExposureAttr()) {
+    float exposure = 0.0f;
+    if (exposure_attr.Get(&exposure, motionSampleTime)) {
+      blight->energy *= pow(2.0f, exposure);
     }
   }
 
+  /* Color. */
   if (pxr::UsdAttribute color_attr = light_api.GetColorAttr()) {
     pxr::GfVec3f color;
     if (color_attr.Get(&color, motionSampleTime)) {
@@ -122,115 +184,35 @@ void USDLightReader::read_object_data(Main *bmain, const double motionSampleTime
     }
   }
 
-  /* TODO(makowalski): Not currently supported. */
-#if 0
-  pxr::VtValue use_color_temp;
-  light_prim.GetEnableColorTemperatureAttr().Get(&use_color_temp, motionSampleTime);
-#endif
-
-  /* TODO(makowalski): Not currently supported. */
-#if 0
-  pxr::VtValue color_temp;
-  light_prim.GetColorTemperatureAttr().Get(&color_temp, motionSampleTime);
-#endif
-
-  switch (blight->type) {
-    case LA_AREA:
-      if (blight->area_shape == LA_AREA_RECT && prim_.IsA<pxr::UsdLuxRectLight>()) {
-
-        pxr::UsdLuxRectLight rect_light(prim_);
-
-        if (!rect_light) {
-          break;
-        }
-
-        if (pxr::UsdAttribute width_attr = rect_light.GetWidthAttr()) {
-          float width = 0.0f;
-          if (width_attr.Get(&width, motionSampleTime)) {
-            blight->area_size = width;
-          }
-        }
-
-        if (pxr::UsdAttribute height_attr = rect_light.GetHeightAttr()) {
-          float height = 0.0f;
-          if (height_attr.Get(&height, motionSampleTime)) {
-            blight->area_sizey = height;
-          }
-        }
-      }
-      else if (blight->area_shape == LA_AREA_DISK && prim_.IsA<pxr::UsdLuxDiskLight>()) {
-
-        pxr::UsdLuxDiskLight disk_light(prim_);
-
-        if (!disk_light) {
-          break;
-        }
-
-        if (pxr::UsdAttribute radius_attr = disk_light.GetRadiusAttr()) {
-          float radius = 0.0f;
-          if (radius_attr.Get(&radius, motionSampleTime)) {
-            blight->area_size = radius * 2.0f;
-          }
-        }
-      }
-      break;
-    case LA_LOCAL:
-    case LA_SPOT:
-      if (prim_.IsA<pxr::UsdLuxSphereLight>()) {
-
-        pxr::UsdLuxSphereLight sphere_light(prim_);
-
-        if (!sphere_light) {
-          break;
-        }
-
-        pxr::UsdAttribute treatAsPoint_attr = sphere_light.GetTreatAsPointAttr();
-        bool treatAsPoint;
-        if (treatAsPoint_attr && treatAsPoint_attr.Get(&treatAsPoint, motionSampleTime) &&
-            treatAsPoint) {
-          blight->radius = 0.0f;
-        }
-        else if (pxr::UsdAttribute radius_attr = sphere_light.GetRadiusAttr()) {
-          float radius = 0.0f;
-          if (radius_attr.Get(&radius, motionSampleTime)) {
-            blight->radius = radius;
-          }
-        }
-
-        if (blight->type == LA_SPOT && shaping_api) {
-          if (pxr::UsdAttribute cone_angle_attr = shaping_api.GetShapingConeAngleAttr()) {
-            float cone_angle = 0.0f;
-            if (cone_angle_attr.Get(&cone_angle, motionSampleTime)) {
-              blight->spotsize = DEG2RADF(cone_angle) * 2.0f;
-            }
-          }
-
-          if (pxr::UsdAttribute cone_softness_attr = shaping_api.GetShapingConeSoftnessAttr()) {
-            float cone_softness = 0.0f;
-            if (cone_softness_attr.Get(&cone_softness, motionSampleTime)) {
-              blight->spotblend = cone_softness;
-            }
-          }
-        }
-      }
-      break;
-    case LA_SUN:
-      if (prim_.IsA<pxr::UsdLuxDistantLight>()) {
-        pxr::UsdLuxDistantLight distant_light(prim_);
-
-        if (!distant_light) {
-          break;
-        }
-
-        if (pxr::UsdAttribute angle_attr = distant_light.GetAngleAttr()) {
-          float angle = 0.0f;
-          if (angle_attr.Get(&angle, motionSampleTime)) {
-            blight->sun_angle = angle * float(M_PI) / 180.0f;
-          }
-        }
-      }
-      break;
+  /* Diffuse and Specular. */
+  if (pxr::UsdAttribute diff_attr = light_api.GetDiffuseAttr()) {
+    float diff_fac = 1.0f;
+    if (diff_attr.Get(&diff_fac, motionSampleTime)) {
+      blight->diff_fac = diff_fac;
+    }
   }
+  if (pxr::UsdAttribute spec_attr = light_api.GetSpecularAttr()) {
+    float spec_fac = 1.0f;
+    if (spec_attr.Get(&spec_fac, motionSampleTime)) {
+      blight->spec_fac = spec_fac;
+    }
+  }
+
+  /* Normalize: Blender lights are always normalized, so inverse correct for it
+   * TODO: take into account object transform, or natively support this as a
+   * setting on lights in Blender. */
+  bool normalize = false;
+  if (pxr::UsdAttribute normalize_attr = light_api.GetNormalizeAttr()) {
+    normalize_attr.Get(&normalize, motionSampleTime);
+  }
+  if (!normalize) {
+    blight->energy *= light_surface_area;
+  }
+
+  /* TODO:
+   * bool GetEnableColorTemperatureAttr
+   * float GetColorTemperatureAttr
+   */
 
   USDXformReader::read_object_data(bmain, motionSampleTime);
 }
--- source/blender/io/usd/intern/usd_writer_light.cc
+++ source/blender/io/usd/intern/usd_writer_light.cc
@@ -21,10 +21,9 @@ namespace blender::io::usd {
 
 USDLightWriter::USDLightWriter(const USDExporterContext &ctx) : USDAbstractWriter(ctx) {}
 
-bool USDLightWriter::is_supported(const HierarchyContext *context) const
+bool USDLightWriter::is_supported(const HierarchyContext * /*context*/) const
 {
-  Light *light = static_cast<Light *>(context->object->data);
-  return ELEM(light->type, LA_AREA, LA_LOCAL, LA_SUN, LA_SPOT);
+  return true;
 }
 
 void USDLightWriter::do_write(HierarchyContext &context)
@@ -34,51 +33,42 @@ void USDLightWriter::do_write(HierarchyContext &context)
   pxr::UsdTimeCode timecode = get_export_time_code();
 
   Light *light = static_cast<Light *>(context.object->data);
-#if PXR_VERSION >= 2111
   pxr::UsdLuxLightAPI usd_light_api;
-#else
-  pxr::UsdLuxLight usd_light_api;
-
-#endif
 
   switch (light->type) {
-    case LA_AREA:
+    case LA_AREA: {
       switch (light->area_shape) {
-        case LA_AREA_DISK:
-        case LA_AREA_ELLIPSE: { /* An ellipse light will deteriorate into a disk light. */
-          pxr::UsdLuxDiskLight disk_light = pxr::UsdLuxDiskLight::Define(stage, usd_path);
-          disk_light.CreateRadiusAttr().Set(light->area_size, timecode);
-#if PXR_VERSION >= 2111
-          usd_light_api = disk_light.LightAPI();
-#else
-          usd_light_api = disk_light;
-#endif
-          break;
-        }
         case LA_AREA_RECT: {
           pxr::UsdLuxRectLight rect_light = pxr::UsdLuxRectLight::Define(stage, usd_path);
           rect_light.CreateWidthAttr().Set(light->area_size, timecode);
           rect_light.CreateHeightAttr().Set(light->area_sizey, timecode);
-#if PXR_VERSION >= 2111
           usd_light_api = rect_light.LightAPI();
-#else
-          usd_light_api = rect_light;
-#endif
           break;
         }
         case LA_AREA_SQUARE: {
           pxr::UsdLuxRectLight rect_light = pxr::UsdLuxRectLight::Define(stage, usd_path);
           rect_light.CreateWidthAttr().Set(light->area_size, timecode);
           rect_light.CreateHeightAttr().Set(light->area_size, timecode);
-#if PXR_VERSION >= 2111
           usd_light_api = rect_light.LightAPI();
-#else
-          usd_light_api = rect_light;
-#endif
+          break;
+        }
+        case LA_AREA_DISK: {
+          pxr::UsdLuxDiskLight disk_light = pxr::UsdLuxDiskLight::Define(stage, usd_path);
+          disk_light.CreateRadiusAttr().Set(light->area_size / 2.0f, timecode);
+          usd_light_api = disk_light.LightAPI();
+          break;
+        }
+        case LA_AREA_ELLIPSE: {
+          /* An ellipse light deteriorates into a disk light. */
+          pxr::UsdLuxDiskLight disk_light = pxr::UsdLuxDiskLight::Define(stage, usd_path);
+          disk_light.CreateRadiusAttr().Set((light->area_size + light->area_sizey) / 4.0f,
+                                            timecode);
+          usd_light_api = disk_light.LightAPI();
           break;
         }
       }
       break;
+    }
     case LA_LOCAL:
     case LA_SPOT: {
       pxr::UsdLuxSphereLight sphere_light = pxr::UsdLuxSphereLight::Define(stage, usd_path);
@@ -95,39 +85,34 @@ void USDLightWriter::do_write(HierarchyContext &context)
         }
       }
 
-#if PXR_VERSION >= 2111
       usd_light_api = sphere_light.LightAPI();
-#else
-      usd_light_api = sphere_light;
-#endif
       break;
     }
     case LA_SUN: {
       pxr::UsdLuxDistantLight distant_light = pxr::UsdLuxDistantLight::Define(stage, usd_path);
-      /* TODO(makowalski): set angle attribute here. */
-#if PXR_VERSION >= 2111
+      distant_light.CreateAngleAttr().Set(RAD2DEGF(light->sun_angle / 2.0f), timecode);
       usd_light_api = distant_light.LightAPI();
-#else
-      usd_light_api = distant_light;
-#endif
       break;
     }
     default:
-      BLI_assert_msg(0, "is_supported() returned true for unsupported light type");
+      BLI_assert_unreachable();
+      break;
   }
 
-  float usd_intensity;
+  float intensity;
   if (light->type == LA_SUN) {
     /* Unclear why, but approximately matches Karma. */
-    usd_intensity = light->energy / 4.0f;
+    intensity = light->energy / 4.0f;
   }
   else {
     /* Convert from radiant flux to intensity. */
-    usd_intensity = light->energy / M_PI;
+    intensity = light->energy / M_PI;
   }
 
-  usd_light_api.CreateIntensityAttr().Set(usd_intensity, timecode);
+  usd_light_api.CreateIntensityAttr().Set(intensity, timecode);
+  usd_light_api.CreateExposureAttr().Set(0.0f, timecode);
   usd_light_api.CreateColorAttr().Set(pxr::GfVec3f(light->r, light->g, light->b), timecode);
+  usd_light_api.CreateDiffuseAttr().Set(light->diff_fac, timecode);
   usd_light_api.CreateSpecularAttr().Set(light->spec_fac, timecode);
   usd_light_api.CreateNormalizeAttr().Set(true, timecode);
 }
