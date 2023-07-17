b767a62f32ad5d6342ea57363776d7224db63043
kaspian.jakobssongmail.com <kaspian.jakobsson@gmail.com>
2023-07-17 20:06:41 +0200

    Fix #109679: Normal Map node strength interpolation artifacts
    Previously the normal strength linearly interpolated and extrapolated
    the normal in world space. Instead do it in tangent space, in a way
    that ensure the normal remains above the surface and valid.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/109763

--- source/blender/gpu/shaders/material/gpu_shader_material_normal_map.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_normal_map.glsl
@@ -1,6 +1,6 @@
 
 #ifdef OBINFO_LIB
-void node_normal_map(vec4 tangent, vec3 texnormal, out vec3 outnormal)
+void node_normal_map(vec4 tangent, float strength, vec3 texnormal, out vec3 outnormal)
 {
   if (all(equal(tangent, vec4(0.0, 0.0, 0.0, 1.0)))) {
     outnormal = g_data.Ni;
@@ -8,6 +8,9 @@ void node_normal_map(vec4 tangent, vec3 texnormal, out vec3 outnormal)
   }
   tangent *= (FrontFacing ? 1.0 : -1.0);
   vec3 B = tangent.w * cross(g_data.Ni, tangent.xyz) * sign(ObjectInfo.w);
+
+  /* Apply strength here instead of in node_normal_map_mix for tangent space. */
+  texnormal.xy *= strength;
 
   outnormal = texnormal.x * tangent.xyz + texnormal.y * B + texnormal.z * g_data.Ni;
   outnormal = normalize(outnormal);
@@ -26,5 +29,5 @@ void color_to_blender_normal_new_shading(vec3 color, out vec3 normal)
 
 void node_normal_map_mix(float strength, vec3 newnormal, out vec3 outnormal)
 {
-  outnormal = normalize(mix(g_data.N, newnormal, max(strength, 0.0)));
+  outnormal = normalize(mix(g_data.N, newnormal, max(0.0, strength)));
 }
