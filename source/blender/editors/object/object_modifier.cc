a30bcc806d16045bd4536b50e3a5300942164c8d
Philipp Oeser <philipp@blender.org>
2023-07-17 09:31:46 +0200

    Fix #110021: Deform modifier binding can go wrong in certain stacks
    This affected `Mesh Deform`, `Corrective Smooth` and `Laplacian Smooth`.
    
    Exposed by 57ea827bfb (e5ec04d73c78).
    
    Evaluating the incoming mesh can be different in
    `BKE_object_handle_data_update` as opposed to
    `object_force_modifier_update_for_bind` due to different
    `CustomData_MeshMasks` being used (the former used a handpicked set, the
    later was still using `CD_MASK_BAREMESH`). But since moving many
    relevant [to modifiers] data to attributes, this could lead to binding go
    wrong (e.g. reporting a vertex count mismatch as reported in #110021
    which was due to the fact that creases were not propagated and that
    changed the output [also vertex count] of a subdivision modifier in the
    stack).
    
    Now use `CD_MASK_DERIVEDMESH` in `object_force_modifier_update_for_bind`
    to get all relevant attributes for evaluation (this is not used in `BKE_object_handle_data_update`
    to avoid a big performance impact there).
    
    Also fixes #109626 afaict.
    
    Should probably go into 3.6 LTS (maybe even 3.3 LTS).
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/110095

--- source/blender/editors/object/object_modifier.cc
+++ source/blender/editors/object/object_modifier.cc
@@ -112,7 +112,7 @@ static void object_force_modifier_update_for_bind(Depsgraph *depsgraph, Object *
   Object *ob_eval = DEG_get_evaluated_object(depsgraph, ob);
   BKE_object_eval_reset(ob_eval);
   if (ob->type == OB_MESH) {
-    Mesh *me_eval = mesh_create_eval_final(depsgraph, scene_eval, ob_eval, &CD_MASK_BAREMESH);
+    Mesh *me_eval = mesh_create_eval_final(depsgraph, scene_eval, ob_eval, &CD_MASK_DERIVEDMESH);
     BKE_mesh_eval_delete(me_eval);
   }
   else if (ob->type == OB_LATTICE) {
