4c72dc98c26da31954851dccc0de8271a367c6b9
Omar Emara <mail@OmarEmara.dev>
2023-07-19 14:51:34 +0300

    Fix #109869: Switch View node crashes on editing
    The Switch View node crashes on editing the compositor node tree with
    the GPU compositor enabled.
    
    That's because interactive edits are are in contexts that are not
    multi-view, which is indicated by an empty view name.
    
    To fix this, we fallback to the first input for non multi-view contexts.

--- source/blender/nodes/composite/nodes/node_composite_switchview.cc
+++ source/blender/nodes/composite/nodes/node_composite_switchview.cc
@@ -151,8 +151,16 @@ class SwitchViewOperation : public NodeOperation {
 
   void execute() override
   {
-    Result &input = get_input(context().get_view_name());
     Result &result = get_result("Image");
+
+    /* A context that is not multi view, pass the first input through as a fallback. */
+    if (context().get_view_name().is_empty()) {
+      Result &input = get_input(node().input(0)->identifier);
+      input.pass_through(result);
+      return;
+    }
+
+    Result &input = get_input(context().get_view_name());
     input.pass_through(result);
   }
 };
