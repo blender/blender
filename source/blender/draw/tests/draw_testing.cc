530e1ac15123c6a133d3333d027393cee9e9ed93
Campbell Barton <campbell@blender.org>
2023-07-21 14:03:27 +1000

    Fix building drawing test WITH_OPENGL_BACKEND=OFF

--- source/blender/draw/tests/draw_testing.cc
+++ source/blender/draw/tests/draw_testing.cc
@@ -11,11 +11,13 @@
 namespace blender::draw {
 
 /* Base class for draw test cases. It will setup and tear down the GPU part around each test. */
+#ifdef WITH_OPENGL_BACKEND
 void DrawOpenGLTest::SetUp()
 {
   GPUOpenGLTest::SetUp();
   DRW_draw_state_init_gtests(GPU_SHADER_CFG_DEFAULT);
 }
+#endif
 
 #ifdef WITH_METAL_BACKEND
 void DrawMetalTest::SetUp()
--- source/blender/draw/tests/draw_testing.hh
+++ source/blender/draw/tests/draw_testing.hh
@@ -7,15 +7,20 @@
 namespace blender::draw {
 
 /* Base class for draw test cases. It will setup and tear down the GPU part around each test. */
+#ifdef WITH_OPENGL_BACKEND
 class DrawOpenGLTest : public blender::gpu::GPUOpenGLTest {
  public:
   void SetUp() override;
 };
-#define DRAW_OPENGL_TEST(test_name) \
-  TEST_F(DrawOpenGLTest, test_name) \
-  { \
-    test_##test_name(); \
-  }
+
+#  define DRAW_OPENGL_TEST(test_name) \
+    TEST_F(DrawOpenGLTest, test_name) \
+    { \
+      test_##test_name(); \
+    }
+#else
+#  define DRAW_OPENGL_TEST(test_name)
+#endif
 
 #ifdef WITH_METAL_BACKEND
 class DrawMetalTest : public blender::gpu::GPUMetalTest {
