ef00b42982cd702de251b8d6c1480cdfc2a10442
Anthony <anthony-roberts@noreply.localhost>
2023-08-01 16:36:39 +0200

    Fix: failure to open on Windows ARM64 and WSL
    This is done by forcing the Mesa D3D12 driver (which both use to render
    Blender) to report that it is OpenGL 4.3 compatible.
    
    It mostly is, other than a few texture extensions that are not yet
    implemented. Blender (currently) does not use those.
    
    There is a discussion pending with the viewport/evee team on those
    extentions
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/110582

--- source/creator/creator.c
+++ source/creator/creator.c
@@ -14,6 +14,10 @@
 #  include <windows.h>
 #endif
 
+#ifdef __linux__
+#include <unistd.h>
+#endif
+
 #if defined(WITH_TBB_MALLOC) && defined(_MSC_VER) && defined(NDEBUG)
 #  pragma comment(lib, "tbbmalloc_proxy.lib")
 #  pragma comment(linker, "/include:__TBB_malloc_proxy")
@@ -25,6 +29,7 @@
 
 #include "DNA_genfile.h"
 
+#include "BLI_path_util.h"
 #include "BLI_string.h"
 #include "BLI_system.h"
 #include "BLI_task.h"
@@ -328,6 +333,19 @@ int main(int argc,
   }
 #  endif /* USE_WIN32_UNICODE_ARGS */
 #endif   /* WIN32 */
+
+/* Here we check for Windows ARM64 or WSL, and override the Mesa reported OpenGL version */
+#if defined(WIN32) || defined(__linux__)
+#  if defined(WIN32)
+  if(strncmp(BLI_getenv("PROCESSOR_IDENTIFIER"), "ARM", 3) == 0)
+#  else /* Must be linux, so check if we're in WSL */
+  if(access("/proc/sys/fs/binfmt_misc/WSLInterop", F_OK) == 0)
+#  endif
+  {
+    BLI_setenv_if_new("MESA_GLSL_VERSION_OVERRIDE", "430");
+    BLI_setenv_if_new("MESA_GL_VERSION_OVERRIDE", "4.3");
+  }
+#endif
 
   /* NOTE: Special exception for guarded allocator type switch:
    *       we need to perform switch from lock-free to fully
