63115218d897926172b47b6e856b31a51e201e27
Lukas Tönne <lukas@blender.org>
2023-08-09 16:04:59 +0200

    Nodes: Fixed accidental typo.

--- source/blender/blenkernel/BKE_node_tree_interface.hh
+++ source/blender/blenkernel/BKE_node_tree_interface.hh
@@ -184,7 +184,7 @@ inline bNodeTreeInterfaceSocket *add_interface_socket_from_node(bNodeTree &ntree
 {
   eNodeTreeInterfaceSocketFlag flag = eNodeTreeInterfaceSocketFlag(0);
   SET_FLAG_FROM_TEST(flag, from_sock.in_out & SOCK_IN, NODE_INTERFACE_SOCKET_INPUT);
-  q SET_FLAG_FROM_TEST(flag, from_sock.in_out & SOCK_OUT, NODE_INTERFACE_SOCKET_OUTPUT);
+  SET_FLAG_FROM_TEST(flag, from_sock.in_out & SOCK_OUT, NODE_INTERFACE_SOCKET_OUTPUT);
 
   bNodeTreeInterfaceSocket *iosock = ntree.tree_interface.add_socket(
       name.data(), from_sock.description, socket_type, flag, nullptr);
