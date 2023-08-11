4ad04c9501ef1290f3811dfdb15fede58eb5cc76
Richard Antalik <richardantalik@gmail.com>
2023-08-11 08:00:56 +0200

    Fix #110727: VSE relative paths do not work with linked scenes
    This is identical issue as #108094, but with image strips.
    
    Use `ID_BLEND_PATH_FROM_GLOBAL` instead of
    `BKE_main_blendfile_path_from_global`.

--- source/blender/sequencer/intern/render.cc
+++ source/blender/sequencer/intern/render.cc
@@ -983,7 +983,7 @@ static ImBuf *seq_render_image_strip(const SeqRenderData *context,
   }
 
   BLI_path_join(filepath, sizeof(filepath), seq->strip->dirpath, s_elem->filename);
-  BLI_path_abs(filepath, BKE_main_blendfile_path_from_global());
+  BLI_path_abs(filepath, ID_BLEND_PATH_FROM_GLOBAL(&context->scene->id));
 
   /* Try to get a proxy image. */
   ibuf = seq_proxy_fetch(context, seq, timeline_frame);
