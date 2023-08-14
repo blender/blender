769fe076683ab85ee3647e873863c2f7db30c39e
Johannes J <johannesj@noreply.localhost>
2023-08-14 11:22:03 +0200

    GPU: Fix signed/unsigned mismatches in shader code
    This fixes a crash when switching to face selection mode in the UV
    editor.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/111094

--- source/blender/draw/engines/overlay/shaders/overlay_edit_uv_face_dots_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_edit_uv_face_dots_vert.glsl
@@ -5,6 +5,6 @@ void main()
   vec3 world_pos = point_object_to_world(vec3(au, 0.0));
   gl_Position = point_world_to_ndc(world_pos);
 
-  finalColor = ((flag & FACE_UV_SELECT) != 0) ? colorFaceDot : vec4(colorWire.rgb, 1.0);
+  finalColor = ((flag & FACE_UV_SELECT) != 0u) ? colorFaceDot : vec4(colorWire.rgb, 1.0);
   gl_PointSize = pointSize;
 }
