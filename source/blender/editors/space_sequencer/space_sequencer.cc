675b66d8524139b77dd8d27c3f6f7e7e6f68f600
Richard Antalik <richardantalik@gmail.com>
2023-08-14 04:04:18 +0200

    Fix #110846: VSE tools region not updated with other regions
    Add listener to the region.

--- source/blender/editors/space_sequencer/space_sequencer.cc
+++ source/blender/editors/space_sequencer/space_sequencer.cc
@@ -1067,6 +1067,7 @@ void ED_spacetype_sequencer()
   art->snap_size = ED_region_generic_tools_region_snap_size;
   art->init = sequencer_tools_region_init;
   art->draw = sequencer_tools_region_draw;
+  art->listener = sequencer_main_region_listener;
   BLI_addhead(&st->regiontypes, art);
 
   /* Channels. */
