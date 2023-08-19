9d08be1eec62ec4028372ed4e4175bc711c1afd3
Habib Gahbiche <habibgahbiche@gmail.com>
2023-08-19 15:00:54 +0200

    Realtime Compositor: add regression tests
    Tests must be enabled manually using the CMake flag `WITH_COMPOSITOR_REALTIME_TEST`. Reasons are F12 for realtime compositor is experimental and buildbots have no GPU. Failing tests are excluded.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/109878

--- tests/python/compositor_realtime_render_tests.py
+++ tests/python/compositor_realtime_render_tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2015-2022 Blender Authors
+# SPDX-FileCopyrightText: 2015-2023 Blender Foundation
 #
 # SPDX-License-Identifier: Apache-2.0
 
@@ -15,6 +15,22 @@ try:
 except ImportError:
     inside_blender = False
 
+BLACKLIST_UNSUPPORTED_RENDER_TESTS = [
+    'node_cryptomatte.blend',
+    'node_cryptomatte_legacy.blend',
+    'node_keying_screen.blend'
+]
+
+BLACKLIST_CRASHING_TESTS = [
+    'node_keying.blend',
+    'node_keying_edge.blend',
+    'node_keying_matte.blend'
+]
+
+ENABLE_REALTIME_COMPOSITOR_SCRIPT = "import bpy; " \
+    "bpy.context.preferences.experimental.use_experimental_compositors = True; " \
+    "bpy.data.scenes[0].node_tree.execution_mode = 'REALTIME'"
+
 
 def get_arguments(filepath, output_filepath):
     return [
@@ -25,11 +41,12 @@ def get_arguments(filepath, output_filepath):
         "--debug-memory",
         "--debug-exit-on-error",
         filepath,
-        "-P",
-        os.path.realpath(__file__),
+        "-P", os.path.realpath(__file__),
+        "--python-expr", ENABLE_REALTIME_COMPOSITOR_SCRIPT,
         "-o", output_filepath,
         "-F", "PNG",
-        "-f", "1"]
+        "-f", "1"
+    ]
 
 
 def create_argparse():
@@ -50,20 +67,10 @@ def main():
     idiff = args.idiff[0]
     output_dir = args.outdir[0]
 
+    blacklist_all = BLACKLIST_CRASHING_TESTS + BLACKLIST_UNSUPPORTED_RENDER_TESTS
     from modules import render_report
-    report = render_report.Report("Compositor", output_dir, idiff)
-    report.set_pixelated(True)
-    report.set_reference_dir("compositor_renders")
-
-    if os.path.basename(test_dir) == 'filter':
-        # Temporary change to pass OpenImageDenoise test with both 1.3 and 1.4.
-        report.set_fail_threshold(0.05)
-    elif os.path.basename(test_dir) == 'matte':
-        # The node_keying_matte.blend test is very sensitive to the exact values in the
-        # input image. It makes it hard to precisely match results on different systems
-        # (with and without SSE, i.e.), especially when OCIO has different precision for
-        # the exponent transform on different platforms.
-        report.set_fail_threshold(0.05)
+    report = render_report.Report("Compositor Realtime", output_dir, idiff, blacklist=blacklist_all)
+    report.set_reference_dir("compositor_realtime_renders")
 
     ok = report.run(test_dir, blender, get_arguments, batch=True)
 
