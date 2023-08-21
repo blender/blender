1c7adc61ae0f0b993b4ec07f7b178cadf694d010
Campbell Barton <campbell@blender.org>
2023-08-21 10:31:57 +1000

    Cleanup: de-duplicate comments & expand on why UV's are clipped

--- source/blender/editors/transform/transform_mode_shear.cc
+++ source/blender/editors/transform/transform_mode_shear.cc
@@ -294,10 +294,7 @@ static void apply_shear(TransInfo *t)
       apply_shear_value(t, t->values_final[0]);
     }
 
-    /* In proportional edit it can happen that */
-    /* vertices in the radius of the brush end */
-    /* outside the clipping area               */
-    /* XXX HACK - dg */
+    /* Not ideal, see #clipUVData code-comment. */
     if (t->flag & T_PROP_EDIT) {
       clipUVData(t);
     }
