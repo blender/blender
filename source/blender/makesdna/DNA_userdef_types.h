45f342655926bb3d65c40a11988113addc2f8b47
Miguel Pozo <pragma37@gmail.com>
2023-08-23 15:33:02 +0200

    Fix: UserDef_Experimental padding
    Fix compiler error after 85b02b893a6

--- source/blender/makesdna/DNA_userdef_types.h
+++ source/blender/makesdna/DNA_userdef_types.h
@@ -718,7 +718,6 @@ typedef struct UserDef_Experimental {
   char use_node_group_operators;
   char use_shader_node_previews;
   char use_extension_repos;
-  char _pad[7];
   /** `makesdna` does not allow empty structs. */
 } UserDef_Experimental;
 
