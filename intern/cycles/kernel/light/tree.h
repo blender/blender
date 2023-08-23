206ab6437b970421d8fca4ba202a16f9e62cfd42
Alaska <alaskayou01@gmail.com>
2023-08-23 11:54:20 +0200

    Fix illegal address error in Cycles Light Tree when no emitter is selected
    Discovered during an investigation into #111277
    in rare situations (E.G. When normals are NaN), an emitter
    won't be selected as part of `light_tree_cluster_select_emitter()`
    and as a result of that, an `emitter_index` of `-1` is passed to
    `kernel_data_fetch(light_tree_emitters, emitter_index)` resulting in
    an "illegal address" error on some devices.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/111292

--- intern/cycles/kernel/light/tree.h
+++ intern/cycles/kernel/light/tree.h
@@ -596,7 +596,7 @@ ccl_device int light_tree_cluster_select_emitter(KernelGlobals kg,
     has_importance |= ((importance[0] > 0) << i);
   }
 
-  if (total_importance[0] == 0.0f) {
+  if (total_importance[0] == 0.0f || selected_index == -1) {
     return -1;
   }
 
