8d8f03be9c389e8fb101e7cb5a51964a1ba95025
Miguel Pozo <pragma37@gmail.com>
2023-08-23 16:10:38 +0200

    Cleanup: Draw: Clarify comment

--- source/blender/draw/intern/draw_sculpt.hh
+++ source/blender/draw/intern/draw_sculpt.hh
@@ -34,7 +34,7 @@ ENUM_OPERATORS(SculptBatchFeature, SCULPT_BATCH_UV);
 /** Used by engines that don't use GPUMaterials, like the Workbench and Overlay engines. */
 Vector<SculptBatch> sculpt_batches_get(Object *ob, SculptBatchFeature features);
 
-/** Used by EEVEE. */
+/** Used by EEVEE and Workbench Material/Texture modes. */
 Vector<SculptBatch> sculpt_batches_per_material_get(Object *ob,
                                                     MutableSpan<GPUMaterial *> materials);
 
