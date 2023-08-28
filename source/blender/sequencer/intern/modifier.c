c655db61857a6b13520902118edf2925d4c38bda
ChengduLittleA <xp8110@outlook.com>
2023-08-28 10:05:22 +0200

    Fix #111601: VSE strip modifier copy ensure unique name
    Previously `SEQ_modifier_list_copy` in append mode does not ensure
    unique strip name, which will result in duplicated names in target
    modifier list, then `strip_modifier_remove(name="something")` can remove
    the wrong one later on. Now fixed using `BLI_uniquename`.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/111602

--- source/blender/sequencer/intern/modifier.c
+++ source/blender/sequencer/intern/modifier.c
@@ -1554,6 +1554,8 @@ void SEQ_modifier_list_copy(Sequence *seqn, Sequence *seq)
 
     smdn->next = smdn->prev = NULL;
     BLI_addtail(&seqn->modifiers, smdn);
+    BLI_uniquename(
+        &seqn->modifiers, smdn, "Strip Modifier", '.', offsetof(SequenceModifierData, name), 64);
   }
 }
 
