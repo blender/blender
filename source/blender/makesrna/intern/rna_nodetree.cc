0798aae01c3070d581013df8c18c432f0035b278
ChengduLittleA <xp8110@outlook.com>
2023-08-29 09:35:45 +0200

    Fix #111607: Do not unregister internal nodes
    Registering a node with a `bl_idname` same as a built-in node will lead
    to crash because `rna_Node_unregister` does not check `nt->rna_ext.data`
    to see whether it's not null (which indicates whether this node is
    registered with python or not). Now fixed.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/111615

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -1768,7 +1768,9 @@ static bool rna_Node_unregister(Main * /*bmain*/, StructRNA *type)
 {
   bNodeType *nt = static_cast<bNodeType *>(RNA_struct_blender_type_get(type));
 
-  if (!nt) {
+  /* `nt->rna_ext.data` is the python object. If it's nullptr then it's an
+   * internally registered node, thus can't unregister. */
+  if (!nt || !nt->rna_ext.data) {
     return false;
   }
 
