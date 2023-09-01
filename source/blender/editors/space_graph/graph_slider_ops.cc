d361d67e11f4756306a452527411b56571af38a6
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-09-01 12:00:33 +0200

    Fix: Cursor wrapping in Graph Editor slider operators
    For operators that allow overshoot it is important that the cursor wraps,
    so the user is not limited by the bounds of the screen.
    This was missed on a few of the latest additions.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/111785

--- source/blender/editors/space_graph/graph_slider_ops.cc
+++ source/blender/editors/space_graph/graph_slider_ops.cc
@@ -1061,7 +1061,7 @@ void GRAPH_OT_blend_offset(wmOperatorType *ot)
   ot->poll = graphop_editable_keyframes_poll;
 
   /* Flags. */
-  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO | OPTYPE_BLOCKING | OPTYPE_GRAB_CURSOR_X;
 
   RNA_def_float_factor(ot->srna,
                        "factor",
@@ -1438,7 +1438,7 @@ void GRAPH_OT_shear(wmOperatorType *ot)
   ot->poll = graphop_editable_keyframes_poll;
 
   /* Flags. */
-  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO | OPTYPE_BLOCKING | OPTYPE_GRAB_CURSOR_X;
 
   RNA_def_float_factor(ot->srna,
                        "factor",
