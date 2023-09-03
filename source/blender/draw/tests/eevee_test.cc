2637bbf0164cd22a9918896d81ef295cc55272dc
Clément Foucault <foucault.clem@gmail.com>
2023-09-03 13:38:21 +0200

    EEVEE-Next: Add LUT test
    This avoid the generation code to become desync with
    the precomputed tabled.
    
    This is also a good way to test any changes to them.

--- source/blender/draw/tests/eevee_test.cc
+++ source/blender/draw/tests/eevee_test.cc
@@ -16,6 +16,7 @@
 #include "draw_shader.h"
 #include "draw_testing.hh"
 #include "engines/eevee_next/eevee_instance.hh"
+#include "engines/eevee_next/eevee_lut.hh"
 
 namespace blender::draw {
 
@@ -1439,5 +1440,25 @@ static void test_eevee_surfel_list()
   GPU_render_end();
 }
 DRAW_TEST(eevee_surfel_list)
+
+static void test_eevee_lut_gen()
+{
+  GPU_render_begin();
+
+  Manager manager;
+
+  /* Check if LUT generation matches the header version. */
+  auto bsdf_ggx_gen = LookUpTable(manager, LUT_GGX_BRDF_SPLIT_SUM, {64, 64, 1}).data<float2>();
+  auto btdf_ggx_gen = LookUpTable(manager, LUT_GGX_BTDF_SPLIT_SUM, {64, 64, 16}).data<float2>();
+
+  Span<float2> bsdf_ggx_lut(reinterpret_cast<const float2 *>(bsdf_split_sum_ggx), 64 * 64);
+  Span<float2> btdf_ggx_lut(reinterpret_cast<const float2 *>(btdf_split_sum_ggx), 64 * 64 * 16);
+
+  EXPECT_NEAR_ARRAY_ND(bsdf_ggx_lut.data(), bsdf_ggx_gen.data(), bsdf_ggx_gen.size(), 2, 5e-4f);
+  EXPECT_NEAR_ARRAY_ND(btdf_ggx_lut.data(), btdf_ggx_gen.data(), btdf_ggx_gen.size(), 2, 5e-4f);
+
+  GPU_render_end();
+}
+DRAW_TEST(eevee_lut_gen)
 
 }  // namespace blender::draw
