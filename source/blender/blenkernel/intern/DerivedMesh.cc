blender/blender

commit 56659f1510b1d8ab1345efa9d6e7ed6050476d68

Revert "Fix #107185: Edit mode or existing attribute break rest position"
The commit that this reverts fixed the rest position attribute, which
wasn't added in edit mode, since the modifier evaluation is different.
The change was simple, but caused quite a few problems because it broke
the assumption in places that the evaluated result of the mesh modifier
stack in edit mode was also a BMesh rather than a mesh. In main this
has been resolved by a refactor, 91b27ab637334383b57b, but that change
is too complex to backport to 3.6. As discussed in the most recent nodes
module meeting, this reverts the original fix. That does mean that
the rest position attribute won't function properly in edit mode.

Fixes #110136, #110925, #109301, #109432

This reverts commit 82ca3d3604bded96f72650c23a7f95f29b956f30.

Pull Request: https://projects.blender.org/blender/blender/pulls/111396

source/blender/blenkernel/intern/DerivedMesh.cc
@@ -569,27 +569,6 @@ static Mesh *modifier_modify_mesh_and_geometry_set(ModifierData *md,
   return mesh_output;
 }
 
-static void set_rest_position(Mesh &mesh)
-{
-  using namespace blender;
-  using namespace blender::bke;
-  MutableAttributeAccessor attributes = mesh.attributes_for_write();
-  const AttributeReader positions = attributes.lookup<float3>("position");
-  attributes.remove("rest_position");
-  if (positions) {
-    if (positions.sharing_info && positions.varray.is_span()) {
-      attributes.add<float3>("rest_position",
-                             ATTR_DOMAIN_POINT,
-                             AttributeInitShared(positions.varray.get_internal_span().data(),
-                                                 *positions.sharing_info));
-    }
-    else {
-      attributes.add<float3>(
-          "rest_position", ATTR_DOMAIN_POINT, AttributeInitVArray(positions.varray));
-    }
-  }
-}
-
 static void mesh_calc_modifiers(struct Depsgraph *depsgraph,
                                 const Scene *scene,
                                 Object *ob,
@@ -678,7 +657,20 @@ static void mesh_calc_modifiers(struct Depsgraph *depsgraph,
       mesh_final = BKE_mesh_copy_for_eval(mesh_input);
       ASSERT_IS_VALID_MESH(mesh_final);
     }
-    set_rest_position(*mesh_final);
+    MutableAttributeAccessor attributes = mesh_final->attributes_for_write();
+    const AttributeReader positions = attributes.lookup<float3>("position");
+    if (positions) {
+      if (positions.sharing_info && positions.varray.is_span()) {
+        attributes.add<float3>("rest_position",
+                               ATTR_DOMAIN_POINT,
+                               AttributeInitShared(positions.varray.get_internal_span().data(),
+                                                   *positions.sharing_info));
+      }
+      else {
+        attributes.add<float3>(
+            "rest_position", ATTR_DOMAIN_POINT, AttributeInitVArray(positions.varray));
+      }
+    }
   }
 
   /* Apply all leading deform modifiers. */
@@ -1201,14 +1193,6 @@ static void editbmesh_calc_modifiers(struct Depsgraph *depsgraph,
 
   /* Clear errors before evaluation. */
   BKE_modifiers_clear_errors(ob);
-
-  if (ob->modifier_flag & OB_MODIFIER_FLAG_ADD_REST_POSITION) {
-    if (mesh_final == nullptr) {
-      mesh_final = BKE_mesh_from_bmesh_for_eval_nomain(em_input->bm, nullptr, mesh_input);
-      ASSERT_IS_VALID_MESH(mesh_final);
-    }
-    set_rest_position(*mesh_final);
-  }
 
   for (int i = 0; md; i++, md = md->next, md_datamask = md_datamask->next) {
     const ModifierTypeInfo *mti = BKE_modifier_get_info((ModifierType)md->type);

commit 439e37466cb109142d8e9db8e321fa4f559ce882

Fix #111933: wrong rest position
This was caused by 56659f1510b1d8ab1345efa9d6e7ed6050476d68.
The line added by this patch was added by the reverted 82ca3d3604bded96f72650c23a7f95f29b956f30, but should not have been reverted. It was not obvious enough that two different things were changed by the same patch.

Pull Request: https://projects.blender.org/blender/blender/pulls/111958

source/blender/blenkernel/intern/DerivedMesh.cc
@@ -659,6 +659,7 @@ static void mesh_calc_modifiers(struct Depsgraph *depsgraph,
     }
     MutableAttributeAccessor attributes = mesh_final->attributes_for_write();
     const AttributeReader positions = attributes.lookup<float3>("position");
+    attributes.remove("rest_position");
     if (positions) {
       if (positions.sharing_info && positions.varray.is_span()) {
         attributes.add<float3>("rest_position",