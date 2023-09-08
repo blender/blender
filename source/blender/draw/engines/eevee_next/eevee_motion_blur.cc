1faa52288008229007e0bd0acaf6fd938690fa39
Miguel Pozo <pragma37@gmail.com>
2023-09-08 20:29:29 +0200

    Fix: EEVEE-Next: Motion blur on image renders
    overscan_changed is always true the first frame.

--- source/blender/draw/engines/eevee_next/eevee_motion_blur.cc
+++ source/blender/draw/engines/eevee_next/eevee_motion_blur.cc
@@ -125,7 +125,9 @@ void MotionBlurModule::sync()
 {
   /* Disable motion blur in viewport when changing camera projection type.
    * Avoids really high velocities. */
-  if (inst_.velocity.camera_changed_projection() || inst_.camera.overscan_changed()) {
+  if (inst_.velocity.camera_changed_projection() ||
+      (inst_.is_viewport() && inst_.camera.overscan_changed()))
+  {
     motion_blur_fx_enabled_ = false;
   }
 
