6fcdcd4108f3c1545ba9421e7473179c797b43f2
Bastien Montagne <bastien@blender.org>
2023-09-11 19:50:29 +0200

    UI/I18N: Fix a couple of issues in new 'Add Modifiers' menu.
    The recent change to the 'Add Modifier' created at least two issues:
    * A complete duplicate of UI info for each modifier (its name and icon),
      now existing in both the RNA enum definition of modifiers types, and
      in the pyhton UI code.
    * An implicit duplication of these UI names in two different translation
      contexts, since the ones from the enum use the default one, while
      explicit labels passed to the `layout.operator` UI API get assigned a
      default 'operator' context. See PR !112246 for details about this.
    
    Both issues can be easily solved by making the new python code for these
    menus a bit smarter. Adding a helper function that adds the `add_modifier`
    operator with the right parameters, just based on the operator type.
    
    Both names (labels) and icons can be found in the enum property
    `bl_rna` definition itself then.
    
    This change:
    * Avoids duplicating UI info.
    * Fixes translation context mismatch, by forcing the usage of the
      default one also from the python code.
    * Makes code less verbose and overall more readable.
    
    NOTE: An attempt has been made to use the `get_name` callback of
    operator types to automatically return the right name based on the
    defined type, but this is currently utterly failing with regular
    layout-based UI code. This will be reported and handled separately.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/112252

--- scripts/startup/bl_ui/properties_data_modifier.py
+++ scripts/startup/bl_ui/properties_data_modifier.py
@@ -13,6 +13,25 @@ class ModifierButtonsPanel:
     bl_options = {'HIDE_HEADER'}
 
 
+class ModifierAddMenu:
+    MODIFIER_TYPES_TO_LABELS = {enum_it.identifier: enum_it.name
+                                for enum_it in bpy.types.Modifier.bl_rna.properties['type'].enum_items_static}
+    MODIFIER_TYPES_TO_ICONS = {enum_it.identifier: enum_it.icon
+                               for enum_it in bpy.types.Modifier.bl_rna.properties['type'].enum_items_static}
+    MODIFIER_TYPES_I18N_CONTEXT = bpy.types.Modifier.bl_rna.properties['type'].translation_context
+
+    @classmethod
+    def operator_modifier_add(cls, layout, mod_type):
+        layout.operator(
+            "object.modifier_add",
+            text=cls.MODIFIER_TYPES_TO_LABELS[mod_type],
+            # Although these are operators, the label actually comes from an (enum) property,
+            # so the property's translation context must be used here.
+            text_ctxt=cls.MODIFIER_TYPES_I18N_CONTEXT,
+            icon=cls.MODIFIER_TYPES_TO_ICONS[mod_type],
+        ).type = mod_type
+
+
 class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
     bl_label = "Modifiers"
 
@@ -27,7 +46,7 @@ class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
         layout.template_modifiers()
 
 
-class OBJECT_MT_modifier_add(Menu):
+class OBJECT_MT_modifier_add(ModifierAddMenu, Menu):
     bl_label = "Add Modifier"
     bl_options = {'SEARCH_ON_KEY_PRESS'}
 
@@ -36,7 +55,7 @@ class OBJECT_MT_modifier_add(Menu):
         ob_type = context.object.type
         geometry_nodes_supported = ob_type in {'MESH', 'CURVE', 'CURVES', 'FONT', 'SURFACE', 'VOLUME', 'POINTCLOUD'}
         if geometry_nodes_supported:
-            layout.operator("object.modifier_add", icon='GEOMETRY_NODES', text="Geometry Nodes").type = 'NODES'
+            self.operator_modifier_add(layout, 'NODES')
             layout.separator()
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
             layout.menu("OBJECT_MT_modifier_add_edit")
@@ -51,159 +70,122 @@ class OBJECT_MT_modifier_add(Menu):
             layout.menu_contents("OBJECT_MT_modifier_add_root_catalogs")
 
 
-class OBJECT_MT_modifier_add_edit(Menu):
+class OBJECT_MT_modifier_add_edit(ModifierAddMenu, Menu):
     bl_label = "Edit"
 
     def draw(self, context):
         layout = self.layout
         ob_type = context.object.type
         if ob_type == 'MESH':
-            layout.operator(
-                "object.modifier_add",
-                text="Data Transfer",
-                icon='MOD_DATA_TRANSFER',
-            ).type = 'DATA_TRANSFER'
+            self.operator_modifier_add(layout, 'DATA_TRANSFER')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
-            layout.operator("object.modifier_add", text="Mesh Cache", icon='MOD_MESHDEFORM').type = 'MESH_CACHE'
+            self.operator_modifier_add(layout, 'MESH_CACHE')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator(
-                "object.modifier_add",
-                text="Mesh Sequence Cache",
-                icon='MOD_MESHDEFORM',
-            ).type = 'MESH_SEQUENCE_CACHE'
+            self.operator_modifier_add(layout, 'MESH_SEQUENCE_CACHE')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Normal Edit", icon='MOD_NORMALEDIT').type = 'NORMAL_EDIT'
-            layout.operator(
-                "object.modifier_add",
-                text="Weighted Normal",
-                icon='MOD_NORMALEDIT',
-            ).type = 'WEIGHTED_NORMAL'
-            layout.operator("object.modifier_add", text="UV Project", icon='MOD_UVPROJECT').type = 'UV_PROJECT'
-            layout.operator("object.modifier_add", text="UV Warp", icon='MOD_UVPROJECT').type = 'UV_WARP'
-            layout.operator(
-                "object.modifier_add",
-                text="Vertex Weight Edit",
-                icon='MOD_VERTEX_WEIGHT',
-            ).type = 'VERTEX_WEIGHT_EDIT'
-            layout.operator(
-                "object.modifier_add",
-                text="Vertex Weight Mix",
-                icon='MOD_VERTEX_WEIGHT',
-            ).type = 'VERTEX_WEIGHT_MIX'
-            layout.operator(
-                "object.modifier_add",
-                text="Vertex Weight Proximity",
-                icon='MOD_VERTEX_WEIGHT',
-            ).type = 'VERTEX_WEIGHT_PROXIMITY'
+            self.operator_modifier_add(layout, 'NORMAL_EDIT')
+            self.operator_modifier_add(layout, 'WEIGHTED_NORMAL')
+            self.operator_modifier_add(layout, 'UV_PROJECT')
+            self.operator_modifier_add(layout, 'UV_WARP')
+            self.operator_modifier_add(layout, 'VERTEX_WEIGHT_EDIT')
+            self.operator_modifier_add(layout, 'VERTEX_WEIGHT_MIX')
+            self.operator_modifier_add(layout, 'VERTEX_WEIGHT_PROXIMITY')
         layout.template_modifier_asset_menu_items(catalog_path=self.bl_label)
 
 
-class OBJECT_MT_modifier_add_generate(Menu):
+class OBJECT_MT_modifier_add_generate(ModifierAddMenu, Menu):
     bl_label = "Generate"
 
     def draw(self, context):
         layout = self.layout
         ob_type = context.object.type
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator("object.modifier_add", text="Array", icon='MOD_ARRAY').type = 'ARRAY'
-            layout.operator("object.modifier_add", text="Bevel", icon='MOD_BEVEL').type = 'BEVEL'
+            self.operator_modifier_add(layout, 'ARRAY')
+            self.operator_modifier_add(layout, 'BEVEL')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Boolean", icon='MOD_BOOLEAN').type = 'BOOLEAN'
+            self.operator_modifier_add(layout, 'BOOLEAN')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator("object.modifier_add", text="Build", icon='MOD_BUILD').type = 'BUILD'
-            layout.operator("object.modifier_add", text="Decimate", icon='MOD_DECIM').type = 'DECIMATE'
-            layout.operator("object.modifier_add", text="Edge Split", icon='MOD_EDGESPLIT').type = 'EDGE_SPLIT'
+            self.operator_modifier_add(layout, 'BUILD')
+            self.operator_modifier_add(layout, 'DECIMATE')
+            self.operator_modifier_add(layout, 'EDGE_SPLIT')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Mask", icon='MOD_MASK').type = 'MASK'
+            self.operator_modifier_add(layout, 'MASK')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator("object.modifier_add", text="Mirror", icon='MOD_MIRROR').type = 'MIRROR'
+            self.operator_modifier_add(layout, 'MIRROR')
         if ob_type == 'VOLUME':
-            layout.operator("object.modifier_add", text="Mesh to Volume", icon='VOLUME_DATA').type = 'MESH_TO_VOLUME'
+            self.operator_modifier_add(layout, 'MESH_TO_VOLUME')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Multiresolution", icon='MOD_MULTIRES').type = 'MULTIRES'
+            self.operator_modifier_add(layout, 'MULTIRES')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator("object.modifier_add", text="Remesh", icon='MOD_REMESH').type = 'REMESH'
-            layout.operator("object.modifier_add", text="Screw", icon='MOD_SCREW').type = 'SCREW'
+            self.operator_modifier_add(layout, 'REMESH')
+            self.operator_modifier_add(layout, 'SCREW')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Skin", icon='MOD_SKIN').type = 'SKIN'
+            self.operator_modifier_add(layout, 'SKIN')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator("object.modifier_add", text="Solidify", icon='MOD_SOLIDIFY').type = 'SOLIDIFY'
-            layout.operator("object.modifier_add", text="Subdivision Surface", icon='MOD_SUBSURF').type = 'SUBSURF'
-            layout.operator("object.modifier_add", text="Triangulate", icon='MOD_TRIANGULATE').type = 'TRIANGULATE'
+            self.operator_modifier_add(layout, 'SOLIDIFY')
+            self.operator_modifier_add(layout, 'SUBSURF')
+            self.operator_modifier_add(layout, 'TRIANGULATE')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Volume to Mesh", icon='VOLUME_DATA').type = 'VOLUME_TO_MESH'
+            self.operator_modifier_add(layout, 'VOLUME_TO_MESH')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator("object.modifier_add", text="Weld", icon='AUTOMERGE_OFF').type = 'WELD'
+            self.operator_modifier_add(layout, 'WELD')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Wireframe", icon='MOD_WIREFRAME').type = 'WIREFRAME'
+            self.operator_modifier_add(layout, 'WIREFRAME')
         layout.template_modifier_asset_menu_items(catalog_path=self.bl_label)
 
 
-class OBJECT_MT_modifier_add_deform(Menu):
+class OBJECT_MT_modifier_add_deform(ModifierAddMenu, Menu):
     bl_label = "Deform"
 
     def draw(self, context):
         layout = self.layout
         ob_type = context.object.type
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
-            layout.operator("object.modifier_add", text="Armature", icon='MOD_ARMATURE').type = 'ARMATURE'
-            layout.operator("object.modifier_add", text="Cast", icon='MOD_CAST').type = 'CAST'
-            layout.operator("object.modifier_add", text="Curve", icon='MOD_CURVE').type = 'CURVE'
+            self.operator_modifier_add(layout, 'ARMATURE')
+            self.operator_modifier_add(layout, 'CAST')
+            self.operator_modifier_add(layout, 'CURVE')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Displace", icon='MOD_DISPLACE').type = 'DISPLACE'
+            self.operator_modifier_add(layout, 'DISPLACE')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
-            layout.operator("object.modifier_add", text="Hook", icon='HOOK').type = 'HOOK'
+            self.operator_modifier_add(layout, 'HOOK')
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Laplacian Deform",
-                            icon='MOD_MESHDEFORM').type = 'LAPLACIANDEFORM'
+            self.operator_modifier_add(layout, 'LAPLACIANDEFORM')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
-            layout.operator("object.modifier_add", text="Lattice", icon='MOD_LATTICE').type = 'LATTICE'
-            layout.operator("object.modifier_add", text="Mesh Deform", icon='MOD_MESHDEFORM').type = 'MESH_DEFORM'
-            layout.operator("object.modifier_add", text="Shrinkwrap", icon='MOD_SHRINKWRAP').type = 'SHRINKWRAP'
-            layout.operator("object.modifier_add", text="Simple Deform", icon='MOD_SIMPLEDEFORM').type = 'SIMPLE_DEFORM'
+            self.operator_modifier_add(layout, 'LATTICE')
+            self.operator_modifier_add(layout, 'MESH_DEFORM')
+            self.operator_modifier_add(layout, 'SHRINKWRAP')
+            self.operator_modifier_add(layout, 'SIMPLE_DEFORM')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
-            layout.operator("object.modifier_add", text="Smooth", icon='MOD_SMOOTH').type = 'SMOOTH'
+            self.operator_modifier_add(layout, 'SMOOTH')
         if ob_type == 'MESH':
-            layout.operator(
-                "object.modifier_add",
-                text="Smooth Corrective",
-                icon='MOD_SMOOTH',
-            ).type = 'CORRECTIVE_SMOOTH'
-            layout.operator("object.modifier_add", text="Smooth Laplacian", icon='MOD_SMOOTH').type = 'LAPLACIANSMOOTH'
-            layout.operator("object.modifier_add", text="Surface Deform", icon='MOD_MESHDEFORM').type = 'SURFACE_DEFORM'
+            self.operator_modifier_add(layout, 'CORRECTIVE_SMOOTH')
+            self.operator_modifier_add(layout, 'LAPLACIANSMOOTH')
+            self.operator_modifier_add(layout, 'SURFACE_DEFORM')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
-            layout.operator("object.modifier_add", text="Warp", icon='MOD_WARP').type = 'WARP'
-            layout.operator("object.modifier_add", text="Wave", icon='MOD_WAVE').type = 'WAVE'
+            self.operator_modifier_add(layout, 'WARP')
+            self.operator_modifier_add(layout, 'WAVE')
         if ob_type == 'VOLUME':
-            layout.operator("object.modifier_add", text="Volume Displace", icon='VOLUME_DATA').type = 'VOLUME_DISPLACE'
+            self.operator_modifier_add(layout, 'VOLUME_DISPLACE')
         layout.template_modifier_asset_menu_items(catalog_path=self.bl_label)
 
 
-class OBJECT_MT_modifier_add_physics(Menu):
+class OBJECT_MT_modifier_add_physics(ModifierAddMenu, Menu):
     bl_label = "Physics"
 
     def draw(self, context):
         layout = self.layout
         ob_type = context.object.type
         if ob_type == 'MESH':
-            layout.operator("object.modifier_add", text="Cloth", icon='MOD_CLOTH').type = 'CLOTH'
-            layout.operator("object.modifier_add", text="Collision", icon='MOD_PHYSICS').type = 'COLLISION'
-            layout.operator("object.modifier_add", text="Dynamic Paint", icon='MOD_DYNAMICPAINT').type = 'DYNAMIC_PAINT'
-            layout.operator("object.modifier_add", text="Explode", icon='MOD_EXPLODE').type = 'EXPLODE'
-            layout.operator("object.modifier_add", text="Fluid", icon='MOD_FLUIDSIM').type = 'FLUID'
-            layout.operator("object.modifier_add", text="Ocean", icon='MOD_OCEAN').type = 'OCEAN'
-            layout.operator(
-                "object.modifier_add",
-                text="Particle Instance",
-                icon='MOD_PARTICLE_INSTANCE',
-            ).type = 'PARTICLE_INSTANCE'
-            layout.operator(
-                "object.modifier_add",
-                text="Particle System",
-                icon='MOD_PARTICLES',
-            ).type = 'PARTICLE_SYSTEM'
+            self.operator_modifier_add(layout, 'CLOTH')
+            self.operator_modifier_add(layout, 'COLLISION')
+            self.operator_modifier_add(layout, 'DYNAMIC_PAINT')
+            self.operator_modifier_add(layout, 'EXPLODE')
+            self.operator_modifier_add(layout, 'FLUID')
+            self.operator_modifier_add(layout, 'OCEAN')
+            self.operator_modifier_add(layout, 'PARTICLE_INSTANCE')
+            self.operator_modifier_add(layout, 'PARTICLE_SYSTEM')
         if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
-            layout.operator("object.modifier_add", text="Soft Body", icon='MOD_SOFT').type = 'SOFT_BODY'
+            self.operator_modifier_add(layout, 'SOFT_BODY')
         layout.template_modifier_asset_menu_items(catalog_path=self.bl_label)
 
 
