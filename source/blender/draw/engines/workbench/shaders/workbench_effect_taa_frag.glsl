443254b1a67836ec195b1f908d288e369ef52df0
Miguel Pozo <pragma37@gmail.com>
2023-09-12 16:27:40 +0200

    Fix #112211: Workbench: Clamp infinite inputs in TAA

--- source/blender/draw/engines/workbench/shaders/workbench_effect_taa_frag.glsl
+++ source/blender/draw/engines/workbench/shaders/workbench_effect_taa_frag.glsl
@@ -11,8 +11,11 @@ void main()
   int i = 0;
   for (int x = -1; x <= 1; x++) {
     for (int y = -1; y <= 1; y++, i++) {
+      vec4 color = texture(colorBuffer, uv + vec2(x, y) * texel_size);
+      /* Clamp infinite inputs (See #112211). */
+      color = clamp(color, vec4(0.0), vec4(1e10));
       /* Use log2 space to avoid highlights creating too much aliasing. */
-      vec4 color = log2(texture(colorBuffer, uv + vec2(x, y) * texel_size) + 0.5);
+      color = log2(color + 0.5);
 
       fragColor += color * samplesWeights[i];
     }
