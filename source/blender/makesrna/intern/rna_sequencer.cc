9f06aec32f859b3034778790efd5e57ea8e9cac8
Campbell Barton <campbell@blender.org>
2023-09-19 17:21:31 +1000

    Cleanup: de-duplicate sequencer modifier enum items

--- source/blender/makesrna/intern/rna_sequencer.cc
+++ source/blender/makesrna/intern/rna_sequencer.cc
@@ -64,31 +64,34 @@ struct EffectInfo {
   int inputs;
 };
 
+/* These wrap strangely, disable formatting for fixed indentation and wrapping.  */
+/* clang-format off */
+#define RNA_ENUM_SEQUENCER_VIDEO_MODIFIER_TYPE_ITEMS \
+  {seqModifierType_BrightContrast, "BRIGHT_CONTRAST", ICON_NONE, "Bright/Contrast", ""}, \
+  {seqModifierType_ColorBalance, "COLOR_BALANCE", ICON_NONE, "Color Balance", ""}, \
+  {seqModifierType_Curves, "CURVES", ICON_NONE, "Curves", ""}, \
+  {seqModifierType_HueCorrect, "HUE_CORRECT", ICON_NONE, "Hue Correct", ""}, \
+  {seqModifierType_Mask, "MASK", ICON_NONE, "Mask", ""}, \
+  {seqModifierType_Tonemap, "TONEMAP", ICON_NONE, "Tone Map", ""}, \
+  {seqModifierType_WhiteBalance, "WHITE_BALANCE", ICON_NONE, "White Balance", ""}
+
+#define RNA_ENUM_SEQUENCER_AUDIO_MODIFIER_TYPE_ITEMS \
+  {seqModifierType_SoundEqualizer, "SOUND_EQUALIZER", ICON_NONE, "Equalizer", ""}
+/* clang-format on */
+
 const EnumPropertyItem rna_enum_sequence_modifier_type_items[] = {
-    {seqModifierType_BrightContrast, "BRIGHT_CONTRAST", ICON_NONE, "Brightness/Contrast", ""},
-    {seqModifierType_ColorBalance, "COLOR_BALANCE", ICON_NONE, "Color Balance", ""},
-    {seqModifierType_Curves, "CURVES", ICON_NONE, "Curves", ""},
-    {seqModifierType_HueCorrect, "HUE_CORRECT", ICON_NONE, "Hue Correct", ""},
-    {seqModifierType_Mask, "MASK", ICON_NONE, "Mask", ""},
-    {seqModifierType_Tonemap, "TONEMAP", ICON_NONE, "Tone Map", ""},
-    {seqModifierType_WhiteBalance, "WHITE_BALANCE", ICON_NONE, "White Balance", ""},
-    {seqModifierType_SoundEqualizer, "SOUND_EQUALIZER", ICON_NONE, "Sound Equalizer", ""},
+    RNA_ENUM_SEQUENCER_VIDEO_MODIFIER_TYPE_ITEMS,
+    RNA_ENUM_SEQUENCER_AUDIO_MODIFIER_TYPE_ITEMS,
     {0, nullptr, 0, nullptr, nullptr},
 };
 
 const EnumPropertyItem rna_enum_sequence_video_modifier_type_items[] = {
-    {seqModifierType_BrightContrast, "BRIGHT_CONTRAST", ICON_NONE, "Bright/Contrast", ""},
-    {seqModifierType_ColorBalance, "COLOR_BALANCE", ICON_NONE, "Color Balance", ""},
-    {seqModifierType_Curves, "CURVES", ICON_NONE, "Curves", ""},
-    {seqModifierType_HueCorrect, "HUE_CORRECT", ICON_NONE, "Hue Correct", ""},
-    {seqModifierType_Mask, "MASK", ICON_NONE, "Mask", ""},
-    {seqModifierType_Tonemap, "TONEMAP", ICON_NONE, "Tone Map", ""},
-    {seqModifierType_WhiteBalance, "WHITE_BALANCE", ICON_NONE, "White Balance", ""},
+    RNA_ENUM_SEQUENCER_VIDEO_MODIFIER_TYPE_ITEMS,
     {0, nullptr, 0, nullptr, nullptr},
 };
 
 const EnumPropertyItem rna_enum_sequence_sound_modifier_type_items[] = {
-    {seqModifierType_SoundEqualizer, "SOUND_EQUALIZER", ICON_NONE, "Equalizer", ""},
+    RNA_ENUM_SEQUENCER_AUDIO_MODIFIER_TYPE_ITEMS,
     {0, nullptr, 0, nullptr, nullptr},
 };
 
