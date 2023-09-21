e7df63ca724e2c39deb2f87e0001436df8873093
Harley Acheson <harley.acheson@gmail.com>
2023-09-21 18:48:07 +0200

    Revert "UI: File Loading Waiting Feedback"
    This reverts commit 9173c142d6 as this can cause problems from scripts
    and command-line.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/112682

--- source/blender/windowmanager/intern/wm_files.cc
+++ source/blender/windowmanager/intern/wm_files.cc
@@ -692,8 +692,6 @@ static void wm_file_read_post(bContext *C,
     CTX_wm_window_set(C, static_cast<wmWindow *>(wm->windows.first));
   }
 
-  WM_cursor_wait(true);
-
 #ifdef WITH_PYTHON
   if (is_startup_file) {
     /* On startup (by default), Python won't have been initialized.
@@ -787,12 +785,6 @@ static void wm_file_read_post(bContext *C,
 #endif
   }
 
-#ifndef WITH_HEADLESS
-  if (!G.background) {
-    WM_redraw_windows(C);
-  }
-#endif /* WITH_HEADLESS */
-
   /* report any errors.
    * currently disabled if addons aren't yet loaded */
   if (addons_loaded) {
@@ -826,8 +818,6 @@ static void wm_file_read_post(bContext *C,
       WM_toolsystem_init(C);
     }
   }
-
-  WM_cursor_wait(false);
 }
 
 static void wm_read_callback_pre_wrapper(bContext *C, const char *filepath)
@@ -1012,26 +1002,6 @@ bool WM_file_read(bContext *C, const char *filepath, ReportList *reports)
   /* so we can get the error message */
   errno = 0;
 
-#ifndef WITH_HEADLESS
-  if (!G.background) {
-    wmWindowManager *wm = CTX_wm_manager(C);
-    wmWindow *win = CTX_wm_window(C);
-    bool win_was_null = (win == nullptr);
-    if (win_was_null) {
-      win = static_cast<wmWindow *>(wm->windows.first);
-      CTX_wm_window_set(C, win);
-    }
-    if (win != nullptr) {
-      /* Redraw to remove any open menus. */
-      WM_redraw_windows(C);
-
-      if (win_was_null) {
-        CTX_wm_window_set(C, nullptr);
-      }
-    }
-  }
-#endif /* WITH_HEADLESS */
-
   WM_cursor_wait(true);
 
   /* first try to append data from exotic file formats... */
@@ -1097,7 +1067,6 @@ bool WM_file_read(bContext *C, const char *filepath, ReportList *reports)
       bf_reports.duration.whole = PIL_check_seconds_timer() - bf_reports.duration.whole;
       file_read_reports_finalize(&bf_reports);
 
-      WM_cursor_wait(true);
       success = true;
     }
   }
@@ -1201,8 +1170,6 @@ void wm_homefile_read_ex(bContext *C,
   bool filepath_startup_is_factory = true;
   char filepath_startup[FILE_MAX];
   char filepath_userdef[FILE_MAX];
-
-  WM_cursor_wait(true);
 
   /* When 'app_template' is set:
    * '{BLENDER_USER_CONFIG}/{app_template}' */
@@ -1521,8 +1488,6 @@ void wm_homefile_read_ex(bContext *C,
       CTX_wm_window_set(C, nullptr);
     }
   }
-
-  WM_cursor_wait(false);
 }
 
 void wm_homefile_read(bContext *C,
