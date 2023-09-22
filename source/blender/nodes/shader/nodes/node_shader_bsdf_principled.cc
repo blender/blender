d3b14bd1a986f2419355b88845d2517a88ada0b0
Brecht Van Lommel <brecht@blender.org>
2023-09-22 17:54:37 +0200

    Shaders: move subsurface scale input below radius in Principled BSDF
    To help clarify that this is a scale on the radius.
    
    Ref #99447
    Ref #112848

--- source/blender/nodes/shader/nodes/node_shader_bsdf_principled.cc
+++ source/blender/nodes/shader/nodes/node_shader_bsdf_principled.cc
@@ -69,20 +69,20 @@ static void node_declare(NodeDeclarationBuilder &b)
           "Blend between diffuse surface and subsurface scattering. "
           "Typically should be zero or one (either fully diffuse or subsurface)");
 #define SOCK_SUBSURFACE_ID 8
-  sss.add_input<decl::Float>("Subsurface Scale")
-      .default_value(0.05f)
-      .min(0.0f)
-      .max(10.0f)
-      .subtype(PROP_DISTANCE)
-      .description("Scale of the subsurface scattering (multiplied with Radius)");
-#define SOCK_SUBSURFACE_SCALE_ID 9
   sss.add_input<decl::Vector>("Subsurface Radius")
       .default_value({1.0f, 0.2f, 0.1f})
       .min(0.0f)
       .max(100.0f)
       .compact()
       .description("Scattering radius to use for subsurface component (multiplied with Scale)");
-#define SOCK_SUBSURFACE_RADIUS_ID 10
+#define SOCK_SUBSURFACE_RADIUS_ID 9
+  sss.add_input<decl::Float>("Subsurface Scale")
+      .default_value(0.05f)
+      .min(0.0f)
+      .max(10.0f)
+      .subtype(PROP_DISTANCE)
+      .description("Scale of the subsurface scattering (multiplied with Radius)");
+#define SOCK_SUBSURFACE_SCALE_ID 10
   sss.add_input<decl::Float>("Subsurface IOR")
       .default_value(1.4f)
       .min(1.01f)
