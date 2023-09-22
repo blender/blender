zyxcambridge/blender_llm

Fix #101550: Transfer Mode operator doesn't update outliner selection
Notifiers was sent to outliner for redraw but outliner was out of sync
due to missing flag which is required for syncing.
This will correctly set the select/active outliner element flags with
the help of Base in `outliner_select_sync_from_object`. So correct active
tree element will be highlighted in `outliner_draw_highlights`

Pull Request: https://projects.blender.org/blender/blender/pulls/112370

source/blender/editors/object/object_modes.cc
@@ -49,6 +49,7 @@
 #include "ED_transform_snap_object_context.hh"
 #include "ED_undo.hh"
 #include "ED_view3d.hh"
+#include "ED_outliner.hh"
 
 #include "WM_toolsystem.h"
 
@@ -492,6 +493,8 @@ static bool object_transfer_mode_to_base(bContext *C, wmOperator *op, Base *base
     }
 
     WM_event_add_notifier(C, NC_SCENE | ND_OB_SELECT, scene);
+    ED_outliner_select_sync_from_object_tag(C);
+
     WM_toolsystem_update_from_context_view3d(C);
     mode_transferred = true;
   }

blender/blender

source/blender/editors/object/object_modes.cc
@@ -46,6 +46,7 @@
 #include "ED_transform_snap_object_context.h"
 #include "ED_undo.h"
 #include "ED_view3d.h"
+#include "ED_outliner.h"
 
 #include "WM_toolsystem.h"
 
@@ -475,6 +476,8 @@ static bool object_transfer_mode_to_base(bContext *C, wmOperator *op, Base *base
     }
 
     WM_event_add_notifier(C, NC_SCENE | ND_OB_SELECT, scene);
+    ED_outliner_select_sync_from_object_tag(C);
+
     WM_toolsystem_update_from_context_view3d(C);
     mode_transferred = true;
   }