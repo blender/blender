bb6a1ffb381874b71c1eb7576e2ed2e4cb7a6c5c
Hans Goudey <h.goudey@me.com>
2023-09-25 13:39:44 +0200

    Geometry Nodes: Add option to force inputs to be single values
    Add a checkbox to node group inputs to force inputs to be single values
    and not accept varying fields. While this could also be done more
    automatically, and it could be argued that requiring the flexibility is
    helpful for users who want to use attributes, there are cases where the
    field inferencing doesn't work, or the "use attribute" option is just
    unhelpful. People use workarounds with primitive nodes to get the same
    behavior anyway.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/112745

--- source/blender/makesrna/intern/rna_node_tree_interface.cc
+++ source/blender/makesrna/intern/rna_node_tree_interface.cc
@@ -912,6 +912,13 @@ static void rna_def_node_interface_socket(BlenderRNA *brna)
                            "Don't show the input value in the geometry nodes modifier interface");
   RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_NodeTreeInterfaceItem_update");
 
+  prop = RNA_def_property(srna, "force_non_field", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_sdna(prop, nullptr, "flag", NODE_INTERFACE_SOCKET_SINGLE_VALUE_ONLY);
+  RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
+  RNA_def_property_ui_text(
+      prop, "Single Value", "Only allow single value inputs rather than fields");
+  RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_NodeTreeInterfaceItem_update");
+
   prop = RNA_def_property(srna, "attribute_domain", PROP_ENUM, PROP_NONE);
   RNA_def_property_enum_items(prop, rna_enum_attribute_domain_items);
   RNA_def_property_ui_text(
