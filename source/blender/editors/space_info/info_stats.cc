e64e39e3d0c326b926c0fb6ff393621086a72cab
Harley Acheson <harley.acheson@gmail.com>
2023-09-26 23:07:29 +0200

    UI: Change Overlay Text Size with Label Text Style
    Text Overlays are currently hard-coded to 11 points. This allows them
    to be set with Preferences / Themes / Text Styles, Widget Label.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/112875

--- source/blender/editors/space_info/info_stats.cc
+++ source/blender/editors/space_info/info_stats.cc
@@ -760,7 +760,7 @@ void ED_info_draw_stats(
   Object *ob = BKE_view_layer_active_object_get(view_layer);
   Object *obedit = OBEDIT_FROM_OBACT(ob);
   eObjectMode object_mode = ob ? (eObjectMode)ob->mode : OB_MODE_OBJECT;
-  const int font_id = BLF_set_default();
+  const int font_id = BLF_default();
 
   UI_FontThemeColor(font_id, TH_TEXT_HI);
   BLF_enable(font_id, BLF_SHADOW);
