cea02c15f810beba5dd65a300a6389ccc29970ad
Pratik Borhade <pratikborhade302@gmail.com>
2023-09-27 12:07:12 +0200

    Fix #112896: Search menu: First character remains in text field
    Search menu (F3) displays character which was pressed to open a context
    specific search. This also breaks the old behavior of showing last searched
    operator in text field, instead shows the pressed character.
    To fix this, first store the `g_search_text/but->poin` string in
    temp_buffer (before pressed key char is copied to `g_search_text`).
    Later, restore the g_search_text after invoking the search menu.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/112938

--- source/blender/windowmanager/intern/wm_operators.cc
+++ source/blender/windowmanager/intern/wm_operators.cc
@@ -1856,6 +1856,8 @@ static int wm_search_menu_invoke(bContext *C, wmOperator *op, const wmEvent *eve
   }
 
   static SearchPopupInit_Data data{};
+  char temp_buffer[256] = "";
+  STRNCPY(temp_buffer, g_search_text);
 
   if (search_type == SEARCH_TYPE_SINGLE_MENU) {
     {
@@ -1875,6 +1877,10 @@ static int wm_search_menu_invoke(bContext *C, wmOperator *op, const wmEvent *eve
   data.size[1] = UI_searchbox_size_y();
 
   UI_popup_block_invoke_ex(C, wm_block_search_menu, &data, nullptr, false);
+
+  /* g_search_text contains pressed letter here, copy previous searched
+   * value back to it, this will retain last searched result. see: #112896 */
+  STRNCPY(g_search_text, temp_buffer);
 
   return OPERATOR_INTERFACE;
 }
