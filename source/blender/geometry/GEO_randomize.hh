63e1d062337bc3cd714efd075c882f11bdeb088e
Jacques Lucke <jacques@blender.org>
2023-09-30 16:29:08 +0200

    Geometry Nodes: add utility for debug randomization of instances
    Also see cc7da09c1b820b91b8cfc7639893284e2bfc7ac2.

--- source/blender/geometry/GEO_randomize.hh
+++ source/blender/geometry/GEO_randomize.hh
@@ -8,7 +8,8 @@ struct Mesh;
 struct PointCloud;
 namespace blender::bke {
 class CurvesGeometry;
-}
+class Instances;
+}  // namespace blender::bke
 
 namespace blender::geometry {
 
@@ -20,5 +21,6 @@ void debug_randomize_face_order(Mesh *mesh);
 void debug_randomize_mesh_order(Mesh *mesh);
 void debug_randomize_point_order(PointCloud *pointcloud);
 void debug_randomize_curve_order(bke::CurvesGeometry *curves);
+void debug_randomize_instance_order(bke::Instances *instances);
 
 };  // namespace blender::geometry
