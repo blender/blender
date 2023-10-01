206ec6cf00a7f898a5665d399d7ad085f8e76c04
Clément Foucault <foucault.clem@gmail.com>
2023-10-01 15:27:21 +0200

    GL: Add subpass input emulation
    This adds basic emulation of the subpass input feature
    of vulkan and to a lower extend Raster Order Group on Metal.
    
    This help test paths that might use this feature in the future
    (like shadow rendering) on all platform and or simplify higher
    level code for supporting older hardware.
    
    This add clear description to the load/store ops and to the
    new `GPUAttachementState`.
    
    The OpenGL backend will correctly mask un-writable
    attachments and will bind as texture readable attachments.
    
    Even if possible by the vulkan standard, the GPU API prohibit
    the read and write to the same attachment inside the same
    subpass.
    
    In the GL backend, this is implemented using `glTextureBarrier`
    and `texelFetch` as it is described in the ARB_texture_barrier
    extension.
    https://registry.khronos.org/OpenGL/extensions/ARB/ARB_texture_barrier.txt
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/112051

--- source/blender/gpu/opengl/gl_backend.cc
+++ source/blender/gpu/opengl/gl_backend.cc
@@ -271,6 +271,8 @@ static void detect_workarounds()
 
     /* Turn off vendor specific extensions. */
     GLContext::native_barycentric_support = false;
+    GLContext::framebuffer_fetch_support = false;
+    GLContext::texture_barrier_support = false;
 
     /* Do not alter OpenGL 4.3 features.
      * These code paths should be removed. */
@@ -510,6 +512,7 @@ bool GLContext::copy_image_support = false;
 bool GLContext::debug_layer_support = false;
 bool GLContext::direct_state_access_support = false;
 bool GLContext::explicit_location_support = false;
+bool GLContext::framebuffer_fetch_support = false;
 bool GLContext::geometry_shader_invocations = false;
 bool GLContext::fixed_restart_index_support = false;
 bool GLContext::layered_rendering_support = false;
@@ -519,6 +522,7 @@ bool GLContext::multi_bind_image_support = false;
 bool GLContext::multi_draw_indirect_support = false;
 bool GLContext::shader_draw_parameters_support = false;
 bool GLContext::stencil_texturing_support = false;
+bool GLContext::texture_barrier_support = false;
 bool GLContext::texture_cube_map_array_support = false;
 bool GLContext::texture_filter_anisotropic_support = false;
 bool GLContext::texture_gather_support = false;
@@ -609,6 +613,8 @@ void GLBackend::capabilities_init()
   GLContext::explicit_location_support = epoxy_gl_version() >= 43;
   GLContext::geometry_shader_invocations = epoxy_has_gl_extension("GL_ARB_gpu_shader5");
   GLContext::fixed_restart_index_support = epoxy_has_gl_extension("GL_ARB_ES3_compatibility");
+  GLContext::framebuffer_fetch_support = epoxy_has_gl_extension("GL_EXT_shader_framebuffer_fetch");
+  GLContext::texture_barrier_support = epoxy_has_gl_extension("GL_ARB_texture_barrier");
   GLContext::layered_rendering_support = epoxy_has_gl_extension(
       "GL_ARB_shader_viewport_layer_array");
   GLContext::native_barycentric_support = epoxy_has_gl_extension(
@@ -627,6 +633,9 @@ void GLBackend::capabilities_init()
   GLContext::texture_storage_support = epoxy_gl_version() >= 43;
   GLContext::vertex_attrib_binding_support = epoxy_has_gl_extension(
       "GL_ARB_vertex_attrib_binding");
+
+  /* Disabled until it is proven to work. */
+  GLContext::framebuffer_fetch_support = false;
 
   detect_workarounds();
 
