5db605110649b720d1e8471b0ebf135cd4e2b6b3
Hans Goudey <h.goudey@me.com>
2023-10-02 17:36:44 -0400

    Fix #113142: Sculpt fairing operators don't update normals
    Tagging the PBVH nodes isn't enough, each vertex has to be tagged too.

--- source/blender/editors/sculpt_paint/sculpt_face_set.cc
+++ source/blender/editors/sculpt_paint/sculpt_face_set.cc
@@ -1448,7 +1448,8 @@ static void sculpt_face_set_edit_fair_face_set(Object *ob,
     float *co = ss->bm ? reinterpret_cast<BMVert *>(vertex.i)->co : positions[i];
 
     if (fair_verts[i]) {
-      interp_v3_v3v3(co, orig_positions[i], co, strength);
+      interp_v3_v3v3(positions[i], orig_positions[i], positions[i], strength);
+      BKE_pbvh_vert_tag_update_normal(ss->pbvh, BKE_pbvh_index_to_vertex(ss->pbvh, i));
     }
   }
 }
