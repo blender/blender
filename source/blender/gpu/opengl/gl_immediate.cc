a309a381d57f7d4aa11428024b6a2006d93f3f0a
Clément Foucault <foucault.clem@gmail.com>
2023-10-02 10:29:57 +0200

    GL: Remove Apple platform specific workarounds
    We don't ship with OpenGL anymore on Mac platform.
    The build option is not even available anymore.
    
    This removes any reference to Apple or Mac in the
    Opengl module.
    
    Note that I left the depth_blitting_workaround
    even if it originaly meant for Mac because it
    might be still useful for other hardware.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/113126

--- source/blender/gpu/opengl/gl_immediate.cc
+++ source/blender/gpu/opengl/gl_immediate.cc
@@ -147,13 +147,8 @@ void GLImmediate::end()
     /* Update matrices. */
     GPU_shader_bind(shader);
 
-#ifdef __APPLE__
-    glDisable(GL_PRIMITIVE_RESTART);
-#endif
     glDrawArrays(to_gl(prim_type), 0, vertex_len);
-#ifdef __APPLE__
-    glEnable(GL_PRIMITIVE_RESTART);
-#endif
+
     /* These lines are causing crash on startup on some old GPU + drivers.
      * They are not required so just comment them. (#55722) */
     // glBindBuffer(GL_ARRAY_BUFFER, 0);
