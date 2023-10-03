779239f5e180ef49abbd3766ac8dcc92ac33f331
Campbell Barton <campbell@blender.org>
2023-10-03 11:16:36 +1100

    Cleanup: spelling in comments

--- source/blender/compositor/realtime_compositor/shaders/compositor_jump_flooding.glsl
+++ source/blender/compositor/realtime_compositor/shaders/compositor_jump_flooding.glsl
@@ -12,7 +12,7 @@
  * The shader is a straightforward implementation of the aforementioned sections of the paper,
  * noting that the nil special value in the paper is equivalent to JUMP_FLOODING_NON_FLOODED_VALUE.
  *
- * The gpu_shader_compositor_jump_flooding_lib.glsl library contains the necessary utility
+ * The `gpu_shader_compositor_jump_flooding_lib.glsl` library contains the necessary utility
  * functions to initialize, encode, and extract the information in the jump flooding values. */
 
 #pragma BLENDER_REQUIRE(common_math_lib.glsl)
@@ -32,8 +32,8 @@ void main()
     for (int i = -1; i <= 1; i++) {
       ivec2 offset = ivec2(i, j) * step_size;
 
-      /* Use JUMP_FLOODING_NON_FLOODED_VALUE as a fallback value to exempt out of bound pixels from
-       * the loop as can be seen in the following continue condition. */
+      /* Use #JUMP_FLOODING_NON_FLOODED_VALUE as a fallback value to exempt out of bound pixels
+       * from the loop as can be seen in the following continue condition. */
       vec4 value = texture_load(input_tx, texel + offset, JUMP_FLOODING_NON_FLOODED_VALUE);
 
       /* The pixel is either not flooded yet or is out of bound, so skip it. */
@@ -41,7 +41,7 @@ void main()
         continue;
       }
 
-      /* Extract the position of the closest seed pixel to this neighbouring pixel and compute the
+      /* Extract the position of the closest seed pixel to this neighboring pixel and compute the
        * squared distance from that position to the center pixel. */
       ivec2 position = extract_jump_flooding_closest_seed_texel(value);
       float squared_distance = distance_squared(vec2(position), vec2(texel));
@@ -53,7 +53,7 @@ void main()
     }
   }
 
-  /* If the minimum squared distance is still FLT_MAX, that means the loop never got past the
+  /* If the minimum squared distance is still #FLT_MAX, that means the loop never got past the
    * continue condition and thus no flooding happened. If flooding happened, we write the closest
    * seed position as well as the distance to it. */
   bool flooding_happened = minimum_squared_distance != FLT_MAX;
