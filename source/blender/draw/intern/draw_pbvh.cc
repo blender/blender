9d201dc16b31fc031c830374f83aea80c099dd7a
Hans Goudey <hans@blender.org>
2023-10-06 18:16:30 +0200

    Cleanup: Simplify BMesh PBVH draw data upload
    Instead of using a lambda with a FunctionRef argument, just write the
    loops explicitly. This results in a bit more boilerplate code and a bit
    more repetition, but the overall design and flow is much simpler. Based
    on the results in f10965dcb89bcd09cd69, it can improve performance too.

--- source/blender/draw/intern/draw_pbvh.cc
+++ source/blender/draw/intern/draw_pbvh.cc
@@ -709,7 +709,8 @@ struct PBVHBatches {
           extract_data_vert_faces<float, float>(args, {mask, args.me->totvert}, vert_buf);
         }
         else {
-          MutableSpan(static_cast<float *>(GPU_vertbuf_get_data(vbo.vert_buf)), totvert).fill(0);
+          MutableSpan(static_cast<float *>(GPU_vertbuf_get_data(vbo.vert_buf)), totvert)
+              .fill(0.0f);
         }
         break;
       }
@@ -801,20 +802,6 @@ struct PBVHBatches {
 
   void fill_vbo_bmesh(PBVHVbo &vbo, const PBVH_GPU_Args &args)
   {
-    auto foreach_bmesh = [&](FunctionRef<void(BMLoop * l)> callback) {
-      GSET_FOREACH_BEGIN (BMFace *, f, args.bm_faces) {
-        if (BM_elem_flag_test(f, BM_ELEM_HIDDEN)) {
-          continue;
-        }
-
-        BMLoop *l = f->l_first;
-        callback(l->prev);
-        callback(l);
-        callback(l->next);
-      }
-      GSET_FOREACH_END();
-    };
-
     faces_count = tris_count = count_faces(args);
 
     int existing_num = GPU_vertbuf_get_vertex_len(vbo.vert_buf);
@@ -867,34 +854,54 @@ struct PBVHBatches {
     }
 
     switch (vbo.type) {
-      case CD_PBVH_CO_TYPE:
-        foreach_bmesh(
-            [&](BMLoop *l) { *static_cast<float3 *>(GPU_vertbuf_raw_step(&access)) = l->v->co; });
+      case CD_PBVH_CO_TYPE: {
+        float3 *data = static_cast<float3 *>(GPU_vertbuf_get_data(vbo.vert_buf));
+        GSET_FOREACH_BEGIN (const BMFace *, f, args.bm_faces) {
+          if (BM_elem_flag_test(f, BM_ELEM_HIDDEN)) {
+            continue;
+          }
+          const BMLoop *l = f->l_first;
+          *data = l->prev->v->co;
+          data++;
+          *data = l->v->co;
+          data++;
+          *data = l->next->v->co;
+          data++;
+        }
+        GSET_FOREACH_END();
         break;
-
-      case CD_PBVH_NO_TYPE:
-        foreach_bmesh([&](BMLoop *l) {
-          short no[3];
-          bool smooth = BM_elem_flag_test(l->f, BM_ELEM_SMOOTH);
-
-          normal_float_to_short_v3(no, smooth ? l->v->no : l->f->no);
-          *static_cast<short3 *>(GPU_vertbuf_raw_step(&access)) = no;
-        });
+      }
+      case CD_PBVH_NO_TYPE: {
+        short4 *data = static_cast<short4 *>(GPU_vertbuf_get_data(vbo.vert_buf));
+        GSET_FOREACH_BEGIN (const BMFace *, f, args.bm_faces) {
+          if (BM_elem_flag_test(f, BM_ELEM_HIDDEN)) {
+            continue;
+          }
+          if (BM_elem_flag_test(f, BM_ELEM_SMOOTH)) {
+            const BMLoop *l = f->l_first;
+            *data = convert_value<float3, short4>(l->prev->v->no);
+            data++;
+            *data = convert_value<float3, short4>(l->v->no);
+            data++;
+            *data = convert_value<float3, short4>(l->next->v->no);
+            data++;
+          }
+          else {
+            *data = *(data + 1) = *(data + 2) = convert_value<float3, short4>(f->no);
+            data += 3;
+          }
+        }
+        GSET_FOREACH_END();
         break;
-
+      }
       case CD_PBVH_MASK_TYPE: {
-        int cd_mask = args.cd_mask_layer;
-
-        if (cd_mask == -1) {
-          foreach_bmesh(
-              [&](BMLoop * /*l*/) { *static_cast<float *>(GPU_vertbuf_raw_step(&access)) = 0; });
+        if (args.cd_mask_layer != -1) {
+          extract_data_vert_bmesh<float, float>(args, args.cd_mask_layer, *vbo.vert_buf);
         }
         else {
-          foreach_bmesh([&](BMLoop *l) {
-            float mask = BM_ELEM_CD_GET_FLOAT(l->v, cd_mask);
-
-            *static_cast<float *>(GPU_vertbuf_raw_step(&access)) = mask;
-          });
+          MutableSpan(static_cast<float *>(GPU_vertbuf_get_data(vbo.vert_buf)),
+                      GPU_vertbuf_get_vertex_len(vbo.vert_buf))
+              .fill(0.0f);
         }
         break;
       }
