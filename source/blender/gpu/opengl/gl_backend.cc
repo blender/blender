76a1824c522aaeb982353059a6c86d9288f1161f
Jeroen Bakker <jeroen@blender.org>
2023-10-12 07:58:33 +0200

    GPU: Force Workarounds Viewport Layer Array
    The OpenGL extension `GL_ARB_shader_viewport_layer_array` wasn't turned
    off when starting blender with `--debug-gpu-force-workarounds`. Although
    all known supported platforms support this extension and we haven't seen
    any failures with it, it was mentioned to be phased out as we consider
    it to be a core extension, which it isn't.
    
    This PR will enable the workaround for this extension and remove the
    extension from the phase out list.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/113572

--- source/blender/gpu/opengl/gl_backend.cc
+++ source/blender/gpu/opengl/gl_backend.cc
@@ -266,7 +266,8 @@ static void detect_workarounds()
     /* Although an OpenGL 4.3 feature, our implementation requires shader_draw_parameters_support.
      * NOTE: we should untangle this by checking both features for clarity. */
     GLContext::multi_draw_indirect_support = false;
-
+    /* Turn off extensions. */
+    GLContext::layered_rendering_support = false;
     /* Turn off vendor specific extensions. */
     GLContext::native_barycentric_support = false;
     GLContext::framebuffer_fetch_support = false;
@@ -281,7 +282,6 @@ static void detect_workarounds()
     GLContext::debug_layer_support = false;
     GLContext::fixed_restart_index_support = false;
     GLContext::geometry_shader_invocations = false;
-    GLContext::layered_rendering_support = false;
     GLContext::texture_cube_map_array_support = false;
     GLContext::texture_gather_support = false;
     GLContext::texture_storage_support = false;
