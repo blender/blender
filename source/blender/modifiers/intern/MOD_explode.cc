zyxcambridge/blender_llm

Fix memory leak when the explode modifier is used with vertex groups
Initializing all vertices from the original could allocater vertex
groups into the custom-data which were later overwritten witout freeing.

The leak happened in the test file:
.../lib/tests/modifier_stack/explode_modifier.blend

source/blender/modifiers/intern/MOD_explode.cc
@@ -761,6 +761,7 @@ static Mesh *cutEdges(ExplodeModifierData *emd, Mesh *mesh)
     const int ed_v1 = edgehash[esplit].v_low;
     const int ed_v2 = edgehash[esplit].v_high;
 
+    CustomData_free_elem(&split_m->vert_data, esplit, 1);
     CustomData_copy_data(&split_m->vert_data, &split_m->vert_data, ed_v2, esplit, 1);
 
     dupve = split_m_positions[esplit];

blender/blender

source/blender/modifiers/intern/MOD_explode.cc
@@ -768,6 +768,7 @@ static Mesh *cutEdges(ExplodeModifierData *emd, Mesh *mesh)
     BLI_edgehashIterator_getKey(ehi, &ed_v1, &ed_v2);
     esplit = POINTER_AS_INT(BLI_edgehashIterator_getValue(ehi));
 
+    CustomData_free_elem(&split_m->vdata, esplit, 1);
     CustomData_copy_data(&split_m->vdata, &split_m->vdata, ed_v2, esplit, 1);
 
     dupve = split_m_positions[esplit];