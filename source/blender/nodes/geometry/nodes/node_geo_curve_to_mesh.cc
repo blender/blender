0dcf92f945e18eb81ca1cd589fb71894bd917668
Dalai Felinto <dalai@blender.org>
2023-10-16 17:31:42 +0200

    GPv3: Fix Curve to Mesh node replacing instances
    The code is already replacing instances when needed (when there was no
    instances previously created in the file). To do it regardless may lead
    to remove instances from the loop.
    
    The other related nodes did not have that issue:
    * Curve to Points
    * Curve Fill

--- source/blender/nodes/geometry/nodes/node_geo_curve_to_mesh.cc
+++ source/blender/nodes/geometry/nodes/node_geo_curve_to_mesh.cc
@@ -91,7 +91,6 @@ static void grease_pencil_to_mesh(GeometrySet &geometry_set,
     const int handle = instances->add_reference(bke::InstanceReference{temp_set});
     instances->add_instance(handle, float4x4::identity());
   }
-  geometry_set.replace_instances(instances);
   GeometrySet::propagate_attributes_from_layer_to_instances(
       geometry_set.get_grease_pencil()->attributes(),
       geometry_set.get_instances_for_write()->attributes_for_write(),
