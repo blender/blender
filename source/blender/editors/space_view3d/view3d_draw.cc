9a3fa4d8cc49b2992c43ae976bac535668aaadca
Harley Acheson <harley.acheson@gmail.com>
2023-10-18 00:50:05 +0200

    Fix #113826: Set Explicit Font Size for Overlays
    The complaint illustrates how text measurement can be broken by a
    drawing callback. This is because overlays are setting global_font_size
    value, which is used by subsequent drawing, but does not explicitly set
    size on the global_font_default font, which is needed for measurement.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/113868

--- source/blender/editors/space_view3d/view3d_draw.cc
+++ source/blender/editors/space_view3d/view3d_draw.cc
@@ -1535,6 +1535,7 @@ void view3d_draw_region_info(const bContext *C, ARegion *region)
     int xoffset = rect->xmin + (0.5f * U.widget_unit);
     int yoffset = rect->ymax - (0.1f * U.widget_unit);
     BLF_default_size(UI_style_get()->widgetlabel.points);
+    BLF_set_default();
 
     if ((v3d->overlay.flag & V3D_OVERLAY_HIDE_TEXT) == 0) {
       if ((U.uiflag & USER_SHOW_FPS) && ED_screen_animation_no_scrub(wm)) {
