e42281084b07c0b5773c1b22e0b163807ec6435d
Dalai Felinto <dalai@blender.org>
2023-10-20 11:04:15 +0200

    GPv3: Named Layer Selection node
    This node supports either a Layer or a Layer Group name as input,
    and outputs a selection field for it.
    
    Known limitations to be addressed separately:
    
    * We are not warning/keeping track of the named layers.
    * There is no lookup for layers (groups) yet.
    
    Ref !113908.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/113908

--- scripts/startup/bl_ui/node_add_menu_geometry.py
+++ scripts/startup/bl_ui/node_add_menu_geometry.py
@@ -286,6 +286,8 @@ class NODE_MT_geometry_node_GEO_INPUT_SCENE(Menu):
         node_add_menu.add_node_type(layout, "GeometryNodeCollectionInfo")
         node_add_menu.add_node_type(layout, "GeometryNodeImageInfo")
         node_add_menu.add_node_type(layout, "GeometryNodeIsViewport")
+        if context.preferences.experimental.use_grease_pencil_version3:
+            node_add_menu.add_node_type(layout, "GeometryNodeInputNamedLayerSelection")
         node_add_menu.add_node_type(layout, "GeometryNodeObjectInfo")
         node_add_menu.add_node_type(layout, "GeometryNodeInputSceneTime")
         node_add_menu.add_node_type(layout, "GeometryNodeSelfObject")
--- source/blender/blenkernel/BKE_node.h
+++ source/blender/blenkernel/BKE_node.h
@@ -1315,6 +1315,7 @@ void BKE_nodetree_remove_layer_n(struct bNodeTree *ntree, struct Scene *scene, i
 #define GEO_NODE_POINTS_TO_CURVES 2114
 #define GEO_NODE_INPUT_EDGE_SMOOTH 2115
 #define GEO_NODE_SPLIT_TO_INSTANCES 2116
+#define GEO_NODE_INPUT_NAMED_LAYER_SELECTION 2117
 
 /** \} */
 
