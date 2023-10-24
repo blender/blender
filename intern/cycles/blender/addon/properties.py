af629b8c208e06149b5dea448571dc30f1ed4d24
Michael Jones <michael_p_jones@apple.com>
2023-10-24 14:33:47 +0200

    Cycles: Remove MetalRT experimental status, and add "auto enable" option
    This PR removes the "experimental" disclaimer from the MetalRT control now that the unit tests all render correctly with it enabled. As well as "Off" and "On", this adds a third "Auto" setting  - a new default which can be used to pick the best option.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/114067

--- intern/cycles/blender/addon/properties.py
+++ intern/cycles/blender/addon/properties.py
@@ -1477,11 +1477,16 @@ class CyclesPreferences(bpy.types.AddonPreferences):
         default=False,
     )
 
-    use_metalrt: BoolProperty(
-        name="MetalRT (Experimental)",
+    metalrt: EnumProperty(
+        name="MetalRT",
         description="MetalRT for ray tracing uses less memory for scenes which use curves extensively, and can give better "
-                    "performance in specific cases. However this support is experimental and some scenes may render incorrectly",
-        default=False,
+                    "performance in specific cases.",
+        default='AUTO',
+        items=(
+            ('OFF', "Off", "Disable MetalRT (uses BVH2 layout for intersection queries)"),
+            ('ON', "On", "Enable MetalRT for intersection queries"),
+            ('AUTO', "Auto", "Automatically pick the fastest intersection method"),
+        ),
     )
 
     use_hiprt: BoolProperty(
@@ -1716,7 +1721,7 @@ class CyclesPreferences(bpy.types.AddonPreferences):
                 if is_arm64:
                     col.prop(self, "kernel_optimization_level")
                 if has_rt_api_support:
-                    col.prop(self, "use_metalrt")
+                    col.prop(self, "metalrt")
 
         if compute_device_type == 'HIP':
             has_cuda, has_optix, has_hip, has_metal, has_oneapi, has_hiprt = _cycles.get_device_types()
