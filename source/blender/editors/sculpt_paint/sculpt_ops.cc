a48d4184b52878cf27dcccdb124b7d6fc9b4f3eb
Philipp Oeser <philipp@blender.org>
2023-10-24 13:11:27 +0200

    Fix #114094: Mask by Color brush has stopped working
    Caused by 97f2b01ea9.
    
    Above commit refactored code in a way that it was always early out
    (always comparing the current mask value to itself and not the new mask
    value).
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/114101

--- source/blender/editors/sculpt_paint/sculpt_ops.cc
+++ source/blender/editors/sculpt_paint/sculpt_ops.cc
@@ -748,7 +748,7 @@ static void do_mask_by_color_contiguous_update_node(Object *ob,
     const float new_mask = mask_by_color_floodfill[vd.index];
     const float mask = sculpt_mask_by_color_final_mask_get(
         current_mask, new_mask, invert, preserve_mask);
-    if (current_mask == current_mask) {
+    if (current_mask == mask) {
       continue;
     }
 
@@ -861,7 +861,7 @@ static void do_mask_by_color_task(Object *ob,
     const float new_mask = sculpt_mask_by_color_delta_get(active_color, col, threshold, invert);
     const float mask = sculpt_mask_by_color_final_mask_get(
         current_mask, new_mask, invert, preserve_mask);
-    if (current_mask == *vd.mask) {
+    if (current_mask == mask) {
       continue;
     }
 
