fcb26fe72d63a1df3c48d1fb4282b970d03a749d
Jeroen Bakker <jeroen@blender.org>
2023-10-30 15:04:13 +0100

    EEVEE: Update UI Names
    This PR will use "EEVEE (Legacy)" for the legacy EEVEE and renames
    "EEVEE Next" to "EEVEE".
    
    When starting Blender EEVEE Legacy will still be the default. We
    will change the default to EEVEE when all missing features have been
    addressed.
    
    After that we can cleanup idnames and panels etc.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/114272

--- source/blender/draw/engines/eevee/eevee_engine.cc
+++ source/blender/draw/engines/eevee/eevee_engine.cc
@@ -668,7 +668,7 @@ RenderEngineType DRW_engine_viewport_eevee_type = {
     /*next*/ nullptr,
     /*prev*/ nullptr,
     /*idname*/ EEVEE_ENGINE,
-    /*name*/ N_("EEVEE"),
+    /*name*/ N_("EEVEE (Legacy)"),
     /*flag*/ RE_INTERNAL | RE_USE_PREVIEW | RE_USE_STEREO_VIEWPORT | RE_USE_GPU_CONTEXT,
     /*update*/ nullptr,
     /*render*/ &DRW_render_to_image,
