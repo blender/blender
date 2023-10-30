adc79ac8d8d3725c1d0cdf7476d13ad443d91460
Thomas Barlow <github@mysterymayhem.co.uk>
2023-10-30 21:03:34 +0100

    PyAPI: Enable MeshPolygon.loop_start raw array access
    This patch replaces MeshPolygon.loop_start's use of
    RNA_def_property_int_funcs with RNA_def_property_int_sdna to enable raw
    array access.
    
    RNA_def_property_int_funcs disables the PROP_INTERN_RAW_ACCESS flag,
    which forces rna_raw_access to fall back to its slower loop when using
    bpy_prop_collection.foreach_get/foreach_set from Python.
    
    Because MeshPolygon uses the MIntProperty struct and loop_start accesses
    the "i" field of MIntProperty directly, it can use
    RNA_def_property_int_sdna like other props that directly access the "i"
    field of a StructRNA using the MIntProperty struct.
    
    This then speeds up the Python API bpy_prop_collection.foreach_get/
    foreach_set access of MeshPolygon.loop_total to the same speeds as
    generic int attributes.
    
    Given a mesh with 98304 faces:
    
    foreach_get with a compatible buffer object:
    - Face int attribute: ~0.23ms
    - MeshPolygon.loop_start (before): ~1.8ms
    - MeshPolygon.loop_start (after): ~0.23ms
    
    foreach_get with a Python list:
    - Face int attribute: ~2.2ms
    - MeshPolygon.loop_start (before): ~3.4ms
    - MeshPolygon.loop_start (after): ~2.2ms
    
    foreach_set with a compatible buffer object:
    - Face int attribute: ~0.25ms
    - MeshPolygon.loop_start (before): ~2.3ms
    - MeshPolygon.loop_start (after): ~0.25ms
    
    foreach_set with a Python list:
    - Face int attribute: ~1.1ms
    - MeshPolygon.loop_start (before): ~3.1ms
    - MeshPolygon.loop_start (after): ~1.1ms
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/114262

--- source/blender/makesrna/intern/rna_mesh.cc
+++ source/blender/makesrna/intern/rna_mesh.cc
@@ -1261,16 +1261,6 @@ static void rna_MeshPoly_vertices_get(PointerRNA *ptr, int *values)
   memcpy(values, &me->corner_verts()[poly_start], sizeof(int) * poly_size);
 }
 
-static int rna_MeshPolygon_loop_start_get(PointerRNA *ptr)
-{
-  return *static_cast<const int *>(ptr->data);
-}
-
-static void rna_MeshPolygon_loop_start_set(PointerRNA *ptr, int value)
-{
-  *static_cast<int *>(ptr->data) = value;
-}
-
 static int rna_MeshPolygon_loop_total_get(PointerRNA *ptr)
 {
   const int *data = static_cast<const int *>(ptr->data);
@@ -2282,8 +2272,7 @@ static void rna_def_mpolygon(BlenderRNA *brna)
 
   /* these are both very low level access */
   prop = RNA_def_property(srna, "loop_start", PROP_INT, PROP_UNSIGNED);
-  RNA_def_property_int_funcs(
-      prop, "rna_MeshPolygon_loop_start_get", "rna_MeshPolygon_loop_start_set", nullptr);
+  RNA_def_property_int_sdna(prop, nullptr, "i");
   RNA_def_property_ui_text(prop, "Loop Start", "Index of the first loop of this face");
   /* also low level */
   prop = RNA_def_property(srna, "loop_total", PROP_INT, PROP_UNSIGNED);
