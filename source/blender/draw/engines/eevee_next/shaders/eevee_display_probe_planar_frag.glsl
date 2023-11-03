e3d4db5ded35684358fbd8e8f26a0013e8c9bcf2
Miguel Pozo <pragma37@gmail.com>
2023-11-03 15:26:19 +0100

    Fix: EEVEE-Next: Planar display probe shader compilation

--- source/blender/draw/engines/eevee_next/shaders/eevee_display_probe_planar_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_display_probe_planar_frag.glsl
@@ -4,7 +4,7 @@
 
 void main()
 {
-  vec2 uv = gl_FragCoord.xy / textureSize(planar_radiance_tx, 0).xy;
+  vec2 uv = gl_FragCoord.xy / vec2(textureSize(planar_radiance_tx, 0).xy);
   out_color = texture(planar_radiance_tx, vec3(uv, probe_index));
   out_color.a = 0.0;
 }
