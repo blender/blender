c05faeb053cc91afd6cf6b09c9772705eaa17c94
Julian Eisel <julian@blender.org>
2023-11-10 11:33:15 +0100

    Fix #114432: Custom themes overridden by default theme
    Code was copying the default theme over the active theme, instead of
    selectively updating a single theme option to fix #113683.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/114675

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -130,7 +130,7 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
   }
 
   if (!USER_VERSION_ATLEAST(400, 35)) {
-    memcpy(btheme, &U_theme_default, sizeof(*btheme));
+    FROM_DEFAULT_V4_UCHAR(tui.wcol_list_item.item);
   }
 
   /**
