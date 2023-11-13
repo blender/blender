0746435cdd899b1fe48de7ee9f07be1753f6acb2
Sergey Sharybin <sergey@blender.org>
2023-11-13 11:50:20 +0100

    Depsgraph: Remove misleading FPS counter
    It was weird concept which was calculating scene FPS, but from
    the depgraph side. It was misleading, especially for the initial
    evaluation of FPS of nan.
    
    If we want FPS counters this needs to be done on a higher level
    in the code.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/114782

--- source/blender/depsgraph/intern/debug/deg_debug.cc
+++ source/blender/depsgraph/intern/debug/deg_debug.cc
@@ -21,10 +21,7 @@
 
 namespace blender::deg {
 
-DepsgraphDebug::DepsgraphDebug()
-    : flags(G.debug), is_ever_evaluated(false), graph_evaluation_start_time_(0)
-{
-}
+DepsgraphDebug::DepsgraphDebug() : flags(G.debug), graph_evaluation_start_time_(0) {}
 
 bool DepsgraphDebug::do_time_debug() const
 {
@@ -39,10 +36,6 @@ void DepsgraphDebug::begin_graph_evaluation()
 
   const double current_time = PIL_check_seconds_timer();
 
-  if (is_ever_evaluated) {
-    fps_samples_.add_sample(current_time - graph_evaluation_start_time_);
-  }
-
   graph_evaluation_start_time_ = current_time;
 }
 
@@ -55,19 +48,12 @@ void DepsgraphDebug::end_graph_evaluation()
   const double graph_eval_end_time = PIL_check_seconds_timer();
   const double graph_eval_time = graph_eval_end_time - graph_evaluation_start_time_;
 
-  const double fps_samples = fps_samples_.get_averaged();
-  const double fps = fps_samples ? 1.0 / fps_samples_.get_averaged() : 0.0;
-
   if (name.empty()) {
     printf("Depsgraph updated in %f seconds.\n", graph_eval_time);
-    printf("Depsgraph evaluation FPS: %f\n", fps);
   }
   else {
     printf("Depsgraph [%s] updated in %f seconds.\n", name.c_str(), graph_eval_time);
-    printf("Depsgraph [%s] evaluation FPS: %f\n", name.c_str(), fps);
   }
-
-  is_ever_evaluated = true;
 }
 
 bool terminal_do_color()
