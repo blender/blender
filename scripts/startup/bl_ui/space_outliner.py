f815484e7d4538c29e5d47614466dafafd6ab5b1
Philipp Oeser <philipp@blender.org>
2023-11-17 00:49:04 +0100

    Outliner: add "Expand/Collapse All" to view context menu
    This was only in the blender keymap, but seems useful to have this in a
    menu as well.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/114138

--- scripts/startup/bl_ui/space_outliner.py
+++ scripts/startup/bl_ui/space_outliner.py
@@ -140,6 +140,7 @@ class OUTLINER_MT_context_menu_view(Menu):
 
         layout.separator()
 
+        layout.operator("outliner.expanded_toggle")
         layout.operator("outliner.show_hierarchy", text="Show Object Hierarchy")
         layout.operator("outliner.show_one_level", text="Show One Level")
         layout.operator("outliner.show_one_level", text="Hide One Level").open = False
