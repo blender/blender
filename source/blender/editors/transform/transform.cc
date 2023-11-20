07b5e1bd8065da8fd6ec367791a31756c944043f
Germano Cavalcante <germano.costa@ig.com.br>
2023-11-20 11:09:16 -0300

    Fix #115153: Snap Base in Camera View crashes Blender
    Snap to geometry is not supported when moving camera in camera view.
    
    This is because the camera would make jumps that would be difficult to
    predict and control.
    
    The same happens if Snap Base were supported.
    
    Therefore, disable Edit Snap Base when transforming camera into camera
    view.

--- source/blender/editors/transform/transform.cc
+++ source/blender/editors/transform/transform.cc
@@ -707,6 +707,10 @@ static bool transform_modal_item_poll(const wmOperator *op, int value)
         /* More modes can be added over time if this feature proves useful for them. */
         return false;
       }
+      if (t->options & CTX_CAMERA) {
+        /* Not supported. */
+        return false;
+      }
       break;
     }
     case TFM_MODAL_PASSTHROUGH_NAVIGATE:
