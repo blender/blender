3f778150a93d073c5a433c92a571f7c80cd0a53a
Miguel Pozo <pragma37@gmail.com>
2023-11-21 20:15:59 +0100

    GPU: Sanitize closure nodes inputs
    Ensure all Closures are filled with correct values,
    like the principled bsdf node already does.
    
    The main reason is that the new AgX color transform doesn't play well
    with negative values (see #113220), but it's probably best to ensure we
    use sanitized values in the rendering code as a whole.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/115059

--- source/blender/gpu/shaders/material/gpu_shader_material_refraction.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_refraction.glsl
@@ -5,6 +5,9 @@
 void node_bsdf_refraction(
     vec4 color, float roughness, float ior, vec3 N, float weight, out Closure result)
 {
+  color = max(color, vec4(0));
+  roughness = saturate(roughness);
+  ior = max(ior, 1e-5);
   N = safe_normalize(N);
 
   ClosureRefraction refraction_data;
--- source/blender/gpu/shaders/material/gpu_shader_material_transparent.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_transparent.glsl
@@ -4,6 +4,8 @@
 
 void node_bsdf_transparent(vec4 color, float weight, out Closure result)
 {
+  color = max(color, vec4(0));
+
   ClosureTransparency transparency_data;
   transparency_data.weight = weight;
   transparency_data.transmittance = color.rgb;
