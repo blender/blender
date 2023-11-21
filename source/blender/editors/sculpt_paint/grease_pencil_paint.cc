6064e726d4dd4590abfa2b185b740926f49117ea
Falk David <falk@blender.org>
2023-11-21 12:41:57 +0100

    Fix: GPv3: Stutter while drawing
    While drawing, the line beeing drawn would seemingly only update once
    in a while. This was because there was a bug in the interpolation
    function that would write the value being interpolated from
    directly to the destination as the first value.
    In our case, we only wanted to write new values (so
    we need to exclude the first one).
    
    Making sure the interpolation always excludes the value
    interpolated from, fixes the drawing stutter issue.

--- source/blender/editors/sculpt_paint/grease_pencil_paint.cc
+++ source/blender/editors/sculpt_paint/grease_pencil_paint.cc
@@ -43,10 +43,9 @@ static float calc_brush_radius(ViewContext *vc,
 template<typename T>
 static inline void linear_interpolation(const T &a, const T &b, MutableSpan<T> dst)
 {
-  dst.first() = a;
-  const float step = 1.0f / dst.size();
-  for (const int i : dst.index_range().drop_front(1)) {
-    dst[i] = bke::attribute_math::mix2(i * step, a, b);
+  const float step = 1.0f / float(dst.size());
+  for (const int i : dst.index_range()) {
+    dst[i] = bke::attribute_math::mix2(float(i + 1) * step, a, b);
   }
 }
 
