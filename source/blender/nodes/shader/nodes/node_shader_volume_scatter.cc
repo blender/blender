44fa9f37eb7e1ffedd1c31bc55b890a7b6f0bc4a
Clément Foucault <foucault.clem@gmail.com>
2023-11-22 21:17:59 +0100

    GPUMaterial: Add volume shader flags
    This allows for some optimization when we know
    some effects are not present. Also this is needed
    to detect the case when world contains absorption
    in order to disable distant lighting.
    
    Related #114062

--- source/blender/nodes/shader/nodes/node_shader_volume_scatter.cc
+++ source/blender/nodes/shader/nodes/node_shader_volume_scatter.cc
@@ -9,7 +9,9 @@ namespace blender::nodes::node_shader_volume_scatter_cc {
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Color>("Color").default_value({0.8f, 0.8f, 0.8f, 1.0f});
+#define SOCK_COLOR_ID 0
   b.add_input<decl::Float>("Density").default_value(1.0f).min(0.0f).max(1000.0f);
+#define SOCK_DENSITY_ID 1
   b.add_input<decl::Float>("Anisotropy")
       .default_value(0.0f)
       .min(-1.0f)
@@ -19,14 +21,28 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_output<decl::Shader>("Volume").translation_context(BLT_I18NCONTEXT_ID_ID);
 }
 
+#define socket_not_zero(sock) (in[sock].link || (clamp_f(in[sock].vec[0], 0.0f, 1.0f) > 1e-5f))
+#define socket_not_black(sock) \
+  (in[sock].link || (clamp_f(in[sock].vec[0], 0.0f, 1.0f) > 1e-5f && \
+                     clamp_f(in[sock].vec[1], 0.0f, 1.0f) > 1e-5f && \
+                     clamp_f(in[sock].vec[2], 0.0f, 1.0f) > 1e-5f))
+
 static int node_shader_gpu_volume_scatter(GPUMaterial *mat,
                                           bNode *node,
                                           bNodeExecData * /*execdata*/,
                                           GPUNodeStack *in,
                                           GPUNodeStack *out)
 {
+  if (socket_not_zero(SOCK_DENSITY_ID) && socket_not_black(SOCK_COLOR_ID)) {
+    /* Consider there is absorption phenomenon when there is scattering since
+     * `extinction = scattering + absorption`. */
+    GPU_material_flag_set(mat, GPU_MATFLAG_VOLUME_SCATTER | GPU_MATFLAG_VOLUME_ABSORPTION);
+  }
   return GPU_stack_link(mat, node, "node_volume_scatter", in, out);
 }
+
+#undef SOCK_COLOR_ID
+#undef SOCK_DENSITY_ID
 
 }  // namespace blender::nodes::node_shader_volume_scatter_cc
 
