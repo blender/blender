3f6d1d098c9b068ade44ea6b5e640b338a42fc89
Pratik Borhade <pratikborhade302@gmail.com>
2023-11-24 07:24:44 +0100

    NLA: Console warnings about missing channel_index property
    channel_index was renamed to track_index in f9b9ec3b26. Commit missed
    one change in `ANIM_channel_draw_widgets`, hence the error in console
    when mouse is over NLA editor.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/115332

--- source/blender/editors/animation/anim_channels_defines.cc
+++ source/blender/editors/animation/anim_channels_defines.cc
@@ -5750,7 +5750,7 @@ void ANIM_channel_draw_widgets(const bContext *C,
                             nullptr);
 
         opptr_b = UI_but_operator_ptr_get(but);
-        RNA_int_set(opptr_b, "channel_index", channel_index);
+        RNA_int_set(opptr_b, "track_index", channel_index);
 
         UI_block_emboss_set(block, UI_EMBOSS_NONE);
       }
