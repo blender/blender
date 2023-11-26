f000b94a647a93d2b922d06738afb74bb48e5e18
Campbell Barton <campbell@blender.org>
2023-11-27 10:55:42 +1100

    Cleanup: declare functions with overrides in GHOST_ContextVK

--- intern/ghost/intern/GHOST_ContextVK.hh
+++ intern/ghost/intern/GHOST_ContextVK.hh
@@ -84,32 +84,32 @@ class GHOST_ContextVK : public GHOST_Context {
    * Swaps front and back buffers of a window.
    * \return  A boolean success indicator.
    */
-  GHOST_TSuccess swapBuffers();
+  GHOST_TSuccess swapBuffers() override;
 
   /**
    * Activates the drawing context of this window.
    * \return  A boolean success indicator.
    */
-  GHOST_TSuccess activateDrawingContext();
+  GHOST_TSuccess activateDrawingContext() override;
 
   /**
    * Release the drawing context of the calling thread.
    * \return  A boolean success indicator.
    */
-  GHOST_TSuccess releaseDrawingContext();
+  GHOST_TSuccess releaseDrawingContext() override;
 
   /**
    * Call immediately after new to initialize.  If this fails then immediately delete the object.
    * \return Indication as to whether initialization has succeeded.
    */
-  GHOST_TSuccess initializeDrawingContext();
+  GHOST_TSuccess initializeDrawingContext() override;
 
   /**
    * Removes references to native handles from this context and then returns
    * \return GHOST_kSuccess if it is OK for the parent to release the handles and
    * GHOST_kFailure if releasing the handles will interfere with sharing
    */
-  GHOST_TSuccess releaseNativeHandles();
+  GHOST_TSuccess releaseNativeHandles() override;
 
   /**
    * Gets the Vulkan context related resource handles.
@@ -119,7 +119,7 @@ class GHOST_ContextVK : public GHOST_Context {
                                   void *r_physical_device,
                                   void *r_device,
                                   uint32_t *r_graphic_queue_family,
-                                  void *r_queue);
+                                  void *r_queue) override;
 
   GHOST_TSuccess getVulkanSwapChainFormat(GHOST_VulkanSwapChainData *r_swap_chain_data) override;
 
@@ -132,7 +132,7 @@ class GHOST_ContextVK : public GHOST_Context {
    * \param interval: The swap interval to use.
    * \return A boolean success indicator.
    */
-  GHOST_TSuccess setSwapInterval(int /* interval */)
+  GHOST_TSuccess setSwapInterval(int /*interval*/) override
   {
     return GHOST_kFailure;
   }
@@ -142,7 +142,7 @@ class GHOST_ContextVK : public GHOST_Context {
    * \param intervalOut: Variable to store the swap interval if it can be read.
    * \return Whether the swap interval can be read.
    */
-  GHOST_TSuccess getSwapInterval(int &)
+  GHOST_TSuccess getSwapInterval(int & /*intervalOut*/) override
   {
     return GHOST_kFailure;
   };
