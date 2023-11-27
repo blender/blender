a9c8bf305becbf8a5168cb75c1de79f639465c75
Charles Wardlaw <charleswardlaw@noreply.localhost>
2023-11-27 21:08:17 +0100

    Cleanup: USD: Fix unused declaration and argument
    Pull Request: https://projects.blender.org/blender/blender/pulls/115495

--- source/blender/io/usd/intern/usd_reader_material.h
+++ source/blender/io/usd/intern/usd_reader_material.h
@@ -130,7 +130,6 @@ class USDMaterialReader {
                               NodePlacementContext *r_ctx) const;
 
   void convert_usd_transform_2d(const pxr::UsdShadeShader &usd_shader,
-                                const pxr::TfToken &usd_source_name,
                                 bNode *dest_node,
                                 const char *dest_socket_name,
                                 bNodeTree *ntree,
