65022ad1cf7744892f590b7a782f2ee49079e6c4
Campbell Barton <campbell@blender.org>
2023-11-28 13:24:44 +1100

    Cleanup: remove redundant exception/report handling from FreeStyle
    `BPY_run_*` functions print & clear the errors making any calls
    afterwards that attempt to extract exception messages into NOOP's.

--- source/blender/freestyle/intern/system/PythonInterpreter.h
+++ source/blender/freestyle/intern/system/PythonInterpreter.h
@@ -11,23 +11,12 @@
 
 #include <iostream>
 
-extern "C" {
-#include <Python.h>
-}
-
 #include "Interpreter.h"
-#include "StringUtils.h"
-
-#include "MEM_guardedalloc.h"
-
-// soc
-#include "DNA_text_types.h"
 
 #include "BKE_context.hh"
 #include "BKE_global.h"
 #include "BKE_lib_id.h"
 #include "BKE_main.h"
-#include "BKE_report.h"
 #include "BKE_text.h"
 
 #include "BPY_extern_run.h"
@@ -52,20 +41,18 @@ class PythonInterpreter : public Interpreter {
 
   int interpretFile(const string &filename)
   {
-    ReportList *reports = CTX_wm_reports(_context);
-    BKE_reports_clear(reports);
     char *fn = const_cast<char *>(filename.c_str());
 #if 0
-    bool ok = BPY_run_filepath(_context, fn, reports);
+    bool ok = BPY_run_filepath(_context, fn, nullptr);
 #else
     bool ok;
     Text *text = BKE_text_load(&_freestyle_bmain, fn, G_MAIN->filepath);
     if (text) {
-      ok = BPY_run_text(_context, text, reports, false);
+      ok = BPY_run_text(_context, text, nullptr, false);
       BKE_id_delete(&_freestyle_bmain, text);
     }
     else {
-      BKE_reportf(reports, RPT_ERROR, "Cannot open file: %s", fn);
+      cerr << "Cannot open file" << endl;
       ok = false;
     }
 #endif
@@ -73,55 +60,30 @@ class PythonInterpreter : public Interpreter {
     if (ok == false) {
       cerr << "\nError executing Python script from PythonInterpreter::interpretFile" << endl;
       cerr << "File: " << fn << endl;
-      cerr << "Errors: " << endl;
-      BKE_reports_print(reports, RPT_ERROR);
       return 1;
     }
-
-    // cleaning up
-    BKE_reports_clear(reports);
 
     return 0;
   }
 
   int interpretString(const string &str, const string &name)
   {
-    ReportList *reports = CTX_wm_reports(_context);
-
-    BKE_reports_clear(reports);
-
     if (!BPY_run_string_eval(_context, nullptr, str.c_str())) {
-      BPy_errors_to_report(reports);
-      PyErr_Print();
-      PyErr_Clear();
       cerr << "\nError executing Python script from PythonInterpreter::interpretString" << endl;
       cerr << "Name: " << name << endl;
-      cerr << "Errors: " << endl;
-      BKE_reports_print(reports, RPT_ERROR);
       return 1;
     }
-
-    BKE_reports_clear(reports);
 
     return 0;
   }
 
   int interpretText(struct Text *text, const string &name)
   {
-    ReportList *reports = CTX_wm_reports(_context);
-
-    BKE_reports_clear(reports);
-
-    if (!BPY_run_text(_context, text, reports, false)) {
+    if (!BPY_run_text(_context, text, nullptr, false)) {
       cerr << "\nError executing Python script from PythonInterpreter::interpretText" << endl;
       cerr << "Name: " << name << endl;
-      cerr << "Errors: " << endl;
-      BKE_reports_print(reports, RPT_ERROR);
       return 1;
     }
-
-    BKE_reports_clear(reports);
-
     return 0;
   }
 
