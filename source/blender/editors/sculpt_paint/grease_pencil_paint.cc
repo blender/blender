1317c951fb314f1b67155390096efe1ad5d43a07
Harley Acheson <harley.acheson@gmail.com>
2023-11-30 09:29:35 -0800

    Cleanup: Make format
    Formatting changes resulting from Make Format

--- source/blender/editors/sculpt_paint/grease_pencil_paint.cc
+++ source/blender/editors/sculpt_paint/grease_pencil_paint.cc
@@ -301,7 +301,8 @@ struct PaintOperationExecutor {
         corner_angle_threshold,
         memory);
 
-    /* Pre-blur the coordinates for the curve fitting. This generally leads to a better (more stable) fit. */
+    /* Pre-blur the coordinates for the curve fitting. This generally leads to a better (more
+     * stable) fit. */
     Array<float2> coords_pre_blur(smooth_window.size());
     const int pre_blur_iterations = 3;
     ed::greasepencil::gaussian_blur_1D(coords_to_smooth,
