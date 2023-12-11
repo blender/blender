blender/blender

commit 85f97583028133fa4d1d25b73b60a21d6be8f6e0

Fix: EEVEE-Next: Crash when using Planar Probe
This was caused by missing cryptomatte and
depth buffer input for shadow tagging.

source/blender/draw/engines/eevee_next/eevee_shadow.cc
@@ -824,7 +824,6 @@ void ShadowModule::begin_sync()
 
   {
     Manager &manager = *inst_.manager;
-    RenderBuffers &render_buffers = inst_.render_buffers;
 
     PassMain &pass = tilemap_usage_ps_;
     pass.init();
@@ -861,7 +860,7 @@ void ShadowModule::begin_sync()
       sub.shader_set(inst_.shaders.static_shader_get(SHADOW_TILEMAP_TAG_USAGE_OPAQUE));
       sub.bind_ssbo("tilemaps_buf", &tilemap_pool.tilemaps_data);
       sub.bind_ssbo("tiles_buf", &tilemap_pool.tiles_data);
-      sub.bind_texture("depth_tx", &render_buffers.depth_tx);
+      sub.bind_texture("depth_tx", &src_depth_tx_);
       sub.push_constant("tilemap_projection_ratio", &tilemap_projection_ratio_);
       inst_.lights.bind_resources(sub);
       sub.dispatch(&dispatch_depth_scan_size_);
@@ -1263,15 +1262,15 @@ float ShadowModule::tilemap_pixel_radius()
   return cubeface_diagonal / pixel_count;
 }
 
-/* Update all shadow regions visible inside the view.
- * If called multiple time for the same view, it will only do the depth buffer scanning
- * to check any new opaque surfaces.
- * Needs to be called after `LightModule::set_view();`. */
-void ShadowModule::set_view(View &view)
+void ShadowModule::set_view(View &view, GPUTexture *depth_tx)
 {
   GPUFrameBuffer *prev_fb = GPU_framebuffer_active_get();
 
-  int3 target_size = inst_.render_buffers.depth_tx.size();
+  src_depth_tx_ = depth_tx;
+
+  int3 target_size(0);
+  GPU_texture_get_mipmap_size(depth_tx, 0, target_size);
+
   dispatch_depth_scan_size_ = math::divide_ceil(target_size, int3(SHADOW_DEPTH_SCAN_GROUP_SIZE));
 
   pixel_world_radius_ = screen_pixel_radius(view, int2(target_size));

source/blender/draw/engines/eevee_next/eevee_irradiance_cache.cc
@@ -1012,9 +1012,8 @@ void IrradianceBake::surfels_lights_eval()
   /* TODO(fclem): Remove this. It is only present to avoid crash inside `shadows.set_view` */
   inst_.render_buffers.acquire(int2(1));
   inst_.hiz_buffer.set_source(&inst_.render_buffers.depth_tx);
-
   inst_.lights.set_view(view_z_, grid_pixel_extent_.xy());
-  inst_.shadows.set_view(view_z_);
+  inst_.shadows.set_view(view_z_, inst_.render_buffers.depth_tx);
   inst_.render_buffers.release();
 
   inst_.manager->submit(surfel_light_eval_ps_, view_z_);

source/blender/draw/engines/eevee_next/eevee_pipeline.cc
@@ -388,7 +388,7 @@ void ForwardPipeline::render(View &view, Framebuffer &prepass_fb, Framebuffer &c
 
   inst_.hiz_buffer.set_dirty();
 
-  inst_.shadows.set_view(view);
+  inst_.shadows.set_view(view, inst_.render_buffers.depth_tx);
   inst_.irradiance_cache.set_view(view);
 
   combined_fb.bind();
@@ -657,7 +657,7 @@ void DeferredLayer::render(View &main_view,
   inst_.hiz_buffer.update();
 
   inst_.irradiance_cache.set_view(render_view);
-  inst_.shadows.set_view(render_view);
+  inst_.shadows.set_view(render_view, inst_.render_buffers.depth_tx);
 
   if (/* FIXME(fclem): Vulkan doesn't implement load / store config yet. */
       GPU_backend_get_type() == GPU_BACKEND_VULKAN)
@@ -1197,7 +1197,7 @@ void DeferredProbeLayer::render(View &view,
 
   inst_.hiz_buffer.set_source(&inst_.render_buffers.depth_tx);
   inst_.lights.set_view(view, extent);
-  inst_.shadows.set_view(view);
+  inst_.shadows.set_view(view, inst_.render_buffers.depth_tx);
   inst_.irradiance_cache.set_view(view);
 
   /* Update for lighting pass. */
@@ -1356,7 +1356,7 @@ void PlanarProbePipeline::render(View &view,
 
   inst_.hiz_buffer.set_source(&depth_layer_tx);
   inst_.lights.set_view(view, extent);
-  inst_.shadows.set_view(view);
+  inst_.shadows.set_view(view, depth_layer_tx);
   inst_.irradiance_cache.set_view(view);
 
   /* Update for lighting pass. */

source/blender/draw/engines/eevee_next/eevee_shadow.hh
@@ -223,6 +223,8 @@ class ShadowModule {
 
   PassMain::Sub *tilemap_usage_transparent_ps_ = nullptr;
   GPUBatch *box_batch_ = nullptr;
+  /* Source texture for depth buffer analysis. */
+  GPUTexture *src_depth_tx_ = nullptr;
 
   Framebuffer usage_tag_fb;
 
@@ -336,7 +338,11 @@ class ShadowModule {
 
   void set_lights_data();
 
-  void set_view(View &view);
+  /* Update all shadow regions visible inside the view.
+   * If called multiple time for the same view, it will only do the depth buffer scanning
+   * to check any new opaque surfaces.
+   * Needs to be called after `LightModule::set_view();`. */
+  void set_view(View &view, GPUTexture *depth_tx = nullptr);
 
   void debug_end_sync();
   void debug_draw(View &view, GPUFrameBuffer *view_fb);

source/blender/draw/engines/eevee_next/eevee_view.cc
@@ -82,12 +82,13 @@ void ShadingView::render()
 
   DRW_stats_group_start(name_);
 
-  /* Needs to be before anything else because it query its own gbuffer. */
-  inst_.planar_probes.set_view(render_view_, extent_);
-
-  /* Query temp textures and create frame-buffers. */
+  /* Needs to be before planar_probes because it needs correct crypto-matte & render-pass buffers
+   * to reuse the same deferred shaders. */
   RenderBuffers &rbufs = inst_.render_buffers;
   rbufs.acquire(extent_);
+
+  /* Needs to be before anything else because it query its own gbuffer. */
+  inst_.planar_probes.set_view(render_view_, extent_);
 
   combined_fb_.ensure(GPU_ATTACHMENT_TEXTURE(rbufs.depth_tx),
                       GPU_ATTACHMENT_TEXTURE(rbufs.combined_tx));

commit 3313e9976aafd9669ea3fdffa64ef0b84860099d

Fix: EEVEE-Next: Broken Shadow usage tagging
Broken after recent changes.

Caused by 85f97583028133fa4d1d25b73b60a21d6be8f6e0

source/blender/draw/engines/eevee_next/eevee_shadow.cc
@@ -1268,7 +1268,7 @@ void ShadowModule::set_view(View &view, GPUTexture *depth_tx)
 
   src_depth_tx_ = depth_tx;
 
-  int3 target_size(0);
+  int3 target_size(1);
   GPU_texture_get_mipmap_size(depth_tx, 0, target_size);
 
   dispatch_depth_scan_size_ = math::divide_ceil(target_size, int3(SHADOW_DEPTH_SCAN_GROUP_SIZE));