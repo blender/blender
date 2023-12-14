40a89c0984c2551ebddcbd77157f835fbfab262d
rajveermalviya <rajveer0malviya@gmail.com>
2023-12-14 15:10:04 +0530

    GHOST/X11: Enable desktop sample capability
    The functionality was added [0] but the flag wasn't enabled.
    
    Ref: !116178
    
    [0]: e5a0d11c4ed07fbf1b3e08ddb75b635ce76c9d05

--- intern/ghost/intern/GHOST_SystemX11.cc
+++ intern/ghost/intern/GHOST_SystemX11.cc
@@ -1785,8 +1785,6 @@ GHOST_TCapabilityFlag GHOST_SystemX11::getCapabilities() const
 {
   return GHOST_TCapabilityFlag(GHOST_CAPABILITY_FLAG_ALL &
                                ~(
-                                   /* No support yet for desktop sampling. */
-                                   GHOST_kCapabilityDesktopSample |
                                    /* No support yet for image copy/paste. */
                                    GHOST_kCapabilityClipboardImages |
                                    /* No support yet for IME input methods. */
