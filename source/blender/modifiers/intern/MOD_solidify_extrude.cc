blender/blender

commit b52a071e7a3a11728821d11efc3f462da991b3e0

Cleanup: Remove unnecessary Mesh C-API functions
Like mentioned in the docstrings, use the C++ API or access methods
in C++ code.

source/blender/modifiers/intern/MOD_solidify_extrude.cc
@@ -19,6 +19,7 @@
 
 #include "MEM_guardedalloc.h"
 
+#include "BKE_attribute.hh"
 #include "BKE_deform.h"
 #include "BKE_mesh.hh"
 #include "BKE_particle.h"
@@ -144,6 +145,7 @@ static void mesh_calc_hq_normal(Mesh *mesh,
 /* NOLINTNEXTLINE: readability-function-size */
 Mesh *MOD_solidify_extrude_modifyMesh(ModifierData *md, const ModifierEvalContext *ctx, Mesh *mesh)
 {
+  using namespace blender;
   Mesh *result;
   const SolidifyModifierData *smd = (SolidifyModifierData *)md;
 
@@ -415,7 +417,9 @@ Mesh *MOD_solidify_extrude_modifyMesh(ModifierData *md, const ModifierEvalContex
   } \
   (void)0
 
-  int *dst_material_index = BKE_mesh_material_indices_for_write(result);
+  bke::MutableAttributeAccessor dst_attributes = mesh->attributes_for_write();
+  bke::SpanAttributeWriter dst_material_index = dst_attributes.lookup_or_add_for_write_span<int>(
+      "material_index", ATTR_DOMAIN_FACE);
 
   /* flip normals */
 
@@ -451,8 +455,8 @@ Mesh *MOD_solidify_extrude_modifyMesh(ModifierData *md, const ModifierEvalContex
 #endif
 
       if (mat_ofs) {
-        dst_material_index[faces_num + i] += mat_ofs;
-        CLAMP(dst_material_index[faces_num + i], 0, mat_nr_max);
+        dst_material_index.span[faces_num + i] += mat_ofs;
+        CLAMP(dst_material_index.span[faces_num + i], 0, mat_nr_max);
       }
 
       e = corner_edges[corner_2 + 0];
@@ -982,7 +986,7 @@ Mesh *MOD_solidify_extrude_modifyMesh(ModifierData *md, const ModifierEvalContex
 
   /* Add vertex weights for rim and shell vgroups. */
   if (shell_defgrp_index != -1 || rim_defgrp_index != -1) {
-    MDeformVert *dst_dvert = BKE_mesh_deform_verts_for_write(result);
+    MDeformVert *dst_dvert = result->deform_verts_for_write().data();
 
     /* Ultimate security check. */
     if (dst_dvert != nullptr) {
@@ -1121,8 +1125,8 @@ Mesh *MOD_solidify_extrude_modifyMesh(ModifierData *md, const ModifierEvalContex
 
       /* use the next material index if option enabled */
       if (mat_ofs_rim) {
-        dst_material_index[new_face_index] += mat_ofs_rim;
-        CLAMP(dst_material_index[new_face_index], 0, mat_nr_max);
+        dst_material_index.span[new_face_index] += mat_ofs_rim;
+        CLAMP(dst_material_index.span[new_face_index], 0, mat_nr_max);
       }
       if (crease_outer) {
         /* crease += crease_outer; without wrapping */
@@ -1151,6 +1155,8 @@ Mesh *MOD_solidify_extrude_modifyMesh(ModifierData *md, const ModifierEvalContex
   if (old_vert_arr) {
     MEM_freeN(old_vert_arr);
   }
+
+  dst_material_index.finish();
 
   return result;
 }

source/blender/blenkernel/BKE_mesh.h
@@ -14,7 +14,6 @@
 #include "DNA_mesh_types.h"
 #include "DNA_meshdata_types.h"
 
-#include "BKE_customdata.hh"
 
 struct BMesh;
 struct BMeshCreateParams;
@@ -562,46 +561,6 @@ void BKE_mesh_debug_print(const struct Mesh *mesh) ATTR_NONNULL(1);
 /* -------------------------------------------------------------------- */
 /** \name Inline Mesh Data Access
  * \{ */
-
-/**
- * \return The material index for each face. May be null.
- * \note In C++ code, prefer using the attribute API (#AttributeAccessor).
- */
-BLI_INLINE const int *BKE_mesh_material_indices(const Mesh *mesh)
-{
-  return (const int *)CustomData_get_layer_named(
-      &mesh->face_data, CD_PROP_INT32, "material_index");
-}
-
-/**
- * \return The material index for each face. Create the layer if it doesn't exist.
- * \note In C++ code, prefer using the attribute API (#MutableAttributeAccessor).
- */
-BLI_INLINE int *BKE_mesh_material_indices_for_write(Mesh *mesh)
-{
-  int *indices = (int *)CustomData_get_layer_named_for_write(
-      &mesh->face_data, CD_PROP_INT32, "material_index", mesh->faces_num);
-  if (indices) {
-    return indices;
-  }
-  return (int *)CustomData_add_layer_named(
-      &mesh->face_data, CD_PROP_INT32, CD_SET_DEFAULT, mesh->faces_num, "material_index");
-}
-
-BLI_INLINE const MDeformVert *BKE_mesh_deform_verts(const Mesh *mesh)
-{
-  return (const MDeformVert *)CustomData_get_layer(&mesh->vert_data, CD_MDEFORMVERT);
-}
-BLI_INLINE MDeformVert *BKE_mesh_deform_verts_for_write(Mesh *mesh)
-{
-  MDeformVert *dvert = (MDeformVert *)CustomData_get_layer_for_write(
-      &mesh->vert_data, CD_MDEFORMVERT, mesh->totvert);
-  if (dvert) {
-    return dvert;
-  }
-  return (MDeformVert *)CustomData_add_layer(
-      &mesh->vert_data, CD_MDEFORMVERT, CD_SET_DEFAULT, mesh->totvert);
-}
 
 #ifdef __cplusplus
 }

source/blender/blenkernel/BKE_mesh.hh
@@ -10,6 +10,7 @@
 
 #include "BLI_index_mask.hh"
 
+#include "BKE_customdata.hh"
 #include "BKE_mesh.h"
 #include "BKE_mesh_types.hh"
 
@@ -389,7 +390,8 @@ inline blender::MutableSpan<int> Mesh::corner_edges_for_write()
 
 inline blender::Span<MDeformVert> Mesh::deform_verts() const
 {
-  const MDeformVert *dverts = BKE_mesh_deform_verts(this);
+  const MDeformVert *dverts = static_cast<const MDeformVert *>(
+      CustomData_get_layer(&this->vert_data, CD_MDEFORMVERT));
   if (!dverts) {
     return {};
   }
@@ -397,7 +399,14 @@ inline blender::Span<MDeformVert> Mesh::deform_verts() const
 }
 inline blender::MutableSpan<MDeformVert> Mesh::deform_verts_for_write()
 {
-  return {BKE_mesh_deform_verts_for_write(this), this->totvert};
+  MDeformVert *dvert = static_cast<MDeformVert *>(
+      CustomData_get_layer_for_write(&this->vert_data, CD_MDEFORMVERT, this->totvert));
+  if (dvert) {
+    return {dvert, this->totvert};
+  }
+  return {static_cast<MDeformVert *>(CustomData_add_layer(
+              &this->vert_data, CD_MDEFORMVERT, CD_SET_DEFAULT, this->totvert)),
+          this->totvert};
 }
 
 /** \} */

source/blender/blenkernel/intern/armature_deform.cc
@@ -520,7 +520,7 @@ static void armature_deform_coords_impl(const Object *ob_arm,
       target_data_id = me_target == nullptr ? (const ID *)ob_target->data : &me_target->id;
       if (em_target == nullptr) {
         const Mesh *mesh = (const Mesh *)target_data_id;
-        dverts = BKE_mesh_deform_verts(mesh);
+        dverts = mesh->deform_verts().data();
         if (dverts) {
           dverts_len = mesh->totvert;
         }
@@ -555,7 +555,7 @@ static void armature_deform_coords_impl(const Object *ob_arm,
         use_dverts = (cd_dvert_offset != -1);
       }
       else if (me_target) {
-        use_dverts = (BKE_mesh_deform_verts(me_target) != nullptr);
+        use_dverts = !me_target->deform_verts().is_empty();
       }
       else if (dverts) {
         use_dverts = true;

source/blender/blenkernel/intern/fluid.cc
@@ -2085,7 +2085,7 @@ static void emit_from_mesh(
     const blender::Span<int> corner_verts = mesh->corner_verts();
     const blender::Span<MLoopTri> looptris = mesh->looptris();
     const int numverts = mesh->totvert;
-    const MDeformVert *dvert = BKE_mesh_deform_verts(mesh);
+    const MDeformVert *dvert = mesh->deform_verts().data();
     const float(*mloopuv)[2] = static_cast<const float(*)[2]>(
         CustomData_get_layer_named(&mesh->loop_data, CD_PROP_FLOAT2, ffs->uvlayer_name));
 
@@ -3219,14 +3219,17 @@ static Mesh *create_liquid_geometry(FluidDomainSettings *fds,
                                     Mesh *orgmesh,
                                     Object *ob)
 {
+  using namespace blender;
   Mesh *mesh;
   float min[3];
   float max[3];
   float size[3];
   float cell_size_scaled[3];
 
-  const int *orig_material_indices = BKE_mesh_material_indices(orgmesh);
-  const short mp_mat_nr = orig_material_indices ? orig_material_indices[0] : 0;
+  const bke::AttributeAccessor orig_attributes = orgmesh->attributes();
+  const VArraySpan orig_material_indices = *orig_attributes.lookup<int>("material_index",
+                                                                        ATTR_DOMAIN_FACE);
+  const short mp_mat_nr = orig_material_indices.is_empty() ? 0 : orig_material_indices[0];
 
   int i;
   int num_verts, num_faces;
@@ -3341,12 +3344,14 @@ static Mesh *create_liquid_geometry(FluidDomainSettings *fds,
     }
   }
 
-  int *material_indices = BKE_mesh_material_indices_for_write(mesh);
+  bke::MutableAttributeAccessor attributes = mesh->attributes_for_write();
+  bke::SpanAttributeWriter material_indices = attributes.lookup_or_add_for_write_span<int>(
+      "material_index", ATTR_DOMAIN_FACE);
 
   /* Loop for triangles. */
   for (const int i : face_offsets.index_range().drop_back(1)) {
     /* Initialize from existing face. */
-    material_indices[i] = mp_mat_nr;
+    material_indices.span[i] = mp_mat_nr;
 
     face_offsets[i] = i * 3;
 
@@ -3361,6 +3366,8 @@ static Mesh *create_liquid_geometry(FluidDomainSettings *fds,
            mloops[2].v);
 #  endif
   }
+
+  material_indices.finish();
 
   BKE_mesh_calc_edges(mesh, false, false);
 

source/blender/blenkernel/intern/key.cc
@@ -1274,7 +1274,7 @@ static float *get_weights_array(Object *ob, char *vgroup, WeightsArrayCache *cac
   /* gather dvert and totvert */
   if (ob->type == OB_MESH) {
     Mesh *mesh = static_cast<Mesh *>(ob->data);
-    dvert = BKE_mesh_deform_verts(mesh);
+    dvert = mesh->deform_verts().data();
     totvert = mesh->totvert;
 
     if (mesh->edit_mesh && mesh->edit_mesh->bm->totvert == totvert) {

source/blender/blenkernel/intern/lattice_deform.cc
@@ -372,7 +372,7 @@ static void lattice_deform_coords_impl(const Object *ob_lattice,
         dvert = ((Lattice *)ob_target->data)->dvert;
       }
       else {
-        dvert = BKE_mesh_deform_verts((Mesh *)ob_target->data);
+        dvert = ((Mesh *)ob_target->data)->deform_verts().data();
       }
     }
   }

source/blender/blenkernel/intern/mesh_mirror.cc
@@ -447,7 +447,7 @@ Mesh *BKE_mesh_mirror_apply_mirror_on_axis_for_modifier(MirrorModifierData *mmd,
   /* handle vgroup stuff */
   if (BKE_object_supports_vertex_groups(ob)) {
     if ((mmd->flag & MOD_MIR_VGROUP) && CustomData_has_layer(&result->vert_data, CD_MDEFORMVERT)) {
-      MDeformVert *dvert = BKE_mesh_deform_verts_for_write(result) + src_verts_num;
+      MDeformVert *dvert = result->deform_verts_for_write().data() + src_verts_num;
       int flip_map_len = 0;
       int *flip_map = BKE_object_defgroup_flip_map(ob, false, &flip_map_len);
       if (flip_map) {

commit 13148cdf39ecd660ecbcd11bf0bc066a07153325

Fix: (studio-reported) crash in Solidify Modifier code.
Regression from b52a071e7a3a, mistakenly replaced the destination by the
source mesh for some data (like face material indices attribute).

source/blender/modifiers/intern/MOD_solidify_extrude.cc
@@ -417,7 +417,7 @@ Mesh *MOD_solidify_extrude_modifyMesh(ModifierData *md, const ModifierEvalContex
   } \
   (void)0
 
-  bke::MutableAttributeAccessor dst_attributes = mesh->attributes_for_write();
+  bke::MutableAttributeAccessor dst_attributes = result->attributes_for_write();
   bke::SpanAttributeWriter dst_material_index = dst_attributes.lookup_or_add_for_write_span<int>(
       "material_index", ATTR_DOMAIN_FACE);
 