0b00b360f7aed3b9b57be9164c264fcdbea1b7c7
Germano Cavalcante <germano.costa@ig.com.br>
2023-12-18 10:26:11 -0300

    Fix #116058: 'Align Rotation to Target' does not consider object orientation
    Correction of 3612e0ef04.
    
    `ElementRotation_ex` only works with matrix in global space.
    Therefore convert the vectors to global space.

--- source/blender/editors/transform/transform_mode_translate.cc
+++ source/blender/editors/transform/transform_mode_translate.cc
@@ -74,7 +74,6 @@ struct TransDataArgs_Translate {
   const TransInfo *t;
   const TransDataContainer *tc;
   float3 snap_source_local;
-  float3 snap_normal_local;
   float3 vec;
   enum eTranslateRotateMode rotate_mode;
 };
@@ -83,7 +82,6 @@ static void transdata_elem_translate(const TransInfo *t,
                                      const TransDataContainer *tc,
                                      TransData *td,
                                      const float3 &snap_source_local,
-                                     const float3 &snap_normal_local,
                                      const float3 &vec,
                                      enum eTranslateRotateMode rotate_mode)
 {
@@ -100,7 +98,7 @@ static void transdata_elem_translate(const TransInfo *t,
     else {
       BLI_assert(rotate_mode == TRANSLATE_ROTATE_ON);
 
-      const float *original_normal;
+      float3 original_normal;
 
       /* In pose mode, we want to align normals with Y axis of bones. */
       if (t->options & CTX_POSE_BONE) {
@@ -110,7 +108,13 @@ static void transdata_elem_translate(const TransInfo *t,
         original_normal = td->axismtx[2];
       }
 
-      rotation_between_vecs_to_mat3(mat, original_normal, snap_normal_local);
+      if (t->flag & T_POINTS) {
+        /* Convert to Global Space since #ElementRotation_ex operates with the matrix in global
+         * space. */
+        original_normal = math::transform_direction(float3x3(td->mtx), original_normal);
+      }
+
+      rotation_between_vecs_to_mat3(mat, original_normal, t->tsnap.snapNormal);
     }
 
     ElementRotation_ex(t, tc, td, mat, snap_source_local);
@@ -169,13 +173,8 @@ static void transdata_elem_translate_fn(void *__restrict iter_data_v,
   if (td->flag & TD_SKIP) {
     return;
   }
-  transdata_elem_translate(data->t,
-                           data->tc,
-                           td,
-                           data->snap_source_local,
-                           data->snap_normal_local,
-                           data->vec,
-                           data->rotate_mode);
+  transdata_elem_translate(
+      data->t, data->tc, td, data->snap_source_local, data->vec, data->rotate_mode);
 }
 
 /** \} */
@@ -504,17 +503,12 @@ static void applyTranslationValue(TransInfo *t, const float vec[3])
   }
 
   FOREACH_TRANS_DATA_CONTAINER (t, tc) {
-    float3 snap_source_local, snap_normal_local;
+    float3 snap_source_local;
     if (rotate_mode != TRANSLATE_ROTATE_OFF) {
       snap_source_local = t->tsnap.snap_source;
-      snap_normal_local = t->tsnap.snapNormal;
       if (tc->use_local_mat) {
         /* The pivot has to be in local-space (see #49494) */
         snap_source_local = math::transform_point(float4x4(tc->imat), snap_source_local);
-        if (t->data_type == &TransConvertType_Mesh) {
-          /* The #td->axismtx of other element types (e.g. Pose) are already in global space. */
-          snap_normal_local = math::transform_direction(float3x3(tc->imat3), snap_normal_local);
-        }
       }
     }
 
@@ -524,8 +518,7 @@ static void applyTranslationValue(TransInfo *t, const float vec[3])
         if (td->flag & TD_SKIP) {
           continue;
         }
-        transdata_elem_translate(
-            t, tc, td, snap_source_local, snap_normal_local, vec, rotate_mode);
+        transdata_elem_translate(t, tc, td, snap_source_local, vec, rotate_mode);
       }
     }
     else {
@@ -533,7 +526,6 @@ static void applyTranslationValue(TransInfo *t, const float vec[3])
       data.t = t;
       data.tc = tc;
       data.snap_source_local = snap_source_local;
-      data.snap_normal_local = snap_normal_local;
       data.vec = vec;
       data.rotate_mode = rotate_mode;
 
