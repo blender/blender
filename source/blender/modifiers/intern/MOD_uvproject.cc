blender/blender

commit 2a1ac6145a97ace82a7e1c10bea37d247c0b6fa0

Cleanup: Use C++ attribute API to add attributes in a few places
This is a bit more flexible and future proof than CustomData.

source/blender/modifiers/intern/MOD_uvproject.cc
@@ -22,6 +22,7 @@
 #include "DNA_object_types.h"
 #include "DNA_screen_types.h"
 
+#include "BKE_attribute.hh"
 #include "BKE_camera.h"
 #include "BKE_context.h"
 #include "BKE_lib_query.h"
@@ -119,10 +120,8 @@ static Mesh *uvprojectModifier_do(UVProjectModifierData *umd,
 
   /* Create a new layer if no UV Maps are available
    * (e.g. if a preceding modifier could not preserve it). */
-  if (!CustomData_has_layer(&mesh->ldata, CD_PROP_FLOAT2)) {
-    CustomData_add_layer_named(
-        &mesh->ldata, CD_PROP_FLOAT2, CD_SET_DEFAULT, mesh->totloop, umd->uvlayer_name);
-  }
+  mesh->attributes_for_write().add<float2>(
+      umd->uvlayer_name, ATTR_DOMAIN_CORNER, bke::AttributeInitDefaultValue());
 
   /* make sure we're using an existing layer */
   CustomData_validate_layer_name(&mesh->ldata, CD_PROP_FLOAT2, umd->uvlayer_name, uvname);

source/blender/blenkernel/intern/curves_geometry.cc
@@ -60,8 +60,8 @@ CurvesGeometry::CurvesGeometry(const int point_num, const int curve_num)
   CustomData_reset(&this->point_data);
   CustomData_reset(&this->curve_data);
 
-  CustomData_add_layer_named(
-      &this->point_data, CD_PROP_FLOAT3, CD_CONSTRUCT, this->point_num, ATTR_POSITION.c_str());
+  this->attributes_for_write().add<float3>(
+      "position", ATTR_DOMAIN_POINT, AttributeInitConstruct());
 
   this->runtime = MEM_new<CurvesGeometryRuntime>(__func__);
 

source/blender/blenkernel/intern/mesh_calc_edges.cc
@@ -205,10 +205,8 @@ void BKE_mesh_calc_edges(Mesh *mesh, bool keep_existing_edges, const bool select
   }
 
   /* Create new edges. */
-  if (!CustomData_has_layer_named(&mesh->ldata, CD_PROP_INT32, ".corner_edge")) {
-    CustomData_add_layer_named(
-        &mesh->ldata, CD_PROP_INT32, CD_CONSTRUCT, mesh->totloop, ".corner_edge");
-  }
+  MutableAttributeAccessor attributes = mesh->attributes_for_write();
+  attributes.add<int>(".corner_edge", ATTR_DOMAIN_CORNER, AttributeInitConstruct());
   MutableSpan<int2> new_edges{
       static_cast<int2 *>(MEM_calloc_arrayN(new_totedge, sizeof(int2), __func__)), new_totedge};
   calc_edges::serialize_and_initialize_deduplicated_edges(edge_maps, new_edges);
@@ -221,9 +219,8 @@ void BKE_mesh_calc_edges(Mesh *mesh, bool keep_existing_edges, const bool select
   /* Free old CustomData and assign new one. */
   CustomData_free(&mesh->edata, mesh->totedge);
   CustomData_reset(&mesh->edata);
-  CustomData_add_layer_named_with_data(
-      &mesh->edata, CD_PROP_INT32_2D, new_edges.data(), new_totedge, ".edge_verts", nullptr);
   mesh->totedge = new_totedge;
+  attributes.add<int2>(".edge_verts", ATTR_DOMAIN_EDGE, AttributeInitMoveArray(new_edges.data()));
 
   if (select_new_edges) {
     MutableAttributeAccessor attributes = mesh->attributes_for_write();

source/blender/blenkernel/intern/mesh_convert.cc
@@ -69,6 +69,7 @@ static CLG_LogRef LOG = {"bke.mesh_convert"};
 
 static Mesh *mesh_nurbs_displist_to_mesh(const Curve *cu, const ListBase *dispbase)
 {
+  using namespace blender;
   using namespace blender::bke;
   int a, b, ofs;
   const bool conv_polys = (
@@ -127,9 +128,9 @@ static Mesh *mesh_nurbs_displist_to_mesh(const Curve *cu, const ListBase *dispba
       "material_index", ATTR_DOMAIN_FACE);
   SpanAttributeWriter<bool> sharp_faces = attributes.lookup_or_add_for_write_span<bool>(
       "sharp_face", ATTR_DOMAIN_FACE);
-
-  blender::float2 *mloopuv = static_cast<blender::float2 *>(CustomData_add_layer_named(
-      &mesh->ldata, CD_PROP_FLOAT2, CD_SET_DEFAULT, mesh->totloop, DATA_("UVMap")));
+  SpanAttributeWriter<float2> uv_attribute = attributes.lookup_or_add_for_write_span<float2>(
+      DATA_("UVMap"), ATTR_DOMAIN_CORNER);
+  MutableSpan<float2> uv_map = uv_attribute.span;
 
   int dst_vert = 0;
   int dst_edge = 0;
@@ -203,11 +204,9 @@ static Mesh *mesh_nurbs_displist_to_mesh(const Curve *cu, const ListBase *dispba
         poly_offsets[dst_poly] = dst_loop;
         material_indices.span[dst_poly] = dl->col;
 
-        if (mloopuv) {
-          for (int i = 0; i < 3; i++, mloopuv++) {
-            (*mloopuv)[0] = (corner_verts[dst_loop + i] - startvert) / float(dl->nr - 1);
-            (*mloopuv)[1] = 0.0f;
-          }
+        for (int i = 0; i < 3; i++) {
+          uv_map[dst_loop + i][0] = (corner_verts[dst_loop + i] - startvert) / float(dl->nr - 1);
+          uv_map[dst_loop + i][1] = 0.0f;
         }
 
         sharp_faces.span[dst_poly] = !is_smooth;
@@ -260,35 +259,33 @@ static Mesh *mesh_nurbs_displist_to_mesh(const Curve *cu, const ListBase *dispba
           poly_offsets[dst_poly] = dst_loop;
           material_indices.span[dst_poly] = dl->col;
 
-          if (mloopuv) {
-            int orco_sizeu = dl->nr - 1;
-            int orco_sizev = dl->parts - 1;
+          int orco_sizeu = dl->nr - 1;
+          int orco_sizev = dl->parts - 1;
 
-            /* exception as handled in convertblender.c too */
-            if (dl->flag & DL_CYCL_U) {
-              orco_sizeu++;
-              if (dl->flag & DL_CYCL_V) {
-                orco_sizev++;
-              }
-            }
-            else if (dl->flag & DL_CYCL_V) {
+          /* exception as handled in convertblender.c too */
+          if (dl->flag & DL_CYCL_U) {
+            orco_sizeu++;
+            if (dl->flag & DL_CYCL_V) {
               orco_sizev++;
             }
+          }
+          else if (dl->flag & DL_CYCL_V) {
+            orco_sizev++;
+          }
 
-            for (int i = 0; i < 4; i++, mloopuv++) {
-              /* find uv based on vertex index into grid array */
-              int v = corner_verts[dst_loop + i] - startvert;
+          for (int i = 0; i < 4; i++) {
+            /* find uv based on vertex index into grid array */
+            int v = corner_verts[dst_loop + i] - startvert;
 
-              (*mloopuv)[0] = (v / dl->nr) / float(orco_sizev);
-              (*mloopuv)[1] = (v % dl->nr) / float(orco_sizeu);
+            uv_map[dst_loop + i][0] = (v / dl->nr) / float(orco_sizev);
+            uv_map[dst_loop + i][1] = (v % dl->nr) / float(orco_sizeu);
 
-              /* cyclic correction */
-              if (ELEM(i, 1, 2) && (*mloopuv)[0] == 0.0f) {
-                (*mloopuv)[0] = 1.0f;
-              }
-              if (ELEM(i, 0, 1) && (*mloopuv)[1] == 0.0f) {
-                (*mloopuv)[1] = 1.0f;
-              }
+            /* cyclic correction */
+            if (ELEM(i, 1, 2) && uv_map[dst_loop + i][0] == 0.0f) {
+              uv_map[dst_loop + i][0] = 1.0f;
+            }
+            if (ELEM(i, 0, 1) && uv_map[dst_loop + i][1] == 0.0f) {
+              uv_map[dst_loop + i][1] = 1.0f;
             }
           }
 
@@ -311,6 +308,7 @@ static Mesh *mesh_nurbs_displist_to_mesh(const Curve *cu, const ListBase *dispba
 
   material_indices.finish();
   sharp_faces.finish();
+  uv_attribute.finish();
 
   return mesh;
 }

source/blender/blenkernel/intern/pointcloud.cc
@@ -64,11 +64,8 @@ static void pointcloud_init_data(ID *id)
   MEMCPY_STRUCT_AFTER(pointcloud, DNA_struct_default_get(PointCloud), id);
 
   CustomData_reset(&pointcloud->pdata);
-  CustomData_add_layer_named(&pointcloud->pdata,
-                             CD_PROP_FLOAT3,
-                             CD_CONSTRUCT,
-                             pointcloud->totpoint,
-                             POINTCLOUD_ATTR_POSITION);
+  pointcloud->attributes_for_write().add<float3>(
+      "position", ATTR_DOMAIN_POINT, blender::bke::AttributeInitConstruct());
 
   pointcloud->runtime = new blender::bke::PointCloudRuntime();
 }

source/blender/editors/sculpt_paint/sculpt_undo.cc
@@ -1840,8 +1840,7 @@ static void sculpt_undo_set_active_layer(bContext *C, SculptAttrRef *attr)
   SculptAttrRef existing;
   sculpt_save_active_attribute(ob, &existing);
 
-  CustomDataLayer *layer;
-  layer = BKE_id_attribute_find(&me->id, attr->name, attr->type, attr->domain);
+  CustomDataLayer *layer = BKE_id_attribute_find(&me->id, attr->name, attr->type, attr->domain);
 
   /* Temporary fix for #97408. This is a fundamental
    * bug in the undo stack; the operator code needs to push
@@ -1864,11 +1863,8 @@ static void sculpt_undo_set_active_layer(bContext *C, SculptAttrRef *attr)
 
   if (!layer) {
     /* Memfile undo killed the layer; re-create it. */
-    CustomData *cdata = attr->domain == ATTR_DOMAIN_POINT ? &me->vdata : &me->ldata;
-    int totelem = attr->domain == ATTR_DOMAIN_POINT ? me->totvert : me->totloop;
-
-    CustomData_add_layer_named(
-        cdata, eCustomDataType(attr->type), CD_SET_DEFAULT, totelem, attr->name);
+    me->attributes_for_write().add(
+        attr->name, attr->domain, attr->type, blender::bke::AttributeInitDefaultValue());
     layer = BKE_id_attribute_find(&me->id, attr->name, attr->type, attr->domain);
   }
 

commit 2396d07f4e4b6a2cb95fdf57e30184f0386bd801

Fix #115857: UV project modifier crash with invalid name
Caused by 2a1ac6145a97ace82a7e1c10bea37d247c0b6fa0

If the attribute creation failed, the modifier would try to fill in a
null UV map. The combination of CustomData and attribute API functions
was a bit confusing, so restore the old behavior more explicitly using
just the attribute API (including the slightly weird "Float2" default
name. That could be changed later.)

source/blender/modifiers/intern/MOD_uvproject.cc
@@ -93,6 +93,20 @@ struct Projector {
   void *uci;           /* optional uv-project info (panorama projection) */
 };
 
+static blender::bke::SpanAttributeWriter<blender::float2> get_uv_attribute(
+    Mesh &mesh, const blender::StringRef md_name)
+{
+  using namespace blender;
+  bke::MutableAttributeAccessor attributes = mesh.attributes_for_write();
+  if (md_name.is_empty()) {
+    const StringRef name = CustomData_get_active_layer_name(&mesh.corner_data, CD_PROP_FLOAT2);
+    return attributes.lookup_or_add_for_write_span<float2>(name.is_empty() ? "Float2" : name,
+                                                           bke::AttrDomain::Corner);
+  }
+  const std::string name = BKE_id_attribute_calc_unique_name(mesh.id, md_name);
+  return attributes.lookup_or_add_for_write_span<float2>(name, bke::AttrDomain::Corner);
+}
+
 static Mesh *uvprojectModifier_do(UVProjectModifierData *umd,
                                   const ModifierEvalContext * /*ctx*/,
                                   Object *ob,
@@ -101,12 +115,11 @@ static Mesh *uvprojectModifier_do(UVProjectModifierData *umd,
   using namespace blender;
   Projector projectors[MOD_UVPROJECT_MAXPROJECTORS];
   int projectors_num = 0;
-  char uvname[MAX_CUSTOMDATA_LAYER_NAME];
   float aspx = umd->aspectx ? umd->aspectx : 1.0f;
   float aspy = umd->aspecty ? umd->aspecty : 1.0f;
   float scax = umd->scalex ? umd->scalex : 1.0f;
   float scay = umd->scaley ? umd->scaley : 1.0f;
-  int free_uci = 0;
+  bool free_uci = false;
 
   for (int i = 0; i < umd->projectors_num; i++) {
     if (umd->projectors[i] != nullptr) {
@@ -118,13 +131,10 @@ static Mesh *uvprojectModifier_do(UVProjectModifierData *umd,
     return mesh;
   }
 
-  /* Create a new layer if no UV Maps are available
-   * (e.g. if a preceding modifier could not preserve it). */
-  mesh->attributes_for_write().add<float2>(
-      umd->uvlayer_name, bke::AttrDomain::Corner, bke::AttributeInitDefaultValue());
-
-  /* make sure we're using an existing layer */
-  CustomData_validate_layer_name(&mesh->corner_data, CD_PROP_FLOAT2, umd->uvlayer_name, uvname);
+  bke::SpanAttributeWriter uv_attribute = get_uv_attribute(*mesh, umd->uvlayer_name);
+  if (!uv_attribute) {
+    return mesh;
+  }
 
   /* calculate a projection matrix and normal for each projector */
   for (int i = 0; i < projectors_num; i++) {
@@ -142,7 +152,7 @@ static Mesh *uvprojectModifier_do(UVProjectModifierData *umd,
         projectors[i].uci = BLI_uvproject_camera_info(projectors[i].ob, nullptr, aspx, aspy);
         BLI_uvproject_camera_info_scale(
             static_cast<ProjCameraInfo *>(projectors[i].uci), scax, scay);
-        free_uci = 1;
+        free_uci = true;
       }
       else {
         CameraParams params;
@@ -184,13 +194,10 @@ static Mesh *uvprojectModifier_do(UVProjectModifierData *umd,
   const Span<float3> positions = mesh->vert_positions();
   const OffsetIndices faces = mesh->faces();
   const Span<int> corner_verts = mesh->corner_verts();
-
-  float(*mloop_uv)[2] = static_cast<float(*)[2]>(CustomData_get_layer_named_for_write(
-      &mesh->corner_data, CD_PROP_FLOAT2, uvname, corner_verts.size()));
-
-  Array<float3> coords(positions.size());
+  MutableSpan<float2> mloop_uv = uv_attribute.span;
 
   /* Convert coords to world-space. */
+  Array<float3> coords(positions.size());
   for (int64_t i = 0; i < positions.size(); i++) {
     mul_v3_m4v3(coords[i], ob->object_to_world, positions[i]);
   }
@@ -269,6 +276,8 @@ static Mesh *uvprojectModifier_do(UVProjectModifierData *umd,
       }
     }
   }
+
+  uv_attribute.finish();
 
   mesh->runtime->is_original_bmesh = false;
 