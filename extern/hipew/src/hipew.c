f753cf14fca2b97d4bbf48eccc1defcaa1d17daa
bsavery <brian.savery@gmail.com>
2024-01-03 18:16:07 +0100

    Cycles: add ROCm 6 compatibility for HIP
    ROCm 6 brings some changes to the HIP API. This pull request is meant to be
    backward and forward compatible.
    
    That is Blender could be compiled with either ROCM 6 or 5 and run on either.
    The main change is the hipMemoryType enum, which we check based on the
    runtime version to use the correct enum values.
    
    Without this, HIP will not work on Windows with upcoming 23.40 driver.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/116713

--- extern/hipew/src/hipew.c
+++ extern/hipew/src/hipew.c
@@ -35,6 +35,7 @@ thipGetErrorString *hipGetErrorString;
 thipGetLastError *hipGetLastError;
 thipInit *hipInit;
 thipDriverGetVersion *hipDriverGetVersion;
+thipRuntimeGetVersion *hipRuntimeGetVersion;
 thipGetDevice *hipGetDevice;
 thipGetDeviceCount *hipGetDeviceCount;
 thipGetDeviceProperties *hipGetDeviceProperties;
@@ -285,6 +286,7 @@ static int hipewHipInit(void) {
   HIP_LIBRARY_FIND_CHECKED(hipGetLastError);
   HIP_LIBRARY_FIND_CHECKED(hipInit);
   HIP_LIBRARY_FIND_CHECKED(hipDriverGetVersion);
+  HIP_LIBRARY_FIND_CHECKED(hipRuntimeGetVersion);
   HIP_LIBRARY_FIND_CHECKED(hipGetDevice);
   HIP_LIBRARY_FIND_CHECKED(hipGetDeviceCount);
   HIP_LIBRARY_FIND_CHECKED(hipGetDeviceProperties);
@@ -410,7 +412,28 @@ static int hipewHipInit(void) {
   return result;
 }
 
+hipMemoryType get_hip_memory_type(hipMemoryType mem_type, int runtime_version) {
+  /** Convert hipMemoryType for backwards compatibility with rocm5/6. 
+   * This can be removed when support for ROCm 5 is removed. */
 
+  /* If version is 5 we need to use the old enum vals (60000000 is start of ROCm 6) */
+  if (runtime_version > 60000000) {
+    return mem_type;
+  }
+
+  switch (mem_type) {
+    case hipMemoryTypeHost:
+      return hipMemoryTypeHost_v5;
+    case hipMemoryTypeDevice:
+      return hipMemoryTypeDevice_v5;
+    case hipMemoryTypeArray:
+      return hipMemoryTypeArray_v5;
+    case hipMemoryTypeUnified:
+      return hipMemoryTypeUnified_v5;
+    default:
+      return hipMemoryTypeUnregistered;  /* This should not happen. */
+  }
+}
 
 int hipewInit(hipuint32_t flags) {
   int result = HIPEW_SUCCESS;
--- intern/cycles/device/hip/device_impl.h
+++ intern/cycles/device/hip/device_impl.h
@@ -32,6 +32,7 @@ class HIPDevice : public GPUDevice {
   int pitch_alignment;
   int hipDevId;
   int hipDevArchitecture;
+  int hipRuntimeVersion;
   bool first_error;
 
   HIPDeviceKernels kernels;
@@ -102,6 +103,7 @@ class HIPDevice : public GPUDevice {
  protected:
   bool get_device_attribute(hipDeviceAttribute_t attribute, int *value);
   int get_device_default_attribute(hipDeviceAttribute_t attribute, int default_value);
+  hipMemoryType get_memory_type(hipMemoryType mem_type);
 };
 
 CCL_NAMESPACE_END
