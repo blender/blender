2e2b5dcd52ad7c92b0d937751e1fe471377525e0
Sybren A. Stüvel <sybren@blender.org>
2024-01-08 12:30:58 +0100

    Anim: Outliner, show bone collections in their hierarchy
    The Outliner now shows an Armature's bone collections as a hierarchy
    instead of a flat list.

--- source/blender/editors/space_outliner/tree/tree_element_bone_collection.cc
+++ source/blender/editors/space_outliner/tree/tree_element_bone_collection.cc
@@ -30,19 +30,32 @@ TreeElementBoneCollectionBase::TreeElementBoneCollectionBase(TreeElement &legacy
 
 void TreeElementBoneCollectionBase::expand(SpaceOutliner & /*space_outliner*/) const
 {
-  for (int index = 0; index < armature_.collection_array_num; index++) {
-    BoneCollection *bcoll = armature_.collection_array[index];
+  int index = 0;
+  for (BoneCollection *bcoll : armature_.collections_roots()) {
     add_element(
         &legacy_te_.subtree, &armature_.id, bcoll, &legacy_te_, TSE_BONE_COLLECTION, index);
+    index++;
   }
 }
 
-TreeElementBoneCollection::TreeElementBoneCollection(TreeElement &legacy_te, BoneCollection &bcoll)
-    : AbstractTreeElement(legacy_te), bcoll_(bcoll)
+TreeElementBoneCollection::TreeElementBoneCollection(TreeElement &legacy_te,
+                                                     bArmature &armature,
+                                                     BoneCollection &bcoll)
+    : AbstractTreeElement(legacy_te), armature_(armature), bcoll_(bcoll)
 {
   BLI_assert(legacy_te.store_elem->type == TSE_BONE_COLLECTION);
   legacy_te.name = bcoll_.name;
   legacy_te.directdata = &bcoll_;
+}
+
+void TreeElementBoneCollection::expand(SpaceOutliner & /*space_outliner*/) const
+{
+  int index = 0;
+  for (BoneCollection *child_bcoll : armature_.collection_children(&bcoll_)) {
+    add_element(
+        &legacy_te_.subtree, &armature_.id, child_bcoll, &legacy_te_, TSE_BONE_COLLECTION, index);
+    index++;
+  }
 }
 
 }  // namespace blender::ed::outliner
