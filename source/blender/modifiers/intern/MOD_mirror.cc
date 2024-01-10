d9d9b12e794d9ab73aab995b08ecf96791de90fe
Campbell Barton <campbell@blender.org>
2024-01-10 23:36:30 +1100

    Fix error in mirror modifier UI only checking X axis bisect

--- source/blender/modifiers/intern/MOD_mirror.cc
+++ source/blender/modifiers/intern/MOD_mirror.cc
@@ -145,7 +145,7 @@ static void panel_draw(const bContext * /*C*/, Panel *panel)
   PointerRNA *ptr = modifier_panel_get_property_pointers(panel, &ob_ptr);
   MirrorModifierData *mmd = (MirrorModifierData *)ptr->data;
   bool has_bisect = (mmd->flag &
-                     (MOD_MIR_BISECT_AXIS_X | MOD_MIR_BISECT_AXIS_X | MOD_MIR_BISECT_AXIS_X));
+                     (MOD_MIR_BISECT_AXIS_X | MOD_MIR_BISECT_AXIS_Y | MOD_MIR_BISECT_AXIS_Z));
 
   col = uiLayoutColumn(layout, false);
   uiLayoutSetPropSep(col, true);
