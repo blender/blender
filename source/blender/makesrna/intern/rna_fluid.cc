67b21ce54d23cea73e00722af40f870a571845dd
Aaron Carlisle <blendify@noreply.localhost>
2024-01-12 16:55:05 +0100

    UI: Clarify Liquid Diffusion/Viscosity Properties
    The viscosity panel was confusing for users because viscosity
    is defined by the base and exponent properties in the Diffusion panel.
    
    What this setting actually does is use a special solver algorithm that is designed for high viscosity liquids.
    See 635694c0ff8fc5c9828bf920ecb81bb9bf792a82 for details.
    
    The below changes are designed to better represent this to the user.
    
    - Move the "Viscosity" Panel to a sub panel of "Diffusion"
    - Rename "Viscosity" to "High Viscosity Solver" to better represent what this property does
    - Update `use_viscosity` tooltip to better explain that this uses a different solver algorithm.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/116118

--- source/blender/makesrna/intern/rna_fluid.cc
+++ source/blender/makesrna/intern/rna_fluid.cc
@@ -1904,7 +1904,8 @@ static void rna_def_fluid_domain_settings(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "use_viscosity", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flags", FLUID_DOMAIN_USE_VISCOSITY);
-  RNA_def_property_ui_text(prop, "Use Viscosity", "Enable fluid viscosity settings");
+  RNA_def_property_ui_text(
+      prop, "Use Viscosity", "Simulate fluids with high viscosity using a special solver");
   RNA_def_property_update(prop, NC_OBJECT | ND_MODIFIER, "rna_Fluid_datacache_reset");
 
   prop = RNA_def_property(srna, "viscosity_value", PROP_FLOAT, PROP_NONE);
