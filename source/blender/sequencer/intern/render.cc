753c52ba15f4aae97b4208ba8465f7f4633bb613
Omar Emara <mail@OmarEmara.dev>
2024-01-16 09:13:30 +0100

    Fix: Sequencer crash with scene strip of no composite output
    If a scene sequencer strip references a scene with no composite output,
    it will crash due to null image buffer output. This patch fixes that by
    allocating an empty buffer in those cases.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/117135

--- source/blender/sequencer/intern/render.cc
+++ source/blender/sequencer/intern/render.cc
@@ -1598,11 +1598,14 @@ static ImBuf *seq_render_scene_strip(const SeqRenderData *context,
         /* float buffers in the sequencer are not linear */
         seq_imbuf_to_sequencer_space(context->scene, ibufs_arr[view_id], false);
       }
-      else if (rres.ibuf->byte_buffer.data) {
+      else if (rres.ibuf && rres.ibuf->byte_buffer.data) {
         ibufs_arr[view_id] = IMB_allocImBuf(rres.rectx, rres.recty, 32, IB_rect);
         memcpy(ibufs_arr[view_id]->byte_buffer.data,
                rres.ibuf->byte_buffer.data,
                4 * rres.rectx * rres.recty);
+      }
+      else {
+        ibufs_arr[view_id] = IMB_allocImBuf(rres.rectx, rres.recty, 32, IB_rect);
       }
 
       if (view_id != context->view_id) {
