633e8f09c891956e5f4ba71953a62b7842e905c8
Hans Goudey <hans@blender.org>
2024-01-17 11:03:12 -0500

    Cleanup: Remove unused asset C functions
    This information can be retrieved from `AssetRepresentation` now.
    Also remove unnecessarily specific lambda capture. That usually suggests
    usage in `std::function` with a more complex lifetime.

--- source/blender/editors/asset/ED_asset_handle.h
+++ source/blender/editors/asset/ED_asset_handle.h
@@ -35,14 +35,8 @@ extern "C" {
 struct AssetHandle;
 
 AssetRepresentationHandle *ED_asset_handle_get_representation(const struct AssetHandle *asset);
-ID_Type ED_asset_handle_get_id_type(const struct AssetHandle *asset);
 int ED_asset_handle_get_preview_icon_id(const struct AssetHandle *asset);
 int ED_asset_handle_get_preview_or_type_icon_id(const struct AssetHandle *asset);
-void ED_asset_handle_get_full_library_path(
-    const struct AssetHandle *asset,
-    /* `1024` for #FILE_MAX,
-     * rely on warnings to let us know if this gets out of sync. */
-    char r_full_lib_path[1024]);
 
 #ifdef __cplusplus
 }
