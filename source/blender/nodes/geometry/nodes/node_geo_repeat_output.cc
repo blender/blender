f811e9c6d64325c34642f2f59afb25a27766a8d6
Jacques Lucke <jacques@blender.org>
2024-01-17 12:45:06 +0100

    Fix: crash when adding repeat zone from search
    The issue was that some code expected the "extend" sockets
    to always exist. This was already always true from the user
    point of view, but not internally.
    
    There are different possible fixes, but this patch makes sure
    that the extend socket will be available as expected. This is
    achieved by making them part of the static node declaration.
    We want to extend the static declaration of such dynamic
    nodes anyway, to improve reflection capabilities without
    having to instantiate nodes.

--- source/blender/nodes/geometry/nodes/node_geo_repeat_output.cc
+++ source/blender/nodes/geometry/nodes/node_geo_repeat_output.cc
@@ -27,20 +27,19 @@ NODE_STORAGE_FUNCS(NodeGeometryRepeatOutput);
 static void node_declare(NodeDeclarationBuilder &b)
 {
   const bNode *node = b.node_or_null();
-  if (node == nullptr) {
-    return;
-  }
-  const NodeGeometryRepeatOutput &storage = node_storage(*node);
-  for (const int i : IndexRange(storage.items_num)) {
-    const NodeRepeatItem &item = storage.items[i];
-    const eNodeSocketDatatype socket_type = eNodeSocketDatatype(item.socket_type);
-    const StringRef name = item.name ? item.name : "";
-    const std::string identifier = RepeatItemsAccessor::socket_identifier_for_item(item);
-    auto &input_decl = b.add_input(socket_type, name, identifier);
-    auto &output_decl = b.add_output(socket_type, name, identifier);
-    if (socket_type_supports_fields(socket_type)) {
-      input_decl.supports_field();
-      output_decl.dependent_field({input_decl.input_index()});
+  if (node) {
+    const NodeGeometryRepeatOutput &storage = node_storage(*node);
+    for (const int i : IndexRange(storage.items_num)) {
+      const NodeRepeatItem &item = storage.items[i];
+      const eNodeSocketDatatype socket_type = eNodeSocketDatatype(item.socket_type);
+      const StringRef name = item.name ? item.name : "";
+      const std::string identifier = RepeatItemsAccessor::socket_identifier_for_item(item);
+      auto &input_decl = b.add_input(socket_type, name, identifier);
+      auto &output_decl = b.add_output(socket_type, name, identifier);
+      if (socket_type_supports_fields(socket_type)) {
+        input_decl.supports_field();
+        output_decl.dependent_field({input_decl.input_index()});
+      }
     }
   }
   b.add_input<decl::Extend>("", "__extend__");
