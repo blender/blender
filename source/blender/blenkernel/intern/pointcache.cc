8189e2266b086bd495325c5922c27495a64841d2
Philipp Oeser <philipp@blender.org>
2024-01-19 14:53:43 +0100

    Revert "Fix #68436: External Particle Disk Cache Playback Is Broken"
    this broke the dynamic paint test
    
    This reverts commit ab9c0af1ceb0fed54a08c21467ad0ceeea515c8a.

--- source/blender/blenkernel/intern/pointcache.cc
+++ source/blender/blenkernel/intern/pointcache.cc
@@ -61,8 +61,6 @@
 
 #include "BLO_read_write.hh"
 
-#include "DEG_depsgraph_query.hh"
-
 #include "BIK_api.h"
 
 #ifdef WITH_BULLET
@@ -3350,7 +3348,6 @@ void BKE_ptcache_bake(PTCacheBaker *baker)
   }
 
   /* clear baking flag */
-  PTCacheID pid_eval;
   if (pid && cache) {
     cache->flag &= ~(PTCACHE_BAKING | PTCACHE_REDO_NEEDED);
     cache->flag |= PTCACHE_SIMULATION_VALID;
@@ -3358,13 +3355,7 @@ void BKE_ptcache_bake(PTCacheBaker *baker)
       cache->flag |= PTCACHE_BAKED;
       /* write info file */
       if (cache->flag & PTCACHE_DISK_CACHE) {
-        ID *id = pid->owner_id;
-        Object *ob = (GS(id->name) == ID_OB) ? reinterpret_cast<Object *>(id) : nullptr;
-        Object *ob_eval = DEG_get_evaluated_object(depsgraph, ob);
-        ParticleSystem *psys = static_cast<ParticleSystem *>(pid->calldata);
-        ParticleSystem *psys_eval = psys_eval_get(depsgraph, ob, psys);
-        BKE_ptcache_id_from_particles(&pid_eval, ob_eval, psys_eval);
-        BKE_ptcache_write(&pid_eval, 0);
+        BKE_ptcache_write(pid, 0);
       }
     }
   }
@@ -3394,13 +3385,7 @@ void BKE_ptcache_bake(PTCacheBaker *baker)
         if (bake) {
           cache->flag |= PTCACHE_BAKED;
           if (cache->flag & PTCACHE_DISK_CACHE) {
-            ID *id = pid->owner_id;
-            Object *ob = (GS(id->name) == ID_OB) ? reinterpret_cast<Object *>(id) : nullptr;
-            Object *ob_eval = DEG_get_evaluated_object(depsgraph, ob);
-            ParticleSystem *psys = static_cast<ParticleSystem *>(pid->calldata);
-            ParticleSystem *psys_eval = psys_eval_get(depsgraph, ob, psys);
-            BKE_ptcache_id_from_particles(&pid_eval, ob_eval, psys_eval);
-            BKE_ptcache_write(&pid_eval, 0);
+            BKE_ptcache_write(pid, 0);
           }
         }
       }
