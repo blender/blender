741dfa6dcac7d6df01ed4ff0d3b13fdf451c87b6
Pablo Vazquez <pablo@blender.org>
2024-01-22 16:30:04 +0100

    Fix: Typo in release notes script
    Typo in the `--issue` argument help text:
    `Task that is contains` -> `Task that contains`

--- release/lts/create_release_notes.py
+++ release/lts/create_release_notes.py
@@ -20,7 +20,7 @@ parser.add_argument(
     help="Version string in the form of {major}.{minor}.{patch} (e.g. 3.3.2)")
 parser.add_argument(
     "--issue",
-    help="Task that is contains the release notes information (e.g. #77348)")
+    help="Task that contains the release notes information (e.g. #77348)")
 parser.add_argument(
     "--format",
     help="Format the result in `text`, `steam`, `markdown` or `html`",
