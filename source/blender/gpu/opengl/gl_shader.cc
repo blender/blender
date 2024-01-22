18b5b0812bc20e36abad67b8cbba89e1f40f3877
Jeroen Bakker <jeroen@blender.org>
2024-01-22 14:50:42 +0100

    Cleanup: OpenGL program creation and linking
    This PR improves the place when shader stages are attached to glPrograms.
    Previously it was done when shaders stages where created, in the function
    create_shader_stage.
    
    This PR will attach the shader stages inside link program.
    Ensuring that create_shader_stage doesn't alter the program, which isn't
    clear in its name.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/117407

--- source/blender/gpu/opengl/gl_shader.cc
+++ source/blender/gpu/opengl/gl_shader.cc
@@ -1236,8 +1236,6 @@ GLuint GLShader::create_shader_stage(GLenum gl_stage,
   }
 
   debug::object_label(gl_stage, shader, name);
-
-  glAttachShader(program_active_->program_id, shader);
   return shader;
 }
 
@@ -1291,10 +1289,11 @@ bool GLShader::finalize(const shader::ShaderCreateInfo *info)
     Vector<const char *> sources;
     sources.append("version");
     sources.append(source.c_str());
-    geometry_shader_from_glsl(sources);
     if (!constants.types.is_empty()) {
       geometry_sources_ = sources;
     }
+
+    geometry_shader_from_glsl(sources);
   }
 
   if (!program_link()) {
@@ -1518,7 +1517,25 @@ GLShader::GLProgram::~GLProgram()
 
 bool GLShader::program_link()
 {
+  BLI_assert(program_active_ != nullptr);
+  if (program_active_->program_id == 0) {
+    program_active_->program_id = glCreateProgram();
+    debug::object_label(GL_PROGRAM, program_active_->program_id, name);
+  }
   GLuint program_id = program_active_->program_id;
+
+  if (program_active_->vert_shader) {
+    glAttachShader(program_id, program_active_->vert_shader);
+  }
+  if (program_active_->geom_shader) {
+    glAttachShader(program_id, program_active_->geom_shader);
+  }
+  if (program_active_->frag_shader) {
+    glAttachShader(program_id, program_active_->frag_shader);
+  }
+  if (program_active_->compute_shader) {
+    glAttachShader(program_id, program_active_->compute_shader);
+  }
   glLinkProgram(program_id);
 
   GLint status;
@@ -1564,8 +1581,6 @@ GLuint GLShader::program_get()
 
   program_active_ = &program_cache_.lookup_or_add_default(constants.values);
   if (!program_active_->program_id) {
-    program_active_->program_id = glCreateProgram();
-    debug::object_label(GL_PROGRAM, program_active_->program_id, name);
     MutableSpan<const char *> no_sources;
     if (!vertex_sources_.is_empty()) {
       program_active_->vert_shader = create_shader_stage(
