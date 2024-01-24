aecf1fba841ac4c4776b46ebe73a387b45a2a1e2
Sergey Sharybin <sergey@blender.org>
2024-01-24 09:48:27 +0100

    Fix #116098: Change default texture mapping for brushes to View Plane
    The Tiled default is the least commonly used mapping mode, switch the
    default to something that is more commonly used.
    
    Do it for all modes, since according to Julien it is reasonable default
    behavior.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/116933

--- source/blender/blenloader/intern/versioning_defaults.cc
+++ source/blender/blenloader/intern/versioning_defaults.cc
@@ -817,12 +817,17 @@ void BLO_update_defaults_startup_blend(Main *bmain, const char *app_template)
   }
 
   {
-    /* Use the same tool icon color in the brush cursor */
     LISTBASE_FOREACH (Brush *, brush, &bmain->brushes) {
+      /* Use the same tool icon color in the brush cursor */
       if (brush->ob_mode & OB_MODE_SCULPT) {
         BLI_assert(brush->sculpt_tool != 0);
         BKE_brush_sculpt_reset(brush);
       }
+
+      /* Set the default texture mapping.
+       * Do it for all brushes, since some of them might be coming from the startup file. */
+      brush->mtex.brush_map_mode = MTEX_MAP_MODE_VIEW;
+      brush->mask_mtex.brush_map_mode = MTEX_MAP_MODE_VIEW;
     }
   }
 }
