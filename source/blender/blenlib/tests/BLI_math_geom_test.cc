f6276ec1638908cbf88ad9f467a61808ed5c2de5
Aras Pranckevicius <aras@nesnausk.org>
2024-02-03 18:46:39 +0200

    Fix: 2D tri/quad vs point intersection tests edge cases
    isect_point_tri_v2 and isect_point_quad_v2 are handling tris/quads
    in either clockwise or counter-clockwise vertex orderings. However,
    for clockwise order it was considering points that lie on the edges
    or vertices as "inside", whereas for counter-clockwise it was treating
    them as "outside".
    
    Visibly affected place is VSE: it has an optimization that checks
    whether a fully opaque strip image fully covers the rendered area.
    When the strip was scaled up to *exactly* cover the rendered area,
    the check was failing since isect_point_quad_v2 was saying that a
    point is outside the rect.
    
    As far as I can tell, the functions have been "slightly wrong" in
    this way for at least 15 years; harder to see through earlier
    history in git.
    
    Added a bunch of unit tests to cover this. Without the fix, "edge"
    and "corner" cases against "cw" tri/quad were failing.
    
    Performance (checked on clang15 on M1 Max):
    - isect_point_tri_v2 is pretty much the same performance (assembly
      several instructions shorter),
    - isect_point_quad_v2 is about three times *faster* (assembly 2x
      shorter), seemingly the compiler is able to use some SIMD now.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/117786

--- source/blender/blenlib/tests/BLI_math_geom_test.cc
+++ source/blender/blenlib/tests/BLI_math_geom_test.cc
@@ -1,10 +1,13 @@
-/* SPDX-FileCopyrightText: 2023 Blender Authors
+/* SPDX-FileCopyrightText: 2024 Blender Authors
  *
  * SPDX-License-Identifier: Apache-2.0 */
 
 #include "testing/testing.h"
 
 #include "BLI_math_geom.h"
+#include "BLI_math_vector_types.hh"
+
+using namespace blender;
 
 TEST(math_geom, DistToLine2DSimple)
 {
@@ -18,4 +21,99 @@ TEST(math_geom, DistToLineSegment2DSimple)
   float p[2] = {3.0f, 1.0f}, a[2] = {0.0f, 0.0f}, b[2] = {2.0f, 0.0f};
   float distance = dist_to_line_segment_v2(p, a, b);
   EXPECT_NEAR(sqrtf(2.0f), distance, 1e-6);
+}
+
+#include "BLI_rand.hh"
+#include "BLI_timeit.hh"
+
+TEST(math_geom, IsectPointTri2D)
+{
+  float2 tri_cw[3] = {{-2, 1}, {4, 4}, {2, -3}};
+  float2 tri_ccw[3] = {{-2, 1}, {2, -3}, {4, 4}};
+
+  float2 inside1{0, 0};
+  float2 inside2{2, 2};
+  float2 inside3{2, -1};
+  float2 inside4{-1, 1};
+  EXPECT_EQ(-1, isect_point_tri_v2(inside1, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(inside1, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(-1, isect_point_tri_v2(inside2, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(inside2, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(-1, isect_point_tri_v2(inside3, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(inside3, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(-1, isect_point_tri_v2(inside4, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(inside4, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+
+  float2 outside1{2, 4};
+  float2 outside2{-1, -1};
+  float2 outside3{0, 3};
+  float2 outside4{-4, 0};
+  EXPECT_EQ(0, isect_point_tri_v2(outside1, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(0, isect_point_tri_v2(outside1, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(0, isect_point_tri_v2(outside2, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(0, isect_point_tri_v2(outside2, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(0, isect_point_tri_v2(outside3, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(0, isect_point_tri_v2(outside3, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(0, isect_point_tri_v2(outside4, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(0, isect_point_tri_v2(outside4, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+
+  float2 edge1{0, 2};
+  float2 edge2{1, -2};
+  EXPECT_EQ(-1, isect_point_tri_v2(edge1, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(edge1, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(-1, isect_point_tri_v2(edge2, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(edge2, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+
+  float2 corner1{4, 4};
+  float2 corner2{2, -3};
+  EXPECT_EQ(-1, isect_point_tri_v2(corner1, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(corner1, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+  EXPECT_EQ(-1, isect_point_tri_v2(corner2, tri_cw[0], tri_cw[1], tri_cw[2]));
+  EXPECT_EQ(+1, isect_point_tri_v2(corner2, tri_ccw[0], tri_ccw[1], tri_ccw[2]));
+}
+
+TEST(math_geom, IsectPointQuad2D)
+{
+  float2 quad_cw[4] = {{-2, 1}, {4, 4}, {5, 1}, {2, -3}};
+  float2 quad_ccw[4] = {{-2, 1}, {2, -3}, {5, 1}, {4, 4}};
+
+  float2 inside1{0, 0};
+  float2 inside2{2, 2};
+  float2 inside3{3, -1};
+  float2 inside4{-1, 1};
+  EXPECT_EQ(-1, isect_point_quad_v2(inside1, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(inside1, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(-1, isect_point_quad_v2(inside2, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(inside2, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(-1, isect_point_quad_v2(inside3, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(inside3, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(-1, isect_point_quad_v2(inside4, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(inside4, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+
+  float2 outside1{2, 4};
+  float2 outside2{-1, -1};
+  float2 outside3{0, 3};
+  float2 outside4{-4, 0};
+  EXPECT_EQ(0, isect_point_quad_v2(outside1, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(0, isect_point_quad_v2(outside1, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(0, isect_point_quad_v2(outside2, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(0, isect_point_quad_v2(outside2, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(0, isect_point_quad_v2(outside3, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(0, isect_point_quad_v2(outside3, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(0, isect_point_quad_v2(outside4, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(0, isect_point_quad_v2(outside4, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+
+  float2 edge1{0, 2};
+  float2 edge2{1, -2};
+  EXPECT_EQ(-1, isect_point_quad_v2(edge1, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(edge1, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(-1, isect_point_quad_v2(edge2, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(edge2, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+
+  float2 corner1{4, 4};
+  float2 corner2{2, -3};
+  EXPECT_EQ(-1, isect_point_quad_v2(corner1, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(corner1, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
+  EXPECT_EQ(-1, isect_point_quad_v2(corner2, quad_cw[0], quad_cw[1], quad_cw[2], quad_cw[3]));
+  EXPECT_EQ(+1, isect_point_quad_v2(corner2, quad_ccw[0], quad_ccw[1], quad_ccw[2], quad_ccw[3]));
 }
