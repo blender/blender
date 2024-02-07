fb0f06a2ac5fc7f35e560ae9995a402dabdb793e
Thomas Dinges <thomas@blender.org>
2024-02-07 12:53:08 +0100

    Revert "Release cycle: 4.1, Bcon3"
    This reverts commit 4db14268185eb9b496c12e8b532cc46410ce17c5.

--- build_files/build_environment/cmake/download.cmake
+++ build_files/build_environment/cmake/download.cmake
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 ## Update and uncomment this in the release branch
-set(BLENDER_VERSION 4.1)
+# set(BLENDER_VERSION 3.1)
 
 function(download_source dep)
   set(TARGET_FILE ${${dep}_FILE})
