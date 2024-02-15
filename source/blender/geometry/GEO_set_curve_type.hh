af9709089ee6f12278fa46af29f51f8ed0a96125
Hans Goudey <hans@blender.org>
2024-02-15 11:35:33 -0500

    Cleanup: Remove unnecessary set curve type optimization
    Now that attributes use implicit sharing, copying CurvesGeometry
    is relatively cheap, and the complexity of this function isn't worth
    the remaining optimization of avoiding user count updates.

--- source/blender/geometry/GEO_set_curve_type.hh
+++ source/blender/geometry/GEO_set_curve_type.hh
@@ -12,19 +12,6 @@
 namespace blender::geometry {
 
 /**
- * Try the conversion to the #dst_type-- avoiding the majority of the work done in
- * #convert_curves by modifying an existing object in place rather than creating a new one.
- *
- * \note This function is necessary because attributes do not have proper support for CoW.
- *
- * \param get_writable_curves_fn: Should return the write-able curves to change directly if
- * possible. This is a function in order to avoid the cost of retrieval when unnecessary.
- */
-bool try_curves_conversion_in_place(const IndexMask &selection,
-                                    CurveType dst_type,
-                                    FunctionRef<bke::CurvesGeometry &()> get_writable_curves_fn);
-
-/**
  * Change the types of the selected curves, potentially changing the total point count.
  */
 bke::CurvesGeometry convert_curves(const bke::CurvesGeometry &src_curves,
