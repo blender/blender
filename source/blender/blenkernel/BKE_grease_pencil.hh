d9a1e906f56353766a7dfc5dd9861376c1a6101b
Hans Goudey <hans@blender.org>
2024-02-16 17:49:42 +0100

    Cleanup: GPv3: Add specific header for legacy conversion functions
    Similar to `BKE_curve_legacy_convert.hh` and `BKE_mesh_legacy_convert.hh`
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/118390

--- source/blender/blenkernel/BKE_grease_pencil.hh
+++ source/blender/blenkernel/BKE_grease_pencil.hh
@@ -724,21 +724,6 @@ inline LayerGroup &Layer::parent_group() const
 
 TREENODE_COMMON_METHODS_FORWARD_IMPL(LayerGroup);
 
-namespace convert {
-
-void legacy_gpencil_frame_to_grease_pencil_drawing(const bGPDframe &gpf,
-                                                   const ListBase &vertex_group_names,
-                                                   GreasePencilDrawing &r_drawing);
-void legacy_gpencil_to_grease_pencil(Main &bmain, GreasePencil &grease_pencil, bGPdata &gpd);
-
-void legacy_gpencil_object(Main &bmain, Object &object);
-
-void thickness_factor_to_modifier(const bGPdata &src_object_data, Object &dst_object);
-void layer_adjustments_to_modifiers(Main &bmain,
-                                    const bGPdata &src_object_data,
-                                    Object &dst_object);
-
-}  // namespace convert
 }  // namespace greasepencil
 
 class GreasePencilRuntime {
--- source/blender/editors/object/object_add.cc
+++ source/blender/editors/object/object_add.cc
@@ -65,6 +65,7 @@
 #include "BKE_gpencil_legacy.h"
 #include "BKE_gpencil_modifier_legacy.h"
 #include "BKE_grease_pencil.hh"
+#include "BKE_grease_pencil_legacy_convert.hh"
 #include "BKE_key.hh"
 #include "BKE_lattice.hh"
 #include "BKE_layer.hh"
