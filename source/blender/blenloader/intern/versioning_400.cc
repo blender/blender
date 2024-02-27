96dc7a48efe302665e7c31da1c7f8b5d2805dd5b
Hans Goudey <hans@blender.org>
2024-02-27 16:35:25 -0500

    Cleanup: Use C++ style cast, use standard variable name

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -1939,10 +1939,10 @@ static bool seq_filter_bilinear_to_auto(Sequence *seq, void * /*user_data*/)
   return true;
 }
 
-static void image_settings_avi_to_ffmpeg(Scene *sce)
+static void image_settings_avi_to_ffmpeg(Scene *scene)
 {
-  if (ELEM(sce->r.im_format.imtype, R_IMF_IMTYPE_AVIRAW, R_IMF_IMTYPE_AVIJPEG)) {
-    sce->r.im_format.imtype = R_IMF_IMTYPE_FFMPEG;
+  if (ELEM(scene->r.im_format.imtype, R_IMF_IMTYPE_AVIRAW, R_IMF_IMTYPE_AVIJPEG)) {
+    scene->r.im_format.imtype = R_IMF_IMTYPE_FFMPEG;
   }
 }
 
@@ -2951,7 +2951,7 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
         LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
           LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
             if (sl->spacetype == SPACE_IMAGE) {
-              SpaceImage *sima = (SpaceImage *)sl;
+              SpaceImage *sima = reinterpret_cast<SpaceImage *>(sl);
               sima->stretch_opacity = 0.9f;
             }
           }
@@ -2961,8 +2961,8 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
   }
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 402, 5)) {
-    LISTBASE_FOREACH (Scene *, sce, &bmain->scenes) {
-      image_settings_avi_to_ffmpeg(sce);
+    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+      image_settings_avi_to_ffmpeg(scene);
     }
   }
 
