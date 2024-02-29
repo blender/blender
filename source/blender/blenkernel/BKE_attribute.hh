c9bddce79b625559e5a91bf15e3a4392b5cc1db6
Raul Fernandez <farsthary84@gmail.com>
2024-02-29 18:52:29 +0100

    Cleanup: Grammar in comment
    Comment's wording improvement
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/118913

--- source/blender/blenkernel/BKE_attribute.hh
+++ source/blender/blenkernel/BKE_attribute.hh
@@ -29,7 +29,7 @@ class GField;
 namespace blender::bke {
 
 enum class AttrDomain : int8_t {
-  /* Use for to choose automatically based on other data. */
+  /* Used to choose automatically based on other data. */
   Auto = -1,
   /* Mesh, Curve or Point Cloud Point. */
   Point = 0,
