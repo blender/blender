e72bf54791acff387527a9a4631f4fea0d025fac
Jacques Lucke <jacques@blender.org>
2024-03-02 23:59:48 +0100

    Fix #118926: missing transform relation when using Object/Collection Info node
    This was probably accidentally broken when we moved the rna code into node files.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/119017

--- source/blender/nodes/geometry/nodes/node_geo_collection_info.cc
+++ source/blender/nodes/geometry/nodes/node_geo_collection_info.cc
@@ -161,7 +161,7 @@ static void node_rna(StructRNA *srna)
       {0, nullptr, 0, nullptr, nullptr},
   };
 
-  RNA_def_node_enum(
+  PropertyRNA *prop = RNA_def_node_enum(
       srna,
       "transform_space",
       "Transform Space",
@@ -169,6 +169,7 @@ static void node_rna(StructRNA *srna)
       rna_node_geometry_collection_info_transform_space_items,
       NOD_storage_enum_accessors(transform_space),
       GEO_NODE_TRANSFORM_SPACE_ORIGINAL);
+  RNA_def_property_update_runtime(prop, rna_Node_update_relations);
 }
 
 static void node_register()
