f64f3e852416698d97c959f8ada10a5ccd44af6a
Campbell Barton <campbell@blender.org>
2024-03-21 11:35:29 +1100

    Extensions: recursively remove custom-directories on repo removal
    Even though there is a dialog users must accept when removing a
    repository & directory being removed is shown, it's possible users
    assume this only removes files which are part of the repository after
    pointing the custom-directory to their home directory or similar.
    
    Removing repositories which point to a custom-directory now only
    remove packages and server meta-data to prevent accidents.
    
    Resolves #119481.

--- source/blender/editors/space_userpref/userpref_ops.cc
+++ source/blender/editors/space_userpref/userpref_ops.cc
@@ -578,10 +578,25 @@ static int preferences_extension_repo_remove_exec(bContext *C, wmOperator *op)
     char dirpath[FILE_MAX];
     BKE_preferences_extension_repo_dirpath_get(repo, dirpath, sizeof(dirpath));
     if (dirpath[0] && BLI_is_dir(dirpath)) {
-      if (BLI_delete(dirpath, true, true) != 0) {
+
+      /* Removing custom directories has the potential to remove user data
+       * if users accidently point this to their home directory or similar.
+       * Even though the UI shows a warning, we better prevent any accidents
+       * caused by recursive removal, see #119481.
+       * Only check custom directories because the non-custom directory is always
+       * a spesific location under Blender's local extensions directory. */
+      const bool recursive = (repo->flag & USER_EXTENSION_REPO_FLAG_USE_CUSTOM_DIRECTORY) == 0;
+
+      /* Perform package manager spesific clear operations,
+       * needed when `recursive` is false so the empty directory can be removed.
+       * If it's not empty there will be a warning that the directory couldn't be removed.
+       * The user will have to do this manually which is good since unknown files may user data. */
+      BKE_callback_exec_string(bmain, BKE_CB_EVT_EXTENSION_REPOS_FILES_CLEAR, dirpath);
+
+      if (BLI_delete(dirpath, true, recursive) != 0) {
         BKE_reportf(op->reports,
-                    RPT_ERROR,
-                    "Error removing directory: %s",
+                    RPT_WARNING,
+                    "Unable to remove directory: %s",
                     errno ? strerror(errno) : "unknown");
       }
     }
