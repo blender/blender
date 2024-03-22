62f140e048ad06d89d3bf89bb17783ca813a86a6
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-03-22 13:22:35 +0100

    Anim: Add Sharpness to Ease operator
    This PR extends the existing `Ease` operator with a sharpness property.
    
    This means it has two parameters:
    * Curve Bend: Define which key to favor. At 0 it's exactly between the two keys.
    * Sharpness: Higher values make the change more abrupt.
    
    During modal operation it is possible to switch the slider between those two by pressing `TAB`.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/117287

--- source/blender/editors/include/ED_keyframes_edit.hh
+++ source/blender/editors/include/ED_keyframes_edit.hh
@@ -470,7 +470,10 @@ void smooth_fcurve_segment(FCurve *fcu,
                            float factor,
                            int kernel_size,
                            double *kernel);
-void ease_fcurve_segment(FCurve *fcu, FCurveSegment *segment, float factor);
+/** Snap the keys on the given FCurve segment to an S-Curve. By modifying the `factor` the part of
+ * the S-Curve that the keys are snapped to is moved on the x-axis.*/
+void ease_fcurve_segment(FCurve *fcu, FCurveSegment *segment, float factor, float width);
+
 enum tShearDirection {
   SHEAR_FROM_LEFT = 1,
   SHEAR_FROM_RIGHT,
