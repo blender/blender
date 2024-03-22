303014bfac01309eb3b533110e7483bd02bdc139
Julian Eisel <julian@blender.org>
2024-03-22 12:25:39 +0100

    Fix #108078: Crash when inverting results in pose library sidebar
    a958ae36e8 introduced support for UI lists to reference items that would
    never be shown, regardless of filter settings. This was to skip assets
    in the asset view template that were not of the requested type. UI list
    sorting code wasn't updated to account for such items that should be
    entirely ignored.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/109157

--- doc/python_api/examples/bpy.types.UIList.2.py
+++ doc/python_api/examples/bpy.types.UIList.2.py
@@ -136,8 +136,9 @@ class MESH_UL_vgroups_slow(bpy.types.UIList):
     def filter_items(self, context, data, propname):
         # This function gets the collection property (as the usual tuple (data, propname)), and must return two lists:
         # * The first one is for filtering, it must contain 32bit integers were self.bitflag_filter_item marks the
-        #   matching item as filtered (i.e. to be shown), and 31 other bits are free for custom needs. Here we use the
-        #   first one to mark VGROUP_EMPTY.
+        #   matching item as filtered (i.e. to be shown). The upper 16 bits (including self.bitflag_filter_item) are
+        #   reserved for internal use, the lower 16 bits are free for custom use. Here we use the first bit to mark
+        #   VGROUP_EMPTY.
         # * The second one is for reordering, it must return a list containing the new indices of the items (which
         #   gives us a mapping org_idx -> new_idx).
         # Please note that the default UI_UL_list defines helper functions for common tasks (see its doc for more info).
--- source/blender/makesrna/intern/rna_ui.cc
+++ source/blender/makesrna/intern/rna_ui.cc
@@ -625,7 +625,10 @@ static void uilist_filter_items(uiList *ui_list,
         int t_idx, t_ni, prev_ni;
         flt_data->items_shown = 0;
         for (i = 0, shown_idx = 0; i < len; i++) {
-          if ((filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude) {
+          if (filter_flags[i] & UILST_FLT_ITEM_NEVER_SHOW) {
+            BLI_assert_msg(false, "Bit reserved for internal use");
+          }
+          else if ((filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude) {
             filter_neworder[shown_idx++] = filter_neworder[i];
           }
         }
@@ -653,7 +656,10 @@ static void uilist_filter_items(uiList *ui_list,
         /* we still have to set flt_data->items_shown... */
         flt_data->items_shown = 0;
         for (i = 0; i < len; i++) {
-          if ((filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude) {
+          if (filter_flags[i] & UILST_FLT_ITEM_NEVER_SHOW) {
+            /* Pass. */
+          }
+          else if ((filter_flags[i] & UILST_FLT_ITEM) ^ filter_exclude) {
             flt_data->items_shown++;
           }
         }
@@ -2073,11 +2079,11 @@ static void rna_def_uilist(BlenderRNA *brna)
   prop = RNA_def_property(func, "filter_flags", PROP_INT, PROP_UNSIGNED);
   RNA_def_property_flag(prop, PropertyFlag(PARM_REQUIRED | PROP_DYNAMIC));
   RNA_def_property_array(prop, 1); /* XXX Dummy value, default 0 does not work */
-  RNA_def_property_ui_text(
-      prop,
-      "",
-      "An array of filter flags, one for each item in the collection (NOTE: "
-      "FILTER_ITEM bit is reserved, it defines whether the item is shown or not)");
+  RNA_def_property_ui_text(prop,
+                           "",
+                           "An array of filter flags, one for each item in the collection (NOTE: "
+                           "The upper 16 bits, including FILTER_ITEM, are reserved, only use the "
+                           "lower 16 bits for custom usages).");
   RNA_def_function_output(func, prop);
   prop = RNA_def_property(func, "filter_neworder", PROP_INT, PROP_UNSIGNED);
   RNA_def_property_flag(prop, PropertyFlag(PARM_REQUIRED | PROP_DYNAMIC));
