2d9f19c9624ea476f3ab28397cda5faf75a0a046
Hans Goudey <hans@blender.org>
2024-03-27 22:25:35 -0400

    Cleanup: Use std::move for asset identifier construction
    Pass strings by value and move their result. This gives the caller
    the potential to move existing strings into the class. Moving the
    std::shared_ptr should just avoid reference counting here.

--- source/blender/asset_system/intern/asset_identifier.cc
+++ source/blender/asset_system/intern/asset_identifier.cc
@@ -19,8 +19,9 @@
 namespace blender::asset_system {
 
 AssetIdentifier::AssetIdentifier(std::shared_ptr<std::string> library_root_path,
-                                 const std::string &relative_asset_path)
-    : library_root_path_(library_root_path), relative_asset_path_(relative_asset_path)
+                                 std::string relative_asset_path)
+    : library_root_path_(std::move(library_root_path)),
+      relative_asset_path_(std::move(relative_asset_path))
 {
 }
 
