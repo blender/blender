8ef57654e133acc6892f7defb58c13825d6f0aa1
Thomas Dinges <thomas@blender.org>
2024-03-28 14:40:03 +0100

    Release: Bump to 4.1.1 RC

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -23,9 +23,9 @@ extern "C" {
 /** Blender major and minor version. */
 #define BLENDER_VERSION 401
 /** Blender patch version for bug-fix releases. */
-#define BLENDER_VERSION_PATCH 0
+#define BLENDER_VERSION_PATCH 1
 /** Blender release cycle stage: alpha/beta/rc/release. */
-#define BLENDER_VERSION_CYCLE release
+#define BLENDER_VERSION_CYCLE rc
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
