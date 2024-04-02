03bcd7390a31710d834d495c34c395d884e42b82
Falk David <falk@blender.org>
2024-04-02 16:46:27 +0200

    Formatting

--- tools/triage/issues_module_listing.py
+++ tools/triage/issues_module_listing.py
@@ -16,27 +16,29 @@ import dataclasses
 from datetime import date
 from gitea_utils import gitea_json_issues_search
 
+
 @dataclasses.dataclass
 class ModuleInfo:
     name: str
     labelid: str
     buglist: list[str] = dataclasses.field(default_factory=list)
 
+
 # Label names and IDs are taken from https://projects.blender.org/blender/blender/labels.
 modules = {
-    "Module/Animation & Rigging" : ModuleInfo(name="Animation & Rigging", labelid="268"),
-    "Module/Core" : ModuleInfo(name="Core", labelid="269"),
-    "Module/EEVEE & Viewport" : ModuleInfo(name="EEVEE & Viewport", labelid="272"),
-    "Module/Grease Pencil" : ModuleInfo(name="Grease Pencil", labelid="273"),
-    "Module/Modeling" : ModuleInfo(name="Modeling", labelid="274"),
-    "Module/Nodes & Physics" : ModuleInfo(name="Nodes & Physics", labelid="275"),
-    "Module/Pipeline, Assets & IO" : ModuleInfo(name="Pipeline, Assets & I/O", labelid="276"),
-    "Module/Platforms, Builds, Test & Devices" : ModuleInfo(name="Platforms, Builds, Test & Devices", labelid="278"),
-    "Module/Python API" : ModuleInfo(name="Python API", labelid="279"),
-    "Module/Render & Cycles" : ModuleInfo(name="Render & Cycles", labelid="280"),
-    "Module/Sculpt, Paint & Texture" : ModuleInfo(name="Sculpt, Paint & Texture", labelid="281"),
-    "Module/User Interface" : ModuleInfo(name="User Interface", labelid="283"),
-    "Module/VFX & Video" : ModuleInfo(name="VFX & Video", labelid="284"),
+    "Module/Animation & Rigging": ModuleInfo(name="Animation & Rigging", labelid="268"),
+    "Module/Core": ModuleInfo(name="Core", labelid="269"),
+    "Module/EEVEE & Viewport": ModuleInfo(name="EEVEE & Viewport", labelid="272"),
+    "Module/Grease Pencil": ModuleInfo(name="Grease Pencil", labelid="273"),
+    "Module/Modeling": ModuleInfo(name="Modeling", labelid="274"),
+    "Module/Nodes & Physics": ModuleInfo(name="Nodes & Physics", labelid="275"),
+    "Module/Pipeline, Assets & IO": ModuleInfo(name="Pipeline, Assets & I/O", labelid="276"),
+    "Module/Platforms, Builds, Test & Devices": ModuleInfo(name="Platforms, Builds, Test & Devices", labelid="278"),
+    "Module/Python API": ModuleInfo(name="Python API", labelid="279"),
+    "Module/Render & Cycles": ModuleInfo(name="Render & Cycles", labelid="280"),
+    "Module/Sculpt, Paint & Texture": ModuleInfo(name="Sculpt, Paint & Texture", labelid="281"),
+    "Module/User Interface": ModuleInfo(name="User Interface", labelid="283"),
+    "Module/VFX & Video": ModuleInfo(name="VFX & Video", labelid="284"),
 }
 
 base_url = "https://projects.blender.org/blender/blender/issues?q=&type=all&sort=&state=open&labels="
@@ -118,7 +120,7 @@ def main() -> None:
     )
 
     args = parser.parse_args()
-        
+
     compile_list(args.severity)
 
 
