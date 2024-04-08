76c7d1c66b3b6ea463eac8cf70916999b314cc8c
Campbell Barton <campbell@blender.org>
2024-04-08 12:03:00 +1000

    Fix assert in the case of a null library_reference items
    Missing return allowed a null return (which asserts) & ignored the
    r_free assignment.

--- source/blender/makesrna/intern/rna_asset.cc
+++ source/blender/makesrna/intern/rna_asset.cc
@@ -463,8 +463,8 @@ const EnumPropertyItem *rna_asset_library_reference_itemf(bContext * /*C*/,
   const EnumPropertyItem *items = blender::ed::asset::library_reference_to_rna_enum_itemf(true);
   if (!items) {
     *r_free = false;
+    return rna_enum_dummy_NULL_items;
   }
-
   *r_free = true;
   return items;
 }
