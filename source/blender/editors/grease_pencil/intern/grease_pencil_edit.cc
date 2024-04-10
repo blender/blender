d049cd1dbf4fba9fe6664bea85490e96a86415b7
Pratik Borhade <pratikborhade302@gmail.com>
2024-04-10 11:15:47 +0200

    Fix: GPv3: Clear copy strokes buffer if nothing is selected during copy
    If `clipboard.curves` has some curve and the copy operation is called
    again when nothing is selected, the buffer is expected to be cleared.
    Otherwise the paste operations will paste the previous copied curves.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/120454

--- source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -2354,6 +2354,7 @@ static int grease_pencil_copy_strokes_exec(bContext *C, wmOperator *op)
   }
 
   if (!anything_copied) {
+    clipboard.curves.resize(0, 0);
     return OPERATOR_CANCELLED;
   }
 
