8b9743eb407fb1a5c09041b9ca39cf59cc3ce6b3
Ray Molenkamp <github@lazydodo.com>
2024-04-11 20:46:14 +0200

    CMake: Change build flags to use SSE42 if available
    This cleans up our `TEST_SSE_SUPPORT` macro to only test
    for SSE42 and passes the flags to the CMAKE_C/CXX_FLAGS
    
    the cpu check module needed to move to its own folder since
    the flags at the end of a CMakeLists.txt appear to be used
    for all targets inside a CMakeLists file and cpu_check cannot
    be build with sse42 flags.
    
    This only affects Mac/Linux since MSVC has no buildflags
    to target SSE42
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/118468

--- build_files/cmake/macros.cmake
+++ build_files/cmake/macros.cmake
@@ -540,49 +540,37 @@ function(setup_platform_linker_libs
 endfunction()
 
 macro(TEST_SSE_SUPPORT
-  _sse_flags
-  _sse2_flags)
+  _sse42_flags)
 
   include(CheckCSourceRuns)
 
   # message(STATUS "Detecting SSE support")
   if(CMAKE_COMPILER_IS_GNUCC OR (CMAKE_C_COMPILER_ID MATCHES "Clang"))
-    set(${_sse_flags} "-msse")
-    set(${_sse2_flags} "-msse2")
+    set(${_sse42_flags} "-march=x86-64-v2")
   elseif(MSVC)
-    # x86_64 has this auto enabled
-    if("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
-      set(${_sse_flags} "")
-      set(${_sse2_flags} "")
-    else()
-      set(${_sse_flags} "/arch:SSE")
-      set(${_sse2_flags} "/arch:SSE2")
-    endif()
+    # msvc has no specific build flags for SSE42, but when using intrinsics it will
+    # generate the right instructions.
+    set(${_sse42_flags} "")
   elseif(CMAKE_C_COMPILER_ID STREQUAL "Intel")
-    set(${_sse_flags} "")  # icc defaults to -msse
-    set(${_sse2_flags} "")  # icc defaults to -msse2
+    if(WIN32)
+      set(${_sse42_flags} "/QxSSE4.2")
+    else()
+      set(${_sse42_flags} "-xsse4.2")
+    endif()
   else()
     message(WARNING "SSE flags for this compiler: '${CMAKE_C_COMPILER_ID}' not known")
-    set(${_sse_flags})
-    set(${_sse2_flags})
+    set(${_sse42_flags})
   endif()
 
-  set(CMAKE_REQUIRED_FLAGS "${${_sse_flags}} ${${_sse2_flags}}")
+  set(CMAKE_REQUIRED_FLAGS "${${_sse42_flags}}")
 
-  if(NOT DEFINED SUPPORT_SSE_BUILD)
+  if(NOT DEFINED SUPPORT_SSE42_BUILD)
     # result cached
     check_c_source_runs("
-      #include <xmmintrin.h>
-      int main(void) { __m128 v = _mm_setzero_ps(); return 0; }"
-    SUPPORT_SSE_BUILD)
-  endif()
-
-  if(NOT DEFINED SUPPORT_SSE2_BUILD)
-    # result cached
-    check_c_source_runs("
+      #include <nmmintrin.h>
       #include <emmintrin.h>
-      int main(void) { __m128d v = _mm_setzero_pd(); return 0; }"
-    SUPPORT_SSE2_BUILD)
+      int main(void) { __m128i v = _mm_setzero_si128(); v = _mm_cmpgt_epi64(v,v); return 0; }"
+    SUPPORT_SSE42_BUILD)
   endif()
 
   unset(CMAKE_REQUIRED_FLAGS)
