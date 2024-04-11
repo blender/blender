486947398f27d8ab44b5fdb56404a8ebf1df0f4b
Jacques Lucke <jacques@blender.org>
2024-04-11 18:28:57 +0200

    Geometry Nodes: add invertable boolean output to invert matrix node
    Not all matrices are invertable (e.g. when a transformation matrix is has
    a scale of zero), and it's sometimes important when a matrix is.
    
    This also better specifies the behavior when the matrix is not invertable:
    the identity matrix is returned.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/120530

--- source/blender/nodes/function/nodes/node_fn_invert_matrix.cc
+++ source/blender/nodes/function/nodes/node_fn_invert_matrix.cc
@@ -14,7 +14,9 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.is_function_node();
   b.add_input<decl::Matrix>("Matrix");
-  b.add_output<decl::Matrix>("Matrix");
+  b.add_output<decl::Matrix>("Matrix").description(
+      "The inverted matrix or the identity matrix if the input is not invertable");
+  b.add_output<decl::Bool>("Invertable").description("True if the input matrix is invertable");
 }
 
 static void search_link_ops(GatherLinkSearchOpParams &params)
@@ -24,10 +26,45 @@ static void search_link_ops(GatherLinkSearchOpParams &params)
   }
 }
 
+class InvertMatrixFunction : public mf::MultiFunction {
+ public:
+  InvertMatrixFunction()
+  {
+    static mf::Signature signature = []() {
+      mf::Signature signature;
+      mf::SignatureBuilder builder{"Invert Matrix", signature};
+      builder.single_input<float4x4>("Matrix");
+      builder.single_output<float4x4>("Matrix", mf::ParamFlag::SupportsUnusedOutput);
+      builder.single_output<bool>("Invertable", mf::ParamFlag::SupportsUnusedOutput);
+      return signature;
+    }();
+    this->set_signature(&signature);
+  }
+
+  void call(const IndexMask &mask, mf::Params params, mf::Context /*context*/) const override
+  {
+    const VArraySpan<float4x4> in_matrices = params.readonly_single_input<float4x4>(0, "Matrix");
+    MutableSpan<float4x4> out_matrices = params.uninitialized_single_output_if_required<float4x4>(
+        1, "Matrix");
+    MutableSpan<bool> out_invertable = params.uninitialized_single_output_if_required<bool>(
+        2, "Invertable");
+    mask.foreach_index([&](const int64_t i) {
+      const float4x4 &matrix = in_matrices[i];
+      bool success;
+      float4x4 inverted_matrix = math::invert(matrix, success);
+      if (!out_matrices.is_empty()) {
+        out_matrices[i] = success ? inverted_matrix : float4x4::identity();
+      }
+      if (!out_invertable.is_empty()) {
+        out_invertable[i] = success;
+      }
+    });
+  }
+};
+
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
 {
-  static auto fn = mf::build::SI1_SO<float4x4, float4x4>(
-      "Invert Matrix", [](float4x4 matrix) { return math::invert(matrix); });
+  static InvertMatrixFunction fn;
   builder.set_matching_fn(fn);
 }
 
