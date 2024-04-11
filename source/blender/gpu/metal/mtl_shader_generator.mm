5f86faf3a59756cc34117eadb493af2f4cf174c5
Jason Fielder <jason-fielder@noreply.localhost>
2024-04-11 18:17:40 +0200

    Metal: Fix write-only qualifier on shader generation
    A small logic issue caused all write-only image resources
    to be tagged as read-write in all cases. This caused
    correctness issues on Intel and AMD GPUs which
    are resolved through this change.
    
    Change also yields a small performance uplift
    due to enabling improved non-dependent workload
    scheduling.
    
    Authored by Apple: Michael Parkin-White
    
    Co-authored-by: Michael Parkin-White <mparkinwhite@apple.com>
    Pull Request: https://projects.blender.org/blender/blender/pulls/120528

--- source/blender/gpu/metal/mtl_shader_generator.mm
+++ source/blender/gpu/metal/mtl_shader_generator.mm
@@ -1902,7 +1902,7 @@ void MSLGeneratorInterface::prepare_from_createinfo(const shader::ShaderCreateIn
         case shader::ShaderCreateInfo::Resource::BindType::IMAGE: {
           /* Flatten qualifier flags into final access state. */
           MSLTextureSamplerAccess access;
-          if (bool(res.image.qualifiers & Qualifier::READ_WRITE)) {
+          if ((res.image.qualifiers & Qualifier::READ_WRITE) == Qualifier::READ_WRITE) {
             access = MSLTextureSamplerAccess::TEXTURE_ACCESS_READWRITE;
           }
           else if (bool(res.image.qualifiers & Qualifier::WRITE)) {
