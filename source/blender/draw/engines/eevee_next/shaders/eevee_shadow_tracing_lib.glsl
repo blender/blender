a4e922431734960ab0fcd3e3474d82d24e78ed03
Clément Foucault <foucault.clem@gmail.com>
2024-04-13 10:50:33 +0200

    Fix: EEVEE-Next: Shadow broken for transmission BSDFs
    Was not inverting the normal for `shadow_ray_above_horizon_ensure`.

--- source/blender/draw/engines/eevee_next/shaders/eevee_shadow_tracing_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_shadow_tracing_lib.glsl
@@ -547,6 +547,7 @@ ShadowEvalResult shadow_eval(LightData light,
   vec3 lP = is_directional ? light_world_to_local(light, P) :
                              light_world_to_local(light, P - light._position);
   vec3 lNg = light_world_to_local(light, Ng);
+  lNg = is_transmission ? -Ng : Ng;
 
   float surface_hit = 0.0;
   for (int ray_index = 0; ray_index < ray_count && ray_index < SHADOW_MAX_RAY; ray_index++) {
