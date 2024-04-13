fdf5a05bdebcc6c35d418d8513b5e3ea38718ace
Clément Foucault <foucault.clem@gmail.com>
2024-04-13 12:57:50 +0200

    Fix: EEVEE-Next: Broken shader compilation because of displacement
    Oversight inside f371624b2ea390bbb8027a4b73b83f3601c5ceea

--- source/blender/gpu/shaders/material/gpu_shader_material_output_material.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_output_material.glsl
@@ -2,6 +2,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#pragma BLENDER_REQUIRE(gpu_shader_material_transform_utils.glsl)
+
 void node_output_material_surface(Closure surface, out Closure out_surface)
 {
   out_surface = surface;
