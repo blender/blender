1079f0918e5f02e5e9613d8e4f0708346e7a5c47
dupoxy <dupoxy@noreply.localhost>
2024-04-14 11:51:28 +0200

    Cleanup: use function style cast in MAKE_ID macro
    Ref: !120630

--- intern/guardedalloc/intern/mallocn_guarded_impl.cc
+++ intern/guardedalloc/intern/mallocn_guarded_impl.cc
@@ -157,7 +157,7 @@ static const char *check_memlist(MemHead *memh);
 /* --------------------------------------------------------------------- */
 
 #ifdef __BIG_ENDIAN__
-#  define MAKE_ID(a, b, c, d) ((int)(a) << 24 | (int)(b) << 16 | (c) << 8 | (d))
+#  define MAKE_ID(a, b, c, d) (int(a) << 24 | int(b) << 16 | (c) << 8 | (d))
 #else
 #  define MAKE_ID(a, b, c, d) (int(d) << 24 | int(c) << 16 | (b) << 8 | (a))
 #endif
