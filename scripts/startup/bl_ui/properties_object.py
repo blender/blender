0791d7c34952674743d316a95041fdf98c796211
Clément Foucault <foucault.clem@gmail.com>
2024-04-18 22:01:18 +0200

    EEVEE-Next: Light: Add ray visbility support
    This is very straightforward.
    
    We don't do any versionning as we assume that
    if a scene was using these, it was not using eevee
    as default renderer.

--- scripts/startup/bl_ui/properties_object.py
+++ scripts/startup/bl_ui/properties_object.py
@@ -395,6 +395,14 @@ class OBJECT_PT_visibility(ObjectButtonsPanel, Panel):
                 col = layout.column(heading="Ray Visibility")
                 col.prop(ob, "visible_shadow", text="Shadow", toggle=False)
 
+            if ob.type in {'LIGHT'}:
+                layout.separator()
+                col = layout.column(heading="Ray Visibility")
+                col.prop(ob, "visible_diffuse", text="Diffuse", toggle=False)
+                col.prop(ob, "visible_glossy", text="Glossy", toggle=False)
+                col.prop(ob, "visible_transmission", text="Transmission", toggle=False)
+                col.prop(ob, "visible_volume_scatter", text="Volume Scatter", toggle=False)
+
             if ob.type in {'MESH', 'CURVE', 'SURFACE', 'META', 'FONT', 'CURVES', 'POINTCLOUD', 'VOLUME'}:
                 layout.separator()
                 col = layout.column(heading="Light Probes")
