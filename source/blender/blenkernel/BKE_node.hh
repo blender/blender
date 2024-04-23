0c3763ddda5625ddbb2e50cc59d7d4c2d0ac17de
Hans Goudey <hans@blender.org>
2024-04-23 14:48:59 +0200

    Geometry Nodes: Add SDF grid boolean nodes
    Add a simple node to compute the intersection, difference, or union
    between SDF grids. This should be the first new use case for the
    new volume grid nodes that wasn't possible before.
    
    For naming and multi-inputs, the node uses the same design as the
    mesh boolean node. We considered splitting each operation into a
    separate node, but though most users considered these different
    "modes" of the same operation.
    
    One thing to keep in mind is that it's important for the grids to
    have exactly the same transform. If they have different transforms,
    the second grid must be resampled to match the first, because the
    OpenVDB CSG tools have that requirement. Resampling is expensive
    (for SDF grids it means a grid -> mesh -> grid round trip) and should
    be avoided.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/118879

--- source/blender/blenkernel/BKE_node.hh
+++ source/blender/blenkernel/BKE_node.hh
@@ -1277,6 +1277,7 @@ void BKE_nodetree_remove_layer_n(bNodeTree *ntree, Scene *scene, int layer_index
 #define GEO_NODE_POINTS_TO_SDF_GRID 2128
 #define GEO_NODE_GRID_TO_MESH 2129
 #define GEO_NODE_DISTRIBUTE_POINTS_IN_GRID 2130
+#define GEO_NODE_SDF_GRID_BOOLEAN 2131
 
 /** \} */
 
