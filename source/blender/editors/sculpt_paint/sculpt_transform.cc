7f7e276e4d8fd2e4a4c677300126a4afbfb5f429
Campbell Barton <campbell@blender.org>
2024-04-24 14:03:52 +1000

    Fix sculpt "Set Pivot Position" activating immediately from the menu

--- source/blender/editors/sculpt_paint/sculpt_transform.cc
+++ source/blender/editors/sculpt_paint/sculpt_transform.cc
@@ -492,7 +492,7 @@ void SCULPT_OT_set_pivot_position(wmOperatorType *ot)
   ot->exec = sculpt_set_pivot_position_exec;
   ot->poll = SCULPT_mode_poll;
 
-  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO | OPTYPE_DEPENDS_ON_CURSOR;
   RNA_def_enum(ot->srna,
                "mode",
                prop_sculpt_pivot_position_types,
