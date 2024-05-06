5ed14b64d24d5d7433aa993590f02ef1162aef0b
Brecht Van Lommel <brecht@blender.org>
2024-05-06 11:22:14 +0200

    Fix #121416: Blender 3.3 source package includes precompiled libraries

--- build_files/utils/make_source_archive.py
+++ build_files/utils/make_source_archive.py
@@ -175,9 +175,7 @@ def submodules_to_manifest(
     skip_addon_contrib = version.is_release
     assert not blender_srcdir.is_absolute()
 
-    for line in git_command("-C", blender_srcdir, "submodule"):
-        submodule = line.split()[1]
-
+    for submodule in ("release/datafiles/locale", "release/scripts/addons", "release/scripts/addons_contrib", "source/tools"):
         # Don't use native slashes as GIT for MS-Windows outputs forward slashes.
         if skip_addon_contrib and submodule == "release/scripts/addons_contrib":
             continue
