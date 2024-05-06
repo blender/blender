3aa00e14e41b4a8c8bbc54727f20470cbd307baa
Brecht Van Lommel <brecht@blender.org>
2024-05-06 17:06:38 +0200

    Build: Use CMake for sndfile deps building on all platforms
    Resolves issues finding opus, and makes Linux and macOS match
    Windows more closely.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/121401

--- build_files/build_environment/cmake/sndfile.cmake
+++ build_files/build_environment/cmake/sndfile.cmake
@@ -2,72 +2,53 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-set(SNDFILE_EXTRA_ARGS)
-set(SNDFILE_ENV)
+set(SNDFILE_EXTRA_ARGS
+  -DCMAKE_POLICY_DEFAULT_CMP0074=NEW
+  -DOgg_ROOT=${LIBDIR}/ogg
+  -DVorbis_ROOT=${LIBDIR}/vorbis
+  -DOpus_ROOT=${LIBDIR}/opus
+  -DFLAC_ROOT=${LIBDIR}/flac
+  # Use two roots because of apparent mistake in sndfile.
+  -DLAME_ROOT=${LIBDIR}/lame/include
+  -DMP3LAME_ROOT=${LIBDIR}/lame/lib
+  -DCMAKE_DISABLE_FIND_PACKAGE_ALSA=ON
+  -DCMAKE_DISABLE_FIND_PACKAGE_mpg123=ON
+  -DCMAKE_DISABLE_FIND_PACKAGE_Speex=ON
+  -DCMAKE_DISABLE_FIND_PACKAGE_SQLite3=ON
+  -DBUILD_PROGRAMS=OFF
+  -DBUILD_EXAMPLES=OFF
+  -DBUILD_TESTING=OFF
+  -DPYTHON_EXECUTABLE=${PYTHON_BINARY}
+)
 
-if(NOT WIN32)
-  set(SNDFILE_OPTIONS --enable-static --disable-shared )
-endif()
-
-if(NOT WIN32)
-  ExternalProject_Add(external_sndfile
-    URL file://${PACKAGE_DIR}/${SNDFILE_FILE}
-    DOWNLOAD_DIR ${DOWNLOAD_DIR}
-    URL_HASH ${SNDFILE_HASH_TYPE}=${SNDFILE_HASH}
-    PREFIX ${BUILD_DIR}/sndfile
-
-    CONFIGURE_COMMAND ${CONFIGURE_ENV} &&
-      cd ${BUILD_DIR}/sndfile/src/external_sndfile/ &&
-      ${SNDFILE_ENV} ${CONFIGURE_COMMAND} ${SNDFILE_OPTIONS} --prefix=${mingw_LIBDIR}/sndfile
-
-    BUILD_COMMAND ${CONFIGURE_ENV} &&
-      cd ${BUILD_DIR}/sndfile/src/external_sndfile/ &&
-      make -j${MAKE_THREADS}
-
-    INSTALL_COMMAND ${CONFIGURE_ENV} &&
-      cd ${BUILD_DIR}/sndfile/src/external_sndfile/ &&
-      make install
-
-    INSTALL_DIR ${LIBDIR}/sndfile
-  )
-else()
+if(WIN32)
   # We set FLAC__NO_DLL, otherwise we cannot statically link FLAC
   set(SNDFILE_C_FLAGS "${CMAKE_C_FLAGS} -DFLAC__NO_DLL=1")
-
   set(SNDFILE_EXTRA_ARGS
-    -DCMAKE_POLICY_DEFAULT_CMP0074=NEW
-    -DOgg_ROOT=${LIBDIR}/ogg
-    -DVorbis_ROOT=${LIBDIR}/vorbis
-    -DOpus_ROOT=${LIBDIR}/opus
-    -DLame_ROOT=${LIBDIR}/lame
-    -DLAME_INCLUDE_DIR=${LIBDIR}/lame/include/
-    -DFLAC_ROOT=${LIBDIR}/flac
-    -DBUILD_PROGRAMS=OFF
-    -DBUILD_EXAMPLES=OFF
-    -DBUILD_TESTING=OFF
+    ${SNDFILE_EXTRA_ARGS}
     -DBUILD_SHARED_LIBS=ON
-    -DPYTHON_EXECUTABLE=${PYTHON_BINARY}
     -DCMAKE_C_FLAGS=${SNDFILE_C_FLAGS}
   )
-
-  ExternalProject_Add(external_sndfile
-    URL file://${PACKAGE_DIR}/${SNDFILE_FILE}
-    DOWNLOAD_DIR ${DOWNLOAD_DIR}
-    URL_HASH ${SNDFILE_HASH_TYPE}=${SNDFILE_HASH}
-    PREFIX ${BUILD_DIR}/sndfile
-
-    PATCH_COMMAND ${CMAKE_COMMAND} -E copy
-      ${PATCH_DIR}/cmake/modules/FindLame.cmake
-      ${BUILD_DIR}/sndfile/src/external_sndfile/cmake/FindLame.cmake
-
-    CMAKE_ARGS
-      -DCMAKE_INSTALL_PREFIX=${LIBDIR}/sndfile
-      ${DEFAULT_CMAKE_FLAGS}
-      ${SNDFILE_EXTRA_ARGS}
-
-    INSTALL_DIR ${LIBDIR}/sndfile
+else()
+  set(SNDFILE_EXTRA_ARGS
+    ${SNDFILE_EXTRA_ARGS}
+    -DBUILD_SHARED_LIBS=OFF
   )
 endif()
+
+ExternalProject_Add(external_sndfile
+  URL file://${PACKAGE_DIR}/${SNDFILE_FILE}
+  DOWNLOAD_DIR ${DOWNLOAD_DIR}
+  URL_HASH ${SNDFILE_HASH_TYPE}=${SNDFILE_HASH}
+  PREFIX ${BUILD_DIR}/sndfile
+
+  CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX=${LIBDIR}/sndfile
+    ${DEFAULT_CMAKE_FLAGS}
+    ${SNDFILE_EXTRA_ARGS}
+
+  INSTALL_DIR ${LIBDIR}/sndfile
+)
 
 if(BUILD_MODE STREQUAL Release AND WIN32)
   ExternalProject_Add_Step(external_sndfile after_install
@@ -91,4 +72,5 @@ add_dependencies(
   external_vorbis
   external_opus
   external_flac
+  external_lame
 )
