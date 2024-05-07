238ee9683a4403fd0def114ef9e696017df9e190
Philipp Oeser <philipp@blender.org>
2024-05-07 15:19:05 +0200

    Fix: memleak in Graph Editor Keyframe jump operator
    In case no frame to jump to was found, `ANIM_animdata_freelist` wasnt
    called.
    
    Spotted while looking into #121463
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/121484

--- source/blender/editors/space_graph/graph_edit.cc
+++ source/blender/editors/space_graph/graph_edit.cc
@@ -2302,14 +2302,14 @@ static int keyframe_jump_exec(bContext *C, wmOperator *op)
     }
   }
 
+  ANIM_animdata_freelist(&anim_data);
+
   if (!found) {
     BKE_report(op->reports, RPT_INFO, "No more keyframes to jump to in this direction");
     return OPERATOR_CANCELLED;
   }
 
   BKE_scene_frame_set(scene, closest_frame);
-
-  ANIM_animdata_freelist(&anim_data);
 
   /* Set notifier that things have changed. */
   WM_event_add_notifier(C, NC_SCENE | ND_FRAME, ac.scene);
