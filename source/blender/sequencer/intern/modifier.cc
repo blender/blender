897664ccc555f40772d7b58bf2de830ab9a9a4cd
Richard Antalik <richardantalik@gmail.com>
2024-05-07 19:37:24 +0200

    Fix #121319: Mask modifier not clamped to mask strip
    Skip modifier, when using strip as a mask that ends before specified
    timeline frame. To make behavior of missing mask more consistent,
    modifier is also skipped, when strip produces blank output due to
    missing data.
    
    Skipping modifier due strip boundary only works, when "Mask Time" is set
    to Relative mode. This is because absolute mode ignores strip offsets.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/121498

--- source/blender/sequencer/intern/modifier.cc
+++ source/blender/sequencer/intern/modifier.cc
@@ -31,6 +31,8 @@
 #include "SEQ_modifier.hh"
 #include "SEQ_render.hh"
 #include "SEQ_sound.hh"
+#include "SEQ_time.hh"
+#include "SEQ_utils.hh"
 
 #include "BLO_read_write.hh"
 
@@ -1493,6 +1495,23 @@ SequenceModifierData *SEQ_modifier_find_by_name(Sequence *seq, const char *name)
       BLI_findstring(&(seq->modifiers), name, offsetof(SequenceModifierData, name)));
 }
 
+static bool skip_modifier(Scene *scene, const SequenceModifierData *smd, int timeline_frame)
+{
+  using namespace blender::seq;
+
+  if (smd->mask_sequence == nullptr) {
+    return false;
+  }
+  const bool strip_has_ended_skip = smd->mask_input_type == SEQUENCE_MASK_INPUT_STRIP &&
+                                    smd->mask_time == SEQUENCE_MASK_TIME_RELATIVE &&
+                                    !SEQ_time_strip_intersects_frame(
+                                        scene, smd->mask_sequence, timeline_frame);
+  const bool missing_data_skip = !SEQ_sequence_has_valid_data(smd->mask_sequence) ||
+                                 media_presence_is_missing(scene, smd->mask_sequence);
+
+  return strip_has_ended_skip || missing_data_skip;
+}
+
 ImBuf *SEQ_modifier_apply_stack(const SeqRenderData *context,
                                 Sequence *seq,
                                 ImBuf *ibuf,
@@ -1518,7 +1537,7 @@ ImBuf *SEQ_modifier_apply_stack(const SeqRenderData *context,
       continue;
     }
 
-    if (smti->apply) {
+    if (smti->apply && !skip_modifier(context->scene, smd, timeline_frame)) {
       int frame_offset;
       if (smd->mask_time == SEQUENCE_MASK_TIME_RELATIVE) {
         frame_offset = seq->start;
