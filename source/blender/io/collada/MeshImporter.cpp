zyxcambridge/blender_llm

Fix #120721: Collada: Import with loose edges not immediately show them
Exposed by 1ea169d90e

Colladas `MeshImporter::mesh_add_edges` is a copy from `mesh_add_edges`
in `source/blender/editors/mesh/mesh_data.cc` (done in 2c0519037467 --
there is a comment to deduplicate this into BKE, but this wont be done
in this patch).

However, as time passed there have been additions to the later, namely:

- 353fe610ed2d (clearing the mesh runtime caches)
- 12becbf0dffe (altering selection handling, guess this can be ignored
here since collada generally does not seem to select geometry)

So in order to resolve, now also clear the caches for collada as in
353fe610ed2d (even though just the subset of just `mesh->runtime-
>loose_edges_cache.tag_dirty()` would also be enough).

Pull Request: https://projects.blender.org/blender/blender/pulls/120737

source/blender/io/collada/MeshImporter.cpp
@@ -25,6 +25,7 @@
 #include "BKE_lib_id.hh"
 #include "BKE_material.h"
 #include "BKE_mesh.hh"
+#include "BKE_mesh_runtime.hh"
 #include "BKE_object.hh"
 
 #include "DNA_meshdata_types.h"
@@ -566,6 +567,9 @@ void MeshImporter::mesh_add_edges(Mesh *mesh, int len)
 
   CustomData_free(&mesh->edge_data, mesh->edges_num);
   mesh->edge_data = edge_data;
+
+  BKE_mesh_runtime_clear_cache(mesh);
+
   mesh->edges_num = totedge;
 }
 

blender/blender

source/blender/io/collada/MeshImporter.cpp
@@ -23,6 +23,7 @@
 #include "BKE_lib_id.h"
 #include "BKE_material.h"
 #include "BKE_mesh.hh"
+#include "BKE_mesh_runtime.h"
 #include "BKE_object.h"
 
 #include "BLI_edgehash.h"
@@ -560,6 +561,7 @@ void MeshImporter::mesh_add_edges(Mesh *mesh, int len)
   CustomData_free(&mesh->edata, mesh->totedge);
   mesh->edata = edata;
   mesh->totedge = totedge;
+  BKE_mesh_runtime_clear_cache(mesh);
 }
 
 void MeshImporter::read_lines(COLLADAFW::Mesh *mesh, Mesh *me)