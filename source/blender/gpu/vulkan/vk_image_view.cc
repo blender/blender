5d5cbca7ed390cebe66ded79fb1601c2493df9c3
Jeroen Bakker <jeroen@blender.org>
2024-05-14 10:46:12 +0200

    Vulkan: Refactor image views
    Move ownership of image views to VKTexture. VKFramebuffer can request
    access to the image views. This allows reconfiguring framebuffers when
    the previous configuration is still in use by the render graph.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/121727

--- source/blender/gpu/vulkan/vk_image_view.cc
+++ source/blender/gpu/vulkan/vk_image_view.cc
@@ -29,22 +29,17 @@ static VkFormat to_non_srgb_format(const VkFormat format)
   return format;
 }
 
-VKImageView::VKImageView(VKTexture &texture,
-                         eImageViewUsage usage,
-                         IndexRange layer_range,
-                         IndexRange mip_range,
-                         bool use_stencil,
-                         bool use_srgb,
-                         StringRefNull name)
+VKImageView::VKImageView(VKTexture &texture, const VKImageViewInfo &info, StringRefNull name)
+    : info(info)
 {
   const VkImageAspectFlags allowed_bits = VK_IMAGE_ASPECT_COLOR_BIT |
-                                          (use_stencil ? VK_IMAGE_ASPECT_STENCIL_BIT :
-                                                         VK_IMAGE_ASPECT_DEPTH_BIT);
+                                          (info.use_stencil ? VK_IMAGE_ASPECT_STENCIL_BIT :
+                                                              VK_IMAGE_ASPECT_DEPTH_BIT);
   eGPUTextureFormat device_format = texture.device_format_get();
   VkImageAspectFlags image_aspect = to_vk_image_aspect_flag_bits(device_format) & allowed_bits;
 
   vk_format_ = to_vk_format(device_format);
-  if (texture.format_flag_get() & GPU_FORMAT_SRGB && !use_srgb) {
+  if (texture.format_flag_get() & GPU_FORMAT_SRGB && !info.use_srgb) {
     vk_format_ = to_non_srgb_format(vk_format_);
   }
 
@@ -52,14 +47,17 @@ VKImageView::VKImageView(VKTexture &texture,
   VkImageViewCreateInfo image_view_info = {};
   image_view_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
   image_view_info.image = texture.vk_image_handle();
-  image_view_info.viewType = to_vk_image_view_type(texture.type_get(), usage);
+  image_view_info.viewType = to_vk_image_view_type(texture.type_get(), info.usage);
   image_view_info.format = vk_format_;
-  image_view_info.components = texture.vk_component_mapping_get();
+  image_view_info.components.r = to_vk_component_swizzle(info.swizzle[0]);
+  image_view_info.components.g = to_vk_component_swizzle(info.swizzle[1]);
+  image_view_info.components.b = to_vk_component_swizzle(info.swizzle[2]);
+  image_view_info.components.a = to_vk_component_swizzle(info.swizzle[3]);
   image_view_info.subresourceRange.aspectMask = image_aspect;
-  image_view_info.subresourceRange.baseMipLevel = mip_range.first();
-  image_view_info.subresourceRange.levelCount = mip_range.size();
-  image_view_info.subresourceRange.baseArrayLayer = layer_range.first();
-  image_view_info.subresourceRange.layerCount = layer_range.size();
+  image_view_info.subresourceRange.baseMipLevel = info.mip_range.first();
+  image_view_info.subresourceRange.levelCount = info.mip_range.size();
+  image_view_info.subresourceRange.baseArrayLayer = info.layer_range.first();
+  image_view_info.subresourceRange.layerCount = info.layer_range.size();
 
   const VKDevice &device = VKBackend::get().device_get();
   vkCreateImageView(
@@ -67,7 +65,7 @@ VKImageView::VKImageView(VKTexture &texture,
   debug::object_label(vk_image_view_, name.c_str());
 }
 
-VKImageView::VKImageView(VKImageView &&other)
+VKImageView::VKImageView(VKImageView &&other) : info(other.info)
 {
   vk_image_view_ = other.vk_image_view_;
   other.vk_image_view_ = VK_NULL_HANDLE;
--- source/blender/gpu/vulkan/vk_image_view.hh
+++ source/blender/gpu/vulkan/vk_image_view.hh
@@ -16,18 +16,30 @@
 namespace blender::gpu {
 class VKTexture;
 
+struct VKImageViewInfo {
+  eImageViewUsage usage;
+  IndexRange layer_range;
+  IndexRange mip_range;
+  char swizzle[4];
+  bool use_stencil;
+  bool use_srgb;
+
+  bool operator==(const VKImageViewInfo &other) const
+  {
+    return usage == other.usage && layer_range == other.layer_range &&
+           mip_range == other.mip_range && strncmp(swizzle, other.swizzle, sizeof(swizzle)) &&
+           use_stencil == other.use_stencil && use_srgb == other.use_srgb;
+  }
+};
+
 class VKImageView : NonCopyable {
   VkImageView vk_image_view_ = VK_NULL_HANDLE;
   VkFormat vk_format_ = VK_FORMAT_UNDEFINED;
 
  public:
-  VKImageView(VKTexture &texture,
-              eImageViewUsage usage,
-              IndexRange layer_range,
-              IndexRange mip_range,
-              bool use_stencil,
-              bool use_srgb,
-              StringRefNull name);
+  const VKImageViewInfo info;
+
+  VKImageView(VKTexture &texture, const VKImageViewInfo &info, StringRefNull name);
 
   VKImageView(VKImageView &&other);
   ~VKImageView();
