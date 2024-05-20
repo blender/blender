669fad29e9ccd8cf7ac1e967f6235a58ace2b1e1
Campbell Barton <campbell@blender.org>
2024-05-20 10:29:22 +1000

    Cleanup: clarify doc-strings for BLI_fftw
    Reserve comments immediately before the function for doc-strings in
    the header. Otherwise both are candidates as doc-strings.

--- source/blender/blenlib/BLI_fftw.hh
+++ source/blender/blenlib/BLI_fftw.hh
@@ -8,15 +8,19 @@
 
 namespace blender::fftw {
 
-/* FFTW's real to complex and complex to real transforms are more efficient when their input has a
+/**
+ * FFTW's real to complex and complex to real transforms are more efficient when their input has a
  * specific size. This function finds the most optimal size that is more than or equal the given
  * size. The input data can then be zero padded to the optimal size for better performance. See
- * Section 4.3.3 Real-data DFTs in the FFTW manual for more information. */
+ * Section 4.3.3 Real-data DFTs in the FFTW manual for more information.
+ */
 int optimal_size_for_real_transform(int size);
 int2 optimal_size_for_real_transform(int2 size);
 
-/* Initialize the float variant of FFTW. This essentially setup the multithreading hooks to enable
- * multithreading using TBB's parallel_for and makes the FFTW planner thread safe. */
+/**
+ * Initialize the float variant of FFTW. This essentially setup the multi-threading hooks to enable
+ * multi-threading using TBB's parallel_for and makes the FFTW planner thread safe.
+ */
 void initialize_float();
 
 }  // namespace blender::fftw
