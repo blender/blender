f3c32a36bcfcad4fb948fb1eb205a2790967b1de
Philipp Oeser <philipp@blender.org>
2024-05-23 17:33:40 +0200

    Fix #103562: preserve custom normals in BM_mesh_triangulate
    `BM_mesh_triangulate` is used in exporters (when the "Triangulate"
    option is ON), the `Triangulate` modifier and currently also in the
    `Triangulate` geometry node (even though there are plans to change this,
    see !112264)
    
    So in practice, exporters (Alembic/FBX/OBJ/Collada) were breaking
    custom normals for game pipelines (unless everything was triangulated
    beforehand).
    
    This change builds upon 93c8955a722b (uses the use
    `BM_custom_loop_normals_to_vector_layer` /
    `BM_custom_loop_normals_from_vector_layer` pair of calls).
    
    In the case of the `Triangulate` modifier, this had its own try at
    preserving custom normals in 7d0fcaa69a64  -- doing very similar
    things but as an option -- this is now removed (so it is always done,
    which fits into "interpolate custom data if it's there" design that we have
    nowadays).
    
    NOTE: the "Triangulate Faces" operator already did the same
    Pull Request: https://projects.blender.org/blender/blender/pulls/121871

--- source/blender/bmesh/tools/bmesh_triangulate.cc
+++ source/blender/bmesh/tools/bmesh_triangulate.cc
@@ -90,6 +90,8 @@ void BM_mesh_triangulate(BMesh *bm,
   MemArena *pf_arena;
   Heap *pf_heap;
 
+  BM_custom_loop_normals_to_vector_layer(bm);
+
   pf_arena = BLI_memarena_new(BLI_POLYFILL_ARENA_SIZE, __func__);
 
   if (ngon_method == MOD_TRIANGULATE_NGON_BEAUTY) {
@@ -149,6 +151,8 @@ void BM_mesh_triangulate(BMesh *bm,
   }
 
   BLI_memarena_free(pf_arena);
+
+  BM_custom_loop_normals_from_vector_layer(bm, false);
 
   if (ngon_method == MOD_TRIANGULATE_NGON_BEAUTY) {
     BLI_heap_free(pf_heap, nullptr);
