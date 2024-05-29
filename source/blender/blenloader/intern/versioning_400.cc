32259aa0b6e080230de6ce5bf3bef9956a045d6e
Clément Foucault <foucault.clem@gmail.com>
2024-05-29 16:19:32 +0200

    Fix: EEVEE-Next: Versioning older file to remove indirect volume light
    Make sure older file have indirect volume lighting disabled to
    avoid this new feature washing out the volume object lighting.

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -3334,6 +3334,9 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
 
     LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
       scene->eevee.clamp_surface_indirect = 10.0f;
+      /* Make contribution of indirect lighting very small (but non-null) to avoid world lighting
+       * and volume lightprobe changing the appearance of volume objects. */
+      scene->eevee.clamp_surface_indirect = 1e-8f;
     }
   }
 
