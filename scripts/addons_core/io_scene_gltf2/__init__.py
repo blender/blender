9ba22d4bc780f084ee78527cec0d41277b7cd082
Julien Duroure <julien.duroure@gmail.com>
2024-05-30 15:14:00 +0200

    glTF exporter: Fix #122379: Fix GN data type check

--- scripts/addons_core/io_scene_gltf2/__init__.py
+++ scripts/addons_core/io_scene_gltf2/__init__.py
@@ -5,7 +5,7 @@
 bl_info = {
     'name': 'glTF 2.0 format',
     'author': 'Julien Duroure, Scurest, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein, and many external contributors',
-    "version": (4, 2, 41),
+    "version": (4, 2, 42),
     'blender': (4, 2, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',
