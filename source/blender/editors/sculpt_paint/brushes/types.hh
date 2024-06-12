9810281e5d919b225fb5b634e160770517ddf632
Hans Goudey <hans@blender.org>
2024-06-12 11:18:11 -0400

    Cleanup: Alphabetical formatting for sculpt brushes

--- source/blender/editors/sculpt_paint/brushes/types.hh
+++ source/blender/editors/sculpt_paint/brushes/types.hh
@@ -12,20 +12,20 @@ struct PBVHNode;
 
 namespace blender::ed::sculpt_paint {
 
+void do_displacement_eraser_brush(const Sculpt &sd, Object &ob, Span<PBVHNode *> nodes);
 /** A simple normal-direction displacement. */
 void do_draw_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
-void do_nudge_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
 /** A simple normal-direction displacement based on image texture RGB/XYZ values. */
 void do_draw_vector_displacement_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
+void do_fill_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
+void do_flatten_brush(const Sculpt &sd, Object &ob, Span<PBVHNode *> nodes);
+void do_inflate_brush(const Sculpt &sd, Object &ob, Span<PBVHNode *> nodes);
+void do_nudge_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
+void do_scrape_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
 /** Smooth positions with neighboring vertices. */
 void do_smooth_brush(const Sculpt &sd,
                      Object &object,
                      Span<PBVHNode *> nodes,
                      float brush_strength);
-void do_scrape_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
-void do_fill_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
-void do_displacement_eraser_brush(const Sculpt &sd, Object &ob, Span<PBVHNode *> nodes);
-void do_flatten_brush(const Sculpt &sd, Object &ob, Span<PBVHNode *> nodes);
-void do_inflate_brush(const Sculpt &sd, Object &ob, Span<PBVHNode *> nodes);
 
 }  // namespace blender::ed::sculpt_paint
