355c7b788ec5cadbf4b25c7771653d8c6faa00f8
Clément Foucault <foucault.clem@gmail.com>
2024-06-18 18:17:34 +0200

    Fix: EEVEE: Remaining references to EEVEE-Legacy in codebase
    Fix #123387

--- source/blender/editors/render/render_preview.cc
+++ source/blender/editors/render/render_preview.cc
@@ -275,7 +275,7 @@ const char *ED_preview_collection_name(const ePreviewType pr_type)
 
 static bool render_engine_supports_ray_visibility(const Scene *sce)
 {
-  return !STREQ(sce->r.engine, RE_engine_id_BLENDER_EEVEE);
+  return !STREQ(sce->r.engine, RE_engine_id_BLENDER_EEVEE_NEXT);
 }
 
 static void switch_preview_collection_visibility(ViewLayer *view_layer, const ePreviewType pr_type)
@@ -517,7 +517,7 @@ static Scene *preview_prepare_scene(
 
     if (id_type == ID_TE) {
       /* Texture is not actually rendered with engine, just set dummy value. */
-      STRNCPY(sce->r.engine, RE_engine_id_BLENDER_EEVEE);
+      STRNCPY(sce->r.engine, RE_engine_id_BLENDER_EEVEE_NEXT);
     }
 
     if (id_type == ID_MA) {
