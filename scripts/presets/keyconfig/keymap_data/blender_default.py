dab338a9b0f2a5d86d0a1193c9ad177b8d66dae5
Falk David <falk@blender.org>
2024-06-19 15:28:21 +0200

    Fix: GPv3: Shortcut 'F' to change brush radius no longer working
    This was introduced with b7c570c8543a246054e56a793096f6744662fdb3.
    For the draw tools, we no longer use the unified paint settings.
    This means that we can't use the `_template_paint_radial_control` anymore.
    
    The fix explicitly sets up the shortcut to use the `brush.size` and `brush.strength`
    for the `radial_control` operator.

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -4600,7 +4600,17 @@ def km_grease_pencil_brush_stroke(_params):
          {"properties": [("mode", 'INVERT')]}),
         ("grease_pencil.brush_stroke", {"type": 'LEFTMOUSE', "value": 'PRESS', "shift": True},
          {"properties": [("mode", 'SMOOTH')]}),
-        *_template_paint_radial_control("gpencil_paint"),
+        # Brush size
+        ("wm.radial_control", {"type": 'F', "value": 'PRESS'},
+         {"properties": [("data_path_primary", 'tool_settings.gpencil_paint.brush.size')]}),
+        # Brush strength
+        ("wm.radial_control", {"type": 'F', "value": 'PRESS', "shift": True},
+         {"properties": [("data_path_primary", 'tool_settings.gpencil_paint.brush.strength')]}),
+        # Increase/Decrease brush size
+        ("brush.scale_size", {"type": 'LEFT_BRACKET', "value": 'PRESS', "repeat": True},
+         {"properties": [("scalar", 0.9)]}),
+        ("brush.scale_size", {"type": 'RIGHT_BRACKET', "value": 'PRESS', "repeat": True},
+         {"properties": [("scalar", 1.0 / 0.9)]}),
     ])
 
     return keymap
