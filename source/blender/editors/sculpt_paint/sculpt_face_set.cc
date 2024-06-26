30ad15c01cf736bbc5f9ab4e6b40a3ca0557b1f9
Mangal Kushwah <mangal-kushwah@noreply.localhost>
2024-06-26 02:22:16 +0200

    Fix: #123349 Incorrect initialization of face sets by material
    Changes the way the face set index generated for intialize by material.
    In current implementation face set index is generated by adding 1 to
    material index. In most cases it'll work fine. But in some cases hidden
    face sets and material face sets can have same index.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/123465

--- source/blender/editors/sculpt_paint/sculpt_face_set.cc
+++ source/blender/editors/sculpt_paint/sculpt_face_set.cc
@@ -888,24 +888,16 @@ static int init_op_exec(bContext *C, wmOperator *op)
           "material_index", bke::AttrDomain::Face, 0);
       const VArraySpan<bool> hide_poly = *attributes.lookup<bool>(".hide_poly",
                                                                   bke::AttrDomain::Face);
-      const Set<int> hidden_face_sets = gather_hidden_face_sets(hide_poly, face_sets.span);
-
-      int prev_material = material_indices[0];
-      int material_face_set = 1;
       for (const int i : IndexRange(mesh->faces_num)) {
         if (!hide_poly.is_empty() && hide_poly[i]) {
           continue;
         }
-        if (prev_material != material_indices[i]) {
-          material_face_set += 1;
-        }
-        while (hidden_face_sets.contains(material_face_set)) {
-          material_face_set += 1;
-        }
 
-        face_sets.span[i] = material_face_set;
-        prev_material = material_indices[i];
+        /* In some cases material face set index could be same as hidden face set index
+         * A more robust implementation is needed to avoid this */
+        face_sets.span[i] = material_indices[i] + 1;
       }
+
       face_sets.finish();
       break;
     }
