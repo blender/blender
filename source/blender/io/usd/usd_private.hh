e9ba414799e787a7eaeb2e404180575cf916bc49
Jesse Yurkovich <jesse.y@gmail.com>
2024-07-03 22:02:53 +0200

    Fix #124103: Build error when using WITH_USD but not WITH_HYDRA
    Move a few functions to a common location so that they're all accessible
    from both USD and Hydra.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/124119

--- source/blender/io/usd/usd_private.hh
+++ source/blender/io/usd/usd_private.hh
@@ -6,6 +6,8 @@
 
 #include <pxr/usd/usd/common.h>
 
+#include <string>
+
 #include "usd.hh"
 
 struct Depsgraph;
@@ -16,4 +18,8 @@ pxr::UsdStageRefPtr export_to_stage(const USDExportParams &params,
                                     Depsgraph *depsgraph,
                                     const char *filepath);
 
-};
+std::string image_cache_file_path();
+std::string get_image_cache_file(const std::string &file_name, bool mkdir = true);
+std::string cache_image_color(float color[4]);
+
+};  // namespace blender::io::usd
