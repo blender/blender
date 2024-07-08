987b781412f1e87079c0c9e081775ac741c397bb
Harley Acheson <harley.acheson@gmail.com>
2024-07-08 19:12:56 +0200

    UI: Various Grammar Changes for Extensions
    Various minimal grammar changes in the new Extensions system. For
    example changing "Please turn Online Access on the System settings" to
    "Please turn Online Access on in the System settings"
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/124262

--- scripts/addons_core/bl_pkg/bl_extension_ops.py
+++ scripts/addons_core/bl_pkg/bl_extension_ops.py
@@ -1834,7 +1834,7 @@ class EXTENSIONS_OT_package_install_marked(Operator, _ExtCmdMixIn):
                 self._repo_map_packages_addon_only.append((repo_item.directory, pkg_id_sequence_addon_only))
 
         if not cmd_batch:
-            self.report({'ERROR'}, "No un-installed packages marked")
+            self.report({'ERROR'}, "No uninstalled packages marked")
             return None
 
         # Lock repositories.
@@ -2894,9 +2894,9 @@ class EXTENSIONS_OT_package_install(Operator, _ExtCmdMixIn):
 
         if item_local is not None:
             if item_local.type == "add-on":
-                message = rpt_("Add-on \"{:s}\" already installed!")
+                message = rpt_("Add-on \"{:s}\" is already installed!")
             elif item_local.type == "theme":
-                message = rpt_("Theme \"{:s}\" already installed!")
+                message = rpt_("Theme \"{:s}\" is already installed!")
             else:
                 assert False, "Unreachable"
             self._draw_override = (
@@ -2956,7 +2956,7 @@ class EXTENSIONS_OT_package_install(Operator, _ExtCmdMixIn):
             subcol = box.column(align=True)
             subcol.label(text=iface_("URL: {:s}").format(url_for_display), translate=False)
         else:
-            col.label(text="Alternatively download the extension to Install from Disk.")
+            col.label(text="Alternatively, download the extension to Install from Disk.")
 
         layout.operator_context = 'INVOKE_DEFAULT'
         props = layout.template_popup_confirm("preferences.extension_repo_add", text="Add Repository...")
@@ -3473,7 +3473,7 @@ class EXTENSIONS_OT_userpref_show_online(Operator):
     def poll(cls, _context):
         if bpy.app.online_access_override:
             if not bpy.app.online_access:
-                cls.poll_message_set("Blender was launched in offline-mode which cannot be changed at runtime")
+                cls.poll_message_set("Blender was launched in offline mode, which cannot be changed at runtime")
                 return False
         return True
 
@@ -3493,7 +3493,7 @@ class EXTENSIONS_OT_userpref_allow_online(Operator):
     def poll(cls, _context):
         if bpy.app.online_access_override:
             if not bpy.app.online_access:
-                cls.poll_message_set("Blender was launched in offline-mode which cannot be changed at runtime")
+                cls.poll_message_set("Blender was launched in offline mode, which cannot be changed at runtime")
                 return False
         return True
 
@@ -3543,7 +3543,7 @@ class EXTENSIONS_OT_userpref_allow_online_popup(Operator):
             )
         else:
             lines = (
-                rpt_("Please turn Online Access on the System settings."),
+                rpt_("Please turn Online Access on in the System settings."),
                 "",
                 rpt_("Internet access is required to install extensions from the internet."),
             )
