c7bcd1137fecbd931b7bac52e80e33a34e9c92dc
Clément Foucault <foucault.clem@gmail.com>
2024-07-15 11:59:26 +0200

    Fix: UI: Hide studiolight world orientation button for Cycles
    This option is not available in EEVEE anymore. Limit the
    visibility to Workbench.
    
    Fix #124521
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/124717

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -6918,7 +6918,7 @@ class VIEW3D_PT_shading_lighting(Panel):
 
                 engine = context.scene.render.engine
                 row = col.row()
-                if engine != 'BLENDER_EEVEE_NEXT':
+                if engine == 'BLENDER_WORKBENCH':
                     row.prop(shading, "use_studiolight_view_rotation", text="", icon='WORLD', toggle=True)
                     row = row.row()
                 row.prop(shading, "studiolight_rotate_z", text="Rotation")
