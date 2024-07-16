3875ceb28002f11f4a1ef3f2d5ec23e8a9895430
Pratik Borhade <pratikborhade302@gmail.com>
2024-07-16 19:23:22 +0200

    Fix #124647: UV editing console error due to missing enum
    IMAGE_AST_brush_paint poll should check context.space_data.mode
    rather than context.space_data.ui_mode.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/124762

--- scripts/startup/bl_ui/space_image.py
+++ scripts/startup/bl_ui/space_image.py
@@ -1706,7 +1706,7 @@ class IMAGE_AST_brush_paint(ImageAssetShelf, AssetShelf):
 
     @classmethod
     def poll(cls, context):
-        return context.space_data and context.space_data.ui_mode == 'PAINT'
+        return context.space_data and context.space_data.mode == 'PAINT'
 
 
 classes = (
