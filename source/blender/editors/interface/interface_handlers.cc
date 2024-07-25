d2bcca680ed541c374af237d4c92812c88bd5539
Julian Eisel <julian@blender.org>
2024-07-25 17:37:58 +0200

    Fix asset shelf popup not closing when selecting asset with enter
    When typing to search an asset and confirming with enter, the popup
    should close.
    
    Mistake in 2781725734.
    
    Steps to reproduce:
    - Enter sculpt mode with the default cube active
    - Press Shift+Space to open the brush asset selector popup
    - Type to search for a brush
    - Press enter -- the popup didn't close as it was supposed to

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -4915,7 +4915,17 @@ static int ui_do_but_TOG(bContext *C, uiBut *but, uiHandleButtonData *data, cons
   return WM_UI_HANDLER_CONTINUE;
 }
 
-static void force_activate_view_item_but(bContext *C, ARegion *region, uiButViewItem *but)
+/**
+ * \param close_popup: In most cases activating the view item should close the popup it is in
+ *                     (unless #AbstractView::keep_open() was called when building the view), if
+ *                     any. But this should only be done when activating the view item directly,
+ *                     things like clicking nested buttons or calling the context menu should keep
+ *                     the popup open for further interaction.
+ */
+static void force_activate_view_item_but(bContext *C,
+                                         ARegion *region,
+                                         uiButViewItem *but,
+                                         const bool close_popup = true)
 {
   if (but->active) {
     ui_apply_but(C, but->block, but, but->active, true);
@@ -4924,8 +4934,7 @@ static void force_activate_view_item_but(bContext *C, ARegion *region, uiButView
     UI_but_execute(C, region, but);
   }
 
-  /* By default, activating a view item closes the popup (only when clicking the item itself). */
-  if (but->active && !UI_view_item_popup_keep_open(*but->view_item)) {
+  if (close_popup && !UI_view_item_popup_keep_open(*but->view_item)) {
     UI_popup_menu_close_from_but(but);
   }
 }
@@ -10061,7 +10070,9 @@ static int ui_handle_view_item_event(bContext *C,
                 ui_view_item_find_mouse_over(region, event->xy));
         /* Will free active button if there already is one. */
         if (view_but) {
-          force_activate_view_item_but(C, region, view_but);
+          /* Close the popup when clicking on the view item directly, not any overlapped button. */
+          const bool close_popup = view_but == active_but;
+          force_activate_view_item_but(C, region, view_but, close_popup);
         }
       }
       break;
