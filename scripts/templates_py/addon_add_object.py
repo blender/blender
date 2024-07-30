2be7442332487da9c16fa524957a962c43485776
Dalai Felinto <dalai@blender.org>
2024-07-30 16:44:12 +0200

    Templates: Remove bl_info from addon_add_object

--- scripts/templates_py/addon_add_object.py
+++ scripts/templates_py/addon_add_object.py
@@ -1,15 +1,5 @@
-bl_info = {
-    "name": "New Object",
-    "author": "Your Name Here",
-    "version": (1, 0),
-    "blender": (2, 80, 0),
-    "location": "View3D > Add > Mesh > New Object",
-    "description": "Adds a new Mesh Object",
-    "warning": "",
-    "doc_url": "",
-    "category": "Add Mesh",
-}
-
+# To make this add-on installable, create an extension with it:
+# https://docs.blender.org/manual/en/latest/advanced/extensions/getting_started.html
 
 import bpy
 from bpy.types import Operator
