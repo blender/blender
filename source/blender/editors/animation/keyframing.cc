af28dcfb7d380350185c014e070bb7aa045126cb
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-08-06 13:25:22 +0200

    Revert "Fix #124125: Depsgraph and scene time out of sync"
    This reverts commit 780dc67f6d827213d84514398504f0e3ad705c77.
    The patch caused a performance regression when inserting many
    keys via python, as is done with the "Whole Character"
    keying set.
    Regression report: #125888

--- source/blender/editors/animation/keyframing.cc
+++ source/blender/editors/animation/keyframing.cc
@@ -38,7 +38,6 @@
 #include "BKE_scene.hh"
 
 #include "DEG_depsgraph.hh"
-#include "DEG_depsgraph_query.hh"
 
 #include "ED_anim_api.hh"
 #include "ED_keyframing.hh"
@@ -377,9 +376,6 @@ static int insert_key(bContext *C, wmOperator *op)
     return OPERATOR_CANCELLED;
   }
 
-  /* The depsgraph needs to be in an evaluated state to ensure the values we get from the
-   * properties are actually the values of the current frame. */
-  Depsgraph *depsgraph = CTX_data_ensure_evaluated_depsgraph(C);
   Main *bmain = CTX_data_main(C);
   Scene *scene = CTX_data_scene(C);
   const float scene_frame = BKE_scene_frame_get(scene);
@@ -387,6 +383,7 @@ static int insert_key(bContext *C, wmOperator *op)
   const eInsertKeyFlags insert_key_flags = animrig::get_keyframing_flags(scene);
   const eBezTriple_KeyframeType key_type = eBezTriple_KeyframeType(
       scene->toolsettings->keyframe_type);
+  Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
   const AnimationEvalContext anim_eval_context = BKE_animsys_eval_context_construct(
       depsgraph, BKE_scene_frame_get(scene));
 
@@ -994,12 +991,8 @@ static int insert_key_button_exec(bContext *C, wmOperator *op)
   PointerRNA ptr = {nullptr};
   PropertyRNA *prop = nullptr;
   uiBut *but;
-
-  /* The depsgraph needs to be in an evaluated state to ensure the values we get from the
-   * properties are actually the values of the current frame. */
-  Depsgraph *depsgraph = CTX_data_ensure_evaluated_depsgraph(C);
   const AnimationEvalContext anim_eval_context = BKE_animsys_eval_context_construct(
-      depsgraph, BKE_scene_frame_get(scene));
+      CTX_data_depsgraph_pointer(C), BKE_scene_frame_get(scene));
   bool changed = false;
   int index;
   const bool all = RNA_boolean_get(op->ptr, "all");
