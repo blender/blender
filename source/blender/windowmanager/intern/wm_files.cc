d8ec92ed0b2a618ca8af9d8ed837f6ec513851db
Lorenzo Pierfederici <lpierfederici@atomiccartoons.com>
2024-08-12 12:28:01 -0700

    WM: Don't notify WM when file is saved as copy
    When saving a copy, the active file remains unchanged so the "saved"
    status and window title shouldn't be updated.

--- source/blender/windowmanager/intern/wm_files.cc
+++ source/blender/windowmanager/intern/wm_files.cc
@@ -3577,13 +3577,17 @@ static int wm_save_as_mainfile_exec(bContext *C, wmOperator *op)
     bmain->has_forward_compatibility_issues = false;
   }
 
-  WM_event_add_notifier(C, NC_WM | ND_FILESAVE, nullptr);
-  if (wmWindowManager *wm = CTX_wm_manager(C)) {
-    /* Restart auto-save timer to avoid unnecessary unexpected freezing (because of auto-save) when
-     * often saving manually. */
-    wm_autosave_timer_end(wm);
-    wm_autosave_timer_begin(wm);
-    wm->autosave_scheduled = false;
+  if (!use_save_as_copy) {
+    /* If saved file is the active one, notify WM so that saved status and window title can be
+     * updated. */
+    WM_event_add_notifier(C, NC_WM | ND_FILESAVE, nullptr);
+    if (wmWindowManager *wm = CTX_wm_manager(C)) {
+      /* Restart auto-save timer to avoid unnecessary unexpected freezing (because of auto-save)
+       * when often saving manually. */
+      wm_autosave_timer_end(wm);
+      wm_autosave_timer_begin(wm);
+      wm->autosave_scheduled = false;
+    }
   }
 
   if (!is_save_as && RNA_boolean_get(op->ptr, "exit")) {
