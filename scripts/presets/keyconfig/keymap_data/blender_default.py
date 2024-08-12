64211aa8c61da7473c5ff78f405df5aaafb2c195
Sean Kim <SeanCTKim@protonmail.com>
2024-08-12 17:06:25 +0200

    Sculpt: Add polyline gesture submit on double click
    Originally, the polyline gesture tools were intended to have the gesture
    submit on clicking on the starting point and pressing enter only. It was
    brought up that the knife tool which has similar behavior also exposes
    this on double clicking. To better align expected behavior across tools,
    this commit adds similar functionality for the polyline tools.
    
    Implements #125249
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/125391

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -6648,6 +6648,7 @@ def km_gesture_polyline(_params):
 
     items.extend([
         ("CONFIRM", {"type": 'RET', "value": 'PRESS', "any": True}, None),
+        ("CONFIRM", {"type": 'LEFTMOUSE', "value": 'DOUBLE_CLICK', "any": True}, None),
         ("CANCEL", {"type": 'ESC', "value": 'PRESS', "any": True}, None),
         ("CANCEL", {"type": 'RIGHTMOUSE', "value": 'ANY', "any": True}, None),
         ("SELECT", {"type": 'LEFTMOUSE', "value": 'PRESS', "any": True}, None),
