738c2a5e1cf963c2e71b649512d6be4c119480e9
casey bianco-davis <caseycasey739@gmail.com>
2024-08-13 15:01:48 +0200

    Fix #126241: Some attributes set to default.
    The problem was that the attributes where hard coded, rather then using the function.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/126251

--- source/blender/editors/grease_pencil/intern/grease_pencil_primitive.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_primitive.cc
@@ -422,6 +422,37 @@ static int grease_pencil_primitive_curve_points_number(PrimitiveToolOperation &p
   return 0;
 }
 
+/* Attributes that are defined explicitly and should not be copied from original geometry. */
+static Set<std::string> skipped_attribute_ids(const PrimitiveToolOperation &ptd,
+                                              const bke::AttrDomain domain)
+{
+  switch (domain) {
+    case bke::AttrDomain::Point:
+      if (ptd.vertex_color) {
+        return {"position", "radius", "opacity", "vertex_color"};
+      }
+      else {
+        return {"position", "radius", "opacity"};
+      }
+    case bke::AttrDomain::Curve:
+      if (ptd.fill_color) {
+        return {"curve_type",
+                "material_index",
+                "cyclic",
+                "softness",
+                "start_cap",
+                "end_cap",
+                "fill_color"};
+      }
+      else {
+        return {"curve_type", "material_index", "cyclic", "softness", "start_cap", "end_cap"};
+      }
+    default:
+      return {};
+  }
+  return {};
+}
+
 static void grease_pencil_primitive_update_curves(PrimitiveToolOperation &ptd)
 {
   bke::CurvesGeometry &curves = ptd.drawing->strokes_for_write();
@@ -473,50 +504,19 @@ static void grease_pencil_primitive_update_curves(PrimitiveToolOperation &ptd)
   }
 
   /* Initialize the rest of the attributes with default values. */
-  Set<std::string> point_attributes_to_skip = {"position", "radius", "opacity", "vertex_color"};
-  Set<std::string> curve_attributes_to_skip = {};
   bke::MutableAttributeAccessor attributes = curves.attributes_for_write();
-  bke::fill_attribute_range_default(
-      attributes, bke::AttrDomain::Point, point_attributes_to_skip, curve_points);
+  bke::fill_attribute_range_default(attributes,
+                                    bke::AttrDomain::Point,
+                                    skipped_attribute_ids(ptd, bke::AttrDomain::Point),
+                                    curve_points);
   bke::fill_attribute_range_default(attributes,
                                     bke::AttrDomain::Curve,
-                                    curve_attributes_to_skip,
+                                    skipped_attribute_ids(ptd, bke::AttrDomain::Curve),
                                     curves.curves_range().take_back(1));
 
   ptd.drawing->tag_topology_changed();
   ptd.drawing->set_texture_matrices({ptd.texture_space},
                                     IndexRange::from_single(curves.curves_range().last()));
-}
-
-/* Attributes that are defined explicitly and should not be copied from original geometry. */
-static Set<std::string> skipped_attribute_ids(const PrimitiveToolOperation &ptd,
-                                              const bke::AttrDomain domain)
-{
-  switch (domain) {
-    case bke::AttrDomain::Point:
-      if (ptd.vertex_color) {
-        return {"position", "radius", "opacity", "vertex_color"};
-      }
-      else {
-        return {"position", "radius", "opacity"};
-      }
-    case bke::AttrDomain::Curve:
-      if (ptd.fill_color) {
-        return {"curve_type",
-                "material_index",
-                "cyclic",
-                "softness",
-                "start_cap",
-                "end_cap",
-                "fill_color"};
-      }
-      else {
-        return {"curve_type", "material_index", "cyclic", "softness", "start_cap", "end_cap"};
-      }
-    default:
-      return {};
-  }
-  return {};
 }
 
 static void grease_pencil_primitive_init_curves(PrimitiveToolOperation &ptd)
