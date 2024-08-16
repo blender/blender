b04c0da6f04cbd3f38c0d8a5fd137375209a1fc1
Harley Acheson <harley.acheson@gmail.com>
2024-08-16 21:05:38 +0200

    UI: Increase the Editor Edge Move Zone Size
    This increases the hit size for the zone that allows resizing editors.
    By approximately one more pixel on each side. This should make sizing
    a little bit easier, especially with pens. Corner zone and region
    reveal zone take precedence so I haven't found any time this causes any
    interference.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/126376

--- source/blender/editors/screen/screen_intern.hh
+++ source/blender/editors/screen/screen_intern.hh
@@ -68,7 +68,7 @@ enum class AreaDockTarget {
 /**
  * Expanded interaction influence of area borders.
  */
-#define BORDERPADDING ((2.0f * UI_SCALE_FAC) + U.pixelsize)
+#define BORDERPADDING ((3.0f * UI_SCALE_FAC) + U.pixelsize)
 
 /* `area.cc` */
 
