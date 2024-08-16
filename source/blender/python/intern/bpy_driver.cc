53f0c2c14d8d97d90c5fc16bb386fa79f2d56b0a
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-08-16 12:41:53 +0200

    Fix: Wrong message in driver for disabled execution
    This backports the following 2 commits to 4.2
    1a8053939bd79e0bd7a896ae0f3b1b9195ecb142
    e2bc41598d8e66f0804a255e17001fff0aa8c92b
    
    These two fix an issue where the driver stated it is invalid,
    while in fact it couldn't execute due to python being blocked.
    
    Original report: #106372
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/126088

--- source/blender/python/intern/bpy_driver.cc
+++ source/blender/python/intern/bpy_driver.cc
@@ -527,6 +527,7 @@ float BPY_driver_exec(PathResolvedRNA *anim_rna,
 
       printf("skipping driver '%s', automatic scripts are disabled\n", expr);
     }
+    driver_orig->flag |= DRIVER_FLAG_PYTHON_BLOCKED;
     return 0.0f;
   }
 #else
@@ -581,6 +582,7 @@ float BPY_driver_exec(PathResolvedRNA *anim_rna,
 
     /* Maybe this can be removed but for now best keep until were sure. */
     driver_orig->flag |= DRIVER_FLAG_RENAMEVAR;
+    driver_orig->flag &= ~DRIVER_FLAG_PYTHON_BLOCKED;
 #ifdef USE_BYTECODE_WHITELIST
     is_recompile = true;
 #endif
@@ -689,6 +691,7 @@ float BPY_driver_exec(PathResolvedRNA *anim_rna,
         Py_DECREF(expr_code);
         expr_code = nullptr;
         PyTuple_SET_ITEM(((PyObject *)driver_orig->expr_comp), 0, nullptr);
+        driver_orig->flag |= DRIVER_FLAG_PYTHON_BLOCKED;
       }
     }
   }
