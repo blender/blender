fe6ab4f344fb3a2d76663d5df716e94fb08aedfb
Campbell Barton <ideasman42@gmail.com>
2024-08-19 10:48:06 +1000

    PyDoc: corrections to formatting and resolve warnings
    Use RST formatting for doc-strings used in Python API docs
    as well as minor corrections, also add links for foreach_get/set.

--- scripts/modules/bpy_types.py
+++ scripts/modules/bpy_types.py
@@ -1405,8 +1405,10 @@ class GreasePencilDrawing(StructRNA):
         """
         Return a collection of all the Grease Pencil strokes in this drawing.
 
-        .. note:: This API should *not* be used for performance critical operations.
-        Use the :class:`GreasePencilDrawing.attributes` API instead.
+        .. note::
+
+           This API should *not* be used for performance critical operations.
+           Use the :class:`GreasePencilDrawing.attributes` API instead.
         """
         from grease_pencil_python import GreasePencilStrokeSlice
         num_strokes = self.attributes.domain_size('CURVE')
