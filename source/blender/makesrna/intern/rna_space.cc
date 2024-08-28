ab261355ab264ef4088d93a7b73d690a2ead6dcc
Campbell Barton <campbell@blender.org>
2024-08-28 13:11:43 +1000

    UI: clarify "show faces" description
    The text made it seem the overlay *only* highlighted selected faces
    when this applies to unselected faces too.

--- source/blender/makesrna/intern/rna_space.cc
+++ source/blender/makesrna/intern/rna_space.cc
@@ -4774,7 +4774,7 @@ static void rna_def_space_view3d_overlay(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "show_faces", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "overlay.edit_flag", V3D_OVERLAY_EDIT_FACES);
-  RNA_def_property_ui_text(prop, "Display Faces", "Highlight selected faces");
+  RNA_def_property_ui_text(prop, "Display Faces", "Display a face selection overlay");
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_VIEW3D, nullptr);
 
   prop = RNA_def_property(srna, "show_face_center", PROP_BOOLEAN, PROP_NONE);
