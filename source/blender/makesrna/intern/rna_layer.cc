b6e022d913c762ac99a12855598124865f4eb888
Bastien Montagne <bastien@blender.org>
2024-08-29 15:27:36 +0200

    BPY/RNA: Add a note to `ObjectBase` documentation that it is never exposed.
    Blender Python API does not expose the 'base' object data currently (it
    always converts it to the actual object).

--- source/blender/makesrna/intern/rna_layer.cc
+++ source/blender/makesrna/intern/rna_layer.cc
@@ -574,7 +574,10 @@ static void rna_def_object_base(BlenderRNA *brna)
 
   srna = RNA_def_struct(brna, "ObjectBase", nullptr);
   RNA_def_struct_sdna(srna, "Base");
-  RNA_def_struct_ui_text(srna, "Object Base", "An object instance in a render layer");
+  RNA_def_struct_ui_text(
+      srna,
+      "Object Base",
+      "An object instance in a View Layer (currently never exposed in Python API)");
   RNA_def_struct_ui_icon(srna, ICON_OBJECT_DATA);
 
   prop = RNA_def_property(srna, "object", PROP_POINTER, PROP_NONE);
