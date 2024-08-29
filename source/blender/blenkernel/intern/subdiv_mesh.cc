784b4184753778fb6a031406dc4f02badd2b616d
Hans Goudey <hans@blender.org>
2024-08-29 08:19:36 -0400

    Cleanup: Use simpler method to convert boolean array to bits

--- source/blender/blenkernel/intern/subdiv_mesh.cc
+++ source/blender/blenkernel/intern/subdiv_mesh.cc
@@ -1226,16 +1226,8 @@ Mesh *subdiv_to_mesh(Subdiv *subdiv, const ToMeshSettings *settings, const Mesh 
 
   /* Move the optimal display edge array to the final bit vector. */
   if (!subdiv_context.subdiv_display_edges.is_empty()) {
-    const Span<bool> span = subdiv_context.subdiv_display_edges;
-    BitVector<> &bit_vector = result->runtime->subsurf_optimal_display_edges;
-    bit_vector.clear();
-    bit_vector.resize(subdiv_context.subdiv_display_edges.size());
-    threading::parallel_for_aligned(
-        span.index_range(), 4096, bits::BitsPerInt, [&](const IndexRange range) {
-          for (const int i : range) {
-            bit_vector[i].set(span[i]);
-          }
-        });
+    result->runtime->subsurf_optimal_display_edges = BitVector<>(
+        subdiv_context.subdiv_display_edges);
   }
 
   if (coarse_mesh->verts_no_face().count == 0) {
