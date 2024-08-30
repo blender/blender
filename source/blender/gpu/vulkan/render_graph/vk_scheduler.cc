e8e89b751dbf8ef77170cd8186d59b3b13209c49
Jeroen Bakker <jeroen@blender.org>
2024-08-30 13:24:57 +0200

    Vulkan: Fix incorrect reordering when copying attachment
    Solved by not supporting complex reordering at this moment. It
    is currently better to focus on quality and add back performance
    later. During tests I didn't detect any user noticeable performance
    degradation. Could also be because we support rendering
    suspending/resuming.
    
    Fixes artifacts in EEVEE volumes and raytracing.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/126974

--- source/blender/gpu/vulkan/render_graph/vk_scheduler.cc
+++ source/blender/gpu/vulkan/render_graph/vk_scheduler.cc
@@ -89,10 +89,6 @@ std::optional<std::pair<int64_t, int64_t>> VKScheduler::find_rendering_scope(
 void VKScheduler::move_transfer_and_dispatch_outside_rendering_scope(
     const VKRenderGraph &render_graph)
 {
-  Vector<NodeHandle> pre_rendering_handles;
-  Vector<NodeHandle> rendering_handles;
-  Set<ResourceHandle> accessed_resources;
-
   foreach_rendering_scope(render_graph, [&](int64_t start_index, int64_t end_index) {
     /* Move end_rendering right after the last graphics node. */
     for (int index = end_index - 1; index >= start_index; index--) {
@@ -114,60 +110,6 @@ void VKScheduler::move_transfer_and_dispatch_outside_rendering_scope(
       }
       std::swap(result_[start_index], result_[index]);
       start_index += 1;
-    }
-
-    /* Move all other none graphics commands to before the begin_rendering unless the resource they
-     * are modifying is already being used by a draw command. Uses temporary allocated vectors to
-     * reduce the amount and size of copying data. */
-    pre_rendering_handles.clear();
-    rendering_handles.clear();
-    accessed_resources.clear();
-
-    for (int index = start_index + 1; index < end_index; index++) {
-      NodeHandle node_handle = result_[index];
-      const VKRenderGraphNode &node = render_graph.nodes_[node_handle];
-      if (pre_rendering_handles.is_empty()) {
-        if (!node_type_is_rendering(node.type)) {
-          rendering_handles.extend(&result_[start_index], index - start_index);
-          pre_rendering_handles.append(node_handle);
-        }
-      }
-      else {
-        const VKRenderGraphNodeLinks &node_links = render_graph.links_[node_handle];
-        if (node_type_is_rendering(node.type)) {
-          rendering_handles.append(node_handle);
-          for (const VKRenderGraphLink &input : node_links.inputs) {
-            accessed_resources.add(input.resource.handle);
-          }
-        }
-        else {
-          bool prepend = true;
-          for (const VKRenderGraphLink &output : node_links.outputs) {
-            if (accessed_resources.contains(output.resource.handle)) {
-              accessed_resources.remove(output.resource.handle);
-              prepend = false;
-            }
-          }
-          if (prepend) {
-            pre_rendering_handles.append(node_handle);
-          }
-          else {
-            // This adds a none rendering node into a rendering scope.
-            // later on the rendering will be suspended when the commands
-            // for these nodes are build.
-            rendering_handles.append(node_handle);
-          }
-        }
-      }
-    }
-    if (!pre_rendering_handles.is_empty()) {
-      MutableSpan<NodeHandle> store_none_rendering = result_.as_mutable_span().slice(
-          start_index, pre_rendering_handles.size());
-      MutableSpan<NodeHandle> store_rendering = result_.as_mutable_span().slice(
-          start_index + pre_rendering_handles.size(), rendering_handles.size());
-      store_none_rendering.copy_from(pre_rendering_handles);
-      store_rendering.copy_from(rendering_handles);
-      start_index += pre_rendering_handles.size();
     }
   });
 }
