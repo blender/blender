6191eb1e9df0637b22db805de6b97db40321b5e7
Julien Duroure <julien.duroure@gmail.com>
2024-08-30 06:23:11 +0200

    glTF exporter: Fix custom prop when apply modifier
    Since 4.2, props are Statically Typed
    so no need to copy them in that case, because overwriting them will crash

--- scripts/addons_core/io_scene_gltf2/blender/exp/nodes.py
+++ scripts/addons_core/io_scene_gltf2/blender/exp/nodes.py
@@ -297,8 +297,16 @@ def __gather_mesh(vnode, blender_object, export_settings):
                 depsgraph = bpy.context.evaluated_depsgraph_get()
                 blender_mesh_owner = blender_object.evaluated_get(depsgraph)
                 blender_mesh = blender_mesh_owner.to_mesh(preserve_all_data_layers=True, depsgraph=depsgraph)
-                for prop in [p for p in blender_object.data.keys() if p not in BLACK_LIST]:
-                    blender_mesh[prop] = blender_object.data[prop]
+                # Seems now (from 4.2) the custom properties are Statically Typed
+                # so no need to copy them in that case, because overwriting them will crash
+                if len(blender_mesh.keys()) == 0:
+                    # Copy custom properties
+                    for prop in [p for p in blender_object.data.keys() if p not in BLACK_LIST]:
+                        blender_mesh[prop] = blender_object.data[prop]
+                else:
+                    # But we need to remove some properties that are not needed
+                    for prop in [p for p in blender_object.data.keys() if p in BLACK_LIST]:
+                        del blender_mesh[prop]
 
                 if export_settings['gltf_skins']:
                     # restore Armature modifiers
