d460fbae600050cb2608b84f0156f90e7ef14ba4
Campbell Barton <campbell@blender.org>
2024-09-03 20:39:09 +1000

    Fix #120861: BMFace.copy_from_face_interp looses reference
    Remove call to BM_elem_attrs_copy in face interpolation function.
    This was copying custom-data which cleared the faces CD_BM_ELEM_PYPTR,
    making the Python objects behave as if it was removed.
    
    While the issue could also be fixed by skipping the PYPTR layer,
    copying attributes such as the material & normal in a face-data
    interpolation function isn't correct. In cases where this is needed,
    it's better the caller handles copying attributes.

--- source/blender/bmesh/intern/bmesh_interp.c
+++ source/blender/bmesh/intern/bmesh_interp.c
@@ -150,10 +150,6 @@ void BM_face_interp_from_face_ex(BMesh *bm,
   float *w = BLI_array_alloca(w, f_src->len);
   float co[2];
 
-  if (f_src != f_dst) {
-    BM_elem_attrs_copy(bm, bm, f_src, f_dst);
-  }
-
   /* interpolate */
   l_iter = l_first = BM_FACE_FIRST_LOOP(f_dst);
   do {
