23f2db9eedfe32cc0674924f61283ac0572a01ee
Richard Antalik <richardantalik@gmail.com>
2024-09-04 08:40:01 +0200

    FFmpeg: Simplify code for determining video frame count
    Original code was quite unorganized and not as easy to read through.
    
    There were basically 3 code paths:
    1. Division of `AVStream` duration by its timebase
    2. Division of `AVFormatContext` duration by its timebase, with possible
       AV offset compensation
    3. Simply using frame count from `AVStream`
    
    Finally there was possibility of ending up with duration of 0 in
    specific case where `pFormatCtx->duration == AV_NOPTS_VALUE`, but I did
    not find any test case for this (Added in aebb32748e).
    
    During investigation for PR #126866, I have concluded, that before
    commit 8903205dd9, the code which compared duration of stream and
    container was incorrectly always true. But it resulted in correct
    behavior for about 4 years. Code was reorganized with that assumption,
    so above listed code paths are in order of possible execution. Exception
    is code path 3, which is run first, but it's result is pretty much always
    discarded.
    
    Since all these workarounds are added around code path 3, it is safe
    to assume, that it is least precise, so it should be considered last.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/127010

--- source/blender/imbuf/intern/anim_movie.cc
+++ source/blender/imbuf/intern/anim_movie.cc
@@ -163,15 +163,85 @@ void IMB_suffix_anim(ImBufAnim *anim, const char *suffix)
 
 #ifdef WITH_FFMPEG
 
+static double ffmpeg_stream_start_time_get(AVStream *stream)
+{
+  if (stream->start_time == AV_NOPTS_VALUE) {
+    return 0.0;
+  }
+
+  return stream->start_time * av_q2d(stream->time_base);
+}
+
+static int ffmpeg_container_frame_count_get(AVFormatContext *pFormatCtx, AVStream *video_stream)
+{
+  /* Find audio stream to guess the duration of the video.
+   * Sometimes the audio AND the video stream have a start offset.
+   * The difference between these is the offset we want to use to
+   * calculate the video duration.
+   */
+  const double video_start = ffmpeg_stream_start_time_get(video_stream);
+  double audio_start = 0;
+
+  for (int i = 0; i < pFormatCtx->nb_streams; i++) {
+    if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+      AVStream *audio_stream = pFormatCtx->streams[i];
+      audio_start = ffmpeg_stream_start_time_get(audio_stream);
+      break;
+    }
+  }
+
+  const AVRational frame_rate = av_guess_frame_rate(pFormatCtx, video_stream, nullptr);
+  double stream_dur;
+
+  if (video_start > audio_start) {
+    stream_dur = double(pFormatCtx->duration) / AV_TIME_BASE - (video_start - audio_start);
+  }
+  else {
+    /* The video stream starts before or at the same time as the audio stream!
+     * We have to assume that the video stream is as long as the full pFormatCtx->duration.
+     */
+    stream_dur = double(pFormatCtx->duration) / AV_TIME_BASE;
+  }
+
+  return lround(stream_dur * av_q2d(frame_rate));
+}
+
+static int ffmpeg_frame_count_get(AVFormatContext *pFormatCtx, AVStream *video_stream)
+{
+  /* Use stream duration to determine frame count. */
+  if (video_stream->duration != AV_NOPTS_VALUE) {
+    const AVRational frame_rate = av_guess_frame_rate(pFormatCtx, video_stream, nullptr);
+    const double stream_dur = video_stream->duration * av_q2d(video_stream->time_base);
+    return lround(stream_dur * av_q2d(frame_rate));
+  }
+
+  /* Fall back to manually estimating the video stream duration.
+   * This is because the video stream duration can be shorter than the `pFormatCtx->duration`.
+   */
+  if (pFormatCtx->duration != AV_NOPTS_VALUE) {
+    return ffmpeg_container_frame_count_get(pFormatCtx, video_stream);
+  }
+
+  /* Read frame count from the stream if we can. Note, that this value can not be trusted. */
+  if (video_stream->nb_frames != 0) {
+    return video_stream->nb_frames;
+  }
+
+  /* The duration has not been set, happens for single JPEG2000 images.
+   * NOTE: Leave the duration zeroed, although it could set to 1 so the file is recognized
+   * as a movie with 1 frame, leave as-is since image loading code-paths are preferred
+   * in this case. The following assertion should be valid in this case. */
+  BLI_assert(pFormatCtx->duration == AV_NOPTS_VALUE);
+  return 0;
+}
+
 static int startffmpeg(ImBufAnim *anim)
 {
-  int i, video_stream_index;
-
   const AVCodec *pCodec;
   AVFormatContext *pFormatCtx = nullptr;
   AVCodecContext *pCodecCtx;
-  AVRational frame_rate;
   AVStream *video_stream;
+  int video_stream_index;
   int frs_num;
   double frs_den;
   int streamcount;
@@ -201,7 +271,7 @@ static int startffmpeg(ImBufAnim *anim)
   /* Find the video stream */
   video_stream_index = -1;
 
-  for (i = 0; i < pFormatCtx->nb_streams; i++) {
+  for (int i = 0; i < pFormatCtx->nb_streams; i++) {
     if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
       if (streamcount > 0) {
         streamcount--;
@@ -254,82 +324,7 @@ static int startffmpeg(ImBufAnim *anim)
     return -1;
   }
 
-  double video_start = 0;
-  double pts_time_base = av_q2d(video_stream->time_base);
-
-  if (video_stream->start_time != AV_NOPTS_VALUE) {
-    video_start = video_stream->start_time * pts_time_base;
-  }
-
-  frame_rate = av_guess_frame_rate(pFormatCtx, video_stream, nullptr);
-  anim->duration_in_frames = 0;
-
-  /* Take from the stream if we can. */
-  if (video_stream->nb_frames != 0) {
-    anim->duration_in_frames = video_stream->nb_frames;
-
-    /* Sanity check on the detected duration. This is to work around corruption like reported in
-     * #68091. */
-    if (frame_rate.den != 0 && pFormatCtx->duration > 0) {
-      double stream_sec = anim->duration_in_frames / av_q2d(frame_rate);
-      double container_sec = pFormatCtx->duration / double(AV_TIME_BASE);
-      if (blender::math::abs(stream_sec - container_sec) > container_sec / 3.0) {
-        /* The stream duration is significantly different than the container duration, which is
-         * suspicious. */
-        anim->duration_in_frames = 0;
-      }
-    }
-  }
-
-  if (anim->duration_in_frames != 0) {
-    /* Pass (already valid). */
-  }
-  else if (pFormatCtx->duration == AV_NOPTS_VALUE) {
-    /* The duration has not been set, happens for single JPEG2000 images.
-     * NOTE: Leave the duration zeroed, although it could set to 1 so the file is recognized
-     * as a movie with 1 frame, leave as-is since image loading code-paths are preferred
-     * in this case. */
-  }
-  else {
-    /* Fall back to manually estimating the video stream duration.
-     * This is because the video stream duration can be shorter than the `pFormatCtx->duration`. */
-    BLI_assert(anim->duration_in_frames == 0);
-    double stream_dur;
-
-    if (video_stream->duration != AV_NOPTS_VALUE) {
-      stream_dur = video_stream->duration * pts_time_base;
-    }
-    else {
-      double audio_start = 0;
-
-      /* Find audio stream to guess the duration of the video.
-       * Sometimes the audio AND the video stream have a start offset.
-       * The difference between these is the offset we want to use to
-       * calculate the video duration.
-       */
-      for (i = 0; i < pFormatCtx->nb_streams; i++) {
-        if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-          AVStream *audio_stream = pFormatCtx->streams[i];
-          if (audio_stream->start_time != AV_NOPTS_VALUE) {
-            audio_start = audio_stream->start_time * av_q2d(audio_stream->time_base);
-          }
-          break;
-        }
-      }
-
-      if (video_start > audio_start) {
-        stream_dur = double(pFormatCtx->duration) / AV_TIME_BASE - (video_start - audio_start);
-      }
-      else {
-        /* The video stream starts before or at the same time as the audio stream!
-         * We have to assume that the video stream is as long as the full pFormatCtx->duration.
-         */
-        stream_dur = double(pFormatCtx->duration) / AV_TIME_BASE;
-      }
-    }
-    anim->duration_in_frames = int(stream_dur * av_q2d(frame_rate) + 0.5f);
-  }
-
+  const AVRational frame_rate = av_guess_frame_rate(pFormatCtx, video_stream, nullptr);
   frs_num = frame_rate.num;
   frs_den = frame_rate.den;
 
@@ -344,7 +339,8 @@ static int startffmpeg(ImBufAnim *anim)
   anim->frs_sec_base = frs_den;
   /* Save the relative start time for the video. IE the start time in relation to where playback
    * starts. */
-  anim->start_offset = video_start;
+  anim->start_offset = ffmpeg_stream_start_time_get(video_stream);
+  anim->duration_in_frames = ffmpeg_frame_count_get(pFormatCtx, video_stream);
 
   anim->x = pCodecCtx->width;
   anim->y = pCodecCtx->height;
