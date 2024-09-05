ba17f6fb1483412ba96731828b91936eaf7989e6
Thomas Dinges <thomas@blender.org>
2024-09-05 09:54:19 +0200

    Tools: Update issues script after module rename.

--- tools/triage/issues_module_listing.py
+++ tools/triage/issues_module_listing.py
@@ -29,7 +29,6 @@ class ModuleInfo:
 modules = {
     "Module/Animation & Rigging": ModuleInfo(name="Animation & Rigging", labelid="268"),
     "Module/Core": ModuleInfo(name="Core", labelid="269"),
-    "Module/EEVEE & Viewport": ModuleInfo(name="EEVEE & Viewport", labelid="272"),
     "Module/Grease Pencil": ModuleInfo(name="Grease Pencil", labelid="273"),
     "Module/Modeling": ModuleInfo(name="Modeling", labelid="274"),
     "Module/Nodes & Physics": ModuleInfo(name="Nodes & Physics", labelid="275"),
@@ -40,6 +39,7 @@ modules = {
     "Module/Sculpt, Paint & Texture": ModuleInfo(name="Sculpt, Paint & Texture", labelid="281"),
     "Module/User Interface": ModuleInfo(name="User Interface", labelid="283"),
     "Module/VFX & Video": ModuleInfo(name="VFX & Video", labelid="284"),
+    "Module/Viewport & EEVEE": ModuleInfo(name="Viewport & EEVEE", labelid="272"),
 }
 
 base_url = (
