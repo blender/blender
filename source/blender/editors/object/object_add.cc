06e70e7bc07d87b3f3cc31cf7fb7b09129f7cf15
YimingWu <xp8110@outlook.com>
2024-09-10 13:36:31 +0200

    GPv3: Implement Grease Pencil to mesh conversion
    Support Object > Convert > Mesh option for grease pencil v3.
    This does the conversion by first converting it into Curves then use the
    same routine to convert Curves into Mesh wires.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/123835

--- source/blender/editors/object/object_add.cc
+++ source/blender/editors/object/object_add.cc
@@ -37,7 +37,9 @@
 #include "BLI_ghash.h"
 #include "BLI_listbase.h"
 #include "BLI_math_matrix.h"
+#include "BLI_math_matrix_types.hh"
 #include "BLI_math_rotation.h"
+#include "BLI_math_vector_types.hh"
 #include "BLI_string.h"
 #include "BLI_string_utf8.h"
 #include "BLI_utildefines.h"
@@ -55,6 +57,7 @@
 #include "BKE_curve.hh"
 #include "BKE_curve_to_mesh.hh"
 #include "BKE_curves.h"
+#include "BKE_curves.hh"
 #include "BKE_customdata.hh"
 #include "BKE_displist.h"
 #include "BKE_duplilist.hh"
@@ -3391,10 +3394,112 @@ static int object_convert_exec(bContext *C, wmOperator *op)
         else if (const GreasePencil *grease_pencil = geometry.get_grease_pencil()) {
           const Vector<ed::greasepencil::DrawingInfo> drawings =
               ed::greasepencil::retrieve_visible_drawings(*scene, *grease_pencil, false);
+          if (drawings.size() > 0) {
+            Array<bke::GeometrySet> geometries(drawings.size());
+            for (const int i : drawings.index_range()) {
+              Curves *curves_id = static_cast<Curves *>(BKE_id_new_nomain(ID_CV, nullptr));
+              curves_id->geometry.wrap() = drawings[i].drawing.strokes();
+              geometries[i] = bke::GeometrySet::from_curves(curves_id);
+            }
+            bke::GeometrySet joined_curves = geometry::join_geometries(geometries, {});
+
+            new_curves->geometry.wrap() = joined_curves.get_curves()->geometry.wrap();
+            new_curves->geometry.wrap().tag_topology_changed();
+            BKE_object_material_from_eval_data(bmain, newob, &joined_curves.get_curves()->id);
+          }
+        }
+
+        BKE_object_free_derived_caches(newob);
+        BKE_object_free_modifiers(newob, 0);
+      }
+      else {
+        BKE_reportf(op->reports,
+                    RPT_WARNING,
+                    "Object '%s' has no evaluated grease pencil data",
+                    ob->id.name + 2);
+      }
+    }
+    else if (ob->type == OB_GREASE_PENCIL && target == OB_MESH) {
+      /* Mostly same as converting to OB_CURVES, the mesh will be converted from Curves afterwards
+       * . */
+
+      ob->flag |= OB_DONE;
+
+      Object *ob_eval = DEG_get_evaluated_object(depsgraph, ob);
+      bke::GeometrySet geometry;
+      if (ob_eval->runtime->geometry_set_eval != nullptr) {
+        geometry = *ob_eval->runtime->geometry_set_eval;
+      }
+
+      if (geometry.has_curves()) {
+        if (keep_original) {
+          basen = duplibase_for_convert(bmain, depsgraph, scene, view_layer, base, nullptr);
+          newob = basen->object;
+
+          /* Decrement original curve's usage count. */
+          Curve *legacy_curve = static_cast<Curve *>(newob->data);
+          id_us_min(&legacy_curve->id);
+
+          /* Make a copy of the curve. */
+          newob->data = BKE_id_copy(bmain, &legacy_curve->id);
+        }
+        else {
+          newob = ob;
+        }
+
+        const Curves *curves_eval = geometry.get_curves();
+        Curves *new_curves = static_cast<Curves *>(BKE_id_new(bmain, ID_CV, newob->id.name + 2));
+
+        newob->data = new_curves;
+        newob->type = OB_CURVES;
+
+        new_curves->geometry.wrap() = curves_eval->geometry.wrap();
+        BKE_object_material_from_eval_data(bmain, newob, &curves_eval->id);
+
+        BKE_object_free_derived_caches(newob);
+        BKE_object_free_modifiers(newob, 0);
+      }
+      else if (geometry.has_grease_pencil()) {
+        if (keep_original) {
+          basen = duplibase_for_convert(bmain, depsgraph, scene, view_layer, base, nullptr);
+          newob = basen->object;
+
+          /* Decrement original curve's usage count. */
+          Curve *legacy_curve = static_cast<Curve *>(newob->data);
+          id_us_min(&legacy_curve->id);
+
+          /* Make a copy of the curve. */
+          newob->data = BKE_id_copy(bmain, &legacy_curve->id);
+        }
+        else {
+          newob = ob;
+        }
+
+        /* Do not link `new_curves` to `bmain` since it's temporary. */
+        Curves *new_curves = static_cast<Curves *>(BKE_id_new_nomain(ID_CV, newob->id.name + 2));
+
+        newob->data = new_curves;
+        newob->type = OB_CURVES;
+
+        if (const Curves *curves_eval = geometry.get_curves()) {
+          new_curves->geometry.wrap() = curves_eval->geometry.wrap();
+          BKE_object_material_from_eval_data(bmain, newob, &curves_eval->id);
+        }
+        else if (const GreasePencil *grease_pencil = geometry.get_grease_pencil()) {
+          const Vector<ed::greasepencil::DrawingInfo> drawings =
+              ed::greasepencil::retrieve_visible_drawings(*scene, *grease_pencil, false);
           Array<bke::GeometrySet> geometries(drawings.size());
           for (const int i : drawings.index_range()) {
             Curves *curves_id = static_cast<Curves *>(BKE_id_new_nomain(ID_CV, nullptr));
             curves_id->geometry.wrap() = drawings[i].drawing.strokes();
+            const int layer_index = drawings[i].layer_index;
+            const bke::greasepencil::Layer *layer = grease_pencil->layers()[layer_index];
+            blender::float4x4 to_object = layer->to_object_space(*ob);
+            bke::CurvesGeometry &new_curves = curves_id->geometry.wrap();
+            MutableSpan<blender::float3> positions = new_curves.positions_for_write();
+            for (const int point_i : new_curves.points_range()) {
+              positions[point_i] = blender::math::transform_point(to_object, positions[point_i]);
+            }
             geometries[i] = bke::GeometrySet::from_curves(curves_id);
           }
           if (geometries.size() > 0) {
@@ -3405,6 +3510,20 @@ static int object_convert_exec(bContext *C, wmOperator *op)
             BKE_object_material_from_eval_data(bmain, newob, &joined_curves.get_curves()->id);
           }
         }
+
+        Mesh *new_mesh = static_cast<Mesh *>(BKE_id_new(bmain, ID_ME, newob->id.name + 2));
+        newob->data = new_mesh;
+        newob->type = OB_MESH;
+
+        Mesh *mesh = bke::curve_to_wire_mesh(new_curves->geometry.wrap(), {});
+        if (!mesh) {
+          mesh = BKE_mesh_new_nomain(0, 0, 0, 0);
+        }
+        BKE_mesh_nomain_to_mesh(mesh, new_mesh, newob);
+        BKE_object_material_from_eval_data(bmain, newob, &new_curves->id);
+
+        /* Free `new_curves` because it is just an intermediate. */
+        BKE_id_free(nullptr, new_curves);
 
         BKE_object_free_derived_caches(newob);
         BKE_object_free_modifiers(newob, 0);
