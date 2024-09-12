8f86a04c7220c1f52b200d730dc63931efa04892
Falk David <falk@blender.org>
2024-09-12 13:39:28 +0200

    Fix: GPv3: Missing context mode case in overlay options panel
    The vertex paint mode options was missing in this panel.

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -8256,6 +8256,7 @@ class VIEW3D_PT_overlay_grease_pencil_options(Panel):
             'WEIGHT_GREASE_PENCIL': iface_("Weight Grease Pencil"),
             'OBJECT': iface_("Grease Pencil"),
             'SCULPT_GREASE_PENCIL': iface_("Sculpt Grease Pencil"),
+            'VERTEX_GREASE_PENCIL': iface_("Vertex Grease Pencil"),
         }[context.mode], translate=False)
 
         layout.prop(overlay, "use_gpencil_onion_skin", text="Onion Skin")
