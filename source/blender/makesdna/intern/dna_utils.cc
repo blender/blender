zyxcambridge/blender_llm

Fix crash reading files from v2.66
Old files can contain names without any name part (`*` in this case).

Resolve the buffer overflow by checking for nil before the name begins.

source/blender/makesdna/intern/dna_utils.cc
@@ -74,7 +74,11 @@ static bool is_identifier(const char c)
 uint DNA_member_id_offset_start(const char *member_full)
 {
   uint elem_full_offset = 0;
-  while (!is_identifier(member_full[elem_full_offset])) {
+  /* NOTE(@ideasman42): checking nil is needed for invalid names such as `*`,
+   * these were written by older versions of Blender (v2.66).
+   * In this case the "name" part will be an empty string.
+   * The member cannot be used, this just prevents a crash. */
+  while (!is_identifier(member_full[elem_full_offset]) && member_full[elem_full_offset]) {
     elem_full_offset++;
   }
   return elem_full_offset;

blender/blender

source/blender/makesdna/intern/dna_utils.cc
@@ -74,7 +74,11 @@ static bool is_identifier(const char c)
 uint DNA_elem_id_offset_start(const char *elem_full)
 {
   uint elem_full_offset = 0;
-  while (!is_identifier(elem_full[elem_full_offset])) {
+  /* NOTE(@ideasman42): checking nil is needed for invalid names such as `*`,
+   * these were written by older versions of Blender (v2.66).
+   * In this case the "name" part will be an empty string.
+   * The member cannot be used, this just prevents a crash. */
+  while (!is_identifier(elem_full[elem_full_offset]) && elem_full[elem_full_offset]) {
     elem_full_offset++;
   }
   return elem_full_offset;