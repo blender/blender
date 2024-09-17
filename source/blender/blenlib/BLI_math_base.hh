dceccfda5ac6ab7f1237f0b60e0a33344778be11
Iliya Katushenock <modormoder@gmail.com>
2024-09-17 15:48:06 +0200

    BLI: support mod_periodic on integer types
    See #125876 for more details.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/125876

--- source/blender/blenlib/BLI_math_base.hh
+++ source/blender/blenlib/BLI_math_base.hh
@@ -103,18 +103,18 @@ template<typename T> inline T round(const T &a)
 
 /**
  * Repeats the saw-tooth pattern even on negative numbers.
- * ex: `mod_periodic(-3, 4) = 1`, `mod(-3, 4)= -3`
+ * ex: `mod_periodic(-3, 4) = 1`, `mod(-3, 4)= -3`. This will cause undefined behavior for negative
+ * b.
  */
 template<typename T> inline T mod_periodic(const T &a, const T &b)
 {
+  BLI_assert(b > 0);
+  if constexpr (std::is_integral_v<T>) {
+    BLI_assert(std::numeric_limits<T>::max() - math::abs(a) >= b);
+    return ((a % b) + b) % b;
+  }
+
   return a - (b * math::floor(a / b));
-}
-template<> inline int64_t mod_periodic(const int64_t &a, const int64_t &b)
-{
-  int64_t c = (a >= 0) ? a : (-1 - a);
-  int64_t tmp = c - (b * (c / b));
-  /* Negative integers have different rounding that do not match floor(). */
-  return (a >= 0) ? tmp : (b - 1 - tmp);
 }
 
 template<typename T> inline T ceil(const T &a)
