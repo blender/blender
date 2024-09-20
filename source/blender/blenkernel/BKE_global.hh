09e459589f2862405e0d648f28e62b1f0bdd62da
Campbell Barton <campbell@blender.org>
2024-09-20 19:52:01 +1000

    Cleanup: add missing struct qualifier

--- source/blender/blenkernel/BKE_global.hh
+++ source/blender/blenkernel/BKE_global.hh
@@ -31,13 +31,13 @@ struct Global {
    * Note that `CTX_data_main(C)` should be used where possible.
    * Otherwise access via #G_MAIN.
    */
-  Main *main;
+  struct Main *main;
 
   /**
    * Preview main is stored to avoid loading the preview file in multiple scenarios.
    * It is actually shared between shader node previews and asset previews.
    */
-  Main *pr_main;
+  struct Main *pr_main;
 
   /** Last saved location for images. */
   char filepath_last_image[/*FILE_MAX*/ 1024];
