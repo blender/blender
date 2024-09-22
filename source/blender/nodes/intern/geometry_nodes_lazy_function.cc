9239c00edab96bb791e2cc3edfd41916c391e65c
Jacques Lucke <jacques@blender.org>
2024-09-22 22:52:25 +0200

    Geometry Nodes: support showing viewer attribute for grease pencil in spreadsheet

--- source/blender/nodes/intern/geometry_nodes_lazy_function.cc
+++ source/blender/nodes/intern/geometry_nodes_lazy_function.cc
@@ -794,9 +794,11 @@ class LazyFunctionForViewerNode : public LazyFunction {
       }
       else {
         geometry.modify_geometry_sets([&](GeometrySet &geometry) {
-          for (const bke::GeometryComponent::Type type : {bke::GeometryComponent::Type::Mesh,
-                                                          bke::GeometryComponent::Type::PointCloud,
-                                                          bke::GeometryComponent::Type::Curve})
+          for (const bke::GeometryComponent::Type type :
+               {bke::GeometryComponent::Type::Mesh,
+                bke::GeometryComponent::Type::PointCloud,
+                bke::GeometryComponent::Type::Curve,
+                bke::GeometryComponent::Type::GreasePencil})
           {
             if (geometry.has(type)) {
               GeometryComponent &component = geometry.get_component_for_write(type);
