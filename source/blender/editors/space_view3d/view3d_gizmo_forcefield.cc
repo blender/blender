1fbc32b609b83e8fb92d53a3e5d990b9a5ab4f0c
Campbell Barton <ideasman42@gmail.com>
2024-09-23 10:26:46 +0200

    4.2: Gizmo undo (back-ported fixes)
    Back port:
    
    - 46cd88f229c90d87d7528ad3e0169ec5229c6dfb
    - d254a0886edfe757f0233e2bd9753421c82a0bcb
    - 5387605833e3f7ddf07c2bdb7bc01860290a3f58
    - 24dc9a21b1fee24c87442f1a15598d34c9301507 (undo support)
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/127548

--- source/blender/editors/space_view3d/view3d_gizmo_forcefield.cc
+++ source/blender/editors/space_view3d/view3d_gizmo_forcefield.cc
@@ -70,6 +70,11 @@ static void WIDGETGROUP_forcefield_setup(const bContext * /*C*/, wmGizmoGroup *g
 
   UI_GetThemeColor3fv(TH_GIZMO_PRIMARY, gz->color);
   UI_GetThemeColor3fv(TH_GIZMO_HI, gz->color_hi);
+
+  /* All gizmos must perform undo. */
+  LISTBASE_FOREACH (wmGizmo *, gz, &gzgroup->gizmos) {
+    WM_gizmo_set_flag(gz, WM_GIZMO_NEEDS_UNDO, true);
+  }
 }
 
 static void WIDGETGROUP_forcefield_refresh(const bContext *C, wmGizmoGroup *gzgroup)
