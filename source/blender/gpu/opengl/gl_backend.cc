f4c8845b6082468c3e05f2320e740470c1f20034
Campbell Barton <campbell@blender.org>
2024-09-23 10:34:04 +1000

    Cleanup: remove duplicate check for AMD W6800 GPU
    This read like a typo (which missed W6700) however the GPU
    wasn't released. Remove the duplicate check and add a note.
    
    Ref !127993

--- source/blender/gpu/opengl/gl_backend.cc
+++ source/blender/gpu/opengl/gl_backend.cc
@@ -513,7 +513,8 @@ static void detect_workarounds()
         strstr(renderer, "RX 6750") || strstr(renderer, "RX 6800") ||
         strstr(renderer, "RX 6850") || strstr(renderer, "RX 6900") ||
         strstr(renderer, "RX 6950") || strstr(renderer, "W6300") || strstr(renderer, "W6400") ||
-        strstr(renderer, "W6500") || strstr(renderer, "W6600") || strstr(renderer, "W6800") ||
+        strstr(renderer, "W6500") || strstr(renderer, "W6600") ||
+        /* NOTE: `W6700` was never released, so it's not in this list. */
         strstr(renderer, "W6800") || strstr(renderer, "W6900"))
     {
       GLContext::layered_rendering_support = false;
