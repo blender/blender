c852f7402ed52a3b7ac91b7551f08ef15d52e469
Campbell Barton <campbell@blender.org>
2024-09-30 13:18:36 +1000

    Fix #128226: Memory leak selecting meshes in multi-object edit-mode
    The meshes custom data-mask edit-mode check only considered the object
    in edit-mode if it was the active object. This isn't correct as multiple
    objects can be in edit-mode at once.
    
    The potential for `mesh_get_eval_deform` to leak memory remains,
    this just avoids an unnecessary mesh re-generation on selection
    (which was causing the leak), see: !128228 for details.

--- source/blender/blenkernel/intern/mesh_data_update.cc
+++ source/blender/blenkernel/intern/mesh_data_update.cc
@@ -1134,7 +1134,10 @@ static void object_get_datamask(const Depsgraph &depsgraph,
     if (ob.mode & OB_MODE_WEIGHT_PAINT) {
       r_mask.vmask |= CD_MASK_MDEFORMVERT;
     }
+  }
 
+  /* Multiple objects can be in edit-mode at once. */
+  if (actob && (actob->mode & OB_MODE_EDIT)) {
     if (ob.mode & OB_MODE_EDIT) {
       r_mask.vmask |= CD_MASK_MVERT_SKIN;
     }
