4c47c7e634e286ae569b40ce6e387b7ae0a6b9c4
Thomas Dinges <thomas@blender.org>
2024-10-02 12:35:26 +0200

    Revert "Release cycle: Blender 4.3 Beta"
    This reverts commit 906bc801f44d56c61f9e4c4d95dc9d80cfa2cda8.

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -25,7 +25,7 @@ extern "C" {
 /** Blender patch version for bug-fix releases. */
 #define BLENDER_VERSION_PATCH 0
 /** Blender release cycle stage: alpha/beta/rc/release. */
-#define BLENDER_VERSION_CYCLE beta
+#define BLENDER_VERSION_CYCLE alpha
 /** Blender release type suffix. LTS or blank. */
 #define BLENDER_VERSION_SUFFIX
 
