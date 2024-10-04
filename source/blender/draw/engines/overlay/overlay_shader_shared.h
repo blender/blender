8165d52f90b347f11cdac199718b3924e3c6b576
Clément Foucault <foucault.clem@gmail.com>
2024-10-04 23:30:28 +0200

    Fix: Overlay: Broken shader compilation

--- source/blender/draw/engines/overlay/overlay_shader_shared.h
+++ source/blender/draw/engines/overlay/overlay_shader_shared.h
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#if !defined(DRAW_VIEW_CREATE_INFO) && !defined(GLSL_CPP_STUBS)
+#if !defined(GPU_SHADER) && !defined(GLSL_CPP_STUBS)
 #  pragma once
 
 #  include "GPU_shader_shared_utils.hh"
