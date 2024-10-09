5f7a6bf3c86594a72a169e9e9a3ee47c1f49eef5
Miguel Pozo <pragma37@gmail.com>
2024-10-09 16:21:00 +0200

    Update eevee_npr_info.hh

--- source/blender/draw/engines/eevee_next/shaders/infos/eevee_npr_info.hh
+++ source/blender/draw/engines/eevee_next/shaders/infos/eevee_npr_info.hh
@@ -6,38 +6,40 @@
 #include "gpu_shader_create_info.hh"
 
 GPU_SHADER_CREATE_INFO(npr_surface_common)
-    .sampler(INDEX_NPR_TX_SLOT, ImageType::UINT_2D, "npr_index_tx")
-    /* eevee_gbuffer_data */
-    .define("GBUFFER_LOAD")
-    .sampler(GBUF_NORMAL_NPR_TX_SLOT, ImageType::FLOAT_2D_ARRAY, "gbuf_normal_tx")
-    .sampler(GBUF_HEADER_NPR_TX_SLOT, ImageType::UINT_2D, "gbuf_header_tx")
-    .sampler(GBUF_CLOSURE_NPR_TX_SLOT, ImageType::FLOAT_2D_ARRAY, "gbuf_closure_tx")
-    /* eevee_gbuffer_data */
-    .sampler(RADIANCE_TX_SLOT, ImageType::FLOAT_2D, "radiance_tx")
-    /* eevee_deferred_combine */
-    .sampler(DIRECT_RADIANCE_NPR_TX_SLOT_1 + 0, ImageType::UINT_2D, "direct_radiance_1_tx")
-    .sampler(DIRECT_RADIANCE_NPR_TX_SLOT_1 + 1, ImageType::UINT_2D, "direct_radiance_2_tx")
-    .sampler(DIRECT_RADIANCE_NPR_TX_SLOT_1 + 2, ImageType::UINT_2D, "direct_radiance_3_tx")
-    .sampler(INDIRECT_RADIANCE_NPR_TX_SLOT_1 + 0, ImageType::FLOAT_2D, "indirect_radiance_1_tx")
-    .sampler(INDIRECT_RADIANCE_NPR_TX_SLOT_1 + 1, ImageType::FLOAT_2D, "indirect_radiance_2_tx")
-    .sampler(INDIRECT_RADIANCE_NPR_TX_SLOT_1 + 2, ImageType::FLOAT_2D, "indirect_radiance_3_tx")
-    .push_constant(Type::BOOL, "use_split_radiance")
-    /* eevee_deferred_combine */
-    .sampler(BACK_RADIANCE_TX_SLOT, ImageType::FLOAT_2D, "radiance_back_tx")
-    .sampler(BACK_HIZ_TX_SLOT, ImageType::FLOAT_2D, "hiz_back_tx")
-    .push_constant(Type::INT, "npr_index")
-    .define("NPR_SHADER")
-    .fragment_out(0, Type::VEC4, "out_radiance", DualBlend::SRC_0)
-    .fragment_out(0, Type::VEC4, "out_transmittance", DualBlend::SRC_1)
-    .additional_info("draw_view",
-                     "eevee_shared",
-                     "eevee_global_ubo",
-                     "eevee_light_data",
-                     "eevee_shadow_data",
-                     "eevee_utility_texture",
-                     "eevee_sampling_data",
-                     "eevee_hiz_data");
+SAMPLER(INDEX_NPR_TX_SLOT, UINT_2D, npr_index_tx)
+/* eevee_gbuffer_data */
+DEFINE("GBUFFER_LOAD")
+SAMPLER(GBUF_NORMAL_NPR_TX_SLOT, FLOAT_2D_ARRAY, gbuf_normal_tx)
+SAMPLER(GBUF_HEADER_NPR_TX_SLOT, UINT_2D, gbuf_header_tx)
+SAMPLER(GBUF_CLOSURE_NPR_TX_SLOT, FLOAT_2D_ARRAY, gbuf_closure_tx)
+/* eevee_gbuffer_data */
+SAMPLER(RADIANCE_TX_SLOT, FLOAT_2D, radiance_tx)
+/* eevee_deferred_combine */
+SAMPLER(DIRECT_RADIANCE_NPR_TX_SLOT_1 + 0, UINT_2D, direct_radiance_1_tx)
+SAMPLER(DIRECT_RADIANCE_NPR_TX_SLOT_1 + 1, UINT_2D, direct_radiance_2_tx)
+SAMPLER(DIRECT_RADIANCE_NPR_TX_SLOT_1 + 2, UINT_2D, direct_radiance_3_tx)
+SAMPLER(INDIRECT_RADIANCE_NPR_TX_SLOT_1 + 0, FLOAT_2D, indirect_radiance_1_tx)
+SAMPLER(INDIRECT_RADIANCE_NPR_TX_SLOT_1 + 1, FLOAT_2D, indirect_radiance_2_tx)
+SAMPLER(INDIRECT_RADIANCE_NPR_TX_SLOT_1 + 2, FLOAT_2D, indirect_radiance_3_tx)
+PUSH_CONSTANT(BOOL, use_split_radiance)
+/* eevee_deferred_combine */
+SAMPLER(BACK_RADIANCE_TX_SLOT, FLOAT_2D, radiance_back_tx)
+SAMPLER(BACK_HIZ_TX_SLOT, FLOAT_2D, hiz_back_tx)
+PUSH_CONSTANT(INT, npr_index)
+DEFINE("NPR_SHADER")
+FRAGMENT_OUT_DUAL(0, VEC4, out_radiance, SRC_0)
+FRAGMENT_OUT_DUAL(0, VEC4, out_transmittance, SRC_1)
+ADDITIONAL_INFO(draw_view)
+ADDITIONAL_INFO(eevee_shared)
+ADDITIONAL_INFO(eevee_global_ubo)
+ADDITIONAL_INFO(eevee_light_data)
+ADDITIONAL_INFO(eevee_shadow_data)
+ADDITIONAL_INFO(eevee_utility_texture)
+ADDITIONAL_INFO(eevee_sampling_data)
+ADDITIONAL_INFO(eevee_hiz_data)
+GPU_SHADER_CREATE_END()
 
 GPU_SHADER_CREATE_INFO(eevee_surf_npr)
-    .fragment_source("eevee_surf_deferred_npr_frag.glsl")
-    .additional_info("npr_surface_common");
+FRAGMENT_SOURCE("eevee_surf_deferred_npr_frag.glsl")
+ADDITIONAL_INFO(npr_surface_common)
+GPU_SHADER_CREATE_END()
