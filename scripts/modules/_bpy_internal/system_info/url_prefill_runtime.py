cd793b2c3f8e45f7fdf1b7c24ae3d0d63d2ed4fb
Miguel Pozo <pragma37@gmail.com>
2024-10-17 17:58:25 +0200

    Report npr-prototype bugs on a separate tracker
    Based on #129165 by @Alaska

--- scripts/modules/_bpy_internal/system_info/url_prefill_runtime.py
+++ scripts/modules/_bpy_internal/system_info/url_prefill_runtime.py
@@ -8,6 +8,29 @@ __all__ = (
     "url_from_blender",
 )
 
+# Use this to open reports of experimental branches out of the main tracker
+def custom_gitea_report(repo, template_path, query_params):
+    import urllib.parse
+
+    # A reconstruction of the expected data to go on the report
+    body_string = "**System Information**\n"
+    body_string += f"Operating System: {query_params['os']}\n"
+    body_string += f"Graphics card: {query_params['gpu']}\n\n"
+    body_string += "**Blender Version**\n"
+    body_string += f"Broken: {query_params['broken_version']}\n"
+    body_string += "Worked: (newest version of Blender that worked as expected)\n\n"
+    body_string += "**Short description of error**\n"
+    body_string += "[Please fill out a short description of the error here]\n\n"
+    body_string += "**Exact steps for others to reproduce the error**\n"
+    body_string += "[Please describe the exact steps needed to reproduce the issue]\n"
+    body_string += "[Based on the default startup or an attached .blend file (as simple as possible)]\n"
+
+    full_url = "https://projects.blender.org/"
+    full_url += f"{repo}/issues/new?template="
+    full_url += f"{urllib.parse.quote_plus(template_path)}&field:body="
+    full_url += f"{urllib.parse.quote_plus(body_string)}"
+
+    return full_url
 
 def url_from_blender(*, addon_info=None):
     import bpy
@@ -51,6 +74,10 @@ def url_from_blender(*, addon_info=None):
         addon_info_lines = addon_info.splitlines()
         query_params["addon_name"] = addon_info_lines[0].removeprefix("Name: ")
         query_params["addon_author"] = addon_info_lines[1].removeprefix("Author: ")
+    
+    if addon_info is None:
+        # Report npr-prototype bugs on a separate tracker.
+        return custom_gitea_report("pragma37/npr-tracker", ".gitea/issue_template/bug.yaml", query_params)
 
     query_str = urllib.parse.urlencode(query_params)
     return "https://redirect.blender.org/?" + query_str
