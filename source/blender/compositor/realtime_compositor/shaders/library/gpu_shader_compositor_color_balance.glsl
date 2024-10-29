c6d4c759eb3c5dfd2ba5988f7f68c9f1baec2bd1
Omar Emara <mail@OmarEmara.dev>
2024-10-29 10:44:20 +0300

    Compositor: Implement Color Balance node for new CPU compositor
    Reference #125968.

--- source/blender/compositor/realtime_compositor/shaders/library/gpu_shader_compositor_color_balance.glsl
+++ source/blender/compositor/realtime_compositor/shaders/library/gpu_shader_compositor_color_balance.glsl
@@ -15,22 +15,15 @@ void node_composite_color_balance_lgg(
   gain_balanced = max(gain_balanced, vec3(0.0));
 
   vec3 linear_color = srgb_to_linear_rgb(gain_balanced);
-  vec3 sanitized_gamma = mix(gamma, vec3(1e-6), equal(gamma, vec3(0.0)));
-  vec3 gamma_balanced = pow(linear_color, 1.0 / sanitized_gamma);
+  vec3 gamma_balanced = pow(linear_color, 1.0 / gamma);
 
   result = vec4(mix(color.rgb, gamma_balanced, min(factor, 1.0)), color.a);
 }
 
-void node_composite_color_balance_asc_cdl(float factor,
-                                          vec4 color,
-                                          vec3 offset,
-                                          vec3 power,
-                                          vec3 slope,
-                                          float offset_basis,
-                                          out vec4 result)
+void node_composite_color_balance_asc_cdl(
+    float factor, vec4 color, vec3 offset, vec3 power, vec3 slope, out vec4 result)
 {
-  vec3 full_offset = offset + offset_basis;
-  vec3 balanced = color.rgb * slope + full_offset;
+  vec3 balanced = color.rgb * slope + offset;
   balanced = pow(max(balanced, vec3(0.0)), power);
   result = vec4(mix(color.rgb, balanced, min(factor, 1.0)), color.a);
 }
