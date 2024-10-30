f86710b90fb52a2b7829436d8bbbade4b957fef7
Campbell Barton <campbell@blender.org>
2024-10-30 16:31:25 +1100

    Cleanup: pass multiple arguments to str.startswith

--- tools/debug/gdb/blender_gdb_extension.py
+++ tools/debug/gdb/blender_gdb_extension.py
@@ -531,8 +531,10 @@ class LazyFunctionEvalFilter:
     @staticmethod
     def frame_to_name(frame):
         function_name = frame.function()
-        if (function_name.startswith("blender::fn::lazy_function::LazyFunction::execute")
-                or function_name.startswith("blender::fn::lazy_function::Executor::push_to_task_pool")):
+        if function_name.startswith((
+                "blender::fn::lazy_function::LazyFunction::execute",
+                "blender::fn::lazy_function::Executor::push_to_task_pool",
+        )):
             return "Execute Lazy Function"
 
 
