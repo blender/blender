cf8c0f9703f7ffcd752ef90c0c1a67782eb289fd
Miguel Pozo <pragma37@gmail.com>
2024-11-01 20:21:21 +0100

    Populate the default npr node tree

--- source/blender/blenkernel/intern/npr.cc
+++ source/blender/blenkernel/intern/npr.cc
@@ -7,11 +7,30 @@
  */
 
 #include "BKE_node.hh"
+#include "BKE_node_tree_update.hh"
 #include "DNA_node_types.h"
 #include "NOD_shader.h"
 
 bNodeTree *BKE_npr_tree_add(Main *bmain, const char *name)
 {
   bNodeTree *ntree = blender::bke::node_tree_add_tree(bmain, name, ntreeType_Shader->idname);
+
+  bNode *input = blender::bke::node_add_static_node(nullptr, ntree, SH_NODE_NPR_INPUT);
+  bNode *output = blender::bke::node_add_static_node(nullptr, ntree, SH_NODE_NPR_OUTPUT);
+
+  /* Weird offsets, but these are the same as the default material node trees. */
+  input->locx = 10.0f;
+  input->locy = 300.0f;
+  output->locx = 300.0f;
+  output->locy = 300.0f;
+
+  blender::bke::node_add_link(ntree,
+                              input,
+                              blender::bke::node_find_socket(input, SOCK_OUT, "Combined Color"),
+                              output,
+                              blender::bke::node_find_socket(output, SOCK_IN, "Color"));
+
+  BKE_ntree_update_main_tree(bmain, ntree, nullptr);
+
   return ntree;
 }
