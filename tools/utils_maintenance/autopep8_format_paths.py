326b65f27c77f90a8ed4fd271b278eeded14e151
Campbell Barton <campbell@blender.org>
2024-11-06 10:49:44 +1100

    Tools: bump autopep8 min version

--- tools/utils_maintenance/autopep8_format_paths.py
+++ tools/utils_maintenance/autopep8_format_paths.py
@@ -27,8 +27,7 @@ from typing import (
     Optional,
 )
 
-# Temporary, until all platforms update to 2.3.1.
-VERSION_MIN = (1, 6, 0)
+VERSION_MIN = (2, 3, 1)
 VERSION_MAX_RECOMMENDED = (2, 3, 1)
 AUTOPEP8_FORMAT_CMD = "autopep8"
 AUTOPEP8_FORMAT_DEFAULT_ARGS = (
