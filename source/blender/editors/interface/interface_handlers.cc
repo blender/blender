0e0939b83fe83a4784e23d0fd65fbf49fe431783
Julian Eisel <julian@blender.org>
2024-11-07 12:57:28 +0100

    Fix #129112: Asset shelf ignores double-click keymap item
    Double clicks would be captured by view-item renaming, even when renaming
    wasn't supported.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/129919

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -5004,10 +5004,13 @@ static int ui_do_but_VIEW_ITEM(bContext *C,
            * example). */
           return WM_UI_HANDLER_CONTINUE;
         case KM_DBL_CLICK:
-          data->cancel = true;
-          UI_view_item_begin_rename(*view_item_but->view_item);
-          ED_region_tag_redraw(CTX_wm_region(C));
-          return WM_UI_HANDLER_BREAK;
+          if (UI_view_item_can_rename(*view_item_but->view_item)) {
+            data->cancel = true;
+            UI_view_item_begin_rename(*view_item_but->view_item);
+            ED_region_tag_redraw(CTX_wm_region(C));
+            return WM_UI_HANDLER_BREAK;
+          }
+          return WM_UI_HANDLER_CONTINUE;
       }
     }
   }
