ab051e1f17905162942368a5b6fa3be1cd24d604
quackarooni <alfonsomartzii@gmail.com>
2024-11-10 12:20:31 +0100

    Fix: GPv3: "Lineart" and "Opacity" modifier don't follow alphabetical order
    The `Lineart` and `Opacity` modifier appear at the bottom of their respective
    menus instead of following alphabetical order. There doesn't seem to be any
    conceivable reason to do this, and it is inconsistent with how the modifiers
    were laid out in older Blender versions. Fixes the order of the modifiers.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/130080

--- scripts/startup/bl_ui/properties_data_modifier.py
+++ scripts/startup/bl_ui/properties_data_modifier.py
@@ -165,12 +165,12 @@ class OBJECT_MT_modifier_add_generate(ModifierAddMenu, Menu):
             self.operator_modifier_add(layout, 'GREASE_PENCIL_DASH')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_ENVELOPE')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_LENGTH')
+            self.operator_modifier_add(layout, 'LINEART')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_MIRROR')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_MULTIPLY')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_OUTLINE')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_SIMPLIFY')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_SUBDIV')
-            self.operator_modifier_add(layout, 'LINEART')
         layout.template_modifier_asset_menu_items(catalog_path=self.bl_label)
 
 
@@ -262,8 +262,8 @@ class OBJECT_MT_modifier_add_color(ModifierAddMenu, Menu):
         ob_type = context.object.type
         if ob_type == 'GREASEPENCIL':
             self.operator_modifier_add(layout, 'GREASE_PENCIL_COLOR')
-            self.operator_modifier_add(layout, 'GREASE_PENCIL_TINT')
             self.operator_modifier_add(layout, 'GREASE_PENCIL_OPACITY')
+            self.operator_modifier_add(layout, 'GREASE_PENCIL_TINT')
         layout.template_modifier_asset_menu_items(catalog_path=self.bl_label)
 
 
