8bc9e48f140068a7e91e1b5a397f0cc7a335ff21
Iliya Katueshenock <modormoder@gmail.com>
2024-11-12 15:28:45 +0100

    Cleanup: Geometry Nodes: use general an attribute copy function GP-Instances conversion
    This is replacement of a too specific function to copy attributes from a grease pencil to instances.
    With #127026 there is no more reason to have such a specific implementation so general `copy` is used now.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/127027

--- source/blender/blenkernel/BKE_geometry_set.hh
+++ source/blender/blenkernel/BKE_geometry_set.hh
@@ -255,11 +255,6 @@ struct GeometrySet {
                          bool include_instances,
                          AttributeForeachCallback callback) const;
 
-  static void propagate_attributes_from_layer_to_instances(
-      const AttributeAccessor src_attributes,
-      MutableAttributeAccessor dst_attributes,
-      const AttributeFilter &attribute_filter);
-
   void gather_attributes_for_propagation(Span<GeometryComponent::Type> component_types,
                                          GeometryComponent::Type dst_component_type,
                                          bool include_instances,
--- source/blender/nodes/geometry/nodes/node_geo_curve_to_points.cc
+++ source/blender/nodes/geometry/nodes/node_geo_curve_to_points.cc
@@ -287,10 +287,12 @@ static void grease_pencil_to_points(GeometrySet &geometry_set,
           const int handle = instances->add_reference(bke::InstanceReference{temp_set});
           instances->add_instance(handle, float4x4::identity());
         }
-        GeometrySet::propagate_attributes_from_layer_to_instances(
-            geometry.get_grease_pencil()->attributes(),
-            instances->attributes_for_write(),
-            attribute_filter);
+
+        bke::copy_attributes(geometry.get_grease_pencil()->attributes(),
+                             bke::AttrDomain::Layer,
+                             bke::AttrDomain::Instance,
+                             attribute_filter,
+                             instances->attributes_for_write());
         InstancesComponent &dst_component = geometry.get_component_for_write<InstancesComponent>();
         GeometrySet new_instances = geometry::join_geometries(
             {GeometrySet::from_instances(dst_component.release()),
