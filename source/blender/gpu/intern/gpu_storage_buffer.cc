9ad998461182ea6bcd9b6847b9ccb99ead774111
Miguel Pozo <pragma37@gmail.com>
2024-11-14 15:31:20 +0100

    GPU: Fill uninitialized buffers with poison values
    Pull Request: https://projects.blender.org/blender/blender/pulls/130240

--- source/blender/gpu/intern/gpu_storage_buffer.cc
+++ source/blender/gpu/intern/gpu_storage_buffer.cc
@@ -12,6 +12,8 @@
 #include "BLI_blenlib.h"
 #include "BLI_math_base.h"
 
+#include "BKE_global.hh"
+
 #include "gpu_backend.hh"
 
 #include "GPU_material.hh"
@@ -62,6 +64,13 @@ GPUStorageBuf *GPU_storagebuf_create_ex(size_t size,
   if (data != nullptr) {
     ssbo->update(data);
   }
+  else if (G.debug & G_DEBUG_GPU) {
+    /* Fill the buffer with poison values.
+     * (NaN for floats, -1 for ints and "max value" for uints). */
+    blender::Vector<uchar> uninitialized_data(size, 0xFF);
+    ssbo->update(uninitialized_data.data());
+  }
+
   return wrap(ssbo);
 }
 
