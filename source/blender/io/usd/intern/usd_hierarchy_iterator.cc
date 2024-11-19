428ab699dcf58c607a8995b9d5e77b8ddb7e3679
Charles Flèche <charlesfleche@noreply.localhost>
2024-11-19 19:18:53 +0100

    USD: Add option to merge transform and shape on export
    Adds the option `merge_parent_xform` to the USD export operator and
    panels so the transform and shapes are merged into a single USD Prim.
    
    Without the option (existing default), a top-level mesh would be
    exported as a top-level `Xform` that has a `Mesh` child:
    ```
    def Xform "MyBlenderMeshObject"
    {
      matrix4d xformOp:transform = ...
    
      def Mesh "MyBlenderMeshData"
      {
      }
    }
    ```
    
    This matches the Blender data model, where a transformable object
    contains a geometric shape (like a mesh). This structure is also very
    valid in USD, where we don't want to directly instantiate geometric
    primitives[1]
    
    However, "since number of prims on a stage is one of the primary factors
    that governs how USD scale"[2], to reduce the number of prims in a
    stage, geometric primitives *are transformable* themselves (see the
    inheritence diagram[3]).
    
    As such, the new export option allows to export geometric primitives
    without the parent transform:
    ```
    def Mesh "MyBlenderMeshObject"
    {
      matrix4d xformOp:transform = ...
    }
    ```
    
    This MR adds a the `is_object_data_context` flag to the
    `HierarchyContext` context structure. The point of this change is to
    make unambiguous in `USDHierarchyIterator::create_usd_export_context`
    the fact that an `object` or a `data` is currently being exported: the
    new `merge_parent_xform` option is meaningless for `object`. Only `data`
    can be exported with a parent `Xform` or as a child of said `Xform`.
    
    Ideally this flag would not be needed at all: the final USD prim path
    *could* be computed in an override of the virtual
    `AbstractHierarchyIterator::get_object_data_path` method. However, this
    would mean that an `object` and a `data` would have the same export path.
    This does not currently play well with
    `AbstractHierarchyIterator::ensure_writer`, where `writers` are cached
    *by their export path*: it would cache a transform writer, but will skip
    the subsequent data writer.
    
    Additionally, another new `is_parent` flag is added to handle the case
    where merging the Xform is invalid: objects that are parents to other
    objects should remain unmerged as otherwise that would yield invalid USD
    files.
    
    [1] https://openusd.org/release/glossary.html#usdglossary-gprim
    [2] https://openusd.org/release/glossary.html#usdglossary-instancing
    [3] https://openusd.org/release/api/class_usd_geom_xformable.html
    
    Co-authored-by: Odréanne Breton <odreanne.breton@ubisoft.com>
    Co-authored-by: Sttevan Carnali Joga <sttevan.carnali-joga@ubisoft.com>
    Co-authored-by: Charles Flèche <charles.fleche@ubisoft.com>

--- source/blender/io/usd/intern/usd_hierarchy_iterator.cc
+++ source/blender/io/usd/intern/usd_hierarchy_iterator.cc
@@ -110,6 +110,17 @@ USDExporterContext USDHierarchyIterator::create_usd_export_context(const Hierarc
     path = pxr::SdfPath(context->export_path);
   }
 
+  if (params_.merge_parent_xform && context->is_object_data_context && !context->is_parent) {
+    bool can_merge_with_xform = true;
+    if (params_.export_shapekeys && is_mesh_with_shape_keys(context->object)) {
+      can_merge_with_xform = false;
+    }
+
+    if (can_merge_with_xform) {
+      path = path.GetParentPath();
+    }
+  }
+
   /* Returns the same path that was passed to `stage_` object during it's creation (via
    * `pxr::UsdStage::CreateNew` function). */
   const pxr::SdfLayerHandle root_layer = stage_->GetRootLayer();
