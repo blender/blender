c365f08b393cdf38c834fb44a1b55d6a825e7711
Clément Foucault <foucault.clem@gmail.com>
2024-11-20 14:41:34 +0100

    Fix #130467!: Overlay-Next: Selection overlay missing GP vertex paint mode
    It was unimplemented feature.

--- source/blender/draw/engines/overlay/overlay_next_grease_pencil.hh
+++ source/blender/draw/engines/overlay/overlay_next_grease_pencil.hh
@@ -66,6 +66,8 @@ class GreasePencil {
         break;
       case OB_MODE_VERTEX_GREASE_PENCIL:
         /* Vertex paint mode. */
+        show_lines_ = ED_grease_pencil_vertex_selection_domain_get(ts) == bke::AttrDomain::Point;
+        show_lines_ = show_lines;
         break;
       case OB_MODE_EDIT:
         /* Edit mode. */
