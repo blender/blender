2b82d0cb6b20b6bb65155e6614a2db1b2e03e6c5
Miguel Pozo <pragma37@gmail.com>
2024-11-22 17:01:15 +0100

    NPR: Add propper view vector

--- source/blender/gpu/shaders/material/gpu_shader_material_camera.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_camera.glsl
@@ -4,7 +4,7 @@
 
 #include "gpu_shader_material_transform_utils.glsl"
 
-void camera(out vec3 outview, out float outdepth, out float outdist)
+void camera(out vec3 outview, out float outdepth, out float outdist, out vec3 outray)
 {
   vec3 vP;
   point_transform_world_to_view(g_data.P, vP);
@@ -12,4 +12,5 @@ void camera(out vec3 outview, out float outdepth, out float outdist)
   outdepth = abs(vP.z);
   outdist = length(vP);
   outview = normalize(vP);
+  outray = coordinate_incoming(g_data.P);
 }
