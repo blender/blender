blender/blender

commit 9396c8f3887de980bb75088400e79b160d4df263

Workbench: Replace usage of legacy GLSL libraries by new ones

source/blender/draw/engines/workbench/shaders/workbench_transparent_accum_frag.glsl
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "common_view_lib.glsl"
+#include "draw_view_lib.glsl"
 #include "workbench_common_lib.glsl"
 #include "workbench_image_lib.glsl"
 #include "workbench_matcap_lib.glsl"
@@ -52,7 +52,7 @@ void main()
 {
   /* Normal and Incident vector are in view-space. Lighting is evaluated in view-space. */
   vec2 uv_viewport = gl_FragCoord.xy * world_data.viewport_size_inv;
-  vec3 I = get_view_vector_from_screen_uv(uv_viewport);
+  vec3 I = normalize(drw_point_screen_to_view(vec3(uv_viewport, 0.5)));
   vec3 N = normalize(normal_interp);
 
   vec3 color = color_interp;

source/blender/draw/engines/workbench/shaders/workbench_cavity_lib.glsl
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include "common_view_lib.glsl"
+#include "draw_view_lib.glsl"
 #include "workbench_common_lib.glsl"
 
 /*  From The Alchemy screen-space ambient obscurance algorithm
@@ -33,7 +33,7 @@ void cavity_compute(vec2 screenco,
     return;
   }
 
-  vec3 position = get_view_space_from_depth(screenco, depth);
+  vec3 position = drw_point_screen_to_view(vec3(screenco, depth));
   vec3 normal = workbench_normal_decode(texture(normalBuffer, screenco));
 
   vec2 jitter_co = (screenco * world_data.viewport_size.xy) * world_data.cavity_jitter_scale;
@@ -73,7 +73,7 @@ void cavity_compute(vec2 screenco,
     bool is_background = (s_depth == 1.0);
     /* This trick provide good edge effect even if no neighbor is found. */
     s_depth = (is_background) ? depth : s_depth;
-    vec3 s_pos = get_view_space_from_depth(uvcoords, s_depth);
+    vec3 s_pos = drw_point_screen_to_view(vec3(uvcoords, s_depth));
 
     if (is_background) {
       s_pos.z -= world_data.cavity_distance;

source/blender/draw/engines/workbench/shaders/workbench_composite_frag.glsl
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "common_view_lib.glsl"
+#include "draw_view_lib.glsl"
 #include "workbench_cavity_lib.glsl"
 #include "workbench_common_lib.glsl"
 #include "workbench_curvature_lib.glsl"
@@ -21,7 +21,8 @@ void main()
   }
 
   /* Normal and Incident vector are in view-space. Lighting is evaluated in view-space. */
-  vec3 V = get_view_vector_from_screen_uv(uv);
+  vec3 P = drw_point_screen_to_view(vec3(uv, 0.5));
+  vec3 V = drw_view_incident_vector(P);
   vec3 N = workbench_normal_decode(texture(normal_tx, uv));
   vec4 mat_data = texture(material_tx, uv);
 

source/blender/draw/engines/workbench/shaders/workbench_effect_dof_frag.glsl
@@ -8,8 +8,9 @@
  * Converted and adapted from HLSL to GLSL by Clément Foucault
  */
 
-#include "common_math_lib.glsl"
-#include "common_view_lib.glsl"
+#include "draw_view_lib.glsl"
+#include "gpu_shader_math_vector_lib.glsl"
+#include "gpu_shader_utildefines_lib.glsl"
 
 #define dof_aperturesize dofParams.x
 #define dof_distance dofParams.y
@@ -63,8 +64,8 @@ void main()
   vec4 cocs_near = calculate_coc(zdepths);
   vec4 cocs_far = -cocs_near;
 
-  float coc_near = max(max_v4(cocs_near), 0.0);
-  float coc_far = max(max_v4(cocs_far), 0.0);
+  float coc_near = max(reduce_max(cocs_near), 0.0);
+  float coc_far = max(reduce_max(cocs_far), 0.0);
 
   /* now we need to write the near-far fields premultiplied by the coc
    * also use bilateral weighting by each coc values to avoid bleeding. */
@@ -109,8 +110,8 @@ void main()
   vec4 cocs_near = vec4(cocs1.r, cocs2.r, cocs3.r, cocs4.r) * MAX_COC_SIZE;
   vec4 cocs_far = vec4(cocs1.g, cocs2.g, cocs3.g, cocs4.g) * MAX_COC_SIZE;
 
-  float coc_near = max_v4(cocs_near);
-  float coc_far = max_v4(cocs_far);
+  float coc_near = reduce_max(cocs_near);
+  float coc_far = reduce_max(cocs_far);
 
   /* now we need to write the near-far fields premultiplied by the coc
    * also use bilateral weighting by each coc values to avoid bleeding. */

source/blender/draw/engines/workbench/shaders/workbench_prepass_frag.glsl
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "common_view_lib.glsl"
+#include "draw_view_lib.glsl"
 #include "workbench_common_lib.glsl"
 #include "workbench_image_lib.glsl"
 

source/blender/draw/engines/workbench/shaders/workbench_prepass_hair_vert.glsl
@@ -4,7 +4,7 @@
 
 #include "common_hair_lib.glsl"
 #include "common_view_clipping_lib.glsl"
-#include "common_view_lib.glsl"
+#include "draw_view_lib.glsl"
 #include "workbench_common_lib.glsl"
 #include "workbench_image_lib.glsl"
 #include "workbench_material_lib.glsl"
@@ -61,7 +61,7 @@ void main()
                               thickness,
                               thick_time);
 
-  gl_Position = point_world_to_ndc(world_pos);
+  gl_Position = drw_point_world_to_homogenous(world_pos);
 
   float hair_rand = integer_noise(hair_get_strand_id());
   vec3 nor = workbench_hair_random_normal(tan, binor, hair_rand);

source/blender/draw/engines/workbench/shaders/workbench_prepass_pointcloud_vert.glsl
@@ -2,9 +2,11 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "draw_view_lib.glsl"
+
 #include "common_pointcloud_lib.glsl"
 #include "common_view_clipping_lib.glsl"
-#include "common_view_lib.glsl"
+
 #include "workbench_common_lib.glsl"
 #include "workbench_image_lib.glsl"
 #include "workbench_material_lib.glsl"
@@ -16,7 +18,7 @@ void main()
 
   normal_interp = normalize(normal_world_to_view(normal_interp));
 
-  gl_Position = point_world_to_ndc(world_pos);
+  gl_Position = drw_point_world_to_homogenous(world_pos);
 
   view_clipping_distances(world_pos);
 

source/blender/draw/engines/workbench/shaders/workbench_prepass_vert.glsl
@@ -3,15 +3,16 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "common_view_clipping_lib.glsl"
-#include "common_view_lib.glsl"
+#include "draw_model_lib.glsl"
+#include "draw_view_lib.glsl"
 #include "workbench_common_lib.glsl"
 #include "workbench_image_lib.glsl"
 #include "workbench_material_lib.glsl"
 
 void main()
 {
-  vec3 world_pos = point_object_to_world(pos);
-  gl_Position = point_world_to_ndc(world_pos);
+  vec3 world_pos = drw_point_object_to_world(pos);
+  gl_Position = drw_point_world_to_homogenous(world_pos);
 
   view_clipping_distances(world_pos);
 

commit 1105f2573e28f1b328c4d9b458af5cf941c53531

Fix #130846: Workbench: X-Ray incoming vector
Regression from
9396c8f3887de980bb75088400e79b160d4df263

source/blender/draw/engines/workbench/shaders/workbench_transparent_accum_frag.glsl
@@ -58,7 +58,8 @@ void main()
 {
   /* Normal and Incident vector are in view-space. Lighting is evaluated in view-space. */
   vec2 uv_viewport = gl_FragCoord.xy * world_data.viewport_size_inv;
-  vec3 I = normalize(drw_point_screen_to_view(vec3(uv_viewport, 0.5)));
+  vec3 vP = drw_point_screen_to_view(vec3(uv_viewport, 0.5));
+  vec3 I = drw_view_incident_vector(vP);
   vec3 N = normalize(normal_interp);
 
   vec3 color = color_interp;