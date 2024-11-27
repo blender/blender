b39843e7b0aea517668978a7a1a933e62eb6d3d8
Clément Foucault <foucault.clem@gmail.com>
2024-11-27 23:14:44 +0100

    GPU: Use extern for non static expressions and use non zero value for gl_WorkGroupSize
    Pull Request: https://projects.blender.org/blender/blender/pulls/131064

--- source/blender/gpu/shaders/gpu_glsl_cpp_stubs.hh
+++ source/blender/gpu/shaders/gpu_glsl_cpp_stubs.hh
@@ -819,11 +819,11 @@ bool is_zero(vec4) RET;
 
 namespace gl_VertexShader {
 
-const int gl_VertexID = 0;
-const int gl_InstanceID = 0;
-const int gl_BaseVertex = 0;
-const int gpu_BaseInstance = 0;
-const int gpu_InstanceIndex = 0;
+extern const int gl_VertexID;
+extern const int gl_InstanceID;
+extern const int gl_BaseVertex;
+extern const int gpu_BaseInstance;
+extern const int gpu_InstanceIndex;
 float4 gl_Position = float4(0);
 double gl_PointSize = 0;
 float gl_ClipDistance[6] = {0};
@@ -834,7 +834,7 @@ int gpu_ViewportIndex = 0;
 
 namespace gl_FragmentShader {
 
-const float4 gl_FragCoord = float4(0);
+extern const float4 gl_FragCoord;
 const bool gl_FrontFacing = true;
 const float2 gl_PointCoord = float2(0);
 const int gl_PrimitiveID = 0;
@@ -847,12 +847,12 @@ const int gpu_ViewportIndex = 0;
 
 namespace gl_ComputeShader {
 
-const uint3 gl_NumWorkGroups = {};
-constexpr uint3 gl_WorkGroupSize = {};
-const uint3 gl_WorkGroupID = {};
-const uint3 gl_LocalInvocationID = {};
-const uint3 gl_GlobalInvocationID = {};
-const uint gl_LocalInvocationIndex = {};
+constexpr uint3 gl_WorkGroupSize = uint3(16, 16, 16);
+extern const uint3 gl_NumWorkGroups;
+extern const uint3 gl_WorkGroupID;
+extern const uint3 gl_LocalInvocationID;
+extern const uint3 gl_GlobalInvocationID;
+extern const uint gl_LocalInvocationIndex;
 
 }  // namespace gl_ComputeShader
 
