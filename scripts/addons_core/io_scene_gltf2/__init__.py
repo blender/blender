f7a2d0095e8040b7a2ddc4e6df637aeca2f5b408
Julien Duroure <julien.duroure@gmail.com>
2024-11-28 09:33:22 +0100

    glTF exporter: Fix debug logging
    Set INFO level if user set debug to whatever value not used to set logging level

--- scripts/addons_core/io_scene_gltf2/__init__.py
+++ scripts/addons_core/io_scene_gltf2/__init__.py
@@ -5,7 +5,7 @@
 bl_info = {
     'name': 'glTF 2.0 format',
     'author': 'Julien Duroure, Scurest, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein, and many external contributors',
-    "version": (4, 4, 19),
+    "version": (4, 4, 20),
     'blender': (4, 3, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',
@@ -175,6 +175,8 @@ def set_debug_log():
         return logging.CRITICAL
     elif bpy.app.debug_value == 4:
         return logging.DEBUG
+    else:
+        return logging.INFO
 
 
 class ConvertGLTF2_Base:
