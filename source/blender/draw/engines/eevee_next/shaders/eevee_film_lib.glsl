ad4a306049fc7bebb5230e024aeec57b9951dd07
Clément Foucault <foucault.clem@gmail.com>
2024-12-04 11:49:41 +0100

    Fix #124013: EEVEE: Artifact when using larger pixel size
    Rely on Overlay engine to output correct fullres depth
    instead of trying to upsample EEVEE's one.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/130228

--- source/blender/draw/engines/eevee_next/shaders/eevee_film_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_film_lib.glsl
@@ -623,6 +623,11 @@ void film_store_weight(ivec2 texel, float value)
 
 float film_display_depth_amend(ivec2 texel, float depth)
 {
+  if (scaling_factor > 1) {
+    /* The workaround below creates ugly artifacts for overlays relying on depth equal tests.
+     * In this case, it is better to rely on overlay engine to do a depth pass (see #124013). */
+    return 0.0;
+  }
   /* This effectively offsets the depth of the whole 2x2 region to the lowest value of the region
    * twice. One for X and one for Y direction. */
   /* TODO(fclem): This could be improved as it gives flickering result at depth discontinuity.
