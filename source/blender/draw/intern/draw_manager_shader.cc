071fb86a075e086a757976cb5ee7317d4bc0bcfe
Hans Goudey <hans@blender.org>
2024-12-11 10:23:45 -0500

    Cleanup: Remove unused function

--- source/blender/draw/intern/draw_manager_shader.cc
+++ source/blender/draw/intern/draw_manager_shader.cc
@@ -445,9 +445,4 @@ void DRW_shader_queue_optimize_material(GPUMaterial *mat)
   drw_deferred_queue_append(mat, true);
 }
 
-static void UNUSED_FUNCTION(DRW_shader_free)(GPUShader *shader)
-{
-  GPU_shader_free(shader);
-}
-
 /** \} */
