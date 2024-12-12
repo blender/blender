ba6b4d45531058c5d59c1f475369737b36cf156b
Germano Cavalcante <germano.costa@ig.com.br>
2024-12-12 12:12:47 -0300

    Fix: mixed snap not working on geometry generated by curves
    `snap_polygon` and `snap_edge_points` were, for no reason, limited to
    Mesh type Objects.

--- source/blender/editors/transform/transform_snap_object.cc
+++ source/blender/editors/transform/transform_snap_object.cc
@@ -831,7 +831,7 @@ void cb_snap_edge(void *userdata,
 
 static eSnapMode snap_polygon(SnapObjectContext *sctx, eSnapMode snap_to_flag)
 {
-  if (sctx->ret.ob->type != OB_MESH || !sctx->ret.data || GS(sctx->ret.data->name) != ID_ME) {
+  if (!sctx->ret.data || GS(sctx->ret.data->name) != ID_ME) {
     return SCE_SNAP_TO_NONE;
   }
 
@@ -841,7 +841,7 @@ static eSnapMode snap_polygon(SnapObjectContext *sctx, eSnapMode snap_to_flag)
 
 static eSnapMode snap_edge_points(SnapObjectContext *sctx, const float dist_px_sq_orig)
 {
-  if (sctx->ret.ob->type != OB_MESH || !sctx->ret.data || GS(sctx->ret.data->name) != ID_ME) {
+  if (!sctx->ret.data || GS(sctx->ret.data->name) != ID_ME) {
     return SCE_SNAP_TO_EDGE;
   }
 
