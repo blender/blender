075cc13119eaddbf05bb41e9c2cb84df0017b432
Jeroen Bakker <jeroen@blender.org>
2024-12-16 09:58:42 +0100

    GPU: Missing create info when running tests
    When running tests `WITH_GTESTS` and `WITH_GPU_DRAW_TESTS` the
    GPUShaderCreateInfo's specfically created for the tests could not be
    found. This failed running tests on any backend.
    
    This PR fixes this. The root cause what that the name of the compile
    directive was incorrect. It should have been `WITH_GTESTS` but was
    `WITH_GTEST`.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/131956

--- source/blender/gpu/intern/gpu_shader_create_info_list.hh
+++ source/blender/gpu/intern/gpu_shader_create_info_list.hh
@@ -38,7 +38,7 @@
 #include "gpu_shader_text_info.hh"
 #include "gpu_srgb_to_framebuffer_space_info.hh"
 
-#ifdef WITH_GTEST
+#ifdef WITH_GTESTS
 #  ifdef WITH_GPU_DRAW_TESTS
 #    include "gpu_shader_test_info.hh"
 #  endif
