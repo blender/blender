5beff8011836309a52381f38af6ae124e85b0850
Falk David <falk@blender.org>
2024-12-17 18:45:47 +0100

    Fix: Grease Pencil: Undefined truncation behavior in legacy conversion
    The `double inittime` attribute on strokes is truncated to a `uint32_t`
    range. This wasn't done explicitly and the code only did a cast.
    
    The `double` should be clamped to a legal `uint32_t` value before
    casting it.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/131625

--- source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
+++ source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
@@ -916,7 +916,9 @@ static Drawing legacy_gpencil_frame_to_grease_pencil_drawing(const bGPDframe &gp
 
     stroke_cyclic.span[stroke_i] = (gps->flag & GP_STROKE_CYCLIC) != 0;
     /* Truncating time in ms to uint32 then we don't lose precision in lower bits. */
-    stroke_init_times.span[stroke_i] = float(uint32_t(gps->inittime * double(1e3))) / float(1e3);
+    const uint32_t clamped_init_time = static_cast<uint32_t>(
+        std::clamp(gps->inittime * 1e3, 0.0, double(std::numeric_limits<uint32_t>::max())));
+    stroke_init_times.span[stroke_i] = float(clamped_init_time) / float(1e3);
     stroke_start_caps.span[stroke_i] = int8_t(gps->caps[0]);
     stroke_end_caps.span[stroke_i] = int8_t(gps->caps[1]);
     stroke_softness.span[stroke_i] = 1.0f - gps->hardness;
