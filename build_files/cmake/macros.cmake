0de684eba602c9a304516b3a55b67c111a75fe7c
Ray molenkamp <lazydodo@noreply.localhost>
2024-12-27 22:45:43 +0100

    CMake: Raise LINK_INTERFACE_MULTIPLICITY to 3
    The internal blender library dependency loops are longer than
    the default cmake settings account for. As the work in [1]
    adds more dependencies where they are needed this has become
    a problem.
    
    Bumping to 3 now as it solves the immediate linker problems in
    #132375 but we can bump this further in the future if needed.
    
    [1] https://devtalk.blender.org/t/cmake-cleanup/30260
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/132385

--- build_files/cmake/macros.cmake
+++ build_files/cmake/macros.cmake
@@ -439,6 +439,9 @@ function(blender_add_lib__impl
   # Not for system includes because they can resolve to the same path
   # list_assert_duplicates("${includes_sys}")
 
+  # blenders dependency loops are longer than cmake expects and we need additional loops to 
+  # properly link.
+  set_property(TARGET ${name} APPEND PROPERTY LINK_INTERFACE_MULTIPLICITY 3)
 endfunction()
 
 
