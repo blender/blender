37c3c70150620c182dcb1bc850788002b11b8400
Sean Kim <SeanCTKim@protonmail.com>
2025-01-02 18:42:21 +0100

    Fix #122078: Color Filter redo panel doesn't work occasionally
    When a file is initially loaded or when the redo panel is used, the PBVH
    may be null on an object. Because this filter requires the PBVH to check
    the underlying mesh representation to prevent execution on Dyntopo &
    Multires, we need to ensure that the PBVH is built.
    
    This commit changes the `pbvh_get` call into a `pbvh_ensure` call and
    moves some variable declarations around to avoid holding onto runtime
    data that may get wiped out by requesting the evaluated depsgraph.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/132048

--- source/blender/editors/sculpt_paint/sculpt_filter_color.cc
+++ source/blender/editors/sculpt_paint/sculpt_filter_color.cc
@@ -455,7 +455,6 @@ static int sculpt_color_filter_init(bContext *C, wmOperator *op)
   const Scene &scene = *CTX_data_scene(C);
   Object &ob = *CTX_data_active_object(C);
   const Sculpt &sd = *CTX_data_tool_settings(C)->sculpt;
-  SculptSession &ss = *ob.sculpt;
   View3D *v3d = CTX_wm_view3d(C);
 
   const Base *base = CTX_data_active_base(C);
@@ -478,15 +477,18 @@ static int sculpt_color_filter_init(bContext *C, wmOperator *op)
   }
 
   /* Disable for multires and dyntopo for now */
-  if (!bke::object::pbvh_get(ob) || !color_supported_check(scene, ob, op->reports)) {
+  if (!color_supported_check(scene, ob, op->reports)) {
     return OPERATOR_CANCELLED;
   }
+
+  /* Ensure that we have a PBVH to be able to push changes on only visible nodes. */
+  bke::object::pbvh_ensure(*CTX_data_ensure_evaluated_depsgraph(C), ob);
 
   undo::push_begin(scene, ob, op);
   BKE_sculpt_color_layer_create_if_needed(&ob);
 
-  /* CTX_data_ensure_evaluated_depsgraph should be used at the end to include the updates of
-   * earlier steps modifying the data. */
+  /* CTX_data_ensure_evaluated_depsgraph should be used at the end to include the potential
+   * creation of color layer data. */
   Depsgraph *depsgraph = CTX_data_ensure_evaluated_depsgraph(C);
   BKE_sculpt_update_object_for_edit(depsgraph, &ob, true);
 
@@ -497,6 +499,7 @@ static int sculpt_color_filter_init(bContext *C, wmOperator *op)
                      mval_fl,
                      RNA_float_get(op->ptr, "area_normal_radius"),
                      RNA_float_get(op->ptr, "strength"));
+  const SculptSession &ss = *ob.sculpt;
   filter::Cache *filter_cache = ss.filter_cache;
   filter_cache->active_face_set = SCULPT_FACE_SET_NONE;
   filter_cache->automasking = auto_mask::cache_init(*depsgraph, sd, ob);
