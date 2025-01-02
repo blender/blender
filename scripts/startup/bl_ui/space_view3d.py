9e78f1c840874671e5f535a9a773ce9eff054f3e
Sean Kim <SeanCTKim@protonmail.com>
2025-01-02 18:11:39 +0100

    Paint: Rename 'Frame Selected' Menu operator to 'Frame Last Stroke'
    This commit better aligns the naming of this operator to what it does in
    Sculpt, Vertex Paint, Weight Paint, and Texture Paint
    
    Ref: #84044
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/132248

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -1444,7 +1444,10 @@ class VIEW3D_MT_view(Menu):
 
         layout.separator()
 
-        layout.operator("view3d.view_selected", text="Frame Selected").use_all_regions = False
+        if context.mode in ['PAINT_TEXTURE', 'PAINT_VERTEX', 'PAINT_WEIGHT', 'SCULPT']:
+            layout.operator("view3d.view_selected", text="Frame Last Stroke").use_all_regions = False
+        else:
+            layout.operator("view3d.view_selected", text="Frame Selected").use_all_regions = False
         if view.region_quadviews:
             layout.operator("view3d.view_selected", text="Frame Selected (Quad View)").use_all_regions = True
 
