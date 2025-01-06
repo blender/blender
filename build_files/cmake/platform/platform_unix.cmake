bb532146618d216e054d661808604c7e356bfe14
Sebastian Parborg <sebastian@blender.org>
2025-01-06 18:19:08 +0100

    Audaspace: Enforce minimal pipewire library version
    While I implemented pipewire support I forgot that some of the functions I use is not available in older pipewire versions.
    
    This adds proper library version sanity checks for pipewire.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/131610

--- build_files/cmake/platform/platform_unix.cmake
+++ build_files/cmake/platform/platform_unix.cmake
@@ -724,7 +724,7 @@ endif()
 # PipeWire is intended to use the system library.
 if(WITH_PIPEWIRE)
   find_package(PkgConfig)
-  pkg_check_modules(PIPEWIRE libpipewire-0.3)
+  pkg_check_modules(PIPEWIRE libpipewire-0.3>=1.1.0)
   set_and_warn_library_found("PipeWire" PIPEWIRE_FOUND WITH_PIPEWIRE)
 endif()
 
