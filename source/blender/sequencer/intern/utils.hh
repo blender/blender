9421d4f623391d1e6acbfff056f4a09fd6956bf3
Falk David <falk@blender.org>
2025-01-07 16:35:40 +0100

    Cleanup: VSE: Remove unused declaration in header

--- source/blender/sequencer/intern/utils.hh
+++ source/blender/sequencer/intern/utils.hh
@@ -8,9 +8,8 @@
  * \ingroup sequencer
  */
 
-struct ListBase;
 struct Scene;
-struct Sequence;
+struct Strip;
 
 bool sequencer_seq_generates_image(Strip *strip);
 void strip_open_anim_file(Scene *scene, Strip *strip, bool openfile);
