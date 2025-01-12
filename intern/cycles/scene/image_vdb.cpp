8111152c6775e4726f19bc65154e190147ffcfce
Brecht Van Lommel <brecht@blender.org>
2025-01-12 07:24:50 +0100

    Refactor: Cycles: Add some OpenVDB and NanoVDB functions to util
    OpenVDB to NanoVDB was moved, a new NanoVDB to OpenVDB mask grid was
    added for future use. Some redundant CMake code was simplified.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/132908

--- intern/cycles/scene/image_vdb.cpp
+++ intern/cycles/scene/image_vdb.cpp
@@ -5,128 +5,29 @@
 #include "scene/image_vdb.h"
 
 #include "util/log.h"
+#include "util/nanovdb.h"
 #include "util/openvdb.h"
 
 #ifdef WITH_OPENVDB
 #  include <openvdb/tools/Dense.h>
 #endif
-#ifdef WITH_NANOVDB
-#  define NANOVDB_USE_OPENVDB
-#  include <nanovdb/NanoVDB.h>
-#  if NANOVDB_MAJOR_VERSION_NUMBER > 32 || \
-      (NANOVDB_MAJOR_VERSION_NUMBER == 32 && NANOVDB_MINOR_VERSION_NUMBER >= 7)
-#    include <nanovdb/tools/CreateNanoGrid.h>
-#  else
-#    include <nanovdb/util/OpenToNanoVDB.h>
-#  endif
-#endif
 
 CCL_NAMESPACE_BEGIN
 
 #ifdef WITH_OPENVDB
-struct NumChannelsOp {
-  int num_channels = 0;
-
-  template<typename GridType, typename FloatGridType, typename FloatDataType, const int channels>
-  bool operator()(const openvdb::GridBase::ConstPtr & /*unused*/)
-  {
-    num_channels = channels;
-    return true;
-  }
-};
-
 struct ToDenseOp {
   openvdb::CoordBBox bbox;
   void *pixels;
 
-  template<typename GridType, typename FloatGridType, typename FloatDataType, const int channels>
-  bool operator()(const openvdb::GridBase::ConstPtr &grid)
+  template<typename GridType, typename FloatDataType, const int channels>
+  bool operator()(const typename GridType::ConstPtr &grid)
   {
     openvdb::tools::Dense<FloatDataType, openvdb::tools::LayoutXYZ> dense(bbox,
                                                                           (FloatDataType *)pixels);
-    openvdb::tools::copyToDense(*openvdb::gridConstPtrCast<GridType>(grid), dense);
+    openvdb::tools::copyToDense(*grid, dense);
     return true;
   }
 };
-
-#  ifdef WITH_NANOVDB
-struct ToNanoOp {
-  nanovdb::GridHandle<> nanogrid;
-  int precision;
-
-  template<typename GridType, typename FloatGridType, typename FloatDataType, const int channels>
-  bool operator()(const openvdb::GridBase::ConstPtr &grid)
-  {
-    if constexpr (!std::is_same_v<GridType, openvdb::MaskGrid>) {
-      try {
-#    if NANOVDB_MAJOR_VERSION_NUMBER > 32 || \
-        (NANOVDB_MAJOR_VERSION_NUMBER == 32 && NANOVDB_MINOR_VERSION_NUMBER >= 6)
-#      if NANOVDB_MAJOR_VERSION_NUMBER > 32 || \
-          (NANOVDB_MAJOR_VERSION_NUMBER == 32 && NANOVDB_MINOR_VERSION_NUMBER >= 7)
-        /* OpenVDB 12. */
-        using nanovdb::tools::createNanoGrid;
-        using nanovdb::tools::StatsMode;
-#      else
-        /* OpenVDB 11. */
-        using nanovdb::createNanoGrid;
-        using nanovdb::StatsMode;
-#      endif
-
-        if constexpr (std::is_same_v<FloatGridType, openvdb::FloatGrid>) {
-          const openvdb::FloatGrid floatgrid(*openvdb::gridConstPtrCast<GridType>(grid));
-          if (precision == 0) {
-            nanogrid = createNanoGrid<openvdb::FloatGrid, nanovdb::FpN>(floatgrid);
-          }
-          else if (precision == 16) {
-            nanogrid = createNanoGrid<openvdb::FloatGrid, nanovdb::Fp16>(floatgrid);
-          }
-          else {
-            nanogrid = createNanoGrid<openvdb::FloatGrid, float>(floatgrid);
-          }
-        }
-        else if constexpr (std::is_same_v<FloatGridType, openvdb::Vec3fGrid>) {
-          const openvdb::Vec3fGrid floatgrid(*openvdb::gridConstPtrCast<GridType>(grid));
-          nanogrid = createNanoGrid<openvdb::Vec3fGrid, nanovdb::Vec3f>(floatgrid,
-                                                                        StatsMode::Disable);
-        }
-#    else
-        /* OpenVDB 10. */
-        if constexpr (std::is_same_v<FloatGridType, openvdb::FloatGrid>) {
-          openvdb::FloatGrid floatgrid(*openvdb::gridConstPtrCast<GridType>(grid));
-          if (precision == 0) {
-            nanogrid =
-                nanovdb::openToNanoVDB<nanovdb::HostBuffer, openvdb::FloatTree, nanovdb::FpN>(
-                    floatgrid);
-          }
-          else if (precision == 16) {
-            nanogrid =
-                nanovdb::openToNanoVDB<nanovdb::HostBuffer, openvdb::FloatTree, nanovdb::Fp16>(
-                    floatgrid);
-          }
-          else {
-            nanogrid = nanovdb::openToNanoVDB(floatgrid);
-          }
-        }
-        else if constexpr (std::is_same_v<FloatGridType, openvdb::Vec3fGrid>) {
-          openvdb::Vec3fGrid floatgrid(*openvdb::gridConstPtrCast<GridType>(grid));
-          nanogrid = nanovdb::openToNanoVDB(floatgrid);
-        }
-#    endif
-      }
-      catch (const std::exception &e) {
-        LOG(WARNING) << "Error converting OpenVDB to NanoVDB grid: " << e.what();
-      }
-      catch (...) {
-        LOG(WARNING) << "Error converting OpenVDB to NanoVDB grid: Unknown error";
-      }
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-};
-#  endif
 
 VDBImageLoader::VDBImageLoader(openvdb::GridBase::ConstPtr grid_, const string &grid_name)
     : grid_name(grid_name), grid(grid_)
@@ -145,29 +46,23 @@ bool VDBImageLoader::load_metadata(const ImageDeviceFeatures &features, ImageMet
     return false;
   }
 
-  /* Get number of channels from type. */
-  NumChannelsOp op;
-  if (!openvdb::grid_type_operation(grid, op)) {
+  /* Convert to the few float types that we know. */
+  grid = openvdb_convert_to_known_type(grid);
+  if (!grid) {
     return false;
   }
 
-  metadata.channels = op.num_channels;
+  /* Get number of channels from type. */
+  metadata.channels = openvdb_num_channels(grid);
 
   /* Set data type. */
 #  ifdef WITH_NANOVDB
   if (features.has_nanovdb) {
-    /* NanoVDB expects no inactive leaf nodes. */
-#    if 0
-    openvdb::FloatGrid &pruned_grid = *openvdb::gridPtrCast<openvdb::FloatGrid>(grid);
-    openvdb::tools::pruneInactive(pruned_grid.tree());
-    nanogrid = nanovdb::openToNanoVDB(pruned_grid);
-#    endif
-    ToNanoOp op;
-    op.precision = precision;
-    if (!openvdb::grid_type_operation(grid, op)) {
+    /* Convert OpenVDB to NanoVDB grid. */
+    nanogrid = openvdb_to_nanovdb(grid, precision, 0.0f);
+    if (!nanogrid) {
       return false;
     }
-    nanogrid = std::move(op.nanogrid);
   }
 #  endif
 
@@ -263,7 +158,7 @@ bool VDBImageLoader::load_pixels(const ImageMetaData & /*metadata*/,
     ToDenseOp op;
     op.pixels = pixels;
     op.bbox = bbox;
-    openvdb::grid_type_operation(grid, op);
+    openvdb_grid_type_operation(grid, op);
   }
   return true;
 #else
