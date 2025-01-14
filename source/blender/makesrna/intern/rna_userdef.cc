6922418a953a1bf53d5ab8849d4b473e533fb13f
Harley Acheson <harley.acheson@gmail.com>
2025-01-14 20:54:16 +0100

    Fix #79388: Increase User Scale Hard Min to 0.5
    Increase the hard minimum for Resolution Scale from 0.25 to 0.5. 0.5 is
    also the current soft limit (the least you can get by just dragging on
    the widget). But, as illustrated by the bug report, accidentally typing
    in a value below 0.5 is difficult to recover from since the text is too
    small to see. It is a useful minimum though in that this value is good
    for tests and captures on High DPI system to simulate low dpi.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/131688

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -5146,7 +5146,7 @@ static void rna_def_userdef_view(BlenderRNA *brna)
   prop = RNA_def_property(srna, "ui_scale", PROP_FLOAT, PROP_NONE);
   RNA_def_property_ui_text(
       prop, "UI Scale", "Changes the size of the fonts and widgets in the interface");
-  RNA_def_property_range(prop, 0.25f, 6.0f);
+  RNA_def_property_range(prop, 0.5f, 6.0f);
   RNA_def_property_ui_range(prop, 0.5f, 3.0f, 1, 2);
   RNA_def_property_update(prop, 0, "rna_userdef_gpu_update");
 
