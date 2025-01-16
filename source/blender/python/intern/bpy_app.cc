59585cf978d2019a59074845335a90441d3cf4dc
Sybren A. Stüvel <sybren@blender.org>
2025-01-16 11:06:59 +0100

    Cleanup: document difference between `bpy.app.version` and `.version_file`
    Clarify the meaning `bpy.app.version_file`, by being more explicit in the
    description of `bpy.app.version` and their differences.
    
    ### Old:
    
    `bpy.app.version`
    : The Blender version as a tuple of 3 numbers. eg. (2, 83, 1)
    
    `bpy.app.version_file`
    : The Blender version, as a tuple, last used to save a .blend file, compatible with ``bpy.data.version``. This value should be used for handling compatibility changes between Blender versions
    
    ### New:
    
    `bpy.app.version`
    : The Blender version as a tuple of 3 numbers (major, minor, micro). eg. (4, 3, 1)
    
    `bpy.app.version_file`
    : The Blender File version, as a tuple of 3 numbers (major, minor, file sub-version), that will be used to save a .blend file. The last item in this tuple indicates the file sub-version, which is different from the release micro version (the last item of the `bpy.app.version` tuple). The file sub-version can be incremented multiple times while a Blender version is under development. This value is, and should be, used for handling compatibility changes between Blender versions
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/133142

--- source/blender/python/intern/bpy_app.cc
+++ source/blender/python/intern/bpy_app.cc
@@ -75,11 +75,15 @@ extern "C" char build_system[];
 static PyTypeObject BlenderAppType;
 
 static PyStructSequence_Field app_info_fields[] = {
-    {"version", "The Blender version as a tuple of 3 numbers. eg. (2, 83, 1)"},
+    {"version",
+     "The Blender version as a tuple of 3 numbers (major, minor, micro). eg. (4, 3, 1)"},
     {"version_file",
-     "The Blender version, as a tuple, last used to save a .blend file, compatible with "
-     "``bpy.data.version``. This value should be used for handling compatibility changes between "
-     "Blender versions"},
+     "The Blender File version, as a tuple of 3 numbers (major, minor, file sub-version), that "
+     "will be used to save a .blend file. The last item in this tuple indicates the file "
+     "sub-version, which is different from the release micro version (the last item of the "
+     "`bpy.app.version` tuple). The file sub-version can be incremented multiple times while a "
+     "Blender version is under development. This value is, and should be, used for handling "
+     "compatibility changes between Blender versions"},
     {"version_string", "The Blender version formatted as a string"},
     {"version_cycle", "The release status of this build alpha/beta/rc/release"},
     {"background",
