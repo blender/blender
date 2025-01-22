549c724f78b00b40d4a6a3e10488bc189f8afa18
Omar Emara <mail@OmarEmara.dev>
2025-01-22 16:15:21 +0200

    Revert "Cleanup: Remove check for SSE4.1 in denoise node"
    This reverts commit 19e9092cb64764183e83569ff631be16babdd399. The
    minimum requirement is for builds that Blender officially provides. We
    still need to maintain code for none SSE builds.

--- source/blender/nodes/composite/nodes/node_composite_denoise.cc
+++ source/blender/nodes/composite/nodes/node_composite_denoise.cc
@@ -62,6 +62,13 @@ static void node_composit_buts_denoise(uiLayout *layout, bContext * /*C*/, Point
 {
 #ifndef WITH_OPENIMAGEDENOISE
   uiItemL(layout, RPT_("Disabled, built without OpenImageDenoise"), ICON_ERROR);
+#else
+  /* Always supported through Accelerate framework BNNS on macOS. */
+#  ifndef __APPLE__
+  if (!BLI_cpu_support_sse42()) {
+    uiItemL(layout, RPT_("Disabled, CPU with SSE4.2 is required"), ICON_ERROR);
+  }
+#  endif
 #endif
 
   uiItemL(layout, IFACE_("Prefilter:"), ICON_NONE);
@@ -290,13 +297,19 @@ class DenoiseOperation : public NodeOperation {
   }
 #endif /* WITH_OPENIMAGEDENOISE */
 
-  /* OIDN can be disabled as a build option, so check WITH_OPENIMAGEDENOISE. */
+  /* OIDN can be disabled as a build option, so check WITH_OPENIMAGEDENOISE. Additionally, it is
+   * only supported at runtime for CPUs that supports SSE4.1, except for MacOS where it is always
+   * supported through the Accelerate framework BNNS on macOS. */
   bool is_oidn_supported()
   {
-#ifdef WITH_OPENIMAGEDENOISE
-    return true;
-#else
+#ifndef WITH_OPENIMAGEDENOISE
     return false;
+#else
+#  ifdef __APPLE__
+    return true;
+#  else
+    return BLI_cpu_support_sse42();
+#  endif
 #endif
   }
 };
