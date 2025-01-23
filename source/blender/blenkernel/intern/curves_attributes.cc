a449625fc4d30eda2b39659fdf4437901c227014
Jacques Lucke <jacques@blender.org>
2025-01-23 16:24:22 +0100

    Curves: make material index attribute builtin on curves geometry
    This makes the `material_index` attribute built-in on `CurvesGeometry` which
    means that it's built-in on `Curves` and `GreasePencil`. While only Grease
    Pencil can use it so far during rendering, we already depend on them on curves
    when converting between curves and Grease Pencil.
    
    Making the attribute built-in implies that it is locked to one domain and type
    (`Curve` domain and `int32` type in this case).
    
    This makes curves more consistent with meshes.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/133488

--- source/blender/blenkernel/intern/curves_attributes.cc
+++ source/blender/blenkernel/intern/curves_attributes.cc
@@ -336,6 +336,21 @@ static GeometryAttributeProviders create_attribute_providers_for_curve()
                                                curve_access,
                                                tag_component_topology_changed);
 
+  static const auto material_index_clamp = mf::build::SI1_SO<int, int>(
+      "Material Index Validate",
+      [](int value) {
+        /* Use #short for the maximum since many areas still use that type for indices. */
+        return std::clamp<int>(value, 0, std::numeric_limits<short>::max());
+      },
+      mf::build::exec_presets::AllSpanOrSingle());
+  static BuiltinCustomDataLayerProvider material_index("material_index",
+                                                       AttrDomain::Curve,
+                                                       CD_PROP_INT32,
+                                                       BuiltinAttributeProvider::Deletable,
+                                                       curve_access,
+                                                       nullptr,
+                                                       AttributeValidator{&material_index_clamp});
+
   static CurvesVertexGroupsAttributeProvider vertex_groups;
   static CustomDataAttributeProvider curve_custom_data(AttrDomain::Curve, curve_access);
   static CustomDataAttributeProvider point_custom_data(AttrDomain::Point, point_access);
@@ -355,7 +370,8 @@ static GeometryAttributeProviders create_attribute_providers_for_curve()
                                      &nurbs_weight,
                                      &curve_type,
                                      &resolution,
-                                     &cyclic},
+                                     &cyclic,
+                                     &material_index},
                                     {&vertex_groups, &curve_custom_data, &point_custom_data});
 }
 
