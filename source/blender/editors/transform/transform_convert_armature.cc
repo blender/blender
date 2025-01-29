8703317b81c77d66f410103492a3d810a59c52ec
Philipp Oeser <philipp@blender.org>
2025-01-29 10:07:27 +0100

    Fix #133687: editbone transform wrong with hidden connected parent bone
    If the parent editbone is hidden, it will (rightfully) not be taken into
    account in `createTransArmatureVerts`.
    In `recalcData_edit_armature` though, there was the assumption in a
    particular code path that "this bone has a parent tip that has been
    moved" even though it wasnt (because it was hidden -- see above).
    
    To resolve, guard this code path with a `EBONE_VISIBLE` check.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/133707

--- source/blender/editors/transform/transform_convert_armature.cc
+++ source/blender/editors/transform/transform_convert_armature.cc
@@ -1053,7 +1053,7 @@ static void recalcData_edit_armature(TransInfo *t)
 
       if (ebo_parent) {
         /* If this bone has a parent tip that has been moved. */
-        if (ebo_parent->flag & BONE_TIPSEL) {
+        if (EBONE_VISIBLE(arm, ebo_parent) && (ebo_parent->flag & BONE_TIPSEL)) {
           copy_v3_v3(ebo->head, ebo_parent->tail);
           if (t->mode == TFM_BONE_ENVELOPE) {
             ebo->rad_head = ebo_parent->rad_tail;
