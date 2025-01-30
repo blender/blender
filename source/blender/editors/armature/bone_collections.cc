4fc6d7685bb23c380d95e58fac3c3f3a0d081c36
Philipp Oeser <philipp@blender.org>
2025-01-30 16:40:21 +0100

    Fix #133635: "Move to Collection" acts on EditBones in hidden groups
    Operators should not act on non-visible entities really.
    
    Posemode already did the combination of "visible and selected" via
    `FOREACH_PCHAN_SELECTED_IN_OBJECT_BEGIN`, editmode didnt.
    
    To resolve, add `EBONE_VISIBLE` as a condition (as done elsewhere).
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/133660

--- source/blender/editors/armature/bone_collections.cc
+++ source/blender/editors/armature/bone_collections.cc
@@ -268,7 +268,7 @@ static void bone_collection_assign_editbones(bContext *C,
   ED_armature_edit_sync_selection(arm->edbo);
 
   LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-    if (!EBONE_EDITABLE(ebone)) {
+    if (!EBONE_EDITABLE(ebone) || !EBONE_VISIBLE(arm, ebone)) {
       continue;
     }
     *made_any_changes |= assign_func(bcoll, ebone);
