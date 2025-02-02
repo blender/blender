b2dc852b1b3859d60b12a3a8b43bdb6aaa16ff82
Campbell Barton <campbell@blender.org>
2025-02-02 15:25:34 +1100

    Docs: note why operator "register" flag is set without "undo"

--- source/blender/windowmanager/WM_types.hh
+++ source/blender/windowmanager/WM_types.hh
@@ -156,7 +156,17 @@ struct wmGenericCallback {
 
 /** #wmOperatorType.flag */
 enum {
-  /** Register operators in stack after finishing (needed for redo). */
+  /**
+   * Register operators in stack after finishing (needed for redo).
+   *
+   * \note Typically this flag should be enabled along with #OPTYPE_UNDO.
+   * There is an exception to this, some operators can perform an undo push indirectly.
+   * (`UI_OT_reset_default_button` for example).
+   *
+   * In this case, register needs to be enabled so as not to clear the "Redo" panel, see #133761.
+   * Unless otherwise stated, any operators that register without the undo flag
+   * can be assumed to be creating undo steps indirectly (potentially at least).
+   */
   OPTYPE_REGISTER = (1 << 0),
   /** Do an undo push after the operator runs. */
   OPTYPE_UNDO = (1 << 1),
