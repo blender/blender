3a8658958f606ec3461f5beb6c43911554259ca7
Campbell Barton <campbell@blender.org>
2025-02-06 10:08:05 +1100

    Correct error bumping patch level instead of subversion
    Correct mistake in 30399fd1655b1bdb8bc64486d2f9bde1a3946d03

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -23,7 +23,7 @@ extern "C" {
 /** Blender major and minor version. */
 #define BLENDER_VERSION 405
 /** Blender patch version for bug-fix releases. */
-#define BLENDER_VERSION_PATCH 1
+#define BLENDER_VERSION_PATCH 0
 /** Blender release cycle stage: alpha/beta/rc/release. */
 #define BLENDER_VERSION_CYCLE alpha
 /** Blender release type suffix. LTS or blank. */
@@ -31,7 +31,7 @@ extern "C" {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 0
+#define BLENDER_FILE_SUBVERSION 1
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to
