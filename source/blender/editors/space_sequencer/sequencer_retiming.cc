b2660dbd2b26c4c491edd3745e0d35477b46fff0
Richard Antalik <richardantalik@gmail.com>
2025-02-07 22:41:18 +0100

    Fix: Improve retiming speed get/set code precision
    Use float for intermediate values when calculating speed or timeline
    frames.
    
    More details on precision improvement itself: In some cases, setting
    strip speed to say 70% would result in actual speed being set to say
    69.5%. With this change, the number would be closer or equal to 70%.
    
    There few tangentially related changes:
    - `SEQ_retiming_key_speed_get` was changed so both functions work in
      same time domain.
    - `SEQ_retiming_key_speed_set` now expects speed as float factor
      instead of percentage. This is consistent with get function return
       value.
    - Improve variable names, for better code readability.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/131782

--- source/blender/editors/space_sequencer/sequencer_retiming.cc
+++ source/blender/editors/space_sequencer/sequencer_retiming.cc
@@ -654,7 +654,7 @@ static int strip_speed_set_exec(bContext *C, const wmOperator *op)
       continue;
     }
     /* TODO: it would be nice to multiply speed with complex retiming by a factor. */
-    SEQ_retiming_key_speed_set(scene, strip, key, RNA_float_get(op->ptr, "speed"), false);
+    SEQ_retiming_key_speed_set(scene, strip, key, RNA_float_get(op->ptr, "speed") / 100.0f, false);
 
     ListBase *seqbase = SEQ_active_seqbase_get(SEQ_editing_get(scene));
     if (SEQ_transform_test_overlap(scene, seqbase, strip)) {
@@ -679,7 +679,7 @@ static int segment_speed_set_exec(const bContext *C,
     SEQ_retiming_key_speed_set(scene,
                                item.value,
                                item.key,
-                               RNA_float_get(op->ptr, "speed"),
+                               RNA_float_get(op->ptr, "speed") / 100.0f,
                                RNA_boolean_get(op->ptr, "keep_retiming"));
 
     if (SEQ_transform_test_overlap(scene, seqbase, item.value)) {
