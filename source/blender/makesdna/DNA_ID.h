3e03bc22787e2ae304df7e3717348634064119c3
Brecht Van Lommel <brecht@blender.org>
2025-02-07 12:45:45 +0100

    Brush Assets: Add Material to linked editable assets
    These are used to define grease pencil brushes. Not having these editable
    but the brush and images being editable leads to confusing UI.
    
    Fix #131186
    Fix #134268
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/134226

--- source/blender/makesdna/DNA_ID.h
+++ source/blender/makesdna/DNA_ID.h
@@ -656,7 +656,8 @@ typedef struct PreviewImage {
 
 #define ID_IS_LINKED(_id) (((const ID *)(_id))->lib != NULL)
 
-#define ID_TYPE_SUPPORTS_ASSET_EDITABLE(id_type) ELEM(id_type, ID_BR, ID_TE, ID_NT, ID_IM, ID_PC)
+#define ID_TYPE_SUPPORTS_ASSET_EDITABLE(id_type) \
+  ELEM(id_type, ID_BR, ID_TE, ID_NT, ID_IM, ID_PC, ID_MA)
 
 #define ID_IS_EDITABLE(_id) \
   ((((const ID *)(_id))->lib == NULL) || \
