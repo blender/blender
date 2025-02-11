623c81c7ec3d34284abfd8b85db12803745132a0
Clément Foucault <foucault.clem@gmail.com>
2025-02-11 12:54:07 +0100

    Fix: GPU: Broken gizmo color
    Some unrelated change was brought back inside
    86b70143d5a492d8bc0e5635db84faed97156721
    
    Revert the offending change.

--- source/blender/gpu/shaders/gpu_shader_colorspace_lib.glsl
+++ source/blender/gpu/shaders/gpu_shader_colorspace_lib.glsl
@@ -20,13 +20,11 @@ uniform bool srgbTarget = false;
 
 vec4 blender_srgb_to_framebuffer_space(vec4 in_color)
 {
-#ifndef USE_GPU_SHADER_CREATE_INFO
   if (srgbTarget) {
     vec3 c = max(in_color.rgb, vec3(0.0));
     vec3 c1 = c * (1.0 / 12.92);
     vec3 c2 = pow((c + 0.055) * (1.0 / 1.055), vec3(2.4));
     in_color.rgb = mix(c1, c2, step(vec3(0.04045), c));
   }
-#endif
   return in_color;
 }
