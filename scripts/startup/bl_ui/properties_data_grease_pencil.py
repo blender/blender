f702eba53f0f6a0cb5bbd2587974abd63bc85ceb
Janne Nylander <j.nylander4444@gmail.com>
2025-02-13 12:24:06 +0100

    Grease Pencil: Add python operator for masking with above/below layer
    Adds a new python operator to Grease Pencil for adding the layer
    above or below  as a masking layers to the active layer.
    
    Creates a new file for Grease Pencil python operators in
    `scripts/startup/bl_operators/grease_pencil.py`.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/134401

--- scripts/startup/bl_ui/properties_data_grease_pencil.py
+++ scripts/startup/bl_ui/properties_data_grease_pencil.py
@@ -228,6 +228,10 @@ class GREASE_PENCIL_MT_grease_pencil_add_layer_extra(Menu):
         layout.operator("grease_pencil.layer_merge", text="Merge All").mode = 'ALL'
 
         layout.separator()
+        layout.operator("grease_pencil.relative_layer_mask_add", text="Mask with Layer Above").mode = 'ABOVE'
+        layout.operator("grease_pencil.relative_layer_mask_add", text="Mask with Layer Below").mode = 'BELOW'
+
+        layout.separator()
         layout.operator("grease_pencil.layer_duplicate_object", text="Copy Layer to Selected").only_active = True
         layout.operator("grease_pencil.layer_duplicate_object", text="Copy All Layers to Selected").only_active = False
 
