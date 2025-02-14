3f6bf5ae05c4a7f6a6b31a9abd74d170eb796316
Sean Kim <SeanCTKim@protonmail.com>
2025-02-15 00:09:10 +0100

    Cleanup: Minor changes for recently added bl_object.py
    * Adds SPDX license header
    * Resets scene in more standard way
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/134543

--- tests/python/bl_object.py
+++ tests/python/bl_object.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Blender Authors
+#
+# SPDX-License-Identifier: GPL-2.0-or-later */
+
 import unittest
 
 import bpy
@@ -6,9 +10,7 @@ from mathutils import Vector
 
 class TestObjectApi(unittest.TestCase):
     def test_closest_point_on_mesh_of_default_cube(self):
-        bpy.ops.object.select_all(action='SELECT')
-        bpy.ops.object.delete(use_global=False)
-        bpy.ops.outliner.orphans_purge()
+        bpy.ops.wm.read_factory_settings(use_empty=True)
         bpy.ops.mesh.primitive_cube_add()
         ret_val = bpy.context.scene.objects[0].closest_point_on_mesh(Vector((0.0, 0.0, 2.0)))
         self.assertTrue(ret_val[0])
