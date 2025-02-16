c3d92f32dc3046bf95d73c864c98fb0ff0316146
kgalik <kgalik@3dconnexion.com>
2025-02-16 23:39:24 +0100

    NDOF: Rework input handling on macOS
    This patch modifies the logic behind handling NDOF device events on
    macOS so that it can benefit from the `Blender` profile available in
    3DConnexion driver v10.8.7 and later.
    
    A new device command was introduced: `kConnexionCmdAppEvent`, which is
    sent by the driver upong getting an apprioriate NDOF device button
    input. This allow the driver to consumes all NDOF device input and then
    send appropriate app events based on its configuration instead of
    forwarding raw data to the application directly.
    
    When using 3DConnexion driver versions prior to v10.8.7, the behavior
    is unchanged. This approach allows for supporting all of the SpaceMouse
    Enterprise buttons, long presses included (solving issue #119206 on macOS)
    
    Co-authored-by: Sergey Sharybin <sergey@blender.org>
    Pull Request: https://projects.blender.org/blender/blender/pulls/126694

--- intern/ghost/intern/GHOST_NDOFManager.cc
+++ intern/ghost/intern/GHOST_NDOFManager.cc
@@ -510,13 +510,6 @@ void GHOST_NDOFManager::updateButton(int button_number, bool press, uint64_t tim
 {
   GHOST_NDOF_ButtonT button = static_cast<GHOST_NDOF_ButtonT>(button_number);
 
-  /* For bit-mask devices button mapping isn't unified, therefore check the button map. */
-  if (std::find(bitmask_devices_.begin(), bitmask_devices_.end(), device_type_) !=
-      bitmask_devices_.end())
-  {
-    button = hid_map_[button_number];
-  }
-
   if (button == GHOST_NDOF_BUTTON_INVALID) {
     CLOG_INFO(
         LOG, 2, "button=%d, press=%d (mapped to none, ignoring!)", button_number, int(press));
@@ -578,7 +571,11 @@ void GHOST_NDOFManager::updateButtonsBitmask(int button_bits, uint64_t time)
       else {
         button_depressed_ &= ~mask; /* Clear this button's bit. */
       }
-      updateButton(button_number, press, time);
+
+      /* Bitmask devices don't have unified keymaps, so button numbers needs to be looked up in the
+       * map. */
+      int button = hid_map_[button_number];
+      updateButton(button, press, time);
     }
   }
 }
