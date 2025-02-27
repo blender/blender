20897f5f20d36952e478ba881898ee5839cb2206
Omar Emara <mail@OmarEmara.dev>
2025-02-27 11:19:24 +0200

    Cleanup: Warning about integers of different signs
    The SAT shaders in the compositor contain comparisons of different
    signed integers, use uint to fix this.

--- source/blender/compositor/shaders/compositor_summed_area_table_compute_complete_blocks.glsl
+++ source/blender/compositor/shaders/compositor_summed_area_table_compute_complete_blocks.glsl
@@ -14,7 +14,7 @@ void main()
   if (gl_LocalInvocationID.x == 0) {
     ivec2 x_prologue_texel = ivec2(gl_GlobalInvocationID.y, gl_WorkGroupID.x);
     vec4 x_accumulated_color = texture_load(complete_x_prologues_tx, x_prologue_texel, vec4(0.0));
-    for (int i = 0; i < gl_WorkGroupSize.x; i++) {
+    for (uint i = 0; i < gl_WorkGroupSize.x; i++) {
       ivec2 texel = ivec2(gl_WorkGroupID.x * gl_WorkGroupSize.x + i, gl_GlobalInvocationID.y);
       x_accumulated_color += OPERATION(texture_load(input_tx, texel, vec4(0.0)));
       block[i][gl_LocalInvocationID.y] = x_accumulated_color;
@@ -29,7 +29,7 @@ void main()
   if (gl_LocalInvocationID.y == 0) {
     ivec2 y_prologue_texel = ivec2(gl_GlobalInvocationID.x, gl_WorkGroupID.y);
     vec4 y_accumulated_color = texture_load(complete_y_prologues_tx, y_prologue_texel, vec4(0.0));
-    for (int i = 0; i < gl_WorkGroupSize.y; i++) {
+    for (uint i = 0; i < gl_WorkGroupSize.y; i++) {
       y_accumulated_color += block[gl_LocalInvocationID.x][i];
       ivec2 texel = ivec2(gl_GlobalInvocationID.x, gl_WorkGroupID.y * gl_WorkGroupSize.y + i);
       imageStore(output_img, texel, y_accumulated_color);
