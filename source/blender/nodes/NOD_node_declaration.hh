0c04952a763c5bdda693d38cc0c1acbe8a0d2431
Hans Goudey <hans@blender.org>
2025-02-27 10:14:29 -0500

    Cleanup: Specify backing type for node enums
    Otherwise it defaults to int. This change can make a few structs
    slightly smaller. Also use a signed integer type for the compositor
    enum; that's the convention for non-flag enums.

--- source/blender/nodes/NOD_node_declaration.hh
+++ source/blender/nodes/NOD_node_declaration.hh
@@ -26,7 +26,7 @@ namespace blender::nodes {
 
 class NodeDeclarationBuilder;
 
-enum class InputSocketFieldType {
+enum class InputSocketFieldType : int8_t {
   /** The input is required to be a single value. */
   None,
   /** The input can be a field. */
@@ -35,7 +35,7 @@ enum class InputSocketFieldType {
   Implicit,
 };
 
-enum class OutputSocketFieldType {
+enum class OutputSocketFieldType : int8_t {
   /** The output is always a single value. */
   None,
   /** The output is always a field, independent of the inputs. */
@@ -50,7 +50,7 @@ enum class OutputSocketFieldType {
 /**
  * An enum that maps to the #compositor::InputRealizationMode.
  */
-enum class CompositorInputRealizationMode : uint8_t {
+enum class CompositorInputRealizationMode : int8_t {
   None,
   Transforms,
   OperationDomain,
