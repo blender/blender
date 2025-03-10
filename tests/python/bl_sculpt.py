331df61e325a0d39518ae1617597cf33a243831b
Sean Kim <SeanCTKim@protonmail.com>
2025-03-10 16:27:08 +0100

    Tests: Add `sculpt.detail_flood_fill` operator test
    Conveniently, this operator serves as a useful basic test for dyntopo
    subdivision.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/135558

--- tests/python/bl_sculpt.py
+++ tests/python/bl_sculpt.py
@@ -127,6 +127,48 @@ class MaskFromCavityTest(unittest.TestCase):
                                     f"Vertex {i} should not be fully masked ({position_data[i]}) -> {mask_data[i]}")
 
 
+class DetailFloodFillTest(unittest.TestCase):
+    def setUp(self):
+        bpy.ops.wm.read_factory_settings(use_empty=True)
+        bpy.ops.ed.undo_push()
+
+        bpy.ops.mesh.primitive_cube_add()
+        bpy.ops.sculpt.sculptmode_toggle()
+        bpy.ops.sculpt.dynamic_topology_toggle()
+
+    def test_operator_subdivides_mesh(self):
+        """Test that the operator generates a mesh with appropriately sized edges."""
+
+        max_edge_length = 1.0
+        # Based on the detail_size::EDGE_LENGTH_MIN_FACTOR constant
+        min_edge_length = max_edge_length * 0.4
+
+        bpy.context.scene.tool_settings.sculpt.detail_type_method = 'CONSTANT'
+        bpy.context.scene.tool_settings.sculpt.constant_detail_resolution = max_edge_length
+
+        ret_val = bpy.ops.sculpt.detail_flood_fill()
+        self.assertEqual({'FINISHED'}, ret_val)
+
+        # Toggle to ensure the mesh data is refreshed.
+        bpy.ops.sculpt.dynamic_topology_toggle()
+
+        mesh = bpy.context.object.data
+        for edge in mesh.edges:
+            v0 = mesh.vertices[edge.vertices[0]]
+            v1 = mesh.vertices[edge.vertices[1]]
+
+            length = (v0.co - v1.co).length
+
+            self.assertGreaterEqual(
+                length,
+                min_edge_length,
+                f"Edge between {v0.index} and {v1.index} should be longer than minimum length")
+            self.assertLessEqual(
+                length,
+                max_edge_length,
+                f"Edge between {v0.index} and {v1.index} should be shorter than maximum length")
+
+
 def main():
     global args
     import argparse
