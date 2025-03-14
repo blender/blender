fed2364d183903cc5c80f4867443a34d40db1f0e
Sean Kim <SeanCTKim@protonmail.com>
2025-03-14 04:48:41 +0100

    Fix #135931: Paint brush with gravity factor can crash Blender
    This commit prevents the "Gravity" value from having an effect on
    brushes that are based on either the Paint or Smear brush in addition to
    the ones that are already filtered. Prior versions of blender don't
    crash, but still deformed the mesh, which is unexpected & undesired.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/135944

--- source/blender/editors/sculpt_paint/sculpt_intern.hh
+++ source/blender/editors/sculpt_paint/sculpt_intern.hh
@@ -988,6 +988,16 @@ inline bool brush_uses_vector_displacement(const Brush &brush)
          brush.mtex.brush_map_mode == MTEX_MAP_MODE_AREA;
 }
 
+inline bool brush_type_supports_gravity(const int tool)
+{
+  return !brush_type_is_attribute_only(tool) && !ELEM(tool,
+                                                      SCULPT_BRUSH_TYPE_BOUNDARY,
+                                                      SCULPT_BRUSH_TYPE_SMOOTH,
+                                                      SCULPT_BRUSH_TYPE_SIMPLIFY,
+                                                      SCULPT_BRUSH_TYPE_DISPLACEMENT_SMEAR,
+                                                      SCULPT_BRUSH_TYPE_DISPLACEMENT_ERASER);
+}
+
 }  // namespace blender::ed::sculpt_paint
 
 namespace blender::ed::sculpt_paint {
