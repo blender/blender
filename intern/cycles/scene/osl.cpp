990f8df7cc522558e5e12df51c699787c4af81b3
Brecht Van Lommel <brecht@blender.org>
2025-03-14 12:39:53 +0100

    Fix: Build error with OpenImageIO 2.x after OSL manager changes
    Ref #135050

--- intern/cycles/scene/osl.cpp
+++ intern/cycles/scene/osl.cpp
@@ -233,7 +233,9 @@ void OSLManager::texture_system_init()
 #  if OIIO_VERSION_MAJOR >= 3
     ts_shared = OSL::TextureSystem::create(false);
 #  else
-    ts_shared = shared_ptr(OSL::TextureSystem::create(false), OSL::TextureSystem::destroy);
+    ts_shared = std::shared_ptr<OSL::TextureSystem>(
+        OSL::TextureSystem::create(false),
+        [](OSL::TextureSystem *ts) { OSL::TextureSystem::destroy(ts); });
 #  endif
 
     ts_shared->attribute("automip", 1);
