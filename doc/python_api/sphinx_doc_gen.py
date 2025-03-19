812aaa957d2047877c3f8a505c8fc366440bdea9
tariqsulley <tariqsulley3c@gmail.com>
2025-03-19 00:38:00 +0000

    Fix #136079: Broken source file links in docs pages
    Since add-ons are no longer in their own repository,
    they don't need to have a separate base URL.
    
    Ref: !136106

--- doc/python_api/sphinx_doc_gen.py
+++ doc/python_api/sphinx_doc_gen.py
@@ -1886,7 +1886,6 @@ def pyrna2sphinx(basepath):
     # Operators.
     def write_ops():
         API_BASEURL = "https://projects.blender.org/blender/blender/src/branch/main/scripts"
-        API_BASEURL_ADDON = "https://projects.blender.org/blender/blender-addons"
 
         op_modules = {}
         op = None
@@ -1925,13 +1924,8 @@ def pyrna2sphinx(basepath):
 
                 location = op.get_location()
                 if location != (None, None):
-                    if location[0].startswith("addons_core" + os.sep):
-                        url_base = API_BASEURL_ADDON
-                    else:
-                        url_base = API_BASEURL
-
                     fw("   :File: `{:s}\\:{:d} <{:s}/{:s}#L{:d}>`__\n\n".format(
-                        location[0], location[1], url_base, location[0], location[1]
+                        location[0], location[1], API_BASEURL, location[0], location[1]
                     ))
 
                 if op.args:
