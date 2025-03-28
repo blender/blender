2ad17b956a38fecd94c5939dd50ee8bf76692324
Sean Kim <SeanCTKim@protonmail.com>
2025-03-28 22:28:59 +0100

    BLI: Add rolling average support to ScopedTimerAverage
    Adds a new argument for the class constructor as well as a new macro to
    support printing the rolling average of a number of samples intead of
    the total average.
    
    This can be useful for getting a sample of an average durations in
    functions that are continually running, e.g. the main event loop.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/136530

--- source/blender/blenlib/intern/timeit.cc
+++ source/blender/blenlib/intern/timeit.cc
@@ -64,11 +64,24 @@ ScopedTimerAveraged::~ScopedTimerAveraged()
 
   total_count_++;
   total_time_ += duration;
+
+  if (!window_size_ || total_count_ < window_size_) {
+    rolling_average_ = total_time_ / total_count_;
+  }
+  else {
+    rolling_average_ = (rolling_average_ * (window_size_.value() - 1) / window_size_.value()) +
+                       (duration / window_size_.value());
+  }
+
   min_time_ = std::min(duration, min_time_);
 
   fmt::memory_buffer buf;
   fmt::format_to(fmt::appender(buf), FMT_STRING("Timer '{}': (Average: "), name_);
-  format_duration(total_time_ / total_count_, buf);
+  format_duration(rolling_average_, buf);
+  if (window_size_) {
+    fmt::format_to(
+        fmt::appender(buf), " of last {} events", std::min(window_size_.value(), total_count_));
+  }
   buf.append(StringRef(", Min: "));
   format_duration(min_time_, buf);
   buf.append(StringRef(", Last: "));
