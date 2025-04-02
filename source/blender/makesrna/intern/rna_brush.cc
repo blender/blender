bb7e8898883607b0f7fcfe0debd82ba1ff8d58db
Sean Kim <SeanCTKim@protonmail.com>
2025-04-02 21:39:54 +0200

    Sculpt: Allow negative values for tilt_strength_factor
    Part of #82877
    
    * Adds to description to indicate new functionality.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/136856

--- source/blender/makesrna/intern/rna_brush.cc
+++ source/blender/makesrna/intern/rna_brush.cc
@@ -3277,10 +3277,12 @@ static void rna_def_brush(BlenderRNA *brna)
   prop = RNA_def_property(srna, "tilt_strength_factor", PROP_FLOAT, PROP_FACTOR);
   RNA_def_property_float_sdna(prop, nullptr, "tilt_strength_factor");
   RNA_def_property_float_default(prop, 0);
-  RNA_def_property_range(prop, 0.0f, 1.0f);
-  RNA_def_property_ui_range(prop, 0.0f, 1.0f, 0.001, 3);
-  RNA_def_property_ui_text(
-      prop, "Tilt Strength", "How much the tilt of the pen will affect the brush");
+  RNA_def_property_range(prop, -1.0f, 1.0f);
+  RNA_def_property_ui_range(prop, -1.0f, 1.0f, 0.001, 3);
+  RNA_def_property_ui_text(prop,
+                           "Tilt Strength",
+                           "How much the tilt of the pen will affect the brush. Negative values "
+                           "indicate inverting the tilt directions. ");
   RNA_def_property_update(prop, 0, "rna_Brush_update");
 
   prop = RNA_def_property(srna, "normal_radius_factor", PROP_FLOAT, PROP_FACTOR);
