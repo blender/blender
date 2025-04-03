3ce223f5bb4bf9637169afa031c46a447bcceea1
Nathan Vegdahl <cessen@cessen.com>
2025-04-03 12:26:30 +0200

    Fix #136887: collapsed channels not getting filtered properly
    When the user filters channels by name, collapsed channel groups weren't getting
    filtered properly.
    
    For example, consider the following situation:
    - You have a channel group "Transforms" with the F-Curve "X Location" under it.
    - You use the name filtering feature with the string "Z".
    
    The single F-Curve under the group will get filtered out by the string, and thus
    should be hidden. That also means that the group, now with no visible f-curves,
    should also no longer be displayed.
    
    With this bug, if the channel was expanded then this filtering still proceeded
    correctly, and both the f-curve and the group were hidden. However, if the group
    was *collapsed* then the group would *not* get filtered out, which is incorrect.
    
    The underlying cause is that an early-out "we found an un-filtered fcurve,
    that's all the info we need with these flags!" clause in the f-curve filtering
    code was happening too early, so it wasn't accounting for name-based filtering.
    This PR fixes the issue by moving that early-out code to *after* the name-based
    filtering so that it's properly accounted for.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/136929

--- source/blender/editors/animation/anim_filter.cc
+++ source/blender/editors/animation/anim_filter.cc
@@ -1395,11 +1395,6 @@ static size_t animfilter_fcurves_span(bAnimContext *ac,
       continue;
     }
 
-    if (filter_mode & ANIMFILTER_TMP_PEEK) {
-      /* Found an animation channel, which is good enough for the 'TMP_PEEK' mode. */
-      return 1;
-    }
-
     bAnimListElem *ale = make_new_animlistelem(
         ac->bmain, fcu, ANIMTYPE_FCURVE, animated_id, fcurve_owner_id);
 
@@ -1408,6 +1403,12 @@ static size_t animfilter_fcurves_span(bAnimContext *ac,
     if (filter_by_name && !ale_name_matches_dopesheet_filter(*ac->ads, *ale)) {
       MEM_freeN(ale);
       continue;
+    }
+
+    if (filter_mode & ANIMFILTER_TMP_PEEK) {
+      /* Found an animation channel, which is good enough for the 'TMP_PEEK' mode. */
+      MEM_freeN(ale);
+      return 1;
     }
 
     /* bAnimListElem::slot_handle is exposed as int32_t and not as slot_handle_t, so better
