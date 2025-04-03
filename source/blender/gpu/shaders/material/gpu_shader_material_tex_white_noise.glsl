0b67bbc16afd79d7743c6b256466d50790edd7d1
Omar Emara <mail@OmarEmara.dev>
2025-04-03 19:09:58 +0200

    Fix #136812: Glitches in White Noise node in compositor
    The White Noise node in the compositor glitches. That's because the
    alpha channel in GPU code is uninitialized. So we simply initialize it
    to 1.

--- source/blender/gpu/shaders/material/gpu_shader_material_tex_white_noise.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_tex_white_noise.glsl
@@ -9,23 +9,23 @@
 void node_white_noise_1d(vec3 vector, float w, out float value, out vec4 color)
 {
   value = hash_float_to_float(w);
-  color.xyz = hash_float_to_vec3(w);
+  color = vec4(hash_float_to_vec3(w), 1.0);
 }
 
 void node_white_noise_2d(vec3 vector, float w, out float value, out vec4 color)
 {
   value = hash_vec2_to_float(vector.xy);
-  color.xyz = hash_vec2_to_vec3(vector.xy);
+  color = vec4(hash_vec2_to_vec3(vector.xy), 1.0);
 }
 
 void node_white_noise_3d(vec3 vector, float w, out float value, out vec4 color)
 {
   value = hash_vec3_to_float(vector);
-  color.xyz = hash_vec3_to_vec3(vector);
+  color = vec4(hash_vec3_to_vec3(vector), 1.0);
 }
 
 void node_white_noise_4d(vec3 vector, float w, out float value, out vec4 color)
 {
   value = hash_vec4_to_float(vec4(vector, w));
-  color.xyz = hash_vec4_to_vec3(vec4(vector, w));
+  color = vec4(hash_vec4_to_vec3(vec4(vector, w)), 1.0);
 }
