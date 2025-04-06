96181c55503bba0fa89611c4f888ae424dee37c2
Campbell Barton <campbell@blender.org>
2025-04-06 22:07:07 +0000

    Revert "ImBuf: remove ".tx" extension from the list of supported images"
    This reverts commit cf55a0acdc11ecae2b0ffa19fb03210e25858bf9.
    
    While this was added with the commit that added support for tile cache,
    keep the extension check as Blender can load them.

--- source/blender/imbuf/intern/util.cc
+++ source/blender/imbuf/intern/util.cc
@@ -26,7 +26,7 @@
 #define UTIL_DEBUG 0
 
 const char *imb_ext_image[] = {
-    ".png",  ".tga", ".bmp", ".jpg", ".jpeg", ".sgi", ".rgb", ".rgba", ".tif", ".tiff",
+    ".png",  ".tga", ".bmp", ".jpg", ".jpeg", ".sgi", ".rgb", ".rgba", ".tif", ".tiff", ".tx",
 #ifdef WITH_OPENJPEG
     ".jp2",  ".j2c",
 #endif
