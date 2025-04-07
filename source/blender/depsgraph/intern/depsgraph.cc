ea13b19738848d709d522d8012b5b1997fe34b5f
Brecht Van Lommel <brecht@blender.org>
2025-04-07 13:46:24 +0200

    Cleanup: Compiler warning about initialization order

--- source/blender/depsgraph/intern/depsgraph.cc
+++ source/blender/depsgraph/intern/depsgraph.cc
@@ -53,12 +53,12 @@ Depsgraph::Depsgraph(Main *bmain, Scene *scene, ViewLayer *view_layer, eEvaluati
       ctime(BKE_scene_ctime_get(scene)),
       scene_cow(nullptr),
       is_active(false),
-      sync_writeback(DEG_EVALUATE_SYNC_WRITEBACK_NO),
       use_visibility_optimization(true),
       is_evaluating(false),
       is_render_pipeline_depsgraph(false),
       use_editors_update(false),
-      update_count(0)
+      update_count(0),
+      sync_writeback(DEG_EVALUATE_SYNC_WRITEBACK_NO)
 {
   BLI_spin_init(&lock);
   memset(id_type_updated, 0, sizeof(id_type_updated));
