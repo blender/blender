6f6df389a14372cdc3545dd77100310cb865d102
Jeroen Bakker <jeroen@blender.org>
2025-04-10 09:32:48 +0200

    Cleanup: OpenXR: Move Direct3D to its own compile unit
    _No response_
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/137255

---
+++ intern/ghost/intern/GHOST_XrGraphicsBindingD3D.hh
@@ -0,0 +1,71 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup GHOST
+ */
+#pragma once
+
+#ifndef _WIN32
+#  error "GHOST_XrGraphcisBindingD3D can only be compiled on Windows platforms."
+#endif
+
+#include "GHOST_ContextD3D.hh"
+#include "GHOST_ContextWGL.hh"
+#include "GHOST_IXrGraphicsBinding.hh"
+
+/**
+ * Base class for bridging to an OpenXR platform that only supports Direct3D.
+ *
+ * OpenGL/Vulkan have their own specific implementations.
+ */
+class GHOST_XrGraphicsBindingD3D : public GHOST_IXrGraphicsBinding {
+ public:
+  GHOST_XrGraphicsBindingD3D();
+  ~GHOST_XrGraphicsBindingD3D() override;
+
+  /**
+   * Check the version requirements to use OpenXR with the Vulkan backend.
+   */
+  bool checkVersionRequirements(GHOST_Context &ghost_ctx,
+                                XrInstance instance,
+                                XrSystemId system_id,
+                                std::string *r_requirement_info) const override;
+
+  void initFromGhostContext(GHOST_Context &ghost_ctx,
+                            XrInstance instance,
+                            XrSystemId system_id) override;
+  std::optional<int64_t> chooseSwapchainFormat(const std::vector<int64_t> &runtime_formats,
+                                               GHOST_TXrSwapchainFormat &r_format,
+                                               bool &r_is_srgb_format) const override;
+  std::vector<XrSwapchainImageBaseHeader *> createSwapchainImages(uint32_t image_count) override;
+
+  bool needsUpsideDownDrawing(GHOST_Context &ghost_ctx) const override;
+
+ protected:
+  /** Secondary DirectX 11 context used by OpenXR. */
+  GHOST_ContextD3D *m_ghost_d3d_ctx = nullptr;
+
+  std::list<std::vector<XrSwapchainImageD3D11KHR>> m_image_cache;
+};
+
+/**
+ * OpenXR bridge between OpenGL and D3D.
+ *
+ * The D3D swapchain image is imported into OpenGL.
+ */
+class GHOST_XrGraphicsBindingOpenGLD3D : public GHOST_XrGraphicsBindingD3D {
+ public:
+  GHOST_XrGraphicsBindingOpenGLD3D(GHOST_Context &ghost_ctx);
+  ~GHOST_XrGraphicsBindingOpenGLD3D();
+
+  void submitToSwapchainImage(XrSwapchainImageBaseHeader &swapchain_image,
+                              const GHOST_XrDrawViewInfo &draw_info) override;
+
+ private:
+  /** Primary OpenGL context for Blender to use for drawing. */
+  GHOST_ContextWGL &m_ghost_wgl_ctx;
+  /** Handle to shared resource object. */
+  GHOST_SharedOpenGLResource *m_shared_resource = nullptr;
+};
