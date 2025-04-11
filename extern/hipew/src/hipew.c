ed1ccefb95bf0c617bdb34995181318524f4219d
Sahar A. Kashi <salipour@noreply.localhost>
2025-04-11 14:11:23 +0200

    Handling SDK/ROCm 6+ lack of backward compatibility with pre ROCm 6 in Blender 4.2
    This commit intends to port changes introduced in https://projects.blender.org/blender/blender/pulls/130153 to blender-v4.2. The update primarily targets Linux, and it's best to keep kernel binaries on Windows compiled with SDK 5.7. This is due to how HIP-RT is distributed on Windows before Blender v4.3—via the AMD driver package. The HIP-RT version shipped with the driver is based on SDK 5.7, and mixing versions could result in undefined behaviour.
    
    Co-authored-by: salipourto <sahar.alipourkashi@amd.com>
    Pull Request: https://projects.blender.org/blender/blender/pulls/134892

--- extern/hipew/src/hipew.c
+++ extern/hipew/src/hipew.c
@@ -38,7 +38,11 @@ thipDriverGetVersion *hipDriverGetVersion;
 thipRuntimeGetVersion *hipRuntimeGetVersion;
 thipGetDevice *hipGetDevice;
 thipGetDeviceCount *hipGetDeviceCount;
-thipGetDeviceProperties *hipGetDeviceProperties;
+#ifdef WITH_HIP_SDK_5
+  thipGetDeviceProperties *hipGetDeviceProperties;
+#else
+  thipGetDevicePropertiesR0600 *hipGetDevicePropertiesR0600;
+#endif
 thipDeviceGet* hipDeviceGet;
 thipDeviceGetName *hipDeviceGetName;
 thipDeviceGetAttribute *hipDeviceGetAttribute;
@@ -233,20 +237,29 @@ static int hipewHipInit(void) {
   /* Library paths. */
 #ifdef _WIN32
   /* Expected in C:/Windows/System32 or similar, no path needed. */
+#ifdef WITH_HIP_SDK_5
   const char *hip_paths[] = {"amdhip64.dll", "amdhip64_6.dll", NULL};
-
+#else
+  const char* hip_paths[] = {"amdhip64_6.dll", NULL };
+#endif
 #elif defined(__APPLE__)
   /* Default installation path. */
   const char *hip_paths[] = {"", NULL};
 #else
   /* ROCm 6 changes paths from /opt/rocm/hip/lib to /opt/rocm/lib, so
    * search for libraries there. It still includes .so.5. */
-  const char *hip_paths[] = {"libamdhip64.so.5",
-                             "/opt/rocm/lib/libamdhip64.so.5",
-                             "/opt/rocm/hip/lib/libamdhip64.so.5",
-                             "libamdhip64.so",
-                             "/opt/rocm/lib/libamdhip64.so",
-                             "/opt/rocm/hip/lib/libamdhip64.so", NULL};
+#ifdef WITH_HIP_SDK_5
+  const char* hip_paths[] = { "libamdhip64.so.5",
+                           "/opt/rocm/lib/libamdhip64.so.5",
+                           "/opt/rocm/hip/lib/libamdhip64.so.5",
+                            NULL };
+#else
+  const char* hip_paths[] = { "libamdhip64.so.6",
+                              "/opt/rocm/lib/libamdhip64.so.6",
+                              "/opt/rocm/hip/lib/libamdhip64.so.6",
+                               NULL };
+
+#endif
 #endif
   static int initialized = 0;
   static int result = 0;
@@ -281,6 +294,11 @@ static int hipewHipInit(void) {
   }
 
   /* Fetch all function pointers. */
+#ifdef WITH_HIP_SDK_5
+  HIP_LIBRARY_FIND_CHECKED(hipGetDeviceProperties);
+#else
+  HIP_LIBRARY_FIND_CHECKED(hipGetDevicePropertiesR0600);
+#endif
   HIP_LIBRARY_FIND_CHECKED(hipGetErrorName);
   HIP_LIBRARY_FIND_CHECKED(hipGetErrorString);
   HIP_LIBRARY_FIND_CHECKED(hipGetLastError);
@@ -289,7 +307,6 @@ static int hipewHipInit(void) {
   HIP_LIBRARY_FIND_CHECKED(hipRuntimeGetVersion);
   HIP_LIBRARY_FIND_CHECKED(hipGetDevice);
   HIP_LIBRARY_FIND_CHECKED(hipGetDeviceCount);
-  HIP_LIBRARY_FIND_CHECKED(hipGetDeviceProperties);
   HIP_LIBRARY_FIND_CHECKED(hipDeviceGet);
   HIP_LIBRARY_FIND_CHECKED(hipDeviceGetName);
   HIP_LIBRARY_FIND_CHECKED(hipDeviceGetAttribute);
