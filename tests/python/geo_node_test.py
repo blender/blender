c11055c1cf0dfba4406a17e5ab592eddfafff5e9
Jacques Lucke <jacques@blender.org>
2025-04-13 13:55:23 +0200

    Geometry Nodes: enable closure regression tests
    Some special care had to be taken because these tests require an experimental
    feature to be turned on to work.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/137414

--- tests/python/geo_node_test.py
+++ tests/python/geo_node_test.py
@@ -5,9 +5,17 @@
 
 import os
 import sys
+import bpy
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from modules.mesh_test import BlendFileTest
+
+if "closure" in bpy.data.filepath:
+    if bpy.app.version_cycle == "alpha":
+        bpy.context.preferences.experimental.use_bundle_and_closure_nodes = True
+    else:
+        print("Skipped because bundles and closures are still experimental.")
+        sys.exit(0)
 
 geo_node_test = BlendFileTest("test_object", "expected_object", threshold=1e-4)
 result = geo_node_test.run_test()
