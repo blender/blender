719351a9e691f25d1c052219484251f5aaeffd84
Miguel Pozo <pragma37@gmail.com>
2025-04-14 17:32:01 +0200

    Fix #137427: Overlay: Darker curve sculpt overlay
    Use alpha blend (same as legacy).
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/137480

--- source/blender/draw/engines/overlay/overlay_next_sculpt.hh
+++ source/blender/draw/engines/overlay/overlay_next_sculpt.hh
@@ -72,7 +72,7 @@ class Sculpts : Overlay {
       sculpt_mask_.bind_ubo(OVERLAY_GLOBALS_SLOT, &res.globals_buf);
       sculpt_mask_.bind_ubo(DRW_CLIPPING_UBO_SLOT, &res.clip_planes_buf);
       sculpt_mask_.state_set(DRW_STATE_WRITE_COLOR | DRW_STATE_DEPTH_LESS_EQUAL |
-                                 DRW_STATE_BLEND_MUL,
+                                 DRW_STATE_BLEND_ALPHA,
                              state.clipping_plane_count);
       {
         auto &sub = sculpt_mask_.sub("Mesh");
