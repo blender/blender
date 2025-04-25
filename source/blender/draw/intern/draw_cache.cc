48f9212483ed246fbad3fa57c06a261a6a13cdc7
YimingWu <xp8110@outlook.com>
2025-04-25 13:07:54 +0200

    Fix #137700: Guard null `lt.editlatt` in `DRW_cache_lattice_wire_get`
    Switching to lattice edit mode when it has an armature modifier can
    crash if the armature modifier's `show_in_editmode` is turned on. Now
    prevent null `editlatt` access.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/137701

--- source/blender/draw/intern/draw_cache.cc
+++ source/blender/draw/intern/draw_cache.cc
@@ -403,7 +403,9 @@ blender::gpu::Batch *DRW_cache_lattice_wire_get(Object *ob, bool use_weight)
   Lattice &lt = DRW_object_get_data_for_drawing<Lattice>(*ob);
   int actdef = -1;
 
-  if (use_weight && !BLI_listbase_is_empty(&lt.vertex_group_names) && lt.editlatt->latt->dvert) {
+  if (use_weight && !BLI_listbase_is_empty(&lt.vertex_group_names) && lt.editlatt &&
+      lt.editlatt->latt->dvert)
+  {
     actdef = lt.vertex_group_active_index - 1;
   }
 
