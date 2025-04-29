b23e5e6068aaba5d7614fa40d070118cc356bd19
airyz <airyz@proton.me>
2025-04-29 06:28:47 +0200

    UI: Expose MOD_MESHSEQ_READ_ATTRIBUTES flag for Mesh Sequence Cache modifier
    There is an internal flag which gets set only during USD import,
    which allows for custom attributes to be read from Mesh Sequence Cache.
    This flag is not shown anywhere in the UI, and is not accessible through
    python, which can lead to confusing behavior.
    
    For example, loading a .usd file through `bpy.ops.wm.usd_import` will read
    custom attributes, but when  `Mesh Sequence Cache` is added manually then
    loading the same usd file through `bpy.ops.cachefile.open` will not load
    custom attributes.
    
    Further, if the user edits `object.modifiers['MeshSequenceCache'].read_data`
    then `MOD_MESHSEQ_READ_ATTRIBUTES` flag will be removed with no way
    to bring it back.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/132475

--- source/blender/makesrna/intern/rna_modifier.cc
+++ source/blender/makesrna/intern/rna_modifier.cc
@@ -6981,6 +6981,7 @@ static void rna_def_modifier_meshseqcache(BlenderRNA *brna)
       {MOD_MESHSEQ_READ_POLY, "POLY", 0, "Faces", ""},
       {MOD_MESHSEQ_READ_UV, "UV", 0, "UV", ""},
       {MOD_MESHSEQ_READ_COLOR, "COLOR", 0, "Color", ""},
+      {MOD_MESHSEQ_READ_ATTRIBUTES, "ATTRIBUTES", 0, "Attributes", ""},
       {0, nullptr, 0, nullptr, nullptr},
   };
 
