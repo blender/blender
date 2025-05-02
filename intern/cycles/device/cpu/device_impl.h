9eb9493ecda9416dded3f69680c0b304f36ea30b
Campbell Barton <campbell@blender.org>
2025-05-02 04:22:17 +0000

    Fix: build error WITH_CYCLES_OSL & GCC 15.1
    Ref !138238

--- intern/cycles/device/cpu/device_impl.h
+++ intern/cycles/device/cpu/device_impl.h
@@ -6,6 +6,7 @@
 
 /* So ImathMath is included before our kernel_cpu_compat. */
 #ifdef WITH_OSL
+#  include <cstdint> /* Needed before `sdlexec.h` for `int32_t` with GCC 15.1. */
 /* So no context pollution happens from indirectly included windows.h */
 #  ifdef _WIN32
 #    include "util/windows.h"
--- intern/cycles/scene/shader.h
+++ intern/cycles/scene/shader.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #ifdef WITH_OSL
+#  include <cstdint> /* Needed before `sdlexec.h` for `int32_t` with GCC 15.1. */
 /* So no context pollution happens from indirectly included windows.h */
 #  ifdef _WIN32
 #    include "util/windows.h"
