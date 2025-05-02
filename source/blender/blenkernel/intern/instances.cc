ed68c0a6350998d14f4e06bf92438ba6b8e9478e
Hans Goudey <hans@blender.org>
2025-05-02 10:11:22 -0400

    Cleanup: Clarify instances almost unique IDs cache
    Use the safer VArraySpan instead of get_internal_span(). And remove
    the size check for the output data. If the callback inside `ensure` is
    called it means that recomputing the data is necessary, regardless of
    the array's current size.

--- source/blender/blenkernel/intern/instances.cc
+++ source/blender/blenkernel/intern/instances.cc
@@ -505,17 +505,13 @@ Span<int> Instances::reference_user_counts() const
 Span<int> Instances::almost_unique_ids() const
 {
   almost_unique_ids_cache_.ensure([&](Array<int> &r_data) {
-    bke::AttributeReader<int> instance_ids_attribute = this->attributes().lookup<int>("id");
-    if (instance_ids_attribute) {
-      Span<int> instance_ids = instance_ids_attribute.varray.get_internal_span();
-      if (r_data.size() != instance_ids.size()) {
-        r_data = generate_unique_instance_ids(instance_ids);
-      }
-    }
-    else {
+    const VArraySpan<int> instance_ids = *this->attributes().lookup<int>("id");
+    if (instance_ids.is_empty()) {
       r_data.reinitialize(instances_num_);
       array_utils::fill_index_range(r_data.as_mutable_span());
+      return;
     }
+    r_data = generate_unique_instance_ids(instance_ids);
   });
   return almost_unique_ids_cache_.data();
 }
