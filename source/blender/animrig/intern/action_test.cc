c271c3b2917b042b02308beb23b355f5c50ab772
Jacques Lucke <jacques@blender.org>
2025-05-05 11:47:36 +0200

    Fix: ID code not should not be passed as part of name
    Found in #138383.

--- source/blender/animrig/intern/action_test.cc
+++ source/blender/animrig/intern/action_test.cc
@@ -31,7 +31,7 @@ namespace blender::animrig::tests {
 
 TEST(action, low_level_initialisation)
 {
-  bAction *action = static_cast<bAction *>(BKE_id_new_nomain(ID_AC, "ACNewAction"));
+  bAction *action = static_cast<bAction *>(BKE_id_new_nomain(ID_AC, "NewAction"));
 
   EXPECT_NE(action->last_slot_handle, 0)
       << "bAction::last_slot_handle should not be initialised to 0";
