767cf78dda55f5e1bbb829f7130428a4b5e82614
Aras Pranckevicius <aras@nesnausk.org>
2025-05-07 16:31:58 +0300

    Cleanup: factor out 4.1 versioning code into versioning_410.cc

--- source/blender/blenloader/intern/readfile.cc
+++ source/blender/blenloader/intern/readfile.cc
@@ -3096,6 +3096,9 @@ static void do_versions(FileData *fd, Library *lib, Main *main)
     blo_do_versions_400(fd, lib, main);
   }
   if (!main->is_read_invalid) {
+    blo_do_versions_410(fd, lib, main);
+  }
+  if (!main->is_read_invalid) {
     blo_do_versions_450(fd, lib, main);
   }
 
@@ -3142,6 +3145,9 @@ static void do_versions_after_linking(FileData *fd, Main *main)
   }
   if (!main->is_read_invalid) {
     do_versions_after_linking_400(fd, main);
+  }
+  if (!main->is_read_invalid) {
+    do_versions_after_linking_410(fd, main);
   }
   if (!main->is_read_invalid) {
     do_versions_after_linking_450(fd, main);
