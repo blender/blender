blender/blender

commit 53d076612a91582ed7f4918919f239db8c654834

Tools: add a hash-length argument for the weekly reports generator
Allows generating reports without abbreviated hashes.

tools/triage/weekly_report.py
@@ -82,6 +82,14 @@ def argparse_create() -> argparse.ArgumentParser:
     )
 
     parser.add_argument(
+        "--hash-length",
+        dest="hash_length",
+        type=int,
+        default=10,
+        help="Number of characters to abbreviate the hash to (0 to disable).",
+    )
+
+    parser.add_argument(
         "-v",
         "--verbose",
         action="store_true",
@@ -91,7 +99,13 @@ def argparse_create() -> argparse.ArgumentParser:
     return parser
 
 
-def report_personal_weekly_get(username: str, start: datetime.datetime, verbose: bool = True) -> None:
+def report_personal_weekly_get(
+        username: str,
+        start: datetime.datetime,
+        *,
+        hash_length: int,
+        verbose: bool = True,
+) -> None:
 
     data_cache: Dict[str, Dict[str, Any]] = {}
 
@@ -174,7 +188,9 @@ def report_personal_weekly_get(username: str, start: datetime.datetime, verbose:
                         # Substitute occurrences of "#\d+" with "repo#\d+"
                         title = re.sub(r"#(\d+)", rf"{repo_fullname}#\1", title)
 
-                        hash_value = commits["Sha1"][:10]
+                        hash_value = commits["Sha1"]
+                        if hash_length > 0:
+                            hash_value[:hash_length]
                         commits_main.append(f"{title} ({repo_fullname}@{hash_value})")
 
     date_end = date_curr
@@ -341,7 +357,12 @@ def main() -> None:
     end_date_str = str(sunday.day) if start_date.month == sunday.month else sunday.strftime('%B ') + str(sunday.day)
 
     print(f"## {start_date_str} - {end_date_str}\n")
-    report_personal_weekly_get(username, start_date, verbose=args.verbose)
+    report_personal_weekly_get(
+        username,
+        start_date,
+        hash_length=args.hash_length,
+        verbose=args.verbose,
+    )
 
 
 if __name__ == "__main__":

commit 9128348e4837afda7c1aebe7ee5478fca1cc1494

Fix: Tools: Commit hashes not shortened in weekly report script
Broken since 53d076612a.

tools/triage/weekly_report.py
@@ -193,7 +193,7 @@ def report_personal_weekly_get(
 
                         hash_value = commits["Sha1"]
                         if hash_length > 0:
-                            hash_value[:hash_length]
+                            hash_value = hash_value[:hash_length]
                         commits_main.append(f"{title} ({repo_fullname}@{hash_value})")
 
     date_end = date_curr