f2327a956b9f8887d26c01babef6f0e49cfd40cd
Hans Goudey <hans@blender.org>
2025-05-12 16:38:07 -0400

    Cleanup: Remove disabled code
    This only makes this function even more difficult to understand.

--- source/blender/draw/intern/draw_cache_impl_mesh.cc
+++ source/blender/draw/intern/draw_cache_impl_mesh.cc
@@ -219,12 +219,6 @@ static DRW_MeshCDMask mesh_cd_calc_used_gpu_layers(const Object &object,
           layer = CustomData_get_named_layer(&cd_ldata, CD_PROP_FLOAT2, name);
           type = CD_MTFACE;
 
-#if 0 /* Tangents are always from UVs - this will never happen. */
-          if (layer == -1) {
-            layer = CustomData_get_named_layer(cd_ldata, CD_TANGENT, name);
-            type = CD_TANGENT;
-          }
-#endif
           if (layer == -1) {
             /* Try to match a generic attribute, we use the first attribute domain with a
              * matching name. */
