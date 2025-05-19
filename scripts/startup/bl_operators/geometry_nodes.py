5180930e0d9b1a9741f64755db12fe862cb6b5e0
Lukas Tönne <lukas@blender.org>
2025-05-19 15:49:45 +0200

    Fix #138933: Support assigning a default value to menus wrapping node groups
    The modifier input value for menu items is an int property, which is not
    compatible with the enum property used in node group socket definitions for
    menus. Wrapping a node group with a menu will try to assign the int value from
    the modifier directly to the enum, which expects a string identifier.
    
    To aquire the matching string identifier, use the UI properties in the modifier
    RNA, which contain the complete enum item definition.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/139074

--- scripts/startup/bl_operators/geometry_nodes.py
+++ scripts/startup/bl_operators/geometry_nodes.py
@@ -182,7 +182,20 @@ def create_wrapper_group(operator, modifier, old_group):
             output_socket = get_enabled_socket_with_name(input_node.outputs, "Attribute")
             group.links.new(output_socket, group_node_input)
         elif hasattr(input_socket, "default_value"):
-            group_node_input.default_value = modifier[identifier]
+            # Special case for menu sockets: the modifier property is just the int
+            # value, which must be converted to the enum identifier to set the new
+            # interface default value. Use the RNA definition of the modifier property
+            # UI to get that identifier.
+            if input_socket.socket_type == 'NodeSocketMenu':
+                default_value_int = modifier[identifier]
+                menu_enum_items = modifier.id_properties_ui(identifier).as_dict()['items']
+                # Tuples have same order as in bpy.props.EnumProperty: (identifier, name, description, icon, number).
+                # In the case of an unconnected menu socket there will be one valid "DUMMY" item only.
+                if len(menu_enum_items) > 1:
+                    default_value_enum_item = next(item for item in menu_enum_items if item[4] == default_value_int)
+                    group_node_input.default_value = default_value_enum_item[0]
+            else:
+                group_node_input.default_value = modifier[identifier]
 
     if first_geometry_input:
         group.links.new(
