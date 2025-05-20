blender/blender

commit c6995d03dd30d409a9cef77706b1007d74de4a54

Nodes: Show error when image node cannot be added
Currently, if a user drags an image file into a node editor with
no node tree, then the operator silently fails.

This patch adds an error to warn the user that adding an image is
only possible if a node tree exists already.

Pull Request: https://projects.blender.org/blender/blender/pulls/138997

source/blender/editors/space_node/node_add.cc
@@ -699,11 +699,13 @@ void NODE_OT_add_collection(wmOperatorType *ot)
 static bool node_add_image_poll(bContext *C)
 {
   const SpaceNode *snode = CTX_wm_space_node(C);
-  return ED_operator_node_editable(C) && STR_ELEM(snode->tree_idname,
-                                                  "ShaderNodeTree",
-                                                  "CompositorNodeTree",
-                                                  "TextureNodeTree",
-                                                  "GeometryNodeTree");
+
+  /* Note: Validity of snode->nodetree is checked later for better error reporting. */
+  return STR_ELEM(snode->tree_idname,
+                  "ShaderNodeTree",
+                  "CompositorNodeTree",
+                  "TextureNodeTree",
+                  "GeometryNodeTree");
 }
 
 /** Node stack animation data, sorts nodes so each node is placed on top of each other. */
@@ -860,6 +862,13 @@ static wmOperatorStatus node_add_image_invoke(bContext *C, wmOperator *op, const
 {
   ARegion *region = CTX_wm_region(C);
   SpaceNode *snode = CTX_wm_space_node(C);
+
+  if (!ED_operator_node_editable(C)) {
+    BKE_report(op->reports,
+               RPT_ERROR,
+               "Could not add image. A node tree has not been created or assigned");
+    return OPERATOR_CANCELLED;
+  }
 
   /* Convert mouse coordinates to `v2d` space. */
   UI_view2d_region_to_view(&region->v2d,

commit c0afb85a2c59d59795ec2c56483f415ff4ce7696

Fix: Menu search crash outside of node editor
Caused by c6995d03dd30d409a9cef77706b1007d74de4a54.

source/blender/editors/space_node/node_add.cc
@@ -699,6 +699,9 @@ void NODE_OT_add_collection(wmOperatorType *ot)
 static bool node_add_image_poll(bContext *C)
 {
   const SpaceNode *snode = CTX_wm_space_node(C);
+  if (!snode) {
+    return false;
+  }
 
   /* Note: Validity of snode->nodetree is checked later for better error reporting. */
   return STR_ELEM(snode->tree_idname,