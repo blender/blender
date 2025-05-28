5106c4e6557f071bbfd53fafd34047ddf7c61e88
Sebastian Parborg <sebastian@blender.org>
2025-05-28 19:31:22 +0200

    JACK: Fix a mutex deadlock when trying to start/stop playback

--- source/blender/windowmanager/intern/wm_init_exit.cc
+++ source/blender/windowmanager/intern/wm_init_exit.cc
@@ -187,10 +187,8 @@ static void sound_jack_sync_callback(Main *bmain, int mode, double time)
     if (depsgraph == nullptr) {
       continue;
     }
-    BKE_sound_lock();
     Scene *scene_eval = DEG_get_evaluated_scene(depsgraph);
     BKE_sound_jack_scene_update(scene_eval, mode, time);
-    BKE_sound_unlock();
   }
 }
 
