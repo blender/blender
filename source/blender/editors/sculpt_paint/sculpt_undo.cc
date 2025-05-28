71330894b7b6ba5b4ca3b9964a71838fe6d17a7d
Sean Kim <SeanCTKim@protonmail.com>
2025-05-29 00:57:44 +0200

    Cleanup: Remove unused variables in sculpt_undo.cc
    Previously, the undo type was stored at the node level, so undo pushes
    inside sculpt mode that didn't need nodes still appended this data so
    that they could be appropriately filtered out. This commit removes
    instances where this was the case and clarifies other checks that used
    the vector of undo nodes.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/130479

--- source/blender/editors/sculpt_paint/sculpt_undo.cc
+++ source/blender/editors/sculpt_paint/sculpt_undo.cc
@@ -838,6 +838,7 @@ static void restore_list(bContext *C, Depsgraph *depsgraph, StepData &step_data)
 
   /* Switching to sculpt mode does not push a particular type.
    * See #124484. */
+  /* TODO: Add explicit type for switching into Sculpt Mode. */
   if (step_data.type == Type::None && step_data.nodes.is_empty()) {
     return;
   }
@@ -1459,11 +1460,12 @@ BLI_NOINLINE static void bmesh_push(const Object &object,
 
   std::scoped_lock lock(step_data->nodes_mutex);
 
-  Node *unode = step_data->nodes.is_empty() ? nullptr : step_data->nodes.first().get();
-
-  if (unode == nullptr) {
+  if (step_data->nodes.is_empty()) {
+    /* We currently need to append data here so that the overall undo system knows to indicate that
+     * data should be flushed to the memfile */
+    /* TODO: Once we store entering Sculpt Mode as a specific type of action, we can remove this
+     * call. */
     step_data->nodes.append(std::make_unique<Node>());
-    unode = step_data->nodes.last().get();
 
     step_data->type = type;
 
@@ -1912,13 +1914,13 @@ static bool step_encode(bContext * /*C*/, Main *bmain, UndoStep *us_p)
   SculptUndoStep *us = reinterpret_cast<SculptUndoStep *>(us_p);
   us->step.data_size = us->data.undo_size;
 
-  Node *unode = us->data.nodes.is_empty() ? nullptr : us->data.nodes.last().get();
-  if (unode && us->data.type == Type::DyntopoEnd) {
+  if (us->data.type == Type::DyntopoEnd) {
     us->step.use_memfile_step = true;
   }
   us->step.is_applied = true;
 
-  if (!us->data.nodes.is_empty()) {
+  /* We do not flush data when entering sculpt mode - this is currently indicated by Type::None */
+  if (us->data.type != Type::None) {
     bmain->is_memfile_undo_flush_needed = true;
   }
 
