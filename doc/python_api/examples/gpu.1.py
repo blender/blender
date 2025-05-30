a06c7fddbb2acf7c5246890031f11cd1bb78b085
Jeroen Bakker <jeroen@blender.org>
2025-05-30 09:10:51 +0200

    Fix #139565: Update GPU polyline examples
    Update GPU line examples to use polyline shaders.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/139587

--- doc/python_api/examples/gpu.1.py
+++ doc/python_api/examples/gpu.1.py
@@ -173,11 +173,13 @@ import gpu
 from gpu_extras.batch import batch_for_shader
 
 coords = [(1, 1, 1), (-2, 0, 0), (-2, -1, 3), (0, 1, 1)]
-shader = gpu.shader.from_builtin('UNIFORM_COLOR')
+shader = gpu.shader.from_builtin('POLYLINE_UNIFORM_COLOR')
 batch = batch_for_shader(shader, 'LINES', {"pos": coords})
 
 
 def draw():
+    shader.uniform_float("viewportSize", gpu.state.viewport_get()[2:])
+    shader.uniform_float("lineWidth", 4.5)
     shader.uniform_float("color", (1, 1, 0, 1))
     batch.draw(shader)
 
