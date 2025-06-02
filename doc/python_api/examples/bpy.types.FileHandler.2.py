0fa8c0f62abc09bd71c10f79916366a32a483a5f
Jesse Yurkovich <jesse.y@gmail.com>
2025-06-02 19:21:38 +0200

    Docs: Grammar and wording cleanups for FileHandler examples
    Cleanup grammar and simplify the wording used in the FileHandler
    Python API examples.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/139366

--- doc/python_api/examples/bpy.types.FileHandler.2.py
+++ doc/python_api/examples/bpy.types.FileHandler.2.py
@@ -1,17 +1,21 @@
 """
-Basic FileHandler for Operator that imports multiple files
-----------------------------------------------------------
+FileHandler for Importing multiple files and exposing Operator options
+----------------------------------------------------------------------
 
-Also operators can be invoked with multiple files from 'drag-and-drop',
-but for this it is require to define the following properties:
+Operators which support being executed with multiple files from 'drag-and-drop' require the
+following properties be defined:
 
 .. code-block:: python
 
-    directory: StringProperty(subtype='DIR_PATH')
-    files: CollectionProperty(type=bpy.types.OperatorFileListElement)
+    directory: StringProperty(subtype='DIR_PATH', options={'SKIP_SAVE', 'HIDDEN'})
+    files: CollectionProperty(type=OperatorFileListElement, options={'SKIP_SAVE', 'HIDDEN'})
 
-This ``directory`` and ``files`` properties now will be used by the
-``FileHandler`` to set 'drag-and-drop' filepath data.
+These ``directory`` and ``files`` properties will be set with the necessary data from the
+`drag-and-drop` operation.
+
+Additionally, if the operator provides operator properties that need to be accessible to the user,
+the :class:`ImportHelper.invoke_popup` method can be used to show a dialog leveraging the standard
+:class:`Operator.draw` method for layout and display.
 
 """
 
@@ -21,18 +25,18 @@ from mathutils import Vector
 
 
 class ShaderScriptImport(bpy.types.Operator, ImportHelper):
-    """Test importer that creates scripts nodes from .txt files"""
+    """
+    Creates one or more Shader Script nodes from text files.
+    """
     bl_idname = "shader.script_import"
     bl_label = "Import a text file as a script node"
 
-    """
-    This Operator can import multiple .txt files, we need following directory and files
-    properties that the file handler will use to set files path data
-    """
+    # This Operator supports processing multiple `.txt` files at a time. The following properties
+    # must be defined.
     directory: bpy.props.StringProperty(subtype='DIR_PATH', options={'SKIP_SAVE', 'HIDDEN'})
     files: bpy.props.CollectionProperty(type=bpy.types.OperatorFileListElement, options={'SKIP_SAVE', 'HIDDEN'})
 
-    """Allow the user to select if the node's label is set or not"""
+    # Allow the user to choose whether the node's label is set or not
     set_label: bpy.props.BoolProperty(name="Set Label", default=False)
 
     @classmethod
@@ -45,22 +49,22 @@ class ShaderScriptImport(bpy.types.Operator, ImportHelper):
         )
 
     def execute(self, context):
-        """ The directory property need to be set. """
+        # The directory property must be set.
         if not self.directory:
             return {'CANCELLED'}
+
         x = 0.0
         y = 0.0
         for file in self.files:
-            """
-            Calls to the operator can set unfiltered file names,
-            ensure the file extension is .txt
-            """
+            # Direct calls to this Operator may use unsupported file-paths. Ensure the incoming
+            # files are ones that are supported.
             if file.name.endswith(".txt"):
+                import os
+                filepath = os.path.join(self.directory, file.name)
+
                 node_tree = context.material.node_tree
                 text_node = node_tree.nodes.new(type="ShaderNodeScript")
                 text_node.mode = 'EXTERNAL'
-                import os
-                filepath = os.path.join(self.directory, file.name)
                 text_node.filepath = filepath
                 text_node.location = Vector((x, y))
 
@@ -70,12 +74,13 @@ class ShaderScriptImport(bpy.types.Operator, ImportHelper):
 
                 x += 20.0
                 y -= 20.0
+
         return {'FINISHED'}
 
     # Use ImportHelper's invoke_popup() to handle the invocation so that this operator's properties
-    # are shown in a popup. This allows the user to configure additional settings on the operator like
-    # the `set_label` property. Consider having a draw() method on the operator in order to layout the
-    # properties in the UI appropriately.
+    # are shown in a popup. This allows the user to configure additional settings on the operator
+    # like the `set_label` property. Consider having a draw() method on the operator in order to
+    # layout the properties in the UI appropriately.
     #
     # If filepath information is not provided the file select window will be invoked instead.
 
@@ -83,6 +88,9 @@ class ShaderScriptImport(bpy.types.Operator, ImportHelper):
         return self.invoke_popup(context)
 
 
+# Define a file handler that supports the following set of conditions:
+#  - Execute the `shader.script_import` operator
+#  - When `.txt` files are dropped in the Shader Editor
 class SHADER_FH_script_import(bpy.types.FileHandler):
     bl_idname = "SHADER_FH_script_import"
     bl_label = "File handler for shader script node import"
