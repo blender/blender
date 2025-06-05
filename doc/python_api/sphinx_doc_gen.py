6fbef14f4b66ecb0381b988358cd7e6fde1e5609
Campbell Barton <campbell@blender.org>
2025-06-05 05:31:51 +0000

    PyDoc: quiet wanings/prints when building docs

--- doc/python_api/sphinx_doc_gen.py
+++ doc/python_api/sphinx_doc_gen.py
@@ -1228,7 +1228,6 @@ context_type_map = {
     "particle_system": [("ParticleSystem", False)],
     "particle_system_editable": [("ParticleSystem", False)],
     "property": [("AnyType", False), ("str", False), ("int", False)],
-    "pointcloud": [("PointCloud", False)],
     "pose_bone": [("PoseBone", False)],
     "pose_object": [("Object", False)],
     "scene": [("Scene", False)],
