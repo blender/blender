a48e599bea6f1d6e8652f724bf3d60b22446d89e
Omar Emara <mail@OmarEmara.dev>
2025-06-10 09:45:30 +0300

    Fix #139856: Ellipse/Box mask size is clamped
    The Ellipse/Box Mask nodes recently got their size clamped by their soft
    limit, which was not previously the case, breaking compatibility and
    limiting the node's use in some setups. The same goes for their
    position. We fix this by allowing any size and position.

--- source/blender/nodes/composite/nodes/node_composite_boxmask.cc
+++ source/blender/nodes/composite/nodes/node_composite_boxmask.cc
@@ -259,18 +259,14 @@ class BoxMaskOperation : public NodeOperation {
 
   float2 get_location()
   {
-    return math::clamp(
-        this->get_input("Position").get_single_value_default(float3(0.5f, 0.5f, 0.0f)).xy(),
-        float2(-0.5f),
-        float2(1.5f));
+    return this->get_input("Position").get_single_value_default(float3(0.5f, 0.5f, 0.0f)).xy();
   }
 
   float2 get_size()
   {
-    return math::clamp(
-        this->get_input("Size").get_single_value_default(float3(0.2f, 0.1f, 0.0f)).xy(),
+    return math::max(
         float2(0.0f),
-        float2(1.0f));
+        this->get_input("Size").get_single_value_default(float3(0.2f, 0.1f, 0.0f)).xy());
   }
 
   float get_angle()
