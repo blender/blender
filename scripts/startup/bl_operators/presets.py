6ed8e1dd96a69995a7ac73019a75e3f401ce3291
Campbell Barton <campbell@blender.org>
2025-06-10 17:17:03 +1000

    Fix wm.operator_presets_cleanup failing to exclude collections
    Only the collection clearing was removed, adding an item
    and assigning values to it was kept - breaking the preset.
    
    Detect collections and removed all lines which add & manipulate items.
    
    Based on the fix from !139309 with fewer changes.
    
    Also include FBX operators when cleaning up presets.
    
    Co-authored-by: Guillermo Venegas <guillermovcra@gmail.com>

--- scripts/startup/bl_operators/presets.py
+++ scripts/startup/bl_operators/presets.py
@@ -864,13 +864,29 @@ class WM_OT_operator_presets_cleanup(Operator):
         if not (os.path.isfile(filepath) and os.path.splitext(filepath)[1].lower() == ".py"):
             return
         with open(filepath, "r", encoding="utf-8") as fh:
-            lines = fh.read().splitlines(True)
-        if not lines:
+            lines_prev = fh.read().splitlines(True)
+        if not lines_prev:
             return
         regex_exclude = re.compile("(" + "|".join([re.escape("op." + prop) for prop in properties_exclude]) + ")\\b")
-        lines = [line for line in lines if not regex_exclude.match(line)]
+        lines_next = []
+
+        i = 0
+        while i < len(lines_prev):
+            m = regex_exclude.match(lines_prev[i])
+            if m is None:
+                lines_next.append(lines_prev[i])
+                i += 1
+            else:
+                is_collection = lines_prev[i][m.end():].startswith(".clear()")
+                i += 1
+
+                # Skip non operator lines.
+                if is_collection:
+                    while i < len(lines_prev) and (not lines_prev[i].startswith("op.")):
+                        i += 1
+
         with open(filepath, "w", encoding="utf-8") as fh:
-            fh.write("".join(lines))
+            fh.write("".join(lines_next))
 
     def _cleanup_operators_presets(self, operators, properties_exclude):
         import os
@@ -908,6 +924,8 @@ class WM_OT_operator_presets_cleanup(Operator):
                 "WM_OT_stl_import",
                 "WM_OT_usd_export",
                 "WM_OT_usd_import",
+                "EXPORT_SCENE_OT_fbx",
+                "IMPORT_SCENE_OT_fbx",
             ]
             properties_exclude = [
                 "filepath",
