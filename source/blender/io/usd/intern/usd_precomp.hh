9353fc280d4ec4b776c4793ffc3e1c310554a712
Jesse Yurkovich <jesse.y@gmail.com>
2025-06-16 22:51:58 +0200

    Cleanup: Improve precompiled header for USD
    Improve the selection of headers included in the precompiled header.
    
    This reduces build time of the full `io/usd` directory by 12 to 19%.
    
    Laptop i7-8750h: Average 30.3 sec --> 24.5 sec
    Desktop i7-13700k: Average 12.1 sec --> 10.7 sec
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/140418

--- source/blender/io/usd/intern/usd_precomp.hh
+++ source/blender/io/usd/intern/usd_precomp.hh
@@ -3,8 +3,11 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <pxr/pxr.h>
-#include <pxr/usd/sdf/layer.h>
 #include <pxr/usd/usd/object.h>
 #include <pxr/usd/usd/prim.h>
 #include <pxr/usd/usd/primData.h>
 #include <pxr/usd/usdGeom/xformable.h>
+#include <pxr/usd/usdShade/material.h>
+
+#include <pxr/imaging/hd/sceneDelegate.h>
+#include <pxr/imaging/hd/tokens.h>
