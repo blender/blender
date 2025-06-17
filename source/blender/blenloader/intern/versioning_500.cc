dce3ce7a043440f33822a81abf25472ac8bfa707
Jonas Holzman <jonas@holzman.fr>
2025-06-17 10:36:55 +0200

    UI: Nodes: Rename Levels node "Std Dev" output to "Standard Deviation"
    This commit renames the Compositor Levels node output socket from
    "Std Dev" to "Standard Deviation". This is a 5.0 breaking change,
    following up on PR #140498, #140495 and #96219
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/140500

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -195,6 +195,15 @@ void blo_do_versions_500(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     }
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 13)) {
+    FOREACH_NODETREE_BEGIN (bmain, ntree, id) {
+      if (ntree->type == NTREE_COMPOSIT) {
+        version_node_socket_name(ntree, CMP_NODE_VIEW_LEVELS, "Std Dev", "Standard Deviation");
+      }
+    }
+    FOREACH_NODETREE_END;
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
