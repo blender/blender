83a9cda08f78f608567825233524e3b6e28e09df
Pablo Vazquez <pablo@blender.org>
2025-06-18 16:42:35 +0200

    UI: Clip Graph: Use theme colors for speed axes
    Replace the hardcoded red, green, blue colors for speed and
    reprojection error curves in the Clip Editor Graph, with
    theme X, Y, and Z settings respectively.
    
    Update the tooltip as well to reflect the change.
    
    See PR for details and screenshots.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/140409

--- source/blender/makesrna/intern/rna_space.cc
+++ source/blender/makesrna/intern/rna_space.cc
@@ -8420,10 +8420,7 @@ static void rna_def_space_clip(BlenderRNA *brna)
   prop = RNA_def_property(srna, "show_graph_tracks_motion", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", SC_SHOW_GRAPH_TRACKS_MOTION);
   RNA_def_property_ui_text(
-      prop,
-      "Show Tracks Motion",
-      "Display the speed curves (in \"x\" direction red, in \"y\" direction green) "
-      "for the selected tracks");
+      prop, "Show Tracks Motion", "Display speed curves for the selected tracks");
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_CLIP, nullptr);
 
   /* show graph tracks motion */
