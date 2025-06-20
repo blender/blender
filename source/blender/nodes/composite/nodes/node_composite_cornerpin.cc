blender/blender

commit 091db9d52b46379a952c3e667c5c73830036c31d

Cleanup: UI: Use UI_interface_layout.hh instead of UI_interface.hh
This removes the include `UI_interface_layout.hh` from
`UI_interface_c.hh`, and in many places this swaps the include
from `UI_interface.hh` to `UI_interface_layout.hh`.

Also, cleanups some `UI_interface.hh` includes with
`UI_interface_icons.hh` or `UI_interface_types.hh`

source/blender/nodes/composite/nodes/node_composite_cornerpin.cc
@@ -16,7 +16,7 @@
 
 #include "BKE_tracking.h"
 
-#include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 
 #include "COM_algorithm_smaa.hh"
 #include "COM_node_operation.hh"

source/blender/nodes/composite/nodes/node_composite_keyingscreen.cc
@@ -22,7 +22,7 @@
 #include "RNA_access.hh"
 #include "RNA_prototypes.hh"
 
-#include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 
 #include "COM_keying_screen.hh"

source/blender/nodes/composite/nodes/node_composite_moviedistortion.cc
@@ -15,7 +15,7 @@
 #include "BKE_lib_id.hh"
 #include "BKE_tracking.h"
 
-#include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 
 #include "GPU_shader.hh"

source/blender/nodes/composite/nodes/node_composite_planetrackdeform.cc
@@ -23,7 +23,7 @@
 #include "RNA_access.hh"
 #include "RNA_prototypes.hh"
 
-#include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 
 #include "GPU_shader.hh"

source/blender/nodes/function/nodes/node_fn_bit_math.cc
@@ -6,7 +6,7 @@
 
 #include "RNA_enum_types.hh"
 
-#include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 
 #include "NOD_rna_define.hh"
 #include "NOD_socket_search_link.hh"

source/blender/nodes/function/nodes/node_fn_match_string.cc
@@ -5,7 +5,7 @@
 #include "BLI_string_ref.hh"
 #include "BLI_string_utf8.h"
 
-#include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 
 #include "RNA_enum_types.hh"
 

source/blender/nodes/geometry/nodes/node_geo_input_normal.cc
@@ -4,7 +4,7 @@
 
 #include "node_geometry_util.hh"
 
-#include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 
 namespace blender::nodes::node_geo_input_normal_cc {
 

source/blender/nodes/geometry/nodes/node_geo_input_object.cc
@@ -2,7 +2,6 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "UI_interface.hh"
 #include "UI_resources.hh"
 
 #include "node_geometry_util.hh"

commit 86d60ae6635fbedd9d6503bf69f02430f8dabc69

Cleanup: Build errors without unity builds after recent UI refactor
Caused by 091db9d52b46379a952c3e667c5c73830036c31d

source/blender/nodes/composite/nodes/node_composite_cornerpin.cc
@@ -17,6 +17,7 @@
 #include "BKE_tracking.h"
 
 #include "UI_interface_layout.hh"
+#include "UI_resources.hh"
 
 #include "COM_algorithm_smaa.hh"
 #include "COM_node_operation.hh"

source/blender/nodes/composite/nodes/node_composite_keyingscreen.cc
@@ -22,6 +22,7 @@
 #include "RNA_access.hh"
 #include "RNA_prototypes.hh"
 
+#include "UI_interface_c.hh"
 #include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 

source/blender/nodes/composite/nodes/node_composite_moviedistortion.cc
@@ -15,6 +15,7 @@
 #include "BKE_lib_id.hh"
 #include "BKE_tracking.h"
 
+#include "UI_interface.hh"
 #include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 

source/blender/nodes/composite/nodes/node_composite_planetrackdeform.cc
@@ -23,6 +23,7 @@
 #include "RNA_access.hh"
 #include "RNA_prototypes.hh"
 
+#include "UI_interface.hh"
 #include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 

source/blender/nodes/function/nodes/node_fn_bit_math.cc
@@ -7,6 +7,7 @@
 #include "RNA_enum_types.hh"
 
 #include "UI_interface_layout.hh"
+#include "UI_resources.hh"
 
 #include "NOD_rna_define.hh"
 #include "NOD_socket_search_link.hh"

source/blender/nodes/function/nodes/node_fn_match_string.cc
@@ -6,6 +6,7 @@
 #include "BLI_string_utf8.h"
 
 #include "UI_interface_layout.hh"
+#include "UI_resources.hh"
 
 #include "RNA_enum_types.hh"
 

source/blender/nodes/geometry/nodes/node_geo_image.cc
@@ -7,6 +7,7 @@
 #include "node_geometry_util.hh"
 
 #include "UI_interface.hh"
+#include "UI_interface_layout.hh"
 
 namespace blender::nodes::node_geo_image_cc {
 

source/blender/nodes/geometry/nodes/node_geo_input_normal.cc
@@ -5,6 +5,7 @@
 #include "node_geometry_util.hh"
 
 #include "UI_interface_layout.hh"
+#include "UI_resources.hh"
 
 namespace blender::nodes::node_geo_input_normal_cc {
 

source/blender/nodes/geometry/nodes/node_geo_input_object.cc
@@ -2,6 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 
 #include "node_geometry_util.hh"

source/blender/nodes/geometry/nodes/node_geo_set_grease_pencil_color.cc
@@ -6,6 +6,7 @@
 #include "BKE_grease_pencil.hh"
 
 #include "UI_interface_layout.hh"
+#include "UI_resources.hh"
 
 #include "NOD_rna_define.hh"
 

source/blender/nodes/geometry/nodes/node_geo_set_grease_pencil_depth_mode.cc
@@ -5,6 +5,7 @@
 #include "BKE_grease_pencil.hh"
 
 #include "UI_interface_layout.hh"
+#include "UI_resources.hh"
 
 #include "NOD_rna_define.hh"
 

source/blender/nodes/geometry/nodes/node_geo_string_to_curves.cc
@@ -15,6 +15,7 @@
 #include "BLI_string_utf8.h"
 #include "BLI_task.hh"
 
+#include "UI_interface.hh"
 #include "UI_interface_layout.hh"
 #include "UI_resources.hh"
 

source/blender/nodes/geometry/nodes/node_geo_uv_pack_islands.cc
@@ -12,6 +12,7 @@
 #include "NOD_rna_define.hh"
 
 #include "UI_interface_layout.hh"
+#include "UI_resources.hh"
 
 namespace blender::nodes::node_geo_uv_pack_islands_cc {
 