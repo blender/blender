1b9f9dd96fee37bacb0118a94a194b0e9f5ae280
Harley Acheson <harley.acheson@gmail.com>
2025-06-20 21:02:23 +0200

    Fix #140535: Simplify Outliner Drag Feedback
    Outliner is currently drawing row highlighting with a combination of
    immediate mode drawing and widget shaders. The complaint shows it not
    working correctly in some circumstances. This PR simplifies it by using
    only widget shaders. There should be no other visual change.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/140687

--- source/blender/editors/space_outliner/outliner_draw.cc
+++ source/blender/editors/space_outliner/outliner_draw.cc
@@ -3733,14 +3733,13 @@ static void outliner_draw_struct_marks(ARegion *region,
   }
 }
 
-static void outliner_draw_highlights(uint pos,
-                                     const ARegion *region,
+static void outliner_draw_highlights(const ARegion *region,
                                      const SpaceOutliner *space_outliner,
                                      const float col_selection[4],
                                      const float col_active[4],
                                      const float col_highlight[4],
                                      const float col_searchmatch[4],
-                                     int start_x,
+                                     int /* start_x */,
                                      int *io_start_y)
 {
   const bool is_searching = (SEARCHING_OUTLINER(space_outliner) ||
@@ -3776,34 +3775,25 @@ static void outliner_draw_highlights(uint pos,
 
     /* Highlights. */
     if (tselem->flag & (TSE_DRAG_ANY | TSE_HIGHLIGHTED | TSE_SEARCHMATCH)) {
-      const int end_x = int(region->v2d.cur.xmax);
-
       if (tselem->flag & TSE_DRAG_ANY) {
         /* Drag and drop highlight. */
         float col_outline[4];
         UI_GetThemeColorBlend4f(TH_TEXT, TH_BACK, 0.4f, col_outline);
 
         if (tselem->flag & TSE_DRAG_BEFORE) {
-          GPU_blend(GPU_BLEND_ALPHA);
-          immUniformColor4fv(col_outline);
-          immRectf(pos,
-                   start_x,
-                   start_y + UI_UNIT_Y - U.pixelsize,
-                   end_x,
-                   start_y + UI_UNIT_Y + U.pixelsize);
-          GPU_blend(GPU_BLEND_NONE);
+          rect.ymax += (1.0f * UI_SCALE_FAC) + (1.0f * U.pixelsize);
+          rect.ymin = rect.ymax - (2.0f * U.pixelsize);
+          UI_draw_roundbox_4fv(&rect, true, 0.0f, col_outline);
         }
         else if (tselem->flag & TSE_DRAG_AFTER) {
-          GPU_blend(GPU_BLEND_ALPHA);
-          immUniformColor4fv(col_outline);
-          immRectf(pos, start_x, start_y - U.pixelsize, end_x, start_y + U.pixelsize);
-          GPU_blend(GPU_BLEND_NONE);
+          rect.ymin -= (1.0f * UI_SCALE_FAC) + (1.0f * U.pixelsize);
+          rect.ymax = rect.ymin + (2.0f * U.pixelsize);
+          UI_draw_roundbox_4fv(&rect, true, 0.0f, col_outline);
         }
         else {
           float col_bg[4];
           UI_GetThemeColorShade4fv(TH_BACK, 40, col_bg);
-          UI_draw_roundbox_4fv(&rect, true, radius, col_bg);
-          UI_draw_roundbox_4fv(&rect, false, radius, col_outline);
+          UI_draw_roundbox_4fv_ex(&rect, col_bg, nullptr, 1.0f, col_outline, U.pixelsize, radius);
         }
       }
       else {
@@ -3839,11 +3829,7 @@ static void outliner_draw_highlights(ARegion *region,
   col_searchmatch[3] = 0.5f;
 
   GPU_blend(GPU_BLEND_ALPHA);
-  GPUVertFormat *format = immVertexFormat();
-  uint pos = GPU_vertformat_attr_add(format, "pos", blender::gpu::VertAttrType::SFLOAT_32_32);
-  immBindBuiltinProgram(GPU_SHADER_3D_UNIFORM_COLOR);
-  outliner_draw_highlights(pos,
-                           region,
+  outliner_draw_highlights(region,
                            space_outliner,
                            col_selection,
                            col_active,
@@ -3851,7 +3837,6 @@ static void outliner_draw_highlights(ARegion *region,
                            col_searchmatch,
                            startx,
                            starty);
-  immUnbindProgram();
   GPU_blend(GPU_BLEND_NONE);
 }
 
