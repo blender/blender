c8cb24121fbea7e281dedd59f8c9070714d3c79a
Habib Gahbiche <habib@blender.org>
2025-06-28 15:27:30 +0200

    Fix #140381: Crash after deleting a node group and redo
    The fix in #140381 addressed the crash after redo. This commit fixes
    the crash after redo (make group) and deleting the node group.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/140858

--- source/blender/editors/space_node/node_edit.cc
+++ source/blender/editors/space_node/node_edit.cc
@@ -2091,6 +2091,7 @@ static wmOperatorStatus node_delete_exec(bContext *C, wmOperator * /*op*/)
     }
   }
 
+  ED_node_set_active_viewer_key(snode);
   BKE_main_ensure_invariants(*bmain, snode->edittree->id);
 
   return OPERATOR_FINISHED;
