5cbb1c62c4aa05f1311649ed43c820d8f4af86d0
Aras Pranckevicius <aras@nesnausk.org>
2025-07-01 10:49:10 +0200

    Fix #139552: VSE strips wrongly occlude others, when render resolution scale is small
    get_strip_screen_quad function was not taking render scale into account,
    unlike all other places that did a similar calculation.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/141229

--- source/blender/sequencer/intern/render.cc
+++ source/blender/sequencer/intern/render.cc
@@ -281,7 +281,9 @@ StripScreenQuad get_strip_screen_quad(const RenderData *context, const Strip *st
   const float2 offset{x * 0.5f, y * 0.5f};
 
   Array<float2> quad = image_transform_final_quad_get(scene, strip);
-  const float scale = rendersize_to_scale_factor(context->preview_render_size);
+  const float scale = context->preview_render_size == SEQ_RENDER_SIZE_SCENE ?
+                          float(scene->r.size) / 100.0f :
+                          rendersize_to_scale_factor(context->preview_render_size);
   return StripScreenQuad{float2(quad[0] * scale + offset),
                          float2(quad[1] * scale + offset),
                          float2(quad[2] * scale + offset),
