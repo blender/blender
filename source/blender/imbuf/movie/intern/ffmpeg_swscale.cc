ff5e9c66e798b0a480c64822c62a6ffe17e33668
Aras Pranckevicius <aras@nesnausk.org>
2025-07-01 09:27:48 +0200

    Fix #141033: VSE proxies have inaccurate colors for YUV444 or full-range videos
    When converting from say YUV444 to the YUV420 that proxies use,
    ffmpeg libswscale was not being told of the full vs limited value
    ranges as needed.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/141241

--- source/blender/imbuf/movie/intern/ffmpeg_swscale.cc
+++ source/blender/imbuf/movie/intern/ffmpeg_swscale.cc
@@ -33,6 +33,8 @@ struct SwscaleContext {
   int src_width = 0, src_height = 0;
   int dst_width = 0, dst_height = 0;
   AVPixelFormat src_format = AV_PIX_FMT_NONE, dst_format = AV_PIX_FMT_NONE;
+  bool src_full_range = false, dst_full_range = false;
+  int src_colorspace = -1, dst_colorspace = -1;
   int flags = 0;
 
   SwsContext *context = nullptr;
@@ -135,9 +137,13 @@ static void maintain_swscale_cache_size()
 SwsContext *ffmpeg_sws_get_context(int src_width,
                                    int src_height,
                                    int av_src_format,
+                                   bool src_full_range,
+                                   int src_color_space,
                                    int dst_width,
                                    int dst_height,
                                    int av_dst_format,
+                                   bool dst_full_range,
+                                   int dst_color_space,
                                    int sws_flags)
 {
   std::lock_guard lock(swscale_cache_lock);
@@ -150,8 +156,11 @@ SwsContext *ffmpeg_sws_get_context(int src_width,
   SwsContext *ctx = nullptr;
   for (SwscaleContext &c : *swscale_cache) {
     if (!c.is_used && c.src_width == src_width && c.src_height == src_height &&
-        c.src_format == av_src_format && c.dst_width == dst_width && c.dst_height == dst_height &&
-        c.dst_format == av_dst_format && c.flags == sws_flags)
+        c.src_format == av_src_format && c.src_full_range == src_full_range &&
+        c.src_colorspace == src_color_space && c.dst_width == dst_width &&
+        c.dst_height == dst_height && c.dst_format == av_dst_format &&
+        c.dst_full_range == dst_full_range && c.dst_colorspace == dst_color_space &&
+        c.flags == sws_flags)
     {
       ctx = c.context;
       /* Mark as used. */
@@ -164,6 +173,34 @@ SwsContext *ffmpeg_sws_get_context(int src_width,
     /* No free matching context in cache: create a new one. */
     ctx = sws_create_context(
         src_width, src_height, av_src_format, dst_width, dst_height, av_dst_format, sws_flags);
+
+    int src_range, dst_range, brightness, contrast, saturation;
+    const int *table, *inv_table;
+    if (sws_getColorspaceDetails(ctx,
+                                 (int **)&inv_table,
+                                 &src_range,
+                                 (int **)&table,
+                                 &dst_range,
+                                 &brightness,
+                                 &contrast,
+                                 &saturation) >= 0)
+    {
+      if (src_full_range) {
+        src_range = 1;
+      }
+      if (dst_full_range) {
+        dst_range = 1;
+      }
+      if (src_color_space >= 0) {
+        inv_table = sws_getCoefficients(src_color_space);
+      }
+      if (dst_color_space >= 0) {
+        table = sws_getCoefficients(dst_color_space);
+      }
+      sws_setColorspaceDetails(
+          ctx, (int *)inv_table, src_range, table, dst_range, brightness, contrast, saturation);
+    }
+
     SwscaleContext c;
     c.src_width = src_width;
     c.src_height = src_height;
@@ -171,6 +208,10 @@ SwsContext *ffmpeg_sws_get_context(int src_width,
     c.dst_height = dst_height;
     c.src_format = AVPixelFormat(av_src_format);
     c.dst_format = AVPixelFormat(av_dst_format);
+    c.src_full_range = src_full_range;
+    c.dst_full_range = dst_full_range;
+    c.src_colorspace = src_color_space;
+    c.dst_colorspace = dst_color_space;
     c.flags = sws_flags;
     c.context = ctx;
     c.is_used = true;
