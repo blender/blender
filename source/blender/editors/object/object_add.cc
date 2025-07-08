07994003b69fcf91c3ee07dd4efc6102c69f2622
Eitan Traurig <eitant13@gmail.com>
2025-07-08 11:39:10 +0200

    UI: Disable mirror and update shape from objects shapekey operators when list is empty
    Grey out these operators when shape key list is empty.
    
    See images in PR description
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/141468

--- source/blender/editors/object/object_add.cc
+++ source/blender/editors/object/object_add.cc
@@ -4970,6 +4970,20 @@ static wmOperatorStatus update_all_shape_keys_exec(bContext *C, wmOperator *op)
   return ED_mesh_shapes_join_objects_exec(C, false, op->reports);
 }
 
+static bool object_update_shapes_poll(bContext *C)
+{
+  if (!active_shape_key_editable_poll(C)) {
+    return false;
+  }
+
+  Object *ob = CTX_data_active_object(C);
+  const Key *key = BKE_key_from_object(ob);
+  if (!key || BLI_listbase_is_empty(&key->block)) {
+    return false;
+  }
+  return true;
+}
+
 void OBJECT_OT_update_shapes(wmOperatorType *ot)
 {
   ot->name = "Update from Objects";
@@ -4979,7 +4993,7 @@ void OBJECT_OT_update_shapes(wmOperatorType *ot)
   ot->idname = "OBJECT_OT_update_shapes";
 
   ot->exec = update_all_shape_keys_exec;
-  ot->poll = active_shape_key_editable_poll;
+  ot->poll = object_update_shapes_poll;
 
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 }
