blender/blender

commit 68759af516853eda749b08df714a6d7279673800

Attributes: Use AttributeStorage for curves and Grease Pencil
This commit moves Curves and Grease Pencil to use `AttributeStorage`
instead of `CustomData`, except for vertex groups. This PR mostly
involves extending the changes from the above commit for point clouds
to generalize to other geometry types.

This is mostly straightforward, though a couple non-trivial places of
note are the joining of Grease Pencil objects (`merge_attributes`), the
"default render fallback" UV for curves objects which was previously
unused at the UI level and just ended up being the first attribute, and
the `update_curve_types()` call in the curves versioning function.

Similar to:
- fa03c53d4a8ba16150232316a6ff8843ce02bb34
- f74e304b00a99489ff0b63ac2928b13821656107

Part of #122398.

Pull Request: https://projects.blender.org/blender/blender/pulls/140936

source/blender/blenkernel/intern/grease_pencil.cc
@@ -78,6 +78,8 @@
 
 #include "MEM_guardedalloc.h"
 
+#include "attribute_storage_access.hh"
+
 using blender::float3;
 using blender::int3;
 using blender::Span;
@@ -107,7 +109,7 @@ static void grease_pencil_init_data(ID *id)
   grease_pencil->root_group_ptr = MEM_new<greasepencil::LayerGroup>(__func__);
   grease_pencil->set_active_node(nullptr);
 
-  CustomData_reset(&grease_pencil->layers_data);
+  CustomData_reset(&grease_pencil->layers_data_legacy);
   new (&grease_pencil->attribute_storage.wrap()) blender::bke::AttributeStorage();
 
   grease_pencil->runtime = MEM_new<GreasePencilRuntime>(__func__);
@@ -200,10 +202,6 @@ static void grease_pencil_copy_data(Main * /*bmain*/,
     grease_pencil_dst->set_active_node(active_node);
   }
 
-  CustomData_init_from(&grease_pencil_src->layers_data,
-                       &grease_pencil_dst->layers_data,
-                       CD_MASK_ALL,
-                       grease_pencil_dst->layers().size());
   new (&grease_pencil_dst->attribute_storage.wrap())
       blender::bke::AttributeStorage(grease_pencil_src->attribute_storage.wrap());
 
@@ -228,7 +226,6 @@ static void grease_pencil_free_data(ID *id)
 
   MEM_SAFE_FREE(grease_pencil->material_array);
 
-  CustomData_free(&grease_pencil->layers_data);
   grease_pencil->attribute_storage.wrap().~AttributeStorage();
 
   free_drawing_array(*grease_pencil);
@@ -273,11 +270,6 @@ static void grease_pencil_blend_write(BlendWriter *writer, ID *id, const void *i
   blender::Vector<CustomDataLayer, 16> layers_data_layers;
   blender::bke::AttributeStorage::BlendWriteData attribute_data{scope};
   attribute_storage_blend_write_prepare(grease_pencil->attribute_storage.wrap(), attribute_data);
-  CustomData_blend_write_prepare(grease_pencil->layers_data,
-                                 AttrDomain::Layer,
-                                 grease_pencil->layers().size(),
-                                 layers_data_layers,
-                                 attribute_data);
   grease_pencil->attribute_storage.dna_attributes = attribute_data.attributes.data();
   grease_pencil->attribute_storage.dna_attributes_num = attribute_data.attributes.size();
 
@@ -285,12 +277,6 @@ static void grease_pencil_blend_write(BlendWriter *writer, ID *id, const void *i
   BLO_write_id_struct(writer, GreasePencil, id_address, &grease_pencil->id);
   BKE_id_blend_write(writer, &grease_pencil->id);
 
-  CustomData_blend_write(writer,
-                         &grease_pencil->layers_data,
-                         layers_data_layers,
-                         grease_pencil->layers().size(),
-                         CD_MASK_ALL,
-                         id);
   grease_pencil->attribute_storage.wrap().blend_write(*writer, attribute_data);
 
   /* Write drawings. */
@@ -315,11 +301,9 @@ static void grease_pencil_blend_read_data(BlendDataReader *reader, ID *id)
   /* Read layer tree. */
   read_layer_tree(*grease_pencil, reader);
 
-  CustomData_blend_read(reader, &grease_pencil->layers_data, grease_pencil->layers().size());
+  CustomData_blend_read(
+      reader, &grease_pencil->layers_data_legacy, grease_pencil->layers().size());
   grease_pencil->attribute_storage.wrap().blend_read(*reader);
-
-  /* Forward compatibility. To be removed when runtime format changes. */
-  blender::bke::grease_pencil_convert_storage_to_customdata(*grease_pencil);
 
   /* Read materials. */
   BLO_read_pointer_array(reader,
@@ -367,40 +351,6 @@ constexpr StringRef ATTR_RADIUS = "radius";
 constexpr StringRef ATTR_OPACITY = "opacity";
 constexpr StringRef ATTR_VERTEX_COLOR = "vertex_color";
 constexpr StringRef ATTR_FILL_COLOR = "fill_color";
-
-/* Curves attributes getters */
-static int domain_num(const CurvesGeometry &curves, const AttrDomain domain)
-{
-  return domain == AttrDomain::Point ? curves.points_num() : curves.curves_num();
-}
-static CustomData &domain_custom_data(CurvesGeometry &curves, const AttrDomain domain)
-{
-  return domain == AttrDomain::Point ? curves.point_data : curves.curve_data;
-}
-template<typename T>
-static MutableSpan<T> get_mutable_attribute(CurvesGeometry &curves,
-                                            const AttrDomain domain,
-                                            const StringRef name,
-                                            const T default_value = T())
-{
-  const int num = domain_num(curves, domain);
-  if (num <= 0) {
-    return {};
-  }
-  const eCustomDataType type = cpp_type_to_custom_data_type(CPPType::get<T>());
-  CustomData &custom_data = domain_custom_data(curves, domain);
-
-  T *data = (T *)CustomData_get_layer_named_for_write(&custom_data, type, name, num);
-  if (data != nullptr) {
-    return {data, num};
-  }
-  data = (T *)CustomData_add_layer_named(&custom_data, type, CD_SET_DEFAULT, num, name);
-  MutableSpan<T> span = {data, num};
-  if (span.first() != default_value) {
-    span.fill(default_value);
-  }
-  return span;
-}
 
 Drawing::Drawing()
 {
@@ -823,8 +773,12 @@ VArray<float> Drawing::radii() const
 
 MutableSpan<float> Drawing::radii_for_write()
 {
-  return get_mutable_attribute<float>(
-      this->strokes_for_write(), AttrDomain::Point, ATTR_RADIUS, 0.01f);
+  return blender::bke::get_mutable_attribute<float>(
+      this->strokes_for_write().attribute_storage.wrap(),
+      AttrDomain::Point,
+      ATTR_RADIUS,
+      this->strokes().points_num(),
+      0.01f);
 }
 
 VArray<float> Drawing::opacities() const
@@ -835,8 +789,12 @@ VArray<float> Drawing::opacities() const
 
 MutableSpan<float> Drawing::opacities_for_write()
 {
-  return get_mutable_attribute<float>(
-      this->strokes_for_write(), AttrDomain::Point, ATTR_OPACITY, 1.0f);
+  return blender::bke::get_mutable_attribute<float>(
+      this->strokes_for_write().attribute_storage.wrap(),
+      AttrDomain::Point,
+      ATTR_OPACITY,
+      this->strokes().points_num(),
+      1.0f);
 }
 
 VArray<ColorGeometry4f> Drawing::vertex_colors() const
@@ -847,10 +805,12 @@ VArray<ColorGeometry4f> Drawing::vertex_colors() const
 
 MutableSpan<ColorGeometry4f> Drawing::vertex_colors_for_write()
 {
-  return get_mutable_attribute<ColorGeometry4f>(this->strokes_for_write(),
-                                                AttrDomain::Point,
-                                                ATTR_VERTEX_COLOR,
-                                                ColorGeometry4f(0.0f, 0.0f, 0.0f, 0.0f));
+  return blender::bke::get_mutable_attribute<ColorGeometry4f>(
+      this->strokes_for_write().attribute_storage.wrap(),
+      AttrDomain::Point,
+      ATTR_VERTEX_COLOR,
+      this->strokes().points_num(),
+      ColorGeometry4f(0.0f, 0.0f, 0.0f, 0.0f));
 }
 
 VArray<ColorGeometry4f> Drawing::fill_colors() const
@@ -861,10 +821,12 @@ VArray<ColorGeometry4f> Drawing::fill_colors() const
 
 MutableSpan<ColorGeometry4f> Drawing::fill_colors_for_write()
 {
-  return get_mutable_attribute<ColorGeometry4f>(this->strokes_for_write(),
-                                                AttrDomain::Curve,
-                                                ATTR_FILL_COLOR,
-                                                ColorGeometry4f(0.0f, 0.0f, 0.0f, 0.0f));
+  return blender::bke::get_mutable_attribute<ColorGeometry4f>(
+      this->strokes_for_write().attribute_storage.wrap(),
+      AttrDomain::Curve,
+      ATTR_FILL_COLOR,
+      this->strokes().curves_num(),
+      ColorGeometry4f(0.0f, 0.0f, 0.0f, 0.0f));
 }
 
 void Drawing::tag_texture_matrices_changed()
@@ -2122,7 +2084,6 @@ void BKE_grease_pencil_nomain_to_grease_pencil(GreasePencil *grease_pencil_src,
   }
 
   /* Layers. */
-  CustomData_free(&grease_pencil_dst->layers_data);
   if (grease_pencil_dst->root_group_ptr) {
     MEM_delete(&grease_pencil_dst->root_group());
   }
@@ -2134,10 +2095,8 @@ void BKE_grease_pencil_nomain_to_grease_pencil(GreasePencil *grease_pencil_src,
   /* Reset the active node. */
   grease_pencil_dst->active_node = nullptr;
 
-  CustomData_init_from(&grease_pencil_src->layers_data,
-                       &grease_pencil_dst->layers_data,
-                       eCustomDataMask(CD_MASK_ALL),
-                       grease_pencil_src->layers().size());
+  grease_pencil_dst->attribute_storage.wrap() = std::move(
+      grease_pencil_src->attribute_storage.wrap());
 
   DEG_id_tag_update(&grease_pencil_dst->id, ID_RECALC_GEOMETRY);
 
@@ -3724,7 +3683,7 @@ blender::bke::greasepencil::Layer &GreasePencil::add_layer(const blender::String
   using namespace blender;
   std::string unique_name = check_name_is_unique ? unique_layer_name(name) : std::string(name);
   const int numLayers = layers().size();
-  CustomData_realloc(&layers_data, numLayers, numLayers + 1, CD_SET_DEFAULT);
+  this->attribute_storage.wrap().resize(bke::AttrDomain::Layer, numLayers + 1);
   bke::greasepencil::Layer *new_layer = MEM_new<bke::greasepencil::Layer>(__func__, unique_name);
   /* Hide masks by default. */
   new_layer->base.flag |= GP_LAYER_TREE_NODE_HIDE_MASKS;
@@ -3755,7 +3714,7 @@ void GreasePencil::add_layers_for_eval(const int num_new_layers)
 {
   using namespace blender;
   const int num_layers = this->layers().size();
-  CustomData_realloc(&layers_data, num_layers, num_layers + num_new_layers);
+  this->attribute_storage.wrap().resize(bke::AttrDomain::Layer, num_layers + num_new_layers);
   for ([[maybe_unused]] const int i : IndexRange(num_new_layers)) {
     bke::greasepencil::Layer *new_layer = MEM_new<bke::greasepencil::Layer>(__func__);
     /* Hide masks by default. */
@@ -3772,11 +3731,13 @@ blender::bke::greasepencil::Layer &GreasePencil::duplicate_layer(
   std::optional<int> duplicate_layer_idx = get_layer_index(duplicate_layer);
   BLI_assert(duplicate_layer_idx.has_value());
   const int numLayers = layers().size();
-  CustomData_realloc(&layers_data, numLayers, numLayers + 1);
-  for (const int layer_index : IndexRange(layers_data.totlayer)) {
-    CustomData_copy_data_layer(
-        &layers_data, &layers_data, layer_index, layer_index, *duplicate_layer_idx, numLayers, 1);
-  }
+  this->attribute_storage.wrap().resize(bke::AttrDomain::Layer, numLayers + 1);
+  bke::MutableAttributeAccessor attributes = this->attributes_for_write();
+  attributes.foreach_attribute([&](const bke::AttributeIter &iter) {
+    bke::GSpanAttributeWriter attr = attributes.lookup_for_write_span(iter.name);
+    GMutableSpan span = attr.span;
+    span.type().copy_assign(span[*duplicate_layer_idx], span[numLayers]);
+  });
   bke::greasepencil::Layer *new_layer = MEM_new<bke::greasepencil::Layer>(__func__,
                                                                           duplicate_layer);
   root_group().add_node(new_layer->as_node());
@@ -3817,17 +3778,30 @@ blender::bke::greasepencil::LayerGroup &GreasePencil::add_layer_group(
   return new_group;
 }
 
-static void reorder_customdata(CustomData &data, const Span<int> new_by_old_map)
+static void reorder_attribute_domain(blender::bke::AttributeStorage &data,
+                                     const blender::bke::AttrDomain domain,
+                                     const Span<int> new_by_old_map)
 {
-  CustomData new_data;
-  CustomData_init_layout_from(&data, &new_data, CD_MASK_ALL, CD_CONSTRUCT, new_by_old_map.size());
-
-  for (const int old_i : new_by_old_map.index_range()) {
-    const int new_i = new_by_old_map[old_i];
-    CustomData_copy_data(&data, &new_data, old_i, new_i, 1);
-  }
-  CustomData_free(&data);
-  data = new_data;
+  using namespace blender;
+  data.foreach([&](bke::Attribute &attr) {
+    if (attr.domain() != domain) {
+      return;
+    }
+    const CPPType &type = bke::attribute_type_to_cpp_type(attr.data_type());
+    switch (attr.storage_type()) {
+      case bke::AttrStorageType::Array: {
+        const auto &data = std::get<bke::Attribute::ArrayData>(attr.data());
+        auto new_data = bke::Attribute::ArrayData::ForConstructed(type, new_by_old_map.size());
+        bke::attribute_math::gather(GSpan(type, data.data, data.size),
+                                    new_by_old_map,
+                                    GMutableSpan(type, new_data.data, new_data.size));
+        attr.assign_data(std::move(new_data));
+      }
+      case bke::AttrStorageType::Single: {
+        return;
+      }
+    }
+  });
 }
 
 static void reorder_layer_data(GreasePencil &grease_pencil,
@@ -3859,7 +3833,8 @@ static void reorder_layer_data(GreasePencil &grease_pencil,
   BLI_assert(old_layer_index_by_layer.is_empty());
 
   /* Use the mapping to re-order the custom data */
-  reorder_customdata(grease_pencil.layers_data, new_by_old_map);
+  reorder_attribute_domain(
+      grease_pencil.attribute_storage.wrap(), bke::AttrDomain::Layer, new_by_old_map);
 }
 
 void GreasePencil::move_node_up(blender::bke::greasepencil::TreeNode &node, const int step)
@@ -4143,27 +4118,34 @@ void GreasePencil::rename_node(Main &bmain,
   }
 }
 
-static void shrink_customdata(CustomData &data, const int index_to_remove, const int size)
+static void shrink_attribute_storage(blender::bke::AttributeStorage &storage,
+                                     const int index_to_remove,
+                                     const int size)
 {
   using namespace blender;
-  CustomData new_data;
-  CustomData_init_layout_from(&data, &new_data, CD_MASK_ALL, CD_CONSTRUCT, size);
-  CustomData_realloc(&new_data, size, size - 1);
-
   const IndexRange range_before(index_to_remove);
   const IndexRange range_after(index_to_remove + 1, size - index_to_remove - 1);
 
-  if (!range_before.is_empty()) {
-    CustomData_copy_data(
-        &data, &new_data, range_before.start(), range_before.start(), range_before.size());
-  }
-  if (!range_after.is_empty()) {
-    CustomData_copy_data(
-        &data, &new_data, range_after.start(), range_after.start() - 1, range_after.size());
-  }
+  storage.foreach([&](bke::Attribute &attr) {
+    const CPPType &type = bke::attribute_type_to_cpp_type(attr.data_type());
+    switch (attr.storage_type()) {
+      case bke::AttrStorageType::Array: {
+        const auto &data = std::get<bke::Attribute::ArrayData>(attr.data());
 
-  CustomData_free(&data);
-  data = new_data;
+        auto new_data = bke::Attribute::ArrayData::ForUninitialized(type, size - 1);
+        type.copy_construct_n(data.data, new_data.data, range_before.size());
+        type.copy_construct_n(
+            POINTER_OFFSET(data.data, type.size * range_after.start()),
+            POINTER_OFFSET(new_data.data, type.size * (range_before.start() - 1)),
+            range_after.size());
+
+        attr.assign_data(std::move(new_data));
+      }
+      case bke::AttrStorageType::Single: {
+        return;
+      }
+    }
+  });
 }
 
 static void update_active_node_from_node_to_remove(
@@ -4200,7 +4182,7 @@ void GreasePencil::remove_layer(blender::bke::greasepencil::Layer &layer)
 
   /* Remove all the layer attributes and shrink the `CustomData`. */
   const int layer_index = *this->get_layer_index(layer);
-  shrink_customdata(this->layers_data, layer_index, this->layers().size());
+  shrink_attribute_storage(this->attribute_storage.wrap(), layer_index, this->layers().size());
 
   /* Unlink the layer from the parent group. */
   layer.parent_group().unlink_node(layer.as_node());

source/blender/blenkernel/BKE_attribute_legacy_convert.hh
@@ -44,17 +44,11 @@ void mesh_convert_storage_to_customdata(Mesh &mesh);
  */
 void mesh_convert_customdata_to_storage(Mesh &mesh);
 
-/** See #mesh_convert_storage_to_customdata. */
-void curves_convert_storage_to_customdata(CurvesGeometry &curves);
-
 /** See #mesh_convert_customdata_to_storage. */
 void curves_convert_customdata_to_storage(CurvesGeometry &curves);
 
 /** See #mesh_convert_customdata_to_storage. */
 void pointcloud_convert_customdata_to_storage(PointCloud &pointcloud);
-
-/** See #mesh_convert_storage_to_customdata. */
-void grease_pencil_convert_storage_to_customdata(GreasePencil &grease_pencil);
 
 /** See #mesh_convert_customdata_to_storage. */
 void grease_pencil_convert_customdata_to_storage(GreasePencil &grease_pencil);

source/blender/blenkernel/BKE_blender_version.h
@@ -27,7 +27,7 @@
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 32
+#define BLENDER_FILE_SUBVERSION 33
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/intern/attribute.cc
@@ -152,6 +152,9 @@ static std::array<DomainInfo, ATTR_DOMAIN_NUM> get_domains(const AttributeOwner 
   std::array<DomainInfo, ATTR_DOMAIN_NUM> info;
 
   switch (owner.type()) {
+    case AttributeOwnerType::Curves:
+    case AttributeOwnerType::GreasePencil:
+    case AttributeOwnerType::GreasePencilDrawing:
     case AttributeOwnerType::PointCloud: {
       /* This should be implemented with #AttributeStorage instead. */
       BLI_assert_unreachable();
@@ -180,28 +183,6 @@ static std::array<DomainInfo, ATTR_DOMAIN_NUM> get_domains(const AttributeOwner 
         info[int(AttrDomain::Face)].customdata = &mesh->face_data;
         info[int(AttrDomain::Face)].length = mesh->faces_num;
       }
-      break;
-    }
-    case AttributeOwnerType::Curves: {
-      Curves *curves = owner.get_curves();
-      info[int(AttrDomain::Point)].customdata = &curves->geometry.point_data;
-      info[int(AttrDomain::Point)].length = curves->geometry.point_num;
-      info[int(AttrDomain::Curve)].customdata = &curves->geometry.curve_data;
-      info[int(AttrDomain::Curve)].length = curves->geometry.curve_num;
-      break;
-    }
-    case AttributeOwnerType::GreasePencil: {
-      GreasePencil *grease_pencil = owner.get_grease_pencil();
-      info[int(AttrDomain::Layer)].customdata = &grease_pencil->layers_data;
-      info[int(AttrDomain::Layer)].length = grease_pencil->layers().size();
-      break;
-    }
-    case AttributeOwnerType::GreasePencilDrawing: {
-      blender::bke::greasepencil::Drawing &drawing = owner.get_grease_pencil_drawing()->wrap();
-      info[int(AttrDomain::Point)].customdata = &drawing.geometry.point_data;
-      info[int(AttrDomain::Point)].length = drawing.geometry.point_num;
-      info[int(AttrDomain::Curve)].customdata = &drawing.geometry.curve_data;
-      info[int(AttrDomain::Curve)].length = drawing.geometry.curve_num;
       break;
     }
   }
@@ -281,7 +262,7 @@ bool BKE_attribute_rename(AttributeOwner &owner,
     return false;
   }
 
-  if (owner.type() == AttributeOwnerType::PointCloud) {
+  if (owner.type() != AttributeOwnerType::Mesh) {
     bke::AttributeStorage &attributes = *owner.get_storage();
     if (!attributes.lookup(old_name)) {
       BKE_report(reports, RPT_ERROR, "Attribute is not part of this geometry");
@@ -394,7 +375,7 @@ static bool attribute_name_exists(const AttributeOwner &owner, const StringRef n
 
 std::string BKE_attribute_calc_unique_name(const AttributeOwner &owner, const StringRef name)
 {
-  if (owner.type() == AttributeOwnerType::PointCloud) {
+  if (owner.type() != AttributeOwnerType::Mesh) {
     blender::bke::AttributeStorage &storage = *owner.get_storage();
     return storage.unique_name_calc(name);
   }
@@ -822,7 +803,7 @@ std::optional<blender::StringRefNull> BKE_attributes_active_name_get(AttributeOw
   if (active_index == -1) {
     return std::nullopt;
   }
-  if (owner.type() == AttributeOwnerType::PointCloud) {
+  if (owner.type() != AttributeOwnerType::Mesh) {
     bke::AttributeStorage &storage = *owner.get_storage();
     if (active_index >= storage.count()) {
       return std::nullopt;
@@ -863,7 +844,7 @@ std::optional<blender::StringRefNull> BKE_attributes_active_name_get(AttributeOw
 void BKE_attributes_active_set(AttributeOwner &owner, const StringRef name)
 {
   using namespace blender;
-  if (owner.type() == AttributeOwnerType::PointCloud) {
+  if (owner.type() != AttributeOwnerType::Mesh) {
     bke::AttributeStorage &attributes = *owner.get_storage();
     *BKE_attributes_active_index_p(owner) = attributes.index_of(name);
     return;

source/blender/blenkernel/intern/attribute_legacy_convert.cc
@@ -269,18 +269,15 @@ void mesh_convert_customdata_to_storage(Mesh &mesh)
       mesh.attribute_storage.wrap());
 }
 
-void curves_convert_storage_to_customdata(CurvesGeometry &curves)
-{
-  convert_storage_to_customdata(curves.attribute_storage.wrap(),
-                                {{AttrDomain::Point, {curves.point_data, curves.points_num()}},
-                                 {AttrDomain::Curve, {curves.curve_data, curves.curves_num()}}});
-}
 void curves_convert_customdata_to_storage(CurvesGeometry &curves)
 {
   attribute_legacy_convert_customdata_to_storage(
       {{AttrDomain::Point, {curves.point_data, curves.points_num()}},
-       {AttrDomain::Curve, {curves.curve_data, curves.curves_num()}}},
+       {AttrDomain::Curve, {curves.curve_data_legacy, curves.curves_num()}}},
       curves.attribute_storage.wrap());
+  /* Update the curve type count again (the first time was done on file-read, where
+   * #AttributeStorage data doesn't exist yet for older fiels). */
+  curves.update_curve_types();
 }
 
 void pointcloud_convert_customdata_to_storage(PointCloud &pointcloud)
@@ -290,16 +287,11 @@ void pointcloud_convert_customdata_to_storage(PointCloud &pointcloud)
       pointcloud.attribute_storage.wrap());
 }
 
-void grease_pencil_convert_storage_to_customdata(GreasePencil &grease_pencil)
-{
-  convert_storage_to_customdata(
-      grease_pencil.attribute_storage.wrap(),
-      {{AttrDomain::Layer, {grease_pencil.layers_data, int(grease_pencil.layers().size())}}});
-}
 void grease_pencil_convert_customdata_to_storage(GreasePencil &grease_pencil)
 {
   attribute_legacy_convert_customdata_to_storage(
-      {{AttrDomain::Layer, {grease_pencil.layers_data, int(grease_pencil.layers().size())}}},
+      {{AttrDomain::Layer,
+        {grease_pencil.layers_data_legacy, int(grease_pencil.layers().size())}}},
       grease_pencil.attribute_storage.wrap());
 }
 

source/blender/blenkernel/intern/bake_items_serialize.cc
@@ -627,7 +627,7 @@ static std::optional<CurvesGeometry> try_load_curves_geometry(const DictionaryVa
   }
 
   CurvesGeometry curves;
-  CustomData_free_layer_named(&curves.point_data, "position");
+  curves.attribute_storage.wrap().remove("position");
   curves.point_num = io_curves.lookup_int("num_points").value_or(0);
   curves.curve_num = io_curves.lookup_int("num_curves").value_or(0);
 

commit 07b070404a853cb4ea1f66347043f537e338aea7

Fix: Grease Pencil: Problem removing last layer with attributes
Caused by 68759af516
Deleting first layer (i.e. last in the tree view), layer attribute values
of previous layers gets messed up due to wrong offset value when
copying attribute data

Pull Request: https://projects.blender.org/blender/blender/pulls/141772

source/blender/blenkernel/intern/grease_pencil.cc
@@ -4147,10 +4147,9 @@ static void shrink_attribute_storage(blender::bke::AttributeStorage &storage,
 
         auto new_data = bke::Attribute::ArrayData::ForUninitialized(type, size - 1);
         type.copy_construct_n(data.data, new_data.data, range_before.size());
-        type.copy_construct_n(
-            POINTER_OFFSET(data.data, type.size * range_after.start()),
-            POINTER_OFFSET(new_data.data, type.size * (range_before.start() - 1)),
-            range_after.size());
+        type.copy_construct_n(POINTER_OFFSET(data.data, type.size * range_after.start()),
+                              POINTER_OFFSET(new_data.data, type.size * index_to_remove),
+                              range_after.size());
 
         attr.assign_data(std::move(new_data));
       }