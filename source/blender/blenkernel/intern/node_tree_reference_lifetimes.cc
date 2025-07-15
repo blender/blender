655afbb23547fd00f816c58c99d9939a8d2e5365
Jacques Lucke <jacques@blender.org>
2025-07-15 06:48:24 +0200

    Fix: Geometry Nodes: output fields from closures
    This fixes the anonymous attribute lifetime inferencing for the closure zone and
    the evaluate closure node. It also adds new regression tests for cases where a
    closure outputs a field.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/141925

--- source/blender/blenkernel/intern/node_tree_reference_lifetimes.cc
+++ source/blender/blenkernel/intern/node_tree_reference_lifetimes.cc
@@ -55,7 +55,7 @@ std::ostream &operator<<(std::ostream &stream, const ReferenceSetInfo &info)
 
 static bool socket_may_have_reference(const bNodeSocket &socket)
 {
-  return socket.may_be_field();
+  return socket.may_be_field() || ELEM(socket.type, SOCK_BUNDLE, SOCK_CLOSURE);
 }
 
 static bool or_into_each_other_masked(MutableBoundedBitSpan a,
@@ -293,6 +293,27 @@ static Vector<ReferenceSetInfo> find_reference_sets(
       }
     }
   }
+  /* Each output of the Evaluate Closure node may reference data in any other output. We can't know
+   * exactly what references what here. */
+  for (const bNode *node : tree.nodes_by_type("GeometryNodeEvaluateClosure")) {
+    const auto &storage = *static_cast<NodeGeometryEvaluateClosure *>(node->storage);
+    Vector<const bNodeSocket *> reference_outputs;
+    for (const int i : IndexRange(storage.output_items.items_num)) {
+      const NodeGeometryEvaluateClosureOutputItem &item = storage.output_items.items[i];
+      if (can_contain_referenced_data(eNodeSocketDatatype(item.socket_type))) {
+        reference_outputs.append(&node->output_socket(i));
+      }
+    }
+    if (!reference_outputs.is_empty()) {
+      for (const int i : IndexRange(storage.output_items.items_num)) {
+        const NodeGeometryEvaluateClosureOutputItem &item = storage.output_items.items[i];
+        if (can_contain_reference(eNodeSocketDatatype(item.socket_type))) {
+          reference_sets.append({ReferenceSetType::LocalReferenceSet, &node->output_socket(i)});
+          reference_sets.last().potential_data_origins.extend(reference_outputs);
+        }
+      }
+    }
+  }
 
   const bNodeTreeZones *zones = tree.zones();
   if (!zones) {
@@ -518,12 +539,29 @@ static bool pass_left_to_right(const bNodeTree &tree,
         /* References passed through border links are referenced by the closure. */
         const BitVector<> passed_in_references = get_references_coming_from_outside_zone(
             *zone, {&r_potential_reference_by_socket});
+        const BitVector<> passed_in_data = get_references_coming_from_outside_zone(
+            *zone, {&r_potential_data_by_socket});
         const int dst_index = output_node.output_socket(0).index_in_tree();
-        for (const int i : node->input_sockets().index_range()) {
-          const int src_index = output_node.input_socket(i).index_in_tree();
-          r_potential_data_by_socket[dst_index] |= r_potential_data_by_socket[src_index];
-          r_potential_reference_by_socket[dst_index] |= r_potential_reference_by_socket[src_index];
+        for ([[maybe_unused]] const int i : node->input_sockets().index_range()) {
+          r_potential_data_by_socket[dst_index] |= passed_in_data;
           r_potential_reference_by_socket[dst_index] |= passed_in_references;
+        }
+        break;
+      }
+      case GEO_NODE_EVALUATE_CLOSURE: {
+        BitVector<> potential_input_references(r_potential_reference_by_socket.group_size());
+        BitVector<> potential_input_data(r_potential_data_by_socket.group_size());
+        /* Gather all references and data from all inputs, including the once on the closure input.
+         * The output may reference any of those. */
+        for (const bNodeSocket *socket : node->input_sockets()) {
+          const int src_index = socket->index_in_tree();
+          potential_input_references |= r_potential_reference_by_socket[src_index];
+          potential_input_data |= r_potential_data_by_socket[src_index];
+        }
+        for (const bNodeSocket *out_socket : node->output_sockets()) {
+          const int dst_index = out_socket->index_in_tree();
+          r_potential_reference_by_socket[dst_index] |= potential_input_references;
+          r_potential_data_by_socket[dst_index] |= potential_input_data;
         }
         break;
       }
@@ -803,11 +841,15 @@ static bool pass_right_to_left(const bNodeTree &tree,
       case GEO_NODE_EVALUATE_CLOSURE: {
         /* Data referenced by the closure is required on all the other inputs. */
         const bNodeSocket &closure_socket = node->input_socket(0);
-        const BoundedBitSpan required_references =
+        BitVector<> required_data_on_inputs =
             potential_reference_by_socket[closure_socket.index_in_tree()];
-        for (const bNodeSocket *input_socket : node->input_sockets().drop_front(1)) {
-          const int dst_index = input_socket->index_in_tree();
-          r_required_data_by_socket[dst_index] |= required_references;
+        /* Data required on outputs is also required on inputs. */
+        for (const bNodeSocket *socket : node->output_sockets()) {
+          required_data_on_inputs |= r_required_data_by_socket[socket->index_in_tree()];
+        }
+        for (const bNodeSocket *socket : node->input_sockets()) {
+          const int dst_index = socket->index_in_tree();
+          r_required_data_by_socket[dst_index] |= required_data_on_inputs;
         }
         break;
       }
@@ -1004,8 +1046,9 @@ static std::unique_ptr<ReferenceLifetimesInfo> make_reference_lifetimes_info(con
 #if 0
   std::cout << "\n\n"
             << node_tree_to_dot(tree,
-                                bNodeTreeBitGroupVectorOptions(
-                                    {potential_reference_by_socket, required_data_by_socket}))
+                                bNodeTreeBitGroupVectorOptions({potential_data_by_socket,
+                                                                potential_reference_by_socket,
+                                                                required_data_by_socket}))
 
             << "\n\n";
 #endif
