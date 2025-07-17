blender/blender

commit 358a0479e86268607541fa8dc00336eb1d5a0e4d

Anim: create pose assets to different libraries
Similar to how brush assets are created and managed this
PR allows to export pose assets into a different library.
Because of this there is a limitation to this where each
asset is stored in a separate blend file.
This may be lifted in the future as there are planned changes in
the design phase: #122061

### Create Asset

Now available in the 3D viewport in the "Pose" menu: "Create Pose Asset".
The button in the Dope Sheet will now call this new operator as well.

Clicking either of those will open a popup in which you can:

* Choose the name of the asset, which library and catalog it goes into.
* Clicking "Create" will create a pose asset on disk in the given library.

It is possible to create files into an outside library or add it in the current file.
The latter option does a lot less since it basically just creates the
action and tags it as an asset.

If no Asset Shelf **AND** no Asset Browser is visible anywhere in Blender,
the Asset Shelf will be shown on the 3D viewport from which
the operator was called.

### Adjust Pose Asset

Right clicking a pose asset that has been created in the way described
before will have options to overwrite it.
Only the active object will be considered for updating a pose asset

Available Options (the latter 3 under the "Modify Pose Asset" submenu):
* Adjust Pose Asset: From the selected bones, update ONLY channels that
are also present in the asset. This is the default.
* Replace: Will completely replace the data in the Pose Asset from
the current selection
* Add: Adds the current selection to the Pose Asset. Any already existing
channels have their values updated
* Remove: Remove selected bones from the pose asset

Currently this refreshes the thumbnail. In the case of custom
thumbnails it might not be something want

### Deleting an existing Pose Asset

Right click on a Pose Asset and hit "Delete Pose Asset". Works in the shelf

tests/python/bl_pose_assets.py
@@ -1,0 +1,172 @@
+import unittest
+import bpy
+import pathlib
+import sys
+import tempfile
+import os
+
+"""
+blender -b --factory-startup --python tests/python/bl_pose_assets.py -- --testdir /path/to/tests/data/animation
+"""
+
+_BONE_NAME_1 = "bone"
+_BONE_NAME_2 = "bone_2"
+_LIB_NAME = "unit_test"
+
+_BBONE_VALUES = {
+    f'pose.bones["{_BONE_NAME_1}"].bbone_curveinx': (0, ),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_curveoutx': (0, ),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_curveinz': (0, ),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_curveoutz': (0, ),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_rollin': (0, ),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_rollout': (0, ),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_scalein': (1, 1, 1),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_scaleout': (1, 1, 1),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_easein': (0, ),
+    f'pose.bones["{_BONE_NAME_1}"].bbone_easeout': (0, ),
+}
+
+
+def _create_armature():
+    armature = bpy.data.armatures.new("anim_armature")
+    armature_obj = bpy.data.objects.new("anim_object", armature)
+    bpy.context.scene.collection.objects.link(armature_obj)
+    bpy.context.view_layer.objects.active = armature_obj
+    armature_obj.select_set(True)
+
+    bpy.ops.object.mode_set(mode='EDIT')
+    edit_bone = armature.edit_bones.new(_BONE_NAME_1)
+    edit_bone.head = (1, 0, 0)
+    edit_bone = armature.edit_bones.new(_BONE_NAME_2)
+    edit_bone.head = (1, 0, 0)
+
+    return armature_obj
+
+
+class CreateAssetTest(unittest.TestCase):
+
+    _library_folder = None
+    _library = None
+    _armature_object = None
+
+    def setUp(self):
+        super().setUp()
+        bpy.ops.wm.read_homefile(use_factory_startup=True)
+        self._armature_object = _create_armature()
+        self._library_folder = tempfile.TemporaryDirectory("pose_asset_test")
+
+        self._library = bpy.types.AssetLibraryCollection.new(name=_LIB_NAME, directory=self._library_folder.name)
+
+        bpy.context.view_layer.objects.active = self._armature_object
+        bpy.ops.object.mode_set(mode='POSE')
+        self._armature_object.pose.bones[_BONE_NAME_1]["bool_test"] = True
+        self._armature_object.pose.bones[_BONE_NAME_1]["float_test"] = 3.14
+        self._armature_object.pose.bones[_BONE_NAME_1]["string_test"] = "foobar"
+
+    def tearDown(self):
+        super().tearDown()
+        bpy.types.AssetLibraryCollection.remove(self._library)
+        self._library = None
+        self._library_folder.cleanup()
+
+    def test_create_local_asset(self):
+        self._armature_object.pose.bones[_BONE_NAME_1].location = (1, 1, 2)
+        self._armature_object.pose.bones[_BONE_NAME_2].location = (-1, 0, 0)
+
+        self._armature_object.pose.bones[_BONE_NAME_1].bone.select = True
+        self._armature_object.pose.bones[_BONE_NAME_2].bone.select = False
+
+        self.assertEqual(len(bpy.data.actions), 0)
+        bpy.ops.poselib.create_pose_asset(
+            pose_name="local_asset",
+            asset_library_reference='LOCAL',
+            catalog_path="unit_test")
+
+        self.assertEqual(len(bpy.data.actions), 1, "Local poses should be stored as actions")
+        pose_action = bpy.data.actions[0]
+        self.assertTrue(pose_action.asset_data is not None, "The created action should be marked as an asset")
+
+        expected_pose_values = {
+            f'pose.bones["{_BONE_NAME_1}"].location': (1, 1, 2),
+            f'pose.bones["{_BONE_NAME_1}"].rotation_quaternion': (1, 0, 0, 0),
+            f'pose.bones["{_BONE_NAME_1}"].scale': (1, 1, 1),
+
+            f'pose.bones["{_BONE_NAME_1}"]["bool_test"]': (True, ),
+            f'pose.bones["{_BONE_NAME_1}"]["float_test"]': (3.14, ),
+            # string_test is not here because it should not be keyed.
+        }
+        expected_pose_values.update(_BBONE_VALUES)
+        self.assertEqual(len(pose_action.fcurves), 26)
+        for fcurve in pose_action.fcurves:
+            self.assertTrue(
+                fcurve.data_path in expected_pose_values,
+                "Only the selected bone should be in the pose asset")
+            self.assertEqual(len(fcurve.keyframe_points), 1, "Only one key should have been created")
+            self.assertEqual(fcurve.keyframe_points[0].co.x, 1, "Poses should be on the first frame")
+            self.assertAlmostEqual(fcurve.keyframe_points[0].co.y,
+                                   expected_pose_values[fcurve.data_path][fcurve.array_index], 4)
+
+    def test_create_outside_asset(self):
+        self._armature_object.pose.bones[_BONE_NAME_1].location = (1, 1, 2)
+        self._armature_object.pose.bones[_BONE_NAME_2].location = (-1, 0, 0)
+
+        self._armature_object.pose.bones[_BONE_NAME_1].bone.select = True
+        self._armature_object.pose.bones[_BONE_NAME_2].bone.select = False
+
+        self.assertEqual(len(bpy.data.actions), 0)
+        bpy.ops.poselib.create_pose_asset(
+            pose_name="local_asset",
+            asset_library_reference=_LIB_NAME,
+            catalog_path="unit_test")
+
+        self.assertEqual(len(bpy.data.actions), 0, "The asset should not have been created in this file")
+        actions_folder = os.path.join(self._library.path, "Saved", "Actions")
+        asset_files = os.listdir(actions_folder)
+        self.assertEqual(len(asset_files),
+                         1, "The pose asset file should have been created")
+
+        with bpy.data.libraries.load(os.path.join(actions_folder, asset_files[0])) as (data_from, data_to):
+            self.assertEqual(data_from.actions, ["local_asset"])
+            data_to.actions = data_from.actions
+
+        pose_action = data_to.actions[0]
+
+        self.assertTrue(pose_action.asset_data is not None, "The created action should be marked as an asset")
+        expected_pose_values = {
+            f'pose.bones["{_BONE_NAME_1}"].location': (1, 1, 2),
+            f'pose.bones["{_BONE_NAME_1}"].rotation_quaternion': (1, 0, 0, 0),
+            f'pose.bones["{_BONE_NAME_1}"].scale': (1, 1, 1),
+            f'pose.bones["{_BONE_NAME_1}"]["bool_test"]': (True, ),
+            f'pose.bones["{_BONE_NAME_1}"]["float_test"]': (3.14, ),
+            # string_test is not here because it should not be keyed.
+        }
+        expected_pose_values.update(_BBONE_VALUES)
+        self.assertEqual(len(pose_action.fcurves), 26)
+        for fcurve in pose_action.fcurves:
+            self.assertTrue(
+                fcurve.data_path in expected_pose_values,
+                "Only the selected bone should be in the pose asset")
+            self.assertEqual(len(fcurve.keyframe_points), 1, "Only one key should have been created")
+            self.assertEqual(fcurve.keyframe_points[0].co.x, 1, "Poses should be on the first frame")
+            self.assertAlmostEqual(fcurve.keyframe_points[0].co.y,
+                                   expected_pose_values[fcurve.data_path][fcurve.array_index], 4)
+
+
+def main():
+    global args
+    import argparse
+
+    if '--' in sys.argv:
+        argv = [sys.argv[0]] + sys.argv[sys.argv.index('--') + 1:]
+    else:
+        argv = sys.argv
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--testdir', required=True, type=pathlib.Path)
+    args, remaining = parser.parse_known_args(argv)
+
+    unittest.main(argv=remaining)
+
+
+if __name__ == "__main__":
+    main()

scripts/addons_core/pose_library/gui.py
@@ -18,6 +18,16 @@ from bpy.types import (
 from bl_ui_utils.layout import operator_context
 
 
+class VIEW3D_MT_pose_modify(Menu):
+    bl_label = "Modify Pose Asset"
+
+    def draw(self, _context):
+        layout = self.layout
+
+        layout.operator("poselib.asset_modify", text="Replace").mode = "REPLACE"
+        layout.operator("poselib.asset_modify", text="Add Selected Bones").mode = "ADD"
+        layout.operator("poselib.asset_modify", text="Remove Selected Bones").mode = "REMOVE"
+
 class PoseLibraryPanel:
     @classmethod
     def pose_library_panel_poll(cls, context: Context) -> bool:
@@ -58,6 +68,11 @@ class VIEW3D_AST_pose_library(bpy.types.AssetShelf):
         props.select = False
 
         layout.separator()
+        layout.operator("poselib.asset_modify", text="Adjust Pose Asset").mode = 'ADJUST'
+        layout.menu("VIEW3D_MT_pose_modify")
+        layout.operator("poselib.asset_delete")
+
+        layout.separator()
         layout.operator("asset.open_containing_blend_file")
 
 
@@ -92,6 +107,12 @@ def pose_library_asset_browser_context_menu(self: UIList, context: Context) -> N
     props.select = False
 
     layout.separator()
+    layout.operator("poselib.asset_modify", text="Adjust Pose Asset").mode = 'ADJUST'
+    layout.menu("VIEW3D_MT_pose_modify")
+    with operator_context(layout, 'INVOKE_DEFAULT'):
+        layout.operator("poselib.asset_delete")
+
+    layout.separator()
     layout.operator("asset.assign_action")
 
     layout.separator()
@@ -107,7 +128,7 @@ class DOPESHEET_PT_asset_panel(PoseLibraryPanel, Panel):
         layout = self.layout
         col = layout.column(align=True)
         row = col.row(align=True)
-        row.operator("poselib.create_pose_asset").activate_new_action = True
+        row.operator("poselib.create_pose_asset")
         if bpy.types.POSELIB_OT_restore_previous_action.poll(context):
             row.operator("poselib.restore_previous_action", text="", icon='LOOP_BACK')
         col.operator("poselib.copy_as_asset", icon="COPYDOWN")
@@ -134,7 +155,7 @@ class ASSETBROWSER_MT_asset(Menu):
 
         layout.operator("poselib.paste_asset", icon='PASTEDOWN')
         layout.separator()
-        layout.operator("poselib.create_pose_asset").activate_new_action = False
+        layout.operator("poselib.create_pose_asset")
 
 
 # Messagebus subscription to monitor asset library changes.
@@ -181,6 +202,7 @@ def _on_blendfile_load_post(none, other_none) -> None:
 classes = (
     DOPESHEET_PT_asset_panel,
     ASSETBROWSER_MT_asset,
+    VIEW3D_MT_pose_modify,
     VIEW3D_AST_pose_library,
 )
 

scripts/addons_core/pose_library/operators.py
@@ -7,7 +7,7 @@ Pose Library - operators.
 """
 
 from pathlib import Path
-from typing import Optional, Set
+from typing import Set
 
 _need_reload = "functions" in locals()
 from . import asset_browser, functions, pose_creation, pose_usage
@@ -22,7 +22,7 @@ if _need_reload:
 
 
 import bpy
-from bpy.props import BoolProperty, StringProperty
+from bpy.props import BoolProperty
 from bpy.types import (
     Action,
     AssetRepresentation,
@@ -44,109 +44,6 @@ class PoseAssetCreator:
             and context.object.pose
             and context.selected_pose_bones_from_active_object
         )
-
-
-class POSELIB_OT_create_pose_asset(PoseAssetCreator, Operator):
-    bl_idname = "poselib.create_pose_asset"
-    bl_label = "Create Pose Asset"
-    bl_description = (
-        "Create a new Action that contains the pose of the selected bones, and mark it as Asset. "
-        "The asset will be stored in the current blend file"
-    )
-    bl_options = {'REGISTER', 'UNDO'}
-
-    pose_name: StringProperty(name="Pose Name")  # type: ignore
-    activate_new_action: BoolProperty(name="Activate New Action", default=True)  # type: ignore
-
-    @classmethod
-    def poll(cls, context: Context) -> bool:
-        if context.object is None or context.object.mode != "POSE":
-            # The operator assumes pose mode, so that bone selection is visible.
-            cls.poll_message_set("An active armature object in pose mode is needed")
-            return False
-
-        # Make sure that if there is an asset browser open, the artist can see the newly created pose asset.
-        asset_browse_area: Optional[bpy.types.Area] = asset_browser.area_from_context(context)
-        if not asset_browse_area:
-            # No asset browser is visible, so there also aren't any expectations
-            # that this asset will be visible.
-            return True
-
-        asset_space_params = asset_browser.params(asset_browse_area)
-        if asset_space_params.asset_library_reference != 'LOCAL':
-            cls.poll_message_set("Asset Browser must be set to the Current File library")
-            return False
-
-        return True
-
-    def execute(self, context: Context) -> Set[str]:
-        pose_name = self.pose_name or context.object.name
-        asset = pose_creation.create_pose_asset_from_context(context, pose_name)
-        if not asset:
-            self.report({"WARNING"}, "No keyframes were found for this pose")
-            return {"CANCELLED"}
-
-        if self.activate_new_action:
-            self._set_active_action(context, asset)
-        self._activate_asset_in_browser(context, asset)
-        return {'FINISHED'}
-
-    def _set_active_action(self, context: Context, asset: Action) -> None:
-        self._prevent_action_loss(context.object)
-
-        anim_data = context.object.animation_data_create()
-        context.window_manager.poselib_previous_action = anim_data.action
-        anim_data.action = asset
-        # The `pass` on `AttributeError` and `IndexError` is just for while
-        # slotted actions are still behind an experimental flag, and thus the
-        # `slots` attribute may not exist when Blender is built without
-        # experimental features or it might be empty when the flag is simply
-        # disabled. The `try/except` here can be removed when slotted actions
-        # are taken out of experimental.
-        try:
-            anim_data.action_slot = asset.slots[0]
-        except (AttributeError, IndexError):
-            pass
-
-    def _activate_asset_in_browser(self, context: Context, asset: Action) -> None:
-        """Activate the new asset in the appropriate Asset Browser.
-
-        This makes it possible to immediately check & edit the created pose asset.
-        """
-
-        asset_browse_area: Optional[bpy.types.Area] = asset_browser.area_from_context(context)
-        if not asset_browse_area:
-            return
-
-        # After creating an asset, the window manager has to process the
-        # notifiers before editors should be manipulated.
-        pose_creation.assign_from_asset_browser(asset, asset_browse_area)
-
-        # Pass deferred=True, because we just created a new asset that isn't
-        # known to the Asset Browser space yet. That requires the processing of
-        # notifiers, which will only happen after this code has finished
-        # running.
-        asset_browser.activate_asset(asset, asset_browse_area, deferred=True)
-
-    def _prevent_action_loss(self, object: Object) -> None:
-        """Mark the action with Fake User if necessary.
-
-        This is to prevent action loss when we reduce its reference counter by one.
-        """
-
-        if not object.animation_data:
-            return
-
-        action = object.animation_data.action
-        if not action:
-            return
-
-        if action.use_fake_user or action.users > 1:
-            # Removing one user won't GC it.
-            return
-
-        action.use_fake_user = True
-        self.report({'WARNING'}, tip_("Action %s marked Fake User to prevent loss") % action.name)
 
 
 class POSELIB_OT_restore_previous_action(Operator):
@@ -461,7 +358,6 @@ classes = (
     POSELIB_OT_convert_old_poselib,
     POSELIB_OT_convert_old_object_poselib,
     POSELIB_OT_copy_as_asset,
-    POSELIB_OT_create_pose_asset,
     POSELIB_OT_paste_asset,
     POSELIB_OT_pose_asset_select_bones,
     POSELIB_OT_restore_previous_action,

scripts/startup/bl_ui/space_view3d.py
@@ -4151,6 +4151,9 @@ class VIEW3D_MT_pose(Menu):
         layout.menu("VIEW3D_MT_pose_showhide")
         layout.menu("VIEW3D_MT_bone_options_toggle", text="Bone Settings")
 
+        layout.separator()
+        layout.operator("POSELIB.create_pose_asset")
+
 
 class VIEW3D_MT_pose_transform(Menu):
     bl_label = "Clear Transform"

source/blender/animrig/ANIM_rna.hh
@@ -13,6 +13,11 @@
 #include "DNA_action_types.h"
 #include "RNA_types.hh"
 
+#include "RNA_path.hh"
+
+struct PointerRNA;
+struct PropertyRNA;
+
 namespace blender::animrig {
 
 /** Get the values of the given property. Casts non-float properties to float. */
@@ -20,5 +25,11 @@ Vector<float> get_rna_values(PointerRNA *ptr, PropertyRNA *prop);
 
 /** Get the rna path for the given rotation mode. */
 StringRef get_rotation_mode_path(eRotationModes rotation_mode);
+
+/**
+ * Returns a Vector of ID properties on the given pointer that can be animated. Not all pointer
+ * types are supported. Unsupported pointer types will return an empty vector.
+ */
+Vector<RNAPath> get_keyable_id_property_paths(const PointerRNA &ptr);
 
 }  // namespace blender::animrig

source/blender/animrig/intern/anim_rna.cc
@@ -6,10 +6,19 @@
  * \ingroup animrig
  */
 
+#include <fmt/format.h>
+
 #include "ANIM_rna.hh"
+
+#include "BLI_listbase.h"
+#include "BLI_string.h"
 #include "BLI_vector.hh"
-#include "DNA_action_types.h"
+
+#include "DNA_object_types.h"
+
 #include "RNA_access.hh"
+#include "RNA_path.hh"
+#include "RNA_prototypes.hh"
 
 namespace blender::animrig {
 
@@ -81,4 +90,85 @@ StringRef get_rotation_mode_path(const eRotationModes rotation_mode)
       return "rotation_euler";
   }
 }
+
+static bool is_idproperty_keyable(const IDProperty *id_prop, PointerRNA *ptr, PropertyRNA *prop)
+{
+  /* While you can cast the IDProperty* to a PropertyRNA* and pass it to the RNA_* functions, this
+   * does not work because it will not have the right flags set. Instead the resolved
+   * PointerRNA and PropertyRNA need to be passed. */
+  if (!RNA_property_anim_editable(ptr, prop)) {
+    return false;
+  }
+
+  if (ELEM(id_prop->type,
+           eIDPropertyType::IDP_BOOLEAN,
+           eIDPropertyType::IDP_INT,
+           eIDPropertyType::IDP_FLOAT,
+           eIDPropertyType::IDP_DOUBLE))
+  {
+    return true;
+  }
+
+  if (id_prop->type == eIDPropertyType::IDP_ARRAY) {
+    if (ELEM(id_prop->subtype,
+             eIDPropertyType::IDP_BOOLEAN,
+             eIDPropertyType::IDP_INT,
+             eIDPropertyType::IDP_FLOAT,
+             eIDPropertyType::IDP_DOUBLE))
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+Vector<RNAPath> get_keyable_id_property_paths(const PointerRNA &ptr)
+{
+  IDProperty *properties;
+
+  if (ptr.type == &RNA_PoseBone) {
+    const bPoseChannel *pchan = static_cast<bPoseChannel *>(ptr.data);
+    properties = pchan->prop;
+  }
+  else if (ptr.type == &RNA_Object) {
+    const Object *ob = static_cast<Object *>(ptr.data);
+    properties = ob->id.properties;
+  }
+  else {
+    /* Pointer type not supported. */
+    return {};
+  }
+
+  if (!properties) {
+    return {};
+  }
+
+  blender::Vector<RNAPath> paths;
+  LISTBASE_FOREACH (const IDProperty *, id_prop, &properties->data.group) {
+    PointerRNA resolved_ptr;
+    PropertyRNA *resolved_prop;
+    std::string path = id_prop->name;
+    /* Resolving the path twice, once as RNA property (without brackets, `"propname"`),
+     * and once as ID property (with brackets, `["propname"]`).
+     * This is required to support IDProperties that have been defined as part of an add-on.
+     * Those need to be animated through an RNA path without the brackets. */
+    bool is_resolved = RNA_path_resolve_property(
+        &ptr, path.c_str(), &resolved_ptr, &resolved_prop);
+    if (!is_resolved) {
+      char name_escaped[MAX_IDPROP_NAME * 2];
+      BLI_str_escape(name_escaped, id_prop->name, sizeof(name_escaped));
+      path = fmt::format("[\"{}\"]", name_escaped);
+      is_resolved = RNA_path_resolve_property(&ptr, path.c_str(), &resolved_ptr, &resolved_prop);
+    }
+    if (!is_resolved) {
+      continue;
+    }
+    if (is_idproperty_keyable(id_prop, &resolved_ptr, resolved_prop)) {
+      paths.append({path});
+    }
+  }
+  return paths;
+}
+
 }  // namespace blender::animrig

source/blender/editors/animation/CMakeLists.txt
@@ -3,8 +3,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 set(INC
+  ../asset
   ../include
   ../../makesrna
+  ../../asset_system
   ../../../../extern/fmtlib/include
   # RNA_prototypes.hh
   ${CMAKE_BINARY_DIR}/source/blender/makesrna
@@ -14,6 +16,7 @@ set(INC_SYS
 )
 
 set(SRC
+  anim_asset_ops.cc
   anim_channels_defines.cc
   anim_channels_edit.cc
   anim_deps.cc

commit 01918486718696f7e57694e95f3894535aa939bc

Fix #141909: Creating a pose assets captures unkeyed custom properties
This was an oversight caused by 358a0479e86268607541fa8dc00336eb1d5a0e4d

Before this, only keyed custom properties were capture into the pose asset.
This behavior is now restored.

Pull Request: https://projects.blender.org/blender/blender/pulls/141937

source/blender/editors/animation/anim_asset_ops.cc
@@ -122,6 +122,19 @@ static blender::animrig::Action &extract_pose(Main &bmain,
     BLI_assert(pose_object->pose);
     Slot &slot = action.slot_add_for_id(pose_object->id);
     const bArmature *armature = static_cast<bArmature *>(pose_object->data);
+
+    Set<RNAPath> existing_paths;
+    if (pose_object->adt && pose_object->adt->action &&
+        pose_object->adt->slot_handle != Slot::unassigned)
+    {
+      Action &pose_object_action = pose_object->adt->action->wrap();
+      const slot_handle_t pose_object_slot = pose_object->adt->slot_handle;
+      foreach_fcurve_in_action_slot(pose_object_action, pose_object_slot, [&](FCurve &fcurve) {
+        RNAPath existing_path = {fcurve.rna_path, std::nullopt, fcurve.array_index};
+        existing_paths.add(existing_path);
+      });
+    }
+
     LISTBASE_FOREACH (bPoseChannel *, pose_bone, &pose_object->pose->chanbase) {
       if (!(pose_bone->bone->flag & BONE_SELECTED) ||
           !blender::animrig::bone_is_visible(armature, pose_bone->bone))
@@ -147,6 +160,12 @@ static blender::animrig::Action &extract_pose(Main &bmain,
           continue;
         }
         for (const int i : values.index_range()) {
+          if (RNA_property_is_idprop(resolved_property) &&
+              !existing_paths.contains({rna_path_id_to_prop.value(), std::nullopt, i}))
+          {
+            /* Skipping custom properties without animation. */
+            continue;
+          }
           strip_data.keyframe_insert(
               &bmain, slot, {rna_path_id_to_prop.value(), i}, {1, values[i]}, key_settings);
         }

tests/python/bl_pose_assets.py
@@ -85,14 +85,18 @@ class CreateAssetTest(unittest.TestCase):
         self._armature_object.pose.bones[_BONE_NAME_1].bone.select = True
         self._armature_object.pose.bones[_BONE_NAME_2].bone.select = False
 
-        self.assertEqual(len(bpy.data.actions), 0)
+        self._armature_object.pose.bones[_BONE_NAME_1].keyframe_insert('["bool_test"]')
+        self._armature_object.pose.bones[_BONE_NAME_1].keyframe_insert('["float_test"]')
+
+        # There is an action for the custom properties.
+        self.assertEqual(len(bpy.data.actions), 1)
         bpy.ops.poselib.create_pose_asset(
             pose_name="local_asset",
             asset_library_reference='LOCAL',
             catalog_path="unit_test")
 
-        self.assertEqual(len(bpy.data.actions), 1, "Local poses should be stored as actions")
-        pose_action = bpy.data.actions[0]
+        self.assertEqual(len(bpy.data.actions), 2, "Local poses should be stored as actions")
+        pose_action = bpy.data.actions[1]
         self.assertTrue(pose_action.asset_data is not None, "The created action should be marked as an asset")
 
         expected_pose_values = {
@@ -122,13 +126,17 @@ class CreateAssetTest(unittest.TestCase):
         self._armature_object.pose.bones[_BONE_NAME_1].bone.select = True
         self._armature_object.pose.bones[_BONE_NAME_2].bone.select = False
 
-        self.assertEqual(len(bpy.data.actions), 0)
+        self._armature_object.pose.bones[_BONE_NAME_1].keyframe_insert('["bool_test"]')
+        self._armature_object.pose.bones[_BONE_NAME_1].keyframe_insert('["float_test"]')
+
+        # There is an action for the custom properties.
+        self.assertEqual(len(bpy.data.actions), 1)
         bpy.ops.poselib.create_pose_asset(
             pose_name="local_asset",
             asset_library_reference=_LIB_NAME,
             catalog_path="unit_test")
 
-        self.assertEqual(len(bpy.data.actions), 0, "The asset should not have been created in this file")
+        self.assertEqual(len(bpy.data.actions), 1, "The asset should not have been created in this file")
         actions_folder = os.path.join(self._library.path, "Saved", "Actions")
         asset_files = os.listdir(actions_folder)
         self.assertEqual(len(asset_files),
@@ -151,6 +159,41 @@ class CreateAssetTest(unittest.TestCase):
         }
         expected_pose_values.update(_BBONE_VALUES)
         self.assertEqual(len(pose_action.fcurves), 26)
+        for fcurve in pose_action.fcurves:
+            self.assertTrue(
+                fcurve.data_path in expected_pose_values,
+                "Only the selected bone should be in the pose asset")
+            self.assertEqual(len(fcurve.keyframe_points), 1, "Only one key should have been created")
+            self.assertEqual(fcurve.keyframe_points[0].co.x, 1, "Poses should be on the first frame")
+            self.assertAlmostEqual(fcurve.keyframe_points[0].co.y,
+                                   expected_pose_values[fcurve.data_path][fcurve.array_index], 4)
+
+    def test_custom_properties_without_keys(self):
+        # Custom properties without keys should not be added to the pose asset.
+        self._armature_object.pose.bones[_BONE_NAME_1].location = (1, 1, 2)
+        self._armature_object.pose.bones[_BONE_NAME_2].location = (-1, 0, 0)
+
+        self._armature_object.pose.bones[_BONE_NAME_1].bone.select = True
+        self._armature_object.pose.bones[_BONE_NAME_2].bone.select = False
+
+        self.assertEqual(len(bpy.data.actions), 0)
+        bpy.ops.poselib.create_pose_asset(
+            pose_name="local_asset",
+            asset_library_reference='LOCAL',
+            catalog_path="unit_test")
+
+        pose_action = bpy.data.actions[0]
+        self.assertTrue(pose_action.asset_data is not None, "The created action should be marked as an asset")
+
+        expected_pose_values = {
+            f'pose.bones["{_BONE_NAME_1}"].location': (1, 1, 2),
+            f'pose.bones["{_BONE_NAME_1}"].rotation_quaternion': (1, 0, 0, 0),
+            f'pose.bones["{_BONE_NAME_1}"].scale': (1, 1, 1),
+
+            # The custom properties are not keyed, thus they should not be in the pose asset.
+        }
+        expected_pose_values.update(_BBONE_VALUES)
+        self.assertEqual(len(pose_action.fcurves), 24)
         for fcurve in pose_action.fcurves:
             self.assertTrue(
                 fcurve.data_path in expected_pose_values,