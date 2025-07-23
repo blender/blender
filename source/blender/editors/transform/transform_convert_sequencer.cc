6571236b59558f2863ecbfe4c6d8f739c7fdd459
John Kiril Swenson <kirilswenson@gmail.com>
2025-07-23 04:14:07 +0200

    VSE: Clamp retiming key header values and snap drawing
    Fixes #142735.
    
    The original clamp patch in #134319 did not add any cases for retiming
    key transformation code. A simple solution to the crash would be to
    disable the behavior, but this PR opts to bring feature parity to
    retiming keys too.
    
    This means that header values are now more accurate, and snap lines do
    not erroneously draw when retiming keys get clamped.
    
    It also changes transition key transformation slightly, aligning the
    direction of movement with the movement of the two keys. Before, moving
    the mouse backwards would have the same effect, which was clunky and
    unintuitive.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/142809

--- source/blender/editors/transform/transform_convert_sequencer.cc
+++ source/blender/editors/transform/transform_convert_sequencer.cc
@@ -858,7 +858,7 @@ bool transform_convert_sequencer_clamp(const TransInfo *t, float r_val[2])
   int val[2] = {round_fl_to_int(r_val[0]), round_fl_to_int(r_val[1])};
   bool clamped = false;
 
-  /* Unconditional channel and handle clamping. Should never be ignored. */
+  /* Unconditional channel, retiming key, and handle clamping. Should never be ignored. */
   if (BLI_rcti_clamp_pt_v(&ts->offset_clamp, val)) {
     r_val[0] = static_cast<float>(val[0]);
     r_val[1] = float(val[1]);
--- source/blender/editors/transform/transform_convert_sequencer_retiming.cc
+++ source/blender/editors/transform/transform_convert_sequencer_retiming.cc
@@ -14,6 +14,7 @@
 
 #include "BLI_math_matrix.h"
 #include "BLI_math_vector.h"
+#include "BLI_rect.h"
 
 #include "BKE_context.hh"
 
@@ -33,6 +34,12 @@ struct TransDataSeq {
   Strip *strip;
   int orig_timeline_frame;
   int key_index; /* Some actions may need to destroy original data, use index to access it. */
+};
+
+struct TransSeq {
+  TransDataSeq *tdseq;
+  /* Maximum delta allowed before clamping selected retiming keys. Always active. */
+  rcti offset_clamp;
 };
 
 static TransData *SeqToTransData(const Scene *scene,
@@ -65,7 +72,7 @@ static TransData *SeqToTransData(const Scene *scene,
   return td;
 }
 
-static void freeSeqData(TransInfo *t, TransDataContainer *tc, TransCustomData * /*custom_data*/)
+static void freeSeqData(TransInfo *t, TransDataContainer *tc, TransCustomData *custom_data)
 {
   const TransData *const td = tc->data;
   Scene *scene = t->scene;
@@ -93,7 +100,74 @@ static void freeSeqData(TransInfo *t, TransDataContainer *tc, TransCustomData * 
         scene, seqbasep, transformed_strips, dependant, use_sync_markers);
   }
 
-  MEM_freeN(td->extra);
+  if ((custom_data->data != nullptr) && custom_data->use_free) {
+    TransSeq *ts = static_cast<TransSeq *>(custom_data->data);
+    MEM_freeN(ts->tdseq);
+    MEM_delete(ts);
+    custom_data->data = nullptr;
+  }
+}
+
+static void create_trans_seq_clamp_data(TransInfo *t, const Scene *scene)
+{
+  TransSeq *ts = (TransSeq *)TRANS_DATA_CONTAINER_FIRST_SINGLE(t)->custom.type.data;
+  const Editing *ed = seq::editing_get(scene);
+
+  /* Prevent snaps and change in `values` past `offset_clamp` for all selected retiming keys. */
+  BLI_rcti_init(&ts->offset_clamp, -INT_MAX, INT_MAX, 0, 0);
+
+  blender::Map selection = seq::retiming_selection_get(ed);
+  for (auto item : selection.items()) {
+    SeqRetimingKey *key = item.key;
+
+    /* Transition retiming key. */
+    if (seq::retiming_key_is_transition_type(key) &&
+        !seq::retiming_selection_has_whole_transition(ed, key))
+    {
+      SeqRetimingKey *key_start = seq::retiming_transition_start_get(key);
+      SeqRetimingKey *key_end = key_start + 1;
+      SeqRetimingKey *key_prev = key_start - 1;
+      SeqRetimingKey *key_next = key_end + 1;
+
+      const float midpoint = key_start->original_strip_frame_index;
+
+      /* Ensure start transition key cannot pass the previous key, or linked end transition key
+       * cannot pass the next key. This transform behavior is symmetrical and limited by the
+       * smallest distance between keys.*/
+      const int max_offset = min_ii(key_start->strip_frame_index - key_prev->strip_frame_index - 1,
+                                    key_next->strip_frame_index - key_end->strip_frame_index - 1);
+
+      if (key_start->flag & SEQ_KEY_SELECTED) {
+        /* Ensure start transition key cannot pass the midpoint. */
+        ts->offset_clamp.xmax = min_ii(midpoint - key_start->strip_frame_index,
+                                       ts->offset_clamp.xmax);
+        ts->offset_clamp.xmin = max_ii(-max_offset, ts->offset_clamp.xmin);
+      }
+      else {
+        /* Ensure end transition key cannot pass the midpoint. */
+        ts->offset_clamp.xmin = max_ii(-(key_end->strip_frame_index - midpoint - 1),
+                                       ts->offset_clamp.xmin);
+        ts->offset_clamp.xmax = min_ii(max_offset, ts->offset_clamp.xmax);
+      }
+    }
+    /* Non-transition retiming key. */
+    else {
+      Strip *strip = item.value;
+      SeqRetimingKey *key_prev = key - 1, *key_next = key + 1;
+      if (!seq::retiming_is_last_key(strip, key)) {
+        /* Ensure that this key cannot pass the next key. */
+        ts->offset_clamp.xmax = min_ii(key_next->strip_frame_index - key->strip_frame_index - 1,
+                                       ts->offset_clamp.xmax);
+        /* XXX: There is an off-by-one error for the last "fake" key's `strip_frame_index`, which
+         * is 1 less than it should be. This is not an immediate issue but should be fixed. */
+      }
+      if (key->strip_frame_index != 0) {
+        /* Ensure that this key cannot pass the previous key. */
+        ts->offset_clamp.xmin = max_ii(-(key->strip_frame_index - key_prev->strip_frame_index - 1),
+                                       ts->offset_clamp.xmin);
+      }
+    }
+  }
 }
 
 static void createTransSeqRetimingData(bContext * /*C*/, TransInfo *t)
@@ -111,17 +185,24 @@ static void createTransSeqRetimingData(bContext * /*C*/, TransInfo *t)
 
   TransDataContainer *tc = TRANS_DATA_CONTAINER_FIRST_SINGLE(t);
   tc->custom.type.free_cb = freeSeqData;
-
   tc->data_len = selection.size();
-  tc->data = MEM_calloc_arrayN<TransData>(tc->data_len, "TransSeq TransData");
-  tc->data_2d = MEM_calloc_arrayN<TransData2D>(tc->data_len, "TransSeq TransData2D");
+
+  TransSeq *ts = MEM_new<TransSeq>(__func__);
+  tc->custom.type.data = ts;
+  tc->custom.type.use_free = true;
+
+  TransData *td = MEM_calloc_arrayN<TransData>(tc->data_len, "TransSeq TransData");
+  TransData2D *td2d = MEM_calloc_arrayN<TransData2D>(tc->data_len, "TransSeq TransData2D");
   TransDataSeq *tdseq = MEM_calloc_arrayN<TransDataSeq>(tc->data_len, "TransSeq TransDataSeq");
-  TransData *td = tc->data;
-  TransData2D *td2d = tc->data_2d;
+  tc->data = td;
+  tc->data_2d = td2d;
+  ts->tdseq = tdseq;
 
   for (auto item : selection.items()) {
     SeqToTransData(t->scene, item.value, item.key, td++, td2d++, tdseq++);
   }
+
+  create_trans_seq_clamp_data(t, t->scene);
 }
 
 static void recalcData_sequencer_retiming(TransInfo *t)
@@ -137,6 +218,14 @@ static void recalcData_sequencer_retiming(TransInfo *t)
     const TransDataSeq *tdseq = static_cast<TransDataSeq *>(td->extra);
     Strip *strip = tdseq->strip;
 
+    float offset[2];
+    float offset_clamped[2];
+    sub_v2_v2v2(offset, td->loc, td->iloc);
+    copy_v2_v2(offset_clamped, offset);
+
+    transform_convert_sequencer_clamp(t, offset_clamped);
+    const int new_frame = round_fl_to_int(td->iloc[0] + offset_clamped[0]);
+
     transformed_strips.add(strip);
 
     /* Calculate translation. */
@@ -147,10 +236,10 @@ static void recalcData_sequencer_retiming(TransInfo *t)
     if (seq::retiming_key_is_transition_type(key) &&
         !seq::retiming_selection_has_whole_transition(seq::editing_get(t->scene), key))
     {
-      seq::retiming_transition_key_frame_set(t->scene, strip, key, round_fl_to_int(td2d->loc[0]));
+      seq::retiming_transition_key_frame_set(t->scene, strip, key, round_fl_to_int(new_frame));
     }
     else {
-      seq::retiming_key_timeline_frame_set(t->scene, strip, key, td2d->loc[0]);
+      seq::retiming_key_timeline_frame_set(t->scene, strip, key, new_frame);
     }
 
     seq::relations_invalidate_cache(t->scene, strip);
