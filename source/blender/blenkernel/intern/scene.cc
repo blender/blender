bedf19f1cab8656ad2546ab74215fb74d800db2c
Aras Pranckevicius <aras@nesnausk.org>
2025-07-24 20:37:16 +0200

    Cleanup: No longer require VSE Strip struct memory layout to never change
    Previously code that was reading Strip data assumed that seqbasep
    and channels members would stay at fixed offsets within a struct,
    forever into the future. Fix this by inferring their offsets from
    the file SDNA data where needed.
    
    Actual Strip DNA layout is not changed in this commit yet.
    
    Co-authored-by: Sergey Sharybin <sergey@blender.org>
    Pull Request: https://projects.blender.org/blender/blender/pulls/142940

--- source/blender/blenkernel/intern/scene.cc
+++ source/blender/blenkernel/intern/scene.cc
@@ -1370,34 +1370,24 @@ static void scene_blend_read_data(BlendDataReader *reader, ID *id)
     /* link metastack, slight abuse of structs here,
      * have to restore pointer to internal part in struct */
     {
-      void *seqbase_poin;
-      void *channels_poin;
-      /* This whole thing with seqbasep offsets is really not good
-       * and prevents changes to the Sequence struct. A more correct approach
-       * would be to calculate offset using sDNA from the file (NOT from the
-       * current Blender). Even better would be having some sort of dedicated
-       * map of seqbase pointers to avoid this offset magic. */
-      constexpr intptr_t seqbase_offset = offsetof(Strip, seqbase);
-      constexpr intptr_t channels_offset = offsetof(Strip, channels);
-#if ARCH_CPU_64_BITS
-      static_assert(seqbase_offset == 264, "Sequence seqbase member offset cannot be changed");
-      static_assert(channels_offset == 280, "Sequence channels member offset cannot be changed");
-#else
-      static_assert(seqbase_offset == 204, "Sequence seqbase member offset cannot be changed");
-      static_assert(channels_offset == 212, "Sequence channels member offset cannot be changed");
-#endif
+      const int seqbase_offset_file = BLO_read_struct_member_offset(
+          reader, "Strip", "ListBase", "seqbase");
+      const int channels_offset_file = BLO_read_struct_member_offset(
+          reader, "Strip", "ListBase", "channels");
+      const size_t seqbase_offset_mem = offsetof(Strip, seqbase);
+      const size_t channels_offset_mem = offsetof(Strip, channels);
 
       /* seqbase root pointer */
-      if (ed->seqbasep == old_seqbasep) {
+      if (ed->seqbasep == old_seqbasep || seqbase_offset_file < 0) {
         ed->seqbasep = &ed->seqbase;
       }
       else {
-        seqbase_poin = POINTER_OFFSET(ed->seqbasep, -seqbase_offset);
+        void *seqbase_poin = POINTER_OFFSET(ed->seqbasep, -seqbase_offset_file);
 
         seqbase_poin = BLO_read_get_new_data_address_no_us(reader, seqbase_poin, sizeof(Strip));
 
         if (seqbase_poin) {
-          ed->seqbasep = (ListBase *)POINTER_OFFSET(seqbase_poin, seqbase_offset);
+          ed->seqbasep = (ListBase *)POINTER_OFFSET(seqbase_poin, seqbase_offset_mem);
         }
         else {
           ed->seqbasep = &ed->seqbase;
@@ -1405,16 +1395,18 @@ static void scene_blend_read_data(BlendDataReader *reader, ID *id)
       }
 
       /* Active channels root pointer. */
-      if (ELEM(ed->displayed_channels, old_displayed_channels, nullptr)) {
+      if (ELEM(ed->displayed_channels, old_displayed_channels, nullptr) ||
+          channels_offset_file < 0)
+      {
         ed->displayed_channels = &ed->channels;
       }
       else {
-        channels_poin = POINTER_OFFSET(ed->displayed_channels, -channels_offset);
+        void *channels_poin = POINTER_OFFSET(ed->displayed_channels, -channels_offset_file);
         channels_poin = BLO_read_get_new_data_address_no_us(
             reader, channels_poin, sizeof(SeqTimelineChannel));
 
         if (channels_poin) {
-          ed->displayed_channels = (ListBase *)POINTER_OFFSET(channels_poin, channels_offset);
+          ed->displayed_channels = (ListBase *)POINTER_OFFSET(channels_poin, channels_offset_mem);
         }
         else {
           ed->displayed_channels = &ed->channels;
@@ -1427,30 +1419,30 @@ static void scene_blend_read_data(BlendDataReader *reader, ID *id)
       LISTBASE_FOREACH (MetaStack *, ms, &ed->metastack) {
         BLO_read_struct(reader, Strip, &ms->parent_strip);
 
-        if (ms->oldbasep == old_seqbasep) {
+        if (ms->oldbasep == old_seqbasep || seqbase_offset_file < 0) {
           ms->oldbasep = &ed->seqbase;
         }
         else {
-          seqbase_poin = POINTER_OFFSET(ms->oldbasep, -seqbase_offset);
+          void *seqbase_poin = POINTER_OFFSET(ms->oldbasep, -seqbase_offset_file);
           seqbase_poin = BLO_read_get_new_data_address_no_us(reader, seqbase_poin, sizeof(Strip));
           if (seqbase_poin) {
-            ms->oldbasep = (ListBase *)POINTER_OFFSET(seqbase_poin, seqbase_offset);
+            ms->oldbasep = (ListBase *)POINTER_OFFSET(seqbase_poin, seqbase_offset_mem);
           }
           else {
             ms->oldbasep = &ed->seqbase;
           }
         }
 
-        if (ELEM(ms->old_channels, old_displayed_channels, nullptr)) {
+        if (ELEM(ms->old_channels, old_displayed_channels, nullptr) || channels_offset_file < 0) {
           ms->old_channels = &ed->channels;
         }
         else {
-          channels_poin = POINTER_OFFSET(ms->old_channels, -channels_offset);
+          void *channels_poin = POINTER_OFFSET(ms->old_channels, -channels_offset_file);
           channels_poin = BLO_read_get_new_data_address_no_us(
               reader, channels_poin, sizeof(SeqTimelineChannel));
 
           if (channels_poin) {
-            ms->old_channels = (ListBase *)POINTER_OFFSET(channels_poin, channels_offset);
+            ms->old_channels = (ListBase *)POINTER_OFFSET(channels_poin, channels_offset_mem);
           }
           else {
             ms->old_channels = &ed->channels;
