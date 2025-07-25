efe14ff1667723a3b1d5db6c6385bd788b566480
Harley Acheson <harley.acheson@gmail.com>
2025-07-25 18:09:02 +0200

    UI: Fix VSE Channel Label Jiggling
    In the video below you will see how VSE labels can jiggle vertically
    while the area is resized. This is caused by the incorrect calculation
    of rect height in UI_view2d_view_to_region_y. Our BLI_rcti_size_y does
    not return the correct size, but max-min, which one unit too small for
    our inclusive integer rects like v2d->mask or region->winrct. This PR
    corrects the calculation in UI_view2d_region_to_view_y, and also in
    UI_view2d_region_to_view_x because these are so similar.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/143095

--- source/blender/editors/interface/view2d/view2d.cc
+++ source/blender/editors/interface/view2d/view2d.cc
@@ -1690,13 +1690,13 @@ void UI_view2d_region_to_view_rctf(const View2D *v2d, const rctf *rect_src, rctf
 
 float UI_view2d_view_to_region_x(const View2D *v2d, float x)
 {
-  return (v2d->mask.xmin +
-          (((x - v2d->cur.xmin) / BLI_rctf_size_x(&v2d->cur)) * BLI_rcti_size_x(&v2d->mask)));
+  return (v2d->mask.xmin + (((x - v2d->cur.xmin) / BLI_rctf_size_x(&v2d->cur)) *
+                            float(BLI_rcti_size_x(&v2d->mask) + 1)));
 }
 float UI_view2d_view_to_region_y(const View2D *v2d, float y)
 {
-  return (v2d->mask.ymin +
-          (((y - v2d->cur.ymin) / BLI_rctf_size_y(&v2d->cur)) * BLI_rcti_size_y(&v2d->mask)));
+  return (v2d->mask.ymin + (((y - v2d->cur.ymin) / BLI_rctf_size_y(&v2d->cur)) *
+                            float(BLI_rcti_size_y(&v2d->mask) + 1)));
 }
 
 bool UI_view2d_view_to_region_clip(
