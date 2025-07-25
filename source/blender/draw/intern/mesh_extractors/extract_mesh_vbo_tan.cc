blender/blender

commit b19696c0b8124d9ddfe229035f68ae841013c629

Mesh: Simplify tangents calculation
- Tangent calculation functions no longer use the CustomData struct as
  an input and output.
- The "orco" and UV map calculations are exposed as separate API
  functions to avoid a confusing internal choice between the two
- Redundancy in the API is removed, function names are clarified
- Code is moved to C++ namespace
- The "orco" case is clarified in the mesh draw tangent VBO extraction
- CD_TANGENT layers are not stored in CustomData anymore, so some of
  that infrastructure is removed.
- Broken logic for caching tangents in CustomData is removed. That
  hasn't worked for many years, if it every worked. We could investigate
  adding caching again later if that's helpful.

Overall the change is motivated by the need to move away from CustomData
for #122398. But the changes should be a general improvement that makes
the code easier to understand either way.

Testing for this PR included using the default render UV in materials,
referencing specific UV tangents by name, using the spherical position-
based tangents in a material, and baking textures (multires and normal
baking).

Pull Request: https://projects.blender.org/blender/blender/pulls/141799

source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_tan.cc
@@ -12,6 +12,7 @@
 
 #include "GPU_attribute_convert.hh"
 
+#include "BKE_attribute.hh"
 #include "BKE_editmesh_tangent.hh"
 #include "BKE_mesh.hh"
 #include "BKE_mesh_tangent.hh"
@@ -22,29 +23,17 @@
 
 namespace blender::draw {
 
-static void extract_tan_init_common(const MeshRenderData &mr,
-                                    const MeshBatchCache &cache,
-                                    GPUVertFormat *format,
-                                    gpu::VertAttrType gpu_attr_type,
-                                    CustomData *r_loop_data,
-                                    int *r_v_len,
-                                    int *r_tan_len,
-                                    char r_tangent_names[MAX_MTFACE][MAX_CUSTOMDATA_LAYER_NAME],
-                                    bool *r_use_orco_tan)
+static Array<Array<float4>> extract_tan_init_common(const MeshRenderData &mr,
+                                                    const MeshBatchCache &cache,
+                                                    GPUVertFormat *format,
+                                                    gpu::VertAttrType gpu_attr_type)
 {
   GPU_vertformat_deinterleave(format);
 
   const CustomData *cd_ldata = (mr.extract_type == MeshExtractType::BMesh) ? &mr.bm->ldata :
                                                                              &mr.mesh->corner_data;
-  const CustomData *cd_vdata = (mr.extract_type == MeshExtractType::BMesh) ? &mr.bm->vdata :
-                                                                             &mr.mesh->vert_data;
   uint32_t tan_layers = cache.cd_used.tan;
-  const float3 *orco_ptr = static_cast<const float3 *>(CustomData_get_layer(cd_vdata, CD_ORCO));
-  Span<float3> orco = orco_ptr ? Span(orco_ptr, mr.verts_num) : Span<float3>();
-  Array<float3> orco_allocated;
   bool use_orco_tan = cache.cd_used.tan_orco != 0;
-
-  int tan_len = 0;
 
   /* FIXME(#91838): This is to avoid a crash when orco tangent was requested but there are valid
    * uv layers. It would be better to fix the root cause. */
@@ -55,6 +44,50 @@ static void extract_tan_init_common(const MeshRenderData &mr,
     use_orco_tan = false;
   }
 
+  if (use_orco_tan) {
+    Array<float4> tangents;
+    if (mr.extract_type == MeshExtractType::BMesh) {
+      Array<float3> positions = BM_mesh_vert_coords_alloc(mr.bm);
+      tangents = BKE_editmesh_orco_tangents_calc(
+          mr.edit_bmesh, mr.bm_face_normals, mr.bm_loop_normals, positions);
+    }
+    else {
+      Span<float3> orco;
+      Array<float3> orco_allocated;
+      if (const float3 *orco_ptr = static_cast<const float3 *>(
+              CustomData_get_layer(&mr.mesh->vert_data, CD_ORCO)))
+      {
+        orco = Span(orco_ptr, mr.verts_num);
+      }
+      else {
+        orco_allocated = mr.vert_positions;
+        /* TODO: This is not thread-safe. Draw extraction should not modify the mesh. */
+        BKE_mesh_orco_verts_transform(const_cast<Mesh *>(mr.mesh), orco_allocated, false);
+        orco = orco_allocated;
+      }
+      tangents = bke::mesh::calc_orco_tangents(mr.vert_positions,
+                                               mr.faces,
+                                               mr.corner_verts,
+                                               mr.mesh->corner_tris(),
+                                               mr.mesh->corner_tri_faces(),
+                                               mr.sharp_faces,
+                                               mr.mesh->vert_normals(),
+                                               mr.face_normals,
+                                               mr.corner_normals,
+                                               orco);
+    }
+
+    char attr_name[32], attr_safe_name[GPU_MAX_SAFE_ATTR_NAME];
+    GPU_vertformat_safe_attr_name("orco", attr_safe_name, GPU_MAX_SAFE_ATTR_NAME);
+    SNPRINTF(attr_name, "t%s", attr_safe_name);
+    GPU_vertformat_attr_add(format, attr_name, gpu_attr_type);
+    GPU_vertformat_alias_add(format, "t");
+    GPU_vertformat_alias_add(format, "at");
+
+    return {std::move(tangents)};
+  }
+
+  Vector<StringRef> uv_names;
   for (int i = 0; i < MAX_MTFACE; i++) {
     if (tan_layers & (1 << i)) {
       char attr_name[32], attr_safe_name[GPU_MAX_SAFE_ATTR_NAME];
@@ -72,90 +105,45 @@ static void extract_tan_init_common(const MeshRenderData &mr,
         GPU_vertformat_alias_add(format, "at");
       }
 
-      STRNCPY(r_tangent_names[tan_len++], layer_name);
-    }
-  }
-  if (use_orco_tan && orco.is_empty()) {
-    /* If `orco` is not available compute it ourselves */
-    orco_allocated.reinitialize(mr.verts_num);
-
-    if (mr.extract_type == MeshExtractType::BMesh) {
-      BMesh *bm = mr.bm;
-      for (int v = 0; v < mr.verts_num; v++) {
-        const BMVert *eve = BM_vert_at_index(bm, v);
-        /* Exceptional case where #bm_vert_co_get can be avoided, as we want the original coords.
-         * not the distorted ones. */
-        orco_allocated[v] = eve->co;
-      }
-    }
-    else {
-      for (int v = 0; v < mr.verts_num; v++) {
-        orco_allocated[v] = mr.vert_positions[v];
-      }
-    }
-    /* TODO: This is not thread-safe. Draw extraction should not modify the mesh. */
-    BKE_mesh_orco_verts_transform(const_cast<Mesh *>(mr.mesh), orco_allocated, false);
-    orco = orco_allocated;
-  }
-
-  /* Start Fresh */
-  CustomData_reset(r_loop_data);
-  if (tan_len != 0 || use_orco_tan) {
-    short tangent_mask = 0;
-    bool calc_active_tangent = false;
-    if (mr.extract_type == MeshExtractType::BMesh) {
-      BKE_editmesh_loop_tangent_calc(mr.edit_bmesh,
-                                     calc_active_tangent,
-                                     r_tangent_names,
-                                     tan_len,
-                                     mr.bm_face_normals,
-                                     mr.bm_loop_normals,
-                                     orco,
-                                     r_loop_data,
-                                     mr.corners_num,
-                                     &tangent_mask);
-    }
-    else {
-      BKE_mesh_calc_loop_tangent_ex(mr.vert_positions,
-                                    mr.faces,
-                                    mr.corner_verts,
-                                    mr.mesh->corner_tris(),
-                                    mr.mesh->corner_tri_faces(),
-                                    mr.sharp_faces,
-                                    cd_ldata,
-                                    calc_active_tangent,
-                                    r_tangent_names,
-                                    tan_len,
-                                    mr.mesh->vert_normals(),
-                                    mr.face_normals,
-                                    mr.corner_normals,
-                                    orco,
-                                    r_loop_data,
-                                    mr.corner_verts.size(),
-                                    &tangent_mask);
+      uv_names.append(layer_name);
     }
   }
 
-  if (use_orco_tan) {
-    char attr_name[32], attr_safe_name[GPU_MAX_SAFE_ATTR_NAME];
-    const char *layer_name = CustomData_get_layer_name(r_loop_data, CD_TANGENT, 0);
-    GPU_vertformat_safe_attr_name(layer_name, attr_safe_name, GPU_MAX_SAFE_ATTR_NAME);
-    SNPRINTF(attr_name, "t%s", attr_safe_name);
-    GPU_vertformat_attr_add(format, attr_name, gpu_attr_type);
-    GPU_vertformat_alias_add(format, "t");
-    GPU_vertformat_alias_add(format, "at");
+  if (uv_names.is_empty()) {
+    GPU_vertformat_attr_add(format, "dummy", blender::gpu::VertAttrType::SFLOAT_32);
+    return {};
   }
 
-  int v_len = mr.corners_num;
+  Array<Array<float4>> results;
+  if (mr.extract_type == MeshExtractType::BMesh) {
+    results = BKE_editmesh_uv_tangents_calc(
+        mr.edit_bmesh, mr.bm_face_normals, mr.bm_loop_normals, uv_names);
+  }
+  else {
+    Array<VArraySpan<float2>> uv_maps(uv_names.size());
+    Array<Span<float2>> uv_map_spans(uv_names.size());
+    const bke::AttributeAccessor attributes = mr.mesh->attributes();
+    for (const int i : uv_names.index_range()) {
+      uv_maps[i] = *attributes.lookup<float2>(uv_names[i], bke::AttrDomain::Corner);
+      uv_map_spans[i] = uv_maps[i];
+    }
+    results = bke::mesh::calc_uv_tangents(mr.vert_positions,
+                                          mr.faces,
+                                          mr.corner_verts,
+                                          mr.mesh->corner_tris(),
+                                          mr.mesh->corner_tri_faces(),
+                                          mr.sharp_faces,
+                                          mr.mesh->vert_normals(),
+                                          mr.face_normals,
+                                          mr.corner_normals,
+                                          uv_map_spans);
+  }
+
   if (format->attr_len == 0) {
     GPU_vertformat_attr_add(format, "dummy", blender::gpu::VertAttrType::SFLOAT_32);
-    /* VBO will not be used, only allocate minimum of memory. */
-    v_len = 1;
   }
 
-  *r_use_orco_tan = use_orco_tan;
-  *r_v_len = v_len;
-  *r_tan_len = tan_len;
+  return results;
 }
 
 gpu::VertBufPtr extract_tangents(const MeshRenderData &mr,
@@ -166,41 +154,19 @@ gpu::VertBufPtr extract_tangents(const MeshRenderData &mr,
                                              gpu::VertAttrType::SNORM_10_10_10_2;
 
   GPUVertFormat format = {0};
-  CustomData corner_data;
-  int v_len = 0;
-  int tan_len = 0;
-  bool use_orco_tan;
-  char tangent_names[MAX_MTFACE][MAX_CUSTOMDATA_LAYER_NAME];
-  extract_tan_init_common(mr,
-                          cache,
-                          &format,
-                          gpu_attr_type,
-                          &corner_data,
-                          &v_len,
-                          &tan_len,
-                          tangent_names,
-                          &use_orco_tan);
+  const Array<Array<float4>> tangents = extract_tan_init_common(mr, cache, &format, gpu_attr_type);
+
+  const int vbo_size = tangents.size() * mr.corners_num;
 
   gpu::VertBufPtr vbo = gpu::VertBufPtr(GPU_vertbuf_create_with_format(format));
-  GPU_vertbuf_data_alloc(*vbo, v_len);
+  GPU_vertbuf_data_alloc(*vbo, vbo_size);
 
   if (use_hq) {
     short4 *tan_data = vbo->data<short4>().data();
-    for (int i = 0; i < tan_len; i++) {
-      const char *name = tangent_names[i];
-      const float(*layer_data)[4] = (const float(*)[4])CustomData_get_layer_named(
-          &corner_data, CD_TANGENT, name);
+    for (const int i : tangents.index_range()) {
+      const Span<float4> layer_data = tangents[i];
       for (int corner = 0; corner < mr.corners_num; corner++) {
-        *tan_data = gpu::convert_normal<short4>(layer_data[corner]);
-        (*tan_data)[3] = (layer_data[corner][3] > 0.0f) ? SHRT_MAX : SHRT_MIN;
-        tan_data++;
-      }
-    }
-    if (use_orco_tan) {
-      const float(*layer_data)[4] = (const float(*)[4])CustomData_get_layer_n(
-          &corner_data, CD_TANGENT, 0);
-      for (int corner = 0; corner < mr.corners_num; corner++) {
-        *tan_data = gpu::convert_normal<short4>(layer_data[corner]);
+        *tan_data = gpu::convert_normal<short4>(float3(layer_data[corner]));
         (*tan_data)[3] = (layer_data[corner][3] > 0.0f) ? SHRT_MAX : SHRT_MIN;
         tan_data++;
       }
@@ -208,28 +174,16 @@ gpu::VertBufPtr extract_tangents(const MeshRenderData &mr,
   }
   else {
     gpu::PackedNormal *tan_data = vbo->data<gpu::PackedNormal>().data();
-    for (int i = 0; i < tan_len; i++) {
-      const char *name = tangent_names[i];
-      const float(*layer_data)[4] = (const float(*)[4])CustomData_get_layer_named(
-          &corner_data, CD_TANGENT, name);
+    for (const int i : tangents.index_range()) {
+      const Span<float4> layer_data = tangents[i];
       for (int corner = 0; corner < mr.corners_num; corner++) {
-        *tan_data = gpu::convert_normal<gpu::PackedNormal>(layer_data[corner]);
-        tan_data->w = (layer_data[corner][3] > 0.0f) ? 1 : -2;
-        tan_data++;
-      }
-    }
-    if (use_orco_tan) {
-      const float(*layer_data)[4] = (const float(*)[4])CustomData_get_layer_n(
-          &corner_data, CD_TANGENT, 0);
-      for (int corner = 0; corner < mr.corners_num; corner++) {
-        *tan_data = gpu::convert_normal<gpu::PackedNormal>(layer_data[corner]);
+        *tan_data = gpu::convert_normal<gpu::PackedNormal>(float3(layer_data[corner]));
         tan_data->w = (layer_data[corner][3] > 0.0f) ? 1 : -2;
         tan_data++;
       }
     }
   }
 
-  CustomData_free(&corner_data);
   return vbo;
 }
 
@@ -246,20 +200,9 @@ gpu::VertBufPtr extract_tangents_subdiv(const MeshRenderData &mr,
 {
   gpu::VertAttrType gpu_attr_type = gpu::VertAttrType::SFLOAT_32_32_32_32;
   GPUVertFormat format = {0};
-  CustomData corner_data;
-  int coarse_len = 0;
-  int tan_len = 0;
-  bool use_orco_tan;
-  char tangent_names[MAX_MTFACE][MAX_CUSTOMDATA_LAYER_NAME];
-  extract_tan_init_common(mr,
-                          cache,
-                          &format,
-                          gpu_attr_type,
-                          &corner_data,
-                          &coarse_len,
-                          &tan_len,
-                          tangent_names,
-                          &use_orco_tan);
+  const Array<Array<float4>> tangents = extract_tan_init_common(mr, cache, &format, gpu_attr_type);
+
+  const int coarse_vbo_size = tangents.size() * mr.corners_num;
 
   gpu::VertBufPtr vbo = gpu::VertBufPtr(
       GPU_vertbuf_create_on_device(format, subdiv_cache.num_subdiv_loops));
@@ -267,35 +210,17 @@ gpu::VertBufPtr extract_tangents_subdiv(const MeshRenderData &mr,
   gpu::VertBuf *coarse_vbo = GPU_vertbuf_calloc();
   /* Dynamic as we upload and interpolate layers one at a time. */
   GPU_vertbuf_init_with_format_ex(*coarse_vbo, get_coarse_tan_format(), GPU_USAGE_DYNAMIC);
-  GPU_vertbuf_data_alloc(*coarse_vbo, coarse_len);
+  GPU_vertbuf_data_alloc(*coarse_vbo, coarse_vbo_size);
 
   /* Index of the tangent layer in the compact buffer. Used layers are stored in a single buffer.
    */
   int pack_layer_index = 0;
-  for (int i = 0; i < tan_len; i++) {
+  for (const int i : tangents.index_range()) {
     float4 *tan_data = coarse_vbo->data<float4>().data();
-    const char *name = tangent_names[i];
-    const float(*layer_data)[4] = (const float(*)[4])CustomData_get_layer_named(
-        &corner_data, CD_TANGENT, name);
+    const Span<float4> values = tangents[i];
     for (int corner = 0; corner < mr.corners_num; corner++) {
-      *tan_data = layer_data[corner];
-      (*tan_data)[3] = (layer_data[corner][3] > 0.0f) ? 1.0f : -1.0f;
-      tan_data++;
-    }
-
-    /* Ensure data is uploaded properly. */
-    GPU_vertbuf_tag_dirty(coarse_vbo);
-    /* Include stride in offset. */
-    const int dst_offset = int(subdiv_cache.num_subdiv_loops) * 4 * pack_layer_index++;
-    draw_subdiv_interp_custom_data(subdiv_cache, *coarse_vbo, *vbo, GPU_COMP_F32, 4, dst_offset);
-  }
-  if (use_orco_tan) {
-    float4 *tan_data = coarse_vbo->data<float4>().data();
-    const float(*layer_data)[4] = (const float(*)[4])CustomData_get_layer_n(
-        &corner_data, CD_TANGENT, 0);
-    for (int corner = 0; corner < mr.corners_num; corner++) {
-      *tan_data = layer_data[corner];
-      (*tan_data)[3] = (layer_data[corner][3] > 0.0f) ? 1.0f : -1.0f;
+      *tan_data = values[corner];
+      (*tan_data)[3] = (values[corner][3] > 0.0f) ? 1.0f : -1.0f;
       tan_data++;
     }
 
@@ -306,7 +231,6 @@ gpu::VertBufPtr extract_tangents_subdiv(const MeshRenderData &mr,
     draw_subdiv_interp_custom_data(subdiv_cache, *coarse_vbo, *vbo, GPU_COMP_F32, 4, dst_offset);
   }
 
-  CustomData_free(&corner_data);
   GPU_vertbuf_discard(coarse_vbo);
   return vbo;
 }

source/blender/blenkernel/BKE_editmesh_tangent.hh
@@ -8,27 +8,24 @@
  * \ingroup bke
  */
 
+#include "BLI_array.hh"
 #include "BLI_math_vector_types.hh"
 #include "BLI_span.hh"
-
-#include "DNA_customdata_types.h"
+#include "BLI_string_ref.hh"
 
 struct BMEditMesh;
 
 /**
  * \see #BKE_mesh_calc_loop_tangent, same logic but used arrays instead of #BMesh data.
- *
- * \note This function is not so normal, its using #BMesh.ldata as input,
- * but output's to #Mesh.corner_data.
- * This is done because #CD_TANGENT is cache data used only for drawing.
  */
-void BKE_editmesh_loop_tangent_calc(BMEditMesh *em,
-                                    bool calc_active_tangent,
-                                    const char (*tangent_names)[MAX_CUSTOMDATA_LAYER_NAME],
-                                    int tangent_names_len,
-                                    blender::Span<blender::float3> face_normals,
-                                    blender::Span<blender::float3> corner_normals,
-                                    blender::Span<blender::float3> vert_orco,
-                                    CustomData *dm_loopdata_out,
-                                    uint dm_loopdata_out_len,
-                                    short *tangent_mask_curr_p);
+blender::Array<blender::Array<blender::float4>> BKE_editmesh_uv_tangents_calc(
+    BMEditMesh *em,
+    blender::Span<blender::float3> face_normals,
+    blender::Span<blender::float3> corner_normals,
+    blender::Span<blender::StringRef> uv_names);
+
+blender::Array<blender::float4> BKE_editmesh_orco_tangents_calc(
+    BMEditMesh *em,
+    blender::Span<blender::float3> face_normals,
+    blender::Span<blender::float3> corner_normals,
+    blender::Span<blender::float3> vert_orco);

source/blender/blenkernel/BKE_mesh_legacy_derived_mesh.hh
@@ -69,8 +69,6 @@ struct DerivedMesh {
   /* Always owned by this object. */
   int *face_offsets;
 
-  short tangent_mask; /* which tangent layers are calculated */
-
   /* Misc. Queries */
 
   /* Also called in Editmode */

source/blender/blenkernel/BKE_mesh_runtime.hh
@@ -33,8 +33,6 @@ void BKE_mesh_runtime_ensure_edit_data(Mesh *mesh);
  *
  * For "smaller" changes to meshes like updating positions, consider calling a more specific update
  * function like #Mesh::tag_positions_changed().
- *
- * Also note that some derived caches like #CD_TANGENT are stored directly in #CustomData.
  */
 void BKE_mesh_runtime_clear_geometry(Mesh *mesh);
 

source/blender/blenkernel/BKE_mesh_tangent.hh
@@ -7,11 +7,9 @@
  * \ingroup bke
  */
 
-#include "DNA_customdata_types.h"
-
+#include "BLI_array.hh"
 #include "BLI_math_vector_types.hh"
 #include "BLI_offset_indices.hh"
-#include "BLI_sys_types.h"
 
 struct ReportList;
 struct Mesh;
@@ -44,54 +42,31 @@ void BKE_mesh_calc_loop_tangent_single(Mesh *mesh,
                                        float (*r_looptangents)[4],
                                        ReportList *reports);
 
+namespace blender::bke::mesh {
+
 /**
- * See: #BKE_editmesh_loop_tangent_calc (matching logic).
+ * See: #BKE_editmesh_uv_tangents_calc (matching logic).
  */
-void BKE_mesh_calc_loop_tangent_ex(blender::Span<blender::float3> vert_positions,
-                                   blender::OffsetIndices<int> faces,
-                                   blender::Span<int> corner_verts,
-                                   blender::Span<blender::int3> corner_tris,
-                                   blender::Span<int> corner_tri_faces,
-                                   blender::Span<bool> sharp_faces,
-                                   const CustomData *loopdata,
-                                   bool calc_active_tangent,
-                                   const char (*tangent_names)[MAX_CUSTOMDATA_LAYER_NAME],
-                                   int tangent_names_len,
-                                   blender::Span<blender::float3> vert_normals,
-                                   blender::Span<blender::float3> face_normals,
-                                   blender::Span<blender::float3> corner_normals,
-                                   blender::Span<blender::float3> vert_orco,
-                                   /* result */
-                                   CustomData *loopdata_out,
-                                   uint loopdata_out_len,
-                                   short *tangent_mask_curr_p);
+Array<Array<float4>> calc_uv_tangents(Span<float3> vert_positions,
+                                      OffsetIndices<int> faces,
+                                      Span<int> corner_verts,
+                                      Span<int3> corner_tris,
+                                      Span<int> corner_tri_faces,
+                                      Span<bool> sharp_faces,
+                                      Span<float3> vert_normals,
+                                      Span<float3> face_normals,
+                                      Span<float3> corner_normals,
+                                      Span<Span<float2>> uv_maps);
 
-void BKE_mesh_calc_loop_tangents(Mesh *mesh_eval,
-                                 bool calc_active_tangent,
-                                 const char (*tangent_names)[MAX_CUSTOMDATA_LAYER_NAME],
-                                 int tangent_names_len);
+Array<float4> calc_orco_tangents(Span<float3> vert_positions,
+                                 OffsetIndices<int> faces,
+                                 Span<int> corner_verts,
+                                 Span<int3> corner_tris,
+                                 Span<int> corner_tri_faces,
+                                 Span<bool> sharp_faces,
+                                 Span<float3> vert_normals,
+                                 Span<float3> face_normals,
+                                 Span<float3> corner_normals,
+                                 Span<float3> vert_orco);
 
-/* Helpers */
-void BKE_mesh_add_loop_tangent_named_layer_for_uv(const CustomData *uv_data,
-                                                  CustomData *tan_data,
-                                                  int numLoopData,
-                                                  const char *layer_name);
-
-#define DM_TANGENT_MASK_ORCO (1 << 9)
-/**
- * Here we get some useful information such as active uv layer name and
- * search if it is already in tangent_names.
- * Also, we calculate tangent_mask that works as a descriptor of tangents state.
- * If tangent_mask has changed, then recalculate tangents.
- */
-void BKE_mesh_calc_loop_tangent_step_0(const CustomData *loopData,
-                                       bool calc_active_tangent,
-                                       const char (*tangent_names)[MAX_CUSTOMDATA_LAYER_NAME],
-                                       int tangent_names_count,
-                                       bool *rcalc_act,
-                                       bool *rcalc_ren,
-                                       int *ract_uv_n,
-                                       int *rren_uv_n,
-                                       char *ract_uv_name,
-                                       char *rren_uv_name,
-                                       short *rtangent_mask);
+}  // namespace blender::bke::mesh

source/blender/blenkernel/intern/attribute_legacy_convert.cc
@@ -23,6 +23,7 @@ std::optional<AttrType> custom_data_type_to_attr_type(const eCustomDataType data
   switch (data_type) {
     case CD_NUMTYPES:
     case CD_AUTO_FROM_NAME:
+    case CD_TANGENT:
       /* These type is not used for actual #CustomData layers. */
       BLI_assert_unreachable();
       return std::nullopt;
@@ -57,7 +58,6 @@ std::optional<AttrType> custom_data_type_to_attr_type(const eCustomDataType data
     case CD_NORMAL:
     case CD_ORIGSPACE:
     case CD_ORCO:
-    case CD_TANGENT:
     case CD_MDISPS:
     case CD_CLOTH_ORCO:
     case CD_ORIGSPACE_MLOOP:

commit 33b430892cc87dfb26666145c05ca12156e42217

Fix #143218: Uninitialized VBO data using multiple UV tangents
Caused by b19696c0b8124d9ddfe229035f68ae841013c629.

I misunderstood the meaning of the vertex buffer allocation size.
Even though the attributes aren't interleaved, the fact that there
are multiple attributes is still included in the "element size" that's
multiplied with the size argument to `GPU_vertbuf_data_alloc`.

Also switch to spans and indices rather than incrementing raw pointers,
which would have made this much faster to debug.

source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_tan.cc
@@ -7,6 +7,7 @@
  */
 
 #include <climits>
+#include <iostream>
 
 #include "BLI_string.h"
 
@@ -156,32 +157,35 @@ gpu::VertBufPtr extract_tangents(const MeshRenderData &mr,
   GPUVertFormat format = {0};
   const Array<Array<float4>> tangents = extract_tan_init_common(mr, cache, &format, gpu_attr_type);
 
-  const int vbo_size = tangents.size() * mr.corners_num;
-
   gpu::VertBufPtr vbo = gpu::VertBufPtr(GPU_vertbuf_create_with_format(format));
-  GPU_vertbuf_data_alloc(*vbo, vbo_size);
+  GPU_vertbuf_data_alloc(*vbo, mr.corners_num);
 
   if (use_hq) {
-    short4 *tan_data = vbo->data<short4>().data();
+    MutableSpan tan_data = vbo->data<short4>();
+    int vbo_index = 0;
     for (const int i : tangents.index_range()) {
       const Span<float4> layer_data = tangents[i];
       for (int corner = 0; corner < mr.corners_num; corner++) {
-        *tan_data = gpu::convert_normal<short4>(float3(layer_data[corner]));
-        (*tan_data)[3] = (layer_data[corner][3] > 0.0f) ? SHRT_MAX : SHRT_MIN;
-        tan_data++;
+        tan_data[vbo_index] = gpu::convert_normal<short4>(float3(layer_data[corner]));
+        tan_data[vbo_index].w = (layer_data[corner][3] > 0.0f) ? SHRT_MAX : SHRT_MIN;
+        vbo_index++;
       }
     }
+    BLI_assert(vbo_index == tan_data.size());
   }
   else {
-    gpu::PackedNormal *tan_data = vbo->data<gpu::PackedNormal>().data();
+    MutableSpan tan_data = vbo->data<gpu::PackedNormal>();
+    int vbo_index = 0;
+    vbo->data<gpu::PackedNormal>().fill(gpu::PackedNormal{101, 102, 103, -2});
     for (const int i : tangents.index_range()) {
       const Span<float4> layer_data = tangents[i];
       for (int corner = 0; corner < mr.corners_num; corner++) {
-        *tan_data = gpu::convert_normal<gpu::PackedNormal>(float3(layer_data[corner]));
-        tan_data->w = (layer_data[corner][3] > 0.0f) ? 1 : -2;
-        tan_data++;
+        tan_data[vbo_index] = gpu::convert_normal<gpu::PackedNormal>(float3(layer_data[corner]));
+        tan_data[vbo_index].w = (layer_data[corner][3] > 0.0f) ? 1 : -2;
+        vbo_index++;
       }
     }
+    BLI_assert(vbo_index == tan_data.size());
   }
 
   return vbo;
@@ -202,15 +206,13 @@ gpu::VertBufPtr extract_tangents_subdiv(const MeshRenderData &mr,
   GPUVertFormat format = {0};
   const Array<Array<float4>> tangents = extract_tan_init_common(mr, cache, &format, gpu_attr_type);
 
-  const int coarse_vbo_size = tangents.size() * mr.corners_num;
-
   gpu::VertBufPtr vbo = gpu::VertBufPtr(
       GPU_vertbuf_create_on_device(format, subdiv_cache.num_subdiv_loops));
 
   gpu::VertBuf *coarse_vbo = GPU_vertbuf_calloc();
   /* Dynamic as we upload and interpolate layers one at a time. */
   GPU_vertbuf_init_with_format_ex(*coarse_vbo, get_coarse_tan_format(), GPU_USAGE_DYNAMIC);
-  GPU_vertbuf_data_alloc(*coarse_vbo, coarse_vbo_size);
+  GPU_vertbuf_data_alloc(*coarse_vbo, mr.corners_num);
 
   /* Index of the tangent layer in the compact buffer. Used layers are stored in a single buffer.
    */