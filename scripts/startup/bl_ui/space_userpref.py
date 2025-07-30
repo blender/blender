503429df1dfc3f7b078939e3ddb91f7e78a7b081
Campbell Barton <campbell@blender.org>
2025-07-30 13:15:09 +1000

    Cleanup: replace "enabled" with "active" in the NDOF UI
    Prefer "active" so the settings can still be changed,
    typically this is preferred unless changing them would cause problems.
    
    Also assign "active" immediately after assignment,
    following the convention use elsewhere in scripts.

--- scripts/startup/bl_ui/space_userpref.py
+++ scripts/startup/bl_ui/space_userpref.py
@@ -2100,19 +2100,19 @@ class USERPREF_PT_ndof_settings(Panel):
 
         if show_3dview_settings:
             col = layout.column(heading="Orbit Center")
+            col.active = props.ndof_navigation_mode == 'OBJECT'
             col.prop(props, "ndof_orbit_center_auto")
             colsub = col.column()
+            colsub.active = props.ndof_orbit_center_auto
             colsub.prop(props, "ndof_orbit_center_selected")
-            colsub.enabled = props.ndof_orbit_center_auto
             del colsub
-            col.enabled = props.ndof_navigation_mode == 'OBJECT'
             col.separator()
 
             col = layout.column(heading="Show")
             col.prop(props, "ndof_show_guide_orbit_axis", text="Orbit Axis")
             colsub = col.column()
+            colsub.active = props.ndof_navigation_mode == 'OBJECT'
             colsub.prop(props, "ndof_show_guide_orbit_center", text="Orbit Center")
-            colsub.enabled = props.ndof_navigation_mode == 'OBJECT'
             del colsub
 
         layout.separator()
