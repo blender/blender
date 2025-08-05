blender/blender

commit 7b7c63001858280ffec1a13c2751c22cbcf27686

Refactor: Nodes: use structure type inferencing for field links
The goal here is to reduce our reliance on the existing field inferencing code
and use the new and more general structure inferencing code instead.

This patch changes patch refactors link drawing so that whether links are drawn
dashed does not depend on field inferencing anymore. Same for the invalid field
links.

I don't expect that this changes the drawing of any existing links. It just
makes it easier to fully remove the old field inferencing in the future.

Pull Request: https://projects.blender.org/blender/blender/pulls/140869

source/blender/blenkernel/intern/node_tree_update.cc
@@ -858,13 +858,12 @@ class NodeTreeMainUpdater {
     /* Automatically tag a bake item as attribute when the input is a field. The flag should not be
      * removed automatically even when the field input is disconnected because the baked data may
      * still contain attribute data instead of a single value. */
-    const Span<bke::FieldSocketState> field_states = ntree.runtime->field_states;
     for (bNode *node : ntree.nodes_by_type("GeometryNodeBake")) {
       NodeGeometryBake &storage = *static_cast<NodeGeometryBake *>(node->storage);
       for (const int i : IndexRange(storage.items_num)) {
         const bNodeSocket &socket = node->input_socket(i);
         NodeGeometryBakeItem &item = storage.items[i];
-        if (field_states[socket.index_in_tree()] == FieldSocketState::IsField) {
+        if (socket.may_be_field()) {
           item.flag |= GEO_NODE_BAKE_ITEM_IS_ATTRIBUTE;
         }
       }
@@ -876,9 +875,6 @@ class NodeTreeMainUpdater {
   {
     if (decl.identifier == "__extend__") {
       return SOCK_DISPLAY_SHAPE_CIRCLE;
-    }
-    if (socket_type_always_single(decl.socket_type)) {
-      return SOCK_DISPLAY_SHAPE_LINE;
     }
     switch (structure_type) {
       case StructureType::Single:
@@ -899,9 +895,6 @@ class NodeTreeMainUpdater {
   {
     if (decl.identifier == "__extend__") {
       return SOCK_DISPLAY_SHAPE_CIRCLE;
-    }
-    if (socket_type_always_single(decl.socket_type)) {
-      return SOCK_DISPLAY_SHAPE_LINE;
     }
     switch (structure_type) {
       case StructureType::Single: {
@@ -1308,10 +1301,7 @@ class NodeTreeMainUpdater {
         continue;
       }
       if (ntree.type == NTREE_GEOMETRY) {
-        const Span<FieldSocketState> field_states = ntree.runtime->field_states;
-        if (field_states[link->fromsock->index_in_tree()] == FieldSocketState::IsField &&
-            field_states[link->tosock->index_in_tree()] != FieldSocketState::IsField)
-        {
+        if (link->fromsock->may_be_field() && !link->tosock->may_be_field()) {
           link->flag &= ~NODE_LINK_VALID;
           ntree.runtime->link_errors.add(
               NodeLinkKey{*link}, NodeLinkError{TIP_("The node input does not support fields")});

source/blender/blenkernel/BKE_node_runtime.hh
@@ -1023,6 +1023,13 @@ inline bool bNodeSocket::is_icon_visible() const
          (this->owner_node().flag & NODE_HIDDEN || !this->is_panel_collapsed());
 }
 
+inline bool bNodeSocket::may_be_field() const
+{
+  return ELEM(this->owner_tree().runtime->inferred_structure_types[this->index_in_tree()],
+              blender::nodes::StructureType::Field,
+              blender::nodes::StructureType::Dynamic);
+}
+
 inline bNode &bNodeSocket::owner_node()
 {
   BLI_assert(blender::bke::node_tree_runtime::topology_cache_is_available(*this));

source/blender/blenkernel/intern/node_tree_reference_lifetimes.cc
@@ -53,9 +53,9 @@ std::ostream &operator<<(std::ostream &stream, const ReferenceSetInfo &info)
   return stream;
 }
 
-static bool socket_may_have_reference(const bNodeTree &tree, const bNodeSocket &socket)
+static bool socket_may_have_reference(const bNodeSocket &socket)
 {
-  return tree.runtime->field_states[socket.index_in_tree()] == FieldSocketState::IsField;
+  return socket.may_be_field();
 }
 
 static bool or_into_each_other_masked(MutableBoundedBitSpan a,
@@ -132,7 +132,7 @@ static Array<const aal::RelationsInNode *> prepare_relations_by_node(const bNode
             if (prev_geometry_index == -1) {
               continue;
             }
-            if (socket_may_have_reference(tree, socket)) {
+            if (socket_may_have_reference(socket)) {
               relations.eval_relations.append({i, prev_geometry_index});
             }
           }
@@ -148,7 +148,7 @@ static Array<const aal::RelationsInNode *> prepare_relations_by_node(const bNode
             if (prev_geometry_index == -1) {
               continue;
             }
-            if (socket_may_have_reference(tree, socket)) {
+            if (socket_may_have_reference(socket)) {
               relations.available_relations.append({i, prev_geometry_index});
             }
           }
@@ -162,7 +162,7 @@ static Array<const aal::RelationsInNode *> prepare_relations_by_node(const bNode
         for (const bNodeSocket *socket : node->output_sockets()) {
           if (can_contain_referenced_data(eNodeSocketDatatype(socket->type))) {
             for (const bNodeSocket *other_output : node->output_sockets()) {
-              if (socket_may_have_reference(tree, *other_output)) {
+              if (socket_may_have_reference(*other_output)) {
                 relations.available_relations.append({other_output->index(), socket->index()});
               }
             }
@@ -171,7 +171,7 @@ static Array<const aal::RelationsInNode *> prepare_relations_by_node(const bNode
         for (const bNodeSocket *socket : node->input_sockets()) {
           if (can_contain_referenced_data(eNodeSocketDatatype(socket->type))) {
             for (const bNodeSocket *other_input : node->input_sockets()) {
-              if (socket_may_have_reference(tree, *other_input)) {
+              if (socket_may_have_reference(*other_input)) {
                 relations.eval_relations.append({other_input->index(), socket->index()});
               }
             }
@@ -191,7 +191,7 @@ static Array<const aal::RelationsInNode *> prepare_relations_by_node(const bNode
             if (can_contain_referenced_data(eNodeSocketDatatype(input_socket.type))) {
               relations.propagate_relations.append({input_index, output_index});
             }
-            else if (socket_may_have_reference(tree, input_socket)) {
+            else if (socket_may_have_reference(input_socket)) {
               relations.reference_relations.append({input_index, output_index});
             }
           }

source/blender/blenkernel/intern/node_tree_structure_type_inferencing.cc
@@ -15,6 +15,7 @@
 #include "DNA_node_types.h"
 
 #include "NOD_node_declaration.hh"
+#include "NOD_socket.hh"
 
 namespace blender::bke::node_structure_type_inferencing {
 
@@ -142,6 +143,10 @@ static void init_input_requirements(const bNodeTree &tree,
       const nodes::SocketDeclaration *declaration = socket->runtime->declaration;
       if (!declaration) {
         requirement = DataRequirement::None;
+        continue;
+      }
+      if (nodes::socket_type_always_single(eNodeSocketDatatype(socket->type))) {
+        requirement = DataRequirement::Single;
         continue;
       }
       switch (declaration->structure_type) {
@@ -799,6 +804,14 @@ static StructureTypeInferenceResult calc_structure_type_interface(const bNodeTre
       tree, node_interfaces, result.group_interface.inputs, result.socket_structure_types);
   store_group_output_structure_types(
       tree, node_interfaces, result.socket_structure_types, result.group_interface);
+
+  /* Ensure that the structure type is never invalid. */
+  for (const int i : tree.all_sockets().index_range()) {
+    const bNodeSocket &socket = *tree.all_sockets()[i];
+    if (nodes::socket_type_always_single(eNodeSocketDatatype(socket.type))) {
+      result.socket_structure_types[i] = StructureType::Single;
+    }
+  }
 
   return result;
 }

source/blender/editors/space_node/drawnode.cc
@@ -2191,10 +2191,7 @@ static bool node_link_is_field_link(const SpaceNode &snode, const bNodeLink &lin
   if (tree.type != NTREE_GEOMETRY) {
     return false;
   }
-  const Span<bke::FieldSocketState> field_states = tree.runtime->field_states;
-  if (link.fromsock &&
-      field_states[link.fromsock->index_in_tree()] == bke::FieldSocketState::IsField)
-  {
+  if (link.fromsock && link.fromsock->may_be_field()) {
     return true;
   }
   return false;

source/blender/makesdna/DNA_node_types.h
@@ -240,6 +240,11 @@ typedef struct bNodeSocket {
    * visibility is controlled by a menu should be hidden.
    */
   bool inferred_input_socket_visibility() const;
+  /**
+   * True when the value of this socket may be a field. This is inferred during structure type
+   * inferencing.
+   */
+  bool may_be_field() const;
 
   bool is_multi_input() const;
   bool is_input() const;

commit 0f65acef25d33d805c3ca755fae352c35cc0c2b7

Fix #143839: missing warning when connecting field to single value
The behavior of the field inferencing changed slightly. So instead of checking
if the link connects a socket that has a field to one that does not, check the
source is a field and the target does not support one. Note the difference
between "having a field" and "supporting a field".

This was probably broken in 7b7c63001858280ffec1a13c2751c22cbcf27686.

Pull Request: https://projects.blender.org/blender/blender/pulls/143975

source/blender/blenkernel/intern/node_tree_update.cc
@@ -1363,7 +1363,7 @@ class NodeTreeMainUpdater {
         continue;
       }
       if (ntree.type == NTREE_GEOMETRY) {
-        if (link->fromsock->may_be_field() && !link->tosock->may_be_field()) {
+        if (this->is_invalid_field_link(*link)) {
           link->flag &= ~NODE_LINK_VALID;
           ntree.runtime->link_errors.add(
               NodeLinkKey{*link}, NodeLinkError{TIP_("The node input does not support fields")});
@@ -1411,6 +1411,24 @@ class NodeTreeMainUpdater {
         }
       }
     }
+  }
+
+  bool is_invalid_field_link(const bNodeLink &link)
+  {
+    if (!link.fromsock->may_be_field()) {
+      return false;
+    }
+    const nodes::SocketDeclaration *to_socket_decl = link.tosock->runtime->declaration;
+    if (!to_socket_decl) {
+      return false;
+    }
+    if (ELEM(to_socket_decl->structure_type, StructureType::Dynamic, StructureType::Field)) {
+      return false;
+    }
+    if (link.tonode->is_group_output() || link.tonode->is_type("NodeClosureOutput")) {
+      return false;
+    }
+    return true;
   }
 
   bool check_if_output_changed(const bNodeTree &tree)