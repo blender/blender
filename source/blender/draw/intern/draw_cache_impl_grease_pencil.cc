2a8da4c1fa8cce282345331814ea4f80c0658a03
Hans Goudey <hans@blender.org>
2025-08-05 12:55:28 -0400

    Fix #143952: Broken Grease Pencil edit lines overlay in some cases
    Caused by 5f6e94ca58f74e279baa28af35949ad72fbdd360
    
    When there are more than 2^16 points, the GPU index buffer code tries
    to compress the indices to uint16 because we passed the incorrect max
    index. In general that optimization just isn't worth the complexity
    of precalculating the max index in this situation. There are other
    potential optimizations here that would be vastly more helpful.
    So just pass INT_MAX to disable the compression.

--- source/blender/draw/intern/draw_cache_impl_grease_pencil.cc
+++ source/blender/draw/intern/draw_cache_impl_grease_pencil.cc
@@ -1044,9 +1044,9 @@ static void grease_pencil_edit_batch_ensure(Object &object,
     }
   }
 
-  cache->edit_line_indices = GPU_indexbuf_build_ex(&lines_builder, 0, total_points_num, true);
-  cache->edit_points_indices = GPU_indexbuf_build_ex(&points_builder, 0, total_points_num, false);
-  cache->edit_handles_ibo = GPU_indexbuf_build_ex(&handles_builder, 0, total_points_num, false);
+  cache->edit_line_indices = GPU_indexbuf_build_ex(&lines_builder, 0, INT_MAX, true);
+  cache->edit_points_indices = GPU_indexbuf_build_ex(&points_builder, 0, INT_MAX, false);
+  cache->edit_handles_ibo = GPU_indexbuf_build_ex(&handles_builder, 0, INT_MAX, false);
 
   /* Create the batches */
   cache->edit_points = GPU_batch_create(
