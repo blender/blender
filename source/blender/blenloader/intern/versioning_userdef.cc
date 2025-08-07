02181347cea6c7a1afb0f247e95655c2ed5c2b8a
Hans Goudey <hans@blender.org>
2025-08-07 15:27:02 -0400

    Geometry Nodes: Change closure color, reset repeat zone color
    Follow up to f646948ace6232fc1b8e6e164697bf26109550a7
    The repeat zone color is changed back to the color it used in 4.5, to
    avoid the need for people to re-learn the color. The closure socket and
    zone share the same color, which is made more yellow-ish to distinguish
    it from the bundle socket color.

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -363,6 +363,11 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(tui.waxis);
   }
 
+  if (!USER_VERSION_ATLEAST(405, 55)) {
+    FROM_DEFAULT_V4_UCHAR(space_node.node_zone_closure);
+    FROM_DEFAULT_V4_UCHAR(space_node.node_zone_repeat);
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a USER_VERSION_ATLEAST check.
