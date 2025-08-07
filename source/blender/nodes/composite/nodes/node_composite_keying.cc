28e59d3e36dba317d386264e417e4361a282263a
Omar Emara <mail@OmarEmara.dev>
2025-08-07 11:56:10 +0300

    Fix #144070: Keying morphology can not do erosion
    The morphology operations in the Keying node in the compositor can no
    longer do erosion by using negative Feather and Dilate inputs. Clamping
    to positive values was accidentally done when porting options to inputs,
    so this patch fixes this by removing the clamping.

--- source/blender/nodes/composite/nodes/node_composite_keying.cc
+++ source/blender/nodes/composite/nodes/node_composite_keying.cc
@@ -687,7 +687,7 @@ class KeyingOperation : public NodeOperation {
 
   int get_postprocess_dilate_size()
   {
-    return math::max(0, this->get_input("Postprocess Dilate Size").get_single_value_default(0));
+    return this->get_input("Postprocess Dilate Size").get_single_value_default(0);
   }
 
   Result compute_feathered_matte(Result &input_matte)
@@ -711,7 +711,7 @@ class KeyingOperation : public NodeOperation {
 
   int get_postprocess_feather_size()
   {
-    return math::max(0, this->get_input("Postprocess Feather Size").get_single_value_default(0));
+    return this->get_input("Postprocess Feather Size").get_single_value_default(0);
   }
 
   void compute_image(Result &matte)
