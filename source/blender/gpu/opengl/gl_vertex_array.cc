ca57cf0750e0b73aeea41d83703dc3f02e017ab8
Clément Foucault <foucault.clem@gmail.com>
2025-08-11 14:22:01 +0200

    Fix #144174: DRW: Base instance workaround not working
    The part of the workaround that was inside the gpu module
    was not updated.
    
    Candidate for backporting to 4.5 LTS.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/144346

--- source/blender/gpu/opengl/gl_vertex_array.cc
+++ source/blender/gpu/opengl/gl_vertex_array.cc
@@ -107,11 +107,11 @@ void GLVertArray::update_bindings(const GLuint vao,
   }
 
   if (batch->resource_id_buf) {
-    const ShaderInput *input = interface->attr_get("drw_ResourceID");
+    const ShaderInput *input = interface->attr_get("in_resource_id");
     int component_len = 1;
     if (input == nullptr) {
       /* Uses Custom IDs */
-      input = interface->attr_get("vertex_in_drw_ResourceID");
+      input = interface->attr_get("in_resource_id_custom_id");
       component_len = 2;
     }
     if (input) {
@@ -119,7 +119,7 @@ void GLVertArray::update_bindings(const GLuint vao,
       glEnableVertexAttribArray(input->location);
       glVertexAttribDivisor(input->location, 1);
       glVertexAttribIPointer(
-          input->location, component_len, to_gl(GPU_COMP_I32), 0, (GLvoid *)nullptr);
+          input->location, component_len, to_gl(GPU_COMP_U32), 0, (GLvoid *)nullptr);
       attr_mask &= ~(1 << input->location);
     }
   }
