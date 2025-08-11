blender/blender

commit 76a861ab4156f9aaa3174c1c39c90e4947e22123

Geometry Nodes: move more menu inputs to sockets
This is a continuation of #140705. It changes more menu node options to be input
sockets. It also changes the order of the input sockets in a few cases to make
the node look a bit better.

Forward and backward compatibility is preserved.

Pull Request: https://projects.blender.org/blender/blender/pulls/142220

source/blender/nodes/function/nodes/node_fn_match_string.cc
@@ -7,9 +7,6 @@
 
 #include "BKE_node_runtime.hh"
 
-#include "UI_interface_layout.hh"
-#include "UI_resources.hh"
-
 #include "node_function_util.hh"
 
 #include "NOD_socket_search_link.hh"
@@ -39,8 +36,8 @@ const EnumPropertyItem rna_enum_node_match_string_items[] = {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_input<decl::Menu>("Operation").static_items(rna_enum_node_match_string_items);
   b.add_input<decl::String>("String").hide_label().is_default_link_socket();
+  b.add_input<decl::Menu>("Operation").static_items(rna_enum_node_match_string_items);
   b.add_input<decl::String>("Key").hide_label().description(
       "The string to find in the input string");
   b.add_output<decl::Bool>("Result");
@@ -48,8 +45,8 @@ static void node_declare(NodeDeclarationBuilder &b)
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
 {
-  static auto fn = mf::build::SI3_SO<int, std::string, std::string, bool>(
-      "Starts With", [](const int mode, const std::string &a, const std::string &b) {
+  static auto fn = mf::build::SI3_SO<std::string, std::string, int, bool>(
+      "Starts With", [](const std::string &a, const std::string &b, const int mode) {
         const StringRef strref_a(a);
         const StringRef strref_b(b);
         switch (MatchStringOperation(mode)) {

source/blender/blenkernel/BKE_blender_version.h
@@ -27,7 +27,7 @@
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 44
+#define BLENDER_FILE_SUBVERSION 45
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/intern/node.cc
@@ -700,7 +700,76 @@ static void write_legacy_properties(bNodeTree &ntree)
           const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Resolution Mode");
           storage.resolution_mode = socket->default_value_typed<bNodeSocketValueMenu>()->value;
         }
-        else if (STREQ(node->idname, "FunctionNodeMatchString")) {
+        else if (node->type_legacy == GEO_NODE_FILL_CURVE) {
+          auto &storage = *static_cast<NodeGeometryCurveFill *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Mode");
+          storage.mode = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_FILLET_CURVE) {
+          auto &storage = *static_cast<NodeGeometryCurveFillet *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Mode");
+          storage.mode = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_RESAMPLE_CURVE) {
+          auto &storage = *static_cast<NodeGeometryCurveResample *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Mode");
+          storage.mode = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_DISTRIBUTE_POINTS_IN_VOLUME) {
+          auto &storage = *static_cast<NodeGeometryDistributePointsInVolume *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Mode");
+          storage.mode = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_MERGE_BY_DISTANCE) {
+          auto &storage = *static_cast<NodeGeometryMergeByDistance *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Mode");
+          storage.mode = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_MESH_TO_VOLUME) {
+          auto &storage = *static_cast<NodeGeometryMeshToVolume *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Resolution Mode");
+          storage.resolution_mode = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_RAYCAST) {
+          auto &storage = *static_cast<NodeGeometryRaycast *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Interpolation");
+          storage.mapping = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_REMOVE_ATTRIBUTE) {
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Pattern Mode");
+          node->custom1 = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_SAMPLE_GRID) {
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Interpolation");
+          node->custom2 = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_SCALE_ELEMENTS) {
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Scale Mode");
+          node->custom2 = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_SET_CURVE_NORMAL) {
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Mode");
+          node->custom1 = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_SUBDIVISION_SURFACE) {
+          auto &storage = *static_cast<NodeGeometrySubdivisionSurface *>(node->storage);
+          const bNodeSocket *uv_smooth_socket = node_find_socket(*node, SOCK_IN, "UV Smooth");
+          const bNodeSocket *boundary_smooth_socket = node_find_socket(
+              *node, SOCK_IN, "Boundary Smooth");
+          storage.uv_smooth = uv_smooth_socket->default_value_typed<bNodeSocketValueMenu>()->value;
+          storage.boundary_smooth =
+              boundary_smooth_socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_UV_PACK_ISLANDS) {
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Method");
+          node->custom1 = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_UV_UNWRAP) {
+          auto &storage = *static_cast<NodeGeometryUVUnwrap *>(node->storage);
+          const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Method");
+          storage.method = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->is_type("FunctionNodeMatchString")) {
           const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Operation");
           node->custom1 = socket->default_value_typed<bNodeSocketValueMenu>()->value;
         }

source/blender/blenloader/intern/versioning_500.cc
@@ -569,6 +569,151 @@ static void do_version_match_string_options_to_inputs(bNodeTree &ntree, bNode &n
   socket.default_value_typed<bNodeSocketValueMenu>()->value = node.custom1;
 }
 
+static void do_version_fill_curve_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Mode")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryCurveFill *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.mode;
+}
+
+static void do_version_fillet_curve_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Mode")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryCurveFillet *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.mode;
+}
+
+static void do_version_resample_curve_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Mode")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryCurveResample *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.mode;
+}
+
+static void do_version_distribute_points_in_volume_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Mode")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryDistributePointsInVolume *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.mode;
+}
+
+static void do_version_merge_by_distance_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Mode")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryMergeByDistance *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.mode;
+}
+
+static void do_version_mesh_to_volume_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Resolution Mode")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryMeshToVolume *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(
+      ntree, node, SOCK_IN, "NodeSocketMenu", "Resolution Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.resolution_mode;
+}
+
+static void do_version_raycast_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Interpolation")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryRaycast *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(
+      ntree, node, SOCK_IN, "NodeSocketMenu", "Interpolation");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.mapping;
+}
+
+static void do_version_remove_attribute_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Pattern Mode")) {
+    return;
+  }
+  bNodeSocket &socket = version_node_add_socket(
+      ntree, node, SOCK_IN, "NodeSocketMenu", "Pattern Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = node.custom1;
+}
+
+static void do_version_sample_grid_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Interpolation")) {
+    return;
+  }
+  bNodeSocket &socket = version_node_add_socket(
+      ntree, node, SOCK_IN, "NodeSocketMenu", "Interpolation");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = node.custom2;
+}
+
+static void do_version_scale_elements_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Scale Mode")) {
+    return;
+  }
+  bNodeSocket &socket = version_node_add_socket(
+      ntree, node, SOCK_IN, "NodeSocketMenu", "Scale Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = node.custom2;
+}
+
+static void do_version_set_curve_normal_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Mode")) {
+    return;
+  }
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Mode");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = node.custom1;
+}
+
+static void do_version_subdivision_surface_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  auto &storage = *static_cast<NodeGeometrySubdivisionSurface *>(node.storage);
+  if (!blender::bke::node_find_socket(node, SOCK_IN, "UV Smooth")) {
+    bNodeSocket &socket = version_node_add_socket(
+        ntree, node, SOCK_IN, "NodeSocketMenu", "UV Smooth");
+    socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.uv_smooth;
+  }
+  if (!blender::bke::node_find_socket(node, SOCK_IN, "Boundary Smooth")) {
+    bNodeSocket &socket = version_node_add_socket(
+        ntree, node, SOCK_IN, "NodeSocketMenu", "Boundary Smooth");
+    socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.boundary_smooth;
+  }
+}
+
+static void do_version_uv_pack_islands_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Method")) {
+    return;
+  }
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Method");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = node.custom1;
+}
+
+static void do_version_uv_unwrap_options_to_inputs(bNodeTree &ntree, bNode &node)
+{
+  if (blender::bke::node_find_socket(node, SOCK_IN, "Method")) {
+    return;
+  }
+  const auto &storage = *static_cast<NodeGeometryUVUnwrap *>(node.storage);
+  bNodeSocket &socket = version_node_add_socket(ntree, node, SOCK_IN, "NodeSocketMenu", "Method");
+  socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.method;
+}
+
 static void version_seq_text_from_legacy(Main *bmain)
 {
   LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
@@ -1762,6 +1907,58 @@ void blo_do_versions_500(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     LISTBASE_FOREACH (World *, world, &bmain->worlds) {
       do_version_world_remove_use_nodes(bmain, world);
     }
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 45)) {
+    FOREACH_NODETREE_BEGIN (bmain, node_tree, id) {
+      if (node_tree->type == NTREE_GEOMETRY) {
+        LISTBASE_FOREACH (bNode *, node, &node_tree->nodes) {
+          if (node->type_legacy == GEO_NODE_FILL_CURVE) {
+            do_version_fill_curve_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_FILLET_CURVE) {
+            do_version_fillet_curve_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_RESAMPLE_CURVE) {
+            do_version_resample_curve_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_DISTRIBUTE_POINTS_IN_VOLUME) {
+            do_version_distribute_points_in_volume_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_MERGE_BY_DISTANCE) {
+            do_version_merge_by_distance_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_MESH_TO_VOLUME) {
+            do_version_mesh_to_volume_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_RAYCAST) {
+            do_version_raycast_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_REMOVE_ATTRIBUTE) {
+            do_version_remove_attribute_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_SAMPLE_GRID) {
+            do_version_sample_grid_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_SCALE_ELEMENTS) {
+            do_version_scale_elements_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_SET_CURVE_NORMAL) {
+            do_version_set_curve_normal_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_SUBDIVISION_SURFACE) {
+            do_version_subdivision_surface_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_UV_PACK_ISLANDS) {
+            do_version_uv_pack_islands_options_to_inputs(*node_tree, *node);
+          }
+          else if (node->type_legacy == GEO_NODE_UV_UNWRAP) {
+            do_version_uv_unwrap_options_to_inputs(*node_tree, *node);
+          }
+        }
+      }
+    }
+    FOREACH_NODETREE_END;
   }
 
   /**

source/blender/nodes/geometry/nodes/node_geo_curve_fill.cc
@@ -14,16 +14,17 @@
 
 #include "BLI_task.hh"
 
-#include "NOD_rna_define.hh"
-
-#include "UI_interface_layout.hh"
-#include "UI_resources.hh"
-
 #include "node_geometry_util.hh"
 
 namespace blender::nodes::node_geo_curve_fill_cc {
 
 NODE_STORAGE_FUNCS(NodeGeometryCurveFill)
+
+static const EnumPropertyItem mode_items[] = {
+    {GEO_NODE_CURVE_FILL_MODE_TRIANGULATED, "TRIANGLES", 0, "Triangles", ""},
+    {GEO_NODE_CURVE_FILL_MODE_NGONS, "NGONS", 0, "N-gons", ""},
+    {0, nullptr, 0, nullptr, nullptr},
+};
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
@@ -36,20 +37,16 @@ static void node_declare(NodeDeclarationBuilder &b)
       .hide_value()
       .description(
           "An index used to group curves together. Filling is done separately for each group");
+  b.add_input<decl::Menu>("Mode")
+      .static_items(mode_items)
+      .default_value(GEO_NODE_CURVE_FILL_MODE_TRIANGULATED);
   b.add_output<decl::Geometry>("Mesh").propagate_all_instance_attributes();
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  layout->prop(ptr, "mode", UI_ITEM_R_EXPAND, std::nullopt, ICON_NONE);
 }
 
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
-  NodeGeometryCurveFill *data = MEM_callocN<NodeGeometryCurveFill>(__func__);
-
-  data->mode = GEO_NODE_CURVE_FILL_MODE_TRIANGULATED;
-  node->storage = data;
+  /* Still used for forward compatibility. */
+  node->storage = MEM_callocN<NodeGeometryCurveFill>(__func__);
 }
 
 static void fill_curve_vert_indices(const OffsetIndices<int> offsets,
@@ -311,31 +308,12 @@ static void node_geo_exec(GeoNodeExecParams params)
 {
   GeometrySet geometry_set = params.extract_input<GeometrySet>("Curve");
   Field<int> group_index = params.extract_input<Field<int>>("Group ID");
-
-  const NodeGeometryCurveFill &storage = node_storage(params.node());
-  const GeometryNodeCurveFillMode mode = (GeometryNodeCurveFillMode)storage.mode;
+  const GeometryNodeCurveFillMode mode = params.extract_input<GeometryNodeCurveFillMode>("Mode");
 
   geometry_set.modify_geometry_sets(
       [&](GeometrySet &geometry_set) { curve_fill_calculate(geometry_set, mode, group_index); });
 
   params.set_output("Mesh", std::move(geometry_set));
-}
-
-static void node_rna(StructRNA *srna)
-{
-  static const EnumPropertyItem mode_items[] = {
-      {GEO_NODE_CURVE_FILL_MODE_TRIANGULATED, "TRIANGLES", 0, "Triangles", ""},
-      {GEO_NODE_CURVE_FILL_MODE_NGONS, "NGONS", 0, "N-gons", ""},
-      {0, nullptr, 0, nullptr, nullptr},
-  };
-
-  RNA_def_node_enum(srna,
-                    "mode",
-                    "Mode",
-                    "",
-                    mode_items,
-                    NOD_storage_enum_accessors(mode),
-                    GEO_NODE_CURVE_FILL_MODE_TRIANGULATED);
 }
 
 static void node_register()
@@ -352,10 +330,7 @@ static void node_register()
       ntype, "NodeGeometryCurveFill", node_free_standard_storage, node_copy_standard_storage);
   ntype.declare = node_declare;
   ntype.geometry_node_execute = node_geo_exec;
-  ntype.draw_buttons = node_layout;
   blender::bke::node_register_type(ntype);
-
-  node_rna(ntype.rna_ext.srna);
 }
 NOD_REGISTER_NODE(node_register)
 

source/blender/nodes/geometry/nodes/node_geo_curve_fillet.cc
@@ -2,14 +2,9 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "NOD_rna_define.hh"
-
 #include "DNA_grease_pencil_types.h"
 
 #include "BKE_grease_pencil.hh"
-
-#include "UI_interface_layout.hh"
-#include "UI_resources.hh"
 
 #include "GEO_fillet_curves.hh"
 
@@ -19,23 +14,28 @@ namespace blender::nodes::node_geo_curve_fillet_cc {
 
 NODE_STORAGE_FUNCS(NodeGeometryCurveFillet)
 
+static EnumPropertyItem mode_items[] = {
+    {GEO_NODE_CURVE_FILLET_BEZIER,
+     "BEZIER",
+     0,
+     "Bézier",
+     "Align Bézier handles to create circular arcs at each control point"},
+    {GEO_NODE_CURVE_FILLET_POLY,
+     "POLY",
+     0,
+     "Poly",
+     "Add control points along a circular arc (handle type is vector if Bézier Spline)"},
+    {0, nullptr, 0, nullptr, nullptr},
+};
+
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.use_custom_socket_order();
   b.allow_any_socket_order();
-  b.add_default_layout();
   b.add_input<decl::Geometry>("Curve")
       .supported_type({GeometryComponent::Type::Curve, GeometryComponent::Type::GreasePencil})
       .description("Curves to generated rounded corners on");
   b.add_output<decl::Geometry>("Curve").propagate_all().align_with_previous();
-  auto &count_input = b.add_input<decl::Int>("Count")
-                          .default_value(1)
-                          .min(1)
-                          .max(1000)
-                          .field_on_all()
-                          .make_available([](bNode &node) {
-                            node_storage(node).mode = GEO_NODE_CURVE_FILLET_POLY;
-                          });
   b.add_input<decl::Float>("Radius")
       .min(0.0f)
       .max(FLT_MAX)
@@ -44,24 +44,21 @@ static void node_declare(NodeDeclarationBuilder &b)
       .field_on_all();
   b.add_input<decl::Bool>("Limit Radius")
       .description("Limit the maximum value of the radius in order to avoid overlapping fillets");
-
-  const bNode *node = b.node_or_null();
-  if (node != nullptr) {
-    const NodeGeometryCurveFillet &storage = node_storage(*node);
-    count_input.available(GeometryNodeCurveFilletMode(storage.mode) == GEO_NODE_CURVE_FILLET_POLY);
-  }
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  layout->prop(ptr, "mode", UI_ITEM_R_EXPAND, std::nullopt, ICON_NONE);
+  b.add_input<decl::Menu>("Mode")
+      .static_items(mode_items)
+      .description("How to choose number of vertices on fillet");
+  b.add_input<decl::Int>("Count")
+      .default_value(1)
+      .min(1)
+      .max(1000)
+      .field_on_all()
+      .usage_by_single_menu(GEO_NODE_CURVE_FILLET_POLY);
 }
 
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
-  NodeGeometryCurveFillet *data = MEM_callocN<NodeGeometryCurveFillet>(__func__);
-  data->mode = GEO_NODE_CURVE_FILLET_BEZIER;
-  node->storage = data;
+  /* Still used for forward compatibility. */
+  node->storage = MEM_callocN<NodeGeometryCurveFillet>(__func__);
 }
 
 static bke::CurvesGeometry fillet_curve(const bke::CurvesGeometry &src_curves,
@@ -132,9 +129,8 @@ static void fillet_grease_pencil(GreasePencil &grease_pencil,
 static void node_geo_exec(GeoNodeExecParams params)
 {
   GeometrySet geometry_set = params.extract_input<GeometrySet>("Curve");
-
-  const NodeGeometryCurveFillet &storage = node_storage(params.node());
-  const GeometryNodeCurveFilletMode mode = (GeometryNodeCurveFilletMode)storage.mode;
+  const GeometryNodeCurveFilletMode mode = params.extract_input<GeometryNodeCurveFilletMode>(
+      "Mode");
 
   Field<float> radius_field = params.extract_input<Field<float>>("Radius");
   const bool limit_radius = params.extract_input<bool>("Limit Radius");
@@ -172,31 +168,6 @@ static void node_geo_exec(GeoNodeExecParams params)
   params.set_output("Curve", std::move(geometry_set));
 }
 
-static void node_rna(StructRNA *srna)
-{
-  static EnumPropertyItem mode_items[] = {
-      {GEO_NODE_CURVE_FILLET_BEZIER,
-       "BEZIER",
-       0,
-       "Bézier",
-       "Align Bézier handles to create circular arcs at each control point"},
-      {GEO_NODE_CURVE_FILLET_POLY,
-       "POLY",
-       0,
-       "Poly",
-       "Add control points along a circular arc (handle type is vector if Bézier Spline)"},
-      {0, nullptr, 0, nullptr, nullptr},
-  };
-
-  RNA_def_node_enum(srna,
-                    "mode",
-                    "Mode",
-                    "How to choose number of vertices on fillet",
-                    mode_items,
-                    NOD_storage_enum_accessors(mode),
-                    GEO_NODE_CURVE_FILLET_BEZIER);
-}
-
 static void node_register()
 {
   static blender::bke::bNodeType ntype;
@@ -206,15 +177,12 @@ static void node_register()
   ntype.ui_description = "Round corners by generating circular arcs on each control point";
   ntype.enum_name_legacy = "FILLET_CURVE";
   ntype.nclass = NODE_CLASS_GEOMETRY;
-  ntype.draw_buttons = node_layout;
   blender::bke::node_type_storage(
       ntype, "NodeGeometryCurveFillet", node_free_standard_storage, node_copy_standard_storage);
   ntype.declare = node_declare;
   ntype.initfunc = node_init;
   ntype.geometry_node_execute = node_geo_exec;
   blender::bke::node_register_type(ntype);
-
-  node_rna(ntype.rna_ext.srna);
 }
 NOD_REGISTER_NODE(node_register)
 

commit 429dc0920cb000a88d3aa22cd0ae855ac1b9aad4

Fix #144362: Match string node crash after recent change
Caused by 76a861ab4156f9aaa3174c1c39c90e4947e22123
That commit used the wrong order internally for the "Operation" input.

source/blender/nodes/function/nodes/node_fn_match_string.cc
@@ -45,8 +45,8 @@ static void node_declare(NodeDeclarationBuilder &b)
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
 {
-  static auto fn = mf::build::SI3_SO<std::string, std::string, int, bool>(
-      "Starts With", [](const std::string &a, const std::string &b, const int mode) {
+  static auto fn = mf::build::SI3_SO<std::string, int, std::string, bool>(
+      "Starts With", [](const std::string &a, const int mode, const std::string &b) {
         const StringRef strref_a(a);
         const StringRef strref_b(b);
         switch (MatchStringOperation(mode)) {