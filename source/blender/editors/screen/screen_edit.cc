2753e7a5290ce88ae881126e657686a281ae90c8
Campbell Barton <campbell@blender.org>
2025-08-22 10:45:00 +0000

    Fix #144958: Crash changing screens with a menu open
    Deleting or changing the screen with a popup (error report in this case)
    wasn't removing the popup, it would attempt to display using it's
    region which had been freed when changing the screen.

--- source/blender/editors/screen/screen_edit.cc
+++ source/blender/editors/screen/screen_edit.cc
@@ -1364,6 +1364,11 @@ void screen_change_prepare(
     LISTBASE_FOREACH (ScrArea *, area, &screen_old->areabase) {
       WM_event_remove_area_handler(&win->modalhandlers, area);
     }
+    /* Remove popup handlers (menus), while unlikely, it's possible an "error"
+     * popup is displayed when switching screens, see: #144958. */
+    LISTBASE_FOREACH (ARegion *, region, &screen_old->regionbase) {
+      UI_popup_handlers_remove_by_region(&win->modalhandlers, region);
+    }
 
     /* we put timer to sleep, so screen_exit has to think there's no timer */
     screen_old->animtimer = nullptr;
