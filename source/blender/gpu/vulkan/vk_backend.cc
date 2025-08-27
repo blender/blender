af656e39951e0d4293b780f3bbde3a76a4d749d1
Jeroen Bakker <jeroen@blender.org>
2025-08-27 14:45:10 +0200

    Fix #144625: Vulkan: Blacklist NVIDIA 580.76.05 on Linux
    This PR disables support for NVIDIA 580.76.05 drivers as it can hang
    on startup with Wayland. There are reports from other applications
    that this driver is faulty and NVIDIA confirmed that an updated driver
    is in the making.
    
    For now we will blacklist this specific driver on any Linux system.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/145094
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/145107

--- source/blender/gpu/vulkan/vk_backend.cc
+++ source/blender/gpu/vulkan/vk_backend.cc
@@ -89,6 +89,19 @@ bool GPU_vulkan_is_supported_driver(VkPhysicalDevice vk_physical_device)
   {
     return false;
   }
+
+  /* NVIDIA driver 580.76.05 doesn't start using specific Wayland configurations #144625. There are
+   * multiple reports also not Blender related and NVIDIA mentions that a new driver will be
+   * released. It is unclear if that driver will fix our issue. For now disabling this driver on
+   * Linux. This also disables it for configurations that are working as well (including X11). */
+  if (vk_physical_device_driver_properties.driverID == VK_DRIVER_ID_NVIDIA_PROPRIETARY &&
+      ((StringRefNull(vk_physical_device_driver_properties.driverInfo).find("580.76.5", 0) !=
+        StringRef::not_found) ||
+       (StringRefNull(vk_physical_device_driver_properties.driverInfo).find("580.76.05", 0) !=
+        StringRef::not_found)))
+  {
+    return false;
+  }
 #endif
 
 #ifdef _WIN32
