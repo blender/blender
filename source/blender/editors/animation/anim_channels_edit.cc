0aa3a0dc0a623a91260eaf9ed024b2c4ee836e7c
Sybren A. Stüvel <sybren@blender.org>
2025-09-09 17:57:27 +0200

    Cleanup: fix unused parameter in release builds
    Add `UNUSED_VARS_NDEBUG(ac);` to silence the 'unused parameter' warning
    in non-debug builds.
    
    No functional changes.

--- source/blender/editors/animation/anim_channels_edit.cc
+++ source/blender/editors/animation/anim_channels_edit.cc
@@ -2907,6 +2907,7 @@ static bool animchannels_delete_containers(const bContext *C, bAnimContext *ac)
 
 void ED_anim_ale_fcurve_delete(bAnimContext &ac, bAnimListElem &ale)
 {
+  UNUSED_VARS_NDEBUG(ac);
   BLI_assert(ELEM(ale.type, ANIMTYPE_FCURVE, ANIMTYPE_NLACURVE));
 
   switch (ale.type) {
