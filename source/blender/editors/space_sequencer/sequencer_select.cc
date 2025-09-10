c241ea108311191a7be8befa57a94a5f0c5c8adc
Campbell Barton <campbell@blender.org>
2025-09-10 14:27:56 +1000

    Cleanup: remove unused variable

--- source/blender/editors/space_sequencer/sequencer_select.cc
+++ source/blender/editors/space_sequencer/sequencer_select.cc
@@ -1193,7 +1193,6 @@ wmOperatorStatus sequencer_select_exec(bContext *C, wmOperator *op)
   Scene *scene = CTX_data_sequencer_scene(C);
   Editing *ed = seq::editing_get(scene);
   ARegion *region = CTX_wm_region(C);
-  ScrArea *area = CTX_wm_area(C);
 
   if (ed == nullptr) {
     return OPERATOR_CANCELLED;
