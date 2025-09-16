b474325aa19bbc27e3297b9604b485f3f06ca569
Philipp Oeser <philipp@blender.org>
2025-09-16 10:56:59 +0200

    Fix #146288: Crash displaying frames without a scene
    Caused by ab43c57a2a52
    
    The VSE allows for "no scene", prior to above commit an "arbitrary"
    framerate of 25 was hardcoded to display "something" in that case.
    
    To resolve, pick what we defined as default for framerate (getting rid
    of the "arbitrary") in case we have no (VSE) scene.
    
    NOTE: that is 24 atm (instead of the hardcoded 25 we had before)
    Pull Request: https://projects.blender.org/blender/blender/pulls/146300

--- source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
+++ source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
@@ -20,6 +20,7 @@
 #include "BLI_threads.h"
 #include "BLI_utildefines.h"
 
+#include "DNA_defaults.h"
 #include "DNA_scene_types.h"
 #include "DNA_screen_types.h"
 #include "DNA_sequence_types.h"
@@ -1828,8 +1829,15 @@ static void draw_timeline_grid(TimelineDrawContext *ctx)
   }
 
   U.v2d_min_gridsize *= 3;
+
+  const Scene *scene = ctx->scene;
+  if (scene == nullptr) {
+    /* If we don't have a scene available, pick what we defined as default for framerate to show
+     * *something*. */
+    scene = DNA_struct_default_get(Scene);
+  }
   UI_view2d_draw_lines_x__discrete_frames_or_seconds(
-      ctx->v2d, ctx->scene, (ctx->sseq->flag & SEQ_DRAWFRAMES) == 0, false);
+      ctx->v2d, scene, (ctx->sseq->flag & SEQ_DRAWFRAMES) == 0, false);
   U.v2d_min_gridsize /= 3;
 }
 
