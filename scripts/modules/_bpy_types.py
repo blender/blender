8111fe236987f1fea15138a75177fb4684b5477f
Jeroen Bakker <jeroen@blender.org>
2025-09-25 17:22:35 +0200

    Fix #146655: GPU: Call of deprecated function
    There are deprecation usages in the gpu presets and custom
    shapes python module. This PR uses the correct calls to remove the
    deprecation warnings.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/146746

--- scripts/modules/_bpy_types.py
+++ scripts/modules/_bpy_types.py
@@ -970,7 +970,7 @@ class Gizmo(_StructRNA):
 
         with gpu.matrix.push_pop():
             gpu.matrix.multiply_matrix(matrix)
-            batch.draw()
+            batch.draw(shader)
 
         if use_blend:
             gpu.state.blend_set('NONE')
@@ -1002,7 +1002,6 @@ class Gizmo(_StructRNA):
         vbo.attr_fill(id=pos_id, data=verts)
         batch = GPUBatch(type=type, buf=vbo)
         shader = gpu.shader.from_builtin('UNIFORM_COLOR')
-        batch.program_set(shader)
         return (batch, shader)
 
 
