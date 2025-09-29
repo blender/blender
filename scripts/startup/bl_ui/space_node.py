99b47b72aa3f637f97458f08abcb007c55be9f7c
quackarooni <alfonsomartzii@gmail.com>
2025-09-29 17:22:15 +0200

    Nodes: Remove "Swap" menu from editor header
    Relocated the "Swap" menu from the header to inside of "Node" menu.
    As it wasn't deemed ideal to put such a menu at the root level of the header.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/146964

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -279,7 +279,6 @@ class NODE_MT_editor_menus(Menu):
         layout.menu("NODE_MT_view")
         layout.menu("NODE_MT_select")
         layout.menu("NODE_MT_add")
-        layout.menu("NODE_MT_swap")
         layout.menu("NODE_MT_node")
 
 
@@ -462,6 +461,7 @@ class NODE_MT_node(Menu):
             layout.operator("node.group_ungroup")
 
         layout.separator()
+        layout.menu("NODE_MT_swap")
         layout.menu("NODE_MT_context_menu_show_hide_menu")
 
         if is_compositor:
