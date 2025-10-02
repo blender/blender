7f36eebf5c004f9081074dd91aff36d019400273
Campbell Barton <campbell@blender.org>
2025-10-02 11:51:03 +0000

    Wayland: lower the requirement for the xdg_output_manager for Rocky8
    GHOST/Wayland failed to initialize on older versions of GNOME.
    Lower the requirement so Blender can start on the build-bot.
    
    Ref !147165

--- intern/ghost/intern/GHOST_SystemWayland.cc
+++ intern/ghost/intern/GHOST_SystemWayland.cc
@@ -6760,7 +6760,10 @@ static void gwl_registry_xdg_decoration_manager_remove(GWL_Display *display,
 static void gwl_registry_xdg_output_manager_add(GWL_Display *display,
                                                 const GWL_RegisteryAdd_Params &params)
 {
-  const uint version = GWL_IFACE_VERSION_CLAMP(params.version, 2u, 3u);
+  /* NOTE(@ideasman42): only version 3 and over is well supported.
+   * Support version 1 so GNOME-32 (on the build-bots Rocky8) can be used.
+   * Use `zxdg_output_v1_get_version` to ensure feature support at runtime. */
+  const uint version = GWL_IFACE_VERSION_CLAMP(params.version, 1u, 3u);
 
   display->xdg.output_manager = static_cast<zxdg_output_manager_v1 *>(wl_registry_bind(
       display->wl.registry, params.name, &zxdg_output_manager_v1_interface, version));
