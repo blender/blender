e810d7459275233244fec8f60a77310d8906c503
Jacques Lucke <jacques@blender.org>
2025-10-03 10:30:38 +0200

    Fix: Geometry Nodes: crash when not using all outputs of Field to Grid node

--- source/blender/nodes/geometry/nodes/node_geo_field_to_grid.cc
+++ source/blender/nodes/geometry/nodes/node_geo_field_to_grid.cc
@@ -302,8 +302,8 @@ static void node_geo_exec(GeoNodeExecParams params)
 
   Vector<fn::GField> fields(required_items.size());
   for (const int i : required_items.index_range()) {
-    const int input_i = required_items[i];
-    const std::string identifier = ItemsAccessor::input_socket_identifier_for_item(items[input_i]);
+    const int item_i = required_items[i];
+    const std::string identifier = ItemsAccessor::input_socket_identifier_for_item(items[item_i]);
     fields[i] = params.extract_input<fn::GField>(identifier);
   }
 
@@ -313,7 +313,8 @@ static void node_geo_exec(GeoNodeExecParams params)
 
   Vector<openvdb::GridBase::Ptr> output_grids(required_items.size());
   for (const int i : required_items.index_range()) {
-    const eNodeSocketDatatype socket_type = eNodeSocketDatatype(items[i].data_type);
+    const int item_i = required_items[i];
+    const eNodeSocketDatatype socket_type = eNodeSocketDatatype(items[item_i].data_type);
     const VolumeGridType grid_type = *bke::socket_type_to_grid_type(socket_type);
     output_grids[i] = grid::create_grid_with_topology(mask_tree, transform, grid_type);
   }
@@ -336,9 +337,8 @@ static void node_geo_exec(GeoNodeExecParams params)
   process_background(fields, transform, output_grids);
 
   for (const int i : required_items.index_range()) {
-    const int output_i = required_items[i];
-    const std::string identifier = ItemsAccessor::output_socket_identifier_for_item(
-        items[output_i]);
+    const int item_i = required_items[i];
+    const std::string identifier = ItemsAccessor::output_socket_identifier_for_item(items[item_i]);
     params.set_output(identifier, bke::GVolumeGrid(std::move(output_grids[i])));
   }
 
