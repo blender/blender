blender/blender

commit 1067112c11176807609eabeea71c64fbd2a93e27

UI: Theme: Remove "Active Spline" theme property
In 3D Viewport theme settings there is "Active Spline" property, which behaves weirdly.
Color of that property is multiplied to color of curve handles. Even though it says active,
it's multiplied in selected and unselected states, for all curves all the time.

That doesn't make much sense, has no real value, and ends up only causing confusion.
Having this property on anything but pure black means that whatever colors you choose
for curve handles in Preferences isn't actually what you're getting them. If color is set to
high-saturation color it completely washes away all colors and makes it difficult to differentiate
between handle types.

I think there is no reason for this property to exist, so this PR just removes that property.

Pull Request: https://projects.blender.org/blender/blender/pulls/145360

release/datafiles/userdef/userdef_default_theme.c
@@ -413,7 +413,6 @@ const bTheme U_theme_default = {
     .time_gp_keyframe = RGBA(0x7a9b14ff),
     .nurb_uline = RGBA(0x909000ff),
     .nurb_vline = RGBA(0x803060ff),
-    .act_spline = RGBA(0xdb2512ff),
     .nurb_sel_uline = RGBA(0xf0ff40ff),
     .nurb_sel_vline = RGBA(0xf090a0ff),
     .vertex_size = 3,

scripts/presets/interface_theme/Blender_Light.xml
@@ -506,7 +506,6 @@
         nurb_vline="#803060"
         nurb_sel_uline="#f0ff40"
         nurb_sel_vline="#f090a0"
-        act_spline="#db2512"
         extra_edge_len="#ff6149"
         extra_edge_angle="#cccc00"
         extra_face_angle="#b3b3ff"

source/blender/draw/engines/overlay/overlay_instance.cc
@@ -342,7 +342,6 @@ void Resources::update_theme_settings(const DRWContext *ctx, const State &state)
   UI_GetThemeColor4fv(TH_NURB_VLINE, gb.colors.nurb_vline);
   UI_GetThemeColor4fv(TH_NURB_SEL_ULINE, gb.colors.nurb_sel_uline);
   UI_GetThemeColor4fv(TH_NURB_SEL_VLINE, gb.colors.nurb_sel_vline);
-  UI_GetThemeColor4fv(TH_ACTIVE_SPLINE, gb.colors.active_spline);
 
   UI_GetThemeColor4fv(TH_CFRAME, gb.colors.current_frame);
   UI_GetThemeColor4fv(TH_FRAME_BEFORE, gb.colors.before_frame);

source/blender/draw/engines/overlay/overlay_shader_shared.hh
@@ -234,7 +234,6 @@ struct ThemeColors {
   float4 nurb_vline;
   float4 nurb_sel_uline;
   float4 nurb_sel_vline;
-  float4 active_spline;
 
   float4 bone_pose;
   float4 bone_pose_active;

source/blender/draw/engines/overlay/shaders/overlay_edit_curve_handle_vert.glsl
@@ -159,7 +159,7 @@ void geometry_main(VertOut geom_in[2],
   }
 
   /* Minimize active color bleeding on inner_color. */
-  float4 active_color = mix(theme.colors.active_spline, inner_color, 0.25f);
+  float4 active_color = mix(float4(0.0, 0.0, 0.0, 1.0), inner_color, 0.25f);
   float4 outer_color = (is_active_nurb != 0u) ? active_color : float4(inner_color.rgb, 0.0f);
 
   float2 v1_2 = (v2.xy / v2.w - v1.xy / v1.w);
@@ -172,7 +172,7 @@ void geometry_main(VertOut geom_in[2],
     offset.x = 0.0f;
   }
 
-  float4 border_color = float4(theme.colors.active_spline.rgb, 0.0f);
+  float4 border_color = float4(0.0, 0.0, 0.0, 0.0);
   /* Draw the transparent border (AA). */
   if (is_active_nurb != 0u) {
     offset *= 0.75f; /* Don't make the active "halo" appear very thick. */

source/blender/draw/engines/overlay/shaders/overlay_edit_curves_handle_vert.glsl
@@ -162,7 +162,7 @@ void geometry_main(VertOut geom_in[2],
   }
 
   /* Minimize active color bleeding on inner_color. */
-  float4 active_color = mix(theme.colors.active_spline, inner_color, 0.25f);
+  float4 active_color = mix(float4(0.0, 0.0, 0.0, 1.0), inner_color, 0.25f);
   float4 outer_color = is_active ? active_color : float4(inner_color.rgb, 0.0f);
 
   float2 v1_2 = (v2.xy / v2.w - v1.xy / v1.w);
@@ -178,7 +178,7 @@ void geometry_main(VertOut geom_in[2],
     offset.x = 0.0f;
   }
 
-  float4 border_color = float4(theme.colors.active_spline.rgb, 0.0f);
+  float4 border_color = float4(0.0, 0.0, 0.0, 0.0);
   /* Draw the transparent border (AA). */
   if (is_active) {
     offset *= 0.75f; /* Don't make the active "halo" appear very thick. */

source/blender/editors/include/UI_resources.hh
@@ -137,8 +137,6 @@ enum ThemeColorID {
   TH_HANDLE_SEL_ALIGN,
   TH_HANDLE_SEL_AUTOCLAMP,
 
-  TH_ACTIVE_SPLINE,
-
   TH_SYNTAX_B,
   TH_SYNTAX_V,
   TH_SYNTAX_R,

source/blender/editors/interface/resources.cc
@@ -565,9 +565,6 @@ const uchar *UI_ThemeGetColorPtr(bTheme *btheme, int spacetype, int colorid)
         case TH_NURB_SEL_VLINE:
           cp = ts->nurb_sel_vline;
           break;
-        case TH_ACTIVE_SPLINE:
-          cp = ts->act_spline;
-          break;
 
         case TH_HANDLE_FREE:
           cp = btheme->common.curves.handle_free;

commit 559f9b29cdffbe5a092d4959839a71e21531747f

UI: Theme: Update Curves Handle Type colors
This PR Updates the default colors of the Curve Handle types,
to increase readability and contrast.
Each color more different from each other to not be confusing.

Logic for the new colors:
* Purple/Dark blue is used for the `Vector` type to match the header
  and sockets of Vector nodes.
* Red is used for `Auto` to show that they're locked and can't be
  changed without converting the type.
* Pink/Salmon color is used for `Auto-Clamped`  to be similar to `Auto`
   while still being distinct.
* The remaining colors are chosen to be visually distinct from the
  other colors.

This also fixes a problem caused by 1067112c11 (#145360) where
Free handle were left fully black. These black handles are extremely
hard to use with Grease Pencil objects, because Grease Pencil strokes
often are black. Because the `Free` handle selected color is also black,
handles do not show if they are selected.

Co-authored-by: Nika Kutsniashvili <nickberckley@gmail.com>
Pull Request: https://projects.blender.org/blender/blender/pulls/147335

release/datafiles/userdef/userdef_default_theme.c
@@ -334,16 +334,16 @@ const bTheme U_theme_default = {
       .long_key_selected = RGBA(0xff8c0099),
     },
     .curves = {
-      .handle_free = RGBA(0x000000ff),
-      .handle_auto = RGBA(0x909000ff),
-      .handle_vect = RGBA(0x409030ff),
-      .handle_align = RGBA(0x803060ff),
-      .handle_auto_clamped = RGBA(0x994030ff),
-      .handle_sel_free = RGBA(0x000000ff),
-      .handle_sel_auto = RGBA(0xf0ff40ff),
-      .handle_sel_vect = RGBA(0x40c030ff),
-      .handle_sel_align = RGBA(0xf090a0ff),
-      .handle_sel_auto_clamped = RGBA(0xf0af90ff),
+      .handle_free = RGBA(0x745900ff),
+      .handle_auto = RGBA(0x740d00ff),
+      .handle_vect = RGBA(0x232374ff),
+      .handle_align = RGBA(0x157000ff),
+      .handle_auto_clamped = RGBA(0x803060ff),
+      .handle_sel_free = RGBA(0xffc300ff),
+      .handle_sel_auto = RGBA(0xff1900ff),
+      .handle_sel_vect = RGBA(0x4444ffff),
+      .handle_sel_align = RGBA(0x72ff57ff),
+      .handle_sel_auto_clamped = RGBA(0xf090a0ff),
       .handle_vertex = RGBA(0x000000ff),
       .handle_vertex_select = RGBA(0xff8500ff),
       .handle_vertex_size = 5,

scripts/presets/interface_theme/Blender_Light.xml
@@ -449,16 +449,16 @@
         </anim>
         <curves>
           <ThemeCommonCurves
-            handle_free="#000000"
-            handle_sel_free="#000000"
-            handle_auto="#909000"
-            handle_sel_auto="#f0ff40"
-            handle_vect="#409030"
-            handle_sel_vect="#40c030"
-            handle_align="#803060"
-            handle_sel_align="#f090a0"
-            handle_auto_clamped="#994030"
-            handle_sel_auto_clamped="#f0af90"
+            handle_free="#745900"
+            handle_sel_free="#ffc300"
+            handle_auto="#740d00"
+            handle_sel_auto="#ff1900"
+            handle_vect="#232374"
+            handle_sel_vect="#4444ff"
+            handle_align="#157000"
+            handle_sel_align="#72ff57"
+            handle_auto_clamped="#803060"
+            handle_sel_auto_clamped="#f090a0"
             handle_vertex="#000000"
             handle_vertex_select="#ff8500"
             handle_vertex_size="5"