b133019f9f2166a99aeb19b19c64c2b9640544d9
Nikita Sirgienko <nikita.sirgienko@intel.com>
2025-10-08 13:36:08 +0200

    Cycles: oneAPI: use ocloc 101.8132 on Windows
    This new version of the graphics compiler improves performance
    for the majority of supported Intel devices and adds support
    for upcoming Intel hardware. Such an upgrade also requires
    an increase in the minimal supported driver version on Windows,
    which is why these changes are combined together with
    the ocloc upgrade.
    
    Previously set minimal version 101.6557 was increased to 101.8132.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/147460

--- intern/cycles/device/oneapi/device_impl.cpp
+++ intern/cycles/device/oneapi/device_impl.cpp
@@ -1385,11 +1385,11 @@ void OneapiDevice::get_adjusted_global_and_local_sizes(SyclQueue *queue,
 
 /* Compute-runtime (ie. NEO) version is what gets returned by sycl/L0 on Windows
  * since Windows driver 101.3268. */
-static const int lowest_supported_driver_version_win = 1016554;
+static const int lowest_supported_driver_version_win = 1018132;
 #  ifdef _WIN32
-/* For Windows driver 101.6557, compute-runtime version is 31896.
+/* For Windows driver 101.8132, compute-runtime version is 34938.
  * This information is returned by `ocloc query OCL_DRIVER_VERSION`. */
-static const int lowest_supported_driver_version_neo = 31896;
+static const int lowest_supported_driver_version_neo = 34938;
 #  else
 static const int lowest_supported_driver_version_neo = 34666;
 #  endif
