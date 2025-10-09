5841cb21f73c92e89892112d8e0415177190cf58
Omar Emara <mail@OmarEmara.dev>
2025-10-09 17:23:12 +0200

    Fix #146759: Radial Tilling node crashes in the compositor
    The Radial Tilling node crashes in the compositor in GPU mode. This is
    because the compositor does not yet support 2D vectors in shader code
    generation, but properly supports and expects them in the interface,
    which the Radial Tilling node declares. This results in a bad shader
    which crashes Blender.
    
    To properly fix this, we need to:
    
    - Support 2D vectors in compositor GPU material shader code generation.
    - Support 2D vectors in shader node GPU stack construction.
    - Adjust the interface of radial tilling to actually use 2D vectors.
    
    This seems risky for 5.0, so this patch temporarily drops support for
    the node in the compositor in 5.0. Then once 2D vectors are supported,
    it can be enabled again.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/147627

--- source/blender/nodes/shader/nodes/node_shader_radial_tiling.cc
+++ source/blender/nodes/shader/nodes/node_shader_radial_tiling.cc
@@ -209,7 +209,7 @@ void register_node_type_sh_radial_tiling()
 
   static blender::bke::bNodeType ntype;
 
-  common_node_type_base(&ntype, "ShaderNodeRadialTiling");
+  sh_geo_node_type_base(&ntype, "ShaderNodeRadialTiling");
   ntype.ui_name = "Radial Tiling";
   ntype.ui_description = "Transform Coordinate System for Radial Tiling";
   ntype.nclass = NODE_CLASS_OP_VECTOR;
