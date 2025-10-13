c1781eba5189a23121c9bfda9a4571476dd84311
Bastien Montagne <bastien@blender.org>
2025-10-13 14:39:26 +0200

    Revert "Fix: Simplify import in VR add-on"
    This breaks tests (and the VR addon itself of course).
    
    Needs to be done differently, will create new PR for proper fix.
    
    This reverts commit 0c22ef5ae7c7ac8cb11d0670f698a104ae1be823.

--- scripts/addons_core/viewport_vr_preview/gui.py
+++ scripts/addons_core/viewport_vr_preview/gui.py
@@ -18,7 +18,11 @@ from bpy.types import (
     Panel,
     UIList,
 )
-from bpy.types import VIEW3D_PT_object_type_visibility
+# Add space_view3d.py to module search path for VIEW3D_PT_object_type_visibility import.
+import os.path
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../startup/bl_ui')))
+from space_view3d import VIEW3D_PT_object_type_visibility
 
 
 # Session.
