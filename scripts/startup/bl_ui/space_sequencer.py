blender/blender

commit 2b53ed9add9023d3ee4c1d84e60b21e7d2bfde27

Fix: VSE: Python errors when no sequencer scene is present
There were multiple places in the UI code that assumed
that the sequencer scene and tool settings exist, when
they might not.

scripts/startup/bl_ui/space_sequencer.py
@@ -1123,9 +1123,9 @@ class SEQUENCER_MT_strip_retiming(Menu):
         layout = self.layout
 
         is_retiming = (
-            context.sequencer_scene and
+            context.sequencer_scene is not None and
             context.sequencer_scene.sequence_editor is not None and
-            context.sequencer_scene.sequence_editor.selected_retiming_keys
+            context.sequencer_scene.sequence_editor.selected_retiming_keys is not None
         )
         strip = context.active_strip
 

scripts/startup/bl_ui/space_time.py
@@ -45,17 +45,19 @@ def playback_controls(layout, context):
     tool_settings = context.tool_settings
     screen = context.screen
 
-    layout.popover(
-        panel="TIME_PT_playback",
-        text="Playback",
-    )
+    if scene:
+        layout.popover(
+            panel="TIME_PT_playback",
+            text="Playback",
+        )
 
-    icon_keytype = 'KEYTYPE_{:s}_VEC'.format(context.tool_settings.keyframe_type)
-    layout.popover(
-        panel="TIME_PT_keyframing_settings",
-        text_ctxt=i18n_contexts.id_windowmanager,
-        icon=icon_keytype,
-    )
+    if tool_settings:
+        icon_keytype = 'KEYTYPE_{:s}_VEC'.format(tool_settings.keyframe_type)
+        layout.popover(
+            panel="TIME_PT_keyframing_settings",
+            text_ctxt=i18n_contexts.id_windowmanager,
+            icon=icon_keytype,
+        )
 
     if is_sequencer:
         layout.prop(context.workspace, "use_scene_time_sync", text="Sync Scene Time")
@@ -95,10 +97,11 @@ def playback_controls(layout, context):
     row.operator("screen.keyframe_jump", text="", icon='NEXT_KEYFRAME').next = True
     row.operator("screen.frame_jump", text="", icon='FF').end = True
 
-    row = layout.row(align=True)
-    row.prop(tool_settings, "use_snap_playhead", text="")
-    sub = row.row(align=True)
-    sub.popover(panel="TIME_PT_playhead_snapping", text="")
+    if tool_settings:
+        row = layout.row(align=True)
+        row.prop(tool_settings, "use_snap_playhead", text="")
+        sub = row.row(align=True)
+        sub.popover(panel="TIME_PT_playhead_snapping", text="")
 
     layout.separator_spacer()
 

commit 0bcc088a36720b983729c428f99462d9897fdf8c

Fix #147934: VSE: Reset retiming never active
Caused by 2b53ed9add

The "is not None" check wasnt not working because it was used against a
boolean.
(Also bring two occasions of the "is_retiming" in sync)

Pull Request: https://projects.blender.org/blender/blender/pulls/147960

scripts/startup/bl_ui/space_sequencer.py
@@ -529,7 +529,7 @@ class SEQUENCER_MT_select(Menu):
         st = context.space_data
         has_sequencer, has_preview = _space_view_types(st)
         is_retiming = (
-            context.sequencer_scene and
+            context.sequencer_scene is not None and
             context.sequencer_scene.sequence_editor is not None and
             context.sequencer_scene.sequence_editor.selected_retiming_keys
         )
@@ -1044,7 +1044,7 @@ class SEQUENCER_MT_strip_retiming(Menu):
         is_retiming = (
             context.sequencer_scene is not None and
             context.sequencer_scene.sequence_editor is not None and
-            context.sequencer_scene.sequence_editor.selected_retiming_keys is not None
+            context.sequencer_scene.sequence_editor.selected_retiming_keys
         )
         strip = context.active_strip
 