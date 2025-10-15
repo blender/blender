aaebf2aaf8030de12fcdabbeff7d24564f798b7a
Sean Kim <SeanCTKim@protonmail.com>
2025-10-15 10:41:25 +0200

    Fix #145879: UV Sculpt settings missing from new scene
    ---
    
    Backport PR for #145945 that uses the correct 'size' value
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/145947

--- source/blender/makesdna/DNA_scene_defaults.h
+++ source/blender/makesdna/DNA_scene_defaults.h
@@ -354,6 +354,13 @@
     .sharp_max = DEG2RADF(180.0f), \
   }
 
+#define _DNA_DEFAULTS_UvSculpt \
+  { \
+    .size = 50, \
+    .strength = 1.0f, \
+    .curve_preset = BRUSH_CURVE_SMOOTH, \
+  }
+
 #define _DNA_DEFAULT_ToolSettings \
   { \
     .object_flag = SCE_OBJECT_MODE_LOCK, \
@@ -416,6 +423,7 @@
     .gpencil_v2d_align = GP_PROJECT_VIEWSPACE, \
  \
     /* UV painting */ \
+    .uvsculpt = _DNA_DEFAULTS_UvSculpt, \
     .uv_sculpt_settings = 0, \
  \
     /* Placement */ \
