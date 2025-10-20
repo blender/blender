blender/blender

commit 8adb3e758f543e87d9e8166c9a2982ed512ac537

Draw: Use ResourceHandleRange
Optimize instancing performance by syncing all instances at once.
Part of #130291

At the moment, it only works for certain Object types in Workbench and
Overlay.
Everything else follows a path similar to the previous one.

Performance on instancing heavy scenes can range from 1.5x to 3x faster
depending on hardware and platform.

Pull Request: https://projects.blender.org/blender/blender/pulls/140378

source/blender/draw/intern/draw_manager.hh
@@ -136,10 +136,10 @@ class Manager {
    * Create a new resource handle for the given object, but optionally override model matrix and
    * bounds.
    */
-  ResourceHandle resource_handle(const ObjectRef &ref,
-                                 const float4x4 *model_matrix,
-                                 const float3 *bounds_center,
-                                 const float3 *bounds_half_extent);
+  ResourceHandleRange resource_handle(const ObjectRef &ref,
+                                      const float4x4 *model_matrix,
+                                      const float3 *bounds_center,
+                                      const float3 *bounds_half_extent);
   /**
    * Get resource id for a loose matrix. The draw-calls for this resource handle won't be culled
    * and there won't be any associated object info / bounds. Assumes correct handedness / winding.
@@ -326,17 +326,42 @@ inline ResourceHandleRange Manager::resource_handle(const ObjectRef &ref, float 
   bool is_active_object = ref.is_active(object_active);
   bool is_edit_mode = object_active && DRW_object_is_in_edit_mode(object_active) &&
                       ref.object->mode == object_active->mode;
-  matrix_buf.current().get_or_resize(resource_len_).sync(*ref.object);
-  bounds_buf.current().get_or_resize(resource_len_).sync(*ref.object, inflate_bounds);
-  infos_buf.current().get_or_resize(resource_len_).sync(ref, is_active_object, is_edit_mode);
-  return ResourceHandle(resource_len_++, (ref.object->transflag & OB_NEG_SCALE) != 0);
+
+  if (ref.duplis_) {
+    uint start = resource_len_;
+
+    ObjectBounds proto_bounds;
+    proto_bounds.sync(*ref.object, inflate_bounds);
+
+    ObjectInfos proto_info;
+    proto_info.sync(ref, is_active_object, is_edit_mode);
+
+    for (const DupliObject *dupli : *ref.duplis_) {
+      matrix_buf.current().get_or_resize(resource_len_).sync(float4x4(dupli->mat));
+      bounds_buf.current().get_or_resize(resource_len_) = proto_bounds;
+
+      ObjectInfos &info = infos_buf.current().get_or_resize(resource_len_);
+      info = proto_info;
+      info.random = dupli->random_id * (1.0f / (float)0xFFFFFFFF);
+
+      resource_len_++;
+    }
+    return ResourceHandleRange(start, resource_len_ - start);
+  }
+  else {
+    matrix_buf.current().get_or_resize(resource_len_).sync(*ref.object);
+    bounds_buf.current().get_or_resize(resource_len_).sync(*ref.object, inflate_bounds);
+    infos_buf.current().get_or_resize(resource_len_).sync(ref, is_active_object, is_edit_mode);
+    return ResourceHandle(resource_len_++, (ref.object->transflag & OB_NEG_SCALE) != 0);
+  }
 }
 
-inline ResourceHandle Manager::resource_handle(const ObjectRef &ref,
-                                               const float4x4 *model_matrix,
-                                               const float3 *bounds_center,
-                                               const float3 *bounds_half_extent)
+inline ResourceHandleRange Manager::resource_handle(const ObjectRef &ref,
+                                                    const float4x4 *model_matrix,
+                                                    const float3 *bounds_center,
+                                                    const float3 *bounds_half_extent)
 {
+  BLI_assert(!ref.duplis_);
   bool is_active_object = ref.is_active(object_active);
   bool is_edit_mode = object_active && DRW_object_is_in_edit_mode(object_active) &&
                       ref.object->mode == object_active->mode;
@@ -377,6 +402,7 @@ inline ResourceHandle Manager::resource_handle(const float4x4 &model_matrix,
 inline ResourceHandle Manager::resource_handle_for_psys(const ObjectRef &ref,
                                                         const float4x4 &model_matrix)
 {
+  BLI_assert(!ref.duplis_);
   bool is_active_object = ref.is_active(object_active);
   bool is_edit_mode = object_active && DRW_object_is_in_edit_mode(object_active) &&
                       ref.object->mode == object_active->mode;

source/blender/draw/intern/draw_handle.hh
@@ -215,6 +215,9 @@ class ObjectRef {
   /** Object that created the dupli-list the current object is part of. */
   Object *const dupli_parent_ = nullptr;
 
+  /** List of (render-compatible) duplis when rendering a ranges. */
+  const VectorList<DupliObject *> *duplis_ = nullptr;
+
   /** Unique handle per object ref. */
   ResourceHandleRange handle_ = {};
   ResourceHandleRange sculpt_handle_ = {};
@@ -222,23 +225,34 @@ class ObjectRef {
  public:
   Object *const object;
 
-  ObjectRef(DEGObjectIterData &iter_data, Object *ob);
-  explicit ObjectRef(Object *ob);
+  explicit ObjectRef(Object *ob,
+                     Object *dupli_parent = nullptr,
+                     DupliObject *dupli_object = nullptr);
+  explicit ObjectRef(Object &ob, Object *dupli_parent, const VectorList<DupliObject *> &duplis);
 
   /* Is the object coming from a Dupli system. */
   bool is_dupli() const
   {
-    return dupli_object_ != nullptr;
+    return dupli_parent_ != nullptr;
   }
 
   bool is_active(const Object *active_object) const
   {
-    return (dupli_object_ ? dupli_parent_ : object) == active_object;
+    return (dupli_parent_ ? dupli_parent_ : object) == active_object;
   }
 
   float random() const
   {
-    if (dupli_object_ == nullptr) {
+    if (duplis_) {
+      /* NOTE: The random property is only used by EEVEE, which currently doesn't support
+      instancing optimizations. However, ObjectInfos always call this function so the code is still
+      reachable even if its result won't be used. */
+      // BLI_assert_unreachable();
+      /* TODO: This should fill a span instead. */
+      return 0.0;
+    }
+
+    if (dupli_parent_ == nullptr) {
       /* TODO(fclem): this is rather costly to do at draw time. Maybe we can
        * put it in ob->runtime and make depsgraph ensure it is up to date. */
       return BLI_hash_int_2d(BLI_hash_string(object->id.name + 2), 0) * (1.0f / (float)0xFFFFFFFF);
@@ -248,6 +262,14 @@ class ObjectRef {
 
   bool find_rgba_attribute(const GPUUniformAttr &attr, float r_value[4]) const
   {
+    if (duplis_) {
+      /* NOTE: This function is only called for EEVEE, which currently doesn't support instancing
+       * optimizations, so this code should be unreachable. */
+      BLI_assert_unreachable();
+      /* TODO: r_value should be a Span. */
+      return false;
+    }
+
     /* If requesting instance data, check the parent particle system and object. */
     if (attr.use_dupli) {
       return BKE_object_dupli_find_rgba_attribute(
@@ -258,7 +280,6 @@ class ObjectRef {
 
   LightLinking *light_linking() const
   {
-    /* TODO: Could this be handled directly by deg_iterator_duplis_step?  */
     return dupli_parent_ ? dupli_parent_->light_linking : object->light_linking;
   }
 
@@ -285,6 +306,14 @@ class ObjectRef {
    * systems need to be offset appropriately. */
   float4x4 particles_matrix() const
   {
+    if (duplis_) {
+      /* NOTE: Objects with particles don't support instancing optimizations yet, so this code
+       * should be unreachable. */
+      BLI_assert_unreachable();
+      /* TODO: This should fill a span instead. */
+      return float4x4::identity();
+    }
+
     /* TODO: Pass particle systems as a separate ObRef? */
     float4x4 dupli_mat = float4x4::identity();
     if (dupli_parent_ && dupli_object_) {

commit 0ddf51daa5df9ae21359d9788f9d6cd4a39b350b

Fix #148254: Broken negative scale instances
Regression from 8adb3e758f543e87d9e8166c9a2982ed512ac537

The OB_NEG_SCALE flag was set correctly, but it wasn't being taken into
account when creating the ResourceHandleRange.

Pull Request: https://projects.blender.org/blender/blender/pulls/148411

source/blender/draw/intern/draw_manager.hh
@@ -347,7 +347,8 @@ inline ResourceHandleRange Manager::resource_handle(const ObjectRef &ref, float 
 
       resource_len_++;
     }
-    return ResourceHandleRange(start, resource_len_ - start);
+    return ResourceHandleRange(ResourceHandle(start, (ref.object->transflag & OB_NEG_SCALE) != 0),
+                               resource_len_ - start);
   }
   else {
     matrix_buf.current().get_or_resize(resource_len_).sync(*ref.object);