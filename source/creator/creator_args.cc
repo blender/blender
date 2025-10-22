bdf096e583c263f5d7bd712fcc38366e080b8a90
Brecht Van Lommel <brecht@blender.org>
2025-10-22 18:20:26 +0200

    Fix #148432: blender -E help crashes
    This case is a bit unusual as it runs after WM has been initialized but
    can still only print and exit.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/148459

--- source/creator/creator_args.cc
+++ source/creator/creator_args.cc
@@ -2130,6 +2130,7 @@ static int arg_handle_engine_set(int argc, const char **argv, void *data)
       LISTBASE_FOREACH (RenderEngineType *, type, &R_engines) {
         printf("\t%s\n", type->idname);
       }
+      WM_exit_ex(C, false, false);
       exit(0);
     }
     else {
@@ -2146,6 +2147,7 @@ static int arg_handle_engine_set(int argc, const char **argv, void *data)
         }
         else {
           fprintf(stderr, "\nError: engine not found '%s'\n", engine_name);
+          WM_exit_ex(C, false, false);
           exit(1);
         }
       }
