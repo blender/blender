blender/blender

commit 0d32a530bc8e4214bd0df96a22cd674af5de349b

Refactor: Consistent handling of space runtime data
Apply 124a670184ac0971ba4f to other editors. Generally it's clearer if
DNA struct runtime data is always available. That's the design used
elsewhere, use it here too.

Pull Request: https://projects.blender.org/blender/blender/pulls/148586

source/blender/editors/space_buttons/space_buttons.cc
@@ -118,23 +118,12 @@ static void buttons_free(SpaceLink *sl)
     MEM_freeN(ct);
   }
 
-  if (sbuts->runtime != nullptr) {
-    MEM_SAFE_FREE(sbuts->runtime->tab_search_results);
-    MEM_freeN(sbuts->runtime);
-  }
+  MEM_SAFE_FREE(sbuts->runtime->tab_search_results);
+  MEM_delete(sbuts->runtime);
 }
 
 /* spacetype; init callback */
-static void buttons_init(wmWindowManager * /*wm*/, ScrArea *area)
-{
-  SpaceProperties *sbuts = (SpaceProperties *)area->spacedata.first;
-
-  if (sbuts->runtime == nullptr) {
-    sbuts->runtime = MEM_mallocN<SpaceProperties_Runtime>(__func__);
-    sbuts->runtime->search_string[0] = '\0';
-    sbuts->runtime->tab_search_results = BLI_BITMAP_NEW(BCONTEXT_TOT * 2, __func__);
-  }
-}
+static void buttons_init(wmWindowManager * /*wm*/, ScrArea * /*area*/) {}
 
 static SpaceLink *buttons_duplicate(SpaceLink *sl)
 {
@@ -144,11 +133,9 @@ static SpaceLink *buttons_duplicate(SpaceLink *sl)
   /* clear or remove stuff from old */
   sbutsn->path = nullptr;
   sbutsn->texuser = nullptr;
-  if (sfile_old->runtime != nullptr) {
-    sbutsn->runtime = static_cast<SpaceProperties_Runtime *>(MEM_dupallocN(sfile_old->runtime));
-    sbutsn->runtime->search_string[0] = '\0';
-    sbutsn->runtime->tab_search_results = BLI_BITMAP_NEW(BCONTEXT_TOT, __func__);
-  }
+  sbutsn->runtime = MEM_new<SpaceProperties_Runtime>(__func__, *sfile_old->runtime);
+  sbutsn->runtime->search_string[0] = '\0';
+  sbutsn->runtime->tab_search_results = BLI_BITMAP_NEW(BCONTEXT_TOT, __func__);
 
   return (SpaceLink *)sbutsn;
 }
@@ -345,16 +332,11 @@ int ED_buttons_search_string_length(SpaceProperties *sbuts)
 
 void ED_buttons_search_string_set(SpaceProperties *sbuts, const char *value)
 {
-  if (sbuts->runtime) {
-    STRNCPY_UTF8(sbuts->runtime->search_string, value);
-  }
+  STRNCPY_UTF8(sbuts->runtime->search_string, value);
 }
 
 bool ED_buttons_tab_has_search_result(SpaceProperties *sbuts, const int index)
 {
-  if (!sbuts->runtime) {
-    return false;
-  }
   return BLI_BITMAP_TEST(sbuts->runtime->tab_search_results, index);
 }
 
@@ -429,7 +411,7 @@ static void property_search_all_tabs(const bContext *C,
   SpaceProperties sbuts_copy = blender::dna::shallow_copy(*sbuts);
   sbuts_copy.path = nullptr;
   sbuts_copy.texuser = nullptr;
-  sbuts_copy.runtime = static_cast<SpaceProperties_Runtime *>(MEM_dupallocN(sbuts->runtime));
+  sbuts_copy.runtime = MEM_new<SpaceProperties_Runtime>(__func__, *sbuts->runtime);
   sbuts_copy.runtime->tab_search_results = nullptr;
   BLI_listbase_clear(&area_copy.spacedata);
   BLI_addtail(&area_copy.spacedata, &sbuts_copy);
@@ -1055,12 +1037,14 @@ static void buttons_foreach_id(SpaceLink *space_link, LibraryForeachIDData *data
 static void buttons_space_blend_read_data(BlendDataReader * /*reader*/, SpaceLink *sl)
 {
   SpaceProperties *sbuts = (SpaceProperties *)sl;
+  sbuts->runtime = MEM_new<SpaceProperties_Runtime>(__func__);
+  sbuts->runtime->search_string[0] = '\0';
+  sbuts->runtime->tab_search_results = BLI_BITMAP_NEW(BCONTEXT_TOT * 2, __func__);
 
   sbuts->path = nullptr;
   sbuts->texuser = nullptr;
   sbuts->mainbo = sbuts->mainb;
   sbuts->mainbuser = sbuts->mainb;
-  sbuts->runtime = nullptr;
 }
 
 static void buttons_space_blend_read_after_liblink(BlendLibReader * /*reader*/,

source/blender/editors/space_node/space_node.cc
@@ -653,10 +653,7 @@ static void node_init(wmWindowManager * /*wm*/, ScrArea * /*area*/) {}
 static void node_exit(wmWindowManager *wm, ScrArea *area)
 {
   SpaceNode *snode = static_cast<SpaceNode *>(area->spacedata.first);
-
-  if (snode->runtime) {
-    free_previews(*wm, *snode);
-  }
+  free_previews(*wm, *snode);
 }
 
 static bool any_node_uses_id(const bNodeTree *ntree, const ID *id)

source/blender/editors/space_outliner/space_outliner.cc
@@ -385,6 +385,7 @@ static SpaceLink *outliner_create(const ScrArea * /*area*/, const Scene * /*scen
   SpaceOutliner *space_outliner;
 
   space_outliner = MEM_callocN<SpaceOutliner>("initoutliner");
+  space_outliner->runtime = MEM_new<SpaceOutliner_Runtime>(__func__);
   space_outliner->spacetype = SPACE_OUTLINER;
   space_outliner->filter_id_type = ID_GR;
   space_outliner->show_restrict_flags = SO_RESTRICT_ENABLE | SO_RESTRICT_HIDE | SO_RESTRICT_RENDER;
@@ -423,30 +424,18 @@ static void outliner_free(SpaceLink *sl)
 }
 
 /* spacetype; init callback */
-static void outliner_init(wmWindowManager * /*wm*/, ScrArea *area)
-{
-  SpaceOutliner *space_outliner = static_cast<SpaceOutliner *>(area->spacedata.first);
-
-  if (space_outliner->runtime == nullptr) {
-    space_outliner->runtime = MEM_new<SpaceOutliner_Runtime>("SpaceOutliner_Runtime");
-  }
-}
+static void outliner_init(wmWindowManager * /*wm*/, ScrArea * /*area*/) {}
 
 static SpaceLink *outliner_duplicate(SpaceLink *sl)
 {
   SpaceOutliner *space_outliner = (SpaceOutliner *)sl;
   SpaceOutliner *space_outliner_new = MEM_dupallocN<SpaceOutliner>(__func__, *space_outliner);
+  space_outliner_new->runtime = MEM_new<SpaceOutliner_Runtime>(__func__, *space_outliner->runtime);
 
   BLI_listbase_clear(&space_outliner_new->tree);
   space_outliner_new->treestore = nullptr;
 
   space_outliner_new->sync_select_dirty = WM_OUTLINER_SYNC_SELECT_FROM_ALL;
-
-  if (space_outliner->runtime) {
-    /* Copy constructor handles details. */
-    space_outliner_new->runtime = MEM_new<SpaceOutliner_Runtime>("SpaceOutliner_runtime dup",
-                                                                 *space_outliner->runtime);
-  }
 
   return (SpaceLink *)space_outliner_new;
 }
@@ -544,6 +533,7 @@ static void outliner_deactivate(ScrArea *area)
 static void outliner_space_blend_read_data(BlendDataReader *reader, SpaceLink *sl)
 {
   SpaceOutliner *space_outliner = (SpaceOutliner *)sl;
+  space_outliner->runtime = MEM_new<SpaceOutliner_Runtime>(__func__);
 
   /* use #BLO_read_get_new_data_address_no_us and do not free old memory avoiding double
    * frees and use of freed memory. this could happen because of a
@@ -569,7 +559,6 @@ static void outliner_space_blend_read_data(BlendDataReader *reader, SpaceLink *s
     space_outliner->storeflag |= SO_TREESTORE_CLEANUP; /* at first draw */
   }
   BLI_listbase_clear(&space_outliner->tree);
-  space_outliner->runtime = nullptr;
 }
 
 static void outliner_space_blend_read_after_liblink(BlendLibReader * /*reader*/,

source/blender/editors/space_sequencer/space_sequencer.cc
@@ -87,6 +87,7 @@ static SpaceLink *sequencer_create(const ScrArea * /*area*/, const Scene *scene)
   SpaceSeq *sseq;
 
   sseq = MEM_callocN<SpaceSeq>("initsequencer");
+  sseq->runtime = MEM_new<SpaceSeq_Runtime>(__func__);
   sseq->spacetype = SPACE_SEQ;
   sseq->chanshown = 0;
   sseq->view = SEQ_VIEW_SEQUENCE;
@@ -217,13 +218,7 @@ static void sequencer_free(SpaceLink *sl)
 }
 
 /* Space-type init callback. */
-static void sequencer_init(wmWindowManager * /*wm*/, ScrArea *area)
-{
-  SpaceSeq *sseq = (SpaceSeq *)area->spacedata.first;
-  if (sseq->runtime == nullptr) {
-    sseq->runtime = MEM_new<SpaceSeq_Runtime>(__func__);
-  }
-}
+static void sequencer_init(wmWindowManager * /*wm*/, ScrArea * /*area*/) {}
 
 static void sequencer_refresh(const bContext *C, ScrArea *area)
 {
@@ -1121,8 +1116,7 @@ static void sequencer_channel_region_draw(const bContext *C, ARegion *region)
 static void sequencer_space_blend_read_data(BlendDataReader * /*reader*/, SpaceLink *sl)
 {
   SpaceSeq *sseq = (SpaceSeq *)sl;
-
-  sseq->runtime = nullptr;
+  sseq->runtime = MEM_new<SpaceSeq_Runtime>(__func__);
 
   /* grease pencil data is not a direct data and can't be linked from direct_link*
    * functions, it should be linked from lib_link* functions instead

source/blender/editors/space_spreadsheet/space_spreadsheet.cc
@@ -54,6 +54,7 @@ namespace blender::ed::spreadsheet {
 static SpaceLink *spreadsheet_create(const ScrArea * /*area*/, const Scene * /*scene*/)
 {
   SpaceSpreadsheet *spreadsheet_space = MEM_callocN<SpaceSpreadsheet>("spreadsheet space");
+  spreadsheet_space->runtime = MEM_new<SpaceSpreadsheet_Runtime>(__func__);
   spreadsheet_space->spacetype = SPACE_SPREADSHEET;
 
   spreadsheet_space->geometry_id.base.type = SPREADSHEET_TABLE_ID_TYPE_GEOMETRY;
@@ -118,25 +119,14 @@ static void spreadsheet_free(SpaceLink *sl)
   spreadsheet_table_id_free_content(&sspreadsheet->geometry_id.base);
 }
 
-static void spreadsheet_init(wmWindowManager * /*wm*/, ScrArea *area)
-{
-  SpaceSpreadsheet *sspreadsheet = (SpaceSpreadsheet *)area->spacedata.first;
-  if (sspreadsheet->runtime == nullptr) {
-    sspreadsheet->runtime = MEM_new<SpaceSpreadsheet_Runtime>(__func__);
-  }
-}
+static void spreadsheet_init(wmWindowManager * /*wm*/, ScrArea * /*area*/) {}
 
 static SpaceLink *spreadsheet_duplicate(SpaceLink *sl)
 {
   const SpaceSpreadsheet *sspreadsheet_old = (SpaceSpreadsheet *)sl;
   SpaceSpreadsheet *sspreadsheet_new = (SpaceSpreadsheet *)MEM_dupallocN(sspreadsheet_old);
-  if (sspreadsheet_old->runtime) {
-    sspreadsheet_new->runtime = MEM_new<SpaceSpreadsheet_Runtime>(__func__,
-                                                                  *sspreadsheet_old->runtime);
-  }
-  else {
-    sspreadsheet_new->runtime = MEM_new<SpaceSpreadsheet_Runtime>(__func__);
-  }
+  sspreadsheet_new->runtime = MEM_new<SpaceSpreadsheet_Runtime>(__func__,
+                                                                *sspreadsheet_old->runtime);
 
   BLI_listbase_clear(&sspreadsheet_new->row_filters);
   LISTBASE_FOREACH (const SpreadsheetRowFilter *, src_filter, &sspreadsheet_old->row_filters) {
@@ -726,7 +716,7 @@ static void spreadsheet_blend_read_data(BlendDataReader *reader, SpaceLink *sl)
 {
   SpaceSpreadsheet *sspreadsheet = (SpaceSpreadsheet *)sl;
 
-  sspreadsheet->runtime = nullptr;
+  sspreadsheet->runtime = MEM_new<SpaceSpreadsheet_Runtime>(__func__);
   BLO_read_struct_list(reader, SpreadsheetRowFilter, &sspreadsheet->row_filters);
   LISTBASE_FOREACH (SpreadsheetRowFilter *, row_filter, &sspreadsheet->row_filters) {
     BLO_read_string(reader, &row_filter->value_string);

source/blender/nodes/intern/sync_sockets.cc
@@ -686,11 +686,8 @@ void node_can_sync_cache_clear(Main &bmain)
         SpaceLink *sl = static_cast<SpaceLink *>(area->spacedata.first);
         if (sl->spacetype == SPACE_NODE) {
           SpaceNode *snode = reinterpret_cast<SpaceNode *>(sl);
-          /* This may be called before runtime data is initialized currently. */
-          if (snode->runtime) {
-            Map<int, bool> &cache = ed::space_node::node_can_sync_cache_get(*snode);
-            cache.clear();
-          }
+          Map<int, bool> &cache = ed::space_node::node_can_sync_cache_get(*snode);
+          cache.clear();
         }
       }
     }

commit 09b557affbd31cadbc08a9709cfb1b5dd73e7555

Fix: Crash missing property editor runtime data after recent commit
Caused by 0d32a530bc8e4214bd0df96a22cd674af5de349b

source/blender/editors/space_buttons/space_buttons.cc
@@ -65,6 +65,11 @@ static SpaceLink *buttons_create(const ScrArea * /*area*/, const Scene * /*scene
   SpaceProperties *sbuts;
 
   sbuts = MEM_callocN<SpaceProperties>("initbuts");
+
+  sbuts->runtime = MEM_new<SpaceProperties_Runtime>(__func__);
+  sbuts->runtime->search_string[0] = '\0';
+  sbuts->runtime->tab_search_results = BLI_BITMAP_NEW(BCONTEXT_TOT, __func__);
+
   sbuts->spacetype = SPACE_PROPERTIES;
   sbuts->mainb = sbuts->mainbuser = BCONTEXT_OBJECT;
   sbuts->visible_tabs = uint(-1); /* 0xFFFFFFFF - All tabs visible by default. */