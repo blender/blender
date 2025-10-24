63414e7ae3f9b2e010bf3bf49a38c106727bd2d7
Campbell Barton <campbell@blender.org>
2025-10-24 00:59:58 +0000

    Fix: incorrect expansion of relative paths for library data-blocks
    Correct the basepath argument to BLI_path_abs which caused
    invalid file-path use for library data-blocks.
    
    Ref !148687

--- source/blender/sequencer/intern/media_presence.cc
+++ source/blender/sequencer/intern/media_presence.cc
@@ -32,7 +32,7 @@ static const char *strip_base_path_get(const Strip *strip)
                         BKE_main_blendfile_path_from_global();
 }
 
-static bool check_sound_media_missing(const bSound *sound, const Strip *strip)
+static bool check_sound_media_missing(const bSound *sound)
 {
   if (sound == nullptr) {
     return false;
@@ -40,8 +40,7 @@ static bool check_sound_media_missing(const bSound *sound, const Strip *strip)
 
   char filepath[FILE_MAX];
   STRNCPY(filepath, sound->filepath);
-  const char *basepath = strip_base_path_get(strip);
-  BLI_path_abs(filepath, basepath);
+  BLI_path_abs(filepath, ID_BLEND_PATH_FROM_GLOBAL(&sound->id));
   return !BLI_exists(filepath);
 }
 
@@ -121,7 +120,7 @@ bool media_presence_is_missing(Scene *scene, const Strip *strip)
       missing = *val;
     }
     else {
-      missing = check_sound_media_missing(sound, strip);
+      missing = check_sound_media_missing(sound);
       presence->map_sound.add_new(sound, missing);
     }
   }
