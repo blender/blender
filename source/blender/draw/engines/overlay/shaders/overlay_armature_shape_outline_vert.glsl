b296cf2dac7b4ee1db6b3a43f914f238b7a3e2cc
Miguel Pozo <pragma37@gmail.com>
2025-10-27 15:34:00 +0100

    Fix #148766: Overlay: Wrong outline at small scales
    Regression from f7ef7eff5e5.
    
    Normalize the cross product so the epsilon is not size-dependent.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/148866

--- source/blender/draw/engines/overlay/shaders/overlay_armature_shape_outline_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_armature_shape_outline_vert.glsl
@@ -12,6 +12,7 @@ VERTEX_SHADER_CREATE_INFO(overlay_armature_shape_outline)
 #include "gpu_shader_index_load_lib.glsl"
 
 #include "gpu_shader_math_matrix_transform_lib.glsl"
+#include "gpu_shader_math_safe_lib.glsl"
 #include "gpu_shader_utildefines_lib.glsl"
 #include "overlay_common_lib.glsl"
 #include "select_lib.glsl"
@@ -100,8 +101,12 @@ void geometry_main(VertOut geom_in[4],
   float3 v12 = geom_in[2].vs_P - geom_in[1].vs_P;
   float3 v13 = geom_in[3].vs_P - geom_in[1].vs_P;
 
+  /* Known Issue: This also generates outlines for connected-overlapping edges, since their vector
+   * is zero-length. */
   float3 n0 = cross(v12, v10);
+  n0 *= safe_rcp(length(n0));
   float3 n3 = cross(v13, v12);
+  n3 *= safe_rcp(length(n3));
 
   float fac0 = dot(view_vec, n0);
   float fac3 = dot(view_vec, n3);
