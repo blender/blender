a6ee0eb560fdee8ef1fbdde45fea37fa60e206a5
Sean Kim <SeanCTKim@protonmail.com>
2025-10-28 22:08:29 +0100

    Fix #148915: Crash when using uninitialized pressure curve
    Introduced in 5892069296
    
    Despite the naming, the block of code in `paint_brush_update` guarded by
    the `brush_init` variable does not occur early enough to generically
    handle where some custom curves could be used.
    
    This commit moves the related `CurveMapping` initialization higher up in
    the main paint modal function and renames variables / comments to make
    the organization less confusing.
    
    Further cleanup of initialization logic for paint operators is outside
    the scope of this PR.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/148960

--- source/blender/editors/sculpt_paint/paint_stroke.cc
+++ source/blender/editors/sculpt_paint/paint_stroke.cc
@@ -109,10 +109,9 @@ struct PaintStroke {
   int event_type;
   /* check if stroke variables have been initialized */
   bool stroke_init;
-  /* check if various brush mapping variables have been initialized */
-  bool brush_init;
+  /* check if input variables have been initialized (e.g. cursor position & pressure)*/
+  bool input_init;
   float2 initial_mouse;
-  /* cached_pressure stores initial pressure for size pressure influence mainly */
   float cached_size_pressure;
   /* last pressure will store last pressure value for use in interpolation for space strokes */
   float last_pressure;
@@ -334,17 +333,14 @@ bool paint_brush_update(bContext *C,
    *      brush coord/pressure/etc.
    *      It's more an events design issue, which doesn't split coordinate/pressure/angle
    *      changing events. We should avoid this after events system re-design */
-  if (!stroke->brush_init) {
+  if (!stroke->input_init) {
     copy_v2_v2(stroke->initial_mouse, mouse);
     copy_v2_v2(paint_runtime.last_rake, mouse);
     copy_v2_v2(paint_runtime.tex_mouse, mouse);
     copy_v2_v2(paint_runtime.mask_tex_mouse, mouse);
     stroke->cached_size_pressure = pressure;
-    BKE_curvemapping_init(brush.curve_size);
-    BKE_curvemapping_init(brush.curve_strength);
-    BKE_curvemapping_init(brush.curve_jitter);
 
-    stroke->brush_init = true;
+    stroke->input_init = true;
   }
 
   if (paint_supports_dynamic_size(brush, mode)) {
@@ -1589,6 +1585,10 @@ wmOperatorStatus paint_stroke_modal(bContext *C,
         stroke->stroke_cursor = WM_paint_cursor_activate(
             SPACE_TYPE_ANY, RGN_TYPE_ANY, paint_brush_cursor_poll, paint_draw_line_cursor, stroke);
       }
+
+      BKE_curvemapping_init(br->curve_size);
+      BKE_curvemapping_init(br->curve_strength);
+      BKE_curvemapping_init(br->curve_jitter);
 
       first_dab = true;
     }
