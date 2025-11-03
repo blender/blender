blender/blender

commit 1b46ffccb60d3aa9302f7b3aa83efd477f7f9888

Fix #148754: ShaderCreateInfo use after free
Always wait for ongoing compilations, so `ShaderCreateInfo` can be
safely deleted after Batch cancellation.
Note that this may increase app exit time if shaders are still being
compiled.

Pull Request: https://projects.blender.org/blender/blender/pulls/148933

source/blender/gpu/intern/gpu_shader.cc
@@ -911,20 +911,11 @@ void ShaderCompiler::batch_cancel(BatchHandle &handle)
   Batch *batch = batches_.pop(handle);
   compilation_queue_.remove_batch(batch);
 
-  if (batch->is_specialization_batch()) {
-    /* For specialization batches, we block until ready, since base shader compilation may be
-     * cancelled afterwards, leaving the specialization with a deleted base shader. */
-    compilation_finished_notification_.wait(lock, [&]() { return batch->is_ready(); });
-  }
-
-  if (batch->is_ready()) {
-    batch->free_shaders();
-    MEM_delete(batch);
-  }
-  else {
-    /* If it's currently compiling, the compilation thread makes the cleanup. */
-    batch->is_cancelled = true;
-  }
+  /* If it was already being compiled, wait until it's ready so the calling thread can safely
+   * delete the ShaderCreateInfos. */
+  compilation_finished_notification_.wait(lock, [&]() { return batch->is_ready(); });
+  batch->free_shaders();
+  MEM_delete(batch);
 
   handle = 0;
 }
@@ -1015,14 +1006,7 @@ void ShaderCompiler::do_work(void *work_payload)
     specialize_shader(batch->specializations[shader_index]);
   }
 
-  {
-    std::lock_guard lock(mutex_);
-    batch->pending_compilations--;
-    if (batch->is_ready() && batch->is_cancelled) {
-      batch->free_shaders();
-      MEM_delete(batch);
-    }
-  }
+  batch->pending_compilations--;
 
   compilation_finished_notification_.notify_all();
 }

source/blender/gpu/intern/gpu_shader_private.hh
@@ -142,7 +142,6 @@ class ShaderCompiler {
     Vector<ShaderSpecialization> specializations;
 
     std::atomic<int> pending_compilations = 0;
-    std::atomic<bool> is_cancelled = false;
 
     bool is_specialization_batch()
     {

commit 7eb767c650e2cbad7919956b0ee1fef052d5588a

Fix #149267: Occasional hangs in ShaderCompiler::wait_for_all
Fix race condition introduced in 1b46ffccb6.

`batch->pending_compilations--` and
`compilation_finished_notification_.notify_all()` could happen
 while `is_compiling_impl` was running.

Pull Request: https://projects.blender.org/blender/blender/pulls/149302

source/blender/gpu/intern/gpu_shader.cc
@@ -1006,7 +1006,10 @@ void ShaderCompiler::do_work(void *work_payload)
     specialize_shader(batch->specializations[shader_index]);
   }
 
-  batch->pending_compilations--;
+  {
+    std::unique_lock lock(mutex_);
+    batch->pending_compilations--;
+  }
 
   compilation_finished_notification_.notify_all();
 }