1a30058e75290b16b537a2ca1a9f87a4ab3d7a02
YimingWu <xp8110@outlook.com>
2025-11-03 13:35:22 +0100

    GreasePencil: Make it possible to use lattice parenting option
    When parenting a grease pencil object to lattice, there's a `Lattice
    Deform` option available before grease pencil rewrite in 4.3, however
    the option wasn't working due to changed modifier types. This fix allows
    this option to add grease pencil lattice modifier when lattice deform
    option is chosen.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/149252

--- source/blender/editors/object/object_relations.cc
+++ source/blender/editors/object/object_relations.cc
@@ -643,9 +643,18 @@ static bool parent_set_with_depsgraph(ReportList *reports,
             break;
           case PAR_LATTICE: /* lattice deform */
             if (BKE_modifiers_is_deformed_by_lattice(ob) != par) {
-              md = modifier_add(reports, bmain, scene, ob, nullptr, eModifierType_Lattice);
+              const bool is_grease_pencil = ob->type == OB_GREASE_PENCIL;
+              const ModifierType lattice_modifier_type = is_grease_pencil ?
+                                                             eModifierType_GreasePencilLattice :
+                                                             eModifierType_Lattice;
+              md = modifier_add(reports, bmain, scene, ob, nullptr, lattice_modifier_type);
               if (md) {
-                ((LatticeModifierData *)md)->object = par;
+                if (is_grease_pencil) {
+                  reinterpret_cast<GreasePencilLatticeModifierData *>(md)->object = par;
+                }
+                else {
+                  reinterpret_cast<LatticeModifierData *>(md)->object = par;
+                }
               }
             }
             break;
