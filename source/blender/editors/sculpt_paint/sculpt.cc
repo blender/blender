5408f7e13d8347acdf633c517a2f90b397d4c1d7
Sean Kim <SeanCTKim@protonmail.com>
2025-11-04 00:10:03 +0100

    Fix #149229: Multires Boundary Smooth brush crashes
    Introduced with 583ae8990cc60896ce86014227b28a37cc771691
    
    The multires variant of `calc_vert_neighbors` incorrectly populated the
    offset data for its `GroupedSpan`:
    * Entries were only populated once per grid, not once per grid
      coordinate
    * The final offset was not populated
    
    This lead to slices of data accessing invalid memory due to the
    neighboring offset values being uninitialized data and resulting in
    negative or arbitrarily sized slices
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/149311

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -7627,10 +7627,12 @@ GroupedSpan<int> calc_vert_neighbors(const SubdivCCG &subdiv_ccg,
   for (const int i : grids.index_range()) {
     const int grid = grids[i];
     const int node_verts_start = i * key.grid_area;
-    r_offset_data[node_verts_start] = r_data.size();
 
     for (const short y : IndexRange(key.grid_size)) {
       for (const short x : IndexRange(key.grid_size)) {
+        const int offset = CCG_grid_xy_to_index(key.grid_size, x, y);
+        r_offset_data[node_verts_start + offset] = r_data.size();
+
         SubdivCCGCoord coord{};
         coord.grid_index = grid;
         coord.x = x;
@@ -7642,6 +7644,7 @@ GroupedSpan<int> calc_vert_neighbors(const SubdivCCG &subdiv_ccg,
       }
     }
   }
+  r_offset_data.last() = r_data.size();
   return GroupedSpan<int>(r_offset_data.as_span(), r_data.as_span());
 }
 
