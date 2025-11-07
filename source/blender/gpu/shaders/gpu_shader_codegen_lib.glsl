blender/blender

commit 34e176d255b5d7ad9119cb90c3f0156e04220d62

Fix #138191: EEVEE: Bump is incorrect with pixel size greater than 1x
This was caused by the hardware derivatives output being affected
by render resolution. Scaling them back to the full resolution
value fixes the issue.

This also fixes the Wireframe node that also relies on derivatives.

Pull Request: https://projects.blender.org/blender/blender/pulls/142101

source/blender/gpu/shaders/gpu_shader_codegen_lib.glsl
@@ -340,8 +340,8 @@ void dF_branch(float fn, out float2 result)
 {
   /* NOTE: this function is currently unused, once it is used we need to check if
    * `g_derivative_filter_width` needs to be applied. */
-  result.x = gpu_dfdx(fn);
-  result.y = gpu_dfdy(fn);
+  result.x = gpu_dfdx(fn) * derivative_scale_get();
+  result.y = gpu_dfdy(fn) * derivative_scale_get();
 }
 
 #else

source/blender/draw/engines/eevee/shaders/eevee_nodetree_lib.glsl
@@ -608,6 +608,11 @@ float2 bsdf_lut(float cos_theta, float roughness, float ior, bool do_multiscatte
  *
  * \{ */
 
+#ifndef GPU_METAL
+/* Prototype. */
+float derivative_scale_get();
+#endif
+
 #ifdef MAT_DISPLACEMENT_BUMP
 /* Return new shading normal. */
 float3 displacement_bump()
@@ -620,8 +625,8 @@ float3 displacement_bump()
   float2 dHd;
   dF_branch(dot(nodetree_displacement(), g_data.N + dF_impl(g_data.N)), bump_filter_width, dHd);
 
-  float3 dPdx = gpu_dfdx(g_data.P);
-  float3 dPdy = gpu_dfdy(g_data.P);
+  float3 dPdx = gpu_dfdx(g_data.P) * derivative_scale_get();
+  float3 dPdy = gpu_dfdy(g_data.P) * derivative_scale_get();
 
   /* Get surface tangents from normal. */
   float3 Rx = cross(dPdy, g_data.N);
@@ -723,6 +728,17 @@ float3 coordinate_incoming(float3 P)
 float texture_lod_bias_get()
 {
   return uniform_buf.film.texture_lod_bias;
+}
+
+/**
+ * Scale hardware derivatives depending on render resolution.
+ * This is because the distance between pixels increases as we lower the resolution. The hardware
+ * uses neighboring pixels to compute derivatives and thus the value increases as we lower the
+ * resolution. So we compensate by scaling them back to the expected amplitude at full resolution.
+ */
+float derivative_scale_get()
+{
+  return 1.0 / float(uniform_buf.film.scaling_factor);
 }
 
 /** \} */

source/blender/gpu/shaders/material/gpu_shader_material_bump.glsl
@@ -28,8 +28,8 @@ void node_bump(float strength,
   dist *= FrontFacing ? invert : -invert;
 
 #ifdef GPU_FRAGMENT_SHADER
-  float3 dPdx = gpu_dfdx(g_data.P);
-  float3 dPdy = gpu_dfdy(g_data.P);
+  float3 dPdx = gpu_dfdx(g_data.P) * derivative_scale_get();
+  float3 dPdy = gpu_dfdy(g_data.P) * derivative_scale_get();
 
   /* Get surface tangents from normal. */
   float3 Rx = cross(dPdy, N);

source/blender/gpu/shaders/material/gpu_shader_material_wireframe.glsl
@@ -20,8 +20,8 @@ void node_wireframe_screenspace(float size, out float fac)
 
 #ifdef GPU_FRAGMENT_SHADER
   size *= (1.0f / 3.0f);
-  float3 dx = gpu_dfdx(barys);
-  float3 dy = gpu_dfdy(barys);
+  float3 dx = gpu_dfdx(barys) * derivative_scale_get();
+  float3 dy = gpu_dfdy(barys) * derivative_scale_get();
   float3 deltas = sqrt(dx * dx + dy * dy);
 
   float3 s = step(-deltas * size, -barys);

commit cc44dffdf8a31c43009015e115fdfa490622ba97

Fix #149496: EEVEE: Bump node bump strength varies with pixel size
The bump filter width was missing the same scaling as
the other derivatives. This now makes bump truly
independant of pixel scale.

The issue was introduced by 34e176d255b5d7ad9119cb90c3f0156e04220d62
which fixed some of the `dFdx/dFdy` but missed this one.

Pull Request: https://projects.blender.org/blender/blender/pulls/149518

source/blender/gpu/shaders/gpu_shader_codegen_lib.glsl
@@ -287,7 +287,7 @@ float3 dF_impl(float3 v)
 
 #  define dF_branch(fn, filter_width, result) \
     if (true) { \
-      g_derivative_filter_width = filter_width; \
+      g_derivative_filter_width = filter_width * derivative_scale_get(); \
       g_derivative_flag = 1; \
       result.x = (fn); \
       g_derivative_flag = -1; \
@@ -299,7 +299,7 @@ float3 dF_impl(float3 v)
 /* Used when the non-offset value is already computed elsewhere */
 #  define dF_branch_incomplete(fn, filter_width, result) \
     if (true) { \
-      g_derivative_filter_width = filter_width; \
+      g_derivative_filter_width = filter_width * derivative_scale_get(); \
       g_derivative_flag = 1; \
       result.x = (fn); \
       g_derivative_flag = -1; \