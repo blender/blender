d4d272b1fa2b86e417ecf887a235a34c87742a95
Campbell Barton <campbell@blender.org>
2025-11-09 12:44:18 +1100

    Cleanup: use a timeout for requests_basic_access test

--- tools/check_blender_release/scripts/requests_basic_access.py
+++ tools/check_blender_release/scripts/requests_basic_access.py
@@ -6,8 +6,8 @@ import requests
 
 website = "https://blender.org/"
 
-r = requests.get(website, verify=True)
+r = requests.get(website, verify=True, timeout=30)
 
 assert r.status_code == 200, f"{website} returned a status code {r.status_code}, we expected 200"
 assert r.reason == "OK", f"Didn't get 'OK' response from {website}, got {r.reason}"
-assert len(r.content) > 256, f"The content we got from the web request is too small to be valid"
+assert len(r.content) > 256, "The content we got from the web request is too small to be valid"
