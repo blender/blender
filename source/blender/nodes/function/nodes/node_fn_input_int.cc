aa4cdc41e3d742585b64eb28a5b635de3ed1f1df
W_Cloud <yunkezengren@outlook.com>
2025-11-10 15:03:46 +0100

    Fix: Nodes: Ensure input node content expands to fill available space
    Fix layout regression by setting `LayoutAlign::Expand` to several input nodes layout,
    ensuring their contents correctly fills the available space when resized.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/149641

--- source/blender/nodes/function/nodes/node_fn_input_int.cc
+++ source/blender/nodes/function/nodes/node_fn_input_int.cc
@@ -14,6 +14,7 @@ namespace blender::nodes::node_fn_input_int_cc {
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Int>("Integer").custom_draw([](CustomSocketDrawParams &params) {
+    params.layout.alignment_set(ui::LayoutAlign::Expand);
     uiLayout &row = params.layout.row(true);
     row.prop(&params.node_ptr, "integer", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
     if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
