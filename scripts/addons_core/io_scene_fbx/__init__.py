b490acfe25190410c1e227b2e7bde408e1bcad4e
Bastien Montagne <bastien@blender.org>
2025-11-11 10:59:22 +0100

    Fix #146700: Preserve better Materials orders per object.
    The limitations of FBX format when it comes to Face material indices
    (they map to the order in which materials are connected to the object
    using the mesh) makes it impossible to guarantee a correct, preserved
    order of Blender material indices at export in the geometry.
    
    However, by iterating over the objects and then their materials will
    help giving better, more consistent results, and address issues like the
    one reported in #146700.
    
    (The previous process was iterating over all gathered materials, and
    then their user objects, which could lead to re-ordering of the
    materials in some objects under certain circumstances).
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/148876
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/148911

--- scripts/addons_core/io_scene_fbx/__init__.py
+++ scripts/addons_core/io_scene_fbx/__init__.py
@@ -5,7 +5,7 @@
 bl_info = {
     "name": "FBX format",
     "author": "Campbell Barton, Bastien Montagne, Jens Restemeier, @Mysteryem",
-    "version": (5, 13, 0),
+    "version": (5, 13, 1),
     "blender": (4, 5, 0),
     "location": "File > Import-Export",
     "description": "FBX IO meshes, UVs, vertex colors, materials, textures, cameras, lamps and actions",
