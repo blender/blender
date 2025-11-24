64e24856a5fa146fff09c1d93ebe634ba0e31e30
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-11-24 10:35:28 +0100

    Anim: reserve size for evaluation result map
    Since we know how big the `Map` has to be we
    can reserve the size in the constructor. This
    increases performance by not having to move memory
    around as the map size increases.
    
    The higher the FCurve count, the larger the improvement.
    
    | - | main | PR |
    | - | - | - |
    | rain | 0.0107s | 0.0104s |
    | high_bone_count_armature | 0.0164s | 0.0144s |
    
    part of #149405
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/150127

--- source/blender/animrig/ANIM_evaluation.hh
+++ source/blender/animrig/ANIM_evaluation.hh
@@ -88,6 +88,8 @@ class EvaluationResult {
   EvaluationMap result_;
 
  public:
+  EvaluationResult() = default;
+
   operator bool() const
   {
     return !this->is_empty();
@@ -96,6 +98,15 @@ class EvaluationResult {
   {
     return result_.is_empty();
   }
+
+  /**
+   * When the expected count of entries is known, reserving with size instead of growing on demand
+   * is more performant.
+   */
+  void reserve(const int64_t size)
+  {
+    result_.reserve(size);
+  };
 
   void store(const StringRefNull rna_path,
              const int array_index,
