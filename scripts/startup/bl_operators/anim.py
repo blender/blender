c0699601786e2b0a7bd06825aa5348a54ae5cda7
shikhin <shikhindah@gmail.com>
2025-11-24 13:56:52 +0100

    Fix #149724: Baking animation removes constraints from linked file
    The issue was that the code did not check if a constraint came from
    a linked file.
    Fix it by checking the `is_override` condition and only remove local constraints.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/150309

--- scripts/startup/bl_operators/anim.py
+++ scripts/startup/bl_operators/anim.py
@@ -222,7 +222,7 @@ class NLA_OT_bake(Operator):
         default=False,
     )
     clear_constraints: BoolProperty(
-        name="Clear Constraints",
+        name="Clear Local Constraints",
         description="Remove all constraints from keyed object/bones, and do 'visual' keying",
         default=False,
     )
