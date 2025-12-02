blender/blender

commit d4b32b8218db6631c41b63e54ec9571d9d51067c

Fix #150705: Relative multires navigation doesn't work with unselected object
When using the `object.subdivision_set` operator, it was possible for
there to be an active but unselected object (e.g. in Sculpt mode),
leading to the operator failing to execute. This is rather confusing
behavior, so it has been changed to fallback to the active object if
no selected objects exist.

Pull Request: https://projects.blender.org/blender/blender/pulls/150758

scripts/startup/bl_operators/object.py
@@ -249,12 +249,21 @@ class SubdivisionSet(Operator):
         relative = self.relative
         ensure_modifier = self.ensure_modifier
 
+        objs = context.selected_editable_objects
+        if not objs:
+            active = context.active_object
+            if active:
+                objs = [active]
+            else:
+                self.report({'WARNING'}, "No active object")
+                return {'CANCELLED'}
+
         if relative and level == 0:
             return {'CANCELLED'}  # nothing to do
 
         if not ensure_modifier:
             any_object_has_relevant_modifier = False
-            for obj in context.selected_editable_objects:
+            for obj in objs:
                 if obj.mode == 'SCULPT':
                     any_object_has_relevant_modifier |= any(mod.type == 'MULTIRES' for mod in obj.modifiers)
                 elif obj.mode == 'OBJECT':
@@ -322,7 +331,7 @@ class SubdivisionSet(Operator):
                 except Exception:
                     self.report({'WARNING'}, rpt_("Modifiers cannot be added to object: {:s}").format(obj.name))
 
-        for obj in context.selected_editable_objects:
+        for obj in objs:
             set_object_subd(obj)
 
         return {'FINISHED'}

commit 0591216ce42b54114cddd80b57fd0e0ba15beb9a

Fix: `object.subdivison_set` behaves inconsistently in Object mode
Introduced with d4b32b8218db6631c41b63e54ec9571d9d51067c

In Object Mode, the operator continues to only work on selected,
editable objects. In paint modes, the object now only works on the
current active object, though the only paint mode with a default
keybind that uses this operator remains Sculpt Mode.

Additionally, the `poll` function is removed, as it is no longer
consistent and cannot be updated to remain so while still being
performant due to using `selected_editable_objects`

Pull Request: https://projects.blender.org/blender/blender/pulls/150940

scripts/startup/bl_operators/object.py
@@ -239,24 +239,24 @@ class SubdivisionSet(Operator):
         options={'HIDDEN'}
     )
 
-    @classmethod
-    def poll(cls, context):
-        obs = context.selected_editable_objects
-        return (obs is not None)
-
     def execute(self, context):
         level = self.level
         relative = self.relative
         ensure_modifier = self.ensure_modifier
 
-        objs = context.selected_editable_objects
+        active_object = context.active_object
+
+        # For paint modes, defer to the active object, not the selected objects.
+        if active_object and active_object.mode in {'SCULPT', 'VERTEX_PAINT', 'WEIGHT_PAINT', 'TEXTURE_PAINT'}:
+            objs = [active_object]
+        else:
+            objs = context.selected_editable_objects
+
         if not objs:
-            active = context.active_object
-            if active:
-                objs = [active]
-            else:
-                self.report({'WARNING'}, "No active object")
-                return {'CANCELLED'}
+            # Note that this will never be true for the paint cases, hence the following report only mentions
+            # the selected & editable objects.
+            self.report({'WARNING'}, "No selected editable objects to operate on")
+            return {'CANCELLED'}
 
         if relative and level == 0:
             return {'CANCELLED'}  # nothing to do