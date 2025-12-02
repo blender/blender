accaf2b6a995fa06d71d415581c9a006c3ee8420
Bastien Montagne <bastien@blender.org>
2025-12-02 17:56:33 +0100

    Fix #150794: Crash in Outliner when drag-n-dropping across windows.
    When the source and destination windows of a drag'n'drop operation are
    different, both may need to get the notification, such that they can
    properly update their outliner trees data.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/150984

--- source/blender/editors/space_outliner/outliner_dragdrop.cc
+++ source/blender/editors/space_outliner/outliner_dragdrop.cc
@@ -1368,7 +1368,10 @@ static wmOperatorStatus collection_drop_invoke(bContext *C,
   /* Update dependency graph. */
   DEG_id_tag_update(&data.to->id, ID_RECALC_SYNC_TO_EVAL | ID_RECALC_HIERARCHY);
   DEG_relations_tag_update(bmain);
-  WM_event_add_notifier(C, NC_SCENE | ND_LAYER, scene);
+  /* NOTE: It is possible to drag'n'drop between different windows, which means that the source
+   * window/Outliner may also need to be updated. So do not pass the current window in this
+   * notifier (unless there is a way to get the drag source window as well?). */
+  WM_event_add_notifier_ex(CTX_wm_manager(C), nullptr, NC_SCENE | ND_LAYER, nullptr);
 
   return OPERATOR_FINISHED;
 }
