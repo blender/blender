cc95035814fd5f82dc414336e2626d22cdec500a
kavu <kavukamari@gmail.com>
2025-12-04 06:38:58 +0100

    Fix #140422: Unable to zoom in on a 1x1 image
    When images were very small (1x1 was the given example) the user could
    not zoom any further than filling the screen. This change instead bases
    the max and min zoom on the ratio of image size to screen size:
      pixel as 25x the screen's largest dimension for zooming in
      image as 1/100x the screen's largest dimension for zooming out
    I feel like this intuitively makes more sense than 4x4 smallest, and
    screen size maximum, especially since the previous method used
    magic numbers for determining when to check zoom levels
    (these are still magic numbers, but in relation to contextual values)
    I'm unsure how floating point errors or astronomically large images
    will work with this, but as far as my testing went, it seemed fine
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/149042

--- source/blender/editors/space_image/image_ops.cc
+++ source/blender/editors/space_image/image_ops.cc
@@ -104,22 +104,28 @@ static void sima_zoom_set(
 
   sima->zoom = zoom;
 
-  if (sima->zoom < 0.1f || sima->zoom > 4.0f) {
-    /* check zoom limits */
-    ED_space_image_get_size(sima, &width, &height);
+  ED_space_image_get_size(sima, &width, &height);
 
-    width *= sima->zoom;
-    height *= sima->zoom;
-
-    if ((width < 4) && (height < 4) && sima->zoom < oldzoom) {
-      sima->zoom = oldzoom;
-    }
-    else if (BLI_rcti_size_x(&region->winrct) <= sima->zoom) {
-      sima->zoom = oldzoom;
-    }
-    else if (BLI_rcti_size_y(&region->winrct) <= sima->zoom) {
-      sima->zoom = oldzoom;
-    }
+  /* Preset zoom limits.
+   *   Zoom `min` is based on ratio of apparent image size to largest screen dimension.
+   *   Zoom `max` is based on ratio of apparent pixel size to largest screen dimension.
+   *      (The reason being so the user can still zoom in on very large images.)
+   */
+  const float sima_minzoom_image_over_screen = 1.0f / 100.0f;
+  const float sima_maxzoom_pixel_over_screen = 10.0f / 1.0f;
+  /* Check zoom limits. */
+  /* Get largest dimensions for image and workspace for comparison. */
+  float comp_image = std::max(width, height);
+  float comp_screen = std::max(BLI_rcti_size_x(&region->winrct), BLI_rcti_size_y(&region->winrct));
+  /* Stop when image size is less than minzoom ratio compared to screen size. */
+  if (comp_image * sima->zoom < comp_screen * sima_minzoom_image_over_screen &&
+      sima->zoom < oldzoom)
+  {
+    sima->zoom = oldzoom;
+  }
+  /* Stop when max relative screen bounds is smaller than apparent pixel size. */
+  else if (comp_screen * sima_maxzoom_pixel_over_screen <= sima->zoom && sima->zoom > oldzoom) {
+    sima->zoom = oldzoom;
   }
 
   if (zoom_to_pos && location) {
