ecc782ef869a1318dd3f2c1c99966644d6972562
W_Cloud <yunkezengren@outlook.com>
2025-12-05 16:34:54 +0100

    Fix #140344: Nodes: Panel toggle label out of node bounds
    Fix panel toggle label out of node bounds by reserving space for the
    checkbox. I spent some time looking for a better solution but didn't
    succeed, so manually tweaking the number was the easiest fix.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/151127

--- source/blender/editors/space_node/node_draw.cc
+++ source/blender/editors/space_node/node_draw.cc
@@ -2086,13 +2086,14 @@ static void node_draw_panels(bNodeTree &ntree, const bNode &node, uiBlock &block
     const char *panel_translation_context = (panel_decl.translation_context.has_value() ?
                                                  panel_decl.translation_context->c_str() :
                                                  nullptr);
+    const float left_padding = (input_socket == nullptr) ? 40.0f : 60.0f;
     uiBut *label_but = uiDefBut(
         &block,
         ButType::Label,
         CTX_IFACE_(panel_translation_context, panel_decl.name),
         offsetx,
         int(*panel_runtime.header_center_y - NODE_DYS),
-        short(draw_bounds.xmax - draw_bounds.xmin - (30.0f * UI_SCALE_FAC)),
+        short(draw_bounds.xmax - draw_bounds.xmin - (left_padding * UI_SCALE_FAC)),
         NODE_DY,
         nullptr,
         0,
