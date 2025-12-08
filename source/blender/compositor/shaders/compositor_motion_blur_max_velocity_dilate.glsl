1895df7fe82fb72fefbd43ea313a288f37832b54
Jeroen Bakker <jeroen@blender.org>
2025-12-08 10:36:52 +0100

    Cleanup: Compositor: Removed unused variable + call
    Pull Request: https://projects.blender.org/blender/blender/pulls/150978

--- source/blender/compositor/shaders/compositor_motion_blur_max_velocity_dilate.glsl
+++ source/blender/compositor/shaders/compositor_motion_blur_max_velocity_dilate.glsl
@@ -100,8 +100,6 @@ void main()
   }
 
   if (true) {
-    MotionPayload payload = motion_blur_tile_indirection_pack_payload(max_motion.zw,
-                                                                      uint2(src_tile));
     /* Rectangular area (in tiles) where the motion vector spreads. */
     MotionRect motion_rect = compute_motion_rect(src_tile, max_motion.zw);
     MotionLine motion_line = compute_motion_line(src_tile, max_motion.zw);
