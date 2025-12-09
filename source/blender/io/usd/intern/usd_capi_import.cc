9201984e657014cbcd1ef5ac6dc4a1cbcf593273
Jesse Yurkovich <jesse.y@gmail.com>
2025-12-09 01:14:30 +0100

    USD: Allow cancelation during object creation and tidy up progress calc
    The object creation step, before data is loaded into each object, can be
    quite long due to inefficiencies in creating large numbers of objects.
    Allow cancelation to occur during this step[1].
    
    While at it, simplify progress bar calculations and make it slightly
    more finer grained.
    
    [1] It seems that cancelation generates ID user count management errors
    on the terminal. This deserves investigation and a separate, isolated,
    fix if necessary as this has seemingly existed in main for quite some
    time.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/151286

--- source/blender/io/usd/intern/usd_capi_import.cc
+++ source/blender/io/usd/intern/usd_capi_import.cc
@@ -100,10 +100,6 @@ struct ImportJobData {
 
   USDStageReader *archive;
 
-  bool *stop;
-  bool *do_update;
-  float *progress;
-
   char error_code;
   bool was_canceled;
   bool import_ok;
@@ -124,10 +120,6 @@ static void report_job_duration(const ImportJobData *data)
 static void import_startjob(void *customdata, wmJobWorkerStatus *worker_status)
 {
   ImportJobData *data = static_cast<ImportJobData *>(customdata);
-
-  data->stop = &worker_status->stop;
-  data->do_update = &worker_status->do_update;
-  data->progress = &worker_status->progress;
   data->was_canceled = false;
   data->archive = nullptr;
   data->start_time = timeit::Clock::now();
@@ -155,16 +147,12 @@ static void import_startjob(void *customdata, wmJobWorkerStatus *worker_status)
 
   BLI_path_abs(data->filepath, BKE_main_blendfile_path_from_global());
 
-  *data->do_update = true;
-  *data->progress = 0.05f;
-
+  worker_status->progress = 0.05f;
+  worker_status->do_update = true;
   if (G.is_break) {
     data->was_canceled = true;
     return;
   }
-
-  *data->do_update = true;
-  *data->progress = 0.1f;
 
   pxr::UsdStagePopulationMask pop_mask;
   for (const std::string &mask_token : pxr::TfStringTokenize(data->params.prim_path_mask, ",;")) {
@@ -188,6 +176,13 @@ static void import_startjob(void *customdata, wmJobWorkerStatus *worker_status)
     return;
   }
 
+  worker_status->progress = 0.1f;
+  worker_status->do_update = true;
+  if (G.is_break) {
+    data->was_canceled = true;
+    return;
+  }
+
   double scene_scale = data->params.scale;
   if (data->params.apply_unit_conversion_scale) {
     scene_scale *= pxr::UsdGeomGetStageMetersPerUnit(stage);
@@ -198,9 +193,6 @@ static void import_startjob(void *customdata, wmJobWorkerStatus *worker_status)
     data->scene->r.sfra = stage->GetStartTimeCode();
     data->scene->r.efra = stage->GetEndTimeCode();
   }
-
-  *data->do_update = true;
-  *data->progress = 0.15f;
 
   /* Callback function to lazily create a cache file when converting
    * time varying data. */
@@ -225,15 +217,21 @@ static void import_startjob(void *customdata, wmJobWorkerStatus *worker_status)
   };
 
   USDStageReader *archive = new USDStageReader(stage, data->params, get_cache_file);
+  data->archive = archive;
 
   /* Ensure Python types for invoking hooks are registered. */
   register_hook_converters();
 
   archive->find_material_import_hook_sources();
 
-  data->archive = archive;
-
   archive->collect_readers();
+
+  worker_status->progress = 0.15f;
+  worker_status->do_update = true;
+  if (G.is_break) {
+    data->was_canceled = true;
+    return;
+  }
 
   if (data->params.import_lights && data->params.create_world_material &&
       !archive->dome_light_readers().is_empty())
@@ -246,24 +244,29 @@ static void import_startjob(void *customdata, wmJobWorkerStatus *worker_status)
     archive->import_all_materials(data->bmain);
   }
 
-  *data->do_update = true;
-  *data->progress = 0.2f;
-
-  const float size = float(archive->readers().size());
-  size_t i = 0;
+  worker_status->progress = 0.2f;
+  worker_status->do_update = true;
 
   /* Sort readers by name: when creating a lot of objects in Blender,
    * it is much faster if the order is sorted by name. */
   archive->sort_readers();
-  *data->do_update = true;
-  *data->progress = 0.25f;
+
+  worker_status->progress = 0.25f;
+  worker_status->do_update = true;
+
+  const float size = float(archive->readers().size());
+  size_t i = 0;
 
   /* Create blender objects. */
   for (USDPrimReader *reader : archive->readers()) {
     reader->create_object(data->bmain);
-    if ((++i & 1023) == 0) {
-      *data->do_update = true;
-      *data->progress = 0.25f + 0.25f * (i / size);
+
+    worker_status->progress = 0.25f + 0.25f * (++i / size);
+    worker_status->do_update = true;
+
+    if (G.is_break) {
+      data->was_canceled = true;
+      return;
     }
   }
 
@@ -281,8 +284,8 @@ static void import_startjob(void *customdata, wmJobWorkerStatus *worker_status)
       ob->parent = parent->object();
     }
 
-    *data->progress = 0.5f + 0.5f * (++i / size);
-    *data->do_update = true;
+    worker_status->progress = 0.5f + 0.5f * (++i / size);
+    worker_status->do_update = true;
 
     if (G.is_break) {
       data->was_canceled = true;
