0b9b8fa0736a85a3e7b9b79f44f489ba63a8e138
Julian Eisel <julian@blender.org>
2025-12-16 18:36:44 +0100

    Tests: UI: Add more UI tests for stacked fullscreens
    Adds automated UI tests to ensure the behavior fixed in ef7fd50f8a keeps
    working as intended.
    
    Expands the fullscreen tests introduced in 871fc1e4a1.

--- tests/python/ui_simulate/test_fullscreen.py
+++ tests/python/ui_simulate/test_fullscreen.py
@@ -47,6 +47,8 @@ def wm_toggle_fullscreen():
     t.assertNotEqual(len(window.screen.areas), 1)
 
 
+# Checks that opening a temporary file browser exits correctly, as well as exiting a temporary file
+# browser on top of a maximized area.
 def wm_toggle_stacked_fullscreen_file_browser():
     import bpy
 
@@ -94,3 +96,43 @@ def wm_toggle_stacked_fullscreen_file_browser():
     yield from _call_by_name(e, "Toggle Maximize Area")
     yield e.ret()
     t.assertNotEqual(len(window.screen.areas), 1)
+
+
+
+# Checks that stacking a temporary file browser on top of a temporary image editor exits correctly.
+def wm_toggle_stacked_fullscreens():
+    import bpy
+
+    e, t = _test_vars(window := _test_window())
+
+    t.assertNotEqual(len(window.screen.areas), 1, "Expected a window with more than one area")
+
+    # Ensure temporary file browsers will be opened in a maximized screen.
+    bpy.context.preferences.view.filebrowser_display_type = 'SCREEN'
+    bpy.context.preferences.view.render_display_type = 'SCREEN'
+
+    initial_area = window.screen.areas[0]
+
+    # Open temporary image editor (would use F12, but better to not involve rendering in tests).
+    yield e.f11()
+    t.assertEqual(len(window.screen.areas), 1)
+    t.assertEqual(window.screen.areas[0].type, 'IMAGE_EDITOR')
+    # Create a new image (wouldn't be needed with F12).
+    yield e.alt.n()
+    yield e.ret()
+    yield e.ret()
+
+    # Save image to spawn a temporary file browser.
+    yield e.alt.s()
+    t.assertEqual(len(window.screen.areas), 1)
+    t.assertEqual(window.screen.areas[0].type, 'FILE_BROWSER')
+
+    # Cancel file browser, back to temporary image editor.
+    yield e.esc()
+    t.assertEqual(len(window.screen.areas), 1)
+    t.assertEqual(window.screen.areas[0].type, 'IMAGE_EDITOR')
+
+    # Cancel temporary image editor, back to normal screen.
+    yield e.esc()
+    t.assertNotEqual(len(window.screen.areas), 1)
+    t.assertEqual(window.screen.areas[0], initial_area)
