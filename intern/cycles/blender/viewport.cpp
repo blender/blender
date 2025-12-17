blender/blender

commit e7d4720e750ec6cb709e7eb142e4ec64d9fcfcce

Refactor: Cycles: Remove use of RNA C++ API: viewport.cpp
Part of #130989

intern/cycles/blender/viewport.cpp
@@ -7,6 +7,9 @@
 
 #include "scene/pass.h"
 
+#include "DNA_screen_types.h"
+#include "DNA_view3d_types.h"
+
 CCL_NAMESPACE_BEGIN
 
 BlenderViewportParameters::BlenderViewportParameters()
@@ -20,27 +23,30 @@ BlenderViewportParameters::BlenderViewportParameters()
 {
 }
 
-BlenderViewportParameters::BlenderViewportParameters(BL::SpaceView3D &b_v3d, bool use_developer_ui)
+BlenderViewportParameters::BlenderViewportParameters(::View3D *b_v3d, bool use_developer_ui)
     : BlenderViewportParameters()
 {
   if (!b_v3d) {
     return;
   }
 
-  BL::View3DShading shading = b_v3d.shading();
-  PointerRNA cshading = RNA_pointer_get(&shading.ptr, "cycles");
+  ::View3DShading shading = b_v3d->shading;
+  PointerRNA v3d_rna_ptr = RNA_pointer_create_discrete(nullptr, &RNA_SpaceView3D, b_v3d);
+  PointerRNA shading_rna_ptr = RNA_pointer_get(&v3d_rna_ptr, "shading");
 
   /* We only copy the shading parameters if we are in look-dev mode.
    * Otherwise defaults are being used. These defaults mimic normal render settings. */
-  if (shading.type() == BL::View3DShading::type_RENDERED) {
-    use_scene_world = shading.use_scene_world_render();
-    use_scene_lights = shading.use_scene_lights_render();
+  if (shading.type == OB_RENDER) {
+    use_scene_world = shading.flag & V3D_SHADING_SCENE_WORLD_RENDER;
+    use_scene_lights = shading.flag & V3D_SHADING_SCENE_LIGHTS_RENDER;
 
     if (!use_scene_world) {
-      studiolight_rotate_z = shading.studiolight_rotate_z();
-      studiolight_intensity = shading.studiolight_intensity();
-      studiolight_background_alpha = shading.studiolight_background_alpha();
-      studiolight_path = shading.selected_studio_light().path();
+      studiolight_rotate_z = shading.studiolight_rot_z;
+      studiolight_intensity = shading.studiolight_intensity;
+      studiolight_background_alpha = shading.studiolight_background;
+      PointerRNA selected_studiolight_rna_ptr = RNA_pointer_get(&shading_rna_ptr,
+                                                                "selected_studio_light");
+      studiolight_path = RNA_string_get(&selected_studiolight_rna_ptr, "path");
     }
   }
 
@@ -52,7 +58,7 @@ BlenderViewportParameters::BlenderViewportParameters(BL::SpaceView3D &b_v3d, boo
 
   display_pass = PASS_COMBINED;
 
-  const string display_pass_identifier = get_enum_identifier(cshading, "render_pass");
+  const string display_pass_identifier = get_enum_identifier(shading_rna_ptr, "render_pass");
   if (!display_pass_identifier.empty()) {
     const ustring pass_type_identifier(string_to_lower(display_pass_identifier));
     const NodeEnum *pass_type_enum = Pass::get_type_enum();
@@ -62,7 +68,7 @@ BlenderViewportParameters::BlenderViewportParameters(BL::SpaceView3D &b_v3d, boo
   }
 
   if (use_developer_ui) {
-    show_active_pixels = get_boolean(cshading, "show_active_pixels");
+    show_active_pixels = get_boolean(shading_rna_ptr, "show_active_pixels");
   }
 }
 

intern/cycles/blender/light.cpp
@@ -180,7 +180,7 @@ void BlenderSync::sync_background_light(BL::SpaceView3D &b_v3d)
 
   world_map = b_world.ptr.data;
   world_recalc = false;
-  viewport_parameters = BlenderViewportParameters(b_v3d, use_developer_ui);
+  viewport_parameters = BlenderViewportParameters(b_v3d.ptr.data_as<::View3D>(), use_developer_ui);
 }
 
 CCL_NAMESPACE_END

intern/cycles/blender/object.cpp
@@ -471,7 +471,8 @@ void BlenderSync::sync_objects(BL::Depsgraph &b_depsgraph,
 
   /* object loop */
   bool cancel = false;
-  const bool show_lights = BlenderViewportParameters(b_v3d, use_developer_ui).use_scene_lights;
+  const bool show_lights =
+      BlenderViewportParameters(b_v3d.ptr.data_as<::View3D>(), use_developer_ui).use_scene_lights;
 
   BL::ViewLayer b_view_layer = b_depsgraph.view_layer_eval();
   BL::Depsgraph::object_instances_iterator b_instance_iter;

intern/cycles/blender/shader.cpp
@@ -1672,7 +1672,8 @@ void BlenderSync::sync_world(BL::Depsgraph &b_depsgraph, BL::SpaceView3D &b_v3d,
 
   BL::World b_world = view_layer.world_override ? view_layer.world_override : b_scene.world();
 
-  const BlenderViewportParameters new_viewport_parameters(b_v3d, use_developer_ui);
+  const BlenderViewportParameters new_viewport_parameters(b_v3d.ptr.data_as<::View3D>(),
+                                                          use_developer_ui);
 
   Shader *shader = scene->default_background;
 

intern/cycles/blender/sync.cpp
@@ -265,7 +265,8 @@ void BlenderSync::sync_recalc(BL::Depsgraph &b_depsgraph,
   }
 
   if (b_v3d) {
-    const BlenderViewportParameters new_viewport_parameters(b_v3d, use_developer_ui);
+    const BlenderViewportParameters new_viewport_parameters(b_v3d.ptr.data_as<::View3D>(),
+                                                            use_developer_ui);
 
     if (viewport_parameters.shader_modified(new_viewport_parameters)) {
       world_recalc = true;
@@ -557,7 +558,8 @@ void BlenderSync::sync_film(BL::ViewLayer &b_view_layer, BL::SpaceView3D &b_v3d)
   Film *film = scene->film;
 
   if (b_v3d) {
-    const BlenderViewportParameters new_viewport_parameters(b_v3d, use_developer_ui);
+    const BlenderViewportParameters new_viewport_parameters(b_v3d.ptr.data_as<::View3D>(),
+                                                            use_developer_ui);
     film->set_display_pass(new_viewport_parameters.display_pass);
     film->set_show_active_pixels(new_viewport_parameters.show_active_pixels);
   }

intern/cycles/blender/viewport.h
@@ -4,11 +4,12 @@
 
 #pragma once
 
-#include "RNA_blender_cpp.hh"
-
 #include "kernel/types.h"
 
 #include "util/param.h"
+
+struct View3D;
+struct bScreen;
 
 CCL_NAMESPACE_BEGIN
 
@@ -27,7 +28,7 @@ class BlenderViewportParameters {
   bool show_active_pixels;
 
   BlenderViewportParameters();
-  BlenderViewportParameters(BL::SpaceView3D &b_v3d, bool use_developer_ui);
+  BlenderViewportParameters(::View3D *b_v3d, bool use_developer_ui);
 
   /* Check whether any of shading related settings are different from the given parameters. */
   bool shader_modified(const BlenderViewportParameters &other) const;

commit 80a7b0212f46ca7b0ffac1d4562687329a353b76

Fix: Cycles crash in viewport render
Introduced by e7d4720e750.

Very sneaky: the "render_pass" property was accessed from the DNA
View3D shading structure, not from Cycles shading settings.

intern/cycles/blender/viewport.cpp
@@ -58,7 +58,8 @@ BlenderViewportParameters::BlenderViewportParameters(::View3D *b_v3d, bool use_d
 
   display_pass = PASS_COMBINED;
 
-  const string display_pass_identifier = get_enum_identifier(shading_rna_ptr, "render_pass");
+  PointerRNA cycles_shading_ptr = RNA_pointer_get(&shading_rna_ptr, "cycles");
+  const string display_pass_identifier = get_enum_identifier(cycles_shading_ptr, "render_pass");
   if (!display_pass_identifier.empty()) {
     const ustring pass_type_identifier(string_to_lower(display_pass_identifier));
     const NodeEnum *pass_type_enum = Pass::get_type_enum();
@@ -68,7 +69,7 @@ BlenderViewportParameters::BlenderViewportParameters(::View3D *b_v3d, bool use_d
   }
 
   if (use_developer_ui) {
-    show_active_pixels = get_boolean(shading_rna_ptr, "show_active_pixels");
+    show_active_pixels = get_boolean(cycles_shading_ptr, "show_active_pixels");
   }
 }
 