2ae058812ca8cacb155244b1967843513b911f1e
Clément Foucault <foucault.clem@gmail.com>
2025-12-17 14:44:37 +0100

    Fix: GPU: Shader Tool: Failing tests
    Tests were not updated after the latest changes requiring
    alignment and padding of unions.

--- source/blender/gpu/tests/shader_preprocess_test.cc
+++ source/blender/gpu/tests/shader_preprocess_test.cc
@@ -133,70 +133,64 @@ static void test_preprocess_union()
   {
     string input = R"(
 struct [[host_shared]] T {
-  float foo;
-  float bar;
-  float baz;
   union {
-    union_t<uint> a;
-    union_t<int> b;
-    union_t<float> c;
+    union_t<uint4> a;
+    union_t<int4> b;
+    union_t<float4> c;
   };
 };
 )";
     string expect =
         R"(
-#line 6
+#line 3
 struct                 T_union0 {
-  float data0;
+  float4 data0;
 
 };
 #line 2
 struct                 T {
-  float foo;
-  float bar;
-  float baz;
          T_union0 union0;
-#line 41
+#line 38
 };
 #ifndef GPU_METAL
-uint _a(const T this_);
-void _a_set_(_ref(T ,this_), uint value);
-int _b(const T this_);
-void _b_set_(_ref(T ,this_), int value);
-float _c(const T this_);
-void _c_set_(_ref(T ,this_), float value);
+uint4 _a(const T this_);
+void _a_set_(_ref(T ,this_), uint4 value);
+int4 _b(const T this_);
+void _b_set_(_ref(T ,this_), int4 value);
+float4 _c(const T this_);
+void _c_set_(_ref(T ,this_), float4 value);
 #endif
-#line 12
-uint _a(const T this_)       {
-  uint val;
+#line 9
+uint4 _a(const T this_)       {
+  uint4 val;
   val = floatBitsToUint(this_.union0.data0);
   return val;
 }
-#line 18
-void _a_set_(_ref(T ,this_), uint value) {
+#line 15
+void _a_set_(_ref(T ,this_), uint4 value) {
   this_.union0.data0 = uintBitsToFloat(value);
 }
-#line 22
-int _b(const T this_)       {
-  int val;
+#line 19
+int4 _b(const T this_)       {
+  int4 val;
   val = floatBitsToInt(this_.union0.data0);
   return val;
 }
-#line 28
-void _b_set_(_ref(T ,this_), int value) {
+#line 25
+void _b_set_(_ref(T ,this_), int4 value) {
   this_.union0.data0 = intBitsToFloat(value);
 }
-#line 32
-float _c(const T this_)       {
-  float val;
+#line 29
+float4 _c(const T this_)       {
+  float4 val;
   val = this_.union0.data0;
   return val;
 }
-#line 38
-void _c_set_(_ref(T ,this_), float value) {
+#line 35
+void _c_set_(_ref(T ,this_), float4 value) {
   this_.union0.data0 = value;
 }
-#line 42
+#line 39
 )";
     string error;
     string output = process_test_string(input, error);
@@ -206,13 +200,13 @@ void _c_set_(_ref(T ,this_), float value) {
   {
     string input = R"(
 struct [[host_shared]] T {
-  float foo;
-  float bar;
+  float2 foo;
+  float2 bar;
   union {
-    union_t<uint> a;
+    union_t<uint4> a;
   };
   union {
-    union_t<uint> b;
+    union_t<uint4> b;
   };
 };
 )";
@@ -220,47 +214,47 @@ struct [[host_shared]] T {
         R"(
 #line 5
 struct                 T_union0 {
-  float data0;
+  float4 data0;
 
 };
 #line 8
 struct                 T_union1 {
-  float data0;
+  float4 data0;
 
 };
 #line 2
 struct                 T {
-  float foo;
-  float bar;
+  float2 foo;
+  float2 bar;
          T_union0 union0;
 #line 8
          T_union1 union1;
 #line 31
 };
 #ifndef GPU_METAL
-uint _a(const T this_);
-void _a_set_(_ref(T ,this_), uint value);
-uint _b(const T this_);
-void _b_set_(_ref(T ,this_), uint value);
+uint4 _a(const T this_);
+void _a_set_(_ref(T ,this_), uint4 value);
+uint4 _b(const T this_);
+void _b_set_(_ref(T ,this_), uint4 value);
 #endif
 #line 12
-uint _a(const T this_)       {
-  uint val;
+uint4 _a(const T this_)       {
+  uint4 val;
   val = floatBitsToUint(this_.union0.data0);
   return val;
 }
 #line 18
-void _a_set_(_ref(T ,this_), uint value) {
+void _a_set_(_ref(T ,this_), uint4 value) {
   this_.union0.data0 = uintBitsToFloat(value);
 }
 #line 22
-uint _b(const T this_)       {
-  uint val;
+uint4 _b(const T this_)       {
+  uint4 val;
   val = floatBitsToUint(this_.union1.data0);
   return val;
 }
 #line 28
-void _b_set_(_ref(T ,this_), uint value) {
+void _b_set_(_ref(T ,this_), uint4 value) {
   this_.union1.data0 = uintBitsToFloat(value);
 }
 #line 32
@@ -372,7 +366,6 @@ void _a_set_(_ref(T ,this_), float4x4 value) {
   this_.union0.data3 = value[3];
 }
 #line 23
-
 )";
     string error;
     string output = process_test_string(input, error);
