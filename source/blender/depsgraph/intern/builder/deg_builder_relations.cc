613588c751c00c3908561689021a532787992287
Bastien Montagne <bastien@blender.org>
2025-12-29 11:07:45 +0100

    Fix #151868: Missing object relations building from Collections in some cases.
    This could later lead to crashes, due to evaluating code expecting some
    propagation of recalc flags between geometry and their objects, which
    would not happen due to the missed relations building.
    
    See also !152082.
    
    Co-authored-by: Sergey <sergey@blender.org>
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/152084

--- source/blender/depsgraph/intern/builder/deg_builder_relations.cc
+++ source/blender/depsgraph/intern/builder/deg_builder_relations.cc
@@ -625,6 +625,12 @@ void DepsgraphRelationBuilder::build_idproperties(IDProperty *id_property)
 void DepsgraphRelationBuilder::build_collection(LayerCollection *from_layer_collection,
                                                 Collection *collection)
 {
+  if (!built_map_.check_is_built_and_tag(collection, BuilderMap::TAG_COLLECTION_PROPERTIES)) {
+    build_idproperties(collection->id.properties);
+    build_idproperties(collection->id.system_properties);
+    build_parameters(&collection->id);
+  }
+
   if (from_layer_collection != nullptr) {
     /* If we came from layer collection we don't go deeper, view layer builder takes care of going
      * deeper.
@@ -645,6 +651,13 @@ void DepsgraphRelationBuilder::build_collection(LayerCollection *from_layer_coll
         /* Check whether the object hierarchy node exists, because the view layer builder can skip
          * bases if they are constantly excluded from the collections. */
         if (Node *object_hierarchy_node = this->find_node(object_hierarchy_key)) {
+          /* Note that while view layer builder ensures objects are built first and only then layer
+           * collections are built, it is not possible to assert() here: the object might be pulled
+           * into the dependency graph via different indirections, and it might not have been
+           * traversed into at this point.
+           * This build_object() is more of a safety measure, which follows the common rule in the
+           * dependency graph builder: ensure the entity is built when you encounter it. */
+          build_object(object);
           this->add_operation_relation(collection_hierarchy_exit,
                                        object_hierarchy_node->get_entry_operation(),
                                        "Collection -> Object hierarchy");
@@ -658,10 +671,6 @@ void DepsgraphRelationBuilder::build_collection(LayerCollection *from_layer_coll
   if (built_map_.check_is_built_and_tag(collection)) {
     return;
   }
-
-  build_idproperties(collection->id.properties);
-  build_idproperties(collection->id.system_properties);
-  build_parameters(&collection->id);
 
   const BuilderStack::ScopedEntry stack_entry = stack_.trace(collection->id);
 
