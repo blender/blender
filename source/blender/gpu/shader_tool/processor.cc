blender/blender

commit 952c198eb73ea9fea5bc9bb4d18378bd954e714d

GPU: Shader Tool: Replace last usage of regex and allow partial parsing
Doing partial parsing make it safer for GLSL input (python shaders).
Only the tokenizer and token merging pass will run and these
have no failure case (can't crash).

source/blender/gpu/shader_tool/processor.cc
@@ -8,9 +8,7 @@
 
 #include <cctype>
 #include <cstdint>
-#include <functional>
 #include <iostream>
-#include <regex>
 #include <set>
 #include <string>
 #include <unordered_set>
@@ -39,7 +37,7 @@ SourceProcessor::Result SourceProcessor::convert(vector<Symbol> symbols_set)
   metadata_.symbol_table.insert(
       metadata_.symbol_table.end(), symbols_set.begin(), symbols_set.end());
 
-  const string filename = regex_replace(filepath_, regex(R"((?:.*)\/(.*))"), "$1");
+  const string filename = filepath_.substr(filepath_.find_last_of('/') + 1);
 
   string str = this->source_;
 
@@ -265,12 +263,12 @@ void SourceProcessor::cleanup_whitespace(Parser &parser)
   parser.apply_mutations();
 }
 
-/* Safer version without Parser. */
 string SourceProcessor::cleanup_whitespace(const string &str)
 {
   /* Remove trailing white space as they make the subsequent regex much slower. */
-  regex regex(R"((\ )*?\n)");
-  return regex_replace(str, regex, "\n");
+  Parser parser(str, report_error_, ParserStage::MergeTokens);
+  cleanup_whitespace(parser);
+  return parser.result_get();
 }
 
 /* Parse defines in order to output them with the create infos.
@@ -1404,13 +1402,41 @@ string SourceProcessor::matrix_constructor_mutation(const string &str)
   if (str.find("mat") == string::npos) {
     return str;
   }
-  /* Example: `mat2(x)` > `mat2x2(x)` */
-  regex regex_parenthesis(R"(\bmat([234])\()");
-  string out = regex_replace(str, regex_parenthesis, "mat$1x$1(");
-  /* Only process square matrices since this is the only types we overload the constructors. */
-  /* Example: `mat2x2(x)` > `__mat2x2(x)` */
-  regex regex(R"(\bmat(2x2|3x3|4x4)\()");
-  return regex_replace(out, regex, "__mat$1(");
+
+  Parser parser(str, report_error_, ParserStage::MergeTokens);
+  parser().foreach_token(ParOpen, [&](const Token t) {
+    if (t.prev() == Word) {
+      Token fn_name = t.prev();
+      string_view fn_name_str = fn_name.str_view();
+      bool is_mat = false;
+      if (fn_name_str.size() == 4) {
+        /* Example: `mat2(x)` > `mat2x2(x)` */
+        if (fn_name_str == "mat2") {
+          parser.replace(fn_name, "mat2x2", true);
+          is_mat = true;
+        }
+        if (fn_name_str == "mat3") {
+          parser.replace(fn_name, "mat3x3", true);
+          is_mat = true;
+        }
+        if (fn_name_str == "mat4") {
+          parser.replace(fn_name, "mat4x4", true);
+          is_mat = true;
+        }
+      }
+      else if (fn_name_str.size() == 6) {
+        if (fn_name_str == "mat2x2" || fn_name_str == "mat3x3" || fn_name_str == "mat4x4") {
+          is_mat = true;
+        }
+      }
+      /* Only process square matrices since this is the only types we overload the constructors. */
+      /* Example: `mat2x2(x)` > `__mat2x2(x)` */
+      if (is_mat) {
+        parser.insert_before(fn_name, "__");
+      }
+    }
+  });
+  return parser.result_get();
 }
 
 /* To be run before `argument_decorator_macro_injection()`. */
@@ -1561,14 +1587,35 @@ void SourceProcessor::lower_argument_qualifiers(Parser &parser)
 
 string SourceProcessor::argument_decorator_macro_injection(const string &str)
 {
-  regex regex(R"((out|inout|in|shared)\s+(\w+)\s+(\w+))");
-  return regex_replace(str, regex, "$1 $2 _$1_sta $3 _$1_end");
+  Parser parser(str, report_error_, ParserStage::MergeTokens);
+  /* Example: `out float foo` > `out float _out_sta foo _out_end` */
+  parser().foreach_match("www", [&](const Tokens &t) {
+    string_view qualifier = t[0].str_view();
+    if (qualifier == "out" || qualifier == "inout" || qualifier == "in" || qualifier == "shared") {
+      parser.insert_after(t[1], " _" + string(qualifier) + "_sta");
+      parser.insert_after(t[2], " _" + string(qualifier) + "_end");
+    }
+  });
+  return parser.result_get();
 }
 
 string SourceProcessor::array_constructor_macro_injection(const string &str)
 {
-  regex regex(R"(=\s*(\w+)\s*\[[^\]]*\]\s*\()");
-  return regex_replace(str, regex, "= ARRAY_T($1) ARRAY_V(");
+  Parser parser(str, report_error_, ParserStage::MergeTokens);
+  parser().foreach_match("=w[", [&](const Tokens toks) {
+    Token array_len_start = toks.back();
+    Token array_len_end = array_len_start.find_next(SquareClose);
+    if (array_len_end.is_valid()) {
+      Token type = toks[1];
+      Token array_start = array_len_end.next();
+      if (array_start == '(') {
+        parser.insert_before(type, " ARRAY_T(");
+        parser.replace(array_len_start, array_len_end, ") ");
+        parser.insert_before(array_start, "ARRAY_V");
+      }
+    }
+  });
+  return parser.result_get();
 }
 
 /* Assume formatted source with our code style. Cannot be applied to python shaders. */

source/blender/gpu/shader_tool/intermediate.cc
@@ -95,7 +95,7 @@ struct TokenData {
   std::vector<uint32_t> sizes;
 };
 
-void TokenStream::lexical_analysis()
+void TokenStream::lexical_analysis(ParserStage stop_after)
 {
   if (str.empty()) {
     *this = {};
@@ -105,8 +105,19 @@ void TokenStream::lexical_analysis()
   TokenData data;
 
   tokenize(data);
+  if (stop_after == Tokenize) {
+    goto end;
+  }
+
   merge_tokens(data);
+  if (stop_after == MergeTokens) {
+    goto end;
+  }
+
   identify_keywords(data);
+
+end:
+  /* TODO(fclem): Get rid of this.*/
   /* Convert vector of char to string for faster lookups. */
   this->token_types = std::string(reinterpret_cast<char *>(data.types.data()), data.types.size());
   this->token_offsets = std::move(data.offsets);
@@ -627,9 +638,15 @@ void TokenStream::identify_keywords(TokenData &tokens)
   }
 }
 
-void TokenStream::semantic_analysis(report_callback &report_error)
+void TokenStream::semantic_analysis(ParserStage stop_after, report_callback &report_error)
 {
-  build_scope_tree(report_error);
+  if (stop_after == BuildScopeTree) {
+    build_scope_tree(report_error);
+  }
+  else {
+    this->scope_types = "G";
+    this->scope_ranges = {IndexRange(0, token_types.size())};
+  }
   build_token_to_scope_map();
 }
 
@@ -1017,16 +1034,16 @@ bool IntermediateForm::only_apply_mutations()
   return true;
 }
 
-void IntermediateForm::parse(report_callback &report_error)
+void IntermediateForm::parse(ParserStage stop_after, report_callback &report_error)
 {
   TimeIt::Duration lex_time, sem_time;
   {
     TimeIt time_it(lex_time);
-    data_.lexical_analysis();
+    data_.lexical_analysis(stop_after);
   }
   {
     TimeIt time_it(sem_time);
-    data_.semantic_analysis(report_error);
+    data_.semantic_analysis(stop_after, report_error);
   }
   lexical_time = lex_time.count();
   semantic_time = sem_time.count();

source/blender/gpu/shader_tool/intermediate.hh
@@ -70,12 +70,16 @@ struct IntermediateForm {
 
   report_callback &report_error;
 
+  ParserStage stop_parser_after_stage;
+
  public:
-  IntermediateForm(const std::string &input, report_callback &report_error)
-      : report_error(report_error)
+  IntermediateForm(const std::string &input,
+                   report_callback &report_error,
+                   ParserStage stop_parser_after_stage = ParserStage::BuildScopeTree)
+      : report_error(report_error), stop_parser_after_stage(stop_parser_after_stage)
   {
     data_.str = input;
-    parse(report_error);
+    parse(stop_parser_after_stage, report_error);
   }
 
   /* Main access operator. Returns the root scope (aka global scope). */
@@ -273,7 +277,7 @@ struct IntermediateForm {
   {
     bool applied = only_apply_mutations();
     if (applied) {
-      this->parse(report_error);
+      this->parse(stop_parser_after_stage, report_error);
     }
     return applied;
   }
@@ -319,7 +323,7 @@ struct IntermediateForm {
   uint64_t lexical_time;
   uint64_t semantic_time;
 
-  void parse(report_callback &report_error);
+  void parse(ParserStage stop_after, report_callback &report_error);
 
  public:
   void print_stats()

source/blender/gpu/shader_tool/token_stream.hh
@@ -13,6 +13,15 @@
 
 namespace blender::gpu::shader::parser {
 
+/* Used to select at which stage to stop.
+ *  */
+enum ParserStage {
+  Tokenize,
+  MergeTokens,
+  IdentifyKeywords,
+  BuildScopeTree,
+};
+
 /**
  * This a tiny bit more than a token stream as it contains ranges or tokens (called scopes) from
  * syntax. The scopes and tokens have bi-directional mapping.
@@ -32,9 +41,9 @@ struct TokenStream {
   /** Range of token per scope. */
   std::vector<IndexRange> scope_ranges;
 
-  void lexical_analysis();
+  void lexical_analysis(ParserStage stop_after);
 
-  void semantic_analysis(report_callback &report_error);
+  void semantic_analysis(ParserStage stop_after, report_callback &report_error);
 
  private:
   /* Create tokens based on character stream. */

commit 458a0519c42acdf03ba7b18b9f3c50e0bb9a726a

Fix: Crash with ACEScg working space after recent shader tool change
fn_name was invalid after the first string replacement, so do it in one go.

Issue introduced in 952c198eb73.

Pull Request: https://projects.blender.org/blender/blender/pulls/152483

source/blender/gpu/shader_tool/processor.cc
@@ -1408,31 +1408,25 @@ string SourceProcessor::matrix_constructor_mutation(const string &str)
     if (t.prev() == Word) {
       Token fn_name = t.prev();
       string_view fn_name_str = fn_name.str_view();
-      bool is_mat = false;
       if (fn_name_str.size() == 4) {
-        /* Example: `mat2(x)` > `mat2x2(x)` */
+        /* Example: `mat2(x)` > `__mat2x2(x)` */
         if (fn_name_str == "mat2") {
-          parser.replace(fn_name, "mat2x2", true);
-          is_mat = true;
+          parser.replace(fn_name, "__mat2x2", true);
         }
-        if (fn_name_str == "mat3") {
-          parser.replace(fn_name, "mat3x3", true);
-          is_mat = true;
+        else if (fn_name_str == "mat3") {
+          parser.replace(fn_name, "__mat3x3", true);
         }
-        if (fn_name_str == "mat4") {
-          parser.replace(fn_name, "mat4x4", true);
-          is_mat = true;
+        else if (fn_name_str == "mat4") {
+          parser.replace(fn_name, "__mat4x4", true);
         }
       }
       else if (fn_name_str.size() == 6) {
         if (fn_name_str == "mat2x2" || fn_name_str == "mat3x3" || fn_name_str == "mat4x4") {
-          is_mat = true;
+          /* Only process square matrices since this is the only types we overload the
+           * constructors. */
+          /* Example: `mat2x2(x)` > `__mat2x2(x)` */
+          parser.insert_before(fn_name, "__");
         }
-      }
-      /* Only process square matrices since this is the only types we overload the constructors. */
-      /* Example: `mat2x2(x)` > `__mat2x2(x)` */
-      if (is_mat) {
-        parser.insert_before(fn_name, "__");
       }
     }
   });