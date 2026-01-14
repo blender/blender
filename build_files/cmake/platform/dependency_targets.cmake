e46f4c1a7eede0196481a13905a42db01a132e30
Brecht Van Lommel <brecht@blender.org>
2026-01-14 17:45:17 +0100

    Refactor: Use more modern bf::dependencies and bf::extern in CMake
    Replacing various include directories, defines and libraries specified
    directly.
    
    Additionally, this makes it so bf::dependencies are automatically added
    last to the target. This ensures include directories from bf::extern libraries
    have priority over them, so that e.g. our own fmtlib will be used rather than
    one that might be installed on the system.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/151920

--- build_files/cmake/platform/dependency_targets.cmake
+++ build_files/cmake/platform/dependency_targets.cmake
@@ -23,6 +23,9 @@ if(WITH_TBB)
   target_link_libraries(bf_deps_optional_tbb INTERFACE ${TBB_LIBRARIES})
 endif()
 
+# -----------------------------------------------------------------------------
+# Configure Manifold
+
 add_library(bf_deps_optional_manifold INTERFACE)
 add_library(bf::dependencies::optional::manifold ALIAS bf_deps_optional_manifold)
 if(WITH_MANIFOLD)
@@ -61,7 +64,6 @@ if(WITH_OPENVDB)
   )
 endif()
 
-
 # -----------------------------------------------------------------------------
 # Configure Eigen
 
@@ -86,4 +88,350 @@ if(WITH_OPENCOLORIO)
   target_compile_definitions(bf_deps_optional_opencolorio INTERFACE WITH_OPENCOLORIO)
   target_include_directories(bf_deps_optional_opencolorio SYSTEM INTERFACE ${OPENCOLORIO_INCLUDE_DIRS})
   target_link_libraries(bf_deps_optional_opencolorio INTERFACE ${OPENCOLORIO_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Zlib
+
+add_library(bf_deps_zlib INTERFACE)
+add_library(bf::dependencies::zlib ALIAS bf_deps_zlib)
+
+target_include_directories(bf_deps_zlib SYSTEM INTERFACE ${ZLIB_INCLUDE_DIRS})
+target_link_libraries(bf_deps_zlib INTERFACE ${ZLIB_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure ZSTD
+
+add_library(bf_deps_zstd INTERFACE)
+add_library(bf::dependencies::zstd ALIAS bf_deps_zstd)
+
+target_include_directories(bf_deps_zstd SYSTEM INTERFACE ${ZSTD_INCLUDE_DIRS})
+target_link_libraries(bf_deps_zstd INTERFACE ${ZSTD_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure Freetype
+
+add_library(bf_deps_freetype INTERFACE)
+add_library(bf::dependencies::freetype ALIAS bf_deps_freetype)
+
+target_include_directories(bf_deps_freetype SYSTEM INTERFACE ${FREETYPE_INCLUDE_DIRS})
+target_link_libraries(bf_deps_freetype INTERFACE ${FREETYPE_LIBRARIES} ${BROTLI_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure JPEG
+
+add_library(bf_deps_jpeg INTERFACE)
+add_library(bf::dependencies::jpeg ALIAS bf_deps_jpeg)
+
+target_include_directories(bf_deps_jpeg SYSTEM INTERFACE ${JPEG_INCLUDE_DIR})
+target_link_libraries(bf_deps_jpeg INTERFACE ${JPEG_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure PNG
+
+add_library(bf_deps_png INTERFACE)
+add_library(bf::dependencies::png ALIAS bf_deps_png)
+
+target_include_directories(bf_deps_png SYSTEM INTERFACE ${PNG_INCLUDE_DIRS})
+target_link_libraries(bf_deps_png INTERFACE ${PNG_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure OpenImageIO
+
+add_library(bf_deps_openimageio INTERFACE)
+add_library(bf::dependencies::openimageio ALIAS bf_deps_openimageio)
+
+target_include_directories(bf_deps_openimageio SYSTEM INTERFACE ${OPENIMAGEIO_INCLUDE_DIRS})
+target_link_libraries(bf_deps_openimageio INTERFACE ${OPENIMAGEIO_LIBRARIES})
+
+# OpenImageIO headers include `Imath` headers when there is no SSE support for
+# matrix operations. This depends on the specific architecture and compiler
+# flags, most reliable is to always include the `Imath` headers if we have them.
+if(DEFINED IMATH_INCLUDE_DIRS)
+  target_include_directories(bf_deps_openimageio SYSTEM INTERFACE ${IMATH_INCLUDE_DIRS})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure USD
+
+add_library(bf_deps_optional_usd INTERFACE)
+add_library(bf::dependencies::optional::usd ALIAS bf_deps_optional_usd)
+
+if(WITH_USD)
+  target_compile_definitions(bf_deps_optional_usd INTERFACE WITH_USD)
+  target_include_directories(bf_deps_optional_usd SYSTEM INTERFACE ${BOOST_INCLUDE_DIR} ${USD_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_usd INTERFACE ${USD_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Alembic
+
+add_library(bf_deps_optional_alembic INTERFACE)
+add_library(bf::dependencies::optional::alembic ALIAS bf_deps_optional_alembic)
+
+if(WITH_ALEMBIC)
+  target_compile_definitions(bf_deps_optional_alembic INTERFACE WITH_ALEMBIC)
+  target_include_directories(bf_deps_optional_alembic SYSTEM INTERFACE ${ALEMBIC_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_alembic INTERFACE ${ALEMBIC_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure OpenSubdiv
+
+add_library(bf_deps_optional_opensubdiv INTERFACE)
+add_library(bf::dependencies::optional::opensubdiv ALIAS bf_deps_optional_opensubdiv)
+
+if(WITH_OPENSUBDIV)
+  target_compile_definitions(bf_deps_optional_opensubdiv INTERFACE WITH_OPENSUBDIV)
+  target_include_directories(bf_deps_optional_opensubdiv SYSTEM INTERFACE ${OPENSUBDIV_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_opensubdiv INTERFACE ${OPENSUBDIV_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure OpenEXR
+
+add_library(bf_deps_optional_openexr INTERFACE)
+add_library(bf::dependencies::optional::openexr ALIAS bf_deps_optional_openexr)
+
+if(WITH_IMAGE_OPENEXR)
+  target_compile_definitions(bf_deps_optional_openexr INTERFACE WITH_IMAGE_OPENEXR)
+  target_include_directories(bf_deps_optional_openexr SYSTEM INTERFACE ${OPENEXR_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_openexr INTERFACE ${OPENEXR_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure WebP
+
+add_library(bf_deps_optional_webp INTERFACE)
+add_library(bf::dependencies::optional::webp ALIAS bf_deps_optional_webp)
+
+if(WITH_IMAGE_WEBP)
+  target_compile_definitions(bf_deps_optional_webp INTERFACE WITH_IMAGE_WEBP)
+  target_include_directories(bf_deps_optional_webp SYSTEM INTERFACE ${WEBP_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_webp INTERFACE ${WEBP_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure OpenJPEG
+
+add_library(bf_deps_optional_openjpeg INTERFACE)
+add_library(bf::dependencies::optional::openjpeg ALIAS bf_deps_optional_openjpeg)
+
+if(WITH_IMAGE_OPENJPEG)
+  target_compile_definitions(bf_deps_optional_openjpeg INTERFACE WITH_IMAGE_OPENJPEG)
+  target_compile_definitions(bf_deps_optional_openjpeg INTERFACE ${OPENJPEG_DEFINES})
+  target_include_directories(bf_deps_optional_openjpeg SYSTEM INTERFACE ${OPENJPEG_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_openjpeg INTERFACE ${OPENJPEG_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure SDL
+
+add_library(bf_deps_optional_sdl INTERFACE)
+add_library(bf::dependencies::optional::sdl ALIAS bf_deps_optional_sdl)
+
+if(WITH_SDL)
+  target_compile_definitions(bf_deps_optional_sdl INTERFACE WITH_SDL)
+  target_include_directories(bf_deps_optional_sdl SYSTEM INTERFACE ${SDL_INCLUDE_DIR})
+  target_link_libraries(bf_deps_optional_sdl INTERFACE ${SDL_LIBRARY})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure FFmpeg
+
+add_library(bf_deps_optional_ffmpeg INTERFACE)
+add_library(bf::dependencies::optional::ffmpeg ALIAS bf_deps_optional_ffmpeg)
+
+if(WITH_CODEC_FFMPEG)
+  target_compile_definitions(bf_deps_optional_ffmpeg INTERFACE WITH_FFMPEG)
+  target_include_directories(bf_deps_optional_ffmpeg SYSTEM INTERFACE ${FFMPEG_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_ffmpeg INTERFACE ${FFMPEG_LIBRARIES})
+  if(WITH_IMAGE_OPENJPEG)
+    target_link_libraries(bf_deps_optional_ffmpeg INTERFACE ${OPENJPEG_LIBRARIES})
+  endif()
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Python
+
+add_library(bf_deps_optional_python INTERFACE)
+add_library(bf::dependencies::optional::python ALIAS bf_deps_optional_python)
+
+if(WITH_PYTHON)
+  target_compile_definitions(bf_deps_optional_python INTERFACE WITH_PYTHON)
+  if(WITH_PYTHON_MODULE)
+    target_compile_definitions(bf_deps_optional_python INTERFACE WITH_PYTHON_MODULE)
+  endif()
+  target_include_directories(bf_deps_optional_python SYSTEM INTERFACE ${PYTHON_INCLUDE_DIR})
+  target_link_libraries(bf_deps_optional_python INTERFACE ${PYTHON_LINKFLAGS} ${PYTHON_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure GMP
+
+add_library(bf_deps_optional_gmp INTERFACE)
+add_library(bf::dependencies::optional::gmp ALIAS bf_deps_optional_gmp)
+
+if(WITH_GMP)
+  target_compile_definitions(bf_deps_optional_gmp INTERFACE WITH_GMP)
+  target_include_directories(bf_deps_optional_gmp SYSTEM INTERFACE ${GMP_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_gmp INTERFACE ${GMP_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure PugiXML
+
+add_library(bf_deps_optional_pugixml INTERFACE)
+add_library(bf::dependencies::optional::pugixml ALIAS bf_deps_optional_pugixml)
+
+if(WITH_PUGIXML)
+  target_compile_definitions(bf_deps_optional_pugixml INTERFACE WITH_PUGIXML)
+  target_include_directories(bf_deps_optional_pugixml SYSTEM INTERFACE ${PUGIXML_INCLUDE_DIR})
+  target_link_libraries(bf_deps_optional_pugixml INTERFACE ${PUGIXML_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Haru
+
+add_library(bf_deps_optional_haru INTERFACE)
+add_library(bf::dependencies::optional::haru ALIAS bf_deps_optional_haru)
+
+if(WITH_HARU)
+  target_compile_definitions(bf_deps_optional_haru INTERFACE WITH_HARU)
+  target_include_directories(bf_deps_optional_haru SYSTEM INTERFACE ${HARU_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_haru INTERFACE ${HARU_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Vulkan
+
+add_library(bf_deps_optional_vulkan INTERFACE)
+add_library(bf::dependencies::optional::vulkan ALIAS bf_deps_optional_vulkan)
+
+if(WITH_VULKAN_BACKEND)
+  target_include_directories(bf_deps_optional_vulkan SYSTEM INTERFACE ${VULKAN_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_vulkan INTERFACE ${VULKAN_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure ShaderC
+
+add_library(bf_deps_optional_shaderc INTERFACE)
+add_library(bf::dependencies::optional::shaderc ALIAS bf_deps_optional_shaderc)
+
+if(WITH_VULKAN_BACKEND)
+  target_compile_definitions(bf_deps_optional_shaderc INTERFACE WITH_SHADERC)
+  target_include_directories(bf_deps_optional_shaderc SYSTEM INTERFACE ${SHADERC_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_shaderc INTERFACE ${SHADERC_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Epoxy
+
+add_library(bf_deps_epoxy INTERFACE)
+add_library(bf::dependencies::epoxy ALIAS bf_deps_epoxy)
+
+target_include_directories(bf_deps_epoxy SYSTEM INTERFACE ${Epoxy_INCLUDE_DIRS})
+target_link_libraries(bf_deps_epoxy INTERFACE ${Epoxy_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure Gflags
+
+add_library(bf_deps_gflags INTERFACE)
+add_library(bf::dependencies::gflags ALIAS bf_deps_gflags)
+
+target_compile_definitions(bf_deps_gflags INTERFACE ${GFLAGS_DEFINES})
+target_include_directories(bf_deps_gflags SYSTEM INTERFACE ${GFLAGS_INCLUDE_DIRS})
+target_link_libraries(bf_deps_gflags INTERFACE ${GFLAGS_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure Glog
+
+add_library(bf_deps_glog INTERFACE)
+add_library(bf::dependencies::glog ALIAS bf_deps_glog)
+
+target_compile_definitions(bf_deps_glog INTERFACE ${GLOG_DEFINES})
+target_include_directories(bf_deps_glog SYSTEM INTERFACE ${GLOG_INCLUDE_DIRS})
+target_link_libraries(bf_deps_glog INTERFACE ${GLOG_LIBRARIES})
+
+# -----------------------------------------------------------------------------
+# Configure OpenImageDenoise
+
+add_library(bf_deps_optional_openimagedenoise INTERFACE)
+add_library(bf::dependencies::optional::openimagedenoise ALIAS bf_deps_optional_openimagedenoise)
+
+if(WITH_OPENIMAGEDENOISE)
+  target_compile_definitions(bf_deps_optional_openimagedenoise INTERFACE WITH_OPENIMAGEDENOISE)
+  target_compile_definitions(bf_deps_optional_openimagedenoise INTERFACE OIDN_STATIC_LIB)
+  target_include_directories(bf_deps_optional_openimagedenoise SYSTEM INTERFACE ${OPENIMAGEDENOISE_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_openimagedenoise INTERFACE ${OPENIMAGEDENOISE_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure FFTW3
+
+add_library(bf_deps_optional_fftw3 INTERFACE)
+add_library(bf::dependencies::optional::fftw3 ALIAS bf_deps_optional_fftw3)
+
+if(WITH_FFTW3)
+  target_compile_definitions(bf_deps_optional_fftw3 INTERFACE WITH_FFTW3)
+  target_include_directories(bf_deps_optional_fftw3 SYSTEM INTERFACE ${FFTW3_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_fftw3 INTERFACE ${FFTW3_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Bullet
+
+add_library(bf_deps_optional_bullet INTERFACE)
+add_library(bf::dependencies::optional::bullet ALIAS bf_deps_optional_bullet)
+
+if(WITH_BULLET)
+  target_compile_definitions(bf_deps_optional_bullet INTERFACE WITH_BULLET)
+  target_include_directories(bf_deps_optional_bullet SYSTEM INTERFACE ${BULLET_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_bullet INTERFACE ${BULLET_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Audaspace
+
+add_library(bf_deps_optional_audaspace INTERFACE)
+add_library(bf::dependencies::optional::audaspace ALIAS bf_deps_optional_audaspace)
+
+if(WITH_AUDASPACE)
+  target_compile_definitions(bf_deps_optional_audaspace INTERFACE WITH_AUDASPACE)
+  if(WITH_RUBBERBAND)
+    target_compile_definitions(bf_deps_optional_audaspace INTERFACE WITH_RUBBERBAND)
+  endif()
+  target_include_directories(bf_deps_optional_audaspace SYSTEM INTERFACE ${AUDASPACE_C_INCLUDE_DIRS} ${AUDASPACE_PY_INCLUDE_DIRS})
+  if(WITH_SYSTEM_AUDASPACE)
+    target_link_libraries(bf_deps_optional_audaspace INTERFACE ${AUDASPACE_C_LIBRARIES} ${AUDASPACE_PY_LIBRARIES})
+  else()
+    target_link_libraries(bf_deps_optional_audaspace INTERFACE audaspace audaspace-py)
+  endif()
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Potrace
+
+add_library(bf_deps_optional_potrace INTERFACE)
+add_library(bf::dependencies::optional::potrace ALIAS bf_deps_optional_potrace)
+
+if(WITH_POTRACE)
+  target_compile_definitions(bf_deps_optional_potrace INTERFACE WITH_POTRACE)
+  target_include_directories(bf_deps_optional_potrace SYSTEM INTERFACE ${POTRACE_INCLUDE_DIRS})
+  target_link_libraries(bf_deps_optional_potrace INTERFACE ${POTRACE_LIBRARIES})
+endif()
+
+# -----------------------------------------------------------------------------
+# Configure Pthreads
+#
+add_library(bf_deps_pthreads INTERFACE)
+add_library(bf::dependencies::pthreads ALIAS bf_deps_pthreads)
+
+# Include directories on non-Windows are handled globally.
+if(WIN32 AND NOT UNIX)
+  target_include_directories(bf_deps_pthreads SYSTEM INTERFACE ${PTHREADS_INC})
+endif()
+if(DEFINED PTHREADS_LIBRARIES)
+  target_link_libraries(bf_deps_pthreads INTERFACE ${PTHREADS_LIBRARIES})
 endif()
