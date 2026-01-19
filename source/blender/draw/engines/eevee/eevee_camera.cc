blender/blender

commit 64038c44ac96f6bd04140fc5f3e3550b7af5b40a

Fix #151586: EEVEE: Wrong projection when using Render Region + Fly/Walk Navigation
The projection matrix generated by the navigation operators doesn't
take the render region into account.

Offset and scale the projection matrix so the NDC only covers the
render region.
This doesn't take into account overscan, since it's disabled when using
Render Region.

Pull Request: https://projects.blender.org/blender/blender/pulls/151923

source/blender/draw/engines/eevee/eevee_camera.cc
@@ -7,6 +7,7 @@
  */
 
 #include "BLI_bounds.hh"
+#include "BLI_math_matrix.hh"
 #include "BLI_rect.h"
 
 #include "DRW_render.hh"
@@ -156,7 +157,12 @@ void Camera::sync()
     else {
       /* Can happen for the case of XR or if `rv3d->dist == 0`.
        * In this case the produced winmat is degenerate. So just revert to the input matrix. */
+      float2 film_center = float2(film_offset) + float2(film_extent) / 2.0f;
+      float2 uv_offset = float2(0.5f) - (film_center / float2(display_extent));
       data.winmat = inst_.drw_view->winmat();
+      data.winmat = math::projection::translate(data.winmat, uv_offset * 2.0f);
+      data.winmat = math::from_scale<float4x4>(float4(1.0f / data.uv_scale, 1.0f, 1.0f)) *
+                    data.winmat;
     }
   }
   else if (inst_.render) {

commit 3bab252ba77f90fe6a86e09c85b3627d047f36e1

Fix #153033: Walk navigation causes EEVEE camera view to render incorrectly
Don't modify camera matrices.
Regression from 64038c44ac.

This just reverts to the previous behavior for camera views.
Render Region + Camera View + Fly/Walk Navigation is still broken,
but that was also the case before 64038c44ac.

Pull Request: https://projects.blender.org/blender/blender/pulls/153039

source/blender/draw/engines/eevee/eevee_camera.cc
@@ -157,12 +157,17 @@ void Camera::sync()
     else {
       /* Can happen for the case of XR or if `rv3d->dist == 0`.
        * In this case the produced winmat is degenerate. So just revert to the input matrix. */
-      float2 film_center = float2(film_offset) + float2(film_extent) / 2.0f;
-      float2 uv_offset = float2(0.5f) - (film_center / float2(display_extent));
       data.winmat = inst_.drw_view->winmat();
-      data.winmat = math::projection::translate(data.winmat, uv_offset * 2.0f);
-      data.winmat = math::from_scale<float4x4>(float4(1.0f / data.uv_scale, 1.0f, 1.0f)) *
-                    data.winmat;
+      if (!camera_eval) {
+        /* Apply the render region, but only for non-camera views. See #153033. */
+        /* FIXME(@pragma37): This is still broken with Camera View + Render Region + Fly/Walk
+         * Navigation. Untangle this whole walk/fly navigation projection matrix mess. */
+        float2 film_center = float2(film_offset) + float2(film_extent) / 2.0f;
+        float2 uv_offset = float2(0.5f) - (film_center / float2(display_extent));
+        data.winmat = math::projection::translate(data.winmat, uv_offset * 2.0f);
+        data.winmat = math::from_scale<float4x4>(float4(1.0f / data.uv_scale, 1.0f, 1.0f)) *
+                      data.winmat;
+      }
     }
   }
   else if (inst_.render) {