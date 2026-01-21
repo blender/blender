58ec3d6d1ff2ecc7274ad22abc945380f687b6b6
Philipp Oeser <philipp@blender.org>
2026-01-21 07:18:34 +0100

    Fix: Paint vertex loop selection not working with RMB select
    Implemented in d0be4a8da5f0
    
    There was already a note about this, so add to that keymap as well.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/153080

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -5202,8 +5202,9 @@ def km_paint_vertex_mask(params):
         ("paint.vert_select_less", {"type": 'NUMPAD_MINUS', "value": 'PRESS', "ctrl": True}, None),
     ])
 
-    # TODO: use `_template_view3d_paint_mask_select_loop` if loop-select is supported.
-    # See: `km_paint_face_mask`.
+    # For left mouse the tool key-maps are used because this interferes with Alt-LMB for regular selection.
+    if params.select_mouse == 'RIGHTMOUSE':
+        items.extend(_template_view3d_paint_mask_select_loop(params))
 
     return keymap
 
