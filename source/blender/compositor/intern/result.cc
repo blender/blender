c5c06c4ea861c4a2dbaf768912562fbd6d6e9aac
Omar Emara <mail@OmarEmara.dev>
2026-01-26 13:29:55 +0100

    Refactor: Compositor: Remove strict assert
    The steal data method of the compositor result needs both results to
    have the same precision, which seems too strict for real world cases. So
    we should remove it.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/153407

--- source/blender/compositor/intern/result.cc
+++ source/blender/compositor/intern/result.cc
@@ -533,7 +533,6 @@ void Result::share_data(const Result &source)
 void Result::steal_data(Result &source)
 {
   BLI_assert(type_ == source.type_);
-  BLI_assert(precision_ == source.precision_);
   BLI_assert(!this->is_allocated() && source.is_allocated());
 
   /* Overwrite everything except reference counts. */
