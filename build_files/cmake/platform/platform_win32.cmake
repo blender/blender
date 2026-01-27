733176db8bb902344308ef257ae3c1a40fbd3e5d
Harley Acheson <harley.acheson@gmail.com>
2026-01-27 16:25:25 +0100

    UI: Icon for Cloud Storage Drive on Windows Platform
    The Windows platform allows mapping drive letters to remote shares that
    are a uniform resource locator rather than network path. This is mostly
    as test to see if this can be properly detected. If so it will be shown
    with an "internet" icon in the File Browser volumes list.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/151155

--- build_files/cmake/platform/platform_win32.cmake
+++ build_files/cmake/platform/platform_win32.cmake
@@ -107,7 +107,7 @@ endif()
 list(APPEND PLATFORM_LINKLIBS
   ws2_32 vfw32 winmm kernel32 user32 gdi32 comdlg32 Comctl32 version
   advapi32 shfolder shell32 ole32 oleaut32 uuid psapi Dbghelp Shlwapi
-  pathcch Shcore Dwmapi Crypt32 Bcrypt
+  pathcch Shcore Dwmapi Crypt32 Bcrypt Mpr
 )
 
 if(WITH_INPUT_IME)
