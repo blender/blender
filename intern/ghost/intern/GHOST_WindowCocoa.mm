2967432ba6cf7f37fe74628e6550b8e7ba5eafcc
Jonas Holzman <jonas@blender.org>
2026-01-29 14:26:54 +0100

    Fix #152585: macOS: Nonfunctional --no-native-pixels flag
    This commit re-implements the (previously broken because of deprecation)
    `--no-native-pixels` flag on macOS, allowing the window framebuffer
    content to be rendered using the virtual OS resolution instead of the
    "native" backing store macOS resolution.
    
    Changes:
    - Add a new GHOST_WindowCocoa `updateDrawingSize()` method, computing
      the window's view Metal layer `contentScale` and `drawableSize`
      property depending on the nativePixel flag. This method is then called
      in conjuction with the GHOST_Window (macOS only)
      `updateDrawingContext()` method on window creation and resize.
    - Adapt the GHOST_ContextMTL updateFramebuffer function to use the
      computed `drawableSize` when drawing the window framebuffer. Porting
      its backingSize conversion logic to the `updateDrawingSize` function.
    - Adapt the `setNativePixelSize` method to compute the native pixel
      size UI value depending on the set Metal layer `drawableSize`. This
      also removes a (now fully unused) OpenGL view ternary switch. This
      deprecated OpenGL view (and related deprecated OpenGL code) will be
      fully removed in a future refactor.
    
    For a bit of history, this flag was originally introduced *for* macOS
    when native pixel support was first implemented within Blender with the
    advent of Retina displays. As time went on, usage of the backing "high
    resolution" store buffer became the default, and the now deprecated
    OpenGL specific view property which used to be set to enable high
    resolution drawing became obsolete.
    
    Reference:
    - Apple High Resolution Guideline Docs: https://developer.apple.com/library/archive/documentation/GraphicsAnimation/Conceptual/HighResolutionOSX/Explained/Explained.html
    - Equivalent SDL implementation: https://github.com/libsdl-org/SDL/blob/main/src/video/cocoa/SDL_cocoametalview.m#L113
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/153487

--- intern/ghost/intern/GHOST_WindowCocoa.mm
+++ intern/ghost/intern/GHOST_WindowCocoa.mm
@@ -425,18 +425,12 @@ GHOST_WindowCocoa::GHOST_WindowCocoa(GHOST_SystemCocoa *systemCocoa,
       view = opengl_view_;
     }
 
-    if (system_cocoa_->native_pixel_) {
-      /* Needs to happen early when building with the 10.14 SDK, otherwise
-       * has no effect until resizing the window. */
-      if ([view respondsToSelector:@selector(setWantsBestResolutionOpenGLSurface:)]) {
-        view.wantsBestResolutionOpenGLSurface = YES;
-      }
-    }
-
     window_.contentView = view;
     window_.initialFirstResponder = view;
 
     [window_ makeKeyAndOrderFront:nil];
+
+    updateDrawingSize();
 
     setDrawingContextType(type);
     updateDrawingContext();
@@ -802,13 +796,11 @@ NSScreen *GHOST_WindowCocoa::getPrimaryScreen()
 /* called for event, when window leaves monitor to another */
 void GHOST_WindowCocoa::setNativePixelSize()
 {
-  NSView *view = (opengl_view_) ? opengl_view_ : metal_view_;
-  const NSRect backingBounds = [view convertRectToBacking:[view bounds]];
-
   GHOST_Rect rect;
   getClientBounds(rect);
 
-  native_pixel_size_ = float(backingBounds.size.width) / float(rect.getWidth());
+  CAMetalLayer *metalLayer = (CAMetalLayer *)metal_view_.layer;
+  native_pixel_size_ = float(metalLayer.drawableSize.width) / float(rect.getWidth());
 }
 
 /**
@@ -896,6 +888,21 @@ GHOST_TSuccess GHOST_WindowCocoa::setOrder(GHOST_TWindowOrder order)
 /* --------------------------------------------------------------------
  * Drawing context.
  */
+
+void GHOST_WindowCocoa::updateDrawingSize()
+{
+  NSSize viewSize = metal_view_.bounds.size;
+  NSSize backingSize = viewSize;
+
+  if (system_cocoa_->native_pixel_) {
+    backingSize = [metal_view_ convertSizeToBacking:viewSize];
+  }
+
+  CAMetalLayer *metalLayer = (CAMetalLayer *)metal_view_.layer;
+
+  metalLayer.contentsScale = backingSize.height / viewSize.height;
+  metalLayer.drawableSize = NSSizeToCGSize(backingSize);
+}
 
 GHOST_Context *GHOST_WindowCocoa::newDrawingContext(GHOST_TDrawingContextType type)
 {
