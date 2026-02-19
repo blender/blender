fcb71034fe292293ca0bd82c6237637e8502d333
Martijn Versteegh <baardaap@noreply.localhost>
2026-02-19 16:16:56 +0100

    Fix #154592: Add forgotten nullptr check
    Pull Request: https://projects.blender.org/blender/blender/pulls/154609

--- source/blender/editors/space_node/space_node.cc
+++ source/blender/editors/space_node/space_node.cc
@@ -1469,11 +1469,13 @@ static int /*eContextResult*/ node_context(const bContext *C,
     return CTX_RESULT_OK;
   }
   if (CTX_data_equals(member, "edit_image")) {
-    if (bNode *node = bke::node_get_active(*snode->edittree)) {
-      if (ELEM(node->type_legacy, SH_NODE_TEX_IMAGE, SH_NODE_TEX_ENVIRONMENT)) {
-        Image *image = id_cast<Image *>(node->id);
-        CTX_data_id_pointer_set(result, &image->id);
-        return CTX_RESULT_OK;
+    if (snode->edittree != nullptr) {
+      if (bNode *node = bke::node_get_active(*snode->edittree)) {
+        if (ELEM(node->type_legacy, SH_NODE_TEX_IMAGE, SH_NODE_TEX_ENVIRONMENT)) {
+          Image *image = id_cast<Image *>(node->id);
+          CTX_data_id_pointer_set(result, &image->id);
+          return CTX_RESULT_OK;
+        }
       }
     }
   }
