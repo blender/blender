47a01751e5f154af264e4beb863a6b9d83772fe4
Philipp Oeser <philipp@blender.org>
2026-02-27 12:45:42 +0100

    Fix: Grease Pencil: Missing versioning for edit mode wire color
    Grease Pencils edit mode wire color was introduced in e7cf9652b11.
    
    Would get all black, zero alpha otherwise, resulting in reports like
    #154918 :)
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/154943

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -427,6 +427,10 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(space_view3d.grid_major);
   }
 
+  if (!USER_VERSION_ATLEAST(501, 28)) {
+    FROM_DEFAULT_V4_UCHAR(space_view3d.gp_wire_edit);
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a USER_VERSION_ATLEAST check.
