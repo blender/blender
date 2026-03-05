d9c91af3b726497f1b3f2544d6ab083d89f000fe
Sean Kim <SeanCTKim@protonmail.com>
2026-03-05 23:09:57 +0100

    Cleanup: Add default values for `PaintCursorContext` struct
    Pull Request: https://projects.blender.org/blender/blender/pulls/155279

--- source/blender/editors/sculpt_paint/paint_cursor.hh
+++ source/blender/editors/sculpt_paint/paint_cursor.hh
@@ -27,64 +27,64 @@ enum class PaintCursorDrawingType {
 };
 
 struct PaintCursorContext {
-  ARegion *region;
-  wmWindow *win;
-  wmWindowManager *wm;
-  bScreen *screen;
-  Depsgraph *depsgraph;
-  Scene *scene;
-  Object *object;
-  Base *base;
-  UnifiedPaintSettings *ups;
-  Brush *brush;
-  Paint *paint;
-  PaintMode mode;
-  ViewContext vc;
+  ARegion *region = nullptr;
+  wmWindow *win = nullptr;
+  wmWindowManager *wm = nullptr;
+  bScreen *screen = nullptr;
+  Depsgraph *depsgraph = nullptr;
+  Scene *scene = nullptr;
+  Object *object = nullptr;
+  Base *base = nullptr;
+  UnifiedPaintSettings *ups = nullptr;
+  Brush *brush = nullptr;
+  Paint *paint = nullptr;
+  PaintMode mode = PaintMode::Invalid;
+  ViewContext vc = {};
 
   /* Sculpt related data. */
-  Sculpt *sd;
-  SculptSession *ss;
+  Sculpt *sd = nullptr;
+  SculptSession *ss = nullptr;
 
   /**
    * Previous active vertex index , used to determine if the preview is updated for the pose brush.
    */
-  int prev_active_vert_index;
+  int prev_active_vert_index = -1;
 
   /** Whether the current tool is a brush. We can have a `Brush *` without this being true */
-  bool is_brush_active;
-  bool is_stroke_active;
-  bool is_cursor_over_mesh;
-  float radius;
+  bool is_brush_active = false;
+  bool is_stroke_active = false;
+  bool is_cursor_over_mesh = false;
+  float radius = 0.0f;
 
   /* 3D view cursor position and normal. */
-  float3 location;
-  float3 scene_space_location;
-  float3 normal;
+  float3 location = float3(0.0f);
+  float3 scene_space_location = float3(0.0f);
+  float3 normal = float3(0.0f);
 
   /* Cursor main colors. */
-  float3 outline_col;
-  float outline_alpha;
+  float3 outline_col = float3(0.0f);
+  float outline_alpha = 0.0f;
 
   /* GPU attribute for drawing. */
-  uint pos;
+  uint pos = 0;
 
-  PaintCursorDrawingType cursor_type;
+  PaintCursorDrawingType cursor_type = PaintCursorDrawingType::Cursor2D;
 
   /* This variable is set after drawing the overlay, not on initialization. It can't be used for
    * checking if alpha overlay is enabled before drawing it. */
-  bool alpha_overlay_drawn;
+  bool alpha_overlay_drawn = false;
 
-  float zoomx;
+  float zoomx = 0.0f;
   /* Coordinates in region space */
-  int2 mval;
+  int2 mval = int2(0);
 
   /* TODO: Figure out why this and mval are used interchangeably */
-  float2 translation;
+  float2 translation = float2(0.0f);
 
-  float2 tilt;
+  float2 tilt = float2(0.0f);
 
-  float final_radius;
-  int pixel_radius;
+  float final_radius = 0.0f;
+  int pixel_radius = 0;
 };
 
 void grease_pencil_cursor_draw(PaintCursorContext &pcontext);
