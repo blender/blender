7edd570c52213d8ea823924bc264590b4f0039cf
Ray Molenkamp <github@lazydodo.com>
2026-03-10 16:36:59 +0100

    deps: Fix leaking rubberband exports
    Rubberband is leaking exports into the blender binary, it incorrectly
    puts a dllexport attribute on several methods.
    
    Some of these can be controlled with a define others you can't so I
    just obliterated all of them in a patch and called it a day.

--- build_files/build_environment/cmake/rubberband.cmake
+++ build_files/build_environment/cmake/rubberband.cmake
@@ -21,6 +21,10 @@ ExternalProject_Add(external_rubberband
   URL_HASH ${RUBBERBAND_HASH_TYPE}=${RUBBERBAND_HASH}
   PREFIX ${BUILD_DIR}/rubberband
 
+  PATCH_COMMAND ${PATCH_CMD} -p 1 
+    -d ${BUILD_DIR}/rubberband/src/external_rubberband 
+    -i ${PATCH_DIR}/rubberband_remove_dll_exports.diff
+
   CONFIGURE_COMMAND ${RUBBERBAND_CONFIGURE_ENV} &&
     ${CMAKE_COMMAND} -E env ${RUBBERBAND_PKG_ENV} ${MESON} setup
       --prefix ${LIBDIR}/rubberband
