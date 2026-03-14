blender/blender

commit ba9f8e97fbd34dcdbb7a049710bf111d08648329

IMB: Add span version of `IMB_blend_color_float`
This commit extracts the switch statement outside of the loop to avoid
the constant check when processing many elements.

Part of #155365

Pull Request: https://projects.blender.org/blender/blender/pulls/155602

source/blender/imbuf/IMB_imbuf.hh
@@ -239,6 +239,10 @@ void IMB_blend_color_float(float dst[4],
                            const float src1[4],
                            const float src2[4],
                            IMB_BlendMode mode);
+void IMB_blend_color_float(MutableSpan<float4> dst,
+                           Span<float4> src1,
+                           Span<float4> src2,
+                           IMB_BlendMode mode);
 
 /**
  * In-place image crop.

source/blender/imbuf/intern/rectop.cc
@@ -202,6 +202,143 @@ void IMB_blend_color_float(float dst[4],
   }
 }
 
+void IMB_blend_color_float(const MutableSpan<float4> dst,
+                           const Span<float4> src1,
+                           const Span<float4> src2,
+                           const IMB_BlendMode mode)
+{
+  BLI_assert(dst.size() == src1.size());
+  BLI_assert(dst.size() == src2.size());
+
+  switch (mode) {
+    case IMB_BLEND_MIX:
+      for (const int i : dst.index_range()) {
+        blend_color_mix_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_ADD:
+      for (const int i : dst.index_range()) {
+        blend_color_add_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_SUB:
+      for (const int i : dst.index_range()) {
+        blend_color_sub_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_MUL:
+      for (const int i : dst.index_range()) {
+        blend_color_mul_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_LIGHTEN:
+      for (const int i : dst.index_range()) {
+        blend_color_lighten_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_DARKEN:
+      for (const int i : dst.index_range()) {
+        blend_color_darken_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_ERASE_ALPHA:
+      for (const int i : dst.index_range()) {
+        blend_color_erase_alpha_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_ADD_ALPHA:
+      for (const int i : dst.index_range()) {
+        blend_color_add_alpha_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_OVERLAY:
+      for (const int i : dst.index_range()) {
+        blend_color_overlay_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_HARDLIGHT:
+      for (const int i : dst.index_range()) {
+        blend_color_hardlight_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_COLORBURN:
+      for (const int i : dst.index_range()) {
+        blend_color_burn_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_LINEARBURN:
+      for (const int i : dst.index_range()) {
+        blend_color_linearburn_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_COLORDODGE:
+      for (const int i : dst.index_range()) {
+        blend_color_dodge_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_SCREEN:
+      for (const int i : dst.index_range()) {
+        blend_color_screen_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_SOFTLIGHT:
+      for (const int i : dst.index_range()) {
+        blend_color_softlight_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_PINLIGHT:
+      for (const int i : dst.index_range()) {
+        blend_color_pinlight_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_LINEARLIGHT:
+      for (const int i : dst.index_range()) {
+        blend_color_linearlight_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_VIVIDLIGHT:
+      for (const int i : dst.index_range()) {
+        blend_color_vividlight_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_DIFFERENCE:
+      for (const int i : dst.index_range()) {
+        blend_color_difference_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_EXCLUSION:
+      for (const int i : dst.index_range()) {
+        blend_color_exclusion_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_COLOR:
+      for (const int i : dst.index_range()) {
+        blend_color_color_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_HUE:
+      for (const int i : dst.index_range()) {
+        blend_color_hue_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_SATURATION:
+      for (const int i : dst.index_range()) {
+        blend_color_saturation_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    case IMB_BLEND_LUMINOSITY:
+      for (const int i : dst.index_range()) {
+        blend_color_luminosity_float(dst[i], src1[i], src2[i]);
+      }
+      break;
+    default:
+      for (const int i : dst.index_range()) {
+        dst[i] = src1[i];
+      }
+      break;
+  }
+}
+
 /* -------------------------------------------------------------------- */
 /** \name Crop
  * \{ */

commit 6a971f141703b7f35ba9fec9d39ed2f99d328573

Fix: Compile error in previous commit
Introduced in ba9f8e97fbd34dcdbb7a049710bf111d08648329

Pull Request: https://projects.blender.org/blender/blender/pulls/155642

source/blender/imbuf/IMB_imbuf.hh
@@ -13,6 +13,7 @@
 
 #include "BLI_enum_flags.hh"
 #include "BLI_math_matrix_types.hh"
+#include "BLI_span.hh"
 
 #include "IMB_imbuf_types.hh"
 