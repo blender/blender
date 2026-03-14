c6b37603098e12b0c11d6fb963f5c24c85e97cc8
Campbell Barton <campbell@blender.org>
2026-03-14 15:24:23 +1100

    Cleanup: add missing license header

--- source/blender/animrig/intern/anim_rna_test.cc
+++ source/blender/animrig/intern/anim_rna_test.cc
@@ -1,3 +1,11 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup animrig
+ */
+
 #include "ANIM_rna.hh"
 
 #include "testing/testing.h"
