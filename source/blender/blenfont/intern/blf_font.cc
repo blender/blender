070772a5b760875a90afaba9a564c8ff1d8ac8f3
Harley Acheson <harley.acheson@gmail.com>
2026-03-15 18:00:30 +0100

    UI: Improved CJK Unicode Ranges
    We cache the Unicode range coverage for each of the fonts we distribute
    so we can find the right font without having to always check each one.
    This PR just improves the noted coverage for one font, "Noto Sans CJK
    Regular", to indicate it includes TT_UCR_HANGUL_JAMO,
    TT_UCR_HANGUL_COMPATIBILITY_JAMO, TT_UCR_HANGUL, and
    TT_UCR_HALFWIDTH_FULLWIDTH_FORMS.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/155679

--- source/blender/blenfont/intern/blf_font.cc
+++ source/blender/blenfont/intern/blf_font.cc
@@ -1964,11 +1964,12 @@ struct FaceDetails {
 /* Details about the fallback fonts we ship, so that we can load only when needed. */
 static const FaceDetails static_face_details[] = {
     {"Noto Sans CJK Regular.woff2",
-     0,
+     TT_UCR_HANGUL_JAMO,
      TT_UCR_CJK_SYMBOLS | TT_UCR_HIRAGANA | TT_UCR_KATAKANA | TT_UCR_BOPOMOFO | TT_UCR_CJK_MISC |
          TT_UCR_ENCLOSED_CJK_LETTERS_MONTHS | TT_UCR_CJK_COMPATIBILITY |
-         TT_UCR_CJK_UNIFIED_IDEOGRAPHS | TT_UCR_CJK_COMPATIBILITY_IDEOGRAPHS,
-     TT_UCR_CJK_COMPATIBILITY_FORMS,
+         TT_UCR_CJK_UNIFIED_IDEOGRAPHS | TT_UCR_CJK_COMPATIBILITY_IDEOGRAPHS |
+         TT_UCR_HANGUL_COMPATIBILITY_JAMO | TT_UCR_HANGUL,
+     TT_UCR_CJK_COMPATIBILITY_FORMS | TT_UCR_HALFWIDTH_FULLWIDTH_FORMS,
      0},
     {"NotoEmoji-VariableFont_wght.woff2", 0x80000003L, 0x241E4ACL, 0x14000000L, 0x4000000L},
     {"NotoSansArabic-VariableFont_wdth,wght.woff2",
