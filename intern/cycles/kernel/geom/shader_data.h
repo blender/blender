c3c4dbf6a06da0cf235c638e9fafbe1c07634aba
Xavier Hallade <xavier.hallade@intel.com>
2026-03-18 13:56:32 +0100

    Cycles: Add inlining hint back on shader_setup_from_ray
    This recovers most of the performance regression seen on Windows+Intel
    GPUs after f2394a431e.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/155794

--- intern/cycles/kernel/geom/shader_data.h
+++ intern/cycles/kernel/geom/shader_data.h
@@ -36,7 +36,7 @@ ccl_device void shader_setup_object_transforms(KernelGlobals kg,
 
 /* TODO: break this up if it helps reduce register pressure to load data from
  * global memory as we write it to shader-data. */
-ccl_device_noinline void shader_setup_from_ray(KernelGlobals kg,
+ccl_device_inline void shader_setup_from_ray(KernelGlobals kg,
                                                ccl_private ShaderData *ccl_restrict sd,
                                                const ccl_private Ray *ccl_restrict ray,
                                                const ccl_private Intersection *ccl_restrict isect)
