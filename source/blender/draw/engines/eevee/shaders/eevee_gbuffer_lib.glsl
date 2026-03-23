blender/blender

commit 148f766a7585bc616bea27ea52c93970af71070e

Refactor: EEVEE: replace `thickness` float with `Thickness` wrapper object
Replaces use of `float thickness` to encode object material thickness and
thickness mode (sphere, slab) with a wrapper struct `Thickness`, to avoid
some cryptic equality checks in shader code.

Pull Request: https://projects.blender.org/blender/blender/pulls/154820

source/blender/draw/engines/eevee/shaders/eevee_gbuffer_lib.glsl
@@ -23,6 +23,8 @@
 #include "gpu_shader_math_vector_reduce_lib.glsl"
 #include "infos/eevee_common_infos.hh"
 
+#include "eevee_thickness_lib.glsl"
+
 #include "gpu_shader_codegen_lib.glsl"
 #include "gpu_shader_math_vector_lib.glsl"
 #include "gpu_shader_utildefines_lib.glsl"
@@ -176,24 +178,25 @@ float ior_unpack(float ior_packed)
   return (ior_packed > 0.5f) ? (0.5f / (1.0f - ior_packed)) : (2.0f * ior_packed);
 }
 
-float thickness_pack(float thickness)
+float thickness_pack(Thickness thickness)
 {
   /* TODO(fclem): If needed, we could increase precision by defining a ceiling value like the view
    * distance and remap to it. Or tweak the hyperbole equality. */
   /* NOTE: Sign encodes the thickness mode. */
   /* Remap [0..+inf) to [0..1/2]. */
-  float thickness_packed = abs(thickness) / (1.0f + 2.0f * abs(thickness));
+  float thickness_packed = thickness.data / (1.0f + 2.0f * thickness.data);
   /* Mirror the negative from [0..1/2] to [1..1/2]. O is mapped to 0 for precision. */
-  return (thickness < 0.0f) ? 1.0f - thickness_packed : thickness_packed;
+  return thickness.data < 0.0f ? 1.0f - thickness_packed : thickness_packed;
 }
-float thickness_unpack(float thickness_packed)
+
+Thickness thickness_unpack(float thickness_packed)
 {
   /* Undo mirroring. */
   float thickness = (thickness_packed > 0.5f) ? 1.0f - thickness_packed : thickness_packed;
   /* Remap [0..1/2] to [0..+inf). */
   thickness = thickness / (1.0f - 2.0f * thickness);
-  /* Retrieve sign. */
-  return (thickness_packed > 0.5f) ? -thickness : thickness;
+  /* Retrieve mode. */
+  return {.data = (thickness_packed > 0.5f ? -thickness : thickness)};
 }
 
 /**
@@ -488,16 +491,16 @@ struct Header {
 
 /* Added data inside the Tangent Space layers. */
 struct AdditionalInfo {
-  float thickness;
+  Thickness thickness;
 
-  static float2 pack(float thickness)
+  static float2 pack(Thickness thickness)
   {
     return float2(thickness_pack(thickness), 0.0f /* UNUSED */);
   }
 
   static AdditionalInfo unpack(float2 data)
   {
-    return {thickness_unpack(data.x)};
+    return AdditionalInfo{thickness_unpack(data.x)};
   }
 };
 

source/blender/draw/engines/eevee/eevee_shader.cc
@@ -1221,7 +1221,9 @@ void ShaderModule::material_create_info_amend(GPUMaterial *gpumat, GPUCodegenOut
     frag_gen << "}\n\n";
 
     /* TODO(fclem): Find a way to pass material parameters inside the material UBO. */
-    info.define("thickness_mode", thickness_type == MAT_THICKNESS_SLAB ? "-1.0" : "1.0");
+    info.define("thickness_mode",
+                thickness_type == MAT_THICKNESS_SLAB ? "THICKNESS_MODE_SLAB" :
+                                                       "THICKNESS_MODE_SPHERE");
 
     frag_gen << "float nodetree_thickness()\n";
     frag_gen << "{\n";

source/blender/draw/engines/eevee/shaders/eevee_bxdf_diffuse_lib.glsl
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "eevee_bxdf_lib.glsl"
+#include "eevee_thickness_lib.glsl"
 #include "gpu_shader_codegen_lib.glsl"
 #include "gpu_shader_math_base_lib.glsl"
 #include "gpu_shader_math_safe_lib.glsl"
@@ -75,9 +76,9 @@ ClosureLight bxdf_diffuse_light(ClosureUndetermined cl)
  * \param thickness: Thickness of the object. 0 is considered thin.
  * \return pdf: the pdf of sampling the reflected/refracted ray. 0 if ray is invalid.
  */
-BsdfSample bxdf_translucent_sample(float3 rand, float thickness)
+BsdfSample bxdf_translucent_sample(float3 rand, Thickness thickness)
 {
-  if (thickness > 0.0f) {
+  if (thickness.mode() == THICKNESS_MODE_SPHERE) {
     /* Two transmission events inside a sphere is a uniform sphere distribution. */
     float cos_theta = rand.x * 2.0f - 1.0f;
     BsdfSample samp;
@@ -92,9 +93,9 @@ BsdfSample bxdf_translucent_sample(float3 rand, float thickness)
   return samp;
 }
 
-BsdfEval bxdf_translucent_eval(float3 N, float3 L, float thickness)
+BsdfEval bxdf_translucent_eval(float3 N, float3 L, Thickness thickness)
 {
-  if (thickness > 0.0f) {
+  if (thickness.mode() == THICKNESS_MODE_SPHERE) {
     /* Two transmission events inside a sphere is a uniform sphere distribution. */
     BsdfEval eval;
     eval.throughput = eval.pdf = 0.25f * M_1_PI;
@@ -111,26 +112,26 @@ float bxdf_translucent_perceived_roughness()
   return 1.0f;
 }
 
-LightProbeRay bxdf_translucent_lightprobe(float3 N, float thickness)
+LightProbeRay bxdf_translucent_lightprobe(float3 N, Thickness thickness)
 {
   LightProbeRay probe;
   probe.perceptual_roughness = bxdf_translucent_perceived_roughness();
   /* If using the spherical assumption, discard any directionality from the lighting. */
-  probe.dominant_direction = (thickness > 0.0f) ? float3(0.0f) : -N;
+  probe.dominant_direction = (thickness.mode() == THICKNESS_MODE_SPHERE) ? float3(0.0f) : -N;
   return probe;
 }
 
-Ray bxdf_translucent_ray_amend(ClosureUndetermined cl, float3 V, Ray ray, float thickness)
+Ray bxdf_translucent_ray_amend(ClosureUndetermined cl, float3 V, Ray ray, Thickness thickness)
 {
-  if (thickness > 0.0f) {
+  if (thickness.mode() == THICKNESS_MODE_SPHERE) {
     /* Ray direction is distributed on the whole sphere.
      * Move the ray origin to the sphere surface (with bias to avoid self-intersection). */
-    ray.origin += (ray.direction - cl.N) * thickness * 0.505f;
+    ray.origin += (ray.direction - cl.N) * thickness.value() * 0.505f;
   }
   return ray;
 }
 
-ClosureLight bxdf_translucent_light(ClosureUndetermined cl, float3 V, float thickness)
+ClosureLight bxdf_translucent_light(ClosureUndetermined cl, float3 V, Thickness thickness)
 {
   /* A translucent sphere lit by a light outside the sphere transmits the
    * light uniformly over the sphere. To mimic this phenomenon, we use the light vector
@@ -143,7 +144,7 @@ ClosureLight bxdf_translucent_light(ClosureUndetermined cl, float3 V, float thic
   light.ltc_mat = float4(
       1.0f, 0.0f, 0.0f, 1.0f); /* No transform, just plain cosine distribution. */
   light.N = -cl.N;
-  light.type = (thickness != 0.0f) ? LIGHT_TRANSLUCENT_WITH_THICKNESS : LIGHT_DIFFUSE;
+  light.type = (thickness.value() != 0.0f) ? LIGHT_TRANSLUCENT_WITH_THICKNESS : LIGHT_DIFFUSE;
   return light;
 }
 

source/blender/draw/engines/eevee/shaders/eevee_bxdf_microfacet_lib.glsl
@@ -203,9 +203,9 @@ float3 bxdf_ggx_sample_vndf(float3 rand, float3 Vt, float alpha, float &G_V)
  * \return: the sampled direction and the pdf of sampling the direction.
  */
 BsdfSample bxdf_ggx_sample_refraction(
-    float3 rand, float3 Vt, float alpha, float ior, float thickness, const bool do_clamp)
+    float3 rand, float3 Vt, float alpha, float ior, Thickness thickness, const bool do_clamp)
 {
-  if (thickness != 0.0f) {
+  if (thickness.value() != 0.0f) {
     /* The incoming ray is inside the material for the second refraction event. */
     ior = 1.0f / ior;
   }
@@ -239,9 +239,9 @@ BsdfSample bxdf_ggx_sample_refraction(
 /* Evaluate the GGX BTDF without the Fresnel term, multiplied by the cosine foreshortening term.
  * Also evaluate the probability of sampling the refraction direction. */
 BsdfEval bxdf_ggx_eval_refraction(
-    float3 N, float3 L, float3 V, float alpha, float ior, float thickness, const bool do_clamp)
+    float3 N, float3 L, float3 V, float alpha, float ior, Thickness thickness, const bool do_clamp)
 {
-  if (thickness != 0.0f) {
+  if (thickness.value() != 0.0f) {
     ior = 1.0f / ior;
   }
 
@@ -366,18 +366,18 @@ LightProbeRay bxdf_ggx_lightprobe_reflection(ClosureReflection cl, float3 V)
   return probe;
 }
 
-LightProbeRay bxdf_ggx_lightprobe_transmission(ClosureRefraction cl, float3 V, float thickness)
+LightProbeRay bxdf_ggx_lightprobe_transmission(ClosureRefraction cl, float3 V, Thickness thickness)
 {
   LightProbeRay probe;
   probe.perceptual_roughness = bxdf_ggx_perceived_roughness_transmission(cl.roughness, cl.ior);
   probe.dominant_direction = bxdf_ggx_dominant_direction_transmission(
-      cl.N, V, thickness != 0.0f ? 1.0f / cl.ior : cl.ior, probe.perceptual_roughness);
+      cl.N, V, thickness.value() != 0.0f ? 1.0f / cl.ior : cl.ior, probe.perceptual_roughness);
   return probe;
 }
 
-void bxdf_ggx_context_amend_transmission(ClosureUndetermined &cl, float3 &V, float thickness)
+void bxdf_ggx_context_amend_transmission(ClosureUndetermined &cl, float3 &V, Thickness thickness)
 {
-  if (thickness != 0.0f) {
+  if (thickness.value() != 0.0f) {
     ClosureRefraction bsdf = to_closure_refraction(cl);
     float perceived_roughness = bxdf_ggx_perceived_roughness_transmission(bsdf.roughness,
                                                                           bsdf.ior);
@@ -387,9 +387,9 @@ void bxdf_ggx_context_amend_transmission(ClosureUndetermined &cl, float3 &V, flo
   }
 }
 
-Ray bxdf_ggx_ray_amend_transmission(ClosureUndetermined cl, float3 V, Ray ray, float thickness)
+Ray bxdf_ggx_ray_amend_transmission(ClosureUndetermined cl, float3 V, Ray ray, Thickness thickness)
 {
-  if (thickness != 0.0f) {
+  if (thickness.value() != 0.0f) {
     ClosureRefraction bsdf = to_closure_refraction(cl);
     float perceived_roughness = bxdf_ggx_perceived_roughness_transmission(bsdf.roughness,
                                                                           bsdf.ior);
@@ -410,18 +410,18 @@ ClosureLight bxdf_ggx_light_reflection(ClosureReflection cl, float3 V)
   return light;
 }
 
-ClosureLight bxdf_ggx_light_transmission(ClosureRefraction cl, float3 V, float thickness)
+ClosureLight bxdf_ggx_light_transmission(ClosureRefraction cl, float3 V, Thickness thickness)
 {
   float perceptual_roughness = bxdf_ggx_perceived_roughness_transmission(cl.roughness, cl.ior);
 
-  if (thickness != 0.0f) {
+  if (thickness.value() != 0.0f) {
     float3 L = bxdf_ggx_dominant_direction_transmission(cl.N, V, cl.ior, perceptual_roughness);
     cl.N = -thickness_shape_intersect(thickness, cl.N, L).hit_N;
     V = -L;
   }
   /* Ad-hoc solution to reuse the reflection LUT. To be eventually replaced by own precomputed
    * table. */
-  float3 R = refract(-V, cl.N, (thickness != 0.0f) ? cl.ior : (1.0f / cl.ior));
+  float3 R = refract(-V, cl.N, (thickness.value() != 0.0f) ? cl.ior : (1.0f / cl.ior));
   float cos_theta = dot(-cl.N, R);
 
   ClosureLight light;

source/blender/draw/engines/eevee/shaders/eevee_closure_lib.glsl
@@ -28,7 +28,7 @@ float closure_apparent_roughness_get(ClosureUndetermined cl)
   return 0.0f;
 }
 
-float closure_evaluate_pdf(ClosureUndetermined cl, float3 L, float3 V, float thickness)
+float closure_evaluate_pdf(ClosureUndetermined cl, float3 L, float3 V, Thickness thickness)
 {
   switch (cl.type) {
     case CLOSURE_BSDF_TRANSLUCENT_ID:
@@ -55,7 +55,7 @@ float closure_evaluate_pdf(ClosureUndetermined cl, float3 L, float3 V, float thi
   return 0.0f;
 }
 
-LightProbeRay bxdf_lightprobe_ray(ClosureUndetermined cl, float3 P, float3 V, float thickness)
+LightProbeRay bxdf_lightprobe_ray(ClosureUndetermined cl, float3 P, float3 V, Thickness thickness)
 {
   switch (cl.type) {
     case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
@@ -92,7 +92,7 @@ LightProbeRay bxdf_lightprobe_ray(ClosureUndetermined cl, float3 P, float3 V, fl
 
 ClosureLight closure_light_new_ex(ClosureUndetermined cl,
                                   float3 V,
-                                  float thickness,
+                                  Thickness thickness,
                                   const bool is_transmission)
 {
   ClosureLight cl_light;
@@ -103,7 +103,7 @@ ClosureLight closure_light_new_ex(ClosureUndetermined cl,
         /* If the `thickness / sss_radius` ratio is near 0, this transmission term should converge
          * to a uniform term like the translucent BSDF. But we need to find what to do in other
          * cases. For now, approximate the transmission term as just back-facing. */
-        cl_light = bxdf_translucent_light(cl, V, 0.0f);
+        cl_light = bxdf_translucent_light(cl, V, Thickness::zero());
         break;
       case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
         cl_light = bxdf_ggx_light_transmission(to_closure_refraction(cl), V, thickness);
@@ -138,12 +138,12 @@ ClosureLight closure_light_new_ex(ClosureUndetermined cl,
   return cl_light;
 }
 
-ClosureLight closure_light_new(ClosureUndetermined cl, float3 V, float thickness)
+ClosureLight closure_light_new(ClosureUndetermined cl, float3 V, Thickness thickness)
 {
   return closure_light_new_ex(cl, V, thickness, true);
 }
 
 ClosureLight closure_light_new(ClosureUndetermined cl, float3 V)
 {
-  return closure_light_new_ex(cl, V, 0.0f, false);
+  return closure_light_new_ex(cl, V, Thickness::zero(), false);
 }

source/blender/draw/engines/eevee/shaders/eevee_deferred_capture_frag.glsl
@@ -30,7 +30,7 @@ void main()
   }
 
   const uchar closure_count = gbuf.header.closure_len();
-  const float thickness = gbuffer::read_thickness(gbuf.header, texel);
+  const Thickness thickness = gbuffer::read_thickness(gbuf.header, texel);
 
   float3 albedo_front = float3(0.0f);
   float3 albedo_back = float3(0.0f);
@@ -45,7 +45,7 @@ void main()
         break;
       case CLOSURE_BSDF_TRANSLUCENT_ID:
       case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
-        albedo_back += (thickness != 0.0f) ? square(cl.color) : cl.color;
+        albedo_back += (thickness.value() != 0.0f) ? square(cl.color) : cl.color;
         break;
       case CLOSURE_NONE_ID:
         /* TODO(fclem): Assert. */

source/blender/draw/engines/eevee/shaders/eevee_deferred_combine_frag.glsl
@@ -105,7 +105,7 @@ void main()
 
     if ((cl.type == CLOSURE_BSDF_TRANSLUCENT_ID ||
          cl.type == CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID) &&
-        (gbuffer::read_thickness(gbuf.header, texel) != 0.0f))
+        (gbuffer::read_thickness(gbuf.header, texel).value() != 0.0f))
     {
       /* We model two transmission event, so the surface color need to be applied twice. */
       cl.color *= cl.color;

source/blender/draw/engines/eevee/shaders/eevee_deferred_light_frag.glsl
@@ -58,7 +58,7 @@ void main()
 
   const float depth = texelFetch(hiz_tx, texel, 0).r - bias;
   const gbuffer::Layers gbuf = gbuffer::read_layers(texel);
-  const float thickness = gbuffer::read_thickness(gbuf.header, texel);
+  const Thickness thickness = gbuffer::read_thickness(gbuf.header, texel);
   const uchar closure_count = gbuf.header.closure_len();
 
   const float3 P = drw_point_screen_to_world(float3(screen_uv, depth));
@@ -111,7 +111,7 @@ void main()
     if (cl_transmit.type == CLOSURE_BSSRDF_BURLEY_ID) {
       /* Apply transmission profile onto transmitted light and sum with reflected light. */
       float3 sss_profile = subsurface_transmission(to_closure_subsurface(cl_transmit).sss_radius,
-                                                   abs(thickness));
+                                                   thickness.value());
       stack.cl[0].light_shadowed *= sss_profile;
       stack.cl[0].light_unshadowed *= sss_profile;
       stack.cl[0].light_shadowed += sss_reflect_shadowed;

commit 3ede13fe3a4cca7333769a13fcc5bebdc46ab826

Fix #156148: slab mode thickness not working
Fix for slab mode thickness misbehaving, caused by refactor regression
from 148f766a7585bc616bea27ea52c93970af71070e.

Pull Request: https://projects.blender.org/blender/blender/pulls/156151

source/blender/draw/engines/eevee/shaders/eevee_gbuffer_lib.glsl
@@ -184,7 +184,7 @@ float thickness_pack(Thickness thickness)
    * distance and remap to it. Or tweak the hyperbole equality. */
   /* NOTE: Sign encodes the thickness mode. */
   /* Remap [0..+inf) to [0..1/2]. */
-  float thickness_packed = thickness.data / (1.0f + 2.0f * thickness.data);
+  float thickness_packed = thickness.value() / (1.0f + 2.0f * thickness.value());
   /* Mirror the negative from [0..1/2] to [1..1/2]. O is mapped to 0 for precision. */
   return thickness.data < 0.0f ? 1.0f - thickness_packed : thickness_packed;
 }