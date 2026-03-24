f521002b3f30862d8093a77c7db41db2a13a775d
Martijn Versteegh <baardaap@noreply.localhost>
2026-03-24 14:43:36 +0100

    Fix #155971: Suppress outputting the full render report when the rendering is canceled.
    When the rendering is canceled the full render report is meaningless,
    so don't print it.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/155977

--- intern/cycles/session/session.cpp
+++ intern/cycles/session/session.cpp
@@ -164,13 +164,20 @@ void Session::run_main_render_loop()
   while (true) {
     RenderWork render_work = run_update_for_next_iteration();
 
+    const bool did_cancel = progress.get_cancel();
+
     if (!render_work) {
       if (LOG_IS_ON(LOG_LEVEL_INFO)) {
-        double total_time;
-        double render_time;
-        progress.get_time(total_time, render_time);
-        LOG_INFO << "Rendering in main loop is done in " << render_time << " seconds.";
-        LOG_INFO << path_trace_->full_report();
+        if (did_cancel) {
+          LOG_INFO << "Rendering was canceled.";
+        }
+        else {
+          double total_time;
+          double render_time;
+          progress.get_time(total_time, render_time);
+          LOG_INFO << "Rendering in main loop is done in " << render_time << " seconds.";
+          LOG_INFO << path_trace_->full_report();
+        }
       }
 
       if (params.background) {
@@ -180,7 +187,6 @@ void Session::run_main_render_loop()
       }
     }
 
-    const bool did_cancel = progress.get_cancel();
     if (did_cancel) {
       render_scheduler_.render_work_reschedule_on_cancel(render_work);
       if (!render_work) {
