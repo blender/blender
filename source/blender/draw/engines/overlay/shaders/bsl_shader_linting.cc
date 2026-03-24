b291b213806c66d499f94806ae7d2aa425f272fd
Mark van de Ruit <mark@blender.org>
2026-03-24 15:22:05 +0100

    Fix: BSL/GLSL source compilation warnings, primarily in overlay shaders
    Fix for `pragma once` warnings in BSL overlay shaders.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/156221

---
+++ source/blender/draw/engines/overlay/shaders/bsl_shader_linting.cc
@@ -0,0 +1,12 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/**
+ * Compile shader files as C++ inside one compilation unit to lint syntax and get IDE integration.
+ */
+
+#include "overlay_antialiasing.bsl.hh" /* IWYU pragma: export */
+#include "overlay_xray_fade.bsl.hh"    /* IWYU pragma: export */
+
+void main() {}
