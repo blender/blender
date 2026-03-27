4fe1725c560b5a823ce910c30b468ed8d4670239
Sean Kim <SeanCTKim@protonmail.com>
2026-03-27 22:37:52 +0100

    Fix #156423: Crash when using sculpt filter tool & topology automasking
    Introduced with d8716f070775e5d412f5eee86c9fe64b55458c97
    
    It is incorrect to assume the `ss.cache` is always available when
    calculating the automasking factor. If a filter tool is being used, use
    the '0th' symmetry pass to index into the initial island id array.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/156451

--- source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
+++ source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
@@ -794,10 +794,13 @@ void calc_face_factors(const Depsgraph &depsgraph,
         }
       }
 
+      /* The filter tools do not take symmetry into consideration, use the "generic" (i.e. 0)
+       * symmetry pass instead */
+      const int current_symmetry_pass = ss.cache ? ss.cache->mirror_symmetry_pass : 0;
       if (!automasking.settings.topology_use_brush_limit &&
           automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
           islands::vert_id_get(ss, vert) !=
-              automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
+              automasking.settings.initial_island_nr[current_symmetry_pass])
       {
         factor = 0.0f;
         continue;
@@ -923,10 +926,13 @@ void calc_grids_factors(const Depsgraph &depsgraph,
         }
       }
 
+      /* The filter tools do not take symmetry into consideration, use the "generic" (i.e. 0)
+       * symmetry pass instead */
+      const int current_symmetry_pass = ss.cache ? ss.cache->mirror_symmetry_pass : 0;
       if (!automasking.settings.topology_use_brush_limit &&
           automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
           islands::vert_id_get(ss, vert) !=
-              automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
+              automasking.settings.initial_island_nr[current_symmetry_pass])
       {
         factors[node_vert] = 0.0f;
         continue;
@@ -1046,10 +1052,13 @@ void calc_vert_factors(const Depsgraph &depsgraph,
       }
     }
 
+    /* The filter tools do not take symmetry into consideration, use the "generic" (i.e. 0)
+     * symmetry pass instead */
+    const int current_symmetry_pass = ss.cache ? ss.cache->mirror_symmetry_pass : 0;
     if (!automasking.settings.topology_use_brush_limit &&
         automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
         islands::vert_id_get(ss, vert_i) !=
-            automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
+            automasking.settings.initial_island_nr[current_symmetry_pass])
     {
       factors[i] = 0.0f;
       continue;
