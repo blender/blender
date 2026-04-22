95fb45ef957da8c0439a6d55a433e566c03f7d39
Jonas Holzman <jonas@blender.org>
2026-04-22 11:50:46 +0200

    Deps: bzip2 Android build port

--- build_files/build_environment/cmake/bzip2.cmake
+++ build_files/build_environment/cmake/bzip2.cmake
@@ -3,20 +3,24 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 set(BZIP2_PREFIX "${LIBDIR}/bzip2")
-set(BZIP2_CONFIGURE_ENV echo .)
 set(BZIP2_CONFIGURATION_ARGS "")
 
 if(UNIX AND NOT APPLE)
   set(BZIP2_LDFLAGS "-Wl,--as-needed")
   set(BZIP2_CFLAGS "-fPIC -Wall -Winline -O2 -g -D_FILE_OFFSET_BITS=64")
-  set(BZIP2_CONFIGURE_ENV
-    ${BZIP2_CONFIGURE_ENV} &&
-    export LDFLAGS=${BZIP2_LDFLAGS} &&
-    export CFLAGS=${BZIP2_CFLAGS} &&
-    export PREFIX=${BZIP2_PREFIX}
-  )
-else()
-  set(BZIP2_CONFIGURE_ENV ${CONFIGURE_ENV})
+  if(ANDROID)
+    set(BZIP2_CFLAGS "${BZIP2_CFLAGS} --target=${CMAKE_C_COMPILER_TARGET}")
+  endif()
+endif()
+
+set(BZIP2_MAKE_EXTRA_FLAGS "")
+set(BZIP2_MAKE_TARGET "") # Implicit `all`.
+if(ANDROID)
+  # Override compiler from Android CMake toolchain file.
+  set(BZIP2_MAKE_EXTRA_FLAGS ${BZIP2_MAKE_FLAGS} CC=${CMAKE_C_COMPILER} AR=${CMAKE_AR} RANLIB=${CMAKE_RANLIB})
+
+  # Skip testing phase, which would otherwise try to run the cross-compiled bzip2 executable on the wrong arch.
+  set(BZIP2_MAKE_TARGET libbz2.a bzip2 bzip2recover)
 endif()
 
 ExternalProject_Add(external_bzip2
@@ -26,13 +30,13 @@ ExternalProject_Add(external_bzip2
   PREFIX ${BUILD_DIR}/bzip2
   CONFIGURE_COMMAND echo .
 
-  BUILD_COMMAND ${BZIP2_CONFIGURE_ENV} &&
+  BUILD_COMMAND ${CONFIGURE_ENV} &&
     cd ${BUILD_DIR}/bzip2/src/external_bzip2/ &&
-    make CFLAGS=${BZIP2_CFLAGS} LDFLAGS=${BZIP2_LDFLAGS} -j${MAKE_THREADS}
+    make CFLAGS=${BZIP2_CFLAGS} LDFLAGS=${BZIP2_LDFLAGS} ${BZIP2_MAKE_EXTRA_FLAGS} -j${MAKE_THREADS} ${BZIP2_MAKE_TARGET}
 
-  INSTALL_COMMAND ${BZIP2_CONFIGURE_ENV} &&
+  INSTALL_COMMAND ${CONFIGURE_ENV} &&
     cd ${BUILD_DIR}/bzip2/src/external_bzip2/ &&
-    make CFLAGS=${BZIP2_CFLAGS} LDFLAGS=${BZIP2_LDFLAGS} PREFIX=${BZIP2_PREFIX} install
+    make PREFIX=${BZIP2_PREFIX} install
 
   INSTALL_DIR ${LIBDIR}/bzip2
 )
