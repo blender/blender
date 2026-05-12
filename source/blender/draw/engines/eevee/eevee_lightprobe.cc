blender/blender

commit 8a26ce1d6646fae6f3e6353558e66a7fc1d08684

Cleanup: EEVEE: Remove ununsed iostream includes

source/blender/draw/engines/eevee/eevee_lightprobe.cc
@@ -15,8 +15,6 @@
 #include "eevee_instance.hh"
 #include "eevee_lightprobe.hh"
 
-#include <iostream>
-
 namespace blender::eevee {
 
 /* -------------------------------------------------------------------- */

source/blender/draw/engines/eevee/eevee_raytrace.cc
@@ -11,7 +11,6 @@
 #include "GPU_debug.hh"
 
 #include "eevee_instance.hh"
-#include <iostream>
 
 #include "eevee_raytrace.hh"
 

commit d685c5eaaba26ac4d1c6f4b79ea0354aad9e7d5b

Fix: EEVEE: Broken compilation
Caused by 8a26ce1d6646fae6f3e6353558e66a7fc1d08684

source/blender/draw/engines/eevee/eevee_lightprobe.cc
@@ -15,6 +15,8 @@
 #include "eevee_instance.hh"
 #include "eevee_lightprobe.hh"
 
+#include <iostream>
+
 namespace blender::eevee {
 
 /* -------------------------------------------------------------------- */