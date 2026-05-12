42acacd9d1200d9f9e32278b2d15f1878fc63a45
Bastien Montagne <bastien@blender.org>
2026-05-12 14:12:58 +0200

    Fix #158225: Crash in a presence of a pointer to a scene in 5.2 Alpha.
    Need to ensure that all scenes/viewlayers are in sync before evaluating
    a scene, since others may be pulled in by random ID dependencies.
    
    Also fixes #158386.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/158235

--- source/blender/blenkernel/intern/scene.cc
+++ source/blender/blenkernel/intern/scene.cc
@@ -2727,7 +2727,12 @@ static void scene_graph_update_tagged(Depsgraph *depsgraph, Main *bmain, bool on
     BKE_callback_exec_id(bmain, &scene->id, BKE_CB_EVT_DEPSGRAPH_UPDATE_PRE);
   }
 
-  BKE_scene_view_layers_synced_ensure(*bmain, scene);
+  /* Cannot limit this to the currently evaluated scene/view layer, as the depsgraph may have
+   * dependencies on others, see e.g. #158225, which pulls in another scene. */
+  /* TODO: If this becomes a performance issue, we'll likely have to find a way in the depsgraph
+   * itself to gather all 'known' scenes, and ensure that their viewlayers / collections
+   * hierarchies are in sync. */
+  BKE_main_view_layers_synced_ensure(bmain);
 
   for (int pass = 0; pass < 2; pass++) {
     /* (Re-)build dependency graph if needed. */
