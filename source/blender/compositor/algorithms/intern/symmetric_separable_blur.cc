goshtdev/upbge

commit ddda68eda12a29e8367ab8ae3472f086c2f9fc2e

Compositor: Optimize extended boundary blurring
This patch optimized the Blur node when Extend Boundary is enabled for
the separable cases. This is done by performing zero padding in each
direction separately to reduce the total workload. This also means that
the padding code is now duplicate and specified to each blur algorithm.

This improves the performance by around 30%. Relevant benchmark files
are now generally faster than 4.5.

Resolved #144350.

Pull Request: https://projects.blender.org/blender/blender/pulls/158327

source/blender/compositor/algorithms/intern/symmetric_separable_blur.cc
@@ -13,6 +13,7 @@
 #include "COM_result.hh"
 #include "COM_utilities.hh"
 
+#include "COM_algorithm_pad.hh"
 #include "COM_algorithm_symmetric_separable_blur.hh"
 
 #include "COM_symmetric_separable_blur_weights.hh"
@@ -67,10 +68,11 @@ static const char *get_blur_shader(const ResultType type)
   return nullptr;
 }
 
-static Result horizontal_pass_gpu(Context &context,
-                                  const Result &input,
-                                  const float radius,
-                                  const math::FilterKernel filter_type)
+static Result blur_pass_gpu(Context &context,
+                            const Result &input,
+                            Result &output,
+                            const float radius,
+                            const math::FilterKernel filter_type)
 {
   gpu::Shader *shader = context.get_shader(get_blur_shader(input.type()));
   GPU_shader_bind(shader);
@@ -92,7 +94,6 @@ static Result horizontal_pass_gpu(Context &context,
   Domain domain = input.domain();
   const int2 transposed_domain = int2(domain.data_size.y, domain.data_size.x);
 
-  Result output = context.create_result(input.type());
   output.allocate_texture(transposed_domain);
   output.bind_as_image(shader, "output_img");
 
@@ -106,26 +107,25 @@ static Result horizontal_pass_gpu(Context &context,
   return output;
 }
 
-static Result horizontal_pass_cpu(Context &context,
-                                  const Result &input,
-                                  const float radius,
-                                  const math::FilterKernel filter_type)
+static Result blur_pass_cpu(Context &context,
+                            const Result &input,
+                            Result &output,
+                            const float radius,
+                            const math::FilterKernel filter_type)
 {
   const Result &weights = context.cache_manager().symmetric_separable_blur_weights.get(
       context, filter_type, radius);
 
   /* We allocate an output image of a transposed size, that is, with a height equivalent to the
-   * width of the input and vice versa. This is done as a performance optimization. The shader
-   * will blur the image horizontally and write it to the intermediate output transposed. Then
-   * the vertical pass will execute the same horizontal blur shader, but since its input is
-   * transposed, it will effectively do a vertical blur and write to the output transposed,
-   * effectively undoing the transposition in the horizontal pass. This is done to improve
-   * spatial cache locality in the shader and to avoid having two separate shaders for each blur
-   * pass. */
+   * width of the input and vice versa. This is done as a performance optimization. The code will
+   * blur the image horizontally and write it to the intermediate output transposed. Then the
+   * vertical pass will execute the same horizontal blur shader, but since its input is transposed,
+   * it will effectively do a vertical blur and write to the output transposed, effectively undoing
+   * the transposition in the horizontal pass. This is done to improve spatial cache locality in
+   * the shader and to avoid having two separate shaders for each blur pass. */
   const Domain domain = input.domain();
   const int2 transposed_domain = int2(domain.data_size.y, domain.data_size.x);
 
-  Result output = context.create_result(input.type());
   output.allocate_texture(transposed_domain);
 
   switch (input.type()) {
@@ -146,101 +146,51 @@ static Result horizontal_pass_cpu(Context &context,
   return output;
 }
 
-static Result horizontal_pass(Context &context,
-                              const Result &input,
-                              const float radius,
-                              const math::FilterKernel filter_type)
+static Result blur_pass(Context &context,
+                        const Result &input,
+                        Result &output,
+                        const float radius,
+                        const math::FilterKernel filter_type)
 {
   if (context.use_gpu()) {
-    return horizontal_pass_gpu(context, input, radius, filter_type);
+    return blur_pass_gpu(context, input, output, radius, filter_type);
   }
-  return horizontal_pass_cpu(context, input, radius, filter_type);
-}
-
-static void vertical_pass_gpu(Context &context,
-                              const Result &original_input,
-                              const Result &horizontal_pass_result,
-                              Result &output,
-                              const float2 &radius,
-                              const math::FilterKernel filter_type)
-{
-  gpu::Shader *shader = context.get_shader(get_blur_shader(original_input.type()));
-  GPU_shader_bind(shader);
-
-  horizontal_pass_result.bind_as_texture(shader, "input_tx");
-
-  const Result &weights = context.cache_manager().symmetric_separable_blur_weights.get(
-      context, filter_type, radius.y);
-  weights.bind_as_texture(shader, "weights_tx");
-
-  const Domain domain = original_input.domain();
-  output.allocate_texture(domain);
-  output.bind_as_image(shader, "output_img");
-
-  /* Notice that the domain is transposed, see the note on the horizontal pass method for more
-   * information on the reasoning behind this. */
-  compute_dispatch_threads_at_least(shader, int2(domain.data_size.y, domain.data_size.x));
-
-  GPU_shader_unbind();
-  horizontal_pass_result.unbind_as_texture();
-  output.unbind_as_image();
-  weights.unbind_as_texture();
-}
-
-static void vertical_pass_cpu(Context &context,
-                              const Result &original_input,
-                              const Result &horizontal_pass_result,
-                              Result &output,
-                              const float2 &radius,
-                              const math::FilterKernel filter_type)
-{
-  const Result &weights = context.cache_manager().symmetric_separable_blur_weights.get(
-      context, filter_type, radius.y);
-
-  output.allocate_texture(original_input.domain());
-
-  switch (original_input.type()) {
-    case ResultType::Float:
-      blur_pass<float>(horizontal_pass_result, weights, output);
-      break;
-    case ResultType::Float4:
-      blur_pass<float4>(horizontal_pass_result, weights, output);
-      break;
-    case ResultType::Color:
-      blur_pass<Color>(horizontal_pass_result, weights, output);
-      break;
-    default:
-      BLI_assert_unreachable();
-      break;
-  }
-}
-
-static void vertical_pass(Context &context,
-                          const Result &original_input,
-                          const Result &horizontal_pass_result,
-                          Result &output,
-                          const float2 &radius,
-                          const math::FilterKernel filter_type)
-{
-  if (context.use_gpu()) {
-    vertical_pass_gpu(
-        context, original_input, horizontal_pass_result, output, radius, filter_type);
-  }
-  else {
-    vertical_pass_cpu(
-        context, original_input, horizontal_pass_result, output, radius, filter_type);
-  }
+  return blur_pass_cpu(context, input, output, radius, filter_type);
 }
 
 void symmetric_separable_blur(Context &context,
                               const Result &input,
                               Result &output,
                               const float2 &radius,
-                              const math::FilterKernel filter_type)
+                              const math::FilterKernel filter_type,
+                              const bool extend_bounds)
 {
-  Result horizontal_pass_result = horizontal_pass(context, input, radius.x, filter_type);
-  vertical_pass(context, input, horizontal_pass_result, output, radius, filter_type);
-  horizontal_pass_result.release();
+  if (extend_bounds) {
+    const int2 padding_size = int2(math::ceil(radius));
+    Result padded_input = context.create_result(input.type());
+    pad(context, input, padded_input, int2(padding_size.x, 0), PaddingMethod::Zero);
+
+    Result horizontal_pass_result = context.create_result(input.type());
+    blur_pass(context, padded_input, horizontal_pass_result, radius.x, filter_type);
+    padded_input.release();
+
+    Result padded_horizontal_pass_result = context.create_result(input.type());
+    pad(context,
+        horizontal_pass_result,
+        padded_horizontal_pass_result,
+        int2(padding_size.y, 0),
+        PaddingMethod::Zero);
+    horizontal_pass_result.release();
+
+    blur_pass(context, padded_horizontal_pass_result, output, radius.y, filter_type);
+    padded_horizontal_pass_result.release();
+  }
+  else {
+    Result horizontal_pass_result = context.create_result(input.type());
+    blur_pass(context, input, horizontal_pass_result, radius.x, filter_type);
+    blur_pass(context, horizontal_pass_result, output, radius.y, filter_type);
+    horizontal_pass_result.release();
+  }
 }
 
 }  // namespace blender::compositor

source/blender/compositor/algorithms/COM_algorithm_deriche_gaussian_blur.hh
@@ -24,11 +24,14 @@ namespace blender::compositor {
  * the Van Vliet filter instead. Further, for sigma values less than 3, use direct convolution
  * instead, since it is faster and more accurate. Neumann boundary is assumed.
  *
+ * If extend_bounds is true, the input will be zero padded by radius amount of pixels.
+ *
  * The output is written to the given output result, which will be allocated internally and is thus
  * expected not to be previously allocated. */
 void deriche_gaussian_blur(Context &context,
                            const Result &input,
                            Result &output,
-                           const float2 &sigma);
+                           const float2 &sigma,
+                           const bool extend_bounds);
 
 }  // namespace blender::compositor

source/blender/compositor/algorithms/COM_algorithm_recursive_gaussian_blur.hh
@@ -17,11 +17,14 @@ namespace blender::compositor {
  * approximation that might suffer from fringing and artifacts, though those are typically
  * unnoticeable. Neumann boundary is assumed.
  *
+ * If extend_bounds is true, the input will be zero padded by radius amount of pixels.
+ *
  * The output is written to the given output result, which will be allocated internally and is thus
  * expected not to be previously allocated. */
 void recursive_gaussian_blur(Context &context,
                              const Result &input,
                              Result &output,
-                             const float2 &radius);
+                             const float2 &radius,
+                             const bool extend_bounds = false);
 
 }  // namespace blender::compositor

source/blender/compositor/algorithms/COM_algorithm_symmetric_separable_blur.hh
@@ -13,12 +13,14 @@
 namespace blender::compositor {
 
 /* Blur the input using a horizontal and a vertical separable blur passes given a certain radius
- * and filter type using SymmetricSeparableBlurWeights. The result is written to the given output,
- * which will be allocated internally and is thus expected not to be previously allocated. */
+ * and filter type using SymmetricSeparableBlurWeights. If extend_bounds is true, the input will be
+ * zero padded by radius amount of pixels. The result is written to the given output, which will be
+ * allocated internally and is thus expected not to be previously allocated. */
 void symmetric_separable_blur(Context &context,
                               const Result &input,
                               Result &output,
                               const float2 &radius,
-                              const math::FilterKernel filter_type = math::FilterKernel::Gauss);
+                              const math::FilterKernel filter_type = math::FilterKernel::Gauss,
+                              const bool extend_bounds = false);
 
 }  // namespace blender::compositor

source/blender/compositor/algorithms/COM_algorithm_van_vliet_gaussian_blur.hh
@@ -29,11 +29,14 @@ namespace blender::compositor {
  * Further, for sigma values less than 3, use direct convolution instead, since it is faster and
  * more accurate. Neumann boundary is assumed.
  *
+ * If extend_bounds is true, the input will be zero padded by radius amount of pixels.
+ *
  * The output is written to the given output result, which will be allocated internally and is thus
  * expected not to be previously allocated. */
 void van_vliet_gaussian_blur(Context &context,
                              const Result &input,
                              Result &output,
-                             const float2 &sigma);
+                             const float2 &sigma,
+                             const bool extend_bounds);
 
 }  // namespace blender::compositor

source/blender/compositor/algorithms/intern/deriche_gaussian_blur.cc
@@ -13,6 +13,7 @@
 #include "COM_utilities.hh"
 
 #include "COM_algorithm_deriche_gaussian_blur.hh"
+#include "COM_algorithm_pad.hh"
 #include "COM_deriche_gaussian_coefficients.hh"
 
 namespace blender::compositor {
@@ -257,10 +258,18 @@ static void blur_pass(Context &context, const Result &input, Result &output, con
   non_causal_result.release();
 }
 
+/* Computes the inverse of compute_sigma_from_radius in recursive_gaussian_blur.cc, see that
+ * function for more information. */
+static float2 compute_radius_from_sigma(const float2 sigma)
+{
+  return sigma * 3.0f;
+}
+
 void deriche_gaussian_blur(Context &context,
                            const Result &input,
                            Result &output,
-                           const float2 &sigma)
+                           const float2 &sigma,
+                           const bool extend_bounds)
 {
   BLI_assert_msg(math::reduce_max(sigma) >= 3.0f,
                  "Deriche filter is slower and less accurate than direct convolution for sigma "
@@ -269,10 +278,32 @@ void deriche_gaussian_blur(Context &context,
                  "Deriche filter is not accurate nor numerically stable for sigma values larger "
                  "than 32. Use Van Vliet filter instead.");
 
-  Result horizontal_pass_result = context.create_result(ResultType::Color);
-  blur_pass(context, input, horizontal_pass_result, sigma.x);
-  blur_pass(context, horizontal_pass_result, output, sigma.y);
-  horizontal_pass_result.release();
+  if (extend_bounds) {
+    const int2 padding_size = int2(math::ceil(compute_radius_from_sigma(sigma)));
+    Result padded_input = context.create_result(input.type());
+    pad(context, input, padded_input, int2(padding_size.x, 0), PaddingMethod::Zero);
+
+    Result horizontal_pass_result = context.create_result(input.type());
+    blur_pass(context, padded_input, horizontal_pass_result, sigma.x);
+    padded_input.release();
+
+    Result padded_horizontal_pass_result = context.create_result(input.type());
+    pad(context,
+        horizontal_pass_result,
+        padded_horizontal_pass_result,
+        int2(padding_size.y, 0),
+        PaddingMethod::Zero);
+    horizontal_pass_result.release();
+
+    blur_pass(context, padded_horizontal_pass_result, output, sigma.y);
+    padded_horizontal_pass_result.release();
+  }
+  else {
+    Result horizontal_pass_result = context.create_result(input.type());
+    blur_pass(context, input, horizontal_pass_result, sigma.x);
+    blur_pass(context, horizontal_pass_result, output, sigma.y);
+    horizontal_pass_result.release();
+  }
 }
 
 }  // namespace blender::compositor

source/blender/compositor/algorithms/intern/pad.cc
@@ -18,7 +18,10 @@ static const char *get_shader_name(const ResultType type, const PaddingMethod pa
     case PaddingMethod::Zero:
       switch (type) {
         case ResultType::Color:
+        case ResultType::Float4:
           return "compositor_pad_zero_float4";
+        case ResultType::Float:
+          return "compositor_pad_zero_float";
         default:
           break;
       }
@@ -72,6 +75,16 @@ static void zero_pad_cpu(const Result &input,
         case ResultType::Color:
           parallel_for(output_size, [&](const int2 texel) {
             output.store_pixel(texel, input.load_pixel_zero<Color>(texel - size));
+          });
+          break;
+        case ResultType::Float4:
+          parallel_for(output_size, [&](const int2 texel) {
+            output.store_pixel(texel, input.load_pixel_zero<float4>(texel - size));
+          });
+          break;
+        case ResultType::Float:
+          parallel_for(output_size, [&](const int2 texel) {
+            output.store_pixel(texel, input.load_pixel_zero<float>(texel - size));
           });
           break;
         default:

source/blender/compositor/algorithms/intern/recursive_gaussian_blur.cc
@@ -49,23 +49,25 @@ static float2 compute_sigma_from_radius(float2 radius)
 void recursive_gaussian_blur(Context &context,
                              const Result &input,
                              Result &output,
-                             const float2 &radius)
+                             const float2 &radius,
+                             const bool extend_bounds)
 {
   /* The radius is in pixel units, while both recursive implementations expect the sigma value of
    * the Gaussian function. */
   const float2 sigma = compute_sigma_from_radius(radius);
 
   if (math::reduce_max(sigma) < 3.0f) {
-    symmetric_separable_blur(context, input, output, radius);
+    symmetric_separable_blur(
+        context, input, output, radius, math::FilterKernel::Gauss, extend_bounds);
     return;
   }
 
   if (math::reduce_max(sigma) < 32.0f) {
-    deriche_gaussian_blur(context, input, output, sigma);
+    deriche_gaussian_blur(context, input, output, sigma, extend_bounds);
     return;
   }
 
-  van_vliet_gaussian_blur(context, input, output, sigma);
+  van_vliet_gaussian_blur(context, input, output, sigma, extend_bounds);
 }
 
 }  // namespace blender::compositor

blender/blender

commit 8f56f8ee8395b05345d2b0b66550a7541b50fe9f

Fix: Blur node ignores input translation
The blur node ignores the translation of the input and does not
propagate it to the output.

Regression in ddda68eda1.

Pull Request: https://projects.blender.org/blender/blender/pulls/158550

source/blender/compositor/algorithms/intern/symmetric_separable_blur.cc
@@ -91,13 +91,10 @@ static Result blur_pass_gpu(Context &context,
    * effectively undoing the transposition in the horizontal pass. This is done to improve
    * spatial cache locality in the shader and to avoid having two separate shaders for each blur
    * pass. */
-  Domain domain = input.domain();
-  const int2 transposed_domain = int2(domain.data_size.y, domain.data_size.x);
-
-  output.allocate_texture(transposed_domain);
+  output.allocate_texture(input.domain().transposed());
   output.bind_as_image(shader, "output_img");
 
-  compute_dispatch_threads_at_least(shader, domain.data_size);
+  compute_dispatch_threads_at_least(shader, input.domain().data_size);
 
   GPU_shader_unbind();
   input.unbind_as_texture();
@@ -123,10 +120,7 @@ static Result blur_pass_cpu(Context &context,
    * it will effectively do a vertical blur and write to the output transposed, effectively undoing
    * the transposition in the horizontal pass. This is done to improve spatial cache locality in
    * the shader and to avoid having two separate shaders for each blur pass. */
-  const Domain domain = input.domain();
-  const int2 transposed_domain = int2(domain.data_size.y, domain.data_size.x);
-
-  output.allocate_texture(transposed_domain);
+  output.allocate_texture(input.domain().transposed());
 
   switch (input.type()) {
     case ResultType::Float: