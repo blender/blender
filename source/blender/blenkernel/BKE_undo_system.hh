blender/blender

commit 1dc6558c5dc03c93ab1e174e25b44e8b6d6e7a5d

Fix #144058: push undo step when stopping playback
Starting and stopping playback used to not register undo steps.
This causes the issue that the current frame jumps around when undoing
an action just after playback stop.
The fix is to register an undo step when the playback is stopped.
It is important to only do so on stop since on playback start nothing
meaningful has happened yet.

This uses grouped undo with the undo group "Frame Change"
which is also used for scrubbing. This makes it so that a sequence
of inputs of either playback or scrubbing will be undone in one go.

Discussed in a Grease Pencil module meeting [1]
Discussed in an Animation & Rigging module meeting [2]

[1] https://devtalk.blender.org/t/2026-03-02-grease-pencil-module-meeting/44474
[2] https://devtalk.blender.org/t/2026-02-10-animation-rigging-module-meeting/44314

Pull Request: https://projects.blender.org/blender/blender/pulls/154141

source/blender/editors/screen/screen_ops.cc
@@ -72,6 +72,7 @@
 #include "ED_screen_types.hh"
 #include "ED_sequencer.hh"
 #include "ED_space_graph.hh"
+#include "ED_undo.hh"
 #include "ED_view3d.hh"
 
 #include "RNA_access.hh"
@@ -6496,7 +6497,15 @@ static wmOperatorStatus screen_animation_play_exec(bContext *C, wmOperator *op)
     sync = RNA_boolean_get(op->ptr, "sync");
   }
 
-  return ED_screen_animation_play(C, sync, mode);
+  const wmOperatorStatus status = ED_screen_animation_play(C, sync, mode);
+
+  if (!ED_screen_animation_playing(CTX_wm_manager(C))) {
+    /* Only pushing undo step when stopping playback so that there are no undo steps that seemingly
+     * do nothing. Creating an undo step is important though so that when undoing an action right
+     * after playback stop doesn't also change the current frame. See #144058. */
+    ED_undo_grouped_push_op(C, op);
+  }
+  return status;
 }
 
 static void SCREEN_OT_animation_play(wmOperatorType *ot)
@@ -6512,6 +6521,7 @@ static void SCREEN_OT_animation_play(wmOperatorType *ot)
   ot->exec = screen_animation_play_exec;
 
   ot->poll = operator_screenactive_norender;
+  ot->undo_group = "Frame Change";
 
   prop = RNA_def_boolean(
       ot->srna, "reverse", false, "Play in Reverse", "Animation is played backwards");

commit 1ceafc6bd0c1fef4637a4dda1a3390b5545dd4f0

Fix #157688: Playback removes redo steps
Caused by 1dc6558c5dc
With that commit, playing back would introduce an undo step.
While that fixes the reported issue, it created a new issue in that
this would remove all **redo** steps.
Artists relied on that behavior to do A/B comparisons of animation.
The fix is to **only add an undo step if there is no redo available**.

To do this, I had to add a new API function to the BKE undo system
`BKE_undosys_stack_has_redo`.

As it stands this only fixes it for playback.
Scrubbing still adds an undo step regardless of the redo steps.

Pull Request: https://projects.blender.org/blender/blender/pulls/158119

source/blender/blenkernel/BKE_undo_system.hh
@@ -207,6 +207,10 @@ void BKE_undosys_stack_clear(UndoStack *ustack);
 void BKE_undosys_stack_clear_active(UndoStack *ustack);
 /* name optional */
 bool BKE_undosys_stack_has_undo(const UndoStack *ustack, const char *name);
+/**
+ * Returns true if there is a redo step from the stack's active step.
+ */
+bool BKE_undosys_stack_has_redo(const UndoStack *ustack);
 void BKE_undosys_stack_init_from_main(UndoStack *ustack, Main *bmain);
 /* Called after #BKE_undosys_stack_init_from_main. */
 void BKE_undosys_stack_init_from_context(UndoStack *ustack, bContext *C);

source/blender/blenkernel/intern/undo_system.cc
@@ -388,6 +388,14 @@ bool BKE_undosys_stack_has_undo(const UndoStack *ustack, const char *name)
   return !BLI_listbase_is_empty(&ustack->steps);
 }
 
+bool BKE_undosys_stack_has_redo(const UndoStack *ustack)
+{
+  if (!ustack->step_active) {
+    return false;
+  }
+  return ustack->step_active->next != nullptr;
+}
+
 UndoStep *BKE_undosys_stack_active_with_type(UndoStack *ustack, const UndoType *ut)
 {
   UndoStep *us = ustack->step_active;

source/blender/editors/include/ED_undo.hh
@@ -64,6 +64,10 @@ void ED_undo_operator_repeat_cb_evt(bContext *C, void *arg_op, int arg_unused);
  * Name optionally, function used to check for operator redo panel.
  */
 bool ED_undo_is_valid(const bContext *C, const char *undoname);
+/**
+ * Returns true if there are redo steps available.
+ */
+bool ED_undo_has_redo_step(const bContext *C);
 
 bool ED_undo_is_memfile_compatible(const bContext *C);
 

source/blender/editors/screen/screen_ops.cc
@@ -6681,10 +6681,11 @@ static wmOperatorStatus screen_animation_play_exec(bContext *C, wmOperator *op)
 
   const wmOperatorStatus status = ED_screen_animation_play(C, sync, mode);
 
-  if (!ED_screen_animation_playing(CTX_wm_manager(C))) {
+  if (!ED_screen_animation_playing(CTX_wm_manager(C)) && !ED_undo_has_redo_step(C)) {
     /* Only pushing undo step when stopping playback so that there are no undo steps that seemingly
-     * do nothing. Creating an undo step is important though so that when undoing an action right
-     * after playback stop doesn't also change the current frame. See #144058. */
+     * do nothing. Creating an undo step is important though, so that undoing an action right
+     * after playback stop doesn't also change the current frame. See #144058. However adding an
+     * undo step would remove any redo steps so we only do it when there are none. See #157688.*/
     ED_undo_grouped_push_op(C, op);
   }
   return status;

source/blender/editors/undo/ed_undo.cc
@@ -387,6 +387,12 @@ bool ED_undo_is_valid(const bContext *C, const char *undoname)
   return BKE_undosys_stack_has_undo(wm->runtime->undo_stack, undoname);
 }
 
+bool ED_undo_has_redo_step(const bContext *C)
+{
+  const wmWindowManager *wm = CTX_wm_manager(C);
+  return BKE_undosys_stack_has_redo(wm->runtime->undo_stack);
+}
+
 bool ED_undo_is_memfile_compatible(const bContext *C)
 {
   /* Some modes don't co-exist with memfile undo, disable their use: #60593