e9d8a09756fab4fd011f7690a51635f7a089ac90
Jonas Holzman <jonas@blender.org>
2026-05-14 12:07:06 +0200

    Fix: OpenSSL: Conditionally add Android API extra argument for non-cross-compiled builds

--- build_files/build_environment/cmake/ssl.cmake
+++ build_files/build_environment/cmake/ssl.cmake
@@ -41,6 +41,29 @@ else()
     endif()
   endif()
 
+  set(SSL_EXTRA_ARGS
+    --prefix=${LIBDIR}/ssl
+    --openssldir=${LIBDIR}/ssl
+    # Without this: Python will use the build directories.
+    # using the system directory `/etc/ssl` might seem the obvious choice,
+    # there is no guarantee the version of SSL used with Blender is compatible with the systems,
+    # where changes to the SSL configuration format can cause SSL not to load (see #114452).
+    # So reference a directory known not to exist. Ideally Blender could distribute its own SSL
+    # directory, but this isn't compatible with hard coded paths.
+    # See #111132 & https://github.com/openssl/openssl/issues/20185 for details.
+    -DOPENSSLDIR=\\"/dev/null\\"
+    no-shared
+    no-idea no-mdc2 no-rc5 no-zlib no-ssl3 enable-unit-test no-ssl3-method enable-rfc3779 enable-cms
+    --config=${CMAKE_CURRENT_SOURCE_DIR}/cmake/ssl.conf
+  )
+
+  if(ANDROID)
+    set(SSL_EXTRA_ARGS
+      ${SSL_EXTRA_ARGS}
+      -D__ANDROID_API__=${ANDROID_PLATFORM_LEVEL}
+    )
+  endif()
+
   ExternalProject_Add(external_ssl
     URL file://${PACKAGE_DIR}/${SSL_FILE}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
@@ -50,20 +73,7 @@ else()
     CONFIGURE_COMMAND ${SSL_CONFIGURE_ENV} &&
       cd ${BUILD_DIR}/ssl/src/external_ssl/ &&
       ${SSL_CONFIGURE_COMMAND}
-        --prefix=${LIBDIR}/ssl
-        --openssldir=${LIBDIR}/ssl
-        # Without this: Python will use the build directories.
-        # using the system directory `/etc/ssl` might seem the obvious choice,
-        # there is no guarantee the version of SSL used with Blender is compatible with the systems,
-        # where changes to the SSL configuration format can cause SSL not to load (see #114452).
-        # So reference a directory known not to exist. Ideally Blender could distribute its own SSL
-        # directory, but this isn't compatible with hard coded paths.
-        # See #111132 & https://github.com/openssl/openssl/issues/20185 for details.
-        -DOPENSSLDIR=\\"/dev/null\\"
-        no-shared
-        no-idea no-mdc2 no-rc5 no-zlib no-ssl3 enable-unit-test no-ssl3-method enable-rfc3779 enable-cms
-        --config=${CMAKE_CURRENT_SOURCE_DIR}/cmake/ssl.conf
-        -D__ANDROID_API__=${ANDROID_PLATFORM_LEVEL}
+        ${SSL_EXTRA_ARGS}
         ${SSL_OS_COMPILER}
 
     BUILD_COMMAND ${SSL_CONFIGURE_ENV} &&
