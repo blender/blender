a8d933daca329c4b74f0e936ef2d28a357445377
Jonas Holzman <jonas@blender.org>
2026-05-15 16:37:21 +0200

    Deps: Don't build Vulkan Loader on Android as Android provides its own implementation

--- build_files/build_environment/cmake/vulkan.cmake
+++ build_files/build_environment/cmake/vulkan.cmake
@@ -84,64 +84,67 @@ add_dependencies(
   external_spirv_headers
 )
 
-if(UNIX AND NOT APPLE)
-  # These are used in `cmake/FindWayland.cmake` from `external_vulkan_loader`.
-  # NOTE: When upgrading to CMAKE 3.22 it would be cleaner to use: `PKG_CONFIG_ARGN`,
-  # so `pkgconfig` would find wayland.
-  set(VULKAN_LOADER_EXTRA_ARGS
-    ${VULKAN_LOADER_EXTRA_ARGS}
-    -DPKG_WAYLAND_INCLUDE_DIRS=${LIBDIR}/wayland/include
-    -DPKG_WAYLAND_LIBRARY_DIRS=${LIBDIR}/wayland/lib64
-  )
-elseif(BLENDER_PLATFORM_WINDOWS_ARM)
-  set(VULKAN_LOADER_EXTRA_ARGS
-    -DUSE_MASM=OFF
-    -DVulkanHeaders_DIR=${LIBDIR}/vulkan_headers/share/cmake/VulkanHeaders
-  )
-endif()
-
-ExternalProject_Add(external_vulkan_loader
-  URL file://${PACKAGE_DIR}/${VULKAN_LOADER_FILE}
-  URL_HASH ${VULKAN_LOADER_HASH_TYPE}=${VULKAN_LOADER_HASH}
-  PREFIX ${BUILD_DIR}/vulkan_loader
-
-  CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX=${LIBDIR}/vulkan_loader
-    -Wno-dev
-    ${DEFAULT_CMAKE_FLAGS}
-    ${VULKAN_LOADER_EXTRA_ARGS}
-
-  INSTALL_DIR ${LIBDIR}/vulkan_loader
-)
-
-add_dependencies(
-  external_vulkan_loader
-  external_vulkan_headers
-)
-
-if(UNIX AND NOT APPLE)
-  add_dependencies(
-    external_vulkan_loader
-    external_wayland
-  )
-endif()
-
-if(WIN32)
-  if(BUILD_MODE STREQUAL Release)
-    ExternalProject_Add_Step(external_vulkan_loader after_install
-      COMMAND ${CMAKE_COMMAND} -E copy_directory
-        ${LIBDIR}/vulkan_loader/
-        ${HARVEST_TARGET}/vulkan
-      COMMAND ${CMAKE_COMMAND} -E copy_directory
-        ${LIBDIR}/vulkan_headers/
-        ${HARVEST_TARGET}/vulkan
-      DEPENDEES install
+# Android provide its own Vulkan Loader implementation.
+if(NOT ANDROID)
+  if(UNIX AND NOT APPLE)
+    # These are used in `cmake/FindWayland.cmake` from `external_vulkan_loader`.
+    # NOTE: When upgrading to CMAKE 3.22 it would be cleaner to use: `PKG_CONFIG_ARGN`,
+    # so `pkgconfig` would find wayland.
+    set(VULKAN_LOADER_EXTRA_ARGS
+      ${VULKAN_LOADER_EXTRA_ARGS}
+      -DPKG_WAYLAND_INCLUDE_DIRS=${LIBDIR}/wayland/include
+      -DPKG_WAYLAND_LIBRARY_DIRS=${LIBDIR}/wayland/lib64
+    )
+  elseif(BLENDER_PLATFORM_WINDOWS_ARM)
+    set(VULKAN_LOADER_EXTRA_ARGS
+      -DUSE_MASM=OFF
+      -DVulkanHeaders_DIR=${LIBDIR}/vulkan_headers/share/cmake/VulkanHeaders
     )
   endif()
-else()
-  harvest(external_vulkan_loader vulkan_headers vulkan "*")
-  harvest_rpath_lib(external_vulkan_loader vulkan_loader/lib vulkan/lib "*${SHAREDLIBEXT}*")
-  if(APPLE)
-    harvest(external_vulkan_loader vulkan_loader/loader vulkan/loader "*")
+
+  ExternalProject_Add(external_vulkan_loader
+    URL file://${PACKAGE_DIR}/${VULKAN_LOADER_FILE}
+    URL_HASH ${VULKAN_LOADER_HASH_TYPE}=${VULKAN_LOADER_HASH}
+    PREFIX ${BUILD_DIR}/vulkan_loader
+
+    CMAKE_ARGS
+      -DCMAKE_INSTALL_PREFIX=${LIBDIR}/vulkan_loader
+      -Wno-dev
+      ${DEFAULT_CMAKE_FLAGS}
+      ${VULKAN_LOADER_EXTRA_ARGS}
+
+    INSTALL_DIR ${LIBDIR}/vulkan_loader
+  )
+
+  add_dependencies(
+    external_vulkan_loader
+    external_vulkan_headers
+  )
+
+  if(UNIX AND NOT APPLE)
+    add_dependencies(
+      external_vulkan_loader
+      external_wayland
+    )
+  endif()
+
+  if(WIN32)
+    if(BUILD_MODE STREQUAL Release)
+      ExternalProject_Add_Step(external_vulkan_loader after_install
+        COMMAND ${CMAKE_COMMAND} -E copy_directory
+          ${LIBDIR}/vulkan_loader/
+          ${HARVEST_TARGET}/vulkan
+        COMMAND ${CMAKE_COMMAND} -E copy_directory
+          ${LIBDIR}/vulkan_headers/
+          ${HARVEST_TARGET}/vulkan
+        DEPENDEES install
+      )
+    endif()
+  else()
+    harvest(external_vulkan_loader vulkan_headers vulkan "*")
+    harvest_rpath_lib(external_vulkan_loader vulkan_loader/lib vulkan/lib "*${SHAREDLIBEXT}*")
+    if(APPLE)
+      harvest(external_vulkan_loader vulkan_loader/loader vulkan/loader "*")
+    endif()
   endif()
 endif()
