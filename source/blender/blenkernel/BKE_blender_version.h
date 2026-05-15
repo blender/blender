96bbff21afa5942c54dcde670c313074b989fcd2
Thomas Dinges <thomas@blender.org>
2026-05-15 08:02:13 +0200

    Release: Bump to 5.1.2 release candidate

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -22,9 +22,9 @@ namespace blender {
 /** Blender major and minor version. */
 #define BLENDER_VERSION 501
 /** Blender patch version for bug-fix releases. */
-#define BLENDER_VERSION_PATCH 1
+#define BLENDER_VERSION_PATCH 2
 /** Blender release cycle stage: alpha/beta/rc/release. */
-#define BLENDER_VERSION_CYCLE release
+#define BLENDER_VERSION_CYCLE rc
 /** Blender release type suffix. LTS or blank. */
 #define BLENDER_VERSION_SUFFIX
 
