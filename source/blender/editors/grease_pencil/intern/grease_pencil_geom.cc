f08b39cef5837468c118102f9dca512fa22476a7
Philipp Oeser <philipp@blender.org>
2026-05-21 13:03:53 +0200

    Fix #157998: Grease Pencil Simplify Stroke 'Merge' breaks weights
    Vertex weights values were correct, just the `vertex_group_names` were
    not copied to the newly created `CurvesGeometry`.
    
    NOTE: this is fixing the operator. Curves already infected are not
    "corrected" with this PR
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/158848

--- source/blender/editors/grease_pencil/intern/grease_pencil_geom.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_geom.cc
@@ -21,6 +21,7 @@
 #include "BKE_attribute.hh"
 #include "BKE_curves.hh"
 #include "BKE_curves_utils.hh"
+#include "BKE_deform.hh"
 #include "BKE_grease_pencil.hh"
 
 #include "DNA_curves_types.h"
@@ -225,6 +226,9 @@ bke::CurvesGeometry curves_merge_by_distance(const bke::CurvesGeometry &src_curv
   src_curves.ensure_evaluated_lengths();
 
   bke::CurvesGeometry dst_curves = bke::curves::copy_only_curve_domain(src_curves);
+
+  BKE_defgroup_copy_list(&dst_curves.vertex_group_names, &src_curves.vertex_group_names);
+
   MutableSpan<int> dst_offsets = dst_curves.offsets_for_write();
 
   std::atomic<int> total_duplicate_count = 0;
