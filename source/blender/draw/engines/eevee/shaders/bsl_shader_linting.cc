cc5a028888dc140c38fb537afd439c0f45d070eb
Clément Foucault <foucault.clem@gmail.com>
2026-05-22 15:43:23 +0200

    EEVEE: Port lightprobe sphere baking to BSL
    Rel !151442

--- source/blender/draw/engines/eevee/shaders/bsl_shader_linting.cc
+++ source/blender/draw/engines/eevee/shaders/bsl_shader_linting.cc
@@ -25,6 +25,7 @@
 #include "eevee_light_shadow_setup.bsl.hh"           /* IWYU pragma: export */
 #include "eevee_light_shape_display.bsl.hh"          /* IWYU pragma: export */
 #include "eevee_lightprobe_display.bsl.hh"           /* IWYU pragma: export */
+#include "eevee_lightprobe_sphere_bake.bsl.hh"       /* IWYU pragma: export */
 #include "eevee_lightprobe_volume_bake.bsl.hh"       /* IWYU pragma: export */
 #include "eevee_lightprobe_volume_load.bsl.hh"       /* IWYU pragma: export */
 #include "eevee_ltc_lib.bsl.hh"                      /* IWYU pragma: export */
