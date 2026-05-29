3eee99eb92b26a980bf995d1fb2145856d562f2c
Miguel Pozo <pragma37@gmail.com>
2026-05-29 15:07:58 +0200

    Tests: Add missing render tests
    Part of #148449.
    Add untested features, mostly driven by recent regressions.
    
    - camera_negative_scale
    - camera_negative_scale_ortho
    - motion_vectors
    - holdout (updated to test multiple methods, transparency, and
      reflections)
    - material_output_selection
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/154032

--- tests/python/storm_render_tests.py
+++ tests/python/storm_render_tests.py
@@ -87,6 +87,7 @@ BLOCKLIST_METAL = [
 # AMD seems to have similar limitations as Metal for transparency.
 BLOCKLIST_AMD = BLOCKLIST_METAL + [
     "volume_tricubic_interpolation.blend",
+    "holdout.blend",
 ]
 
 # Minor difference in texture coordinate for white noise hash.
