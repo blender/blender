bdef4be6770228f182bf806dba868cc10805aa61
Campbell Barton <campbell@blender.org>
2026-05-31 21:17:49 +1000

    Fix: error in MESH_OT_mod_weighted_strength selecting hidden face
    No check for hidden face before selecting, causing ill defined
    behavior for tools that ran after.
    
    Also don't add to selection-history as it was adding faces in the
    order they exist in the mesh - which isn't a user defined order.
    
    Ref !159504

--- source/blender/editors/mesh/editmesh_tools.cc
+++ source/blender/editors/mesh/editmesh_tools.cc
@@ -9991,14 +9991,11 @@ static wmOperatorStatus edbm_mod_weighted_strength_exec(bContext *C, wmOperator 
     }
     else {
       BM_ITER_MESH (f, &fiter, bm, BM_FACES_OF_MESH) {
+        if (BM_elem_flag_test(f, BM_ELEM_HIDDEN)) {
+          continue;
+        }
         const int *strength = static_cast<int *>(BM_ELEM_CD_GET_VOID_P(f, cd_prop_int_offset));
-        if (*strength == face_strength) {
-          BM_face_select_set(bm, f, true);
-          BM_select_history_store(bm, f);
-        }
-        else {
-          BM_face_select_set(bm, f, false);
-        }
+        BM_face_select_set(bm, f, *strength == face_strength);
       }
     }
 
