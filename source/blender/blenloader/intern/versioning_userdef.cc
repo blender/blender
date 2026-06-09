265e3ca5b41c480c075f00d54b033eeca2a90dc8
Julian Eisel <julian@blender.org>
2026-06-09 16:47:23 +0200

    Fix #159775: Preferences from 5.2 don't have online essentials enabled
    The "Include Online Essentials" Preferences option was introduced in the
    release branch, and versioning was correctly done there for files older
    than 5.2 beta. But for the main branch we need to do versioning again,
    so that Preferences stored in 5.2 beta or newer still have the option
    enabled by default correctly.

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -1776,13 +1776,14 @@ void blo_do_versions_userdef(UserDef *userdef)
     userdef->experimental.use_remote_asset_libraries = true;
   }
 
-  if (!USER_VERSION_ATLEAST(502, 42)) {
-    userdef->asset_flag |= USER_ASSETS_USE_ONLINE_ESSENTIALS;
-  }
-
   if (!USER_VERSION_ATLEAST(503, 1)) {
     userdef->pref_flag |= USER_PREF_FLAG_PROJECT_SAVE;
   }
+
+  if (!USER_VERSION_ATLEAST(503, 2)) {
+    userdef->asset_flag |= USER_ASSETS_USE_ONLINE_ESSENTIALS;
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a USER_VERSION_ATLEAST check.
