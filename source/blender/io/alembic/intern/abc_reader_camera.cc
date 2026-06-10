e3368f4c73f755a80a82fe98aeb81efa5f0b54e3
Kévin Dietrich <kevin.dietrich@mailoo.org>
2026-06-10 18:31:35 +0200

    Fix: missing property animation for Alembic cameras
    The RNA path for the properties relating to depth of field on steroscopy
    were wrong, so the FCurves for those properties were silently ignored.
    
    To fix this the ENUMERATE_CAMERA_PROPERTIES macro is extended with a
    `short_name` used for creating members and variables, and the `rna_name`
    is replaced by a proper `rna_path`. The test file is also updated to
    include tests for all properties which should have been done from the
    get-go.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/159812

--- source/blender/io/alembic/intern/abc_reader_camera.cc
+++ source/blender/io/alembic/intern/abc_reader_camera.cc
@@ -113,19 +113,20 @@ void AbcCameraReader::readObjectData(Main *bmain, const ISampleSelector &sample_
   m_object->data = id_cast<ID *>(bcam);
 }
 
-/* The macro that needs to be passed should have arguments : (rna_name, member_accessor) */
+/* The macro that needs to be passed should have arguments :
+ * (short_name, rna_path, member_accessor) */
 #define ENUMERATE_CAMERA_PROPERTIES(X) \
-  X(lens, lens) \
-  X(sensor_width, sensor_x) \
-  X(sensor_height, sensor_y) \
-  X(clip_start, clip_start) \
-  X(clip_end, clip_end) \
-  X(shift_x, shiftx) \
-  X(shift_y, shifty) \
-  X(focus_distance, dof.focus_distance) \
-  X(aperture_fstop, dof.aperture_fstop) \
-  X(interocular_distance, stereo.interocular_distance) \
-  X(convergence_distance, stereo.convergence_distance)
+  X(lens, lens, lens) \
+  X(sensor_width, sensor_width, sensor_x) \
+  X(sensor_height, sensor_height, sensor_y) \
+  X(clip_start, clip_start, clip_start) \
+  X(clip_end, clip_end, clip_end) \
+  X(shift_x, shift_x, shiftx) \
+  X(shift_y, shift_y, shifty) \
+  X(focus_distance, dof.focus_distance, dof.focus_distance) \
+  X(aperture_fstop, dof.aperture_fstop, dof.aperture_fstop) \
+  X(interocular_distance, stereo.interocular_distance, stereo.interocular_distance) \
+  X(convergence_distance, stereo.convergence_distance, stereo.convergence_distance)
 
 class CameraFCurveCreationHelper : public FCurveCreationHelper {
   Camera *camera_ = nullptr;
@@ -134,14 +135,15 @@ class CameraFCurveCreationHelper : public FCurveCreationHelper {
   /* Keep track of what has been modified to remove unnecessary fcurves at the end as Alembic
    * seemingly does not have per property information. */
   struct MemberModified {
-#define DECLARE_MEMBER(rna_name, member_accessor) bool rna_name = false;
+#define DECLARE_MEMBER(short_name, rna_path, member_accessor) bool short_name = false;
     ENUMERATE_CAMERA_PROPERTIES(DECLARE_MEMBER)
 #undef DECLARE_MEMBER
   };
 
   MemberModified member_modified_{};
 
-#define DECLARE_FCURVES(rna_name, member_accessor) FCurve *rna_name##_fcurve = nullptr;
+#define DECLARE_FCURVES(short_name, rna_path, member_accessor) \
+  FCurve *short_name##_fcurve = nullptr;
   ENUMERATE_CAMERA_PROPERTIES(DECLARE_FCURVES)
 #undef DECLARE_FCURVES
 
@@ -153,8 +155,8 @@ class CameraFCurveCreationHelper : public FCurveCreationHelper {
 
   void create_fcurves(const int sample_count) override
   {
-#define CREATE_FCURVE(rna_name, member_accessor) \
-  rna_name##_fcurve = create_fcurve({#rna_name, 0}, sample_count);
+#define CREATE_FCURVE(short_name, rna_path, member_accessor) \
+  short_name##_fcurve = create_fcurve({#rna_path, 0}, sample_count);
     ENUMERATE_CAMERA_PROPERTIES(CREATE_FCURVE)
 #undef CREATE_FCURVE
   }
@@ -165,19 +167,21 @@ class CameraFCurveCreationHelper : public FCurveCreationHelper {
     Camera last_camera = *camera_;
     read_camera_sample(camera_, schema_, sample_info.selector);
 
-#define SET_FCURVE_SAMPLE(rna_name, member_accessor) \
-  set_fcurve_sample( \
-      rna_name##_fcurve, sample_info.sample_index, sample_info.frame, camera_->member_accessor); \
-  member_modified_.rna_name |= last_camera.member_accessor != camera_->member_accessor;
+#define SET_FCURVE_SAMPLE(short_name, rna_path, member_accessor) \
+  set_fcurve_sample(short_name##_fcurve, \
+                    sample_info.sample_index, \
+                    sample_info.frame, \
+                    camera_->member_accessor); \
+  member_modified_.short_name |= last_camera.member_accessor != camera_->member_accessor;
     ENUMERATE_CAMERA_PROPERTIES(SET_FCURVE_SAMPLE)
 #undef SET_FCURVE_SAMPLE
   }
 
   void remove_unnecessary_fcurves() override
   {
-#define REMOVE_UNNECESSARY_FCURVE(rna_name, member_accessor) \
-  if (member_modified_.rna_name == false) { \
-    channelbag->fcurve_remove(*rna_name##_fcurve); \
+#define REMOVE_UNNECESSARY_FCURVE(short_name, rna_path, member_accessor) \
+  if (member_modified_.short_name == false) { \
+    channelbag->fcurve_remove(*short_name##_fcurve); \
   }
     ENUMERATE_CAMERA_PROPERTIES(REMOVE_UNNECESSARY_FCURVE)
 #undef REMOVE_UNNECESSARY_FCURVE
