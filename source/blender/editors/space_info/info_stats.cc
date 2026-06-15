19f0da436a7cb165b7d54e5f1e3eeec73eddb31d
Guillermo Venegas <guillermovcra@gmail.com>
2026-06-15 17:55:30 +0200

    Fix #159496: Unbreak translated Scene Duration format string
    `Duration: %s (Frame %i/%i)` is translated as `(i/%i% ﻢﯾﺮﻓ) %s‬‪ :ﮧﯿﻧﺍﺭﻭﺩ`
    in `Urdu`, so `BLI_snprintf_utf8_rlen` would try to  use `frame_count`
    int value as a string pointer value, making this to crash.
    
    This uses `fmt::format` instead which allows positional args,
    so the translation for this case could be `({1}/{2} ﻢﯾﺮﻓ) {0} :ﮧﯿﻧﺍﺭﻭﺩ`
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/159529

--- source/blender/editors/space_info/info_stats.cc
+++ source/blender/editors/space_info/info_stats.cc
@@ -781,13 +781,12 @@ const char *ED_info_statusbar_string_ex(Main *bmain,
                                   FRA2TIME(frame_count),
                                   scene->frames_per_second(),
                                   U.timecode_style);
-    ofs += BLI_snprintf_utf8_rlen(info + ofs,
-                                  len - ofs,
-
-                                  IFACE_("Duration: %s (Frame %i/%i)"),
-                                  timecode,
-                                  relative_current_frame,
-                                  frame_count);
+    const std::string scene_duration = fmt::format(
+        fmt::runtime(IFACE_("Duration: {} (Frame {}/{})")),
+        timecode,
+        relative_current_frame,
+        frame_count);
+    ofs += BLI_strncpy_utf8_rlen(info + ofs, scene_duration.c_str(), len - ofs);
   }
 
   /* Memory status. */
