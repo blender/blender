b9da43df3b4a7b216dfa97f9b20f909f3581dccf
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-06-24 08:15:59 +0200

    UI: Use Action Constraint Icon
    Changes the icon of the action constraint to use
    the already existing icon for it.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/160563

--- source/blender/makesrna/intern/rna_constraint.cc
+++ source/blender/makesrna/intern/rna_constraint.cc
@@ -133,7 +133,7 @@ const EnumPropertyItem rna_enum_constraint_type_items[] = {
     RNA_ENUM_ITEM_HEADING(N_("Relationship"), nullptr),
     {CONSTRAINT_TYPE_ACTION,
      "ACTION",
-     ICON_ACTION,
+     ICON_CON_ACTION,
      "Action",
      "Use transform property of target to look up pose for owner from an Action"},
     {CONSTRAINT_TYPE_ARMATURE,
@@ -1914,7 +1914,7 @@ static void rna_def_constraint_action(BlenderRNA *brna)
   RNA_def_struct_ui_text(
       srna, "Action Constraint", "Map an action to the transform axes of a bone");
   RNA_def_struct_sdna_from(srna, "bActionConstraint", "data");
-  RNA_def_struct_ui_icon(srna, ICON_ACTION);
+  RNA_def_struct_ui_icon(srna, ICON_CON_ACTION);
 
   rna_def_constraint_target_common(srna);
 
