3ec6bbef164729e44b7ce04cc9eb81747ab51753
Guillermo Venegas <guillermovcra@gmail.com>
2026-06-25 21:49:04 +0200

    Fix: UI: Category tabs width not snapped in clip editor tools sidebar
    This enables regions with `ARegionTypeFlag::UsePanelCategoryTabs` to fix
    their size when `USER_UIFLAG2_PANEL_TABS_COMPACT` flag changes.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/160681

--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -1778,9 +1778,7 @@ static void region_rect_recursive(
       }
     }
     else {
-      if (region->regiontype == RGN_TYPE_UI &&
-          BKE_regiontype_uses_categories(region->runtime->type))
-      {
+      if (BKE_regiontype_uses_category_tabs(region->runtime->type)) {
         /* Update category tab width when #USER_UIFLAG2_PANEL_TABS_COMPACT flag is set/unset. */
         const float aspect = (region->v2d.flag & V2D_IS_INIT) ?
                                  (BLI_rctf_size_y(&region->v2d.cur) /
