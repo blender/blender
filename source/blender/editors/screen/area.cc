94263050205b8efe9ec04b6c775e7ca5afc8819e
Julian Eisel <julian@blender.org>
2026-06-25 17:32:17 +0200

    Fix: Division by 0 when opening dopesheet
    Opening a Dopesheet would do some divisions by zero, which some
    sanitizers reported. This also made the new UI tests proposed in
    blender/blender!158780 fail.
    
    Same fix as for f07288a589, but for larger areas.
    
    Also use const for the variable introduced by f07288a589.

--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -1747,10 +1747,10 @@ static void region_rect_recursive(
       region->flag |= RGN_FLAG_TOO_SMALL;
     }
     else if (width < prefsizex) {
-      float aspect = (region->v2d.flag & V2D_IS_INIT) ?
-                         (BLI_rctf_size_y(&region->v2d.cur) /
-                          (BLI_rcti_size_y(&region->v2d.mask) + 1)) :
-                         1.0f;
+      const float aspect = (region->v2d.flag & V2D_IS_INIT) ?
+                               (BLI_rctf_size_y(&region->v2d.cur) /
+                                (BLI_rcti_size_y(&region->v2d.mask) + 1)) :
+                               1.0f;
 
       const bool has_tabs = BKE_regiontype_uses_category_tabs(region->runtime->type);
       const int min = int(UI_SCALE_FAC *
@@ -1782,8 +1782,10 @@ static void region_rect_recursive(
           BKE_regiontype_uses_categories(region->runtime->type))
       {
         /* Update category tab width when #USER_UIFLAG2_PANEL_TABS_COMPACT flag is set/unset. */
-        const float aspect = BLI_rctf_size_y(&region->v2d.cur) /
-                             (BLI_rcti_size_y(&region->v2d.mask) + 1);
+        const float aspect = (region->v2d.flag & V2D_IS_INIT) ?
+                                 (BLI_rctf_size_y(&region->v2d.cur) /
+                                  (BLI_rcti_size_y(&region->v2d.mask) + 1)) :
+                                 1.0f;
         const int tab_auto_snap_width = (UI_PANEL_CATEGORY_MIN_WIDTH + ui::PANEL_MIN_DRAW_WIDTH) *
                                         UI_SCALE_FAC / aspect;
         if (prefsizex < tab_auto_snap_width) {
