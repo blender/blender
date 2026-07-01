blender/blender

commit 530ceecde2f88fba1045a334891bdd8469417b4d

I18n: Translate color space menus
Color space UI was changed in Blender 5.2. Since then menus and
submenus are no longer translated, though the labels and descriptions
are still extracted from the OpenColorIO config.

This commit adds translation for color space names and descriptions
(menu entries), as well as families (submenus).

Families need to be extracted from the OCIO config, and translated
when showing menus, but they may be composed of multiple path
components representing submenus, so each component is extracted and
translated independently.

Reported by Ye Gui in #43295.

Pull Request: https://projects.blender.org/blender/blender/pulls/160883

source/blender/editors/interface/interface_ops_color.cc
@@ -172,7 +172,7 @@ static void colorspaces_menu_draw(const bContext *C, Menu *menu)
   for (const StringRef &dir : subdirs) {
     std::string path = root + std::string(dir);
     col.context_string_set("colorspace_parent_path", path);
-    col.menu(COLORSPACE_MENU_ID, dir, ICON_NONE);
+    col.menu(COLORSPACE_MENU_ID, IFACE_(dir), ICON_NONE);
   }
 
   if (!subdirs.is_empty() && !colorspaces_at_this_level.is_empty()) {
@@ -183,9 +183,9 @@ static void colorspaces_menu_draw(const bContext *C, Menu *menu)
     colorspaces_menu_add_button(col,
                                 ptr,
                                 prop,
-                                IMB_colormanagement_colorspace_get_name(cs),
-                                IMB_colormanagement_colorspace_get_name(cs),
-                                IMB_colormanagement_colorspace_get_description(cs));
+                                IFACE_(IMB_colormanagement_colorspace_get_name(cs)),
+                                IFACE_(IMB_colormanagement_colorspace_get_name(cs)),
+                                TIP_(IMB_colormanagement_colorspace_get_description(cs)));
   }
 
   if (!parent_path) {

scripts/modules/_bl_i18n_utils/bl_extract_messages.py
@@ -1022,6 +1022,14 @@ def dump_ocio_config(msgs, reports, settings):
             msgs, settings.DEFAULT_CONTEXT, description, msgsrc,
             reports, None, settings,
         )
+        family = colorspace.getFamily()
+        family_separator = config.getFamilySeparator()
+        for family_part in family.split(family_separator):
+            msgsrc = "Colorspace family from OCIO config"
+            process_msg(
+                msgs, settings.DEFAULT_CONTEXT, family_part, msgsrc,
+                reports, None, settings,
+            )
 
 
 def dump_asset_messages(msgs, reports, settings):

commit 13797784fd8214da0befdb49af86b3e51614ee2e

Fix: I18n: Translated color spaces not selectable
Color spaces were made translatable again in !160883, but both the UI
label and the identifer were translated. This made translated color
spaces impossible to select, as it would fall back on the
default (usually "Linear CIE-XYZ D65" with the default OCIO config).

Regression introduced in 530ceecde2.

Pull Request: https://projects.blender.org/blender/blender/pulls/160925

source/blender/editors/interface/interface_ops_color.cc
@@ -183,7 +183,7 @@ static void colorspaces_menu_draw(const bContext *C, Menu *menu)
     colorspaces_menu_add_button(col,
                                 ptr,
                                 prop,
-                                IFACE_(IMB_colormanagement_colorspace_get_name(cs)),
+                                IMB_colormanagement_colorspace_get_name(cs),
                                 IFACE_(IMB_colormanagement_colorspace_get_name(cs)),
                                 TIP_(IMB_colormanagement_colorspace_get_description(cs)));
   }