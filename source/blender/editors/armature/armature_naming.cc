a30a3117e6db1c49eab17e384080295e9cbf1903
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-07-02 13:06:48 +0200

    Refactor: ED_armature_bone_rename
    No functional changes.
    
    This just inverts the if statement at the start of
    `ED_armature_bone_rename` to return early
    and reduce the indentation of the function.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/160968

--- source/blender/editors/armature/armature_naming.cc
+++ source/blender/editors/armature/armature_naming.cc
@@ -131,10 +131,11 @@ static void constraint_bone_name_fix(Object *rename_ob,
     /* constraint targets */
     if (BKE_constraint_targets_get(&curcon, &targets)) {
       for (bConstraintTarget &ct : targets) {
-        if (ct.tar == rename_ob) {
-          if (STREQ(ct.subtarget, oldname)) {
-            STRNCPY_UTF8(ct.subtarget, newname);
-          }
+        if (ct.tar != rename_ob) {
+          continue;
+        }
+        if (STREQ(ct.subtarget, oldname)) {
+          STRNCPY_UTF8(ct.subtarget, newname);
         }
       }
 
@@ -167,224 +168,226 @@ void ED_armature_bone_rename(Main *bmain,
                              const char *oldnamep,
                              const char *newnamep)
 {
-  Object *ob;
+
+  /* If old and new name match we don't need to do anything. */
+  if (STREQLEN(oldnamep, newnamep, MAXBONENAME)) {
+    return;
+  }
+
   char newname[MAXBONENAME];
   char oldname[MAXBONENAME];
+  /* we alter newname string... so make copy */
+  STRNCPY_UTF8(newname, newnamep);
+  /* we use oldname for search... so make copy */
+  STRNCPY(oldname, oldnamep); /* Allow non UTF8 encoding for the old name. */
 
-  /* names better differ! */
-  if (!STREQLEN(oldnamep, newnamep, MAXBONENAME)) {
+  /* now check if we're in editmode, we need to find the unique name */
+  if (arm->edbo) {
+    EditBone *eBone = ED_armature_ebone_find_name(arm->edbo, oldname);
 
-    /* we alter newname string... so make copy */
-    STRNCPY_UTF8(newname, newnamep);
-    /* we use oldname for search... so make copy */
-    STRNCPY(oldname, oldnamep); /* Allow non UTF8 encoding for the old name. */
+    if (eBone) {
+      ED_armature_ebone_unique_name(arm->edbo, newname, nullptr);
+      STRNCPY_UTF8(eBone->name, newname);
+    }
+    else {
+      return;
+    }
+  }
+  else {
+    Bone *bone = BKE_armature_find_bone_name(arm, oldname);
 
-    /* now check if we're in editmode, we need to find the unique name */
-    if (arm->edbo) {
-      EditBone *eBone = ED_armature_ebone_find_name(arm->edbo, oldname);
+    if (bone) {
+      ed_armature_bone_unique_name(arm, newname);
 
-      if (eBone) {
-        ED_armature_ebone_unique_name(arm->edbo, newname, nullptr);
-        STRNCPY_UTF8(eBone->name, newname);
+      if (arm->bonehash) {
+        BLI_assert(BLI_ghash_haskey(arm->bonehash, bone->name));
+        BLI_ghash_remove(arm->bonehash, bone->name, nullptr, nullptr);
       }
-      else {
-        return;
+
+      STRNCPY_UTF8(bone->name, newname);
+
+      if (arm->bonehash) {
+        BLI_ghash_insert(arm->bonehash, bone->name, bone);
       }
     }
     else {
-      Bone *bone = BKE_armature_find_bone_name(arm, oldname);
+      return;
+    }
+  }
 
-      if (bone) {
-        ed_armature_bone_unique_name(arm, newname);
+  /* force evaluation copy to update database */
+  DEG_id_tag_update(&arm->id, ID_RECALC_SYNC_TO_EVAL);
 
-        if (arm->bonehash) {
-          BLI_assert(BLI_ghash_haskey(arm->bonehash, bone->name));
-          BLI_ghash_remove(arm->bonehash, bone->name, nullptr, nullptr);
+  Object *ob;
+  /* do entire dbase - objects */
+  for (ob = static_cast<Object *>(bmain->objects.first); ob;
+       ob = static_cast<Object *>(ob->id.next))
+  {
+
+    /* we have the object using the armature */
+    if (id_cast<const ID *>(arm) == ob->data) {
+      Object *cob;
+
+      /* Rename the pose channel, if it exists */
+      if (ob->pose) {
+        bPoseChannel *pchan = BKE_pose_channel_find_name(ob->pose, oldname);
+        if (pchan) {
+          GHash *gh = ob->pose->chanhash;
+
+          /* remove the old hash entry, and replace with the new name */
+          if (gh) {
+            BLI_assert(BLI_ghash_haskey(gh, pchan->name));
+            BLI_ghash_remove(gh, pchan->name, nullptr, nullptr);
+          }
+
+          STRNCPY_UTF8(pchan->name, newname);
+
+          if (gh) {
+            BLI_ghash_insert(gh, pchan->name, pchan);
+          }
         }
 
-        STRNCPY_UTF8(bone->name, newname);
-
-        if (arm->bonehash) {
-          BLI_ghash_insert(arm->bonehash, bone->name, bone);
-        }
+        BLI_assert(BKE_pose_channels_is_valid(ob->pose) == true);
       }
-      else {
-        return;
+
+      /* Update any object constraints to use the new bone name */
+      for (cob = static_cast<Object *>(bmain->objects.first); cob;
+           cob = static_cast<Object *>(cob->id.next))
+      {
+        if (cob->constraints.first) {
+          constraint_bone_name_fix(ob, cob, &cob->constraints, oldname, newname);
+        }
+        if (cob->pose) {
+          for (bPoseChannel &pchan : cob->pose->chanbase) {
+            constraint_bone_name_fix(ob, cob, &pchan.constraints, oldname, newname);
+          }
+        }
       }
     }
 
-    /* force evaluation copy to update database */
-    DEG_id_tag_update(&arm->id, ID_RECALC_SYNC_TO_EVAL);
+    /* See if an object is parented to this armature */
+    if (ob->parent && (ob->parent->data == id_cast<const ID *>(arm))) {
+      if (ob->partype == PARBONE) {
+        /* bone name in object */
+        if (STREQ(ob->parsubstr, oldname)) {
+          STRNCPY_UTF8(ob->parsubstr, newname);
+        }
+      }
+    }
 
-    /* do entire dbase - objects */
-    for (ob = static_cast<Object *>(bmain->objects.first); ob;
-         ob = static_cast<Object *>(ob->id.next))
+    if (BKE_modifiers_uses_armature(ob, arm) && BKE_object_supports_vertex_groups(ob)) {
+      if (BKE_object_defgroup_find_name(ob, newname)) {
+        WM_global_reportf(eReportType::RPT_WARNING,
+                          "New bone name collides with an existing vertex "
+                          "group name, vertex group "
+                          "names are unchanged. (%s::%s)",
+                          &ob->id.name[2],
+                          newname);
+        /* Not renaming vertex group could cause bone to bind to other vertex group, in this case
+         * deformation could change, so we tag this object for depsgraph update. */
+        DEG_id_tag_update(ob->data, ID_RECALC_GEOMETRY);
+      }
+      else if (bDeformGroup *dg = BKE_object_defgroup_find_name(ob, oldname)) {
+        STRNCPY_UTF8(dg->name, newname);
+
+        if (ob->type == OB_GREASE_PENCIL) {
+          /* Update vgroup names stored in CurvesGeometry */
+          BKE_grease_pencil_vgroup_name_update(ob, oldname, dg->name);
+        }
+
+        DEG_id_tag_update(ob->data, ID_RECALC_GEOMETRY);
+      }
+    }
+
+    /* fix modifiers that might be using this name */
+    for (ModifierData &md : ob->modifiers) {
+      switch (md.type) {
+        case eModifierType_Hook: {
+          HookModifierData *hmd = reinterpret_cast<HookModifierData *>(&md);
+
+          if (hmd->object && (hmd->object->data == id_cast<const ID *>(arm))) {
+            if (STREQ(hmd->subtarget, oldname)) {
+              STRNCPY_UTF8(hmd->subtarget, newname);
+            }
+          }
+          break;
+        }
+        case eModifierType_UVWarp: {
+          UVWarpModifierData *umd = reinterpret_cast<UVWarpModifierData *>(&md);
+
+          if (umd->object_src && (umd->object_src->data == id_cast<const ID *>(arm))) {
+            if (STREQ(umd->bone_src, oldname)) {
+              STRNCPY_UTF8(umd->bone_src, newname);
+            }
+          }
+          if (umd->object_dst && (umd->object_dst->data == id_cast<const ID *>(arm))) {
+            if (STREQ(umd->bone_dst, oldname)) {
+              STRNCPY_UTF8(umd->bone_dst, newname);
+            }
+          }
+          break;
+        }
+        default:
+          break;
+      }
+    }
+
+    /* fix camera focus */
+    if (ob->type == OB_CAMERA) {
+      Camera *cam = id_cast<Camera *>(ob->data);
+      if ((cam->dof.focus_object != nullptr) &&
+          (cam->dof.focus_object->data == id_cast<const ID *>(arm)))
+      {
+        if (STREQ(cam->dof.focus_subtarget, oldname)) {
+          STRNCPY_UTF8(cam->dof.focus_subtarget, newname);
+          DEG_id_tag_update(&cam->id, ID_RECALC_SYNC_TO_EVAL);
+        }
+      }
+    }
+
+    if (ob->type == OB_GREASE_PENCIL) {
+      GreasePencil &grease_pencil = *id_cast<GreasePencil *>(ob->data);
+      for (bke::greasepencil::Layer *layer : grease_pencil.layers_for_write()) {
+        Object *parent = layer->parent;
+        if (parent == nullptr) {
+          continue;
+        }
+        StringRefNull bone_name = layer->parent_bone_name();
+        if (!bone_name.is_empty() && bone_name == StringRef(oldname)) {
+          layer->set_parent_bone_name(newname);
+        }
+      }
+    }
+
+    DEG_id_tag_update(&ob->id, ID_RECALC_SYNC_TO_EVAL);
+  }
+
+  /* Fix all animdata that may refer to this bone -
+   * we can't just do the ones attached to objects,
+   * since other ID-blocks may have drivers referring to this bone #29822. */
+
+  /* XXX: the ID here is for armatures,
+   * but most bone drivers are actually on the object instead. */
+  {
+    BKE_animdata_fix_paths_rename_all(&arm->id, "bones", oldname, newname);
+  }
+
+  /* correct view locking */
+  {
+    bScreen *screen;
+    for (screen = static_cast<bScreen *>(bmain->screens.first); screen;
+         screen = static_cast<bScreen *>(screen->id.next))
     {
-
-      /* we have the object using the armature */
-      if (id_cast<const ID *>(arm) == ob->data) {
-        Object *cob;
-
-        /* Rename the pose channel, if it exists */
-        if (ob->pose) {
-          bPoseChannel *pchan = BKE_pose_channel_find_name(ob->pose, oldname);
-          if (pchan) {
-            GHash *gh = ob->pose->chanhash;
-
-            /* remove the old hash entry, and replace with the new name */
-            if (gh) {
-              BLI_assert(BLI_ghash_haskey(gh, pchan->name));
-              BLI_ghash_remove(gh, pchan->name, nullptr, nullptr);
-            }
-
-            STRNCPY_UTF8(pchan->name, newname);
-
-            if (gh) {
-              BLI_ghash_insert(gh, pchan->name, pchan);
-            }
-          }
-
-          BLI_assert(BKE_pose_channels_is_valid(ob->pose) == true);
-        }
-
-        /* Update any object constraints to use the new bone name */
-        for (cob = static_cast<Object *>(bmain->objects.first); cob;
-             cob = static_cast<Object *>(cob->id.next))
-        {
-          if (cob->constraints.first) {
-            constraint_bone_name_fix(ob, cob, &cob->constraints, oldname, newname);
-          }
-          if (cob->pose) {
-            for (bPoseChannel &pchan : cob->pose->chanbase) {
-              constraint_bone_name_fix(ob, cob, &pchan.constraints, oldname, newname);
-            }
-          }
-        }
-      }
-
-      /* See if an object is parented to this armature */
-      if (ob->parent && (ob->parent->data == id_cast<const ID *>(arm))) {
-        if (ob->partype == PARBONE) {
-          /* bone name in object */
-          if (STREQ(ob->parsubstr, oldname)) {
-            STRNCPY_UTF8(ob->parsubstr, newname);
-          }
-        }
-      }
-
-      if (BKE_modifiers_uses_armature(ob, arm) && BKE_object_supports_vertex_groups(ob)) {
-        if (BKE_object_defgroup_find_name(ob, newname)) {
-          WM_global_reportf(eReportType::RPT_WARNING,
-                            "New bone name collides with an existing vertex "
-                            "group name, vertex group "
-                            "names are unchanged. (%s::%s)",
-                            &ob->id.name[2],
-                            newname);
-          /* Not renaming vertex group could cause bone to bind to other vertex group, in this case
-           * deformation could change, so we tag this object for depsgraph update. */
-          DEG_id_tag_update(ob->data, ID_RECALC_GEOMETRY);
-        }
-        else if (bDeformGroup *dg = BKE_object_defgroup_find_name(ob, oldname)) {
-          STRNCPY_UTF8(dg->name, newname);
-
-          if (ob->type == OB_GREASE_PENCIL) {
-            /* Update vgroup names stored in CurvesGeometry */
-            BKE_grease_pencil_vgroup_name_update(ob, oldname, dg->name);
-          }
-
-          DEG_id_tag_update(ob->data, ID_RECALC_GEOMETRY);
-        }
-      }
-
-      /* fix modifiers that might be using this name */
-      for (ModifierData &md : ob->modifiers) {
-        switch (md.type) {
-          case eModifierType_Hook: {
-            HookModifierData *hmd = reinterpret_cast<HookModifierData *>(&md);
-
-            if (hmd->object && (hmd->object->data == id_cast<const ID *>(arm))) {
-              if (STREQ(hmd->subtarget, oldname)) {
-                STRNCPY_UTF8(hmd->subtarget, newname);
-              }
-            }
-            break;
-          }
-          case eModifierType_UVWarp: {
-            UVWarpModifierData *umd = reinterpret_cast<UVWarpModifierData *>(&md);
-
-            if (umd->object_src && (umd->object_src->data == id_cast<const ID *>(arm))) {
-              if (STREQ(umd->bone_src, oldname)) {
-                STRNCPY_UTF8(umd->bone_src, newname);
-              }
-            }
-            if (umd->object_dst && (umd->object_dst->data == id_cast<const ID *>(arm))) {
-              if (STREQ(umd->bone_dst, oldname)) {
-                STRNCPY_UTF8(umd->bone_dst, newname);
-              }
-            }
-            break;
-          }
-          default:
-            break;
-        }
-      }
-
-      /* fix camera focus */
-      if (ob->type == OB_CAMERA) {
-        Camera *cam = id_cast<Camera *>(ob->data);
-        if ((cam->dof.focus_object != nullptr) &&
-            (cam->dof.focus_object->data == id_cast<const ID *>(arm)))
-        {
-          if (STREQ(cam->dof.focus_subtarget, oldname)) {
-            STRNCPY_UTF8(cam->dof.focus_subtarget, newname);
-            DEG_id_tag_update(&cam->id, ID_RECALC_SYNC_TO_EVAL);
-          }
-        }
-      }
-
-      if (ob->type == OB_GREASE_PENCIL) {
-        GreasePencil &grease_pencil = *id_cast<GreasePencil *>(ob->data);
-        for (bke::greasepencil::Layer *layer : grease_pencil.layers_for_write()) {
-          Object *parent = layer->parent;
-          if (parent == nullptr) {
+      /* add regions */
+      for (ScrArea &area : screen->areabase) {
+        for (SpaceLink &sl : area.spacedata) {
+          if (sl.spacetype != SPACE_VIEW3D) {
             continue;
           }
-          StringRefNull bone_name = layer->parent_bone_name();
-          if (!bone_name.is_empty() && bone_name == StringRef(oldname)) {
-            layer->set_parent_bone_name(newname);
-          }
-        }
-      }
-
-      DEG_id_tag_update(&ob->id, ID_RECALC_SYNC_TO_EVAL);
-    }
-
-    /* Fix all animdata that may refer to this bone -
-     * we can't just do the ones attached to objects,
-     * since other ID-blocks may have drivers referring to this bone #29822. */
-
-    /* XXX: the ID here is for armatures,
-     * but most bone drivers are actually on the object instead. */
-    {
-      BKE_animdata_fix_paths_rename_all(&arm->id, "bones", oldname, newname);
-    }
-
-    /* correct view locking */
-    {
-      bScreen *screen;
-      for (screen = static_cast<bScreen *>(bmain->screens.first); screen;
-           screen = static_cast<bScreen *>(screen->id.next))
-      {
-        /* add regions */
-        for (ScrArea &area : screen->areabase) {
-          for (SpaceLink &sl : area.spacedata) {
-            if (sl.spacetype == SPACE_VIEW3D) {
-              View3D *v3d = reinterpret_cast<View3D *>(&sl);
-              if (v3d->ob_center && v3d->ob_center->data == id_cast<const ID *>(arm)) {
-                if (STREQ(v3d->ob_center_bone, oldname)) {
-                  STRNCPY_UTF8(v3d->ob_center_bone, newname);
-                }
-              }
+          View3D *v3d = reinterpret_cast<View3D *>(&sl);
+          if (v3d->ob_center && v3d->ob_center->data == id_cast<const ID *>(arm)) {
+            if (STREQ(v3d->ob_center_bone, oldname)) {
+              STRNCPY_UTF8(v3d->ob_center_bone, newname);
             }
           }
         }
