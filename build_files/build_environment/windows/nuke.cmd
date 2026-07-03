c72328991efac41ce82416ad9ec06c84bbd27c8b
Bart van der Braak <bart@blender.org>
2026-07-03 16:31:19 +0200

    Deps: Fix directories in nuke script
    Due to changes in !161017 we need to quickly fix the referenced
    directories in the `nuke.cmd` script. This fix is not elegant at
    all, so I suggest this as a quick "hotfix" and I will try to come
    up with a more elegant solution in !160643
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/161045

--- build_files/build_environment/windows/nuke.cmd
+++ build_files/build_environment/windows/nuke.cmd
@@ -25,3 +25,41 @@ if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" )
 set CurPath=%ROOT%\output\win64_vc15\%1\
 if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
 
+set CurPath=%ROOT%\s\vs1664D\debug\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1664D\x64\debug\external_%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1664D\x64\debug\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1664D\build\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1664R\release\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1664R\x64\Release\external_%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1664R\x64\Release\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1664R\build\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\output\win64_vc16\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+
+set CurPath=%ROOT%\s\vs1764D\debug\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1764D\x64\debug\external_%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1764D\x64\debug\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1764D\build\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1764R\release\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1764R\x64\Release\external_%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1764R\x64\Release\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\s\vs1764R\build\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+set CurPath=%ROOT%\output\win64_vc17\%1\
+if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+
