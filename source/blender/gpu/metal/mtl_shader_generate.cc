blender/blender

commit 348bb4a2d0636c6a4108eabfbe1242d7e67bd70f

GPU: Hardware Ray Queries support (Vulkan-only)
- Add GPU API for building and binding raytracing acceleration
  structures.
- Implement Vulkan support through the VK_KHR_ray_query extension.
- Add BLAS caching to `MeshBatchCache`.

This PR was extracted from #158921, which also included the Workbench
RT Shadows implementation.
#158921 was a continuation of #146142

The Workbench RT shadows will be implemented in a separate PR.

Co-authored by: Jeroen Bakker

Pull Request: https://projects.blender.org/blender/blender/pulls/160674

source/blender/gpu/vulkan/vk_shader.cc
@@ -393,6 +393,9 @@ static void print_resource(std::ostream &os,
       os << "buffer _" << res.storagebuf.name.str_no_array() << " { "
          << info.buffer_typename(res.storagebuf.type_name) << " " << res.storagebuf.name << "; };";
       break;
+    case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+      os << "uniform accelerationStructureEXT " << res.acceleration_structure.name << ";";
+      break;
   }
 }
 
@@ -518,6 +521,7 @@ void VKShader::init(const shader::ShaderCreateInfo &info, bool /*is_codegen_only
   for (const ShaderCreateInfo::SubpassIn &input : info.subpass_inputs_) {
     max_input_attachment_index_ = max_uu(max_input_attachment_index_, uint32_t(input.index));
   }
+  use_ray_query_ = bool(info.builtins_ & BuiltinBits::RAY_QUERY);
 }
 
 VKShader::~VKShader()
@@ -544,16 +548,16 @@ void VKShader::build_shader_module(MutableSpan<StringRefNull> sources,
 
   switch (stage) {
     case shaderc_vertex_shader:
-      source_patch = device.glsl_vertex_patch_get();
+      source_patch = device.glsl_vertex_patch_get(use_ray_query_);
       break;
     case shaderc_geometry_shader:
       source_patch = device.glsl_geometry_patch_get();
       break;
     case shaderc_fragment_shader:
-      source_patch = device.glsl_fragment_patch_get();
+      source_patch = device.glsl_fragment_patch_get(use_ray_query_);
       break;
     case shaderc_compute_shader:
-      source_patch = device.glsl_compute_patch_get();
+      source_patch = device.glsl_compute_patch_get(use_ray_query_);
       break;
     default:
       BLI_assert_msg(0, "Only forced ShaderC shader kinds are supported.");

commit 9cc84ad47255f61e1e4e51a4a0b646000adfccc4

Fix: Compiler Warnings
Mainly caused by 348bb4a2d0.

Pull Request: https://projects.blender.org/blender/blender/pulls/161136

source/blender/gpu/metal/mtl_shader_generate.cc
@@ -8,7 +8,6 @@
 
 #include "BLI_math_bits.hh"
 
-#include "gpu_shader_dependency_private.hh"
 #include "mtl_backend.hh"
 #include "mtl_shader_generate.hh"
 
@@ -743,6 +742,9 @@ static void generate_resource(GeneratedStreams &generated,
                       MTL_SSBO_SLOT_OFFSET + res.slot,
                       stage);
       break;
+    case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+      BLI_assert_unreachable();
+      break;
   }
 }
 
@@ -1464,6 +1466,9 @@ uint32_t available_buffer_slots(const ShaderCreateInfo &info)
       case ShaderCreateInfo::Resource::BindType::SAMPLER:
       case ShaderCreateInfo::Resource::BindType::IMAGE:
         break;
+      case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+        BLI_assert_unreachable();
+        break;
     };
   };
 
@@ -1526,6 +1531,9 @@ void patch_create_info_atomic_workaround(std::unique_ptr<PatchedShaderCreateInfo
         break;
       case ShaderCreateInfo::Resource::BindType::UNIFORM_BUFFER:
       case ShaderCreateInfo::Resource::BindType::STORAGE_BUFFER:
+        break;
+      case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+        BLI_assert_unreachable();
         break;
     }
   };

source/blender/gpu/metal/mtl_shader_interface.mm
@@ -60,6 +60,9 @@ MTLShaderInterface::MTLShaderInterface(const char *name,
       case ShaderCreateInfo::Resource::BindType::STORAGE_BUFFER:
         ssbo_len_++;
         break;
+      case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+        BLI_assert_unreachable();
+        break;
     }
   }
 

source/blender/gpu/vulkan/vk_ray_tracing.cc
@@ -7,7 +7,6 @@
 #include "vk_backend.hh"
 #include "vk_context.hh"
 #include "vk_index_buffer.hh"
-#include "vk_memory_layout.hh"
 #include "vk_state_manager.hh"
 #include "vk_vertex_buffer.hh"
 
@@ -38,7 +37,7 @@ VKTopLevelAS::VKTopLevelAS(const char *name) : TopLevelAS(name), max_primitive_c
       0,
       nullptr,
       nullptr,
-      0};
+      {0}};
 }
 
 VKTopLevelAS::~VKTopLevelAS()
@@ -228,7 +227,7 @@ void VKTopLevelAS::build()
       uint32_t(node_data.vk_acceleration_structure_geometries.size()),
       node_data.vk_acceleration_structure_geometries.data(),
       nullptr,
-      0};
+      {0}};
 
   /* Determine acceleration structure + scratch space */
   VkAccelerationStructureBuildSizesInfoKHR vk_acceleration_structure_build_sizes_info = {
@@ -342,7 +341,7 @@ VKBottomLevelAS::VKBottomLevelAS(const char *name) : BottomLevelAS(name)
       0,
       nullptr,
       nullptr,
-      0};
+      {0}};
 }
 
 VKBottomLevelAS::~VKBottomLevelAS()
@@ -404,15 +403,15 @@ void VKBottomLevelAS::add_geometry(IndexBuf &index_buffer_, VertBuf &vertex_buff
        nullptr,
        VK_GEOMETRY_TYPE_TRIANGLES_KHR,
        {
-           VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_TRIANGLES_DATA_KHR,
-           nullptr,
-           vertex_format,
-           {vertex_buffer.device_address_get()},
-           vertex_buffer.format.stride,
-           vertex_buffer.vertex_len - 1,
-           index_buffer.vk_index_type(),
-           index_buffer.device_address_get(),
-           0,
+           {VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_TRIANGLES_DATA_KHR,
+            nullptr,
+            vertex_format,
+            {vertex_buffer.device_address_get()},
+            vertex_buffer.format.stride,
+            vertex_buffer.vertex_len - 1,
+            index_buffer.vk_index_type(),
+            {index_buffer.device_address_get()},
+            {0}},
        },
        VK_GEOMETRY_OPAQUE_BIT_KHR});
   const uint32_t num_primitives = uint32_t(index_buffer.index_len_get() / 3);
@@ -445,7 +444,7 @@ void VKBottomLevelAS::build()
       uint32_t(node_data.vk_acceleration_structure_geometries.size()),
       node_data.vk_acceleration_structure_geometries.data(),
       nullptr,
-      0};
+      {0}};
 
   /* Determine acceleration structure + scratch space */
   VkAccelerationStructureBuildSizesInfoKHR vk_acceleration_structure_build_sizes_info = {

source/blender/gpu/vulkan/vk_shader.cc
@@ -8,8 +8,6 @@
 
 #include <sstream>
 
-#include "GPU_capabilities.hh"
-
 #include "vk_shader.hh"
 
 #include "vk_backend.hh"
@@ -19,7 +17,6 @@
 #include "vk_state_manager.hh"
 #include "vk_vertex_attribute_object.hh"
 
-#include "BLI_string_utils.hh"
 #include "BLI_vector.hh"
 
 #include "BKE_global.hh"