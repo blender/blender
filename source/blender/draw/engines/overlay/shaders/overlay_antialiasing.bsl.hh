91b25171e58256c22f0ec949ab7a016845646314
Mark van de Ruit <mark@blender.org>
2026-07-08 15:47:44 +0200

    Fix #154502: Overlay: Support x-ray mode in overlay anti-aliasing
    Modifies the overlay AA to better support X-ray mode and edit overlays.
    The overlay AA now relies on `depth_target_tx`, does non-premultiplied
    alpha blending in X-ray, and checks for pixels that did their own AA.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/160231

--- source/blender/draw/engines/overlay/shaders/overlay_antialiasing.bsl.hh
+++ source/blender/draw/engines/overlay/shaders/overlay_antialiasing.bsl.hh
@@ -14,41 +14,77 @@
 
 #include "gpu_shader_compat.hh"
 #include "gpu_shader_fullscreen_lib.glsl"
-#include "gpu_shader_math_base_lib.glsl"
 #include "gpu_shader_math_constants_lib.glsl"
+#include "gpu_shader_math_vector_compare_lib.glsl"
 #include "infos/overlay_common_infos.hh"
+#include "overlay_common_lib.glsl"
 #include "overlay_shader_shared.hh"
 
 SHADER_LIBRARY_CREATE_INFO(draw_globals)
 
 namespace overlay::antialiasing {
 
-/* Per-pixel line data, unpacked as a direction and covered distance. */
+/* Per-pixel line data, unpacked as its perpendicular direction, and distance to origin. */
 struct Line {
   float2 dir;
   float dist;
   float dist_raw;
 
-  static Line decode(float2 data)
+  /**
+   * Create a default invalid line.
+   */
+  static Line zero()
   {
-    /* Unpack distance to edge, remove 0.1f boundary that differentiates cleared pixels. */
-    float dist = (data.y - 0.5f) * 2.5f;
+    return {.dir = float2(0.0f), .dist = 0.0f, .dist_raw = 0.0f};
+  }
 
-    /* Recover perpendicular vector from packed sin_theta. */
-    float sin_theta = (data.x - 0.5f) * 2.0f;
-    float cos_theta = cos_from_sin(sin_theta);
-    float2 perp = normalize(float2(sin_theta, cos_theta));
+  static Line unpack(float2 data)
+  {
+    /* If the data indicates a blocked pixel, store 0.0f instead of 1.0f,
+     * specifically for the small line fix at the bottom.. */
+    float dist_raw = data.y == 1.0f ? 0.0f : data.y;
 
-    return {
-        .dir = perp,
-        .dist = dist,
-        .dist_raw = data.y,
-    };
+    Line line = {.dist_raw = dist_raw};
+    unpack_line_data(data, line.dir, line.dist);
+    return line;
   }
 
   bool is_valid() const
   {
-    return dist_raw != 0.0f;
+    /* If 0.0: likely no write was performed to this pixel.
+     * If 1.0: a line overlay wrote to this pixel indicating it does its own AA. */
+    return dist_raw != 0.0f && dist_raw != 1.0f;
+  }
+
+  /**
+   * Coverage of a line based on its stored distance to origin. Here, `kernel_size` is
+   * the inner size of the line with 100% coverage.
+   */
+  float coverage(float kernel_size, bool do_smooth_lines) const
+  {
+    if (do_smooth_lines) {
+      return smoothstep(LINE_SMOOTH_END, LINE_SMOOTH_START, abs(dist) - kernel_size);
+    }
+    return step(-0.5f, kernel_size - abs(dist));
+  }
+
+  /**
+   * Update the stored line distance so it represents a neighboring pixel;
+   * if the neighbor should not have influence, distance is set to a maximal value.
+   */
+  void offset_to_neighbor(int2 offset)
+  {
+    bool is_dir_horizontal = abs(dir.x) > abs(dir.y);
+    bool is_ofs_horizontal = offset.x != 0;
+
+    if (!is_valid() || is_ofs_horizontal != is_dir_horizontal) {
+      /* No line. */
+      dist = 1e10f;
+      return;
+    }
+
+    /* Add projection of the line direction onto a unit vector. */
+    dist = dist + dot(float2(offset), -dir);
   }
 };
 
@@ -56,6 +92,11 @@ struct TexelData {
   float4 color;
   float depth;
   Line line;
+
+  static TexelData zero()
+  {
+    return {.color = float4(0.0f), .depth = 1.0f, .line = Line::zero()};
+  }
 };
 
 struct Resources {
@@ -66,69 +107,97 @@ struct Resources {
   [[sampler(2)]] const sampler2D line_tx;
 
   [[push_constant]] const bool do_smooth_lines;
+  [[push_constant]] const bool do_background_fetch;
 
   TexelData fetch_texel(int2 texel, int2 offset)
   {
     int2 texel_actual = texel + offset;
-    return {
+    TexelData data = {
         .color = texelFetch(color_tx, texel_actual, 0),
         .depth = texelFetch(depth_tx, texel_actual, 0).r,
-        .line = Line::decode(texelFetch(line_tx, texel_actual, 0).rg),
+        .line = Line::unpack(texelFetch(line_tx, texel_actual, 0).rg),
     };
+    if (any(notEqual(offset, int2(0)))) {
+      data.line.offset_to_neighbor(offset);
+    }
+    return data;
   }
 };
 
 /**
- * Coverage of a line onto a sample that is `distance_to_Line` pixels from the line.
- * Here, `line_kernel_size` is the inner size of the line with 100% coverage.
+ * Return the furthest non-line texel in the neighboring crosshair, or the
+ * center if there is none.
  */
-template<typename T>
-T line_coverage(T distance_to_line, float line_kernel_size, bool do_smooth_lines)
+TexelData furthest_texel(TexelData center, TexelData neighbors[4], bool do_background_fetch)
 {
-  if (do_smooth_lines) {
-    return smoothstep(
-        LINE_SMOOTH_END, LINE_SMOOTH_START, abs(distance_to_line) - line_kernel_size);
-  }
-  return step(-0.5f, line_kernel_size - abs(distance_to_line));
-}
-
-template float line_coverage<float>(float, float, bool);
-template float4 line_coverage<float4>(float4, float, bool);
-
-/**
- * Compute distance-to-line for one of the neighboring crosshair pixels, dependent
- * on whether that pixel has influence or not; in which case distance is set to
- * a maximal value.
- */
-float neighbor_dist(const TexelData &neighbor, int2 offset)
-{
-  bool is_dir_horizontal = abs(neighbor.line.dir.x) > abs(neighbor.line.dir.y);
-  bool is_ofs_horizontal = offset.x != 0;
-
-  if (!neighbor.line.is_valid() || is_ofs_horizontal != is_dir_horizontal) {
-    return 1e10f; /* No line. */
+  if (!do_background_fetch) {
+    return TexelData::zero();
   }
 
-  /* Add projection of the line direction onto a unit vector. */
-  return neighbor.line.dist + dot(float2(offset), -neighbor.line.dir);
+  TexelData furthest = center;
+  for (int i = 0; i < 4; i++) [[unroll]] {
+    if (neighbors[i].depth > furthest.depth) {
+      furthest = neighbors[i];
+    }
+  }
+  return furthest;
 }
 
 /**
- * Blend the neighbor pixel onto the target pixel, based on the pixels'
- * relative depths doing alpha-over or alpha-under. The resulting pixel's
- * depth is then adjusted to the closest depth.
+ * Compute the center pixel's line coverage, and blend it over the current background.
  */
-void neighbor_blend(TexelData neighbor, TexelData &target, float line_coverage)
+void center_kernel(TexelData &target,
+                   const TexelData background,
+                   float kernel_size,
+                   bool do_smooth_lines)
 {
-  neighbor.color *= line_coverage;
-  if (line_coverage > 0.0f && neighbor.depth < target.depth) {
-    /* Alpha over blending, and update to new closest target depth. */
-    target.color = neighbor.color + target.color * (1.0f - neighbor.color.a);
-    target.depth = neighbor.depth;
+  if (!target.line.is_valid()) {
+    return;
+  }
+
+  float coverage = target.line.coverage(kernel_size, do_smooth_lines);
+  target.color = mix(background.color, target.color, coverage);
+}
+
+/**
+ * Compute a neighbor pixel's line coverage, and alpha-over/alpha-under blend it with
+ * current center. The target pixel tracks the closest depth of center/neighbor.
+ */
+void neighbor_kernel(TexelData &target,
+                     TexelData neighbor,
+                     TexelData background,
+                     float kernel_size,
+                     bool do_smooth_lines)
+{
+  if (!neighbor.line.is_valid()) {
+    return;
+  }
+
+  float coverage = neighbor.line.coverage(kernel_size, do_smooth_lines);
+  if (coverage == 0.0f) {
+    return;
+  }
+
+  /* Blend neighbor over background based on their respective line coverages. */
+  neighbor.color = mix(background.color, neighbor.color, coverage);
+
+  /* Select over/under for alpha blending, based on relative depth. */
+  bool target_over_neighbor = target.depth < neighbor.depth;
+  float4 over = target_over_neighbor ? target.color : neighbor.color;
+  float4 under = target_over_neighbor ? neighbor.color : target.color;
+
+  if (background.color.a == 0.0) {
+    /* Without background, we do additive alpha on the current framebuffer. */
+    target.color = over + under * (1.0f - over.a);
   }
   else {
-    /* Alpha under blending */
-    target.color = target.color + neighbor.color * (1.0f - target.color.a);
+    /* With background, we have to avoid pre-multiplied alpha. */
+    under.a *= (1.0f - over.a);
+    target.color = (over * over.a + under * under.a) / (over.a + under.a);
+  }
+
+  if (!target_over_neighbor) {
+    target.depth = neighbor.depth;
   }
 }
 
@@ -145,13 +214,10 @@ struct FragOut {
                             [[resource_table]] Resources &srt,
                             [[out]] FragOut &frag)
 {
-  int2 texel = int2(frag_coord.xy);
+  const int2 texel = int2(frag_coord.xy);
 
-  /* Fetch data at the center pixel. */
+  /* Fetch center pixel. */
   TexelData center = srt.fetch_texel(texel, int2(0));
-
-  /* Store until end of function; does the center pixel have alpha? */
-  bool original_center_has_alpha = center.color.a < 1.0f;
 
   /* Guard; check if no expansion or AA should be applied. */
   if (!srt.do_smooth_lines && center.line.dist <= 1.0f) {
@@ -159,38 +225,31 @@ struct FragOut {
     return;
   }
 
-  /* Fetch data for a cross of neighboring pixels. */
+  /* Fetch a cross of neighboring pixels. */
   TexelData neighbors[] = {srt.fetch_texel(texel, int2(1, 0)),
                            srt.fetch_texel(texel, int2(-1, 0)),
                            srt.fetch_texel(texel, int2(0, 1)),
                            srt.fetch_texel(texel, int2(0, -1))};
 
-  float4 neighbor_dists = float4(neighbor_dist(neighbors[0], int2(1, 0)),
-                                 neighbor_dist(neighbors[1], int2(-1, 0)),
-                                 neighbor_dist(neighbors[2], int2(0, 1)),
-                                 neighbor_dist(neighbors[3], int2(0, -1)));
+  /* Find the furthest non-line texel among neighbors. */
+  TexelData background = furthest_texel(center, neighbors, srt.do_background_fetch);
 
-  /* Compute per-neighbor line coverage */
-  float line_kernel = theme.sizes.pixel * 0.5f - 0.5f;
-  float4 coverage = line_coverage(neighbor_dists, line_kernel, srt.do_smooth_lines);
+  /* Store until after blend, does the center pixel have alpha? */
+  const bool original_center_has_alpha = center.color.a < 1.0f;
 
-  /* Multiply current output color by center pixel's line coverage. */
-  if (center.line.is_valid()) {
-    float coverage = line_coverage(center.line.dist, line_kernel, srt.do_smooth_lines);
-    center.color *= coverage;
-  }
-
-  /* We don't order fragments; instead, we blend using alpha-over/alpha-under
-   * based on the tracked depth of each neighbor pixel, using the center pixel
-   * as reference input and tracked value. */
-  neighbor_blend(neighbors[0], center, coverage.x);
-  neighbor_blend(neighbors[1], center, coverage.y);
-  neighbor_blend(neighbors[2], center, coverage.z);
-  neighbor_blend(neighbors[3], center, coverage.w);
+  /* Blend center color over background. */
+  const float kernel_size = theme.sizes.pixel * 0.5f - 0.5f;
+  center_kernel(center, background, kernel_size, srt.do_smooth_lines);
+  /* We don't order fragments; intsead blending neighbors alpha-over/alpha-under based on
+   * a tracked depth for each neighbor, using the center pixel as starting reference. */
+  neighbor_kernel(center, neighbors[0], background, kernel_size, srt.do_smooth_lines);
+  neighbor_kernel(center, neighbors[1], background, kernel_size, srt.do_smooth_lines);
+  neighbor_kernel(center, neighbors[2], background, kernel_size, srt.do_smooth_lines);
+  neighbor_kernel(center, neighbors[3], background, kernel_size, srt.do_smooth_lines);
 
 #if 1
   /* Fix aliasing issue with really dense meshes and 1 pixel sized lines. */
-  if (!original_center_has_alpha && center.line.is_valid() && line_kernel < 0.45f) {
+  if (!original_center_has_alpha && center.line.is_valid() && kernel_size < 0.45f) {
     float4 lines_raw = float4(neighbors[0].line.dist_raw,
                               neighbors[1].line.dist_raw,
                               neighbors[2].line.dist_raw,
