blender/blender

commit 84515ec0174c86f11eb00d31d2691d28b825fca2

Fix #147659: Shadow rays for hair don't support colored transparency
Cycles used a single float for the baked transparency.
Change it to RGBE, so that it is colored, and does not increase the
state size.

Pull Request: https://projects.blender.org/blender/blender/pulls/160680

intern/cycles/kernel/bvh/intersect_filter.h
@@ -117,7 +117,7 @@ BVH_SHADOW_ALL_PAYLOAD_SUBCLASS(BVHShadowAllPayload, BVHPayload)
   /* Accumulated throughput of transparent curve intersections.
    * Curves are using special optimization by baking their transparency and handling it in the
    * filter function. */
-  float throughput = 1.0f;
+  float3 throughput = one_float3();
 };
 
 /* Filter intersection with possibly transparent surface.
@@ -170,7 +170,7 @@ ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
   const int shader_flags = intersection_get_shader_flags(kg, isect.prim, isect.type);
   if ((shader_flags & SD_HAS_TRANSPARENT_SHADOW) == 0) {
     /* No transparent shadows for the shader, all light is blocked, and we can stop immediately. */
-    payload.throughput = 0.0f;
+    payload.throughput = zero_float3();
     return false;
   }
 
@@ -206,7 +206,7 @@ ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
   if (payload.num_transparent_hits > payload.max_transparent_hits) {
     /* The maximum number of intersections has been reached, consider that all light has been
      * blocked. */
-    payload.throughput = 0.0f;
+    payload.throughput = zero_float3();
     return false;
   }
 
@@ -217,10 +217,10 @@ ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
       payload.throughput *= intersection_curve_shadow_transparency(
           kg, isect.object, isect.prim, isect.type, isect.u);
 
-      if (payload.throughput < CURVE_SHADOW_TRANSPARENCY_CUTOFF) {
+      if (reduce_max(payload.throughput) < CURVE_SHADOW_TRANSPARENCY_CUTOFF) {
         /* Light attenuated too much through the curve intersections, assume all light is blocked
          * and do early output. */
-        payload.throughput = 0.0f;
+        payload.throughput = zero_float3();
         return false;
       }
 

intern/cycles/kernel/bake/bake.h
@@ -17,6 +17,8 @@
 
 #include "kernel/util/colorspace.h"
 
+#include "util/types_rgbe.h"
+
 CCL_NAMESPACE_BEGIN
 
 ccl_device void kernel_displace_evaluate(KernelGlobals kg,
@@ -136,7 +138,7 @@ ccl_device void kernel_curve_shadow_transparency_evaluate(
   }
 
   /* Write output. */
-  output[offset] = clamp(average(surface_shader_transparency(&sd)), 0.0f, 1.0f);
+  output[offset] = rgb_to_rgbe(saturate(spectrum_to_rgb(surface_shader_transparency(&sd)))).f;
 #endif
 }
 

intern/cycles/kernel/bvh/bvh.h
@@ -117,14 +117,14 @@ ccl_device_intersect void scene_intersect_shadow_all(KernelGlobals kg,
                                                      const uint visibility,
                                                      const uint max_transparent_hits,
                                                      ccl_private uint *num_recorded_hits,
-                                                     ccl_private float *throughput)
+                                                     ccl_private float3 *throughput)
 {
 #  if !defined(__KERNEL_OPTIX__)
   /* OptiX does not perform well with conditional trace calls, so it handles the validity of the
    * ray in the scene_intersect_shadow_all_optix(). */
   if (!intersection_ray_valid(ray)) {
     *num_recorded_hits = 0;
-    *throughput = 1.0f;
+    *throughput = one_float3();
     return;
   }
 #  endif

intern/cycles/kernel/bvh/util.h
@@ -7,6 +7,7 @@
 #include "kernel/globals.h"
 #include "kernel/integrator/state.h"
 #include "kernel/types.h"
+#include "util/types_rgbe.h"
 
 CCL_NAMESPACE_BEGIN
 
@@ -216,14 +217,14 @@ ccl_device_inline int intersection_find_attribute(KernelGlobals kg,
 /* Cut-off value to stop transparent shadow tracing when practically opaque. */
 #define CURVE_SHADOW_TRANSPARENCY_CUTOFF 0.001f
 
-ccl_device_inline float intersection_curve_shadow_transparency(
+ccl_device_inline float3 intersection_curve_shadow_transparency(
     KernelGlobals kg, const int object, const int prim, const int type, const float u)
 {
   /* Find attribute. */
   const int offset = intersection_find_attribute(kg, object, ATTR_STD_SHADOW_TRANSPARENCY);
   if (offset == ATTR_STD_NOT_FOUND) {
     /* If no shadow transparency attribute, assume opaque. */
-    return 0.0f;
+    return zero_float3();
   }
 
   /* Interpolate transparency between curve keys. */
@@ -231,8 +232,11 @@ ccl_device_inline float intersection_curve_shadow_transparency(
   const int k0 = kcurve.first_key + PRIMITIVE_UNPACK_SEGMENT(type);
   const int k1 = k0 + 1;
 
-  const float f0 = kernel_data_fetch(attributes_float, offset + k0);
-  const float f1 = kernel_data_fetch(attributes_float, offset + k1);
+  const float f0_encoded = kernel_data_fetch(attributes_float, offset + k0);
+  const float f1_encoded = kernel_data_fetch(attributes_float, offset + k1);
+
+  const float3 f0 = rgbe_to_rgb(RGBE(f0_encoded));
+  const float3 f1 = rgbe_to_rgb(RGBE(f1_encoded));
 
   return (1.0f - u) * f0 + u * f1;
 }

intern/cycles/kernel/integrator/intersect_shadow.h
@@ -118,15 +118,17 @@ ccl_device bool integrate_intersect_shadow_transparent(KernelGlobals kg,
    * have available in the integrator state. */
   const uint max_transparent_hits = integrate_shadow_max_transparent_hits(kg, state);
   uint num_hits = 0;
-  float throughput = 1.0f;
+  float3 throughput = one_float3();
   scene_intersect_shadow_all(
       kg, state, ray, visibility, max_transparent_hits, &num_hits, &throughput);
 
-  const bool opaque_hit = (throughput == 0.0f);
+  const bool opaque_hit = is_zero(throughput);
 
   /* Computed throughput from baked shadow transparency, where we can bypass recording
-   * intersections and shader evaluation. */
-  if (throughput != 1.0f) {
+   * intersections and shader evaluation.
+   * The hypothesis here is that the majority of the shadow rays do not hit anything, and the
+   * kernel is memory-bound. Try to avoid unnecessary access and writes to the global memory. */
+  if (!isequal(throughput, one_float3())) {
     INTEGRATOR_STATE_WRITE(state, shadow_path, throughput) *= throughput;
   }
 

intern/cycles/scene/attribute.cpp
@@ -805,6 +805,7 @@ static TypeDesc find_type_from_geometry_std(Geometry *geometry, AttributeStandar
       case ATTR_STD_RANDOM_PER_ISLAND:
         return TypeFloat;
       case ATTR_STD_SHADOW_TRANSPARENCY:
+        /* Stored as float but uses RGBE encoding. */
         return TypeFloat;
       default:
         assert(0);

tests/files/render/hair/cycles_renders/transparent_shadow_hair_colored.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dfa8dd2f0348c7ee1f1ea8eeedd28a075f505a4ae03a7a7706ae166f02fae91c
+size 28281

tests/files/render/hair/eevee_renders/transparent_shadow_hair_colored.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4619659e00ed143b82e2e77a94cfb08e8605d3179a20b6740ca9519003b18241
+size 20025

commit 749518deb2f0735a22361a07488b35f7ea5c2fdf

Fix: Cycles: Build failure when transparent shadows are disabled in kernel
Issue introduced in 84515ec0174c86f11eb00d31d2691d28b825fca2.

intern/cycles/kernel/bvh/intersect_filter.h
@@ -161,7 +161,7 @@ ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
 
 #if !defined(__TRANSPARENT_SHADOWS__)
   /* No transparent shadows in the scene, all light is blocked and we can stop immediately. */
-  payload.throughput = 0.0f;
+  payload.throughput = zero_float3();
   return false;
 #else
   /* Detect if this surface has a shader with transparent shadows. */

intern/cycles/kernel/device/metal/kernel.metal
@@ -244,7 +244,7 @@ bool metalrt_shadow_all_hit(constant KernelParamsMetal &launch_params_metal,
           kg, payload.state, payload, payload.base.ray_self, payload.base.ray_visibility, isect);
 
 #  else  /* __TRANSPARENT_SHADOWS__ */
-  payload.throughput = 0.0f;
+  payload.throughput = zero_float3();
   return false;
 #  endif /* __TRANSPARENT_SHADOWS__ */
 }