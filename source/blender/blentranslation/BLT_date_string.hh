blender/blender

commit 7b556f8c4cde5e9b0e297bc226ef7a2478820b8b

UI: Date & Time Internationalization
This PR allows the formatting of date, time, and date/time strings
according to the currently-selected output language or based on
user-selectable formats.

Pull Request: https://projects.blender.org/blender/blender/pulls/156613

source/blender/blentranslation/BLT_date_string.hh
@@ -1,0 +1,51 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/** \file
+ * \ingroup blt
+ *
+ * This allows converting std::tm datetime structures to localized date and
+ * time strings. The localization is based on the CLDR from the current locale.
+ */
+
+#include <ctime>
+#include <string>
+
+#include "BLI_string_ref.hh"
+
+namespace blender::date_string {
+
+enum class DateFormat : uint8_t {
+  Default = 0, /* Convention based on output language. */
+  LE_Slash,    /* dd/mm/yyyy */
+  LE_Dot,      /* dd.mm.yyyy */
+  LE_Dash,     /* dd-mm-yyyy */
+  ME_Slash,    /* mm/dd/yyyy */
+  BE_Slash,    /* yyyy/mm/dd */
+  BE_Dot,      /* yyyy.mm.dd */
+  BE_Dash,     /* yyyy-mm-dd */
+};
+
+enum class TimeFormat : uint8_t {
+  H24 = 0, /* 23:59 */
+  H12,     /* 11:59 PM */
+};
+
+std::string date(const std::tm *date_time,
+                 const StringRef locale_iso = {},
+                 DateFormat format = DateFormat::Default);
+
+std::string time(const std::tm *date_time, TimeFormat format = TimeFormat::H24);
+
+std::string datetime(const std::tm *date_time,
+                     const StringRef locale_iso = {},
+                     DateFormat date_format = DateFormat::Default,
+                     TimeFormat time_format = TimeFormat::H24,
+                     const std::tm *now = nullptr,
+                     const StringRef today = {},
+                     const StringRef yesterday = {});
+
+}  // namespace blender::date_string

source/blender/blentranslation/intern/date_string.cc
@@ -1,0 +1,159 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/** \file
+ * \ingroup blt
+ */
+
+#include <algorithm>
+#include <array>
+#include <iomanip>
+#include <string>
+
+#include <fmt/format.h>
+
+#include "BLT_date_string.hh"
+
+#include "BLT_translation.hh"
+
+namespace blender::date_string {
+
+struct LocalePatterns {
+  StringRef locale;
+  StringRef date;
+};
+
+static const LocalePatterns *get_locale_patterns(const StringRef locale_iso)
+{
+  static constexpr std::array<LocalePatterns, 9> patterns = {{
+      {"", "{d:02} {b} {Y}"},         /* default */
+      {"en_US", "{d:02} {b} {Y}"},    /* English (US) */
+      {"ar_EG", "{d:02} {b} {Y}"},    /* Arabic (Egypt) */
+      {"zh_HANS", "{Y}年{m}月{d}日"}, /* Chinese (Simplified) */
+      {"zh_HANT", "{Y}年{m}月{d}日"}, /* Chinese (Traditional) */
+      {"hu_HU", "{Y}. {b} {d:02}"},   /* Hungarian */
+      {"ja_JP", "{Y}年{m}月{d}日"},   /* Japanese */
+      {"ko_KR", "{Y}년 {m}월 {d}일"}, /* Korean */
+      {"ur", "{d:02} {b} {Y}"},       /* Urdu */
+  }};
+
+  for (const LocalePatterns &pattern : patterns) {
+    if (pattern.locale == locale_iso) {
+      return &pattern;
+    }
+  }
+
+  /* Fallback default pattern (index 0). */
+  return &patterns[0];
+}
+
+std::string time(const std::tm *date_time, TimeFormat format)
+{
+  std::string time_format_str;
+
+  if (format == TimeFormat::H24) {
+    time_format_str = "{H:02}:{M:02}";
+  }
+  else if (format == TimeFormat::H12) {
+    time_format_str = "{I}:{M:02} {p}";
+  }
+
+  return fmt::format(
+      fmt::runtime(time_format_str),
+      fmt::arg("H", date_time->tm_hour),
+      fmt::arg("M", date_time->tm_min),
+      fmt::arg("S", date_time->tm_sec),
+      fmt::arg("I", (date_time->tm_hour % 12) == 0 ? 12 : (date_time->tm_hour % 12)),
+      fmt::arg("p",
+               (date_time->tm_hour < 12) ? CTX_IFACE_(BLT_I18NCONTEXT_TIME, "AM") :
+                                           CTX_IFACE_(BLT_I18NCONTEXT_TIME, "PM")));
+}
+
+std::string date(const std::tm *date_time, const StringRef locale_iso, DateFormat format)
+{
+  static constexpr std::array<StringRef, 12> months = {CTX_N_(BLT_I18NCONTEXT_TIME, "Jan"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Feb"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Mar"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Apr"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "May"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Jun"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Jul"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Aug"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Sep"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Oct"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Nov"),
+                                                       CTX_N_(BLT_I18NCONTEXT_TIME, "Dec")};
+
+  std::string date_format_str;
+
+  BLI_assert(date_time->tm_mon >= 0 && date_time->tm_mon < 12);
+  const int month_index = std::clamp(date_time->tm_mon, 0, 11);
+
+  if (format == DateFormat::Default) {
+    const LocalePatterns *pattern = get_locale_patterns(locale_iso);
+    date_format_str = pattern->date;
+  }
+  else if (format == DateFormat::LE_Slash) {
+    date_format_str = "{d:02}/{m:02}/{Y}";
+  }
+  else if (format == DateFormat::LE_Dot) {
+    date_format_str = "{d:02}.{m:02}.{Y}";
+  }
+  else if (format == DateFormat::LE_Dash) {
+    date_format_str = "{d:02}-{m:02}-{Y}";
+  }
+  else if (format == DateFormat::ME_Slash) {
+    date_format_str = "{m:02}/{d:02}/{Y}";
+  }
+  else if (format == DateFormat::BE_Slash) {
+    date_format_str = "{Y}/{m:02}/{d:02}";
+  }
+  else if (format == DateFormat::BE_Dot) {
+    date_format_str = "{Y}.{m:02}.{d:02}";
+  }
+  else if (format == DateFormat::BE_Dash) {
+    date_format_str = "{Y}-{m:02}-{d:02}";
+  }
+
+  return fmt::format(fmt::runtime(date_format_str),
+                     fmt::arg("Y", date_time->tm_year + 1900),
+                     fmt::arg("m", date_time->tm_mon + 1),
+                     fmt::arg("b", CTX_IFACE_(BLT_I18NCONTEXT_TIME, months[month_index])),
+                     fmt::arg("d", date_time->tm_mday));
+}
+
+std::string datetime(const std::tm *datetime,
+                     const StringRef locale_iso,
+                     DateFormat date_format,
+                     TimeFormat time_format,
+                     const std::tm *now,
+                     const StringRef today,
+                     const StringRef yesterday)
+{
+  bool is_today = false;
+  bool is_yesterday = false;
+  if (now && !today.is_empty() && !yesterday.is_empty()) {
+    is_today = (datetime->tm_yday == now->tm_yday && datetime->tm_year == now->tm_year);
+    std::tm yesterday_tm = *now;
+    yesterday_tm.tm_mday--;
+    mktime(&yesterday_tm);
+    is_yesterday = (datetime->tm_yday == yesterday_tm.tm_yday &&
+                    datetime->tm_year == yesterday_tm.tm_year);
+  }
+
+  const std::string time_s = time(datetime, time_format);
+
+  if (is_today) {
+    return std::string(today) + " " + time_s;
+  }
+  else if (is_yesterday) {
+    return std::string(yesterday) + " " + time_s;
+  }
+  else {
+    const std::string date_s = date(datetime, locale_iso, date_format);
+    return date_s + " " + time_s;
+  }
+}
+
+}  // namespace blender::date_string

source/blender/editors/interface/templates/interface_template_recent_files.cc
@@ -16,6 +16,8 @@
 
 #include "BLO_readfile.hh"
 
+#include "BLT_date_string.hh"
+#include "BLT_lang.hh"
 #include "BLT_translation.hh"
 
 #include "BKE_blendfile.hh"
@@ -82,19 +84,19 @@ static void template_recent_files_tooltip_func(bContext & /*C*/,
 
   BLI_stat_t status;
   if (BLI_stat(path, &status) != -1) {
-    char date_str[FILELIST_DIRENTRY_DATE_LEN], time_st[FILELIST_DIRENTRY_TIME_LEN];
-    bool is_today, is_yesterday;
-    std::string day_string;
-    BLI_filelist_entry_datetime_to_string(
-        nullptr, int64_t(status.st_mtime), false, time_st, date_str, &is_today, &is_yesterday);
-    if (is_today || is_yesterday) {
-      day_string = (is_today ? TIP_("Today") : TIP_("Yesterday")) + std::string(" ");
-    }
+    const tm mod_time = *localtime(&status.st_mtime);
+    const time_t ts_now = time(nullptr);
+    const tm now = *localtime(&ts_now);
+    const char *lang = BLT_lang_get();
+    std::string modified_s = date_string::datetime(&mod_time,
+                                                   lang,
+                                                   date_string::DateFormat(U.date_format),
+                                                   date_string::TimeFormat(U.time_format),
+                                                   &now,
+                                                   TIP_("Today"),
+                                                   TIP_("Yesterday"));
     tooltip_text_field_add(tip,
-                           fmt::format(fmt::runtime(TIP_("Modified: {}{}{}")),
-                                       day_string,
-                                       (is_today || is_yesterday) ? "" : date_str,
-                                       (is_today || is_yesterday) ? time_st : ""),
+                           fmt::format(fmt::runtime(TIP_("Modified: {}")), modified_s),
                            {},
                            TIP_STYLE_NORMAL,
                            TIP_LC_NORMAL);

source/blender/editors/space_file/file_draw.cc
@@ -43,6 +43,8 @@
 
 #include "BLO_readfile.hh"
 
+#include "BLT_date_string.hh"
+#include "BLT_lang.hh"
 #include "BLT_translation.hh"
 
 #include "BLF_api.hh"
@@ -305,19 +307,20 @@ static void file_draw_tooltip_custom_func(bContext & /*C*/,
       free_imbuf = true;
     }
 
-    char date_str[FILELIST_DIRENTRY_DATE_LEN], time_str[FILELIST_DIRENTRY_TIME_LEN];
-    bool is_today, is_yesterday;
-    std::string day_string;
-    BLI_filelist_entry_datetime_to_string(
-        nullptr, file->time, false, time_str, date_str, &is_today, &is_yesterday);
-    if (is_today || is_yesterday) {
-      day_string = (is_today ? TIP_("Today") : TIP_("Yesterday")) + std::string(" ");
-    }
+    const time_t file_time = (time_t)file->time;
+    const std::tm mod_time = *std::localtime(&file_time);
+    const time_t ts_now = time(nullptr);
+    const std::tm now = *std::localtime(&ts_now);
+    const char *lang = BLT_lang_get();
+    std::string modified_s = blender::date_string::datetime(&mod_time,
+                                                            lang,
+                                                            date_string::DateFormat(U.date_format),
+                                                            date_string::TimeFormat(U.time_format),
+                                                            &now,
+                                                            TIP_("Today"),
+                                                            TIP_("Yesterday"));
     tooltip_text_field_add(tip,
-                           fmt::format(fmt::runtime(TIP_("Modified: {}{}{}")),
-                                       day_string,
-                                       (is_today || is_yesterday) ? "" : date_str,
-                                       (is_today || is_yesterday) ? time_str : ""),
+                           fmt::format(fmt::runtime(TIP_("Modified: {}")), modified_s),
                            {},
                            ui::TIP_STYLE_NORMAL,
                            ui::TIP_LC_NORMAL);
@@ -1230,18 +1233,23 @@ static const char *filelist_get_details_column_string(
     case COLUMN_DATETIME:
       if (!(file->typeflag & FILE_TYPE_BLENDERLIB) && !FILENAME_IS_CURRPAR(file->relpath)) {
         if (file->draw_data.datetime_str[0] == '\0' || update_stat_strings) {
-          char date[FILELIST_DIRENTRY_DATE_LEN], time[FILELIST_DIRENTRY_TIME_LEN];
-          bool is_today, is_yesterday;
-
-          BLI_filelist_entry_datetime_to_string(
-              nullptr, file->time, compact, time, date, &is_today, &is_yesterday);
-
-          if (!compact && (is_today || is_yesterday)) {
-            STRNCPY_UTF8(date, is_today ? IFACE_("Today") : IFACE_("Yesterday"));
-          }
-          SNPRINTF_UTF8(file->draw_data.datetime_str, compact ? "%s" : "%s %s", date, time);
+          const time_t file_time = (time_t)file->time;
+          const std::tm mod_time = *std::localtime(&file_time);
+          const time_t ts_now = time(nullptr);
+          const std::tm now = *std::localtime(&ts_now);
+          const char *lang = BLT_lang_get();
+          std::string modified_s =
+              compact ?
+                  date_string::date(&mod_time, lang, date_string::DateFormat(U.date_format)) :
+                  date_string::datetime(&mod_time,
+                                        lang,
+                                        date_string::DateFormat(U.date_format),
+                                        date_string::TimeFormat(U.time_format),
+                                        &now,
+                                        TIP_("Today"),
+                                        TIP_("Yesterday"));
+          STRNCPY_UTF8(file->draw_data.datetime_str, modified_s.c_str());
         }
-
         return file->draw_data.datetime_str;
       }
       break;

source/blender/windowmanager/intern/wm_files.cc
@@ -53,6 +53,7 @@
 #include "BLO_core_file_reader.hh"
 #include "BLO_readfile.hh"
 
+#include "BLT_date_string.hh"
 #include "BLT_lang.hh"
 #include "BLT_translation.hh"
 
@@ -3309,26 +3310,24 @@ static std::string wm_open_mainfile_get_description(bContext * /*C*/,
   }
 
   /* Date. */
-  char date_str[FILELIST_DIRENTRY_DATE_LEN];
-  char time_str[FILELIST_DIRENTRY_TIME_LEN];
-  bool is_today, is_yesterday;
-  BLI_filelist_entry_datetime_to_string(
-      nullptr, int64_t(stats.st_mtime), false, time_str, date_str, &is_today, &is_yesterday);
-  if (is_today || is_yesterday) {
-    STRNCPY(date_str, is_today ? TIP_("Today") : TIP_("Yesterday"));
-  }
+  const tm mod_time = *localtime(&stats.st_mtime);
+  const time_t ts_now = time(nullptr);
+  const tm now_tm = *localtime(&ts_now);
+  const char *lang = BLT_lang_get();
+  std::string modified_s = blender::date_string::datetime(&mod_time,
+                                                          lang,
+                                                          date_string::DateFormat(U.date_format),
+                                                          date_string::TimeFormat(U.time_format),
+                                                          &now_tm,
+                                                          TIP_("Today"),
+                                                          TIP_("Yesterday"));
 
   /* Size. */
   char size_str[FILELIST_DIRENTRY_SIZE_LEN];
   BLI_filelist_entry_size_to_string(nullptr, uint64_t(stats.st_size), false, size_str);
 
-  return fmt::format("{}\n\n{}: {} {}\n{}: {}",
-                     filepath,
-                     TIP_("Modified"),
-                     date_str,
-                     time_str,
-                     TIP_("Size"),
-                     size_str);
+  return fmt::format(
+      "{}\n\n{}: {}\n{}: {}", filepath, TIP_("Modified"), modified_s, TIP_("Size"), size_str);
 }
 
 /* Currently fits in a pointer. */

scripts/startup/bl_ui/space_userpref.py
@@ -289,6 +289,9 @@ class USERPREF_PT_interface_translation(InterfacePanel, CenterAlignMixIn, Panel)
         col.prop(view, "use_translate_reports", text="Reports")
         col.prop(view, "use_translate_new_dataname", text="New Data")
 
+        layout.prop(view, "date_format")
+        layout.prop(view, "time_format", text="Time")
+
 
 class USERPREF_PT_interface_accessibility(InterfacePanel, CenterAlignMixIn, Panel):
     bl_label = "Accessibility"

source/blender/blenlib/BLI_fileops.h
@@ -311,19 +311,6 @@ void BLI_filelist_entry_mode_to_string(const struct stat *st,
 void BLI_filelist_entry_owner_to_string(const struct stat *st,
                                         bool compact,
                                         char r_owner[FILELIST_DIRENTRY_OWNER_LEN]);
-/**
- * Convert given entry's time into human-readable strings.
- *
- * \param r_is_today: optional, returns true if the date matches today's.
- * \param r_is_yesterday: optional, returns true if the date matches yesterday's.
- */
-void BLI_filelist_entry_datetime_to_string(const struct stat *st,
-                                           int64_t ts,
-                                           bool compact,
-                                           char r_time[FILELIST_DIRENTRY_TIME_LEN],
-                                           char r_date[FILELIST_DIRENTRY_DATE_LEN],
-                                           bool *r_is_today,
-                                           bool *r_is_yesterday);
 
 /** \} */
 

source/blender/blenlib/intern/BLI_filelist.cc
@@ -343,68 +343,6 @@ void BLI_filelist_entry_owner_to_string(const struct stat *st,
 #endif
 }
 
-void BLI_filelist_entry_datetime_to_string(const struct stat *st,
-                                           const int64_t ts,
-                                           const bool compact,
-                                           char r_time[FILELIST_DIRENTRY_TIME_LEN],
-                                           char r_date[FILELIST_DIRENTRY_DATE_LEN],
-                                           bool *r_is_today,
-                                           bool *r_is_yesterday)
-{
-  int today_year = 0;
-  int today_yday = 0;
-  int yesterday_year = 0;
-  int yesterday_yday = 0;
-
-  if (r_is_today || r_is_yesterday) {
-    /* `localtime()` has only one buffer so need to get data out before called again. */
-    const time_t ts_now = time(nullptr);
-    tm *today = localtime(&ts_now);
-
-    today_year = today->tm_year;
-    today_yday = today->tm_yday;
-    /* Handle a yesterday that spans a year */
-    today->tm_mday--;
-    mktime(today);
-    yesterday_year = today->tm_year;
-    yesterday_yday = today->tm_yday;
-
-    if (r_is_today) {
-      *r_is_today = false;
-    }
-    if (r_is_yesterday) {
-      *r_is_yesterday = false;
-    }
-  }
-
-  const time_t ts_mtime = ts;
-  const tm *tm = localtime(st ? &st->st_mtime : &ts_mtime);
-  const time_t zero = 0;
-
-  /* Prevent impossible dates in windows. */
-  if (tm == nullptr) {
-    tm = localtime(&zero);
-  }
-
-  if (r_time) {
-    strftime(r_time, sizeof(*r_time) * FILELIST_DIRENTRY_TIME_LEN, "%H:%M", tm);
-  }
-
-  if (r_date) {
-    strftime(r_date,
-             sizeof(*r_date) * FILELIST_DIRENTRY_DATE_LEN,
-             compact ? "%d/%m/%y" : "%d %b %Y",
-             tm);
-  }
-
-  if (r_is_today && (tm->tm_year == today_year) && (tm->tm_yday == today_yday)) {
-    *r_is_today = true;
-  }
-  else if (r_is_yesterday && (tm->tm_year == yesterday_year) && (tm->tm_yday == yesterday_yday)) {
-    *r_is_yesterday = true;
-  }
-}
-
 void BLI_filelist_entry_duplicate(direntry *dst, const direntry *src)
 {
   *dst = *src;

commit ecf38b6c312edacec64889263749567080362980

Fix #161550: Crash displaying file info with an incorrect modified date
Accidentally regressed from 7b556f8c4cde5e9b0e297bc226ef7a2478820b8b
which removed a guard in case `std::localtime` returned nullptr.

Put the guard back by creating a new `localtime_safe` function which
restores the previous behavior (and doesn't crash).

Pull Request: https://projects.blender.org/blender/blender/pulls/161553

source/blender/blentranslation/BLT_date_string.hh
@@ -44,6 +44,8 @@ enum class TimeFormat : uint8_t {
   H12 = 1,
 };
 
+std::tm localtime_safe(time_t time);
+
 std::string date(const std::tm &date_time,
                  StringRef locale_iso = {},
                  DateFormat format = DateFormat::Default);

source/blender/blentranslation/intern/date_string.cc
@@ -8,6 +8,7 @@
 
 #include <algorithm>
 #include <array>
+#include <ctime>
 #include <string>
 
 #include <fmt/format.h>
@@ -45,6 +46,18 @@ static const LocalePatterns &get_locale_patterns(const StringRef locale_iso)
 
   /* Fallback default pattern (index 0). */
   return patterns[0];
+}
+
+std::tm localtime_safe(const time_t time)
+{
+  const std::tm *ltime = std::localtime(&time);
+  if (ltime) {
+    return *ltime;
+  }
+
+  /* If localtime fails, return the epoch time. */
+  const time_t zero = 0;
+  return *std::localtime(&zero);
 }
 
 std::string time(const std::tm &date_time, TimeFormat format)

source/blender/editors/interface/templates/interface_template_recent_files.cc
@@ -84,9 +84,9 @@ static void template_recent_files_tooltip_func(bContext & /*C*/,
 
   BLI_stat_t status;
   if (BLI_stat(path, &status) != -1) {
-    const tm mod_time = *localtime(&status.st_mtime);
+    const tm mod_time = date_string::localtime_safe(status.st_mtime);
     const time_t ts_now = time(nullptr);
-    const tm now = *localtime(&ts_now);
+    const tm now = date_string::localtime_safe(ts_now);
     const char *lang = BLT_lang_get();
     std::string modified_s = date_string::datetime(mod_time,
                                                    lang,

source/blender/editors/space_file/file_draw.cc
@@ -314,9 +314,9 @@ static void file_draw_tooltip_custom_func(bContext & /*C*/,
     }
 
     const time_t file_time = (time_t)file->time;
-    const std::tm mod_time = *std::localtime(&file_time);
+    const std::tm mod_time = date_string::localtime_safe(file_time);
     const time_t ts_now = time(nullptr);
-    const std::tm now = *std::localtime(&ts_now);
+    const std::tm now = date_string::localtime_safe(ts_now);
     const char *lang = BLT_lang_get();
     std::string modified_s = blender::date_string::datetime(mod_time,
                                                             lang,
@@ -1311,9 +1311,9 @@ static const char *filelist_get_details_column_string(
       if (!(file->typeflag & FILE_TYPE_BLENDERLIB) && !FILENAME_IS_CURRPAR(file->relpath)) {
         if (file->draw_data.datetime_str[0] == '\0' || update_stat_strings) {
           const time_t file_time = (time_t)file->time;
-          const std::tm mod_time = *std::localtime(&file_time);
+          const std::tm mod_time = date_string::localtime_safe(file_time);
           const time_t ts_now = time(nullptr);
-          const std::tm now = *std::localtime(&ts_now);
+          const std::tm now = date_string::localtime_safe(ts_now);
           const char *lang = BLT_lang_get();
           std::string modified_s =
               compact ? date_string::date(mod_time, lang, date_string::DateFormat(U.date_format)) :

source/blender/windowmanager/intern/wm_files.cc
@@ -3311,9 +3311,9 @@ static std::string wm_open_mainfile_get_description(bContext * /*C*/,
   }
 
   /* Date. */
-  const tm mod_time = *localtime(&stats.st_mtime);
+  const tm mod_time = date_string::localtime_safe(stats.st_mtime);
   const time_t ts_now = time(nullptr);
-  const tm now_tm = *localtime(&ts_now);
+  const tm now_tm = date_string::localtime_safe(ts_now);
   const char *lang = BLT_lang_get();
   std::string modified_s = blender::date_string::datetime(mod_time,
                                                           lang,