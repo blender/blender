blender/blender

commit 1bdfb0538fa51c5f30a0aadee974480e9b6fb8ec

Fix: Out of bounds memory access with curves of size 1
This has been in Blender since forever but only now came up
because of the test `bl_animation_shapekey` which was added in
9725e1d3860514256d908229eb50a1ab7c9ab717

In that case we create a curve with a single point and then add
shapekeys to it. This fails under ASAN with a heap-buffer-overflow
since the code is set up to start the loop by creating a pointer to
the next BezTriple assuming it exists which of course it doesn't
for curves with only 1 element.

I assume that bug never came up since adding shapekeys
to curves with only 1 point is not particularly useful.

Pull Request: https://projects.blender.org/blender/blender/pulls/151664

source/blender/editors/curve/editcurve.cc
@@ -602,8 +602,14 @@ static void calc_keyHandles(ListBase *nurb, float *key)
         prevfp = nullptr;
       }
 
-      nextp = bezt + 1;
-      nextfp = fp + KEYELEM_FLOAT_LEN_BEZTRIPLE;
+      if (nu->pntsu > 1) {
+        nextp = bezt + 1;
+        nextfp = fp + KEYELEM_FLOAT_LEN_BEZTRIPLE;
+      }
+      else {
+        nextp = nullptr;
+        nextfp = nullptr;
+      }
 
       while (a--) {
         key_to_bezt(fp, bezt, &cur);

commit c543fa302a5d5cb01d058fe3879053a733e8c6ed

Fix #161734: Crash calculating single point curve shapekey handles
Caused by 1bdfb0538fa5

- `calchandlesNurb_intern` (called with a Nurb as input) already returns
when `nu->pntsu < 2`
- `calchandleNurb_intern` (called with a BezTriple as input) does not
check the "single point" case (as in: when both `previous` and `next`
are nullptr)

After 1bdfb0538fa5 though, we were running into this exact scenario:
calling `BKE_nurb_handle_calc` [that calls `calchandleNurb_intern`] with
both both `previous` and `next` being nullptr)

So we could either make `calchandleNurb_intern` safe against that case,
and/or
- make `calc_keyHandles` same as `calchandlesNurb_intern` /
`BKE_nurb_handles_calc` early out for a single point

This PR does both (and therefor also reverts the now unneeded
1bdfb0538fa5).

Pull Request: https://projects.blender.org/blender/blender/pulls/161835

source/blender/blenkernel/intern/curve.cc
@@ -3113,6 +3113,11 @@ static void calchandleNurb_intern(BezTriple *bezt,
     return;
   }
 
+  if (prev == nullptr && next == nullptr) {
+    /* Single point, no need to calculat handles. */
+    return;
+  }
+
   p2 = bezt->vec[1];
 
   if (prev == nullptr) {

source/blender/editors/curve/editcurve.cc
@@ -595,6 +595,12 @@ static void calc_keyHandles(ListBaseT<Nurb> *nurb, float *key)
 
   for (Nurb &nu : *nurb) {
     if (nu.bezt) {
+      if (nu.pntsu < 2) {
+        /* Single point, no need to calculat handles, proceed to th next Nurb. */
+        fp += nu.pntsu * KEYELEM_FLOAT_LEN_BEZTRIPLE;
+        continue;
+      }
+
       BezTriple *prevp, *nextp;
       BezTriple cur, prev, next;
       float *startfp, *prevfp, *nextfp;
@@ -612,14 +618,8 @@ static void calc_keyHandles(ListBaseT<Nurb> *nurb, float *key)
         prevfp = nullptr;
       }
 
-      if (nu.pntsu > 1) {
-        nextp = bezt + 1;
-        nextfp = fp + KEYELEM_FLOAT_LEN_BEZTRIPLE;
-      }
-      else {
-        nextp = nullptr;
-        nextfp = nullptr;
-      }
+      nextp = bezt + 1;
+      nextfp = fp + KEYELEM_FLOAT_LEN_BEZTRIPLE;
 
       while (a--) {
         key_to_bezt(fp, bezt, &cur);