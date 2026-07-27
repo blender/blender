zNightlord/Bforartists

commit d3fce7746180ce577c02a52170979bbe8b8bfd70

Fix #157000: Vertex paint operations also affect hidden geometry
Previous work e3801a2bd417b63f8b30c4c2dfa19b2efdbf9ce7 was only doing it
for painting with brushes, so (intentionally) left menu operators
untouched.

This PR now does the same (do not act on hidden geometry) for menu
operators:
- Smooth Vertex Colors
- Dirty Vertex Colors
- Invert
- Levels
- Hue/Saturation/Value
- Brightness/Contrast

Implementation wise, this spreads over a couple of functions, for one of
them (`fill_mesh_color`) we need the "skipping" be optional (since this
also gets called from places where new attributes are created, and I
dont think we should be respecting hidden geometry in that case).
Therefor, `object_active_color_fill` has also been renamed to
`object_active_color_init`.

NOTE: same should probably be done for weightpaint, but there are a lot
more operators to take care of and for some of them it might not
entirely be clear if the desired behavior is skipping hidden geometry or
not -- so a bit of design/research has to be done)

Pull Request: https://projects.blender.org/blender/blender/pulls/157054

source/blender/editors/sculpt_paint/mesh/paint_vertex.cc
@@ -2166,11 +2166,15 @@ static void fill_bm_face_or_corner_attribute(BMesh &bm,
                                              const T &value,
                                              const AttrDomain domain,
                                              const int cd_offset,
-                                             const bool use_vert_sel)
+                                             const bool use_vert_sel,
+                                             const bool only_visible = false)
 {
   BMFace *f;
   BMIter iter;
   BM_ITER_MESH (f, &iter, &bm, BM_FACES_OF_MESH) {
+    if (only_visible && paint_is_bmesh_face_hidden(f)) {
+      continue;
+    }
     BMLoop *l = f->l_first;
     do {
       if (!(use_vert_sel && !BM_elem_flag_test(l->v, BM_ELEM_SELECT))) {
@@ -2192,9 +2196,11 @@ static void fill_mesh_face_or_corner_attribute(Mesh &mesh,
                                                const MutableSpan<T> data,
                                                const bool use_vert_sel,
                                                const bool use_face_sel,
-                                               const bool affect_alpha)
+                                               const bool affect_alpha,
+                                               const bool only_visible = false)
 {
   const bke::AttributeAccessor attributes = mesh.attributes();
+  const VArraySpan hide_poly = *attributes.lookup<bool>(".hide_poly", bke::AttrDomain::Face);
   VArraySpan<bool> select_vert;
   if (use_vert_sel) {
     select_vert = *attributes.lookup<bool>(".select_vert", bke::AttrDomain::Point);
@@ -2208,6 +2214,9 @@ static void fill_mesh_face_or_corner_attribute(Mesh &mesh,
   const Span<int> corner_verts = mesh.corner_verts();
 
   for (const int i : faces.index_range()) {
+    if (only_visible && !hide_poly.is_empty() && hide_poly[i]) {
+      continue;
+    }
     if (!select_poly.is_empty() && !select_poly[i]) {
       continue;
     }
@@ -2234,18 +2243,23 @@ static void fill_mesh_color(Mesh &mesh,
                             const StringRef name,
                             const bool use_vert_sel,
                             const bool use_face_sel,
-                            const bool affect_alpha)
+                            const bool affect_alpha,
+                            const bool only_visible = false)
 {
   if (BMEditMesh *em = mesh.runtime->edit_mesh.get()) {
     BMesh *bm = em->bm;
     const BMDataLayerLookup attr = BM_data_layer_lookup(*mesh.runtime->edit_mesh->bm, name);
     if (attr.type == bke::AttrType::ColorFloat) {
       fill_bm_face_or_corner_attribute<ColorPaint4f>(
-          *bm, color, attr.domain, attr.offset, use_vert_sel);
+          *bm, color, attr.domain, attr.offset, use_vert_sel, only_visible);
     }
     else if (attr.type == bke::AttrType::ColorByte) {
-      fill_bm_face_or_corner_attribute<ColorPaint4b>(
-          *bm, blender::color::encode(color), attr.domain, attr.offset, use_vert_sel);
+      fill_bm_face_or_corner_attribute<ColorPaint4b>(*bm,
+                                                     blender::color::encode(color),
+                                                     attr.domain,
+                                                     attr.offset,
+                                                     use_vert_sel,
+                                                     only_visible);
     }
   }
   else {
@@ -2258,7 +2272,8 @@ static void fill_mesh_color(Mesh &mesh,
           attribute.span.typed<ColorGeometry4f>().cast<ColorPaint4f>(),
           use_vert_sel,
           use_face_sel,
-          affect_alpha);
+          affect_alpha,
+          only_visible);
     }
     else if (attribute.span.type().is<ColorGeometry4b>()) {
       fill_mesh_face_or_corner_attribute<ColorPaint4b>(
@@ -2268,7 +2283,8 @@ static void fill_mesh_color(Mesh &mesh,
           attribute.span.typed<ColorGeometry4b>().cast<ColorPaint4b>(),
           use_vert_sel,
           use_face_sel,
-          affect_alpha);
+          affect_alpha,
+          only_visible);
     }
     attribute.finish();
   }
@@ -2277,7 +2293,8 @@ static void fill_mesh_color(Mesh &mesh,
 static bool fill_active_color(Object &ob,
                               ColorPaint4f fill_color,
                               bool only_selected = true,
-                              bool affect_alpha = true)
+                              bool affect_alpha = true,
+                              const bool only_visible = false)
 {
   Mesh *mesh = BKE_mesh_from_object(&ob);
   if (!mesh) {
@@ -2286,8 +2303,13 @@ static bool fill_active_color(Object &ob,
 
   const bool use_face_sel = only_selected ? (mesh->editflag & ME_EDIT_PAINT_FACE_SEL) != 0 : false;
   const bool use_vert_sel = only_selected ? (mesh->editflag & ME_EDIT_PAINT_VERT_SEL) != 0 : false;
-  fill_mesh_color(
-      *mesh, fill_color, mesh->active_color_attribute, use_vert_sel, use_face_sel, affect_alpha);
+  fill_mesh_color(*mesh,
+                  fill_color,
+                  mesh->active_color_attribute,
+                  use_vert_sel,
+                  use_face_sel,
+                  affect_alpha,
+                  only_visible);
 
   DEG_id_tag_update(&mesh->id, ID_RECALC_SYNC_TO_EVAL);
 
@@ -2297,9 +2319,9 @@ static bool fill_active_color(Object &ob,
   return true;
 }
 
-bool object_active_color_fill(Object &ob, const float fill_color[4], bool only_selected)
+bool object_active_color_init(Object &ob, const float fill_color[4])
 {
-  return fill_active_color(ob, ColorPaint4f(fill_color), only_selected);
+  return fill_active_color(ob, ColorPaint4f(fill_color), false, false);
 }
 
 }  // namespace ed::sculpt_paint
@@ -2328,7 +2350,7 @@ static wmOperatorStatus vertex_color_set_exec(bContext *C, wmOperator *op)
   IndexMaskMemory memory;
   const IndexMask node_mask = bke::pbvh::all_leaf_nodes(pbvh, memory);
 
-  fill_active_color(obact, paintcol, true, affect_alpha);
+  fill_active_color(obact, paintcol, true, affect_alpha, true);
 
   pbvh.tag_attribute_changed(node_mask, mesh->active_color_attribute);
 

scripts/startup/bl_operators/vertexpaint_dirt.py
@@ -105,20 +105,23 @@ def applyVertexDirt(me, blur_iterations, blur_strength, clamp_dirt, clamp_clean,
 
     use_paint_mask = me.use_paint_mask
     for i, p in enumerate(me.polygons):
-        if not use_paint_mask or p.select:
-            for loop_index in p.loop_indices:
-                loop = me.loops[loop_index]
-                v = loop.vertex_index
-                col = attribute_data[v if point_domain else loop_index].color
-                tone = vert_tone[v]
-                tone = (tone - min_tone) * tone_range
+        if p.hide:
+            continue
+        if use_paint_mask and not p.select:
+            continue
+        for loop_index in p.loop_indices:
+            loop = me.loops[loop_index]
+            v = loop.vertex_index
+            col = attribute_data[v if point_domain else loop_index].color
+            tone = vert_tone[v]
+            tone = (tone - min_tone) * tone_range
 
-                if dirt_only:
-                    tone = min(tone, 0.5) * 2.0
+            if dirt_only:
+                tone = min(tone, 0.5) * 2.0
 
-                col[0] = tone * col[0]
-                col[1] = tone * col[1]
-                col[2] = tone * col[2]
+            col[0] = tone * col[0]
+            col[1] = tone * col[1]
+            col[2] = tone * col[2]
     me.update()
     return {'FINISHED'}
 

source/blender/editors/geometry/geometry_attributes.cc
@@ -534,7 +534,7 @@ static wmOperatorStatus geometry_color_attribute_add_exec(bContext *C, wmOperato
       {
         BKE_id_attributes_default_color_set(id, unique_name);
       }
-      sculpt_paint::object_active_color_fill(*ob, color, false);
+      sculpt_paint::object_active_color_init(*ob, color);
       DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
       WM_main_add_notifier(NC_GEOM | ND_DATA, id);
       return OPERATOR_FINISHED;
@@ -551,7 +551,7 @@ static wmOperatorStatus geometry_color_attribute_add_exec(bContext *C, wmOperato
   if (!BKE_id_attributes_color_find(id, BKE_id_attributes_default_color_name(id).value_or(""))) {
     BKE_id_attributes_default_color_set(id, unique_name);
   }
-  sculpt_paint::object_active_color_fill(*ob, color, false);
+  sculpt_paint::object_active_color_init(*ob, color);
   DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
   WM_main_add_notifier(NC_GEOM | ND_DATA, id);
 

source/blender/editors/include/ED_sculpt.hh
@@ -95,13 +95,11 @@ int active_update_and_get(bContext *C, Object &ob, const float mval_fl[2]);
 }  // namespace face_set
 
 /**
- * Fills the object's active color attribute layer with the fill color.
- *
- * \param only_selected: Limit the fill to selected faces or vertices.
+ * Fills the entire object's active color attribute layer with the fill color.
  *
  * \return #true if successful.
  */
-bool object_active_color_fill(Object &ob, const float fill_color[4], bool only_selected);
+bool object_active_color_init(Object &ob, const float fill_color[4]);
 
 /**
  * Fully replace the sculpt mesh with a mesh outside of #Main. This implements various checks to

source/blender/editors/sculpt_paint/mesh/paint_image_proj.cc
@@ -6750,7 +6750,7 @@ static std::optional<std::string> proj_paint_color_attribute_create(wmOperator *
     BKE_id_attributes_default_color_set(&mesh->id, unique_name);
   }
 
-  ed::sculpt_paint::object_active_color_fill(ob, color, false);
+  ed::sculpt_paint::object_active_color_init(ob, color);
 
   return unique_name;
 }

source/blender/editors/sculpt_paint/mesh/paint_vertex_color_ops.cc
@@ -157,17 +157,22 @@ static IndexMask get_selected_indices(const Mesh &mesh,
 {
   const bke::AttributeAccessor attributes = mesh.attributes();
 
+  /* Hidden should never count as selected. */
+  IndexMask visible = IndexMask::from_bools_inverse(
+      *attributes.lookup_or_default<bool>(".hide_poly", domain, false), memory);
+
   if (mesh.editflag & ME_EDIT_PAINT_FACE_SEL) {
     const VArray<bool> selection = *attributes.lookup_or_default<bool>(
         ".select_poly", domain, false);
-    return IndexMask::from_bools(selection, memory);
+    return IndexMask::from_bools(visible, selection, memory);
   }
   if (mesh.editflag & ME_EDIT_PAINT_VERT_SEL) {
     const VArray<bool> selection = *attributes.lookup_or_default<bool>(
         ".select_vert", domain, false);
-    return IndexMask::from_bools(selection, memory);
+    return IndexMask::from_bools(visible, selection, memory);
   }
-  return IndexMask(attributes.domain_size(domain));
+
+  return visible;
 }
 
 static void face_corner_color_equalize_verts(Mesh &mesh, const IndexMask selection)

blender/blender

commit 1f545b1432a8be09d274b7538aedd7102bc69336

Fix #161882: New color attributes always have an alpha of 1
Caused by d3fce77461

`object_active_color_init` (no parameters except the color) is calling
`fill_active_color` (three optional parameters except the color:
`only_selected` , `affect_alpha` & `only_visible`).

While the intention from d3fce77461 was to call `fill_active_color` with
`only_selected` / `only_visible` being false, it accidentally passed
false twice (one of them ending up in `affect_alpha`...)

Now be super clear, pass all three (false, true, false).

Should be backported to 5.2

Pull Request: https://projects.blender.org/blender/blender/pulls/161910

source/blender/editors/sculpt_paint/mesh/paint_vertex.cc
@@ -2372,7 +2372,7 @@ static bool fill_active_color(Object &ob,
 
 bool object_active_color_init(Object &ob, const float fill_color[4])
 {
-  return fill_active_color(ob, ColorPaint4f(fill_color), false, false);
+  return fill_active_color(ob, ColorPaint4f(fill_color), false, true, false);
 }
 
 }  // namespace ed::sculpt_paint