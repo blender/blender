dcb7a69b5d286141846e810b3c03582ee34f9b29
Bastien Montagne <bastien@blender.org>
2026-07-29 11:43:48 +0200

    I18N: Refactor handling of fomratting tokens, add checks for `{}` syntax.
    This commit makes fairly extensive changes to how the code parses and
    handles formatting tokens (both 'printf' and 'format' syntaxes), and the
    escaping of LTR sequences in RTL languages pre-processing. It should also
    be noticiably more performant.
    * Factorize formatting tokens parsing into a new utils (`FormatToken` in
      `utils_format.py`). Improve its parsing logic by skipping blocks of
      text until the next potential token, and using a cheaper logic to find
      this potential token starting index.
    * Implement same 'skipping' logic in RTL escape code
      (`protect_format_seq` in `utils_rtl.py`).
    * Use this new FormatToken API both in code validating translated
      messages (`I18nMessages::check`) and handling RTL languages
      (`I18nMessages::rtl_process`).
    
    Note: A WIP version of these changes was used to fix #161681 in Blender
    5.2 (see !162050).

--- scripts/modules/_bl_i18n_utils/settings.py
+++ scripts/modules/_bl_i18n_utils/settings.py
@@ -343,16 +343,6 @@ PYGETTEXT_KEYWORDS = (() +
 # autopep8: on
 
 
-# Check printf mismatches between msgid and msgstr.
-CHECK_PRINTF_FORMAT = (
-    r"(?!<%)(?:%%)*%"          # Beginning, with handling for crazy things like '%%%%%s'
-    r"[-+#0]?"                 # Flags (note: do not add the ' ' (space) flag here, generates too much false positives!)
-    r"(?:\*|[0-9]+)?"          # Width
-    r"(?:\.(?:\*|[0-9]+))?"    # Precision
-    r"(?:[hljztL]|hh|ll)?"     # Length
-    r"[tldiuoxXfFeEgGaAcspn]"  # Specifiers (note we have Blender-specific %t and %l ones too)
-)
-
 # Should po parser warn when finding a first letter not capitalized?
 WARN_MSGID_NOT_CAPITALIZED = True
 
