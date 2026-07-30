ee1ad850dac0a842c6071a6f4360b70dbbb5c0eb
Bastien Montagne <bastien@blender.org>
2026-07-30 15:47:10 +0200

    Fix: invalid data access in new `ID::deep_hash` RNA property.
    Own mistake in 33fc0eb202, also added some warning in the doc of
    `PROP_BYTESTRING`, as this is fairly easy to miss.
    
    Reported by LLM in
    https://projects.blender.org/llm-test/llm-reports/src/branch/main/llm_review/log_2026_w30.md#id-add-rna-read-only-access-to-packed-linked-ids-deep-hash

--- source/blender/makesrna/RNA_types.hh
+++ source/blender/makesrna/RNA_types.hh
@@ -238,7 +238,13 @@ enum PropertySubType {
   PROP_FILEPATH = 1,
   PROP_DIRPATH = 2,
   PROP_FILENAME = 3,
-  /** A string which should be represented as bytes in python, NULL terminated though. */
+  /**
+   * A string which should be represented as bytes in python, NULL terminated though.
+   *
+   * WARNING: Default generated RNA accessor code will treat the underlying data as a regular
+   * C string (using `strlen` e.g. to get its length). If the bytes array may contain null chars,
+   * the RNA property _must_ have custom accessors defined (through
+   * #RNA_def_property_string_funcs). */
   PROP_BYTESTRING = 4,
   /* 5 was used by "PROP_TRANSLATE" sub-type, which is now a flag. */
   /** A string which should not be displayed in UI. */
