blender/blender

commit cc93b7f5a480e2198edd8ed26e70f25b758a8d74

Refactor: Cycles: Separate shader flags and runtime flags
We are running out of bits. Split the original `ShaderDataFlag` into
`ShaderRuntimeFlag`, which is determined by closures in the shader and
set up during rendering, and `ShaderDataFlag`, which is the same for the
whole shader graph and hence determined during shader compilation

The size of `ShaderData` did not change due to padding

Pull Request: https://projects.blender.org/blender/blender/pulls/161856

intern/cycles/kernel/integrator/subsurface.h
@@ -123,7 +123,7 @@ ccl_device int subsurface_bounce(KernelGlobals kg,
     INTEGRATOR_STATE_WRITE(state, subsurface, N) = sd->N;
   }
 
-  if (sd->flag & SD_BACKFACING) {
+  if (sd->runtime_flag & SR_BACKFACING) {
     path_flag |= PATH_RAY_SUBSURFACE_BACKFACING;
   }
 
@@ -156,12 +156,12 @@ ccl_device void subsurface_shader_data_setup(KernelGlobals kg, ccl_private Shade
 {
   /* Get bump mapped normal from shader evaluation at exit point. */
   float3 N = sd->N;
-  if (sd->flag & SD_HAS_BSSRDF_BUMP) {
+  if (sd->shader_flag & SD_HAS_BSSRDF_BUMP) {
     N = surface_shader_bssrdf_normal(sd);
   }
 
   /* Setup diffuse BSDF at the exit point. This replaces shader_eval_surface. */
-  sd->flag &= ~SD_CLOSURE_FLAGS;
+  sd->runtime_flag = 0;
   sd->num_closure = 0;
   sd->num_closure_left = kernel_data.max_closures;
 

intern/cycles/kernel/integrator/surface_shader.h
@@ -54,7 +54,7 @@ ccl_device_inline void surface_shader_prepare_guiding(KernelGlobals kg,
                                                       const ccl_private RNGState *rng_state)
 {
   /* Have any BSDF to guide? */
-  if (!(kernel_data.integrator.use_surface_guiding && (sd->flag & SD_BSDF_HAS_EVAL))) {
+  if (!(kernel_data.integrator.use_surface_guiding && (sd->runtime_flag & SR_BSDF_HAS_EVAL))) {
     INTEGRATOR_STATE_WRITE(state, guiding, use_surface_guiding) = false;
     return;
   }
@@ -153,7 +153,7 @@ ccl_device_inline void surface_shader_prepare_closures(KernelGlobals kg,
 
     if (path_visibility & PATH_RAY_VISIBILITY_CAMERA) {
       if (filter_closures & FILTER_CLOSURE_DIRECT_LIGHT) {
-        sd->flag &= ~SD_BSDF_HAS_EVAL;
+        sd->runtime_flag &= ~SR_BSDF_HAS_EVAL;
       }
 
       for (int i = 0; i < sd->num_closure; i++) {
@@ -176,7 +176,7 @@ ccl_device_inline void surface_shader_prepare_closures(KernelGlobals kg,
         {
           sc->type = CLOSURE_HOLDOUT_ID;
           sc->sample_weight = 0.0f;
-          sd->flag |= SD_HOLDOUT;
+          sd->runtime_flag |= SR_HOLDOUT;
         }
       }
     }
@@ -208,7 +208,7 @@ ccl_device_inline void surface_shader_prepare_closures(KernelGlobals kg,
       /* NOTE: this is a sufficient condition. If `blur_roughness < THRESH < original_roughness`
        * then the flag was already set. */
       if (!roughness_is_almost_specular(blur_roughness, blur_roughness)) {
-        sd->flag |= SD_BSDF_HAS_EVAL;
+        sd->runtime_flag |= SR_BSDF_HAS_EVAL;
       }
     }
   }
@@ -1046,10 +1046,10 @@ ccl_device float surface_shader_average_roughness(const ccl_private ShaderData *
 
 ccl_device Spectrum surface_shader_transparency(const ccl_private ShaderData *sd)
 {
-  if (sd->flag & SD_HAS_ONLY_VOLUME) {
+  if (sd->shader_flag & SD_HAS_ONLY_VOLUME) {
     return one_spectrum();
   }
-  if (sd->flag & (SD_TRANSPARENT | SD_RAY_PORTAL)) {
+  if (sd->runtime_flag & (SR_TRANSPARENT | SR_RAY_PORTAL)) {
     return sd->closure_transparent_extinction;
   }
   return zero_spectrum();
@@ -1190,7 +1190,7 @@ ccl_device bool surface_shader_constant_emission(KernelGlobals kg,
 
 ccl_device Spectrum surface_shader_background(const ccl_private ShaderData *sd)
 {
-  if (sd->flag & SD_EMISSION) {
+  if (sd->runtime_flag & SR_EMISSION) {
     return sd->closure_emission_background;
   }
   return zero_spectrum();
@@ -1200,7 +1200,7 @@ ccl_device Spectrum surface_shader_background(const ccl_private ShaderData *sd)
 
 ccl_device Spectrum surface_shader_emission(const ccl_private ShaderData *sd)
 {
-  if (sd->flag & SD_EMISSION) {
+  if (sd->runtime_flag & SR_EMISSION) {
     return emissive_simple_eval(sd->Ng, sd->wi) * sd->closure_emission_background;
   }
   return zero_spectrum();
@@ -1215,7 +1215,7 @@ ccl_device Spectrum surface_shader_apply_holdout(ccl_private ShaderData *sd)
   /* For objects marked as holdout, preserve transparency and remove all other
    * closures, replacing them with a holdout weight. */
   if (sd->object_flag & SD_OBJECT_HOLDOUT_MASK) {
-    if ((sd->flag & SD_TRANSPARENT) && !(sd->flag & SD_HAS_ONLY_VOLUME)) {
+    if ((sd->runtime_flag & SR_TRANSPARENT) && !(sd->shader_flag & SD_HAS_ONLY_VOLUME)) {
       weight = one_spectrum() - sd->closure_transparent_extinction;
 
       for (int i = 0; i < sd->num_closure; i++) {
@@ -1225,7 +1225,7 @@ ccl_device Spectrum surface_shader_apply_holdout(ccl_private ShaderData *sd)
         }
       }
 
-      sd->flag &= ~(SD_CLOSURE_FLAGS - (SD_TRANSPARENT | SD_BSDF));
+      sd->runtime_flag &= (SR_TRANSPARENT | SR_BSDF);
     }
     else {
       weight = one_spectrum();
@@ -1287,7 +1287,7 @@ ccl_device void surface_shader_eval(KernelGlobals kg,
 #else
     if (sd->object == OBJECT_NONE) {
       sd->closure_emission_background = make_spectrum(0.8f);
-      sd->flag |= SD_EMISSION;
+      sd->runtime_flag |= SR_EMISSION;
     }
     else {
       bsdf_diffuse_setup(sd, sd->N, make_spectrum(0.8f));

intern/cycles/kernel/integrator/volume_shader.h
@@ -410,8 +410,8 @@ ccl_device_inline void volume_shader_motion_blur(KernelGlobals kg,
    */
 
   /* Always use linear interpolation for velocity. */
-  const int cubic_flag = sd->flag & SD_VOLUME_CUBIC;
-  sd->flag &= ~SD_VOLUME_CUBIC;
+  const int cubic_flag = sd->shader_flag & SD_VOLUME_CUBIC;
+  sd->shader_flag &= ~SD_VOLUME_CUBIC;
 
   /* Find velocity. */
   float3 velocity = primitive_volume_attribute<float3>(kg, sd, v_desc, false);
@@ -428,7 +428,7 @@ ccl_device_inline void volume_shader_motion_blur(KernelGlobals kg,
   sd->P = P - (time - time_offset) * velocity_scale * velocity;
 
   /* Restore flag. */
-  sd->flag |= cubic_flag;
+  sd->shader_flag |= cubic_flag;
 }
 #  endif
 
@@ -451,8 +451,7 @@ ccl_device_inline bool volume_shader_eval_entry(KernelGlobals kg,
   sd->object = entry.object;
   sd->shader = entry.shader;
 
-  sd->flag &= ~SD_SHADER_FLAGS;
-  sd->flag |= kernel_data_fetch(shaders, (sd->shader & SHADER_MASK)).flags;
+  sd->shader_flag = kernel_data_fetch(shaders, (sd->shader & SHADER_MASK)).flags;
   sd->object_flag &= ~SD_OBJECT_FLAGS;
 
   if (sd->object != OBJECT_NONE) {
@@ -516,7 +515,8 @@ ccl_device_inline void volume_shader_eval(KernelGlobals kg,
    * for all volumes in the stack into a single array of closures */
   sd->num_closure = 0;
   sd->num_closure_left = max_closures;
-  sd->flag = SD_IS_VOLUME_SHADER_EVAL | (sd->flag & SD_CACHE_MISS);
+  sd->shader_flag = 0;
+  sd->runtime_flag = SR_IS_VOLUME_SHADER_EVAL | (sd->runtime_flag & SR_CACHE_MISS);
   sd->object_flag = 0;
 
   for (int i = 0;; i++) {

intern/cycles/kernel/types.h
@@ -900,42 +900,38 @@ struct ccl_align(16) ShaderClosure {
  * are in world space.
  */
 
-enum ShaderDataFlag {
-  /* Runtime flags. */
-
+/* Shader runtime flags, determined during rendering. */
+enum ShaderRuntimeFlag {
   /* Set when ray hits backside of surface. */
-  SD_BACKFACING = (1 << 0),
+  SR_BACKFACING = (1 << 0),
   /* Shader has non-zero emission. */
-  SD_EMISSION = (1 << 1),
+  SR_EMISSION = (1 << 1),
   /* Shader has BSDF closure. */
-  SD_BSDF = (1 << 2),
+  SR_BSDF = (1 << 2),
   /* Shader has non-singular BSDF closure. */
-  SD_BSDF_HAS_EVAL = (1 << 3),
+  SR_BSDF_HAS_EVAL = (1 << 3),
   /* Shader has BSSRDF closure. */
-  SD_BSSRDF = (1 << 4),
+  SR_BSSRDF = (1 << 4),
   /* Shader has holdout closure. */
-  SD_HOLDOUT = (1 << 5),
+  SR_HOLDOUT = (1 << 5),
   /* Shader has non-zero volume extinction. */
-  SD_EXTINCTION = (1 << 6),
+  SR_EXTINCTION = (1 << 6),
   /* Shader has a volume phase (scatter) closure. */
-  SD_SCATTER = (1 << 7),
+  SR_SCATTER = (1 << 7),
   /* Shader is being evaluated in a volume. */
-  SD_IS_VOLUME_SHADER_EVAL = (1 << 8),
+  SR_IS_VOLUME_SHADER_EVAL = (1 << 8),
   /* Shader has transparent closure. */
-  SD_TRANSPARENT = (1 << 9),
+  SR_TRANSPARENT = (1 << 9),
   /* BSDF has a transmissive component. */
-  SD_BSDF_HAS_TRANSMISSION = (1 << 10),
+  SR_BSDF_HAS_TRANSMISSION = (1 << 10),
   /* Shader has ray portal closure. */
-  SD_RAY_PORTAL = (1 << 11),
+  SR_RAY_PORTAL = (1 << 11),
   /* Shader evaluation needs to be redone, because of texture cache miss */
-  SD_CACHE_MISS = (1 << 12),
+  SR_CACHE_MISS = (1 << 12),
+};
 
-  SD_CLOSURE_FLAGS = (SD_EMISSION | SD_BSDF | SD_BSDF_HAS_EVAL | SD_BSSRDF | SD_HOLDOUT |
-                      SD_EXTINCTION | SD_SCATTER | SD_IS_VOLUME_SHADER_EVAL |
-                      SD_BSDF_HAS_TRANSMISSION | SD_RAY_PORTAL | SD_CACHE_MISS),
-
-  /* Shader flags. */
-
+/* Shader flags that are set after compiling the shaders. */
+enum ShaderDataFlag {
   /* If Light Path Node is present in the shader graph. */
   SD_HAS_LIGHT_PATH_NODE = (1 << 13),
   /* Has bump mapping from BSDF connected to surface socket. */
@@ -975,12 +971,6 @@ enum ShaderDataFlag {
   SD_HAS_RAYTRACE = (1 << 30),
   /* Use back side for direct light sampling. */
   SD_MIS_BACK = (1 << 31),
-
-  SD_SHADER_FLAGS = (SD_MIS_FRONT | SD_HAS_TRANSPARENT_SHADOW | SD_HAS_VOLUME |
-                     SD_HAS_ONLY_VOLUME | SD_HETEROGENEOUS_VOLUME | SD_HAS_BSSRDF_BUMP |
-                     SD_VOLUME_EQUIANGULAR | SD_VOLUME_MIS | SD_VOLUME_CUBIC | SD_HAS_BUMP |
-                     SD_HAS_DISPLACEMENT | SD_HAS_CONSTANT_EMISSION | SD_NEED_VOLUME_ATTRIBUTES |
-                     SD_HAS_EMISSION | SD_HAS_RAYTRACE | SD_MIS_BACK)
 };
 
 /* Object flags. */
@@ -1037,8 +1027,10 @@ struct ccl_align(16) ShaderData {
 
   /* shader id */
   int shader;
+  /* booleans describing shader, see ShaderRuntimeFlag */
+  int runtime_flag;
   /* booleans describing shader, see ShaderDataFlag */
-  int flag;
+  int shader_flag;
   /* booleans describing object of the shader, see ShaderDataObjectFlag */
   uint object_flag;
 

commit 3cd17ad53a7036f48a39828e5a8f4de5d3e52a15

Fix: Cycles SSS and holdout incorrectly handles backfacing
Issue introduced in cc93b7f5a480. SD_CLOSURE_FLAGS did not cover all
runtime flags. Specifically SR_BACKFACING and SR_CACHE_MISS still need
to be preserved when replacing all closures.

Pull Request: https://projects.blender.org/blender/blender/pulls/162099

intern/cycles/kernel/integrator/subsurface.h
@@ -161,7 +161,7 @@ ccl_device void subsurface_shader_data_setup(KernelGlobals kg, ccl_private Shade
   }
 
   /* Setup diffuse BSDF at the exit point. This replaces shader_eval_surface. */
-  sd->runtime_flag = 0;
+  sd->runtime_flag &= ~SR_CLOSURE_FLAG;
   sd->num_closure = 0;
   sd->num_closure_left = kernel_data.max_closures;
 

intern/cycles/kernel/integrator/surface_shader.h
@@ -1225,7 +1225,7 @@ ccl_device Spectrum surface_shader_apply_holdout(ccl_private ShaderData *sd)
         }
       }
 
-      sd->runtime_flag &= (SR_TRANSPARENT | SR_BSDF);
+      sd->runtime_flag &= (~SR_CLOSURE_FLAG | SR_TRANSPARENT | SR_BSDF);
     }
     else {
       weight = one_spectrum();

intern/cycles/kernel/integrator/volume_shader.h
@@ -516,7 +516,7 @@ ccl_device_inline void volume_shader_eval(KernelGlobals kg,
   sd->num_closure = 0;
   sd->num_closure_left = max_closures;
   sd->shader_flag = 0;
-  sd->runtime_flag = SR_IS_VOLUME_SHADER_EVAL | (sd->runtime_flag & SR_CACHE_MISS);
+  sd->runtime_flag = SR_IS_VOLUME_SHADER_EVAL | (sd->runtime_flag & ~SR_CLOSURE_FLAG);
   sd->object_flag = 0;
 
   for (int i = 0;; i++) {

intern/cycles/kernel/types.h
@@ -904,30 +904,36 @@ struct ccl_align(16) ShaderClosure {
 enum ShaderRuntimeFlag {
   /* Set when ray hits backside of surface. */
   SR_BACKFACING = (1 << 0),
-  /* Shader has non-zero emission. */
-  SR_EMISSION = (1 << 1),
-  /* Shader has BSDF closure. */
-  SR_BSDF = (1 << 2),
-  /* Shader has non-singular BSDF closure. */
-  SR_BSDF_HAS_EVAL = (1 << 3),
-  /* Shader has BSSRDF closure. */
-  SR_BSSRDF = (1 << 4),
-  /* Shader has holdout closure. */
-  SR_HOLDOUT = (1 << 5),
-  /* Shader has non-zero volume extinction. */
-  SR_EXTINCTION = (1 << 6),
-  /* Shader has a volume phase (scatter) closure. */
-  SR_SCATTER = (1 << 7),
-  /* Shader is being evaluated in a volume. */
-  SR_IS_VOLUME_SHADER_EVAL = (1 << 8),
-  /* Shader has transparent closure. */
-  SR_TRANSPARENT = (1 << 9),
-  /* BSDF has a transmissive component. */
-  SR_BSDF_HAS_TRANSMISSION = (1 << 10),
-  /* Shader has ray portal closure. */
-  SR_RAY_PORTAL = (1 << 11),
   /* Shader evaluation needs to be redone, because of texture cache miss */
-  SR_CACHE_MISS = (1 << 12),
+  SR_CACHE_MISS = (1 << 1),
+
+  /* Shader has non-zero emission. */
+  SR_EMISSION = (1 << 2),
+  /* Shader has BSDF closure. */
+  SR_BSDF = (1 << 3),
+  /* Shader has non-singular BSDF closure. */
+  SR_BSDF_HAS_EVAL = (1 << 4),
+  /* Shader has BSSRDF closure. */
+  SR_BSSRDF = (1 << 5),
+  /* Shader has holdout closure. */
+  SR_HOLDOUT = (1 << 6),
+  /* Shader has non-zero volume extinction. */
+  SR_EXTINCTION = (1 << 7),
+  /* Shader has a volume phase (scatter) closure. */
+  SR_SCATTER = (1 << 8),
+  /* Shader is being evaluated in a volume. */
+  SR_IS_VOLUME_SHADER_EVAL = (1 << 9),
+  /* Shader has transparent closure. */
+  SR_TRANSPARENT = (1 << 10),
+  /* BSDF has a transmissive component. */
+  SR_BSDF_HAS_TRANSMISSION = (1 << 11),
+  /* Shader has ray portal closure. */
+  SR_RAY_PORTAL = (1 << 12),
+
+  /* Flags that indicate the presence of closures. */
+  SR_CLOSURE_FLAG = (SR_EMISSION | SR_BSDF | SR_BSDF_HAS_EVAL | SR_BSSRDF | SR_HOLDOUT |
+                     SR_EXTINCTION | SR_SCATTER | SR_IS_VOLUME_SHADER_EVAL | SR_TRANSPARENT |
+                     SR_BSDF_HAS_TRANSMISSION | SR_RAY_PORTAL)
 };
 
 /* Shader flags that are set after compiling the shaders. */