970d53112c9eb21987eb0b6634a1acddc5d0261e
Sergey Sharybin <sergey@blender.org>
2026-08-04 22:09:15 +0200

    Cycles: Use metal::float3x3 for implementation
    Semantically Cycles float3x3 stays the same: row-major, with the x, y,
    and z components defining the coordinate frame. However, math functions
    are using Metal's native metal::float3x3.
    
    It mainly follows the idea in the Transform. The actual speedup has not
    been measured.
    
    Ref #159470
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/162648

--- intern/cycles/util/types_float3x3.h
+++ intern/cycles/util/types_float3x3.h
@@ -33,10 +33,44 @@ struct float3x3 {
   }
 #endif
 
-  float3 x;
-  float3 y;
-  float3 z;
+#if defined(__KERNEL_METAL__)
+  ccl_device_inline_method float3x3() = default;
+
+  ccl_device_inline_method float3x3(const ccl_private float3x3 &other) = default;
+  ccl_device_inline_method float3x3(const ccl_global float3x3 &other) = default;
+
+  ccl_device_inline_method float3x3(const metal::float3x3 m)
+  {
+    this->m = m;
+  }
+
+  ccl_device_inline_method float3x3(const float3 x, const float3 y, const float3 z)
+  {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+  }
+
+  union {
+    struct {
+      float3 x, y, z;
+    };
+    /* NOTE: Metal uses column-major notation. */
+    metal::float3x3 m;
+  };
+#else
+  float3 x, y, z;
+#endif
 };
+
+#if defined(__KERNEL_METAL__)
+/* Metal implementation reinterpret-casts float3x3 to metal::float3x3 to take full advantage of the
+ * hardware and instruction set. Doing so requires both types to have same size and alignment.
+ *
+ * NOTE: Metal uses column-major notation. */
+static_assert(sizeof(float3x3) == sizeof(metal::float3x3));
+static_assert(alignof(float3x3) == alignof(metal::float3x3));
+#endif
 
 /* Construct new 3x3 matrix from the given coordinate system (rows of the matrix). */
 ccl_device_inline float3x3 make_float3x3(const float3 x, const float3 y, const float3 z)
@@ -60,21 +94,17 @@ ccl_device_inline float3x3 identity_float3x3()
  *
  * For math operations this must be converted to float3x3, so that on the CPU SIMD instructions
  * can be used. */
-/* TODO(sergey): Support on all platforms.
- * The tricky part is Metal, where pointers and references require explicit address space. What is
- * the address space of `this`? */
-#if !defined(__KERNEL_GPU__)
 struct packed_float3x3 {
   packed_float3x3() = default;
 
-  ccl_device_inline_method packed_float3x3(const float3x3 &m) : x(m.x), y(m.y), z(m.z) {}
+  ccl_device_inline_method packed_float3x3(const float3x3 m) : x(m.x), y(m.y), z(m.z) {}
 
   ccl_device_inline_method operator float3x3() const
   {
     return make_float3x3(x, y, z);
   }
 
-  ccl_device_inline_method packed_float3x3 &operator=(const float3x3 &a)
+  ccl_device_inline_method packed_float3x3 operator=(const float3x3 a)
   {
     x = a.x;
     y = a.y;
@@ -87,7 +117,6 @@ struct packed_float3x3 {
   packed_float3 z;
 };
 static_assert(sizeof(packed_float3x3) == 36, "packed_float3x3 expected to be exactly 36 bytes");
-#endif
 
 #if defined __METAL_PRINTF__
 #  define print_float3x3(label, a) \
