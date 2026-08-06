blender/blender

commit 8adba33029f766eba157c283a9e01dd35384f97b

Mesh: Switch from CustomData to AttributeStorage
This commit changes Mesh to use AttributeStorage to store generic
attributes, similar to previous changes for curves and point clouds.
The main benefit is that this matches the generic attribute design
better than CustomData. This is discussed more in #122398.

Most of the necessary changes landed separately over the past year or so
by moving code to use the higher-level attribute API instead of using
CustomData directly. This commits changes the remaining cases where
lower level data access was necessary, and replaces the attribute
provider system.

Resolves #107500

Pull Request: https://projects.blender.org/blender/blender/pulls/152851

source/blender/blenkernel/BKE_customdata.hh
@@ -229,14 +229,6 @@ bool CustomData_bmesh_merge_layout(const CustomData *source,
                                    char htype);
 
 /**
- * Remove layers that aren't stored in BMesh or are stored as flags on BMesh.
- * The `layers` array of the returned #CustomData must be freed, but may be null.
- * Used during conversion of #Mesh data to #BMesh storage format.
- */
-CustomData CustomData_shallow_copy_remove_non_bmesh_attributes(const CustomData *src,
-                                                               eCustomDataMask mask);
-
-/**
  * NULL's all members and resets the #CustomData.typemap.
  *
  * \warning Does not free or release any internal resources.

source/blender/blenkernel/intern/customdata.cc
@@ -2381,30 +2381,6 @@ bool CustomData_merge_layout(const CustomData *source,
   return customdata_merge_internal(source, dest, mask, alloctype, totelem);
 }
 
-CustomData CustomData_shallow_copy_remove_non_bmesh_attributes(const CustomData *src,
-                                                               const eCustomDataMask mask)
-{
-  Vector<CustomDataLayer> dst_layers;
-  for (const CustomDataLayer &layer : Span<CustomDataLayer>{src->layers, src->totlayer}) {
-    if (BM_attribute_stored_in_bmesh_builtin(layer.name)) {
-      continue;
-    }
-    if (!(mask & CD_TYPE_AS_MASK(eCustomDataType(layer.type)))) {
-      continue;
-    }
-    dst_layers.append(layer);
-  }
-
-  CustomData dst = *src;
-  dst.layers = MEM_new_array_for_free<CustomDataLayer>(dst_layers.size(), __func__);
-  dst.maxlayer = dst.totlayer = dst_layers.size();
-  memcpy(dst.layers, dst_layers.data(), dst_layers.as_span().size_in_bytes());
-
-  CustomData_update_typemap(&dst);
-
-  return dst;
-}
-
 /**
  * An #ImplicitSharingInfo that knows how to free the entire referenced custom data layer
  * (including potentially separately allocated chunks like for vertex groups).

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -35,6 +35,7 @@
 
 #include "BKE_attribute.h"
 #include "BKE_attribute.hh"
+#include "BKE_attribute_legacy_convert.hh"
 #include "BKE_customdata.hh"
 #include "BKE_global.hh"
 #include "BKE_idprop.hh"
@@ -746,6 +747,9 @@ void BKE_mesh_convert_mfaces_to_mpolys(Mesh *mesh)
                            &mesh->faces_num);
   BKE_mesh_legacy_convert_loops_to_corners(mesh);
   BKE_mesh_legacy_convert_polys_to_offsets(mesh);
+  mesh->attribute_storage.wrap().remove(".corner_vert");
+  mesh->attribute_storage.wrap().remove(".corner_edge");
+  bke::mesh_convert_customdata_to_storage(*mesh);
 
   mesh_ensure_tessellation_customdata(mesh);
 }
@@ -795,6 +799,7 @@ void BKE_mesh_do_versions_convert_mfaces_to_mpolys(Mesh *mesh)
                            &mesh->faces_num);
   BKE_mesh_legacy_convert_loops_to_corners(mesh);
   BKE_mesh_legacy_convert_polys_to_offsets(mesh);
+  bke::mesh_convert_customdata_to_storage(*mesh);
 
   CustomData_bmesh_do_versions_update_active_layers(&mesh->fdata_legacy, &mesh->corner_data);
 
@@ -2661,19 +2666,30 @@ void BKE_mesh_calc_edges_tessface(Mesh *mesh)
   /* write new edges into a temporary CustomData */
   CustomData edgeData;
   CustomData_reset(&edgeData);
-  CustomData_add_layer_named(&edgeData, CD_PROP_INT32_2D, CD_CONSTRUCT, numEdges, ".edge_verts");
   CustomData_add_layer(&edgeData, CD_ORIGINDEX, CD_SET_DEFAULT, numEdges);
 
-  int2 *ege = static_cast<int2 *>(CustomData_get_layer_named_for_write(
-      &edgeData, CD_PROP_INT32_2D, ".edge_verts", mesh->edges_num));
-  int *index = static_cast<int *>(
-      CustomData_get_layer_for_write(&edgeData, CD_ORIGINDEX, mesh->edges_num));
+  int *index = (int *)CustomData_get_layer_for_write(&edgeData, CD_ORIGINDEX, mesh->edges_num);
 
   memset(index, ORIGINDEX_NONE, sizeof(int) * numEdges);
-  MutableSpan(ege, numEdges).copy_from(eh.as_span().cast<int2>());
 
   /* free old CustomData and assign new one */
   CustomData_free(&mesh->edge_data);
+  Set<StringRef> edge_attributes;
+  mesh->attribute_storage.wrap().foreach([&](const bke::Attribute &attr) {
+    if (attr.domain() == bke::AttrDomain::Edge) {
+      edge_attributes.add(attr.name());
+    }
+  });
+  for (const StringRef name : edge_attributes) {
+    mesh->attribute_storage.wrap().remove(name);
+  }
+  OrderedEdge *vector_data = eh.extract_vector().release().data;
+  bke::Attribute::ArrayData data{};
+  data.size = numEdges;
+  data.data = reinterpret_cast<int2 *>(vector_data);
+  data.sharing_info = ImplicitSharingPtr<>(implicit_sharing::info_for_mem_free(vector_data));
+  mesh->attribute_storage.wrap().add(
+      ".edge_verts", bke::AttrDomain::Edge, bke::AttrType::Int32_2D, std::move(data));
   mesh->edge_data = edgeData;
   mesh->edges_num = numEdges;
 }

commit 8297ddc6351f91f315e948a70370d3740bd18f81

Fix #161905: External Multires data cleared when exiting Edit Mode
Caused by 8adba33029

When moving the backing of Mesh attributes to `AttributeStorage`,
handling external packed data was missed. This adds explicit support for
it for `CD_MDISPS` data when converting `Mesh` to and from `BMesh`.

Additionally, this fixes a number of issues when handling `.blend` files
that reference external `.btx` files.

* Loading external Multires data in background mode may have failed
  previously if the depsgraph was not up-to-date, as the reading process
  would unset the `MDisps` `totdisps` value, resulting in a failure when
  reading 0 bytes of data
* Saving files to different directories could fail to rebase the
  external filepath correctly, this is because the base filepath was
  calculated from the `Main` of the owning ID, rather than the target
  resulting filepath.

Pull Request: https://projects.blender.org/blender/blender/pulls/162110

.gitattributes
@@ -108,4 +108,5 @@
 *.icns filter=lfs diff=lfs merge=lfs -text
 *.blob filter=lfs diff=lfs merge=lfs -text
 *.rgb filter=lfs diff=lfs merge=lfs -text
+*.btx filter=lfs diff=lfs merge=lfs -text
 scripts/modules/_bpy_internal/assets/remote_library/*_openapi.py linguist-generated=true

source/blender/blenkernel/BKE_customdata.hh
@@ -608,8 +608,9 @@ void CustomData_external_remove(CustomData *data, ID *id, eCustomDataType type, 
 bool CustomData_external_test(CustomData *data, eCustomDataType type);
 
 void CustomData_external_write(
-    CustomData *data, ID *id, eCustomDataMask mask, int totelem, int free);
-void CustomData_external_read(CustomData *data, ID *id, eCustomDataMask mask, int totelem);
+    CustomData *data, StringRefNull basepath, ID *id, eCustomDataMask mask, int totelem, int free);
+void CustomData_external_read(
+    CustomData *data, StringRefNull basepath, ID *id, eCustomDataMask mask, int totelem);
 void CustomData_external_reload(CustomData *data, ID *id, eCustomDataMask mask, int totelem);
 
 /* Mesh-to-mesh transfer data. */

source/blender/blenkernel/intern/customdata.cc
@@ -4245,11 +4245,17 @@ static bool CustomData_layer_ensure_data_exists(CustomDataLayer *layer, size_t c
  * \{ */
 
 static void customdata_external_filename(char filepath[FILE_MAX],
+                                         StringRefNull basepath,
                                          ID *id,
                                          CustomDataExternal *external)
 {
   BLI_strncpy(filepath, external->filepath, FILE_MAX);
-  BLI_path_abs(filepath, ID_BLEND_PATH_FROM_GLOBAL(id));
+  if (basepath.is_empty()) {
+    BLI_path_abs(filepath, ID_BLEND_PATH_FROM_GLOBAL(id));
+  }
+  else {
+    BLI_path_abs(filepath, basepath.c_str());
+  }
 }
 
 void CustomData_external_reload(CustomData *data, ID * /*id*/, eCustomDataMask mask, int totelem)
@@ -4270,7 +4276,8 @@ void CustomData_external_reload(CustomData *data, ID * /*id*/, eCustomDataMask m
   }
 }
 
-void CustomData_external_read(CustomData *data, ID *id, eCustomDataMask mask, const int totelem)
+void CustomData_external_read(
+    CustomData *data, StringRefNull basepath, ID *id, eCustomDataMask mask, const int totelem)
 {
   CustomDataExternal *external = data->external;
   CustomDataLayer *layer;
@@ -4300,7 +4307,7 @@ void CustomData_external_read(CustomData *data, ID *id, eCustomDataMask mask, co
     return;
   }
 
-  customdata_external_filename(filepath, id, external);
+  customdata_external_filename(filepath, basepath, id, external);
 
   CDataFile *cdf = cdf_create(CDF_TYPE_MESH);
   if (!cdf_read_open(cdf, filepath)) {
@@ -4346,8 +4353,12 @@ void CustomData_external_read(CustomData *data, ID *id, eCustomDataMask mask, co
   cdf_free(cdf);
 }
 
-void CustomData_external_write(
-    CustomData *data, ID *id, eCustomDataMask mask, const int totelem, const int free)
+void CustomData_external_write(CustomData *data,
+                               StringRefNull basepath,
+                               ID *id,
+                               eCustomDataMask mask,
+                               const int totelem,
+                               const int free)
 {
   CustomDataExternal *external = data->external;
   int update = 0;
@@ -4375,8 +4386,8 @@ void CustomData_external_write(
   }
 
   /* make sure data is read before we try to write */
-  CustomData_external_read(data, id, mask, totelem);
-  customdata_external_filename(filepath, id, external);
+  CustomData_external_read(data, basepath, id, mask, totelem);
+  customdata_external_filename(filepath, basepath, id, external);
 
   CDataFile *cdf = cdf_create(CDF_TYPE_MESH);
 
@@ -4497,7 +4508,8 @@ void CustomData_external_remove(CustomData *data,
 
   if (layer->flag & CD_FLAG_EXTERNAL) {
     if (!(layer->flag & CD_FLAG_IN_MEMORY)) {
-      CustomData_external_read(data, id, CD_TYPE_AS_MASK(eCustomDataType(layer->type)), totelem);
+      CustomData_external_read(
+          data, "", id, CD_TYPE_AS_MASK(eCustomDataType(layer->type)), totelem);
     }
 
     layer->flag &= ~CD_FLAG_EXTERNAL;
@@ -4868,7 +4880,7 @@ void CustomData_blend_write(BlendWriter *writer,
 {
   /* write external customdata (not for undo) */
   if (data->external && !BLO_write_is_undo(writer)) {
-    CustomData_external_write(data, id, cddata_mask, count, 0);
+    CustomData_external_write(data, BLO_write_filepath(writer), id, cddata_mask, count, 0);
   }
 
   for (const CustomDataLayer &layer : layers_to_write) {
@@ -4903,7 +4915,7 @@ static void blend_read_mdisps(BlendDataReader *reader,
                              reinterpret_cast<int8_t **>(&md.hidden),
                              BLI_BITMAP_SIZE(md.totdisp) * sizeof(BLI_bitmap));
       }
-      if (!ok) {
+      if (!external && !ok) {
         md.totdisp = 0;
       }
 

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -522,7 +522,7 @@ static void convert_mfaces_to_mpolys(ID *id,
   if (id) {
     /* ensure external data is transferred */
     /* TODO(sergey): Use multiresModifier_ensure_external_read(). */
-    CustomData_external_read(fdata_legacy, id, CD_MASK_MDISPS, totface_i);
+    CustomData_external_read(fdata_legacy, "", id, CD_MASK_MDISPS, totface_i);
   }
 
   Map<OrderedEdge, int> eh;

source/blender/blenkernel/intern/multires.cc
@@ -732,7 +732,7 @@ void multiresModifier_prepare_join(Depsgraph *depsgraph, Scene *scene, Object *o
 
 void multires_topology_changed(Mesh *mesh)
 {
-  CustomData_external_read(&mesh->corner_data, &mesh->id, CD_MASK_MDISPS, mesh->corners_num);
+  CustomData_external_read(&mesh->corner_data, "", &mesh->id, CD_MASK_MDISPS, mesh->corners_num);
   MDisps *mdisp = static_cast<MDisps *>(
       CustomData_get_layer_for_write(&mesh->corner_data, CD_MDISPS, mesh->corners_num));
 
@@ -795,7 +795,7 @@ void multires_ensure_external_read(Mesh *mesh, const int top_level)
     mdisps[i].totdisp = totdisp;
   }
 
-  CustomData_external_read(&mesh->corner_data, &mesh->id, CD_MASK_MDISPS, mesh->corners_num);
+  CustomData_external_read(&mesh->corner_data, "", &mesh->id, CD_MASK_MDISPS, mesh->corners_num);
 }
 void multiresModifier_ensure_external_read(Mesh *mesh, const MultiresModifierData *mmd)
 {

source/blender/blenloader/BLO_read_write.hh
@@ -299,6 +299,11 @@ struct BLO_Write_IDBuffer {
 /* Misc. */
 
 /**
+ * \note Should not be strictly needed outside of handling external Multires .btx files.
+ */
+StringRefNull BLO_write_filepath(const BlendWriter *writer);
+
+/**
  * Check if the data can be written more efficiently by making use of implicit-sharing. If yes, the
  * user count of the sharing-info is increased making the data immutable. The provided callback
  * should serialize the potentially shared data. It is only called when necessary.

source/blender/blenloader/intern/writefile.cc
@@ -1805,6 +1805,7 @@ static void prepare_stable_data_block_ids(WriteData &wd, Main &bmain)
  */
 static bool write_file_handle(Main *mainvar,
                               WriteWrap *ww,
+                              StringRefNull filepath,
                               MemFile *compare,
                               MemFile *current,
                               const int write_flags,
@@ -1816,6 +1817,7 @@ static bool write_file_handle(Main *mainvar,
 
   wd = mywrite_begin(ww, compare, current);
   wd->debug_dst = debug_dst;
+  wd->filepath = filepath;
   BlendWriter writer = {wd};
 
   BKE_main_view_layers_synced_ensure(mainvar);
@@ -2126,7 +2128,7 @@ static bool BLO_write_file_impl(Main *mainvar,
 
   /* Actual file writing. */
   const bool err = write_file_handle(
-      mainvar, &ww, nullptr, nullptr, write_flags, use_userdef, thumb, debug_dst);
+      mainvar, &ww, filepath, nullptr, nullptr, write_flags, use_userdef, thumb, debug_dst);
 
   const bool close_error = !ww.close();
 
@@ -2197,7 +2199,7 @@ bool BLO_write_file_mem(Main *mainvar, MemFile *compare, MemFile *current, const
   bool use_userdef = false;
 
   const bool err = write_file_handle(
-      mainvar, nullptr, compare, current, write_flags, use_userdef, nullptr, nullptr);
+      mainvar, nullptr, "", compare, current, write_flags, use_userdef, nullptr, nullptr);
 
   return (err == 0);
 }
@@ -2375,6 +2377,11 @@ void BLO_write_generated_pointer_tag(BlendWriter *writer, const void *data)
   BLI_assert(writer->wd->stable_address_ids.pointer_map.lookup_default(data, address_id) ==
              address_id);
   writer->wd->stable_address_ids.pointer_map.add(data, address_id);
+}
+
+StringRefNull BLO_write_filepath(const BlendWriter *writer)
+{
+  return writer->wd->filepath;
 }
 
 void BLO_write_shared(BlendWriter *writer,

source/blender/blenloader/intern/writefile.hh
@@ -141,6 +141,9 @@ struct WriteData {
    */
   WriteWrap *ww;
 
+  /** Filepath being written to, may be the empty string. */
+  std::string filepath;
+
   /**
    * Timestamp info defined when creating the new WriteData. Used for performance logging.
    */

source/blender/bmesh/intern/bmesh_mesh_convert.cc
@@ -304,6 +304,12 @@ static CustomData get_mesh_to_bm_custom_data(const Mesh &mesh,
     }
     CustomData_add_layer_named(
         &custom_data, eCustomDataType(layer.type), CD_SET_DEFAULT, 0, layer.name);
+    if (layer.type == CD_MDISPS && layer.flag & CD_FLAG_EXTERNAL) {
+      const int new_layer_i = CustomData_get_layer_index(&custom_data, layer.type);
+      BLI_assert(new_layer_i != -1);
+      CustomDataLayer &new_layer = custom_data.layers[new_layer_i];
+      new_layer.flag = layer.flag;
+    }
   }
   return custom_data;
 }
@@ -326,6 +332,11 @@ void BM_mesh_bm_from_me(BMesh *bm, const Mesh *mesh, const BMeshFromMeshParams *
   CustomData mesh_pdata = get_mesh_to_bm_custom_data(*mesh, bke::AttrDomain::Face, mask.pmask);
   CustomData mesh_ldata = get_mesh_to_bm_custom_data(*mesh, bke::AttrDomain::Corner, mask.lmask);
 
+  const CustomData &mesh_data = get_mesh_custom_data(*mesh, AttrDomain::Corner);
+  if (mesh_data.external) {
+    mesh_ldata.external = MEM_dupalloc(mesh_data.external);
+  }
+
   Vector<std::string> temporary_layers_to_delete;
 
   for (const int layer_index :
@@ -347,6 +358,8 @@ void BM_mesh_bm_from_me(BMesh *bm, const Mesh *mesh, const BMeshFromMeshParams *
     for (const std::string &name : temporary_layers_to_delete) {
       CustomData_free_layer_named(&mesh_ldata, name);
     }
+
+    MEM_SAFE_DELETE(mesh_ldata.external);
 
     MEM_SAFE_DELETE(mesh_vdata.layers);
     MEM_SAFE_DELETE(mesh_edata.layers);
@@ -1606,6 +1619,12 @@ static void add_bm_cd_to_mesh(const BMesh &bm,
         continue;
       }
       CustomData_add_layer_named(&mesh_data, cd_type, CD_CONSTRUCT, domain_size, layer.name);
+      if (layer.type == CD_MDISPS && layer.flag & CD_FLAG_EXTERNAL) {
+        const int new_layer_i = CustomData_get_layer_index(&mesh_data, layer.type);
+        BLI_assert(new_layer_i != -1);
+        CustomDataLayer &new_layer = mesh_data.layers[new_layer_i];
+        new_layer.flag = layer.flag;
+      }
     }
   }
 }
@@ -1721,6 +1740,13 @@ void BM_mesh_bm_to_me(Main *bmain, BMesh *bm, Mesh *mesh, const BMeshToMeshParam
   {
     CustomData_MeshMasks mask = CD_MASK_MESH;
     CustomData_MeshMasks_update(&mask, &params->cd_mask_extra);
+
+    const CustomData &bm_data = get_bm_custom_data(*bm, AttrDomain::Corner);
+    CustomData &mesh_data = get_mesh_custom_data(*mesh, AttrDomain::Corner);
+    if (bm_data.external) {
+      mesh_data.external = MEM_dupalloc(bm_data.external);
+    }
+
     add_bm_cd_to_mesh(*bm, bke::AttrDomain::Point, mask.vmask, *mesh);
     add_bm_cd_to_mesh(*bm, bke::AttrDomain::Edge, mask.emask, *mesh);
     add_bm_cd_to_mesh(*bm, bke::AttrDomain::Face, mask.pmask, *mesh);
@@ -2035,6 +2061,13 @@ void BM_mesh_bm_to_me_compact(BMesh &bm,
 
   if (add_mesh_attributes) {
     const CustomData_MeshMasks &mask_final = mask ? *mask : CD_MASK_DERIVEDMESH;
+
+    const CustomData &bm_data = get_bm_custom_data(bm, AttrDomain::Corner);
+    CustomData &mesh_data = get_mesh_custom_data(mesh, AttrDomain::Corner);
+    if (bm_data.external) {
+      mesh_data.external = MEM_dupalloc(bm_data.external);
+    }
+
     add_bm_cd_to_mesh(bm, bke::AttrDomain::Point, mask_final.vmask, mesh);
     add_bm_cd_to_mesh(bm, bke::AttrDomain::Edge, mask_final.emask, mesh);
     add_bm_cd_to_mesh(bm, bke::AttrDomain::Face, mask_final.pmask, mesh);

source/blender/editors/object/object_multires_modifier.cc
@@ -273,7 +273,7 @@ static wmOperatorStatus multires_external_save_exec(bContext *C, wmOperator *op)
 
   CustomData_external_add(&mesh->corner_data, &mesh->id, CD_MDISPS, mesh->corners_num, filepath);
   CustomData_external_write(
-      &mesh->corner_data, &mesh->id, CD_MASK_MESH.lmask, mesh->corners_num, 0);
+      &mesh->corner_data, "", &mesh->id, CD_MASK_MESH.lmask, mesh->corners_num, 0);
 
   return OPERATOR_FINISHED;
 }

tests/files/sculpting/Cube.btx
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd970fd39cb92722f2924ae4af74042486f7db13de1c18e950017a10820b688e
+size 1264

tests/files/sculpting/external-multires.blend
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36f36d1a44883d9022e8cc301c54769040a0b1d40cecbb2092ad903f1049e224
+size 78118

tests/python/sculpt_paint/multires_operators_test.py
@@ -233,6 +233,21 @@ class Unsubdivide(unittest.TestCase):
         self.assertAlmostEqual(self._mesh_volume(ob_unsubdiv.data), ob_src_volume, places=4)
 
 
+class ExternalData(unittest.TestCase):
+    def setUp(self):
+        bpy.ops.wm.open_mainfile(filepath=str(args.testdir / "external-multires.blend"), load_ui=False)
+
+    def test_persists_in_bmesh_mesh_round_trip(self):
+        bpy.ops.object.editmode_toggle()
+        bpy.ops.object.editmode_toggle()
+
+        cube = bpy.context.active_object
+        multires_mod = cube.modifiers['Multires']
+
+        self.assertTrue(multires_mod.is_external)
+        self.assertEqual(multires_mod.filepath, "//Cube.btx")
+
+
 def main():
     global args
     import argparse