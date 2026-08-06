8cf5059963d69eebca8f4e092dc7040ce515e8d3
Starz099 <mayankjod016@gmail.com>
2026-08-06 13:26:32 +0200

    Fix #161887: Socket menu missing in N-panel for some nodes
    Fixes issue #161887 where the socket/data type selector dropdown menu was
    missing from the Sidebar (N-panel) for `Get List Item`, `Index Switch`,
    `Get Bundle Item`, and `Store Bundle Item` nodes.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/162177

--- source/blender/nodes/geometry/nodes/node_geo_index_switch.cc
+++ source/blender/nodes/geometry/nodes/node_geo_index_switch.cc
@@ -150,6 +150,7 @@ static void node_layout(ui::Layout &layout, bContext * /*C*/, PointerRNA *ptr)
 
 static void node_layout_ex(ui::Layout &layout, bContext *C, PointerRNA *ptr)
 {
+  layout.prop(ptr, "data_type", UI_ITEM_NONE, "", ICON_NONE);
   bNode &node = *static_cast<bNode *>(ptr->data);
   NodeIndexSwitch &storage = node_storage(node);
   if (ui::Layout *panel = layout.panel(C, "index_switch_items", false, IFACE_("Items"))) {
