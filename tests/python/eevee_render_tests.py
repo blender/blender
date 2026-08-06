7d641fe96810cdc2598b2f37ec4f6192e133e6d8
Christoph Neuhauser <christoph.neuhauser@intel.com>
2026-08-06 16:16:19 +0200

    Tests: EEVEE: Blocklist volume_instance test for Intel Windows
    Previously, the blocklist for volume_instance was only applied to the
    OpenGL backend for Intel Windows. While this issue was presumably fixed,
    the same issue can also be seen sporadically for the Vulkan backend.
    Until the issue has been investigated, it makes sense to blocklist both.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/162411

--- tests/python/eevee_render_tests.py
+++ tests/python/eevee_render_tests.py
@@ -110,7 +110,7 @@ BLOCKLIST_AMD_VK = [
     ".*"
 ]
 
-BLOCKLIST_INTEL_WINDOWS_GL = [
+BLOCKLIST_INTEL_WINDOWS = [
     # Fails sporadically and causes all subsequent volume tests to fail (See #153612).
     "volume_instance.blend"
 ]
@@ -348,8 +348,8 @@ def main():
     if os.getenv("BLENDER_TEST_IGNORE_VENDOR_BLOCKLIST") is None:
         if gpu_vendor == "INTEL":
             blocklist += BLOCKLIST_INTEL
-        if gpu_vendor == "INTEL" and sys.platform == "win32" and args.gpu_backend == "opengl":
-            blocklist += BLOCKLIST_INTEL_WINDOWS_GL
+        if gpu_vendor == "INTEL" and sys.platform == "win32":
+            blocklist += BLOCKLIST_INTEL_WINDOWS
         if gpu_vendor == "NVIDIA" and args.gpu_backend == "opengl":
             blocklist += BLOCKLIST_NVIDIA_GL
         if gpu_vendor == "AMD" and sys.platform == "win32" and args.gpu_backend == "vulkan":
