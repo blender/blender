blender/blender

commit 3b7e5cb19269e97601848b69b2544668aafdbb94

Bake: Conservative rasterization for texture bake
Blender can miss small, thin or long triangles during texture baking
as texel center sampling is used to determine which triangles overlap
texture pixels. So, if a triangle does not overlap texel center, it will
be empty.

The solution used here is conservative triangle rasterization, which
covers any texel that at least partially overlaps with a triangle.
Barycentric coordinates are clipped inside the triangle, and
rasterization is performed in 2 passes, conservative pass first and the
regular one later. This way it can both capture small geometry and get
correct non-overlapping results for regular geometry.

And since the interiors of triangles are overwritten in the regular
pass, we don't need full conservative triangle rasterization, a simple
conservative line rasterizer suffices. If a triangle edge does not
intersect a texel, the texture pixel should be either fully outside or
inside of the triangle.

Pull Request: https://projects.blender.org/blender/blender/pulls/161752

source/blender/render/intern/zbuf.cc
@@ -4,9 +4,6 @@
 
 /** \file
  * \ingroup render
- *
- * \note Some of this logic has been duplicated in `COM_VectorBlurOperation.cc`
- * changes here may also apply also apply to that file.
  */
 
 /*---------------------------------------------------------------------------*/
@@ -18,6 +15,7 @@
 
 #include "MEM_guardedalloc.h"
 
+#include "BLI_math_base.hh"
 #include "BLI_math_base_c.hh"
 
 /* own includes */
@@ -150,12 +148,12 @@ static void zbuf_add_to_span(ZSpan *zspan, const float v1[2], const float v2[2])
 /* Functions                                                 */
 /*-----------------------------------------------------------*/
 
-void zspan_scanconvert(ZSpan *zspan,
-                       void *handle,
-                       float *v1,
-                       float *v2,
-                       float *v3,
-                       void (*func)(void *, int, int, float, float))
+void zspan_rasterize_triangle(ZSpan *zspan,
+                              void *handle,
+                              const float *v1_in,
+                              const float *v2_in,
+                              const float *v3_in,
+                              void (*func)(void *, int, int, float, float))
 {
   float x0, y0, x1, y1, x2, y2, z0, z1, z2;
   float u, v, uxd, uyd, vxd, vyd, uy0, vy0, xx1;
@@ -164,6 +162,22 @@ void zspan_scanconvert(ZSpan *zspan,
 
   /* init */
   zbuf_init_span(zspan);
+
+  /* NOTE(@ideasman42): workaround for pixel aligned UVs which are common and can screw
+   * up our intersection tests where a pixel gets in between 2 faces or the middle of a
+   * quad, camera aligned quads also have this problem but they are less common. Add a
+   * small offset to the UVs, fixes bug #18685.
+   * This effectively shifts sampling point from top left corner to the texel center. */
+  float v1[2], v2[2], v3[2];
+
+  v1[0] = v1_in[0] - (0.5f + 0.001f);
+  v1[1] = v1_in[1] - (0.5f + 0.002f);
+
+  v2[0] = v2_in[0] - (0.5f + 0.001f);
+  v2[1] = v2_in[1] - (0.5f + 0.002f);
+
+  v3[0] = v3_in[0] - (0.5f + 0.001f);
+  v3[1] = v3_in[1] - (0.5f + 0.002f);
 
   /* set spans */
   zbuf_add_to_span(zspan, v1, v2);
@@ -238,6 +252,95 @@ void zspan_scanconvert(ZSpan *zspan,
       func(handle, x, y, u + (j * uxd), v + (j * vxd));
     }
   }
+}
+
+static void zspan_rasterize_conservative_line(ZSpan *zspan,
+                                              void *handle,
+                                              const float *v1,
+                                              const float *v2,
+                                              const float *uv1,
+                                              const float *uv2,
+                                              void (*func)(void *, int, int, float, float))
+{
+  const float miny = std::min(v1[1], v2[1]);
+  const float maxy = std::max(v1[1], v2[1]);
+
+  int y0 = ceil(miny) - 1;
+  int y1 = floor(maxy);
+
+  /* Clip and cull the line. */
+  y0 = std::max(y0, 0);
+  if (y1 >= zspan->recty) {
+    y1 = zspan->recty - 1;
+  }
+
+  if (y0 > y1) {
+    return;
+  }
+
+  /* Line direction divided by length for uv interpolation. */
+  float dir[2] = {v2[0] - v1[0], v2[1] - v1[1]};
+  float len_sqr = dir[0] * dir[0] + dir[1] * dir[1];
+
+  if (len_sqr > FLT_EPSILON) {
+    dir[0] /= len_sqr;
+    dir[1] /= len_sqr;
+  }
+
+  /* X coordinates of the line endpoints. */
+  const float x_miny = (v1[1] < v2[1]) ? v1[0] : v2[0];
+  const float x_maxy = (v1[1] < v2[1]) ? v2[0] : v1[0];
+  const float inv_dy = math::safe_divide(1.0f, maxy - miny);
+
+  for (; y0 <= y1; y0++) {
+    /* Compute line x range inside current scanline, interpolating between endpoints. */
+    const float ta = (clamp_f(y0, miny, maxy) - miny) * inv_dy;
+    const float tb = (maxy - clamp_f(y0 + 1, miny, maxy)) * inv_dy;
+    const float xa = math::interpolate(x_miny, x_maxy, ta);
+    const float xb = math::interpolate(x_miny, x_maxy, 1.0f - tb);
+
+    const float minx = std::min(xa, xb);
+    const float maxx = std::max(xa, xb);
+
+    int x0 = ceil(minx) - 1;
+    int x1 = floor(maxx);
+
+    /* Clip and cull scanline. */
+    x0 = std::max(x0, 0);
+    if (x1 >= zspan->rectx) {
+      x1 = zspan->rectx - 1;
+    }
+
+    if (x0 > x1) {
+      continue;
+    }
+
+    for (; x0 <= x1; x0++) {
+      float w = (x0 - v1[0] + 0.5f + 0.001f) * dir[0] + (y0 - v1[1] + 0.5f + 0.002f) * dir[1];
+      w = clamp_f(w, 0, 1);
+
+      float u = (1 - w) * uv1[0] + w * uv2[0];
+      float v = (1 - w) * uv1[1] + w * uv2[1];
+
+      func(handle, x0, y0, u, v);
+    }
+  }
+}
+
+void zspan_rasterize_conservative_wireframe(ZSpan *zspan,
+                                            void *handle,
+                                            const float *v1,
+                                            const float *v2,
+                                            const float *v3,
+                                            void (*func)(void *, int, int, float, float))
+{
+  float uv1[2] = {1, 0};
+  float uv2[2] = {0, 1};
+  float uv3[2] = {0, 0};
+
+  zspan_rasterize_conservative_line(zspan, handle, v1, v2, uv1, uv2, func);
+  zspan_rasterize_conservative_line(zspan, handle, v2, v3, uv2, uv3, func);
+  zspan_rasterize_conservative_line(zspan, handle, v3, v1, uv3, uv1, func);
 }
 
 }  // namespace blender

source/blender/render/RE_texture_margin.h
@@ -22,12 +22,14 @@ struct Mesh;
  * \param margin: the size of the margin in pixels.
  * \param me: the mesh to use the polygons of.
  * \param uv_layer: The UV layer to use.
+ * \param conservative: if conservative rasterization should be used.
  */
 void RE_generate_texturemargin_adjacentfaces(struct ImBuf *ibuf,
                                              char *mask,
                                              int margin,
                                              struct Mesh const *me,
                                              StringRef uv_layer,
-                                             const float uv_offset[2]);
+                                             const float uv_offset[2],
+                                             bool conservative);
 
 }  // namespace blender

source/blender/render/intern/bake.cc
@@ -151,7 +151,7 @@ void RE_bake_margin(ImBuf *ibuf,
   /* margin */
   switch (margin_type) {
     case R_BAKE_ADJACENT_FACES:
-      RE_generate_texturemargin_adjacentfaces(ibuf, mask, margin, mesh, uv_layer, uv_offset);
+      RE_generate_texturemargin_adjacentfaces(ibuf, mask, margin, mesh, uv_layer, uv_offset, true);
       break;
     default:
     /* fall through */
@@ -752,41 +752,54 @@ void RE_bake_pixels_populate(Mesh *mesh,
 
   const int materials_num = targets->materials_num;
 
-  for (int i = 0; i < tottri; i++) {
-    const int3 &tri = corner_tris[i];
-    const int face_i = tri_faces[i];
+  for (int fill = 0; fill < 2; fill++) {
+    for (int i = 0; i < tottri; i++) {
+      const int3 &tri = corner_tris[i];
+      const int face_i = tri_faces[i];
 
-    bd.primitive_id = i;
+      bd.primitive_id = i;
 
-    /* Find images matching this material. */
-    const int material_index = (!material_indices.is_empty() && materials_num) ?
-                                   clamp_i(material_indices[face_i], 0, materials_num - 1) :
-                                   0;
-    Image *image = targets->material_to_image[material_index];
-    for (int image_id = 0; image_id < targets->images_num; image_id++) {
-      BakeImage *bk_image = &targets->images[image_id];
-      if (bk_image->image != image) {
-        continue;
+      /* Find images matching this material. */
+      const int material_index = (!material_indices.is_empty() && materials_num) ?
+                                     clamp_i(material_indices[face_i], 0, materials_num - 1) :
+                                     0;
+      Image *image = targets->material_to_image[material_index];
+      for (int image_id = 0; image_id < targets->images_num; image_id++) {
+        BakeImage *bk_image = &targets->images[image_id];
+        if (bk_image->image != image) {
+          continue;
+        }
+
+        /* Compute triangle vertex UV coordinates. */
+        float vec[3][2];
+        for (int a = 0; a < 3; a++) {
+          const float2 &uv = uv_map[tri[a]];
+
+          vec[a][0] = (uv[0] - bk_image->uv_offset[0]) * float(bk_image->width);
+          vec[a][1] = (uv[1] - bk_image->uv_offset[1]) * float(bk_image->height);
+        }
+
+        /* Rasterize triangle. */
+        bd.bk_image = bk_image;
+        bake_differentials(&bd, vec[0], vec[1], vec[2]);
+
+        if (!fill) {
+          zspan_rasterize_conservative_wireframe(&bd.zspan[image_id],
+                                                 static_cast<void *>(&bd),
+                                                 vec[0],
+                                                 vec[1],
+                                                 vec[2],
+                                                 store_bake_pixel);
+        }
+        else {
+          zspan_rasterize_triangle(&bd.zspan[image_id],
+                                   static_cast<void *>(&bd),
+                                   vec[0],
+                                   vec[1],
+                                   vec[2],
+                                   store_bake_pixel);
+        }
       }
-
-      /* Compute triangle vertex UV coordinates. */
-      float vec[3][2];
-      for (int a = 0; a < 3; a++) {
-        const float2 &uv = uv_map[tri[a]];
-
-        /* NOTE(@ideasman42): workaround for pixel aligned UVs which are common and can screw
-         * up our intersection tests where a pixel gets in between 2 faces or the middle of a quad,
-         * camera aligned quads also have this problem but they are less common.
-         * Add a small offset to the UVs, fixes bug #18685. */
-        vec[a][0] = (uv[0] - bk_image->uv_offset[0]) * float(bk_image->width) - (0.5f + 0.001f);
-        vec[a][1] = (uv[1] - bk_image->uv_offset[1]) * float(bk_image->height) - (0.5f + 0.002f);
-      }
-
-      /* Rasterize triangle. */
-      bd.bk_image = bk_image;
-      bake_differentials(&bd, vec[0], vec[1], vec[2]);
-      zspan_scanconvert(
-          &bd.zspan[image_id], static_cast<void *>(&bd), vec[0], vec[1], vec[2], store_bake_pixel);
     }
   }
 

source/blender/render/intern/multires_bake.cc
@@ -1581,7 +1581,8 @@ static void bake_ibuf_filter(ImBuf &ibuf,
                                                 margin,
                                                 &bake_level_mesh,
                                                 bake_level_mesh.active_uv_map_name(),
-                                                uv_offset);
+                                                uv_offset,
+                                                false);
         break;
       }
       default:

source/blender/render/intern/texture_margin.cc
@@ -104,15 +104,23 @@ class TextureMarginMap {
     return pixel_data_[y * w_ + x];
   }
 
-  void rasterize_tri(float *v1, float *v2, float *v3, uint32_t value, char *mask, bool writemask)
+  void rasterize_tri(
+      float *v1, float *v2, float *v3, uint32_t value, char *mask, bool writemask, bool fill)
   {
     /* NOTE: This is not thread safe, because the value to be written by the rasterizer is
      * a class member. If this is ever made multi-threaded each thread needs to get its own. */
     value_to_store_ = value;
     mask_ = mask;
     write_mask_ = writemask;
-    zspan_scanconvert(
-        &zspan_, this, &(v1[0]), &(v2[0]), &(v3[0]), TextureMarginMap::zscan_store_pixel);
+
+    if (fill) {
+      zspan_rasterize_triangle(
+          &zspan_, this, &(v1[0]), &(v2[0]), &(v3[0]), TextureMarginMap::zscan_store_pixel);
+    }
+    else {
+      zspan_rasterize_conservative_wireframe(
+          &zspan_, this, &(v1[0]), &(v2[0]), &(v3[0]), TextureMarginMap::zscan_store_pixel);
+    }
   }
 
   static void zscan_store_pixel(
@@ -504,7 +512,8 @@ static void generate_margin(ImBuf *ibuf,
                             const Span<int> corner_edges,
                             const Span<int> corner_verts,
                             const Span<float2> uv_map,
-                            const float uv_offset[2])
+                            const float uv_offset[2],
+                            const bool conservative)
 {
   Array<int3> corner_tris(poly_to_tri_count(faces.size(), corner_edges.size()));
   bke::mesh::corner_tris_calc(vert_positions, faces, corner_verts, corner_tris);
@@ -525,25 +534,23 @@ static void generate_margin(ImBuf *ibuf,
     draw_new_mask = true;
   }
 
-  for (const int i : corner_tris.index_range()) {
-    const int3 tri = corner_tris[i];
-    float vec[3][2];
+  for (int fill = conservative ? 0 : 1; fill < 2; fill++) {
+    for (const int i : corner_tris.index_range()) {
+      const int3 tri = corner_tris[i];
+      float vec[3][2];
 
-    for (int a = 0; a < 3; a++) {
-      const float *uv = uv_map[tri[a]];
+      for (int a = 0; a < 3; a++) {
+        const float *uv = uv_map[tri[a]];
 
-      /* NOTE(@ideasman42): workaround for pixel aligned UVs which are common and can screw up
-       * our intersection tests where a pixel gets in between 2 faces or the middle of a quad,
-       * camera aligned quads also have this problem but they are less common.
-       * Add a small offset to the UVs, fixes bug #18685. */
-      vec[a][0] = (uv[0] - uv_offset[0]) * float(ibuf->x) - (0.5f + 0.001f);
-      vec[a][1] = (uv[1] - uv_offset[1]) * float(ibuf->y) - (0.5f + 0.002f);
+        vec[a][0] = (uv[0] - uv_offset[0]) * float(ibuf->x);
+        vec[a][1] = (uv[1] - uv_offset[1]) * float(ibuf->y);
+      }
+
+      /* NOTE: we need the top bit for the dijkstra distance map. */
+      BLI_assert(tri_faces[i] < 0x80000000);
+
+      map.rasterize_tri(vec[0], vec[1], vec[2], tri_faces[i], mask, draw_new_mask, fill);
     }
-
-    /* NOTE: we need the top bit for the dijkstra distance map. */
-    BLI_assert(tri_faces[i] < 0x80000000);
-
-    map.rasterize_tri(vec[0], vec[1], vec[2], tri_faces[i], mask, draw_new_mask);
   }
 
   char *tmpmask = MEM_dupalloc(mask);
@@ -574,7 +581,8 @@ void RE_generate_texturemargin_adjacentfaces(ImBuf *ibuf,
                                              const int margin,
                                              const Mesh *mesh,
                                              StringRef uv_layer,
-                                             const float uv_offset[2])
+                                             const float uv_offset[2],
+                                             const bool conservative)
 {
   const StringRef name = uv_layer.is_empty() ? mesh->active_uv_map_name() : uv_layer;
   const bke::AttributeAccessor attributes = mesh->attributes();
@@ -589,7 +597,8 @@ void RE_generate_texturemargin_adjacentfaces(ImBuf *ibuf,
                                          mesh->corner_edges(),
                                          mesh->corner_verts(),
                                          uv_map,
-                                         uv_offset);
+                                         uv_offset,
+                                         conservative);
 }
 
 }  // namespace blender

source/blender/render/intern/zbuf.h
@@ -27,13 +27,24 @@ void zbuf_free_span(struct ZSpan *zspan);
 
 /**
  * Scan-convert for strand triangles, calls function for each x, y coordinate
- * and gives UV barycentrics and z.
+ * and gives UV barycentrics.
  */
-void zspan_scanconvert(struct ZSpan *zspan,
-                       void *handle,
-                       float *v1,
-                       float *v2,
-                       float *v3,
-                       void (*func)(void *, int, int, float, float));
+void zspan_rasterize_triangle(struct ZSpan *zspan,
+                              void *handle,
+                              const float *v1,
+                              const float *v2,
+                              const float *v3,
+                              void (*func)(void *, int, int, float, float));
+
+/**
+ * Conservatively rasterize triangle wireframe, calls function for each x, y coordinate
+ * and gives UV barycentrics.
+ */
+void zspan_rasterize_conservative_wireframe(ZSpan *zspan,
+                                            void *handle,
+                                            const float *v1,
+                                            const float *v2,
+                                            const float *v3,
+                                            void (*func)(void *, int, int, float, float));
 
 }  // namespace blender

tests/files/render/bake/cycles_renders/bake_from_active_camera_flat.png
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f736e12cd733ba1d36c46a4cc552e43689bcd506f32ffdb8cedffdc64aeadcd
-size 126697
+oid sha256:27790fcf5ad504ab6b46ee3774cc089c8a4119dbdc7d3efff83124376e7ac02b
+size 127066

tests/files/render/bake/cycles_renders/bake_from_active_camera_smooth.png
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a969f932251cdc748594bf5b726a334c4ee8b488ec9c11225f3a30ec5f46b7a
-size 132916
+oid sha256:11dc82d6450b948f41381d203095b4b5bb2878a91400d4852f0fc989c2dc308a
+size 133330

commit 25fb138788d7404f23df29d741dee6794f4530f4

Fix: Cycles: Conservative bake rasterization inconsistent epsilons
Mismatch between regular triangle and conservative wireframe rasterization
conventions and epsilons.

This issue was introduced in 3b7e5cb1926.

Pull Request: https://projects.blender.org/blender/blender/pulls/162433

source/blender/render/intern/zbuf.cc
@@ -148,6 +148,15 @@ static void zbuf_add_to_span(ZSpan *zspan, const float v1[2], const float v2[2])
 /* Functions                                                 */
 /*-----------------------------------------------------------*/
 
+/* NOTE(@ideasman42): workaround for pixel aligned UVs which are common and can screw
+ * up our intersection tests where a pixel gets in between 2 faces or the middle of a
+ * quad, camera aligned quads also have this problem but they are less common. Add a
+ * small offset to the UVs, fixes bug #18685. */
+constexpr float ZSPAN_X_EPSILON = 0.001f;
+constexpr float ZSPAN_Y_EPSILON = 0.002f;
+constexpr float ZSPAN_X_OFFSET = 0.5f + ZSPAN_X_EPSILON;
+constexpr float ZSPAN_Y_OFFSET = 0.5f + ZSPAN_Y_EPSILON;
+
 void zspan_rasterize_triangle(ZSpan *zspan,
                               void *handle,
                               const float *v1_in,
@@ -163,21 +172,9 @@ void zspan_rasterize_triangle(ZSpan *zspan,
   /* init */
   zbuf_init_span(zspan);
 
-  /* NOTE(@ideasman42): workaround for pixel aligned UVs which are common and can screw
-   * up our intersection tests where a pixel gets in between 2 faces or the middle of a
-   * quad, camera aligned quads also have this problem but they are less common. Add a
-   * small offset to the UVs, fixes bug #18685.
-   * This effectively shifts sampling point from top left corner to the texel center. */
-  float v1[2], v2[2], v3[2];
-
-  v1[0] = v1_in[0] - (0.5f + 0.001f);
-  v1[1] = v1_in[1] - (0.5f + 0.002f);
-
-  v2[0] = v2_in[0] - (0.5f + 0.001f);
-  v2[1] = v2_in[1] - (0.5f + 0.002f);
-
-  v3[0] = v3_in[0] - (0.5f + 0.001f);
-  v3[1] = v3_in[1] - (0.5f + 0.002f);
+  const float v1[2] = {v1_in[0] - ZSPAN_X_OFFSET, v1_in[1] - ZSPAN_Y_OFFSET};
+  const float v2[2] = {v2_in[0] - ZSPAN_X_OFFSET, v2_in[1] - ZSPAN_Y_OFFSET};
+  const float v3[2] = {v3_in[0] - ZSPAN_X_OFFSET, v3_in[1] - ZSPAN_Y_OFFSET};
 
   /* set spans */
   zbuf_add_to_span(zspan, v1, v2);
@@ -256,17 +253,20 @@ void zspan_rasterize_triangle(ZSpan *zspan,
 
 static void zspan_rasterize_conservative_line(ZSpan *zspan,
                                               void *handle,
-                                              const float *v1,
-                                              const float *v2,
+                                              const float *v1_in,
+                                              const float *v2_in,
                                               const float *uv1,
                                               const float *uv2,
                                               void (*func)(void *, int, int, float, float))
 {
+  const float v1[2] = {v1_in[0] - ZSPAN_X_OFFSET, v1_in[1] - ZSPAN_Y_OFFSET};
+  const float v2[2] = {v2_in[0] - ZSPAN_X_OFFSET, v2_in[1] - ZSPAN_Y_OFFSET};
+
   const float miny = std::min(v1[1], v2[1]);
   const float maxy = std::max(v1[1], v2[1]);
 
-  int y0 = ceil(miny) - 1;
-  int y1 = floor(maxy);
+  int y0 = floor(miny + 0.5f);
+  int y1 = floor(maxy + 0.5f);
 
   /* Clip and cull the line. */
   y0 = std::max(y0, 0);
@@ -294,16 +294,16 @@ static void zspan_rasterize_conservative_line(ZSpan *zspan,
 
   for (; y0 <= y1; y0++) {
     /* Compute line x range inside current scanline, interpolating between endpoints. */
-    const float ta = (clamp_f(y0, miny, maxy) - miny) * inv_dy;
-    const float tb = (maxy - clamp_f(y0 + 1, miny, maxy)) * inv_dy;
+    const float ta = (clamp_f(y0 - 0.5f, miny, maxy) - miny) * inv_dy;
+    const float tb = (maxy - clamp_f(y0 + 0.5f, miny, maxy)) * inv_dy;
     const float xa = math::interpolate(x_miny, x_maxy, ta);
     const float xb = math::interpolate(x_miny, x_maxy, 1.0f - tb);
 
     const float minx = std::min(xa, xb);
     const float maxx = std::max(xa, xb);
 
-    int x0 = ceil(minx) - 1;
-    int x1 = floor(maxx);
+    int x0 = floor(minx + 0.5f);
+    int x1 = floor(maxx + 0.5f);
 
     /* Clip and cull scanline. */
     x0 = std::max(x0, 0);
@@ -316,7 +316,7 @@ static void zspan_rasterize_conservative_line(ZSpan *zspan,
     }
 
     for (; x0 <= x1; x0++) {
-      float w = (x0 - v1[0] + 0.5f + 0.001f) * dir[0] + (y0 - v1[1] + 0.5f + 0.002f) * dir[1];
+      float w = (x0 - v1[0]) * dir[0] + (y0 - v1[1]) * dir[1];
       w = clamp_f(w, 0, 1);
 
       float u = (1 - w) * uv1[0] + w * uv2[0];