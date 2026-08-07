blender/blender

commit f281bfa5367bdd18534cd7a5add9c6e99be4bb46

Cleanup: include functions names in Python argument parsing strings
Their inclusion means function names are shown in error messages.

Ref !161605

source/blender/freestyle/intern/python/Interface1D/BPy_FEdge.cpp
@@ -59,8 +59,9 @@ static int FEdge_init(BPy_FEdge *self, PyObject *args, PyObject *kwds)
 
   if (PyArg_ParseTupleAndKeywords(args,
                                   kwds,
-                                  "|" /* Optional arguments. */
-                                  "O!" /* `brother` */,
+                                  "|"  /* Optional arguments. */
+                                  "O!" /* `brother` */
+                                  ":FEdge",
                                   (char **)kwlist_1,
                                   &FEdge_Type,
                                   &obj1))
@@ -76,7 +77,8 @@ static int FEdge_init(BPy_FEdge *self, PyObject *args, PyObject *kwds)
            PyArg_ParseTupleAndKeywords(args,
                                        kwds,
                                        "O!" /* `first_vertex` */
-                                       "O!" /* `second_vertex` */,
+                                       "O!" /* `second_vertex` */
+                                       ":FEdge",
                                        (char **)kwlist_2,
                                        &SVertex_Type,
                                        &obj1,

source/blender/freestyle/intern/python/Interface1D/BPy_Stroke.cpp
@@ -54,8 +54,9 @@ static int Stroke_init(BPy_Stroke *self, PyObject *args, PyObject *kwds)
 
   if (!PyArg_ParseTupleAndKeywords(args,
                                    kwds,
-                                   "|" /* Optional arguments. */
-                                   "O!" /* `brother` */,
+                                   "|"  /* Optional arguments. */
+                                   "O!" /* `brother` */
+                                   ":Stroke",
                                    (char **)kwlist,
                                    &Stroke_Type,
                                    &brother))
@@ -117,7 +118,13 @@ static PyObject *Stroke_compute_sampling(BPy_Stroke *self, PyObject *args, PyObj
   static const char *kwlist[] = {"n", nullptr};
   int i;
 
-  if (!PyArg_ParseTupleAndKeywords(args, kwds, "i" /* `n` */, (char **)kwlist, &i)) {
+  if (!PyArg_ParseTupleAndKeywords(args,
+                                   kwds,
+                                   "i" /* `n` */
+                                   ":compute_sampling",
+                                   (char **)kwlist,
+                                   &i))
+  {
     return nullptr;
   }
   return PyFloat_FromDouble(self->s->ComputeSampling(i));
@@ -151,14 +158,25 @@ static PyObject *Stroke_resample(BPy_Stroke *self, PyObject *args, PyObject *kwd
   int i;
   float f;
 
-  if (PyArg_ParseTupleAndKeywords(args, kwds, "i" /* `n` */, (char **)kwlist_1, &i)) {
+  if (PyArg_ParseTupleAndKeywords(args,
+                                  kwds,
+                                  "i" /* `n` */
+                                  ":resample",
+                                  (char **)kwlist_1,
+                                  &i))
+  {
     if (self->s->Resample(i) < 0) {
       PyErr_SetString(PyExc_RuntimeError, "Stroke resampling (by vertex count) failed");
       return nullptr;
     }
   }
   else if ((void)PyErr_Clear(),
-           PyArg_ParseTupleAndKeywords(args, kwds, "f" /* `sampling` */, (char **)kwlist_2, &f))
+           PyArg_ParseTupleAndKeywords(args,
+                                       kwds,
+                                       "f" /* `sampling` */
+                                       ":resample",
+                                       (char **)kwlist_2,
+                                       &f))
   {
     if (self->s->Resample(f) < 0) {
       PyErr_SetString(PyExc_RuntimeError, "Stroke resampling (by vertex interval) failed");
@@ -194,7 +212,8 @@ static PyObject *Stroke_insert_vertex(BPy_Stroke *self, PyObject *args, PyObject
   if (!PyArg_ParseTupleAndKeywords(args,
                                    kwds,
                                    "O!" /* `vertex` */
-                                   "O!" /* `next` */,
+                                   "O!" /* `next` */
+                                   ":insert_vertex",
                                    (char **)kwlist,
                                    &StrokeVertex_Type,
                                    &py_sv,
@@ -228,8 +247,13 @@ static PyObject *Stroke_remove_vertex(BPy_Stroke *self, PyObject *args, PyObject
   static const char *kwlist[] = {"vertex", nullptr};
   PyObject *py_sv = nullptr;
 
-  if (!PyArg_ParseTupleAndKeywords(
-          args, kwds, "O!" /* `vertex` */, (char **)kwlist, &StrokeVertex_Type, &py_sv))
+  if (!PyArg_ParseTupleAndKeywords(args,
+                                   kwds,
+                                   "O!" /* `vertex` */
+                                   ":remove_vertex",
+                                   (char **)kwlist,
+                                   &StrokeVertex_Type,
+                                   &py_sv))
   {
     return nullptr;
   }
@@ -289,7 +313,8 @@ static PyObject *Stroke_stroke_vertices_begin(BPy_Stroke *self, PyObject *args, 
   if (!PyArg_ParseTupleAndKeywords(args,
                                    kwds,
                                    "|" /* Optional arguments. */
-                                   "f" /* `t` */,
+                                   "f" /* `t` */
+                                   ":stroke_vertices_begin",
                                    (char **)kwlist,
                                    &f))
   {

commit 14c32ec3f62e8286786cef9f6613b62b2643d3a5

Fix: Freestyle: Inconsistent FEdge/Stroke constructor error message
This issue was introduced in f281bfa5367, match similar classes now.

Pull Request: https://projects.blender.org/blender/blender/pulls/162463

source/blender/freestyle/intern/python/Interface1D/BPy_FEdge.cpp
@@ -61,7 +61,7 @@ static int FEdge_init(BPy_FEdge *self, PyObject *args, PyObject *kwds)
                                   kwds,
                                   "|"  /* Optional arguments. */
                                   "O!" /* `brother` */
-                                  ":FEdge",
+                                  ":__init__",
                                   (char **)kwlist_1,
                                   &FEdge_Type,
                                   &obj1))
@@ -78,7 +78,7 @@ static int FEdge_init(BPy_FEdge *self, PyObject *args, PyObject *kwds)
                                        kwds,
                                        "O!" /* `first_vertex` */
                                        "O!" /* `second_vertex` */
-                                       ":FEdge",
+                                       ":__init__",
                                        (char **)kwlist_2,
                                        &SVertex_Type,
                                        &obj1,

source/blender/freestyle/intern/python/Interface1D/BPy_Stroke.cpp
@@ -56,7 +56,7 @@ static int Stroke_init(BPy_Stroke *self, PyObject *args, PyObject *kwds)
                                    kwds,
                                    "|"  /* Optional arguments. */
                                    "O!" /* `brother` */
-                                   ":Stroke",
+                                   ":__init__",
                                    (char **)kwlist,
                                    &Stroke_Type,
                                    &brother))