blender/blender

commit 2287deeb8095a54937cb6b90ee70d9dba45aebd2

Refactor: Remove Sculpt mode default setting ensure
Previously, certain `Sculpt` data members were checked for invalid
values every time Sculpt mode was entered. This moves the initialization
to versioning code and removes the incorrect usage of this method in
Vertex Paint and Weight Paint.

Pull Request: https://projects.blender.org/blender/blender/pulls/162302

source/blender/blenloader/intern/versioning_530.cc
@@ -199,6 +199,40 @@ void blo_do_versions_530(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
       }
     }
   }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 503, 10)) {
+    /* This code formerly ran whenever entering Sculpt Mode, it is unlikely that there are any
+     * files that have these settings with invalid values, but since these values have never been
+     * set in versioning, perform this
+     */
+    for (Scene &scene : bmain->scenes) {
+      Sculpt *sd = scene.toolsettings->sculpt;
+      if (sd == nullptr) {
+        continue;
+      }
+
+      const Sculpt defaults = {};
+      if (sd->detail_percent == 0.0f) {
+        sd->detail_percent = defaults.detail_percent;
+      }
+      if (sd->constant_detail == 0.0f) {
+        sd->constant_detail = defaults.constant_detail;
+      }
+      if (sd->detail_size == 0.0f) {
+        sd->detail_size = defaults.detail_size;
+      }
+
+      if (!sd->paint.tile_offset[0]) {
+        sd->paint.tile_offset[0] = 1.0f;
+      }
+      if (!sd->paint.tile_offset[1]) {
+        sd->paint.tile_offset[1] = 1.0f;
+      }
+      if (!sd->paint.tile_offset[2]) {
+        sd->paint.tile_offset[2] = 1.0f;
+      }
+    }
+  }
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.

source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@ namespace blender {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 10
+#define BLENDER_FILE_SUBVERSION 11
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/BKE_paint.hh
@@ -612,7 +612,6 @@ void BKE_sculpt_mask_layers_ensure(Depsgraph *depsgraph,
                                    Main *bmain,
                                    Object *ob,
                                    MultiresModifierData *mmd);
-void BKE_sculpt_toolsettings_data_ensure(Main *bmain, Scene *scene);
 
 void BKE_sculpt_sync_face_visibility_to_grids(const Mesh &mesh, SubdivCCG &subdiv_ccg);
 

source/blender/blenkernel/intern/paint.cc
@@ -2588,41 +2588,6 @@ void BKE_sculpt_mask_layers_ensure(Depsgraph *depsgraph,
   }
 }
 
-void BKE_sculpt_toolsettings_data_ensure(Main *bmain, Scene *scene)
-{
-  BKE_paint_init(bmain, scene, PaintMode::Sculpt, true);
-
-  Sculpt *sd = scene->toolsettings->sculpt;
-
-  const Sculpt defaults = {};
-
-  /* We have file versioning code here for historical
-   * reasons.  Don't add more checks here, do it properly
-   * in blenloader.
-   */
-
-  if (sd->detail_percent == 0.0f) {
-    sd->detail_percent = defaults.detail_percent;
-  }
-  if (sd->constant_detail == 0.0f) {
-    sd->constant_detail = defaults.constant_detail;
-  }
-  if (sd->detail_size == 0.0f) {
-    sd->detail_size = defaults.detail_size;
-  }
-
-  /* Set sane default tiling offsets. */
-  if (!sd->paint.tile_offset[0]) {
-    sd->paint.tile_offset[0] = 1.0f;
-  }
-  if (!sd->paint.tile_offset[1]) {
-    sd->paint.tile_offset[1] = 1.0f;
-  }
-  if (!sd->paint.tile_offset[2]) {
-    sd->paint.tile_offset[2] = 1.0f;
-  }
-}
-
 static bool check_sculpt_object_deformed(Object *object, const bool for_construction)
 {
   bool deformed = false;

source/blender/editors/sculpt_paint/mesh/paint_vertex.cc
@@ -250,16 +250,8 @@ IndexMask pbvh_gather_generic(const Depsgraph &depsgraph,
 }
 
 /** Toggle operator for turning vertex paint mode on or off (copied from `sculpt.cc`) */
-static void init_session(Main &bmain,
-                         Depsgraph &depsgraph,
-                         Scene &scene,
-                         Paint &paint,
-                         Object &ob,
-                         eObjectMode object_mode)
+static void init_session(Depsgraph &depsgraph, Paint &paint, Object &ob, eObjectMode object_mode)
 {
-  /* Create persistent sculpt mode data */
-  BKE_sculpt_toolsettings_data_ensure(&bmain, &scene);
-
   BLI_assert(ob.runtime->sculpt_session == nullptr);
   ob.runtime->sculpt_session = MEM_new<SculptSession>(__func__);
   ob.runtime->sculpt_session->mode_type = object_mode;
@@ -313,7 +305,7 @@ void mode_enter_generic(
   }
 
   BLI_assert(paint != nullptr);
-  init_session(bmain, depsgraph, scene, *paint, ob, mode_flag);
+  init_session(depsgraph, *paint, ob, mode_flag);
 
   /* Flush object mode. */
   DEG_id_tag_update(&ob.id, ID_RECALC_SYNC_TO_EVAL);

source/blender/editors/sculpt_paint/mesh/sculpt_ops.cc
@@ -321,8 +321,7 @@ static void SCULPT_OT_symmetrize(wmOperatorType *ot)
 
 static void init_sculpt_mode_session(Main &bmain, Depsgraph &depsgraph, Scene &scene, Object &ob)
 {
-  /* Create persistent sculpt mode data. */
-  BKE_sculpt_toolsettings_data_ensure(&bmain, &scene);
+  BKE_paint_init(&bmain, &scene, PaintMode::Sculpt, true);
 
   /* Create sculpt mode session data. */
   if (ob.runtime->sculpt_session != nullptr) {

commit 3504cb962e65d837263d20c669a793befd5a1365

Fix: Incorrect versioning number for recent change
Introduced with 2287deeb8095a54937cb6b90ee70d9dba45aebd2

Pull Request: https://projects.blender.org/blender/blender/pulls/162479

source/blender/blenloader/intern/versioning_530.cc
@@ -200,7 +200,7 @@ void blo_do_versions_530(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     }
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 503, 10)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 503, 11)) {
     /* This code formerly ran whenever entering Sculpt Mode, it is unlikely that there are any
      * files that have these settings with invalid values, but since these values have never been
      * set in versioning, perform this