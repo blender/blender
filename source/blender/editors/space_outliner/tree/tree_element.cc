blender/blender

commit 05f84919acb4150cd4cc852f6c001e16d14e42ed

Refactor: Outliner: Port TSE_ID_BASE to AbstractTreeElement
Derive class from `AbstractTreeElement` for TSE_ID_BASE. All other types
have been ported, just ID base was left. Pointer references to
`directdata` has been removed, they're not used anywhere later.

Part of #96713

Pull Request: https://projects.blender.org/blender/blender/pulls/162394

source/blender/editors/space_outliner/tree/tree_element.cc
@@ -29,6 +29,7 @@
 #include "tree_element_gpencil_layer.hh"
 #include "tree_element_grease_pencil_node.hh"
 #include "tree_element_id.hh"
+#include "tree_element_id_base.hh"
 #include "tree_element_label.hh"
 #include "tree_element_layer_collection.hh"
 #include "tree_element_linked_node_tree.hh"
@@ -211,6 +212,8 @@ std::unique_ptr<AbstractTreeElement> AbstractTreeElement::create_from_type(const
     case TSE_SHAPE_KEY_BASE:
       return std::make_unique<TreeElementShapeKeyBase>(legacy_te,
                                                        *reinterpret_cast<Key *>(owner_id));
+    case TSE_ID_BASE:
+      return std::make_unique<TreeElementIDBase>(legacy_te);
     default:
       break;
   }

source/blender/editors/space_outliner/CMakeLists.txt
@@ -53,6 +53,7 @@ set(SRC
   tree/tree_element_id.cc
   tree/tree_element_id_action.cc
   tree/tree_element_id_armature.cc
+  tree/tree_element_id_base.cc
   tree/tree_element_id_collection.cc
   tree/tree_element_id_curve.cc
   tree/tree_element_id_gpencil_legacy.cc
@@ -100,6 +101,7 @@ set(SRC
   tree/tree_element_id.hh
   tree/tree_element_id_action.hh
   tree/tree_element_id_armature.hh
+  tree/tree_element_id_base.hh
   tree/tree_element_id_collection.hh
   tree/tree_element_id_curve.hh
   tree/tree_element_id_gpencil_legacy.hh

source/blender/editors/space_outliner/outliner_tree.cc
@@ -386,8 +386,7 @@ TreeElement *AbstractTreeDisplay::add_element(ListBaseT<TreeElement> *lb,
   else if (te->abstract_element) {
     tree_element_expand(*te->abstract_element, space_outliner_);
   }
-  /* Only #TSE_ID_BASE isn't ported to use the abstract elements design yet. */
-  else if (!ELEM(type, TSE_ID_BASE)) {
+  else {
     BLI_assert_msg(false, "Element type should use `AbstractTreeElement`");
   }
 

source/blender/editors/space_outliner/tree/tree_display_libraries.cc
@@ -150,7 +150,6 @@ TreeElement *TreeDisplayLibraries::add_library_contents(Main &mainvar,
         else if (id->lib == lib) {
           ten = add_element(
               &tenlib->subtree, reinterpret_cast<ID *>(lib), nullptr, nullptr, TSE_ID_BASE, a);
-          ten->directdata = lbarray[a];
           ten->name = outliner_idcode_to_plural(GS(id->name));
         }
 

source/blender/editors/space_outliner/tree/tree_display_orphaned.cc
@@ -56,7 +56,6 @@ ListBaseT<TreeElement> TreeDisplayIDOrphans::build_tree(const TreeSourceData &so
     if (!filter_id_type) {
       ID *id = static_cast<ID *>(lbarray[a]->first);
       te = add_element(&tree, nullptr, lbarray[a], nullptr, TSE_ID_BASE, 0);
-      te->directdata = lbarray[a];
       te->name = outliner_idcode_to_plural(GS(id->name));
     }
 

source/blender/editors/space_outliner/tree/tree_display_override_library_properties.cc
@@ -82,7 +82,6 @@ ListBaseT<TreeElement> TreeDisplayOverrideLibraryProperties::add_library_content
 
     if (!filter_id_type) {
       id_base_te = add_element(&tree, nullptr, lbarray[a], nullptr, TSE_ID_BASE, 0);
-      id_base_te->directdata = lbarray[a];
       id_base_te->name = outliner_idcode_to_plural(GS(id->name));
 
       lb_to_expand = &id_base_te->subtree;

source/blender/editors/space_outliner/tree/tree_element_id_base.cc
@@ -1,0 +1,20 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup spoutliner
+ */
+
+#include "tree_element_id_base.hh"
+#include "../outliner_intern.hh"
+#include "DNA_outliner_types.h"
+
+namespace blender::ed::outliner {
+
+TreeElementIDBase::TreeElementIDBase(TreeElement &legacy_te) : AbstractTreeElement(legacy_te)
+{
+  BLI_assert(legacy_te.store_elem->type == TSE_ID_BASE);
+  legacy_te.name = "";
+}
+}  // namespace blender::ed::outliner

source/blender/editors/space_outliner/tree/tree_element_id_base.hh
@@ -1,0 +1,23 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup spoutliner
+ */
+
+#pragma once
+
+#include "tree_element.hh"
+
+namespace blender {
+
+namespace ed::outliner {
+
+struct TreeElementIDBase final : public AbstractTreeElement {
+
+ public:
+  TreeElementIDBase(TreeElement &legacy_te);
+};
+}  // namespace ed::outliner
+}  // namespace blender

commit a5fe70e19f195a9100386d6d799d116f9167b91a

Fix #162724: Assert when selecting the Scripting Workspace
Caused by 05f84919acb4150cd4cc852f6c001e16d14e42ed
`AbstractElement` isn't created for TSE_ID_BASE because null arguments
are passed during `add_element()` call. This triggers the assert later.
To fix this add extra condition that will create AbstractElement for
this type.

Actually it's safe to remove the assert but let's keep it around for
some time

Pull Request: https://projects.blender.org/blender/blender/pulls/162731

source/blender/editors/space_outliner/tree/tree_element.cc
@@ -56,7 +56,7 @@ std::unique_ptr<AbstractTreeElement> AbstractTreeElement::create_from_type(const
                                                                            ID *owner_id,
                                                                            void *create_data)
 {
-  if (owner_id == nullptr && create_data == nullptr) {
+  if (owner_id == nullptr && create_data == nullptr && type != TSE_ID_BASE) {
     return nullptr;
   }
 