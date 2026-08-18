blender/blender

commit 6b50353665ea2859c333108080a467ea29ea9293

GPU: Mipmaps: Generate sRGB texture through a non-sRGB view on Vulkan
Compared to Metal and OpenGL, this is much more complicated. We need
to carefully set the appropriate bits on the texture and its views,
and specify the formats we are going to use up front.

This speeds up mipmap generation 2.1x-3.7x on various GPUs, and
is required for partial updates that will be implemented later.

Pull Request: https://projects.blender.org/blender/blender/pulls/161392

source/blender/gpu/vulkan/vk_common.cc
@@ -838,6 +838,28 @@ VkDescriptorType to_vk_descriptor_type(const shader::ShaderCreateInfo::Resource 
   return VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 }
 
+bool vk_need_extended_usage_for_storage_image(const eGPUTextureUsage usage,
+                                              const GPUTextureFormatFlag format_flag)
+{
+  /* sRGB textures are not directly writable as storage images. These need EXTENDED_USAGE to bind
+   * them as another format. */
+  return (usage & GPU_TEXTURE_USAGE_FORMAT_VIEW) && (format_flag & GPU_FORMAT_SRGB);
+}
+
+VkFormat vk_extended_usage_storage_image_format(const VkFormat format)
+{
+  /* sRGB textures are bound as UNORM 8 for storage images. */
+  switch (format) {
+    case VK_FORMAT_R8G8B8_SRGB:
+      return VK_FORMAT_R8G8B8_UNORM;
+    case VK_FORMAT_R8G8B8A8_SRGB:
+      return VK_FORMAT_R8G8B8A8_UNORM;
+    default:
+      break;
+  }
+  return format;
+}
+
 VkImageCreateFlags to_vk_image_create(const GPUTextureType texture_type,
                                       const GPUTextureFormatFlag format_flag,
                                       eGPUTextureUsage usage)
@@ -855,6 +877,13 @@ VkImageCreateFlags to_vk_image_create(const GPUTextureType texture_type,
 
   if (usage & GPU_TEXTURE_USAGE_FORMAT_VIEW) {
     result |= VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
+  }
+
+  /* The image's own format does not support storage usage, so it is bound as a storage image
+   * through a view with a different format. EXTENDED_USAGE lets the image keep the storage usage,
+   * by deferring the check to the view. The mutable format lets the view reinterpret the data. */
+  if (vk_need_extended_usage_for_storage_image(usage, format_flag)) {
+    result |= VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT | VK_IMAGE_CREATE_EXTENDED_USAGE_BIT;
   }
 
   return result;
@@ -898,8 +927,11 @@ VkImageUsageFlags to_vk_image_usage(const eGPUTextureUsage usage,
   }
 
   /* Disable some usages based on the given format flag to support more devices. */
-  if (format_flag & GPU_FORMAT_SRGB) {
-    /* NVIDIA devices don't create SRGB textures when it storage bit is set. */
+  if (format_flag & GPU_FORMAT_SRGB &&
+      !vk_need_extended_usage_for_storage_image(usage, format_flag))
+  {
+    /* NVIDIA devices don't create SRGB textures when the storage bit is set and
+     * EXTENDED_USAGE is not enabled. */
     result &= ~VK_IMAGE_USAGE_STORAGE_BIT;
   }
   if (format_flag & (GPU_FORMAT_DEPTH | GPU_FORMAT_STENCIL)) {

source/blender/gpu/vulkan/vk_backend.cc
@@ -876,6 +876,8 @@ void VKBackend::capabilities_init(VKDevice &device)
       device.physical_device_acceleration_structure_properties_get().maxPrimitiveCount > 0 &&
       device.physical_device_acceleration_structure_properties_get().maxInstanceCount > 0;
 
+  GCaps.srgb_write_view_support = true;
+
   GCaps.max_texture_size = max_ii(limits.maxImageDimension1D, limits.maxImageDimension2D);
   GCaps.max_texture_3d_size = min_uu(limits.maxImageDimension3D, INT_MAX);
   GCaps.max_buffer_texture_size = min_uu(limits.maxTexelBufferElements, UINT_MAX);

source/blender/gpu/vulkan/vk_common.hh
@@ -122,6 +122,13 @@ VkImageUsageFlags to_vk_image_usage(const eGPUTextureUsage usage,
                                     const GPUTextureFormatFlag format_flag,
                                     bool use_image_host_copy);
 
+/** Test if these settings need EXTENDED_USAGE to be able to bind the
+ * texture as a (writable) storage image. */
+bool vk_need_extended_usage_for_storage_image(const eGPUTextureUsage usage,
+                                              const GPUTextureFormatFlag format_flag);
+/** Other format to use for binding a storage image when the format itself is not supported. */
+VkFormat vk_extended_usage_storage_image_format(const VkFormat format);
+
 template<typename T> VkObjectType to_vk_object_type(T /*vk_obj*/)
 {
   const std::type_info &tid = typeid(T);

source/blender/gpu/vulkan/vk_descriptor_set.cc
@@ -331,12 +331,14 @@ void VKDescriptorSetUpdator::bind_image_resource(const VKStateManager &state_man
                                                  const VKResourceBinding &resource_binding)
 {
   VKTexture &texture = *state_manager.images_.get(resource_binding.binding);
-  bind_image(
-      VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
-      VK_NULL_HANDLE,
-      texture.image_view_get(resource_binding.arrayed, VKImageViewFlags::NO_SWIZZLING).vk_handle(),
-      VK_IMAGE_LAYOUT_GENERAL,
-      resource_binding.location);
+  const VKImageView &view = texture.image_view_get(resource_binding.arrayed,
+                                                   VKImageViewFlags::NO_SWIZZLING |
+                                                       VKImageViewFlags::FOR_STORAGE_IMAGE);
+  bind_image(VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
+             VK_NULL_HANDLE,
+             view.vk_handle(),
+             VK_IMAGE_LAYOUT_GENERAL,
+             resource_binding.location);
 }
 
 void VKDescriptorSetUpdator::bind_texture_resource(const VKDevice &device,

source/blender/gpu/vulkan/vk_image_view.cc
@@ -8,6 +8,7 @@
 
 #include "vk_image_view.hh"
 #include "vk_backend.hh"
+#include "vk_common.hh"
 #include "vk_debug.hh"
 #include "vk_device.hh"
 #include "vk_texture.hh"
@@ -31,6 +32,18 @@ VKImageView::VKImageView(VKTexture &texture, const VKImageViewInfo &info, String
   image_view_info.subresourceRange.levelCount = info.mip_range.size();
   image_view_info.subresourceRange.baseArrayLayer = info.layer_range.first();
   image_view_info.subresourceRange.layerCount = info.layer_range.size();
+
+  /* When extended usage is enabled for storage images, it means the original format
+   * is not supported. So we must strip USAGE_STORAGE for that original format, and
+   * only leave it for the other format that will be used for storage images. */
+  VkImageViewUsageCreateInfo view_usage_info = {VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO};
+  if (vk_need_extended_usage_for_storage_image(texture.usage_get(), texture.format_flag_get()) &&
+      info.vk_format == to_vk_format(texture.device_format_get()))
+  {
+    view_usage_info.usage = texture.vk_image_usage_get() & ~VK_IMAGE_USAGE_STORAGE_BIT;
+    view_usage_info.pNext = image_view_info.pNext;
+    image_view_info.pNext = &view_usage_info;
+  }
 
   const VKDevice &device = VKBackend::get().device;
   device.functions.vkCreateImageView(

source/blender/gpu/vulkan/vk_texture.cc
@@ -710,7 +710,20 @@ bool VKTexture::allocate()
   image_info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
   image_info.usage = to_vk_image_usage(
       gpu_image_usage_flags_, format_flag_, allow_host_image_copy_);
+  vk_image_usage_ = image_info.usage;
   image_info.samples = VK_SAMPLE_COUNT_1_BIT;
+
+  /* When this texture will be used with multiple formats, we need to provide them up front. */
+  VkFormat view_formats[2];
+  VkImageFormatListCreateInfo format_list_info = {VK_STRUCTURE_TYPE_IMAGE_FORMAT_LIST_CREATE_INFO};
+  if (vk_need_extended_usage_for_storage_image(texture_usage, format_flag_)) {
+    view_formats[0] = image_info.format;
+    view_formats[1] = vk_extended_usage_storage_image_format(image_info.format);
+    format_list_info.viewFormatCount = 2;
+    format_list_info.pViewFormats = view_formats;
+    format_list_info.pNext = image_info.pNext;
+    image_info.pNext = &format_list_info;
+  }
 
   VkResult result;
   if (G.debug & G_DEBUG_GPU) {
@@ -736,6 +749,7 @@ bool VKTexture::allocate()
   allocCreateInfo.priority = memory_priority(texture_usage);
 
   if (bool(texture_usage & GPU_TEXTURE_USAGE_MEMORY_EXPORT)) {
+    external_memory_create_info.pNext = image_info.pNext;
     image_info.pNext = &external_memory_create_info;
     external_memory_create_info.handleTypes = vk_external_memory_handle_type();
     allocCreateInfo.pool = device.vma_pools.external_memory_image.pool;
@@ -803,6 +817,11 @@ const VKImageView &VKTexture::image_view_get(VKImageViewArrayed arrayed, VKImage
       format_ != static_cast<VKTexture *>(source_texture_)->format_)
   {
     vk_format = to_vk_format(format_);
+  }
+
+  /* For binding as a storage image, we may need to switch to another format. */
+  if (flag_is_set(flags, VKImageViewFlags::FOR_STORAGE_IMAGE)) {
+    vk_format = vk_extended_usage_storage_image_format(vk_format);
   }
 
   VKImageViewInfo image_view_info = {eImageViewUsage::ShaderBinding,

source/blender/gpu/vulkan/vk_texture.hh
@@ -27,6 +27,7 @@ class VKPixelBuffer;
 enum class VKImageViewFlags {
   DEFAULT = 0,
   NO_SWIZZLING = 1 << 0,
+  FOR_STORAGE_IMAGE = 1 << 1,
 };
 ENUM_OPERATORS(VKImageViewFlags)
 
@@ -53,6 +54,7 @@ class VKTexture : public Texture {
    */
   VKVertexBuffer *source_buffer_ = nullptr;
   VkImage vk_image_ = VK_NULL_HANDLE;
+  VkImageUsageFlags vk_image_usage_ = 0;
   VmaAllocation allocation_ = VK_NULL_HANDLE;
   VmaAllocationInfo allocation_info_ = {};
 
@@ -134,6 +136,13 @@ class VKTexture : public Texture {
   TextureFormat device_format_get() const
   {
     return device_format_;
+  }
+
+  /** Vulkan usage flags the underlying image was created with. */
+  VkImageUsageFlags vk_image_usage_get() const
+  {
+    return is_texture_view() ? static_cast<VKTexture *>(source_texture_)->vk_image_usage_get() :
+                               vk_image_usage_;
   }
 
   /**

commit 284919861c3e041842bc341bd4eda5d9d66e06d4

Fix: GPU: Mixups with compressed sRGB textures in recent changes
GPU_FORMAT_SRGB can be combined with GPU_FORMAT_COMPRESSED, and in that
case we can not do shader writes.

And for the image editor info display, take into account that a
compressed image can become uncompressed after painting or other image
edits.

Issues introduced in 6b50353665e, 09be7335352, 794269176ae.

Pull Request: https://projects.blender.org/blender/blender/pulls/162769

source/blender/editors/space_image/image_buttons.cc
@@ -1245,8 +1245,11 @@ void uiTemplateImageInfo(ui::Layout *layout, bContext *C, Image *ima, ImageUser 
 
     gpu::TextureFormat texture_format = gpu::TextureFormat::Invalid;
 
-    /* Try to see if this texture is a compressed format, if not, get the generic format. */
-    if (!IMB_gpu_get_compressed_format(ibuf, &texture_format)) {
+    /* Try to see if this texture is a compressed format, if not, get the generic format.
+     * A modified image is uploaded uncompressed so it can be written to. */
+    const bool is_compressed = (ibuf->userflags & IB_BITMAPDIRTY) == 0 &&
+                               IMB_gpu_get_compressed_format(ibuf, &texture_format);
+    if (!is_compressed) {
       texture_format = IMB_gpu_get_texture_format(
           ibuf, ima->flag & IMA_HIGH_BITDEPTH, ibuf->color_mode == ImColorMode::BW);
     }

source/blender/gpu/intern/gpu_texture_mipmap.cc
@@ -348,16 +348,15 @@ static void texture_update_mipmap_chain(Texture *tex,
   if (use_compute_shaders) {
     const TextureFormat texture_format = tex->format_get();
     const bool is_layered = tex->type_get() & GPU_TEXTURE_ARRAY;
+    const bool is_srgb = texture_format == TextureFormat::SRGBA_8_8_8_8;
     /* For sRGB without direct write support, write with a UNORM_8_8_8_8 format,
      * either in a temporary texture or through a texture view. */
-    const TextureFormat write_format = ((tex->format_flag_get() & GPU_FORMAT_SRGB) &&
-                                        !GCaps.srgb_write_direct_support) ?
+    const TextureFormat write_format = (is_srgb && !GCaps.srgb_write_direct_support) ?
                                            TextureFormat::UNORM_8_8_8_8 :
                                            texture_format;
     /* For sRGB with direct write support, the shader is that same as UNORM_8_8_8_8 and
      * any conversion to/from sRGB happens automatically. */
-    const TextureFormat shader_format = ((tex->format_flag_get() & GPU_FORMAT_SRGB) &&
-                                         GCaps.srgb_write_direct_support) ?
+    const TextureFormat shader_format = (is_srgb && GCaps.srgb_write_direct_support) ?
                                             TextureFormat::UNORM_8_8_8_8 :
                                             texture_format;
     Shader *shader = get_update_mipmap_shader(shader_format, is_layered);

source/blender/gpu/vulkan/vk_common.cc
@@ -842,8 +842,9 @@ bool vk_need_extended_usage_for_storage_image(const eGPUTextureUsage usage,
                                               const GPUTextureFormatFlag format_flag)
 {
   /* sRGB textures are not directly writable as storage images. These need EXTENDED_USAGE to bind
-   * them as another format. */
-  return (usage & GPU_TEXTURE_USAGE_FORMAT_VIEW) && (format_flag & GPU_FORMAT_SRGB);
+   * them as another format. Compressed formats have no storage capable format to bind as. */
+  return (usage & GPU_TEXTURE_USAGE_FORMAT_VIEW) && (format_flag & GPU_FORMAT_SRGB) &&
+         !(format_flag & GPU_FORMAT_COMPRESSED);
 }
 
 VkFormat vk_extended_usage_storage_image_format(const VkFormat format)