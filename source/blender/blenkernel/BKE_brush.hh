blender/blender

commit 6ecfe4e810ee635d9826a326ffe44b218e99bd7b

Paint: Migrate unified paint flag from scene / mode level to brush level
This PR moves the controls of "Use Unified xxx" (such as Use Unified
Size) from the scene / mode level to the brush level, meaning each brush
can have this option toggled on and off separately. It used to be that
every brush shares the same toggle state.

I moved the `UNIFIED_xxx` flags from `UnifiedPaintSettings` to `Brush`.
Ialso added relevant RNA properties and hooked them up to Python
scripts.

Resolves #138469

Pull Request: https://projects.blender.org/blender/blender/pulls/159382

source/blender/blenkernel/intern/brush.cc
@@ -1338,8 +1338,9 @@ bool BKE_brush_use_locked_size(const Paint *paint, const Brush *brush)
 {
   const short us_flag = paint->unified_paint_settings.flag;
 
-  return (us_flag & UNIFIED_PAINT_SIZE) ? (us_flag & UNIFIED_PAINT_BRUSH_LOCK_SIZE) != 0 :
-                                          (brush->flag & BRUSH_LOCK_SIZE) != 0;
+  return (brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_SIZE) ?
+             (us_flag & UNIFIED_PAINT_BRUSH_LOCK_SIZE) != 0 :
+             (brush->flag & BRUSH_LOCK_SIZE) != 0;
 }
 
 bool BKE_brush_use_size_pressure(const Brush *brush)
@@ -1430,14 +1431,15 @@ float BKE_brush_weight_get(const Paint *paint, const Brush *brush)
 {
   const UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  return (ups->flag & UNIFIED_PAINT_WEIGHT) ? ups->weight : brush->weight;
+  return (brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_WEIGHT) ? ups->weight :
+                                                                         brush->weight;
 }
 
 void BKE_brush_weight_set(Paint *paint, Brush *brush, float value)
 {
   UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  if (ups->flag & UNIFIED_PAINT_WEIGHT) {
+  if (brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_WEIGHT) {
     ups->weight = value;
   }
   else {
@@ -1450,14 +1452,16 @@ int BKE_brush_input_samples_get(const Paint *paint, const Brush *brush)
 {
   const UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  return (ups->flag & UNIFIED_PAINT_INPUT_SAMPLES) ? ups->input_samples : brush->input_samples;
+  return (brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_INPUT_SAMPLES) ?
+             ups->input_samples :
+             brush->input_samples;
 }
 
 void BKE_brush_input_samples_set(Paint *paint, Brush *brush, int value)
 {
   UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  if (ups->flag & UNIFIED_PAINT_INPUT_SAMPLES) {
+  if (brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_INPUT_SAMPLES) {
     ups->input_samples = value;
   }
   else {

source/blender/blenkernel/intern/paint.cc
@@ -614,7 +614,7 @@ bool BKE_paint_use_unified_size(const Paint *paint)
     return false;
   }
 
-  return paint->unified_paint_settings.flag & UNIFIED_PAINT_SIZE;
+  return paint->brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_SIZE;
 }
 
 bool BKE_paint_use_unified_strength(const Paint *paint)
@@ -624,7 +624,7 @@ bool BKE_paint_use_unified_strength(const Paint *paint)
     return false;
   }
 
-  return paint->unified_paint_settings.flag & UNIFIED_PAINT_ALPHA;
+  return paint->brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_ALPHA;
 }
 
 bool BKE_paint_use_unified_color(const Paint *paint)
@@ -634,7 +634,7 @@ bool BKE_paint_use_unified_color(const Paint *paint)
     return false;
   }
 
-  return paint->unified_paint_settings.flag & UNIFIED_PAINT_COLOR;
+  return paint->brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_COLOR;
 }
 
 /**

scripts/modules/bl_keymap_utils/versioning.py
@@ -360,4 +360,50 @@ def keyconfig_update(keyconfig_data, keyconfig_version):
                     if index_to_fix != -1:
                         item_prop["properties"][index_to_fix] = ("brush_toggle", value_to_copy)
 
+    if keyconfig_version < (5, 3, 16):
+        if not has_copy:
+            keyconfig_data = copy.deepcopy(keyconfig_data)
+            has_copy = True
+
+        # The `use_unified_*` toggle for radial controls was moved from the paint mode's
+        # `unified_paint_settings` to brushes.
+        #
+        # The following conversion maps from the old values of
+        # `tool_settings.<paint_mode>.unified_paint_settings.use_unified_<property_name>`
+        # to
+        # `tool_settings.<paint_mode>.brush.use_unified_<property_name>`
+        #
+        # It also updates the color override test path:
+        # `tool_settings.<paint_mode>.unified_paint_settings.use_unified_color`
+        # becomes
+        # `tool_settings.<paint_mode>.brush.use_unified_color`
+
+        re_unified_paint_settings = re.compile(r"^(tool_settings)\.([a-z_]+)\.(unified_paint_settings)\.(.*)")
+
+        for _km_name, _km_parms, km_items_data in keyconfig_data:
+            for item_op, _item_event, item_prop in km_items_data["items"]:
+
+                if item_op == "wm.radial_control":
+                    use_secondary_path_index = -1
+                    new_use_secondary_path = ""
+                    fill_color_override_test_path_index = -1
+                    new_fill_color_override_test_path = ""
+
+                    for prop_index, (prop_id, prop_path) in enumerate(item_prop["properties"]):
+                        if (prop_id == "use_secondary" and re_unified_paint_settings.fullmatch(prop_path)):
+
+                            use_secondary_path_index = prop_index
+                            new_use_secondary_path = prop_path.replace("unified_paint_settings", "brush")
+                        elif (prop_id == "fill_color_override_test_path" and re_unified_paint_settings.fullmatch(prop_path)):
+
+                            fill_color_override_test_path_index = prop_index
+                            new_fill_color_override_test_path = prop_path.replace("unified_paint_settings", "brush")
+
+                    if (use_secondary_path_index != -1 and new_use_secondary_path):
+                        item_prop["properties"][use_secondary_path_index] = (
+                            "use_secondary", new_use_secondary_path)
+                    if (fill_color_override_test_path_index != -1 and new_fill_color_override_test_path):
+                        item_prop["properties"][fill_color_override_test_path_index] = (
+                            "fill_color_override_test_path", new_fill_color_override_test_path)
+
     return keyconfig_data

scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -4828,12 +4828,12 @@ def radial_control_properties(
         "properties": [
             ("data_path_primary", "{:s}.{:s}".format(brush_path, prop)),
             ("data_path_secondary", "{:s}.{:s}".format(unified_path, prop) if secondary_prop else ""),
-            ("use_secondary", "{:s}.{:s}".format(unified_path, secondary_prop) if secondary_prop else ""),
+            ("use_secondary", "{:s}.{:s}".format(brush_path, secondary_prop) if secondary_prop else ""),
             ("rotation_path", "{:s}.{:s}".format(brush_path, rotation)),
             ("color_path", "{:s}.cursor_color_add".format(brush_path)),
             ("fill_color_path", "{:s}.color".format(brush_path) if color else ""),
             ("fill_color_override_path", "{:s}.color".format(unified_path) if color else ""),
-            ("fill_color_override_test_path", "{:s}.use_unified_color".format(unified_path) if color else ""),
+            ("fill_color_override_test_path", "{:s}.use_unified_color".format(brush_path) if color else ""),
             ("zoom_path", "space_data.zoom" if zoom else ""),
             ("image_id", brush_path),
             ("secondary_tex", secondary_rotation),

scripts/presets/keyconfig/keymap_data/industry_compatible_data.py
@@ -2710,12 +2710,12 @@ def radial_control_properties(paint, prop, secondary_prop, secondary_rotation=Fa
         "properties": [
             ("data_path_primary", brush_path + "." + prop),
             ("data_path_secondary", unified_path + "." + prop if secondary_prop else ""),
-            ("use_secondary", unified_path + "." + secondary_prop if secondary_prop else ""),
+            ("use_secondary", brush_path + "." + secondary_prop if secondary_prop else ""),
             ("rotation_path", brush_path + "." + rotation),
             ("color_path", brush_path + ".cursor_color_add"),
             ("fill_color_path", brush_path + ".color" if color else ""),
             ("fill_color_override_path", unified_path + ".color" if color else ""),
-            ("fill_color_override_test_path", unified_path + ".use_unified_color" if color else ""),
+            ("fill_color_override_test_path", brush_path + ".use_unified_color" if color else ""),
             ("zoom_path", "space_data.zoom" if zoom else ""),
             ("image_id", brush_path + ""),
             ("secondary_tex", secondary_rotation),

scripts/startup/bl_ui/properties_paint_common.py
@@ -317,14 +317,14 @@ class UnifiedPaintPanel:
         else:
             ups = UnifiedPaintPanel.paint_settings_from_active_tool(context).unified_paint_settings
         prop_owner = brush
-        if unified_name and getattr(ups, unified_name):
+        if unified_name and getattr(brush, unified_name):
             prop_owner = ups
 
         row.prop(prop_owner, prop_name, icon='NONE', text=text, slider=slider)
 
         if unified_name and not header:
             # NOTE: We don't draw UnifiedPaintSettings in the header to reduce clutter. D5928#136281
-            row.prop(ups, unified_name, text="", icon='BRUSHES_ALL')
+            row.prop(brush, unified_name, text="", icon='BRUSHES_ALL')
 
         if pressure_name:
             row.prop(brush, pressure_name, text="")
@@ -360,13 +360,14 @@ class UnifiedPaintPanel:
     @staticmethod
     def prop_unified_color(parent, context, brush, prop_name, *, text=None):
         ups = UnifiedPaintPanel.paint_settings_from_active_tool(context).unified_paint_settings
-        prop_owner = ups if ups.use_unified_color else brush
+        prop_owner = ups if brush.use_unified_color else brush
+
         parent.prop(prop_owner, prop_name, text=text)
 
     @staticmethod
     def prop_unified_color_picker(parent, context, brush, prop_name, value_slider=True):
         ups = UnifiedPaintPanel.paint_settings_from_active_tool(context).unified_paint_settings
-        prop_owner = ups if ups.use_unified_color else brush
+        prop_owner = ups if brush.use_unified_color else brush
         parent.template_color_picker(prop_owner, prop_name, value_slider=value_slider)
 
 
@@ -1015,7 +1016,7 @@ def brush_settings(layout, context, brush, popover=False):
             UnifiedPaintPanel.prop_unified_color(row, context, brush, "secondary_color", text="")
             row.separator()
             row.operator("paint.brush_colors_flip", icon='FILE_REFRESH', text="", emboss=False)
-            row.prop(ups, "use_unified_color", text="", icon='BRUSHES_ALL')
+            row.prop(brush, "use_unified_color", text="", icon='BRUSHES_ALL')
             layout.prop(brush, "blend", text="Blend Mode")
 
         # Per sculpt tool options.
@@ -1305,7 +1306,7 @@ def brush_shared_settings(layout, context, brush, popover=False):
             slider=True,
         )
 
-    size_owner = ups if ups.use_unified_size else brush
+    size_owner = ups if brush.use_unified_size else brush
     size_prop = "size"
     if size_mode and (size_owner.use_locked_size == 'SCENE'):
         size_prop = "unprojected_size"
@@ -1378,7 +1379,7 @@ def brush_shared_settings(layout, context, brush, popover=False):
 def draw_color_jitter_panel(layout, context, brush):
     ups = UnifiedPaintPanel.paint_settings_from_active_tool(context).unified_paint_settings
 
-    prop_owner = ups if ups.use_unified_color else brush
+    prop_owner = ups if brush.use_unified_color else brush
     layout.use_property_split = False
 
     header, panel = layout.panel("color_jitter_panel", default_closed=True)
@@ -1639,7 +1640,7 @@ def draw_color_settings(context, layout, brush, color_type=False):
         UnifiedPaintPanel.prop_unified_color(row, context, brush, "secondary_color", text="")
         row.separator()
         row.operator("paint.brush_colors_flip", icon='FILE_REFRESH', text="", emboss=False)
-        row.prop(ups, "use_unified_color", text="", icon='BRUSHES_ALL')
+        row.prop(brush, "use_unified_color", text="", icon='BRUSHES_ALL')
 
         draw_color_jitter_panel(layout, context, brush)
 

scripts/startup/bl_ui/space_toolsystem_toolbar.py
@@ -2165,10 +2165,11 @@ class _defs_weight_paint:
     def sample_weight():
         def draw_settings(context, layout, _tool):
             ups = context.tool_settings.weight_paint.unified_paint_settings
-            if ups.use_unified_weight:
+            brush = context.tool_settings.weight_paint.brush
+            if brush.use_unified_weight:
                 weight = ups.weight
-            elif context.tool_settings.weight_paint.brush:
-                weight = context.tool_settings.weight_paint.brush.weight
+            elif brush:
+                weight = brush.weight
             else:
                 return
             layout.label(text=iface_("Weight: {:.3f}").format(weight), translate=False)

scripts/startup/bl_ui/space_view3d.py
@@ -287,7 +287,7 @@ class _draw_tool_settings_context_mode:
             layout.prop(brush, "blend", text="", expand=False)
 
         size = "size"
-        size_owner = ups if ups.use_unified_size else brush
+        size_owner = ups if brush.use_unified_size else brush
         if size_owner.use_locked_size == 'SCENE':
             size = "unprojected_size"
 
@@ -421,7 +421,7 @@ class _draw_tool_settings_context_mode:
         ups = paint.unified_paint_settings
 
         size = "size"
-        size_owner = ups if ups.use_unified_size else brush
+        size_owner = ups if brush.use_unified_size else brush
         if size_owner.use_locked_size == 'SCENE':
             size = "unprojected_size"
 
@@ -490,7 +490,7 @@ class _draw_tool_settings_context_mode:
         if brush.gpencil_vertex_brush_type not in {'BLUR', 'AVERAGE', 'SMEAR'}:
             layout.separator(factor=0.4)
             ups = paint.unified_paint_settings
-            prop_owner = ups if ups.use_unified_color else brush
+            prop_owner = ups if brush.use_unified_color else brush
             layout.prop_with_popover(prop_owner, "color", text="", panel="TOPBAR_PT_grease_pencil_vertex_color")
 
         brush_basic_grease_pencil_vertex_settings(layout, context, brush, compact=True)
@@ -8657,16 +8657,16 @@ class VIEW3D_PT_greasepencil_sculpt_context_menu(Panel):
         layout = self.layout
 
         ups = paint.unified_paint_settings
-        size_owner = ups if ups.use_unified_size else brush
-        strength_owner = ups if ups.use_unified_strength else brush
+        size_owner = ups if brush.use_unified_size else brush
+        strength_owner = ups if brush.use_unified_strength else brush
         row = layout.row(align=True)
         row.prop(size_owner, "size", text="")
         row.prop(brush, "use_pressure_size", text="", icon='STYLUS_PRESSURE')
-        row.prop(ups, "use_unified_size", text="", icon='BRUSHES_ALL')
+        row.prop(brush, "use_unified_size", text="", icon='BRUSHES_ALL')
         row = layout.row(align=True)
         row.prop(strength_owner, "strength", text="")
         row.prop(brush, "use_pressure_strength", text="", icon='STYLUS_PRESSURE')
-        row.prop(ups, "use_unified_strength", text="", icon='BRUSHES_ALL')
+        row.prop(brush, "use_unified_strength", text="", icon='BRUSHES_ALL')
 
         layer = context.object.data.layers.active
 
@@ -8697,7 +8697,7 @@ class VIEW3D_PT_greasepencil_vertex_paint_context_menu(Panel):
         if brush.gpencil_vertex_brush_type in {'DRAW', 'REPLACE'}:
             split = layout.split(factor=0.1)
             ups = settings.unified_paint_settings
-            prop_owner = ups if ups.use_unified_color else brush
+            prop_owner = ups if brush.use_unified_color else brush
             split.prop(prop_owner, "color", text="")
             split.template_color_picker(prop_owner, "color", value_slider=True)
 
@@ -8712,11 +8712,11 @@ class VIEW3D_PT_greasepencil_vertex_paint_context_menu(Panel):
 
         if brush.gpencil_vertex_brush_type in {'DRAW', 'BLUR', 'SMEAR'}:
             ups = settings.unified_paint_settings
-            strength_owner = ups if ups.use_unified_strength else brush
+            strength_owner = ups if brush.use_unified_strength else brush
             row = layout.row(align=True)
             row.prop(strength_owner, "strength", text="")
             row.prop(brush, "use_pressure_strength", text="", icon='STYLUS_PRESSURE')
-            row.prop(ups, "use_unified_strength", text="", icon='BRUSHES_ALL')
+            row.prop(brush, "use_unified_strength", text="", icon='BRUSHES_ALL')
 
         layer = context.object.data.layers.active
 
@@ -8942,7 +8942,7 @@ class VIEW3D_PT_sculpt_context_menu(Panel):
 
         ups = paint.unified_paint_settings
         size = "size"
-        size_owner = ups if ups.use_unified_size else brush
+        size_owner = ups if brush.use_unified_size else brush
         if size_owner.use_locked_size == 'SCENE':
             size = "unprojected_size"
 
@@ -9026,7 +9026,7 @@ class TOPBAR_PT_grease_pencil_vertex_color(Panel):
 
         ups = paint.unified_paint_settings
         brush = paint.brush
-        prop_owner = ups if use_unified_paint and ups.use_unified_color else brush
+        prop_owner = ups if use_unified_paint and brush.use_unified_color else brush
 
         col = layout.column()
         col.template_color_picker(prop_owner, "color", value_slider=True)

commit 5f1e86e05433c96baf496fe383e4e0263f8bbeac

Fix: Potential crash when calling BKE_paint_use_unified methods
Introduced with 6ecfe4e810e

It's possible that a `Paint` struct has no `Brush` in the case of tools
/ operators being executed when a brush is not selected. Since the
setting is no longer is on the `Paint` struct, this commit renames the
functions to `BKE_brush_use_unified_*` and returns `true` if no brush is
passed in.

This also ensures that the correct brush is being specified, instead
of assuming callers are always working on the active brush.

source/blender/blenkernel/BKE_brush.hh
@@ -170,6 +170,10 @@ ImBuf *BKE_brush_gen_radial_control_imbuf(Brush *br, bool secondary, bool displa
 
 /* Unified strength size and color. */
 
+bool BKE_brush_use_unified_size(const Paint *paint, const Brush *brush);
+bool BKE_brush_use_unified_strength(const Paint *paint, const Brush *brush);
+bool BKE_brush_use_unified_color(const Paint *paint, const Brush *brush);
+
 struct BrushColorJitterSettings {
   int flag;
   /** Jitter amounts */

source/blender/blenkernel/BKE_paint.hh
@@ -184,9 +184,6 @@ Paint *BKE_paint_get_active(const Main &bmain, Scene *sce, ViewLayer *view_layer
 Paint *BKE_paint_get_active_from_context(const bContext *C);
 PaintMode BKE_paintmode_get_active_from_context(const bContext *C);
 PaintMode BKE_paintmode_get_from_tool(const bToolRef *tref);
-bool BKE_paint_use_unified_size(const Paint *paint);
-bool BKE_paint_use_unified_strength(const Paint *paint);
-bool BKE_paint_use_unified_color(const Paint *paint);
 
 /* Paint brush retrieval and assignment. */
 

source/blender/blenkernel/intern/brush.cc
@@ -1216,9 +1216,57 @@ float BKE_brush_sample_masktex(
 /** \name Unified Settings
  * \{ */
 
+bool BKE_brush_use_unified_size(const Paint *paint, const Brush *brush)
+{
+  /* For now, Grease Pencil Draw mode doesn't use the unified paint settings. */
+  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
+    return false;
+  }
+
+  /* In the case of having no active brush (e.g. for non-brush tools), default to the scene level
+   * settings */
+  if (!brush) {
+    return true;
+  }
+
+  return brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_SIZE;
+}
+
+bool BKE_brush_use_unified_strength(const Paint *paint, const Brush *brush)
+{
+  /* For now, Grease Pencil Draw mode doesn't use the unified paint settings. */
+  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
+    return false;
+  }
+
+  /* In the case of having no active brush (e.g. for non-brush tools), default to the scene level
+   * settings */
+  if (!brush) {
+    return true;
+  }
+
+  return brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_ALPHA;
+}
+
+bool BKE_brush_use_unified_color(const Paint *paint, const Brush *brush)
+{
+  /* For now, Grease Pencil Draw mode doesn't use the unified paint settings. */
+  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
+    return false;
+  }
+
+  /* In the case of having no active brush (e.g. for non-brush tools), default to the scene level
+   * settings */
+  if (!brush) {
+    return true;
+  }
+
+  return brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_COLOR;
+}
+
 float3 BKE_brush_color_get(const Paint *paint, const Brush *brush)
 {
-  if (BKE_paint_use_unified_color(paint)) {
+  if (BKE_brush_use_unified_color(paint, brush)) {
     return paint->unified_paint_settings.color;
   }
   return brush->color;
@@ -1228,7 +1276,7 @@ float3 BKE_brush_color_get(const Paint *paint, const Brush *brush)
 std::optional<BrushColorJitterSettings> BKE_brush_color_jitter_get_settings(const Paint *paint,
                                                                             const Brush *brush)
 {
-  if (BKE_paint_use_unified_color(paint)) {
+  if (BKE_brush_use_unified_color(paint, brush)) {
     if ((paint->unified_paint_settings.flag & UNIFIED_PAINT_COLOR_JITTER) == 0) {
       return std::nullopt;
     }
@@ -1262,7 +1310,7 @@ std::optional<BrushColorJitterSettings> BKE_brush_color_jitter_get_settings(cons
 
 float3 BKE_brush_secondary_color_get(const Paint *paint, const Brush *brush)
 {
-  if (BKE_paint_use_unified_color(paint)) {
+  if (BKE_brush_use_unified_color(paint, brush)) {
     return paint->unified_paint_settings.secondary_color;
   }
   return brush->secondary_color;
@@ -1270,7 +1318,7 @@ float3 BKE_brush_secondary_color_get(const Paint *paint, const Brush *brush)
 
 void BKE_brush_color_set(Paint *paint, Brush *brush, const float3 &color)
 {
-  if (BKE_paint_use_unified_color(paint)) {
+  if (BKE_brush_use_unified_color(paint, brush)) {
     UnifiedPaintSettings *ups = &paint->unified_paint_settings;
     copy_v3_v3(ups->color, color);
     BKE_brush_color_sync_legacy(ups);
@@ -1310,7 +1358,7 @@ void BKE_brush_size_set(Paint *paint, Brush *brush, int size)
   /* make sure range is sane */
   CLAMP(size, 1, MAX_BRUSH_PIXEL_DIAMETER);
 
-  if (BKE_paint_use_unified_size(paint)) {
+  if (BKE_brush_use_unified_size(paint, brush)) {
     ups->size = size;
   }
   else {
@@ -1323,7 +1371,7 @@ int BKE_brush_size_get(const Paint *paint, const Brush *brush)
 {
   const UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  if (BKE_paint_use_unified_size(paint)) {
+  if (BKE_brush_use_unified_size(paint, brush)) {
     return ups->size;
   }
   return brush->size;
@@ -1357,7 +1405,7 @@ void BKE_brush_unprojected_size_set(Paint *paint, Brush *brush, float unprojecte
 {
   UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  if (BKE_paint_use_unified_size(paint)) {
+  if (BKE_brush_use_unified_size(paint, brush)) {
     ups->unprojected_size = unprojected_size;
   }
   else {
@@ -1369,7 +1417,7 @@ void BKE_brush_unprojected_size_set(Paint *paint, Brush *brush, float unprojecte
 float BKE_brush_unprojected_size_get(const Paint *paint, const Brush *brush)
 {
   const UnifiedPaintSettings *ups = &paint->unified_paint_settings;
-  if (BKE_paint_use_unified_size(paint)) {
+  if (BKE_brush_use_unified_size(paint, brush)) {
     return ups->unprojected_size;
   }
   return brush->unprojected_size;
@@ -1408,7 +1456,7 @@ void BKE_brush_alpha_set(Paint *paint, Brush *brush, float alpha)
 {
   UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  if (BKE_paint_use_unified_strength(paint)) {
+  if (BKE_brush_use_unified_strength(paint, brush)) {
     ups->alpha = alpha;
   }
   else {
@@ -1421,7 +1469,7 @@ float BKE_brush_alpha_get(const Paint *paint, const Brush *brush)
 {
   const UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
-  if (BKE_paint_use_unified_strength(paint)) {
+  if (BKE_brush_use_unified_strength(paint, brush)) {
     return ups->alpha;
   }
   return brush->alpha;

source/blender/blenkernel/intern/paint.cc
@@ -607,36 +607,6 @@ PaintMode BKE_paintmode_get_from_tool(const bToolRef *tref)
   return PaintMode::Invalid;
 }
 
-bool BKE_paint_use_unified_size(const Paint *paint)
-{
-  /* For now, Grease Pencil Draw mode doesn't use the unified paint settings. */
-  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
-    return false;
-  }
-
-  return paint->brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_SIZE;
-}
-
-bool BKE_paint_use_unified_strength(const Paint *paint)
-{
-  /* For now, Grease Pencil Draw mode doesn't use the unified paint settings. */
-  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
-    return false;
-  }
-
-  return paint->brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_ALPHA;
-}
-
-bool BKE_paint_use_unified_color(const Paint *paint)
-{
-  /* For now, Grease Pencil Draw mode doesn't use the unified paint settings. */
-  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
-    return false;
-  }
-
-  return paint->brush->unified_paint_flags & BRUSH_USE_UNIFIED_PAINT_COLOR;
-}
-
 /**
  * After changing #Paint.brush_asset_reference, call this to activate the matching brush, importing
  * it if necessary. Has no effect if #Paint.brush is set already.

source/blender/editors/sculpt_paint/mesh/paint_image.cc
@@ -769,7 +769,7 @@ static wmOperatorStatus brush_colors_flip_exec(bContext *C, wmOperator * /*op*/)
   Paint *paint = BKE_paint_get_active_from_context(C);
   Brush *br = BKE_paint_brush(paint);
 
-  if (BKE_paint_use_unified_color(paint)) {
+  if (BKE_brush_use_unified_color(paint, br)) {
     UnifiedPaintSettings &ups = paint->unified_paint_settings;
     swap_v3_v3(ups.color, ups.secondary_color);
     BKE_brush_color_sync_legacy(&ups);