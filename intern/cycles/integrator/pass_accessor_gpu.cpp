zNightlord/Bforartists

commit 02171cc35621fb31fc0ac45b489c7e5707448f5c

Cycles: Add fundamental support for upscaling denoisers
Adds basic infrastructure for denoisers that can also upscale for
viewport rendering, by taking advantage of the existing resolution
divider functionality.

The implementation basically tracks two resolution dividers, the normal
one and an additional one that also has a denoiser upscale factor
applied. It then uses the latter one for rendering and processing on
noisy render buffers, and the former one for any processing happening
after denoising. This has the advantage of allowing an additional
resolution divider on top of denoiser upscaling (so can also set the
pixel size to something other than 1x and that is still being
respected). The resolution divider was made into a floating point value
to allow fractional scale factors.

Pull Request: https://projects.blender.org/blender/blender/pulls/151133

intern/cycles/integrator/path_trace.cpp
@@ -329,22 +329,22 @@ void PathTrace::update_allocated_work_buffer_params()
                                });
 }
 
-static BufferParams scale_buffer_params(const BufferParams &params, const int resolution_divider)
+static BufferParams scale_buffer_params(const BufferParams &params, const float resolution_divider)
 {
   BufferParams scaled_params = params;
 
-  scaled_params.width = max(1, params.width / resolution_divider);
-  scaled_params.height = max(1, params.height / resolution_divider);
+  scaled_params.width = max(1, int(params.width / resolution_divider));
+  scaled_params.height = max(1, int(params.height / resolution_divider));
 
-  scaled_params.window_x = params.window_x / resolution_divider;
-  scaled_params.window_y = params.window_y / resolution_divider;
-  scaled_params.window_width = max(1, params.window_width / resolution_divider);
-  scaled_params.window_height = max(1, params.window_height / resolution_divider);
+  scaled_params.window_x = int(params.window_x / resolution_divider);
+  scaled_params.window_y = int(params.window_y / resolution_divider);
+  scaled_params.window_width = max(1, int(params.window_width / resolution_divider));
+  scaled_params.window_height = max(1, int(params.window_height / resolution_divider));
 
-  scaled_params.full_x = params.full_x / resolution_divider;
-  scaled_params.full_y = params.full_y / resolution_divider;
-  scaled_params.full_width = max(1, params.full_width / resolution_divider);
-  scaled_params.full_height = max(1, params.full_height / resolution_divider);
+  scaled_params.full_x = int(params.full_x / resolution_divider);
+  scaled_params.full_y = int(params.full_y / resolution_divider);
+  scaled_params.full_width = max(1, int(params.full_width / resolution_divider));
+  scaled_params.full_height = max(1, int(params.full_height / resolution_divider));
 
   scaled_params.update_offset_stride();
 
@@ -353,24 +353,32 @@ static BufferParams scale_buffer_params(const BufferParams &params, const int re
 
 void PathTrace::update_effective_work_buffer_params(const RenderWork &render_work)
 {
-  const int resolution_divider = render_work.resolution_divider;
+  const float denoised_resolution_divider = render_work.denoised_resolution_divider;
+  const float resolution_divider = render_work.resolution_divider / denoised_resolution_divider;
 
-  const BufferParams scaled_full_params = scale_buffer_params(full_params_, resolution_divider);
-  const BufferParams scaled_big_tile_params = scale_buffer_params(big_tile_params_,
+  const BufferParams denoised_big_tile_params = scale_buffer_params(big_tile_params_,
+                                                                    denoised_resolution_divider);
+  const BufferParams scaled_big_tile_params = scale_buffer_params(denoised_big_tile_params,
                                                                   resolution_divider);
 
   const int overscan = tile_manager_.get_tile_overscan();
 
-  foreach_sliced_buffer_params(path_trace_works_,
-                               work_balance_infos_,
-                               scaled_big_tile_params,
-                               overscan,
-                               [&](PathTraceWork *path_trace_work, const BufferParams params) {
-                                 path_trace_work->set_effective_buffer_params(
-                                     scaled_full_params, scaled_big_tile_params, params);
-                               });
+  foreach_sliced_buffer_params(
+      path_trace_works_,
+      work_balance_infos_,
+      denoised_big_tile_params,
+      overscan,
+      [&](PathTraceWork *path_trace_work, const BufferParams params) {
+        /* Scale down the sliced buffer parameters again that were scaled by denoising upscale
+         * factor above. This should match the values that would occur when slicing
+         * 'scaled_big_tile_params' directly. */
+        const BufferParams scaled_params = scale_buffer_params(params, resolution_divider);
+        path_trace_work->set_effective_buffer_params(
+            scaled_big_tile_params, scaled_params, denoised_big_tile_params, params);
+      });
 
   render_state_.effective_big_tile_params = scaled_big_tile_params;
+  render_state_.effective_denoised_big_tile_params = denoised_big_tile_params;
 }
 
 void PathTrace::update_work_buffer_params_if_needed(const RenderWork &render_work)
@@ -518,6 +526,7 @@ void PathTrace::set_denoiser_params(const DenoiseParams &params)
 {
   if (!params.use) {
     denoiser_.reset();
+    render_scheduler_.set_denoiser_params(params);
     return;
   }
 
@@ -630,12 +639,14 @@ void PathTrace::denoise(const RenderWork &render_work)
   }
 
   if (big_tile_denoise_work_) {
-    big_tile_denoise_work_->set_effective_buffer_params(render_state_.effective_big_tile_params,
-                                                        render_state_.effective_big_tile_params,
-                                                        render_state_.effective_big_tile_params);
+    big_tile_denoise_work_->set_effective_buffer_params(
+        render_state_.effective_big_tile_params,
+        render_state_.effective_big_tile_params,
+        render_state_.effective_denoised_big_tile_params,
+        render_state_.effective_denoised_big_tile_params);
 
     buffer_to_denoise = big_tile_denoise_work_->get_render_buffers();
-    buffer_to_denoise->reset(render_state_.effective_big_tile_params);
+    buffer_to_denoise->reset(render_state_.effective_denoised_big_tile_params);
 
     copy_to_render_buffers(buffer_to_denoise);
 
@@ -648,6 +659,7 @@ void PathTrace::denoise(const RenderWork &render_work)
   }
 
   if (denoiser_->denoise_buffer(render_state_.effective_big_tile_params,
+                                render_state_.effective_denoised_big_tile_params,
                                 buffer_to_denoise,
                                 get_num_samples_in_buffer(),
                                 allow_inplace_modification))
@@ -751,17 +763,22 @@ void PathTrace::update_display(const RenderWork &render_work)
   if (display_) {
     LOG_DEBUG << "Perform copy to GPUDisplay work.";
 
-    const int texture_width = render_state_.effective_big_tile_params.window_width;
-    const int texture_height = render_state_.effective_big_tile_params.window_height;
+    const PassType pass_type = film_->get_display_pass();
+    const bool show_denoised =
+        ((render_work.display.use_denoised_result && has_denoised_result() &&
+          big_tile_params_.get_pass_offset(pass_type, PassMode::DENOISED) != PASS_UNUSED) ||
+         is_volume_guiding_pass(pass_type));
+
+    const int texture_width = show_denoised ?
+                                  render_state_.effective_denoised_big_tile_params.window_width :
+                                  render_state_.effective_big_tile_params.window_width;
+    const int texture_height = show_denoised ?
+                                   render_state_.effective_denoised_big_tile_params.window_height :
+                                   render_state_.effective_big_tile_params.window_height;
     if (!display_->update_begin(texture_width, texture_height)) {
       LOG_ERROR << "Error beginning GPUDisplay update.";
       return;
     }
-
-    const PassType pass_type = film_->get_display_pass();
-    const bool show_denoised = (render_work.display.use_denoised_result &&
-                                has_denoised_result()) ||
-                               is_volume_guiding_pass(pass_type);
 
     const PassMode pass_mode = show_denoised ? PassMode::DENOISED : PassMode::NOISY;
 
@@ -1114,7 +1131,8 @@ void PathTrace::process_full_buffer_from_disk(string_view filename)
     set_denoiser_params(denoise_params);
 
     /* Number of samples doesn't matter too much, since the samples count pass will be used. */
-    denoiser_->denoise_buffer(full_frame_buffers.params, &full_frame_buffers, 0, false);
+    denoiser_->denoise_buffer(
+        full_frame_buffers.params, full_frame_buffers.params, &full_frame_buffers, 0, false);
 
     render_state_.has_denoised_result = true;
   }

intern/cycles/integrator/path_trace_work_gpu.cpp
@@ -1036,17 +1036,24 @@ void PathTraceWorkGPU::copy_to_display_naive(PathTraceDisplay *display,
                                              PassMode pass_mode,
                                              const int num_samples)
 {
-  const int full_x = effective_buffer_params_.full_x;
-  const int full_y = effective_buffer_params_.full_y;
-  const int width = effective_buffer_params_.window_width;
-  const int height = effective_buffer_params_.window_height;
+  const BufferParams &effective_big_tile_params = (pass_mode == PassMode::DENOISED) ?
+                                                      effective_denoised_big_tile_params_ :
+                                                      effective_big_tile_params_;
+  const BufferParams &effective_buffer_params = (pass_mode == PassMode::DENOISED) ?
+                                                    effective_denoised_buffer_params_ :
+                                                    effective_buffer_params_;
+
+  const int full_x = effective_buffer_params.full_x;
+  const int full_y = effective_buffer_params.full_y;
+  const int width = effective_buffer_params.window_width;
+  const int height = effective_buffer_params.window_height;
   const int final_width = buffers_->params.window_width;
   const int final_height = buffers_->params.window_height;
 
-  const int texture_x = full_x - effective_big_tile_params_.full_x +
-                        effective_buffer_params_.window_x - effective_big_tile_params_.window_x;
-  const int texture_y = full_y - effective_big_tile_params_.full_y +
-                        effective_buffer_params_.window_y - effective_big_tile_params_.window_y;
+  const int texture_x = full_x - effective_big_tile_params.full_x +
+                        effective_buffer_params.window_x - effective_big_tile_params.window_x;
+  const int texture_y = full_y - effective_big_tile_params.full_y +
+                        effective_buffer_params.window_y - effective_big_tile_params.window_y;
 
   /* Re-allocate display memory if needed, and make sure the device pointer is allocated.
    *
@@ -1120,9 +1127,13 @@ void PathTraceWorkGPU::get_render_tile_film_pixels(const PassAccessor::Destinati
     return;
   }
 
+  const BufferParams &effective_buffer_params = (pass_mode == PassMode::DENOISED) ?
+                                                    effective_denoised_buffer_params_ :
+                                                    effective_buffer_params_;
+
   const PassAccessorGPU pass_accessor(queue_.get(), pass_access_info, kfilm.exposure, num_samples);
 
-  pass_accessor.get_render_tile_pixels(buffers_.get(), effective_buffer_params_, destination);
+  pass_accessor.get_render_tile_pixels(buffers_.get(), effective_buffer_params, destination);
 }
 
 int PathTraceWorkGPU::adaptive_sampling_converge_filter_count_active(const float threshold,

intern/cycles/blender/sync.cpp
@@ -560,6 +560,7 @@ void BlenderSync::sync_integrator(blender::ViewLayer &b_view_layer,
     integrator->set_use_denoise_pass_motion(denoise_params.temporally_stable);
     integrator->set_denoiser_prefilter(denoise_params.prefilter);
     integrator->set_denoiser_quality(denoise_params.quality);
+    integrator->set_denoiser_upscale_factor(denoise_params.upscale_factor);
   }
 
   /* UPDATE_NONE as we don't want to tag the integrator as modified (this was done by the

intern/cycles/device/denoise.cpp
@@ -82,6 +82,7 @@ NODE_DEFINE(DenoiseParams)
 
   SOCKET_ENUM(prefilter, "Prefilter", *prefilter_enum, DENOISER_PREFILTER_FAST);
   SOCKET_ENUM(quality, "Quality", *quality_enum, DENOISER_QUALITY_HIGH);
+  SOCKET_FLOAT(upscale_factor, "Upscale Factor", 1.0f);
 
   return type;
 }

intern/cycles/device/denoise.h
@@ -77,6 +77,7 @@ class DenoiseParams : public Node {
 
   DenoiserPrefilter prefilter = DENOISER_PREFILTER_FAST;
   DenoiserQuality quality = DENOISER_QUALITY_HIGH;
+  float upscale_factor = 1.0f;
 
   static const NodeEnum *get_type_enum();
   static const NodeEnum *get_prefilter_enum();

intern/cycles/device/queue.h
@@ -30,7 +30,7 @@ struct DeviceKernelArguments {
     HIPRT_GLOBAL_STACK,
   };
 
-  static const int MAX_ARGS = 18;
+  static const int MAX_ARGS = 19;
   Type types[MAX_ARGS];
   void *values[MAX_ARGS];
   size_t sizes[MAX_ARGS];

intern/cycles/integrator/denoiser.cpp
@@ -141,6 +141,7 @@ DenoiseParams get_effective_denoise_params(Device *denoiser_device,
   /* Always fallback to OIDN on CPU. */
   effective_denoise_params.type = DENOISER_OPENIMAGEDENOISE;
   effective_denoise_params.use_gpu = false;
+  effective_denoise_params.upscale_factor = 1.0f;
 
   return effective_denoise_params;
 }

intern/cycles/integrator/denoiser.h
@@ -90,8 +90,9 @@ class Denoiser {
    * Returns true when all passes are denoised. Will return false if there is a denoiser error (for
    * example, caused by misconfigured denoiser) or when user requested to cancel rendering. */
   virtual bool denoise_buffer(const BufferParams &buffer_params,
+                              const BufferParams &denoised_buffer_params,
                               RenderBuffers *render_buffers,
-                              const int num_samples,
+                              int num_samples,
                               bool allow_inplace_modification) = 0;
 
   /* Get a device which is used to perform actual denoising.

blender/blender

commit cb2479ad5b9e2bcaba1c9bdbd869cd6bf241499c

Fix #162582: Cycles crash vith small render on multiple devices
The render buffer is sliced horizontally to distribute work over
multiple devices. After recent changes this was no longer working well with
scaling for progressive render and denoising upscale.

In particular, when there is not enough work some slices did not properly
become empty but were clamped to height 1 as part of the scaling, which
caused mismatches.

Now the buffer is sliced at full resolution, and then the resulting Y
coordinates are scaled down, rather than slicing at each resolution. Empty
slices are handled better by early outs in a few functions.

Issue introduced in 02171cc3562.

Co-authored-by: Leon Zandman <leon@wirwar.com>

Pull Request: https://projects.blender.org/blender/blender/pulls/162588

intern/cycles/integrator/pass_accessor_gpu.cpp
@@ -31,6 +31,9 @@ void PassAccessorGPU::run_film_convert_kernels(DeviceKernel kernel,
   init_kernel_film_convert(&kfilm_convert, buffer_params, destination);
 
   const int work_size = buffer_params.window_width * buffer_params.window_height;
+  if (!work_size) {
+    return;
+  }
 
   const int destination_stride = destination.stride != 0 ? destination.stride :
                                                            buffer_params.window_width;

intern/cycles/integrator/path_trace.cpp
@@ -269,33 +269,61 @@ void PathTrace::render_deinit_kernel_execution()
   }
 }
 
+/* Scale down an Y coordinate for slicing the buffer. */
+static int scale_window_y(const int window_y,
+                          const int window_height,
+                          const int scaled_window_height,
+                          const float resolution_divider)
+{
+  if (window_y >= window_height) {
+    return scaled_window_height;
+  }
+
+  return min(int(window_y / resolution_divider), scaled_window_height);
+}
+
 /*
  * Get the parameters of the horizontal slice of the buffer, for a given work index and
  * balancing weights. Note the result can be empty with many devices and small height.
- * */
+ *
+ * The split is computed at the full resolution, and then scaled down. This keeps the slices
+ * filling the window exactly at every resolution and keeps a slice that is empty at full
+ * resolution empty at the downscaled resolution. Slicing at different resolutions with
+ * float weights can lead to mismatches.
+ */
 static BufferParams slice_buffer_params(const BufferParams &buffer_params,
+                                        const float resolution_divider,
+                                        const int full_res_window_height,
                                         const vector<WorkBalanceInfo> &work_balance_infos,
                                         const int index,
                                         const int overscan)
 {
   const int num_works = work_balance_infos.size();
-  const int window_height = buffer_params.window_height;
 
-  /* Find the rows at which the slice starts and ends. */
-  int window_y = 0;
-  int window_end_y = 0;
+  /* Find the Y coordinates at which the slice starts and ends, at the full resolution. */
+  int full_res_window_y = 0;
+  int full_res_window_end_y = 0;
 
   for (int i = 0; i <= index; i++) {
-    window_y = window_end_y;
+    full_res_window_y = full_res_window_end_y;
 
     const double weight = work_balance_infos[i].weight;
-    const int slice_window_height = max(lround(window_height * weight), 1);
+    const int slice_window_height = max(lround(full_res_window_height * weight), 1);
 
-    const int remaining_window_height = max(0, window_height - window_y);
-    window_end_y = window_y + ((i < num_works - 1) ?
-                                   min(slice_window_height, remaining_window_height) :
-                                   remaining_window_height);
+    const int remaining_window_height = max(0, full_res_window_height - full_res_window_y);
+    full_res_window_end_y = full_res_window_y +
+                            ((i < num_works - 1) ?
+                                 min(slice_window_height, remaining_window_height) :
+                                 remaining_window_height);
   }
+
+  /* Scale the Y coordinates down to the resolution of the buffer. */
+  const int window_y = scale_window_y(
+      full_res_window_y, full_res_window_height, buffer_params.window_height, resolution_divider);
+  const int window_end_y = scale_window_y(full_res_window_end_y,
+                                          full_res_window_height,
+                                          buffer_params.window_height,
+                                          resolution_divider);
 
   const int slice_window_full_y = buffer_params.full_y + buffer_params.window_y + window_y;
 
@@ -321,7 +349,7 @@ void PathTrace::update_allocated_work_buffer_params()
   const int num_works = path_trace_works_.size();
   for (int i = 0; i < num_works; ++i) {
     const BufferParams slice_params = slice_buffer_params(
-        big_tile_params_, work_balance_infos_, i, overscan);
+        big_tile_params_, 1.0f, big_tile_params_.window_height, work_balance_infos_, i, overscan);
 
     RenderBuffers *buffers = path_trace_works_[i]->get_render_buffers();
     buffers->reset(slice_params);
@@ -332,13 +360,16 @@ static BufferParams scale_buffer_params(const BufferParams &params, const float 
 {
   BufferParams scaled_params = params;
 
-  scaled_params.width = max(1, int(params.width / resolution_divider));
-  scaled_params.height = max(1, int(params.height / resolution_divider));
-
   scaled_params.window_x = int(params.window_x / resolution_divider);
   scaled_params.window_y = int(params.window_y / resolution_divider);
   scaled_params.window_width = max(1, int(params.window_width / resolution_divider));
   scaled_params.window_height = max(1, int(params.window_height / resolution_divider));
+
+  /* Scaled down buffer size, but not smaller than the window that we need to write into. */
+  scaled_params.width = max(int(params.width / resolution_divider),
+                            scaled_params.window_x + scaled_params.window_width);
+  scaled_params.height = max(int(params.height / resolution_divider),
+                             scaled_params.window_y + scaled_params.window_height);
 
   scaled_params.full_x = int(params.full_x / resolution_divider);
   scaled_params.full_y = int(params.full_y / resolution_divider);
@@ -361,17 +392,24 @@ void PathTrace::update_effective_work_buffer_params(const RenderWork &render_wor
                                                                   resolution_divider);
 
   const int overscan = tile_manager_.get_tile_overscan();
+  const int scaled_overscan = int(overscan / resolution_divider);
+  const int full_res_window_height = big_tile_params_.window_height;
 
   const int num_works = path_trace_works_.size();
   for (int i = 0; i < num_works; ++i) {
-    const BufferParams denoised_slice_params = slice_buffer_params(
-        denoised_big_tile_params, work_balance_infos_, i, overscan);
+    const BufferParams denoised_slice_params = slice_buffer_params(denoised_big_tile_params,
+                                                                   denoised_resolution_divider,
+                                                                   full_res_window_height,
+                                                                   work_balance_infos_,
+                                                                   i,
+                                                                   overscan);
 
-    /* Scale down the sliced buffer parameters again that were scaled by denoising upscale
-     * factor above. This should match the values that would occur when slicing
-     * 'scaled_big_tile_params' directly. */
-    const BufferParams slice_params = scale_buffer_params(denoised_slice_params,
-                                                          resolution_divider);
+    const BufferParams slice_params = slice_buffer_params(scaled_big_tile_params,
+                                                          render_work.resolution_divider,
+                                                          full_res_window_height,
+                                                          work_balance_infos_,
+                                                          i,
+                                                          scaled_overscan);
 
     path_trace_works_[i]->set_effective_buffer_params(
         scaled_big_tile_params, slice_params, denoised_big_tile_params, denoised_slice_params);

intern/cycles/integrator/path_trace_work_gpu.cpp
@@ -1225,6 +1225,10 @@ void PathTraceWorkGPU::cryptomatte_postproces()
 
 void PathTraceWorkGPU::denoise_volume_guiding_buffers()
 {
+  if (effective_buffer_params_.width == 0 || effective_buffer_params_.height == 0) {
+    return;
+  }
+
   const DeviceKernelArguments args(&buffers_->buffer.device_pointer,
                                    &effective_buffer_params_.full_x,
                                    &effective_buffer_params_.full_y,