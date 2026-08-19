fea4184c17f0af51c5a5bff3c41457f3a278ab55
Richard Antalik <richardantalik@gmail.com>
2026-08-20 00:19:40 +0200

    Fix #161886: Blender stencil sometimes scales constrained
    Caused by uninitialized memory of `StencilControlData`.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/162816

--- source/blender/editors/sculpt_paint/paint_ops.cc
+++ source/blender/editors/sculpt_paint/paint_ops.cc
@@ -141,27 +141,28 @@ enum StencilTextureMode {
 };
 
 enum StencilConstraint {
+  STENCIL_CONSTRAINT_NONE = 0,
   STENCIL_CONSTRAINT_X = 1,
   STENCIL_CONSTRAINT_Y = 2,
 };
 
 struct StencilControlData {
-  float init_mouse[2];
-  float init_spos[2];
-  float init_sdim[2];
-  float init_rot;
-  float init_angle;
-  float lenorig;
-  float area_size[2];
-  StencilControlMode mode;
-  StencilConstraint constrain_mode;
+  float init_mouse[2] = {0};
+  float init_spos[2] = {0};
+  float init_sdim[2] = {0};
+  float init_rot = 0.0f;
+  float init_angle = 0.0f;
+  float lenorig = 0.0f;
+  float area_size[2] = {0};
+  StencilControlMode mode = STENCIL_TRANSLATE;
+  StencilConstraint constrain_mode = STENCIL_CONSTRAINT_NONE;
   /** We are tweaking mask or color stencil. */
-  int mask;
-  Brush *br;
-  float *dim_target;
-  float *rot_target;
-  float *pos_target;
-  short launch_event;
+  int mask = 0;
+  Brush *br = nullptr;
+  float *dim_target = nullptr;
+  float *rot_target = nullptr;
+  float *pos_target = nullptr;
+  short launch_event = 0;
 };
 
 static void stencil_set_target(StencilControlData *scd)
@@ -216,7 +217,7 @@ static wmOperatorStatus stencil_control_invoke(bContext *C, wmOperator *op, cons
     }
   }
 
-  scd = MEM_new_uninitialized<StencilControlData>(__func__);
+  scd = MEM_new<StencilControlData>(__func__);
   scd->mask = mask;
   scd->br = br;
 
@@ -330,7 +331,7 @@ static wmOperatorStatus stencil_control_modal(bContext *C, wmOperator *op, const
       if (event->val == KM_PRESS) {
 
         if (scd->constrain_mode == STENCIL_CONSTRAINT_X) {
-          scd->constrain_mode = StencilConstraint(0);
+          scd->constrain_mode = STENCIL_CONSTRAINT_NONE;
         }
         else {
           scd->constrain_mode = STENCIL_CONSTRAINT_X;
@@ -342,7 +343,7 @@ static wmOperatorStatus stencil_control_modal(bContext *C, wmOperator *op, const
     case EVT_YKEY:
       if (event->val == KM_PRESS) {
         if (scd->constrain_mode == STENCIL_CONSTRAINT_Y) {
-          scd->constrain_mode = StencilConstraint(0);
+          scd->constrain_mode = STENCIL_CONSTRAINT_NONE;
         }
         else {
           scd->constrain_mode = STENCIL_CONSTRAINT_Y;
