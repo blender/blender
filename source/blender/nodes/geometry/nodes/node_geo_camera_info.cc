blender/blender

commit ed8344f4766561f9c5aea80be7108579b8598037

Nodes: Show error in `Camera Info` if given object is not a camera
A similar thing is already done by `Bone Info` if the given object is
not an armature. This patch implements the warning for `Camera Info` in
both Geometry and Compositor nodes.

Pull Request: https://projects.blender.org/blender/blender/pulls/162428

source/blender/nodes/geometry/nodes/node_geo_camera_info.cc
@@ -102,6 +102,7 @@ static void node_geo_exec(GeoNodeExecParams params)
   const Object *camera_obj = params.extract_input<Object *>("Camera"_ustr);
 
   if (!camera_obj || camera_obj->type != OB_CAMERA) {
+    params.error_message_add(NodeWarningType::Error, TIP_("Object is not a camera"));
     params.set_default_remaining_outputs();
     return;
   }
@@ -139,6 +140,7 @@ class CameraInfoOperation : public NodeOperation {
   {
     const Object *camera_object = this->get_input("Camera").get_single_value<Object *>();
     if (!camera_object || camera_object->type != OB_CAMERA) {
+      this->add_warning(NodeWarningType::Error, TIP_("Object is not a camera"));
       this->allocate_default_remaining_outputs();
       return;
     }

commit 90b493e35d801fdd4e0cdcd84f4b9970fabac84f

Fix: Nodes: Camera Info reports an error when no object is assigned
This issue was introduced in ed8344f4766, as the type check was placed
in the same level as the null check, leading to the report being
triggered if there was no object assigned to the node.

This patch splits the conditions so that having no object leads to an
early return, and only if the object type is invalid will the error be
raised.

Resolves llm-test/llm-reports#1550.

Pull Request: https://projects.blender.org/blender/blender/pulls/162743

source/blender/nodes/geometry/nodes/node_geo_camera_info.cc
@@ -101,7 +101,12 @@ static void node_geo_exec(GeoNodeExecParams params)
 
   const Object *camera_obj = params.extract_input<Object *>("Camera"_ustr);
 
-  if (!camera_obj || camera_obj->type != OB_CAMERA) {
+  if (!camera_obj) {
+    params.set_default_remaining_outputs();
+    return;
+  }
+
+  if (camera_obj->type != OB_CAMERA) {
     params.error_message_add(NodeWarningType::Error, TIP_("Object is not a camera"));
     params.set_default_remaining_outputs();
     return;
@@ -139,7 +144,12 @@ class CameraInfoOperation : public NodeOperation {
   void execute() override
   {
     const Object *camera_object = this->get_input("Camera").get_single_value<Object *>();
-    if (!camera_object || camera_object->type != OB_CAMERA) {
+    if (!camera_object) {
+      this->allocate_default_remaining_outputs();
+      return;
+    }
+
+    if (camera_object->type != OB_CAMERA) {
       this->add_warning(NodeWarningType::Error, TIP_("Object is not a camera"));
       this->allocate_default_remaining_outputs();
       return;