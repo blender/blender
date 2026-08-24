6f8a674864e282ba33fc371c58820618dbefd744
Jonas Holzman <jonas@blender.org>
2026-08-24 19:14:59 +0200

    Cleanup: GHOST: Misc formatting changes
    Small nitpicks found while writing !163060.

--- intern/ghost/intern/GHOST_ISystem.cc
+++ intern/ghost/intern/GHOST_ISystem.cc
@@ -43,7 +43,6 @@ bool GHOST_ISystem::use_window_frame_ = true;
 
 GHOST_TSuccess GHOST_ISystem::createSystem(bool verbose, [[maybe_unused]] bool background)
 {
-
   /* When GHOST fails to start, report the back-ends that were attempted.
    * A Verbose argument could be supported in printing isn't always desired. */
   struct GHOST_BackendInfo {
