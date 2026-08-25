blender/blender

commit 7d18836b8bc1d948a9be1bb7ea5e269a8d00960a

Fix: bad pointer cast in rna_Node_view_layer_itemf()
`rna_Node_view_layer_itemf()` iterates view layers
(`scene->view_layers`), which are of type `ViewLayer *`. It cast those
to a `RenderLayer *`, to feed into `renderresult_layers_add_enum()`.

This didn't cause any trouble, as that function only accesses a few
fields of the structs, and both structs have those at the same offset.
Of course that's pure chance, and not by design, and so I've corrected
the code.

No functional change, just correct code.

Pull Request: https://projects.blender.org/blender/blender/pulls/162672

source/blender/makesrna/intern/rna_nodetree.cc
@@ -3314,32 +3314,6 @@ static void rna_Node_image_layer_view_update(Main *bmain, Scene *scene, PointerR
   rna_Node_update(bmain, scene, ptr);
 }
 
-static const EnumPropertyItem *renderresult_layers_add_enum(RenderLayer *rl)
-{
-  EnumPropertyItem *item = nullptr;
-  EnumPropertyItem tmp = {0};
-  int i = 0, totitem = 0;
-
-  while (rl) {
-    tmp.identifier = rl->name;
-    /* Little trick: using space char instead empty string
-     * makes the item selectable in the drop-down. */
-    if (rl->name[0] == '\0') {
-      tmp.name = " ";
-    }
-    else {
-      tmp.name = rl->name;
-    }
-    tmp.value = i++;
-    RNA_enum_item_add(&item, &totitem, &tmp);
-    rl = rl->next;
-  }
-
-  RNA_enum_item_end(&item, &totitem);
-
-  return item;
-}
-
 static const EnumPropertyItem *rna_ShaderNodeMix_data_type_itemf(bContext * /*C*/,
                                                                  PointerRNA *ptr,
                                                                  PropertyRNA * /*prop*/,
@@ -3382,8 +3356,22 @@ static const EnumPropertyItem *rna_Node_image_layer_itemf(bContext * /*C*/,
     return rna_enum_dummy_NULL_items;
   }
 
-  rl = static_cast<RenderLayer *>(ima->rr->layers.first);
-  item = renderresult_layers_add_enum(rl);
+  const auto &item_from_render_layer = [&](const RenderLayer &rl,
+                                           const int index) -> EnumPropertyItem {
+    EnumPropertyItem tmp;
+    tmp.identifier = rl.name;
+    /* Little trick: using space char instead empty string
+     * makes the item selectable in the drop-down. */
+    if (rl.name[0] == '\0') {
+      tmp.name = " ";
+    }
+    else {
+      tmp.name = rl.name;
+    }
+    tmp.value = index;
+    return tmp;
+  };
+  item = rna_enum_property_items_from_listbase(ima->rr->layers, item_from_render_layer);
 
   *r_free = true;
 
@@ -3492,15 +3480,28 @@ static const EnumPropertyItem *rna_Node_view_layer_itemf(bContext * /*C*/,
   bNode *node = ptr->data_as<bNode>();
   Scene *sce = reinterpret_cast<Scene *>(node->id);
   const EnumPropertyItem *item = nullptr;
-  RenderLayer *rl;
 
   if (sce == nullptr) {
     *r_free = false;
     return rna_enum_dummy_NULL_items;
   }
 
-  rl = static_cast<RenderLayer *>(sce->view_layers.first);
-  item = renderresult_layers_add_enum(rl);
+  const auto &item_from_view_layer = [&](const ViewLayer &vl,
+                                         const int index) -> EnumPropertyItem {
+    EnumPropertyItem tmp;
+    tmp.identifier = vl.name;
+    /* Little trick: using space char instead empty string
+     * makes the item selectable in the drop-down. */
+    if (vl.name[0] == '\0') {
+      tmp.name = " ";
+    }
+    else {
+      tmp.name = vl.name;
+    }
+    tmp.value = index;
+    return tmp;
+  };
+  item = rna_enum_property_items_from_listbase(sce->view_layers, item_from_view_layer);
 
   *r_free = true;
 

source/blender/makesrna/intern/rna_internal.hh
@@ -643,6 +643,26 @@ void rna_iterator_array_end(CollectionPropertyIterator *iter);
 PointerRNA rna_array_lookup_int(
     PointerRNA *ptr, StructRNA *type, void *data, size_t itemsize, int64_t length, int64_t index);
 
+/* Construct a dynamic list of enum property items using a function
+ *   EnumPropertyItem CreateItemFn(const T &list_item, int index) */
+template<typename T, typename CreateItemFn>
+const EnumPropertyItem *rna_enum_property_items_from_listbase(const ListBaseT<T> &listbase,
+                                                              CreateItemFn create_item_fn)
+{
+  EnumPropertyItem *item = nullptr;
+  int i = 0, totitem = 0;
+
+  const T *list_item = static_cast<const T *>(listbase.first);
+  while (list_item) {
+    const EnumPropertyItem tmp = create_item_fn(*list_item, i);
+    RNA_enum_item_add(&item, &totitem, &tmp);
+    list_item = list_item->next;
+    ++i;
+  }
+  RNA_enum_item_end(&item, &totitem);
+  return item;
+}
+
 /* Duplicated code since we can't link in `blenlib`. */
 
 #ifndef RNA_RUNTIME

commit bda7f5a675c5d8d23de0969ae5a2f86ad576adf0

Fix #162823: Inconsistent warning beside viewlayers in Render layer node
Caused by 7d18836b8bc1d948a9be1bb7ea5e269a8d00960a. Struct object
is uninitialized, which throws random icon id during every redraw. init
with 0 to avoid the varying icons.

Pull Request: https://projects.blender.org/blender/blender/pulls/162824

source/blender/makesrna/intern/rna_nodetree.cc
@@ -3368,7 +3368,7 @@ static const EnumPropertyItem *rna_Node_image_layer_itemf(bContext * /*C*/,
 
   const auto &item_from_render_layer = [&](const RenderLayer &rl,
                                            const int index) -> EnumPropertyItem {
-    EnumPropertyItem tmp;
+    EnumPropertyItem tmp = {0};
     tmp.identifier = rl.name;
     /* Little trick: using space char instead empty string
      * makes the item selectable in the drop-down. */
@@ -3498,7 +3498,7 @@ static const EnumPropertyItem *rna_Node_view_layer_itemf(bContext * /*C*/,
 
   const auto &item_from_view_layer = [&](const ViewLayer &vl,
                                          const int index) -> EnumPropertyItem {
-    EnumPropertyItem tmp;
+    EnumPropertyItem tmp = {0};
     tmp.identifier = vl.name;
     /* Little trick: using space char instead empty string
      * makes the item selectable in the drop-down. */