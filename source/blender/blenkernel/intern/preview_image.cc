blender/blender

commit 3c969d9bc282b292bc7aef0b87c7bf14cb288c0e

Fix #154063: Icon preview render and undo crashes
Remove the global queue for preview render restarts, and instead add a
PreviewImage tag. The global queue could get out of sync with the data in
main.

Additionally, file system previews would not be restarted correctly due to
the BKE_previewimg_is_rendering early out check in ED_preview_icon_job and
ED_preview_icon_render.

Pull Request: https://projects.blender.org/blender/blender/pulls/154258

source/blender/blenkernel/intern/preview_image.cc
@@ -472,6 +472,7 @@ void BKE_previewimg_render_start(PreviewImage *prv, const int size, const bool u
   BLI_assert(BLI_thread_is_main());
 
   prv->flag[size] |= PRV_RENDERING;
+  prv->runtime->tag[size] &= ~PRV_TAG_RESTART_RENDERING;
 
   /* When rendering as a job in another thread, tag so that main thread will not
    * free it and defer deletion to the job. */
@@ -517,6 +518,23 @@ void BKE_previewimg_render_end(PreviewImage *prv,
   }
 }
 
+bool BKE_previewimg_render_restart(PreviewImage *prv, const int size)
+{
+  BLI_assert(BLI_thread_is_main());
+
+  if (prv->flag[size] & PRV_USER_EDITED) {
+    /* Don't modify custom previews. */
+    return false;
+  }
+
+  if (prv->runtime->tag[size] & PRV_TAG_RESTART_RENDERING) {
+    prv->runtime->tag[size] &= ~PRV_TAG_RESTART_RENDERING;
+    return true;
+  }
+
+  return false;
+}
+
 bool BKE_previewimg_is_rendering(const PreviewImage *prv, const int size)
 {
   return (prv->flag[size] & PRV_RENDERING);
@@ -524,7 +542,8 @@ bool BKE_previewimg_is_rendering(const PreviewImage *prv, const int size)
 
 bool BKE_previewimg_is_finished(const PreviewImage *prv, const int size)
 {
-  return !(prv->flag[size] & PRV_RENDERING);
+  return !(prv->flag[size] & PRV_RENDERING) &&
+         !(prv->runtime->tag[size] & PRV_TAG_RESTART_RENDERING);
 }
 
 bool BKE_previewimg_is_invalid(const PreviewImage *prv, const int size)
@@ -566,11 +585,14 @@ void BKE_previewimg_blend_read(BlendDataReader *reader, PreviewImage *prv)
       BLO_read_uint32_array(reader, prv->w[i] * prv->h[i], &prv->rect[i]);
     }
 
-    /* PRV_RENDERING is a runtime only flag currently, but don't mess with it on undo! It gets
-     * special handling in #memfile_undosys_restart_unfinished_id_previews() then. */
-    if (!BLO_read_data_is_undo(reader)) {
-      prv->flag[i] &= ~PRV_RENDERING;
+    /* PRV_RENDERING is a runtime only flag currently, but for undo indicates that we need
+     * to restart prevew renders. See ED_preview_restart_work. */
+    if (BLO_read_data_is_undo(reader)) {
+      if ((prv->flag[i] & PRV_RENDERING) && !(prv->flag[i] & PRV_USER_EDITED)) {
+        prv->runtime->tag[i] |= PRV_TAG_RESTART_RENDERING;
+      }
     }
+    prv->flag[i] &= ~PRV_RENDERING;
   }
 }
 

source/blender/editors/render/render_preview.cc
@@ -2409,45 +2409,25 @@ void ED_preview_online_download_finished(wmWindowManager *wm,
   PreviewLoadJob::on_download_completed(wm, preview_full_filepath);
 }
 
-struct PreviewRestartQueueEntry {
-  PreviewRestartQueueEntry *next, *prev;
-
-  enum eIconSizes size;
-  ID *id;
-};
-
-static ListBaseT<PreviewRestartQueueEntry> G_restart_previews_queue;
-
-void ED_preview_restart_queue_free()
+void ED_preview_restart_work(const bContext *C)
 {
-  BLI_freelistN(&G_restart_previews_queue);
-}
+  Main *bmain = CTX_data_main(C);
 
-void ED_preview_restart_queue_add(ID *id, enum eIconSizes size)
-{
-  PreviewRestartQueueEntry *queue_entry = MEM_new_zeroed<PreviewRestartQueueEntry>(__func__);
-  queue_entry->size = size;
-  queue_entry->id = id;
-  BLI_addtail(&G_restart_previews_queue, queue_entry);
-}
-
-void ED_preview_restart_queue_work(const bContext *C)
-{
-  for (PreviewRestartQueueEntry &queue_entry : G_restart_previews_queue.items_mutable()) {
-    PreviewImage *preview = BKE_previewimg_id_get(queue_entry.id);
+  ID *id = nullptr;
+  FOREACH_MAIN_ID_BEGIN (bmain, id) {
+    PreviewImage *preview = BKE_previewimg_id_get(id);
     if (!preview) {
       continue;
     }
-    if (preview->flag[queue_entry.size] & PRV_USER_EDITED) {
-      /* Don't touch custom previews. */
-      continue;
+
+    for (int i = 0; i < NUM_ICON_SIZES; i++) {
+      if (BKE_previewimg_render_restart(preview, i)) {
+        BKE_previewimg_clear_single(preview, eIconSizes(i));
+        ui::icon_render_id(C, nullptr, id, eIconSizes(i), true);
+      }
     }
-
-    BKE_previewimg_clear_single(preview, queue_entry.size);
-    ui::icon_render_id(C, nullptr, queue_entry.id, queue_entry.size, true);
-
-    BLI_freelinkN(&G_restart_previews_queue, &queue_entry);
   }
+  FOREACH_MAIN_ID_END;
 }
 
 /** \} */

source/blender/editors/undo/memfile_undo.cc
@@ -137,32 +137,6 @@ static int memfile_undosys_step_id_reused_cb(LibraryIDLinkCallbackData *cb_data)
   return IDWALK_RET_NOP;
 }
 
-/**
- * ID previews may be generated in a parallel job. So whatever operation generates the preview
- * likely does the undo push before the preview is actually done and stored in the ID. Hence they
- * get some extra treatment here:
- * When undoing back to the moment the preview generation was triggered, this function schedules
- * the preview for regeneration.
- */
-static void memfile_undosys_unfinished_id_previews_restart(ID *id)
-{
-  PreviewImage *preview = BKE_previewimg_id_get(id);
-  if (!preview) {
-    return;
-  }
-
-  for (int i = 0; i < NUM_ICON_SIZES; i++) {
-    if (preview->flag[i] & PRV_USER_EDITED) {
-      /* Don't modify custom previews. */
-      continue;
-    }
-
-    if (BKE_previewimg_is_rendering(preview, i)) {
-      ED_preview_restart_queue_add(id, eIconSizes(i));
-    }
-  }
-}
-
 static void memfile_undosys_step_decode(
     bContext *C, Main *bmain, UndoStep *us_p, const eUndoStepDir undo_direction, bool /*is_final*/)
 {
@@ -207,7 +181,7 @@ static void memfile_undosys_step_decode(
 
   ED_editors_exit(bmain, false);
   /* Ensure there's no preview job running. Unfinished previews will be scheduled for regeneration
-   * via #memfile_undosys_unfinished_id_previews_restart(). */
+   * via #PRV_TAG_RESTART_RENDERING in BKE_previewimg_blend_read. */
   ED_preview_kill_jobs(CTX_wm_manager(C), bmain);
 
   MemFileUndoStep *us = reinterpret_cast<MemFileUndoStep *>(us_p);
@@ -300,9 +274,6 @@ static void memfile_undosys_step_decode(
           }
         }
       }
-
-      /* Restart preview generation if the undo state was generating previews. */
-      memfile_undosys_unfinished_id_previews_restart(id);
     }
     FOREACH_MAIN_ID_END;
 
@@ -325,14 +296,6 @@ static void memfile_undosys_step_decode(
           scene->master_collection->id.recalc_after_undo_push = 0;
         }
       }
-    }
-    FOREACH_MAIN_ID_END;
-  }
-  else {
-    ID *id = nullptr;
-    FOREACH_MAIN_ID_BEGIN (bmain, id) {
-      /* Restart preview generation if the undo state was generating previews. */
-      memfile_undosys_unfinished_id_previews_restart(id);
     }
     FOREACH_MAIN_ID_END;
   }

source/blender/blenkernel/BKE_preview_image.hh
@@ -157,6 +157,7 @@ enum PreviewImageRenderEndStatus {
 
 void BKE_previewimg_render_start(PreviewImage *prv, int size, bool using_job);
 void BKE_previewimg_render_end(PreviewImage *prv, int size, PreviewImageRenderEndStatus status);
+bool BKE_previewimg_render_restart(PreviewImage *prv, int size);
 bool BKE_previewimg_is_rendering(const PreviewImage *prv, int size);
 bool BKE_previewimg_is_finished(const PreviewImage *prv, int size);
 

source/blender/editors/include/ED_render.hh
@@ -113,9 +113,15 @@ void ED_preview_icon_render(
 void ED_preview_icon_job(
     const bContext *C, PreviewImage *prv_img, ID *id, enum eIconSizes icon_size, bool delay);
 
-void ED_preview_restart_queue_free();
-void ED_preview_restart_queue_add(ID *id, enum eIconSizes size);
-void ED_preview_restart_queue_work(const bContext *C);
+/**
+ * ID previews may be generated in a parallel job. The operation that generates the preview
+ * likely does an undo push before the preview is actually done and stored in the ID. The
+ * restart system exists to make sure previews remain up to date.
+ *
+ * When undoing back to the moment the preview generation was triggered, this function
+ * schedules the preview for regeneration.
+ */
+void ED_preview_restart_work(const bContext *C);
 
 void ED_preview_kill_jobs(wmWindowManager *wm, Main *bmain);
 void ED_preview_kill_jobs_for_id(wmWindowManager *wm, const ID *id);

source/blender/makesdna/DNA_ID.h
@@ -621,6 +621,8 @@ enum {
   PRV_TAG_DEFERRED_DELETE = (1 << 2),
   /** This deferred preview could not be loaded (e.g. not found on disk). */
   PRV_TAG_DEFERRED_INVALID = (1 << 3),
+  /* Rendering was interrupted and needs restart. */
+  PRV_TAG_RESTART_RENDERING = (1 << 4),
 };
 
 /**

source/blender/windowmanager/intern/wm_event_system.cc
@@ -631,7 +631,7 @@ void wm_event_do_notifiers(bContext *C)
           WM_window_title_refresh(wm, &win);
         }
         else if (note->data == ND_UNDO) {
-          ED_preview_restart_queue_work(C);
+          ED_preview_restart_work(C);
         }
       }
 

source/blender/windowmanager/intern/wm_files.cc
@@ -668,8 +668,6 @@ static void wm_file_read_pre(bool use_data, bool /*use_userdef*/)
   /* Always do this as both startup and preferences may have loaded in many font's
    * at a different zoom level to the file being loaded. */
   ui::view2d_zoom_cache_reset();
-
-  ED_preview_restart_queue_free();
 }
 
 /**

commit ba4954a828e5d78686327dbf20e79913fe91ac69

Fix #163010: Preview icons stuck loading after undo on unchanged ID
It was not fully accounting for the case where the ID whose preview
render was cancelled was not modified by the undo step. Tag such cases
for restart as well.

Issue introduced in 3c969d9bc2.

Pull Request: https://projects.blender.org/blender/blender/pulls/163106

source/blender/blenkernel/intern/preview_image.cc
@@ -518,9 +518,15 @@ void BKE_previewimg_render_end(PreviewImage *prv,
     prv->runtime->tag[size] |= PRV_TAG_DEFERRED_INVALID;
   }
 
-  /* When job is cancelled for e.g. undo, PRV_RENDERING remains so that
-   * it can resume when going back to that undo step. */
-  if (status != PRV_RENDER_STATUS_CANCELLED) {
+  /* When job is cancelled for e.g. undo, PRV_RENDERING remains so that it can resume
+   * when going back to that undo step. Also tag for restart in the current state, if
+   * the ID itself was not modified in the undo step but still had its preview cancelled. */
+  if (status == PRV_RENDER_STATUS_CANCELLED) {
+    if (!(prv->flag[size] & PRV_USER_EDITED)) {
+      prv->runtime->tag[size] |= PRV_TAG_RESTART_RENDERING;
+    }
+  }
+  else {
     prv->flag[size] &= ~PRV_RENDERING;
   }
 

source/blender/editors/render/render_preview.cc
@@ -155,6 +155,9 @@ struct IconPreview {
   /* May be nullptr, is used for rendering IDs that require some other object for it to be applied
    * on before the ID can be represented as an image, for example when rendering an Action. */
   Object *active_object;
+
+  /* Set by the job when it was stopped before finishing, e.g. by undo. */
+  bool cancelled;
 };
 
 /** \} */
@@ -1639,6 +1642,8 @@ static void icon_preview_startjob_all_sizes(void *customdata, wmJobWorkerStatus 
     }
     other_id_types_preview_render(prv, ip, icon_size, pr_method, worker_status);
   }
+
+  ip->cancelled = worker_status->stop;
 }
 
 static void icon_preview_endjob(void *customdata, const PreviewImageRenderEndStatus status)
@@ -1654,13 +1659,19 @@ static void icon_preview_endjob(void *customdata, const PreviewImageRenderEndSta
       }
     }
 
+    if (status == PRV_RENDER_STATUS_CANCELLED) {
+      ip->bmain->need_preview_render_restart = true;
+    }
+
     ip->owner = nullptr;
   }
 }
 
 static void icon_preview_endjob(void *customdata)
 {
-  icon_preview_endjob(customdata, PRV_RENDER_STATUS_FINISHED);
+  const IconPreview *ip = static_cast<IconPreview *>(customdata);
+  icon_preview_endjob(customdata,
+                      ip->cancelled ? PRV_RENDER_STATUS_CANCELLED : PRV_RENDER_STATUS_FINISHED);
 }
 
 /**

source/blender/editors/undo/memfile_undo.cc
@@ -185,6 +185,7 @@ static void memfile_undosys_step_decode(
   /* Ensure there's no preview job running. Unfinished previews will be scheduled for regeneration
    * via #PRV_TAG_RESTART_RENDERING in BKE_previewimg_blend_read. */
   ED_preview_kill_jobs(CTX_wm_manager(C), bmain);
+  const bool need_preview_render_restart = bmain->need_preview_render_restart;
 
   MemFileUndoStep *us = reinterpret_cast<MemFileUndoStep *>(us_p);
   BKE_memfile_undo_decode(us->data, undo_direction, use_old_bmain_data, C);
@@ -205,6 +206,10 @@ static void memfile_undosys_step_decode(
   /* bmain has been freed. */
   bmain = CTX_data_main(C);
   ED_editors_init_for_undo(bmain);
+
+  /* Cancelled preview jobs set this on the old bmain, carry it over to
+   * restart previews that were interrupted. */
+  bmain->need_preview_render_restart |= need_preview_render_restart;
 
   if (use_old_bmain_data) {
     /* Restore previous depsgraphs into current bmain. */