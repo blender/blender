d1bda88fbc9633191e62f3969356e6ee1361af6b
Clément Foucault <foucault.clem@gmail.com>
2026-08-27 01:33:33 +0200

    GPU: Shader: Port area & window border shaders to BSL
    Rel #151442
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/163177

--- source/blender/gpu/shaders/bsl_shader_linting.cc
+++ source/blender/gpu/shaders/bsl_shader_linting.cc
@@ -9,6 +9,7 @@
 #include "gpu_shader_2D_nodelink.bsl.hh"       /* IWYU pragma: export */
 #include "gpu_shader_2D_update_mipmaps.bsl.hh" /* IWYU pragma: export */
 #include "gpu_shader_2D_widget_base.bsl.hh"    /* IWYU pragma: export */
+#include "gpu_shader_area_borders.bsl.hh"      /* IWYU pragma: export */
 #include "gpu_shader_colorspace.bsl.hh"        /* IWYU pragma: export */
 #include "gpu_shader_icon.bsl.hh"              /* IWYU pragma: export */
 #include "gpu_shader_image.bsl.hh"             /* IWYU pragma: export */
