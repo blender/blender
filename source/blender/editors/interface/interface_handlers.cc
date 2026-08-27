blender/blender

commit 728c8624b362e5944cc131965dd4acaa55778285

UI: Fine adjusting values with arrows on number button
When editing number buttons with arrow keys, allow precision editing
similar to drag editing of button. This supports step_size increment
with shift, ctrl and ctrl+shift key.
There's little issue when incrementing few float properties of Rotation
type. `step_size` is 100 for them which increments value by large factor
during ctrl click.

See PR description for videos

Pull Request: https://projects.blender.org/blender/blender/pulls/161290

source/blender/editors/interface/interface_handlers.cc
@@ -6299,7 +6299,7 @@ static int do_but_NUM(
       click = 1;
     }
     else if (event->val == KM_PRESS) {
-      if (ELEM(event->type, LEFTMOUSE, EVT_PADENTER, EVT_RETKEY) && (event->modifier & KM_CTRL)) {
+      if (ELEM(event->type, EVT_PADENTER, EVT_RETKEY) && (event->modifier & KM_CTRL)) {
         button_activate_state(C, but, BUTTON_STATE_TEXT_EDITING);
         retval = WM_UI_HANDLER_BREAK;
       }
@@ -6410,7 +6410,8 @@ static int do_but_NUM(
       if (but->drawflag & (BUT_HOVER_LEFT | BUT_HOVER_RIGHT)) {
         button_activate_state(C, but, BUTTON_STATE_NUM_EDITING);
 
-        const int value_step = int(number_but->step_size);
+        const int step = number_but->step_size * (event->modifier & KM_SHIFT ? 0.1 : 1);
+        const int value_step = std::max(1, step);
         BLI_assert(value_step > 0);
         const int softmin = round_fl_to_int_clamp(but->softmin);
         const int softmax = round_fl_to_int_clamp(but->softmax);
@@ -6449,13 +6450,20 @@ static int do_but_NUM(
         else {
           value_step = double(number_but->step_size * UI_PRECISION_FLOAT_SCALE);
         }
+
+        const eSnapType snap = event_to_snap(event);
+        value_step = (snap == SNAP_OFF) ? value_step : number_but->step_size;
+        if (event->modifier & KM_SHIFT) {
+          value_step *= 0.1;
+        }
+
         BLI_assert(value_step > 0.0f);
         const double value_test =
             (but->drawflag & BUT_HOVER_LEFT) ?
                 double(max_ff(but->softmin, float(data->value - value_step))) :
                 double(min_ff(but->softmax, float(data->value + value_step)));
         if (value_test != data->value) {
-          data->value = value_test;
+          data->value = numedit_apply_snapf(but, value_test, but->softmin, but->softmax, snap);
         }
         else {
           data->cancel = true;

commit 7c1b761739972a7a874b7beb3864e886117e5187

Fix #163135: Ctrl + Scrollwheel not working on certain number fields
Caused by 728c8624b362e5944cc131965dd4acaa55778285
Use snapping when arrows are pressed with mouse click event

Pull Request: https://projects.blender.org/blender/blender/pulls/163136

source/blender/editors/interface/interface_handlers.cc
@@ -6439,7 +6439,7 @@ static int do_but_NUM(
           value_step = double(number_but->step_size * UI_PRECISION_FLOAT_SCALE);
         }
 
-        const eSnapType snap = event_to_snap(event);
+        const eSnapType snap = ISMOUSE_BUTTON(event->type) ? event_to_snap(event) : SNAP_OFF;
         value_step = (snap == SNAP_OFF) ? value_step : number_but->step_size;
         if (event->modifier & KM_SHIFT) {
           value_step *= 0.1;