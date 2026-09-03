9cb7c2718da9c1b4931ced0bceb65a25e9f08d13
Campbell Barton <campbell@blender.org>
2026-09-03 19:50:52 +1000

    PyAPI: resolve the directory in bpy.path.is_autoexec
    Callers had to split the directory from a blend-file path before checking
    it against the excluded paths. Add "canonicalize" & "strip_filename" to
    BKE_autoexec_match & bpy.path.is_autoexec so this is done in one place.
    
    Depending on the source of the filepath, we can't rely on it
    being normalized, so canonicalize should be used when the path
    comes from external sources such as command line arguments
    or dropped paths.
    
    Ref !163715

--- source/blender/blenkernel/BKE_autoexec.hh
+++ source/blender/blenkernel/BKE_autoexec.hh
@@ -11,14 +11,16 @@ namespace blender {
 
 /**
  * \param path: The path to check against.
+ * \param canonicalize: Resolve `path` first, unless the caller knows it's resolved.
+ * \param strip_filename: Strip the file name from `path`, otherwise it's a directory already.
  * \return Success
  */
-bool BKE_autoexec_match(const char *path);
+bool BKE_autoexec_match(const char *path, bool canonicalize, bool strip_filename);
 
 /**
  * Version of #BKE_autoexec_match which may be called
  * when auto-execution isn't enabled, used by the Python API.
  */
-bool BKE_autoexec_match_unchecked(const char *path);
+bool BKE_autoexec_match_unchecked(const char *path, bool canonicalize, bool strip_filename);
 
 }  // namespace blender
