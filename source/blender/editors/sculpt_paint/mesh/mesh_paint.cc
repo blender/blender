349d0dd79f7927c7329af01c9fe83188c0a3dd40
Sean Kim <SeanCTKim@protonmail.com>
2026-09-04 15:12:18 -0700

    Fix: Overlay texture leak in experimental paint mode

--- source/blender/editors/sculpt_paint/mesh/mesh_paint.cc
+++ source/blender/editors/sculpt_paint/mesh/mesh_paint.cc
@@ -157,6 +157,9 @@ void mode_exit_generic(Scene &scene, Object &ob, const eObjectMode mode_flag)
   else if (mode_flag == OB_MODE_SCULPT) {
     paint = BKE_paint_get_active_from_paintmode(&scene, PaintMode::Sculpt);
   }
+  else if (mode_flag == OB_MODE_TEXTURE_PAINT) {
+    paint = BKE_paint_get_active_from_paintmode(&scene, PaintMode::Texture3D);
+  }
   else {
     BLI_assert(0);
   }
