blender/blender

commit 1feb1674bcca5b1fbec98fcc6755a536f4a81b72

Texture Paint: Migrate 3D draw brush out of Sculpt Mode
This commit moves the experimental 3D Texture Paint brush implementation
out of Sculpt Mode and into Texture Paint mode.

Instead of overriding the "Paint" brush in Sculpt Mode, it now
corresponds to the "Draw" brush in Texture Paint.

This is achieved by extending the existing `PAINT_OT_image_paint`
operator to use a different `PaintStroke` implementation if a supported
brush is being used with the experimental flag toggled.

Part of #162290

Pull Request: https://projects.blender.org/blender/blender/pulls/163220

source/blender/editors/sculpt_paint/mesh/paint_image.cc
@@ -71,6 +71,8 @@
 
 #include "IMB_colormanagement.hh"
 
+#include "mesh_paint.hh"
+
 #include "../paint_intern.hh"
 
 namespace blender {
@@ -648,24 +650,26 @@ void ED_object_texture_paint_mode_enter_ex(Main &bmain,
     ED_space_image_sync(&bmain, ima, false);
   }
 
-  ob.mode |= OB_MODE_TEXTURE_PAINT;
+  if (USER_EXPERIMENTAL_TEST(&U, use_3d_texture_paint)) {
+    ed::sculpt_paint::mode_enter_generic(bmain, depsgraph, scene, ob, OB_MODE_TEXTURE_PAINT);
+  }
+  else {
+    ob.mode |= OB_MODE_TEXTURE_PAINT;
+    BKE_paint_init(&bmain, &scene, PaintMode::Texture3D);
+    BKE_paint_brushes_validate(&bmain, &imapaint.paint);
+    toggle_paint_cursor(scene, true);
 
-  BKE_paint_init(&bmain, &scene, PaintMode::Texture3D);
+    Mesh *mesh = BKE_mesh_from_object(&ob);
+    BLI_assert(mesh != nullptr);
+    DEG_id_tag_update(&mesh->id, ID_RECALC_SYNC_TO_EVAL);
 
-  BKE_paint_brushes_validate(&bmain, &imapaint.paint);
+    /* Ensure we have evaluated data for bounding box. */
+    BKE_scene_graph_evaluated_ensure(&depsgraph, &bmain);
 
-  toggle_paint_cursor(scene, true);
-
-  Mesh *mesh = BKE_mesh_from_object(&ob);
-  BLI_assert(mesh != nullptr);
-  DEG_id_tag_update(&mesh->id, ID_RECALC_SYNC_TO_EVAL);
-
-  /* Ensure we have evaluated data for bounding box. */
-  BKE_scene_graph_evaluated_ensure(&depsgraph, &bmain);
-
-  /* Set pivot to bounding box center. */
-  Object *ob_eval = DEG_get_evaluated(&depsgraph, &ob);
-  paint_init_pivot(ob_eval ? ob_eval : &ob, &scene, &imapaint.paint);
+    /* Set pivot to bounding box center. */
+    Object *ob_eval = DEG_get_evaluated(&depsgraph, &ob);
+    paint_init_pivot(ob_eval ? ob_eval : &ob, &scene, &imapaint.paint);
+  }
 
   WM_main_add_notifier(NC_SCENE | ND_MODE, &scene);
 }
@@ -682,9 +686,14 @@ void ED_object_texture_paint_mode_enter(bContext *C)
 
 void ED_object_texture_paint_mode_exit_ex(Main & /*bmain*/, Scene &scene, Object &ob)
 {
-  ob.mode &= ~OB_MODE_TEXTURE_PAINT;
+  if (USER_EXPERIMENTAL_TEST(&U, use_3d_texture_paint)) {
+    ed::sculpt_paint::mode_exit_generic(ob, OB_MODE_TEXTURE_PAINT);
+  }
+  else {
+    ob.mode &= ~OB_MODE_TEXTURE_PAINT;
 
-  toggle_paint_cursor(scene, false);
+    toggle_paint_cursor(scene, false);
+  }
 
   Mesh *mesh = BKE_mesh_from_object(&ob);
   BLI_assert(mesh != nullptr);

scripts/startup/bl_ui/properties_paint_common.py
@@ -602,7 +602,7 @@ class StrokePanel(BrushPanel):
             row = col.row(align=True)
             row.prop(brush, "spacing", text="Spacing")
 
-        if mode == 'SCULPT':
+        if mode == 'SCULPT' or (mode == 'PAINT_TEXTURE' and bpy.context.preferences.experimental.use_3d_texture_paint):
             col.row().prop(brush, "use_scene_spacing", text="Spacing Distance", expand=True)
 
         if mode in {'PAINT_TEXTURE', 'PAINT_2D', 'SCULPT'}:
@@ -757,6 +757,7 @@ class ShapePanel(BrushPanel):
         settings = self.paint_settings_from_active_tool(context)
         mode = self.get_brush_mode(context)
         brush = settings.brush
+        experimental_texture_paint_enabled = bpy.context.preferences.experimental.use_3d_texture_paint
 
         if brush is None:
             return
@@ -787,6 +788,22 @@ class ShapePanel(BrushPanel):
                 layout.prop(brush, "tip_roundness")
                 layout.prop(brush, "tip_scale_x")
                 layout.separator()
+        elif mode == 'PAINT_TEXTURE' and experimental_texture_paint_enabled:
+            # TODO: Update this once the "capabilities" block has been updated
+            row = layout.row(align=True)
+            row.prop(brush, "hardness", slider=True)
+            row.prop(brush, "use_hardness_pressure", text="")
+            if not self.is_popover:
+                UnifiedPaintPanel.prop_custom_pressure(
+                    layout,
+                    context,
+                    row,
+                    brush,
+                    pressure_name="use_hardness_pressure",
+                    curve_visibility_name="show_hardness_curve",
+                    custom_curve_name="curve_hardness",
+                )
+            layout.separator()
 
         layout.use_property_split = False
         col = layout.column(align=True)
@@ -808,6 +825,8 @@ class ShapePanel(BrushPanel):
 
         show_falloff_shape = False
         if mode in {'SCULPT', 'PAINT_VERTEX', 'PAINT_WEIGHT'} and brush.sculpt_brush_type != 'POSE':
+            show_falloff_shape = True
+        if mode == 'PAINT_TEXTURE' and experimental_texture_paint_enabled:
             show_falloff_shape = True
         if not show_falloff_shape and mode == 'SCULPT_CURVES' and context.space_data.type == 'PROPERTIES':
             show_falloff_shape = True
@@ -1231,6 +1250,8 @@ def brush_shared_settings(layout, context, brush, popover=False):
 
     # 3D and 2D Texture Paint #
     if mode in {'PAINT_TEXTURE', 'PAINT_2D'}:
+        if mode == 'PAINT_TEXTURE' and bpy.context.preferences.experimental.use_3d_texture_paint:
+            size_mode = True
         if not popover:
             blend_mode = brush.image_paint_capabilities.has_color
             size = brush.image_paint_capabilities.has_radius
@@ -1751,6 +1772,8 @@ def brush_mask_texture_settings(layout, brush):
 
 def brush_basic_texpaint_settings(layout, context, brush, *, compact=False):
     """Draw Tool Settings header for Vertex Paint and 2D and 3D Texture Paint modes."""
+
+    # TODO: This shared method is incorrect and unnecessary, remove this layer of abstraction
     capabilities = brush.image_paint_capabilities
 
     if capabilities.has_color:

scripts/startup/bl_ui/space_userpref.py
@@ -3048,7 +3048,7 @@ class USERPREF_PT_experimental_prototypes(ExperimentalPanel, Panel):
             context.preferences,
             (
                 ({"property": "use_new_curves_tools"}, ("blender/blender/issues/68981", "#68981")),
-                ({"property": "use_sculpt_texture_paint"}, ("blender/blender/issues/96225", "#96225")),
+                ({"property": "use_3d_texture_paint"}, ("blender/blender/issues/156410", "#156410")),
             ),
         )
 

scripts/startup/bl_ui/space_view3d.py
@@ -187,6 +187,8 @@ class VIEW3D_HT_tool_header(Header):
                 row.popover(panel="VIEW3D_PT_sculpt_symmetry_for_topbar", text="")
             elif mode_string == 'PAINT_VERTEX':
                 row.popover(panel="VIEW3D_PT_tools_vertexpaint_symmetry_for_topbar", text="")
+            elif mode_string == 'PAINT_TEXTURE' and bpy.context.preferences.experimental.use_3d_texture_paint:
+                row.popover(panel="VIEW3D_PT_tools_imagepaint_symmetry_for_topbar", text="")
         elif mode_string == 'SCULPT_CURVES':
             ob = context.object
             _row, sub = row_for_mirror()
@@ -328,6 +330,7 @@ class _draw_tool_settings_context_mode:
             return False
 
         paint = context.tool_settings.image_paint
+        ups = paint.unified_paint_settings
         brush = paint.brush
 
         BrushAssetShelf.draw_popup_selector(layout, context, brush)
@@ -335,7 +338,44 @@ class _draw_tool_settings_context_mode:
         if brush is None:
             return False
 
-        brush_basic_texpaint_settings(layout, context, brush, compact=True)
+        if context.preferences.experimental.use_3d_texture_paint:
+            size = "size"
+            size_owner = ups if brush.use_unified_size else brush
+            if size_owner.use_locked_size == 'SCENE':
+                size = "unprojected_size"
+
+            capabilities = brush.image_paint_capabilities
+
+            if capabilities.has_color:
+                row = layout.row(align=True)
+                row.ui_units_x = 4
+                UnifiedPaintPanel.prop_unified_color(row, context, brush, "color", text="")
+                UnifiedPaintPanel.prop_unified_color(row, context, brush, "secondary_color", text="")
+                row.separator()
+                layout.prop(brush, "blend", text="", translate=False)
+
+            UnifiedPaintPanel.prop_unified(
+                layout,
+                context,
+                brush,
+                size,
+                pressure_name="use_pressure_size",
+                unified_name="use_unified_size",
+                slider=True,
+                text="Size",
+                header=True,
+            )
+            UnifiedPaintPanel.prop_unified(
+                layout,
+                context,
+                brush,
+                "strength",
+                pressure_name="use_pressure_strength",
+                unified_name="use_unified_strength",
+                header=True,
+            )
+        else:
+            brush_basic_texpaint_settings(layout, context, brush, compact=True)
 
         return True
 
@@ -983,15 +1023,7 @@ class VIEW3D_HT_header(Header):
             row = layout.row()
             row.active = is_paint_tool and color_type == 'VERTEX'
 
-            if context.preferences.experimental.use_sculpt_texture_paint:
-                canvas_source = tool_settings.paint_mode.canvas_source
-                icon = 'GROUP_VCOL' if canvas_source == 'COLOR_ATTRIBUTE' else canvas_source
-                row.popover(panel="VIEW3D_PT_slots_paint_canvas", icon=icon)
-                # TODO: Update this boolean condition so that the Canvas button is only active when
-                # the appropriate color types are selected in Solid mode, I.E. 'TEXTURE'
-                row.active = is_paint_tool
-            else:
-                row.popover(panel="VIEW3D_PT_slots_color_attributes", icon='GROUP_VCOL')
+            row.popover(panel="VIEW3D_PT_slots_color_attributes", icon='GROUP_VCOL')
 
             layout.popover(
                 panel="VIEW3D_PT_sculpt_snapping",

scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -544,7 +544,7 @@ class VIEW3D_PT_slots_paint_canvas(SelectPaintSlotHelper, View3DPanel, Panel):
 
     @classmethod
     def poll(cls, context):
-        if not context.preferences.experimental.use_sculpt_texture_paint:
+        if not context.preferences.experimental.use_3d_texture_paint:
             return False
 
         from bl_ui.space_toolsystem_common import ToolSelectPanelHelper
@@ -898,7 +898,7 @@ class VIEW3D_PT_tools_brush_falloff_normal(View3DPaintPanel, Panel):
 
     @classmethod
     def poll(cls, context):
-        return context.image_paint_object
+        return context.image_paint_object and not bpy.context.preferences.experimental.use_3d_texture_paint
 
     def draw_header(self, context):
         tool_settings = context.tool_settings
@@ -1313,21 +1313,37 @@ class VIEW3D_PT_tools_imagepaint_symmetry(Panel, View3DPaintPanel):
 
     def draw(self, context):
         layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False
 
-        split = layout.split()
+        image_paint = context.tool_settings.image_paint
 
-        col = split.column()
-        col.alignment = 'RIGHT'
-        col.label(text="Mirror")
-
-        col = split.column()
-
-        row = col.row(align=True)
         ob = context.object
         mesh = ob.data
+
+        row = layout.row(align=True, heading="Mirror")
         row.prop(mesh, "use_mirror_x", text="X", toggle=True)
         row.prop(mesh, "use_mirror_y", text="Y", toggle=True)
         row.prop(mesh, "use_mirror_z", text="Z", toggle=True)
+
+        if bpy.context.preferences.experimental.use_3d_texture_paint:
+            row = layout.row(align=True, heading="Tiling")
+            row.prop(image_paint, "tile_x", text="X", toggle=True)
+            row.prop(image_paint, "tile_y", text="Y", toggle=True)
+            row.prop(image_paint, "tile_z", text="Z", toggle=True)
+
+            layout.prop(image_paint, "use_symmetry_feather", text="Feather")
+            layout.prop(mesh, "radial_symmetry", text="Radial")
+            layout.prop(image_paint, "tile_offset", text="Tile Offset")
+
+
+class VIEW3D_PT_tools_imagepaint_symmetry_for_topbar(Panel):
+    bl_space_type = 'TOPBAR'
+    bl_region_type = 'HEADER'
+    bl_label = "Symmetry"
+    bl_ui_units_x = 13
+
+    draw = VIEW3D_PT_tools_imagepaint_symmetry.draw
 
 
 class VIEW3D_PT_tools_imagepaint_options(View3DPaintPanel, Panel):
@@ -1349,12 +1365,17 @@ class VIEW3D_PT_tools_imagepaint_options(View3DPaintPanel, Panel):
         tool_settings = context.tool_settings
         ipaint = tool_settings.image_paint
 
-        layout.prop(ipaint, "seam_bleed")
-        layout.prop(ipaint, "dither", slider=True)
-
         col = layout.column()
-        col.prop(ipaint, "use_occlude")
-        col.prop(ipaint, "use_backface_culling", text="Backface Culling")
+        if bpy.context.preferences.experimental.use_3d_texture_paint:
+            # TODO: Enable dither support
+            col.prop(ipaint, "dither", slider=True)
+            col.active = False
+        else:
+            col.prop(ipaint, "seam_bleed")
+            col.prop(ipaint, "dither", slider=True)
+
+            col.prop(ipaint, "use_occlude")
+            col.prop(ipaint, "use_backface_culling", text="Backface Culling")
 
 
 class VIEW3D_PT_tools_imagepaint_options_cavity(Panel):
@@ -2391,6 +2412,7 @@ classes = (
     VIEW3D_PT_tools_imagepaint_options_cavity,
 
     VIEW3D_PT_tools_imagepaint_symmetry,
+    VIEW3D_PT_tools_imagepaint_symmetry_for_topbar,
     VIEW3D_PT_tools_imagepaint_options,
 
     VIEW3D_PT_tools_imagepaint_options_external,

source/blender/blenkernel/BKE_brush.hh
@@ -242,6 +242,10 @@ float normal_weight_get(const Brush &brush, bool invert);
 /* debugging only */
 void BKE_brush_debug_print_state(Brush *br);
 
+namespace bke::brush {
+bool implements_3d_texture_paint(const Brush &brush);
+}
+
 /* -------------------------------------------------------------------- */
 /** \name Brush Capabilities
  * Common boolean checks used during both brush evaluation and in UI drawing

source/blender/blenkernel/BKE_object.hh
@@ -507,6 +507,7 @@ void BKE_object_handle_update_ex(Depsgraph *depsgraph,
                                  Object *ob,
                                  RigidBodyWorld *rbw);
 
+bool BKE_object_use_sculptsession(eObjectMode mode);
 void BKE_object_sculpt_data_create(Object *ob);
 
 bool BKE_object_obdata_texspace_get(Object *ob,

source/blender/blenkernel/BKE_paint.hh
@@ -28,6 +28,7 @@
 #include "DNA_brush_enums.h"
 #include "DNA_meshdata_types.h"
 #include "DNA_object_enums.h"
+#include "DNA_scene_types.h"
 
 namespace blender {
 
@@ -465,7 +466,7 @@ struct SculptSession : NonCopyable, NonMovable {
   float4 prev_pivot_rot = float4(1.0f, 0.0f, 0.0f, 0.0f);
   float3 prev_pivot_scale = {};
 
-  eObjectMode mode_type;
+  eObjectMode mode_type = OB_MODE_OBJECT;
 
   /**
    * ID data is older than sculpt-mode data.
@@ -610,17 +611,18 @@ bool BKE_object_sculpt_use_dyntopo(const Object *object);
 
 /* paint_canvas.cc */
 
+using CanvasImageUser = std::variant<ImageUser, ImageUser *>;
+using CanvasImageData = std::pair<Image *, CanvasImageUser>;
+
 /**
  * Create a key that can be used to compare with previous ones to identify changes.
  * The resulting 'string' is owned by the caller.
  */
-std::string BKE_paint_canvas_key_get(PaintModeSettings *settings, Object *ob);
+std::string BKE_paint_canvas_key_get(ImagePaintSettings &settings, Object *ob);
 
-bool BKE_paint_canvas_image_get(PaintModeSettings *settings,
-                                Object *ob,
-                                Image **r_image,
-                                ImageUser **r_image_user);
-std::optional<StringRef> BKE_paint_canvas_uvmap_name_get(const PaintModeSettings *settings,
+std::optional<CanvasImageData> BKE_paint_canvas_image_get(const ImagePaintSettings &settings,
+                                                          Object &object);
+std::optional<StringRef> BKE_paint_canvas_uvmap_name_get(const ImagePaintSettings &settings,
                                                          Object *ob);
 CurveMapping *BKE_sculpt_default_cavity_curve();
 CurveMapping *BKE_paint_default_curve();

commit 58ba77b9bc43df0b7de8b3eb6834229db21f863a

Fix: Broken build after recent change
Introduced with 1feb1674bcca5b1fbec98fcc6755a536f4a81b72

The above build was slightly out of date before it was merged in,
missing some related API changes.

Pull Request: https://projects.blender.org/blender/blender/pulls/163545

source/blender/editors/sculpt_paint/mesh/paint_image.cc
@@ -687,7 +687,7 @@ void ED_object_texture_paint_mode_enter(bContext *C)
 void ED_object_texture_paint_mode_exit_ex(Main & /*bmain*/, Scene &scene, Object &ob)
 {
   if (USER_EXPERIMENTAL_TEST(&U, use_3d_texture_paint)) {
-    ed::sculpt_paint::mode_exit_generic(ob, OB_MODE_TEXTURE_PAINT);
+    ed::sculpt_paint::mode_exit_generic(scene, ob, OB_MODE_TEXTURE_PAINT);
   }
   else {
     ob.mode &= ~OB_MODE_TEXTURE_PAINT;