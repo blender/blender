blender/blender

commit 7037997c4d6e4186e6065b1276aec71027ccfa39

Outliner: New options for filtering contents
Add more options to filter out type specific contents from object. New
options are:
- Object data
- Vertex Group
- Shape Key
- Animation data
- Constraint
- Modifier
- Bone Collection
- Grease Pencil Effects
- Pose Bones
- Material

See PR description for image, video and RCS thread

Pull Request: https://projects.blender.org/blender/blender/pulls/156973

source/blender/makesdna/DNA_space_enums.h
@@ -188,6 +188,17 @@ enum eSpaceOutliner_Filter : int {
   SO_FILTER_ID_TYPE = (1 << 19),
 
   SO_FILTER_NO_OB_GREASE_PENCIL = (1 << 20),
+
+  SO_FILTER_NO_OB_DATA = (1 << 21),
+  SO_FILTER_NO_OB_ANIMATION = (1 << 22),
+  SO_FILTER_NO_OB_CONSTRAINTS = (1 << 23),
+  SO_FILTER_NO_OB_SHAPE_KEYS = (1 << 24),
+  SO_FILTER_NO_OB_MATERIAL = (1 << 25),
+  SO_FILTER_NO_OB_DEFGROUP = (1 << 26),
+  SO_FILTER_NO_OB_MODIFIERS = (1 << 27),
+  SO_FILTER_NO_ARMATURE_BONE_COLLECTION = (1 << 28),
+  SO_FILTER_NO_GREASE_PENCIL_EFFECTS = (1 << 29),
+  SO_FILTER_NO_POSE_BONES = (1 << 30),
 };
 ENUM_OPERATORS(eSpaceOutliner_Filter)
 

scripts/startup/bl_ui/space_outliner.py
@@ -530,13 +530,6 @@ class OUTLINER_PT_options_filter(Panel):
         sub = col.column(align=True)
         sub.active = space.use_filter_object
 
-        row = sub.row()
-        row.label(icon='BLANK1')
-        row.prop(space, "use_filter_object_content", text="Object Contents")
-        row = sub.row()
-        row.label(icon='BLANK1')
-        row.prop(space, "use_filter_children", text="Object Children")
-
         if bpy.data.meshes:
             row = sub.row()
             row.label(icon='MESH_DATA')
@@ -576,6 +569,73 @@ class OUTLINER_PT_options_filter(Panel):
             row.label(icon='BLANK1')
             row.prop(space, "use_filter_object_others", text="Others")
 
+        row = sub.row()
+        row.label(icon='BLANK1')
+        row.prop(space, "use_filter_children", text="Object Children")
+
+
+class OUTLINER_PT_options_object_data(Panel):
+    bl_space_type = 'OUTLINER'
+    bl_region_type = 'HEADER'
+    bl_label = ""
+    bl_parent_id = "OUTLINER_PT_options_filter"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw_header(self, context):
+        layout = self.layout
+        space = context.space_data
+        layout.prop(space, "use_filter_object_content", text="Object Contents")
+
+    def draw(self, context):
+        space = context.space_data
+        layout = self.layout
+
+        layout.active = space.use_filter_object and space.use_filter_object_content
+        panel_column = layout.column(align=True)
+        panel_column.use_property_split = False
+        panel_column.use_property_decorate = False
+
+        row = panel_column.row(align=True)
+        row.label(icon='OBJECT_DATA')
+        row.separator()
+        row.prop(space, "use_filter_object_data", text="Object Data")
+        row = panel_column.row(align=True)
+        row.label(icon='GROUP_VERTEX')
+        row.separator()
+        row.prop(space, "use_filter_object_vertex_groups", text="Vertex Groups")
+        row = panel_column.row(align=True)
+        row.label(icon='SHAPEKEY_DATA')
+        row.separator()
+        row.prop(space, "use_filter_object_shape_keys", text="Shape Keys")
+        row = panel_column.row(align=True)
+        row.label(icon='ANIM_DATA')
+        row.separator()
+        row.prop(space, "use_filter_object_animation", text="Animation Data")
+        row = panel_column.row(align=True)
+        row.label(icon='CONSTRAINT')
+        row.separator()
+        row.prop(space, "use_filter_object_constraints", text="Constraints")
+        row = panel_column.row(align=True)
+        row.label(icon='MODIFIER_ON')
+        row.separator()
+        row.prop(space, "use_filter_object_modifiers", text="Modifiers")
+        row = panel_column.row(align=True)
+        row.label(icon='GROUP_BONE')
+        row.separator()
+        row.prop(space, "use_filter_bone_collections", text="Bone Collections")
+        row = panel_column.row(align=True)
+        row.label(icon='SHADERFX')
+        row.separator()
+        row.prop(space, "use_filter_grease_pencil_effects", text="Grease Pencil Effects")
+        row = panel_column.row(align=True)
+        row.label(icon='BONE_DATA')
+        row.separator()
+        row.prop(space, "use_filter_pose_bones", text="Pose Bones")
+        row = panel_column.row(align=True)
+        row.label(icon='MATERIAL')
+        row.separator()
+        row.prop(space, "use_filter_object_materials", text="Materials")
+
 
 classes = (
     OUTLINER_HT_header,
@@ -595,6 +655,7 @@ classes = (
     OUTLINER_PT_filter,
     OUTLINER_PT_options_search,
     OUTLINER_PT_options_filter,
+    OUTLINER_PT_options_object_data,
 )
 
 if __name__ == "__main__":  # only for live edit.

source/blender/editors/space_outliner/outliner_tree.cc
@@ -962,10 +962,50 @@ static bool outliner_element_visible_get(const Main &bmain,
       }
     }
   }
+  else if ((te->idcode == ID_MA) && (exclude_filter & SO_FILTER_NO_OB_MATERIAL)) {
+    return false;
+  }
+  else if ((te->idcode == ID_KE) && (exclude_filter & SO_FILTER_NO_OB_SHAPE_KEYS)) {
+    return false;
+  }
+  else if ((TREESTORE(te)->type == TSE_BONE_COLLECTION_BASE) &&
+           (exclude_filter & SO_FILTER_NO_ARMATURE_BONE_COLLECTION))
+  {
+    return false;
+  }
   else if ((te->parent != nullptr) && (TREESTORE(te->parent)->type == TSE_SOME_ID) &&
            (te->parent->idcode == ID_OB))
   {
     if (exclude_filter & SO_FILTER_NO_OB_CONTENT) {
+      return false;
+    }
+    const eTreeStoreElemType type = eTreeStoreElemType(TREESTORE(te)->type);
+    if (type == TSE_SOME_ID && te->idcode != ID_OB) {
+      if (exclude_filter & SO_FILTER_NO_OB_DATA) {
+        return false;
+      }
+    }
+    else if ((type == TSE_ANIM_DATA) && (exclude_filter & SO_FILTER_NO_OB_ANIMATION)) {
+      return false;
+    }
+    else if ((type == TSE_CONSTRAINT_BASE) && (exclude_filter & SO_FILTER_NO_OB_CONSTRAINTS)) {
+      return false;
+    }
+    else if ((type == TSE_ANIM_DATA) && (exclude_filter & SO_FILTER_NO_OB_ANIMATION)) {
+      return false;
+    }
+    else if ((type == TSE_MODIFIER_BASE) && (exclude_filter & SO_FILTER_NO_OB_MODIFIERS)) {
+      return false;
+    }
+    else if ((type == TSE_DEFGROUP_BASE) && (exclude_filter & SO_FILTER_NO_OB_DEFGROUP)) {
+      return false;
+    }
+    else if ((type == TSE_GPENCIL_EFFECT_BASE) &&
+             (exclude_filter & SO_FILTER_NO_GREASE_PENCIL_EFFECTS))
+    {
+      return false;
+    }
+    else if ((type == TSE_POSE_BASE) && (exclude_filter & SO_FILTER_NO_POSE_BONES)) {
       return false;
     }
   }

source/blender/makesrna/intern/rna_space.cc
@@ -4775,6 +4775,59 @@ static void rna_def_space_outliner(BlenderRNA *brna)
       "defined/controlled automatically (e.g. to make users of an overridden data-block point to "
       "the override data, not the original linked data)");
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  /* Object data filters. */
+  prop = RNA_def_property(srna, "use_filter_object_data", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_OB_DATA);
+  RNA_def_property_ui_text(prop, "Show Object Data", "Show object data");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_object_animation", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_OB_ANIMATION);
+  RNA_def_property_ui_text(prop, "Show Animation Data", "Show animation data");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_object_constraints", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_OB_CONSTRAINTS);
+  RNA_def_property_ui_text(prop, "Show Object Constraints", "Show object constraints");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_object_shape_keys", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_OB_SHAPE_KEYS);
+  RNA_def_property_ui_text(prop, "Show Object Shape Keys", "Show object shape keys");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_object_materials", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_OB_MATERIAL);
+  RNA_def_property_ui_text(prop, "Show Object Materials", "Show object materials");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_object_vertex_groups", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_OB_DEFGROUP);
+  RNA_def_property_ui_text(prop, "Show Object Vertex Groups", "Show object vertex groups");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_object_modifiers", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_OB_MODIFIERS);
+  RNA_def_property_ui_text(prop, "Show Object Modifiers", "Show object modifiers");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_bone_collections", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(
+      prop, nullptr, "filter", SO_FILTER_NO_ARMATURE_BONE_COLLECTION);
+  RNA_def_property_ui_text(prop, "Show Bone Collections", "Show armature bone collections");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_grease_pencil_effects", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(
+      prop, nullptr, "filter", SO_FILTER_NO_GREASE_PENCIL_EFFECTS);
+  RNA_def_property_ui_text(prop, "Show Grease Pencil Effects", "Show grease pencil effects");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
+
+  prop = RNA_def_property(srna, "use_filter_pose_bones", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "filter", SO_FILTER_NO_POSE_BONES);
+  RNA_def_property_ui_text(prop, "Show Pose Bones", "Show pose bones of armatures");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
 }
 
 static void rna_def_space_view3d_shading(BlenderRNA *brna)

commit b7b90053d8cfa1524a29fe558c896bdd0c30e7ed

Fix: Outliner content filters have no effect with All View Layers on
The new per-content-type filter bits were missing from SO_FILTER_ANY,
which outliner_element_visible_get() uses as an early out. With no
legacy filter bit set, such as after enabling All View Layers, every
element was reported visible.

This issue was introduced in 7037997c4d6.

Pull Request: https://projects.blender.org/blender/blender/pulls/163560

source/blender/makesdna/DNA_space_enums.h
@@ -211,9 +211,15 @@ ENUM_OPERATORS(eSpaceOutliner_Filter)
   (SO_FILTER_OB_STATE_VISIBLE | SO_FILTER_OB_STATE_SELECTED | SO_FILTER_OB_STATE_ACTIVE | \
    SO_FILTER_OB_STATE_SELECTABLE)
 
+#define SO_FILTER_OB_CONTENT_TYPE \
+  (SO_FILTER_NO_OB_DATA | SO_FILTER_NO_OB_ANIMATION | SO_FILTER_NO_OB_CONSTRAINTS | \
+   SO_FILTER_NO_OB_SHAPE_KEYS | SO_FILTER_NO_OB_MATERIAL | SO_FILTER_NO_OB_DEFGROUP | \
+   SO_FILTER_NO_OB_MODIFIERS | SO_FILTER_NO_ARMATURE_BONE_COLLECTION | \
+   SO_FILTER_NO_GREASE_PENCIL_EFFECTS | SO_FILTER_NO_POSE_BONES)
+
 #define SO_FILTER_ANY \
   (SO_FILTER_NO_OB_CONTENT | SO_FILTER_NO_CHILDREN | SO_FILTER_OB_TYPE | SO_FILTER_OB_STATE | \
-   SO_FILTER_NO_COLLECTION | SO_FILTER_NO_VIEW_LAYERS)
+   SO_FILTER_NO_COLLECTION | SO_FILTER_NO_VIEW_LAYERS | SO_FILTER_OB_CONTENT_TYPE)
 
 /** #SpaceOutliner.filter_state */
 enum eSpaceOutliner_StateFilter : char {