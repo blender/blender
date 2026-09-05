zNightlord/Bforartists

commit 5f30b51c2b4ce889e4239bce40a88ee493d6a41e

Fix #156587: Crash when Ctrl + Scroll on empty item list in group node setting
Return early when totitems are zero in enum.

Pull Request: https://projects.blender.org/blender/blender/pulls/156617

source/blender/makesrna/intern/rna_access.cc
@@ -4479,6 +4479,11 @@ int RNA_property_enum_step(
   int step_tot = 0;
 
   RNA_property_enum_items(const_cast<bContext *>(C), ptr, prop, &item_array, &totitem, &free);
+
+  if (!totitem) {
+    return result_value;
+  }
+
   i = RNA_enum_from_value(item_array, from_value);
   i_init = i;
 

blender/blender

commit 04293f67367236cb2318c1f2081012e7eaf7bd15

Fix: RNA_property_enum_step leaks memory on empty enum
If the enum had no items, it would return early without freeing
the items array if it was dynamically allocated.

This issue was introduced in 5f30b51c2b4.

Pull Request: https://projects.blender.org/blender/blender/pulls/163506

source/blender/makesrna/intern/rna_access.cc
@@ -4578,6 +4578,9 @@ int RNA_property_enum_step(
   RNA_property_enum_items(const_cast<bContext *>(C), ptr, prop, &item_array, &totitem, &free);
 
   if (!totitem) {
+    if (free) {
+      MEM_delete(item_array);
+    }
     return result_value;
   }
 