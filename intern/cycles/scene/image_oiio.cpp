blender/blender

commit 850b986d894df88e9111f2633c468cd2bec0231e

Refactor: Cycles: Changes to image loading to prepare for texture cache
* Move ImageMetaData into util/ to be reused by tx generator.
* Centralize converting pixels to a format that the kernel understands
  in metadata.conform_pixels(). This must now be used by ImageLoader
  implementations when pixel data does not conform already.
* Move OIIO metadata and pixel loading into ImageMetaData.
* Fix incorrect handling of non-finite pixelsn for half float.
* Remove obsolute device features argument.

Pull Request: https://projects.blender.org/blender/blender/pulls/152665

intern/cycles/scene/image_oiio.cpp
@@ -15,235 +15,18 @@ OIIOImageLoader::OIIOImageLoader(const string &filepath) : filepath(filepath) {}
 
 OIIOImageLoader::~OIIOImageLoader() = default;
 
-bool OIIOImageLoader::load_metadata(const ImageDeviceFeatures & /*features*/,
-                                    ImageMetaData &metadata)
+bool OIIOImageLoader::load_metadata(ImageMetaData &metadata)
 {
-  /* Perform preliminary checks, with meaningful logging. */
-  if (filepath.empty()) {
-    return false;
-  }
-
-  if (!path_exists(filepath.string())) {
-    LOG_ERROR << "Image file '" << filepath.string() << "' does not exist.";
-    return false;
-  }
-  if (path_is_directory(filepath.string())) {
-    LOG_ERROR << "Image file '" << filepath.string() << "' is a directory, cannot use as image.";
-    return false;
-  }
-
-  unique_ptr<ImageInput> in(ImageInput::create(filepath.string()));
-  if (!in) {
-    LOG_ERROR << "Image file '" << filepath.string() << "' failed to load.";
-    return false;
-  }
-
-  ImageSpec spec;
-  if (!in->open(filepath.string(), spec)) {
-    LOG_ERROR << "Image file '" << filepath.string() << "' failed to open.";
-    return false;
-  }
-
-  metadata.width = spec.width;
-  metadata.height = spec.height;
-  metadata.compress_as_srgb = false;
-
-  /* Check the main format, and channel formats. */
-  size_t channel_size = spec.format.basesize();
-
-  bool is_float = false;
-  bool is_half = false;
-
-  if (spec.format.is_floating_point()) {
-    is_float = true;
-  }
-
-  for (size_t channel = 0; channel < spec.channelformats.size(); channel++) {
-    channel_size = max(channel_size, spec.channelformats[channel].basesize());
-    if (spec.channelformats[channel].is_floating_point()) {
-      is_float = true;
-    }
-  }
-
-  /* check if it's half float */
-  if (spec.format == TypeDesc::HALF) {
-    is_half = true;
-  }
-
-  /* set type and channels */
-  metadata.channels = spec.nchannels;
-
-  if (is_half) {
-    metadata.type = (metadata.channels > 1) ? IMAGE_DATA_TYPE_HALF4 : IMAGE_DATA_TYPE_HALF;
-  }
-  else if (is_float) {
-    metadata.type = (metadata.channels > 1) ? IMAGE_DATA_TYPE_FLOAT4 : IMAGE_DATA_TYPE_FLOAT;
-  }
-  else if (spec.format == TypeDesc::USHORT) {
-    metadata.type = (metadata.channels > 1) ? IMAGE_DATA_TYPE_USHORT4 : IMAGE_DATA_TYPE_USHORT;
-  }
-  else {
-    metadata.type = (metadata.channels > 1) ? IMAGE_DATA_TYPE_BYTE4 : IMAGE_DATA_TYPE_BYTE;
-  }
-
-  metadata.colorspace_file_format = in->format_name();
-  metadata.colorspace_file_hint = spec.get_string_attribute("oiio:ColorSpace");
-
-  in->close();
-
-  return true;
+  return metadata.oiio_load_metadata(filepath);
 }
 
-template<TypeDesc::BASETYPE FileFormat, typename StorageType>
-static void oiio_load_pixels(const ImageMetaData &metadata,
-                             const unique_ptr<ImageInput> &in,
-                             const bool associate_alpha,
-                             StorageType *pixels)
+bool OIIOImageLoader::load_pixels(const ImageMetaData &metadata, void *pixels)
 {
-  const size_t width = metadata.width;
-  const size_t height = metadata.height;
-  const int components = metadata.channels;
-
-  /* Read pixels through OpenImageIO. */
-  StorageType *readpixels = pixels;
-  vector<StorageType> tmppixels;
-  if (components > 4) {
-    tmppixels.resize(width * height * components);
-    readpixels = &tmppixels[0];
-  }
-
-  const size_t scanlinesize = width * components * sizeof(StorageType);
-  in->read_image(0,
-                 0,
-                 0,
-                 components,
-                 FileFormat,
-                 (uchar *)readpixels + (height - 1) * scanlinesize,
-                 AutoStride,
-                 -scanlinesize,
-                 AutoStride);
-
-  if (components > 4) {
-    const size_t dimensions = width * height;
-    for (size_t i = dimensions - 1, pixel = 0; pixel < dimensions; pixel++, i--) {
-      pixels[i * 4 + 3] = tmppixels[i * components + 3];
-      pixels[i * 4 + 2] = tmppixels[i * components + 2];
-      pixels[i * 4 + 1] = tmppixels[i * components + 1];
-      pixels[i * 4 + 0] = tmppixels[i * components + 0];
-    }
-    tmppixels.clear();
-  }
-
-  /* CMYK to RGBA. */
-  const bool cmyk = strcmp(in->format_name(), "jpeg") == 0 && components == 4;
-  if (cmyk) {
-    const StorageType one = util_image_cast_from_float<StorageType>(1.0f);
-
-    const size_t num_pixels = width * height;
-    for (size_t i = num_pixels - 1, pixel = 0; pixel < num_pixels; pixel++, i--) {
-      const float c = util_image_cast_to_float(pixels[i * 4 + 0]);
-      const float m = util_image_cast_to_float(pixels[i * 4 + 1]);
-      const float y = util_image_cast_to_float(pixels[i * 4 + 2]);
-      const float k = util_image_cast_to_float(pixels[i * 4 + 3]);
-      pixels[i * 4 + 0] = util_image_cast_from_float<StorageType>((1.0f - c) * (1.0f - k));
-      pixels[i * 4 + 1] = util_image_cast_from_float<StorageType>((1.0f - m) * (1.0f - k));
-      pixels[i * 4 + 2] = util_image_cast_from_float<StorageType>((1.0f - y) * (1.0f - k));
-      pixels[i * 4 + 3] = one;
-    }
-  }
-
-  if (components == 4 && associate_alpha) {
-    const size_t dimensions = width * height;
-    for (size_t i = dimensions - 1, pixel = 0; pixel < dimensions; pixel++, i--) {
-      const StorageType alpha = pixels[i * 4 + 3];
-      pixels[i * 4 + 0] = util_image_multiply_native(pixels[i * 4 + 0], alpha);
-      pixels[i * 4 + 1] = util_image_multiply_native(pixels[i * 4 + 1], alpha);
-      pixels[i * 4 + 2] = util_image_multiply_native(pixels[i * 4 + 2], alpha);
-    }
-  }
-}
-
-bool OIIOImageLoader::load_pixels(const ImageMetaData &metadata,
-                                  void *pixels,
-                                  const size_t /*pixels_size*/,
-                                  const bool associate_alpha)
-{
-  unique_ptr<ImageInput> in = nullptr;
-
-  /* NOTE: Error logging is done in meta data acquisition. */
-  if (!path_exists(filepath.string()) || path_is_directory(filepath.string())) {
+  if (!metadata.oiio_load_pixels(filepath, pixels)) {
     return false;
   }
 
-  /* load image from file through OIIO */
-  in = unique_ptr<ImageInput>(ImageInput::create(filepath.string()));
-  if (!in) {
-    return false;
-  }
-
-  ImageSpec spec = ImageSpec();
-  ImageSpec config = ImageSpec();
-
-  /* Load without automatic OIIO alpha conversion, we do it ourselves. OIIO
-   * will associate alpha in the 8bit buffer for PNGs, which leads to too
-   * much precision loss when we load it as half float to do a color-space transform. */
-  config.attribute("oiio:UnassociatedAlpha", 1);
-
-  if (!in->open(filepath.string(), spec, config)) {
-    return false;
-  }
-
-  bool do_associate_alpha = false;
-  if (associate_alpha) {
-    do_associate_alpha = spec.get_int_attribute("oiio:UnassociatedAlpha", 0);
-
-    if (!do_associate_alpha && spec.alpha_channel != -1) {
-      /* Workaround OIIO not detecting TGA file alpha the same as Blender (since #3019).
-       * We want anything not marked as premultiplied alpha to get associated. */
-      if (strcmp(in->format_name(), "targa") == 0) {
-        do_associate_alpha = spec.get_int_attribute("targa:alpha_type", -1) != 4;
-      }
-      /* OIIO DDS reader never sets UnassociatedAlpha attribute. */
-      if (strcmp(in->format_name(), "dds") == 0) {
-        do_associate_alpha = true;
-      }
-      /* Workaround OIIO bug that sets oiio:UnassociatedAlpha on the last layer
-       * but not composite image that we read. */
-      if (strcmp(in->format_name(), "psd") == 0) {
-        do_associate_alpha = true;
-      }
-    }
-  }
-
-  switch (metadata.type) {
-    case IMAGE_DATA_TYPE_BYTE:
-    case IMAGE_DATA_TYPE_BYTE4:
-      oiio_load_pixels<TypeDesc::UINT8, uchar>(metadata, in, do_associate_alpha, (uchar *)pixels);
-      break;
-    case IMAGE_DATA_TYPE_USHORT:
-    case IMAGE_DATA_TYPE_USHORT4:
-      oiio_load_pixels<TypeDesc::USHORT, uint16_t>(
-          metadata, in, do_associate_alpha, (uint16_t *)pixels);
-      break;
-    case IMAGE_DATA_TYPE_HALF:
-    case IMAGE_DATA_TYPE_HALF4:
-      oiio_load_pixels<TypeDesc::HALF, half>(metadata, in, do_associate_alpha, (half *)pixels);
-      break;
-    case IMAGE_DATA_TYPE_FLOAT:
-    case IMAGE_DATA_TYPE_FLOAT4:
-      oiio_load_pixels<TypeDesc::FLOAT, float>(metadata, in, do_associate_alpha, (float *)pixels);
-      break;
-    case IMAGE_DATA_TYPE_NANOVDB_FLOAT:
-    case IMAGE_DATA_TYPE_NANOVDB_FLOAT3:
-    case IMAGE_DATA_TYPE_NANOVDB_FLOAT4:
-    case IMAGE_DATA_TYPE_NANOVDB_FPN:
-    case IMAGE_DATA_TYPE_NANOVDB_FP16:
-    case IMAGE_DATA_TYPE_NANOVDB_EMPTY:
-    case IMAGE_DATA_NUM_TYPES:
-      break;
-  }
-
-  in->close();
+  metadata.conform_pixels(pixels);
   return true;
 }
 

intern/cycles/blender/image.cpp
@@ -14,7 +14,6 @@
 #include "blender/session.h"
 
 #include "util/half.h"
-#include "util/transform.h"
 #include "util/types_float4.h"
 
 CCL_NAMESPACE_BEGIN
@@ -43,8 +42,7 @@ BlenderImageLoader::BlenderImageLoader(blender::Image *b_image,
   }
 }
 
-bool BlenderImageLoader::load_metadata(const ImageDeviceFeatures & /*features*/,
-                                       ImageMetaData &metadata)
+bool BlenderImageLoader::load_metadata(ImageMetaData &metadata)
 {
   bool is_float = false;
   bool is_data = false;
@@ -58,6 +56,7 @@ bool BlenderImageLoader::load_metadata(const ImageDeviceFeatures & /*features*/,
       metadata.width = ibuf->x;
       metadata.height = ibuf->y;
       metadata.channels = (is_float) ? ibuf->channels : 4;
+      metadata.is_unassociated_alpha = !is_float;
     }
     else {
       metadata.width = 0;
@@ -132,35 +131,22 @@ static void load_float_pixels(const blender::ImBuf *ibuf,
 
 static void load_half_pixels(const blender::ImBuf *ibuf,
                              const ImageMetaData &metadata,
-                             half *out_pixels,
-                             const bool associate_alpha)
+                             half *out_pixels)
 {
   /* Half float. Blender does not have a half type, but in some cases
    * we up-sample byte to half to avoid precision loss for colorspace
    * conversion. */
   const size_t num_pixels = ((size_t)metadata.width) * metadata.height;
   const int out_channels = metadata.channels;
-  const int in_channels = 4;
   const uchar *in_pixels = ibuf->byte_buffer.data;
 
   if (in_pixels) {
     /* Convert uchar to half. */
     const uchar *in_pixel = in_pixels;
     half *out_pixel = out_pixels;
-    if (associate_alpha && out_channels == in_channels) {
-      for (size_t i = 0; i < num_pixels; i++, in_pixel += in_channels, out_pixel += out_channels) {
-        const float alpha = util_image_cast_to_float(in_pixel[3]);
-        out_pixel[0] = float_to_half_image(util_image_cast_to_float(in_pixel[0]) * alpha);
-        out_pixel[1] = float_to_half_image(util_image_cast_to_float(in_pixel[1]) * alpha);
-        out_pixel[2] = float_to_half_image(util_image_cast_to_float(in_pixel[2]) * alpha);
-        out_pixel[3] = float_to_half_image(alpha);
-      }
-    }
-    else {
-      for (size_t i = 0; i < num_pixels; i++) {
-        for (int c = 0; c < out_channels; c++, in_pixel++, out_pixel++) {
-          *out_pixel = float_to_half_image(util_image_cast_to_float(*in_pixel));
-        }
+    for (size_t i = 0; i < num_pixels; i++) {
+      for (int c = 0; c < out_channels; c++, in_pixel++, out_pixel++) {
+        *out_pixel = float_to_half_image(util_image_cast_to_float(*in_pixel));
       }
     }
   }
@@ -179,8 +165,7 @@ static void load_half_pixels(const blender::ImBuf *ibuf,
 
 static void load_byte_pixels(const blender::ImBuf *ibuf,
                              const ImageMetaData &metadata,
-                             uchar *out_pixels,
-                             const bool associate_alpha)
+                             uchar *out_pixels)
 {
   const size_t num_pixels = ((size_t)metadata.width) * metadata.height;
   const int out_channels = metadata.channels;
@@ -190,16 +175,6 @@ static void load_byte_pixels(const blender::ImBuf *ibuf,
   if (in_pixels) {
     /* Straight copy pixel data. */
     memcpy(out_pixels, in_pixels, num_pixels * in_channels * sizeof(unsigned char));
-
-    if (associate_alpha && out_channels == in_channels) {
-      /* Premultiply, byte images are always straight for Blender. */
-      unsigned char *out_pixel = (unsigned char *)out_pixels;
-      for (size_t i = 0; i < num_pixels; i++, out_pixel += 4) {
-        out_pixel[0] = (out_pixel[0] * out_pixel[3]) / 255;
-        out_pixel[1] = (out_pixel[1] * out_pixel[3]) / 255;
-        out_pixel[2] = (out_pixel[2] * out_pixel[3]) / 255;
-      }
-    }
   }
   else {
     /* Missing or invalid pixel data. */
@@ -213,10 +188,7 @@ static void load_byte_pixels(const blender::ImBuf *ibuf,
   }
 }
 
-bool BlenderImageLoader::load_pixels(const ImageMetaData &metadata,
-                                     void *out_pixels,
-                                     const size_t /*out_pixels_size*/,
-                                     const bool associate_alpha)
+bool BlenderImageLoader::load_pixels(const ImageMetaData &metadata, void *out_pixels)
 {
   void *lock;
   blender::ImBuf *ibuf = BKE_image_acquire_ibuf(b_image, &b_iuser, &lock);
@@ -230,10 +202,10 @@ bool BlenderImageLoader::load_pixels(const ImageMetaData &metadata,
       load_float_pixels(ibuf, metadata, (float *)out_pixels);
     }
     else if (metadata.type == IMAGE_DATA_TYPE_HALF || metadata.type == IMAGE_DATA_TYPE_HALF4) {
-      load_half_pixels(ibuf, metadata, (half *)out_pixels, associate_alpha);
+      load_half_pixels(ibuf, metadata, (half *)out_pixels);
     }
     else {
-      load_byte_pixels(ibuf, metadata, (uchar *)out_pixels, associate_alpha);
+      load_byte_pixels(ibuf, metadata, (uchar *)out_pixels);
     }
   }
 
@@ -242,6 +214,10 @@ bool BlenderImageLoader::load_pixels(const ImageMetaData &metadata,
   /* Free image buffers to save memory during render. */
   if (free_cache) {
     BKE_image_free_buffers_ex(b_image, true);
+  }
+
+  if (!mismatch) {
+    metadata.conform_pixels(out_pixels);
   }
 
   return !mismatch;

intern/cycles/blender/image.h
@@ -22,11 +22,8 @@ class BlenderImageLoader : public ImageLoader {
                      const int tile_number,
                      const bool is_preview_render);
 
-  bool load_metadata(const ImageDeviceFeatures &features, ImageMetaData &metadata) override;
-  bool load_pixels(const ImageMetaData &metadata,
-                   void *pixels,
-                   const size_t pixels_size,
-                   const bool associate_alpha) override;
+  bool load_metadata(ImageMetaData &metadata) override;
+  bool load_pixels(const ImageMetaData &metadata, void *pixels) override;
   string name() const override;
   bool equals(const ImageLoader &other) const override;
 

intern/cycles/scene/image.cpp
@@ -225,61 +225,6 @@ bool ImageHandle::operator==(const ImageHandle &other) const
   return manager == other.manager && is_tiled == other.is_tiled && slots == other.slots;
 }
 
-/* Image MetaData */
-
-ImageMetaData::ImageMetaData()
-    : channels(0),
-      width(0),
-      height(0),
-      byte_size(0),
-      type(IMAGE_DATA_NUM_TYPES),
-      colorspace(u_colorspace_scene_linear),
-      colorspace_file_format(""),
-      use_transform_3d(false),
-      compress_as_srgb(false)
-{
-}
-
-bool ImageMetaData::operator==(const ImageMetaData &other) const
-{
-  return channels == other.channels && width == other.width && height == other.height &&
-         use_transform_3d == other.use_transform_3d &&
-         (!use_transform_3d || transform_3d == other.transform_3d) && type == other.type &&
-         colorspace == other.colorspace && compress_as_srgb == other.compress_as_srgb;
-}
-
-bool ImageMetaData::is_float() const
-{
-  return (type == IMAGE_DATA_TYPE_FLOAT || type == IMAGE_DATA_TYPE_FLOAT4 ||
-          type == IMAGE_DATA_TYPE_HALF || type == IMAGE_DATA_TYPE_HALF4);
-}
-
-void ImageMetaData::detect_colorspace()
-{
-  /* Convert used specified color spaces to one we know how to handle. */
-  colorspace = ColorSpaceManager::detect_known_colorspace(
-      colorspace, colorspace_file_hint.c_str(), colorspace_file_format, is_float());
-
-  if (colorspace == u_colorspace_scene_linear || colorspace == u_colorspace_data) {
-    /* Nothing to do. */
-  }
-  else if (colorspace == u_colorspace_scene_linear_srgb) {
-    /* Keep sRGB colorspace stored as sRGB, to save memory and/or loading time
-     * for the common case of 8bit sRGB images like PNG. */
-    compress_as_srgb = true;
-  }
-  else {
-    /* If colorspace conversion needed, use half instead of short so we can
-     * represent HDR values that might result from conversion. */
-    if (type == IMAGE_DATA_TYPE_BYTE || type == IMAGE_DATA_TYPE_USHORT) {
-      type = IMAGE_DATA_TYPE_HALF;
-    }
-    else if (type == IMAGE_DATA_TYPE_BYTE4 || type == IMAGE_DATA_TYPE_USHORT4) {
-      type = IMAGE_DATA_TYPE_HALF4;
-    }
-  }
-}
-
 /* Image Loader */
 
 ImageLoader::ImageLoader() = default;
@@ -309,14 +254,11 @@ bool ImageLoader::is_vdb_loader() const
 
 /* Image Manager */
 
-ImageManager::ImageManager(const DeviceInfo &info)
+ImageManager::ImageManager(const DeviceInfo & /*info*/)
 {
   need_update_ = true;
   osl_texture_system = nullptr;
   animation_frame = 0;
-
-  /* Set image limits */
-  features.has_nanovdb = info.has_nanovdb;
 }
 
 ImageManager::~ImageManager()
@@ -362,16 +304,14 @@ void ImageManager::load_image_metadata(Image *img)
   metadata = ImageMetaData();
   metadata.colorspace = img->params.colorspace;
 
-  if (img->loader->load_metadata(features, metadata)) {
+  if (img->loader->load_metadata(metadata)) {
     assert(metadata.type != IMAGE_DATA_NUM_TYPES);
   }
   else {
     metadata.type = IMAGE_DATA_TYPE_BYTE4;
   }
 
-  metadata.detect_colorspace();
-
-  assert(features.has_nanovdb || !is_nanovdb_type(metadata.type));
+  metadata.finalize(img->params.alpha_type);
 
   img->need_metadata = false;
 }
@@ -525,15 +465,6 @@ ImageManager::Image *ImageManager::get_image_slot(const size_t slot)
   return images[slot].get();
 }
 
-static bool image_associate_alpha(ImageManager::Image *img)
-{
-  /* For typical RGBA images we let OIIO convert to associated alpha,
-   * but some types we want to leave the RGB channels untouched. */
-  return !(ColorSpaceManager::colorspace_is_data(img->params.colorspace) ||
-           img->params.alpha_type == IMAGE_ALPHA_IGNORE ||
-           img->params.alpha_type == IMAGE_ALPHA_CHANNEL_PACKED);
-}
-
 template<TypeDesc::BASETYPE FileFormat, typename StorageType>
 bool ImageManager::file_load_image(Image *img, const int texture_limit)
 {
@@ -545,7 +476,6 @@ bool ImageManager::file_load_image(Image *img, const int texture_limit)
   /* Get metadata. */
   const int width = img->metadata.width;
   const int height = img->metadata.height;
-  const int components = img->metadata.channels;
 
   /* Read pixels. */
   vector<StorageType> pixels_storage;
@@ -571,99 +501,8 @@ bool ImageManager::file_load_image(Image *img, const int texture_limit)
     return false;
   }
 
-  const size_t num_pixels = ((size_t)width) * height;
-  img->loader->load_pixels(
-      img->metadata, pixels, num_pixels * components, image_associate_alpha(img));
-
-  /* The kernel can handle 1 and 4 channel images. Anything that is not a single
-   * channel image is converted to RGBA format. */
-  const bool is_rgba = (img->metadata.type == IMAGE_DATA_TYPE_FLOAT4 ||
-                        img->metadata.type == IMAGE_DATA_TYPE_HALF4 ||
-                        img->metadata.type == IMAGE_DATA_TYPE_BYTE4 ||
-                        img->metadata.type == IMAGE_DATA_TYPE_USHORT4);
-
-  if (is_rgba) {
-    const StorageType one = util_image_cast_from_float<StorageType>(1.0f);
-
-    if (components == 2) {
-      /* Grayscale + alpha to RGBA. */
-      for (size_t i = num_pixels - 1, pixel = 0; pixel < num_pixels; pixel++, i--) {
-        pixels[i * 4 + 3] = pixels[i * 2 + 1];
-        pixels[i * 4 + 2] = pixels[i * 2 + 0];
-        pixels[i * 4 + 1] = pixels[i * 2 + 0];
-        pixels[i * 4 + 0] = pixels[i * 2 + 0];
-      }
-    }
-    else if (components == 3) {
-      /* RGB to RGBA. */
-      for (size_t i = num_pixels - 1, pixel = 0; pixel < num_pixels; pixel++, i--) {
-        pixels[i * 4 + 3] = one;
-        pixels[i * 4 + 2] = pixels[i * 3 + 2];
-        pixels[i * 4 + 1] = pixels[i * 3 + 1];
-        pixels[i * 4 + 0] = pixels[i * 3 + 0];
-      }
-    }
-    else if (components == 1) {
-      /* Grayscale to RGBA. */
-      for (size_t i = num_pixels - 1, pixel = 0; pixel < num_pixels; pixel++, i--) {
-        pixels[i * 4 + 3] = one;
-        pixels[i * 4 + 2] = pixels[i];
-        pixels[i * 4 + 1] = pixels[i];
-        pixels[i * 4 + 0] = pixels[i];
-      }
-    }
-
-    /* Disable alpha if requested by the user. */
-    if (img->params.alpha_type == IMAGE_ALPHA_IGNORE) {
-      for (size_t i = num_pixels - 1, pixel = 0; pixel < num_pixels; pixel++, i--) {
-        pixels[i * 4 + 3] = one;
-      }
-    }
-  }
-
-  if (img->metadata.colorspace != u_colorspace_scene_linear &&
-      img->metadata.colorspace != u_colorspace_scene_linear_srgb &&
-      img->metadata.colorspace != u_colorspace_data)
-  {
-    /* Convert to scene linear. */
-    const bool ignore_alpha = img->params.alpha_type == IMAGE_ALPHA_IGNORE ||
-                              img->params.alpha_type == IMAGE_ALPHA_CHANNEL_PACKED;
-    ColorSpaceManager::to_scene_linear(img->metadata.colorspace,
-                                       pixels,
-                                       width,
-                                       height,
-                                       width * (is_rgba ? 4 : 1),
-                                       is_rgba,
-                                       img->metadata.compress_as_srgb,
-                                       ignore_alpha);
-  }
-
-  /* Make sure we don't have buggy values. */
-  if constexpr (FileFormat == TypeDesc::FLOAT) {
-    /* For RGBA buffers we put all channels to 0 if either of them is not
-     * finite. This way we avoid possible artifacts caused by fully changed
-     * hue. */
-    if (is_rgba) {
-      for (size_t i = 0; i < num_pixels; i += 4) {
-        StorageType *pixel = &pixels[i * 4];
-        if (!isfinite(pixel[0]) || !isfinite(pixel[1]) || !isfinite(pixel[2]) ||
-            !isfinite(pixel[3]))
-        {
-          pixel[0] = 0;
-          pixel[1] = 0;
-          pixel[2] = 0;
-          pixel[3] = 0;
-        }
-      }
-    }
-    else {
-      for (size_t i = 0; i < num_pixels; ++i) {
-        StorageType *pixel = &pixels[i];
-        if (!isfinite(pixel[0])) {
-          pixel[0] = 0;
-        }
-      }
-    }
+  if (!img->loader->load_pixels(img->metadata, pixels)) {
+    return false;
   }
 
   /* Scale image down if needed. */
@@ -680,7 +519,7 @@ bool ImageManager::file_load_image(Image *img, const int texture_limit)
     util_image_resize_pixels(pixels_storage,
                              width,
                              height,
-                             is_rgba ? 4 : 1,
+                             img->metadata.is_rgba() ? 4 : 1,
                              scale_factor,
                              &scaled_pixels,
                              &scaled_width,
@@ -819,10 +658,10 @@ void ImageManager::device_load_image(Device *device,
 #ifdef WITH_NANOVDB
   else if (is_nanovdb_type(type)) {
     const thread_scoped_lock device_lock(device_mutex);
-    void *pixels = img->mem->alloc(img->metadata.byte_size, 0);
+    void *pixels = img->mem->alloc(img->metadata.nanovdb_byte_size, 0);
 
     if (pixels != nullptr) {
-      img->loader->load_pixels(img->metadata, pixels, img->metadata.byte_size, false);
+      img->loader->load_pixels(img->metadata, pixels);
     }
   }
 #endif

intern/cycles/scene/image.h
@@ -7,6 +7,7 @@
 #include "device/memory.h"
 
 #include "util/colorspace.h"
+#include "util/image_metadata.h"
 #include "util/string.h"
 #include "util/thread.h"
 #include "util/transform.h"
@@ -47,41 +48,6 @@ class ImageParams {
   }
 };
 
-/* Image MetaData
- *
- * Information about the image that is available before the image pixels are loaded. */
-class ImageMetaData {
- public:
-  /* Set by ImageLoader.load_metadata(). */
-  int channels;
-  size_t width, height;
-  size_t byte_size;
-  ImageDataType type;
-
-  /* Optional color space, defaults to raw. */
-  ustring colorspace;
-  string colorspace_file_hint;
-  const char *colorspace_file_format;
-
-  /* Optional transform for 3D images. */
-  bool use_transform_3d;
-  Transform transform_3d;
-
-  /* Automatically set. */
-  bool compress_as_srgb;
-
-  ImageMetaData();
-  bool operator==(const ImageMetaData &other) const;
-  bool is_float() const;
-  void detect_colorspace();
-};
-
-/* Information about supported features that Image loaders can use. */
-class ImageDeviceFeatures {
- public:
-  bool has_nanovdb = true;
-};
-
 /* Image loader base class, that can be subclassed to load image data
  * from custom sources (file, memory, procedurally generated, etc). */
 class ImageLoader {
@@ -90,13 +56,10 @@ class ImageLoader {
   virtual ~ImageLoader() = default;
 
   /* Load metadata without actual image yet, should be fast. */
-  virtual bool load_metadata(const ImageDeviceFeatures &features, ImageMetaData &metadata) = 0;
+  virtual bool load_metadata(ImageMetaData &metadata) = 0;
 
   /* Load actual image contents. */
-  virtual bool load_pixels(const ImageMetaData &metadata,
-                           void *pixels,
-                           const size_t pixels_size,
-                           const bool associate_alpha) = 0;
+  virtual bool load_pixels(const ImageMetaData &metadata, void *pixels) = 0;
 
   /* Name for logs and stats. */
   virtual string name() const = 0;
@@ -207,8 +170,6 @@ class ImageManager {
 
  private:
   bool need_update_;
-
-  ImageDeviceFeatures features;
 
   thread_mutex device_mutex;
   thread_mutex images_mutex;

intern/cycles/scene/image_oiio.h
@@ -13,12 +13,9 @@ class OIIOImageLoader : public ImageLoader {
   OIIOImageLoader(const string &filepath);
   ~OIIOImageLoader() override;
 
-  bool load_metadata(const ImageDeviceFeatures &features, ImageMetaData &metadata) override;
+  bool load_metadata(ImageMetaData &metadata) override;
 
-  bool load_pixels(const ImageMetaData &metadata,
-                   void *pixels,
-                   const size_t pixels_size,
-                   const bool associate_alpha) override;
+  bool load_pixels(const ImageMetaData &metadata, void *pixels) override;
 
   string name() const override;
 

intern/cycles/scene/image_sky.cpp
@@ -25,20 +25,17 @@ SkyLoader::SkyLoader(const bool multiple_scattering,
 
 SkyLoader::~SkyLoader() = default;
 
-bool SkyLoader::load_metadata(const ImageDeviceFeatures & /*features*/, ImageMetaData &metadata)
+bool SkyLoader::load_metadata(ImageMetaData &metadata)
 {
   metadata.width = 512;
   metadata.height = 256;
   metadata.channels = 3;
   metadata.type = IMAGE_DATA_TYPE_FLOAT4;
-  metadata.compress_as_srgb = false;
+  metadata.is_compressible_as_srgb = false;
   return true;
 }
 
-bool SkyLoader::load_pixels(const ImageMetaData &metadata,
-                            void *pixels,
-                            const size_t /*pixels_size*/,
-                            const bool /*associate_alpha*/)
+bool SkyLoader::load_pixels(const ImageMetaData &metadata, void *pixels)
 {
   /* Precompute Sky LUT */
   int width = metadata.width;
@@ -67,6 +64,7 @@ bool SkyLoader::load_pixels(const ImageMetaData &metadata,
                                              ozone_density);
   }
 
+  metadata.conform_pixels(pixels);
   return true;
 }
 

intern/cycles/scene/image_sky.h
@@ -24,16 +24,13 @@ class SkyLoader : public ImageLoader {
             const float ozone_density);
   ~SkyLoader() override;
 
-  bool load_metadata(const ImageDeviceFeatures &features, ImageMetaData &metadata) override;
+  bool load_metadata(ImageMetaData &metadata) override;
 
-  bool load_pixels(const ImageMetaData &metadata,
-                   void *pixels,
-                   const size_t /*pixels_size*/,
-                   const bool /*associate_alpha*/) override;
+  bool load_pixels(const ImageMetaData &metadata, void *pixels) override;
 
   string name() const override;
 
-  bool equals(const ImageLoader & /*other*/) const override;
+  bool equals(const ImageLoader &other) const override;
 };
 
 CCL_NAMESPACE_END

commit 527616679ca921d698b55f46282cd30ac09204bc

Fix: Cycles: Don't report errors for image texture nodes without images
This is considered valid by other parts of Blender.

This issue was introduced in 850b986d894.

Pull Request: https://projects.blender.org/blender/blender/pulls/163579

intern/cycles/scene/image_oiio.cpp
@@ -32,6 +32,10 @@ bool OIIOImageLoader::load_metadata(ImageMetaData &metadata,
                                     const ImageLoaderParams &params,
                                     Progress &progress)
 {
+  if (original_filepath_.empty()) {
+    return false;
+  }
+
   if (params.use_texture_cache) {
     const std::string &filepath = get_filepath();
     const bool found = resolve_tx(filepath,