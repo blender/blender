blender/blender

commit ce8c428ac1f1c4b02f8119a5b31d5a774153b78f

Cycles: Fix orthographic camera viewport motion vectors
Two issues are fixed:
- Some operations (orthographic pan/zoom, camera zoom, lens shift)
  resulted in zero viewport motion vectors.
- Orthographic cameras were not handled correctly in
  `camera_motion_vector_direction`. All rays meet at the same point at
  infinity for an orthographic camera.

The former caused ghosting for temporal denoisers.
The latter was so far not used for the denoising motion passes.

Pull Request: https://projects.blender.org/blender/blender/pulls/161797

---------

Co-authored-by: Brecht Van Lommel <brecht@blender.org>

intern/cycles/kernel/film/denoising_passes.h
@@ -291,6 +291,14 @@ ccl_device_forceinline void film_write_denoising_features_background(
     film_overwrite_pass_float(buffer + kernel_data.film.pass_denoising_depth, FLT_MAX);
   }
 
+  if (kernel_data.film.pass_denoising_backward_motion != PASS_UNUSED) {
+    const float4 speed = camera_motion_vector_direction(
+        kg, INTEGRATOR_STATE(state, ray, P), INTEGRATOR_STATE(state, ray, D));
+    const float3 backward_motion = make_float3(speed.x, speed.y, 0.0f);
+    film_overwrite_pass_float3(buffer + kernel_data.film.pass_denoising_backward_motion,
+                               backward_motion);
+  }
+
   /* 'pass_denoising_albedo' is written by 'film_write_emission_or_background_pass' */
 }
 #endif /* __DENOISING_FEATURES__ */

intern/cycles/blender/camera.cpp
@@ -101,9 +101,9 @@ class BlenderCamera {
   int render_width = 0;
   int render_height = 0;
 
-  BoundBox2D border = BoundBox2D();
-  BoundBox2D viewport_camera_border = BoundBox2D();
-  BoundBox2D pano_viewplane = BoundBox2D();
+  BoundBox2D border = BoundBox2D::full;
+  BoundBox2D viewport_camera_border = BoundBox2D::full;
+  BoundBox2D pano_viewplane = BoundBox2D::full;
   float pano_aspectratio = 0.0f;
 
   Transform matrix = transform_identity();

intern/cycles/kernel/camera/camera.h
@@ -662,7 +662,9 @@ ccl_device_forceinline float4 camera_motion_vector(KernelGlobals kg,
   return make_float4(motion_pre.x, motion_pre.y, motion_post.x, motion_post.y);
 }
 
-ccl_device_forceinline float4 camera_motion_vector_direction(KernelGlobals kg, const float3 D)
+ccl_device_forceinline float4 camera_motion_vector_direction(KernelGlobals kg,
+                                                             const float3 P,
+                                                             const float3 D)
 {
   Transform tfm;
   float3 motion_center;
@@ -683,6 +685,24 @@ ccl_device_forceinline float4 camera_motion_vector_direction(KernelGlobals kg, c
 
     tfm = kernel_data.cam.motion_pass_post;
     motion_post = normalize(transform_direction(&tfm, D));
+  }
+  else if (kernel_data.cam.type == CAMERA_ORTHOGRAPHIC) {
+    /* For one orthographic camera matrix, all rays meet at the same point at infinity. When
+     * rotating the camera, the motion vectors become infinite. To get reasonable values for both
+     * translation/zoom and rotation, we use points far away:
+     * - Rotation produces very large motion vectors pointing to outside the image, leading to
+     * rejection of the history.
+     * - Translation/zoom produce valid background motion vectors. */
+    float3 point_far = P + D * 1e4f;
+
+    ProjectionTransform projection = kernel_data.cam.worldtoraster;
+    motion_center = transform_perspective(&projection, point_far);
+
+    projection = kernel_data.cam.perspective_pre;
+    motion_pre = transform_perspective(&projection, point_far);
+
+    projection = kernel_data.cam.perspective_post;
+    motion_post = transform_perspective(&projection, point_far);
   }
   else if (kernel_data.cam.type != CAMERA_PANORAMA) {
     /* Perspective and orthographics camera use the world-to-raster matrix. */

intern/cycles/kernel/film/data_passes.h
@@ -213,7 +213,8 @@ ccl_device_inline void film_write_data_passes_background(
     }
 
     if (flag & PASSMASK(MOTION)) {
-      const float4 speed = camera_motion_vector_direction(kg, INTEGRATOR_STATE(state, ray, D));
+      const float4 speed = camera_motion_vector_direction(
+          kg, INTEGRATOR_STATE(state, ray, P), INTEGRATOR_STATE(state, ray, D));
       film_write_pass_float4(buffer + kernel_data.film.pass_motion, speed);
       film_write_pass_float(buffer + kernel_data.film.pass_motion_weight, 1.0f);
     }

intern/cycles/scene/camera.cpp
@@ -137,19 +137,29 @@ NODE_DEFINE(Camera)
   SOCKET_FLOAT(farclip, "Far Clip", 1e5f);
 
   SOCKET_FLOAT(viewplane.left, "Viewplane Left", 0);
-  SOCKET_FLOAT(viewplane.right, "Viewplane Right", 0);
+  SOCKET_FLOAT(viewplane.right, "Viewplane Right", 1);
   SOCKET_FLOAT(viewplane.bottom, "Viewplane Bottom", 0);
-  SOCKET_FLOAT(viewplane.top, "Viewplane Top", 0);
+  SOCKET_FLOAT(viewplane.top, "Viewplane Top", 1);
+
+  SOCKET_FLOAT(viewplane_pre.left, "Viewplane Pre Left", 0);
+  SOCKET_FLOAT(viewplane_pre.right, "Viewplane Pre Right", 0);
+  SOCKET_FLOAT(viewplane_pre.bottom, "Viewplane Pre Bottom", 0);
+  SOCKET_FLOAT(viewplane_pre.top, "Viewplane Pre Top", 0);
+
+  SOCKET_FLOAT(viewplane_post.left, "Viewplane Post Left", 0);
+  SOCKET_FLOAT(viewplane_post.right, "Viewplane Post Right", 0);
+  SOCKET_FLOAT(viewplane_post.bottom, "Viewplane Post Bottom", 0);
+  SOCKET_FLOAT(viewplane_post.top, "Viewplane Post Top", 0);
 
   SOCKET_FLOAT(border.left, "Border Left", 0);
-  SOCKET_FLOAT(border.right, "Border Right", 0);
+  SOCKET_FLOAT(border.right, "Border Right", 1);
   SOCKET_FLOAT(border.bottom, "Border Bottom", 0);
-  SOCKET_FLOAT(border.top, "Border Top", 0);
+  SOCKET_FLOAT(border.top, "Border Top", 1);
 
   SOCKET_FLOAT(viewport_camera_border.left, "Viewport Border Left", 0);
-  SOCKET_FLOAT(viewport_camera_border.right, "Viewport Border Right", 0);
+  SOCKET_FLOAT(viewport_camera_border.right, "Viewport Border Right", 1);
   SOCKET_FLOAT(viewport_camera_border.bottom, "Viewport Border Bottom", 0);
-  SOCKET_FLOAT(viewport_camera_border.top, "Viewport Border Top", 0);
+  SOCKET_FLOAT(viewport_camera_border.top, "Viewport Border Top", 1);
 
   SOCKET_FLOAT(offscreen_dicing_scale, "Offscreen Dicing Scale", 1.0f);
 
@@ -198,7 +208,7 @@ Camera::~Camera() = default;
 void Camera::compute_auto_viewplane()
 {
   if (camera_type == CAMERA_PANORAMA || camera_type == CAMERA_CUSTOM) {
-    viewplane = BoundBox2D();
+    viewplane = BoundBox2D::full;
   }
   else {
     const float aspect = (float)full_width / (float)full_height;
@@ -371,7 +381,13 @@ void Camera::update(Scene *scene)
       kcam->motion_pass_post = kcam->worldtocamera;
     }
     if (camera_type != CAMERA_PANORAMA && camera_type != CAMERA_CUSTOM) {
-      if (have_motion || fov != fov_pre || fov != fov_post) {
+      const BoundBox2D motion_viewplane_pre = viewplane_pre.is_empty() ? viewplane : viewplane_pre;
+      const BoundBox2D motion_viewplane_post = viewplane_post.is_empty() ? viewplane :
+                                                                           viewplane_post;
+
+      if (have_motion || fov != fov_pre || fov != fov_post || viewplane != motion_viewplane_pre ||
+          viewplane != motion_viewplane_post)
+      {
         /* Note the values for perspective_pre/perspective_post calculated for MOTION_PASS are
          * different to those calculated for MOTION_BLUR below, so the code has not been combined.
          */
@@ -382,8 +398,13 @@ void Camera::update(Scene *scene)
           cameratoscreen_post = projection_perspective(fov_post, nearclip, farclip);
         }
 
-        const ProjectionTransform cameratoraster_pre = screentoraster * cameratoscreen_pre;
-        const ProjectionTransform cameratoraster_post = screentoraster * cameratoscreen_post;
+        const Transform screentoraster_pre = ndctoraster * fulltoborder *
+                                             transform_from_viewplane(motion_viewplane_pre);
+        const Transform screentoraster_post = ndctoraster * fulltoborder *
+                                              transform_from_viewplane(motion_viewplane_post);
+
+        const ProjectionTransform cameratoraster_pre = screentoraster_pre * cameratoscreen_pre;
+        const ProjectionTransform cameratoraster_post = screentoraster_post * cameratoscreen_post;
         if (have_motion) {
           kcam->perspective_pre = cameratoraster_pre * transform_inverse(motion[0]);
           kcam->perspective_post = cameratoraster_post *
@@ -518,6 +539,10 @@ void Camera::update_interactive_motion()
   }
 
   set_fov_pre(fov);
+  set_viewplane_pre_left(viewplane.left);
+  set_viewplane_pre_right(viewplane.right);
+  set_viewplane_pre_bottom(viewplane.bottom);
+  set_viewplane_pre_top(viewplane.top);
 }
 
 void Camera::device_update(Device * /*device*/, DeviceScene *dscene, Scene *scene)

intern/cycles/scene/camera.h
@@ -156,6 +156,17 @@ class Camera : public Node {
   NODE_SOCKET_API(float, fov_pre)
   NODE_SOCKET_API(float, fov_post)
 
+  BoundBox2D viewplane_pre;
+  BoundBox2D viewplane_post;
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_pre, left)
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_pre, right)
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_pre, bottom)
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_pre, top)
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_post, left)
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_post, right)
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_post, bottom)
+  NODE_SOCKET_API_STRUCT_MEMBER(float, viewplane_post, top)
+
   /* computed camera parameters */
   ProjectionTransform screentoworld;
   ProjectionTransform rastertoworld;

intern/cycles/util/boundbox.h
@@ -195,10 +195,7 @@ __forceinline BoundBox intersect(const BoundBox &a, const BoundBox &b, const Bou
 
 class BoundBox2D {
  public:
-  float left = 0.0f;
-  float right = 1.0f;
-  float bottom = 0.0f;
-  float top = 1.0f;
+  float left, right, bottom, top;
 
   BoundBox2D() = default;
 
@@ -207,20 +204,29 @@ class BoundBox2D {
   {
   }
 
+  enum full_t { full = 0 };
+
+  BoundBox2D(full_t /*unused*/) : left(0.0f), right(1.0f), bottom(0.0f), top(1.0f) {}
+
   bool operator==(const BoundBox2D &other) const
   {
     return (left == other.left && right == other.right && bottom == other.bottom &&
             top == other.top);
   }
 
-  float width()
+  float width() const
   {
     return right - left;
   }
 
-  float height()
+  float height() const
   {
     return top - bottom;
+  }
+
+  bool is_empty() const
+  {
+    return width() == 0.0f && height() == 0.0f;
   }
 
   BoundBox2D operator*(float f) const

intern/cycles/util/transform.cpp
@@ -194,7 +194,7 @@ void transform_motion_decompose(DecomposedTransform *decomp,
   }
 }
 
-Transform transform_from_viewplane(BoundBox2D &viewplane)
+Transform transform_from_viewplane(const BoundBox2D &viewplane)
 {
   return transform_scale(1.0f / (viewplane.right - viewplane.left),
                          1.0f / (viewplane.top - viewplane.bottom),

commit c6d56f33b9d44fa5175ed6dffdf00d5911268605

Fix: Cycles: Background denoising motion vectors scaled by sample count
Incorrectly used the function for single samples, should use the one
that accumulates.

This issue was introduced in ce8c428ac1f.

Pull Request: https://projects.blender.org/blender/blender/pulls/163579

intern/cycles/kernel/film/denoising_passes.h
@@ -295,8 +295,8 @@ ccl_device_forceinline void film_write_denoising_features_background(
     const float4 speed = camera_motion_vector_direction(
         kg, INTEGRATOR_STATE(state, ray, P), INTEGRATOR_STATE(state, ray, D));
     const float3 backward_motion = make_float3(speed.x, speed.y, 0.0f);
-    film_overwrite_pass_float3(buffer + kernel_data.film.pass_denoising_backward_motion,
-                               backward_motion);
+    film_write_pass_float3(buffer + kernel_data.film.pass_denoising_backward_motion,
+                           backward_motion);
   }
 
   /* 'pass_denoising_albedo' is written by 'film_write_emission_or_background_pass' */