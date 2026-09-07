blender/blender

commit 0636c9203686c21b0842eb033c858657b461d8ab

Cycles: Add Quaternion attribute type
The main goal is to bring a bit of structure to the use of quaternions
in the Cycles kernel.

Prior to this change there was no dedicated quaternion type and float4
was used instead, and quaternions are stored as (x, y, z, w) matching
naming between the quaternion and float4 fields. However, Blender uses
(w, x, y, z) quaternion order for attributes, which is different from
what Cycles uses. Without dedicated type this either leads to different
quaternion orders depending whether it comes from attribute, or makes
it intrinsically not possible to use implicit sharing.

This change introduces Cycles Quaternion type which is compatible with
Blender attribute math::Quaternion in both layout and alignment, making
it possible to benefit from implicit sharing and use a nice structure
in the kernel.

This change does not modify the existing quaternion usage in the kernel
which is currently used for transform decomposition and interpolation.

There is currently no SIMD for the Quaternion type, which allows to
avoid any special alignment requirement and share attributes with
Blender, but performance might not be ideal.

This attribute type will be used for representing gaussian splat
rotation.

The attribute access and interpolation matches behavior prior to this
change. Added some basic tests for quaternion attribute access for RGB
and alpha, mesh and volume attributes.

Ref #159470

Pull Request: https://projects.blender.org/blender/blender/pulls/163342

intern/cycles/kernel/svm/attribute.h
@@ -144,6 +144,32 @@ svm_node_attr_surface_eval(KernelGlobals kg,
     }
   }
 
+  if (desc.type == NODE_ATTR_QUATERNION) {
+    /* Conversion from quaternion to RGB is not well defined.
+     * Follow the same logic as what it was when quaternions were stored as float4. */
+    if constexpr (is_dual_v<Float3Type>) {
+      const dual<Quaternion> q = primitive_surface_attribute<dual<Quaternion>>(kg, sd, desc);
+      if (type == NODE_ATTR_OUTPUT_FLOAT) {
+        return make_float3(average(make_float3(q)));
+      }
+      if (type == NODE_ATTR_OUTPUT_FLOAT_ALPHA) {
+        return make_float3(dual<float>(q.val.w, q.dx.w, q.dy.w));
+      }
+      return make_float3(q);
+    }
+    else {
+      const Quaternion q = primitive_surface_attribute<Quaternion>(kg, sd, desc);
+      const float4 f = make_float4(q);
+      if (type == NODE_ATTR_OUTPUT_FLOAT) {
+        return make_float3(average(make_float3(f)));
+      }
+      if (type == NODE_ATTR_OUTPUT_FLOAT_ALPHA) {
+        return make_float3(f.w);
+      }
+      return make_float3(f);
+    }
+  }
+
   Float3Type f = primitive_surface_attribute<Float3Type>(kg, sd, desc);
   if (type == NODE_ATTR_OUTPUT_FLOAT) {
     return make_float3(average(f));

intern/cycles/kernel/svm/vertex_color.h
@@ -28,6 +28,14 @@ ccl_device_noinline void svm_node_vertex_color(KernelGlobals kg,
       color = make_float3(vertex_color);
       alpha = vertex_color.w;
     }
+    else if (descriptor.type == NODE_ATTR_QUATERNION) {
+      /* Conversion from quaternion to RGB is not well defined.
+       * Follow the same logic as what it was when quaternions were stored as float4. */
+      const float4 vertex_color = make_float4(
+          primitive_surface_attribute<Quaternion>(kg, sd, descriptor));
+      color = make_float3(vertex_color);
+      alpha = vertex_color.w;
+    }
     else {
       color = primitive_surface_attribute<float3>(kg, sd, descriptor);
       alpha = 1.0f;
@@ -59,6 +67,20 @@ ccl_device_noinline void svm_node_vertex_color_derivative(
   if (is_attribute_found(descriptor)) {
     if (descriptor.type == NODE_ATTR_FLOAT4 || descriptor.type == NODE_ATTR_RGBA) {
       dual4 vertex_color = primitive_surface_attribute<dual4>(kg, sd, descriptor);
+      if (node.bump_offset == NODE_BUMP_OFFSET_DX) {
+        vertex_color.val += vertex_color.dx * node.bump_filter_width;
+      }
+      else if (node.bump_offset == NODE_BUMP_OFFSET_DY) {
+        vertex_color.val += vertex_color.dy * node.bump_filter_width;
+      }
+      color = make_float3(vertex_color.val);
+      alpha = vertex_color.val.w;
+    }
+    else if (descriptor.type == NODE_ATTR_QUATERNION) {
+      /* Conversion from quaternion to RGB is not well defined.
+       * Follow the same logic as what it was when quaternions were stored as float4. */
+      dual<Quaternion> vertex_color = primitive_surface_attribute<dual<Quaternion>>(
+          kg, sd, descriptor);
       if (node.bump_offset == NODE_BUMP_OFFSET_DX) {
         vertex_color.val += vertex_color.dx * node.bump_filter_width;
       }

intern/cycles/kernel/geom/attribute.h
@@ -254,6 +254,13 @@ ccl_device_template_spec Transform attribute_data_fetch(KernelGlobals kg,
   return tfm;
 }
 
+ccl_device_template_spec Quaternion attribute_data_fetch(KernelGlobals kg,
+                                                         AttributeElement /*element*/,
+                                                         const int offset)
+{
+  return kernel_data_fetch(attributes_quaternion, offset);
+}
+
 /* Transform matrix attribute on meshes */
 
 ccl_device Transform primitive_attribute_matrix(KernelGlobals kg, const AttributeDescriptor desc)

intern/cycles/scene/attribute.h
@@ -44,13 +44,14 @@ enum AttrKernelDataType {
   FLOAT4 = 3,
   UCHAR4 = 4,
   NORMAL = 5,
-  NUM = 6
+  QUATERNION = 6,
+  NUM = 7
 };
 
 /* Attribute
  *
  * Arbitrary data layers on meshes.
- * Supported types: Float, Color, Vector, Normal, Point */
+ * Supported types: Float, Color, Vector, Normal, Point, Quaternion. */
 
 class Attribute {
  public:

intern/cycles/blender/attribute_convert.h
@@ -109,13 +109,12 @@ template<> struct AttributeConverter<int8_t> {
   }
 };
 template<> struct AttributeConverter<blender::math::Quaternion> {
-  using CyclesT = float4;
-  static constexpr auto type_desc = TypeFloat4;
-  /* Allocation alignment is not compatible with Cycles */
-  static constexpr bool layout_compatible = false;
+  using CyclesT = Quaternion;
+  static constexpr auto type_desc = TypeQuaternion;
+  static constexpr bool layout_compatible = true;
   static CyclesT convert(const blender::math::Quaternion &value)
   {
-    return make_float4(value.w, value.x, value.y, value.z);
+    return make_quaternion(value.w, value.x, value.y, value.z);
   }
 };
 

intern/cycles/device/memory.h
@@ -212,6 +212,12 @@ template<> struct device_type_traits<uint64_t> {
   static_assert(sizeof(uint64_t) == num_elements * datatype_size(data_type));
 };
 
+template<> struct device_type_traits<Quaternion> {
+  static const DataType data_type = TYPE_FLOAT;
+  static const size_t num_elements = 4;
+  static_assert(sizeof(Quaternion) == num_elements * datatype_size(data_type));
+};
+
 /* Device Memory
  *
  * Base class for all device memory. This should not be allocated directly,

intern/cycles/kernel/CMakeLists.txt
@@ -296,6 +296,7 @@ set(SRC_UTIL_HEADERS
   ../util/math_int8.h
   ../util/math_dual.h
   ../util/math_float3x3.h
+  ../util/math_quaternion.h
   ../util/projection.h
   ../util/projection_inverse.h
   ../util/rect.h
@@ -324,6 +325,7 @@ set(SRC_UTIL_HEADERS
   ../util/types_ushort4.h
   ../util/types_dual.h
   ../util/types_float3x3.h
+  ../util/types_quaternion.h
 )
 
 set(LIB

intern/cycles/kernel/data_arrays.h
@@ -53,6 +53,7 @@ KERNEL_DATA_ARRAY(packed_float3, attributes_float3)
 KERNEL_DATA_ARRAY(float4, attributes_float4)
 KERNEL_DATA_ARRAY(uchar4, attributes_uchar4)
 KERNEL_DATA_ARRAY(packed_normal, attributes_normal)
+KERNEL_DATA_ARRAY(Quaternion, attributes_quaternion)
 
 /* lights */
 KERNEL_DATA_ARRAY(KernelLightDistribution, light_distribution)

commit 4fa3075d21d15dcaa1394f65903afc657264d6e7

Fix: Cycles: Quaternion attribute alpha differs in derivative path
Use make_float4 in both paths, as it swizzles components.

This issue was introduced in 0636c920368.

Pull Request: https://projects.blender.org/blender/blender/pulls/163579

intern/cycles/kernel/svm/attribute.h
@@ -161,7 +161,9 @@ svm_node_attr_surface_eval(KernelGlobals kg,
         return make_float3(average(make_float3(q)));
       }
       if (type == NODE_ATTR_OUTPUT_FLOAT_ALPHA) {
-        return make_float3(dual<float>(q.val.w, q.dx.w, q.dy.w));
+        /* Use make_float4, which is used for the non-dual case and swizzles components. */
+        return make_float3(
+            dual<float>(make_float4(q.val).w, make_float4(q.dx).w, make_float4(q.dy).w));
       }
       return make_float3(q);
     }

intern/cycles/kernel/svm/vertex_color.h
@@ -87,8 +87,9 @@ ccl_device_noinline void svm_node_vertex_color_derivative(
       else if (node.bump_offset == NODE_BUMP_OFFSET_DY) {
         vertex_color.val += vertex_color.dy * node.bump_filter_width;
       }
-      color = make_float3(vertex_color.val);
-      alpha = vertex_color.val.w;
+      const float4 f = make_float4(vertex_color.val);
+      color = make_float3(f);
+      alpha = f.w;
     }
     else {
       dual3 vertex_color = primitive_surface_attribute<dual3>(kg, sd, descriptor);