blender/blender

commit 79d6d1659cbee1c61e43d22340263f4d748e58df

Cycles: Add spherical harmonics attribute
Includes possibility of adding spherical harmonics on geometry, as well
as includes spherical harmonics evaluation.

Spherical harmonics are stored quantized to 8 bit integer, and always
use degree 3. The reason for the limited degree is the memory: degree
3 requires relatively small amount of coefficients (45 int8_t, which
is similar to how realtime renderers pack them into float3x4). Going
higher dimension adds a lot more coefficients to store, and so far it
doesn't seem there are gsplat data-sets that are trained to a degree
higher than 3.

The attribute is currently unused. It is preparation work foe the
gsplat project to make reviews easier.

Ref #159470

Pull Request: https://projects.blender.org/blender/blender/pulls/163409

intern/cycles/scene/attribute.cpp
@@ -24,10 +24,10 @@ Attribute::Attribute(ustring name,
                      AttributePrimitive prim)
     : name(name), std(ATTR_STD_NONE), type(type), element(element), flags(0), modified(true)
 {
-  /* string and matrix not supported! */
+  /* String is not supported! */
   assert(type == TypeFloat || type == TypeColor || type == TypePoint || type == TypeVector ||
          type == TypeNormal || type == TypeMatrix || type == TypeFloat2 || type == TypeFloat4 ||
-         type == TypeRGBA || type == TypeQuaternion);
+         type == TypeRGBA || type == TypeQuaternion || type == TypePackedSphericalHarmonics);
 
   if (element & ATTR_ELEMENT_VOXEL) {
     auto *data = GuardedAllocator<ImageHandle>().allocate(1);
@@ -353,7 +353,9 @@ size_t Attribute::data_sizeof() const
   if (type == TypeQuaternion) {
     return sizeof(Quaternion);
   }
-
+  if (type == TypePackedSphericalHarmonics) {
+    return sizeof(PackedSphericalHarmonics);
+  }
   return sizeof(packed_float3);
 }
 
@@ -605,6 +607,10 @@ AttrKernelDataType Attribute::kernel_type(const Attribute &attr)
 
   if (attr.type == TypeQuaternion) {
     return AttrKernelDataType::QUATERNION;
+  }
+
+  if (attr.type == TypePackedSphericalHarmonics) {
+    return AttrKernelDataType::SPHERICAL_HARMONICS;
   }
 
   return AttrKernelDataType::FLOAT3;
@@ -1140,12 +1146,12 @@ void AttributeSet::clear_modified()
 void AttributeSet::tag_modified(const Attribute &attr)
 {
   const AttrKernelDataType kernel_type = Attribute::kernel_type(attr);
-  modified_flag |= (1u << kernel_type);
+  modified_flag |= (1u << uint(kernel_type));
 }
 
-bool AttributeSet::modified(AttrKernelDataType kernel_type) const
+bool AttributeSet::modified(const AttrKernelDataType kernel_type) const
 {
-  return (modified_flag & (1u << kernel_type)) != 0;
+  return (modified_flag & (1u << uint(kernel_type))) != 0;
 }
 
 /* AttributeRequest */

intern/cycles/util/types_spherical_harmonics.h
@@ -1,0 +1,188 @@
+/* SPDX-FileCopyrightText: 2026 Blender Foundation
+ *
+ * SPDX-License-Identifier: Apache-2.0 */
+
+#pragma once
+
+#include "util/defines.h"
+#include "util/math_float3.h"
+#include "util/types_base.h"
+#include "util/types_float3.h"
+
+#if !defined(__KERNEL_GPU__)
+#  include <cstring> /* for memset(). */
+#endif
+
+CCL_NAMESPACE_BEGIN
+
+/* A packed representation of spherical harmonics.
+ * Supports spherical harmonics for degrees 1 to 3. The values are quantized to 8 bit.
+ * Spherical harmonics for degree 0 is stored in the base radiance attribute.
+ *
+ * The order is: <L=1 M=-1>, <L=1 M=0>, <L=1 M=1>, <L=2 M=-2> ...
+ * where L is the band index, M is the  basis function.
+ *
+ * NOTE: There is an utility spherical_harmonics_get<L, M> to access bands in a more semantic way.
+ */
+struct PackedSphericalHarmonics {
+  /* Some formats (like SPZ) define the maximum degree as 4.
+   * However, in practice it is hard to run across dataset that is actually trained using such
+   * high degree, and the difference that the higher degree brings is probably barely perceivable.
+   * To keep logic simple, assume the most commonly used degree of 3, and allocate coefficients for
+   * it. */
+  ccl_static_constexpr int MAX_DEGREES = 3;
+  ccl_static_constexpr int MAX_COEFFICIENTS = 15;
+
+  uint8_t coefficients[3 * MAX_COEFFICIENTS];
+
+  /* NOTE: Can not have methods here, as the kernel references data from global memory, which makes
+   * it tricky on Metal that either doesn't support it or requires some non-trivial way, as calling
+   * methods from a `ccl_global PackedSphericalHarmonics *` conflicts with the address space for
+   * `this`. */
+};
+
+/* Set all spherical harmonics values to 0. */
+ccl_device_forceinline void spherical_harmonics_fill_zero(
+    ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+#if !defined(__KERNEL_GPU__)
+  memset(
+      packed_spherical_harmonics.coefficients, 0, sizeof(packed_spherical_harmonics.coefficients));
+#else
+  /* Metal does not support memset() call. */
+  for (int i = 0; i < 3 * PackedSphericalHarmonics::MAX_COEFFICIENTS; ++i) {
+    packed_spherical_harmonics.coefficients[i] = 0;
+  }
+#endif
+}
+
+/* Quantize spherical harmonics coefficients from float to uint8_t.
+ * Similar to the float_to_byte(), but deals with input values in the range [-1 .. 1]. */
+ccl_device_forceinline uint8_t quantize_spherical_harmonics(const float value)
+{
+  const float rescaled = saturatef((value + 1.0f) / 2.0f);
+  return ((rescaled <= 0.0f) ? 0 :
+                               ((rescaled > (1.0f - 0.5f / 255.0f)) ?
+                                    255 :
+                                    (uint8_t)((255.0f * rescaled) + 0.5f)));  // NOLINT
+}
+
+ccl_device_forceinline float unquantize_spherical_harmonics(const uint8_t value)
+{
+  return float(value) * (1.0f / 255.0f) * 2.0f - 1.0f;
+}
+
+/* Set the spherical harmonic coefficient by its flat index. */
+ccl_device_forceinline void spherical_harmonics_set_coefficient(
+    ccl_global PackedSphericalHarmonics &packed_spherical_harmonics,
+    const int index,
+    const float3 value)
+{
+  util_assert(index >= 0);
+  util_assert(index < PackedSphericalHarmonics::MAX_COEFFICIENTS);
+  const int i = index * 3;
+  packed_spherical_harmonics.coefficients[i + 0] = quantize_spherical_harmonics(value.x);
+  packed_spherical_harmonics.coefficients[i + 1] = quantize_spherical_harmonics(value.y);
+  packed_spherical_harmonics.coefficients[i + 2] = quantize_spherical_harmonics(value.z);
+}
+
+/* Get unquantized spherical harmonics coefficient by its flat index. */
+ccl_device_forceinline float3 spherical_harmonics_get_coefficient(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics, const int index)
+{
+  util_assert(index >= 0);
+  util_assert(index < PackedSphericalHarmonics::MAX_COEFFICIENTS);
+  const int i = index * 3;
+  return make_float3(
+      unquantize_spherical_harmonics(packed_spherical_harmonics.coefficients[i + 0]),
+      unquantize_spherical_harmonics(packed_spherical_harmonics.coefficients[i + 1]),
+      unquantize_spherical_harmonics(packed_spherical_harmonics.coefficients[i + 2]));
+}
+
+/* Get unquantized spherical harmonics coefficient by the band L and the basis function M. */
+template<int L, int M>
+ccl_device_inline float3
+spherical_harmonics_get(const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics);
+
+/* Band L=1. */
+ccl_device_template_spec float3 spherical_harmonics_get<1, -1>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 0);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<1, 0>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 1);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<1, 1>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 2);
+}
+
+/* Band L=2. */
+ccl_device_template_spec float3 spherical_harmonics_get<2, -2>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 3);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<2, -1>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 4);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<2, 0>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 5);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<2, 1>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 6);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<2, 2>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 7);
+}
+
+/* Band L=3. */
+ccl_device_template_spec float3 spherical_harmonics_get<3, -3>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 8);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<3, -2>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 9);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<3, -1>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 10);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<3, 0>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 11);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<3, 1>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 12);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<3, 2>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 13);
+}
+ccl_device_template_spec float3 spherical_harmonics_get<3, 3>(
+    const ccl_global PackedSphericalHarmonics &packed_spherical_harmonics)
+{
+  return spherical_harmonics_get_coefficient(packed_spherical_harmonics, 14);
+}
+
+CCL_NAMESPACE_END

intern/cycles/test/util_types_spherical_harmonics_test.cpp
@@ -1,0 +1,52 @@
+/* SPDX-FileCopyrightText: 2011-2026 Blender Foundation
+ *
+ * SPDX-License-Identifier: Apache-2.0 */
+
+/* Note: These fixtures test default micro-architecture optimization defined in the
+ * util/optimization.h. */
+
+#include "util/types_spherical_harmonics.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "util/log.h"
+
+CCL_NAMESPACE_BEGIN
+
+/* Use as:
+ * float3 vector1;
+ * float3 vector2;
+ * EXPECT_THAT(vector1, IsNearFloat3(vector2)); */
+MATCHER_P2(IsNearFloat3, expected, eps, "")
+{
+  for (int i = 0; i < 3; ++i) {
+    if (fabsf(arg[i] - expected[i]) > eps) {
+      *result_listener << "component " << i << " should be " << expected[i];
+      return false;
+    }
+  }
+  return true;
+}
+
+class PackedSphericalHarmonicsTest : public ::testing::Test {
+  void SetUp() override
+  {
+    /* The micro-architecture check is not needed here, but use it here as a demonstration of how
+     * it can be implemented in a clear way. */
+    // GTEST_SKIP() << "Test skipped due to uarch capability";
+  }
+};
+
+TEST_F(PackedSphericalHarmonicsTest, PackUnpack)
+{
+  PackedSphericalHarmonics sh;
+
+  spherical_harmonics_fill_zero(sh);
+
+  spherical_harmonics_set_coefficient(sh, 1, make_float3(0.1f, -0.2f, 0.3f));
+  EXPECT_THAT(spherical_harmonics_get_coefficient(sh, 1),
+              IsNearFloat3(make_float3(0.1f, -0.2f, 0.3f), 1.0f / 127.0f));
+}
+
+CCL_NAMESPACE_END

intern/cycles/device/memory.h
@@ -41,6 +41,7 @@ enum DataType {
   TYPE_UINT16,
   TYPE_UINT,
   TYPE_INT,
+  TYPE_INT8,
   TYPE_FLOAT,
   TYPE_HALF,
   TYPE_UINT64,
@@ -61,6 +62,8 @@ static constexpr size_t datatype_size(DataType datatype)
       return sizeof(uint16_t);
     case TYPE_INT:
       return sizeof(int);
+    case TYPE_INT8:
+      return sizeof(int8_t);
     case TYPE_HALF:
       return sizeof(half);
     case TYPE_UINT64:
@@ -216,6 +219,11 @@ template<> struct device_type_traits<Quaternion> {
   static const DataType data_type = TYPE_FLOAT;
   static const size_t num_elements = 4;
   static_assert(sizeof(Quaternion) == num_elements * datatype_size(data_type));
+};
+
+template<> struct device_type_traits<PackedSphericalHarmonics> {
+  static const DataType data_type = TYPE_INT8;
+  static const size_t num_elements = sizeof(PackedSphericalHarmonics);
 };
 
 /* Device Memory
@@ -611,7 +619,7 @@ template<typename T> class device_vector : public device_memory {
  * goes out of scope, which should happen before base memory is freed.
  *
  * NOTE: some devices require offset and size of the sub_ptr to be properly
- * aligned to device->mem_address_alingment(). */
+ * aligned to device->mem_address_alignment(). */
 
 class device_sub_ptr {
  public:

intern/cycles/kernel/CMakeLists.txt
@@ -297,6 +297,7 @@ set(SRC_UTIL_HEADERS
   ../util/math_dual.h
   ../util/math_float3x3.h
   ../util/math_quaternion.h
+  ../util/math_spherical_harmonics.h
   ../util/projection.h
   ../util/projection_inverse.h
   ../util/rect.h
@@ -315,6 +316,7 @@ set(SRC_UTIL_HEADERS
   ../util/types_int8.h
   ../util/types_normal.h
   ../util/types_spectrum.h
+  ../util/types_spherical_harmonics.h
   ../util/types_rgbe.h
   ../util/types_uchar2.h
   ../util/types_uchar3.h

intern/cycles/kernel/data_arrays.h
@@ -54,6 +54,7 @@ KERNEL_DATA_ARRAY(float4, attributes_float4)
 KERNEL_DATA_ARRAY(uchar4, attributes_uchar4)
 KERNEL_DATA_ARRAY(packed_normal, attributes_normal)
 KERNEL_DATA_ARRAY(Quaternion, attributes_quaternion)
+KERNEL_DATA_ARRAY(PackedSphericalHarmonics, attributes_spherical_harmonics)
 
 /* lights */
 KERNEL_DATA_ARRAY(KernelLightDistribution, light_distribution)

intern/cycles/kernel/geom/volume.h
@@ -101,6 +101,8 @@ ccl_device float4 volume_attribute_float4(KernelGlobals kg,
         return kernel_data_fetch(attributes_float4, desc.offset);
       case NODE_ATTR_QUATERNION:
         return make_float4(kernel_data_fetch(attributes_quaternion, desc.offset));
+      case NODE_ATTR_SPHERICAL_HARMONICS:
+        return make_float4(0.0f, 0.0f, 0.0f, 1.0f);
       case NODE_ATTR_MATRIX:
         return zero_float4();
     }

intern/cycles/kernel/svm/attribute.h
@@ -74,6 +74,14 @@ svm_node_attr_surface_eval(KernelGlobals kg,
 {
   using FloatType = dual_scalar_t<Float3Type>;
 
+  /* Spherical harmonics attribute can not be currently accessed.
+   * It is stored as PackedSphericalHarmonics that does not have a float or float3 representation.
+   */
+  if (desc.type == NODE_ATTR_SPHERICAL_HARMONICS) {
+    const float value = (type == NODE_ATTR_OUTPUT_FLOAT_ALPHA) ? 1.0f : 0.0f;
+    return make_float3(FloatType(value));
+  }
+
   if (sd->type == PRIMITIVE_LAMP && node.attr == ATTR_STD_UV) {
     Float3Type uv(make_float3(1.0f - sd->u - sd->v, sd->u, 0.0f));
     if constexpr (is_dual_v<Float3Type>) {

commit 26dbc827082a007c4cbcb858d11f1668243eb138

Fix: Cycles: Zeroing spherical harmonics gives -1 instead of 0
The way these are quantized, 0.0f is not the same as integer 0.

This issue was introduced in 79d6d1659cb.

Pull Request: https://projects.blender.org/blender/blender/pulls/163579

intern/cycles/scene/attribute.cpp
@@ -454,6 +454,11 @@ bool Attribute::same_storage(const TypeDesc a, const TypeDesc b)
 
 void Attribute::zero_data(void *dst)
 {
+  if (type == TypePackedSphericalHarmonicsRest) {
+    spherical_harmonics_rest_fill_zero(*static_cast<PackedSphericalHarmonicsRest *>(dst));
+    return;
+  }
+
   memset(dst, 0, data_sizeof());
 }
 

intern/cycles/util/types_spherical_harmonics.h
@@ -47,22 +47,6 @@ struct PackedSphericalHarmonicsRest {
    * for `this`. */
 };
 
-/* Set all spherical harmonics values to 0. */
-ccl_device_forceinline void spherical_harmonics_rest_fill_zero(
-    ccl_global PackedSphericalHarmonicsRest &packed_spherical_harmonics_rest)
-{
-#if !defined(__KERNEL_GPU__)
-  memset(packed_spherical_harmonics_rest.coefficients,
-         0,
-         sizeof(packed_spherical_harmonics_rest.coefficients));
-#else
-  /* Metal does not support memset() call. */
-  for (int i = 0; i < 3 * PackedSphericalHarmonicsRest::MAX_COEFFICIENTS; ++i) {
-    packed_spherical_harmonics_rest.coefficients[i] = 0;
-  }
-#endif
-}
-
 /* Quantize spherical harmonics coefficients from float to uint8_t.
  * Similar to the float_to_byte(), but deals with input values in the range [-1 .. 1]. */
 ccl_device_forceinline uint8_t quantize_spherical_harmonics(const float value)
@@ -74,6 +58,24 @@ ccl_device_forceinline uint8_t quantize_spherical_harmonics(const float value)
 ccl_device_forceinline float unquantize_spherical_harmonics(const uint8_t value)
 {
   return byte_to_float(value) * 2.0f - 1.0f;
+}
+
+/* Set all spherical harmonics values to 0. */
+ccl_device_forceinline void spherical_harmonics_rest_fill_zero(
+    ccl_global PackedSphericalHarmonicsRest &packed_spherical_harmonics_rest)
+{
+  /* Note this is not the same as integer 0. */
+  const uint8_t zero = quantize_spherical_harmonics(0.0f);
+#if !defined(__KERNEL_GPU__)
+  memset(packed_spherical_harmonics_rest.coefficients,
+         zero,
+         sizeof(packed_spherical_harmonics_rest.coefficients));
+#else
+  /* Metal does not support memset() call. */
+  for (int i = 0; i < 3 * PackedSphericalHarmonicsRest::MAX_COEFFICIENTS; ++i) {
+    packed_spherical_harmonics_rest.coefficients[i] = zero;
+  }
+#endif
 }
 
 /* Set the spherical harmonic coefficient by its flat index. */

intern/cycles/test/util_types_spherical_harmonics_test.cpp
@@ -44,6 +44,11 @@ TEST_F(PackedSphericalHarmonicsTest, PackUnpack)
 
   spherical_harmonics_rest_fill_zero(sh_rest);
 
+  for (int i = 0; i < PackedSphericalHarmonicsRest::MAX_COEFFICIENTS; ++i) {
+    EXPECT_THAT(spherical_harmonics_rest_get_coefficient(sh_rest, i),
+                IsNearFloat3(make_float3(0.0f, 0.0f, 0.0f), 1.0f / 127.0f));
+  }
+
   spherical_harmonics_rest_set_coefficient(sh_rest, 1, make_float3(0.1f, -0.2f, 0.3f));
   EXPECT_THAT(spherical_harmonics_rest_get_coefficient(sh_rest, 1),
               IsNearFloat3(make_float3(0.1f, -0.2f, 0.3f), 1.0f / 127.0f));