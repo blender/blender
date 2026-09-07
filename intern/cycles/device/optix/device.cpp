blender/blender

commit 86374b6b103e345c817d58a9c27b51d50f501d72

Cycles: Add HW-RT to the render summary report
Makes it more explicit in the report whether HW-RT was used or not.

Pull Request: https://projects.blender.org/blender/blender/pulls/152313

intern/cycles/integrator/path_trace.cpp
@@ -1307,6 +1307,9 @@ static string device_info_list_report(const string &message, const DeviceInfo &d
 
   if (device_info.multi_devices.empty()) {
     result += full_device_info_description(device_info) + "\n";
+    result += pad +
+              "    Hardware Ray-Tracing: " + (device_info.use_hardware_raytracing ? "On" : "Off") +
+              "\n";
     return result;
   }
 
@@ -1317,6 +1320,8 @@ static string device_info_list_report(const string &message, const DeviceInfo &d
     }
 
     result += full_device_info_description(sub_device_info) + "\n";
+    result += pad + "    Hardware Ray-Tracing: " +
+              (sub_device_info.use_hardware_raytracing ? "On" : "Off") + "\n";
 
     is_first = false;
   }

commit 976ca9d244790d75837fbbb6c123961a8feaebbf

Fix: Cycles: Logging shows wrong hardware ray-tracing state for OptiX
Set it in the device info, the logging is the only place that uses it
for OptiX currently as CUDA is a separate device type, unlike other
backends.

Also don't print it for denoising devices, it's irrelevant.

This issue was introduced in 86374b6b103.

Pull Request: https://projects.blender.org/blender/blender/pulls/163579

intern/cycles/device/optix/device.cpp
@@ -84,6 +84,9 @@ void device_optix_info(const vector<DeviceInfo> &cuda_devices, vector<DeviceInfo
 
     info.type = DEVICE_OPTIX;
     info.id += "_OptiX";
+
+    info.use_hardware_raytracing = true;
+
 #  if defined(WITH_OSL) && defined(OSL_USE_OPTIX) && \
       (OSL_VERSION_MINOR >= 13 || OSL_VERSION_MAJOR > 1)
     info.has_osl = true;

intern/cycles/integrator/path_trace.cpp
@@ -1366,16 +1366,20 @@ static string full_device_info_description(const DeviceInfo &device_info)
  *
  * Note that the newlines are placed in a way so that the result can be easily concatenated to the
  * full report. */
-static string device_info_list_report(const string &message, const DeviceInfo &device_info)
+static string device_info_list_report(const string &message,
+                                      const DeviceInfo &device_info,
+                                      const bool show_hardware_raytracing)
 {
   string result = "\n" + message + ": ";
   const string pad(message.length() + 2, ' ');
 
   if (device_info.multi_devices.empty()) {
     result += full_device_info_description(device_info) + "\n";
-    result += pad +
-              "    Hardware Ray-Tracing: " + (device_info.use_hardware_raytracing ? "On" : "Off") +
-              "\n";
+    if (show_hardware_raytracing) {
+      result += pad +
+                "  Hardware Ray-Tracing: " + (device_info.use_hardware_raytracing ? "On" : "Off") +
+                "\n";
+    }
     return result;
   }
 
@@ -1386,8 +1390,10 @@ static string device_info_list_report(const string &message, const DeviceInfo &d
     }
 
     result += full_device_info_description(sub_device_info) + "\n";
-    result += pad + "    Hardware Ray-Tracing: " +
-              (sub_device_info.use_hardware_raytracing ? "On" : "Off") + "\n";
+    if (show_hardware_raytracing) {
+      result += pad + "  Hardware Ray-Tracing: " +
+                (sub_device_info.use_hardware_raytracing ? "On" : "Off") + "\n";
+    }
 
     is_first = false;
   }
@@ -1404,7 +1410,7 @@ static string path_trace_devices_report(const vector<unique_ptr<PathTraceWork>> 
     device_info.multi_devices.push_back(path_trace_work->get_device()->info);
   }
 
-  return device_info_list_report("Path tracing on", device_info);
+  return device_info_list_report("Path tracing on", device_info, true);
 }
 
 static string denoiser_device_report(const Denoiser *denoiser)
@@ -1422,7 +1428,7 @@ static string denoiser_device_report(const Denoiser *denoiser)
     return "";
   }
 
-  return device_info_list_report("Denoising on", denoiser_device->info);
+  return device_info_list_report("Denoising on", denoiser_device->info, false);
 }
 
 string PathTrace::full_report() const