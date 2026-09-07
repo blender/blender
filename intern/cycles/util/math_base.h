blender/blender

commit f8ff821217e2c1cb1f89db3892a5a9cdd47cde17

Refactor: Cycles: Don't pull an entire Metal namespace
While it makes some code a bit longer, but it also makes parts more
explicit w.r.t where things are coming from.

More importantly, it allows to have Cycles-specific data-types named
the same as Metal standard library calls them. For example, this change
will allow having own float3x3 type, which might use Metal's one for an
underlying implementation.

Ref #159470

intern/cycles/util/math_base.h
@@ -106,6 +106,16 @@ ccl_device_inline float fminf(const float a, const float b)
 #  endif /* _WIN32 */
 #endif   /* __HIP__, __KERNEL_ONEAPI__ */
 
+#if defined(__KERNEL_METAL__)
+using metal::isfinite;
+using metal::isnan;
+using metal::sqrt;
+
+using metal::abs;
+using metal::max;
+using metal::min;
+#endif /* defined(__KERNEL_METAL__) */
+
 #if !defined(__KERNEL_GPU__) || defined(__KERNEL_ONEAPI__)
 #  ifndef __KERNEL_ONEAPI__
 using std::isfinite;
@@ -362,7 +372,11 @@ ccl_device_inline float ensure_finite(const float v)
   return isfinite_safe(v) ? v : 0.0f;
 }
 
-#if !defined(__KERNEL_METAL__)
+#if defined(__KERNEL_METAL__)
+using metal::clamp;
+using metal::mix;
+using metal::smoothstep;
+#else
 ccl_device_inline int clamp(const int a, const int mn, const int mx)
 {
   return min(max(a, mn), mx);
@@ -562,7 +576,7 @@ ccl_device_inline float safe_sqrtf(const float f)
 ccl_device_inline float inversesqrtf(const float f)
 {
 #if defined(__KERNEL_METAL__)
-  return (f > 0.0f) ? rsqrt(f) : 0.0f;
+  return (f > 0.0f) ? metal::rsqrt(f) : 0.0f;
 #else
   return (f > 0.0f) ? 1.0f / sqrtf(f) : 0.0f;
 #endif
@@ -684,9 +698,9 @@ ccl_device_inline float lgammaf(const float x)
    */
   const float _1_180 = 1.0f / 180.0f;
   const float log2pi = 1.83787706641f;
-  const float logx = log(x);
+  const float logx = logf(x);
   return (log2pi - logx +
-          x * (logx * 2.0f + log(x * sinh(1.0f / x) + (_1_180 / pow(x, 6.0f))) - 2.0f)) *
+          x * (logx * 2.0f + logf(x * sinhf(1.0f / x) + (_1_180 / powf(x, 6.0f))) - 2.0f)) *
          0.5f;
 }
 #endif
@@ -730,7 +744,9 @@ ccl_device_inline uint popcount(const uint64_t x)
 #elif defined(__KERNEL_HIP__)
 /* Use popcll to support 64-bit wave for pre-RDNA AMD GPUs */
 #  define popcount(x) __popcll(x)
-#elif !defined(__KERNEL_METAL__)
+#elif defined(__KERNEL_METAL__)
+using metal::popcount;
+#else
 #  define popcount(x) __popc(x)
 #endif
 
@@ -739,7 +755,7 @@ ccl_device_inline uint count_leading_zeros(const uint x)
 #if defined(__KERNEL_CUDA__) || defined(__KERNEL_OPTIX__) || defined(__KERNEL_HIP__)
   return __clz(x);
 #elif defined(__KERNEL_METAL__)
-  return clz(x);
+  return metal::clz(x);
 #elif defined(__KERNEL_ONEAPI__)
   return sycl::clz(x);
 #else
@@ -759,7 +775,7 @@ ccl_device_inline uint count_trailing_zeros(const uint x)
 #if defined(__KERNEL_CUDA__) || defined(__KERNEL_OPTIX__) || defined(__KERNEL_HIP__)
   return (__ffs(x) - 1);
 #elif defined(__KERNEL_METAL__)
-  return ctz(x);
+  return metal::ctz(x);
 #elif defined(__KERNEL_ONEAPI__)
   return sycl::ctz(x);
 #else
@@ -779,7 +795,7 @@ ccl_device_inline uint find_first_set(const uint x)
 #if defined(__KERNEL_CUDA__) || defined(__KERNEL_OPTIX__) || defined(__KERNEL_HIP__)
   return __ffs(x);
 #elif defined(__KERNEL_METAL__)
-  return (x != 0) ? ctz(x) + 1 : 0;
+  return (x != 0) ? metal::ctz(x) + 1 : 0;
 #else
 #  ifdef _MSC_VER
   return (x != 0) ? (32 - count_leading_zeros(x & (~x + 1))) : 0;
@@ -835,7 +851,7 @@ ccl_device_inline uint32_t reverse_integer_bits(uint32_t x)
 #if defined(__KERNEL_CUDA__)
   return __brev(x);
 #elif defined(__KERNEL_METAL__)
-  return reverse_bits(x);
+  return metal::reverse_bits(x);
 #elif defined(__aarch64__) || (defined(_M_ARM64) && !defined(_MSC_VER))
   /* Assume the rbit is always available on 64bit ARM architecture. */
   __asm__("rbit %w0, %w1" : "=r"(x) : "r"(x));

intern/cycles/kernel/device/metal/bvh.h
@@ -204,7 +204,7 @@ ccl_device_intersect bool scene_intersect(KernelGlobals kg,
       r, metal_ancillaries->accel_struct, ray_mask, metal_ancillaries->ift_default, payload);
 #endif
 
-  if (intersection.type == intersection_type::none) {
+  if (intersection.type == metal::raytracing::intersection_type::none) {
     isect->t = ray->tmax;
     isect->type = PRIMITIVE_NONE;
 
@@ -213,14 +213,16 @@ ccl_device_intersect bool scene_intersect(KernelGlobals kg,
 
   isect->object = intersection.instance_id;
   isect->t = intersection.distance;
-  if (intersection.type == intersection_type::triangle) {
+  if (intersection.type == metal::raytracing::intersection_type::triangle) {
     isect->prim = intersection.primitive_id + intersection.user_instance_id;
     isect->type = kernel_data_fetch(objects, intersection.instance_id).primitive_type;
     isect->u = intersection.triangle_barycentric_coord.x;
     isect->v = intersection.triangle_barycentric_coord.y;
   }
 #ifdef __HAIR__
-  else if (kernel_data.bvh.have_curves && intersection.type == intersection_type::curve) {
+  else if (kernel_data.bvh.have_curves &&
+           intersection.type == metal::raytracing::intersection_type::curve)
+  {
     int prim = intersection.primitive_id + intersection.user_instance_id;
     const KernelCurveSegment segment = kernel_data_fetch(curve_segments, prim);
     isect->prim = segment.prim;
@@ -242,7 +244,9 @@ ccl_device_intersect bool scene_intersect(KernelGlobals kg,
   }
 #endif /* __HAIR__ */
 #ifdef __POINTCLOUD__
-  else if (kernel_data.bvh.have_points && intersection.type == intersection_type::bounding_box) {
+  else if (kernel_data.bvh.have_points &&
+           intersection.type == metal::raytracing::intersection_type::bounding_box)
+  {
     const int object = intersection.instance_id;
     const uint prim = intersection.primitive_id + intersection.user_instance_id;
     const int prim_type = kernel_data_fetch(objects, object).primitive_type;
@@ -320,7 +324,7 @@ ccl_device_intersect bool scene_intersect_shadow(KernelGlobals kg,
   intersection = metalrt_intersect.intersect(
       r, metal_ancillaries->accel_struct, ray_mask, metal_ancillaries->ift_shadow, payload);
 #endif
-  return (intersection.type != intersection_type::none);
+  return (intersection.type != metal::raytracing::intersection_type::none);
 }
 
 #ifdef __BVH_LOCAL__
@@ -381,7 +385,7 @@ ccl_device_intersect bool scene_intersect_local(KernelGlobals kg,
         payload);
 #  endif
 
-    if (intersection.type == intersection_type::none) {
+    if (intersection.type == metal::raytracing::intersection_type::none) {
       local_isect->num_hits = 0;
       return false;
     }
@@ -441,7 +445,7 @@ ccl_device_intersect bool scene_intersect_local(KernelGlobals kg,
     if (max_hits == 0) {
       /* Special case for when no hit information is requested, just report that something was hit
        */
-      return (intersection.type != intersection_type::none);
+      return (intersection.type != metal::raytracing::intersection_type::none);
     }
 
     if (lcg_state) {
@@ -557,7 +561,7 @@ ccl_device_intersect bool scene_intersect_volume(KernelGlobals kg,
       r, metal_ancillaries->accel_struct, ray_mask, metal_ancillaries->ift_volume, payload);
 #  endif
 
-  if (intersection.type == intersection_type::triangle) {
+  if (intersection.type == metal::raytracing::intersection_type::triangle) {
     isect->prim = intersection.primitive_id + intersection.user_instance_id;
     isect->type = kernel_data_fetch(objects, intersection.instance_id).primitive_type;
     isect->u = intersection.triangle_barycentric_coord.x;

intern/cycles/kernel/device/metal/compat.h
@@ -18,12 +18,6 @@
 #include <metal_stdlib>
 #include <simd/simd.h>
 
-using namespace metal;
-
-#ifdef __KERNEL_METALRT__
-using namespace metal::raytracing;
-#endif
-
 #pragma clang diagnostic ignored "-Wunused-variable"
 #pragma clang diagnostic ignored "-Wsign-compare"
 #pragma clang diagnostic ignored "-Wuninitialized"
@@ -76,8 +70,9 @@ using namespace metal::raytracing;
 #define ccl_gpu_thread_idx_x simd_group_index
 #define ccl_gpu_thread_mask(thread_warp) uint64_t((1ull << thread_warp) - 1)
 
-#define ccl_gpu_ballot(predicate) ((uint64_t)((simd_vote::vote_t)simd_ballot(predicate)))
-#define ccl_gpu_syncthreads() threadgroup_barrier(mem_flags::mem_threadgroup);
+#define ccl_gpu_ballot(predicate) \
+  ((uint64_t)((metal::simd_vote::vote_t)metal::simd_ballot(predicate)))
+#define ccl_gpu_syncthreads() threadgroup_barrier(metal::mem_flags::mem_threadgroup);
 
 // clang-format off
 
@@ -118,21 +113,21 @@ using namespace metal::raytracing;
 #ifdef __METAL_GLOBAL_BUILTINS__
 
 #define ccl_gpu_kernel_signature(name, ...) \
-struct kernel_gpu_##name \
+struct kernel_gpu_## name \
 { \
   PARAMS_MAKER(__VA_ARGS__)(__VA_ARGS__) \
   void run(thread MetalKernelContext& context, \
-           threadgroup atomic_int *threadgroup_array) ccl_global const; \
+           threadgroup metal::atomic_int *threadgroup_array) ccl_global const; \
 }; \
-kernel void cycles_metal_##name(device const kernel_gpu_##name *params_struct, \
+kernel void cycles_metal_## name(device const kernel_gpu_## name *params_struct, \
                                 constant KernelParamsMetal &ccl_restrict   _launch_params_metal, \
                                 constant MetalAncillaries *_metal_ancillaries, \
-                                threadgroup atomic_int *threadgroup_array[[ threadgroup(0) ]]) { \
+                                threadgroup metal::atomic_int *threadgroup_array[[ threadgroup(0) ]]) { \
   MetalKernelContext context(_launch_params_metal, _metal_ancillaries); \
   params_struct->run(context, threadgroup_array); \
 } \
-void kernel_gpu_##name::run(thread MetalKernelContext& context, \
-                  threadgroup atomic_int *threadgroup_array) ccl_global const
+void kernel_gpu_## name::run(thread MetalKernelContext& context, \
+                  threadgroup metal::atomic_int *threadgroup_array) ccl_global const
 
 #else
 
@@ -140,11 +135,11 @@ void kernel_gpu_##name::run(thread MetalKernelContext& context, \
  * be accessed through attributed entry-point parameters. */
 
 #define ccl_gpu_kernel_signature(name, ...) \
-struct kernel_gpu_##name \
+struct kernel_gpu_## name \
 { \
   PARAMS_MAKER(__VA_ARGS__)(__VA_ARGS__) \
   void run(thread MetalKernelContext& context, \
-           threadgroup atomic_int *threadgroup_array, \
+           threadgroup metal::atomic_int *threadgroup_array, \
            const uint metal_global_id, \
            const ushort metal_local_id, \
            const ushort metal_local_size, \
@@ -154,10 +149,10 @@ struct kernel_gpu_##name \
            uint simd_group_index, \
            uint num_simd_groups) ccl_global const; \
 }; \
-kernel void cycles_metal_##name(device const kernel_gpu_##name *params_struct, \
+kernel void cycles_metal_## name(device const kernel_gpu_## name *params_struct, \
                                 constant KernelParamsMetal &ccl_restrict   _launch_params_metal, \
                                 constant MetalAncillaries *_metal_ancillaries, \
-                                threadgroup atomic_int *threadgroup_array[[ threadgroup(0) ]], \
+                                threadgroup metal::atomic_int *threadgroup_array[[ threadgroup(0) ]], \
                                 const uint metal_global_id [[thread_position_in_grid]], \
                                 const ushort metal_local_id   [[thread_position_in_threadgroup]], \
                                 const ushort metal_local_size [[threads_per_threadgroup]], \
@@ -169,8 +164,8 @@ kernel void cycles_metal_##name(device const kernel_gpu_##name *params_struct, \
   MetalKernelContext context(_launch_params_metal, _metal_ancillaries); \
   params_struct->run(context, threadgroup_array, metal_global_id, metal_local_id, metal_local_size, metal_grid_id, simdgroup_size, simd_lane_index, simd_group_index, num_simd_groups); \
 } \
-void kernel_gpu_##name::run(thread MetalKernelContext& context, \
-                  threadgroup atomic_int *threadgroup_array, \
+void kernel_gpu_## name::run(thread MetalKernelContext& context, \
+                  threadgroup metal::atomic_int *threadgroup_array, \
                   const uint metal_global_id, \
                   const ushort metal_local_id, \
                   const ushort metal_local_size, \
@@ -262,29 +257,29 @@ ccl_device_forceinline uchar4 make_uchar4(const uchar x,
 #define __int_as_float(x) as_type<float>(x)
 #define __float_as_int(x) as_type<int>(x)
 #define __float2half(x) half(x)
-#define powf(x, y) pow(float(x), float(y))
-#define fabsf(x) fabs(float(x))
-#define copysignf(x, y) copysign(float(x), float(y))
-#define asinf(x) asin(float(x))
-#define acosf(x) acos(float(x))
-#define atanf(x) atan(float(x))
-#define floorf(x) floor(float(x))
-#define ceilf(x) ceil(float(x))
-#define roundf(x) round(float(x))
-#define hypotf(x, y) hypot(float(x), float(y))
-#define atan2f(x, y) atan2(float(x), float(y))
-#define fmaxf(x, y) fmax(float(x), float(y))
-#define fminf(x, y) fmin(float(x), float(y))
-#define fmodf(x, y) fmod(float(x), float(y))
-#define sinhf(x) sinh(float(x))
-#define coshf(x) cosh(float(x))
-#define tanhf(x) tanh(float(x))
-#define saturatef(x) saturate(float(x))
-#define ldexpf(x, y) ldexp(float(x), int(y))
+#define powf(x, y) metal::pow(float(x), float(y))
+#define fabsf(x) metal::fabs(float(x))
+#define copysignf(x, y) metal::copysign(float(x), float(y))
+#define asinf(x) metal::asin(float(x))
+#define acosf(x) metal::acos(float(x))
+#define atanf(x) metal::atan(float(x))
+#define floorf(x) metal::floor(float(x))
+#define ceilf(x) metal::ceil(float(x))
+#define roundf(x) metal::round(float(x))
+#define hypotf(x, y) metal::hypot(float(x), float(y))
+#define atan2f(x, y) metal::atan2(float(x), float(y))
+#define fmaxf(x, y) metal::fmax(float(x), float(y))
+#define fminf(x, y) metal::fmin(float(x), float(y))
+#define fmodf(x, y) metal::fmod(float(x), float(y))
+#define sinhf(x) metal::sinh(float(x))
+#define coshf(x) metal::cosh(float(x))
+#define tanhf(x) metal::tanh(float(x))
+#define saturatef(x) metal::saturate(float(x))
+#define ldexpf(x, y) metal::ldexp(float(x), int(y))
 
 /* Use native functions with possibly lower precision for performance,
  * no issues found so far. */
-#define trigmode fast
+#define trigmode metal::fast
 #define sinf(x) trigmode::sin(float(x))
 #define cosf(x) trigmode::cos(float(x))
 #define tanf(x) trigmode::tan(float(x))
@@ -292,40 +287,82 @@ ccl_device_forceinline uchar4 make_uchar4(const uchar x,
 #define sqrtf(x) trigmode::sqrt(float(x))
 #define logf(x) trigmode::log(float(x))
 
+using metal::all;
+using metal::atan2;
+using metal::ceil;
+using metal::clamp;
+using metal::cos;
+using metal::cross;
+using metal::distance;
+using metal::dot;
+using metal::exp;
+using metal::fabs;
+using metal::faceforward;
+using metal::floor;
+using metal::fmod;
+using metal::log;
+using metal::log2;
+using metal::max;
+using metal::min;
+using metal::normalize;
+using metal::reflect;
+using metal::refract;
+using metal::round;
+using metal::saturate;
+using metal::sin;
+using metal::sincos;
+using metal::sqrt;
+using metal::tan;
+
 #define __device__
 
 #ifdef __KERNEL_METALRT__
 
 #  if defined(__METALRT_MOTION__)
-#    define METALRT_TAGS instancing, instance_motion, primitive_motion
-#    define METALRT_BLAS_TAGS , primitive_motion
+#    define METALRT_TAGS \
+      metal::raytracing::instancing, metal::raytracing::instance_motion, \
+          metal::raytracing::primitive_motion
+#    define METALRT_BLAS_TAGS , metal::raytracing::primitive_motion
 #  else
-#    define METALRT_TAGS instancing
+#    define METALRT_TAGS metal::raytracing::instancing
 #    define METALRT_BLAS_TAGS
 #  endif /* __METALRT_MOTION__ */
 
 #  if defined(__METALRT_EXTENDED_LIMITS__)
-#    define METALRT_LIMITS , extended_limits
+#    define METALRT_LIMITS , metal::raytracing::extended_limits
 #  else
 #    define METALRT_LIMITS
 #  endif /* __METALRT_MOTION__ */
 
-typedef acceleration_structure<METALRT_TAGS> metalrt_as_type;
-typedef intersection_function_table<triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS>
+typedef metal::raytracing::acceleration_structure<METALRT_TAGS> metalrt_as_type;
+typedef metal::raytracing::intersection_function_table<metal::raytracing::triangle_data,
+                                                       metal::raytracing::curve_data,
+                                                       METALRT_TAGS METALRT_LIMITS>
     metalrt_ift_type;
-typedef metal::raytracing::intersector<triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS>
+typedef metal::raytracing::intersector<metal::raytracing::triangle_data,
+                                       metal::raytracing::curve_data,
+                                       METALRT_TAGS METALRT_LIMITS>
     metalrt_intersector_type;
 #  if defined(__METALRT_MOTION__)
-typedef acceleration_structure<primitive_motion> metalrt_blas_as_type;
-typedef intersection_function_table<triangle_data, curve_data, primitive_motion METALRT_LIMITS>
+typedef metal::raytracing::acceleration_structure<metal::raytracing::primitive_motion>
+    metalrt_blas_as_type;
+typedef metal::raytracing::intersection_function_table<
+    metal::raytracing::triangle_data,
+    metal::raytracing::curve_data,
+    metal::raytracing::primitive_motion METALRT_LIMITS>
     metalrt_blas_ift_type;
-typedef metal::raytracing::intersector<triangle_data, curve_data, primitive_motion METALRT_LIMITS>
+typedef metal::raytracing::intersector<metal::raytracing::triangle_data,
+                                       metal::raytracing::curve_data,
+                                       metal::raytracing::primitive_motion METALRT_LIMITS>
     metalrt_blas_intersector_type;
 #  else
-typedef acceleration_structure<> metalrt_blas_as_type;
-typedef intersection_function_table<triangle_data, curve_data METALRT_LIMITS>
+typedef metal::raytracing::acceleration_structure<> metalrt_blas_as_type;
+typedef metal::raytracing::intersection_function_table<
+    metal::raytracing::triangle_data,
+    metal::raytracing::curve_data METALRT_LIMITS>
     metalrt_blas_ift_type;
-typedef metal::raytracing::intersector<triangle_data, curve_data METALRT_LIMITS>
+typedef metal::raytracing::intersector<metal::raytracing::triangle_data,
+                                       metal::raytracing::curve_data METALRT_LIMITS>
     metalrt_blas_intersector_type;
 #  endif
 
@@ -338,7 +375,7 @@ struct TextureParamsMetal {
   uint64_t tex;
 };
 struct Texture2DParamsMetal {
-  texture2d<float, access::sample> tex;
+  metal::texture2d<float, metal::access::sample> tex;
 };
 
 #ifdef __KERNEL_METALRT__
@@ -384,15 +421,15 @@ enum SamplerType {
   SamplerCount
 };
 
-constexpr constant array<sampler, SamplerCount> metal_samplers = {
-    sampler(address::repeat, filter::nearest),
-    sampler(address::clamp_to_edge, filter::nearest),
-    sampler(address::clamp_to_zero, filter::nearest),
-    sampler(address::mirrored_repeat, filter::nearest),
-    sampler(address::repeat, filter::linear),
-    sampler(address::clamp_to_edge, filter::linear),
-    sampler(address::clamp_to_zero, filter::linear),
-    sampler(address::mirrored_repeat, filter::linear),
+constexpr constant metal::array<metal::sampler, SamplerCount> metal_samplers = {
+    metal::sampler(metal::address::repeat, metal::filter::nearest),
+    metal::sampler(metal::address::clamp_to_edge, metal::filter::nearest),
+    metal::sampler(metal::address::clamp_to_zero, metal::filter::nearest),
+    metal::sampler(metal::address::mirrored_repeat, metal::filter::nearest),
+    metal::sampler(metal::address::repeat, metal::filter::linear),
+    metal::sampler(metal::address::clamp_to_edge, metal::filter::linear),
+    metal::sampler(metal::address::clamp_to_zero, metal::filter::linear),
+    metal::sampler(metal::address::mirrored_repeat, metal::filter::linear),
 };
 
 #ifdef __METAL_GLOBAL_BUILTINS__

intern/cycles/kernel/device/metal/kernel.metal
@@ -41,7 +41,9 @@ enum { METALRT_HIT_TRIANGLE, METALRT_HIT_CURVE, METALRT_HIT_BOUNDING_BOX };
 
 /* Hit functions. */
 
-[[intersection(triangle, triangle_data, curve_data)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data)]] PrimitiveIntersectionResult
 __intersection__local_tri_single_hit(
     ray_data MetalKernelContext::MetalRTIntersectionLocalPayload_single_hit &payload [[payload]],
     uint primitive_id [[primitive_id]])
@@ -52,8 +54,10 @@ __intersection__local_tri_single_hit(
   return result;
 }
 
-[[intersection(
-    triangle, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__local_tri_single_hit_mblur(
     ray_data MetalKernelContext::MetalRTIntersectionLocalPayload_single_hit &payload [[payload]],
 #  if defined(__METALRT_MOTION__)
@@ -127,7 +131,9 @@ TReturn metalrt_local_hit(constant KernelParamsMetal &launch_params_metal,
   return result;
 }
 
-[[intersection(triangle, triangle_data, curve_data)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data)]] PrimitiveIntersectionResult
 __intersection__local_tri(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                           ray_data MetalKernelContext::MetalRTIntersectionLocalPayload &payload
                           [[payload]],
@@ -143,8 +149,10 @@ __intersection__local_tri(constant KernelParamsMetal &launch_params_metal [[buff
       launch_params_metal, payload, primitive_id, barycentrics, ray_tmax);
 }
 
-[[intersection(
-    triangle, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__local_tri_mblur(
     constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
     ray_data MetalKernelContext::MetalRTIntersectionLocalPayload &payload [[payload]],
@@ -249,8 +257,10 @@ bool metalrt_shadow_all_hit(constant KernelParamsMetal &launch_params_metal,
 #  endif /* __TRANSPARENT_SHADOWS__ */
 }
 
-[[intersection(
-    triangle, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__tri_shadow_all(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                                ray_data MetalKernelContext::BVHShadowAllPayload &payload
                                [[payload]],
@@ -269,8 +279,10 @@ __intersection__tri_shadow_all(constant KernelParamsMetal &launch_params_metal [
   return result;
 }
 
-[[intersection(
-    triangle, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__volume_tri(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                            ray_data MetalKernelContext::MetalRTIntersectionShadowPayload &payload
                            [[payload]],
@@ -417,8 +429,10 @@ inline TReturnType metalrt_visibility_test_shadow(
   return result;
 }
 
-[[intersection(
-    triangle, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__tri(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                     ray_data MetalKernelContext::MetalRTIntersectionPayload &payload [[payload]],
                     const unsigned int object [[instance_id]],
@@ -438,8 +452,10 @@ __intersection__tri(constant KernelParamsMetal &launch_params_metal [[buffer(1)]
   return result;
 }
 
-[[intersection(
-    triangle, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(triangle,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__tri_shadow(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                            ray_data MetalKernelContext::MetalRTIntersectionShadowPayload &payload
                            [[payload]],
@@ -456,8 +472,10 @@ __intersection__tri_shadow(constant KernelParamsMetal &launch_params_metal [[buf
 
 /* Primitive intersection functions. */
 
-[[intersection(
-    curve, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(curve,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__curve(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                       ray_data MetalKernelContext::MetalRTIntersectionPayload &payload [[payload]],
                       const uint object [[instance_id]],
@@ -491,8 +509,10 @@ __intersection__curve(constant KernelParamsMetal &launch_params_metal [[buffer(1
   return result;
 }
 
-[[intersection(
-    curve, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(curve,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__curve_shadow(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                              ray_data MetalKernelContext::MetalRTIntersectionShadowPayload &payload
                              [[payload]],
@@ -527,8 +547,10 @@ __intersection__curve_shadow(constant KernelParamsMetal &launch_params_metal [[b
   return result;
 }
 
-[[intersection(
-    curve, triangle_data, curve_data, METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
+[[intersection(curve,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
+               METALRT_TAGS METALRT_LIMITS)]] PrimitiveIntersectionResult
 __intersection__curve_shadow_all(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                                  ray_data MetalKernelContext::BVHShadowAllPayload &payload
                                  [[payload]],
@@ -596,8 +618,8 @@ ccl_device_inline void metalrt_intersection_point_shadow_all(
 }
 
 [[intersection(bounding_box,
-               triangle_data,
-               curve_data,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
                METALRT_TAGS METALRT_LIMITS)]] BoundingBoxIntersectionResult
 __intersection__point(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                       ray_data MetalKernelContext::MetalRTIntersectionPayload &payload [[payload]],
@@ -643,8 +665,8 @@ __intersection__point(constant KernelParamsMetal &launch_params_metal [[buffer(1
 #  endif /* __POINTCLOUD__ */
 
 [[intersection(bounding_box,
-               triangle_data,
-               curve_data,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
                METALRT_TAGS METALRT_LIMITS)]] BoundingBoxIntersectionResult
 __intersection__point_shadow(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                              ray_data MetalKernelContext::MetalRTIntersectionShadowPayload &payload
@@ -695,8 +717,8 @@ __intersection__point_shadow(constant KernelParamsMetal &launch_params_metal [[b
 }
 
 [[intersection(bounding_box,
-               triangle_data,
-               curve_data,
+               metal::raytracing::triangle_data,
+               metal::raytracing::curve_data,
                METALRT_TAGS METALRT_LIMITS)]] BoundingBoxIntersectionResult
 __intersection__point_shadow_all(constant KernelParamsMetal &launch_params_metal [[buffer(1)]],
                                  ray_data MetalKernelContext::BVHShadowAllPayload &payload

commit 1ffa6b67a4171ad48ec410d6511c64f2009a1f07

Fix: Cycles: Metal using lower precision for lgammaf than before
Refactoring accidentally changed these to the fast math versions,
and for this usage the precise version might matter.

This issue was introduced in f8ff821217e.

Pull Request: https://projects.blender.org/blender/blender/pulls/163579

intern/cycles/util/math_base.h
@@ -698,9 +698,9 @@ ccl_device_inline float lgammaf(const float x)
    */
   const float _1_180 = 1.0f / 180.0f;
   const float log2pi = 1.83787706641f;
-  const float logx = logf(x);
+  const float logx = metal::log(x);
   return (log2pi - logx +
-          x * (logx * 2.0f + logf(x * sinhf(1.0f / x) + (_1_180 / powf(x, 6.0f))) - 2.0f)) *
+          x * (logx * 2.0f + metal::log(x * sinhf(1.0f / x) + (_1_180 / powf(x, 6.0f))) - 2.0f)) *
          0.5f;
 }
 #endif