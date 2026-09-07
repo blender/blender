blender/blender

commit 527f9ea306a98858618234c1dafcec9084a86a2f

Refactor: Cycles: More consistent naming of image functions and structs
Previously there was a mix of "image" and "texture" to refer to the same
thing, use "image" when possible now. An exception is MEM_IMAGE_TEXTURE
to avoid conflicts with the MEM_IMAGE macro on Windows.

Pull Request: https://projects.blender.org/blender/blender/pulls/152665

intern/cycles/device/cuda/device_impl.cpp
@@ -17,9 +17,9 @@
 #  include "util/path.h"
 #  include "util/string.h"
 #  include "util/system.h"
-#  include "util/texture.h"
 #  include "util/time.h"
 #  include "util/types.h"
+#  include "util/types_image.h"
 
 #  ifdef _WIN32
 #    include "util/windows.h"
@@ -70,7 +70,7 @@ CUDADevice::CUDADevice(const DeviceInfo &info, Stats &stats, Profiler &profiler,
 
   cuModule = nullptr;
 
-  need_texture_info = false;
+  need_image_info = false;
 
   pitch_alignment = 0;
 
@@ -133,7 +133,7 @@ CUDADevice::CUDADevice(const DeviceInfo &info, Stats &stats, Profiler &profiler,
 
 CUDADevice::~CUDADevice()
 {
-  texture_info.free();
+  image_info.free();
   if (cuModule) {
     cuda_assert(cuModuleUnload(cuModule));
   }
@@ -173,7 +173,7 @@ bool CUDADevice::check_peer_access(Device *peer_device)
     return false;
   }
 
-  // Ensure array access over the link is possible as well (for 3D textures)
+  // Ensure array access over the link is possible as well (for 3D images)
   cuda_assert(cuDeviceGetP2PAttribute(&can_access,
                                       CU_DEVICE_P2P_ATTRIBUTE_CUDA_ARRAY_ACCESS_SUPPORTED,
                                       cuDevice,
@@ -567,8 +567,8 @@ void CUDADevice::copy_host_to_device(void *device_pointer, void *host_pointer, c
 
 void CUDADevice::mem_alloc(device_memory &mem)
 {
-  if (mem.type == MEM_TEXTURE) {
-    assert(!"mem_alloc not supported for textures.");
+  if (mem.type == MEM_IMAGE_TEXTURE) {
+    assert(!"mem_alloc not supported for images.");
   }
   else if (mem.type == MEM_GLOBAL) {
     assert(!"mem_alloc not supported for global memory.");
@@ -583,8 +583,8 @@ void CUDADevice::mem_copy_to(device_memory &mem)
   if (mem.type == MEM_GLOBAL) {
     global_copy_to(mem);
   }
-  else if (mem.type == MEM_TEXTURE) {
-    tex_copy_to((device_texture &)mem);
+  else if (mem.type == MEM_IMAGE_TEXTURE) {
+    image_copy_to((device_image &)mem);
   }
   else {
     if (!mem.device_pointer) {
@@ -603,20 +603,20 @@ void CUDADevice::mem_move_to_host(device_memory &mem)
     global_free(mem);
     global_alloc(mem);
   }
-  else if (mem.type == MEM_TEXTURE) {
-    tex_free((device_texture &)mem);
-    tex_alloc((device_texture &)mem);
+  else if (mem.type == MEM_IMAGE_TEXTURE) {
+    image_free((device_image &)mem);
+    image_alloc((device_image &)mem);
   }
   else {
-    assert(!"mem_move_to_host only supported for texture and global memory");
+    assert(!"mem_move_to_host only supported for image and global memory");
   }
 }
 
 void CUDADevice::mem_copy_from(
     device_memory &mem, const size_t y, size_t w, const size_t h, size_t elem)
 {
-  if (mem.type == MEM_TEXTURE || mem.type == MEM_GLOBAL) {
-    assert(!"mem_copy_from not supported for textures.");
+  if (mem.type == MEM_IMAGE_TEXTURE) {
+    assert(!"mem_copy_from not supported for images.");
   }
   else if (mem.host_pointer) {
     const size_t size = elem * w * h;
@@ -656,8 +656,8 @@ void CUDADevice::mem_free(device_memory &mem)
   if (mem.type == MEM_GLOBAL) {
     global_free(mem);
   }
-  else if (mem.type == MEM_TEXTURE) {
-    tex_free((device_texture &)mem);
+  else if (mem.type == MEM_IMAGE_TEXTURE) {
+    image_free((device_image &)mem);
   }
   else {
     generic_free(mem);
@@ -720,14 +720,14 @@ void CUDADevice::global_free(device_memory &mem)
   }
 }
 
-static size_t tex_src_pitch(const device_texture &mem)
+static size_t tex_src_pitch(const device_image &mem)
 {
   return mem.data_width * datatype_size(mem.data_type) * mem.data_elements;
 }
 
-static CUDA_MEMCPY2D tex_2d_copy_param(const device_texture &mem, const int pitch_alignment)
+static CUDA_MEMCPY2D tex_2d_copy_param(const device_image &mem, const int pitch_alignment)
 {
-  /* 2D texture using pitch aligned linear memory. */
+  /* 2D image using pitch aligned linear memory. */
   const size_t src_pitch = tex_src_pitch(mem);
   const size_t dst_pitch = align_up(src_pitch, pitch_alignment);
 
@@ -745,7 +745,7 @@ static CUDA_MEMCPY2D tex_2d_copy_param(const device_texture &mem, const int pitc
   return param;
 }
 
-void CUDADevice::tex_alloc(device_texture &mem)
+void CUDADevice::image_alloc(device_image &mem)
 {
   CUDAContextScope scope(this);
 
@@ -776,13 +776,15 @@ void CUDADevice::tex_alloc(device_texture &mem)
     filter_mode = CU_TR_FILTER_MODE_LINEAR;
   }
 
-  /* Image Texture Storage */
-  /* Cycles expects to read all texture data as normalized float values in
+  /* Image Texture Storage
+   *
+   * Cycles expects to read all image data as normalized float values in
    * kernel/device/gpu/image.h. But storing all data as floats would be very inefficient due to the
-   * huge size of float textures. So in the code below, we define different texture types including
+   * huge size of float image. So in the code below, we define different texture types including
    * integer types, with the aim of using CUDA's default promotion behavior of integer data to
    * floating point data in the range [0, 1], as noted in the CUDA documentation on
    * cuTexObjectCreate API Call.
+   *
    * Note that 32-bit integers are not supported by this promotion behavior and cannot be used
    * with Cycles's current implementation in kernel/device/gpu/image.h.
    */
@@ -813,7 +815,7 @@ void CUDADevice::tex_alloc(device_texture &mem)
     cmem->texobject = 0;
   }
   else if (mem.data_height > 0) {
-    /* 2D texture, using pitch aligned linear memory. */
+    /* 2D image, using pitch aligned linear memory. */
     const size_t dst_pitch = align_up(tex_src_pitch(mem), pitch_alignment);
     const size_t dst_size = dst_pitch * mem.data_height;
 
@@ -826,7 +828,7 @@ void CUDADevice::tex_alloc(device_texture &mem)
     cuda_assert(cuMemcpy2DUnaligned(&param));
   }
   else {
-    /* 1D texture, using linear memory. */
+    /* 1D image, using linear memory. */
     cmem = generic_alloc(mem);
     if (!cmem) {
       return;
@@ -836,7 +838,7 @@ void CUDADevice::tex_alloc(device_texture &mem)
   }
 
   /* Set Mapping and tag that we need to (re-)upload to device */
-  TextureInfo tex_info = mem.info;
+  KernelImageInfo tex_info = mem.info;
 
   if (!is_nanovdb_type(mem.info.data_type)) {
     CUDA_RESOURCE_DESC resDesc;
@@ -868,7 +870,7 @@ void CUDADevice::tex_alloc(device_texture &mem)
     texDesc.addressMode[2] = address_mode;
     texDesc.filterMode = filter_mode;
     /* CUDA's flag CU_TRSF_READ_AS_INTEGER is intentionally not used and it is
-     * significant, see above an explanation about how Blender treat textures. */
+     * significant, see above an explanation about how Blender treat images. */
     texDesc.flags = CU_TRSF_NORMALIZED_COORDINATES;
 
     thread_scoped_lock lock(device_mem_map_mutex);
@@ -883,33 +885,33 @@ void CUDADevice::tex_alloc(device_texture &mem)
   }
 
   {
-    /* Update texture info. */
-    thread_scoped_lock lock(texture_info_mutex);
+    /* Update image info. */
+    thread_scoped_lock lock(image_info_mutex);
     const uint slot = mem.slot;
-    if (slot >= texture_info.size()) {
+    if (slot >= image_info.size()) {
       /* Allocate some slots in advance, to reduce amount of re-allocations. */
-      texture_info.resize(slot + 128);
+      image_info.resize(slot + 128);
     }
-    texture_info[slot] = tex_info;
-    need_texture_info = true;
+    image_info[slot] = tex_info;
+    need_image_info = true;
   }
 }
 
-void CUDADevice::tex_copy_to(device_texture &mem)
+void CUDADevice::image_copy_to(device_image &mem)
 {
   if (!mem.device_pointer) {
     /* Not yet allocated on device. */
-    tex_alloc(mem);
+    image_alloc(mem);
   }
   else if (!mem.is_resident(this)) {
-    /* Peering with another device, may still need to create texture info and object. */
-    bool texture_allocated = false;
+    /* Peering with another device, may still need to create image info and object. */
+    bool image_allocated = false;
     {
-      thread_scoped_lock lock(texture_info_mutex);
-      texture_allocated = mem.slot < texture_info.size() && texture_info[mem.slot].data != 0;
+      thread_scoped_lock lock(image_info_mutex);
+      image_allocated = mem.slot < image_info.size() && image_info[mem.slot].data != 0;
     }
-    if (!texture_allocated) {
-      tex_alloc(mem);
+    if (!image_allocated) {
+      image_alloc(mem);
     }
   }
   else {
@@ -925,7 +927,7 @@ void CUDADevice::tex_copy_to(device_texture &mem)
   }
 }
 
-void CUDADevice::tex_free(device_texture &mem)
+void CUDADevice::image_free(device_image &mem)
 {
   CUDAContextScope scope(this);
   thread_scoped_lock lock(device_mem_map_mutex);
@@ -938,10 +940,10 @@ void CUDADevice::tex_free(device_texture &mem)
 
   const Mem &cmem = it->second;
 
-  /* Always clear texture info and texture object, regardless of residency. */
+  /* Always clear image info and image object, regardless of residency. */
   {
-    thread_scoped_lock lock(texture_info_mutex);
-    texture_info[mem.slot] = TextureInfo();
+    thread_scoped_lock lock(image_info_mutex);
+    image_info[mem.slot] = KernelImageInfo();
   }
 
   if (cmem.texobject) {

commit 3edfd6e396a1bd458a1eacc74bc6a6117e02bee2

Cleanup: Cycles: Fix some mistakes in error messages and comments
These issues were introduced in 527f9ea306a and 41a27b4eed.

Pull Request: https://projects.blender.org/blender/blender/pulls/163579

intern/cycles/blender/display_driver.cpp
@@ -94,7 +94,7 @@ blender::gpu::Shader *BlenderDisplaySpaceShader::get_shader_program()
 /* Higher level representation of a texture from the graphics library. */
 class DisplayGPUTexture {
  public:
-  /* Global counter for all allocated blender::GPUTextures used by instances of this class. */
+  /* Global counter for all allocated GPUTextures used by instances of this class. */
   static inline std::atomic<int> num_used = 0;
 
   DisplayGPUTexture() = default;
@@ -176,7 +176,7 @@ class DisplayGPUTexture {
     --num_used;
   }
 
-  /* Texture resource allocated by the blender::GPU module.
+  /* Texture resource allocated by the GPU module.
    *
    * NOTE: Allocated on the render engine's context. */
   blender::gpu::Texture *gpu_texture = nullptr;
@@ -197,7 +197,7 @@ class DisplayGPUTexture {
 /* Higher level representation of a Pixel Buffer Object (PBO) from the graphics library. */
 class DisplayGPUPixelBuffer {
  public:
-  /* Global counter for all allocated blender::GPU module PBOs used by instances of this class. */
+  /* Global counter for all allocated GPU module PBOs used by instances of this class. */
   static inline std::atomic<int> num_used = 0;
 
   DisplayGPUPixelBuffer() = default;
@@ -281,7 +281,7 @@ class DisplayGPUPixelBuffer {
     --num_used;
   }
 
-  /* Pixel Buffer Object allocated by the blender::GPU module.
+  /* Pixel Buffer Object allocated by the GPU module.
    *
    * NOTE: Allocated on the render engine's context. */
   blender::GPUPixelBuffer *gpu_pixel_buffer = nullptr;
@@ -442,7 +442,7 @@ bool BlenderDisplayDriver::update_begin(const Params &params,
    * at a resolution divider 1. This was we don't need to recreate graphics interoperability
    * objects which are costly and which are tied to the specific underlying buffer size.
    * The downside of this approach is that when graphics interoperability is not used we are
-   * sending too much data to blender::GPU when resolution divider is not 1. */
+   * sending too much data to GPU when resolution divider is not 1. */
   /* TODO(sergey): Investigate whether keeping the PBO exact size of the texture makes non-interop
    * mode faster. */
   const int buffer_width = params.size.x;
@@ -496,17 +496,16 @@ void BlenderDisplayDriver::update_end()
    *
    * This allows to ensure that the unpacking happens while resources like graphics interop (which
    * lifetime is outside of control of the display driver) are still valid, as well as allows to
-   * move the tile from being current to finished blender::immediately after this call.
+   * move the tile from being current to finished immediately after this call.
    *
    * One concern with this approach is that if the update happens more often than drawing then
-   * doing the unpack here occupies blender::GPU transfer for no good reason. However, the render
-   * scheduler takes care of ensuring updates don't happen that often. In regular applications
-   * redraw will happen much more often than this update.
+   * doing the unpack here occupies GPU transfer for no good reason. However, the render scheduler
+   * takes care of ensuring updates don't happen that often. In regular applications redraw will
+   * happen much more often than this update.
    *
-   * On some older blender::GPUs on macOS, there is a driver crash when updating the texture for
-   * viewport renders while Blender is drawing. As a workaround update texture during draw, under
-   * assumption that there is no graphics interop on macOS and viewport render has a single tile.
-   */
+   * On some older GPUs on macOS, there is a driver crash when updating the texture for viewport
+   * renders while Blender is drawing. As a workaround update texture during draw, under assumption
+   * that there is no graphics interop on macOS and viewport render has a single tile. */
   if (!background_ && blender::GPU_type_matches_ex(blender::GPU_DEVICE_NVIDIA,
                                                    blender::GPU_OS_MAC,
                                                    blender::GPU_DRIVER_ANY,
@@ -518,7 +517,7 @@ void BlenderDisplayDriver::update_end()
     update_tile_texture_pixels(tiles_->current_tile);
   }
 
-  /* Ensure blender::GPU fence exists to synchronize upload. */
+  /* Ensure GPU fence exists to synchronize upload. */
   blender::GPU_fence_signal(gpu_upload_sync_);
 
   blender::GPU_flush();
@@ -538,7 +537,7 @@ half4 *BlenderDisplayDriver::map_texture_buffer()
    * and not. For the denoised image it may be able to use graphics interop as that
    * buffer is written to by one device, while the noisy renders can not use it.
    *
-   * We need to clear the graphics interop buffer on that switch, as blender::GPU_pixel_buffer_map
+   * We need to clear the graphics interop buffer on that switch, as GPU_pixel_buffer_map
    * may recreate the buffer or handle. */
   graphics_interop_buffer_.clear();
 
@@ -687,11 +686,11 @@ static void draw_tile(const float2 &zoom,
   const DisplayGPUTexture &texture = draw_tile.texture;
 
   if (!DCHECK_NOTNULL(texture.gpu_texture)) {
-    LOG_ERROR << "Display driver tile blender::GPU texture resource unavailable.";
+    LOG_ERROR << "Display driver tile GPU texture resource unavailable.";
     return;
   }
 
-  /* Trick to keep sharp rendering without jagged edges on all blender::GPUs.
+  /* Trick to keep sharp rendering without jagged edges on all GPUs.
    *
    * The idea here is to enforce driver to use linear interpolation when the image is zoomed out.
    * For the render result with a resolution divider in effect we always use nearest interpolation.
@@ -821,13 +820,13 @@ void BlenderDisplayDriver::draw(const Params &params)
 void BlenderDisplayDriver::gpu_context_create()
 {
   if (!RE_engine_gpu_context_create(&b_engine_)) {
-    LOG_ERROR << "Error creating blender::GPU context.";
+    LOG_ERROR << "Error creating GPU context.";
     return;
   }
 
-  /* Create global blender::GPU resources for display driver. */
+  /* Create global GPU resources for display driver. */
   if (!gpu_resources_create()) {
-    LOG_ERROR << "Error creating blender::GPU resources for Display Driver.";
+    LOG_ERROR << "Error creating GPU resources for Display Driver.";
     return;
   }
 }
@@ -861,7 +860,7 @@ bool BlenderDisplayDriver::gpu_resources_create()
 {
   /* Ensure context is active for resource creation. */
   if (!gpu_context_enable()) {
-    LOG_ERROR << "Error enabling blender::GPU context.";
+    LOG_ERROR << "Error enabling GPU context.";
     return false;
   }
 
@@ -869,7 +868,7 @@ bool BlenderDisplayDriver::gpu_resources_create()
   gpu_render_sync_ = blender::GPU_fence_create();
 
   if (!DCHECK_NOTNULL(gpu_upload_sync_) || !DCHECK_NOTNULL(gpu_render_sync_)) {
-    LOG_ERROR << "Error creating blender::GPU synchronization primitives.";
+    LOG_ERROR << "Error creating GPU synchronization primitives.";
     assert(0);
     return false;
   }

intern/cycles/blender/sync.cpp
@@ -116,7 +116,7 @@ void BlenderSync::sync_recalc(blender::Depsgraph &b_depsgraph,
   blender::Object *b_dicing_camera_object = get_dicing_camera_object(b_v3d, b_rv3d);
   bool dicing_camera_updated = false;
 
-  /* Iterate over all blender::IDs in this depsgraph. */
+  /* Iterate over all IDs in this depsgraph. */
   blender::DEGIDIterData deg_iter_data{};
   deg_iter_data.graph = &b_depsgraph;
   deg_iter_data.only_updated = true;
@@ -129,8 +129,8 @@ void BlenderSync::sync_recalc(blender::Depsgraph &b_depsgraph,
   {
     /* TODO(sergey): Can do more selective filter here. For example, ignore changes made to
      * screen data-block. Note that sync_data() needs to be called after object deletion, and
-     * currently this is ensured by the scene blender::ID tagged for update, which sets the
-     * `has_updates_` flag. */
+     * currently this is ensured by the scene ID tagged for update, which sets the `has_updates_`
+     * flag. */
     has_updates_ = true;
 
     const bool updated_shading = ((b_id->recalc & (blender::ID_RECALC_SHADING |
@@ -837,7 +837,7 @@ void BlenderSync::sync_render_passes(blender::RenderLayer &b_rlay,
   /* Always add combined pass. */
   pass_add(scene, PASS_COMBINED, "Combined");
 
-  /* Cryptomatte stores two blender::ID/weight pairs per RGBA layer.
+  /* Cryptomatte stores two ID/weight pairs per RGBA layer.
    * User facing parameter is the number of pairs. */
   const int crypto_depth = divide_up(min(16, b_view_layer.cryptomatte_levels), 2);
   scene->film->set_cryptomatte_depth(crypto_depth);

intern/cycles/device/cuda/device_impl.cpp
@@ -790,7 +790,7 @@ void CUDADevice::image_alloc(device_image &mem)
    *
    * Cycles expects to read all image data as normalized float values in
    * kernel/device/gpu/image.h. But storing all data as floats would be very inefficient due to the
-   * huge size of float image. So in the code below, we define different texture types including
+   * huge size of float images. So in the code below, we define different texture types including
    * integer types, with the aim of using CUDA's default promotion behavior of integer data to
    * floating point data in the range [0, 1], as noted in the CUDA documentation on
    * cuTexObjectCreate API Call.

intern/cycles/device/oneapi/device_impl.cpp
@@ -946,7 +946,7 @@ void OneapiDevice::image_free(device_image &mem)
           (sycl::ext::oneapi::experimental::image_mem_handle::raw_handle_type)cmem.array};
 
       try {
-        /* We have allocated only standard image, so we also deallocate only them. */
+        /* We have allocated only standard images, so we also deallocate only them. */
         sycl::ext::oneapi::experimental::free_image_mem(
             imgHandle, sycl::ext::oneapi::experimental::image_type::standard, *queue);
       }

intern/cycles/util/types_image.h
@@ -73,7 +73,7 @@ enum ImageFormatType {
   IMAGE_FORMAT_EQUIANGULAR,
 };
 
-/* Extension types for image.
+/* Extension types for images.
  *
  * Defines how the image is extrapolated past its original bounds. */
 enum ExtensionType {