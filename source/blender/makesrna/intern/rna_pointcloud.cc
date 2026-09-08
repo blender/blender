6e18d7a3fd2ddd5a1bd6f4078e8807e0c07bd97b
Sergey Sharybin <sergey@blender.org>
2026-09-08 10:53:56 +0200

    GSplat: Add type field to the PointCloud data-block
    The type could be either "Point" or "3D Gaussian Splat".
    
    It is defined in DNA, RNA, and exposed to the UI but does not actually
    control anything yet. It is the first step towards native integration
    of the 3D Gaussian Splat technique [kerbl3Dgaussians].
    
    In the future the type will control the following aspects:
    - Rendering algorithms
    - Attribute handling to eventually support rigging of gaussian splats.
    
    Attribute convention for the future IO and render engines integration:
    - "scale" for the scale of the gaussian splat, stored as float3
    - "rotation" gaussian splat rotation, stored as quternion
    - "radiance:base" the base (DC) radiance of the splats, followed by
       opacity and stored as float4
    - "radiance:sh_{index}" for the coefficients of the bands >= 1 of the
      spherical harmonics, stored as float3.
    
    Note that the relation of scale and radius is not very trivial (meaning
    storing scale as radius that would become float3 was considered but was
    not accepted). The scale is used to define the covariance matrix, and
    the gaussian splat is technically unbound. It is possible to calculate
    a bounding radius for the splat using scale and a cut-off opacity, but
    it is rather arbitrary.
    
    [kerbl3Dgaussians] Kerbl, Bernhard et. al.
      "3D Gaussian Splatting for Real-Time Radiance Field Rendering" (2023)
      ACM Transactions on Graphics
      https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/
    
    Ref #159470

--- source/blender/makesrna/intern/rna_pointcloud.cc
+++ source/blender/makesrna/intern/rna_pointcloud.cc
@@ -6,6 +6,8 @@
  * \ingroup RNA
  */
 
+#include "DNA_pointcloud_types.h"
+
 #include "RNA_define.hh"
 #include "RNA_enum_types.hh"
 
@@ -164,6 +166,20 @@ static void rna_def_point(BlenderRNA *brna)
 
 static void rna_def_pointcloud(BlenderRNA *brna)
 {
+  static const EnumPropertyItem type_items[] = {
+      {int(PointCloudType::Points),
+       "POINTS",
+       0,
+       "Points",
+       "Simple point cloud represented by points with position and radius"},
+      {int(PointCloudType::GSplat),
+       "GAUSSIAN_SPLAT",
+       0,
+       "3D Gaussian Splat",
+       "A point cloud that represents the original 3D Gaussian Splats technique"},
+      {0, nullptr, 0, nullptr, nullptr},
+  };
+
   StructRNA *srna;
   PropertyRNA *prop;
   FunctionRNA *func;
@@ -208,6 +224,12 @@ static void rna_def_pointcloud(BlenderRNA *brna)
                                     "rna_IDMaterials_assign_int");
 
   rna_def_attributes_common(srna, AttributeOwnerType::PointCloud);
+
+  /* Type. */
+  prop = RNA_def_property(srna, "type", PROP_ENUM, PROP_NONE);
+  RNA_def_property_enum_items(prop, type_items);
+  RNA_def_property_ui_text(prop, "Type", "Representation type of the pointcloud");
+  RNA_def_property_update(prop, 0, "rna_PointCloud_update_data");
 
   /* common */
   rna_def_animdata_common(srna);
