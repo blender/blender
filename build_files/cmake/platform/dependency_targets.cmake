da8bf9908fcd3a27c050718ecf5dbd3ea1e89f9d
Jonas Holzman <jonas@blender.org>
2026-09-08 15:23:17 +0200

    Cleanup: Tracy: Use TARGET check instead of WITH_TRACY in dependency_target.cmake

--- build_files/cmake/platform/dependency_targets.cmake
+++ build_files/cmake/platform/dependency_targets.cmake
@@ -510,7 +510,7 @@ endif()
 add_library(bf_deps_optional_tracy_client INTERFACE)
 add_library(bf::dependencies::optional::tracy_client ALIAS bf_deps_optional_tracy_client)
 
-if(WITH_TRACY)
+if(TARGET Tracy::TracyClient)
   target_compile_definitions(bf_deps_optional_tracy_client INTERFACE WITH_TRACY)
   target_link_libraries(bf_deps_optional_tracy_client INTERFACE Tracy::TracyClient)
 endif()
