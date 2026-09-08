0d0b5c9029fb0dd60efbbba6bc16f3e46bcec8e1
Jonas Holzman <jonas@blender.org>
2026-09-08 15:57:11 +0200

    Add WITH_JOLT to full/lite/release CMake configs

--- build_files/cmake/config/blender_release.cmake
+++ build_files/cmake/config/blender_release.cmake
@@ -65,6 +65,7 @@ set(WITH_USD                 ON  CACHE BOOL "" FORCE)
 set(WITH_MATERIALX           ON  CACHE BOOL "" FORCE)
 set(WITH_HYDRA               ON  CACHE BOOL "" FORCE)
 set(WITH_XR_OPENXR           ON  CACHE BOOL "" FORCE)
+set(WITH_JOLT                ON  CACHE BOOL "" FORCE)
 
 # platform dependent options
 if(APPLE)
