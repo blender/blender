51fdcfdbed6ee717e1cd72e7d0795dbfd4073ea5
Omar Emara <mail@OmarEmara.dev>
2026-09-11 13:32:37 +0200

    Compositor: Allow any image type in transform nodes
    This patch make it such that transform nodes can take any type and not
    just color images. This improves performance by skipping unnecessary
    type conversions.
    
    This skips the scale node for now since it has hardecoded code for color
    images.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/163790

--- source/blender/nodes/composite/nodes/node_composite_transform.cc
+++ source/blender/nodes/composite/nodes/node_composite_transform.cc
@@ -79,7 +79,11 @@ using namespace blender::compositor;
 
 class TransformOperation : public NodeOperation {
  public:
-  using NodeOperation::NodeOperation;
+  TransformOperation(Context &context, const bNode &node) : NodeOperation(context, node)
+  {
+    InputDescriptor &image_descriptor = this->get_input_descriptor("Image");
+    image_descriptor.skip_type_conversion = true;
+  }
 
   void execute() override
   {
@@ -92,6 +96,7 @@ class TransformOperation : public NodeOperation {
 
     const Result &input = this->get_input("Image");
     Result &output = this->get_result("Image");
+    output.set_type(input.type());
     output.share_data(input);
     output.transform(transformation);
     output.get_realization_options().interpolation = this->get_interpolation();
