blender/blender

commit f13eda6d31e778e37f126db0353c4be7c2d50a72

Fix: Ignore non-bone hits for bone selection
Stuff like object centers or geometry displayed by a pinned Viewer node
can end up in the same selection buffer, so whenever we go over
`GPUSelectBuffer` and want to retrieve bones, we should skip
`GPUSelectResult` with ids where
- a `sub_object_id` hasnt been encoded
- `BONESEL_ROOT`, `BONESEL_TIP` or `BONESEL_BONE` hasnt been encoded

Otherwise, those hits could be interpreted as bones, leading to crashes
or wrong selection down the line.

6aedb317b026 broke this with Overlay Next (in the way that the first
bone was always selected along with the origin) and another recent
commit betewen 5.1 and 5.2 broke this for the geometry nodes viewer.

So to resolve, introduce `ED_armature_selectresult_is_bone` including
these checks and call as early as possible (some related calls further
down might already have cleared `BONESEL_ANY` prior to passing on a
selection id)

NOTE: circle select handles this in the shader already (so that wasnt
exposing the problem)

NOTE: a similar issue might exist for metaballs or tracks, but havent
tracked down actual bugs yet.

NOTE: `ED_armature_object_and_ebone_from_select_buffer` seems to be
unused
Fixes #161981, #158055

Pull Request: https://projects.blender.org/blender/blender/pulls/162100

source/blender/editors/space_view3d/view3d_select.cc
@@ -2096,7 +2096,7 @@ static bool bone_mouse_select_menu(bContext *C,
 static bool selectbuffer_has_bones(const Span<GPUSelectResult> hit_results)
 {
   for (const GPUSelectResult &hit_result : hit_results) {
-    if (hit_result.id & 0xFFFF0000) {
+    if (ED_armature_selectresult_is_bone(hit_result)) {
       return true;
     }
   }
@@ -4255,12 +4255,13 @@ static bool do_armature_box_select(const ViewContext *vc, const rcti *rect, cons
 
   /* first we only check points inside the border */
   for (a = 0; a < hits; a++) {
-    const int select_id = buffer.storage[a].id;
-    if (select_id != -1) {
-      if ((select_id & 0xFFFF0000) == 0) {
-        continue;
-      }
+    GPUSelectResult &hit_result = buffer.storage[a];
+    if (!ED_armature_selectresult_is_bone(hit_result)) {
+      continue;
+    }
 
+    const int select_id = hit_result.id;
+    if (select_id != -1) {
       EditBone *ebone;
       Base *base_edit = ED_armature_base_and_ebone_from_select_buffer(bases, select_id, &ebone);
       ebone->temp.i |= select_id & BONESEL_ANY;
@@ -4404,6 +4405,10 @@ static void process_pose_bone_hits(GPUSelectBuffer &buffer,
        buf_iter < buf_end;
        buf_iter++)
   {
+    if (!ED_armature_selectresult_is_bone(*buf_iter)) {
+      continue;
+    }
+
     bPoseChannel *pose_bone;
     Base *base = ED_armature_base_and_pchan_from_select_buffer(bases, buf_iter->id, &pose_bone);
 

source/blender/editors/armature/armature_select.cc
@@ -58,6 +58,21 @@ namespace blender {
 /** \name Select Buffer Queries for PoseMode & EditMode
  * \{ */
 
+bool ED_armature_selectresult_is_bone(const GPUSelectResult &hit_result)
+{
+  /* A selectable bone should have a "sub_object_id" been encodedin the select id. */
+  if ((hit_result.id & 0xFFFF0000) == 0) {
+    return false;
+  }
+  /* A selectable bone should be dawn with either BONESEL_ROOT, BONESEL_TIP or BONESEL_BONE encded
+   * in the select id. */
+  if ((hit_result.id & BONESEL_ANY) == 0) {
+    return false;
+  }
+
+  return true;
+}
+
 Base *ED_armature_base_and_ebone_from_select_buffer(const Span<Base *> bases,
                                                     const uint select_id,
                                                     EditBone **r_ebone)
@@ -173,82 +188,85 @@ static void *ed_armature_pick_bone_from_selectbuffer_impl(const bool is_editmode
         gpu_select_buffer_depth_id_cmp);
 
   for (const GPUSelectResult &hit_result : hit_results_sorted) {
+    if (!ED_armature_selectresult_is_bone(hit_result)) {
+      continue;
+    }
+
     uint hit_id = hit_result.id;
-    if (hit_id & BONESEL_ANY) { /* to avoid including objects in selection */
-      Base *base = nullptr;
-      bool sel;
 
-      hit_id &= ~BONESEL_ANY;
-      /* Determine what the current bone is */
-      if (is_editmode == false) {
-        base = ED_armature_base_and_pchan_from_select_buffer(bases, hit_id, &pchan);
-        if (pchan != nullptr) {
-          if (pchan->bone_get(*base->object)->flag & BONE_UNSELECTABLE) {
-            continue;
-          }
-          if (findunsel) {
-            sel = (pchan->flag & POSE_SELECTED);
-          }
-          else {
-            sel = !(pchan->flag & POSE_SELECTED);
-          }
+    Base *base = nullptr;
+    bool sel;
 
-          data = pchan;
-        }
-        else {
-          data = nullptr;
-          sel = false;
-        }
-      }
-      else {
-        base = ED_armature_base_and_ebone_from_select_buffer(bases, hit_id, &ebone);
-        if (ebone->flag & BONE_UNSELECTABLE) {
+    hit_id &= ~BONESEL_ANY;
+    /* Determine what the current bone is */
+    if (is_editmode == false) {
+      base = ED_armature_base_and_pchan_from_select_buffer(bases, hit_id, &pchan);
+      if (pchan != nullptr) {
+        if (pchan->bone_get(*base->object)->flag & BONE_UNSELECTABLE) {
           continue;
         }
         if (findunsel) {
-          sel = (ebone->flag & BONE_SELECTED);
+          sel = (pchan->flag & POSE_SELECTED);
         }
         else {
-          sel = !(ebone->flag & BONE_SELECTED);
+          sel = !(pchan->flag & POSE_SELECTED);
         }
 
-        data = ebone;
+        data = pchan;
+      }
+      else {
+        data = nullptr;
+        sel = false;
+      }
+    }
+    else {
+      base = ED_armature_base_and_ebone_from_select_buffer(bases, hit_id, &ebone);
+      if (ebone->flag & BONE_UNSELECTABLE) {
+        continue;
+      }
+      if (findunsel) {
+        sel = (ebone->flag & BONE_SELECTED);
+      }
+      else {
+        sel = !(ebone->flag & BONE_SELECTED);
       }
 
-      if (data) {
-        if (sel) {
-          if (do_nearest) {
-            if (minsel > hit_result.depth) {
-              firstSel = data;
-              firstSel_base = base;
-              minsel = hit_result.depth;
-            }
-          }
-          else {
-            if (!firstSel) {
-              firstSel = data;
-              firstSel_base = base;
-            }
-            takeNext = true;
+      data = ebone;
+    }
+
+    if (data) {
+      if (sel) {
+        if (do_nearest) {
+          if (minsel > hit_result.depth) {
+            firstSel = data;
+            firstSel_base = base;
+            minsel = hit_result.depth;
           }
         }
         else {
-          if (do_nearest) {
-            if (minunsel > hit_result.depth) {
-              firstunSel = data;
-              firstunSel_base = base;
-              minunsel = hit_result.depth;
-            }
+          if (!firstSel) {
+            firstSel = data;
+            firstSel_base = base;
           }
-          else {
-            if (!firstunSel) {
-              firstunSel = data;
-              firstunSel_base = base;
-            }
-            if (takeNext) {
-              *r_base = base;
-              return data;
-            }
+          takeNext = true;
+        }
+      }
+      else {
+        if (do_nearest) {
+          if (minunsel > hit_result.depth) {
+            firstunSel = data;
+            firstunSel_base = base;
+            minunsel = hit_result.depth;
+          }
+        }
+        else {
+          if (!firstunSel) {
+            firstunSel = data;
+            firstunSel_base = base;
+          }
+          if (takeNext) {
+            *r_base = base;
+            return data;
           }
         }
       }
@@ -731,13 +749,19 @@ cache_end:
 
   Vector<Base *> bases = BKE_view_layer_array_from_bases_in_edit_mode_unique_data(
       *vc->bmain, vc->scene, vc->view_layer, vc->v3d);
-
   /* See if there are any selected bones in this group */
   if (hits > 0) {
     if (hits == 1) {
-      result_bias.select_id = buffer.storage[0].id;
-      result_bias.base = ED_armature_base_and_ebone_from_select_buffer(
-          bases, result_bias.select_id, &result_bias.ebone);
+      const GPUSelectResult &hit_result = buffer.storage[0];
+      if (ED_armature_selectresult_is_bone(hit_result)) {
+        result_bias.base = ED_armature_base_and_ebone_from_select_buffer(
+            bases, hit_result.id, &result_bias.ebone);
+        /* If this fails, selection code is setting the selection IDs incorrectly. */
+        BLI_assert(result_bias.base && result_bias.ebone);
+        if (result_bias.base != nullptr && result_bias.ebone != nullptr) {
+          result_bias.select_id = hit_result.id;
+        }
+      }
     }
     else {
       int bias_max = INT_MIN;
@@ -773,8 +797,11 @@ cache_end:
       int min_depth = INT_MAX;
       for (int i = 0; i < hits; i++) {
         const GPUSelectResult &hit_result = buffer.storage[i];
-        const uint select_id = hit_result.id;
+        if (!ED_armature_selectresult_is_bone(hit_result)) {
+          continue;
+        }
 
+        const uint select_id = hit_result.id;
         Base *base = nullptr;
         EditBone *ebone;
         base = ED_armature_base_and_ebone_from_select_buffer(bases, select_id, &ebone);

source/blender/editors/include/ED_armature.hh
@@ -139,6 +139,10 @@ wmOperatorStatus ED_armature_join_objects_exec(bContext *C, wmOperator *op);
 
 /* `armature_select.cc` */
 
+/**
+ * To avoid/skip non-bone hits.
+ */
+bool ED_armature_selectresult_is_bone(const GPUSelectResult &hit_result);
 Base *ED_armature_base_and_ebone_from_select_buffer(Span<Base *> bases,
                                                     unsigned int select_id,
                                                     EditBone **r_ebone);

commit af8e35b83435027aaf61de934ef2b82fb0990a1b

Fix #163605: Viewport pick selection of motion tracks fails
Caused by f13eda6d31

After f13eda6d31, we never really got to
`ed_object_select_pick_camera_track` since that was in a codepath that
is checked with
`selectbuffer_has_bones` (so the new `ED_armature_selectresult_is_bone`
which rejects the sub-object IDs used for drawing tracks (see
`object_sync_motion_tracking`)

To resolve, add a separate `selectbuffer_has_sub_object` check and use
it, restricted to camera objects, to gate (but also again allow for) the
track selection path.

Pull Request: https://projects.blender.org/blender/blender/pulls/163607

source/blender/editors/space_view3d/view3d_select.cc
@@ -2103,6 +2103,20 @@ static bool selectbuffer_has_bones(const Span<GPUSelectResult> hit_results)
   return false;
 }
 
+/**
+ * Camera reconstruction bundles encode a sub-object ID without any of the #BONESEL_ANY bits,
+ * so they are not detected by #selectbuffer_has_bones.
+ */
+static bool selectbuffer_has_sub_object(const Span<GPUSelectResult> hit_results)
+{
+  for (const GPUSelectResult &hit_result : hit_results) {
+    if (hit_result.id & 0xFFFF0000) {
+      return true;
+    }
+  }
+  return false;
+}
+
 /* utility function for mixed_bones_object_selectbuffer */
 static int selectbuffer_ret_hits_15(MutableSpan<GPUSelectResult> /*hit_results*/, const int hits15)
 {
@@ -2639,6 +2653,7 @@ static bool ed_object_select_pick(bContext *C,
     int hits;
     bool do_nearest;
     bool has_bones;
+    bool has_sub_object;
   } *gpu = nullptr;
 
   /* First handle menu selection, early exit if a menu opens
@@ -2651,6 +2666,7 @@ static bool ed_object_select_pick(bContext *C,
     gpu = MEM_new<GPUData>(__func__);
     gpu->do_nearest = false;
     gpu->has_bones = false;
+    gpu->has_sub_object = false;
 
     /* If objects have pose-mode set, the bones are in the same selection buffer. */
     const eV3DSelectObjectFilter select_filter =
@@ -2661,6 +2677,10 @@ static bool ed_object_select_pick(bContext *C,
     gpu->has_bones = (object_only && gpu->hits > 0) ?
                          false :
                          selectbuffer_has_bones(gpu->buffer.storage.as_span().slice(0, gpu->hits));
+    gpu->has_sub_object = (object_only && gpu->hits > 0) ?
+                              false :
+                              selectbuffer_has_sub_object(
+                                  gpu->buffer.storage.as_span().slice(0, gpu->hits));
   }
 
   /* First handle menu selection, early exit when a menu was opened.
@@ -2767,46 +2787,49 @@ static bool ed_object_select_pick(bContext *C,
     /* See comment for `has_pose_old`, the same rationale applies here. */
     const bool has_pose_new = (basact &&
                                BKE_object_pose_armature_get_with_wpaint_check(basact->object));
+    /* Select camera-tracks. */
+    if ((gpu->hits > 0) && gpu->has_sub_object && basact && (basact->object->type == OB_CAMERA)) {
+      MovieClip *clip = BKE_object_movieclip_get(scene, basact->object, false);
+      if (clip != nullptr) {
+        if (ed_object_select_pick_camera_track(
+                C, scene, basact, clip, gpu->buffer, gpu->hits, params))
+        {
+          ed::object::base_select(basact, ed::object::BA_SELECT);
+          /* Don't set `handled` here as the object activation may be necessary. */
+          changed_object = true;
 
-    /* Select pose-bones or camera-tracks. */
-    if (((gpu->hits > 0) && gpu->has_bones) ||
-        /* Special case, even when there are no hits, pose logic may de-select all bones. */
-        ((gpu->hits == 0) && has_pose_old))
+          changed_track = true;
+        }
+        else {
+          /* Fallback to regular object selection if no new bundles were selected,
+           * allows to select object parented to reconstruction object. */
+          basact = mouse_select_eval_buffer(
+              &vc, gpu->buffer, gpu->hits, gpu->do_nearest, false, false, nullptr);
+        }
+      }
+      /* Prevent track selecting to pass on to object selecting. */
+      if (basact == oldbasact) {
+        handled = true;
+      }
+    }
+    /* Select pose-bones. */
+    else if (((gpu->hits > 0) && gpu->has_bones) ||
+             /* Special case, even when there are no hits, pose logic may de-select all bones. */
+             ((gpu->hits == 0) && has_pose_old))
     {
       /* Regarding the `basact` null checks.
        * While it's unlikely there are GPU hits *without* `basact` being found,
        * it's possible looking up the selection index fails, see: #143161. */
-
-      if (basact && (gpu->has_bones && (basact->object->type == OB_CAMERA))) {
-        MovieClip *clip = BKE_object_movieclip_get(scene, basact->object, false);
-        if (clip != nullptr) {
-          if (ed_object_select_pick_camera_track(
-                  C, scene, basact, clip, gpu->buffer, gpu->hits, params))
-          {
-            ed::object::base_select(basact, ed::object::BA_SELECT);
-            /* Don't set `handled` here as the object activation may be necessary. */
-            changed_object = true;
-
-            changed_track = true;
-          }
-          else {
-            /* Fallback to regular object selection if no new bundles were selected,
-             * allows to select object parented to reconstruction object. */
-            basact = mouse_select_eval_buffer(
-                &vc, gpu->buffer, gpu->hits, gpu->do_nearest, false, false, nullptr);
-          }
-        }
-      }
-      else if ((basact || oldbasact) && ED_armature_pose_select_pick_with_buffer(
-                                            *vc.bmain,
-                                            scene,
-                                            view_layer,
-                                            v3d,
-                                            basact ? basact : const_cast<Base *>(oldbasact),
-                                            gpu->buffer.storage.data(),
-                                            gpu->hits,
-                                            params,
-                                            gpu->do_nearest))
+      if ((basact || oldbasact) &&
+          ED_armature_pose_select_pick_with_buffer(*vc.bmain,
+                                                   scene,
+                                                   view_layer,
+                                                   v3d,
+                                                   basact ? basact : const_cast<Base *>(oldbasact),
+                                                   gpu->buffer.storage.data(),
+                                                   gpu->hits,
+                                                   params,
+                                                   gpu->do_nearest))
       {
 
         changed_pose = true;
@@ -2859,7 +2882,7 @@ static bool ed_object_select_pick(bContext *C,
           }
         }
       }
-      /* Prevent bone/track selecting to pass on to object selecting. */
+      /* Prevent bone selecting to pass on to object selecting. */
       if (basact == oldbasact) {
         handled = true;
       }