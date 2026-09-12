blender/blender

commit b7e47b113f30303d14ffb6cba06bf44f721ef44f

Cleanup: Clarify usage of `OB_MODE_ALL_PAINT`
The `OB_MODE_ALL_PAINT` macro is confusingly used and named, it
specifically refers to the mesh-related paint modes, not *all* paint
modes.

Rename the existing macro to `OB_MODE_ALL_PAINT_MESH` and introduce a
new `OB_MODE_ALL_PAINT` that actually encompasses all paint modes.

Pull Request: https://projects.blender.org/blender/blender/pulls/163683

source/blender/editors/sculpt_paint/paint_curve.cc
@@ -51,9 +51,7 @@ bool paint_curve_poll(bContext *C)
   RegionView3D *rv3d = CTX_wm_region_view3d(C);
   SpaceImage *sima;
 
-  if (rv3d && !(ob && ((ob->mode & (OB_MODE_ALL_PAINT | OB_MODE_SCULPT_CURVES |
-                                    OB_MODE_SCULPT_GREASE_PENCIL)) != 0)))
-  {
+  if (rv3d && !(ob && (ob->mode & (OB_MODE_ALL_PAINT)) != 0)) {
     return false;
   }
 

source/blender/editors/transform/transform_generics.cc
@@ -274,9 +274,7 @@ void initTransInfo(bContext *C, TransInfo *t, wmOperator *op, const wmEvent *eve
       t->flag |= T_V3D_ALIGN;
     }
 
-    if ((object_mode & OB_MODE_ALL_PAINT) ||
-        (object_mode & (OB_MODE_SCULPT_CURVES | OB_MODE_SCULPT_GREASE_PENCIL)))
-    {
+    if ((object_mode & OB_MODE_ALL_PAINT)) {
       Paint *paint = BKE_paint_get_active_from_context(C);
       Brush *brush = (paint) ? BKE_paint_brush(paint) : nullptr;
       if (brush && (brush->stroke_method == BRUSH_STROKE_CURVE)) {

source/blender/blenloader/intern/versioning_250.cc
@@ -1709,7 +1709,7 @@ void blo_do_versions_250(FileData *fd, Library * /*lib*/, Main *bmain)
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 255, 1)) {
     for (Brush &br : bmain->brushes) {
       if (br.ob_mode == 0) {
-        br.ob_mode = OB_MODE_ALL_PAINT;
+        br.ob_mode = OB_MODE_ALL_PAINT_MESH;
       }
     }
 

source/blender/draw/engines/overlay/overlay_cursor.hh
@@ -149,7 +149,7 @@ class Cursor : Overlay {
     }
 
     /* don't draw cursor in paint modes, but with a few exceptions */
-    if ((state.object_mode & (OB_MODE_ALL_PAINT | OB_MODE_SCULPT_CURVES)) != 0) {
+    if ((state.object_mode & (OB_MODE_ALL_PAINT_MESH | OB_MODE_SCULPT_CURVES)) != 0) {
       /* exception: object is in weight paint and has deforming armature in pose mode */
       if (state.object_mode & OB_MODE_WEIGHT_PAINT) {
         if (BKE_object_pose_armature_get(const_cast<Object *>(state.object_active)) != nullptr) {

source/blender/draw/engines/overlay/overlay_instance.cc
@@ -1029,7 +1029,7 @@ bool Instance::object_is_selected(const ObjectRef &ob_ref)
 bool Instance::object_is_paint_mode(const Object *object)
 {
   return (object == state.object_active) &&
-         (state.object_mode & (OB_MODE_ALL_PAINT | OB_MODE_ALL_PAINT_GPENCIL));
+         (state.object_mode & (OB_MODE_ALL_PAINT_MESH | OB_MODE_ALL_PAINT_GPENCIL));
 }
 
 bool Instance::object_is_sculpt_mode(const ObjectRef &ob_ref)

source/blender/draw/engines/overlay/overlay_origin.hh
@@ -30,9 +30,7 @@ class Origins : Overlay {
 
   void begin_sync(Resources & /*res*/, const State &state) final
   {
-    const bool is_paint_mode = (state.object_mode &
-                                (OB_MODE_ALL_PAINT | OB_MODE_ALL_PAINT_GPENCIL |
-                                 OB_MODE_SCULPT_CURVES)) != 0;
+    const bool is_paint_mode = (state.object_mode & (OB_MODE_ALL_PAINT)) != 0;
     enabled_ = state.is_space_v3d() && !is_paint_mode && state.show_object_origins();
     point_buf_.clear();
   }

source/blender/draw/intern/draw_handle.hh
@@ -419,7 +419,7 @@ class ObjectRef {
       return true;
     }
 
-    if (ob_mode & (OB_MODE_ALL_PAINT | OB_MODE_ALL_PAINT_GPENCIL)) {
+    if (ob_mode & (OB_MODE_ALL_PAINT_MESH | OB_MODE_ALL_PAINT_GPENCIL)) {
       return true;
     }
 

source/blender/editors/object/object_edit.cc
@@ -196,7 +196,7 @@ Vector<Object *> objects_in_mode_or_selected(bContext *C,
      * irrespective of selection. */
     ob = ob_active;
   }
-  else if (ob_active && (ob_active->mode & (OB_MODE_ALL_PAINT | OB_MODE_ALL_PAINT_GPENCIL))) {
+  else if (ob_active && (ob_active->mode & (OB_MODE_ALL_PAINT_MESH | OB_MODE_ALL_PAINT_GPENCIL))) {
     /* When painting, limit to active. */
     ob = ob_active;
   }
@@ -1664,7 +1664,7 @@ static wmOperatorStatus shade_smooth_exec(bContext *C, wmOperator *op)
     ViewLayer *view_layer = CTX_data_view_layer(C);
     BKE_view_layer_synced_ensure(*bmain, scene, view_layer);
     Object *obact = BKE_view_layer_active_object_get(view_layer);
-    if (obact && (obact->mode & OB_MODE_ALL_PAINT)) {
+    if (obact && (obact->mode & OB_MODE_ALL_PAINT_MESH)) {
       ctx_objects.append(RNA_id_pointer_create(&obact->id));
     }
   }

commit 53d5fabf2eb8882a2bde0a0c9df2d485052b517a

Fix: Incorrect usage of OB_MODE_ALL_PAINT
Introduced with b7e47b113f3

Pull Request: https://projects.blender.org/blender/blender/pulls/163830

source/blender/editors/sculpt_paint/paint_curve.cc
@@ -51,7 +51,9 @@ bool paint_curve_poll(bContext *C)
   RegionView3D *rv3d = CTX_wm_region_view3d(C);
   SpaceImage *sima;
 
-  if (rv3d && !(ob && (ob->mode & (OB_MODE_ALL_PAINT)) != 0)) {
+  if (rv3d && !(ob && ((ob->mode & (OB_MODE_ALL_PAINT_MESH | OB_MODE_SCULPT_CURVES |
+                                    OB_MODE_SCULPT_GREASE_PENCIL)) != 0)))
+  {
     return false;
   }
 

source/blender/editors/transform/transform_generics.cc
@@ -274,7 +274,9 @@ void initTransInfo(bContext *C, TransInfo *t, wmOperator *op, const wmEvent *eve
       t->flag |= T_V3D_ALIGN;
     }
 
-    if ((object_mode & OB_MODE_ALL_PAINT)) {
+    if (object_mode &
+        (OB_MODE_ALL_PAINT_MESH | OB_MODE_SCULPT_CURVES | OB_MODE_SCULPT_GREASE_PENCIL))
+    {
       Paint *paint = BKE_paint_get_active_from_context(C);
       Brush *brush = (paint) ? BKE_paint_brush(paint) : nullptr;
       if (brush && (brush->stroke_method == BRUSH_STROKE_CURVE)) {