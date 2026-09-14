blender/blender

commit 954a62b3420b287de83ca92ab80b3ddaf5208aa0

Refactor: Nodes: use separate type for internal links
Previously, normal links and internal links both used `bNodeLink`.
However, they are quite different. Internal links are fully runtime
(only computed for muted nodes) and don't need the flags and other stuff
stored on normal links. When `bNodeLink` is extended with e.g. runtime
data, internal link storage shouldn't need to change.

Pull Request: https://projects.blender.org/blender/blender/pulls/163849

source/blender/nodes/intern/node_exec.cc
@@ -78,31 +78,19 @@ static void node_init_input_index(bNodeSocket *sock, int *index)
   }
 }
 
-static void node_init_output_index_muted(bNodeSocket *sock,
-                                         int *index,
-                                         const MutableSpan<bNodeLink> internal_links)
+static void node_init_output_index_muted(bNodeSocket *sock, int *index)
 {
-  const bNodeLink *link;
-  /* copy the stack index from internally connected input to skip the node */
-  for (bNodeLink &iter_link : internal_links) {
-    if (iter_link.tosock == sock) {
-      sock->stack_index = iter_link.fromsock->stack_index;
-      /* set the link pointer to indicate that this socket
-       * should not overwrite the stack value!
-       */
-      sock->link = &iter_link;
-      link = &iter_link;
-      break;
-    }
+  /* Copy the stack index from the internally connected input to skip the node. */
+  if (const bNodeSocket *internal_input = sock->runtime->internal_link_input) {
+    sock->stack_index = internal_input->stack_index;
+    return;
   }
-  /* if not internally connected, assign a new stack index anyway to avoid bad stack access */
-  if (!link) {
-    if (node_exec_socket_use_stack(sock)) {
-      sock->stack_index = (*index)++;
-    }
-    else {
-      sock->stack_index = -1;
-    }
+  /* If not internally connected, assign a new stack index anyway to avoid bad stack access. */
+  if (node_exec_socket_use_stack(sock)) {
+    sock->stack_index = (*index)++;
+  }
+  else {
+    sock->stack_index = -1;
   }
 }
 
@@ -126,6 +114,10 @@ static bNodeStack *setup_stack(bNodeStack *stack, bNodeTree *ntree, bNode *node,
 
   /* don't mess with remote socket stacks, these are initialized by other nodes! */
   if (sock->link && !(sock->link->flag & NODE_LINK_MUTED)) {
+    return ns;
+  }
+  /* Outputs of muted nodes forward the internally linked input's stack value. */
+  if (sock->runtime->internal_link_input != nullptr) {
     return ns;
   }
 
@@ -231,7 +223,7 @@ bNodeTreeExec *ntree_exec_begin(bNodeExecContext *context,
 
     if (node->is_muted() || node->is_reroute()) {
       for (bNodeSocket &sock : node->outputs) {
-        node_init_output_index_muted(&sock, &index, node->runtime->internal_links);
+        node_init_output_index_muted(&sock, &index);
       }
     }
     else {

intern/cycles/blender/shader.cpp
@@ -1360,8 +1360,8 @@ static void add_nodes_inlined(Scene *scene,
   for (blender::bNode *b_node : b_ntree.all_nodes()) {
     if (b_node->is_muted() || b_node->is_reroute()) {
       /* replace muted node with internal links */
-      for (blender::bNodeLink &b_link : b_node->runtime->internal_links) {
-        blender::bNodeSocket *to_socket = b_link.tosock;
+      for (blender::bNodeInternalLink &b_link : b_node->runtime->internal_links) {
+        blender::bNodeSocket *to_socket = b_link.out;
         const SocketType::Type to_socket_type = convert_socket_type(*to_socket);
         if (to_socket_type == SocketType::UNDEFINED) {
           continue;
@@ -1371,8 +1371,8 @@ static void add_nodes_inlined(Scene *scene,
 
         /* Muted nodes can result in multiple Cycles input sockets mapping to the same Blender
          * input socket, so this needs to be a multimap. */
-        input_map.emplace(b_link.fromsock, proxy->inputs[0]);
-        output_map[b_link.tosock] = proxy->outputs[0];
+        input_map.emplace(b_link.in, proxy->inputs[0]);
+        output_map[b_link.out] = proxy->outputs[0];
       }
     }
     else if (b_node->is_group()) {

source/blender/blenkernel/BKE_node.hh
@@ -752,7 +752,6 @@ void node_remove_node(
 
 float2 node_dimensions_get(const bNode &node);
 void node_tag_update_id(bNode &node);
-void node_internal_links(bNode &node, bNodeLink **r_links, int *r_len);
 
 /**
  * Also used via RNA API, so we check for proper input output direction.

source/blender/blenkernel/BKE_node_runtime.hh
@@ -34,6 +34,13 @@ struct bNode;
 struct bNodeSocket;
 struct bNodeTree;
 
+struct bNodeInternalLink {
+  bNodeSocket *in = nullptr;
+  bNodeSocket *out = nullptr;
+
+  friend bool operator==(const bNodeInternalLink &a, const bNodeInternalLink &b) = default;
+};
+
 namespace nodes {
 struct EvalDependencies;
 struct GeneratedTreeSrnaData;
@@ -388,7 +395,7 @@ class bNodeRuntime : NonCopyable, NonMovable {
   float anim_ofsx;
 
   /** List of cached internal links (input to output), for muted nodes and operators. */
-  Vector<bNodeLink> internal_links;
+  Vector<bNodeInternalLink> internal_links;
 
   /** Eagerly maintained cache of the node's index in the tree. */
   int index_in_tree = -1;
@@ -926,7 +933,7 @@ inline bool bNode::is_type(const UString query_idname) const
   return this->typeinfo->is_type(query_idname);
 }
 
-inline Span<bNodeLink> bNode::internal_links() const
+inline Span<bNodeInternalLink> bNode::internal_links() const
 {
   return this->runtime->internal_links;
 }

source/blender/blenkernel/intern/node.cc
@@ -4014,8 +4014,8 @@ void node_remove_socket_ex(bNodeTree &ntree, bNode &node, bNodeSocket &sock, con
   }
 
   for (const int64_t i : node.runtime->internal_links.index_range()) {
-    const bNodeLink &link = node.runtime->internal_links[i];
-    if (link.fromsock == &sock || link.tosock == &sock) {
+    const bNodeInternalLink &link = node.runtime->internal_links[i];
+    if (link.in == &sock || link.out == &sock) {
       node.runtime->internal_links.remove_and_reorder(i);
       BKE_ntree_update_tag_node_internal_link(&ntree, &node);
       break;
@@ -4373,11 +4373,9 @@ bNode *node_copy_with_mapping(bNodeTree *dst_tree,
       MEM_dupalloc(node_src.panel_states_array));
 
   node_dst->runtime->internal_links = node_src.runtime->internal_links;
-  for (bNodeLink &dst_link : node_dst->runtime->internal_links) {
-    dst_link.fromnode = node_dst;
-    dst_link.tonode = node_dst;
-    dst_link.fromsock = socket_map.lookup(dst_link.fromsock);
-    dst_link.tosock = socket_map.lookup(dst_link.tosock);
+  for (bNodeInternalLink &dst_link : node_dst->runtime->internal_links) {
+    dst_link.in = socket_map.lookup(dst_link.in);
+    dst_link.out = socket_map.lookup(dst_link.out);
   }
 
   if ((flag & LIB_ID_CREATE_NO_USER_REFCOUNT) == 0) {
@@ -4801,11 +4799,6 @@ static void adjust_multi_input_indices_after_removed_link(bNodeTree *ntree,
 
 void node_internal_relink(bNodeTree &ntree, bNode &node)
 {
-  /* store link pointers in output sockets, for efficient lookup */
-  for (bNodeLink &link : node.runtime->internal_links) {
-    link.tosock->link = &link;
-  }
-
   Vector<bNodeLink *> duplicate_links_to_remove;
 
   /* redirect downstream links */
@@ -4815,8 +4808,14 @@ void node_internal_relink(bNodeTree &ntree, bNode &node)
       continue;
     }
 
-    bNodeLink *internal_link = link.fromsock->link;
-    bNodeLink *fromlink = internal_link ? internal_link->fromsock->link : nullptr;
+    const bNodeSocket *internal_input = nullptr;
+    for (const bNodeInternalLink &internal_link : node.runtime->internal_links) {
+      if (internal_link.out == link.fromsock) {
+        internal_input = internal_link.in;
+        break;
+      }
+    }
+    bNodeLink *fromlink = internal_input ? internal_input->link : nullptr;
 
     if (fromlink == nullptr) {
       if (link.tosock->is_multi_input()) {
@@ -5582,12 +5581,6 @@ float2 node_dimensions_get(const bNode &node)
 void node_tag_update_id(bNode &node)
 {
   node.runtime->update |= NODE_UPDATE_ID;
-}
-
-void node_internal_links(bNode &node, bNodeLink **r_links, int *r_len)
-{
-  *r_links = node.runtime->internal_links.data();
-  *r_len = node.runtime->internal_links.size();
 }
 
 /* Node Instance Hash */

source/blender/blenkernel/intern/node_runtime.cc
@@ -109,8 +109,8 @@ static void update_internal_link_inputs(const bNodeTree &ntree)
     for (bNodeSocket *socket : node->runtime->outputs) {
       socket->runtime->internal_link_input = nullptr;
     }
-    for (bNodeLink &link : node->runtime->internal_links) {
-      link.tosock->runtime->internal_link_input = link.fromsock;
+    for (bNodeInternalLink &link : node->runtime->internal_links) {
+      link.out->runtime->internal_link_input = link.in;
     }
   }
 }

source/blender/blenkernel/intern/node_tree_reference_lifetimes.cc
@@ -422,9 +422,9 @@ static bool pass_left_to_right(const bNodeTree &tree,
       }
     }
     if (node->is_muted()) {
-      for (const bNodeLink &link : node->internal_links()) {
-        const bNodeSocket &input_socket = *link.fromsock;
-        const bNodeSocket &output_socket = *link.tosock;
+      for (const bNodeInternalLink &link : node->internal_links()) {
+        const bNodeSocket &input_socket = *link.in;
+        const bNodeSocket &output_socket = *link.out;
         if (!input_socket.is_available() || !output_socket.is_available()) {
           continue;
         }
@@ -724,9 +724,9 @@ static bool pass_right_to_left(const bNodeTree &tree,
       }
     }
     if (node->is_muted()) {
-      for (const bNodeLink &link : node->internal_links()) {
-        const bNodeSocket &input_socket = *link.fromsock;
-        const bNodeSocket &output_socket = *link.tosock;
+      for (const bNodeInternalLink &link : node->internal_links()) {
+        const bNodeSocket &input_socket = *link.in;
+        const bNodeSocket &output_socket = *link.out;
         if (!input_socket.is_available() || !output_socket.is_available()) {
           continue;
         }

source/blender/blenkernel/intern/node_tree_update.cc
@@ -864,25 +864,6 @@ class NodeTreeMainUpdater {
     }
   }
 
-  struct InternalLink {
-    bNodeSocket *from;
-    bNodeSocket *to;
-    int multi_input_sort_id = 0;
-
-    friend bool operator==(const InternalLink &a, const InternalLink &b) = default;
-  };
-
-  const bNodeLink *first_non_dangling_link(const bNodeTree & /*ntree*/,
-                                           const Span<const bNodeLink *> links) const
-  {
-    for (const bNodeLink *link : links) {
-      if (!link->fromnode->is_dangling_reroute()) {
-        return link;
-      }
-    }
-    return nullptr;
-  }
-
   void update_internal_links(bNodeTree &ntree)
   {
     bke::node_tree_runtime::AllowUsingOutdatedInfo allow_outdated_info{ntree};
@@ -892,7 +873,7 @@ class NodeTreeMainUpdater {
         continue;
       }
       /* Find all expected internal links. */
-      Vector<InternalLink> expected_internal_links;
+      Vector<bNodeInternalLink> expected_internal_links;
       for (const bNodeSocket *output_socket : node->output_sockets()) {
         if (!output_socket->is_available()) {
           continue;
@@ -907,14 +888,8 @@ class NodeTreeMainUpdater {
           continue;
         }
 
-        const Span<const bNodeLink *> connected_links = input_socket->directly_linked_links();
-        const bNodeLink *connected_link = first_non_dangling_link(ntree, connected_links);
-
-        const int index = connected_link ? connected_link->multi_input_sort_id :
-                                           std::max<int>(0, connected_links.size() - 1);
-        expected_internal_links.append(InternalLink{const_cast<bNodeSocket *>(input_socket),
-                                                    const_cast<bNodeSocket *>(output_socket),
-                                                    index});
+        expected_internal_links.append(bNodeInternalLink{
+            const_cast<bNodeSocket *>(input_socket), const_cast<bNodeSocket *>(output_socket)});
       }
 
       /* Rebuilt internal links if they have changed. */
@@ -926,9 +901,8 @@ class NodeTreeMainUpdater {
       const bool all_expected_internal_links_exist = std::all_of(
           node->runtime->internal_links.begin(),
           node->runtime->internal_links.end(),
-          [&](const bNodeLink &link) {
-            const InternalLink internal_link{link.fromsock, link.tosock, link.multi_input_sort_id};
-            return expected_internal_links.as_span().contains(internal_link);
+          [&](const bNodeInternalLink &link) {
+            return expected_internal_links.as_span().contains(link);
           });
 
       if (all_expected_internal_links_exist) {
@@ -977,20 +951,10 @@ class NodeTreeMainUpdater {
 
   void update_internal_links_in_node(bNodeTree &ntree,
                                      bNode &node,
-                                     Span<InternalLink> internal_links)
+                                     Span<bNodeInternalLink> internal_links)
   {
     node.runtime->internal_links.clear();
-    node.runtime->internal_links.reserve(internal_links.size());
-    for (const InternalLink &internal_link : internal_links) {
-      bNodeLink link{};
-      link.fromnode = &node;
-      link.fromsock = internal_link.from;
-      link.tonode = &node;
-      link.tosock = internal_link.to;
-      link.multi_input_sort_id = internal_link.multi_input_sort_id;
-      link.flag |= NODE_LINK_VALID;
-      node.runtime->internal_links.append(link);
-    }
+    node.runtime->internal_links.extend(internal_links);
     BKE_ntree_update_tag_node_internal_link(&ntree, &node);
   }
 

commit 7d3ec00cc28dc96388ff5e25ab1f51c38b0c9533

Fix: Texture Nodes: wrong internal link and reroute handling
This was broken by 954a62b3420.

Pull Request: https://projects.blender.org/blender/blender/pulls/163908

source/blender/nodes/intern/node_exec.cc
@@ -117,7 +117,10 @@ static bNodeStack *setup_stack(bNodeStack *stack, bNodeTree *ntree, bNode *node,
     return ns;
   }
   /* Outputs of muted nodes forward the internally linked input's stack value. */
-  if (sock->runtime->internal_link_input != nullptr) {
+  if (node->is_muted() && sock->runtime->internal_link_input != nullptr) {
+    return ns;
+  }
+  if (sock->is_output() && node->is_reroute()) {
     return ns;
   }
 