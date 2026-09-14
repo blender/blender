blender/blender

commit 4c1b8d11ace2c07a905a1ce63aa27c8db1ee9150

Anim: Improved AnimData rename API
Through a convoluted logic the previous AnimData renaming code
basically did:
* For any given ID
* check all FCurves it uses directly for the rename string
(includes owned drivers)
* check all other AnimData in Main if the given ID is being used as
a driver target

When doing that in a loop for many IDs, things can slow down
considerably. The solution is to build a map of ID to the drivers using
it as a target beforehand.
That way, we only have to do loop main once to build the map and
can then reuse it to subsequent calls to the rename function
(which only looks at the given ID)

### Implementation

**Removed `BKE_animdata_fix_paths_rename`**:
Since this function doesn't check for driver targets by looping main,
it missed to rename them leaving invalid drivers behind. When renaming
anything that can be animated it is never correct to ignore driver
targets which is why I removed it. Replaced with calls to
`BKE_animdata_fix_paths`.

**Removed `BKE_animdata_fix_paths_rename_all_ex` and `BKE_animdata_fix_paths_rename_all`**:
This has been replaced with a combination of
`BKE_animdata_build_driver_target_map` and `BKE_animdata_fix_paths`.
By splitting this up we can get the performance
gains when doing many renames at once.

**Added `RNA_path_name_to_infix` and `RNA_path_number_to_infix`**:
The new functions no longer assume anything about the rename string
they are given. Instead the caller has to format the strings accordingly
which is done with this function.
Doing so removes a boolean argument from the API.

In order to get the un-escaped name (which is needed for drivers
targeting pose bones by name) I added `infix_to_name`. This function
is not public because it makes a few assumptions
about the structure of the given string.

**`driver_target_path_fix` no longer verifies the path**
RNA paths for drivers can contain a trailing number like `[0]` to
indicate the array index. For regular rna paths this is usually a

source/blender/blenloader/intern/versioning_500.cc
@@ -65,6 +65,8 @@
 #include "BKE_pointcache.h"
 #include "BKE_report.hh"
 
+#include "RNA_path.hh"
+
 #include "BLT_translation.hh"
 
 #include "BLO_read_write.hh"
@@ -1312,10 +1314,9 @@ static void do_version_convert_to_generic_nodes(bNodeTree *node_tree)
 
 /* Equivalent to do_version_convert_to_generic_nodes but performed after linking for handing things
  * like animation or node construction. */
-static void do_version_convert_to_generic_nodes_after_linking(Main *bmain,
-                                                              bNodeTree *node_tree,
-                                                              ID *id)
+static void do_version_convert_to_generic_nodes_after_linking(Main *bmain, bNodeTree *node_tree)
 {
+  const DriverMap driver_map = BKE_animdata_build_driver_target_map(*bmain);
   for (bNode &node : node_tree->nodes.items_mutable()) {
     char escaped_node_name[sizeof(node.name) * 2 + 1];
     BLI_str_escape(escaped_node_name, node.name, sizeof(escaped_node_name));
@@ -1327,15 +1328,12 @@ static void do_version_convert_to_generic_nodes_after_linking(Main *bmain,
       case SH_NODE_CURVE_VEC: {
         /* The node gained a new Factor input as a first socket, so the vector socket moved to be
          * the second socket and we need to transfer its animation as well. */
-        BKE_animdata_fix_paths_rename_all_ex(bmain,
-                                             id,
-                                             rna_path_prefix.c_str(),
-                                             nullptr,
-                                             nullptr,
-                                             0,
-                                             1,
-                                             /*verify_paths=*/false,
-                                             /*infix_is_name=*/true);
+        BKE_animdata_fix_paths(node_tree->id,
+                               rna_path_prefix,
+                               RNA_path_number_to_infix(0),
+                               RNA_path_number_to_infix(1),
+                               /*verify_paths=*/false,
+                               driver_map);
         break;
       }
       /* Notice that we use the shader type because the node is already converted in versioning
@@ -1343,24 +1341,18 @@ static void do_version_convert_to_generic_nodes_after_linking(Main *bmain,
       case SH_NODE_MIX: {
         /* The node gained multiple new sockets after the factor socket, so the second and third
          * sockets moved to be the 7th and 8th sockets. */
-        BKE_animdata_fix_paths_rename_all_ex(bmain,
-                                             id,
-                                             rna_path_prefix.c_str(),
-                                             nullptr,
-                                             nullptr,
-                                             1,
-                                             6,
-                                             /*verify_paths=*/false,
-                                             /*infix_is_name=*/true);
-        BKE_animdata_fix_paths_rename_all_ex(bmain,
-                                             id,
-                                             rna_path_prefix.c_str(),
-                                             nullptr,
-                                             nullptr,
-                                             2,
-                                             7,
-                                             /*verify_paths=*/false,
-                                             /*infix_is_name=*/true);
+        BKE_animdata_fix_paths(node_tree->id,
+                               rna_path_prefix,
+                               RNA_path_number_to_infix(1),
+                               RNA_path_number_to_infix(6),
+                               /*verify_paths=*/false,
+                               driver_map);
+        BKE_animdata_fix_paths(node_tree->id,
+                               rna_path_prefix,
+                               RNA_path_number_to_infix(2),
+                               RNA_path_number_to_infix(7),
+                               /*verify_paths=*/false,
+                               driver_map);
         break;
       }
       default:
@@ -2611,7 +2603,7 @@ static bool window_has_sequence_editor_open(const wmWindow *win)
 
 /* Merge transform effect properties with strip transform. Because this effect could use modifiers,
  * change its type to gaussian blur with 0 radius. */
-static void sequencer_substitute_transform_effects(Scene *scene)
+static void sequencer_substitute_transform_effects(Main &bmain, Scene *scene)
 {
   seq::foreach_strip(&scene->ed->seqbase, [&](Strip *strip) -> bool {
     if (strip->type == STRIP_TYPE_TRANSFORM_LEGACY && strip->effectdata != nullptr) {
@@ -2633,7 +2625,7 @@ static void sequencer_substitute_transform_effects(Scene *scene)
       GaussianBlurVars *gv = static_cast<GaussianBlurVars *>(strip->effectdata);
       gv->size_x = gv->size_y = 0.0f;
       seq::edit_strip_name_set(scene, strip, "Transform Placeholder (Migrated)");
-      seq::ensure_unique_name(strip, scene);
+      seq::ensure_unique_name(bmain, strip, scene);
     }
     return true;
   });
@@ -2769,7 +2761,7 @@ void do_versions_after_linking_500(FileData *fd, Main *bmain)
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 27)) {
     FOREACH_NODETREE_BEGIN (bmain, ntree, id) {
       if (ntree->type == NTREE_COMPOSIT) {
-        do_version_convert_to_generic_nodes_after_linking(bmain, ntree, id);
+        do_version_convert_to_generic_nodes_after_linking(bmain, ntree);
       }
     }
     FOREACH_NODETREE_END;
@@ -2884,7 +2876,7 @@ void do_versions_after_linking_500(FileData *fd, Main *bmain)
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 97)) {
     for (Scene &scene : bmain->scenes) {
       if (scene.ed != nullptr) {
-        sequencer_substitute_transform_effects(&scene);
+        sequencer_substitute_transform_effects(*bmain, &scene);
       }
     }
   }

source/blender/editors/space_sequencer/sequencer_clipboard.cc
@@ -505,7 +505,7 @@ wmOperatorStatus sequencer_clipboard_paste_exec(bContext *C, wmOperator *op)
     }
     /* Make sure, that pasted strips have unique names. This has to be done after
      * adding strips to seqbase, for lookup cache to work correctly. */
-    seq::ensure_unique_name(&istrip, scene_dst);
+    seq::ensure_unique_name(*bmain_dst, &istrip, scene_dst);
 
     if (region->regiontype == RGN_TYPE_PREVIEW && istrip.type != STRIP_TYPE_SOUND &&
         seq::must_render_strip(seq::query_all_strips(&nseqbase), &istrip))

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -2510,7 +2510,7 @@ static wmOperatorStatus sequencer_add_duplicate_exec(bContext *C, wmOperator *op
     strip->runtime->flag |= seq::StripRuntimeFlag::IgnoreChannelLock;
 
     seq::animation_duplicate_backup_to_scene(scene, strip, &animation_backup);
-    seq::ensure_unique_name(strip, scene);
+    seq::ensure_unique_name(*bmain, strip, scene);
   }
 
   /* Special case for duplicating strips in preview: handle overlap, because strips won't be
@@ -3627,7 +3627,7 @@ static wmOperatorStatus sequencer_change_effect_type_exec(bContext *C, wmOperato
   BLI_string_split_name_number(strip->name + 2, '.', name_base, &name_num);
   if (STREQ(name_base, seq::get_default_stripname_by_type(old_type))) {
     seq::edit_strip_name_set(scene, strip, seq::strip_give_name(strip));
-    seq::ensure_unique_name(strip, scene);
+    seq::ensure_unique_name(*CTX_data_main(C), strip, scene);
   }
 
   /* Init new effect. */

source/blender/sequencer/SEQ_utils.hh
@@ -62,7 +62,7 @@ void set_scale_to_fit(const Strip *strip,
  * \param strip: Strip which name will be ensured to be unique
  * \param scene: Scene in which name must be unique
  */
-void ensure_unique_name(Strip *strip, Scene *scene);
+void ensure_unique_name(Main &bmain, Strip *strip, Scene *scene);
 
 void fontmap_clear();
 

source/blender/sequencer/intern/strip_edit.cc
@@ -468,7 +468,7 @@ Strip *edit_strip_split(Main *bmain,
    * strips to seqbase, for lookup cache to work correctly. */
   Strip *strip_rename = right_strip;
   for (; strip_rename; strip_rename = strip_rename->next) {
-    ensure_unique_name(strip_rename, scene);
+    ensure_unique_name(*bmain, strip_rename, scene);
   }
 
   /* Split strips. */

source/blender/sequencer/intern/utils.cc
@@ -17,6 +17,8 @@
 #include "DNA_scene_types.h"
 #include "DNA_sequence_types.h"
 
+#include "RNA_path.hh"
+
 #include "BLI_listbase.hh"
 #include "BLI_path_utils.hh"
 #include "BLI_string.hh"
@@ -26,6 +28,7 @@
 #include "BLT_translation.hh"
 
 #include "BKE_animsys.hh"
+#include "BKE_global.hh"
 #include "BKE_image.hh"
 #include "BKE_library.hh"
 #include "BKE_main.hh"
@@ -496,26 +499,22 @@ void set_scale_to_fit(const Strip *strip,
   }
 }
 
-void ensure_unique_name(Strip *strip, Scene *scene)
+void ensure_unique_name(Main &bmain, Strip *strip, Scene *scene)
 {
   char name[STRIP_NAME_MAXSTR];
 
   STRNCPY_UTF8(name, strip->name + 2);
   strip_unique_name_set(scene, &scene->ed->seqbase, strip);
-  BKE_animdata_fix_paths_rename(&scene->id,
-                                scene->adt,
-                                nullptr,
-                                "sequence_editor.strips_all",
-                                name,
-                                strip->name + 2,
-                                0,
-                                0,
-                                /*verify_paths=*/false,
-                                /*infix_is_name=*/true);
+  BKE_animdata_fix_paths(scene->id,
+                         "sequence_editor.strips_all",
+                         RNA_path_name_to_infix(name),
+                         RNA_path_name_to_infix(strip->name + 2),
+                         /*verify_paths=*/false,
+                         bmain);
 
   if (strip->type == STRIP_TYPE_META) {
     for (Strip &strip_child : strip->seqbase) {
-      ensure_unique_name(&strip_child, scene);
+      ensure_unique_name(bmain, &strip_child, scene);
     }
   }
 }

source/blender/animrig/ANIM_bone_collections.hh
@@ -204,7 +204,10 @@ BoneCollection *ANIM_armature_bonecoll_get_by_name(bArmature *armature,
 int ANIM_armature_bonecoll_get_index_by_name(bArmature *armature,
                                              const char *name) ATTR_WARN_UNUSED_RESULT;
 
-void ANIM_armature_bonecoll_name_set(bArmature *armature, BoneCollection *bcoll, const char *name);
+void ANIM_armature_bonecoll_name_set(Main &bmain,
+                                     bArmature *armature,
+                                     BoneCollection *bcoll,
+                                     const char *name);
 
 /**
  * Show this bone collection.

source/blender/animrig/intern/bone_collections.cc
@@ -20,9 +20,12 @@
 #include "MEM_guardedalloc.h"
 
 #include "BKE_animsys.hh"
+#include "BKE_global.hh"
 #include "BKE_idprop.hh"
 #include "BKE_lib_id.hh"
 #include "BKE_lib_override.hh"
+
+#include "RNA_path.hh"
 
 #include "ANIM_armature_iter.hh"
 #include "ANIM_bone_collections.hh"
@@ -585,7 +588,10 @@ bool ANIM_armature_bonecoll_move(bArmature *armature, BoneCollection *bcoll, con
   return true;
 }
 
-void ANIM_armature_bonecoll_name_set(bArmature *armature, BoneCollection *bcoll, const char *name)
+void ANIM_armature_bonecoll_name_set(Main &bmain,
+                                     bArmature *armature,
+                                     BoneCollection *bcoll,
+                                     const char *name)
 {
   char old_name[sizeof(bcoll->name)];
 
@@ -604,8 +610,19 @@ void ANIM_armature_bonecoll_name_set(bArmature *armature, BoneCollection *bcoll,
 
   /* Bone collections can be reached via .collections (4.0+) and .collections_all (4.1+).
    * Animation data from 4.0 should have been versioned to only use `.collections_all`. */
-  BKE_animdata_fix_paths_rename_all(&armature->id, "collections", old_name, bcoll->name);
-  BKE_animdata_fix_paths_rename_all(&armature->id, "collections_all", old_name, bcoll->name);
+  const DriverMap driver_map = BKE_animdata_build_driver_target_map(bmain);
+  BKE_animdata_fix_paths(armature->id,
+                         "collections",
+                         RNA_path_name_to_infix(old_name),
+                         RNA_path_name_to_infix(bcoll->name),
+                         /*verify_paths=*/true,
+                         driver_map);
+  BKE_animdata_fix_paths(armature->id,
+                         "collections_all",
+                         RNA_path_name_to_infix(old_name),
+                         RNA_path_name_to_infix(bcoll->name),
+                         /*verify_paths=*/true,
+                         driver_map);
 }
 
 void ANIM_armature_bonecoll_remove_from_index(bArmature *armature, int index)

commit 898bfdf8432454c093aff79fbfac2cb373b46cbf

Fix: Duplicating VSE strip remaps drivers to duplicate
Caused by 4c1b8d11ace2c07a905a1ce63aa27c8db1ee9150

Before that commit the function `BKE_animdata_fix_paths_rename`
was used which did not modify drivers targeting the strip.
While updating the drivers is correct when doing a rename,
the `ensure_unique_name` function is also used in other cases, for
example duplicate, split etc.
In those cases we do not want to remap the drivers pointing
to the original strip to the duplicate.
The fix is to pass an empty `DriverMap` when we don't want
driver targets to be modified.

Reported by LLM

Pull Request: https://projects.blender.org/blender/blender/pulls/163649

source/blender/blenloader/intern/versioning_500.cc
@@ -2603,7 +2603,7 @@ static bool window_has_sequence_editor_open(const wmWindow *win)
 
 /* Merge transform effect properties with strip transform. Because this effect could use modifiers,
  * change its type to gaussian blur with 0 radius. */
-static void sequencer_substitute_transform_effects(Main &bmain, Scene *scene)
+static void sequencer_substitute_transform_effects(Scene *scene, const DriverMap &driver_map)
 {
   seq::foreach_strip(&scene->ed->seqbase, [&](Strip *strip) -> bool {
     if (strip->type == STRIP_TYPE_TRANSFORM_LEGACY && strip->effectdata != nullptr) {
@@ -2625,7 +2625,7 @@ static void sequencer_substitute_transform_effects(Main &bmain, Scene *scene)
       GaussianBlurVars *gv = static_cast<GaussianBlurVars *>(strip->effectdata);
       gv->size_x = gv->size_y = 0.0f;
       seq::edit_strip_name_set(scene, strip, "Transform Placeholder (Migrated)");
-      seq::ensure_unique_name(bmain, strip, scene);
+      seq::ensure_unique_name(strip, scene, driver_map);
     }
     return true;
   });
@@ -2873,9 +2873,10 @@ void do_versions_after_linking_500(FileData *fd, Main *bmain)
   }
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 97)) {
+    const DriverMap driver_map = BKE_animdata_build_driver_target_map(*bmain);
     for (Scene &scene : bmain->scenes) {
       if (scene.ed != nullptr) {
-        sequencer_substitute_transform_effects(*bmain, &scene);
+        sequencer_substitute_transform_effects(&scene, driver_map);
       }
     }
   }

source/blender/editors/space_sequencer/sequencer_clipboard.cc
@@ -29,6 +29,7 @@
 #include "BLI_path_utils.hh"
 
 #include "BKE_anim_data.hh"
+#include "BKE_animsys.hh"
 #include "BKE_appdir.hh"
 #include "BKE_blender_copybuffer.hh"
 #include "BKE_blendfile.hh"
@@ -505,7 +506,7 @@ wmOperatorStatus sequencer_clipboard_paste_exec(bContext *C, wmOperator *op)
     }
     /* Make sure, that pasted strips have unique names. This has to be done after
      * adding strips to seqbase, for lookup cache to work correctly. */
-    seq::ensure_unique_name(*bmain_dst, &istrip, scene_dst);
+    seq::ensure_unique_name(&istrip, scene_dst, {});
 
     if (region->regiontype == RGN_TYPE_PREVIEW && istrip.type != STRIP_TYPE_SOUND &&
         seq::must_render_strip(seq::query_all_strips(&nseqbase), &istrip))

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -29,6 +29,7 @@
 #include "DNA_sequence_types.h"
 #include "DNA_sound_types.h"
 
+#include "BKE_animsys.hh"
 #include "BKE_context.hh"
 #include "BKE_global.hh"
 #include "BKE_idtype.hh"
@@ -2503,7 +2504,7 @@ static wmOperatorStatus sequencer_add_duplicate_exec(bContext *C, wmOperator *op
     strip->runtime->flag |= seq::StripRuntimeFlag::IgnoreChannelLock;
 
     seq::animation_duplicate_backup_to_scene(scene, strip, &animation_backup);
-    seq::ensure_unique_name(*bmain, strip, scene);
+    seq::ensure_unique_name(strip, scene, {});
   }
 
   /* Special case for duplicating strips in preview: handle overlap, because strips won't be
@@ -3624,7 +3625,7 @@ static wmOperatorStatus sequencer_change_effect_type_exec(bContext *C, wmOperato
   BLI_string_split_name_number(strip->name + 2, '.', name_base, &name_num);
   if (STREQ(name_base, seq::get_default_stripname_by_type(old_type))) {
     seq::edit_strip_name_set(scene, strip, seq::strip_give_name(strip));
-    seq::ensure_unique_name(*CTX_data_main(C), strip, scene);
+    seq::ensure_unique_name(strip, scene, BKE_animdata_build_driver_target_map(*CTX_data_main(C)));
   }
 
   /* Init new effect. */

source/blender/sequencer/SEQ_utils.hh
@@ -11,6 +11,8 @@
 #include "DNA_listBase.h"
 #include "DNA_scene_types.h"
 
+#include "BKE_animsys.hh"
+
 namespace blender {
 
 struct bSound;
@@ -61,8 +63,11 @@ void set_scale_to_fit(const Strip *strip,
  *
  * \param strip: Strip which name will be ensured to be unique
  * \param scene: Scene in which name must be unique
+ * \param driver_map: Used to update driver paths to the new name. Pass an empty map when the strip
+ * is a copy and the original still carries the old name.
+ * See `BKE_animdata_build_driver_target_map`.
  */
-void ensure_unique_name(Main &bmain, Strip *strip, Scene *scene);
+void ensure_unique_name(Strip *strip, Scene *scene, const DriverMap &driver_map);
 
 void fontmap_clear();
 

source/blender/sequencer/intern/strip_edit.cc
@@ -18,6 +18,7 @@
 
 #include "BLT_translation.hh"
 
+#include "BKE_animsys.hh"
 #include "BKE_sound.hh"
 
 #include "strip_time.hh"
@@ -455,7 +456,7 @@ Strip *edit_strip_split(Main *bmain,
    * strips to seqbase, for lookup cache to work correctly. */
   Strip *strip_rename = right_strip;
   for (; strip_rename; strip_rename = strip_rename->next) {
-    ensure_unique_name(*bmain, strip_rename, scene);
+    ensure_unique_name(strip_rename, scene, {});
   }
 
   /* Split strips. */

source/blender/sequencer/intern/utils.cc
@@ -409,7 +409,7 @@ void set_scale_to_fit(const Strip *strip,
   }
 }
 
-void ensure_unique_name(Main &bmain, Strip *strip, Scene *scene)
+void ensure_unique_name(Strip *strip, Scene *scene, const DriverMap &driver_map)
 {
   char name[STRIP_NAME_MAXSTR];
 
@@ -420,11 +420,11 @@ void ensure_unique_name(Main &bmain, Strip *strip, Scene *scene)
                          RNA_path_name_to_infix(name),
                          RNA_path_name_to_infix(strip->name + 2),
                          /*verify_paths=*/false,
-                         bmain);
+                         driver_map);
 
   if (strip->type == STRIP_TYPE_META) {
     for (Strip &strip_child : strip->seqbase) {
-      ensure_unique_name(bmain, &strip_child, scene);
+      ensure_unique_name(&strip_child, scene, driver_map);
     }
   }
 }