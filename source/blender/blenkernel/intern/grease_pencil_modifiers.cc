eb72ea196ad03f52d8151cac44a36257c427fd44
Falk David <falk@blender.org>
2026-09-15 18:18:42 +0200

    Cleanup: Rename `BKE_gpencil_modifiers_legacy.h` to `BKE_grease_pencil_modifiers.h`
    These are the functions dealing with the "built-in" Grease Pencil
    modifiers. Since these are still used, I removed the "legacy" term.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/163969

--- source/blender/blenkernel/intern/grease_pencil_modifiers.cc
+++ source/blender/blenkernel/intern/grease_pencil_modifiers.cc
@@ -19,7 +19,7 @@
 
 #include "BKE_colortools.hh"
 #include "BKE_deform.hh"
-#include "BKE_gpencil_modifier_legacy.h"
+#include "BKE_grease_pencil_modifiers.h"
 #include "BKE_lattice.hh"
 #include "BKE_lib_id.hh"
 #include "BKE_lib_query.hh"
