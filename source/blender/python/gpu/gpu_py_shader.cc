blender/blender

commit fae3d71db804a569b5ee9bfab7650bd51f57172b

PyGPU: Add support for SSBO
Expose SSBO through the Python API.

Come with `clear_to_zero` and `read`.

`GPU_BARRIER_SHADER_STORAGE` is also added to `pygpu_compute_dispatch`
However, this doesn't cover storage writes from a fragment or vertex
stage.

So we need to introduce a standalone method from the pyGPU module itself
(not linked to an object type). This will done in a follow up PR.

Ref #145179.

Pull Request: https://projects.blender.org/blender/blender/pulls/161447

source/blender/python/gpu/gpu_py_shader.cc
@@ -15,6 +15,7 @@
 
 #include "GPU_context.hh"
 #include "GPU_shader.hh"
+#include "GPU_storage_buffer.hh"
 #include "GPU_texture.hh"
 #include "GPU_uniform_buffer.hh"
 
@@ -25,6 +26,7 @@
 #include "../mathutils/mathutils.hh"
 
 #include "gpu_py.hh"
+#include "gpu_py_storagebuffer.hh"
 #include "gpu_py_texture.hh"
 #include "gpu_py_uniformbuffer.hh"
 #include "gpu_py_vertex_format.hh"
@@ -659,6 +661,41 @@ static PyObject *pygpu_shader_uniform_block(BPyGPUShader *self, PyObject *args)
 
 PyDoc_STRVAR(
     /* Wrap. */
+    pygpu_shader_storage_block_doc,
+    ".. method:: storage_block(name, ssbo)\n"
+    "\n"
+    "   Specify the value of a storage buffer object variable for the current GPUShader.\n"
+    "\n"
+    "   :param name: Name of the storage block variable whose SSBO is to be specified.\n"
+    "   :type name: str\n"
+    "   :param ssbo: Storage Buffer to attach.\n"
+    "   :type ssbo: :class:`gpu.types.GPUStorageBuf`\n");
+static PyObject *pygpu_shader_storage_block(BPyGPUShader *self, PyObject *args)
+{
+  const char *name;
+  BPyGPUStorageBuf *py_ssbo;
+  if (!PyArg_ParseTuple(
+          args, "sO!:GPUShader.storage_block", &name, &BPyGPUStorageBuf_Type, &py_ssbo))
+  {
+    return nullptr;
+  }
+
+  int binding = GPU_shader_get_ssbo_binding(self->shader, name);
+  if (binding == -1) {
+    PyErr_SetString(
+        PyExc_BufferError,
+        "GPUShader.storage_block: storage block not found, make sure the name is correct");
+    return nullptr;
+  }
+
+  GPU_shader_bind(self->shader);
+  GPU_storagebuf_bind(py_ssbo->ssbo, binding);
+
+  Py_RETURN_NONE;
+}
+
+PyDoc_STRVAR(
+    /* Wrap. */
     pygpu_shader_attr_from_name_doc,
     ".. method:: attr_from_name(name)\n"
     "\n"
@@ -845,6 +882,10 @@ static PyMethodDef pygpu_shader__tp_methods[] = {
      reinterpret_cast<PyCFunction>(pygpu_shader_uniform_block),
      METH_VARARGS,
      pygpu_shader_uniform_block_doc},
+    {"storage_block",
+     reinterpret_cast<PyCFunction>(pygpu_shader_storage_block),
+     METH_VARARGS,
+     pygpu_shader_storage_block_doc},
     {"attr_from_name",
      reinterpret_cast<PyCFunction>(pygpu_shader_attr_from_name),
      METH_O,

tests/python/bl_pyapi_gpu.py
@@ -38,6 +38,95 @@ class TestGpuFrameBuffer(unittest.TestCase):
                 fb.read_color(-1, 0, 1, 1, 4, 0, "UBYTE")
 
 
+class TestGpuStorageBuf(unittest.TestCase):
+    def test_create_update_read(self):
+        import struct
+
+        gpu.init()
+
+        data = struct.pack("4f", 1.0, 2.0, 3.0, 4.0)
+        ssbo = gpu.types.GPUStorageBuf(data)
+
+        self.assertEqual(struct.unpack("4f", bytes(ssbo.read())), (1.0, 2.0, 3.0, 4.0))
+
+        new_data = struct.pack("4f", 10.0, 20.0, 30.0, 40.0)
+        ssbo.update(new_data)
+        self.assertEqual(struct.unpack("4f", bytes(ssbo.read())), (10.0, 20.0, 30.0, 40.0))
+
+        ssbo.clear_to_zero()
+        self.assertEqual(struct.unpack("4f", bytes(ssbo.read())), (0.0, 0.0, 0.0, 0.0))
+
+    def test_compute_shader_binding(self):
+        import struct
+
+        gpu.init()
+
+        info = gpu.types.GPUShaderCreateInfo()
+        info.storage_buf(0, {"READ", "WRITE"}, "float", "data[]")
+        info.local_group_size(4)
+        info.compute_source(
+            """
+            void main() {
+              uint i = gl_GlobalInvocationID.x;
+              data[i] = data[i] * 2.0;
+            }
+            """
+        )
+
+        shader = gpu.shader.create_from_info(info)
+        ssbo = gpu.types.GPUStorageBuf(struct.pack("4f", 1.0, 2.0, 3.0, 4.0))
+
+        shader.bind()
+        shader.storage_block("data", ssbo)
+        gpu.compute.dispatch(shader, 1, 1, 1)
+
+        self.assertEqual(struct.unpack("4f", bytes(ssbo.read())), (2.0, 4.0, 6.0, 8.0))
+
+    def test_typedef_source_struct_array(self):
+        import struct
+
+        gpu.init()
+
+        # std430 struct layout: `vec4` forces 16-byte alignment, so the struct's own
+        # alignment (and therefore its array stride) is rounded up to a multiple of 16.
+        # `vec4 pos` (16 bytes) + `float weight` (4 bytes) = 20 bytes, padded to 32.
+        item_fmt = "<4ff12x"
+        self.assertEqual(struct.calcsize(item_fmt), 32)
+
+        items = [
+            (1.0, 2.0, 3.0, 4.0, 10.0),
+            (5.0, 6.0, 7.0, 8.0, 20.0),
+        ]
+        data = b"".join(struct.pack(item_fmt, *pos, weight) for *pos, weight in items)
+
+        info = gpu.types.GPUShaderCreateInfo()
+        info.typedef_source("struct Item { vec4 pos; float weight; };")
+        info.storage_buf(0, {"READ", "WRITE"}, "Item", "items[]")
+        info.local_group_size(len(items))
+        info.compute_source(
+            """
+            void main() {
+              uint i = gl_GlobalInvocationID.x;
+              items[i].pos = items[i].pos * 2.0;
+              items[i].weight = items[i].weight + 1.0;
+            }
+            """
+        )
+
+        shader = gpu.shader.create_from_info(info)
+        ssbo = gpu.types.GPUStorageBuf(data)
+
+        shader.bind()
+        shader.storage_block("items", ssbo)
+        gpu.compute.dispatch(shader, 1, 1, 1)
+
+        result = bytes(ssbo.read())
+        for i, (x, y, z, w, weight) in enumerate(items):
+            item = struct.unpack_from(item_fmt, result, i * 32)
+            self.assertEqual(item[:4], (x * 2.0, y * 2.0, z * 2.0, w * 2.0))
+            self.assertEqual(item[4], weight + 1.0)
+
+
 if __name__ == "__main__":
     import sys
 

source/blender/python/gpu/CMakeLists.txt
@@ -27,6 +27,7 @@ set(SRC
   gpu_py_shader.cc
   gpu_py_shader_create_info.cc
   gpu_py_state.cc
+  gpu_py_storagebuffer.cc
   gpu_py_texture.cc
   gpu_py_types.cc
   gpu_py_uniformbuffer.cc
@@ -47,6 +48,7 @@ set(SRC
   gpu_py_select.hh
   gpu_py_shader.hh
   gpu_py_state.hh
+  gpu_py_storagebuffer.hh
   gpu_py_texture.hh
   gpu_py_types.hh
   gpu_py_uniformbuffer.hh

source/blender/python/gpu/gpu_py_compute.cc
@@ -96,7 +96,8 @@ static PyObject *pygpu_compute_dispatch(PyObject * /*self*/, PyObject *args, PyO
 
     gpu::Shader *shader = py_shader->shader;
     GPU_compute_dispatch(shader, groups_x_len, groups_y_len, groups_z_len);
-    GPU_memory_barrier(GPU_BARRIER_TEXTURE_FETCH | GPU_BARRIER_SHADER_IMAGE_ACCESS);
+    GPU_memory_barrier(GPU_BARRIER_TEXTURE_FETCH | GPU_BARRIER_SHADER_IMAGE_ACCESS |
+                       GPU_BARRIER_SHADER_STORAGE);
   }
   Py_RETURN_NONE;
 }

source/blender/python/gpu/gpu_py_shader_create_info.cc
@@ -765,6 +765,56 @@ static PyObject *pygpu_shader_info_uniform_buf(BPyGPUShaderCreateInfo *self, PyO
   Py_RETURN_NONE;
 }
 
+PyDoc_STRVAR(
+    /* Wrap. */
+    pygpu_shader_info_storage_buf_doc,
+    ".. method:: storage_buf(slot, qualifiers, type_name, name)\n"
+    "\n"
+    "   Specify a storage buffer variable whose type can be one of those declared in "
+    ":meth:`gpu.types.GPUShaderCreateInfo.typedef_source`.\n"
+    "\n"
+    "   :param slot: The storage buffer variable index.\n"
+    "   :type slot: int\n"
+    "   :param qualifiers: Set containing values that describe how the storage buffer is to be "
+    "read or written.\n"
+    "   :type qualifiers: set[" PYDOC_QUALIFIERS_LITERAL
+    "]\n"
+    "   :param type_name: Name of the data type. "
+    "It can be a struct type defined in the source passed through the "
+    ":meth:`gpu.types.GPUShaderCreateInfo.typedef_source`.\n"
+    "   :type type_name: str\n"
+    "   :param name: The storage buffer variable name.\n"
+    "   :type name: str\n");
+static PyObject *pygpu_shader_info_storage_buf(BPyGPUShaderCreateInfo *self, PyObject *args)
+{
+  int slot;
+  PyObject *py_qualifiers;
+  const char *type_name;
+  const char *name;
+  Qualifier qualifier = Qualifier::no_restrict;
+
+  if (!PyArg_ParseTuple(args, "iOss:storage_buf", &slot, &py_qualifiers, &type_name, &name)) {
+    return nullptr;
+  }
+
+  if (PyC_FlagSet_ToBitfield(
+          pygpu_qualifiers, py_qualifiers, reinterpret_cast<int *>(&qualifier), "storage_buf") ==
+      -1)
+  {
+    return nullptr;
+  }
+
+#ifdef USE_GPU_PY_REFERENCES
+  PyList_Append(self->references, PyTuple_GET_ITEM(args, 2)); /* type_name */
+  PyList_Append(self->references, PyTuple_GET_ITEM(args, 3)); /* name */
+#endif
+
+  ShaderCreateInfo *info = reinterpret_cast<ShaderCreateInfo *>(self->info);
+  info->storage_buf(slot, qualifier, type_name, name);
+
+  Py_RETURN_NONE;
+}
+
 #ifdef USE_PYGPU_SHADER_INFO_IMAGE_METHOD
 PyDoc_STRVAR(
     /* Wrap. */
@@ -1332,6 +1382,10 @@ static PyMethodDef pygpu_shader_info__tp_methods[] = {
      (PyCFunction)(void *)(pygpu_shader_info_uniform_buf),
      METH_VARARGS,
      pygpu_shader_info_uniform_buf_doc},
+    {"storage_buf",
+     (PyCFunction)(void *)(pygpu_shader_info_storage_buf),
+     METH_VARARGS,
+     pygpu_shader_info_storage_buf_doc},
 #ifdef USE_PYGPU_SHADER_INFO_IMAGE_METHOD
     {"image",
      (PyCFunction)(void *)(pygpu_shader_info_image),

source/blender/python/gpu/gpu_py_storagebuffer.cc
@@ -1,0 +1,316 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup bpygpu
+ *
+ * This file defines the storage buffer functionalities of the 'gpu' module
+ *
+ * - Use `bpygpu_` for local API.
+ * - Use `BPyGPU` for public API.
+ */
+
+#include <Python.h>
+
+#include "BLI_string_utf8.hh"
+
+#include "MEM_guardedalloc.h"
+
+#include "GPU_context.hh"
+#include "GPU_storage_buffer.hh"
+#include "GPU_texture.hh"
+
+#include "../generic/python_compat.hh" /* IWYU pragma: keep. */
+
+#include "gpu_py.hh"
+#include "gpu_py_buffer.hh"
+#include "gpu_py_storagebuffer.hh" /* own include */
+
+namespace blender {
+
+/* -------------------------------------------------------------------- */
+/** \name gpu::StorageBuf Common Utilities
+ * \{ */
+
+static int pygpu_storagebuffer_valid_check(BPyGPUStorageBuf *bpygpu_ssbo)
+{
+  if (bpygpu_ssbo->ssbo == nullptr) [[unlikely]] {
+    PyErr_SetString(PyExc_ReferenceError,
+#ifdef BPYGPU_USE_GPUOBJ_FREE_METHOD
+                    "GPU storage buffer was freed, no further access is valid");
+#else
+
+                    "GPU storage buffer: internal error");
+#endif
+    return -1;
+  }
+  return 0;
+}
+
+#define BPYGPU_STORAGEBUF_CHECK_OBJ(bpygpu) \
+  { \
+    if (pygpu_storagebuffer_valid_check(bpygpu) == -1) [[unlikely]] { \
+      return nullptr; \
+    } \
+  } \
+  ((void)0)
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name gpu::StorageBuf Type
+ * \{ */
+
+static PyObject *pygpu_storagebuffer__tp_new(PyTypeObject * /*self*/,
+                                             PyObject *args,
+                                             PyObject *kwds)
+{
+  BPYGPU_IS_INIT_OR_ERROR_OBJ;
+
+  gpu::StorageBuf *ssbo = nullptr;
+  PyObject *pybuffer_obj;
+  Py_ssize_t size = 0;
+  char err_out[256] = "unknown error. See console";
+
+  static const char *_keywords[] = {"data", nullptr};
+  static _PyArg_Parser _parser = {
+      "O" /* `data` */
+      ":GPUStorageBuf.__new__",
+      _keywords,
+      nullptr,
+  };
+  if (!_PyArg_ParseTupleAndKeywordsFast(args, kwds, &_parser, &pybuffer_obj)) {
+    return nullptr;
+  }
+
+  if (!GPU_context_active_get()) {
+    STRNCPY_UTF8(err_out, "No active GPU context found");
+  }
+  else {
+    Py_buffer pybuffer;
+    if (PyObject_GetBuffer(pybuffer_obj, &pybuffer, PyBUF_SIMPLE) == -1) {
+      /* PyObject_GetBuffer raise a PyExc_BufferError */
+      return nullptr;
+    }
+
+    size = pybuffer.len;
+    ssbo = GPU_storagebuf_create_ex(
+        pybuffer.len, pybuffer.buf, GPU_USAGE_DYNAMIC, "python_storagebuffer");
+    PyBuffer_Release(&pybuffer);
+  }
+
+  if (ssbo == nullptr) {
+    PyErr_Format(PyExc_RuntimeError, "GPUStorageBuf.__new__(...) failed with '%s'", err_out);
+    return nullptr;
+  }
+
+  return BPyGPUStorageBuf_CreatePyObject(ssbo, size_t(size));
+}
+
+PyDoc_STRVAR(
+    /* Wrap. */
+    pygpu_storagebuffer_update_doc,
+    ".. method:: update(data)\n"
+    "\n"
+    "   Update the data of the storage buffer object.\n"
+    "\n"
+    "   :param data: Data to fill the buffer.\n"
+    "   :type data: Buffer\n");
+static PyObject *pygpu_storagebuffer_update(BPyGPUStorageBuf *self, PyObject *obj)
+{
+  BPYGPU_STORAGEBUF_CHECK_OBJ(self);
+
+  Py_buffer pybuffer;
+  if (PyObject_GetBuffer(obj, &pybuffer, PyBUF_SIMPLE) == -1) {
+    /* PyObject_GetBuffer raise a PyExc_BufferError */
+    return nullptr;
+  }
+
+  /* The backends copy exactly the size the buffer was created with, regardless of how much
+   * data is actually passed in. Providing less would read past the end of `pybuffer`. */
+  if (size_t(pybuffer.len) != self->size) {
+    PyErr_Format(PyExc_ValueError,
+                 "GPUStorageBuf.update(): expected a buffer of size %zu, got %zu",
+                 self->size,
+                 size_t(pybuffer.len));
+    PyBuffer_Release(&pybuffer);
+    return nullptr;
+  }
+
+  GPU_storagebuf_update(self->ssbo, pybuffer.buf);
+  PyBuffer_Release(&pybuffer);
+  Py_RETURN_NONE;
+}
+
+PyDoc_STRVAR(
+    /* Wrap. */
+    pygpu_storagebuffer_clear_to_zero_doc,
+    ".. method:: clear_to_zero()\n"
+    "\n"
+    "   Clear the storage buffer data to zero.\n");
+static PyObject *pygpu_storagebuffer_clear_to_zero(BPyGPUStorageBuf *self)
+{
+  BPYGPU_STORAGEBUF_CHECK_OBJ(self);
+
+  GPU_storagebuf_clear_to_zero(self->ssbo);
+  Py_RETURN_NONE;
+}
+
+PyDoc_STRVAR(
+    /* Wrap. */
+    pygpu_storagebuffer_read_doc,
+    ".. method:: read()\n"
+    "\n"
+    "   Read back the contents of the storage buffer.\n"
+    "   This waits until all GPU operations are finished, performing the necessary "
+    "synchronization.\n"
+    "\n"
+    "   :return: The Buffer with the read data.\n"
+    "   :rtype: :class:`gpu.types.Buffer`\n");
+static PyObject *pygpu_storagebuffer_read(BPyGPUStorageBuf *self)
+{
+  BPYGPU_STORAGEBUF_CHECK_OBJ(self);
+
+  void *buf = MEM_new_uninitialized(self->size, "python_storagebuffer_read");
+  GPU_storagebuf_read(self->ssbo, buf);
+
+  const Py_ssize_t shape = Py_ssize_t(self->size);
+  return reinterpret_cast<PyObject *>(
+      BPyGPU_Buffer_CreatePyObject(GPU_DATA_UBYTE, &shape, 1, buf));
+}
+
+#ifdef BPYGPU_USE_GPUOBJ_FREE_METHOD
+PyDoc_STRVAR(
+    /* Wrap. */
+    pygpu_storagebuffer_free_doc,
+    ".. method:: free()\n"
+    "\n"
+    "   Free the storage buffer object.\n"
+    "   The storage buffer object will no longer be accessible.\n");
+static PyObject *pygpu_storagebuffer_free(BPyGPUStorageBuf *self)
+{
+  BPYGPU_STORAGEBUF_CHECK_OBJ(self);
+
+  GPU_storagebuf_free(self->ssbo);
+  self->ssbo = nullptr;
+  Py_RETURN_NONE;
+}
+#endif
+
+static void BPyGPUStorageBuf__tp_dealloc(BPyGPUStorageBuf *self)
+{
+  if (self->ssbo) {
+    GPU_storagebuf_free(self->ssbo);
+  }
+  Py_TYPE(self)->tp_free(reinterpret_cast<PyObject *>(self));
+}
+
+static PyGetSetDef pygpu_storagebuffer__tp_getseters[] = {
+    {nullptr, nullptr, nullptr, nullptr, nullptr} /* Sentinel */
+};
+
+static PyMethodDef pygpu_storagebuffer__tp_methods[] = {
+    {"update",
+     reinterpret_cast<PyCFunction>(pygpu_storagebuffer_update),
+     METH_O,
+     pygpu_storagebuffer_update_doc},
+    {"clear_to_zero",
+     reinterpret_cast<PyCFunction>(pygpu_storagebuffer_clear_to_zero),
+     METH_NOARGS,
+     pygpu_storagebuffer_clear_to_zero_doc},
+    {"read",
+     reinterpret_cast<PyCFunction>(pygpu_storagebuffer_read),
+     METH_NOARGS,
+     pygpu_storagebuffer_read_doc},
+#ifdef BPYGPU_USE_GPUOBJ_FREE_METHOD
+    {"free", (PyCFunction)pygpu_storagebuffer_free, METH_NOARGS, pygpu_storagebuffer_free_doc},
+#endif
+    {nullptr, nullptr, 0, nullptr},
+};
+
+PyDoc_STRVAR(
+    /* Wrap. */
+    pygpu_storagebuffer__tp_doc,
+    ".. class:: GPUStorageBuf\n"
+    "\n"
+    "   This object gives access to storage buffers.\n"
+    "\n"
+    "   .. method:: __init__(data)\n"
+    "\n"
+    "      :param data: Data to fill the buffer.\n"
+    "      :type data: Buffer\n");
+PyTypeObject BPyGPUStorageBuf_Type = {
+    /*ob_base*/ PyVarObject_HEAD_INIT(nullptr, 0)
+    /*tp_name*/ "GPUStorageBuf",
+    /*tp_basicsize*/ sizeof(BPyGPUStorageBuf),
+    /*tp_itemsize*/ 0,
+    /*tp_dealloc*/ reinterpret_cast<destructor>(BPyGPUStorageBuf__tp_dealloc),
+    /*tp_vectorcall_offset*/ 0,
+    /*tp_getattr*/ nullptr,
+    /*tp_setattr*/ nullptr,
+    /*tp_as_async*/ nullptr,
+    /*tp_repr*/ nullptr,
+    /*tp_as_number*/ nullptr,
+    /*tp_as_sequence*/ nullptr,
+    /*tp_as_mapping*/ nullptr,
+    /*tp_hash*/ nullptr,
+    /*tp_call*/ nullptr,
+    /*tp_str*/ nullptr,
+    /*tp_getattro*/ nullptr,
+    /*tp_setattro*/ nullptr,
+    /*tp_as_buffer*/ nullptr,
+    /*tp_flags*/ Py_TPFLAGS_DEFAULT,
+    /*tp_doc*/ pygpu_storagebuffer__tp_doc,
+    /*tp_traverse*/ nullptr,
+    /*tp_clear*/ nullptr,
+    /*tp_richcompare*/ nullptr,
+    /*tp_weaklistoffset*/ 0,
+    /*tp_iter*/ nullptr,
+    /*tp_iternext*/ nullptr,
+    /*tp_methods*/ pygpu_storagebuffer__tp_methods,
+    /*tp_members*/ nullptr,
+    /*tp_getset*/ pygpu_storagebuffer__tp_getseters,
+    /*tp_base*/ nullptr,
+    /*tp_dict*/ nullptr,
+    /*tp_descr_get*/ nullptr,
+    /*tp_descr_set*/ nullptr,
+    /*tp_dictoffset*/ 0,
+    /*tp_init*/ nullptr,
+    /*tp_alloc*/ nullptr,
+    /*tp_new*/ pygpu_storagebuffer__tp_new,
+    /*tp_free*/ nullptr,
+    /*tp_is_gc*/ nullptr,
+    /*tp_bases*/ nullptr,
+    /*tp_mro*/ nullptr,
+    /*tp_cache*/ nullptr,
+    /*tp_subclasses*/ nullptr,
+    /*tp_weaklist*/ nullptr,
+    /*tp_del*/ nullptr,
+    /*tp_version_tag*/ 0,
+    /*tp_finalize*/ nullptr,
+    /*tp_vectorcall*/ nullptr,
+};
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Public API
+ * \{ */
+
+PyObject *BPyGPUStorageBuf_CreatePyObject(gpu::StorageBuf *ssbo, size_t size)
+{
+  BPyGPUStorageBuf *self;
+
+  self = PyObject_New(BPyGPUStorageBuf, &BPyGPUStorageBuf_Type);
+  self->ssbo = ssbo;
+  self->size = size;
+
+  return reinterpret_cast<PyObject *>(self);
+}
+
+/** \} */
+
+#undef BPYGPU_STORAGEBUF_CHECK_OBJ
+
+}  // namespace blender

source/blender/python/gpu/gpu_py_storagebuffer.hh
@@ -1,0 +1,35 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup bpygpu
+ */
+
+#pragma once
+
+#include <Python.h>
+
+#include "BLI_compiler_attrs.hh"
+
+namespace blender {
+
+namespace gpu {
+class StorageBuf;
+}  // namespace gpu
+
+extern PyTypeObject BPyGPUStorageBuf_Type;
+
+#define BPyGPUStorageBuf_Check(v) (Py_TYPE(v) == &BPyGPUStorageBuf_Type)
+
+struct BPyGPUStorageBuf {
+  PyObject_HEAD
+  gpu::StorageBuf *ssbo;
+  /** Size in bytes, needed since #gpu::StorageBuf doesn't expose it publicly. */
+  size_t size;
+};
+
+[[nodiscard]] PyObject *BPyGPUStorageBuf_CreatePyObject(gpu::StorageBuf *ssbo, size_t size)
+    ATTR_NONNULL(1);
+
+}  // namespace blender

source/blender/python/gpu/gpu_py_types.cc
@@ -65,6 +65,9 @@ PyObject *bpygpu_types_init()
   if (PyType_Ready(&BPyGPUUniformBuf_Type) < 0) {
     return nullptr;
   }
+  if (PyType_Ready(&BPyGPUStorageBuf_Type) < 0) {
+    return nullptr;
+  }
   if (PyType_Ready(&BPyGPUShaderCreateInfo_Type) < 0) {
     return nullptr;
   }
@@ -93,6 +96,7 @@ PyObject *bpygpu_types_init()
   PyModule_AddType(submodule, &BPyGPUTexture_Type);
   PyModule_AddType(submodule, &BPyGPUFrameBuffer_Type);
   PyModule_AddType(submodule, &BPyGPUUniformBuf_Type);
+  PyModule_AddType(submodule, &BPyGPUStorageBuf_Type);
   PyModule_AddType(submodule, &BPyGPUShaderCreateInfo_Type);
   PyModule_AddType(submodule, &BPyGPUStageInterfaceInfo_Type);
   PyModule_AddType(submodule, &PyGPUMatrixStackContext_Type);

commit 16fb78866719835adcaf7b041b754901e6f35024

Fix: GPU: Python: Missing shader unbind in pygpu tests
Caused by fae3d71db804a569b5ee9bfab7650bd51f57172b.

Also clarify that the pygpu_shader_unbind isn't
a member method of the shader class.

source/blender/python/gpu/gpu_py_shader.cc
@@ -1020,7 +1020,7 @@ PyDoc_STRVAR(
     ".. function:: unbind()\n"
     "\n"
     "   Unbind the bound shader object.\n");
-static PyObject *pygpu_shader_unbind(BPyGPUShader * /*self*/)
+static PyObject *pygpu_shader_unbind(PyObject * /*self*/)
 {
   GPU_shader_unbind();
   Py_RETURN_NONE;

tests/python/bl_pyapi_gpu.py
@@ -79,6 +79,7 @@ class TestGpuStorageBuf(unittest.TestCase):
         shader.bind()
         shader.storage_block("data", ssbo)
         gpu.compute.dispatch(shader, 1, 1, 1)
+        gpu.shader.unbind()
 
         self.assertEqual(struct.unpack("4f", bytes(ssbo.read())), (2.0, 4.0, 6.0, 8.0))
 
@@ -119,6 +120,7 @@ class TestGpuStorageBuf(unittest.TestCase):
         shader.bind()
         shader.storage_block("items", ssbo)
         gpu.compute.dispatch(shader, 1, 1, 1)
+        gpu.shader.unbind()
 
         result = bytes(ssbo.read())
         for i, (x, y, z, w, weight) in enumerate(items):