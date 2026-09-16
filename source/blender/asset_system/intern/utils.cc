blender/blender

commit 89f2284b813a3a7f5daf70fd26e1bb9e14f44f0a

Core: Project Asset Libraries
This adds project asset libraries to Blender projects.
These work in the same way as regular asset libraries but are tied to
the loaded project.

Remote asset libraries were left out as we need to decide if we want
them and how they would work if so. Therefore, only local asset
libraries are supported on the project level at this point.

This also introduces UUIDs for asset libraries. Currently only used
for project asset libraries.

Pull Request: https://projects.blender.org/blender/blender/pulls/160226

source/blender/asset_system/intern/utils.cc
@@ -13,43 +13,72 @@
 
 #include "MEM_guardedalloc.h"
 
+#include "BKE_global.hh"
+#include "BKE_path_templates.hh"
+
 #include "utils.hh"
 
 namespace blender::asset_system::utils {
 
-std::string normalize_directory_path(StringRef directory)
+namespace path_templates = bke::path_templates;
+
+std::string resolve_directory_path(StringRef directory)
 {
   if (directory.is_empty()) {
     return "";
   }
-
-  char dir_normalized[PATH_MAX];
-  BLI_strncpy(dir_normalized,
+  char dir_resolved[PATH_MAX];
+  BLI_strncpy(dir_resolved,
               directory.data(),
               /* + 1 for null terminator. */
-              std::min(directory.size() + 1, int64_t(sizeof(dir_normalized))));
-  BLI_path_slash_native(dir_normalized);
-  BLI_path_normalize_dir(dir_normalized, sizeof(dir_normalized));
-  return std::string(dir_normalized);
+              std::min(directory.size() + 1, int64_t(sizeof(dir_resolved))));
+
+  path_templates::VariableMap template_variables;
+  bke::BlenderProject *project = BKE_blender_project_get(G_MAIN);
+  BKE_add_template_variables_general(template_variables, nullptr, project);
+  const Vector<path_templates::Error> variable_errors = BKE_path_apply_template(
+      dir_resolved, FILE_MAX, template_variables);
+  if (!variable_errors.is_empty()) {
+    /* Do nothing on errors as the "dir_normalized" variable should still contain the original
+     * string. The reset of the code should still gracefully treat this as an invalid path.
+     * However it might be nice to bubble up the exact errors in the future.
+     */
+  }
+  BLI_path_slash_native(dir_resolved);
+  BLI_path_normalize_dir(dir_resolved, sizeof(dir_resolved));
+  return std::string(dir_resolved);
 }
 
-std::string normalize_path(StringRefNull path, int64_t max_len)
+std::string resolve_path(StringRefNull path, int64_t max_len)
 {
   const int64_t len = (max_len == StringRef::not_found) ? path.size() :
                                                           std::min(max_len, path.size());
 
   char *buf = BLI_strdupn(path.c_str(), len);
+
+  path_templates::VariableMap template_variables;
+  bke::BlenderProject *project = BKE_blender_project_get(G_MAIN);
+  BKE_add_template_variables_general(template_variables, nullptr, project);
+  const Vector<path_templates::Error> variable_errors = BKE_path_apply_template(
+      buf, FILE_MAX, template_variables);
+  if (!variable_errors.is_empty()) {
+    /* Do nothing on errors as the "dir_normalized" variable should still contain the original
+     * string. The reset of the code should still gracefully treat this as an invalid path.
+     * However it might be nice to bubble up the exact errors in the future.
+     */
+  }
+
   BLI_path_slash_native(buf);
   BLI_path_normalize(buf);
 
-  std::string normalized_path = buf;
+  std::string resolved_path = buf;
   MEM_delete(buf);
 
   if (len != path.size()) {
-    normalized_path = normalized_path + path.substr(len);
+    resolved_path = resolved_path + path.substr(len);
   }
 
-  return normalized_path;
+  return resolved_path;
 }
 
 }  // namespace blender::asset_system::utils

scripts/startup/bl_operators/project.py
@@ -51,6 +51,7 @@ PROJECT_DIR = ".blender_project"
 PROJECT_CONFIG = "project.toml"
 
 PROJECT_DEFAULT_NAME = "Untitled Project"
+ASSET_LIBRARY_DEFAULT_NAME = "Untitled Asset Library"
 
 
 # -------------------------------------------------------------
@@ -67,6 +68,15 @@ class VariableType(Enum):
 class VariableSubtype(Enum):
     NONE = 'NONE'
     FILEPATH = 'FILEPATH'
+
+
+@dataclass
+class AssetLibraryDefinition:
+    name: str
+    path: str
+    use_relative_path: bool
+    import_method: str
+    uuid: str | None = None
 
 
 @dataclass
@@ -145,6 +155,7 @@ class ProjectConfig:
     schema_version: int
     name: str
     variables: list[ProjectVariable] | None = None
+    asset_libraries: list[AssetLibraryDefinition] | None = None
 
     @staticmethod
     def new_from_real(project):
@@ -153,10 +164,28 @@ class ProjectConfig:
         if len(project.variables) > 0:
             variables = [ProjectVariable.new_from_real(var) for var in project.variables]
 
+        asset_list = None
+        if len(project.asset_libraries) > 0:
+            asset_list = []
+            for asset_lib in project.asset_libraries:
+                uuid_str = asset_lib.uuid
+                if not uuid_str:
+                    # No uuid string present, the uuid must be invalid
+                    uuid_str = asset_lib.invalid_uuid
+
+                asset_data = AssetLibraryDefinition(
+                    asset_lib.name,
+                    asset_lib.path,
+                    asset_lib.use_relative_path,
+                    asset_lib.import_method,
+                    uuid_str)
+                asset_list.append(asset_data)
+
         return ProjectConfig(
             schema_version=PROJECT_SCHEMA_VERSION,
             name=project.name,
             variables=variables,
+            asset_libraries=asset_list,
         )
 
     def populate_real(self, project):
@@ -164,6 +193,23 @@ class ProjectConfig:
         if self.variables is not None:
             for config_var in self.variables:
                 config_var.add_as_real(bpy.data.project.variables)
+
+        if self.asset_libraries is None:
+            return
+
+        # Populate the project asset libraries (if any)
+        for asset_lib in self.asset_libraries:
+            if asset_lib.uuid:
+                # Use the pre-generated UUID
+                lib = project.asset_libraries.new(name=asset_lib.name, directory=asset_lib.path, uuid=asset_lib.uuid)
+            else:
+                # Generate a new UUID for the asset library
+                lib = project.asset_libraries.new(name=asset_lib.name, directory=asset_lib.path)
+
+            if asset_lib.import_method:
+                lib.import_method = asset_lib.import_method
+            if asset_lib.use_relative_path:
+                lib.use_relative_path = asset_lib.use_relative_path
 
     def __post_init__(self):
         """Validation of invariants that cattrs doesn't check."""
@@ -297,6 +343,7 @@ def save_project(project, report=None):
 
     # Write the config TOML file.
     config_path = root_path.joinpath(PROJECT_DIR, PROJECT_CONFIG)
+
     try:
         with config_path.open(mode='wt') as f:
             f.write(config_toml)
@@ -685,6 +732,89 @@ class PROJECT_OT_MoveVariable(Operator):
         return {'FINISHED'}
 
 
+class PROJECT_OT_AssetLibraryAdd(Operator):
+    """
+    Register a directory to be used by the Asset Browser and other places showing assets,
+    as source of assets within the current project.
+    """
+    bl_idname = "project.asset_library_add"
+    bl_label = "Add Asset Library"
+    bl_options = {'INTERNAL'}
+
+    directory: bpy.props.StringProperty(
+        name="Asset Library Directory",
+        subtype='DIR_PATH',
+        default="",
+    )
+
+    filter_folder: bpy.props.BoolProperty(
+        name="Filter folders",
+        default=True,
+        options={'HIDDEN'},
+    )
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.data.project is not None
+
+    def execute(self, context):
+        if self.directory == "":
+            self.report({'ERROR'}, "Cannot create an assset library with an empty directory path.")
+            return {'CANCELLED'}
+
+        # Create an initial name from the folder name
+        #
+        # If the folder name contains no valid unicode (resulting in an empty
+        # string after processing), we fallback to a default.
+        asset_library_path = os.path.normpath(self.directory)
+        asset_library_name = os.path.basename(asset_library_path).title() \
+            .encode('utf-8', 'surrogateescape') \
+            .decode('utf-8', 'ignore') \
+            or data_(ASSET_LIBRARY_DEFAULT_NAME)
+
+        # Replace base path with {project_root} if it is within the project folder.
+        root_path = bpy.data.project.root_path
+        if asset_library_path.startswith(root_path):
+            asset_library_path = "{project_root}" + asset_library_path[len(root_path):]
+        bpy.data.project.asset_libraries.new(name=asset_library_name, directory=asset_library_path)
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+
+class PROJECT_OT_AssetLibraryRemove(Operator):
+    """
+    Deregister an asset library so that its directory will no longer show up in
+    Asset Browsers and other places showing assets.
+    """
+    bl_idname = "project.asset_library_remove"
+    bl_label = "Remove Project Asset Library"
+    bl_options = {'INTERNAL'}
+
+    index: bpy.props.IntProperty(
+        name="Index",
+        default=0,
+    )
+
+    @classmethod
+    def poll(cls, context):
+        if bpy.data.project is None:
+            return False
+        return len(bpy.data.project.asset_libraries) != 0
+
+    def execute(self, context):
+        if self.index >= len(bpy.data.project.asset_libraries):
+            return {'CANCELLED'}
+
+        library = bpy.data.project.asset_libraries[self.index]
+        bpy.data.project.asset_libraries.remove(library)
+
+        return {'FINISHED'}
+
+
 # -------------------------------------------------------------
 # Handler Callbacks
 #
@@ -762,6 +892,8 @@ classes = (
     PROJECT_OT_AddVariable,
     PROJECT_OT_RemoveVariable,
     PROJECT_OT_MoveVariable,
+    PROJECT_OT_AssetLibraryAdd,
+    PROJECT_OT_AssetLibraryRemove,
 )
 
 

source/blender/asset_system/AS_asset_library.hh
@@ -361,6 +361,10 @@ void AS_asset_libraries_exit();
 asset_system::AssetLibrary *AS_asset_library_load_from_directory(const char *name,
                                                                  const char *library_dirpath);
 
+/** Return a normalized path string, will also expand project variables that are in the passed path
+ * string. **/
+std::string AS_asset_library_resolve_path(StringRef path);
+
 /** Return whether any loaded AssetLibrary has unsaved changes to its catalogs. */
 bool AS_asset_library_has_any_unsaved_catalogs();
 

source/blender/asset_system/intern/asset_library.cc
@@ -68,6 +68,11 @@ AssetLibrary *AS_asset_library_load_from_directory(const char *name, const char 
   return lib;
 }
 
+std::string AS_asset_library_resolve_path(StringRef path)
+{
+  return utils::resolve_directory_path(path);
+}
+
 bool AS_asset_library_has_any_unsaved_catalogs()
 {
   AssetLibraryService *service = AssetLibraryService::get();
@@ -85,7 +90,7 @@ std::string AS_asset_library_find_suitable_root_path_from_path(const StringRefNu
   if (bUserAssetLibrary *preferences_lib = BKE_preferences_asset_library_containing_path(
           &U, input_path.c_str()))
   {
-    return preferences_lib->dirpath;
+    return preferences_lib->resolved_dirpath;
   }
 
   char buffer[FILE_MAXDIR];
@@ -222,7 +227,7 @@ AssetLibrary::AssetLibrary(eAssetLibraryType library_type,
     : library_type_(library_type),
       is_read_only_(is_read_only),
       name_(name),
-      root_path_(std::make_shared<std::string>(utils::normalize_directory_path(root_path))),
+      root_path_(std::make_shared<std::string>(utils::resolve_directory_path(root_path))),
       catalog_service_(std::make_unique<AssetCatalogService>(
           *root_path_,
           is_read_only ? std::optional{AssetCatalogService::read_only_tag{}} : std::nullopt))

source/blender/asset_system/intern/asset_library_service.cc
@@ -115,7 +115,7 @@ AssetLibrary *AssetLibraryService::get_asset_library(
         return this->get_preferences_remote_asset_library(*custom_library);
       }
 
-      std::string root_path = custom_library->dirpath;
+      std::string root_path = custom_library->resolved_dirpath;
       if (root_path.empty()) {
         return nullptr;
       }
@@ -184,14 +184,13 @@ AssetLibrary *AssetLibraryService::get_asset_library_on_disk(
     const bool load_catalogs,
     bUserAssetLibrary *preferences_library)
 {
-  const std::string normalized_root_path = utils::normalize_directory_path(root_path);
-
   /* Lock for the entire "lookup and if not found -> create and insert" scope, so no two threads do
    * this in parallel and interfere with each other. */
   std::scoped_lock lock{on_disk_libraries_mutex_};
 
-  if (OnDiskAssetLibrary *lib = this->lookup_on_disk_library(library_type, normalized_root_path)) {
-    CLOG_DEBUG(&LOG, "get \"%s\" (cached)", normalized_root_path.c_str());
+  const std::string resolved_root_path = utils::resolve_directory_path(root_path);
+  if (OnDiskAssetLibrary *lib = this->lookup_on_disk_library(library_type, resolved_root_path)) {
+    CLOG_DEBUG(&LOG, "get \"%s\" (cached)", resolved_root_path.c_str());
     if (load_catalogs) {
       lib->load_or_reload_catalogs();
     }
@@ -207,7 +206,7 @@ AssetLibrary *AssetLibraryService::get_asset_library_on_disk(
       else {
         /* Only used by unit tests. */
         lib_uptr = std::make_unique<OnDiskAssetLibrary>(
-            library_type, name, normalized_root_path, /*is_read_only=*/false);
+            library_type, name, resolved_root_path, /*is_read_only=*/false);
       }
       break;
     case ASSET_LIBRARY_ESSENTIALS:
@@ -215,18 +214,18 @@ AssetLibrary *AssetLibraryService::get_asset_library_on_disk(
       break;
     case ASSET_LIBRARY_LOCAL:
       lib_uptr = std::make_unique<OnDiskAssetLibrary>(
-          library_type, name, normalized_root_path, /*is_read_only=*/false);
+          library_type, name, resolved_root_path, /*is_read_only=*/false);
       break;
     default:
       lib_uptr = std::make_unique<OnDiskAssetLibrary>(
-          library_type, name, normalized_root_path, /*is_read_only=*/true);
+          library_type, name, resolved_root_path, /*is_read_only=*/true);
       break;
   }
 
   /* Get underlying pointer before moving. */
   AssetLibrary *lib = lib_uptr.get();
-  on_disk_libraries_.add_new({library_type, normalized_root_path}, std::move(lib_uptr));
-  CLOG_DEBUG(&LOG, "get \"%s\" (loaded)", normalized_root_path.c_str());
+  on_disk_libraries_.add_new({library_type, resolved_root_path}, std::move(lib_uptr));
+  CLOG_DEBUG(&LOG, "get \"%s\" (loaded)", resolved_root_path.c_str());
 
   if (load_catalogs) {
     lib->load_or_reload_catalogs();
@@ -244,8 +243,11 @@ AssetLibrary *AssetLibraryService::get_asset_library_on_disk_custom(StringRef na
 AssetLibrary *AssetLibraryService::get_asset_library_on_disk_custom_preferences(
     bUserAssetLibrary *custom_library)
 {
-  return this->get_asset_library_on_disk(
-      ASSET_LIBRARY_CUSTOM, custom_library->name, custom_library->dirpath, true, custom_library);
+  return this->get_asset_library_on_disk(ASSET_LIBRARY_CUSTOM,
+                                         custom_library->name,
+                                         custom_library->resolved_dirpath,
+                                         true,
+                                         custom_library);
 }
 
 AssetLibrary *AssetLibraryService::get_asset_library_on_disk_builtin(eAssetLibraryType type,
@@ -390,11 +392,11 @@ OnDiskAssetLibrary *AssetLibraryService::lookup_on_disk_library(eAssetLibraryTyp
   BLI_assert_msg(!root_path.is_empty(),
                  "top level directory must be given for on-disk asset library");
 
-  std::string normalized_root_path = utils::normalize_directory_path(root_path);
+  std::string resolved_root_path = utils::resolve_directory_path(root_path);
 
   std::scoped_lock lock{on_disk_libraries_mutex_};
   std::unique_ptr<OnDiskAssetLibrary> *lib_uptr_ptr = on_disk_libraries_.lookup_ptr(
-      {library_type, normalized_root_path});
+      {library_type, resolved_root_path});
   return lib_uptr_ptr ? lib_uptr_ptr->get() : nullptr;
 }
 
@@ -430,7 +432,7 @@ std::string AssetLibraryService::resolve_asset_weak_reference_to_library_path(
       bUserAssetLibrary *custom_lib = find_custom_preferences_asset_library_from_asset_weak_ref(
           asset_reference);
       if (custom_lib) {
-        library_dirpath = custom_lib->dirpath;
+        library_dirpath = custom_lib->resolved_dirpath;
         break;
       }
 
@@ -456,7 +458,7 @@ std::string AssetLibraryService::resolve_asset_weak_reference_to_library_path(
       return "";
   }
 
-  std::string normalized_library_dirpath = utils::normalize_path(library_dirpath);
+  std::string normalized_library_dirpath = utils::resolve_path(library_dirpath);
   return normalized_library_dirpath;
 }
 
@@ -511,10 +513,10 @@ std::string AssetLibraryService::normalize_asset_weak_reference_relative_asset_i
   const int64_t group_name_sep_pos = relative_asset_identifier.find_first_of(SEP_STR ALTSEP_STR,
                                                                              blend_path_len);
 
-  return utils::normalize_path(relative_asset_identifier,
-                               (group_name_sep_pos == StringRef::not_found) ?
-                                   StringRef::not_found :
-                                   group_name_sep_pos + 1);
+  return utils::resolve_path(relative_asset_identifier,
+                             (group_name_sep_pos == StringRef::not_found) ?
+                                 StringRef::not_found :
+                                 group_name_sep_pos + 1);
 }
 
 std::string AssetLibraryService::resolve_asset_weak_reference_to_full_path(
@@ -534,7 +536,7 @@ std::string AssetLibraryService::resolve_asset_weak_reference_to_full_path(
     return "";
   }
 
-  std::string normalized_full_path = utils::normalize_path(library_dirpath + SEP_STR) +
+  std::string normalized_full_path = utils::resolve_path(library_dirpath + SEP_STR) +
                                      normalize_asset_weak_reference_relative_asset_identifier(
                                          asset_reference);
 
@@ -623,11 +625,11 @@ std::string AssetLibraryService::root_path_from_library_ref(
 
   bUserAssetLibrary *custom_library = find_custom_asset_library_from_library_ref(
       library_reference);
-  if (!custom_library || !custom_library->dirpath[0]) {
+  if (!custom_library || !custom_library->resolved_dirpath[0]) {
     return "";
   }
 
-  return custom_library->dirpath;
+  return custom_library->resolved_dirpath;
 }
 
 void AssetLibraryService::allocate_service_instance()

source/blender/asset_system/intern/library_types/essentials_library.cc
@@ -32,7 +32,7 @@ static CLG_LogRef LOG = {"asset.library.essentials"};
 EssentialsAssetLibrary::EssentialsAssetLibrary()
     : OnDiskAssetLibrary(ASSET_LIBRARY_ESSENTIALS,
                          {},
-                         utils::normalize_directory_path(essentials_directory_path()),
+                         utils::resolve_directory_path(essentials_directory_path()),
                          /*is_read_only=*/true)
 {
 }

source/blender/asset_system/intern/utils.hh
@@ -16,7 +16,7 @@ namespace blender::asset_system::utils {
  * Returns a normalized directory path with a trailing slash, and a maximum length of #PATH_MAX.
  * Slashes are converted to native format.
  */
-std::string normalize_directory_path(StringRef directory);
+std::string resolve_directory_path(StringRef directory);
 
 /**
  * Normalize the given `path` (remove 'parent directory' and double-slashes element etc., and
@@ -26,6 +26,6 @@ std::string normalize_directory_path(StringRef directory);
  * string up to the given length is processed, the rest remains unchanged. Needed to avoid
  * modifying ID name part of linked library paths.
  */
-std::string normalize_path(StringRefNull path, int64_t max_len = StringRef::not_found);
+std::string resolve_path(StringRefNull path, int64_t max_len = StringRef::not_found);
 
 }  // namespace blender::asset_system::utils

source/blender/asset_system/tests/asset_library_service_test.cc
@@ -10,6 +10,7 @@
 
 #include "BKE_appdir.hh"
 #include "BKE_callbacks.hh"
+#include "BKE_global.hh"
 #include "BKE_gtest_base.hh"
 #include "BKE_main.hh"
 
@@ -27,6 +28,7 @@ class AssetLibraryServiceTest : public bke::BlenderGTestBase {
  public:
   CatalogFilePath asset_library_root_;
   CatalogFilePath temp_library_path_;
+  Main *bmain;
 
   void SetUp() override
   {
@@ -34,6 +36,13 @@ class AssetLibraryServiceTest : public bke::BlenderGTestBase {
     if (test_files_dir.empty()) {
       FAIL();
     }
+
+    /* G_MAIN is needed for variable expansion in asset library paths.
+     * IE {project_root}, {blend_name}, {blend_dir} etc.
+     */
+    bmain = BKE_main_new();
+    G_MAIN = bmain;
+
     asset_library_root_ = test_files_dir + SEP_STR + "asset_library";
     temp_library_path_ = "";
   }
@@ -46,6 +55,9 @@ class AssetLibraryServiceTest : public bke::BlenderGTestBase {
       BLI_delete(temp_library_path_.c_str(), true, true);
       temp_library_path_ = "";
     }
+
+    BKE_main_free(bmain);
+    G_MAIN = nullptr;
   }
 
   /* Register a temporary path, which will be removed at the end of the test.

commit e99522baa73bbdcd22b26239863148a07988b6b0

Fix: Projects: Buffer overflow expanding asset library paths
The buffer size did not match the size passed to BKE_path_apply_template.

This issue was introduced in 89f2284b813.

Pull Request: https://projects.blender.org/blender/blender/pulls/163777

source/blender/asset_system/intern/utils.cc
@@ -37,7 +37,7 @@ std::string resolve_directory_path(StringRef directory)
   bke::BlenderProject *project = BKE_blender_project_get(G_MAIN);
   BKE_add_template_variables_general(template_variables, nullptr, project);
   const Vector<path_templates::Error> variable_errors = BKE_path_apply_template(
-      dir_resolved, FILE_MAX, template_variables);
+      dir_resolved, sizeof(dir_resolved), template_variables);
   if (!variable_errors.is_empty()) {
     /* Do nothing on errors as the "dir_normalized" variable should still contain the original
      * string. The reset of the code should still gracefully treat this as an invalid path.
@@ -54,13 +54,21 @@ std::string resolve_path(StringRefNull path, int64_t max_len)
   const int64_t len = (max_len == StringRef::not_found) ? path.size() :
                                                           std::min(max_len, path.size());
 
-  char *buf = BLI_strdupn(path.c_str(), len);
+  char buf[FILE_MAX];
+
+  /* A longer path does not fit the buffer. Leave it untouched, rather than splicing a
+   * truncated result back together with the tail below. */
+  if (len >= int64_t(sizeof(buf))) {
+    return path;
+  }
+
+  BLI_strncpy(buf, path.c_str(), len + 1);
 
   path_templates::VariableMap template_variables;
   bke::BlenderProject *project = BKE_blender_project_get(G_MAIN);
   BKE_add_template_variables_general(template_variables, nullptr, project);
   const Vector<path_templates::Error> variable_errors = BKE_path_apply_template(
-      buf, FILE_MAX, template_variables);
+      buf, sizeof(buf), template_variables);
   if (!variable_errors.is_empty()) {
     /* Do nothing on errors as the "dir_normalized" variable should still contain the original
      * string. The reset of the code should still gracefully treat this as an invalid path.
@@ -72,7 +80,6 @@ std::string resolve_path(StringRefNull path, int64_t max_len)
   BLI_path_normalize(buf);
 
   std::string resolved_path = buf;
-  MEM_delete(buf);
 
   if (len != path.size()) {
     resolved_path = resolved_path + path.substr(len);