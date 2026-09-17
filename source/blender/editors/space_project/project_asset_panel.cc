blender/blender

commit 73915eb274915de1c762266b565f0fceedcbad23

UI: Always draw project categories
Draw all the categories tab even if project doesn't exist. Basically,
remove poll function that decides this.

See PR description for images

Pull Request: https://projects.blender.org/blender/blender/pulls/163183

source/blender/editors/space_project/project_asset_panel.cc
@@ -159,12 +159,6 @@ static void project_asset_panel_draw(const bContext *C, Panel *panel)
   draw_active_library_settings(C, layout, libraries[active_asset_library]);
 }
 
-static bool project_asset_panel_poll(const bContext *C, PanelType * /*panel_type*/)
-{
-  bke::BlenderProject *project = BKE_blender_project_get(CTX_data_main(C));
-  return project != nullptr;
-}
-
 void project_asset_panel_register(ARegionType &region_type)
 {
   PanelType *panel_type = MEM_new_zeroed<PanelType>(__func__);
@@ -176,7 +170,6 @@ void project_asset_panel_register(ARegionType &region_type)
   panel_type->space_type = SPACE_PROJECT;
   panel_type->region_type = RGN_TYPE_WINDOW;
   panel_type->draw = project_asset_panel_draw;
-  panel_type->poll = project_asset_panel_poll;
   panel_type->order = 10; /* Make sure the category are put after the other base categoies. */
   BLI_addtail(&region_type.paneltypes, panel_type);
 }

scripts/startup/bl_ui/space_project.py
@@ -251,10 +251,6 @@ class PROJECT_PT_variables(Panel):
     bl_region_type = 'WINDOW'
     bl_category = "Variables"
 
-    @classmethod
-    def poll(cls, context):
-        return bpy.data.project is not None
-
     def draw(self, context):
         project = bpy.data.project
 

commit db9ca467bb01939a6b70cc679c4f4feea02f873a

Fix: Project crash due to nullptr dereferencing
Caused by 73915eb2749
Return early in case project is nullptr.

Pull Request: https://projects.blender.org/blender/blender/pulls/164049

source/blender/editors/space_project/project_asset_panel.cc
@@ -110,19 +110,30 @@ struct ProjectAssetLibraryListItem : public AssetLibraryListItemCommon {
   void on_activate(bContext &C) override
   {
     bke::BlenderProject *project = BKE_blender_project_get(CTX_data_main(&C));
+    if (!project) {
+      return;
+    }
     project->active_asset_library_index = index_in_list;
   }
   std::optional<bool> should_be_active() const override
   {
-    bke::BlenderProject *project = BKE_blender_project_get(G_MAIN);
+    const bke::BlenderProject *project = BKE_blender_project_get(G_MAIN);
+    if (!project) {
+      return std::nullopt;
+    }
     return project->active_asset_library_index == index_in_list;
   }
 };
 
 static void project_asset_panel_draw(const bContext *C, Panel *panel)
 {
+  const bke::BlenderProject *project = BKE_blender_project_get(CTX_data_main(C));
+  if (!project) {
+    /* The category is always shown, but there is nothing to edit without a project. */
+    return;
+  }
+
   Vector<AnyAssetLibraryDefinition> libraries = project_ui_asset_libraries();
-  bke::BlenderProject *project = BKE_blender_project_get(CTX_data_main(C));
   int active_asset_library = project->active_asset_library_index;
 
   ui::Layout &layout = *panel->layout;