81d6b5898d2a3a02b3650b8e550de3a5a427b332
Jonas Holzman <jonas@blender.org>
2026-09-18 12:14:17 +0200

    UI: Use Android immersive mode, hide navigation bar and keep status bar visible

--- release/android/app/src/main/java/org/blender/blender/BlenderActivity.java
+++ release/android/app/src/main/java/org/blender/blender/BlenderActivity.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.system.Os;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
@@ -155,6 +157,35 @@ public class BlenderActivity extends SDLActivity {
 
         Log.i(LOG_TAG, "Portable version directory extracted to " + extractDir);
         return extractDir;
+    }
+
+    private void hideNavigationBar() {
+        /* Use the same system UI flags as SDL, but keep the status bar visible. */
+        View decorView = getWindow().getDecorView();
+        int flags = decorView.getSystemUiVisibility();
+        flags &= ~(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        flags |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        decorView.setSystemUiVisibility(flags);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideNavigationBar();
+        }
+    }
+
+    @Override
+    public void onSystemUiVisibilityChange(int visibility) {
+        /* Replace SDL's callback, which hides the status bar in fullscreen mode. */
+        if (hasWindowFocus()) {
+            hideNavigationBar();
+        }
     }
 
     @Override
