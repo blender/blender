blender/blender

commit 98f13632caa089c94e4a853a0977c770ca3f1753

Refactor: DNA: Change almost all enums from integer to enum type
Resulting SDNA and default values are unchanged, any fixes and minor type
and padding changes happened in earlier commits.

Various function arguments and variables were changed to use enums types,
and casts were added where necessary (or where changes would be too deep).

Pull Request: https://projects.blender.org/blender/blender/pulls/157811

source/blender/blenkernel/intern/pointcache.cc
@@ -157,7 +157,10 @@ static int ptcache_basic_header_write(PTCacheFile *pf)
 
   return 1;
 }
-static void ptcache_add_extra_data(PTCacheMem *pm, uint type, uint count, void *data)
+static void ptcache_add_extra_data(PTCacheMem *pm,
+                                   ePointCache_ExtraDataType type,
+                                   uint count,
+                                   void *data)
 {
   PTCacheExtra *extra = MEM_new<PTCacheExtra>("Point cache: extra data descriptor");
 
@@ -531,6 +534,8 @@ static void ptcache_particle_extra_read(void *psys_v, PTCacheMem *pm, float /*cf
         psys->tot_fluidsprings = psys->alloc_fluidsprings = extra->totdata;
         break;
       }
+      case BPHYS_EXTRA_CLOTH_ACCELERATION:
+        break;
     }
   }
 }
@@ -633,6 +638,8 @@ static void ptcache_cloth_extra_read(void *cloth_v, PTCacheMem *pm, float /*cfra
         copy_v3_v3(cloth->average_acceleration, static_cast<const float *>(extra->data));
         break;
       }
+      case BPHYS_EXTRA_FLUID_SPRINGS:
+        break;
     }
   }
 }
@@ -1931,7 +1938,7 @@ static PTCacheMem *ptcache_disk_frame_to_mem(PTCacheID *pid, int cfra)
   }
 
   if (!error && pf->flag & PTCACHE_TYPEFLAG_EXTRADATA) {
-    uint extratype = 0;
+    ePointCache_ExtraDataType extratype = ePointCache_ExtraDataType{};
 
     while (!error && ptcache_file_read(pf, &extratype, 1, sizeof(uint))) {
       PTCacheExtra *extra = MEM_new<PTCacheExtra>("Pointcache extradata");
@@ -3353,7 +3360,7 @@ void BKE_ptcache_disk_to_mem(PTCacheID *pid)
 {
   PointCache *cache = pid->cache;
   PTCacheMem *pm = nullptr;
-  int baked = cache->flag & PTCACHE_BAKED;
+  ePointCache_Flag baked = cache->flag & PTCACHE_BAKED;
   int cfra, sfra = cache->startframe, efra = cache->endframe;
 
   /* Remove possible bake flag to allow clear */
@@ -3377,7 +3384,7 @@ void BKE_ptcache_mem_to_disk(PTCacheID *pid)
 {
   PointCache *cache = pid->cache;
   PTCacheMem *pm = static_cast<PTCacheMem *>(cache->mem_cache.first);
-  int baked = cache->flag & PTCACHE_BAKED;
+  ePointCache_Flag baked = cache->flag & PTCACHE_BAKED;
 
   /* Remove possible bake flag to allow clear */
   cache->flag &= ~PTCACHE_BAKED;

commit 0d5a793e044d048beab3cd8b1a36b382411200df

Fix: Physics: Invalid point cache extra data read
Has not caused a known bug, but was reading into too small a datatype
and potentially corrupting other stack variables.

Issue partially pre-existing, partially introduced in 98f13632ca.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/blenkernel/intern/pointcache.cc
@@ -693,7 +693,8 @@ static int ptcache_dynamicpaint_write(PTCacheFile *pf, void *dp_v)
     const int total_points = surface->data->total_points;
 
     /* cache type */
-    ptcache_file_write(pf, &surface->type, 1, sizeof(int));
+    const int surface_type = int(surface->type);
+    ptcache_file_write(pf, &surface_type, 1, sizeof(int));
 
     uint in_stride;
     if (surface->type == MOD_DPAINT_SURFACE_T_PAINT) {
@@ -1933,12 +1934,17 @@ static PTCacheMem *ptcache_disk_frame_to_mem(PTCacheID *pid, int cfra)
   }
 
   if (!error && pf->flag & PTCACHE_TYPEFLAG_EXTRADATA) {
-    ePointCache_ExtraDataType extratype = ePointCache_ExtraDataType{};
+    uint extratype = 0;
 
     while (!error && ptcache_file_read(pf, &extratype, 1, sizeof(uint))) {
+      if (!ELEM(extratype, BPHYS_EXTRA_FLUID_SPRINGS, BPHYS_EXTRA_CLOTH_ACCELERATION)) {
+        error = 1;
+        break;
+      }
+
       PTCacheExtra *extra = MEM_new<PTCacheExtra>("Pointcache extradata");
 
-      extra->type = extratype;
+      extra->type = ePointCache_ExtraDataType(extratype);
 
       ptcache_file_read(pf, &extra->totdata, 1, sizeof(uint));
 
@@ -2020,7 +2026,8 @@ static int ptcache_mem_frame_to_disk(PTCacheID *pid, PTCacheMem *pm)
         continue;
       }
 
-      ptcache_file_write(pf, &extra->type, 1, sizeof(uint));
+      const uint extratype = uint(extra->type);
+      ptcache_file_write(pf, &extratype, 1, sizeof(uint));
       ptcache_file_write(pf, &extra->totdata, 1, sizeof(uint));
 
       ptcache_file_compressed_write(