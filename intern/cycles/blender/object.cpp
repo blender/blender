blender/blender

commit 6e7a0eee32b4674894312f9d9aafbf1e092492fc

Cycles: Raycast Visibility Toggle
Cycles implementation of #157799.
Allows removing objects from Raycast Node queries.

Adds `PathRayVisibilityFlag::PATH_RAY_VISIBILITY_RAYCAST` .
OSL adds a new `__raycast__` traceset.

Pull Request: https://projects.blender.org/blender/blender/pulls/158816

intern/cycles/kernel/types.h
@@ -163,10 +163,12 @@ enum PathRayVisibilityFlag : uint32_t {
   PATH_RAY_VISIBILITY_SHADOW = (PATH_RAY_VISIBILITY_SHADOW_OPAQUE |
                                 PATH_RAY_VISIBILITY_SHADOW_TRANSPARENT),
 
+  PATH_RAY_VISIBILITY_RAYCAST = (1U << 7U),
+
   /* Set of flags used for ray visibility for intersection.
    *
    * NOTE: SHADOW_CATCHER and OSL macros below assume there are no more than 16 visibility bits. */
-  PATH_RAY_VISIBILITY_ALL = ((1U << 7U) - 1U),
+  PATH_RAY_VISIBILITY_ALL = ((1U << 8U) - 1U),
 
   /* Special flag to tag unaligned BVH nodes.
    * Only set and used in BVH nodes to distinguish how to interpret bounding box information stored

intern/cycles/blender/addon/ui.py
@@ -1507,6 +1507,7 @@ class CYCLES_OBJECT_PT_visibility_ray_visibility(CyclesButtonsPanel, Panel):
         if ob.type != 'LIGHT':
             sub = col.column()
             sub.prop(ob, "visible_shadow", text="Shadow")
+            sub.prop(ob, "visible_raycast", text="Raycast")
 
 
 class CYCLES_OBJECT_PT_visibility_culling(CyclesButtonsPanel, Panel):

intern/cycles/blender/util.h
@@ -741,6 +741,9 @@ static inline PathRayVisibility object_ray_visibility(blender::Object &b_ob)
   visibility |= ((b_ob.visibility_flag & blender::OB_HIDE_VOLUME_SCATTER) == 0) ?
                     PATH_RAY_VISIBILITY_VOLUME_SCATTER :
                     PATH_RAY_VISIBILITY_NONE;
+  visibility |= ((b_ob.visibility_flag & blender::OB_HIDE_RAYCAST) == 0) ?
+                    PATH_RAY_VISIBILITY_RAYCAST :
+                    PATH_RAY_VISIBILITY_NONE;
 
   return visibility;
 }

intern/cycles/kernel/osl/services.cpp
@@ -570,8 +570,10 @@ bool OSLRenderServices::trace(TraceOpt &options,
   }
   else {
     /* Ray-trace, leaving out shadow opaque to avoid early exit. */
-    const PathRayVisibility visibility = PATH_RAY_VISIBILITY_ALL &
-                                         ~PATH_RAY_VISIBILITY_SHADOW_OPAQUE;
+    PathRayVisibility visibility = PATH_RAY_VISIBILITY_ALL & ~PATH_RAY_VISIBILITY_SHADOW_OPAQUE;
+    if (options.traceset == DeviceStrings::u_traceset_raycast) {
+      visibility = PATH_RAY_VISIBILITY_RAYCAST;
+    }
     tracedata->hit = scene_intersect(kg, &ray, visibility, &tracedata->isect);
     if (tracedata->hit) {
       tracedata->self_hit = tracedata->isect.object == sd->object;

intern/cycles/kernel/osl/shaders/node_raycast.osl
@@ -36,7 +36,7 @@ shader node_raycast(point Position = P,
             "mindist",
             mindist,
             "traceset",
-            only_local ? "__only_local__" : ""))
+            only_local ? "__only_local__" : "__raycast__"))
   {
     IsHit = 1.0;
     getmessage("trace", "hitself", SelfHit);

intern/cycles/kernel/osl/strings.h
@@ -111,6 +111,7 @@ OSL_DEVICE_STRING(u_distance, "distance", 5661183123366514158ull)
 OSL_DEVICE_STRING(u_index, "index", 15907549540151602841ull)
 OSL_DEVICE_STRING(u_trace, "trace", 13264932728578201327ull)
 OSL_DEVICE_STRING(u_traceset_only_local, "__only_local__", 12891670648956128852ull)
+OSL_DEVICE_STRING(u_traceset_raycast, "__raycast__", 2367107625979882517ull)
 OSL_DEVICE_STRING(u_hit, "hit", 7529033939518063282ull)
 OSL_DEVICE_STRING(u_hitdist, "hitdist", 17066342024105335641ull)
 OSL_DEVICE_STRING(u_hitself, "hitself", 12209754783026028319ull)

intern/cycles/kernel/svm/raycast.h
@@ -76,10 +76,7 @@ ccl_device bool svm_raycast(KernelGlobals kg,
     isect = local_isect.hits[0];
   }
   else {
-    /* Ray-trace, leaving out shadow opaque to avoid early exit. */
-    const PathRayVisibility visibility = PATH_RAY_VISIBILITY_ALL &
-                                         ~PATH_RAY_VISIBILITY_SHADOW_OPAQUE;
-    if (!scene_intersect(kg, &ray, visibility, &isect)) {
+    if (!scene_intersect(kg, &ray, PATH_RAY_VISIBILITY_RAYCAST, &isect)) {
       return false;
     }
   }

tests/files/render/raycast/cycles_renders/raycast_visibility.png
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d4cc55a8147622436202446629386d1ba0885a70f76334e65c0de8a1edb59d9
-size 10234
+oid sha256:bc368d7d3d7dbd286de10b228facc4d46580e21388aa2e73ba18831200d27d91
+size 9815

commit 0dc985ec5ec29c7442a5ed48cde4ed51f7f368bd

Fix: Cycles: Objects visible only to raycast hit by other rayso
The new raycast visibility is not like the other visibility flags, which
cover all possible light transport rays and so PATH_RAY_VISIBILITY_ALL
was a complete mask of those. But raycast rays are separate from that,
and should not affect e.g. volume stack interseciton.

Now define a separate PATH_RAY_VISIBILITY_OBJECT_ALL that indicates all
the possible flags that can be set on an object.

Issue introduced in 6e7a0eee32b.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

intern/cycles/blender/object.cpp
@@ -220,7 +220,7 @@ Object *BlenderSync::sync_object(blender::ViewLayer &b_view_layer,
   /* TODO: make holdout objects on excluded layer invisible for non-camera rays. */
 #if 0
   if (use_holdout && (layer_flag & view_layer.exclude_layer)) {
-    visibility &= ~(PATH_RAY_VISIBILITY_ALL & ~PATH_RAY_VISIBILITY_CAMERA);
+    visibility &= ~(PATH_RAY_VISIBILITY_OBJECT_ALL & ~PATH_RAY_VISIBILITY_CAMERA);
   }
 #endif
 

intern/cycles/hydra/geometry.inl
@@ -163,7 +163,7 @@ void HdCyclesGeometry<Base, CyclesBase>::Sync(HdSceneDelegate *sceneDelegate,
 
   if (HdChangeTracker::IsVisibilityDirty(*dirtyBits, id)) {
     for (Object *instance : _instances) {
-      instance->set_visibility(Base::IsVisible() ? PATH_RAY_VISIBILITY_ALL : 0);
+      instance->set_visibility(Base::IsVisible() ? PATH_RAY_VISIBILITY_OBJECT_ALL : 0);
     }
   }
 

intern/cycles/kernel/bvh/local.h
@@ -88,7 +88,7 @@ ccl_device_inline
                                        tmin,
                                        isect_t,
                                        node_addr,
-                                       PATH_RAY_VISIBILITY_ALL,
+                                       PATH_RAY_VISIBILITY_OBJECT_ALL,
                                        dist);
 
         node_addr = __float_as_int(cnodes.z);

intern/cycles/kernel/device/cpu/bvh.h
@@ -734,7 +734,7 @@ ccl_device_intersect bool kernel_embree_intersect_local(KernelGlobals kg,
   }
   ctx.local_object_id = local_object;
   RTCRay rtc_ray;
-  kernel_embree_setup_ray(*ray, rtc_ray, PATH_RAY_VISIBILITY_ALL);
+  kernel_embree_setup_ray(*ray, rtc_ray, PATH_RAY_VISIBILITY_OBJECT_ALL);
 
   RTCOccludedArguments args;
   rtcInitOccludedArguments(&args);

intern/cycles/kernel/types.h
@@ -166,12 +166,16 @@ enum PathRayVisibilityFlag : uint32_t {
   PATH_RAY_VISIBILITY_SHADOW = (PATH_RAY_VISIBILITY_SHADOW_OPAQUE |
                                 PATH_RAY_VISIBILITY_SHADOW_TRANSPARENT),
 
+  /* Set of flags used for path ray visibility. */
+  PATH_RAY_VISIBILITY_ALL = ((1U << 7U) - 1U),
+
+  /* Raycast shader node rays, not part of the path. */
   PATH_RAY_VISIBILITY_RAYCAST = (1U << 7U),
 
-  /* Set of flags used for ray visibility for intersection.
+  /* Set of all flags an object can be visible to.
    *
    * NOTE: SHADOW_CATCHER and OSL macros below assume there are no more than 16 visibility bits. */
-  PATH_RAY_VISIBILITY_ALL = ((1U << 8U) - 1U),
+  PATH_RAY_VISIBILITY_OBJECT_ALL = (PATH_RAY_VISIBILITY_ALL | PATH_RAY_VISIBILITY_RAYCAST),
 
   /* Special flag to tag unaligned BVH nodes.
    * Only set and used in BVH nodes to distinguish how to interpret bounding box information stored
@@ -308,7 +312,7 @@ enum PathRayMNEE {
  * On shadow catcher paths we want to ignore any intersections with non-catchers,
  * whereas on regular paths we want to intersect all objects. */
 
-static_assert(PATH_RAY_VISIBILITY_ALL <= 0xffff);
+static_assert(PATH_RAY_VISIBILITY_OBJECT_ALL <= 0xffff);
 
 #define SHADOW_CATCHER_VISIBILITY_SHIFT(visibility) (uint32_t(visibility) << 16)
 
@@ -327,7 +331,7 @@ static_assert(PATH_RAY_VISIBILITY_ALL <= 0xffff);
  * Note that while the entire PathRayVisibilityFlag flags are stored in the rayrtype, only part of
  * the PathRayFlag is stored. */
 
-static_assert(PATH_RAY_VISIBILITY_ALL <= 0xffff);
+static_assert(PATH_RAY_VISIBILITY_OBJECT_ALL <= 0xffff);
 
 #define OSL_RAYTYPE_PACK(visibility, path_flag) \
   (int((uint32_t((path_flag) & 0xffff) << 16) | uint32_t((visibility) & 0xffff)))

intern/cycles/scene/geometry_bvh.cpp
@@ -52,7 +52,7 @@ void Geometry::compute_bvh(Device *device,
     /* Ensure all visibility bits are set at the geometry level BVH. In
      * the object level BVH is where actual visibility is tested. */
     object.set_is_shadow_catcher(true);
-    object.set_visibility(PATH_RAY_VISIBILITY_ALL);
+    object.set_visibility(PATH_RAY_VISIBILITY_OBJECT_ALL);
 
     object.set_geometry(this);
 

intern/cycles/scene/object.cpp
@@ -78,7 +78,7 @@ NODE_DEFINE(Object)
 
   SOCKET_NODE(geometry, "Geometry", Geometry::get_node_base_type());
   SOCKET_TRANSFORM(tfm, "Transform", transform_identity());
-  SOCKET_UINT(visibility, "Visibility", PATH_RAY_VISIBILITY_ALL);
+  SOCKET_UINT(visibility, "Visibility", PATH_RAY_VISIBILITY_OBJECT_ALL);
   SOCKET_COLOR(color, "Color", zero_float3());
   SOCKET_FLOAT(alpha, "Alpha", 0.0f);
   SOCKET_UINT(random_id, "Random ID", 0);
@@ -298,7 +298,7 @@ bool Object::is_traceable() const
 
 uint Object::visibility_for_tracing() const
 {
-  assert((visibility & ~uint(PATH_RAY_VISIBILITY_ALL)) == 0);
+  assert((visibility & ~uint(PATH_RAY_VISIBILITY_OBJECT_ALL)) == 0);
   return SHADOW_CATCHER_OBJECT_VISIBILITY(is_shadow_catcher, visibility);
 }
 

source/blender/makesrna/intern/rna_object.cc
@@ -2971,7 +2971,7 @@ static void rna_def_object_visibility(StructRNA *srna)
   RNA_def_property_ui_text(
       prop,
       "Raycast Visibility",
-      "Object visibility to raycast rays. Implicitly false for Blended materials.");
+      "Object visibility to raycast rays. Implicitly false for Blended materials in EEVEE.");
   RNA_def_property_update(prop, NC_OBJECT | ND_DRAW, "rna_Object_internal_update_draw");
 }
 