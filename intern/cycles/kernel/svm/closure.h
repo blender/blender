zNightlord/Bforartists

commit 7ee94c067cf71845727672ed77dc25f088cd5b32

Refactor: Cycles: Replace uint4 SVM packing with structs
* Pass socket links and values in structs for each SVM node.
* Always pass default values along with stack offset, encoded in
  SVMInputFloat and SVMInputFloat3. For slightly more efficient
  memory access the stack offset is encoded in a NaN float.
* Simplify SVMCompiler API to use input_float, input_float3
  input_link and output functions, instead of manual stack
  assignment.

This was a leftover from early CUDA versions and old GPUs, where data
was stored in textures as there was no L1/L2 cache for global memory.

Assisted-by: Claude Opus 4.6
Co-authored-by: Sergey Sharybin <sergey@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/156612

intern/cycles/kernel/svm/geometry.h
@@ -8,6 +8,7 @@
 #include "kernel/geom/primitive.h"
 
 #include "kernel/svm/attribute.h"
+#include "kernel/svm/node_types.h"
 #include "kernel/svm/util.h"
 
 #include "util/hash.h"
@@ -19,7 +20,7 @@ CCL_NAMESPACE_BEGIN
 template<typename Float3Type>
 ccl_device_inline Float3Type svm_node_geometry_eval(KernelGlobals kg,
                                                     ccl_private ShaderData *sd,
-                                                    const uint type)
+                                                    const NodeGeometry type)
 {
   Float3Type data;
 
@@ -55,38 +56,31 @@ ccl_device_inline Float3Type svm_node_geometry_eval(KernelGlobals kg,
   return data;
 }
 
+template<typename Float3Type>
 ccl_device_noinline void svm_node_geometry(KernelGlobals kg,
                                            ccl_private ShaderData *sd,
-                                           ccl_private float *stack,
-                                           const uint4 node)
+                                           ccl_private float *ccl_restrict stack,
+                                           const ccl_global SVMNodeGeometry &ccl_restrict node)
 {
-  uint type, unused1, unused2;
-  svm_unpack_node_uchar3(node.y, &type, &unused1, &unused2);
-  stack_store(stack, node.z, svm_node_geometry_eval<float3>(kg, sd, type));
-}
+  Float3Type data = svm_node_geometry_eval<Float3Type>(kg, sd, node.geom_type);
 
-ccl_device_noinline void svm_node_geometry_derivative(KernelGlobals kg,
-                                                      ccl_private ShaderData *sd,
-                                                      ccl_private float *stack,
-                                                      const uint4 node)
-{
-  uint type, bump_offset, store_derivatives;
-  svm_unpack_node_uchar3(node.y, &type, &bump_offset, &store_derivatives);
-
-  /* Compute with derivatives, then apply first-order bump offset. */
-  dual3 data = svm_node_geometry_eval<dual3>(kg, sd, type);
-  const float bump_filter_width = __uint_as_float(node.w);
-  if (bump_offset == NODE_BUMP_OFFSET_DX) {
-    data.val += data.dx * bump_filter_width;
-  }
-  else if (bump_offset == NODE_BUMP_OFFSET_DY) {
-    data.val += data.dy * bump_filter_width;
-  }
-  if (store_derivatives) {
-    stack_store(stack, node.z, data);
+  if constexpr (is_dual_v<Float3Type>) {
+    /* Apply first-order bump offset. */
+    if (node.bump_offset == NODE_BUMP_OFFSET_DX) {
+      data.val += data.dx * node.bump_filter_width;
+    }
+    else if (node.bump_offset == NODE_BUMP_OFFSET_DY) {
+      data.val += data.dy * node.bump_filter_width;
+    }
+    if (node.store_derivatives) {
+      stack_store(stack, node.out_offset, data);
+    }
+    else {
+      stack_store(stack, node.out_offset, data.val);
+    }
   }
   else {
-    stack_store(stack, node.z, data.val);
+    stack_store(stack, node.out_offset, data);
   }
 }
 
@@ -94,19 +88,19 @@ ccl_device_noinline void svm_node_geometry_derivative(KernelGlobals kg,
 
 ccl_device_noinline void svm_node_object_info(KernelGlobals kg,
                                               ccl_private ShaderData *sd,
-                                              ccl_private float *stack,
-                                              const uint type,
-                                              const uint out_offset)
+                                              ccl_private float *ccl_restrict stack,
+                                              const ccl_global SVMNodeObjectInfo &ccl_restrict
+                                                  node)
 {
   float data;
 
-  switch (type) {
+  switch (node.info_type) {
     case NODE_INFO_OB_LOCATION: {
-      stack_store_float3(stack, out_offset, object_location(kg, sd));
+      stack_store_float3(stack, node.out_offset, object_location(kg, sd));
       return;
     }
     case NODE_INFO_OB_COLOR: {
-      stack_store_float3(stack, out_offset, object_color(kg, sd->object));
+      stack_store_float3(stack, node.out_offset, object_color(kg, sd->object));
       return;
     }
     case NODE_INFO_OB_ALPHA:
@@ -127,64 +121,64 @@ ccl_device_noinline void svm_node_object_info(KernelGlobals kg,
       break;
   }
 
-  stack_store_float(stack, out_offset, data);
+  stack_store_float(stack, node.out_offset, data);
 }
 
 /* Particle Info */
 
 ccl_device_noinline void svm_node_particle_info(KernelGlobals kg,
                                                 ccl_private ShaderData *sd,
-                                                ccl_private float *stack,
-                                                const uint type,
-                                                const uint out_offset)
+                                                ccl_private float *ccl_restrict stack,
+                                                const ccl_global SVMNodeParticleInfo &ccl_restrict
+                                                    node)
 {
-  switch ((NodeParticleInfo)type) {
+  switch (node.info_type) {
     case NODE_INFO_PAR_INDEX: {
       const int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float(stack, out_offset, particle_index(kg, particle_id));
+      stack_store_float(stack, node.out_offset, particle_index(kg, particle_id));
       break;
     }
     case NODE_INFO_PAR_RANDOM: {
       const int particle_id = object_particle_id(kg, sd->object);
       const float random = hash_uint2_to_float(particle_index(kg, particle_id), 0);
-      stack_store_float(stack, out_offset, random);
+      stack_store_float(stack, node.out_offset, random);
       break;
     }
     case NODE_INFO_PAR_AGE: {
       const int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float(stack, out_offset, particle_age(kg, particle_id));
+      stack_store_float(stack, node.out_offset, particle_age(kg, particle_id));
       break;
     }
     case NODE_INFO_PAR_LIFETIME: {
       const int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float(stack, out_offset, particle_lifetime(kg, particle_id));
+      stack_store_float(stack, node.out_offset, particle_lifetime(kg, particle_id));
       break;
     }
     case NODE_INFO_PAR_LOCATION: {
       const int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float3(stack, out_offset, particle_location(kg, particle_id));
+      stack_store_float3(stack, node.out_offset, particle_location(kg, particle_id));
       break;
     }
 #if 0 /* XXX float4 currently not supported in SVM stack */
     case NODE_INFO_PAR_ROTATION: {
       int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float4(stack, out_offset, particle_rotation(kg, particle_id));
+      stack_store_float4(stack, node.out_offset, particle_rotation(kg, particle_id));
       break;
     }
 #endif
     case NODE_INFO_PAR_SIZE: {
       const int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float(stack, out_offset, particle_size(kg, particle_id));
+      stack_store_float(stack, node.out_offset, particle_size(kg, particle_id));
       break;
     }
     case NODE_INFO_PAR_VELOCITY: {
       const int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float3(stack, out_offset, particle_velocity(kg, particle_id));
+      stack_store_float3(stack, node.out_offset, particle_velocity(kg, particle_id));
       break;
     }
     case NODE_INFO_PAR_ANGULAR_VELOCITY: {
       const int particle_id = object_particle_id(kg, sd->object);
-      stack_store_float3(stack, out_offset, particle_angular_velocity(kg, particle_id));
+      stack_store_float3(stack, node.out_offset, particle_angular_velocity(kg, particle_id));
       break;
     }
   }
@@ -196,17 +190,16 @@ ccl_device_noinline void svm_node_particle_info(KernelGlobals kg,
 
 ccl_device_noinline void svm_node_hair_info(KernelGlobals kg,
                                             ccl_private ShaderData *sd,
-                                            ccl_private float *stack,
-                                            const uint type,
-                                            const uint out_offset)
+                                            ccl_private float *ccl_restrict stack,
+                                            const ccl_global SVMNodeHairInfo &ccl_restrict node)
 {
   float data;
   float3 data3;
 
-  switch ((NodeHairInfo)type) {
+  switch (node.info_type) {
     case NODE_INFO_CURVE_IS_STRAND: {
       data = (sd->type & PRIMITIVE_CURVE) != 0;
-      stack_store_float(stack, out_offset, data);
+      stack_store_float(stack, node.out_offset, data);
       break;
     }
     case NODE_INFO_CURVE_INTERCEPT:
@@ -217,12 +210,12 @@ ccl_device_noinline void svm_node_hair_info(KernelGlobals kg,
       break; /* handled as attribute */
     case NODE_INFO_CURVE_THICKNESS: {
       data = curve_thickness(kg, sd);
-      stack_store_float(stack, out_offset, data);
+      stack_store_float(stack, node.out_offset, data);
       break;
     }
     case NODE_INFO_CURVE_TANGENT_NORMAL: {
       data3 = curve_tangent_normal(sd);
-      stack_store_float3(stack, out_offset, data3);
+      stack_store_float3(stack, node.out_offset, data3);
       break;
     }
   }
@@ -235,16 +228,15 @@ ccl_device_noinline void svm_node_hair_info(KernelGlobals kg,
 
 ccl_device_noinline void svm_node_point_info(KernelGlobals kg,
                                              ccl_private ShaderData *sd,
-                                             ccl_private float *stack,
-                                             const uint type,
-                                             const uint out_offset)
+                                             ccl_private float *ccl_restrict stack,
+                                             const ccl_global SVMNodePointInfo &ccl_restrict node)
 {
-  switch ((NodePointInfo)type) {
+  switch (node.info_type) {
     case NODE_INFO_POINT_POSITION:
-      stack_store_float3(stack, out_offset, point_position(kg, sd));
+      stack_store_float3(stack, node.out_offset, point_position(kg, sd));
       break;
     case NODE_INFO_POINT_RADIUS:
-      stack_store_float(stack, out_offset, point_radius(kg, sd));
+      stack_store_float(stack, node.out_offset, point_radius(kg, sd));
       break;
     case NODE_INFO_POINT_RANDOM:
       break; /* handled as attribute */

intern/cycles/scene/svm.h
@@ -40,7 +40,7 @@ class SVMShaderManager : public ShaderManager {
   void device_update_shader(Scene *scene,
                             Shader *shader,
                             Progress &progress,
-                            array<int4> *svm_nodes);
+                            array<int> *svm_nodes);
 };
 
 /* Graph Compiler */
@@ -76,44 +76,67 @@ class SVMCompiler {
   };
 
   SVMCompiler(Scene *scene, Progress &progress);
-  void compile(Shader *shader,
-               array<int4> &svm_nodes,
-               const int index,
-               Summary *summary = nullptr);
+  void compile(Shader *shader, array<int> &svm_nodes, const int index, Summary *summary = nullptr);
 
-  int stack_assign(ShaderOutput *output);
-  int stack_assign(ShaderInput *input);
-  bool is_linked(ShaderInput *input);
-  int stack_assign_if_linked(ShaderInput *input);
-  int stack_assign_if_linked(ShaderOutput *output);
-  int stack_assign_if_not_equal(ShaderInput *input, const float value);
-  int stack_assign_if_not_equal(ShaderInput *input, const float3 value);
-  int stack_find_offset(const int size);
-  int stack_find_offset(const ShaderIO *io);
-  void stack_clear_offset(const ShaderIO *io, const int offset);
+  /* Create input and output node parameters for struct T passed to add_node. */
+  SVMInputFloat input_float(const char *name);
+  SVMInputFloat3 input_float3(const char *name);
+  SVMInputFloat3 input_float3_from_offset(SVMStackOffset offset);
+  SVMStackOffset input_link(const char *name);
+  SVMStackOffset output(const char *name);
+
+  /* Add simple SVM node without parameters. */
+  void add_node(ShaderNodeType type);
+
+  /* Add SVM node with parameters in struct T. */
+  template<typename T>
+  void add_node(const ShaderNode *shader_node,
+                const ShaderNodeType type,
+                const T &node,
+                const bool use_derivatives = false)
+    requires(std::is_class_v<T> && sizeof(T) % sizeof(int) == 0 && alignof(T) <= sizeof(uint))
+  {
+    const ShaderNodeType resolved_type = node_type(shader_node, type, use_derivatives);
+    current_svm_nodes.push_back_slow(resolved_type);
+    const int *data = reinterpret_cast<const int *>(&node);
+    svm_node_types_used[resolved_type] = true;
+    for (size_t i = 0; i < sizeof(T) / sizeof(int); i++) {
+      current_svm_nodes.push_back_slow(data[i]);
+    }
+  }
+
+  /* Add value node. */
+  void add_value_node(const ShaderNode *shader_node, const float value, const int stack_offset);
+  void add_value_node(const ShaderNode *shader_node, const float3 &value, const int stack_offset);
+
+  /* Add extra node data following add_node. */
+  template<typename T>
+  void add_node_data(const T &data)
+    requires(std::is_class_v<T> && sizeof(T) % sizeof(int) == 0 && alignof(T) <= sizeof(uint))
+  {
+    const int *ptr = reinterpret_cast<const int *>(&data);
+    for (size_t i = 0; i < sizeof(T) / sizeof(int); i++) {
+      current_svm_nodes.push_back_slow(ptr[i]);
+    }
+  }
+  void add_node_data_float4(const float4 &f);
+  void add_node_data_float(const float f);
+
+  /* Low level input and output handling for some special nodes. Usually the functions
+   * above should be used instead of these. */
+  SVMStackOffset stack_assign(ShaderInput *input);
+  SVMStackOffset stack_find_offset(const ShaderIO *io);
+  void stack_clear_offset(const ShaderIO *io, const SVMStackOffset offset);
   void stack_link(ShaderInput *input, ShaderOutput *output);
 
-  void add_node(ShaderNodeType type, const int a = 0, const int b = 0, const int c = 0);
-  void add_node_derivative(const ShaderNodeType type,
-                           const bool need_derivatives,
-                           const int a = 0,
-                           const int b = 0,
-                           const int c = 0);
-  void add_node(const int a = 0, const int b = 0, const int c = 0, const int d = 0);
-  void add_node(const ShaderNode *node, const int a = 0, const int b = 0, const int c = 0);
-  void add_node(ShaderNodeType type, const float3 &f, const bool need_derivatives = 0);
-  void add_node(const float4 &f);
-  void add_value_node(const ShaderNode *node, const int value, const int stack_offset);
-  void add_value_node(const ShaderNode *node, const float3 &value, const int stack_offset);
   uint attribute(ustring name);
   uint attribute(AttributeStandard std);
   uint attribute_standard(ustring name);
-  uint encode_uchar4(const uint x, const uint y = 0, const uint z = 0, const uint w = 0);
-  uint closure_mix_weight_offset()
+  SVMStackOffset closure_mix_weight_offset()
   {
     return mix_weight_offset;
   }
-  uint get_bump_state_offset()
+  SVMStackOffset get_bump_state_offset()
   {
     return bump_state_offset;
   }
@@ -126,6 +149,7 @@ class SVMCompiler {
   Scene *scene;
   Progress &progress;
   ShaderGraph *current_graph;
+  ShaderNode *current_node;
   bool background;
 
  protected:
@@ -200,6 +224,13 @@ class SVMCompiler {
     uint node_feature_mask;
   };
 
+  ShaderNodeType node_type(const ShaderNode *shader_node,
+                           const ShaderNodeType type,
+                           const bool use_derivatives);
+
+  SVMStackOffset stack_assign(ShaderOutput *output);
+  SVMStackOffset stack_find_offset(const int size);
+
   void stack_clear_temporary(ShaderNode *node);
   int stack_size(SocketType::Type type);
   int stack_size(const ShaderIO *io);
@@ -230,13 +261,13 @@ class SVMCompiler {
   void compile_type(Shader *shader, ShaderGraph *graph, ShaderType type);
 
   std::atomic_int *svm_node_types_used;
-  array<int4> current_svm_nodes;
+  array<int> current_svm_nodes;
   ShaderType current_type;
   Shader *current_shader;
   Stack active_stack;
   int max_stack_use;
-  uint mix_weight_offset;
-  uint bump_state_offset;
+  SVMStackOffset mix_weight_offset;
+  SVMStackOffset bump_state_offset;
   bool compile_failed;
 };
 

intern/cycles/kernel/CMakeLists.txt
@@ -85,6 +85,7 @@ set(SRC_KERNEL_SVM_HEADERS
   svm/math.h
   svm/math_util.h
   svm/mix.h
+  svm/node_types.h
   svm/node_types_template.h
   svm/noise.h
   svm/noisetex.h

intern/cycles/kernel/data_arrays.h
@@ -71,7 +71,7 @@ KERNEL_DATA_ARRAY(uint, triangle_to_tree)
 KERNEL_DATA_ARRAY(KernelParticle, particles)
 
 /* shaders */
-KERNEL_DATA_ARRAY(uint4, svm_nodes)
+KERNEL_DATA_ARRAY(uint, svm_nodes)
 KERNEL_DATA_ARRAY(KernelShader, shaders)
 
 /* lookup tables */

intern/cycles/kernel/svm/ao.h
@@ -12,6 +12,7 @@
 
 #include "kernel/sample/mapping.h"
 
+#include "kernel/svm/node_types.h"
 #include "kernel/svm/util.h"
 
 CCL_NAMESPACE_BEGIN
@@ -104,42 +105,31 @@ ccl_device_noinline
     svm_node_ao(KernelGlobals kg,
                 ConstIntegratorGenericState state,
                 ccl_private ShaderData *sd,
-                ccl_private float *stack,
-                const uint4 node)
+                ccl_private float *ccl_restrict stack,
+                const ccl_global SVMNodeAmbientOcclusion &ccl_restrict node)
 {
-  uint flags;
-  uint dist_offset;
-  uint normal_offset;
-  uint out_ao_offset;
-  svm_unpack_node_uchar4(node.y, &flags, &dist_offset, &normal_offset, &out_ao_offset);
-
-  uint color_offset;
-  uint out_color_offset;
-  uint samples;
-  svm_unpack_node_uchar3(node.z, &color_offset, &out_color_offset, &samples);
-
   float ao = 1.0f;
 
   IF_KERNEL_NODES_FEATURE(RAYTRACE)
   {
-    float dist = stack_load_float_default(stack, dist_offset, node.w);
-    float3 normal = stack_valid(normal_offset) ? stack_load_float3(stack, normal_offset) : sd->N;
+    float dist = stack_load(stack, node.dist);
+    float3 normal = stack_load_float3_default(stack, node.normal_offset, sd->N);
     normal = safe_normalize(normal);
 
 #  ifdef __KERNEL_OPTIX__
-    ao = optixDirectCall<float>(0, kg, state, sd, normal, dist, samples, flags);
+    ao = optixDirectCall<float>(0, kg, state, sd, normal, dist, node.samples, node.flags);
 #  else
-    ao = svm_ao(kg, state, sd, normal, dist, samples, flags);
+    ao = svm_ao(kg, state, sd, normal, dist, node.samples, node.flags);
 #  endif
   }
 
-  if (stack_valid(out_ao_offset)) {
-    stack_store_float(stack, out_ao_offset, ao);
+  if (stack_valid(node.out_ao_offset)) {
+    stack_store_float(stack, node.out_ao_offset, ao);
   }
 
-  if (stack_valid(out_color_offset)) {
-    const float3 color = stack_load_float3(stack, color_offset);
-    stack_store_float3(stack, out_color_offset, ao * color);
+  if (stack_valid(node.out_color_offset)) {
+    const float3 color = stack_load(stack, node.color);
+    stack_store_float3(stack, node.out_color_offset, ao * color);
   }
 }
 

intern/cycles/kernel/svm/aov.h
@@ -6,6 +6,7 @@
 
 #include "kernel/film/aov_passes.h"
 
+#include "kernel/svm/node_types.h"
 #include "kernel/svm/util.h"
 
 CCL_NAMESPACE_BEGIN
@@ -23,8 +24,8 @@ template<uint node_feature_mask, typename ConstIntegratorGenericState>
 ccl_device void svm_node_aov_color(KernelGlobals kg,
                                    ccl_private ShaderData *sd,
                                    ConstIntegratorGenericState state,
-                                   ccl_private float *stack,
-                                   const uint4 node,
+                                   ccl_private float *ccl_restrict stack,
+                                   const ccl_global SVMNodeAOVColor &ccl_restrict node,
                                    ccl_global float *render_buffer)
 {
   IF_KERNEL_NODES_FEATURE(AOV)
@@ -34,8 +35,8 @@ ccl_device void svm_node_aov_color(KernelGlobals kg,
       return;
     }
 
-    const float3 val = stack_load_float3(stack, node.y);
-    film_write_aov_pass_color(kg, state, render_buffer, node.z, val);
+    const float3 val = stack_load(stack, node.color);
+    film_write_aov_pass_color(kg, state, render_buffer, node.aov_offset, val);
   }
 }
 
@@ -43,8 +44,8 @@ template<uint node_feature_mask, typename ConstIntegratorGenericState>
 ccl_device void svm_node_aov_value(KernelGlobals kg,
                                    ccl_private ShaderData *sd,
                                    ConstIntegratorGenericState state,
-                                   ccl_private float *stack,
-                                   const uint4 node,
+                                   ccl_private float *ccl_restrict stack,
+                                   const ccl_global SVMNodeAOVValue &ccl_restrict node,
                                    ccl_global float *render_buffer)
 {
   IF_KERNEL_NODES_FEATURE(AOV)
@@ -54,8 +55,8 @@ ccl_device void svm_node_aov_value(KernelGlobals kg,
       return;
     }
 
-    const float val = stack_load_float(stack, node.y);
-    film_write_aov_pass_value(kg, state, render_buffer, node.z, val);
+    const float val = stack_load(stack, node.value);
+    film_write_aov_pass_value(kg, state, render_buffer, node.aov_offset, val);
   }
 }
 CCL_NAMESPACE_END

blender/blender

commit 0e9da840ab5fe464d2a97308697afdaacf4bee3e

Fix: Cycles: Crash with kernel compilation without hair or SSS
Need to skip BSDF data properly then. Also better handle hair and point
info node in such cases.

Issue introduced in 7ee94c067c.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

intern/cycles/kernel/svm/closure.h
@@ -856,13 +856,12 @@ ccl_device
       }
       break;
     }
-#ifdef __HAIR__
-#  ifdef __PRINCIPLED_HAIR__
     case CLOSURE_BSDF_HAIR_CHIANG_ID:
     case CLOSURE_BSDF_HAIR_HUANG_ID: {
       const ccl_global SVMNodePrincipledHairBsdfData &hdata =
           svm_node_get<SVMNodePrincipledHairBsdfData>(kg, &offset);
 
+#if defined(__HAIR__) && defined(__PRINCIPLED_HAIR__)
       const Spectrum weight = closure_weight * mix_weight;
 
       const float alpha = stack_load(stack, hdata.offset);
@@ -1009,14 +1008,17 @@ ccl_device
           sd->runtime_flag |= bsdf_hair_huang_setup(sd, bsdf, path_flag);
         }
       }
+#else
+      (void)hdata;
+#endif
       break;
     }
-#  endif /* __PRINCIPLED_HAIR__ */
     case CLOSURE_BSDF_HAIR_REFLECTION_ID:
     case CLOSURE_BSDF_HAIR_TRANSMISSION_ID: {
       const ccl_global SVMNodeHairBsdfData &bsdf_data = svm_node_get<SVMNodeHairBsdfData>(kg,
                                                                                           &offset);
 
+#ifdef __HAIR__
       const Spectrum weight = closure_weight * mix_weight;
 
       ccl_private HairBsdf *bsdf = (ccl_private HairBsdf *)bsdf_alloc(
@@ -1046,17 +1048,19 @@ ccl_device
           sd->runtime_flag |= bsdf_hair_transmission_setup(bsdf);
         }
       }
+#else
+      (void)bsdf_data;
+#endif /* __HAIR__ */
 
       break;
     }
-#endif /* __HAIR__ */
 
-#ifdef __SUBSURFACE__
     case CLOSURE_BSSRDF_BURLEY_ID:
     case CLOSURE_BSSRDF_RANDOM_WALK_ID:
     case CLOSURE_BSSRDF_RANDOM_WALK_LEGACY_ID:
     case CLOSURE_BSSRDF_RANDOM_WALK_SKIN_ID: {
       const ccl_global SVMNodeBssrdfData &bsdf_data = svm_node_get<SVMNodeBssrdfData>(kg, &offset);
+#ifdef __SUBSURFACE__
       float3 N = stack_load_float3_default(stack, bsdf_data.normal_offset, sd->N);
       N = safe_normalize_fallback(N, sd->N);
 
@@ -1074,10 +1078,12 @@ ccl_device
 
         sd->runtime_flag |= bssrdf_setup(sd, bssrdf, path_flag, type);
       }
+#else
+      (void)bsdf_data;
+#endif
 
       break;
     }
-#endif
     default:
       /* Unknown closure type, skip the minimum data payload. */
       svm_node_get<SVMNodeSimpleBsdfData>(kg, &offset);

intern/cycles/kernel/svm/geometry.h
@@ -184,8 +184,6 @@ ccl_device_noinline void svm_node_particle_info(KernelGlobals kg,
   }
 }
 
-#ifdef __HAIR__
-
 /* Hair Info */
 
 ccl_device_noinline void svm_node_hair_info(KernelGlobals kg,
@@ -209,20 +207,27 @@ ccl_device_noinline void svm_node_hair_info(KernelGlobals kg,
     case NODE_INFO_CURVE_RANDOM:
       break; /* handled as attribute */
     case NODE_INFO_CURVE_THICKNESS: {
+#ifdef __HAIR__
       data = curve_thickness(kg, sd);
+#else
+      data = 0.0f;
+#endif
       stack_store_float(stack, node.out_offset, data);
       break;
     }
     case NODE_INFO_CURVE_TANGENT_NORMAL: {
+#ifdef __HAIR__
       data3 = curve_tangent_normal(sd);
+#else
+      data3 = zero_float3();
+#endif
       stack_store_float3(stack, node.out_offset, data3);
       break;
     }
   }
-}
-#endif
 
-#ifdef __POINTCLOUD__
+  (void)kg;
+}
 
 /* Point Info */
 
@@ -233,16 +238,25 @@ ccl_device_noinline void svm_node_point_info(KernelGlobals kg,
 {
   switch (node.info_type) {
     case NODE_INFO_POINT_POSITION:
+#ifdef __POINTCLOUD__
       stack_store_float3(stack, node.out_offset, point_position(kg, sd));
+#else
+      stack_store_float3(stack, node.out_offset, zero_float3());
+#endif
       break;
     case NODE_INFO_POINT_RADIUS:
+#ifdef __POINTCLOUD__
       stack_store_float(stack, node.out_offset, point_radius(kg, sd));
+#else
+      stack_store_float(stack, node.out_offset, 0.0f);
+#endif
       break;
     case NODE_INFO_POINT_RANDOM:
       break; /* handled as attribute */
   }
-}
 
-#endif
+  (void)kg;
+  (void)sd;
+}
 
 CCL_NAMESPACE_END

intern/cycles/kernel/svm/svm.h
@@ -465,16 +465,12 @@ ccl_device void svm_eval_nodes(KernelGlobals kg,
       SVM_CASE(NODE_PARTICLE_INFO)
       svm_node_particle_info(kg, sd, stack, svm_node_get<SVMNodeParticleInfo>(kg, &offset));
       break;
-#if defined(__HAIR__)
       SVM_CASE(NODE_HAIR_INFO)
       svm_node_hair_info(kg, sd, stack, svm_node_get<SVMNodeHairInfo>(kg, &offset));
       break;
-#endif
-#if defined(__POINTCLOUD__)
       SVM_CASE(NODE_POINT_INFO)
       svm_node_point_info(kg, sd, stack, svm_node_get<SVMNodePointInfo>(kg, &offset));
       break;
-#endif
       SVM_CASE(NODE_TEXTURE_MAPPING)
       svm_node_texture_mapping<float3>(stack, svm_node_get<SVMNodeTextureMapping>(kg, &offset));
       break;