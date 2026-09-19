blender/blender

commit a6ac1efb508035b794a4b4dc801822f7320a4965

Core: initial support for stable .blend files
The goal of this patch is to get to a state where opening a .blend file and then
saving it again can result in the byte-per-byte identical .blend file. While
that in itself is not all that useful, it helps indirectly with compression and
potentially other tools build for .blend files in the future. This is basically
an extension to previous work on stable pointers: #127729.

The main thing this patch fixes is that run-time data is more consistently reset
before the file is written. That was already done in a few places, but always
fairly ad-hoc with a custom temporary copy of the data. This patch extends the
`BlendStructWriter` API to make it easy to reset data before it is written to
the file and without having to adjust the original data.

Testing this with a specific .blend file is easiest when
`GENERATE_DEBUG_BLEND_FILE` is set to `1` in `writefile.cc`. This outputs a
human readable version whenever writing a .blend file. Diffing these text files
shows which data needs to be made stable.

While this fixes a large number of places, there are still a few files that are
not stable. Especially when opening a file in the GUI, UI data changes easily
without actually doing much.

I mostly tested this with all our regression test files with a script. The
script does the following:
1. Open the .blend file, then save it to a new location.
2. Open that saved file and save it to yet another location.
3. Same thing again.

These steps give a more accurate picture because sometimes, due to versioning
etc. stuff happens in the first save that does not happen the second time
around, especially for old files. Here is the summary of running that over all
test files:
```
A: First saved file.
B: Second saved file.
C: Third saved file.

A == B == C: 1.107 files
A != B == C:   652 files
A != B != C:   251 files
Other error:    31 files
```

As we can see, the majority of files become stable with this patch (at least

source/blender/blenkernel/intern/image.cc
@@ -414,7 +414,9 @@ static void image_blend_write(BlendWriter *writer, ID *id, const void *id_addres
   }
   writer->write_struct(ima->stereo3d_format);
 
-  writer->write_struct_list(&ima->tiles);
+  writer->write_struct_list(&ima->tiles, [](BlendStructWriter<ImageTile> &struct_writer) {
+    struct_writer.shallow_data.runtime = {};
+  });
 
   ima->packedfile = nullptr;
 

source/blender/blenkernel/intern/action.cc
@@ -309,7 +309,16 @@ static void action_foreach_id(ID *id, LibraryForeachIDData *data)
 
 static void write_channelbag(BlendWriter *writer, animrig::Channelbag &channelbag)
 {
-  writer->write_struct_cast<ActionChannelbag>(&channelbag);
+  writer->write_struct_cast<ActionChannelbag>(
+      &channelbag, [](BlendStructWriter<ActionChannelbag> &struct_writer) {
+        ActionChannelbag &shallow_bag = struct_writer.shallow_data;
+        if (shallow_bag.group_array_num == 0) {
+          shallow_bag.group_array = nullptr;
+        }
+        if (shallow_bag.fcurve_array_num == 0) {
+          shallow_bag.fcurve_array = nullptr;
+        }
+      });
 
   Span<bActionGroup *> groups = channelbag.channel_groups();
   writer->write_pointer_array(groups.size(), groups.data());
@@ -320,8 +329,8 @@ static void write_channelbag(BlendWriter *writer, animrig::Channelbag &channelba
   Span<FCurve *> fcurves = channelbag.fcurves();
   writer->write_pointer_array(fcurves.size(), fcurves.data());
   for (FCurve *fcurve : fcurves) {
-    writer->write_struct(fcurve, [](BlendStructWriter &struct_writer) {
-      struct_writer.runtime_ptr(offsetof(FCurve, runtime));
+    writer->write_struct(fcurve, [](BlendStructWriter<FCurve> &struct_writer) {
+      struct_writer.shallow_data.runtime = nullptr;
     });
     BKE_fcurve_blend_write_data(writer, fcurve);
   }
@@ -373,12 +382,9 @@ static void write_slots(BlendWriter *writer, Span<animrig::Slot *> slots)
 {
   writer->write_pointer_array(slots.size(), slots.data());
   for (animrig::Slot *slot : slots) {
-    /* Make a shallow copy using the C type, so that no new runtime struct is
-     * allocated for the copy. */
-    ActionSlot shallow_copy = *slot;
-    shallow_copy.runtime = nullptr;
-
-    writer->write_struct_at_address(slot, &shallow_copy);
+    writer->write_struct_cast<ActionSlot>(slot, [](BlendStructWriter<ActionSlot> &struct_writer) {
+      struct_writer.shallow_data.runtime = nullptr;
+    });
   }
 }
 
@@ -1903,7 +1909,9 @@ void BKE_pose_blend_write(BlendWriter *writer, bPose *pose)
 
     bke::motionpath::blend_write(writer, chan.mpath);
 
-    writer->write_struct(&chan);
+    writer->write_struct(&chan, [](BlendStructWriter<bPoseChannel> &struct_writer) {
+      struct_writer.shallow_data.runtime = {};
+    });
   }
 
   /* Write groups */
@@ -1920,8 +1928,8 @@ void BKE_pose_blend_write(BlendWriter *writer, bPose *pose)
   }
 
   /* Write this pose */
-  writer->write_struct(pose, [](BlendStructWriter &struct_writer) {
-    struct_writer.runtime_ptr(offsetof(bPose, runtime));
+  writer->write_struct(pose, [](BlendStructWriter<bPose> &struct_writer) {
+    struct_writer.shallow_data.runtime = nullptr;
   });
 }
 

source/blender/blenkernel/intern/attribute_storage.cc
@@ -709,7 +709,7 @@ void AttributeStorage::blend_write(BlendWriter &writer,
   writer.write_struct_array_by_name("Attribute",
                                     write_data.attributes.size(),
                                     write_data.attributes.data(),
-                                    [](BlendStructWriter &struct_writer) {
+                                    [](BlendStructWriterVoid &struct_writer) {
                                       struct_writer.generated_ptr(
                                           offsetof(blender::Attribute, data));
                                     });

source/blender/blenkernel/intern/camera.cc
@@ -217,7 +217,9 @@ static void camera_blend_write(BlendWriter *writer, ID *id, const void *id_addre
   }
 
   /* write LibData */
-  writer->write_id_struct(id_address, cam);
+  writer->write_id_struct(id_address, cam, [](BlendStructWriter<Camera> &struct_writer) {
+    struct_writer.shallow_data.runtime = {};
+  });
   BKE_id_blend_write(writer, &cam->id);
 
   for (CameraBGImage &bgpic : cam->bg_images) {

source/blender/blenkernel/intern/colortools.cc
@@ -1507,7 +1507,23 @@ int BKE_curvemapping_num_channels(const CurveMapping *cumap)
 
 void BKE_curvemapping_blend_write(BlendWriter *writer, const CurveMapping *cumap)
 {
-  writer->write_struct(cumap);
+  writer->write_struct(cumap, [writer](BlendStructWriter<CurveMapping> &struct_writer) {
+    CurveMapping &shallow_cumap = struct_writer.shallow_data;
+    if (!writer->is_undo()) {
+      shallow_cumap.changed_timestamp = 0;
+    }
+    for (const int i : IndexRange(CM_TOT)) {
+      shallow_cumap.cm[i].range = 0;
+      shallow_cumap.cm[i].mintable = 0;
+      shallow_cumap.cm[i].maxtable = 0;
+      zero_v2(shallow_cumap.cm[i].ext_in);
+      zero_v2(shallow_cumap.cm[i].ext_out);
+      shallow_cumap.cm[i].table = nullptr;
+      shallow_cumap.cm[i].premultable = nullptr;
+      zero_v2(shallow_cumap.cm[i].premul_ext_in);
+      zero_v2(shallow_cumap.cm[i].premul_ext_out);
+    }
+  });
   BKE_curvemapping_curves_blend_write(writer, cumap);
 }
 

source/blender/blenkernel/intern/curveprofile.cc
@@ -85,7 +85,11 @@ CurveProfile *BKE_curveprofile_copy(const CurveProfile *profile)
 
 void BKE_curveprofile_blend_write(BlendWriter *writer, const CurveProfile *profile)
 {
-  writer->write_struct(profile);
+  writer->write_struct(profile, [&](BlendStructWriter<CurveProfile> &struct_writer) {
+    if (!writer->is_undo()) {
+      struct_writer.shallow_data.changed_timestamp = 0;
+    }
+  });
   writer->write_struct_array(profile->path_len, profile->path);
 }
 

source/blender/blenkernel/intern/curves.cc
@@ -118,7 +118,7 @@ static void curves_blend_write(BlendWriter *writer, ID *id, const void *id_addre
   curves->geometry.wrap().blend_write_prepare(write_data, !writer->is_undo());
 
   /* Write LibData */
-  writer->write_id_struct(id_address, curves, [](BlendStructWriter &struct_writer) {
+  writer->write_id_struct(id_address, curves, [](BlendStructWriter<Curves> &struct_writer) {
     struct_writer.generated_ptr(offsetof(Curves, geometry.attribute_storage.dna_attributes));
   });
   BKE_id_blend_write(writer, &curves->id);

source/blender/blenkernel/intern/fcurve.cc
@@ -2648,8 +2648,8 @@ void BKE_fcurve_blend_write_data(BlendWriter *writer, FCurve *fcu)
 
 void BKE_fcurve_blend_write_listbase(BlendWriter *writer, ListBaseT<FCurve> *fcurves)
 {
-  writer->write_struct_list(fcurves, [](BlendStructWriter &struct_writer) {
-    struct_writer.runtime_ptr(offsetof(FCurve, runtime));
+  writer->write_struct_list(fcurves, [](BlendStructWriter<FCurve> &struct_writer) {
+    struct_writer.shallow_data.runtime = nullptr;
   });
   for (FCurve &fcu : *fcurves) {
     BKE_fcurve_blend_write_data(writer, &fcu);

commit d75a414c1f6b2947985700258e5e01f84ac0ad23

Fix: Texture Paint: UDIM image issues on undo
Don't clear the image runtime cache on undo for stable blend files,
it should persist UDIM atlas info along with other image buffer data
across undo steps..

Issue introduced in a6ac1efb508.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/blenkernel/intern/image.cc
@@ -414,8 +414,10 @@ static void image_blend_write(BlendWriter *writer, ID *id, const void *id_addres
   }
   writer->write_struct(ima->stereo3d_format);
 
-  writer->write_struct_list(&ima->tiles, [](BlendStructWriter<ImageTile> &struct_writer) {
-    struct_writer.shallow_data.runtime = {};
+  writer->write_struct_list(&ima->tiles, [&](BlendStructWriter<ImageTile> &struct_writer) {
+    if (!is_undo) {
+      struct_writer.shallow_data.runtime = {};
+    }
   });
 
   ima->packedfile = nullptr;