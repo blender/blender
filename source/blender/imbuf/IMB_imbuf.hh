blender/blender

commit 3bd35d9a4b22493519079d808bf70eecaf523fdc

ImBuf: Implicit sharing for CPU buffers
The current mechanisms to avoid copying ImBuf CPU buffers either do not
work in some situations, are fragile, or add too much code complexity
compared to implicit sharing / copy-on-write. This PR implements those
features for the float and byte CPU buffers. The existing "refcounter"
remains, in order to simplify the change, and because it isn't clear how
it should interact with GPU textures. Implicit sharing is added to the
file output compositor node and replaces the need for "externally owned"
semantics in the compositor's "Result" class, at least for the CPU
buffers. In that class, switching to ImplicitSharingPtr for the RAII
features made the change simpler.

Copying `ImBuf` is now quite cheap, and it should be possible to benefit
from this to simplify the code further in the future. For now, this is
used to simplify the image undo tile and paint tile implementations.

Pull Request: https://projects.blender.org/blender/blender/pulls/157645

source/blender/imbuf/IMB_imbuf.hh
@@ -152,15 +152,6 @@ ImBuf *IMB_allocFromBuffer(const uint8_t *byte_buffer,
                            unsigned int channels);
 
 /**
- * Assign the content of the corresponding buffer with the given data and ownership.
- * The current content of the buffer is released corresponding to its ownership configuration.
- *
- * \note Does not modify the topology (width, height, number of channels).
- */
-void IMB_assign_byte_buffer(ImBuf *ibuf, uint8_t *buffer_data, ImBufOwnership ownership);
-void IMB_assign_float_buffer(ImBuf *ibuf, float *buffer_data, ImBufOwnership ownership);
-
-/**
  * Assign the GPU texture of the buffer to the given texture. The current GPU texture is released.
  *
  * \note Does not modify the topology (width, height, number of channels).
@@ -175,33 +166,6 @@ void IMB_assign_gpu_texture(ImBuf *ibuf, gpu::Texture *texture);
  * \warning Not thread-safe, so callee should worry about thread locks.
  */
 void IMB_ensure_host_buffer(ImBuf *ibuf);
-
-/**
- * Assign the content and the color space of the corresponding buffer the data from the given
- * buffer.
- *
- * \note Does not modify the topology (width, height, number of channels).
- *
- * \note The ownership of the data in the source buffer is ignored.
- */
-void IMB_assign_byte_buffer(ImBuf *ibuf, const ImBufByteBuffer &buffer, ImBufOwnership ownership);
-void IMB_assign_float_buffer(ImBuf *ibuf,
-                             const ImBufFloatBuffer &buffer,
-                             ImBufOwnership ownership);
-
-/**
- * Make corresponding buffers available for modification.
- * Is achieved by ensuring that the given ImBuf is the only owner of the underlying buffer data.
- */
-void IMB_make_writable_byte_buffer(ImBuf *ibuf);
-void IMB_make_writable_float_buffer(ImBuf *ibuf);
-
-/**
- * Steal the buffer data pointer: the ImBuf is no longer an owner of this data.
- * \note If the ImBuf does not own the data the behavior is undefined.
- */
-uint8_t *IMB_steal_byte_buffer(ImBuf *ibuf);
-float *IMB_steal_float_buffer(ImBuf *ibuf);
 
 /**
  * Increase reference count to imbuf

source/blender/imbuf/intern/allocimbuf.cc
@@ -36,26 +36,6 @@ namespace blender {
 
 static CLG_LogRef LOG = {"image.buffer"};
 
-/* Free the specified buffer storage, freeing memory when needed and restoring the state of the
- * buffer to its defaults. */
-template<class BufferType> static void imb_free_buffer(BufferType &buffer)
-{
-  if (buffer.data) {
-    switch (buffer.ownership) {
-      case IB_DO_NOT_TAKE_OWNERSHIP:
-        break;
-
-      case IB_TAKE_OWNERSHIP:
-        MEM_delete(buffer.data);
-        break;
-    }
-  }
-
-  /* Reset buffer to defaults. */
-  buffer.data = nullptr;
-  buffer.ownership = IB_DO_NOT_TAKE_OWNERSHIP;
-}
-
 /* Allocate pixel storage of the given buffer. The buffer owns the allocated memory.
  * Returns true of allocation succeeded, false otherwise. */
 template<class BufferType>
@@ -66,60 +46,51 @@ bool imb_alloc_buffer(BufferType &buffer,
                       const size_t type_size,
                       bool initialize_pixels)
 {
-  buffer.data = static_cast<decltype(BufferType::data)>(
-      imb_alloc_pixels(x, y, channels, type_size, initialize_pixels, __func__));
-  if (!buffer.data) {
+  void *data = imb_alloc_pixels(x, y, channels, type_size, initialize_pixels, __func__);
+  if (!data) {
     return false;
   }
-
-  buffer.ownership = IB_TAKE_OWNERSHIP;
-
+  buffer.data = static_cast<decltype(BufferType::data)>(data);
+  buffer.sharing_info = ImplicitSharingPtr<>(implicit_sharing::info_for_mem_free(data));
   return true;
 }
 
-/* Make the buffer available for modification.
- * Is achieved by ensuring that the buffer is the only owner of its data. */
-template<class BufferType> void imb_make_writeable_buffer(BufferType &buffer)
+uint8_t *ImBuf::byte_data_for_write()
 {
-  if (!buffer.data) {
-    return;
-  }
-
-  switch (buffer.ownership) {
-    case IB_DO_NOT_TAKE_OWNERSHIP:
-      buffer.data = MEM_dupalloc(buffer.data);
-      buffer.ownership = IB_TAKE_OWNERSHIP;
-
-    case IB_TAKE_OWNERSHIP:
-      break;
-  }
-}
-
-template<class BufferType>
-auto imb_steal_buffer_data(BufferType &buffer) -> decltype(BufferType::data)
-{
-  if (!buffer.data) {
+  if (!this->byte_buffer.data) {
     return nullptr;
   }
-
-  switch (buffer.ownership) {
-    case IB_DO_NOT_TAKE_OWNERSHIP:
-      BLI_assert_msg(false, "Unexpected behavior: stealing non-owned data pointer");
-      return nullptr;
-
-    case IB_TAKE_OWNERSHIP: {
-      decltype(BufferType::data) data = buffer.data;
-
-      buffer.data = nullptr;
-      buffer.ownership = IB_DO_NOT_TAKE_OWNERSHIP;
-
-      return data;
-    }
+  if (this->byte_buffer.sharing_info->is_mutable()) {
+    this->byte_buffer.sharing_info->tag_ensured_mutable();
   }
+  else {
+    const size_t size = size_t(this->x) * size_t(this->y) * 4;
+    uint8_t *new_data = MEM_new_array_uninitialized<uint8_t>(size, __func__);
+    std::copy_n(this->byte_buffer.data, size, new_data);
+    this->byte_buffer.data = new_data;
+    this->byte_buffer.sharing_info = ImplicitSharingPtr<>(
+        implicit_sharing::info_for_mem_free(new_data));
+  }
+  return const_cast<uint8_t *>(this->byte_buffer.data);
+}
 
-  BLI_assert_unreachable();
-
-  return nullptr;
+float *ImBuf::float_data_for_write()
+{
+  if (!this->float_buffer.data) {
+    return nullptr;
+  }
+  if (this->float_buffer.sharing_info->is_mutable()) {
+    this->float_buffer.sharing_info->tag_ensured_mutable();
+  }
+  else {
+    const size_t size = size_t(this->x) * size_t(this->y) * this->channels;
+    float *new_data = MEM_new_array_uninitialized<float>(size, __func__);
+    std::copy_n(this->float_buffer.data, size, new_data);
+    this->float_buffer.data = new_data;
+    this->float_buffer.sharing_info = ImplicitSharingPtr<>(
+        implicit_sharing::info_for_mem_free(new_data));
+  }
+  return const_cast<float *>(this->float_buffer.data);
 }
 
 void IMB_free_float_pixels(ImBuf *ibuf)
@@ -127,7 +98,7 @@ void IMB_free_float_pixels(ImBuf *ibuf)
   if (ibuf == nullptr) {
     return;
   }
-  imb_free_buffer(ibuf->float_buffer);
+  ibuf->float_buffer = {};
 }
 
 void IMB_free_byte_pixels(ImBuf *ibuf)
@@ -135,7 +106,7 @@ void IMB_free_byte_pixels(ImBuf *ibuf)
   if (ibuf == nullptr) {
     return;
   }
-  imb_free_buffer(ibuf->byte_buffer);
+  ibuf->byte_buffer = {};
 }
 
 void IMB_free_all_data(ImBuf *ibuf)
@@ -241,7 +212,7 @@ bool IMB_alloc_byte_pixels(ImBuf *ibuf, bool initialize_pixels)
     return false;
   }
 
-  imb_free_buffer(ibuf->byte_buffer);
+  ibuf->byte_buffer = {};
 
   if (!imb_alloc_buffer(
           ibuf->byte_buffer, ibuf->x, ibuf->y, 4, sizeof(uint8_t), initialize_pixels))
@@ -254,19 +225,21 @@ bool IMB_alloc_byte_pixels(ImBuf *ibuf, bool initialize_pixels)
 
 void ImBuf::assign_byte_data(uint8_t *data)
 {
-  imb_free_buffer(this->byte_buffer);
+  this->byte_buffer = {};
   if (data) {
     this->byte_buffer.data = data;
-    this->byte_buffer.ownership = IB_TAKE_OWNERSHIP;
+    this->byte_buffer.sharing_info = ImplicitSharingPtr<>(
+        implicit_sharing::info_for_mem_free(data));
   }
 }
 
 void ImBuf::assign_float_data(float *data)
 {
-  imb_free_buffer(this->float_buffer);
+  this->float_buffer = {};
   if (data) {
     this->float_buffer.data = data;
-    this->float_buffer.ownership = IB_TAKE_OWNERSHIP;
+    this->float_buffer.sharing_info = ImplicitSharingPtr<>(
+        implicit_sharing::info_for_mem_free(data));
   }
 }
 
@@ -278,42 +251,20 @@ bool ImBuf::colorspace_is_data() const
   return this->byte_buffer.colorspace && this->byte_buffer.colorspace->is_data();
 }
 
-uint8_t *IMB_steal_byte_buffer(ImBuf *ibuf)
+void ImBuf::assign_byte_data(const uint8_t *data, ImplicitSharingPtr<> sharing_ptr)
 {
-  return imb_steal_buffer_data(ibuf->byte_buffer);
+  BLI_assert(data != nullptr);
+  BLI_assert(sharing_ptr.get() != nullptr);
+  this->byte_buffer.data = data;
+  this->byte_buffer.sharing_info = std::move(sharing_ptr);
 }
 
-float *IMB_steal_float_buffer(ImBuf *ibuf)
+void ImBuf::assign_float_data(const float *data, ImplicitSharingPtr<> sharing_ptr)
 {
-  return imb_steal_buffer_data(ibuf->float_buffer);
-}
-
-void IMB_make_writable_byte_buffer(ImBuf *ibuf)
-{
-  imb_make_writeable_buffer(ibuf->byte_buffer);
-}
-
-void IMB_make_writable_float_buffer(ImBuf *ibuf)
-{
-  imb_make_writeable_buffer(ibuf->float_buffer);
-}
-
-void IMB_assign_byte_buffer(ImBuf *ibuf, uint8_t *buffer_data, const ImBufOwnership ownership)
-{
-  imb_free_buffer(ibuf->byte_buffer);
-  if (buffer_data) {
-    ibuf->byte_buffer.data = buffer_data;
-    ibuf->byte_buffer.ownership = ownership;
-  }
-}
-
-void IMB_assign_float_buffer(ImBuf *ibuf, float *buffer_data, const ImBufOwnership ownership)
-{
-  imb_free_buffer(ibuf->float_buffer);
-  if (buffer_data) {
-    ibuf->float_buffer.data = buffer_data;
-    ibuf->float_buffer.ownership = ownership;
-  }
+  BLI_assert(data != nullptr);
+  BLI_assert(sharing_ptr.get() != nullptr);
+  this->float_buffer.data = data;
+  this->float_buffer.sharing_info = std::move(sharing_ptr);
 }
 
 void IMB_assign_gpu_texture(ImBuf *ibuf, gpu::Texture *texture)
@@ -347,22 +298,6 @@ void IMB_ensure_host_buffer(ImBuf *ibuf)
   if (need_secondary_context) {
     IMB_deactivate_gpu_context();
   }
-}
-
-void IMB_assign_byte_buffer(ImBuf *ibuf,
-                            const ImBufByteBuffer &buffer,
-                            const ImBufOwnership ownership)
-{
-  IMB_assign_byte_buffer(ibuf, buffer.data, ownership);
-  ibuf->byte_buffer.colorspace = buffer.colorspace;
-}
-
-void IMB_assign_float_buffer(ImBuf *ibuf,
-                             const ImBufFloatBuffer &buffer,
-                             const ImBufOwnership ownership)
-{
-  IMB_assign_float_buffer(ibuf, buffer.data, ownership);
-  ibuf->float_buffer.colorspace = buffer.colorspace;
 }
 
 ImBuf *IMB_allocFromBufferOwn(
@@ -491,18 +426,8 @@ ImBuf *IMB_dupImBuf(const ImBuf *ibuf1)
   ibuf2->color_mode = ibuf1->color_mode;
   ibuf2->channels = ibuf1->channels;
   ibuf2->flags = ibuf1->flags;
-  if (const float *src_buffer = ibuf1->float_data()) {
-    IMB_alloc_float_pixels(ibuf2, ibuf1->channels, false);
-    memcpy(ibuf2->float_data_for_write(),
-           src_buffer,
-           sizeof(float) * ibuf1->channels * ibuf1->x * ibuf1->y);
-    ibuf2->float_buffer.colorspace = ibuf1->float_buffer.colorspace;
-  }
-  if (const uint8_t *src_buffer = ibuf1->byte_data()) {
-    IMB_alloc_byte_pixels(ibuf2, false);
-    memcpy(ibuf2->byte_data_for_write(), src_buffer, sizeof(uint8_t) * 4 * ibuf1->x * ibuf1->y);
-    ibuf2->byte_buffer.colorspace = ibuf1->byte_buffer.colorspace;
-  }
+  ibuf2->byte_buffer = ibuf1->byte_buffer;
+  ibuf2->float_buffer = ibuf1->float_buffer;
   /* GPU textures can not be easily copied, as it is not guaranteed that this function is called
    * from within an active GPU context. */
   ibuf2->gpu.texture = nullptr;

source/blender/render/intern/compositor.cc
@@ -193,12 +193,20 @@ class Context : public compositor::Context {
         image_buffer->assign_float_data(output_buffer);
       }
       else {
-        float *data = MEM_new_array_uninitialized<float>(
-            4 * size_t(render_result->rectx) * size_t(render_result->recty), __func__);
-        image_buffer->assign_float_data(data);
-        std::memcpy(image_buffer->float_data_for_write(),
-                    result.cpu_data().data(),
-                    render_result->rectx * render_result->recty * 4 * sizeof(float));
+        if (result.sharing_info()) {
+          image_buffer->float_buffer = ImBufFloatBuffer{
+              .data = static_cast<const float *>(result.cpu_data().data()),
+              .sharing_info = result.sharing_info(),
+              .colorspace = nullptr};
+        }
+        else {
+          float *data = MEM_new_array_uninitialized<float>(
+              4 * size_t(render_result->rectx) * size_t(render_result->recty), __func__);
+          std::memcpy(image_buffer->float_data_for_write(),
+                      result.cpu_data().data(),
+                      render_result->rectx * render_result->recty * 4 * sizeof(float));
+          image_buffer->assign_float_data(data);
+        }
       }
     }
     RE_ReleaseResult(render);
@@ -267,11 +275,6 @@ class Context : public compositor::Context {
     else {
       /* If not using GPU, free any potential previous GPU data. */
       IMB_free_gpu_textures(image_buffer);
-
-      /* Allocate float buffer if not using GPU and no float buffer exists. */
-      if (!image_buffer->float_data()) {
-        IMB_alloc_float_pixels(image_buffer, 4, false);
-      }
     }
 
     if (this->use_gpu()) {
@@ -287,9 +290,18 @@ class Context : public compositor::Context {
     }
     else {
       if (viewer_result.is_single_value()) {
+        IMB_alloc_float_pixels(image_buffer, 4, false);
         IMB_rectfill(image_buffer, viewer_result.get_single_value<compositor::Color>());
       }
-      else {
+      else if (viewer_result.sharing_info()) {
+        image_buffer->channels = 4;
+        image_buffer->float_buffer = ImBufFloatBuffer{
+            .data = static_cast<const float *>(viewer_result.cpu_data().data()),
+            .sharing_info = viewer_result.sharing_info(),
+            .colorspace = nullptr};
+      }
+      else if (viewer_result.cpu_data().data() != image_buffer->float_data()) {
+        IMB_alloc_float_pixels(image_buffer, 4, false);
         std::memcpy(image_buffer->float_data_for_write(),
                     viewer_result.cpu_data().data(),
                     size.x * size.y * 4 * sizeof(float));
@@ -455,8 +467,9 @@ class Context : public compositor::Context {
     else {
       /* Don't assume render will keep pass data stored, add our own reference. */
       IMB_refImBuf(render_pass->ibuf);
-      pass_data.share_data(render_pass->ibuf->float_data_for_write(),
-                           int2(render_pass->ibuf->x, render_pass->ibuf->y));
+      pass_data.share_data(render_pass->ibuf->float_buffer.data,
+                           int2(render_pass->ibuf->x, render_pass->ibuf->y),
+                           render_pass->ibuf->float_buffer.sharing_info);
       cached_cpu_passes_.append(render_pass->ibuf);
     }
 

source/blender/sequencer/intern/compositor.cc
@@ -141,14 +141,22 @@ void CompositorContext::write_output(const compositor::Result &result, ImBuf &im
     IMB_free_all_data(&image);
     image.x = output_size_x;
     image.y = output_size_y;
-    IMB_alloc_float_pixels(&image, 4, false);
   }
 
   if (this->use_gpu()) {
     GPU_memory_barrier(GPU_BARRIER_TEXTURE_UPDATE);
+    IMB_alloc_float_pixels(&image, 4, false);
     GPU_texture_read(result.gpu_texture(), GPU_DATA_FLOAT, 0, image.float_data_for_write());
   }
-  else {
+  else if (result.sharing_info()) {
+    image.channels = 4;
+    image.float_buffer = ImBufFloatBuffer{
+        .data = static_cast<const float *>(result.cpu_data().data()),
+        .sharing_info = result.sharing_info(),
+        .colorspace = nullptr};
+  }
+  else if (result.cpu_data().data() != image.float_data()) {
+    IMB_alloc_float_pixels(&image, 4, false);
     std::memcpy(image.float_data_for_write(),
                 result.cpu_data().data(),
                 IMB_get_pixel_count(&image) * sizeof(float) * 4);

source/blender/compositor/cached_resources/intern/cached_image.cc
@@ -200,15 +200,13 @@ static ImBuf *compute_linear_buffer(ImBuf *image_buffer)
   linear_image_buffer->flags = image_buffer->flags;
 
   /* Assign the float buffer if it exists, as well as its number of channels. */
-  IMB_assign_float_buffer(
-      linear_image_buffer, image_buffer->float_buffer, IB_DO_NOT_TAKE_OWNERSHIP);
+  linear_image_buffer->float_buffer = image_buffer->float_buffer;
   linear_image_buffer->channels = image_buffer->channels;
 
   /* If no float buffer exists, assign it then compute a float buffer from it. This is the main
    * call of this function. */
   if (!linear_image_buffer->float_data()) {
-    IMB_assign_byte_buffer(
-        linear_image_buffer, image_buffer->byte_buffer, IB_DO_NOT_TAKE_OWNERSHIP);
+    linear_image_buffer->byte_buffer = image_buffer->byte_buffer;
     IMB_float_from_byte(linear_image_buffer);
   }
 
@@ -340,7 +338,9 @@ CachedImage::CachedImage(Context &context,
   else {
     const int2 size = int2(image_buffer->x, image_buffer->y);
     Result buffer_result(context, float_type(image_buffer->channels), ResultPrecision::Full);
-    buffer_result.share_data(linear_image_buffer->float_data(), size);
+    buffer_result.share_data(linear_image_buffer->float_buffer.data,
+                             size,
+                             linear_image_buffer->float_buffer.sharing_info);
     this->result.allocate_texture(size, false);
 
     if (buffer_result.type() == ResultType::Color && result.type() == ResultType::Float4) {

source/blender/compositor/intern/render_context.cc
@@ -98,9 +98,18 @@ void FileOutput::add_view(const char *view_name, const Result &data)
   }
   render_view->ibuf = IMB_allocImBuf(UNPACK2(data.domain().data_size), ImBufFlags::Zero);
   render_view->ibuf->color_mode = color_mode;
-  IMB_alloc_float_pixels(render_view->ibuf, data.channels_count(), false);
-  std::memcpy(
-      render_view->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  if (data.sharing_info()) {
+    render_view->ibuf->channels = data.channels_count();
+    render_view->ibuf->float_buffer = ImBufFloatBuffer{
+        .data = static_cast<const float *>(data.cpu_data().data()),
+        .sharing_info = data.sharing_info(),
+        .colorspace = nullptr};
+  }
+  else if (data.cpu_data().data() != render_view->ibuf->float_data()) {
+    IMB_alloc_float_pixels(render_view->ibuf, data.channels_count(), false);
+    std::memcpy(
+        render_view->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  }
 }
 
 void FileOutput::add_pass(const char *pass_name,
@@ -131,9 +140,18 @@ void FileOutput::add_pass(const char *pass_name,
   }
   render_pass->ibuf = IMB_allocImBuf(UNPACK2(data.domain().data_size), ImBufFlags::Zero);
   render_pass->ibuf->color_mode = color_mode;
-  IMB_alloc_float_pixels(render_pass->ibuf, data.channels_count(), false);
-  std::memcpy(
-      render_pass->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  if (data.sharing_info()) {
+    render_pass->ibuf->channels = data.channels_count();
+    render_pass->ibuf->float_buffer = ImBufFloatBuffer{
+        .data = static_cast<const float *>(data.cpu_data().data()),
+        .sharing_info = data.sharing_info(),
+        .colorspace = nullptr};
+  }
+  else if (data.cpu_data().data() != render_pass->ibuf->float_data()) {
+    IMB_alloc_float_pixels(render_pass->ibuf, data.channels_count(), false);
+    std::memcpy(
+        render_pass->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  }
   copy_v2_v2_db(render_pass->ibuf->ppm, render_result_->ppm);
 }
 

source/blender/draw/engines/image/image_buffer_cache.hh
@@ -95,7 +95,8 @@ struct FloatBufferCache {
     ImBuf *new_imbuf = IMB_allocImBuf(image_buffer->x, image_buffer->y, ImBufFlags::Zero);
     new_imbuf->color_mode = image_buffer->color_mode;
 
-    new_imbuf->assign_float_data(IMB_steal_float_buffer(image_buffer));
+    new_imbuf->float_buffer = image_buffer->float_buffer;
+    image_buffer->float_buffer = {};
 
     cache_.append(FloatImageBuffer(image_buffer, new_imbuf));
     return new_imbuf;

source/blender/editors/include/ED_paint.hh
@@ -88,24 +88,24 @@ void ED_image_undo_restore(UndoStep *us);
 /** Export for ED_undo_sys. */
 void ED_image_undosys_type(UndoType *ut);
 
-void *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
-                               Image *image,
-                               ImBuf *ibuf,
-                               ImageUser *iuser,
-                               int x_tile,
-                               int y_tile,
-                               unsigned short **r_mask,
-                               bool validate);
-void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
-                               Image *image,
-                               ImBuf *ibuf,
-                               ImageUser *iuser,
-                               int x_tile,
-                               int y_tile,
-                               unsigned short **r_mask,
-                               bool **r_valid,
-                               bool use_thread_lock,
-                               bool find_prev);
+const ImBuf *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
+                                      Image *image,
+                                      ImBuf *ibuf,
+                                      ImageUser *iuser,
+                                      int x_tile,
+                                      int y_tile,
+                                      unsigned short **r_mask,
+                                      bool validate);
+const ImBuf *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
+                                      Image *image,
+                                      ImBuf *ibuf,
+                                      ImageUser *iuser,
+                                      int x_tile,
+                                      int y_tile,
+                                      unsigned short **r_mask,
+                                      bool **r_valid,
+                                      bool use_thread_lock,
+                                      bool find_prev);
 void ED_image_paint_tile_lock_init();
 void ED_image_paint_tile_lock_end();
 

commit fbf6fd7bae3590cd62d859d995a692598b76125a

Fix: Images: Image buffer color space lost in some cases
For example the image undo buffers did not have appropriate color space.
No known bugs from this, but best to have this correct. Now keep the
color space when freeing pixels so we don't have to do error prone
restore in various places..

Issue introduced in 3bd35d9a4b2, incomplete fix was in 31bfc01b143.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/imbuf/IMB_imbuf.hh
@@ -543,7 +543,7 @@ void *imb_alloc_pixels(unsigned int x,
 bool IMB_alloc_byte_pixels(ImBuf *ibuf, bool initialize_pixels = true);
 
 /**
- * Deallocate image byte storage.
+ * Deallocate image byte storage. The colorspace is kept.
  */
 void IMB_free_byte_pixels(ImBuf *ibuf);
 
@@ -553,7 +553,7 @@ void IMB_free_byte_pixels(ImBuf *ibuf);
  */
 bool IMB_alloc_float_pixels(ImBuf *ibuf, unsigned int channels, bool initialize_pixels = true);
 /**
- * Deallocate image float storage.
+ * Deallocate image float storage. The colorspace is kept.
  */
 void IMB_free_float_pixels(ImBuf *ibuf);
 

source/blender/imbuf/intern/allocimbuf.cc
@@ -99,7 +99,9 @@ void IMB_free_float_pixels(ImBuf *ibuf)
   if (ibuf == nullptr) {
     return;
   }
-  ibuf->float_buffer = {};
+  /* Keep the colorspace, it's used when new pixels are assigned. */
+  ibuf->float_buffer.data = nullptr;
+  ibuf->float_buffer.sharing_info.reset();
 }
 
 void IMB_free_byte_pixels(ImBuf *ibuf)
@@ -107,7 +109,9 @@ void IMB_free_byte_pixels(ImBuf *ibuf)
   if (ibuf == nullptr) {
     return;
   }
-  ibuf->byte_buffer = {};
+  /* Keep the colorspace, it's used when new pixels are assigned. */
+  ibuf->byte_buffer.data = nullptr;
+  ibuf->byte_buffer.sharing_info.reset();
 }
 
 void IMB_free_all_data(ImBuf *ibuf)
@@ -178,9 +182,7 @@ bool IMB_alloc_float_pixels(ImBuf *ibuf, const uint channels, bool initialize_pi
     return false;
   }
 
-  if (ibuf->float_data()) {
-    IMB_free_float_pixels(ibuf);
-  }
+  IMB_free_float_pixels(ibuf);
 
   if (!imb_alloc_buffer(
           ibuf->float_buffer, ibuf->x, ibuf->y, channels, sizeof(float), initialize_pixels))
@@ -201,7 +203,7 @@ bool IMB_alloc_byte_pixels(ImBuf *ibuf, bool initialize_pixels)
     return false;
   }
 
-  ibuf->byte_buffer = {};
+  IMB_free_byte_pixels(ibuf);
 
   if (!imb_alloc_buffer(
           ibuf->byte_buffer, ibuf->x, ibuf->y, 4, sizeof(uint8_t), initialize_pixels))
@@ -214,7 +216,7 @@ bool IMB_alloc_byte_pixels(ImBuf *ibuf, bool initialize_pixels)
 
 void ImBuf::assign_byte_data(uint8_t *data)
 {
-  this->byte_buffer = {};
+  IMB_free_byte_pixels(this);
   if (data) {
     this->byte_buffer.data = data;
     this->byte_buffer.sharing_info = ImplicitSharingPtr<>(
@@ -224,7 +226,7 @@ void ImBuf::assign_byte_data(uint8_t *data)
 
 void ImBuf::assign_float_data(float *data)
 {
-  this->float_buffer = {};
+  IMB_free_float_pixels(this);
   if (data) {
     this->float_buffer.data = data;
     this->float_buffer.sharing_info = ImplicitSharingPtr<>(
@@ -276,9 +278,7 @@ void IMB_ensure_host_buffer(ImBuf *ibuf)
   GPU_memory_barrier(GPU_BARRIER_TEXTURE_UPDATE);
   float *output_buffer = static_cast<float *>(
       GPU_texture_read(ibuf->gpu.texture, GPU_DATA_FLOAT, 0));
-  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
   ibuf->assign_float_data(output_buffer);
-  ibuf->float_buffer.colorspace = float_colorspace;
 
   if (need_secondary_context) {
     IMB_deactivate_gpu_context();

source/blender/imbuf/intern/conversion.cc
@@ -369,6 +369,9 @@ void IMB_float_from_byte(ImBuf *ibuf)
     }
   }
 
+  /* Pixels get converted to scene linear, so clear colorspace. */
+  ibuf->float_buffer.colorspace = nullptr;
+
   rcti region_to_update;
   BLI_rcti_init(&region_to_update, 0, ibuf->x, 0, ibuf->y);
   IMB_float_from_byte_ex(ibuf, ibuf, &region_to_update);

source/blender/imbuf/intern/rectop.cc
@@ -474,17 +474,12 @@ void IMB_crop(ImBuf *ibuf, const int2 &rect_pos, const int2 &rect_size)
     return;
   }
 
-  const ColorSpace *byte_colorspace = ibuf->byte_buffer.colorspace;
-  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
-
   if (const uchar *byte_data = ibuf->byte_data()) {
     ibuf->assign_byte_data(create_cropped_buffer(byte_data, src_size, rect_pos, rect_size));
-    ibuf->byte_buffer.colorspace = byte_colorspace;
   }
   if (const float *float_data = ibuf->float_data()) {
     ibuf->assign_float_data(
         create_cropped_buffer(float_data, src_size, ibuf->channels, rect_pos, rect_size));
-    ibuf->float_buffer.colorspace = float_colorspace;
   }
 
   ibuf->x = rect_size.x;

source/blender/imbuf/intern/rotate.cc
@@ -66,9 +66,6 @@ bool IMB_rotate_orthogonal(ImBuf *ibuf, int degrees)
   const int size_x = ibuf->x;
   const int size_y = ibuf->y;
 
-  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
-  const ColorSpace *byte_colorspace = ibuf->byte_buffer.colorspace;
-
   if (ELEM(degrees, 90, 270)) {
     std::swap(ibuf->x, ibuf->y);
   }
@@ -79,7 +76,6 @@ bool IMB_rotate_orthogonal(ImBuf *ibuf, int degrees)
         size_t(channels) * size_t(size_x) * size_t(size_y), __func__);
     rotate_pixels<float>(degrees, size_x, size_y, src_pixels, dst_pixels, ibuf->channels);
     ibuf->assign_float_data(dst_pixels);
-    ibuf->float_buffer.colorspace = float_colorspace;
     if (ibuf->byte_data()) {
       IMB_byte_from_float(ibuf);
     }
@@ -90,7 +86,6 @@ bool IMB_rotate_orthogonal(ImBuf *ibuf, int degrees)
                                                            __func__);
     rotate_pixels<uchar>(degrees, size_x, size_y, src_pixels, dst_pixels, 4);
     ibuf->assign_byte_data(dst_pixels);
-    ibuf->byte_buffer.colorspace = byte_colorspace;
   }
 
   return true;

source/blender/imbuf/intern/scaling.cc
@@ -476,9 +476,6 @@ bool IMB_scale(ImBuf *ibuf, const int2 new_size, IMBScaleFilter filter, bool thr
     return false;
   }
 
-  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
-  const ColorSpace *byte_colorspace = ibuf->byte_buffer.colorspace;
-
   switch (filter) {
     case IMBScaleFilter::Nearest: {
       if (const float *src = ibuf->float_data()) {
@@ -526,8 +523,6 @@ bool IMB_scale(ImBuf *ibuf, const int2 new_size, IMBScaleFilter filter, bool thr
       break;
     }
   }
-  ibuf->float_buffer.colorspace = float_colorspace;
-  ibuf->byte_buffer.colorspace = byte_colorspace;
   ibuf->x = new_size.x;
   ibuf->y = new_size.y;
   return true;

source/blender/render/intern/compositor.cc
@@ -199,6 +199,9 @@ class Context : public compositor::Context {
         }
       }
 
+      /* Compositor output is scene linear, previous pixels were replaced. */
+      image_buffer->float_buffer.colorspace = nullptr;
+
       /* Free outdated GPU texture. */
       IMB_free_gpu_textures(image_buffer);
       IMB_partial_update_mark_full(image_buffer);