blender/blender

commit c6d3cbd156dc819757bc24730cdc1ced5aebbf53

Fix: RNA: Incorrect handling of nested DNA member defaults
Fixes one actual case, wrong render border default max values.

This issue was introduced in bc393beb31c.

Pull Request: https://projects.blender.org/blender/blender/pulls/159872

source/blender/makesrna/intern/rna_define.cc
@@ -310,10 +310,28 @@ static int rna_find_parsed_struct_index(const StringRef structname)
   return -1;
 }
 
+static const void *rna_default_data(const int struct_idx,
+                                    const int member_idx,
+                                    const void *base_default_data)
+{
+  const void *member_default_data = DNA_member_default_table[struct_idx][member_idx];
+
+  if (base_default_data) {
+    /* For a nested member, offset into the parent struct's default instance. */
+    const void *struct_default_base = DNA_member_default_table[struct_idx][0];
+    return POINTER_OFFSET(base_default_data,
+                          static_cast<const char *>(member_default_data) -
+                              static_cast<const char *>(struct_default_base));
+  }
+
+  return member_default_data;
+}
+
 /** Find DNA type info, recursively following `.` and `->`. */
 static bool rna_find_sdna_member(const StringRef structname,
                                  const StringRef path,
-                                 PropertyDefRNA *r_dp = nullptr)
+                                 PropertyDefRNA *r_dp = nullptr,
+                                 const void *base_default_data = nullptr)
 {
   const int struct_idx = rna_find_parsed_struct_index(structname);
   if (struct_idx == -1) {
@@ -341,7 +359,7 @@ static bool rna_find_sdna_member(const StringRef structname,
         for (int b = 0; pm.member_name[b] == '*'; b++) {
           r_dp->dnapointerlevel++;
         }
-        r_dp->dnadefaultdata = DNA_member_default_table[struct_idx][member_idx];
+        r_dp->dnadefaultdata = rna_default_data(struct_idx, member_idx, base_default_data);
       }
       return true;
     }
@@ -357,7 +375,12 @@ static bool rna_find_sdna_member(const StringRef structname,
       continue;
     }
 
-    rna_find_sdna_member(pm.type_name, remainder, r_dp);
+    /* The return value for nested structs is ignored, to allow recursing into
+     * runtime structs not covered by DNA. */
+    rna_find_sdna_member(pm.type_name,
+                         remainder,
+                         r_dp,
+                         rna_default_data(struct_idx, member_idx, base_default_data));
     return true;
   }
 

commit f9c08f809537648dd0194e1bd634b7262b884bd0

Fix: RNA: Wrong default lookup for DNA pointer member paths
More of a theoretical issue since these are always null, but the logic
for using the nested struct default should not have been used here.

Issue introduced in c6d3cbd156d.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/makesrna/intern/rna_define.cc
@@ -365,8 +365,11 @@ static bool rna_find_sdna_member(const StringRef structname,
     }
 
     /* Recursive into nested members. */
+    const void *nested_default_data = nullptr;
     if (remainder.startswith(".")) {
+      /* Nested struct, offset into parent default. */
       remainder = remainder.drop_known_prefix(".");
+      nested_default_data = rna_default_data(struct_idx, member_idx, base_default_data);
     }
     else if (remainder.startswith("->")) {
       remainder = remainder.drop_known_prefix("->");
@@ -377,10 +380,7 @@ static bool rna_find_sdna_member(const StringRef structname,
 
     /* The return value for nested structs is ignored, to allow recursing into
      * runtime structs not covered by DNA. */
-    rna_find_sdna_member(pm.type_name,
-                         remainder,
-                         r_dp,
-                         rna_default_data(struct_idx, member_idx, base_default_data));
+    rna_find_sdna_member(pm.type_name, remainder, r_dp, nested_default_data);
     return true;
   }
 