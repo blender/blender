blender/blender

commit 4041bffc6924f55a3eaeda87253f0918bcf2cc44

Refactor: Images: Improve ImBuf update marking
* Do more fine grained partial update marking on individual image
  buffers where possible.
* Perform partial update marking for render buffers at the moment
  they are modified, rather than indirectly through callbacks.
* Remove now unnecessary image level marking on render result images.
* Move mark dirty from image to image buffer API.
* Remove remaining BKE partial update code.

Pull Request: https://projects.blender.org/blender/blender/pulls/161094

source/blender/render/intern/pipeline.cc
@@ -74,6 +74,7 @@
 #include "IMB_imbuf.hh"
 #include "IMB_imbuf_types.hh"
 #include "IMB_metadata.hh"
+#include "IMB_partial_update.hh"
 
 #include "MOV_write.hh"
 
@@ -182,7 +183,7 @@ static bool do_write_image_or_movie(Render *re,
                                     const bool write_anim_or_still);
 
 /* default callbacks, set in each new render */
-static void result_rcti_nothing(void * /*arg*/, RenderResult * /*rr*/, rcti * /*rect*/) {}
+static void result_rcti_nothing(void * /*arg*/, RenderResult * /*rr*/) {}
 static void current_scene_nothing(void * /*arg*/, Scene * /*scene*/) {}
 static void float_nothing(void * /*arg*/, float /*val*/) {}
 static bool default_break(void * /*arg*/)
@@ -887,9 +888,7 @@ void RE_InitState(Render *re,
   RE_init_threadcount(re);
 }
 
-void RE_display_update_cb(Render *re,
-                          void *handle,
-                          void (*f)(void *handle, RenderResult *rr, rcti *rect))
+void RE_display_update_cb(Render *re, void *handle, void (*f)(void *handle, RenderResult *rr))
 {
   re->display->display_update_cb = f;
   re->display->duh = handle;
@@ -1041,7 +1040,7 @@ static void render_result_uncrop(Render *re)
 
       BLI_rw_mutex_unlock(&re->resultmutex);
 
-      re->display->display_update(re->result, nullptr);
+      re->display->display_update(re->result);
 
       /* restore the disprect from border */
       re->disprect = orig_disprect;
@@ -1303,7 +1302,7 @@ static void do_render_compositor(Render *re)
   /* Weak: the display callback wants an active render-layer pointer. */
   if (re->result != nullptr) {
     re->result->renlay = render_get_single_layer(re, re->result);
-    re->display->display_update(re->result, nullptr);
+    re->display->display_update(re->result);
   }
 }
 
@@ -1460,7 +1459,7 @@ static void do_render_sequencer(Render *re)
 
     /* would mark display buffers as invalid */
     RE_SetActiveRenderView(re, rv->name);
-    re->display->display_update(re->result, nullptr);
+    re->display->display_update(re->result);
   }
 
   recurs_depth--;
@@ -1504,7 +1503,7 @@ static void do_render_full_pipeline(Render *re)
     }
 
     re->display->stats_draw(&re->i);
-    re->display->display_update(re->result, nullptr);
+    re->display->display_update(re->result);
   }
   else {
     do_render_compositor(re);
@@ -1527,7 +1526,7 @@ static void do_render_full_pipeline(Render *re)
     /* stamp image info here */
     if ((re->scene->r.stamp & R_STAMP_ALL) && (re->scene->r.stamp & R_STAMP_DRAW)) {
       renderresult_stampinfo(re);
-      re->display->display_update(re->result, nullptr);
+      re->display->display_update(re->result);
     }
   }
 }
@@ -2751,6 +2750,7 @@ void RE_layer_load_from_file(
       }
 
       rpass->ibuf->float_buffer = ibuf->float_buffer;
+      IMB_partial_update_mark_full(rpass->ibuf);
     }
     else {
       if ((ibuf->x - x >= layer->rectx) && (ibuf->y - y >= layer->recty)) {
@@ -2758,6 +2758,7 @@ void RE_layer_load_from_file(
           IMB_float_from_byte(ibuf);
         }
         IMB_copy_rect(rpass->ibuf, ibuf, int2(x, y), int2(0, 0), int2(layer->rectx, layer->recty));
+        IMB_partial_update_mark_full(rpass->ibuf);
       }
       else {
         BKE_reportf(reports,

source/blender/blenkernel/BKE_image.hh
@@ -572,7 +572,6 @@ float *BKE_image_get_float_pixels_for_frame(Image *image, int frame, int tile);
 /* Image modifications */
 
 bool BKE_image_is_dirty(Image *image);
-void BKE_image_mark_dirty(Image *image, ImBuf *ibuf);
 bool BKE_image_buffer_format_writable(ImBuf *ibuf);
 
 bool BKE_image_is_dirty_writable(Image *image, bool *r_is_writable);
@@ -614,21 +613,5 @@ RenderSlot *BKE_image_add_renderslot(Image *ima, const char *name);
 bool BKE_image_remove_renderslot(Image *ima, ImageUser *iuser, int slot);
 RenderSlot *BKE_image_get_renderslot(Image *ima, int index);
 bool BKE_image_clear_renderslot(Image *ima, ImageUser *iuser, int slot);
-
-/* --- image_partial_update.cc --- */
-
-/** \brief Mark a region of the image to update. */
-void BKE_image_partial_update_mark_region(Image *image,
-                                          const ImageTile *image_tile,
-                                          const ImBuf *image_buffer,
-                                          const rcti *updated_region);
-/** \brief Mark the whole image to be updated. */
-void BKE_image_partial_update_mark_full_update(Image *image);
-
-/**
- * \brief Variant of #BKE_image_partial_update_mark_full_update for callers
- * that already hold the image's cache mutex.
- */
-void BKE_image_partial_update_mark_full_update_cache_locked(Image *image);
 
 }  // namespace blender

source/blender/blenkernel/BKE_image_gpu.hh
@@ -93,7 +93,7 @@ bool BKE_image_has_gpu_texture(Image *ima);
 
 /**
  * Free GPU textures to recreate them from CPU buffers or reduce memory usage.
- * Most code should use BKE_image_partial_update_mark instead when making changes
+ * Most code should use #IMB_partial_update_mark_full instead when making changes
  * to image CPU buffers.
  */
 void BKE_image_free_gpu_texture_caches(Image *ima);
@@ -103,20 +103,6 @@ void BKE_image_free_all_gpu_texture_caches(Main *bmain);
  * Same as #BKE_image_free_all_gputextures but only free animated images.
  */
 void BKE_image_free_anim_gpu_texture_caches(Main *bmain);
-
-/**
- * Partial update of texture for texture painting.
- * This is often much quicker than fully updating the texture for high resolution images.
- */
-void BKE_image_update_gputexture(Image *ima, ImageUser *iuser, int x, int y, int w, int h);
-
-/**
- * Mark areas on the #gpu::Texture that need to be updated. The areas are marked in chunks.
- * The next time the #gpu::Texture is used these tiles will be refreshed. This saves time
- * when writing to the same place multiple times during foreground rendering.
- */
-void BKE_image_update_gputexture_delayed(
-    Image *ima, ImageTile *image_tile, ImBuf *ibuf, int x, int y, int w, int h);
 
 /**
  * Flush partial update for all UDIM tiles and return new changeset ID that

source/blender/blenkernel/BKE_paint_bvh_pixels.hh
@@ -22,6 +22,7 @@
 #include "BKE_paint_bvh.hh"
 
 #include "IMB_imbuf_types.hh"
+#include "IMB_partial_update.hh"
 
 namespace blender::bke::pbvh::pixels {
 
@@ -129,20 +130,17 @@ struct PixelNode {
     return nullptr;
   }
 
-  void mark_region(UDIMTilePixels &tile,
-                   Image &image,
-                   const image::ImageTileWrapper &image_tile,
-                   ImBuf &image_buffer)
+  void mark_region(UDIMTilePixels &tile, ImBuf &image_buffer)
   {
     if (tile.flags.dirty) {
       if (image_buffer.color_mode == ImColorMode::BW) {
         image_buffer.color_mode = ImColorMode::RGBA;
-        BKE_image_partial_update_mark_full_update(&image);
+        IMB_partial_update_mark_full(&image_buffer);
       }
       else {
-        BKE_image_partial_update_mark_region(
-            &image, image_tile.image_tile, &image_buffer, &tile.dirty_region);
+        IMB_partial_update_mark_region(&image_buffer, tile.dirty_region);
       }
+      IMB_mark_dirty(&image_buffer);
       tile.clear_dirty();
     }
   }

source/blender/blenkernel/CMakeLists.txt
@@ -152,7 +152,6 @@ set(SRC
   intern/image_format.cc
   intern/image_gen.cc
   intern/image_gpu.cc
-  intern/image_partial_update.cc
   intern/image_save.cc
   intern/instances.cc
   intern/instances_attributes.cc

source/blender/blenkernel/intern/image.cc
@@ -41,6 +41,7 @@
 #include "IMB_imbuf_types.hh"
 #include "IMB_metadata.hh"
 #include "IMB_openexr.hh"
+#include "IMB_partial_update.hh"
 
 #include "MOV_read.hh"
 
@@ -930,7 +931,8 @@ bool BKE_image_scale(Image *image, int width, int height, ImageUser *iuser)
 
   if (ibuf) {
     IMB_scale(ibuf, width, height, IMBScaleFilter::Box, false);
-    BKE_image_mark_dirty(image, ibuf);
+    IMB_partial_update_mark_full(ibuf);
+    IMB_mark_dirty(ibuf);
   }
 
   BKE_image_release_ibuf(image, ibuf, lock);
@@ -1500,7 +1502,7 @@ void BKE_image_replace_imbuf(Image *image, ImBuf *ibuf)
 
   /* Consider image dirty since its content can not be re-created unless the image is explicitly
    * saved. */
-  BKE_image_mark_dirty(image, ibuf);
+  IMB_mark_dirty(ibuf);
 }
 
 /** Pack image buffer to memory as PNG or EXR. */
@@ -3269,7 +3271,6 @@ static void image_tag_reload(Image *ima, ID *iuser_id, ImageUser *iuser, void *c
       /* Must copy image user changes to evaluated data-block. */
       DEG_id_tag_update(iuser_id, ID_RECALC_SYNC_TO_EVAL);
     }
-    BKE_image_partial_update_mark_full_update_cache_locked(ima);
   }
 }
 
@@ -3296,7 +3297,6 @@ static void image_free_tile(Image *ima, ImageTile *tile)
 {
   /* UDIM tiles are packed into an atlas for the GPU, so need to free all. */
   BKE_image_free_gpu_udim_textures(ima);
-  BKE_image_partial_update_mark_full_update_cache_locked(ima);
 
   if (BKE_image_is_multiview(ima)) {
     const int totviews = ima->views.count();
@@ -3421,7 +3421,6 @@ void BKE_image_signal(Main *bmain, Image *ima, ImageUser *iuser, int signal)
         image_tag_frame_recalc(ima, nullptr, iuser, ima);
       }
       BKE_image_walk_all_users(bmain, ima, image_tag_frame_recalc);
-      BKE_image_partial_update_mark_full_update_cache_locked(ima);
 
       break;
 
@@ -3684,7 +3683,6 @@ ImageTile *BKE_image_add_tile(Image *ima, int tile_number, const char *label)
   }
 
   BKE_image_free_gpu_udim_textures(ima);
-  BKE_image_partial_update_mark_full_update_cache_locked(ima);
 
   return tile;
 }
@@ -3728,7 +3726,6 @@ void BKE_image_reassign_tile(Image *ima, ImageTile *tile, int new_tile_number)
   }
 
   BKE_image_free_gpu_udim_textures(ima);
-  BKE_image_partial_update_mark_full_update_cache_locked(ima);
 }
 
 static int tile_sort_cb(const void *a, const void *b)
@@ -4039,7 +4036,6 @@ RenderResult *BKE_image_acquire_renderresult(Scene *scene, Image *ima)
     }
     else {
       rr = BKE_image_get_renderslot(ima, ima->render_slot)->render;
-      BKE_image_partial_update_mark_full_update_cache_locked(ima);
     }
 
     /* set proper views */
@@ -5687,11 +5683,6 @@ bool BKE_image_is_dirty_writable(Image *image, bool *r_is_writable)
 bool BKE_image_is_dirty(Image *image)
 {
   return BKE_image_is_dirty_writable(image, nullptr);
-}
-
-void BKE_image_mark_dirty(Image * /*image*/, ImBuf *ibuf)
-{
-  ibuf->userflags |= IB_BITMAPDIRTY;
 }
 
 bool BKE_image_buffer_format_writable(ImBuf *ibuf)

source/blender/blenkernel/intern/image_gpu.cc
@@ -836,35 +836,6 @@ void BKE_image_free_anim_gpu_texture_caches(Main *bmain)
 /** \name Paint Update
  * \{ */
 
-void BKE_image_update_gputexture(Image *ima, ImageUser *iuser, int x, int y, int w, int h)
-{
-  ImageTile *image_tile = BKE_image_get_tile_from_iuser(ima, iuser);
-  ImBuf *ibuf = BKE_image_acquire_ibuf(ima, iuser, nullptr);
-  BKE_image_update_gputexture_delayed(ima, image_tile, ibuf, x, y, w, h);
-  BKE_image_release_ibuf(ima, ibuf, nullptr);
-}
-
-void BKE_image_update_gputexture_delayed(
-    Image *ima, ImageTile *image_tile, ImBuf *ibuf, int x, int y, int w, int h)
-{
-  if (ibuf) {
-    /* Retry creating GPU texture if it failed before. */
-    IMB_clear_gpu_load_failed(ibuf);
-  }
-
-  /* Check for full refresh. */
-  if (ibuf != nullptr && ima->source != IMA_SRC_TILED && x == 0 && y == 0 && w == ibuf->x &&
-      h == ibuf->y)
-  {
-    IMB_partial_update_mark_full(ibuf);
-  }
-  else {
-    rcti dirty_region;
-    BLI_rcti_init(&dirty_region, x, x + w, y, y + h);
-    BKE_image_partial_update_mark_region(ima, image_tile, ibuf, &dirty_region);
-  }
-}
-
 void BKE_image_paint_set_mipmap(Main *bmain, bool mipmap)
 {
   for (Image &ima : bmain->images) {

source/blender/blenkernel/intern/image_partial_update.cc
@@ -1,72 +1,0 @@
-/* SPDX-FileCopyrightText: 2021 Blender Authors
- *
- * SPDX-License-Identifier: GPL-2.0-or-later */
-/**
- * \file
- * \ingroup bke
- *
- * Utilities for marking all image buffers in an image for update.
- * Temporary until callers are updated to call IMB API directly.
- */
-
-#include <mutex>
-
-#include "BKE_image.hh"
-
-#include "DNA_image_types.h"
-
-#include "IMB_cache.hh"
-#include "IMB_imbuf_types.hh"
-#include "IMB_partial_update.hh"
-
-#include "atomic_ops.h"
-
-namespace blender {
-
-void BKE_image_partial_update_mark_region(Image * /*image*/,
-                                          const ImageTile * /*image_tile*/,
-                                          const ImBuf *image_buffer,
-                                          const rcti *updated_region)
-{
-  if (image_buffer != nullptr) {
-    IMB_partial_update_mark_region(const_cast<ImBuf *>(image_buffer), *updated_region);
-  }
-}
-
-static void image_partial_update_mark_buffers_full(Image *image)
-{
-  if (image->runtime->cache == nullptr) {
-    return;
-  }
-  ImBufCacheIter *iter = IMB_cacheIter_new(image->runtime->cache);
-  while (!IMB_cacheIter_done(iter)) {
-    if (ImBuf *ibuf = IMB_cacheIter_getImBuf(iter)) {
-      IMB_partial_update_mark_full(ibuf);
-    }
-    IMB_cacheIter_step(iter);
-  }
-  IMB_cacheIter_free(iter);
-}
-
-static void image_partial_update_mark_full_update(Image *image, const bool cache_locked)
-{
-  if (cache_locked) {
-    image_partial_update_mark_buffers_full(image);
-  }
-  else {
-    std::scoped_lock lock(image->runtime->cache_mutex);
-    image_partial_update_mark_buffers_full(image);
-  }
-}
-
-void BKE_image_partial_update_mark_full_update(Image *image)
-{
-  image_partial_update_mark_full_update(image, false);
-}
-
-void BKE_image_partial_update_mark_full_update_cache_locked(Image *image)
-{
-  image_partial_update_mark_full_update(image, true);
-}
-
-}  // namespace blender

commit c6d24b5be247c0f0f80912f4dcc96bf45c510e53

Fix: Images: Missing image editor redraws for stamp and Freestyle
Need to do mark full update for these now.

Issue was introduced in 4041bffc692.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/freestyle/intern/blender_interface/FRS_freestyle.cpp
@@ -42,6 +42,7 @@
 #include "DEG_depsgraph_query.hh"
 
 #include "IMB_imbuf.hh"
+#include "IMB_partial_update.hh"
 
 #include "pipeline.hh"
 
@@ -471,10 +472,6 @@ static void prepare(Render *re, ViewLayer *view_layer, Depsgraph *depsgraph)
 
 void FRS_composite_result(Render *re, ViewLayer *view_layer, Render *freestyle_render)
 {
-  RenderLayer *rl;
-  float *src, *dest, *pixSrc, *pixDest;
-  int x, y, rectx, recty;
-
   if (freestyle_render == nullptr || freestyle_render->result == nullptr) {
     if (view_layer->freestyle_config.flags & FREESTYLE_AS_RENDER_PASS) {
       // Create a blank render pass output.
@@ -484,7 +481,7 @@ void FRS_composite_result(Render *re, ViewLayer *view_layer, Render *freestyle_r
     return;
   }
 
-  rl = render_get_single_layer(freestyle_render, freestyle_render->result);
+  RenderLayer *rl = render_get_single_layer(freestyle_render, freestyle_render->result);
   if (!rl) {
     if (G.debug & G_DEBUG_FREESTYLE) {
       cout << "No source render layer to composite" << endl;
@@ -492,7 +489,7 @@ void FRS_composite_result(Render *re, ViewLayer *view_layer, Render *freestyle_r
     return;
   }
 
-  src = RE_RenderLayerGetPass(rl, RE_PASSNAME_COMBINED, freestyle_render->viewname);
+  const float *src = RE_RenderLayerGetPass(rl, RE_PASSNAME_COMBINED, freestyle_render->viewname);
   if (!src) {
     if (G.debug & G_DEBUG_FREESTYLE) {
       cout << "No source result image to composite" << endl;
@@ -513,14 +510,18 @@ void FRS_composite_result(Render *re, ViewLayer *view_layer, Render *freestyle_r
     return;
   }
 
+  const char *dest_pass;
   if (view_layer->freestyle_config.flags & FREESTYLE_AS_RENDER_PASS) {
     RE_create_render_pass(
         re->result, RE_PASSNAME_FREESTYLE, 4, "RGBA", view_layer->name, re->viewname, true);
-    dest = RE_RenderLayerGetPass(rl, RE_PASSNAME_FREESTYLE, re->viewname);
+    dest_pass = RE_PASSNAME_FREESTYLE;
   }
   else {
-    dest = RE_RenderLayerGetPass(rl, RE_PASSNAME_COMBINED, re->viewname);
+    dest_pass = RE_PASSNAME_COMBINED;
   }
+
+  ImBuf *dest_ibuf = RE_RenderLayerGetPassImBuf(rl, dest_pass, re->viewname);
+  float *dest = (dest_ibuf) ? dest_ibuf->float_data_for_write() : nullptr;
   if (!dest) {
     if (G.debug & G_DEBUG_FREESTYLE) {
       cout << "No destination result image to composite to" << endl;
@@ -533,17 +534,19 @@ void FRS_composite_result(Render *re, ViewLayer *view_layer, Render *freestyle_r
   }
 #endif
 
-  rectx = re->rectx;
-  recty = re->recty;
-  for (y = 0; y < recty; y++) {
-    for (x = 0; x < rectx; x++) {
-      pixSrc = src + 4 * (rectx * y + x);
+  const int rectx = re->rectx;
+  const int recty = re->recty;
+  for (int y = 0; y < recty; y++) {
+    for (int x = 0; x < rectx; x++) {
+      const float *pixSrc = src + 4 * (rectx * y + x);
       if (pixSrc[3] > 0.0) {
-        pixDest = dest + 4 * (rectx * y + x);
+        float *pixDest = dest + 4 * (rectx * y + x);
         blend_color_mix_float(pixDest, pixDest, pixSrc);
       }
     }
   }
+
+  IMB_partial_update_mark_full(dest_ibuf);
 }
 
 static int displayed_layer_count(ViewLayer *view_layer)

source/blender/render/intern/pipeline.cc
@@ -1431,6 +1431,7 @@ static void renderresult_stampinfo(Render *re)
                           ob_camera_eval,
                           (re->scene->r.stamp & R_STAMP_STRIPMETA) ? rres.stamp_data : nullptr,
                           rres.ibuf);
+      IMB_partial_update_mark_full(rres.ibuf);
     }
 
     RE_ReleaseResultImage(re);