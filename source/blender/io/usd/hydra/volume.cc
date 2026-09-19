blender/blender

commit 590af512e323719a9b11eeca8e5d9d06d963abf6

Render: Hydra: Migrate render delegate to use Hydra 2.0 API
The 1.0 API is deprecated (though not yet removed) in OpenUSD 26.xx
releases. So it's time to migrate.

The API is now based on constructing a scene index with prims and schemas,
rather than getting callbacks through HdSceneDelegate to query data. This
simplifies the implementation somewhat and makes it more similar to Cycles
and EEVEE. The tricky handling of updates, objects, instances and geometry
sets was made to work more similar to Cycles.

Tasks are handled by inserting task prims into the retained scene index
rather than HdSceneDelegate.

The Storm render tests render identical to before, existing limitations
were preserved. There are some things that could be fixed by comparing
to the Cycles implementation but that's left for later.

USD has an abstraction where the host application and delegate can use
different versions. However there are some differences in material networks
that are not fully abstracted. From what I can tell, the latest Maya and
Houdini version are using Hydra 2.0 so this hopefully this improves
compatibility overall.

Pull Request: https://projects.blender.org/blender/blender/pulls/158456

source/blender/io/usd/hydra/volume.cc
@@ -2,158 +2,193 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "volume.hh"
+
+#include <pxr/imaging/hd/materialBindingSchema.h>
+#include <pxr/imaging/hd/materialBindingsSchema.h>
+#include <pxr/imaging/hd/retainedDataSource.h>
 #include <pxr/imaging/hd/tokens.h>
+#include <pxr/imaging/hd/visibilitySchema.h>
+#include <pxr/imaging/hd/volumeFieldBindingSchema.h>
 #include <pxr/imaging/hd/volumeFieldSchema.h>
-#include <pxr/usd/usdHydra/tokens.h>
-#include <pxr/usd/usdVol/tokens.h>
+#include <pxr/imaging/hd/xformSchema.h>
+#include <pxr/usd/sdf/assetPath.h>
 #include <pxr/usdImaging/usdVolImaging/tokens.h>
+
+#include "BLI_index_range.hh"
 
 #include "BKE_material.hh"
 #include "BKE_volume.hh"
-#include "BLI_index_range.hh"
+
+#include "DNA_object_types.h"
+#include "DNA_scene_types.h"
 #include "DNA_volume_types.h"
 
-#include "hydra_scene_delegate.hh"
-#include "volume.hh"
+#include "populate_context.hh"
+#include "util.hh"
+#include "volume_modifier.hh"
 
 namespace blender::io::hydra {
 
-VolumeData::VolumeData(HydraSceneDelegate *scene_delegate,
-                       const Object *object,
-                       pxr::SdfPath const &prim_id)
-    : ObjectData(scene_delegate, object, prim_id)
+/* Bprim data source for one VDB field. */
+static pxr::HdContainerDataSourceHandle build_volume_field_data_source(
+    const std::string &filepath, const pxr::TfToken &field_name)
 {
+  return pxr::HdRetainedContainerDataSource::New(
+      pxr::HdVolumeFieldSchema::GetSchemaToken(),
+      pxr::HdVolumeFieldSchema::Builder()
+          .SetFilePath(pxr::HdRetainedTypedSampledDataSource<pxr::SdfAssetPath>::New(
+              pxr::SdfAssetPath(filepath, filepath)))
+          .SetFieldName(pxr::HdRetainedTypedSampledDataSource<pxr::TfToken>::New(field_name))
+          .SetFieldIndex(pxr::HdRetainedTypedSampledDataSource<int>::New(0))
+          .Build());
 }
 
-void VolumeData::init()
+/* Volume Rprim data source. */
+static pxr::HdContainerDataSourceHandle build_volume_prim_data_source(
+    const Span<VolumeFieldDescriptor> fields,
+    const pxr::GfMatrix4d &transform,
+    const bool visible,
+    const pxr::SdfPath &material_path)
 {
-  field_descriptors_.clear();
-
-  Volume *volume = id_cast<Volume *>((id_cast<const Object *>(this->id))->data);
-  if (!BKE_volume_load(volume, scene_delegate_->bmain)) {
-    return;
+  HdContainerBuilder field_bindings;
+  field_bindings.reserve(fields.size());
+  for (const VolumeFieldDescriptor &f : fields) {
+    field_bindings.add(f.name, f.field_path);
   }
-  filepath_ = BKE_volume_grids_frame_filepath(volume);
-  ID_LOGN("%s", filepath_.c_str());
+  pxr::HdContainerDataSourceHandle bindings = pxr::HdVolumeFieldBindingSchema::BuildRetained(
+      field_bindings.names.size(), field_bindings.names.data(), field_bindings.values.data());
 
+  pxr::HdContainerDataSourceHandle xform =
+      pxr::HdXformSchema::Builder()
+          .SetMatrix(pxr::HdRetainedTypedSampledDataSource<pxr::GfMatrix4d>::New(transform))
+          .SetResetXformStack(pxr::HdRetainedTypedSampledDataSource<bool>::New(false))
+          .Build();
+  pxr::HdContainerDataSourceHandle visibility =
+      pxr::HdVisibilitySchema::Builder()
+          .SetVisibility(pxr::HdRetainedTypedSampledDataSource<bool>::New(visible))
+          .Build();
+
+  HdContainerBuilder b;
+  b.add(pxr::HdVolumeFieldBindingSchema::GetSchemaToken(), bindings);
+  b.add(pxr::HdXformSchema::GetSchemaToken(), xform);
+  b.add(pxr::HdVisibilitySchema::GetSchemaToken(), visibility);
+
+  if (!material_path.IsEmpty()) {
+    pxr::HdContainerDataSourceHandle binding =
+        pxr::HdMaterialBindingSchema::Builder()
+            .SetPath(pxr::HdRetainedTypedSampledDataSource<pxr::SdfPath>::New(material_path))
+            .Build();
+    pxr::HdContainerDataSourceHandle bindings_ds = pxr::HdRetainedContainerDataSource::New(
+        pxr::HdMaterialBindingsSchemaTokens->allPurpose, binding);
+    b.add(pxr::HdMaterialBindingsSchema::GetSchemaToken(), bindings_ds);
+  }
+  return b.build();
+}
+
+/* Resolve the VDB file path and per-grid fields for a volume datablock. */
+static std::string build_volume_fields_from_datablock(const Object *object,
+                                                      const pxr::SdfPath &volume_path,
+                                                      Main *bmain,
+                                                      pxr::GfMatrix4d *r_transform,
+                                                      Vector<VolumeFieldDescriptor> *r_fields)
+{
+  Volume *volume = id_cast<Volume *>(object->data);
+  if (!BKE_volume_load(volume, bmain)) {
+    return {};
+  }
+  std::string filepath = BKE_volume_grids_frame_filepath(volume);
   if (volume->runtime->grids) {
     const int num_grids = BKE_volume_num_grids(volume);
-    if (num_grids) {
-      for (const int i : IndexRange(num_grids)) {
-        const bke::VolumeGridData *grid = BKE_volume_grid_get(volume, i);
-        const std::string grid_name = bke::volume_grid::get_name(*grid);
-
-        field_descriptors_.emplace_back(pxr::TfToken(grid_name),
-                                        pxr::UsdVolImagingTokens->openvdbAsset,
-                                        prim_id.AppendElementString("VF_" + grid_name));
-      }
+    r_fields->reserve(num_grids);
+    for (const int i : IndexRange(num_grids)) {
+      const bke::VolumeGridData *grid = BKE_volume_grid_get(volume, i);
+      const std::string grid_name = bke::volume_grid::get_name(*grid);
+      VolumeFieldDescriptor f;
+      f.name = pxr::TfToken(grid_name);
+      f.field_path = volume_path.AppendElementString("VF_" + grid_name);
+      r_fields->append(f);
     }
   }
-  write_transform();
-  write_materials();
-
+  *r_transform = gf_matrix_from_transform(object->object_to_world().ptr());
   BKE_volume_unload(volume);
+  return filepath;
 }
 
-void VolumeData::insert()
+bool emit_volume_object(PopulateContext &ctx, const Object *object, EmittedObject &emitted)
 {
-  scene_delegate_->GetRenderIndex().InsertRprim(
-      pxr::HdPrimTypeTokens->volume, scene_delegate_, prim_id);
-
-  ID_LOGN("");
-
-  for (auto &desc : field_descriptors_) {
-    scene_delegate_->GetRenderIndex().InsertBprim(
-        desc.fieldPrimType, scene_delegate_, desc.fieldId);
-    ID_LOGN("Volume field %s", desc.fieldId.GetText());
+  const FluidModifierData *fmd = fluid_gas_domain_modifier(object, ctx.depsgraph);
+  const bool is_volume_data = (object->type == OB_VOLUME);
+  if (!is_volume_data && !fmd) {
+    return false;
   }
+
+  Vector<VolumeFieldDescriptor> fields;
+  std::string filepath;
+  pxr::GfMatrix4d geometry_xform(1.0);
+  const pxr::SdfPath volume_path = ctx.object_prim_id(object);
+
+  if (is_volume_data) {
+    pxr::GfMatrix4d ignored(1.0);
+    filepath = build_volume_fields_from_datablock(
+        object, volume_path, ctx.bmain, &ignored, &fields);
+  }
+  else {
+    filepath = build_volume_fields_from_modifier(
+        object, fmd, ctx.scene->r.cfra, volume_path, &geometry_xform, &fields);
+  }
+
+  /* Volume datablock with no grids: nothing to publish. */
+  if (fields.is_empty()) {
+    return true;
+  }
+
+  /* TODO: support multi-material volumes. */
+  const Material *material = (BKE_object_material_count_eval(object) > 0) ?
+                                 BKE_object_material_get_eval(const_cast<Object *>(object), 1) :
+                                 nullptr;
+  const EmittedMaterial *mat_entry = ctx.get_or_create_material(material);
+  const pxr::SdfPath material_path = mat_entry ? mat_entry->path : pxr::SdfPath();
+
+  const pxr::GfMatrix4d transform = geometry_xform *
+                                    gf_matrix_from_transform(object->object_to_world().ptr());
+  pxr::HdContainerDataSourceHandle prim_ds = build_volume_prim_data_source(
+      fields.as_span(), transform, true, material_path);
+  ctx.emit_object_prim(
+      emitted, volume_path, pxr::HdPrimTypeTokens->volume, prim_ds, geometry_xform);
+
+  for (const VolumeFieldDescriptor &f : fields) {
+    pxr::HdContainerDataSourceHandle field_ds = build_volume_field_data_source(filepath, f.name);
+    ctx.emit_object_prim(emitted, f.field_path, pxr::UsdVolImagingTokens->openvdbAsset, field_ds);
+  }
+  return true;
 }
 
-void VolumeData::remove()
+void emit_volume_dupli(PopulateContext &ctx, const Object *source, const float dupli_mat[4][4])
 {
-  for (auto &desc : field_descriptors_) {
-    ID_LOG("%s", desc.fieldId.GetText());
-    scene_delegate_->GetRenderIndex().RemoveBprim(desc.fieldPrimType, desc.fieldId);
-  }
-  ID_LOG("");
-  scene_delegate_->GetRenderIndex().RemoveRprim(prim_id);
-}
-
-void VolumeData::update()
-{
-  const Object *object = id_cast<const Object *>(id);
-  pxr::HdDirtyBits bits = pxr::HdChangeTracker::Clean;
-  if ((id->recalc & ID_RECALC_GEOMETRY) || (object->data->recalc & ID_RECALC_GEOMETRY)) {
-    init();
-    bits = pxr::HdChangeTracker::AllDirty;
-  }
-  if (id->recalc & ID_RECALC_SHADING) {
-    write_materials();
-    bits |= pxr::HdChangeTracker::DirtyMaterialId | pxr::HdChangeTracker::DirtyDoubleSided;
-  }
-  if (id->recalc & ID_RECALC_TRANSFORM) {
-    write_transform();
-    bits |= pxr::HdChangeTracker::DirtyTransform;
-  }
-
-  if (bits == pxr::HdChangeTracker::Clean) {
+  const int idx = ctx.nonmesh_instance_count.lookup_default(source, 0);
+  ctx.nonmesh_instance_count.add_overwrite(source, idx + 1);
+  const pxr::SdfPath volume_path = ctx.instance_clone_prim_id(source, idx);
+  Vector<VolumeFieldDescriptor> fields;
+  pxr::GfMatrix4d unused(1.0);
+  const std::string filepath = build_volume_fields_from_datablock(
+      source, volume_path, ctx.bmain, &unused, &fields);
+  if (fields.is_empty()) {
     return;
   }
-
-  scene_delegate_->GetRenderIndex().GetChangeTracker().MarkRprimDirty(prim_id, bits);
-  ID_LOGN("");
-}
-
-pxr::VtValue VolumeData::get_data(pxr::TfToken const &key) const
-{
-  if (key == pxr::HdVolumeFieldSchemaTokens->filePath) {
-    return pxr::VtValue(pxr::SdfAssetPath(filepath_, filepath_));
+  const Material *material = (BKE_object_material_count_eval(source) > 0) ?
+                                 BKE_object_material_get_eval(const_cast<Object *>(source), 1) :
+                                 nullptr;
+  const EmittedMaterial *mat_entry = ctx.get_or_create_material(material);
+  const pxr::SdfPath material_path = mat_entry ? mat_entry->path : pxr::SdfPath();
+  const pxr::GfMatrix4d transform = gf_matrix_from_transform(dupli_mat);
+  pxr::HdContainerDataSourceHandle prim_ds = build_volume_prim_data_source(
+      fields.as_span(), transform, true, material_path);
+  ctx.emit_prim(volume_path, pxr::HdPrimTypeTokens->volume, prim_ds);
+  for (const VolumeFieldDescriptor &f : fields) {
+    pxr::HdContainerDataSourceHandle field_ds = build_volume_field_data_source(filepath, f.name);
+    ctx.emit_prim(f.field_path, pxr::UsdVolImagingTokens->openvdbAsset, field_ds);
   }
-  if (key == pxr::HdVolumeFieldSchemaTokens->fieldIndex) {
-    return pxr::VtValue(0);
-  }
-  if (key == pxr::UsdHydraTokens->textureMemory) {
-    return pxr::VtValue(0.0f);
-  }
-  return pxr::VtValue();
-}
-
-pxr::VtValue VolumeData::get_data(pxr::SdfPath const &id, pxr::TfToken const &key) const
-{
-  if (key == pxr::HdVolumeFieldSchemaTokens->fieldName) {
-    std::string name = id.GetName();
-    return pxr::VtValue(pxr::TfToken(name.substr(name.find("VF_") + 3)));
-  }
-
-  return get_data(key);
-}
-
-void VolumeData::available_materials(Set<pxr::SdfPath> &paths) const
-{
-  if (mat_data_ && !mat_data_->prim_id.IsEmpty()) {
-    paths.add(mat_data_->prim_id);
-  }
-}
-
-MaterialData *VolumeData::get_material_data(pxr::SdfPath const & /*id*/) const
-{
-  return mat_data_;
-}
-
-pxr::HdVolumeFieldDescriptorVector VolumeData::field_descriptors() const
-{
-  return field_descriptors_;
-}
-
-void VolumeData::write_materials()
-{
-  const Object *object = id_cast<Object *>(const_cast<ID *>(id));
-  const Material *mat = nullptr;
-  /* TODO: Using only first material. Add support for multi-material. */
-  if (BKE_object_material_count_eval(object) > 0) {
-    mat = BKE_object_material_get_eval(const_cast<Object *>(object), 0);
-  }
-  mat_data_ = get_or_create_material(mat);
 }
 
 }  // namespace blender::io::hydra

source/blender/io/usd/CMakeLists.txt
@@ -167,30 +167,29 @@ if(WITH_HYDRA)
   list(APPEND SRC
     hydra/camera_delegate.cc
     hydra/curves.cc
-    hydra/hydra_scene_delegate.cc
-    hydra/id.cc
+    hydra/scene_index.cc
     hydra/image.cc
     hydra/instancer.cc
     hydra/light.cc
     hydra/material.cc
     hydra/mesh.cc
-    hydra/object.cc
-    hydra/usd_scene_delegate.cc
+    hydra/populate_context.cc
+    hydra/usd_scene_index.cc
     hydra/volume.cc
     hydra/volume_modifier.cc
     hydra/world.cc
 
     hydra/camera_delegate.hh
     hydra/curves.hh
-    hydra/hydra_scene_delegate.hh
-    hydra/id.hh
+    hydra/scene_index.hh
     hydra/image.hh
     hydra/instancer.hh
     hydra/light.hh
     hydra/material.hh
     hydra/mesh.hh
-    hydra/object.hh
-    hydra/usd_scene_delegate.hh
+    hydra/populate_context.hh
+    hydra/usd_scene_index.hh
+    hydra/util.hh
     hydra/volume.hh
     hydra/volume_modifier.hh
     hydra/world.hh

source/blender/io/usd/hydra/camera_delegate.cc
@@ -10,63 +10,93 @@
 
 #include "BKE_idprop.hh"
 
-#include <pxr/imaging/hd/camera.h>
+#include "BLI_listbase.h"
+
+#include <pxr/imaging/hd/dataSource.h>
+#include <pxr/imaging/hd/overlayContainerDataSource.h>
+#include <pxr/imaging/hd/retainedDataSource.h>
+#include <pxr/imaging/hd/sceneIndexObserver.h>
+#include <pxr/imaging/hd/tokens.h>
 
 namespace blender::io::hydra {
 
-static pxr::VtValue vt_value(const IDProperty *prop)
-{
-  switch (prop->type) {
-    case IDP_INT:
-      return pxr::VtValue{IDP_int_get(prop)};
-    case IDP_FLOAT:
-      return pxr::VtValue{IDP_float_get(prop)};
-    case IDP_DOUBLE:
-      return pxr::VtValue{IDP_double_get(prop)};
-    case IDP_BOOLEAN:
-      return pxr::VtValue{bool(IDP_bool_get(prop))};
-    default:
-      break;
-  }
-  return pxr::VtValue{};
-}
+class BlenderCameraIDPropertiesDataSource : public pxr::HdContainerDataSource {
+ public:
+  HD_DECLARE_DATASOURCE(BlenderCameraIDPropertiesDataSource);
 
-CameraDelegate::CameraDelegate(pxr::HdRenderIndex *render_index, pxr::SdfPath const &delegate_id)
-    : pxr::HdxFreeCameraSceneDelegate{render_index, delegate_id}
+  void set_camera(const Camera *camera)
+  {
+    camera_ = camera;
+  }
+
+  pxr::TfTokenVector GetNames() override
+  {
+    pxr::TfTokenVector result;
+    if (camera_ && camera_->id.properties) {
+      for (const IDProperty &prop : camera_->id.properties->data.group) {
+        result.push_back(pxr::TfToken(prop.name));
+      }
+    }
+    return result;
+  }
+
+  pxr::HdDataSourceBaseHandle Get(const pxr::TfToken &name) override
+  {
+    if (!camera_ || !camera_->id.properties) {
+      return nullptr;
+    }
+    const IDProperty *prop = IDP_GetPropertyFromGroup(camera_->id.properties, name.GetText());
+    if (!prop) {
+      return nullptr;
+    }
+    switch (prop->type) {
+      case IDP_INT:
+        return pxr::HdRetainedTypedSampledDataSource<int>::New(IDP_int_get(prop));
+      case IDP_FLOAT:
+        return pxr::HdRetainedTypedSampledDataSource<float>::New(IDP_float_get(prop));
+      case IDP_DOUBLE:
+        return pxr::HdRetainedTypedSampledDataSource<double>::New(IDP_double_get(prop));
+      case IDP_BOOLEAN:
+        return pxr::HdRetainedTypedSampledDataSource<bool>::New(bool(IDP_bool_get(prop)));
+      default:
+        return nullptr;
+    }
+  }
+
+ private:
+  const Camera *camera_ = nullptr;
+};
+
+CameraDelegate::CameraDelegate(pxr::HdRenderIndex *render_index, pxr::SdfPath const &camera_id)
+    : camera_scene_index_(pxr::HdRetainedSceneIndex::New()),
+      camera_id_(camera_id),
+      free_camera_ds_(pxr::HdxFreeCameraPrimDataSource::New()),
+      id_properties_ds_(BlenderCameraIDPropertiesDataSource::New())
 {
+  pxr::HdContainerDataSourceHandle camera_ds = pxr::HdOverlayContainerDataSource::New(
+      id_properties_ds_, free_camera_ds_);
+
+  camera_scene_index_->AddPrims({{camera_id_, pxr::HdPrimTypeTokens->camera, camera_ds}});
+
+  render_index->InsertSceneIndex(
+      camera_scene_index_, pxr::SdfPath::AbsoluteRootPath(), /*needsPrefixing=*/false);
 }
 
 void CameraDelegate::sync(const Scene *scene)
 {
-  if (!scene || !scene->camera) {
-    return;
-  }
-
-  const Camera *camera = id_cast<const Camera *>(scene->camera->data);
-  if (camera_ == camera) {
-    return;
-  }
-
-  camera_ = camera;
-  GetRenderIndex().GetChangeTracker().MarkSprimDirty(GetCameraId(), pxr::HdCamera::DirtyParams);
+  const Camera *camera = (scene && scene->camera) ? id_cast<const Camera *>(scene->camera->data) :
+                                                    nullptr;
+  id_properties_ds_->set_camera(camera);
+  camera_scene_index_->DirtyPrims({{camera_id_, pxr::HdDataSourceLocator::EmptyLocator()}});
 }
 
-void CameraDelegate::update(const ID *camera)
+void CameraDelegate::SetCamera(pxr::GfCamera const &camera)
 {
-  if (&camera_->id == camera) {
-    GetRenderIndex().GetChangeTracker().MarkSprimDirty(GetCameraId(), pxr::HdCamera::DirtyParams);
+  pxr::HdDataSourceLocatorSet dirty_locators;
+  free_camera_ds_->SetCamera(camera, &dirty_locators);
+  if (!dirty_locators.IsEmpty()) {
+    camera_scene_index_->DirtyPrims({{camera_id_, dirty_locators}});
   }
-}
-
-pxr::VtValue CameraDelegate::GetCameraParamValue(pxr::SdfPath const &id, pxr::TfToken const &key)
-{
-  if (camera_ && camera_->id.properties) {
-    const IDProperty *prop = IDP_GetPropertyFromGroup(camera_->id.properties, key.GetText());
-    if (prop) {
-      return vt_value(prop);
-    }
-  }
-  return pxr::HdxFreeCameraSceneDelegate::GetCameraParamValue(id, key);
 }
 
 }  // namespace blender::io::hydra

source/blender/io/usd/hydra/camera_delegate.hh
@@ -4,28 +4,39 @@
 
 #pragma once
 
-#include <pxr/imaging/hdx/freeCameraSceneDelegate.h>
+#include <pxr/base/gf/camera.h>
+#include <pxr/imaging/hd/renderIndex.h>
+#include <pxr/imaging/hd/retainedSceneIndex.h>
+#include <pxr/imaging/hdx/freeCameraPrimDataSource.h>
 
 namespace blender {
 
 struct Camera;
-struct ID;
 struct Scene;
 
 namespace io::hydra {
 
-class CameraDelegate : public pxr::HdxFreeCameraSceneDelegate {
+/* Camera prim conforming to HdCameraSchema and HdXformSchema through a retained
+ * scene index. */
+
+class BlenderCameraIDPropertiesDataSource;
+
+class CameraDelegate {
  public:
-  CameraDelegate(pxr::HdRenderIndex *render_index, pxr::SdfPath const &delegate_id);
-  ~CameraDelegate() override = default;
+  CameraDelegate(pxr::HdRenderIndex *render_index, pxr::SdfPath const &camera_id);
 
   void sync(const Scene *scene);
-  void update(const ID *camera);
-
-  pxr::VtValue GetCameraParamValue(pxr::SdfPath const &id, pxr::TfToken const &key) override;
+  void SetCamera(pxr::GfCamera const &camera);
+  pxr::SdfPath const &GetCameraId() const
+  {
+    return camera_id_;
+  }
 
  private:
-  const Camera *camera_{nullptr};
+  pxr::HdRetainedSceneIndexRefPtr camera_scene_index_;
+  pxr::SdfPath camera_id_;
+  pxr::HdxFreeCameraPrimDataSource::Handle free_camera_ds_;
+  std::shared_ptr<BlenderCameraIDPropertiesDataSource> id_properties_ds_;
 };
 
 }  // namespace io::hydra

source/blender/io/usd/hydra/curves.hh
@@ -4,75 +4,25 @@
 
 #pragma once
 
-#include <pxr/base/vt/types.h>
-#include <pxr/imaging/hd/sceneDelegate.h>
-
-#include "BLI_set.hh"
-
-#include "material.hh"
-#include "object.hh"
-
 namespace blender {
 
-struct ParticleSystem;
+struct Object;
 
 namespace io::hydra {
 
-class HydraSceneDelegate;
+struct BObjectInfo;
+struct EmittedObject;
+struct PopulateContext;
 
-class CurvesData : public ObjectData {
- protected:
-  pxr::VtIntArray curve_vertex_counts_;
-  pxr::VtVec3fArray vertices_;
-  pxr::VtVec2fArray uvs_;
-  pxr::VtFloatArray widths_;
+/* Emit a curves object as an Rprim. */
+void emit_curves_object(PopulateContext &ctx, const BObjectInfo &info, EmittedObject &emitted);
+/* Emit a curves instancer prototype. */
+void emit_curves_proto(PopulateContext &ctx, const BObjectInfo &info);
 
-  MaterialData *mat_data_ = nullptr;
-
- public:
-  CurvesData(HydraSceneDelegate *scene_delegate,
-             const Object *object,
-             pxr::SdfPath const &prim_id);
-
-  void init() override;
-  void insert() override;
-  void remove() override;
-  void update() override;
-
-  pxr::VtValue get_data(pxr::TfToken const &key) const override;
-  void available_materials(Set<pxr::SdfPath> &paths) const override;
-
-  pxr::HdBasisCurvesTopology topology() const;
-  pxr::HdPrimvarDescriptorVector primvar_descriptors(pxr::HdInterpolation interpolation) const;
-
-  MaterialData *get_material_data(pxr::SdfPath const &id) const override;
-
- protected:
-  void write_materials() override;
-  virtual void write_curves();
-};
-
-class HairData : public CurvesData {
- private:
-  ParticleSystem *particle_system_;
-
- public:
-  HairData(HydraSceneDelegate *scene_delegate,
-           const Object *object,
-           pxr::SdfPath const &prim_id,
-           ParticleSystem *particle_system);
-
-  static bool is_supported(const ParticleSystem *particle_system);
-  static bool is_visible(HydraSceneDelegate *scene_delegate,
-                         Object *object,
-                         ParticleSystem *particle_system);
-
-  void update() override;
-
- protected:
-  void write_transform() override;
-  void write_curves() override;
-};
+/* Emit particular hair as an Rprim. */
+void emit_hair_for_object(PopulateContext &ctx, Object *object, EmittedObject &emitted);
+/* Emit a hair instancer prototype. */
+void emit_hair_proto(PopulateContext &ctx, Object *source);
 
 }  // namespace io::hydra
 }  // namespace blender

commit f965d4c1328839ef7011a5727b102a04cced7039

Fix: Hydra: Missing volume material after update
Needs to be countered in emitted materials for it to be preserved.

Issue introduced in 590af512e32.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/io/usd/hydra/volume.cc
@@ -149,6 +149,9 @@ bool emit_volume_object(PopulateContext &ctx, const Object *object, EmittedObjec
                                  nullptr;
   const EmittedMaterial *mat_entry = ctx.get_or_create_material(material);
   const pxr::SdfPath material_path = mat_entry ? mat_entry->path : pxr::SdfPath();
+  if (material) {
+    emitted.materials.append_non_duplicates(material);
+  }
 
   const pxr::GfMatrix4d transform = geometry_xform *
                                     gf_matrix_from_transform(object->object_to_world().ptr());