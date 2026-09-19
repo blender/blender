blender/blender

commit 91bef2dd218d2449d9790a30e12598995cd046c0

Fix: Texture Paint: Wrong blending for Add and other blend modes
Match the projection paint formula, with the previous code mix modes
like Add would have an effect even when factor is zero. This was wrong
since the initial experimental 3D brush implementation.

Issue became more noticable after f261bc918f2 since it now skips zero
factors.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/editors/sculpt_paint/mesh/paint_image_3d.cc
@@ -239,15 +239,15 @@ BLI_INLINE float4 paint_blend_pixel(const float4 &brush_color,
                                     const float factor,
                                     const float4 color)
 {
+  const float4 paint_color = brush_color * (factor * brush_alpha);
+
   float4 result;
-  blend_color_mix_float(result, color, brush_color * factor);
-  result *= brush_alpha;
   /* TODO: try making IMB_blend_color_float inline instead. */
   if (is_mix) {
-    blend_color_mix_float(result, color, result);
+    blend_color_mix_float(result, color, paint_color);
   }
   else {
-    IMB_blend_color_float(result, color, result, blend_mode);
+    IMB_blend_color_float(result, color, paint_color, blend_mode);
   }
   return result;
 }

commit 0a4e7ca2eff41a92291a0e89ba201ff6d097ea32

Fix: Texture Paint: Use of uninitialized alpha in paint blending
Issue introduced in 91bef2dd218.

Pull Request: https://projects.blender.org/blender/blender/pulls/164157

source/blender/editors/sculpt_paint/mesh/paint_image_3d.cc
@@ -240,8 +240,8 @@ BLI_INLINE float4 paint_blend_pixel(const float4 &brush_color,
                                     const float4 color)
 {
   const float4 paint_color = brush_color * (factor * brush_alpha);
-
-  float4 result;
+  /* Many blend modes don't write alpha to the result, so copy it. */
+  float4 result = color;
   /* TODO: try making IMB_blend_color_float inline instead. */
   if (is_mix) {
     blend_color_mix_float(result, color, paint_color);