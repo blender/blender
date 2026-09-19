blender/blender

commit 2a1ca5553bac329e8c55502e83e45f0fd2eacd57

Fix #159873: Potential crash in 2D image painting
Introduced with 3bd35d9a4b22493519079d808bf70eecaf523fdc

Pull Request: https://projects.blender.org/blender/blender/pulls/159884

source/blender/editors/sculpt_paint/mesh/paint_image_2d.cc
@@ -1258,13 +1258,15 @@ static void paint_2d_do_making_brush(ImagePaintState *s,
       int origx = region->destx - tx * ED_IMAGE_UNDO_TILE_SIZE;
       int origy = region->desty - ty * ED_IMAGE_UNDO_TILE_SIZE;
 
-      const ImBuf *data = ED_image_paint_tile_find(
-          undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false);
-      if (tile->canvas->float_data()) {
-        tmpbuf.float_buffer = data->float_buffer;
-      }
-      else {
-        tmpbuf.byte_buffer = data->byte_buffer;
+      if (const ImBuf *data = ED_image_paint_tile_find(
+              undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false))
+      {
+        if (tile->canvas->float_data()) {
+          tmpbuf.float_buffer = data->float_buffer;
+        }
+        else {
+          tmpbuf.byte_buffer = data->byte_buffer;
+        }
       }
 
       IMB_rectblend(tile->canvas,

commit 3f833fbb177aca27ed8d2c2355848f333c75a986

Cleanup: Texture Paint: Avoid potential future null dereference
This code relied on IMB_rectblend not dereferencing the pointer when the
region is empty. Better to not run it all since that is a weak assumption.
Issue introduced in 2a1ca5553ba.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/editors/sculpt_paint/mesh/paint_image_2d.cc
@@ -1269,15 +1269,16 @@ static void paint_2d_do_making_brush(ImagePaintState *s,
       int origx = region->destx - tx * ED_IMAGE_UNDO_TILE_SIZE;
       int origy = region->desty - ty * ED_IMAGE_UNDO_TILE_SIZE;
 
-      if (const ImBuf *data = ED_image_paint_tile_find(
-              undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false))
-      {
-        if (tile->canvas->float_data()) {
-          tmpbuf.float_buffer = data->float_buffer;
-        }
-        else {
-          tmpbuf.byte_buffer = data->byte_buffer;
-        }
+      const ImBuf *data = ED_image_paint_tile_find(
+          undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false);
+      if (data == nullptr) {
+        continue;
+      }
+      if (tile->canvas->float_data()) {
+        tmpbuf.float_buffer = data->float_buffer;
+      }
+      else {
+        tmpbuf.byte_buffer = data->byte_buffer;
       }
 
       IMB_rectblend(tile->canvas,