blender/blender

commit 1152d37e4c9b894ba8b575cb37aae3ecefdfa2bb

Fix: Images: Excessive threading overhead in GPU partial update
Better would be to move such heuristics into the scaling code itself but
that is more involved due to TBB task isolation concerns. This fixes a
regression in recent changes with partial update, and keeps threading for
large partial updates.

Issue introduced in ca1da38655.

Pull Request: https://projects.blender.org/blender/blender/pulls/161462

source/blender/imbuf/intern/util_gpu.cc
@@ -302,13 +302,16 @@ static void get_gpu_texture_data(ImBuf *source_buffer,
       return;
     }
 
+    /* Avoid excessive overhead with small updates. */
+    const bool threaded = size.x >= 1024;
+
     if (is_float) {
       IMB_scale_box(static_cast<const float *>(r_upload.data),
                     size,
                     channels,
                     buffer->float_data_for_write(),
                     *scaled_size,
-                    true,
+                    threaded,
                     r_upload.stride);
     }
     else {
@@ -317,7 +320,7 @@ static void get_gpu_texture_data(ImBuf *source_buffer,
                     channels,
                     buffer->byte_data_for_write(),
                     *scaled_size,
-                    true,
+                    threaded,
                     r_upload.stride);
     }
     if (r_upload.tmp_ibuf) {

commit bda8f984cfb7162bb55c499a4ac3a2527d8543c5

Fix: Images: GPU texture scaling not threaded well for all sizes
Use full image resolution rather than just width.

Issue introduced in 1152d37e4c9.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/imbuf/intern/util_gpu.cc
@@ -305,7 +305,7 @@ static void get_gpu_texture_data(ImBuf *source_buffer,
     }
 
     /* Avoid excessive overhead with small updates. */
-    const bool threaded = size.x >= 1024;
+    const bool threaded = int64_t(size.x) * size.y >= 512 * 512;
 
     if (is_float) {
       IMB_scale_box(static_cast<const float *>(r_upload.data),