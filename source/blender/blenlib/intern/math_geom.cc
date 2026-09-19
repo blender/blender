blender/blender

commit 27fff6caa20c3af6855ca5f82b96be1dbd9927e7

Fix #161705: Proportional connected edit affects vertices beyond radius
The geodesic distance approximation estimates a virtual source point
from the known distances at the two vertices of an edge being crossed.
With particular distorted topology, the estimation of this virtual
source point can fail and give a distance smaller than the two edge
vertices.

Reject such small distances, and fall back to Dijkstra approximation.

Pull Request: https://projects.blender.org/blender/blender/pulls/161812

source/blender/blenlib/intern/math_geom.cc
@@ -5685,7 +5685,15 @@ float geodesic_distance_propagate_across_triangle(
          * the edge between v1 and v2. */
         const float x_intercept = S_[0] + h * (v0_[0] - S_[0]) / (v0_[1] + h);
         if (x_intercept >= 0.0f && x_intercept <= d12) {
-          return len_v2v2(S_, v0_);
+          const float dist0 = len_v2v2(S_, v0_);
+
+          /* Only valid if the wavefront reaches v0 after both v1 and v2, as it has to
+           * travel through the edge between them. Otherwise dist1 and dist2 did not
+           * originate from a common source, and the virtual source point is bogus and
+           * can give a distance much shorter than the actual one. */
+          if (dist0 >= std::max(dist1, dist2)) {
+            return dist0;
+          }
         }
       }
     }

commit f27eaaa037292a26b247479a0d68bd457a1d6202

Fix: Modeling: Proportional connected edit less accurate than needed
The virtual source point being closer than only one was too strict,
and rejected some valid paths.

Issue introduced in 27fff6caa20.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/blenlib/intern/math_geom.cc
@@ -5727,11 +5727,10 @@ float geodesic_distance_propagate_across_triangle(
         if (x_intercept >= 0.0f && x_intercept <= d12) {
           const float dist0 = len_v2v2(S_, v0_);
 
-          /* Only valid if the wavefront reaches v0 after both v1 and v2, as it has to
-           * travel through the edge between them. Otherwise dist1 and dist2 did not
-           * originate from a common source, and the virtual source point is bogus and
-           * can give a distance much shorter than the actual one. */
-          if (dist0 >= std::max(dist1, dist2)) {
+          /* Reject the distance when v0 is closer to the virtual source point than both
+           * v1 and v2. This is a heuristic to detect when v1 and v2 did not originate from
+           * the same source, and the virtual source point is bogus. */
+          if (dist0 >= std::min(dist1, dist2)) {
             return dist0;
           }
         }