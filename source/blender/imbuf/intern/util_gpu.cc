blender/blender

commit 8c210523ed9770c62b1eeae787a10da970b6acbc

Refactor: Images: Make partial update marking thread safe
To prepare for render threads doing this on render image buffers.

Pull Request: https://projects.blender.org/blender/blender/pulls/161094

source/blender/imbuf/intern/util_gpu.cc
@@ -773,6 +773,8 @@ gpu::Texture *IMB_acquire_gpu_texture(const char *name,
     return nullptr;
   }
 
+  const int64_t changeset_id = IMB_partial_update_changeset_id_next();
+
   GPUTextureCreateFlags create_flags = GPUTextureCreateFlags::EnableMipmaps;
   if (use_high_bitdepth) {
     create_flags |= GPUTextureCreateFlags::HighBitDepth;
@@ -802,6 +804,7 @@ gpu::Texture *IMB_acquire_gpu_texture(const char *name,
     GPU_texture_mipmap_mode(tex, false, true);
   }
 
+  ibuf->gpu.partial_update_changeset = changeset_id;
   ibuf->gpu.texture = tex;
   ibuf->gpu.lastused = BLI_time_now_seconds_i();
   GPU_texture_ref(tex);
@@ -853,6 +856,7 @@ void IMB_assign_gpu_texture(ImBuf *ibuf, gpu::Texture *texture)
     ibuf->gpu.texture = nullptr;
   }
   ibuf->gpu.flag = ImBufGPUFlag(0);
+  ibuf->gpu.partial_update_changeset = IMB_partial_update_changeset_id_current();
   ibuf->gpu.texture = texture;
 }
 

source/blender/imbuf/IMB_imbuf_types.hh
@@ -226,6 +226,7 @@ struct ImBuf {
 
   /** Partial update tracking for GPU textures and image drawing. */
   imbuf::partial_update::Tracker *partial_update = nullptr;
+  Mutex partial_update_mutex;
 
   /** Resolution in pixels per meter. Multiply by `0.0254` for DPI. */
   double ppm[2] = {0.0, 0.0};

source/blender/imbuf/intern/partial_update.cc
@@ -2,6 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <mutex>
 #include <optional>
 
 #include "IMB_imbuf_types.hh"
@@ -9,6 +10,7 @@
 
 #include "BLI_bit_vector.hh"
 #include "BLI_math_base_c.hh"
+#include "BLI_mutex.hh"
 #include "BLI_rect.hh"
 #include "BLI_vector.hh"
 
@@ -270,8 +272,15 @@ struct Tracker {
 static Tracker &tracker_ensure(ImBuf *ibuf)
 {
   if (ibuf->partial_update == nullptr) {
-    Tracker *tracker = MEM_new<Tracker>(__func__, IMB_partial_update_changeset_id_next());
-    ibuf->partial_update = tracker;
+    /* Allocate new changeset ID for new tracker, needed to disambiguate two ImBufs that happened
+     * to be allocated at the same memory address.
+     *
+     * The GPU changeset ID is used if there is already a GPU texture, so GPU texture consumers
+     * do not get unnecessarily updated. */
+    const int64_t gpu_changeset_id = ibuf->gpu.partial_update_changeset;
+    const int64_t changeset_id = gpu_changeset_id >= 0 ? gpu_changeset_id :
+                                                         IMB_partial_update_changeset_id_next();
+    ibuf->partial_update = MEM_new<Tracker>(__func__, changeset_id);
   }
   return *ibuf->partial_update;
 }
@@ -279,6 +288,15 @@ static Tracker &tracker_ensure(ImBuf *ibuf)
 }  // namespace imbuf::partial_update
 
 using namespace imbuf::partial_update;
+
+/**
+ * Check if update tracking is needed, when either there is a tracker or a
+ * GPU texture exists. To avoid unnecessary memory/performance overhead.
+ */
+static bool imb_track_updates(const ImBuf *ibuf)
+{
+  return ibuf->partial_update != nullptr || ibuf->gpu.texture != nullptr;
+}
 
 int64_t IMB_partial_update_changeset_id_next()
 {
@@ -292,6 +310,10 @@ int64_t IMB_partial_update_changeset_id_current()
 
 void IMB_partial_update_mark_region(ImBuf *ibuf, const rcti &region)
 {
+  if (!imb_track_updates(ibuf)) {
+    return;
+  }
+  std::scoped_lock lock(ibuf->partial_update_mutex);
   Tracker &tracker = tracker_ensure(ibuf);
   tracker.update_resolution(ibuf);
   tracker.mark_region(region);
@@ -299,12 +321,17 @@ void IMB_partial_update_mark_region(ImBuf *ibuf, const rcti &region)
 
 void IMB_partial_update_mark_full(ImBuf *ibuf)
 {
+  if (!imb_track_updates(ibuf)) {
+    return;
+  }
+  std::scoped_lock lock(ibuf->partial_update_mutex);
   Tracker &tracker = tracker_ensure(ibuf);
   tracker.mark_full_update();
 }
 
 void IMB_partial_update_flush(ImBuf *ibuf)
 {
+  std::scoped_lock lock(ibuf->partial_update_mutex);
   Tracker &tracker = tracker_ensure(ibuf);
   tracker.update_resolution(ibuf);
   tracker.ensure_empty_changeset();
@@ -312,6 +339,7 @@ void IMB_partial_update_flush(ImBuf *ibuf)
 
 Changes IMB_partial_update_collect(ImBuf *ibuf, const int64_t last_changeset_id)
 {
+  std::scoped_lock lock(ibuf->partial_update_mutex);
   Tracker &tracker = tracker_ensure(ibuf);
 
   Changes changes;

commit 6fed2c74005c3434fd2be60247e3489fa02f1bff

Fix: Images: Image editor shows outdated image with assigned GPU texture
Should have used next instead of current here. Unclear if there was a bug in
practice since other operations likely increased this counter.

Issue introduced in 8c210523ed9.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/imbuf/intern/util_gpu.cc
@@ -841,6 +841,7 @@ void IMB_free_gpu_textures(ImBuf *ibuf)
     ibuf->gpu.texture = nullptr;
   }
   ibuf->gpu.flag &= ~IMB_GPU_LOAD_FAILED;
+  ibuf->gpu.partial_update_changeset = -1;
 }
 
 void IMB_assign_gpu_texture(ImBuf *ibuf, gpu::Texture *texture)
@@ -855,7 +856,7 @@ void IMB_assign_gpu_texture(ImBuf *ibuf, gpu::Texture *texture)
     ibuf->gpu.texture = nullptr;
   }
   ibuf->gpu.flag &= ~IMB_GPU_LOAD_FAILED;
-  ibuf->gpu.partial_update_changeset = IMB_partial_update_changeset_id_current();
+  ibuf->gpu.partial_update_changeset = texture ? IMB_partial_update_changeset_id_next() : -1;
   ibuf->gpu.texture = texture;
 }
 