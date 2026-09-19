blender/blender

commit bd6b9568c16cc61f6ccfff9096d80ab8fe586128

Refactor: Change most ListBase to typed ListBaseT
Besides changing the type, this also has the following types of changes:
* Add includes and forward declarations for ListBaseT.
* struct ListBase to ListBase.
* Remove comments about link type that are no longer be needed.
* Use cast<ID>() and cast<Link>()
* Reorder some struct definitions.

This reduces the number of untyped ListBase instances from 3275 to 184.
The remaining ones are:
* BLI ListBase API and related utility functions
* Custom listbase code in lineart and RNA
* Blend read and write of ListBase

Pull Request: https://projects.blender.org/blender/blender/pulls/152054

source/blender/blenkernel/intern/collection.cc
@@ -43,6 +43,7 @@
 #include "DNA_ID.h"
 #include "DNA_collection_types.h"
 #include "DNA_layer_types.h"
+#include "DNA_listBase.h"
 #include "DNA_object_types.h"
 #include "DNA_rigidbody_types.h"
 #include "DNA_scene_types.h"
@@ -517,13 +518,14 @@ void BKE_collection_free_data(Collection *collection)
   collection_free_data(&collection->id);
 }
 
-void BKE_collection_exporter_name_set(const ListBase *exporters,
+void BKE_collection_exporter_name_set(const ListBaseT<CollectionExport> *exporters,
                                       CollectionExport *data,
                                       const char *newname)
 {
   /* Only use the new name if it's not empty. */
   if (newname && newname[0] != '\0') {
-    const ListBase list = exporters ? *exporters : BLI_listbase_from_link((Link *)data);
+    ListBaseT<CollectionExport> list = exporters ? *exporters :
+                                                   ListBaseT<CollectionExport>{data, data};
 
     STRNCPY(data->name, newname);
     BLI_uniquename(
@@ -835,7 +837,7 @@ const char *BKE_collection_ui_name_get(Collection *collection)
 /** \name Object List Cache
  * \{ */
 
-static void collection_object_cache_fill(ListBase *lb,
+static void collection_object_cache_fill(ListBaseT<Base> *lb,
                                          Collection *collection,
                                          int parent_restrict,
                                          bool with_instances)
@@ -871,7 +873,7 @@ static void collection_object_cache_fill(ListBase *lb,
   }
 }
 
-ListBase BKE_collection_object_cache_get(Collection *collection)
+ListBaseT<Base> BKE_collection_object_cache_get(Collection *collection)
 {
   if (!(collection->flag & COLLECTION_HAS_OBJECT_CACHE)) {
     static blender::Mutex cache_lock;
@@ -886,7 +888,7 @@ ListBase BKE_collection_object_cache_get(Collection *collection)
   return collection->runtime->object_cache;
 }
 
-ListBase BKE_collection_object_cache_instanced_get(Collection *collection)
+ListBaseT<Base> BKE_collection_object_cache_instanced_get(Collection *collection)
 {
   if (!(collection->flag & COLLECTION_HAS_OBJECT_CACHE_INSTANCED)) {
     static blender::Mutex cache_lock;
@@ -1047,7 +1049,7 @@ static bool collection_object_cyclic_check_internal(Object *object, Collection *
 bool BKE_collection_object_cyclic_check(Main *bmain, Object *object, Collection *collection)
 {
   /* first flag all collections */
-  BKE_main_id_tag_listbase(&bmain->collections, ID_TAG_DOIT, true);
+  BKE_main_id_tag_listbase(&bmain->collections.cast<ID>(), ID_TAG_DOIT, true);
 
   return collection_object_cyclic_check_internal(object, collection);
 }
@@ -1073,7 +1075,7 @@ bool BKE_collection_has_object_recursive(Collection *collection, Object *ob)
     return false;
   }
 
-  const ListBase objects = BKE_collection_object_cache_get(collection);
+  const ListBaseT<Base> objects = BKE_collection_object_cache_get(collection);
   return BLI_findptr(&objects, ob, offsetof(Base, object));
 }
 
@@ -1083,7 +1085,7 @@ bool BKE_collection_has_object_recursive_instanced(Collection *collection, Objec
     return false;
   }
 
-  const ListBase objects = BKE_collection_object_cache_instanced_get(collection);
+  const ListBaseT<Base> objects = BKE_collection_object_cache_instanced_get(collection);
   return BLI_findptr(&objects, ob, offsetof(Base, object));
 }
 
@@ -1092,7 +1094,7 @@ bool BKE_collection_has_object_recursive_instanced_orig_id(Collection *collectio
 {
   BLI_assert(collection_eval->id.tag & ID_TAG_COPIED_ON_EVAL);
   const Object *ob_orig = DEG_get_original(object_eval);
-  const ListBase objects = BKE_collection_object_cache_instanced_get(collection_eval);
+  const ListBaseT<Base> objects = BKE_collection_object_cache_instanced_get(collection_eval);
   LISTBASE_FOREACH (Base *, base, &objects) {
     if (DEG_get_original(base->object) == ob_orig) {
       return true;
@@ -1494,7 +1496,7 @@ CollectionExport *BKE_collection_exporter_add(Collection *collection, char *idna
 
 void BKE_collection_exporter_remove(Collection *collection, CollectionExport *data)
 {
-  ListBase *exporters = &collection->exporters;
+  ListBaseT<CollectionExport> *exporters = &collection->exporters;
   BLI_remlink(exporters, data);
   BKE_collection_exporter_free_data(data);
 
@@ -2518,7 +2520,7 @@ void BKE_scene_objects_iterator_next(BLI_Iterator *iter)
     iter->current = cob->ob;
   }
   else {
-    /* if this is the last object of this ListBase look at the next Collection */
+    /* if this is the last object of this ListBaseT look at the next Collection */
     Collection *collection;
     BKE_scene_collections_iterator_next(&data->scene_collection_iter);
     do {

source/blender/blenkernel/intern/fcurve.cc
@@ -94,7 +94,7 @@ void BKE_fcurve_free(FCurve *fcu)
   MEM_freeN(fcu);
 }
 
-void BKE_fcurves_free(ListBase *list)
+void BKE_fcurves_free(ListBaseT<FCurve> *list)
 {
   /* Sanity check. */
   if (list == nullptr) {
@@ -149,7 +149,7 @@ FCurve *BKE_fcurve_copy(const FCurve *fcu)
   return fcu_d;
 }
 
-void BKE_fcurves_copy(ListBase *dst, ListBase *src)
+void BKE_fcurves_copy(ListBaseT<FCurve> *dst, ListBaseT<FCurve> *src)
 {
   /* Sanity checks. */
   if (ELEM(nullptr, dst, src)) {
@@ -180,7 +180,7 @@ void BKE_fmodifier_name_set(FModifier *fcm, const char *name)
   /* Set default modifier name when name parameter is an empty string.
    * Ensure the name is unique. */
   const FModifierTypeInfo *fmi = get_fmodifier_typeinfo(fcm->type);
-  ListBase list = BLI_listbase_from_link((Link *)fcm);
+  ListBaseT<FModifier> list = {fcm, fcm};
   BLI_uniquename(&list,
                  fcm,
                  CTX_DATA_(BLT_I18NCONTEXT_ID_ACTION, fmi->name),
@@ -250,7 +250,7 @@ FCurve *id_data_find_fcurve(
   return fcu;
 }
 
-FCurve *BKE_fcurve_find(ListBase *list, const char rna_path[], const int array_index)
+FCurve *BKE_fcurve_find(ListBaseT<FCurve> *list, const char rna_path[], const int array_index)
 {
   /* Sanity checks. */
   if (ELEM(nullptr, list, rna_path) || array_index < 0) {
@@ -1805,7 +1805,7 @@ struct tRetainedKeyframe {
 void BKE_fcurve_merge_duplicate_keys(FCurve *fcu, const int sel_flag, const bool use_handle)
 {
   /* NOTE: We assume that all keys are sorted */
-  ListBase retained_keys = {nullptr, nullptr};
+  ListBaseT<tRetainedKeyframe> retained_keys = {nullptr, nullptr};
   const bool can_average_points = ((fcu->flag & (FCURVE_INT_VALUES | FCURVE_DISCRETE_VALUES)) ==
                                    0);
 
@@ -2483,7 +2483,7 @@ float calculate_fcurve(PathResolvedRNA *anim_rna,
 /** \name F-Curve - .blend file API
  * \{ */
 
-void BKE_fmodifiers_blend_write(BlendWriter *writer, ListBase *fmodifiers)
+void BKE_fmodifiers_blend_write(BlendWriter *writer, ListBaseT<FModifier> *fmodifiers)
 {
   /* Write all modifiers first (for faster reloading) */
   BLO_write_struct_list(writer, FModifier, fmodifiers);
@@ -2524,7 +2524,9 @@ void BKE_fmodifiers_blend_write(BlendWriter *writer, ListBase *fmodifiers)
   }
 }
 
-void BKE_fmodifiers_blend_read_data(BlendDataReader *reader, ListBase *fmodifiers, FCurve *curve)
+void BKE_fmodifiers_blend_read_data(BlendDataReader *reader,
+                                    ListBaseT<FModifier> *fmodifiers,
+                                    FCurve *curve)
 {
   LISTBASE_FOREACH (FModifier *, fcm, fmodifiers) {
     const FModifierTypeInfo *fmi = fmodifier_get_typeinfo(fcm);
@@ -2599,7 +2601,7 @@ void BKE_fcurve_blend_write_data(BlendWriter *writer, FCurve *fcu)
   BKE_fmodifiers_blend_write(writer, &fcu->modifiers);
 }
 
-void BKE_fcurve_blend_write_listbase(BlendWriter *writer, ListBase *fcurves)
+void BKE_fcurve_blend_write_listbase(BlendWriter *writer, ListBaseT<FCurve> *fcurves)
 {
   BLO_write_struct_list(writer, FCurve, fcurves);
   LISTBASE_FOREACH (FCurve *, fcu, fcurves) {
@@ -2660,7 +2662,7 @@ void BKE_fcurve_blend_read_data(BlendDataReader *reader, FCurve *fcu)
   BKE_fmodifiers_blend_read_data(reader, &fcu->modifiers, fcu);
 }
 
-void BKE_fcurve_blend_read_data_listbase(BlendDataReader *reader, ListBase *fcurves)
+void BKE_fcurve_blend_read_data_listbase(BlendDataReader *reader, ListBaseT<FCurve> *fcurves)
 {
   /* Link F-Curve data to F-Curve again (non ID-libraries). */
   LISTBASE_FOREACH (FCurve *, fcu, fcurves) {

source/blender/blenkernel/intern/fcurve_driver.cc
@@ -853,7 +853,7 @@ static const DriverVarTypeInfo *get_dvar_typeinfo(int type)
 /** \name Driver API
  * \{ */
 
-void driver_free_variable(ListBase *variables, DriverVar *dvar)
+void driver_free_variable(ListBaseT<DriverVar> *variables, DriverVar *dvar)
 {
   /* Sanity checks. */
   if (dvar == nullptr) {
@@ -886,7 +886,7 @@ void driver_free_variable_ex(ChannelDriver *driver, DriverVar *dvar)
   BKE_driver_invalidate_expression(driver, false, true);
 }
 
-void driver_variables_copy(ListBase *dst_vars, const ListBase *src_vars)
+void driver_variables_copy(ListBaseT<DriverVar> *dst_vars, const ListBaseT<DriverVar> *src_vars)
 {
   BLI_assert(BLI_listbase_is_empty(dst_vars));
   BLI_duplicatelist(dst_vars, src_vars);
@@ -1003,7 +1003,7 @@ void driver_variable_name_validate(DriverVar *dvar)
 
 void driver_variable_unique_name(DriverVar *dvar)
 {
-  ListBase variables = BLI_listbase_from_link((Link *)dvar);
+  ListBaseT<DriverVar> variables = {dvar, dvar};
   BLI_uniquename(&variables, dvar, dvar->name, '_', offsetof(DriverVar, name), sizeof(dvar->name));
 }
 

commit 5c44584c641fae0c03fd08ebd2497d21a3dee6a5

Fix: Core: Unique naming bug with driver variables, f-modifiers, exporters
The refactor incorrectly replaced BLI_listbase_from_link, that doesn't
create a listbase from a single link, but rather gets the full list the
link is part of.

Issue introduced in bd6b9568c16.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

source/blender/blenkernel/intern/collection.cc
@@ -559,8 +559,7 @@ void BKE_collection_exporter_name_set(const ListBaseT<CollectionExport> *exporte
 {
   /* Only use the new name if it's not empty. */
   if (newname && newname[0] != '\0') {
-    ListBaseT<CollectionExport> list = exporters ? *exporters :
-                                                   ListBaseT<CollectionExport>{data, data};
+    ListBaseT<CollectionExport> list = exporters ? *exporters : BLI_listbase_from_link(data);
 
     STRNCPY(data->name, newname);
     BLI_uniquename(

source/blender/blenkernel/intern/fcurve.cc
@@ -207,7 +207,7 @@ void BKE_fmodifier_name_set(FModifier *fcm, const char *name)
   /* Set default modifier name when name parameter is an empty string.
    * Ensure the name is unique. */
   const FModifierTypeInfo *fmi = get_fmodifier_typeinfo(fcm->type);
-  ListBaseT<FModifier> list = {fcm, fcm};
+  ListBaseT<FModifier> list = BLI_listbase_from_link(fcm);
   BLI_uniquename(&list,
                  fcm,
                  CTX_DATA_(BLT_I18NCONTEXT_ID_ACTION, fmi->name),

source/blender/blenkernel/intern/fcurve_driver.cc
@@ -1005,7 +1005,7 @@ void driver_variable_name_validate(DriverVar *dvar)
 
 void driver_variable_unique_name(DriverVar *dvar)
 {
-  ListBaseT<DriverVar> variables = {dvar, dvar};
+  ListBaseT<DriverVar> variables = BLI_listbase_from_link(dvar);
   BLI_uniquename(&variables, dvar, dvar->name, '_', offsetof(DriverVar, name), sizeof(dvar->name));
 }
 

source/blender/blenlib/BLI_listbase.hh
@@ -38,6 +38,15 @@ int BLI_findstringindex(const ListBase *listbase,
  */
 ListBase BLI_listbase_from_link(Link *some_link);
 
+/**
+ * Return a typed ListBase representing the entire list the given link is in.
+ */
+template<typename T> ListBaseT<T> BLI_listbase_from_link(T *some_link)
+{
+  const ListBase list = BLI_listbase_from_link(reinterpret_cast<Link *>(some_link));
+  return ListBaseT<T>{static_cast<T *>(list.first_), static_cast<T *>(list.last_)};
+}
+
 /* Find forwards. */
 
 /**