blender/blender

commit e1fa0c2805eba4d3734d2160e7947143cbdc0c4b

Cycles: Add NVIDIA DLSS support for viewport denoising
Adds the option to use DLSS Ray Reconstruction for viewport denoising
in Cycles. For this to work, scheduling is adjusted to continuously
reset samples (so that independent frames are rendered), pixel jitter
is forced on and the required denoising passes (color, depth, diffuse
albedo, specular albedo, normals, roughness, motion vectors, specular
motion vectors) are enabled.
DLSS expects those inputs in the form of CUDA textures, while Cycles
keeps passes in an interleaved buffer layout. The data therefore has to
be converted, for which specialized versions of the existing denoising
filter kernels are introduced, which read/write directly to temporary
CUDA textures that are managed in denoiser_dlss.cpp.

The integration of DLSS itself is done in a similar fashion to OptiX:
The DLSS SDK is pulled in for the type definitions, but the DLSS
implementation is loaded by the NVIDIA driver installed on the system.

Pull Request: https://projects.blender.org/blender/blender/pulls/153077

intern/cycles/util/string.cpp
@@ -16,6 +16,8 @@
 #  ifndef vsnprintf
 #    define vsnprintf _vsnprintf
 #  endif
+#else
+#  include <locale.h>
 #endif /* _WIN32 */
 
 CCL_NAMESPACE_BEGIN
@@ -239,25 +241,55 @@ string string_remove_gpu_from_cpu_name(const string &s)
 
 /* Wide char strings helpers for Windows. */
 
-#ifdef _WIN32
-
 wstring string_to_wstring(const string &str)
 {
+#ifdef _WIN32
   const int length_wc = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), nullptr, 0);
   wstring str_wc(length_wc, 0);
   MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), &str_wc[0], length_wc);
+#else
+  locale_t new_locale = newlocale(LC_CTYPE_MASK, "en_US.UTF-8", (locale_t)0);
+  locale_t old_locale = uselocale(new_locale);
+  mbstate_t state = {};
+  const char *str_p = str.c_str();
+  const size_t length_wc = mbsrtowcs(nullptr, &str_p, 0, &state);
+  wstring str_wc;
+  if (length_wc != (size_t)-1) {
+    str_wc.resize(length_wc, 0);
+    mbsrtowcs(str_wc.data(), &str_p, str_wc.size(), &state);
+  }
+  uselocale(old_locale);
+  freelocale(new_locale);
+#endif
   return str_wc;
 }
 
 string string_from_wstring(const wstring &str)
 {
+#ifdef _WIN32
   int length_mb = WideCharToMultiByte(
       CP_UTF8, 0, str.c_str(), str.size(), nullptr, 0, nullptr, nullptr);
   string str_mb(length_mb, 0);
   WideCharToMultiByte(
       CP_UTF8, 0, str.c_str(), str.size(), &str_mb[0], length_mb, nullptr, nullptr);
+#else
+  locale_t new_locale = newlocale(LC_CTYPE_MASK, "en_US.UTF-8", (locale_t)0);
+  locale_t old_locale = uselocale(new_locale);
+  const wchar_t *str_p = str.c_str();
+  mbstate_t state = {};
+  const size_t length_mb = wcsrtombs(nullptr, &str_p, 0, &state);
+  string str_mb;
+  if (length_mb != (size_t)-1) {
+    str_mb.resize(length_mb, 0);
+    wcsrtombs(str_mb.data(), &str_p, str_mb.size(), &state);
+  }
+  uselocale(old_locale);
+  freelocale(new_locale);
+#endif
   return str_mb;
 }
+
+#ifdef _WIN32
 
 string string_to_ansi(const string &str)
 {

CMakeLists.txt
@@ -311,6 +311,8 @@ option(WITH_MANIFOLD "Enable features depending on Manifold (Fast Robust Boolean
 
 option(WITH_OPENIMAGEDENOISE "Enable the OpenImageDenoise compositing node" ON)
 
+option(WITH_DLSS "Enable NVIDIA DLSS Ray Reconstruction support" OFF)
+
 option(WITH_OPENSUBDIV "Enable OpenSubdiv for surface subdivision" ON)
 
 option(WITH_POTRACE "Enable features relying on Potrace" ON)

build_files/cmake/Modules/FindDLSS.cmake
@@ -1,0 +1,19 @@
+# SPDX-FileCopyrightText: 2026 Blender Authors
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Find the DLSS SDK. This modules defines
+#  DLSS_INCLUDE_DIR, where to find the NGX headers for DLSS
+#  DLSS_FOUND, if the DLSS SDK is found.
+
+find_path(DLSS_INCLUDE_DIR
+    NAMES
+        "nvsdk_ngx.h"
+    PATHS
+        "${DLSS_SDK_ROOT}/include"
+        "$ENV{DLSS_SDK_ROOT}/include"
+)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(DLSS REQUIRED_VARS DLSS_INCLUDE_DIR)

intern/cycles/CMakeLists.txt
@@ -311,6 +311,15 @@ if(WITH_OPENIMAGEDENOISE)
   add_definitions(-DWITH_OPENIMAGEDENOISE)
 endif()
 
+if(WITH_DLSS)
+  find_package(DLSS)
+  if(DLSS_FOUND)
+    add_definitions(-DWITH_DLSS)
+  else()
+    set_and_warn_library_found("DLSS" DLSS_FOUND WITH_DLSS)
+  endif()
+endif()
+
 if(WITH_OPENVDB)
   add_definitions(-DWITH_OPENVDB)
 endif()

intern/cycles/blender/addon/properties.py
@@ -280,6 +280,15 @@ def enum_openimagedenoise_denoiser(self, context):
     return []
 
 
+def enum_dlss_denoiser(self, context):
+    import _cycles
+    if _cycles.with_dlss and (not context or bool(
+            context.preferences.addons[__package__].preferences.get_devices_for_type('CUDA'))):
+        return [('DLSS', "DLSS",
+                 n_("Use NVIDIA DLSS Ray Reconstruction"), 8)]
+    return []
+
+
 def enum_optix_denoiser(self, context):
     if not context or bool(context.preferences.addons[__package__].preferences.get_devices_for_type('OPTIX')):
         return [('OPTIX', "OptiX", n_(
@@ -290,8 +299,9 @@ def enum_optix_denoiser(self, context):
 def enum_preview_denoiser(self, context):
     optix_items = enum_optix_denoiser(self, context)
     oidn_items = enum_openimagedenoise_denoiser(self, context)
+    dlss_items = enum_dlss_denoiser(self, context)
 
-    if len(optix_items) or len(oidn_items):
+    if len(optix_items) or len(oidn_items) or len(dlss_items):
         items = [
             ('AUTO',
              "Automatic",
@@ -303,6 +313,7 @@ def enum_preview_denoiser(self, context):
 
     items += optix_items
     items += oidn_items
+    items += dlss_items
     return items
 
 
@@ -347,6 +358,28 @@ enum_denoising_quality = (
      "Fast",
      "High performance",
      3),
+)
+enum_denoising_upscale_quality = (
+    ('NONE',
+     "None",
+     "Highest quality without upscaling",
+     0),
+    ('QUALITY',
+     "Quality",
+     "Offers higher image quality than balanced mode",
+     1),
+    ('BALANCED',
+     "Balanced",
+     "Offers both optimized performance and image quality",
+     2),
+    ('PERFORMANCE',
+     "Performance",
+     "Offers a higher performance boost than balanced mode",
+     3),
+    ('ULTRA_PERFORMANCE',
+     "Ultra Performance",
+     "Offers the highest performance boost",
+     4),
 )
 
 enum_direct_light_sampling_type = (
@@ -489,6 +522,12 @@ class CyclesRenderSettings(bpy.types.PropertyGroup):
         name="Denoise Preview on GPU",
         description="Perform denoising on GPU devices configured in the system tab in the user preferences. This is significantly faster than on CPU, but requires additional GPU memory. When large scenes need more GPU memory, this option can be disabled",
         default=True,
+    )
+    preview_denoising_upscale_quality: EnumProperty(
+        name="Viewport Denoising Upscale Quality",
+        description="Overall upscale factor and denoising quality when using DLSS",
+        items=enum_denoising_upscale_quality,
+        default='BALANCED',
     )
 
     samples: IntProperty(
@@ -1846,6 +1885,26 @@ class CyclesPreferences(bpy.types.AddonPreferences):
 
                 has_device_oidn_support = device[5]
                 if has_device_oidn_support and self.find_existing_device_entry(device).use:
+                    return True
+
+        return False
+
+    def has_dlss_gpu_devices(self):
+        compute_device_type = self.get_compute_device_type()
+
+        # We need non-CPU devices, used for rendering and supporting DLSS
+        if compute_device_type != 'NONE':
+            for device in self.get_device_list(compute_device_type):
+                device_type = device[1]
+                if device_type == 'CPU':
+                    continue
+
+                # Skip devices that do not meet the driver requirement.
+                if not device[8]:
+                    continue
+
+                has_device_dlss_support = device[9]
+                if has_device_dlss_support and self.find_existing_device_entry(device).use:
                     return True
 
         return False

intern/cycles/blender/addon/ui.py
@@ -19,6 +19,9 @@ from bl_ui.properties_view_layer import (
 )
 
 from bl_ui.properties_object import has_geometry_visibility
+from bpy.app.translations import (
+    pgettext_rpt as rpt_,
+)
 
 
 class CyclesPresetPanel(PresetPanel, Panel):
@@ -151,6 +154,9 @@ def show_preview_denoise_active(context):
     if not cscene.use_preview_denoising:
         return False
 
+    if cscene.preview_denoiser == 'DLSS':
+        return has_dlss_gpu_devices(context)
+
     if cscene.preview_denoiser == 'OPTIX':
         return has_optixdenoiser_gpu_devices(context)
 
@@ -190,6 +196,10 @@ def has_oidn_gpu_devices(context):
     return context.preferences.addons[__package__].preferences.has_oidn_gpu_devices()
 
 
+def has_dlss_gpu_devices(context):
+    return context.preferences.addons[__package__].preferences.has_dlss_gpu_devices()
+
+
 def has_optixdenoiser_gpu_devices(context):
     return context.preferences.addons[__package__].preferences.has_optixdenoiser_gpu_devices()
 
@@ -214,6 +224,8 @@ class CYCLES_RENDER_PT_sampling_viewport(CyclesButtonsPanel, Panel):
         scene = context.scene
         cscene = scene.cycles
 
+        layout.active = not (cscene.use_preview_denoising and cscene.preview_denoiser == 'DLSS')
+
         layout.use_property_split = True
         layout.use_property_decorate = False
 
@@ -224,12 +236,12 @@ class CYCLES_RENDER_PT_sampling_viewport(CyclesButtonsPanel, Panel):
         sub.active = cscene.use_preview_adaptive_sampling
         sub.prop(cscene, "preview_adaptive_threshold", text="")
 
+        col = layout.column(align=True)
         if cscene.use_preview_adaptive_sampling:
-            col = layout.column(align=True)
             col.prop(cscene, "preview_samples", text="Max Samples")
             col.prop(cscene, "preview_adaptive_min_samples", text="Min Samples")
         else:
-            layout.prop(cscene, "preview_samples", text="Samples")
+            col.prop(cscene, "preview_samples", text="Samples")
 
 
 class CYCLES_RENDER_PT_sampling_viewport_denoise(CyclesButtonsPanel, Panel):
@@ -258,10 +270,21 @@ class CYCLES_RENDER_PT_sampling_viewport_denoise(CyclesButtonsPanel, Panel):
         sub.active = show_preview_denoise_active(context)
         sub.prop(cscene, "preview_denoiser", text="Denoiser")
 
-        col.prop(cscene, "preview_denoising_input_passes", text="Passes")
-
         has_oidn_gpu = has_oidn_gpu_devices(context)
         effective_preview_denoiser = get_effective_preview_denoiser(context, has_oidn_gpu)
+
+        if effective_preview_denoiser == 'DLSS':
+            if has_dlss_gpu_devices(context):
+                col.prop(cscene, "preview_denoising_upscale_quality", text="Upscale Mode")
+            else:
+                col.label(text=rpt_("Requires NVIDIA GPU with compute capability %s") % "7.5",
+                          icon='INFO', translate=False)
+                col.label(text=rpt_("and NVIDIA driver version %s or newer") % "590",
+                          icon='BLANK1', translate=False)
+            return
+
+        col.prop(cscene, "preview_denoising_input_passes", text="Passes")
+
         if effective_preview_denoiser == 'OPENIMAGEDENOISE':
             col.prop(cscene, "preview_denoising_prefilter", text="Prefilter")
             col.prop(cscene, "preview_denoising_quality", text="Quality")

intern/cycles/blender/python.cpp
@@ -486,7 +486,7 @@ static PyObject *available_devices_func(PyObject * /*self*/, PyObject *args)
   for (size_t i = 0; i < devices.size(); i++) {
     const DeviceInfo &device = devices[i];
     const string type_name = Device::string_from_type(device.type);
-    PyObject *device_tuple = PyTuple_New(9);
+    PyObject *device_tuple = PyTuple_New(10);
     PyTuple_SET_ITEM(device_tuple, 0, pyunicode_from_string(device.description.c_str()));
     PyTuple_SET_ITEM(device_tuple, 1, pyunicode_from_string(type_name.c_str()));
     PyTuple_SET_ITEM(device_tuple, 2, pyunicode_from_string(device.id.c_str()));
@@ -497,6 +497,7 @@ static PyObject *available_devices_func(PyObject * /*self*/, PyObject *args)
     PyTuple_SET_ITEM(device_tuple, 6, PyBool_FromLong(device.denoisers & DENOISER_OPTIX));
     PyTuple_SET_ITEM(device_tuple, 7, PyBool_FromLong(device.has_execution_optimization));
     PyTuple_SET_ITEM(device_tuple, 8, PyBool_FromLong(device.meets_driver_requirement));
+    PyTuple_SET_ITEM(device_tuple, 9, PyBool_FromLong(device.denoisers & DENOISER_DLSS));
     PyTuple_SET_ITEM(ret, i, device_tuple);
   }
 
@@ -1006,6 +1007,12 @@ void *blender::CCL_python_module_init()
   else {
     PyModule_AddObjectRef(mod, "with_openimagedenoise", Py_False);
   }
+
+#ifdef WITH_DLSS
+  PyModule_AddObjectRef(mod, "with_dlss", Py_True);
+#else
+  PyModule_AddObjectRef(mod, "with_dlss", Py_False);
+#endif
 
 #ifdef WITH_CYCLES_DEBUG
   PyModule_AddObjectRef(mod, "with_debug", Py_True);

intern/cycles/blender/sync.cpp
@@ -1148,6 +1148,16 @@ DenoiseParams BlenderSync::get_denoise_params(blender::Scene &b_scene,
     DENOISER_INPUT_NUM,
   };
 
+  enum DenoiserUpscaleQuality {
+    DENOISER_UPSCALE_NONE = 0,
+    DENOISER_UPSCALE_QUALITY = 1,
+    DENOISER_UPSCALE_BALANCED = 2,
+    DENOISER_UPSCALE_PERFORMANCE = 3,
+    DENOISER_UPSCALE_ULTRA_PERFORMANCE = 4,
+
+    DENOISER_UPSCALE_NUM,
+  };
+
   DenoiseParams denoising;
   blender::PointerRNA scene_rna_ptr = RNA_id_pointer_create(&b_scene.id);
   blender::PointerRNA cscene = RNA_pointer_get(&scene_rna_ptr, "cycles");
@@ -1197,6 +1207,48 @@ DenoiseParams BlenderSync::get_denoise_params(blender::Scene &b_scene,
       if (denoising.type == DENOISER_NONE) {
         denoising.use = false;
       }
+    }
+
+    if (denoising.type == DENOISER_DLSS) {
+      /* Disable denoising when DLSS is not supported. */
+      if (!Denoiser::is_device_supported(denoising.type, denoise_device_info)) {
+        denoising.use = false;
+      }
+
+      denoising.start_sample = 0;
+
+      switch ((DenoiserUpscaleQuality)get_enum(cscene,
+                                               "preview_denoising_upscale_quality",
+                                               DENOISER_UPSCALE_NUM,
+                                               DENOISER_UPSCALE_BALANCED))
+      {
+        case DENOISER_UPSCALE_NONE:
+          denoising.quality = DENOISER_QUALITY_HIGH;
+          denoising.upscale_factor = 1.0f;
+          break;
+        case DENOISER_UPSCALE_QUALITY:
+          denoising.quality = DENOISER_QUALITY_HIGH;
+          denoising.upscale_factor = 1.0f / 0.66666667f;
+          break;
+        default:
+        case DENOISER_UPSCALE_BALANCED:
+          denoising.quality = DENOISER_QUALITY_BALANCED;
+          denoising.upscale_factor = 1.0f / 0.58f;
+          break;
+        case DENOISER_UPSCALE_PERFORMANCE:
+          denoising.quality = DENOISER_QUALITY_FAST;
+          denoising.upscale_factor = 2.0f;
+          break;
+        case DENOISER_UPSCALE_ULTRA_PERFORMANCE:
+          denoising.quality = DENOISER_QUALITY_FAST;
+          denoising.upscale_factor = 3.0f;
+          break;
+      }
+
+      denoising.passes = DENOISER_PASS_ALBEDO | DENOISER_PASS_SPECULAR_ALBEDO |
+                         DENOISER_PASS_NORMAL | DENOISER_PASS_ROUGHNESS | DENOISER_PASS_DEPTH |
+                         DENOISER_PASS_MOTION | DENOISER_PASS_SPECULAR_MOTION;
+      return denoising;
     }
   }
 

commit bc07e5c47fbdd7e5dc8231a5d344404a53a031d8

Fix: Cycles: Crash in DLSS on Linux system without en_US.UTF-8
Probably not common, but should guard against this.

Issue introduced in e1fa0c2805e.

Pull Request: https://projects.blender.org/blender/blender/pulls/164137

intern/cycles/util/string.cpp
@@ -249,7 +249,7 @@ wstring string_to_wstring(const string &str)
   MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), &str_wc[0], length_wc);
 #else
   locale_t new_locale = newlocale(LC_CTYPE_MASK, "en_US.UTF-8", (locale_t)0);
-  locale_t old_locale = uselocale(new_locale);
+  locale_t old_locale = new_locale ? uselocale(new_locale) : (locale_t)0;
   mbstate_t state = {};
   const char *str_p = str.c_str();
   const size_t length_wc = mbsrtowcs(nullptr, &str_p, 0, &state);
@@ -258,8 +258,10 @@ wstring string_to_wstring(const string &str)
     str_wc.resize(length_wc, 0);
     mbsrtowcs(str_wc.data(), &str_p, str_wc.size(), &state);
   }
-  uselocale(old_locale);
-  freelocale(new_locale);
+  if (new_locale) {
+    uselocale(old_locale);
+    freelocale(new_locale);
+  }
 #endif
   return str_wc;
 }
@@ -274,7 +276,7 @@ string string_from_wstring(const wstring &str)
       CP_UTF8, 0, str.c_str(), str.size(), &str_mb[0], length_mb, nullptr, nullptr);
 #else
   locale_t new_locale = newlocale(LC_CTYPE_MASK, "en_US.UTF-8", (locale_t)0);
-  locale_t old_locale = uselocale(new_locale);
+  locale_t old_locale = new_locale ? uselocale(new_locale) : (locale_t)0;
   const wchar_t *str_p = str.c_str();
   mbstate_t state = {};
   const size_t length_mb = wcsrtombs(nullptr, &str_p, 0, &state);
@@ -283,8 +285,10 @@ string string_from_wstring(const wstring &str)
     str_mb.resize(length_mb, 0);
     wcsrtombs(str_mb.data(), &str_p, str_mb.size(), &state);
   }
-  uselocale(old_locale);
-  freelocale(new_locale);
+  if (new_locale) {
+    uselocale(old_locale);
+    freelocale(new_locale);
+  }
 #endif
   return str_mb;
 }