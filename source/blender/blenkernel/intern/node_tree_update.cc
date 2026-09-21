blender/blender

commit 492a510260a68fb59e0b765ed37f6bbc5e7098b0

Fix #162884: Node tree interface accessors to prevent crash on nullptr
Socket names and descriptions can be set to empty strings, which results
in `nullptr` being assigned to the DNA `char *` fields. Using the
nullptr directly is not supported in many places (such as UI names for
RNA properties) so a lot of boilerplate code is required to check for
nullptr everywhere.

This issue has come up in the past, but there was nothing to prevent
newer uses of name strings to make the same mistakes.

To simplify code and make it more robust in the future the relevant
name and description strings now have accessor functions `name()` and
`description()` for const access, which return a `StringRefNull`,
replacing any nullptr with an empty string. The original DNA fields are
renamed to `name_` and `description_` to indicate they are considered
"private" and should only be accessed directly by internal code that
needs write access.

Pull Request: https://projects.blender.org/blender/blender/pulls/163383

source/blender/blenkernel/intern/node_tree_update.cc
@@ -2258,9 +2258,9 @@ class NodeTreeMainUpdater {
       }
       bNodeTreeInterfacePanel *panel = reinterpret_cast<bNodeTreeInterfacePanel *>(item);
       if (bNodeTreeInterfaceSocket *toggle_socket = panel->header_toggle_socket()) {
-        if (!STREQ(panel->name, toggle_socket->name)) {
-          MEM_SAFE_DELETE(toggle_socket->name);
-          toggle_socket->name = BLI_strdup_null(panel->name);
+        if (!STREQ(panel->name_, toggle_socket->name_)) {
+          MEM_SAFE_DELETE(toggle_socket->name_);
+          toggle_socket->name() = BLI_strdup_null(panel->name_);
           changed = true;
         }
       }

source/blender/blenkernel/intern/node.cc
@@ -2231,12 +2231,9 @@ static std::unique_ptr<IDProperty, idprop::IDPropertyDeleter> create_socket_meta
   const bNodeSocketType *base_typeinfo = node_socket_type_find(socket.socket_type);
 
   auto socket_prop = idprop::create_group(socket.identifier);
-  IDP_AddToGroup(socket_prop.get(),
-                 idprop::create("name", socket.name ? socket.name : "").release());
+  IDP_AddToGroup(socket_prop.get(), idprop::create("name", socket.name()).release());
   IDP_AddToGroup(socket_prop.get(), idprop::create("type", base_typeinfo->type).release());
-  IDP_AddToGroup(
-      socket_prop.get(),
-      idprop::create("description", socket.description ? socket.description : "").release());
+  IDP_AddToGroup(socket_prop.get(), idprop::create("description", socket.description()).release());
   switch (base_typeinfo->type) {
     case SOCK_FLOAT: {
       const auto &value = node_interface::get_socket_data_as<bNodeSocketValueFloat>(socket);
@@ -2434,7 +2431,7 @@ IDProperty *node_create_asset_meta_data_properties(const bNodeTree &node_tree)
    * `output_sockets` in Blender 6.0 (next breaking release). */
   auto outputs = idprop::create_group("outputs");
   for (const bNodeTreeInterfaceSocket *socket : node_tree.interface_outputs()) {
-    auto *prop = idprop::create(socket->name ? socket->name : "", socket->socket_type).release();
+    auto *prop = idprop::create(socket->name(), socket->socket_type).release();
     if (!IDP_AddToGroup(outputs.get(), prop)) {
       IDP_FreeProperty(prop);
     }

source/blender/blenkernel/intern/node_tree_interface.cc
@@ -488,8 +488,8 @@ static void item_copy(bNodeTreeInterfaceItem &dst,
           reinterpret_cast<const bNodeTreeInterfaceSocket &>(src);
       BLI_assert(src_socket.socket_type != nullptr);
 
-      dst_socket.name = BLI_strdup_null(src_socket.name);
-      dst_socket.description = BLI_strdup_null(src_socket.description);
+      dst_socket.name_ = BLI_strdup_null(src_socket.name_);
+      dst_socket.description_ = BLI_strdup_null(src_socket.description_);
       dst_socket.socket_type = BLI_strdup(src_socket.socket_type);
       dst_socket.default_attribute_name = BLI_strdup_null(src_socket.default_attribute_name);
       dst_socket.identifier = generate_uid ? BLI_sprintfN("Socket_%d", generate_uid()) :
@@ -507,8 +507,8 @@ static void item_copy(bNodeTreeInterfaceItem &dst,
       const bNodeTreeInterfacePanel &src_panel = reinterpret_cast<const bNodeTreeInterfacePanel &>(
           src);
 
-      dst_panel.name = BLI_strdup_null(src_panel.name);
-      dst_panel.description = BLI_strdup_null(src_panel.description);
+      dst_panel.name_ = BLI_strdup_null(src_panel.name_);
+      dst_panel.description_ = BLI_strdup_null(src_panel.description_);
       dst_panel.identifier = generate_uid ? generate_uid() : src_panel.identifier;
 
       panel_init(dst_panel, src_panel.items(), flag, generate_uid);
@@ -527,8 +527,8 @@ static void item_free(bNodeTreeInterfaceItem &item, const bool do_id_user)
         socket_types::socket_data_free(socket, do_id_user);
       }
 
-      MEM_SAFE_DELETE(socket.name);
-      MEM_SAFE_DELETE(socket.description);
+      MEM_SAFE_DELETE(socket.name_);
+      MEM_SAFE_DELETE(socket.description_);
       MEM_SAFE_DELETE(socket.socket_type);
       MEM_SAFE_DELETE(socket.default_attribute_name);
       MEM_SAFE_DELETE(socket.identifier);
@@ -541,8 +541,8 @@ static void item_free(bNodeTreeInterfaceItem &item, const bool do_id_user)
       bNodeTreeInterfacePanel &panel = reinterpret_cast<bNodeTreeInterfacePanel &>(item);
 
       panel.clear(do_id_user);
-      MEM_SAFE_DELETE(panel.name);
-      MEM_SAFE_DELETE(panel.description);
+      MEM_SAFE_DELETE(panel.name_);
+      MEM_SAFE_DELETE(panel.description_);
       break;
     }
   }
@@ -557,9 +557,9 @@ static void item_write_data(BlendWriter *writer, bNodeTreeInterfaceItem &item)
   switch (item.item_type) {
     case NodeTreeInterfaceItemType::Socket: {
       bNodeTreeInterfaceSocket &socket = reinterpret_cast<bNodeTreeInterfaceSocket &>(item);
-      writer->write_string(socket.name);
+      writer->write_string(socket.name_);
       writer->write_string(socket.identifier);
-      writer->write_string(socket.description);
+      writer->write_string(socket.description_);
       writer->write_string(socket.socket_type);
       writer->write_string(socket.default_attribute_name);
       if (socket.properties) {
@@ -571,8 +571,8 @@ static void item_write_data(BlendWriter *writer, bNodeTreeInterfaceItem &item)
     }
     case NodeTreeInterfaceItemType::Panel: {
       bNodeTreeInterfacePanel &panel = reinterpret_cast<bNodeTreeInterfacePanel &>(item);
-      writer->write_string(panel.name);
-      writer->write_string(panel.description);
+      writer->write_string(panel.name_);
+      writer->write_string(panel.description_);
       writer->write_pointer_array(panel.items_num, panel.items_array);
       for (bNodeTreeInterfaceItem *child_item : panel.items()) {
         item_write_struct(writer, *child_item);
@@ -666,8 +666,8 @@ static void item_read_data(BlendDataReader *reader, bNodeTreeInterfaceItem &item
   switch (item.item_type) {
     case NodeTreeInterfaceItemType::Socket: {
       bNodeTreeInterfaceSocket &socket = reinterpret_cast<bNodeTreeInterfaceSocket &>(item);
-      BLO_read_string(reader, &socket.name);
-      BLO_read_string(reader, &socket.description);
+      BLO_read_string(reader, &socket.name_);
+      BLO_read_string(reader, &socket.description_);
       BLO_read_string(reader, &socket.socket_type);
       BLO_read_string(reader, &socket.default_attribute_name);
       BLO_read_string(reader, &socket.identifier);
@@ -687,8 +687,8 @@ static void item_read_data(BlendDataReader *reader, bNodeTreeInterfaceItem &item
     }
     case NodeTreeInterfaceItemType::Panel: {
       bNodeTreeInterfacePanel &panel = reinterpret_cast<bNodeTreeInterfacePanel &>(item);
-      BLO_read_string(reader, &panel.name);
-      BLO_read_string(reader, &panel.description);
+      BLO_read_string(reader, &panel.name_);
+      BLO_read_string(reader, &panel.description_);
       BLO_read_pointer_array_and_validate_size(reader, &panel.items_array, &panel.items_num);
 
       /* Read the direct-data for each interface item if possible. The pointer becomes null if the
@@ -756,6 +756,16 @@ static Span<bNodeTreeInterfaceItem *> item_children(bNodeTreeInterfaceItem &item
 }  // namespace bke::node_interface
 
 using namespace blender::bke::node_interface;
+
+StringRefNull bNodeTreeInterfaceSocket::name() const
+{
+  return this->name_ ? this->name_ : "";
+}
+
+StringRefNull bNodeTreeInterfaceSocket::description() const
+{
+  return this->description_ ? this->description_ : "";
+}
 
 bke::bNodeSocketType *bNodeTreeInterfaceSocket::socket_typeinfo() const
 {
@@ -883,6 +893,16 @@ void bNodeTreeInterfaceSocket::init_from_socket_instance(const bNodeSocket *sock
   this->socket_type = BLI_strdup(socket->idname);
   this->socket_data = socket_types::make_socket_data(socket->idname);
   socket_types::socket_data_copy_ptr(*this, socket->default_value, 0);
+}
+
+StringRefNull bNodeTreeInterfacePanel::name() const
+{
+  return this->name_ ? this->name_ : "";
+}
+
+StringRefNull bNodeTreeInterfacePanel::description() const
+{
+  return this->description_ ? this->description_ : "";
 }
 
 IndexRange bNodeTreeInterfacePanel::items_range() const
@@ -1254,10 +1274,10 @@ static bNodeTreeInterfaceSocket *make_socket(const int uid,
   /* Init common socket properties. */
   new_socket->identifier = BLI_sprintfN("Socket_%d", uid);
   new_socket->item.item_type = NodeTreeInterfaceItemType::Socket;
-  new_socket->name = BLI_strdupn(name.data(), name.size());
-  new_socket->description = description.is_empty() ?
-                                nullptr :
-                                BLI_strdupn(description.data(), description.size());
+  new_socket->name_ = BLI_strdupn(name.data(), name.size());
+  new_socket->description_ = description.is_empty() ?
+                                 nullptr :
+                                 BLI_strdupn(description.data(), description.size());
   new_socket->socket_type = BLI_strdupn(socket_type.data(), socket_type.size());
   new_socket->flag = flag;
 
@@ -1683,10 +1703,10 @@ static bNodeTreeInterfacePanel *make_panel(const int uid,
 
   bNodeTreeInterfacePanel *new_panel = MEM_new<bNodeTreeInterfacePanel>(__func__);
   new_panel->item.item_type = NodeTreeInterfaceItemType::Panel;
-  new_panel->name = BLI_strdupn(name.data(), name.size());
-  new_panel->description = description.is_empty() ?
-                               nullptr :
-                               BLI_strdupn(description.data(), description.size());
+  new_panel->name_ = BLI_strdupn(name.data(), name.size());
+  new_panel->description_ = description.is_empty() ?
+                                nullptr :
+                                BLI_strdupn(description.data(), description.size());
   new_panel->identifier = uid;
   new_panel->flag = flag;
   return new_panel;

source/blender/blenloader/intern/versioning_400.cc
@@ -694,9 +694,9 @@ static bNodeTreeInterfaceItem *legacy_socket_move_to_interface(bNodeSocket &lega
   new_socket->item.item_type = NodeTreeInterfaceItemType::Socket;
 
   /* Move reusable data. */
-  new_socket->name = BLI_strdup(legacy_socket.name);
+  new_socket->name_ = BLI_strdup(legacy_socket.name);
   new_socket->identifier = BLI_strdup(legacy_socket.identifier);
-  new_socket->description = BLI_strdup(legacy_socket.description);
+  new_socket->description_ = BLI_strdup(legacy_socket.description);
   /* If the socket idname includes a subtype (e.g. "NodeSocketFloatFactor") this will convert it to
    * the base type name ("NodeSocketFloat"). */
   new_socket->socket_type = BLI_strdup(
@@ -899,8 +899,8 @@ static void version_copy_socket(bNodeTreeInterfaceSocket &dst,
                                 char *identifier)
 {
   /* Node socket copy function based on bNodeTreeInterface::item_copy to avoid using blenkernel. */
-  dst.name = BLI_strdup_null(src.name);
-  dst.description = BLI_strdup_null(src.description);
+  dst.name_ = BLI_strdup_null(src.name_);
+  dst.description_ = BLI_strdup_null(src.description_);
   dst.socket_type = BLI_strdup(src.socket_type);
   dst.default_attribute_name = BLI_strdup_null(src.default_attribute_name);
   dst.identifier = identifier;

source/blender/blenloader/intern/versioning_500.cc
@@ -2949,8 +2949,8 @@ static void remove_in_and_out_node_panel_recursive(bNodeTreeInterfacePanel &pane
 
     bNodeTreeInterfaceSocket *new_output = MEM_new<bNodeTreeInterfaceSocket>(__func__);
     new_output->item.item_type = NodeTreeInterfaceItemType::Socket;
-    new_output->name = BLI_strdup_null(socket->name);
-    new_output->description = BLI_strdup_null(socket->description);
+    new_output->name_ = BLI_strdup_null(socket->name_);
+    new_output->description_ = BLI_strdup_null(socket->description_);
     new_output->socket_type = BLI_strdup_null(socket->socket_type);
     new_output->flag = socket->flag & ~NODE_INTERFACE_SOCKET_INPUT;
     new_output->attribute_domain = socket->attribute_domain;

source/blender/editors/animation/anim_ipo_utils.cc
@@ -183,7 +183,7 @@ std::optional<int> getname_anim_fcurve(char *name, ID *id, FCurve *fcu)
       if (const bNodeTreeInterfaceSocket *input = bke::node_find_interface_input_by_identifier(
               *nmd->node_group, propname))
       {
-        propname = input->name;
+        propname = input->name().c_str();
       }
     }
   }

source/blender/editors/interface/templates/interface_template_node_tree_interface.cc
@@ -91,7 +91,7 @@ class NodeSocketViewItem : public BasicTreeViewItem {
   NodeSocketViewItem(bNodeTree &nodetree,
                      bNodeTreeInterface &interface,
                      bNodeTreeInterfaceSocket &socket)
-      : BasicTreeViewItem(socket.name, ICON_NONE), nodetree_(nodetree), socket_(socket)
+      : BasicTreeViewItem(socket.name(), ICON_NONE), nodetree_(nodetree), socket_(socket)
   {
     set_is_active_fn([interface, &socket]() { return interface.active_item() == &socket.item; });
     set_on_activate_fn([&interface](bContext & /*C*/, BasicTreeViewItem &new_active) {
@@ -159,9 +159,9 @@ class NodeSocketViewItem : public BasicTreeViewItem {
   }
   bool rename(const bContext &C, StringRefNull new_name) override
   {
-    MEM_SAFE_DELETE(socket_.name);
+    MEM_SAFE_DELETE(socket_.name_);
 
-    socket_.name = BLI_strdup(new_name.c_str());
+    socket_.name_ = BLI_strdup(new_name.c_str());
     nodetree_.tree_interface.tag_item_property_changed();
     BKE_main_ensure_invariants(*CTX_data_main(&C), nodetree_.id);
     ED_undo_push(&const_cast<bContext &>(C), new_name.c_str());
@@ -169,7 +169,7 @@ class NodeSocketViewItem : public BasicTreeViewItem {
   }
   StringRef get_rename_string() const override
   {
-    return socket_.name;
+    return socket_.name();
   }
 
   void delete_item(bContext *C) override
@@ -195,7 +195,7 @@ class NodePanelViewItem : public BasicTreeViewItem {
   NodePanelViewItem(bNodeTree &nodetree,
                     bNodeTreeInterface &interface,
                     bNodeTreeInterfacePanel &panel)
-      : BasicTreeViewItem(panel.name, ICON_NONE), nodetree_(nodetree), panel_(panel)
+      : BasicTreeViewItem(panel.name(), ICON_NONE), nodetree_(nodetree), panel_(panel)
   {
     set_is_active_fn([interface, &panel]() { return interface.active_item() == &panel.item; });
     set_on_activate_fn([&interface](bContext & /*C*/, BasicTreeViewItem &new_active) {
@@ -279,7 +279,7 @@ class NodePanelViewItem : public BasicTreeViewItem {
   }
   StringRef get_rename_string() const override
   {
-    return panel_.name;
+    return panel_.name();
   }
 
   void delete_item(bContext *C) override

source/blender/editors/interface/templates/interface_template_scene_compositor_effects.cc
@@ -188,7 +188,7 @@ static void draw_property_for_socket(
     return;
   }
 
-  std::string name = socket.name ? IFACE_(socket.name) : "";
+  std::string name = socket.name();
 
   switch (type) {
     case SOCK_OBJECT: {

commit eb408203928190f1cc69228054d0de1bc997fd63

Fix #164123: Prevent crash when renaming panel toggle inputs
This fixes a crash that happens when renaming inputs inside a Geometry
Nodes panel with a Panel Toggle. The issue was introduced by 492a510260
when the node tree interface name accessors were changed.

The problem was in `update_panel_toggle_names()`. The old name of the
panel toggle socket was deleted, but the new name was being assigned
using `toggle_socket->name()`. Since `name()` returns a `StringRefNull`
instead of the actual `name_` field, the socket's name was not actually
updated. This left the socket name as `nullptr`, which could cause
Blender to crash during later interface updates.

The fix is to assign the new name directly to `toggle_socket->name_`.

Pull Request: https://projects.blender.org/blender/blender/pulls/164136

source/blender/blenkernel/intern/node_tree_update.cc
@@ -2224,7 +2224,7 @@ class NodeTreeMainUpdater {
       if (bNodeTreeInterfaceSocket *toggle_socket = panel->header_toggle_socket()) {
         if (!STREQ(panel->name_, toggle_socket->name_)) {
           MEM_SAFE_DELETE(toggle_socket->name_);
-          toggle_socket->name() = BLI_strdup_null(panel->name_);
+          toggle_socket->name_ = BLI_strdup_null(panel->name_);
           changed = true;
         }
       }