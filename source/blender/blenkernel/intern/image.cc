blender/blender

commit 165160de2d5b28ed177918ddf1cee8a6e0cbca8c

Refactor: Images: Move GPU textures from Image to ImBuf
This is a more natural fit and will make it possible to support multilayer
and multipass images. GPU compositor output was already stored as a GPU
texture on ImBuf, and this extends that to image textures for rendering and
the 3D viewport.

* For garbage collection, the last used time is now on the ImBuf instead
  of Image, separate for CPU and GPU access.
* Getting gpu::Texture* from an image now returns an owning reference,
  to ensure the texture stays alive and is not freed. A mutex was added to
  ImBuf to make this process fully atomic.
* The draw manager now holds a list of deferred textures that will be loaded
  in parallel, instead of MaterialModule doing it. Crucially, it is this list
  that gpu::Texture** bindings for such deferred textures point into, rather
  than pointing into Image or ImBuf that may be modified by another thread
  (see #158372).
* UDIM atlas and tile mapping are stored as GPU-only ImBufs in the image
  cache. This requires some exceptions in the code as these are not
  regular image buffers, but may be the best fit with this design until
  we can do e.g. bindless textures without an atlas.
* For image sequences and movies, as before a single GPU texture for the
  current frame is kept but the mechanism is different. Previously there
  was only a single pointer that needed to be cleared when the frame
  changed, now we free other frames from the image cache.
* Error logging on failure is improved, now mentioning the specific
  failed texture file path rather than a generic message.

Pull Request: https://projects.blender.org/blender/blender/pulls/159750

source/blender/blenkernel/intern/image.cc
@@ -100,7 +100,6 @@
 #include "SEQ_utils.hh" /* SEQ_get_topmost_sequence() */
 
 #include "GPU_material.hh"
-#include "GPU_texture.hh"
 
 #include "BLI_sys_types.hh" /* for intptr_t support */
 
@@ -115,6 +114,8 @@
 #include "DNA_node_types.h"
 #include "DNA_screen_types.h"
 #include "DNA_space_types.h"
+
+#include "image_intern.hh"
 
 namespace blender {
 
@@ -244,26 +245,6 @@ static void image_foreach_cache(ID *id,
 
   key.identifier = runtime_base_id + offsetof(bke::ImageRuntime, cache);
   function_callback(id, &key, reinterpret_cast<void **>(&image->runtime->cache), 0, user_data);
-
-  auto gputexture_offset = [image](int target, int eye) {
-    constexpr size_t base_offset = offsetof(bke::ImageRuntime, gputexture);
-    gpu::Texture **first = &image->runtime->gputexture[0][0];
-    const size_t array_offset = sizeof(*first) *
-                                (&image->runtime->gputexture[target][eye] - first);
-    return base_offset + array_offset;
-  };
-
-  for (int eye = 0; eye < 2; eye++) {
-    for (int a = 0; a < TEXTARGET_COUNT; a++) {
-      const gpu::Texture *texture = image->runtime->gputexture[a][eye];
-      if (texture == nullptr) {
-        continue;
-      }
-      key.identifier = runtime_base_id + gputexture_offset(a, eye);
-      function_callback(
-          id, &key, reinterpret_cast<void **>(&image->runtime->gputexture[a][eye]), 0, user_data);
-    }
-  }
 }
 
 static void image_foreach_texture_cache_path(BPathForeachPathData *bpath_data,
@@ -562,25 +543,11 @@ static ImBuf *image_acquire_ibuf(Image *ima,
 static void image_update_views_format(Image *ima, ImageUser *iuser);
 static void image_add_view(Image *ima, const char *viewname, const char *filepath);
 
-/* max int, to indicate we don't store sequences in ibuf */
-#define IMA_NO_INDEX 0x7FEFEFEF
-
-/* quick lookup: supports 1 million entries, thousand passes */
-#define IMA_MAKE_INDEX(entry, index) (((entry) << 10) + (index))
-#define IMA_INDEX_ENTRY(index) ((index) >> 10)
-#if 0
-#  define IMA_INDEX_PASS(index) (index & ~1023)
-#endif
-
 /** \} */
 
 /* -------------------------------------------------------------------- */
 /** \name Image Cache
  * \{ */
-
-struct ImageCacheKey {
-  int index;
-};
 
 static uint imagecache_hashhash(const void *key_v)
 {
@@ -605,10 +572,8 @@ static void imagecache_keydata(void *userkey, int *framenr, int *proxy, int *ren
   *render_flags = 0;
 }
 
-static void imagecache_put(Image *image, int index, ImBuf *ibuf)
+void imagecache_put(Image *image, ImageCacheKey key, ImBuf *ibuf)
 {
-  ImageCacheKey key;
-
   if (image->runtime->cache == nullptr) {
     // char cache_name[64];
     // SNPRINTF_UTF8(cache_name, "Image Datablock %s", image->id.name);
@@ -618,28 +583,30 @@ static void imagecache_put(Image *image, int index, ImBuf *ibuf)
     IMB_cache_set_getdata_callback(image->runtime->cache, imagecache_keydata);
   }
 
-  key.index = index;
+  if (ibuf != nullptr) {
+    ibuf->lastused = BLI_time_now_seconds_i();
+  }
 
   IMB_cache_put(image->runtime->cache, &key, ibuf);
 }
 
-static void imagecache_remove(Image *image, int index)
+static void imagecache_remove(Image *image, ImageCacheKey key)
 {
   if (image->runtime->cache == nullptr) {
     return;
   }
 
-  ImageCacheKey key;
-  key.index = index;
   IMB_cache_remove(image->runtime->cache, &key);
 }
 
-static ImBuf *imagecache_get(Image *image, int index, bool *r_is_cached_empty)
+static ImBuf *imagecache_get(Image *image, ImageCacheKey key, bool *r_is_cached_empty)
 {
   if (image->runtime->cache) {
-    ImageCacheKey key;
-    key.index = index;
-    return IMB_cache_get(image->runtime->cache, &key, r_is_cached_empty);
+    ImBuf *ibuf = IMB_cache_get(image->runtime->cache, &key, r_is_cached_empty);
+    if (ibuf != nullptr) {
+      ibuf->lastused = BLI_time_now_seconds_i();
+    }
+    return ibuf;
   }
 
   return nullptr;
@@ -740,8 +707,8 @@ void BKE_image_free_buffers(Image *ima)
 
 void BKE_image_free_old_buffers(Main *bmain)
 {
-  static int lasttime = 0;
-  int ctime = int(BLI_time_now_seconds());
+  static int64_t lasttime = 0;
+  const int64_t ctime = BLI_time_now_seconds_i();
 
   /*
    * Run garbage collector once for every collecting period of time
@@ -759,17 +726,54 @@ void BKE_image_free_old_buffers(Main *bmain)
   lasttime = ctime;
 
   for (Image &ima : bmain->images) {
-    if ((ima.flag & IMA_NOCOLLECT) == 0 && ctime - ima.runtime->lastused > U.textimeout) {
-      /* If it's in GPU memory, deallocate and set time tag to current time
-       * This gives CPU buffers a "second chance" to be used before dying. */
-      if (BKE_image_has_gpu_texture(&ima)) {
-        BKE_image_free_gputextures(&ima);
-        ima.runtime->lastused = ctime;
+    if ((ima.flag & IMA_NOCOLLECT) || ima.runtime->cache == nullptr) {
+      continue;
+    }
+
+    bool any_buffer_left = false;
+    {
+      std::scoped_lock lock(ima.runtime->cache_mutex);
+
+      /* Gather entries to remove. */
+      Vector<ImageCacheKey> to_remove;
+      ImBufCacheIter *iter = IMB_cacheIter_new(ima.runtime->cache);
+      while (!IMB_cacheIter_done(iter)) {
+        ImBuf *ibuf = IMB_cacheIter_getImBuf(iter);
+        if (ibuf != nullptr) {
+          /* GPU buffers: free when past timeout and image buffer is not used elsewhere. */
+          bool freed_gpu = false;
+          if (ibuf->gpu.texture != nullptr && ibuf->refcounter == 0 &&
+              (ctime - ibuf->gpu.lastused > U.textimeout))
+          {
+            IMB_free_gpu_textures(ibuf);
+            freed_gpu = true;
+          }
+
+          /* CPU buffers: free whole image buffer if past timeout, and image has not been
+           * edited and has not GPU buffers. When the GPU texture was just freed, keep the
+           * buffer for another more collection cycle to give it a chance to be reused. */
+          if (!freed_gpu && ibuf->gpu.texture == nullptr &&
+              (ibuf->userflags & IB_BITMAPDIRTY) == 0 && (ctime - ibuf->lastused > U.textimeout))
+          {
+            to_remove.append(*static_cast<const ImageCacheKey *>(IMB_cacheIter_getUserKey(iter)));
+          }
+          else {
+            any_buffer_left = true;
+          }
+        }
+        IMB_cacheIter_step(iter);
       }
-      /* Otherwise, just kill the CPU buffers */
-      else {
-        BKE_image_free_buffers(&ima);
+      IMB_cacheIter_free(iter);
+
+      /* Remove entries. */
+      for (const ImageCacheKey &key : to_remove) {
+        imagecache_remove(&ima, key);
       }
+    }
+
+    /* If there are no buffers, also close the movie reader handles. */
+    if (!any_buffer_left && !BLI_listbase_is_empty(&ima.anims)) {
+      image_free_anims(&ima);
     }
   }
 }
@@ -848,7 +852,8 @@ static ImBuf *image_get_cached_ibuf_for_index_entry(Image *ima,
     index = IMA_MAKE_INDEX(entry, index);
   }
 
-  return imagecache_get(ima, index, r_is_cached_empty);
+  ImageCacheKey key = {.index = index};
+  return imagecache_get(ima, key, r_is_cached_empty);
 }
 
 static void image_assign_ibuf(Image *ima, ImBuf *ibuf, int index, int entry)
@@ -857,7 +862,8 @@ static void image_assign_ibuf(Image *ima, ImBuf *ibuf, int index, int entry)
     index = IMA_MAKE_INDEX(entry, index);
   }
 
-  imagecache_put(ima, index, ibuf);
+  ImageCacheKey key = {.index = index};
+  imagecache_put(ima, key, ibuf);
 }
 
 static void image_remove_ibuf(Image *ima, int index, int entry)
@@ -865,7 +871,13 @@ static void image_remove_ibuf(Image *ima, int index, int entry)
   if (index != IMA_NO_INDEX) {
     index = IMA_MAKE_INDEX(entry, index);
   }
-  imagecache_remove(ima, index);
+  ImageCacheKey key = {.index = index};
+  imagecache_remove(ima, key);
+}
+
+static bool image_index_is_gpu_only(const int index)
+{
+  return ELEM(index, IMA_INDEX_UDIM_ATLAS, IMA_INDEX_UDIM_TILE_MAPPING);
 }
 
 static void copy_image_packedfiles(ListBaseT<ImagePackedFile> *lb_dst,
@@ -905,7 +917,7 @@ void BKE_image_merge(Main *bmain, Image *dest, Image *source)
         while (!IMB_cacheIter_done(iter)) {
           ImBuf *ibuf = IMB_cacheIter_getImBuf(iter);
           ImageCacheKey *key = static_cast<ImageCacheKey *>(IMB_cacheIter_getUserKey(iter));
-          imagecache_put(dest, key->index, ibuf);
+          imagecache_put(dest, *key, ibuf);
           IMB_cacheIter_step(iter);
         }
         IMB_cacheIter_free(iter);
@@ -1775,11 +1787,6 @@ void BKE_image_packfile_ensure(
   }
 }
 
-void BKE_image_tag_time(Image *ima)
-{
-  ima->runtime->lastused = BLI_time_now_seconds_i();
-}
-
 static uintptr_t image_mem_size(Image *image)
 {
   uintptr_t size = 0;
@@ -1796,8 +1803,10 @@ static uintptr_t image_mem_size(Image *image)
 
     while (!IMB_cacheIter_done(iter)) {
       ImBuf *ibuf = IMB_cacheIter_getImBuf(iter);
+      const ImageCacheKey *key = static_cast<const ImageCacheKey *>(
+          IMB_cacheIter_getUserKey(iter));
       IMB_cacheIter_step(iter);
-      if (ibuf == nullptr) {
+      if (ibuf == nullptr || image_index_is_gpu_only(key->index)) {
         continue;
       }
 
@@ -1885,25 +1894,21 @@ void BKE_image_free_all_textures(Main *bmain)
 #endif
 }
 
-static bool imagecache_check_free_anim(ImBuf *ibuf, void * /*userkey*/, void *userdata)
+static bool imagecache_check_free_anim(ImBuf *ibuf, void *userkey, void *userdata)
 {
   if (ibuf == nullptr) {
     return true;
   }
-  int except_frame = *static_cast<int *>(userdata);
-  return (ibuf->userflags & IB_BITMAPDIRTY) == 0 && (ibuf->index != IMA_NO_INDEX) &&
-         (except_frame != IMA_INDEX_ENTRY(ibuf->index));
-}
-
-void BKE_image_free_anim_ibufs(Image *ima, int except_frame)
-{
-  std::scoped_lock lock(ima->runtime->cache_mutex);
-  if (ima->runtime->cache != nullptr) {
-    IMB_cache_cleanup(ima->runtime->cache, imagecache_check_free_anim, &except_frame);
+  const ImageCacheKey *key = static_cast<const ImageCacheKey *>(userkey);
+  const int except_frame = *static_cast<int *>(userdata);
+  if (image_index_is_gpu_only(key->index)) {
+    return false;
   }
+  return (ibuf->userflags & IB_BITMAPDIRTY) == 0 && key->index != IMA_NO_INDEX &&
+         (except_frame != IMA_INDEX_ENTRY(key->index));
 }
 
-void BKE_image_all_free_anim_ibufs(Main *bmain, int cfra)
+void BKE_image_all_free_anim_ibufs(Main *bmain, int except_frame)
 {
   Image *ima;
 
@@ -1911,7 +1916,10 @@ void BKE_image_all_free_anim_ibufs(Main *bmain, int cfra)
        ima = static_cast<Image *>(ima->id.next))
   {
     if (BKE_image_is_animated(ima)) {
-      BKE_image_free_anim_ibufs(ima, cfra);
+      std::scoped_lock lock(ima->runtime->cache_mutex);
+      if (ima->runtime->cache != nullptr) {
+        IMB_cache_cleanup(ima->runtime->cache, imagecache_check_free_anim, &except_frame);
+      }
     }
   }
 }
@@ -3286,20 +3294,8 @@ void BKE_image_init_imageuser(Image *ima, ImageUser *iuser)
 
 static void image_free_tile(Image *ima, ImageTile *tile)
 {
-  for (int i = 0; i < TEXTARGET_COUNT; i++) {
-    /* Only two textures depends on all tiles, so if this is a secondary tile we can keep the other
-     * two. */
-    if (tile != ima->tiles.first && !ELEM(i, TEXTARGET_2D_ARRAY, TEXTARGET_TILE_MAPPING)) {
-      continue;
-    }
-
-    for (int eye = 0; eye < 2; eye++) {
-      if (ima->runtime->gputexture[i][eye] != nullptr) {
-        GPU_texture_free(ima->runtime->gputexture[i][eye]);
-        ima->runtime->gputexture[i][eye] = nullptr;
-      }
-    }
-  }
+  /* UDIM tiles are packed into an atlas for the GPU, so need to free all. */
+  BKE_image_free_gpu_udim_textures(ima);
   BKE_image_partial_update_mark_full_update(ima);
 
   if (BKE_image_is_multiview(ima)) {
@@ -3687,17 +3683,7 @@ ImageTile *BKE_image_add_tile(Image *ima, int tile_number, const char *label)
     STRNCPY_UTF8(tile->label, label);
   }
 
-  for (int eye = 0; eye < 2; eye++) {
-    /* Reallocate GPU tile array. */
-    if (ima->runtime->gputexture[TEXTARGET_2D_ARRAY][eye] != nullptr) {
-      GPU_texture_free(ima->runtime->gputexture[TEXTARGET_2D_ARRAY][eye]);
-      ima->runtime->gputexture[TEXTARGET_2D_ARRAY][eye] = nullptr;
-    }
-    if (ima->runtime->gputexture[TEXTARGET_TILE_MAPPING][eye] != nullptr) {
-      GPU_texture_free(ima->runtime->gputexture[TEXTARGET_TILE_MAPPING][eye]);
-      ima->runtime->gputexture[TEXTARGET_TILE_MAPPING][eye] = nullptr;
-    }
-  }
+  BKE_image_free_gpu_udim_textures(ima);
   BKE_image_partial_update_mark_full_update(ima);
 
   return tile;
@@ -3741,17 +3727,7 @@ void BKE_image_reassign_tile(Image *ima, ImageTile *tile, int new_tile_number)
     IMB_freeImBuf(ibuf);
   }
 
-  for (int eye = 0; eye < 2; eye++) {
-    /* Reallocate GPU tile array. */
-    if (ima->runtime->gputexture[TEXTARGET_2D_ARRAY][eye] != nullptr) {
-      GPU_texture_free(ima->runtime->gputexture[TEXTARGET_2D_ARRAY][eye]);
-      ima->runtime->gputexture[TEXTARGET_2D_ARRAY][eye] = nullptr;
-    }
-    if (ima->runtime->gputexture[TEXTARGET_TILE_MAPPING][eye] != nullptr) {
-      GPU_texture_free(ima->runtime->gputexture[TEXTARGET_TILE_MAPPING][eye]);
-      ima->runtime->gputexture[TEXTARGET_TILE_MAPPING][eye] = nullptr;
-    }
-  }
+  BKE_image_free_gpu_udim_textures(ima);
   BKE_image_partial_update_mark_full_update(ima);
 }
 
@@ -4207,9 +4183,6 @@ static void image_init_after_load(Image *ima, ImageUser *iuser, ImBuf * /*ibuf*/
     BKE_icon_changed(BKE_icon_id_ensure(&ima->id));
   }
 
-  /* timer */
-  BKE_image_tag_time(ima);
-
   ImageTile *tile = BKE_image_get_tile_from_iuser(ima, iuser);
   /* Images should never get loaded if the corresponding tile does not exist,
    * but we should at least not crash if it happens due to a bug elsewhere. */
@@ -4287,6 +4260,7 @@ static ImBuf *image_load_sequence_multilayer(Image *ima, ImageUser *iuser, int e
       copy_v2_v2_db(ibuf->ppm, ima->rr->ppm);
 
       image_init_after_load(ima, iuser, ibuf);
+      ibuf->fileframe = frame;
       image_assign_ibuf(ima, ibuf, iuser ? iuser->multi_index : 0, entry);
     }
     // else printf("pass not found\n");
@@ -4474,6 +4448,11 @@ static ImBuf *load_image_single(Image *ima,
     }
     else {
       image_init_after_load(ima, iuser, ibuf);
+
+      /* Remember which frame this sequence buffer holds, for per-frame cache management. */
+      if (is_sequence) {
+        ibuf->fileframe = cfra;
+      }
 
       /* Make packed file for auto-pack. */
       if (!is_sequence && (has_packed == false) && (G.fileflags & G_FILE_AUTOPACK)) {
@@ -5021,8 +5000,6 @@ static ImBuf *image_acquire_ibuf(Image *ima,
     IMB_ensure_host_buffer(ibuf);
   }
 
-  BKE_image_tag_time(ima);
-
   return ibuf;
 }
 
@@ -5390,16 +5367,6 @@ void BKE_image_user_frame_calc(Image *ima, ImageUser *iuser, int cfra)
       iuser->flag |= IMA_USER_FRAME_IN_RANGE;
     }
 
-    if (ima && ima->runtime->gpuframenr != iuser->framenr) {
-      /* NOTE: a single texture and refresh doesn't really work when
-       * multiple image users may use different frames, this is to
-       * be improved with perhaps a GPU texture cache. */
-      if (ima->runtime->gpuframenr != IMAGE_GPU_FRAME_NONE) {
-        BKE_image_partial_update_mark_full_update(ima);
-      }
-      ima->runtime->gpuframenr = iuser->framenr;
-    }
-
     iuser->flag &= ~IMA_NEED_FRAME_RECALC;
   }
 }
@@ -5749,7 +5716,9 @@ bool BKE_image_has_loaded_ibuf(Image *image)
 
     while (!IMB_cacheIter_done(iter)) {
       ImBuf *ibuf = IMB_cacheIter_getImBuf(iter);
-      if (ibuf != nullptr) {
+      const ImageCacheKey *key = static_cast<const ImageCacheKey *>(
+          IMB_cacheIter_getUserKey(iter));
+      if (ibuf != nullptr && !image_index_is_gpu_only(key->index)) {
         has_loaded_ibuf = true;
         break;
       }
@@ -5795,10 +5764,13 @@ ImBuf *BKE_image_get_first_ibuf(Image *image)
 
     while (!IMB_cacheIter_done(iter)) {
       ibuf = IMB_cacheIter_getImBuf(iter);
-      if (ibuf != nullptr) {
+      const ImageCacheKey *key = static_cast<const ImageCacheKey *>(
+          IMB_cacheIter_getUserKey(iter));
+      if (ibuf != nullptr && !image_index_is_gpu_only(key->index)) {
         IMB_refImBuf(ibuf);
+        break;
       }
-      break;
+      IMB_cacheIter_step(iter);
     }
     IMB_cacheIter_free(iter);
   }

source/blender/blenkernel/BKE_image.hh
@@ -56,12 +56,6 @@ enum eImbFileType : int8_t;
 #define IMA_MAX_SPACE 64
 #define IMA_UDIM_MAX 2000
 
-/* Image gpu runtime defaults */
-constexpr int IMAGE_GPU_FRAME_NONE = std::numeric_limits<int>::max();
-constexpr int IMAGE_GPU_PASS_NONE = std::numeric_limits<short>::max();
-constexpr int IMAGE_GPU_LAYER_NONE = std::numeric_limits<short>::max();
-constexpr int IMAGE_GPU_VIEW_NONE = std::numeric_limits<short>::max();
-
 namespace bke {
 
 struct ImageRuntime {
@@ -71,18 +65,6 @@ struct ImageRuntime {
 
   ImBufCache *cache = nullptr;
 
-  /* The 2 is for the left/right stereo eyes. */
-  gpu::Texture *gputexture[/*TEXTARGET_COUNT*/ 3][2] = {};
-
-  /* GPU texture flag. */
-  int gpuframenr = IMAGE_GPU_FRAME_NONE;
-  short gpuflag = 0;
-  short gpu_pass = IMAGE_GPU_PASS_NONE;
-  short gpu_layer = IMAGE_GPU_LAYER_NONE;
-  short gpu_view = IMAGE_GPU_VIEW_NONE;
-
-  int64_t lastused = 0;
-
   /** Register containing partial updates. */
   PartialUpdateRegister *partial_update_register = nullptr;
   /** Partial update user for gpu::Textures stored inside the Image. */
@@ -90,6 +72,9 @@ struct ImageRuntime {
 
   /* The image's current update count. See deg::set_id_update_count for more information. */
   uint64_t update_count = 0;
+
+  /* Log GPU load errors once per image. */
+  bool gpu_load_error_logged = false;
 
   float view_offset[2] = {};
   float view_zoom = 1.0f;
@@ -184,8 +169,6 @@ MovieReader *openanim_noload(const char *filepath,
                              int streamindex,
                              bool keep_original_colorspace,
                              char colorspace[IMA_MAX_SPACE]);
-
-void BKE_image_tag_time(Image *ima);
 
 /* ********************************** NEW IMAGE API *********************** */
 
@@ -413,15 +396,10 @@ void BKE_image_backup_render(Scene *scene, Image *ima, bool free_current_slot);
 void BKE_image_free_all_textures(Main *bmain);
 
 /**
- * Operates on one image only!
+ * Free cached frame buffers of all images with type MOVIE or SEQUENCE.
  * \param except_frame: This is weak, only works for sequences without offset.
  */
-void BKE_image_free_anim_ibufs(Image *ima, int except_frame);
-
-/**
- * Does all images with type MOVIE or SEQUENCE.
- */
-void BKE_image_all_free_anim_ibufs(Main *bmain, int cfra);
+void BKE_image_all_free_anim_ibufs(Main *bmain, int except_frame);
 
 /**
  * Time-based garbage collection for CPU and GPU image caches.

source/blender/blenkernel/BKE_image_gpu.hh
@@ -24,69 +24,50 @@ struct ImageUser;
 struct Main;
 
 /**
- * Not to be use directly.
+ * Acquire the #gpu::Texture for a given #Image, returning an owned reference
+ * that must be released with #GPU_texture_free
  */
-gpu::Texture *BKE_image_create_gpu_texture_from_ibuf(Image *image, ImBuf *ibuf);
-
-/**
- * Ensure that the cached GPU texture inside the image matches the pass, layer, and view of the
- * given image user, if not, invalidate the cache such that the next call to the GPU texture
- * retrieval functions such as BKE_image_get_gpu_texture updates the cache with an image that
- * matches the give image user.
- *
- * This is provided as a separate function and not implemented as part of the GPU texture retrieval
- * functions because the current cache system only allows a single pass, layer, and stereo view to
- * be cached, so possible frequent cache invalidation can have performance implications,
- * and making invalidation explicit by calling this function will help make that clear and pave the
- * way for a more complete cache system in the future.
- */
-void BKE_image_ensure_gpu_texture(Image *image, ImageUser *iuser);
-
-/**
- * Get the #gpu::Texture for a given `Image`.
- *
- *
- *
- * The requested GPU texture will be cached for subsequent calls, but only a single layer, pass,
- * and view can be cached at a time, so the cache should be invalidated in operators and RNA
- * callbacks that change the layer, pass, or view of the image to maintain a correct cache state.
- * However, in some cases, multiple layers, passes, or views might be needed at the same time, like
- * is the case for the compositor. This is currently not supported, so the caller should
- * ensure that the requested layer is indeed the cached one and invalidated the cached otherwise by
- * calling BKE_image_ensure_gpu_texture. This is a workaround until image can support a more
- * complete caching system.
- */
-gpu::Texture *BKE_image_get_gpu_texture(Image *image, ImageUser *iuser);
+gpu::Texture *BKE_image_acquire_gpu_texture(Image *image, ImageUser *iuser);
 
 /*
- * Like BKE_image_get_gpu_texture, but can also get render or compositing result.
+ * Like #BKE_image_acquire_gpu_texture, but can also get render or compositing result.
  */
-gpu::Texture *BKE_image_get_gpu_viewer_texture(Image *image, ImageUser *iuser);
+gpu::Texture *BKE_image_acquire_gpu_viewer_texture(Image *image, ImageUser *iuser);
 
 /*
- * Like BKE_image_get_gpu_viewer_texture, but the image buffer is provided explicitly.
+ * Like #BKE_image_acquire_gpu_viewer_texture, but the image buffer is provided explicitly.
  */
-gpu::Texture *BKE_image_get_gpu_viewer_texture(Image *image,
-                                               ImageUser *iuser,
-                                               ImBuf *image_buffer);
+gpu::Texture *BKE_image_acquire_gpu_viewer_texture(Image *image,
+                                                   ImageUser *iuser,
+                                                   ImBuf *image_buffer);
 
 /*
- * Like BKE_image_get_gpu_texture, but can also return array and tile mapping texture for UDIM
- * tiles as used in material shaders.
+ * Like #BKE_image_acquire_gpu_texture, but can also return a GPU array texture and tile mapping
+ * texture for UDIM tiles as used in material shaders. The caller must release the textures.
  */
 struct ImageGPUTextures {
-  gpu::Texture **texture;
-  gpu::Texture **tile_mapping;
+  gpu::Texture *texture = nullptr;
+  gpu::Texture *tile_mapping = nullptr;
+
+  /* True if the texture needs a tile mapping, set even if textures were not loaded
+   * yet and the pointers are null. */
+  bool need_tile_mapping = false;
 };
 
-ImageGPUTextures BKE_image_get_gpu_material_texture(Image *image,
-                                                    ImageUser *iuser,
-                                                    const bool use_tile_mapping);
-
-/* Same as BKE_image_get_gpu_material_texture but will not load the texture if it isn't already. */
-ImageGPUTextures BKE_image_get_gpu_material_texture_try(Image *image,
+ImageGPUTextures BKE_image_acquire_gpu_material_texture(Image *image,
                                                         ImageUser *iuser,
-                                                        const bool use_tile_mapping);
+                                                        const bool use_tile_mapping,
+                                                        const bool try_only);
+
+/* Return whether the material GPU texture of an image is already loaded, without creating it. */
+bool BKE_image_has_gpu_material_texture(Image *image,
+                                        ImageUser *iuser,
+                                        const bool use_tile_mapping);
+
+/* Ensure the material GPU texture of an image is created, without returning it. */
+void BKE_image_ensure_gpu_material_texture(Image *image,
+                                           ImageUser *iuser,
+                                           const bool use_tile_mapping);
 
 /**
  * Is the alpha of the `gpu::Texture` for a given image/ibuf premultiplied.
@@ -94,11 +75,24 @@ ImageGPUTextures BKE_image_get_gpu_material_texture_try(Image *image,
 bool BKE_image_has_gpu_texture_premultiplied_alpha(Image *image, ImBuf *ibuf);
 
 /**
+ * Put an externally created GPU texture in the image cache. Should not
+ * generally be used, currently needed for synthetic Image datablocks for
+ * studio lights.
+ */
+void BKE_image_assign_gpu_texture(Image *image, gpu::Texture *texture);
+
+/**
+ * Free the process global fallback image buffer.
+ */
+void BKE_image_free_gpu_fallback();
+
+/**
  * Check if image has an associated GPU texture.
  */
 bool BKE_image_has_gpu_texture(Image *ima);
 
 void BKE_image_free_gputextures(Image *ima);
+void BKE_image_free_gpu_udim_textures(Image *ima);
 void BKE_image_free_all_gputextures(Main *bmain);
 /**
  * Same as #BKE_image_free_all_gputextures but only free animated images.

source/blender/blenkernel/CMakeLists.txt
@@ -556,6 +556,7 @@ set(SRC
 
   intern/attribute_storage_access.hh
   intern/data_transfer_intern.hh
+  intern/image_intern.hh
   intern/lib_intern.hh
   intern/multires_inline.hh
   intern/multires_reshape.hh

source/blender/blenkernel/intern/image_intern.hh
@@ -1,0 +1,36 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup bke
+ *
+ * Internal interface between image code in blenkernel.
+ */
+
+#pragma once
+
+namespace blender {
+
+struct Image;
+struct ImBuf;
+
+/** Cache key for an image buffer within an #Image datablock. */
+struct ImageCacheKey {
+  int index;
+};
+
+/* Index to indicate we don't store sequences in ibuf. */
+#define IMA_NO_INDEX 0x7FEFEFEF
+/* Index for UDIM aggregate textures. */
+#define IMA_INDEX_UDIM_ATLAS 0x7FEFEFE0
+#define IMA_INDEX_UDIM_TILE_MAPPING 0x7FEFEFE1
+
+/* Encode and decode animation frame in index. */
+#define IMA_MAKE_INDEX(entry, index) (((entry) << 10) + (index))
+#define IMA_INDEX_ENTRY(index) ((index) >> 10)
+
+/** Insert image buffer into image cache. */
+void imagecache_put(Image *image, ImageCacheKey key, ImBuf *ibuf);
+
+}  // namespace blender

source/blender/draw/engines/eevee/eevee_instance.cc
@@ -459,7 +459,11 @@ void Instance::end_sync()
     loaded_shaders |= shaders.static_shaders_wait_ready(request_bits);
   }
 
-  materials.end_sync();
+  /* Reset temporal accumulation if new textures will be loaded this frame to avoid ghosting. */
+  if (is_viewport() && manager->has_deferred_textures()) {
+    sampling.reset();
+  }
+
   velocity.end_sync();
   volume.end_sync();  /* Needs to be before shadows. */
   shadows.end_sync(); /* Needs to be before lights. */

commit 38b093e6a15eb19f9289e344949d892fc8992347

Fix: Images: Multilayer EXR re-attempts failed image loads
Unlike other image sources, this didn't insert a null ibuf in the cache
to indicate failure to load.

Issue introduced in 165160de2d.

Pull Request: https://projects.blender.org/blender/blender/pulls/164212

source/blender/blenkernel/intern/image.cc
@@ -4262,6 +4262,11 @@ static ImBuf *image_load_sequence_multilayer(Image *ima, ImageUser *iuser, int e
     // else printf("pass not found\n");
   }
 
+  /* Cache null to indicate failed load. */
+  if (ibuf == nullptr && ima->rr == nullptr) {
+    image_assign_ibuf(ima, nullptr, iuser ? iuser->multi_index : 0, entry);
+  }
+
   return ibuf;
 }
 
@@ -4626,6 +4631,11 @@ static ImBuf *image_get_ibuf_multilayer(Image *ima, ImageUser *iuser)
 
       image_assign_ibuf(ima, ibuf, iuser ? iuser->multi_index : IMA_NO_INDEX, 0);
     }
+  }
+
+  /* Cache null to indicate failed load. */
+  if (ibuf == nullptr && ima->rr == nullptr) {
+    image_assign_ibuf(ima, nullptr, iuser ? iuser->multi_index : IMA_NO_INDEX, 0);
   }
 
   return ibuf;