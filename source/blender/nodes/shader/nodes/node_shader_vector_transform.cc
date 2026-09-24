blender/blender

commit 3bcf2d172c1f879561b600d02707617912d22c7d

EEVEE: Add Light Space support for the Vector Tranform node
It follows the same rules as the other Material Lighting nodes.

This also cleans up and applies a consistent ordering to each combined
permutation, following the enum order.

Pull Request: https://projects.blender.org/blender/blender/pulls/164214

source/blender/nodes/shader/nodes/node_shader_vector_transform.cc
@@ -18,6 +18,11 @@ namespace nodes::node_shader_vector_transform_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
+  const bNodeTree *ntree = b.tree_or_null();
+  const bool is_gpu_internal = ntree && (ntree->flag & NTREE_IS_GPU_SHADER_INTERNAL);
+
+  b.add_input<decl::Int>("LightIndex"_ustr).available(is_gpu_internal);
+
   b.add_input<decl::Vector>("Vector"_ustr)
       .default_value({0.5f, 0.5f, 0.5f})
       .min(-10000.0f)
@@ -49,87 +54,50 @@ static void node_shader_init_vect_transform(bNodeTree * /*ntree*/, bNode *node)
 
 static const char *get_gpufn_name_from_to(short from, short to, short vector_type)
 {
-  switch (from) {
-    case SHD_VECT_TRANSFORM_SPACE_OBJECT:
-      switch (to) {
-        case SHD_VECT_TRANSFORM_SPACE_OBJECT:
-          return nullptr;
-        case SHD_VECT_TRANSFORM_SPACE_WORLD:
-          switch (vector_type) {
-            case SHD_VECT_TRANSFORM_TYPE_NORMAL:
-              return "normal_transform_object_to_world";
-            case SHD_VECT_TRANSFORM_TYPE_VECTOR:
-              return "direction_transform_object_to_world";
-            case SHD_VECT_TRANSFORM_TYPE_POINT:
-              return "point_transform_object_to_world";
-          }
-          break;
-        case SHD_VECT_TRANSFORM_SPACE_CAMERA:
-          switch (vector_type) {
-            case SHD_VECT_TRANSFORM_TYPE_NORMAL:
-              return "normal_transform_object_to_view";
-            case SHD_VECT_TRANSFORM_TYPE_VECTOR:
-              return "direction_transform_object_to_view";
-            case SHD_VECT_TRANSFORM_TYPE_POINT:
-              return "point_transform_object_to_view";
-          }
-          break;
-      }
-      break;
-    case SHD_VECT_TRANSFORM_SPACE_WORLD:
-      switch (to) {
-        case SHD_VECT_TRANSFORM_SPACE_WORLD:
-          return nullptr;
-        case SHD_VECT_TRANSFORM_SPACE_CAMERA:
-          switch (vector_type) {
-            case SHD_VECT_TRANSFORM_TYPE_NORMAL:
-              return "normal_transform_world_to_view";
-            case SHD_VECT_TRANSFORM_TYPE_VECTOR:
-              return "direction_transform_world_to_view";
-            case SHD_VECT_TRANSFORM_TYPE_POINT:
-              return "point_transform_world_to_view";
-          }
-          break;
-        case SHD_VECT_TRANSFORM_SPACE_OBJECT:
-          switch (vector_type) {
-            case SHD_VECT_TRANSFORM_TYPE_NORMAL:
-              return "normal_transform_world_to_object";
-            case SHD_VECT_TRANSFORM_TYPE_VECTOR:
-              return "direction_transform_world_to_object";
-            case SHD_VECT_TRANSFORM_TYPE_POINT:
-              return "point_transform_world_to_object";
-          }
-          break;
-      }
-      break;
-    case SHD_VECT_TRANSFORM_SPACE_CAMERA:
-      switch (to) {
-        case SHD_VECT_TRANSFORM_SPACE_CAMERA:
-          return nullptr;
-        case SHD_VECT_TRANSFORM_SPACE_WORLD:
-          switch (vector_type) {
-            case SHD_VECT_TRANSFORM_TYPE_NORMAL:
-              return "normal_transform_view_to_world";
-            case SHD_VECT_TRANSFORM_TYPE_VECTOR:
-              return "direction_transform_view_to_world";
-            case SHD_VECT_TRANSFORM_TYPE_POINT:
-              return "point_transform_view_to_world";
-          }
-          break;
-        case SHD_VECT_TRANSFORM_SPACE_OBJECT:
-          switch (vector_type) {
-            case SHD_VECT_TRANSFORM_TYPE_NORMAL:
-              return "normal_transform_view_to_object";
-            case SHD_VECT_TRANSFORM_TYPE_VECTOR:
-              return "direction_transform_view_to_object";
-            case SHD_VECT_TRANSFORM_TYPE_POINT:
-              return "point_transform_view_to_object";
-          }
-          break;
-      }
-      break;
+  if (from == to) {
+    return nullptr;
   }
+
+#define SWITCH_VECTOR_TYPE(FROM, TO) \
+  switch (vector_type) { \
+    case SHD_VECT_TRANSFORM_TYPE_NORMAL: \
+      return "normal_transform_" FROM "_to_" TO; \
+    case SHD_VECT_TRANSFORM_TYPE_VECTOR: \
+      return "direction_transform_" FROM "_to_" TO; \
+    case SHD_VECT_TRANSFORM_TYPE_POINT: \
+      return "point_transform_" FROM "_to_" TO; \
+  } \
+  break;
+
+#define SWITCH_SPACE_TYPE(FROM) \
+  switch (to) { \
+    case SHD_VECT_TRANSFORM_SPACE_WORLD: \
+      SWITCH_VECTOR_TYPE(FROM, "world"); \
+    case SHD_VECT_TRANSFORM_SPACE_OBJECT: \
+      SWITCH_VECTOR_TYPE(FROM, "object"); \
+    case SHD_VECT_TRANSFORM_SPACE_CAMERA: \
+      SWITCH_VECTOR_TYPE(FROM, "camera"); \
+    case SHD_VECT_TRANSFORM_SPACE_LIGHT: \
+      SWITCH_VECTOR_TYPE(FROM, "light"); \
+  } \
+  break;
+
+  switch (from) {
+    case SHD_VECT_TRANSFORM_SPACE_WORLD:
+      SWITCH_SPACE_TYPE("world");
+    case SHD_VECT_TRANSFORM_SPACE_OBJECT:
+      SWITCH_SPACE_TYPE("object");
+    case SHD_VECT_TRANSFORM_SPACE_CAMERA:
+      SWITCH_SPACE_TYPE("camera");
+    case SHD_VECT_TRANSFORM_SPACE_LIGHT:
+      SWITCH_SPACE_TYPE("light");
+  }
+
+  BLI_assert_unreachable();
   return nullptr;
+
+#undef SWITCH_VECTOR_SPACE
+#undef SWITCH_VECTOR_TYPE
 }
 
 static int gpu_shader_vect_transform(GPUMaterial *mat,
@@ -142,11 +110,18 @@ static int gpu_shader_vect_transform(GPUMaterial *mat,
 
   NodeShaderVectTransform *nodeprop = static_cast<NodeShaderVectTransform *>(node->storage);
 
-  if (in[0].hasinput) {
-    inputlink = in[0].link;
+  const bool has_light_space = ELEM(
+      SHD_VECT_TRANSFORM_SPACE_LIGHT, nodeprop->convert_from, nodeprop->convert_to);
+  if (has_light_space) {
+    /* Error: The node is not linked to a light accumulation node. */
+    BLI_assert(in[0].link);
+  }
+
+  if (in[1].hasinput) {
+    inputlink = in[1].link;
   }
   else {
-    inputlink = GPU_uniform(in[0]);
+    inputlink = GPU_uniform(in[1]);
   }
 
   const char *func_name = get_gpufn_name_from_to(
@@ -161,7 +136,18 @@ static int gpu_shader_vect_transform(GPUMaterial *mat,
       GPU_link(mat, "invert_z", inputlink, &inputlink);
     }
 
-    GPU_link(mat, func_name, inputlink, GPU_kernel_globals(), GPU_shading_data(), &out[0].link);
+    if (has_light_space) {
+      GPU_link(mat,
+               func_name,
+               in[0].link,
+               inputlink,
+               GPU_kernel_globals(),
+               GPU_shading_data(),
+               &out[0].link);
+    }
+    else {
+      GPU_link(mat, func_name, inputlink, GPU_kernel_globals(), GPU_shading_data(), &out[0].link);
+    }
 
     if (nodeprop->convert_to == SHD_VECT_TRANSFORM_SPACE_CAMERA &&
         nodeprop->convert_from != SHD_VECT_TRANSFORM_SPACE_CAMERA)
@@ -198,7 +184,7 @@ NODE_SHADER_MATERIALX_BEGIN
       fromspace = "object";
       break;
     default:
-      /* NOTE: SHD_VECT_TRANSFORM_SPACE_CAMERA don't have an implementation in MaterialX. */
+      /* NOTE: SHD_VECT_TRANSFORM_SPACE_CAMERA/LIGHT don't have an implementation in MaterialX. */
       BLI_assert_unreachable();
       return vector;
   }
@@ -211,7 +197,7 @@ NODE_SHADER_MATERIALX_BEGIN
       tospace = "object";
       break;
     default:
-      /* NOTE: SHD_VECT_TRANSFORM_SPACE_CAMERA don't have an implementation in MaterialX. */
+      /* NOTE: SHD_VECT_TRANSFORM_SPACE_CAMERA/LIGHT don't have an implementation in MaterialX. */
       BLI_assert_unreachable();
       return vector;
   }
@@ -261,6 +247,7 @@ void register_node_type_sh_vect_transform()
   ntype.initfunc = file_ns::node_shader_init_vect_transform;
   bke::node_type_storage(
       ntype, "NodeShaderVectTransform", node_free_standard_storage, node_copy_standard_storage);
+  ntype.gather_link_search_ops = search_link_ops_for_shader_material_lighting_node;
   ntype.gpu_fn = file_ns::gpu_shader_vect_transform;
   ntype.materialx_fn = file_ns::node_shader_materialx;
 

source/blender/blenkernel/intern/node_tree_update.cc
@@ -1558,6 +1558,13 @@ class NodeTreeMainUpdater {
           break;
         }
         ATTR_FALLTHROUGH;
+      case SH_NODE_VECT_TRANSFORM: {
+        NodeShaderVectTransform *nodeprop = static_cast<NodeShaderVectTransform *>(node.storage);
+        if (!ELEM(SHD_VECT_TRANSFORM_SPACE_LIGHT, nodeprop->convert_from, nodeprop->convert_to)) {
+          break;
+        }
+      }
+        ATTR_FALLTHROUGH;
       case SH_NODE_LIGHT_INFO:
       case SH_NODE_LIGHT_EVALUATION:
       case SH_NODE_SHADOW_RAYCAST:

source/blender/draw/engines/eevee/shaders/eevee_nodetree_lib.bsl.hh
@@ -186,6 +186,13 @@ struct KernelGlobals {
     [[resource_table]] const draw::View &views = this->view;
     return views.get(view_id_get(sd));
   }
+
+  ObjectMatrices light_matrices_get(int light_index)
+  {
+    [[resource_table]] const draw::Model &models = this->model;
+    [[resource_table]] const eevee::LightRenderData &lrds = this->lrd;
+    return models.get(lrds.light_buf[light_index].resource_id);
+  }
 };
 
 void closure_weights_reset([[resource_table]] KernelGlobals &kg,

source/blender/gpu/shaders/material/gpu_shader_material_interface.bsl.hh
@@ -117,6 +117,11 @@ struct KernelGlobals {
   {
     return {};
   }
+
+  ObjectMatrices light_matrices_get(int /*light_index*/)
+  {
+    return {};
+  }
 };
 
 /* Closure Nodes. */

source/blender/gpu/shaders/material/gpu_shader_material_transform_utils.bsl.hh
@@ -9,17 +9,6 @@
 #include "gpu_shader_material_interface.bsl.hh"
 
 [[node]]
-void normal_transform_object_to_world(float3 vin,
-                                      [[resource_table]] KernelGlobals &kg,
-                                      const ShadingData &sd,
-                                      float3 &vout)
-{
-  const ObjectMatrices obj = kg.object_matrices_get(sd);
-  /* Expansion of NormalMatrix. */
-  vout = vin * to_float3x3(obj.model_inverse);
-}
-
-[[node]]
 void normal_transform_world_to_object(float3 vin,
                                       [[resource_table]] KernelGlobals &kg,
                                       const ShadingData &sd,
@@ -28,6 +17,39 @@ void normal_transform_world_to_object(float3 vin,
   const ObjectMatrices obj = kg.object_matrices_get(sd);
   /* Expansion of NormalMatrixInverse. */
   vout = vin * to_float3x3(obj.model);
+}
+
+[[node]]
+void normal_transform_world_to_view(float3 vin,
+                                    [[resource_table]] KernelGlobals &kg,
+                                    const ShadingData &sd,
+                                    float3 &vout)
+{
+  const ViewMatrices view = kg.view_matrices_get(sd);
+  vout = to_float3x3(view.viewmat) * vin;
+}
+
+[[node]]
+void normal_transform_world_to_light(float light_index,
+                                     float3 vin,
+                                     [[resource_table]] KernelGlobals &kg,
+                                     const ShadingData & /*sd*/,
+                                     float3 &vout)
+{
+  const ObjectMatrices obj = kg.light_matrices_get(int(light_index));
+  /* Expansion of NormalMatrixInverse. */
+  vout = vin * to_float3x3(obj.model);
+}
+
+[[node]]
+void normal_transform_object_to_world(float3 vin,
+                                      [[resource_table]] KernelGlobals &kg,
+                                      const ShadingData &sd,
+                                      float3 &vout)
+{
+  const ObjectMatrices obj = kg.object_matrices_get(sd);
+  /* Expansion of NormalMatrix. */
+  vout = vin * to_float3x3(obj.model_inverse);
 }
 
 [[node]]
@@ -40,6 +62,17 @@ void normal_transform_object_to_view(float3 vin,
   const ViewMatrices view = kg.view_matrices_get(sd);
   vout = vin * to_float3x3(obj.model_inverse);
   vout = to_float3x3(view.viewmat) * vout;
+}
+
+[[node]]
+void normal_transform_object_to_light(float light_index,
+                                      float3 vin,
+                                      [[resource_table]] KernelGlobals &kg,
+                                      const ShadingData &sd,
+                                      float3 &vout)
+{
+  normal_transform_object_to_world(vin, kg, sd, vout);
+  normal_transform_world_to_light(light_index, vout, kg, sd, vout);
 }
 
 [[node]]
@@ -65,13 +98,79 @@ void normal_transform_view_to_object(float3 vin,
 }
 
 [[node]]
-void normal_transform_world_to_view(float3 vin,
+void normal_transform_view_to_light(float light_index,
+                                    float3 vin,
                                     [[resource_table]] KernelGlobals &kg,
                                     const ShadingData &sd,
                                     float3 &vout)
 {
+  normal_transform_view_to_world(vin, kg, sd, vout);
+  normal_transform_world_to_light(light_index, vout, kg, sd, vout);
+}
+
+[[node]]
+void normal_transform_light_to_world(float light_index,
+                                     float3 vin,
+                                     [[resource_table]] KernelGlobals &kg,
+                                     const ShadingData & /*sd*/,
+                                     float3 &vout)
+{
+  const ObjectMatrices obj = kg.light_matrices_get(int(light_index));
+  /* Expansion of NormalMatrix. */
+  vout = vin * to_float3x3(obj.model_inverse);
+}
+
+[[node]]
+void normal_transform_light_to_object(float light_index,
+                                      float3 vin,
+                                      [[resource_table]] KernelGlobals &kg,
+                                      const ShadingData &sd,
+                                      float3 &vout)
+{
+  normal_transform_light_to_world(light_index, vin, kg, sd, vout);
+  normal_transform_world_to_object(vout, kg, sd, vout);
+}
+
+[[node]]
+void normal_transform_light_to_view(float light_index,
+                                    float3 vin,
+                                    [[resource_table]] KernelGlobals &kg,
+                                    const ShadingData &sd,
+                                    float3 &vout)
+{
+  normal_transform_light_to_world(light_index, vin, kg, sd, vout);
+  normal_transform_world_to_view(vout, kg, sd, vout);
+}
+
+[[node]]
+void direction_transform_world_to_object(float3 vin,
+                                         [[resource_table]] KernelGlobals &kg,
+                                         const ShadingData &sd,
+                                         float3 &vout)
+{
+  const ObjectMatrices obj = kg.object_matrices_get(sd);
+  vout = to_float3x3(obj.model_inverse) * vin;
+}
+
+[[node]]
+void direction_transform_world_to_view(float3 vin,
+                                       [[resource_table]] KernelGlobals &kg,
+                                       const ShadingData &sd,
+                                       float3 &vout)
+{
   const ViewMatrices view = kg.view_matrices_get(sd);
   vout = to_float3x3(view.viewmat) * vin;
+}
+
+[[node]]
+void direction_transform_world_to_light(float light_index,
+                                        float3 vin,
+                                        [[resource_table]] KernelGlobals &kg,
+                                        const ShadingData & /*sd*/,
+                                        float3 &vout)
+{
+  const ObjectMatrices obj = kg.light_matrices_get(int(light_index));
+  vout = to_float3x3(obj.model_inverse) * vin;
 }
 
 [[node]]
@@ -97,6 +196,17 @@ void direction_transform_object_to_view(float3 vin,
 }
 
 [[node]]
+void direction_transform_object_to_light(float light_index,
+                                         float3 vin,
+                                         [[resource_table]] KernelGlobals &kg,
+                                         const ShadingData &sd,
+                                         float3 &vout)
+{
+  direction_transform_object_to_world(vin, kg, sd, vout);
+  direction_transform_world_to_light(light_index, vout, kg, sd, vout);
+}
+
+[[node]]
 void direction_transform_view_to_world(float3 vin,
                                        [[resource_table]] KernelGlobals &kg,
                                        const ShadingData &sd,
@@ -119,23 +229,78 @@ void direction_transform_view_to_object(float3 vin,
 }
 
 [[node]]
-void direction_transform_world_to_view(float3 vin,
+void direction_transform_view_to_light(float light_index,
+                                       float3 vin,
                                        [[resource_table]] KernelGlobals &kg,
                                        const ShadingData &sd,
                                        float3 &vout)
 {
-  const ViewMatrices view = kg.view_matrices_get(sd);
-  vout = to_float3x3(view.viewmat) * vin;
+  direction_transform_view_to_world(vin, kg, sd, vout);
+  direction_transform_world_to_light(light_index, vout, kg, sd, vout);
 }
 
 [[node]]
-void direction_transform_world_to_object(float3 vin,
+void direction_transform_light_to_world(float light_index,
+                                        float3 vin,
+                                        [[resource_table]] KernelGlobals &kg,
+                                        const ShadingData & /*sd*/,
+                                        float3 &vout)
+{
+  const ObjectMatrices obj = kg.light_matrices_get(int(light_index));
+  vout = to_float3x3(obj.model) * vin;
+}
+
+[[node]]
+void direction_transform_light_to_object(float light_index,
+                                         float3 vin,
                                          [[resource_table]] KernelGlobals &kg,
                                          const ShadingData &sd,
                                          float3 &vout)
 {
+  direction_transform_light_to_world(light_index, vin, kg, sd, vout);
+  direction_transform_world_to_object(vout, kg, sd, vout);
+}
+
+[[node]]
+void direction_transform_light_to_view(float light_index,
+                                       float3 vin,
+                                       [[resource_table]] KernelGlobals &kg,
+                                       const ShadingData &sd,
+                                       float3 &vout)
+{
+  direction_transform_light_to_world(light_index, vin, kg, sd, vout);
+  direction_transform_world_to_view(vout, kg, sd, vout);
+}
+
+[[node]]
+void point_transform_world_to_object(float3 vin,
+                                     [[resource_table]] KernelGlobals &kg,
+                                     const ShadingData &sd,
+                                     float3 &vout)
+{
   const ObjectMatrices obj = kg.object_matrices_get(sd);
-  vout = to_float3x3(obj.model_inverse) * vin;
+  vout = (obj.model_inverse * float4(vin, 1.0f)).xyz;
+}
+
+[[node]]
+void point_transform_world_to_view(float3 vin,
+                                   [[resource_table]] KernelGlobals &kg,
+                                   const ShadingData &sd,
+                                   float3 &vout)
+{
+  const ViewMatrices view = kg.view_matrices_get(sd);
+  vout = (view.viewmat * float4(vin, 1.0f)).xyz;
+}
+
+[[node]]
+void point_transform_world_to_light(float light_index,
+                                    float3 vin,
+                                    [[resource_table]] KernelGlobals &kg,
+                                    const ShadingData & /*sd*/,
+                                    float3 &vout)
+{
+  const ObjectMatrices obj = kg.light_matrices_get(int(light_index));
+  vout = (obj.model_inverse * float4(vin, 1.0f)).xyz;
 }
 
 [[node]]
@@ -160,6 +325,17 @@ void point_transform_object_to_view(float3 vin,
 }
 
 [[node]]
+void point_transform_object_to_light(float light_index,
+                                     float3 vin,
+                                     [[resource_table]] KernelGlobals &kg,
+                                     const ShadingData &sd,
+                                     float3 &vout)
+{
+  point_transform_object_to_world(vin, kg, sd, vout);
+  point_transform_world_to_light(light_index, vout, kg, sd, vout);
+}
+
+[[node]]
 void point_transform_view_to_world(float3 vin,
                                    [[resource_table]] KernelGlobals &kg,
                                    const ShadingData &sd,
@@ -181,21 +357,45 @@ void point_transform_view_to_object(float3 vin,
 }
 
 [[node]]
-void point_transform_world_to_view(float3 vin,
+void point_transform_view_to_light(float light_index,
+                                   float3 vin,
                                    [[resource_table]] KernelGlobals &kg,
                                    const ShadingData &sd,
                                    float3 &vout)
 {
-  const ViewMatrices view = kg.view_matrices_get(sd);
-  vout = (view.viewmat * float4(vin, 1.0f)).xyz;
+  point_transform_view_to_world(vin, kg, sd, vout);
+  point_transform_world_to_light(light_index, vout, kg, sd, vout);
 }
 
 [[node]]
-void point_transform_world_to_object(float3 vin,
+void point_transform_light_to_world(float light_index,
+                                    float3 vin,
+                                    [[resource_table]] KernelGlobals &kg,
+                                    const ShadingData & /*sd*/,
+                                    float3 &vout)
+{
+  const ObjectMatrices obj = kg.light_matrices_get(int(light_index));
+  vout = (obj.model * float4(vin, 1.0f)).xyz;
+}
+
+[[node]]
+void point_transform_light_to_object(float light_index,
+                                     float3 vin,
                                      [[resource_table]] KernelGlobals &kg,
                                      const ShadingData &sd,
                                      float3 &vout)
 {
-  const ObjectMatrices obj = kg.object_matrices_get(sd);
-  vout = (obj.model_inverse * float4(vin, 1.0f)).xyz;
+  point_transform_light_to_world(light_index, vin, kg, sd, vout);
+  point_transform_world_to_object(vout, kg, sd, vout);
+}
+
+[[node]]
+void point_transform_light_to_view(float light_index,
+                                   float3 vin,
+                                   [[resource_table]] KernelGlobals &kg,
+                                   const ShadingData &sd,
+                                   float3 &vout)
+{
+  point_transform_light_to_world(light_index, vin, kg, sd, vout);
+  point_transform_world_to_view(vout, kg, sd, vout);
 }

source/blender/makesdna/DNA_node_types.h
@@ -501,6 +501,7 @@ enum eNodeVectorTransform_Space : short {
   SHD_VECT_TRANSFORM_SPACE_WORLD = 0,
   SHD_VECT_TRANSFORM_SPACE_OBJECT = 1,
   SHD_VECT_TRANSFORM_SPACE_CAMERA = 2,
+  SHD_VECT_TRANSFORM_SPACE_LIGHT = 3,
 };
 
 /** #NodeShaderAttribute.type */

source/blender/makesrna/intern/rna_nodetree.cc
@@ -649,6 +649,14 @@ static const EnumPropertyItem rna_enum_shader_attribute_type_items[] = {
     {0, nullptr, 0, nullptr, nullptr},
 };
 
+static const EnumPropertyItem rna_enum_shader_vect_transform_space_items[] = {
+    {SHD_VECT_TRANSFORM_SPACE_WORLD, "WORLD", 0, "World", ""},
+    {SHD_VECT_TRANSFORM_SPACE_OBJECT, "OBJECT", 0, "Object", ""},
+    {SHD_VECT_TRANSFORM_SPACE_CAMERA, "CAMERA", 0, "Camera", ""},
+    {SHD_VECT_TRANSFORM_SPACE_LIGHT, "LIGHT", 0, "Light", ""},
+    {0, nullptr, 0, nullptr, nullptr},
+};
+
 #ifndef RNA_RUNTIME
 
 static const EnumPropertyItem prop_shader_output_target_items[] = {
@@ -4644,6 +4652,32 @@ static const EnumPropertyItem *rna_NodeShaderAttribute_type_itemf(bContext *C,
       });
 }
 
+static const EnumPropertyItem *rna_NodeShaderVectTransform_space_itemf(bContext *C,
+                                                                       PointerRNA * /*ptr*/,
+                                                                       PropertyRNA * /*prop*/,
+                                                                       bool *r_free)
+{
+  if (C == nullptr) {
+    return rna_enum_shader_vect_transform_space_items;
+  }
+
+  Scene *scene = CTX_data_scene(C);
+  SpaceNode *space_node = CTX_wm_space_node(C);
+
+  if (scene == nullptr || space_node == nullptr) {
+    return rna_enum_shader_vect_transform_space_items;
+  }
+
+  *r_free = true;
+
+  bool supports_light_transform = !STREQ(CTX_data_scene(C)->r.engine, RE_engine_id_CYCLES) &&
+                                  CTX_wm_space_node(C)->shaderfrom == SNODE_SHADER_OBJECT;
+  return itemf_function_check(
+      rna_enum_shader_vect_transform_space_items, [&](const EnumPropertyItem *item) {
+        return supports_light_transform || item->value != SHD_VECT_TRANSFORM_SPACE_LIGHT;
+      });
+}
+
 }  // namespace blender
 
 #else
@@ -6234,13 +6268,6 @@ static void def_sh_vect_transform(BlenderRNA * /*brna*/, StructRNA *srna)
       {0, nullptr, 0, nullptr, nullptr},
   };
 
-  static const EnumPropertyItem prop_vect_space_items[] = {
-      {SHD_VECT_TRANSFORM_SPACE_WORLD, "WORLD", 0, "World", ""},
-      {SHD_VECT_TRANSFORM_SPACE_OBJECT, "OBJECT", 0, "Object", ""},
-      {SHD_VECT_TRANSFORM_SPACE_CAMERA, "CAMERA", 0, "Camera", ""},
-      {0, nullptr, 0, nullptr, nullptr},
-  };
-
   PropertyRNA *prop;
 
   RNA_def_struct_sdna_from(srna, "NodeShaderVectTransform", "storage");
@@ -6252,12 +6279,14 @@ static void def_sh_vect_transform(BlenderRNA * /*brna*/, StructRNA *srna)
   RNA_def_property_update(prop, 0, "rna_Node_update");
 
   prop = RNA_def_property(srna, "convert_from", PROP_ENUM, PROP_NONE);
-  RNA_def_property_enum_items(prop, prop_vect_space_items);
+  RNA_def_property_enum_items(prop, rna_enum_shader_vect_transform_space_items);
+  RNA_def_property_enum_funcs(prop, nullptr, nullptr, "rna_NodeShaderVectTransform_space_itemf");
   RNA_def_property_ui_text(prop, "Convert From", "Space to convert from");
   RNA_def_property_update(prop, 0, "rna_Node_update");
 
   prop = RNA_def_property(srna, "convert_to", PROP_ENUM, PROP_NONE);
-  RNA_def_property_enum_items(prop, prop_vect_space_items);
+  RNA_def_property_enum_items(prop, rna_enum_shader_vect_transform_space_items);
+  RNA_def_property_enum_funcs(prop, nullptr, nullptr, "rna_NodeShaderVectTransform_space_itemf");
   RNA_def_property_ui_text(prop, "Convert To", "Space to convert to");
   RNA_def_property_update(prop, 0, "rna_Node_update");
 }

source/blender/nodes/shader/node_shader_tree.cc
@@ -1011,7 +1011,11 @@ static void ntree_shader_setup_custom_lighting_zone(bNodeTree *ntree)
              SH_NODE_LIGHT_EVALUATION,
              SH_NODE_SHADOW_RAYCAST) ||
         (node.type_legacy == SH_NODE_ATTRIBUTE &&
-         static_cast<NodeShaderAttribute *>(node.storage)->type == SHD_ATTRIBUTE_LIGHT))
+         static_cast<NodeShaderAttribute *>(node.storage)->type == SHD_ATTRIBUTE_LIGHT) ||
+        (node.type_legacy == SH_NODE_VECT_TRANSFORM &&
+         ELEM(SHD_VECT_TRANSFORM_SPACE_LIGHT,
+              static_cast<NodeShaderVectTransform *>(node.storage)->convert_from,
+              static_cast<NodeShaderVectTransform *>(node.storage)->convert_to)))
     {
       ensure_nodes();
       /* Connect LightIndex socket */

commit 180be4ab76fbe81a9e65227b0c99b80a0623b877

Fix: EEVEE: Vector Transform functions name mismatch
Regression from 3bcf2d172c1f879561b600d02707617912d22c7d.

C++ code uses "camera" for the space name, but shader code use "view",
causing a missing function error.

source/blender/nodes/shader/nodes/node_shader_vector_transform.cc
@@ -76,7 +76,7 @@ static const char *get_gpufn_name_from_to(short from, short to, short vector_typ
     case SHD_VECT_TRANSFORM_SPACE_OBJECT: \
       SWITCH_VECTOR_TYPE(FROM, "object"); \
     case SHD_VECT_TRANSFORM_SPACE_CAMERA: \
-      SWITCH_VECTOR_TYPE(FROM, "camera"); \
+      SWITCH_VECTOR_TYPE(FROM, "view"); \
     case SHD_VECT_TRANSFORM_SPACE_LIGHT: \
       SWITCH_VECTOR_TYPE(FROM, "light"); \
   } \
@@ -88,7 +88,7 @@ static const char *get_gpufn_name_from_to(short from, short to, short vector_typ
     case SHD_VECT_TRANSFORM_SPACE_OBJECT:
       SWITCH_SPACE_TYPE("object");
     case SHD_VECT_TRANSFORM_SPACE_CAMERA:
-      SWITCH_SPACE_TYPE("camera");
+      SWITCH_SPACE_TYPE("view");
     case SHD_VECT_TRANSFORM_SPACE_LIGHT:
       SWITCH_SPACE_TYPE("light");
   }