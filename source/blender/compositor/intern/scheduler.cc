32aefe9d3bee570588e36e16ff7655e71ac6ed25
Omar Emara <mail@OmarEmara.dev>
2026-09-25 12:32:00 +0200

    Fix: Compositor lazy evaluation does work not across zones
    The compositor lazy evaluation mechanism does not work across zones,
    this is intentionally not yet supported, though the scheduler still
    tries to make use of it, so we need to explicitly disable it for links
    that cross the boundary of zones.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/164324

--- source/blender/compositor/intern/scheduler.cc
+++ source/blender/compositor/intern/scheduler.cc
@@ -227,6 +227,14 @@ static bool is_input_needed(const Context &context,
                             const bNodeSocket &input,
                             SocketResultFn socket_result_fn)
 {
+  /* Lazy evaluation of inputs that reference outputs outside of their own zone are currently not
+   * supported. */
+  const bke::bNodeTreeZones &zones = *node.owner_tree().zones();
+  const bNodeSocket *output = get_output_linked_to_input(input);
+  if (output && zones.get_zone_by_socket(input) != zones.get_zone_by_socket(*output)) {
+    return true;
+  }
+
   if (node.is_group_output()) {
     const Result *result = socket_result_fn(input);
     if (!result) {
